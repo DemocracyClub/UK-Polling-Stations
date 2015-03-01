b2c31e5253b9e50fc12199d5819b37990e4b7402
Sym Roe <sym.roe@talusdesign.co.uk>
2015-03-01 18:57:48 +0000

    cache postcode URL to save API lookups

--- polling_stations/apps/data_finder/urls.py
+++ polling_stations/apps/data_finder/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.views.decorators.cache import cache_page
+from django.views.decorators.cache import cache_page
 
 from constituencies.views import ConstituencyList, ConstituencyView
 
@@ -9,7 +10,7 @@ urlpatterns = patterns(
     url(r'^/$',  cache_page(60*60)(ConstituencyList.as_view()), name='constituencies'),
     # url(r'^/notspots/', view_not_spots, name='constituency_notspots'),
     url(r'^/(?P<pk>[^/]+)(?:/(?P<ignored_slug>.*))?$',
-            ConstituencyView.as_view(),
+            cache_page(60 * 60 * 24)(ConstituencyView.as_view()),
             name='constituency-view'),
 )
 
