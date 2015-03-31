9eec84438c8f9ea7510f613f62feec4393b4c6f8
David Miller <david@deadpansincerity.com>
2015-03-31 13:19:09 +0100

    Serialization of Point/Polygon fields begins.
    Polygons are slow. Maybe we want caching, maybe we want to only have
    /<council>/polingdistricts ?
    
    Disabled for now because soooo slooooow.

--- polling_stations/urls.py
+++ polling_stations/urls.py
@@ -12,30 +12,71 @@ from data_finder.views import HomeView, CouncilView, PostcodeView
 from django.contrib import admin
 admin.autodiscover()
 
+# Begin API Code
+
+from django.utils.encoding import smart_str
 from rest_framework import routers, serializers, viewsets
 from councils.models import Council
 from pollingstations.models import PollingStation, PollingDistrict
 
+# Fields define serialization of complex field types (GEO)
+class PointField(serializers.Field):    
+    type_name = 'PointField'
+    type_label = 'point'
+
+    def to_representation(self, value):
+        """
+        Transform POINT object to json.
+        """
+        if value is None:
+            return value
+
+        value = {
+            "latitude": smart_str(value.y),
+            "longitude": smart_str(value.x)
+        }
+        return value
+
+
+class PolygonField(serializers.Field):
+    type_name = 'PolygonField'
+    type_label = 'polygon'
+
+    def to_representation(self, value):
+        if value is None:
+            return value
+        return value.coords
+
+
 # Serializers define the API representation.
 class CouncilSerializer(serializers.HyperlinkedModelSerializer):
+    location = PointField()
+#    area = PolygonField()
     class Meta:
         model = Council
         fields = (
             'council_id', 'council_type', 'mapit_id', 'name',
-            'email', 'phone', 'website', 'postcode', 'address'
+            'email', 'phone', 'website', 'postcode', 'address',
+            'location', 
+#            'area' # This is super slow ATM - TODO!
         )
 
 
 class PollingStationSerializer(serializers.HyperlinkedModelSerializer):
+    location = PointField()
     class Meta:
         model = PollingStation
         fields = ('council', 'postcode', 'address', 'location')
 
 
 class PollingDistrictSerializer(serializers.HyperlinkedModelSerializer):
+    # area = PolygonField()
     class Meta: 
         model = PollingDistrict
-        fields = ('name', 'council', 'area')
+        fields = (
+            'name', 'council', 
+#            'area' This is super slow ATM TODO!
+        )
 
 
 # ViewSets define the view behavior.
@@ -52,6 +93,7 @@ class PollingStationViewSet(viewsets.ModelViewSet):
 class PollingDistrictViewSet(viewsets.ModelViewSet):
     queryset = PollingDistrict.objects.all()
     serializer_class = PollingDistrictSerializer
+
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
