c15eb5859af41b9c68310a563216f5b7bb09bd77
David Miller <david@deadpansincerity.com>
2015-04-02 23:11:48 +0100

    Adds an importer for Lambeth Council.
    refs #37

---
+++ polling_stations/apps/data_collection/management/commands/import_lambeth.py
@@ -0,0 +1,32 @@
+"""
+Import Lambeth Council
+"""
+from django.contrib.gis.geos import Point
+
+from data_collection.management.commands import BaseJasonImporter
+
+class Command(BaseJasonImporter):
+    """
+    Imports the Polling station/district data from Lambeth Council
+    """
+    council_id     = 'E09000022'
+    districts_name = 'LambethPollingDistricts.json'
+    stations_name  = 'LambethPollingStations_0.csv'
+
+    def district_record_to_dict(self, record):
+        properties = record['properties']
+        return dict(
+            council=self.council,
+            internal_council_id=properties['OBJECTID'],
+            name=properties['WARD'],
+        )
+
+    def station_record_to_dict(self, record):
+        location = Point(int(record.easting), int(record.northing), srid=self.srid)
+        return dict(
+            council=self.council,
+            internal_council_id=record.district_code,
+            postcode=record.postcode,
+            address="\n".join([record.venue, record.address]),
+            location=location
+        )
