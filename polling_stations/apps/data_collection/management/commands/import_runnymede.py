6b22fe537042d6e067039cfb027f767ec34148a0
David Miller <david@deadpansincerity.com>
2015-04-03 21:56:34 +0100

    Adds an importer for runnymede

---
+++ polling_stations/apps/data_collection/management/commands/import_runnymede.py
@@ -0,0 +1,33 @@
+"""
+Import Runnymede
+"""
+from django.contrib.gis.geos import Point
+import ffs
+
+from data_collection.management.commands import BaseShpImporter, import_polling_station_shapefiles
+
+class Command(BaseShpImporter):
+    """
+    Imports the Polling Station data from Islington Council
+    """
+    council_id     = 'E07000212'
+    districts_name = 'RBC_Polling_Districts'
+    stations_name  = 'RBC_Polling_Stations.shp'
+
+    def district_record_to_dict(self, record):
+        return {
+            'council': self.council,
+            'internal_council_id': record[-1],
+            'name': record[0],
+        }
+
+    def station_record_to_dict(self, record):
+        return {
+            'council'            : self.council,
+            'internal_council_id': record[-1],
+            'postcode'           : '(No postcode supplied)',
+            'address'            : record[1],
+        }
+    
+    def import_polling_stations(self):
+        import_polling_station_shapefiles(self)
