961872cf20fcfa52654db8cf2ec09ea53101d536
David Miller <david@deadpansincerity.com>
2015-04-06 13:15:22 +0100

    Adds WIP South Tyneside manual points and WPI importer

---
+++ polling_stations/apps/data_collection/management/commands/import_south_tyneside.py
@@ -0,0 +1,32 @@
+"""
+Import South Tyneside
+"""
+from django.contrib.gis.geos import Point
+import ffs
+
+from data_collection.management.commands import BaseShpImporter, import_polling_station_shapefiles
+
+class Command(BaseShpImporter):
+    """
+    Imports the Polling Station data from South Tyneside Council
+    """
+    council_id     = 'E08000023'
+    districts_name = 'Polling Districts_region'
+    stations_name  = 'Polling Sites.csv'
+
+    def district_record_to_dict(self, record):
+        return {
+            'internal_council_id': record[0],
+            'name': record[1],
+        }
+
+    def station_record_to_dict(self, record):
+        location = Point(float(record.lat), float(record.long), srid=4326)
+        raise Exception('The points are wrong - we tried to set them manually ')
+        return {
+            'internal_council_id': record.polling_district,
+            'postcode'           : '(no postcode supplied)',
+            'address'            : "\n".join(record.address.split(',')),
+            'location'           : location
+        }
+    
