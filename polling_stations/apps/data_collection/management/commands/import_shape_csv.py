2f9f0cdaf919eb07a7e30b6c1c5ccd245b4459f3
David Miller <david@deadpansincerity.com>
2015-04-18 12:11:33 +0100

    Adds a base importer to cargo cult

---
+++ polling_stations/apps/data_collection/management/commands/import_shape_csv.py
@@ -0,0 +1,38 @@
+"""
+Import COUNCIL
+"""
+import sys
+from django.contrib.gis.geos import Point
+
+from data_collection.management.commands import BaseShpImporter
+
+class Command(BaseShpImporter):
+    """
+    Imports the Polling Station data from COUNCIL
+    """
+    council_id     = 'your_councilid'
+    districts_name = 'name_without_.shp'
+    stations_name  = 'your.csv'
+
+    def district_record_to_dict(self, record):
+        print 'District:', record
+        sys.exit(1)
+        return {
+            'internal_council_id': record[0],
+            'name': record[1],
+        }
+
+    def station_record_to_dict(self, record):
+        print 'Station', record
+        sys.exit(1)
+        try:
+            location = Point(int(record.easting), int(record.northing), srid=self.srid)
+        except ValueError:
+            location = Point(float(record.easting), float(record.northing), srid=self.srid)
+        return {
+            'internal_council_id': record.internal_id,
+            'postcode'           : record.address.split(',')[-1],
+            'address'            : "\n".join(record.address.split(',')[:-1]),
+            'location'           : location
+        }
+    
