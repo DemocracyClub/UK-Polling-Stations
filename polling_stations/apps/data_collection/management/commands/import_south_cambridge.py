d468d6a24eb7573163911eee8094c5cc47d1a08f
David Miller <david@deadpansincerity.com>
2015-04-18 13:17:12 +0100

    Add importer for South Cambridgeshire.
    refs #82

---
+++ polling_stations/apps/data_collection/management/commands/import_south_cambridge.py
@@ -0,0 +1,29 @@
+"""
+Import South Cambridge
+"""
+import sys
+
+from data_collection.management.commands import BaseShpShpImporter
+
+class Command(BaseShpShpImporter):
+    """
+    Imports the Polling Station data from South Cambridge
+    """
+    srid = 4326
+    council_id     = 'E07000012'
+    districts_name = 'Polling Districts for Twitter_region'
+    stations_name  = 'Polling Stations for Twitter_point.shp'
+
+    def district_record_to_dict(self, record):
+        return {
+            'internal_council_id': record[1],
+            'name': record[0],
+        }
+
+    def station_record_to_dict(self, record):
+        return {
+            'internal_council_id': record[0],
+            'postcode'           : self.postcode_from_address(record[0]),
+            'address'            : self.string_to_newline_addr(record[0])
+        }
+    
