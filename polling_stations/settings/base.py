ed5f62202b806523718dfba6347a0e8575f62620
Sym Roe <sym.roe@talusdesign.co.uk>
2015-05-06 08:07:48 +0100

    Disable CSRF

--- polling_stations/settings/base.py
+++ polling_stations/settings/base.py
@@ -102,7 +102,7 @@ TEMPLATE_LOADERS = (
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'whitelabel.middleware.WhiteLabelModdleware',
