6dab07184e88cfc5386b80d95f09983ccddfdedd
Sym Roe <sym.roe@talusdesign.co.uk>
2016-02-01 17:39:03 +0000

    Add missing migrations

---
+++ polling_stations/apps/councils/migrations/0002_auto_20160121_1522.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('councils', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='council',
+            name='email',
+            field=models.EmailField(max_length=254, blank=True),
+        ),
+    ]
