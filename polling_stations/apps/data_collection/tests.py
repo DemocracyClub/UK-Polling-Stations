b1f1b3c02d2bc8260398652e849475c93d1c21d6
chris48s <c.mshaw@yahoo.co.uk>
2016-02-13 17:30:24 +0000

    Add a test for parseCsv()
    A drop in the ocean but a step in the right direction.
    Now Travis has something to do :)

--- polling_stations/apps/data_collection/tests.py
+++ polling_stations/apps/data_collection/tests.py
@@ -1,3 +1,24 @@
+import os
 from django.test import TestCase
+from data_collection.management.commands import CsvHelper
 
-# Create your tests here.
+
+class CsvHelperTest(TestCase):
+
+    def test_parse_csv(self):
+        helper = CsvHelper(
+            os.path.join(os.path.dirname(__file__), 'fixtures/test/test.csv')
+        )
+        data = helper.parseCsv()
+
+        self.assertIsInstance(data[0], tuple)
+        self.assertEqual('1', data[0].foo)
+        self.assertEqual('2', data[0].b_a_r)
+        self.assertEqual('3', data[0].baz)
+
+        self.assertIsInstance(data[1], tuple)
+        self.assertEqual('cheese', data[1].foo)
+        self.assertEqual('peas', data[1].b_a_r)
+        self.assertEqual('', data[1].baz)
+
+        self.assertNotIn(2, data)
