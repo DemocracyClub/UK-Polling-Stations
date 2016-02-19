f806a88045c55437744540a228ff12a312e366a9
chris48s <c.mshaw@yahoo.co.uk>
2016-02-19 22:10:20 +0000

    refactor ryedale importer to use get_srid()

--- polling_stations/apps/data_collection/management/commands/import_ryedale.py
+++ polling_stations/apps/data_collection/management/commands/import_ryedale.py
@@ -17,14 +17,7 @@ class Command(BaseKamlImporter):
         # this kml has no altitude co-ordinates so the data is ok as it stands
         geojson = record.geom.geojson
 
-        # The SRID for the KML is 4326 but the CSV is 2770 so we
-        # set it each time we create the polygon.
-        # We could probably do with a more elegant way of doing
-        # this longer term.
-        self._srid = self.srid
-        self.srid = 4326
-        poly = self.clean_poly(GEOSGeometry(geojson, srid=self.srid))
-        self.srid = self._srid
+        poly = self.clean_poly(GEOSGeometry(geojson, srid=self.get_srid('districts')))
         return {
             'internal_council_id': record['Name'].value,
             'name'               : record['Name'].value,
@@ -32,7 +25,7 @@ class Command(BaseKamlImporter):
         }
 
     def station_record_to_dict(self, record):
-        location = Point(float(record.easting), float(record.northin), srid=self.srid)
+        location = Point(float(record.easting), float(record.northin), srid=self.get_srid())
         address_parts = record.address.split(' ')
         address = ' '.join(address_parts[:-2])
         return {
