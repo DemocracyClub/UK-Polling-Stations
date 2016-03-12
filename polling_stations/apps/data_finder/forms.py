4583a4ceff162795cee8a41692e56c38b6b46d59
chris48s <c.mshaw@yahoo.co.uk>
2016-03-12 15:27:41 +0000

    Add front-end for using ResidentialAddresss data

--- polling_stations/apps/data_finder/forms.py
+++ polling_stations/apps/data_finder/forms.py
@@ -4,3 +4,16 @@ from localflavor.gb.forms import GBPostcodeField
 
 class PostcodeLookupForm(forms.Form):
     postcode = GBPostcodeField(label="Enter your postcode")
+
+class AddressSelectForm(forms.Form):
+    address = forms.ChoiceField(
+        choices = (),
+        label = "",
+        initial = "",
+        widget = forms.Select(attrs={'class' : 'input-lg form-control'}),
+        required=True
+    )
+
+    def __init__(self, choices, *args, **kwargs):
+        super(AddressSelectForm, self).__init__(*args, **kwargs)
+        self.fields['address'].choices = choices
