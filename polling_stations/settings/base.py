68ccfc90aa1a14e3ecabaa821def53a7008fc4f3
Sym Roe <sym.roe@talusdesign.co.uk>
2016-03-14 15:59:09 +0000

    move to static_root

--- polling_stations/settings/base.py
+++ polling_stations/settings/base.py
@@ -73,7 +73,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-# STATIC_ROOT = root('static')
+STATIC_ROOT = root('static_root')
 # print(STATIC_ROOT)
 
 # URL prefix for static files.
