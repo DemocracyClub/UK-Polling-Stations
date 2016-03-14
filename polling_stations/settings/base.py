4dd2d328c7d8306ab3377d207c594033af3e7534
Sym Roe <sym.roe@talusdesign.co.uk>
2016-03-14 14:37:33 +0000

    Add dc_base_theme to requirements

--- polling_stations/settings/base.py
+++ polling_stations/settings/base.py
@@ -112,7 +112,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = 'CHANGE THIS!!!'
+SECRET_KEY = 'asdasdasdasdasdasdasd'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
