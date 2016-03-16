fc99b0941c85e4b81cf6a4069b2ea2ed8ca0cbc8
Sym Roe <sym.roe@talusdesign.co.uk>
2016-03-16 17:53:47 +0000

    The bad line isn't there any more !

--- polling_stations/apps/data_collection/management/commands/import_cardiff.py
+++ polling_stations/apps/data_collection/management/commands/import_cardiff.py
@@ -27,16 +27,12 @@ class Command(BaseShpShpImporter):
         }
 
     def station_record_to_dict(self, record):
-        try:
-            return {
-                'internal_council_id': record[0],
-                'postcode'           : "",
-                'address'            : str(record[2]),
-                'polling_district_id': str(record[1]),
-            }
-        except TypeError:
-            # there is a single bad row, it seems
-            pass
+        return {
+            'internal_council_id': record[0],
+            'postcode'           : "",
+            'address'            : str(record[2]),
+            'polling_district_id': str(record[1]),
+        }
 
     @transaction.atomic
     def post_import(self):
