04fcda42222fff1daad780db53190bcfb721d034
chris48s <c.mshaw@yahoo.co.uk>
2016-03-20 13:58:49 +0000

    Fix Mid Sussex Import script
    Set polling_district_id
    Use mserid as internal_council_id to avoid importing duplicate points

--- polling_stations/apps/data_collection/management/commands/import_mid_sussex.py
+++ polling_stations/apps/data_collection/management/commands/import_mid_sussex.py
@@ -2,9 +2,8 @@
 Imports Mid Sussex
 """
 import sys
-
+from lxml import etree
 from django.contrib.gis.geos import Point, GEOSGeometry
-
 from data_collection.management.commands import BaseKamlImporter
 
 class Command(BaseKamlImporter):
@@ -15,12 +14,28 @@ class Command(BaseKamlImporter):
     districts_name = 'msdc_3830_pollingdistricts_polygon.kmz'
     stations_name  = 'R3900_pollingstations.csv'
 
+    def extract_msercode_from_description(self, description):
+        html = etree.HTML(str(description).replace('&', '&amp;'))
+        rows = html.xpath("//td")
+        return rows[7].text
+
+    def district_record_to_dict(self, record):
+        msercode = self.extract_msercode_from_description(record['description'])
+        geojson = self.strip_z_values(record.geom.geojson)
+        poly = self.clean_poly(GEOSGeometry(geojson, srid=self.get_srid('districts')))
+        return {
+            'internal_council_id': msercode,
+            'name'               : record['Name'].value,
+            'area'               : poly
+        }
+
     def station_record_to_dict(self, record):
         location = Point(float(record.xcoord), float(record.ycoord), srid=self.srid)
         address = "\n".join([record.venue, record.street, record.town])
         return {
-            'internal_council_id': record.statnum,
+            'internal_council_id': record.msercode,
             'postcode':            record.postcode,
             'address':             address,
-            'location':            location
+            'location':            location,
+            'polling_district_id': record.msercode
         }
