30d0b214fd50cbc3fccec553e2c875659f670ab4
chris48s <c.mshaw@yahoo.co.uk>
2016-03-27 16:54:13 +0100

    West Berkshire: Use correct polling_station_id
    polling_station_id should correlate with internal_council_id
    in PollingStations table

--- polling_stations/apps/data_collection/management/commands/import_westberks.py
+++ polling_stations/apps/data_collection/management/commands/import_westberks.py
@@ -16,7 +16,7 @@ class Command(BaseShpShpImporter):
         return {
             'internal_council_id': record[4],
             'name': record[2],
-            'polling_station_id': record[6]
+            'polling_station_id': record[4]
         }
 
     def station_record_to_dict(self, record):
