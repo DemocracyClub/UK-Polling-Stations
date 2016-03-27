cadc3d1389f1f074237ce6e749f3f096efde87ed
chris48s <c.mshaw@yahoo.co.uk>
2016-03-27 21:30:41 +0100

    Boston: Handle stations that serve multiple districts

--- polling_stations/apps/data_collection/management/commands/import_boston.py
+++ polling_stations/apps/data_collection/management/commands/import_boston.py
@@ -1,9 +1,9 @@
 """
 Import Boston
 """
+import os
 from django.contrib.gis.geos import Point
-
-from data_collection.management.commands import BaseShpImporter
+from data_collection.management.commands import BaseShpImporter, CsvHelper
 
 class Command(BaseShpImporter):
     """
@@ -17,9 +17,24 @@ class Command(BaseShpImporter):
         return {
             'internal_council_id': record[0],
             'name': record[1],
+            'polling_station_id': record[0]
         }
 
-    def station_record_to_dict(self, record):
+    # station_record_to_dicts() returns an array of dicts in this script
+    def import_polling_stations(self):
+        stations_file = os.path.join(self.base_folder_path, self.stations_name)
+
+        helper = CsvHelper(stations_file, self.csv_encoding)
+        data = helper.parseCsv()
+        for row in data:
+            stations = self.station_record_to_dicts(row)
+            for station in stations:
+                if 'council' not in station:
+                    station['council'] = self.council
+
+                self.add_polling_station(station)
+
+    def station_record_to_dicts(self, record):
         try:
             location = Point(int(record.easting), int(record.northing), srid=self.srid)
         except ValueError:
@@ -29,9 +44,26 @@ class Command(BaseShpImporter):
         address = "\n".join(address_parts[:-1])
         postcode = address_parts[-1]
 
-        return {
-            'internal_council_id': record.internal_id,
-            'postcode'           : postcode,
-            'address'            : address,
-            'location'           : location
-        }
+        """
+        In this data, sometimes a single polling station serves several
+        districts. For simplicity, if record.internal_id is something like "AB,AC" 
+        return the same polling station address/point multiple times with different IDs
+        """
+        internal_ids = record.internal_id.split(",")
+        if (len(internal_ids) == 1):
+            return [{
+                'internal_council_id': record.internal_id,
+                'postcode'           : postcode,
+                'address'            : address,
+                'location'           : location
+            }]
+        else:
+            stations = []
+            for id in internal_ids:
+                stations.append({
+                    'internal_council_id': id,
+                    'postcode'           : postcode,
+                    'address'            : address,
+                    'location'           : location
+                })
+            return stations
