776ad35de4d1d891271a9c6f7fc7f8f6cc995583
chris48s <c.mshaw@yahoo.co.uk>
2016-03-28 14:18:19 +0100

    Salford: Fix address/postcode formatting

--- polling_stations/apps/data_collection/management/commands/import_salford.py
+++ polling_stations/apps/data_collection/management/commands/import_salford.py
@@ -25,11 +25,17 @@ class Command(BaseShpImporter):
             location = Point(int(record.easting), int(record.northing), srid=self.srid)
         except ValueError:
             location = Point(float(record.easting), float(record.northing), srid=self.srid)
+
+        address_parts = [x.strip() for x in record.location.split(' ')]
+        postcode = "%s %s" % (address_parts[-2], address_parts[-1])
+        del(address_parts[-1])
+        del(address_parts[-1])
+        address = " ".join(address_parts)
+
         return {
             'internal_council_id': record.id,
-            'postcode'           : record.location.split(',')[-1],
-            'address'            : "\n".join(record.location.split(',')[:-1]),
+            'postcode'           : postcode,
+            'address'            : address,
             'location'           : location,
             'polling_district_id': record.polling_district_code
         }
-    
