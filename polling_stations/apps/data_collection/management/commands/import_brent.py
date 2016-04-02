a7ba5dd99b974be8ca413c239ccf42f16d9879a6
chris48s <c.mshaw@yahoo.co.uk>
2016-04-02 20:15:06 +0100

    Fix Brent import script
    - Make it run under python 3
    - Map Stations to districts
    - Remove 2x junk rows in stations table
    - Improve address formatting a bit

--- polling_stations/apps/data_collection/management/commands/import_brent.py
+++ polling_stations/apps/data_collection/management/commands/import_brent.py
@@ -17,16 +17,37 @@ class Command(BaseShpImporter):
     def district_record_to_dict(self, record):
         return {
             'internal_council_id': record[-1],
-            'name': record[1],
+            'name': "%s - %s" % (record[1], record[-1]),
+            'polling_station_id': record[-1]
         }
 
     def station_record_to_dict(self, record):
+
+        # There are 2 records with no address info or code
+        # Neither map to a polling station: don't insert them
+        if record[2] == 'x':
+            return None
+        if (isinstance(record[2], bytes)) and\
+            (record[2].decode('ascii').strip() == ''):
+            return None
+
+        # format/clean up address (a bit)
+        if isinstance(record[3], bytes):
+            address = record[3].decode('windows-1252')
+        else:
+            address = record[3]
+        address_parts = address.split(', ')
+        postcode = ''
+        if address_parts[-1][:2] == 'NW' or address_parts[-1][:2] == 'HA':
+            postcode = address_parts[-1]
+            del(address_parts[-1])
+        address = "\n".join(address_parts)
+
         return {
             'internal_council_id': record[2],
-            'postcode'           : record[3].split(',')[-1],
-            'address'            : record[3].decode('windows-1252')
+            'postcode'           : postcode,
+            'address'            : address
         }
-    
-    
+
     def import_polling_stations(self):
         import_polling_station_shapefiles(self)
