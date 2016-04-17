0ff80a0ae9c7cbba575c245f6a7aa91f6df957e6
chris48s <c.mshaw@yahoo.co.uk>
2016-04-17 15:12:00 +0100

    Tag Ceredigion import script with election ids

--- polling_stations/apps/data_collection/management/commands/import_ceredigion.py
+++ polling_stations/apps/data_collection/management/commands/import_ceredigion.py
@@ -15,6 +15,12 @@ class Command(BaseAddressCsvImporter):
     stations_name    = 'Ceredigion_Polling_Stations_processed.csv'
     srid             = 27700
     csv_encoding     = 'latin-1'
+    elections        = [
+        'pcc.2016-05-05',
+        'naw.c.2016-05-05',
+        'naw.r.2016-05-05',
+        'ref.2016-06-23'
+    ]
 
     def station_record_to_dict(self, record):
 
