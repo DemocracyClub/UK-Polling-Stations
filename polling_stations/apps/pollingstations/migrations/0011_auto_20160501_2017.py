f1cc6097c684b86732044ef57e396ffe3ff43c6e
chris48s <c.mshaw@yahoo.co.uk>
2016-05-01 20:26:49 +0100

    Add composite indexes to PollingStation model

---
+++ polling_stations/apps/pollingstations/migrations/0011_auto_20160501_2017.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('pollingstations', '0010_auto_20160417_1416'),
+    ]
+
+    operations = [
+        migrations.AlterIndexTogether(
+            name='pollingstation',
+            index_together=set([('council', 'internal_council_id'), ('council', 'polling_district_id')]),
+        ),
+    ]
