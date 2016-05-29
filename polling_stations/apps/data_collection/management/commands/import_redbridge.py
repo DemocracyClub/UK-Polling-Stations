12c0c2b7b3468eeb830bf74739f08b589205c55f
chris48s <c.mshaw@yahoo.co.uk>
2016-05-30 00:26:29 +0100

    add import script for Redbridge

---
+++ polling_stations/apps/data_collection/management/commands/import_redbridge.py
@@ -0,0 +1,58 @@
+"""
+Import Redbridge
+
+note: this script takes quite a long time to run
+"""
+from time import sleep
+from django.contrib.gis.geos import Point
+from data_collection.management.commands import BaseAddressCsvImporter
+from data_finder.helpers import geocode, PostcodeError
+
+class Command(BaseAddressCsvImporter):
+    """
+    Imports the Polling Station data from Redbridge Council
+    """
+    council_id      = 'E09000026'
+    addresses_name  = 'rev01-2016/LLPG Addresses - Polling Station Finder - EU referendum.csv'
+    stations_name   = 'rev01-2016/Polling Stations - EU referendum.csv'
+    elections       = [
+        'ref.2016-06-23'
+    ]
+
+    def station_record_to_dict(self, record):
+
+        # no points supplied, so attempt to attach them by geocoding
+        sleep(1.3) # ensure we don't hit mapit's usage limit
+        if record.postcode:
+            try:
+                gridref = geocode(record.postcode)
+                location = Point(gridref['wgs84_lon'], gridref['wgs84_lat'], srid=4326)
+            except PostcodeError:
+                location = None
+        else:
+            location = None
+
+        return {
+            'internal_council_id': record.district_code.strip(),
+            'postcode'           : record.postcode.strip(),
+            'address'            : record.address.strip(),
+            'location'           : location
+        }
+
+    def address_record_to_dict(self, record):
+
+        address_parts = record.postal_address.strip().split(", ")
+        postcode = address_parts[-1]   # every address has a postcode :D
+        address = ", ".join(address_parts[:-1])
+
+        # There are 6 addresses which don't
+        # map to any station - exclude them
+        if not record.district_code:
+            return None
+
+        # 20 exact dupes will be discarded
+        return {
+            'address'           : address,
+            'postcode'          : postcode,
+            'polling_station_id': record.district_code.strip()
+        }
