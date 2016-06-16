0ffd7c7d7ef6a4670c67e0227f73da4e22631a2d
Sym Roe <sym.roe@talusdesign.co.uk>
2016-06-16 09:16:52 +0100

    Use signed cookies to store sessions
    This has a few advantages:
    
    1. It cuts down on at least one query per request
    2. It means we don't need to use sticky sessions when we load balance

--- polling_stations/settings/base.py
+++ polling_stations/settings/base.py
@@ -185,6 +185,8 @@ PROJECT_APPS = (
 
 INSTALLED_APPS += PROJECT_APPS
 
+SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
