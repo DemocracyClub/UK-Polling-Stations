99c8051dd33862c10d60fb9b281f276ac624e11c
Sym Roe <sym.roe@talusdesign.co.uk>
2016-06-16 15:56:47 +0100

    Add more indexed for models
    (Sorry this also contains an extra missing migration)

---
+++ polling_stations/apps/data_collection/migrations/0008_auto_20160613_1323.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('data_collection', '0007_auto_20160415_1931'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='dataquality',
+            name='postcodes_contained_by_district',
+            field=models.IntegerField(default=0),
+        ),
+        migrations.AddField(
+            model_name='dataquality',
+            name='postcodes_with_addresses_generated',
+            field=models.IntegerField(default=0),
+        ),
+    ]
