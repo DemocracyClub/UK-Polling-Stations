cf549fedf844e96a1c7508142125b83dce4dad7d
Sym Roe <sym.roe@talusdesign.co.uk>
2016-06-17 16:49:46 +0100

    Import Barking and Dagenham

---
+++ polling_stations/apps/data_collection/management/commands/import_barking_and_dagenham.py
@@ -0,0 +1,28 @@
+"""
+Import Barking-and-Dagenham
+"""
+from data_collection.management.commands import BaseShpShpImporter
+
+class Command(BaseShpShpImporter):
+    """
+    Imports the Polling Station data from Barking-and-Dagenham
+    """
+    council_id = 'E09000002'
+    districts_name = 'Barking_n_Dag_completed_PDs_region_updated/Barking_n_Dag_completed_PDs_region_updated'
+    stations_name = 'Polling_Stations2016/Polling_Stations2016'
+    elections = ['ref.2016-06-23']
+
+    def district_record_to_dict(self, record):
+        return {
+            'internal_council_id': record[0],
+            'name': record[1],
+        }
+
+    def station_record_to_dict(self, record):
+        postcode = " ".join(record[3].split(' ')[-2:])
+        return {
+            'internal_council_id': record[1],
+            'polling_district_id': record[4],
+            'postcode' : postcode,
+            'address' : record[3]
+        }
