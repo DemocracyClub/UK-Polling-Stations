0f2830fda2c97d28294fa340d8ffe7a53b3a6039
Sym Roe <sym.roe@talusdesign.co.uk>
2016-06-20 18:27:36 +0100

    Import Brent

--- polling_stations/apps/data_collection/management/commands/import_brent.py
+++ polling_stations/apps/data_collection/management/commands/import_brent.py
@@ -3,52 +3,78 @@ Import Brent
 """
 from django.contrib.gis.geos import Point
 
-from data_collection.management.commands import BaseShpImporter, import_polling_station_shapefiles
+from data_collection.management.commands import BaseAddressCsvImporter
+from data_finder.helpers import geocode, geocode_point_only, PostcodeError
+from addressbase.models import Address
 
 
-class Command(BaseShpImporter):
+class Command(BaseAddressCsvImporter):
     """
     Imports the Polling Station data from Brent Council
     """
-    council_id     = 'E09000005'
-    districts_name = 'polling_districts'
-    stations_name  = 'polling_places.shp'
-    elections      = ['parl.2015-05-07']
+    council_id = 'E09000005'
+    addresses_name = '2016/Brent polling station GIS data.csv'
+    stations_name = '2016/Brent polling station GIS data.csv'
+    csv_delimiter = ','
+    elections = ['ref.2016-06-23']
 
-    def district_record_to_dict(self, record):
-        return {
-            'internal_council_id': record[-1],
-            'name': "%s - %s" % (record[1], record[-1]),
-            'polling_station_id': record[-1]
-        }
+    def get_station_hash(self, record):
+        return "-".join([
+            record.stationuprn,
+            record.premise_p_code,
+            record.premise_name,
+        ])
 
     def station_record_to_dict(self, record):
+        # format address
+        address = "\n".join([
+            record.premise_name,
+            record.premise_add1,
+            record.premise_add2,
+            record.premise_add3,
+            record.premise_add4,
+            record.premise_add5,
+        ])
+        while "\n\n" in address:
+            address = address.replace("\n\n", "\n").strip()
 
-        # There are 2 records with no address info or code
-        # Neither map to a polling station: don't insert them
-        if record[2] == 'x':
-            return None
-        if (isinstance(record[2], bytes)) and\
-            (record[2].decode('ascii').strip() == ''):
-            return None
-
-        # format/clean up address (a bit)
-        if isinstance(record[3], bytes):
-            address = record[3].decode('windows-1252')
-        else:
-            address = record[3]
-        address_parts = address.split(', ')
-        postcode = ''
-        if address_parts[-1][:2] == 'NW' or address_parts[-1][:2] == 'HA':
-            postcode = address_parts[-1]
-            del(address_parts[-1])
-        address = "\n".join(address_parts)
+        postcode = record.premise_p_code
+        location = None
+        location_data = None
+        # no points supplied, so attempt to attach them by geocoding
+        if len(postcode) > 5:
+            try:
+                location_data = geocode_point_only(postcode)
+            except PostcodeError:
+                pass
+            if location_data:
+                location = Point(
+                    location_data['wgs84_lon'],
+                    location_data['wgs84_lat'],
+                    srid=4326)
 
         return {
-            'internal_council_id': record[2],
+            'internal_council_id': record.stationuprn,
             'postcode'           : postcode,
-            'address'            : address
+            'address'            : address,
+            'location'           : location
         }
 
-    def import_polling_stations(self):
-        import_polling_station_shapefiles(self)
+    def address_record_to_dict(self, record):
+
+        address = ", ".join([f.strip() for f in [
+            record.house_name,
+            record.house_no,
+            record.street_name,
+            record.address_line_1,
+            record.address_line_2,
+            record.address_line_3,
+            record.address_line_4,
+            record.address_line_5,
+        ] if f])
+
+        return {
+            'address'           : address,
+            'postcode'          : record.post_code.strip(),
+            'polling_station_id': record.stationuprn
+        }
