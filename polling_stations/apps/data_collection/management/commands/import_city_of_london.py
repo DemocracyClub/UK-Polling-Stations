bf7efd3038936991c86eab02bdb68b528f1506a5
chris48s <c.mshaw@yahoo.co.uk>
2016-10-07 22:44:38 +0100

    add a few import scripts for data from morph

--- polling_stations/apps/data_collection/management/commands/import_city_of_london.py
+++ polling_stations/apps/data_collection/management/commands/import_city_of_london.py
@@ -1,33 +1,22 @@
-"""
-Imports City of London
-"""
-from django.contrib.gis.geos import GEOSGeometry
-from data_collection.management.commands import BaseApiKmlStationsKmlDistrictsImporter
+from data_collection.management.commands import BaseMorphApiImporter
 
-class Command(BaseApiKmlStationsKmlDistrictsImporter):
-    """
-    Imports the Polling Station data from City of London Corporation
-    """
-    srid             = 27700
-    districts_srid   = 27700
-    council_id       = 'E09000001'
-    districts_url    = 'http://www.mapping2.cityoflondon.gov.uk/arcgis/services/INSPIRE/MapServer/WFSServer?request=GetFeature&version=1.1.0&service=wfs&typeNames=INSPIRE:UK_Parliamentary_General_Election_Polling_Districts'
-    stations_url     = 'http://www.mapping2.cityoflondon.gov.uk/arcgis/services/INSPIRE/MapServer/WFSServer?request=GetFeature&version=1.1.0&service=wfs&typeNames=INSPIRE:UK_Parliamentary_General_Election_Polling_Places'
-    """
-    note:
-    These aren't actually KML - they're QML (hence use of SRID 27700)
-    but gdal.DataSource will just deal with them for us :)
-    """
+class Command(BaseMorphApiImporter):
+
+    srid = 27700
+    districts_srid = 27700
+    council_id = 'E09000001'
     elections = [
         'gla.c.2016-05-05',
         'gla.a.2016-05-05',
         'mayor.london.2016-05-05',
-        'ref.2016-06-23'
+        'ref.2016-06-23',
     ]
+    scraper_name = 'wdiv-scrapers/DC-PollingStations-CityOfLondon'
+    geom_type = 'gml'
+
 
     def district_record_to_dict(self, record):
-        geojson = record.geom.geojson
-        poly = self.clean_poly(GEOSGeometry(geojson, srid=self.get_srid('districts')))
+        poly = self.extract_geometry(record, self.geom_type, self.get_srid('districts'))
 
         return {
             'internal_council_id': record['OBJECTID'],
@@ -35,9 +24,9 @@ class Command(BaseApiKmlStationsKmlDistrictsImporter):
             'area'               : poly
         }
 
+
     def station_record_to_dict(self, record):
-        geojson = record.geom.geojson
-        location = GEOSGeometry(geojson, srid=self.get_srid())
+        location = self.extract_geometry(record, self.geom_type, self.get_srid('stations'))
 
 
         # format address and postcode
--- polling_stations/apps/data_collection/management/commands/import_salford.py
+++ polling_stations/apps/data_collection/management/commands/import_salford.py
@@ -1,42 +1,28 @@
-"""
-Import Salford
-"""
-import sys
-from django.contrib.gis.geos import Point
+from data_collection.management.commands import BaseMorphApiImporter
 
-from data_collection.management.commands import BaseCsvStationsShpDistrictsImporter
+class Command(BaseMorphApiImporter):
 
-class Command(BaseCsvStationsShpDistrictsImporter):
-    """
-    Imports the Polling Station data from Salford
-    """
-    council_id     = 'E08000006'
-    districts_name = 'Salford_Polling_Districts'
-    stations_name  = 'Polling_Stations.csv'
-    elections      = ['parl.2015-05-07']
+    srid = 4326
+    districts_srid  = 4326
+    council_id = 'E08000006'
+    elections = ['mayor.greater-manchester.2017-05-04']
+    scraper_name = 'wdiv-scrapers/DC-PollingStations-Salford'
+    geom_type = 'geojson'
 
     def district_record_to_dict(self, record):
+        poly = self.extract_geometry(record, self.geom_type, self.get_srid('districts'))
         return {
-            'internal_council_id': record[0],
-            'name': record[0],
+            'internal_council_id': record['code'],
+            'name'               : record['code'],
+            'area'               : poly
         }
 
     def station_record_to_dict(self, record):
-        try:
-            location = Point(int(record.easting), int(record.northing), srid=self.srid)
-        except ValueError:
-            location = Point(float(record.easting), float(record.northing), srid=self.srid)
-
-        address_parts = [x.strip() for x in record.location.split(' ')]
-        postcode = "%s %s" % (address_parts[-2], address_parts[-1])
-        del(address_parts[-1])
-        del(address_parts[-1])
-        address = " ".join(address_parts)
-
+        location = self.extract_geometry(record, self.geom_type, self.get_srid('stations'))
         return {
-            'internal_council_id': record.id,
-            'postcode'           : postcode,
-            'address'            : address,
-            'location'           : location,
-            'polling_district_id': record.polling_district_code
+            'internal_council_id': record['polling_district'],
+            'postcode':            '',
+            'address':             record['station_location'],
+            'location':            location,
+            'polling_district_id': record['polling_district']
         }
