507d672f157328eef7586e5e8fc4cd2e5afa5ac3
chris48s <c.mshaw@yahoo.co.uk>
2016-10-14 21:09:31 +0100

    fix incorrect comment

--- polling_stations/settings/base.py
+++ polling_stations/settings/base.py
@@ -270,7 +270,7 @@ WHITELABEL_PREFIXES = (
 INTERNAL_IPS = ('127.0.0.1')
 SITE_TITLE = "Where Do I Vote?"
 
-# Google maps API key used for geocoding in import scripts
+# Google maps API key used by directions helper
 GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY', "")
 
 # Morph API key used for downloading scraped data in import scripts
