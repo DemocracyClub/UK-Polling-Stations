64e476c8ba1ee43c3bdefe37791b2834c7c26514
chris48s <c.mshaw@yahoo.co.uk>
2016-12-31 17:18:01 +0000

    apply same convention to /pollingdistricts
    This allows us to call:
    
    /api/pollingdistricts.json
    (json - all districts, no boundaries)
    /api/pollingdistricts.json?council_id=X01000001
    (json - all districts in council, no boundaries)
    /api/pollingdistricts.json?council_id=X01000001&district_id=AA
    (json - specific district, no boundary)
    
    /api/pollingdistricts/geo.json
    (geojson - all districts, with boundaries)
    /api/pollingdistricts/geo.json?council_id=X01000001
    (geojson - all districts in council, with boundaries)
    /api/pollingdistricts/geo.json?council_id=X01000001&district_id=AA
    (geojson - specific district, with boundary)
    
    attempting to call
    /api/pollingdistricts.json?district_id=AA
    will throw
    HTTP/1.0 400 BAD REQUEST

--- polling_stations/api/pollingdistricts.py
+++ polling_stations/api/pollingdistricts.py
@@ -1,24 +1,70 @@
-from rest_framework.serializers import HyperlinkedModelSerializer
-from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.decorators import list_route
+from rest_framework.mixins import ListModelMixin
+from rest_framework.response import Response
+from rest_framework.serializers import CharField, HyperlinkedModelSerializer
+from rest_framework.viewsets import GenericViewSet
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
 from pollingstations.models import PollingDistrict
 
 
 class PollingDistrictDataSerializer(HyperlinkedModelSerializer):
 
+    district_id = CharField(source='internal_council_id', read_only=True)
+
     class Meta:
         model = PollingDistrict
-        fields = ('name', 'council')
+        fields = ('council', 'district_id', 'name')
 
 
 class PollingDistrictGeoSerializer(GeoFeatureModelSerializer):
 
+    district_id = CharField(source='internal_council_id', read_only=True)
+
     class Meta:
         model = PollingDistrict
         geo_field = 'area'
-        fields = ('name', 'council')
+        fields = ('council', 'district_id', 'name')
 
 
-class PollingDistrictViewSet(ReadOnlyModelViewSet):
+class PollingDistrictViewSet(GenericViewSet, ListModelMixin):
     queryset = PollingDistrict.objects.all()
-    serializer_class = PollingDistrictDataSerializer
+
+    def get_queryset(self):
+        council_id = self.request.query_params.get('council_id', None)
+        district_id = self.request.query_params.get('district_id', None)
+
+        if council_id is None:
+            return PollingDistrict.objects.all()
+
+        if district_id is None:
+            return PollingDistrict.objects.filter(council=council_id)
+
+        return PollingDistrict.objects.filter(
+            council=council_id, internal_council_id=district_id)
+
+    def validate_request(self):
+        if 'district_id' in self.request.query_params and\
+            'council_id' not in self.request.query_params:
+                return False
+        return True
+
+    def list(self, request, *args, **kwargs):
+        if not self.validate_request():
+            return Response(
+                {'detail': 'council_id parameter must be specified'}, 400)
+
+        queryset = self.get_queryset()
+        serializer = PollingDistrictDataSerializer(
+            queryset, many=True, read_only=True, context={'request': request})
+        return Response(serializer.data)
+
+    @list_route(url_path='geo')
+    def geo(self, request, format=None):
+        if not self.validate_request():
+            return Response(
+                {'detail': 'council_id parameter must be specified'}, 400)
+
+        queryset = self.get_queryset()
+        serializer = PollingDistrictGeoSerializer(
+            queryset, many=True, read_only=True, context={'request': request})
+        return Response(serializer.data)
