d1f48436b3efc3d00bd6ab6771a663f9a93b3dc0
chris48s <c.mshaw@yahoo.co.uk>
2016-12-31 16:33:15 +0000

    add filters to /pollingstations endpoint
    This allows us to call:
    /api/pollingstations.json?council_id=X01000001
    (geojson response - all stations in council area)
    
    /api/pollingstations.json?council_id=X01000001&station_id=AA
    (geojson response - specific polling station)
    
    attempting to call
    /api/pollingstations.json?station_id=AA
    will throw
    HTTP/1.0 400 BAD REQUEST

--- polling_stations/api/pollingstations.py
+++ polling_stations/api/pollingstations.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import list_route
 from rest_framework.mixins import ListModelMixin
+from rest_framework.response import Response
 from rest_framework.serializers import CharField
 from rest_framework.viewsets import GenericViewSet
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
@@ -19,6 +20,29 @@ class PollingStationSerializer(GeoFeatureModelSerializer):
 class PollingStationViewSet(GenericViewSet, ListModelMixin):
     queryset = PollingStation.objects.all()
     serializer_class = PollingStationSerializer
+
+    def get_queryset(self):
+        council_id = self.request.query_params.get('council_id', None)
+        station_id = self.request.query_params.get('station_id', None)
+
+        if council_id is None:
+            return PollingStation.objects.all()
+
+        if station_id is None:
+            return PollingStation.objects.filter(council=council_id)
+
+        return PollingStation.objects.filter(
+            council=council_id, internal_council_id=station_id)
+
+    def list(self, request, *args, **kwargs):
+        if 'station_id' in request.query_params and\
+            'council_id' not in request.query_params:
+            return Response(
+                {'detail': 'council_id parameter must be specified'}, 400)
+
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True, read_only=True)
+        return Response(serializer.data)
 
     @list_route(url_path='geo')
     def geo(self, request, format=None):
