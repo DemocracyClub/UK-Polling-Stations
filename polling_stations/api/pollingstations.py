77c1d778856874b09b087b4d90fd9ac35163340c
chris48s <c.mshaw@yahoo.co.uk>
2016-12-31 15:26:32 +0000

    include aliased internal_council_id in station object

--- polling_stations/api/pollingstations.py
+++ polling_stations/api/pollingstations.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import list_route
 from rest_framework.mixins import ListModelMixin
+from rest_framework.serializers import CharField
 from rest_framework.viewsets import GenericViewSet
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
 from pollingstations.models import PollingStation
@@ -7,10 +8,12 @@ from pollingstations.models import PollingStation
 
 class PollingStationSerializer(GeoFeatureModelSerializer):
 
+    station_id = CharField(source='internal_council_id', read_only=True)
+
     class Meta:
         model = PollingStation
         geo_field = 'location'
-        fields = ('council', 'postcode', 'address', 'location')
+        fields = ('council', 'station_id', 'postcode', 'address', 'location')
 
 
 class PollingStationViewSet(GenericViewSet, ListModelMixin):
