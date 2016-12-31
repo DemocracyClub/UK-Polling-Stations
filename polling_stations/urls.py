fe706ad2165a5413164ddd9fbdc1f2fe4102c211
chris48s <c.mshaw@yahoo.co.uk>
2016-12-31 00:01:27 +0000

    remove unnecessary route

--- polling_stations/urls.py
+++ polling_stations/urls.py
@@ -46,7 +46,6 @@ extra_patterns = patterns(
     '',
     url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^api/', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
     url(r'^admin/', include(admin.site.urls)),
 
