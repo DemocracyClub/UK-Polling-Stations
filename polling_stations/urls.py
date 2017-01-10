f7e927e8391223c7f8aa4ac1a1dc4dbb89a659f7
chris48s <c.mshaw@yahoo.co.uk>
2017-01-10 21:32:17 +0000

    move api to /api/v1

--- polling_stations/urls.py
+++ polling_stations/urls.py
@@ -45,7 +45,7 @@ core_patterns = patterns(
 extra_patterns = patterns(
     '',
     url(r'^i18n/', include('django.conf.urls.i18n')),
-    url(r'^api/', include(router.urls)),
+    url(r'^api/v1/', include(router.urls)),
 
     url(r'^admin/', include(admin.site.urls)),
 
