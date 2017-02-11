103b07b39d881f1e3114c5dbd8f71cfd1a7be10c
chris48s <chris.shaw480@gmail.com>
2017-02-11 21:14:26 +0000

    Update geocoding to use AddressBase/ONSAD
    - Add ability to identify council and attach GSS codes to
      postcodes using AddressBase/ONSAD
    - Prioritise AddressBase/ONSAD as a source for geocoding/GSS codes
    - Fall back to centroid-based geocoding using mapit
      if we can't sucessfully geocode using AddressBase/ONSAD
      (there are various situations where this may happen)
    - Identify postcodes which cross local auth boundaries and
      throw MultipleCouncilsException if we encounter one
      (currently this is not handled anywhere)
    - add a 'source' property to geocode() responses
      for debug/testing purposes

--- polling_stations/apps/data_finder/helpers.py
+++ polling_stations/apps/data_finder/helpers.py
@@ -8,13 +8,21 @@ from collections import namedtuple
 from django.utils.translation import ugettext as _
 from django.contrib.gis.geos import Point
 from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
 
 from addressbase.helpers import centre_from_points_qs
-from addressbase.models import Address
+from addressbase.models import Address, Onsad
 
 from pollingstations.models import ResidentialAddress
 
+
 class PostcodeError(Exception):
+    pass
+
+class MultipleCouncilsException(Exception):
+    pass
+
+class CodesNotFoundException(Exception):
     pass
 
 class RateLimitError(Exception):
@@ -23,74 +31,190 @@ class RateLimitError(Exception):
         logger.error(message)
 
 
+class MapitWrapper:
+
+    def __init__(self, postcode):
+        self.postcode = postcode
+
+    def call_mapit(self):
+        headers = {}
+        if settings.MAPIT_UA:
+            headers['User-Agent'] = settings.MAPIT_UA
+
+        res = requests.get("%s/postcode/%s" % (settings.MAPIT_URL, self.postcode), headers=headers)
+
+        if res.status_code != 200:
+            if res.status_code == 403:
+                # we hit MapIt's rate limit
+                raise RateLimitError("Mapit error 403: Rate limit exceeded")
+
+            if res.status_code == 404:
+                # if mapit returns 404, it returns HTML even if we requested json
+                # this will cause an unhandled exception if we try to parse it
+                raise PostcodeError("Mapit error 404: Not Found")
+
+            try:
+                # attempt to parse error from json
+                res_json = res.json()
+                if 'error' in res_json:
+                    raise PostcodeError("Mapit error {}: {}".format(res_json['code'], res_json['error']))
+                else:
+                    raise PostcodeError("Mapit error {}: unknown".format(res.status_code))
+            except ValueError:
+                # if we fail to parse json, raise a less specific exception
+                raise PostcodeError("Mapit error {}: unknown".format(res.status_code))
+
+        return res.json()
+
+    def geocode(self):
+        res_json = self.call_mapit()
+        COUNCIL_TYPES = getattr(settings, 'COUNCIL_TYPES', [])
+        gss_codes = []
+        council_gss = None
+        for area in res_json['areas']:
+            if 'gss' in res_json['areas'][area]['codes']:
+                gss = res_json['areas'][area]['codes']['gss']
+                gss_codes.append(gss)
+                if res_json['areas'][area]['type'] in COUNCIL_TYPES:
+                    council_gss = gss
+
+        return {
+            'source': 'mapit',
+            'wgs84_lon': res_json['wgs84_lon'],
+            'wgs84_lat': res_json['wgs84_lat'],
+            'gss_codes': gss_codes,
+            'council_gss': council_gss,
+        }
+
+
+class AddressBaseWrapper:
+
+    def __init__(self, postcode):
+        self.postcode = self.format_postcode(postcode)
+
+    def format_postcode(self, postcode):
+        # postcodes in AddressBase are in format AA1 1AA
+        # ensure postcode is formatted correctly before we try to query
+        postcode = re.sub('[^A-Z0-9]', '', postcode.upper())
+        postcode = postcode[:-3] + ' ' + postcode[-3:]
+        return postcode
+
+    def get_uprns(self, addresses):
+        return [a.uprn for a in addresses]
+
+    def get_codes(self, uprns):
+        addresses = Onsad.objects.filter(uprn__in=uprns)
+
+        if len(addresses) == 0:
+            # No records in the ONSAD table were found for the given UPRNs
+            # because...reasons
+            raise CodesNotFoundException('Found no records in ONSAD for supplied UPRNs')
+
+        if len(addresses) != len(uprns):
+            # For the moment I'm going to do nothing about this, but lets
+            # leave this condition here to make that decision explicit
+            # TODO: maybe we should actually do....something else??
+            pass
+
+        council_ids = set([a.lad for a in addresses])
+
+        # assemble list of codes
+        gss_codes = set()
+        for address in addresses:
+            extra_codes = [address.cty, address.lad, address.ctry, address.rgn, address.eer]
+            for code in extra_codes:
+                gss_codes.add(code)
+
+        if len(council_ids) == 1:
+            # all the uprns supplied are in the same local authority
+            council_gss = list(council_ids)[0]
+        else:
+            # the urpns supplied are in multiple local authorities
+            raise MultipleCouncilsException('Postcode %s covers UPRNs in more than one local authority' % (self.postcode))
+
+        return {
+            'council_gss': council_gss,
+            'gss_codes': list(gss_codes)
+        }
+
+    def geocode(self):
+        addresses = Address.objects.filter(postcode=self.postcode)
+        if not addresses:
+            raise ObjectDoesNotExist('No addresses found for postcode %s' % (self.postcode))
+
+        codes = self.get_codes(self.get_uprns(addresses))
+        centre = centre_from_points_qs(addresses)
+        return {
+            'source': 'addressbase',
+            'wgs84_lon': centre.x,
+            'wgs84_lat': centre.y,
+            'council_gss': codes['council_gss'],
+            'gss_codes': codes['gss_codes'],
+        }
+
+    def geocode_point_only(self):
+        addresses = Address.objects.filter(postcode=self.postcode)
+        if not addresses:
+            raise ObjectDoesNotExist('No addresses found for postcode %s' % (self.postcode))
+
+        centre = centre_from_points_qs(addresses)
+        return {
+            'source': 'addressbase',
+            'wgs84_lon': centre.x,
+            'wgs84_lat': centre.y,
+        }
+
+
 def geocode_point_only(postcode, sleep=True):
-    """
-    Try to get centre of the point from AddressBase, fall back to MapIt
-    """
-    addresses = Address.objects.filter(postcode=postcode)
-    if not addresses:
+    addressbase = AddressBaseWrapper(postcode)
+    mapit = MapitWrapper(postcode)
+
+    try:
+        # first try addressbase
+        result = addressbase.geocode_point_only()
+    except ObjectDoesNotExist:
+        # we couldn't find this postcode in AddressBase: fall back to mapit
+
         # optional sleep to avoid hammering mapit
         if sleep:
             time.sleep(1.3)
-        return geocode(postcode)
 
-    centre = centre_from_points_qs(addresses)
-    return {
-        'wgs84_lon': centre.x,
-        'wgs84_lat': centre.y,
-    }
+        result = mapit.geocode()
+    except:
+        # something else went wrong: lets give mapit a go anyway
+
+        # optional sleep to avoid hammering mapit
+        if sleep:
+            time.sleep(1.3)
+
+        result = mapit.geocode()
+
+    return result
+
 
 def geocode(postcode):
-    """
-    Use MaPit to convert the postcode to a location and constituency
-    """
+    addressbase = AddressBaseWrapper(postcode)
+    mapit = MapitWrapper(postcode)
 
-    COUNCIL_TYPES = getattr(settings, 'COUNCIL_TYPES', [])
+    try:
+        # first try addressbase
+        result = addressbase.geocode()
+    except ObjectDoesNotExist:
+        # we couldn't find this postcode in AddressBase: fall back to mapit
+        result = mapit.geocode()
+    except CodesNotFoundException:
+        # we did find this postcode in AddressBase, but there were no
+        # corresponding codes in ONSAD: fall back to mapit
+        result = mapit.geocode()
+    except MultipleCouncilsException:
+        # this postcode contains uprns in multiple local authorities
+        # re-raise the exception.
+        raise
+    except:
+        # something else went wrong: lets give mapit a go anyway
+        result = mapit.geocode()
 
-    headers = {}
-    if settings.MAPIT_UA:
-        headers['User-Agent'] = settings.MAPIT_UA
-
-    res = requests.get("%s/postcode/%s" % (settings.MAPIT_URL, postcode), headers=headers)
-
-    if res.status_code != 200:
-        if res.status_code == 403:
-            # we hit MapIt's rate limit
-            raise RateLimitError("Mapit error 403: Rate limit exceeded")
-
-        if res.status_code == 404:
-            # if mapit returns 404, it returns HTML even if we requested json
-            # this will cause an unhandled exception if we try to parse it
-            raise PostcodeError("Mapit error 404: Not Found")
-
-        try:
-            # attempt to parse error from json
-            res_json = res.json()
-            if 'error' in res_json:
-                raise PostcodeError("Mapit error {}: {}".format(res_json['code'], res_json['error']))
-            else:
-                raise PostcodeError("Mapit error {}: unknown".format(res.status_code))
-        except ValueError:
-            # if we fail to parse json, raise a less specific exception
-            raise PostcodeError("Mapit error {}: unknown".format(res.status_code))
-
-    res_json = res.json()
-
-    gss_codes = []
-    council_gss = None
-    for area in res_json['areas']:
-        if 'gss' in res_json['areas'][area]['codes']:
-            gss = res_json['areas'][area]['codes']['gss']
-            gss_codes.append(gss)
-            if res_json['areas'][area]['type'] in COUNCIL_TYPES:
-                council_gss = gss
-
-    return {
-        'wgs84_lon': res_json['wgs84_lon'],
-        'wgs84_lat': res_json['wgs84_lat'],
-        'gss_codes': gss_codes,
-        'council_gss': council_gss,
-    }
+    return result
 
 
 # sort a list of tuples by key in natural/human order
