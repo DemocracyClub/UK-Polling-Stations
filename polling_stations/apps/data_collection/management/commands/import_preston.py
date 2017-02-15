2d8c9c34b5f902efd5d6120dff3cf24bfa80a763
chris48s <chris.shaw480@gmail.com>
2017-02-15 21:06:29 +0000

    add import script for Preston

---
+++ polling_stations/apps/data_collection/management/commands/import_preston.py
@@ -0,0 +1,8 @@
+from data_collection.management.commands import BaseXpressCsvImporter
+
+class Command(BaseXpressCsvImporter):
+    council_id      = 'E07000123'
+    addresses_name  = 'PrestonPropertyPostCodePollingStationWebLookup-2017-02-08.TSV'
+    stations_name   = 'PrestonPropertyPostCodePollingStationWebLookup-2017-02-08.TSV'
+    elections       = ['local.lancashire.2017-05-04']
+    csv_delimiter = '\t'
