5e1eecec9533b5f8a52aaa90619895c58393ece4
chris48s <chris.shaw480@gmail.com>
2017-02-19 16:05:51 +0000

    Build up a set of address objects instead of inserting one-by-one
    This achieves several things:
    - We can use bulk_create to make the database IO a bit faster
    - This is now easier to test because make_addresses_for_postcode
      modifies a property we can inspect instead of producing side effects
    - We can move away from using create_or_update, which can potentially
      mask errors by silently overwriting when it should really raise
      an exception. This will become important when we are also inserting
      data into neighbouring councils.
    
    Also fixes some miscounting in the postcode_report var

--- polling_stations/apps/addressbase/helpers.py
+++ polling_stations/apps/addressbase/helpers.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from pollingstations.models import (PollingDistrict, ResidentialAddress,
                                     PollingStation)
 from addressbase.models import Address
@@ -18,6 +19,84 @@ def centre_from_points_qs(qs):
     return poly.centroid
 
 
+AddressTuple = namedtuple('Address', [
+    'address',
+    'postcode',
+    'council',
+    'polling_station_id',
+    'slug'])
+
+
+class AddressSet:
+
+    def __len__(self):
+        return len(self.elements)
+
+    def __init__(self):
+        self.elements = set()
+
+    def add(self, address):
+        self.elements.add(address)
+
+    def save(self, batch_size):
+
+        addresses_db = []
+        for address in self.elements:
+            record = ResidentialAddress(
+                address=address.address,
+                postcode=address.postcode,
+                polling_station_id=address.polling_station_id,
+                council=address.council,
+                slug=address.slug,
+            )
+            addresses_db.append(record)
+
+        ResidentialAddress.objects.bulk_create(
+            addresses_db, batch_size=batch_size)
+
+
+class EdgeCaseFixer:
+
+    def __init__(self, target_council_id):
+        self.address_set = AddressSet()
+        self.target_council_id = target_council_id
+
+    def make_addresses_for_postcode(self, postcode):
+        addresses = Address.objects.filter(postcode=postcode)
+        for address in addresses:
+            try:
+                district = PollingDistrict.objects.get(
+                    area__covers=address.location, council_id=self.target_council_id)
+            except PollingDistrict.DoesNotExist:
+                # Chances are this is on the edge of the council area, and
+                # we don't have data for the are the property is in
+                continue
+            except PollingDistrict.MultipleObjectsReturned:
+                # This is normally causes by districts the overlap
+                # Because we have no way of knowing what the correct district is,
+                # we need to ignore this address
+                continue
+
+            polling_station = PollingStation.objects.get_polling_station(
+                district.council.pk,
+                polling_district=district)
+
+            if not polling_station:
+                # We do not know which station this district is served by, ignore it
+                continue
+
+            self.address_set.add(AddressTuple(
+                address.address,
+                postcode,
+                district.council,
+                polling_station.internal_council_id,
+                address.uprn,
+            ))
+
+    def get_address_set(self):
+        return self.address_set
+
+
 def district_contains_all_points(district, points):
     return all([district.area.contains(p) for p in points])
 
@@ -36,59 +115,26 @@ def postcodes_not_contained_by_district(district):
     return data
 
 
-def make_addresses_for_postcode(postcode, council_id):
-    addresses = Address.objects.filter(postcode=postcode)
-    created = 0
-    for address in addresses:
-        try:
-            district = PollingDistrict.objects.get(
-                area__covers=address.location, council_id=council_id)
-        except PollingDistrict.DoesNotExist:
-            # Chances are this is on the edge of the council area, and
-            # we don't have data for the are the property is in
-            continue
-        except PollingDistrict.MultipleObjectsReturned:
-            # This is normally causes by districts the overlap
-            # Because we have no way of knowing what the correct district is,
-            # we need to ignore this address
-            continue
-
-        polling_station = PollingStation.objects.get_polling_station(
-            district.council.pk,
-            polling_district=district)
-
-        if not polling_station:
-            # This station doesn't fall inside a district, ignore it
-            continue
-
-        residential_address, _ = ResidentialAddress.objects.get_or_create(
-            slug=address.uprn,
-            defaults={
-                'address': address.address,
-                'postcode': postcode,
-                'council': district.council,
-                'polling_station_id': polling_station.internal_council_id,
-            }
-        )
-        created += 1
-    return created
-
-def create_address_records_for_council(council):
+def create_address_records_for_council(council, batch_size):
     postcode_report = {
         'no_attention_needed': 0,
         'addresses_created': 0,
         'postcodes_needing_address_lookup': set(),
     }
 
+    fixer = EdgeCaseFixer(council.pk)
     for district in PollingDistrict.objects.filter(council=council):
         data = postcodes_not_contained_by_district(district)
 
         postcode_report['no_attention_needed'] += \
             data['total'] - len(data['not_contained'])
+        postcode_report['postcodes_needing_address_lookup'].update(data['not_contained'])
 
         for postcode in data['not_contained']:
-            postcode_report['postcodes_needing_address_lookup'].add(postcode)
-            created = make_addresses_for_postcode(postcode, council.pk)
-            postcode_report['addresses_created'] = created
+            fixer.make_addresses_for_postcode(postcode)
+
+    address_set = fixer.get_address_set()
+    address_set.save(batch_size)
+    postcode_report['addresses_created'] = len(address_set)
 
     return postcode_report
