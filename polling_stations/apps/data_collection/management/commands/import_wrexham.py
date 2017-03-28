e3042aea4c9ebdac7f96fe204f32af5eddc6e4c1
chris48s <chris.shaw480@gmail.com>
2017-03-28 09:20:05 +0100

    add import script for Wrexham

---
+++ polling_stations/apps/data_collection/management/commands/import_wrexham.py
@@ -0,0 +1,8 @@
+from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
+
+class Command(BaseXpressDemocracyClubCsvImporter):
+    council_id = 'W06000006'
+    addresses_name = 'WrexhamDemocracy_Club__04May2017 (2).tsv'
+    stations_name = 'WrexhamDemocracy_Club__04May2017 (2).tsv'
+    elections = ['local.wrexham.2017-05-04']
+    csv_delimiter = '\t'
