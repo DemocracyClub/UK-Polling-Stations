1596d091183d89c703e67555e81f24722dc0d8a2
chris48s <chris.shaw480@gmail.com>
2017-03-28 09:17:45 +0100

    add import script for Chelmsford

---
+++ polling_stations/apps/data_collection/management/commands/import_chelmsford.py
@@ -0,0 +1,9 @@
+from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
+
+class Command(BaseXpressDemocracyClubCsvImporter):
+    council_id = 'E07000070'
+    addresses_name = 'Democracy_Club__04May2017 (1).tsv'
+    stations_name = 'Democracy_Club__04May2017 (1).tsv'
+    elections = ['local.essex.2017-05-04']
+    csv_delimiter = '\t'
+    csv_encoding = 'latin-1'
