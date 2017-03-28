4c71d402990fd05d80c98b35fa1a9985a0722e95
chris48s <chris.shaw480@gmail.com>
2017-03-28 09:17:04 +0100

    add import script for Ashford

---
+++ polling_stations/apps/data_collection/management/commands/import_ashford.py
@@ -0,0 +1,7 @@
+from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
+
+class Command(BaseXpressDemocracyClubCsvImporter):
+    council_id = 'E07000105'
+    addresses_name = 'Ashford_Democracy_Club__04May2017.CSV'
+    stations_name = 'Ashford_Democracy_Club__04May2017.CSV'
+    elections = ['local.kent.2017-05-04']
