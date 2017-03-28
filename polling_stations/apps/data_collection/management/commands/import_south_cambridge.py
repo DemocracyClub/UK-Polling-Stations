786af73b3c4741454bcb29e86dc3298c7ba65fb5
chris48s <chris.shaw480@gmail.com>
2017-03-28 09:16:50 +0100

    add import script for South Cambridgeshire

--- polling_stations/apps/data_collection/management/commands/import_south_cambridge.py
+++ polling_stations/apps/data_collection/management/commands/import_south_cambridge.py
@@ -1,48 +1,10 @@
-"""
-Import South Cambridge
-"""
+from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
 
-from data_collection.management.commands import BaseShpStationsShpDistrictsImporter
-
-class Command(BaseShpStationsShpDistrictsImporter):
-    """
-    Imports the Polling Station data from South Cambridge
-    """
-    srid           = 27700
-    districts_srid = 4326
-    council_id     = 'E07000012'
-    # districts have not changed since 2015
-    districts_name = 'rev01-2015/Polling Districts for Twitter_region'
-    # new polling station data provided for 2016
-    stations_name  = 'rev02-2016/Polling Stations May 2016_point.shp'
-    elections      = [
-        'pcc.2016-05-05',
-        'ref.2016-06-23'
+class Command(BaseXpressDemocracyClubCsvImporter):
+    council_id = 'E07000012'
+    addresses_name = 'May 2017/Democracy_Club__04May2017 (1).CSV'
+    stations_name = 'May 2017/Democracy_Club__04May2017 (1).CSV'
+    elections = [
+        'local.cambridgeshire.2017-05-04',
+        'mayor.cambridgeshire-and-peterborough.2017-05-04'
     ]
-
-    def district_record_to_dict(self, record):
-        return {
-            'internal_council_id': record[1],
-            'name': record[0],
-            'polling_station_id': record[1]
-        }
-
-    def station_record_to_dict(self, record):
-
-        # format address
-        address_parts = record[1].split(" ")
-        if address_parts[-1] != 'Chishill' and address_parts[-1] != 'Hinto' and address_parts[-1] != 'CB8':
-            postcode = "%s %s" % (address_parts[-2], address_parts[-1])
-            del(address_parts[-1])
-            del(address_parts[-1])
-        else:
-            postcode = ''
-
-        address = " ".join(address_parts)
-        address = "\n".join(address.split(", "))
-
-        return {
-            'internal_council_id': record[-1],
-            'postcode'           : postcode,
-            'address'            : address
-        }
