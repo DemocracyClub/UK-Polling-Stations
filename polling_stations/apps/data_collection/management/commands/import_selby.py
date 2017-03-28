c1a378adcfd4ccccc44b0c9272e84a765f61f88a
chris48s <chris.shaw480@gmail.com>
2017-03-28 09:18:42 +0100

    add import script for Selby

---
+++ polling_stations/apps/data_collection/management/commands/import_selby.py
@@ -0,0 +1,8 @@
+from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
+
+class Command(BaseXpressDemocracyClubCsvImporter):
+    council_id = 'E07000169'
+    addresses_name = 'SelbyDemocracy_Club__04May2017.tsv'
+    stations_name = 'SelbyDemocracy_Club__04May2017.tsv'
+    elections = ['local.north-yorkshire.2017-05-04']
+    csv_delimiter = '\t'
