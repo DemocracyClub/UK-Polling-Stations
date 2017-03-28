2084e34ea8998e898edb1411238c030bdff95892
chris48s <chris.shaw480@gmail.com>
2017-03-28 13:29:28 +0100

    add import script for St Edmundsbury

---
+++ polling_stations/apps/data_collection/management/commands/import_st_edmundsbury.py
@@ -0,0 +1,7 @@
+from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
+
+class Command(BaseXpressDemocracyClubCsvImporter):
+    council_id = 'E07000204'
+    addresses_name = 'St_Edmundsbury_split.csv'
+    stations_name = 'St_Edmundsbury_split.csv'
+    elections = ['local.suffolk.2017-05-04']
