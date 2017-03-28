46b8fd76d02843c94238cbe4447bffd606214052
chris48s <chris.shaw480@gmail.com>
2017-03-28 09:18:12 +0100

    add import script for Mid Devon

---
+++ polling_stations/apps/data_collection/management/commands/import_mid_devon.py
@@ -0,0 +1,8 @@
+from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
+
+class Command(BaseXpressDemocracyClubCsvImporter):
+    council_id = 'E07000042'
+    addresses_name = 'MidDevon_Democracy_Club__04May2017.tsv'
+    stations_name = 'MidDevon_Democracy_Club__04May2017.tsv'
+    elections = ['local.devon.2017-05-04']
+    csv_delimiter = '\t'
