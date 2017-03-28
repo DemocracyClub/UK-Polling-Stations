35b5ef2d39363e893796a8384209034093d8a11e
chris48s <chris.shaw480@gmail.com>
2017-03-28 11:21:49 +0100

    add import script for Burnley

---
+++ polling_stations/apps/data_collection/management/commands/import_burnley.py
@@ -0,0 +1,8 @@
+from data_collection.management.commands import BaseXpressWebLookupCsvImporter
+
+class Command(BaseXpressWebLookupCsvImporter):
+    council_id      = 'E07000117'
+    # note: extension is TSV, but file is actually comma seperated
+    addresses_name  = 'BurnleyPropertyPostCodePollingStationWebLookup-2017-03-10.TSV'
+    stations_name   = 'BurnleyPropertyPostCodePollingStationWebLookup-2017-03-10.TSV'
+    elections       = ['local.lancashire.2017-05-04']
