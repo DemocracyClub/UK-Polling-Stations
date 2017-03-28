6a2ec6ce7a1a208352e756daedf689662a2a8c4c
chris48s <chris.shaw480@gmail.com>
2017-03-28 14:16:02 +0100

    update Tendring import script for new data

--- polling_stations/apps/data_collection/management/commands/import_tendring.py
+++ polling_stations/apps/data_collection/management/commands/import_tendring.py
@@ -3,8 +3,8 @@ from data_collection.management.commands import BaseShpStationsShpDistrictsImpor
 class Command(BaseShpStationsShpDistrictsImporter):
     srid = 27700
     council_id = 'E07000076'
-    districts_name = 'PollingDistricts2016'
-    stations_name = 'Polling_Stations_Tendring_2017.shp'
+    districts_name = 'Updated everything 27 Mar 2017/PollingDistricts2016'
+    stations_name = 'Updated everything 27 Mar 2017/Polling_Stations_Tendring_2017.shp'
     elections = ['local.essex.2017-05-04']
 
     def district_record_to_dict(self, record):
