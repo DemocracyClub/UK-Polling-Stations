71bc0d330af84e0ae5d7100cf7767ab296daee88
chris48s <chris.shaw480@gmail.com>
2017-03-29 09:43:01 +0100

    add import script for Broxtowe

---
+++ polling_stations/apps/data_collection/management/commands/import_broxtowe.py
@@ -0,0 +1,28 @@
+from data_collection.management.commands import BaseShpStationsShpDistrictsImporter
+
+class Command(BaseShpStationsShpDistrictsImporter):
+    srid = 27700
+    council_id = 'E07000172'
+    districts_name = 'Broxtowe_Polling_Information'
+    stations_name = 'Broxtowe_Polling_Stations.shp'
+    elections = ['local.nottinghamshire.2017-05-04']
+
+    def district_record_to_dict(self, record):
+        return {
+            'internal_council_id': record[5].strip(),
+            'name': record[5].strip(),
+            'polling_station_id': record[5].strip(),
+        }
+
+    def station_record_to_dict(self, record):
+        # attempt to lay the address out a bit more nicely
+        address = record[2]\
+            .strip()\
+            .replace(" Nottinghamshire ", "\nNottinghamshire\n")\
+            .replace(" Nottinghamshire", "\nNottinghamshire")
+
+        return {
+            'internal_council_id': record[4].strip(),
+            'postcode': record[3].strip(),
+            'address': address,
+        }
