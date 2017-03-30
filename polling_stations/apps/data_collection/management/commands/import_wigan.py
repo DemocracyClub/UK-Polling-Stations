d45d6fdf49a85c92ad53c81d5a25b71f6d0d413a
chris48s <chris.shaw480@gmail.com>
2017-03-30 17:07:00 +0100

    update Wigan data

--- polling_stations/apps/data_collection/management/commands/import_wigan.py
+++ polling_stations/apps/data_collection/management/commands/import_wigan.py
@@ -2,6 +2,6 @@ from data_collection.management.commands import BaseXpressWebLookupCsvImporter
 
 class Command(BaseXpressWebLookupCsvImporter):
     council_id      = 'E08000010'
-    addresses_name  = 'WiganPropertyPollingStation_master.csv'
-    stations_name   = 'WiganPropertyPollingStation_master.csv'
+    addresses_name  = 'WiganNewPropertyPollingStation_master.csv'
+    stations_name   = 'WiganNewPropertyPollingStation_master.csv'
     elections       = ['mayor.greater-manchester-ca.2017-05-04']
