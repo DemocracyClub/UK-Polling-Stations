bb1637024c51a74829ee13d3a9155e277051efea
chris48s <chris.shaw480@gmail.com>
2017-04-03 09:33:07 +0100

    update Cotswold import script

--- polling_stations/apps/data_collection/management/commands/import_cotswold.py
+++ polling_stations/apps/data_collection/management/commands/import_cotswold.py
@@ -2,7 +2,7 @@ from data_collection.management.commands import BaseXpressWebLookupCsvImporter
 
 class Command(BaseXpressWebLookupCsvImporter):
     council_id = 'E07000079'
-    addresses_name = 'CotswoldsPropertyPostCodePollingStationWebLookup-2017-02-28.TSV'
-    stations_name = 'CotswoldsPropertyPostCodePollingStationWebLookup-2017-02-28.TSV'
+    addresses_name = 'CotswoldPropertyPostCodePollingStationWebLookup-2017-03-27.TSV'
+    stations_name = 'CotswoldPropertyPostCodePollingStationWebLookup-2017-03-27.TSV'
     elections = ['local.gloucestershire.2017-05-04']
     csv_delimiter = '\t'
