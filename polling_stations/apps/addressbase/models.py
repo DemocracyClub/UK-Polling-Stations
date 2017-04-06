6dc4e55a87d4629b6df3bdf8d6bd4d47351889cf
chris48s <chris.shaw480@gmail.com>
2017-04-06 11:05:35 +0100

    add model for storing postcode blacklist

--- polling_stations/apps/addressbase/models.py
+++ polling_stations/apps/addressbase/models.py
@@ -60,3 +60,17 @@ class Onsad(models.Model):
     lep2 = models.CharField(blank=True, max_length=9)
     pfa = models.CharField(blank=True, max_length=9)
     imd = models.CharField(blank=True, max_length=5)
+
+
+class Blacklist(models.Model):
+    """
+    Model for storing postcodes containing UPRNs in >1 local authorities
+    This is intentionally de-normalised for performance reasons
+    Ideally ('postcode', 'lad') should be a composite PK,
+    but django's ORM doesn't support them.
+    """
+    postcode = models.CharField(blank=False, max_length=15, db_index=True)
+    lad = models.CharField(blank=False, max_length=9)
+
+    class Meta:
+        unique_together = (('postcode', 'lad'))
