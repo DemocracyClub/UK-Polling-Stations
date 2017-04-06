be95b3be0e2ab8fd43501f8c56ad30e01990182e
chris48s <chris.shaw480@gmail.com>
2017-04-06 13:58:17 +0100

    add tests for new conditions in RoutingHelper

--- polling_stations/apps/data_finder/tests/test_routing_helper.py
+++ polling_stations/apps/data_finder/tests/test_routing_helper.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
+from addressbase.models import Blacklist
 from data_finder.helpers import RoutingHelper
+
 
 class RoutingHelperTest(TestCase):
 
@@ -19,3 +21,32 @@ class RoutingHelperTest(TestCase):
         rh = RoutingHelper('CC11CC')
         endpoint = rh.get_endpoint()
         self.assertEqual('postcode_view', endpoint.view)
+
+    def test_multiple_councils_view(self):
+        # check we are directed to multiple_councils_view if
+        # postcode is attached to multiple councils in the blacklist
+        rh = RoutingHelper('DD11DD')
+        endpoint = rh.get_endpoint()
+        self.assertEqual('multiple_councils_view', endpoint.view)
+
+    def test_multiple_councils_view_override(self):
+        # insert blacklist records for postcodes matching other conditions
+        Blacklist.objects.create(postcode='AA11AA', lad='X01000001')
+        Blacklist.objects.create(postcode='AA11AA', lad='W06000022')
+        Blacklist.objects.create(postcode='BB11BB', lad='X01000001')
+        Blacklist.objects.create(postcode='BB11BB', lad='W06000022')
+        Blacklist.objects.create(postcode='CC11CC', lad='X01000001')
+        Blacklist.objects.create(postcode='CC11CC', lad='W06000022')
+
+        # check the blacklist overrides all other conditions
+        rh = RoutingHelper('AA11AA')
+        endpoint = rh.get_endpoint()
+        self.assertEqual('multiple_councils_view', endpoint.view)
+
+        rh = RoutingHelper('BB11BB')
+        endpoint = rh.get_endpoint()
+        self.assertEqual('multiple_councils_view', endpoint.view)
+
+        rh = RoutingHelper('CC11CC')
+        endpoint = rh.get_endpoint()
+        self.assertEqual('multiple_councils_view', endpoint.view)
