30392a6a50f0d33a2d8ee11d32517c0320a163fb
chris48s <chris.shaw480@gmail.com>
2017-04-07 14:02:35 +0100

    add test case for 'No location information' condition in MapitWrapper

--- polling_stations/apps/data_finder/tests/test_mapit_wrapper.py
+++ polling_stations/apps/data_finder/tests/test_mapit_wrapper.py
@@ -1,22 +1,28 @@
 import json
 from os.path import abspath, dirname
 from django.test import TestCase
-from data_finder.helpers import MapitWrapper
+from data_finder.helpers import MapitWrapper, PostcodeError
 
 
-class MapitWrapperMock(MapitWrapper):
+class MapitWrapperValidMock(MapitWrapper):
 
-    # Mock out the HTTP response from mapit so it returns a known value
+    # Mock out the HTTP response from mapit so it returns a known good value
     def call_mapit(self):
         return json.load(open(abspath(
             dirname(__file__) + '/../fixtures/mapit_responses/SW1A1AA.json'
         )))
 
+class MapitWrapperNoLocationMock(MapitWrapper):
 
-class MapitWrapperTest(TestCase):
+    # Mock out the HTTP response from mapit so it returns a known bad value
+    def call_mapit(self):
+        return {"postcode": "JE3 7DW", "areas": {}}
+
+
+class MapitWrapperValidTest(TestCase):
 
     def test_mapit_wrapper(self):
-        mapit = MapitWrapperMock('SW1A 1AA')
+        mapit = MapitWrapperValidMock('SW1A 1AA')
         result = mapit.geocode()
         self.assertEqual('mapit', result['source'])
         self.assertEqual(-0.14158760012261312, result['wgs84_lon'])
@@ -26,3 +32,9 @@ class MapitWrapperTest(TestCase):
             ['E14000639', 'E15000007', 'E09000033', 'E32000014', 'E05000644'],
             result['gss_codes']
         )
+
+class MapitWrapperNoLocationTest(TestCase):
+
+    def test_mapit_wrapper(self):
+        mapit = MapitWrapperNoLocationMock('JE3 7DW')
+        self.assertRaises(PostcodeError, mapit.geocode)
