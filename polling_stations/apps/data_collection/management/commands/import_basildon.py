a1308e14eb843c1844a1bab38bdda33c4395f53b
chris48s <chris.shaw480@gmail.com>
2017-04-09 11:27:34 +0100

    add import script for Basildon

---
+++ polling_stations/apps/data_collection/management/commands/import_basildon.py
@@ -0,0 +1,8 @@
+from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
+
+class Command(BaseXpressDemocracyClubCsvImporter):
+    council_id = 'E07000066'
+    addresses_name = 'Giant TSV instead/BasildonDemocracy_Club__04May2017 (5).tsv'
+    stations_name = 'Giant TSV instead/BasildonDemocracy_Club__04May2017 (5).tsv'
+    elections = ['local.essex.2017-05-04']
+    csv_delimiter = '\t'
