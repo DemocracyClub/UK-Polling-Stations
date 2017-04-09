9648c7ec335ab0f3962d1ce27cedeb15cef3048e
chris48s <chris.shaw480@gmail.com>
2017-04-09 11:26:42 +0100

    update Manchester import script

--- polling_stations/apps/data_collection/management/commands/import_manchester.py
+++ polling_stations/apps/data_collection/management/commands/import_manchester.py
@@ -2,7 +2,7 @@ from data_collection.management.commands import BaseXpressWebLookupCsvImporter
 
 class Command(BaseXpressWebLookupCsvImporter):
     council_id      = 'E08000003'
-    addresses_name  = 'ManchesterPropertyPostCodePollingStationWebLookup-2017-02-16.TSV'
-    stations_name   = 'ManchesterPropertyPostCodePollingStationWebLookup-2017-02-16.TSV'
+    addresses_name  = 'ManchesterPropertyPostCodePollingStationWebLookup-2017-04-06.TSV'
+    stations_name   = 'ManchesterPropertyPostCodePollingStationWebLookup-2017-04-06.TSV'
     elections       = ['mayor.greater-manchester.2017-05-04']
     csv_delimiter = '\t'
