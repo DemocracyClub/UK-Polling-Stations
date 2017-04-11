d0d33e720457eccac62f4f2001756bd28ec1479b
chris48s <chris.shaw480@gmail.com>
2017-04-11 18:34:24 +0100

    update Roachdale import script

--- polling_stations/apps/data_collection/management/commands/import_rochdale.py
+++ polling_stations/apps/data_collection/management/commands/import_rochdale.py
@@ -1,8 +1,8 @@
-from data_collection.management.commands import BaseXpressWebLookupCsvImporter
+from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
 
-class Command(BaseXpressWebLookupCsvImporter):
+class Command(BaseXpressDemocracyClubCsvImporter):
     council_id      = 'E08000005'
-    addresses_name  = 'May 2017/RochdalePropertyPostCodePollingStationWebLookup-2017-03-15.TSV'
-    stations_name   = 'May 2017/RochdalePropertyPostCodePollingStationWebLookup-2017-03-15.TSV'
+    addresses_name  = 'Rochdale_Democracy_Club__04May2017 (2).CSV'
+    stations_name   = 'Rochdale_Democracy_Club__04May2017 (2).CSV'
     elections       = ['mayor.greater-manchester-ca.2017-05-04']
-    csv_delimiter = '\t'
+    csv_delimiter = ','
