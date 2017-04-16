357daf0a6d1731f426ad896b227e5f688d6d83e9
chris48s <chris.shaw480@gmail.com>
2017-04-16 14:06:53 +0100

    add import script for Torridge

---
+++ polling_stations/apps/data_collection/management/commands/import_torridge.py
@@ -0,0 +1,7 @@
+from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
+
+class Command(BaseXpressDemocracyClubCsvImporter):
+    council_id = 'E07000046'
+    addresses_name = 'Democracy_Club__04May2017 (3).CSV'
+    stations_name = 'Democracy_Club__04May2017 (3).CSV'
+    elections = ['local.devon.2017-05-04']
