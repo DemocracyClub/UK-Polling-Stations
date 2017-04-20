d3a9a4a300acc204111e19945381a995f0f7cdda
chris48s <chris.shaw480@gmail.com>
2017-04-20 15:30:36 +0100

    add import script for Dumfries and Galloway

---
+++ polling_stations/apps/data_collection/management/commands/import_dumfries_and_galloway.py
@@ -0,0 +1,6 @@
+from data_collection.management.commands import BaseScotlandSpatialHubImporter
+
+class Command(BaseScotlandSpatialHubImporter):
+    council_id = 'S12000006'
+    council_name = 'Dumfries and Galloway'
+    elections = ['local.dumfries-and-galloway.2017-05-04']
