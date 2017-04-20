96897c161681ac68ecafb3d1e5f513c1287d7587
chris48s <chris.shaw480@gmail.com>
2017-04-20 15:31:26 +0100

    add import script for Eilean Siar

---
+++ polling_stations/apps/data_collection/management/commands/import_eilean_siar.py
@@ -0,0 +1,21 @@
+from data_collection.management.commands import BaseScotlandSpatialHubImporter
+
+"""
+Note:
+This importer provides coverage for 45/47 districts
+due to incomplete/poor quality data
+"""
+class Command(BaseScotlandSpatialHubImporter):
+    council_id = 'S12000013'
+    council_name = 'Eilean Siar'
+    elections = ['local.eilean-siar.2017-05-04']
+
+    def station_record_to_dict(self, record):
+        # bless 'em
+        record[1] = self.parse_string(record[1]).replace('O', '0')
+
+        # exclude duplicate district code
+        if record[1] == 'E09E':
+            return None
+
+        return super().station_record_to_dict(record)
