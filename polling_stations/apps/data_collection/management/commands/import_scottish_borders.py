cd3b7d4e6c549d3907cd4105e2b3be27337e8268
chris48s <chris.shaw480@gmail.com>
2017-04-20 15:32:53 +0100

    add import script for Scottish Borders

---
+++ polling_stations/apps/data_collection/management/commands/import_scottish_borders.py
@@ -0,0 +1,6 @@
+from data_collection.management.commands import BaseScotlandSpatialHubImporter
+
+class Command(BaseScotlandSpatialHubImporter):
+    council_id = 'S12000026'
+    council_name = 'Scottish Borders'
+    elections = ['local.scottish-borders.2017-05-04']
