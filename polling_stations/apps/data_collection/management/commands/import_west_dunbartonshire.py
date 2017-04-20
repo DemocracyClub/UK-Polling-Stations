fed07576978a39d646b733d37382f0260e8290ef
chris48s <chris.shaw480@gmail.com>
2017-04-20 15:33:04 +0100

    add import script for West Dunbartonshire

---
+++ polling_stations/apps/data_collection/management/commands/import_west_dunbartonshire.py
@@ -0,0 +1,6 @@
+from data_collection.management.commands import BaseScotlandSpatialHubImporter
+
+class Command(BaseScotlandSpatialHubImporter):
+    council_id = 'S12000039'
+    council_name = 'West Dunbartonshire'
+    elections = ['local.west-dunbartonshire.2017-05-04']
