2e16f21163775085f3cd965ca0d3458d3bd111fa
chris48s <chris.shaw480@gmail.com>
2017-04-21 14:24:14 +0100

    add import script for Harborough

---
+++ polling_stations/apps/data_collection/management/commands/import_harborough.py
@@ -0,0 +1,8 @@
+from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
+
+class Command(BaseXpressDemocracyClubCsvImporter):
+    council_id = 'E07000131'
+    addresses_name = 'Democracy_Club__04May2017 Harborough DC.TSV'
+    stations_name = 'Democracy_Club__04May2017 Harborough DC.TSV'
+    elections = ['local.leicestershire.2017-05-04']
+    csv_delimiter = '\t'
