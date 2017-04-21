0b989baf05ed41970f3572b4d711f81260fb9688
chris48s <chris.shaw480@gmail.com>
2017-04-21 15:25:08 +0100

    add import script for West Devon

---
+++ polling_stations/apps/data_collection/management/commands/import_west_devon.py
@@ -0,0 +1,8 @@
+from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
+
+class Command(BaseXpressDemocracyClubCsvImporter):
+    council_id = 'E07000047'
+    addresses_name = 'Democracy_Club__04May2017 - west devon.TSV'
+    stations_name = 'Democracy_Club__04May2017 - west devon.TSV'
+    elections = ['local.devon.2017-05-04']
+    csv_delimiter = '\t'
