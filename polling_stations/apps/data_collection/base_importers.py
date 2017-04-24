291cafea11f5b7a377dd834868650ed51af6fe53
chris48s <chris.shaw480@gmail.com>
2017-04-24 20:50:33 +0100

    set Scotland importers to be run in series

--- polling_stations/apps/data_collection/base_importers.py
+++ polling_stations/apps/data_collection/base_importers.py
@@ -598,6 +598,7 @@ class BaseScotlandSpatialHubImporter(BaseShpStationsShpDistrictsImporter,
     districts_name = 'polling_districts_20170406/polling_districts_20170406.shp'
     stations_name = 'polling_places_20170407/polling_places_20170407.shp'
     data_prefix = 'Scotland May 2017'
+    run_in_series = True
 
     @property
     @abc.abstractmethod
