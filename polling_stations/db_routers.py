548ba645278b248fc655a6ebe0f2dc63bb76ad85
chris48s <chris.shaw480@gmail.com>
2017-04-25 16:59:26 +0100

    route auth/authtoken traffic to logger DB

--- polling_stations/db_routers.py
+++ polling_stations/db_routers.py
@@ -1,29 +1,24 @@
 class LoggerRouter(object):
 
+    logger_apps = ['data_finder', 'auth', 'authtoken']
+
     def db_for_read(self, model, **hints):
-        if model._meta.app_label == 'data_finder':
+        if model._meta.app_label in self.logger_apps:
             return 'logger'
         return None
 
     def db_for_write(self, model, **hints):
-        if model._meta.app_label == 'data_finder':
+        if model._meta.app_label in self.logger_apps:
             return 'logger'
         return None
 
     def allow_relation(self, obj1, obj2, **hints):
-        """
-        Allow relations if a model in the auth app is involved.
-        """
         if obj1._meta.app_label == 'councils' or \
            obj2._meta.app_label == 'councils':
            return True
         return None
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
-        """
-        Make sure the auth app only appears in the 'auth_db'
-        database.
-        """
-        if app_label == 'data_finder':
+        if app_label in self.logger_apps:
             return db == 'logger'
         return None
