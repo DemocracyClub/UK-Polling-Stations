8b2949cce151e4b79e1f223dad746af63ecbfa82
chris48s <chris.shaw480@gmail.com>
2017-05-03 12:35:22 +0100

    update Aylesbury Value import script

--- polling_stations/apps/data_collection/management/commands/import_aylesbury_vale.py
+++ polling_stations/apps/data_collection/management/commands/import_aylesbury_vale.py
@@ -2,7 +2,7 @@ from data_collection.management.commands import BaseXpressWebLookupCsvImporter
 
 class Command(BaseXpressWebLookupCsvImporter):
     council_id = 'E07000004'
-    addresses_name = '2017/AylesburyValePropertyPostCodePollingStationWebLookup-2017-03-02 2.TSV'
-    stations_name = '2017/AylesburyValePropertyPostCodePollingStationWebLookup-2017-03-02 2.TSV'
+    addresses_name = '2017/PropertyPostCodePollingStationWebLookup-2017-05-03.TSV'
+    stations_name = '2017/PropertyPostCodePollingStationWebLookup-2017-05-03.TSV'
     elections = ['local.buckinghamshire.2017-05-04']
     csv_delimiter = '\t'
