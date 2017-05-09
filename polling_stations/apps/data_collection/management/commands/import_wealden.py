8b5202491e8feb2e12ca1b5f08810e99dbcf03fa
chris48s <chris.shaw480@gmail.com>
2017-05-09 09:42:38 +0100

    remove election id from Wealden script (pending feedback)

--- polling_stations/apps/data_collection/management/commands/import_wealden.py
+++ polling_stations/apps/data_collection/management/commands/import_wealden.py
@@ -4,8 +4,8 @@ class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = 'E07000065'
     addresses_name = 'May 2017/Wealden_Democracy_Club__04May2017 (2).tsv'
     stations_name = 'May 2017/Wealden_Democracy_Club__04May2017 (2).tsv'
-    elections = [
+    """elections = [
         'local.east-sussex.2017-05-04',
         'parl.2017-06-08'
-    ]
+    ]"""
     csv_delimiter = '\t'
