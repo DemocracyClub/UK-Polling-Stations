f5a5a19cbbb3ae41fb1fbca67aca889b81323540
chris48s <chris.shaw480@gmail.com>
2017-05-09 14:00:32 +0100

    update West Berkshire import script

--- polling_stations/apps/data_collection/management/commands/import_westberks.py
+++ polling_stations/apps/data_collection/management/commands/import_westberks.py
@@ -1,42 +1,8 @@
-"""
-Import West Berkshire Polling stations
-"""
+from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
 
-from data_collection.management.commands import BaseShpStationsShpDistrictsImporter
-
-class Command(BaseShpStationsShpDistrictsImporter):
-    """
-    Imports the Polling Station data from West Berkshire Council
-    """
-    council_id     = 'E06000037'
-    districts_name = 'rev02-2016/PollingDistricts'
-    stations_name  = 'rev02-2016/PollingStations.shp'
-    elections      = [
-        'pcc.2016-05-05',
-        'ref.2016-06-23'
-    ]
-
-    def district_record_to_dict(self, record):
-        return {
-            'internal_council_id': record[4],
-            'name': "%s - %s" % (record[2], record[4]),
-            'polling_station_id': record[4]
-        }
-
-    def station_record_to_dict(self, record):
-
-        address_parts = record[5].split(' ')
-        if address_parts[-1] != 'Newbury' and address_parts[-1] != 'Reading':
-            postcode = "%s %s" % (address_parts[-2], address_parts[-1])
-            del(address_parts[-1])
-            del(address_parts[-1])
-        else:
-            postcode = ''
-
-        address = " ".join(address_parts)
-
-        return {
-            'internal_council_id': record[4],
-            'postcode'           : postcode,
-            'address'            : address
-        }
+class Command(BaseXpressDemocracyClubCsvImporter):
+    council_id = 'E06000037'
+    addresses_name = 'parl.2017-06-08/Version 1/Democracy_Club__08June2017.tsv'
+    stations_name = 'parl.2017-06-08/Version 1/Democracy_Club__08June2017.tsv'
+    elections = ['parl.2017-06-08']
+    csv_delimiter = '\t'
