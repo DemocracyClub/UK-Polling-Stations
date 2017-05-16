167d38ae0938fe1f0ae0f7d44fd3d062a532618f
chris48s <chris.shaw480@gmail.com>
2017-05-16 18:33:01 +0100

    add import script for Oxford

---
+++ polling_stations/apps/data_collection/management/commands/import_oxford.py
@@ -0,0 +1,8 @@
+from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
+
+class Command(BaseXpressDemocracyClubCsvImporter):
+    council_id = 'E07000178'
+    addresses_name = 'parl.2017-06-08/Version 1/Democracy_Club__08June2017 (1).tsv'
+    stations_name = 'parl.2017-06-08/Version 1/Democracy_Club__08June2017 (1).tsv'
+    elections = ['parl.2017-06-08']
+    csv_delimiter = '\t'
