0c8df53ae8f85766ea273bd537e45a3da66ee853
chris48s <chris.shaw480@gmail.com>
2017-05-16 18:31:39 +0100

    update import script for Norwich for parl.2017-06-08

--- polling_stations/apps/data_collection/management/commands/import_norwich.py
+++ polling_stations/apps/data_collection/management/commands/import_norwich.py
@@ -2,8 +2,8 @@ from data_collection.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id      = 'E07000148'
-    addresses_name  = 'Norwichpolling_station_export-2017-03-17.csv'
-    stations_name   = 'Norwichpolling_station_export-2017-03-17.csv'
+    addresses_name  = 'parl.2017-06-08/Version 1/polling_station_export-2017-05-15.csv'
+    stations_name   = 'parl.2017-06-08/Version 1/polling_station_export-2017-05-15.csv'
     elections       = [
         'local.norfolk.2017-05-04',
         'parl.2017-06-08'
