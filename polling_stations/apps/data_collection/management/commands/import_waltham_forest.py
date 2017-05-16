06ab5a309f2ad0d12020a570cede6186f79f0c13
chris48s <chris.shaw480@gmail.com>
2017-05-16 18:31:49 +0100

    update import script for Waltham Forest for parl.2017-06-08

--- polling_stations/apps/data_collection/management/commands/import_waltham_forest.py
+++ polling_stations/apps/data_collection/management/commands/import_waltham_forest.py
@@ -1,78 +1,8 @@
-"""
-Import Waltham Forest
+from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
 
-note: this script takes quite a long time to run
-"""
-from django.contrib.gis.geos import Point
-from data_collection.management.commands import BaseCsvStationsCsvAddressesImporter
-from data_finder.helpers import geocode_point_only, PostcodeError
-
-class Command(BaseCsvStationsCsvAddressesImporter):
-    """
-    Imports the Polling Station data from Waltham Forest Council
-    """
-    council_id      = 'E09000031'
-    addresses_name  = 'Waltham Forest PropertyPostCodePollingStationWebLookup-2016-06-06 (1).tsv'
-    stations_name   = 'Waltham Forest polling station locations.tsv'
-    csv_delimiter   = '\t'
-    elections       = [
-        'ref.2016-06-23'
-    ]
-
-    def station_record_to_dict(self, record):
-
-        # postcode
-        if record.postcode == 'rd Lane':
-            postcode = ''
-        else:
-            postcode = record.postcode.strip()
-
-
-        # point
-        if record.easting and record.northing:
-            if 'connaught' in record.name.lower():
-                location = Point(
-                    0.004481,
-                    51.564568,
-                    srid=4326
-                )
-            else:
-                location = Point(
-                    float(record.easting),
-                    float(record.northing),
-                    srid=27700
-                )
-        else:
-            # no points supplied, so attempt to attach one by geocoding
-            try:
-                gridref = geocode_point_only(postcode)
-                location = Point(gridref['wgs84_lon'], gridref['wgs84_lat'], srid=4326)
-            except PostcodeError:
-                location = None
-
-        # format address
-        address = record.address
-        address_parts = address.split(', ')
-        address_parts = [x.strip(' ') for x in address_parts]
-        if address_parts[-1] == postcode:
-            del(address_parts[-1])
-        address = "%s\n%s" % (record.name, "\n".join(address_parts))
-
-        return {
-            'internal_council_id': record.district,
-            'postcode'           : postcode,
-            'address'            : address,
-            'location'           : location
-        }
-
-    def address_record_to_dict(self, record):
-        if record.propertynumber.strip() == '0':
-            address = record.streetname.strip()
-        else:
-            address = '%s %s' % (record.propertynumber.strip(), record.streetname.strip())
-
-        return {
-            'address'           : address,
-            'postcode'          : record.postcode.strip(),
-            'polling_station_id': record.pollingdistrictreference
-        }
+class Command(BaseXpressDemocracyClubCsvImporter):
+    council_id = 'E09000031'
+    addresses_name = 'parl.2017-06-08/Version 1/Democracy_Club__08June2017 (5).tsv'
+    stations_name = 'parl.2017-06-08/Version 1/Democracy_Club__08June2017 (5).tsv'
+    elections = ['parl.2017-06-08']
+    csv_delimiter = '\t'
