0767fc00caecc0a0354a7faa602316e3086c7bef
chris48s <chris.shaw480@gmail.com>
2017-05-16 18:31:43 +0100

    update import script for Telford for parl.2017-06-08

--- polling_stations/apps/data_collection/management/commands/import_telford.py
+++ polling_stations/apps/data_collection/management/commands/import_telford.py
@@ -1,29 +1,8 @@
-"""
-Import Telford & Wrekin Polling stations
-"""
+from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
 
-from data_collection.management.commands import BaseShpStationsShpDistrictsImporter
-
-class Command(BaseShpStationsShpDistrictsImporter):
-    """
-    Imports the Polling Station data from Telford & Wrekin Council
-    """
-    council_id     = 'E06000020'
-    districts_name = 'Polling_Districts'
-    stations_name  = 'Polling_Stations.shp'
-    elections      = ['parl.2015-05-07']
-
-    def district_record_to_dict(self, record):
-        return {
-            'internal_council_id': record[2],
-            'name': record[1],
-            'polling_station_id': record[2]
-        }
-
-    def station_record_to_dict(self, record):
-        print(record)
-        return {
-            'internal_council_id': record[1],
-            'postcode'           : record[3].split(',')[-1],
-            'address'            : "\n".join(record[3].split(',')[:-1]),
-        }
+class Command(BaseXpressDemocracyClubCsvImporter):
+    council_id = 'E06000020'
+    addresses_name = 'parl.2017-06-08/Version 1/Democracy_Club__08June2017 3.tsv'
+    stations_name = 'parl.2017-06-08/Version 1/Democracy_Club__08June2017 3.tsv'
+    elections = ['parl.2017-06-08']
+    csv_delimiter = '\t'
