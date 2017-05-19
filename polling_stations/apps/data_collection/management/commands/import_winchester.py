c5805d691a0976adee969dab77ad490e000ee5ec
chris48s <chris.shaw480@gmail.com>
2017-05-19 22:39:35 +0100

    comment out Winchester election id

--- polling_stations/apps/data_collection/management/commands/import_winchester.py
+++ polling_stations/apps/data_collection/management/commands/import_winchester.py
@@ -6,6 +6,6 @@ class Command(BaseXpressDemocracyClubCsvImporter):
     stations_name = 'WinchesterDemocracy_Club__04May2017 (2).tsv'
     elections = [
         'local.hampshire.2017-05-04',
-        'parl.2017-06-08'
+        #'parl.2017-06-08'
     ]
     csv_delimiter = '\t'
