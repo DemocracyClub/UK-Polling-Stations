eb0135828b2384f2242b3e50b244f131c59d8dda
chris48s <chris.shaw480@gmail.com>
2017-05-19 15:31:17 +0100

    update Mid Devon import script for parl.2017-06-08

--- polling_stations/apps/data_collection/management/commands/import_mid_devon.py
+++ polling_stations/apps/data_collection/management/commands/import_mid_devon.py
@@ -1,11 +1,8 @@
-from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
+from data_collection.management.commands import BaseXpressDCCsvInconsistentPostcodesImporter
 
-class Command(BaseXpressDemocracyClubCsvImporter):
+class Command(BaseXpressDCCsvInconsistentPostcodesImporter):
     council_id = 'E07000042'
-    addresses_name = 'MidDevon_Democracy_Club__04May2017.tsv'
-    stations_name = 'MidDevon_Democracy_Club__04May2017.tsv'
-    elections = [
-        'local.devon.2017-05-04',
-        'parl.2017-06-08'
-    ]
+    addresses_name = 'parl.2017-06-08/Version 1/Mid Devon Democracy_Club__08June2017-2.tsv'
+    stations_name = 'parl.2017-06-08/Version 1/Mid Devon Democracy_Club__08June2017-2.tsv'
+    elections = ['parl.2017-06-08']
     csv_delimiter = '\t'
