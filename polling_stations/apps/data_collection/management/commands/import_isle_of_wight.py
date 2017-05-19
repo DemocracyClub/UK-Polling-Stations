94efe57cd04170444c5c1c30e56a99315eb9d3cc
chris48s <chris.shaw480@gmail.com>
2017-05-19 15:31:05 +0100

    update Isle of Wight import script for parl.2017-06-08

--- polling_stations/apps/data_collection/management/commands/import_isle_of_wight.py
+++ polling_stations/apps/data_collection/management/commands/import_isle_of_wight.py
@@ -1,11 +1,8 @@
-from data_collection.management.commands import BaseXpressWebLookupCsvImporter
+from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
 
-class Command(BaseXpressWebLookupCsvImporter):
-    council_id      = 'E06000046'
-    addresses_name  = 'rev02-2017/IsleofWightPropertyPostCodePollingStationWebLookup-2017-02-07.TSV'
-    stations_name   = 'rev02-2017/IsleofWightPropertyPostCodePollingStationWebLookup-2017-02-07.TSV'
-    elections       = [
-        'local.isle-of-wight.2017-05-04',
-        'parl.2017-06-08'
-    ]
+class Command(BaseXpressDemocracyClubCsvImporter):
+    council_id = 'E06000046'
+    addresses_name = 'parl.2017-06-08/Version 1/Isle of Wight Democracy_Club__08June2017-2.tsv'
+    stations_name = 'parl.2017-06-08/Version 1/Isle of Wight Democracy_Club__08June2017-2.tsv'
+    elections = ['parl.2017-06-08']
     csv_delimiter = '\t'
