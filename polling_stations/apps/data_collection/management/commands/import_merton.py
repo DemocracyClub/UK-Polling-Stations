ecb27de227c595f089a18513b2d34fff80d27319
chris48s <chris.shaw480@gmail.com>
2017-05-20 22:21:04 +0100

    add import script for Merton (closes #794)

---
+++ polling_stations/apps/data_collection/management/commands/import_merton.py
@@ -0,0 +1,8 @@
+from data_collection.management.commands import BaseHalaroseCsvImporter
+
+class Command(BaseHalaroseCsvImporter):
+    council_id      = 'E09000024'
+    addresses_name  = 'parl.2017-06-08/Version 2/polling_station_export-2017-05-09 4.csv'
+    stations_name   = 'parl.2017-06-08/Version 2/polling_station_export-2017-05-09 4.csv'
+    elections       = ['parl.2017-06-08']
+    csv_encoding    = 'windows-1252'
