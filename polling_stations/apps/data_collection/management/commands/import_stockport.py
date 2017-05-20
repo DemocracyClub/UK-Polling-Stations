37debf4c7006a00a629c04b0941804ad65ad948e
chris48s <chris.shaw480@gmail.com>
2017-05-20 22:22:40 +0100

    update Stockport script for parl.2017-06-08 (closes #893)

--- polling_stations/apps/data_collection/management/commands/import_stockport.py
+++ polling_stations/apps/data_collection/management/commands/import_stockport.py
@@ -2,9 +2,6 @@ from data_collection.management.commands import BaseXpressWebLookupCsvImporter
 
 class Command(BaseXpressWebLookupCsvImporter):
     council_id      = 'E08000007'
-    addresses_name  = 'StockportPropertyPostCodePollingStationWebLookup-2017-02-16.CSV'
-    stations_name   = 'StockportPropertyPostCodePollingStationWebLookup-2017-02-16.CSV'
-    elections       = [
-        'mayor.greater-manchester.2017-05-04',
-        'parl.2017-06-08'
-    ]
+    addresses_name  = 'parl.2017-06-08/Version 1/PropertyPostCodePollingStationWebLookup-2017-05-18.CSV'
+    stations_name   = 'parl.2017-06-08/Version 1/PropertyPostCodePollingStationWebLookup-2017-05-18.CSV'
+    elections       = ['parl.2017-06-08']
