eaca295d3e90aa4cc3f35c295d1d7306f7eb7c8b
chris48s <chris.shaw480@gmail.com>
2017-05-20 22:24:00 +0100

    update Waltham Forest script for new data (closes #890)

--- polling_stations/apps/data_collection/management/commands/import_waltham_forest.py
+++ polling_stations/apps/data_collection/management/commands/import_waltham_forest.py
@@ -2,7 +2,7 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = 'E09000031'
-    addresses_name = 'parl.2017-06-08/Version 1/Democracy_Club__08June2017 (5).tsv'
-    stations_name = 'parl.2017-06-08/Version 1/Democracy_Club__08June2017 (5).tsv'
+    addresses_name = 'parl.2017-06-08/Version 2/Democracy_Club__08June2017 (18).tsv'
+    stations_name = 'parl.2017-06-08/Version 2/Democracy_Club__08June2017 (18).tsv'
     elections = ['parl.2017-06-08']
     csv_delimiter = '\t'
