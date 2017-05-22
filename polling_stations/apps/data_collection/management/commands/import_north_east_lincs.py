c4c438ffe90db286228a7be25a520c75a727982f
chris48s <chris.shaw480@gmail.com>
2017-05-22 15:44:13 +0100

    add import script for North East Lincolnshire (closes #861)

---
+++ polling_stations/apps/data_collection/management/commands/import_north_east_lincs.py
@@ -0,0 +1,8 @@
+from data_collection.management.commands import BaseHalaroseCsvImporter
+
+class Command(BaseHalaroseCsvImporter):
+    council_id      = 'E06000012'
+    addresses_name  = 'parl.2017-06-08/Version 1/polling_station_export-2017-05-17.csv'
+    stations_name   = 'parl.2017-06-08/Version 1/polling_station_export-2017-05-17.csv'
+    elections       = ['parl.2017-06-08']
+    csv_encoding    = 'windows-1252'
