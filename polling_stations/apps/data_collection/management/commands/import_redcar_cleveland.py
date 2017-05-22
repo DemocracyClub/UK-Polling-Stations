3cb56cbd3ac50059da263cd6e8284812f9e1579b
chris48s <chris.shaw480@gmail.com>
2017-05-22 15:44:13 +0100

    add import script for Redcar and Cleveland (closes #881)

---
+++ polling_stations/apps/data_collection/management/commands/import_redcar_cleveland.py
@@ -0,0 +1,8 @@
+from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
+
+class Command(BaseXpressDemocracyClubCsvImporter):
+    council_id = 'E06000003'
+    addresses_name = 'parl.2017-06-08/Version 1/E06000003-Redcar-and-Cleveland Democracy_Club__08June2017.tsv'
+    stations_name = 'parl.2017-06-08/Version 1/E06000003-Redcar-and-Cleveland Democracy_Club__08June2017.tsv'
+    elections = ['parl.2017-06-08']
+    csv_delimiter = '\t'
