d489346a1d632ba973470502a271945329c454f7
chris48s <chris.shaw480@gmail.com>
2017-05-22 15:44:13 +0100

    add import script for Bath and NES (closes #869)

---
+++ polling_stations/apps/data_collection/management/commands/import_bath_nes.py
@@ -0,0 +1,13 @@
+from data_collection.management.commands import BaseHalaroseCsvImporter
+
+class Command(BaseHalaroseCsvImporter):
+    council_id      = 'E06000022'
+    addresses_name  = 'parl.2017-06-08/Version 1/Bath & North East Somerset polling_station_export-2017-05-17.csv'
+    stations_name   = 'parl.2017-06-08/Version 1/Bath & North East Somerset polling_station_export-2017-05-17.csv'
+    elections       = ['parl.2017-06-08']
+    csv_encoding    = 'windows-1252'
+
+    def station_record_to_dict(self, record):
+        if getattr(record, self.station_id_field).strip() == 'n/a':
+            return None
+        return super().station_record_to_dict(record)
