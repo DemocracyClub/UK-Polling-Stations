0a6137349468d6b8169dfd6fa07554330f16f1f7
chris48s <chris.shaw480@gmail.com>
2017-05-23 14:30:26 +0100

    remove election id from Huntingdonshire

--- polling_stations/apps/data_collection/management/commands/import_huntingdonshire.py
+++ polling_stations/apps/data_collection/management/commands/import_huntingdonshire.py
@@ -4,8 +4,8 @@ class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = 'E07000011'
     addresses_name = 'Hunts_Democracy_Club__04May2017.tsv'
     stations_name = 'Hunts_Democracy_Club__04May2017.tsv'
-    elections = [
+    """elections = [
         'mayor.cambridgeshire-and-peterborough.2017-05-04',
         'parl.2017-06-08'
-    ]
+    ]"""
     csv_delimiter = '\t'
