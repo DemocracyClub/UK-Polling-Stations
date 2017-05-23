7539ae5bb58fcfa15d805f506b0a505309078b1c
chris48s <chris.shaw480@gmail.com>
2017-05-23 19:32:52 +0100

    apply auth and admin migrations using the same DB connection

--- polling_stations/db_routers.py
+++ polling_stations/db_routers.py
@@ -1,6 +1,6 @@
 class LoggerRouter(object):
 
-    logger_apps = ['data_finder', 'auth', 'authtoken']
+    logger_apps = ['data_finder', 'admin', 'auth', 'authtoken']
 
     def db_for_read(self, model, **hints):
         if model._meta.app_label in self.logger_apps:
