96513cb666fba92b405c1b7e8cc94aeba1a51951
chris48s <chris.shaw480@gmail.com>
2017-05-25 12:12:49 +0100

    factor out update_station_point(), better point for C3 Centre, Cambridge

--- polling_stations/apps/data_collection/management/commands/misc_fixes.py
+++ polling_stations/apps/data_collection/management/commands/misc_fixes.py
@@ -4,6 +4,19 @@ from django.contrib.gis.geos import Point
 from pollingstations.models import PollingStation, PollingDistrict
 from councils.models import Council
 
+def update_station_point(council_id, station_id, point):
+    stations = PollingStation.objects.filter(
+        council_id=council_id,
+        internal_council_id=station_id
+    )
+    if len(stations) == 1:
+        station = stations[0]
+        station.location = point
+        station.save()
+        print("..updated")
+    else:
+        print("..NOT updated")
+
 
 class Command(BaseCommand):
 
@@ -17,45 +30,23 @@ class Command(BaseCommand):
 
 
         print("updating point for: Manchester Central Library...")
-        stations = PollingStation.objects.filter(
-            council_id='E08000003',
-            internal_council_id='3019'
-        )
-        if len(stations) == 1:
-            station = stations[0]
-            station.location = Point(-2.24415, 53.47784, srid=4326)
-            station.save()
-            print("..updated")
-        else:
-            print("..NOT updated")
+        update_station_point('E08000003', '3019',
+            Point(-2.24415, 53.47784, srid=4326))
 
 
         print("updating point for: CLASSROOM HS210, WALSALL COLLEGE...")
-        stations = PollingStation.objects.filter(
-            council_id='E08000030',
-            internal_council_id='15'
-        )
-        if len(stations) == 1:
-            station = stations[0]
-            station.location = Point(-1.984424, 52.590524, srid=4326)
-            station.save()
-            print("..updated")
-        else:
-            print("..NOT updated")
+        update_station_point('E08000030', '15',
+            Point(-1.984424, 52.590524, srid=4326))
 
 
         print("updating point for: St Alban's Hall, Oxford...")
-        stations = PollingStation.objects.filter(
-            council_id='E07000178',
-            internal_council_id='4399'
-        )
-        if len(stations) == 1:
-            station = stations[0]
-            station.location = Point(-1.232920, 51.740993, srid=4326)
-            station.save()
-            print("..updated")
-        else:
-            print("..NOT updated")
+        update_station_point('E07000178', '4399',
+            Point(-1.232920, 51.740993, srid=4326))
+
+
+        print("updating point for: C3 Centre, Cambridge...")
+        update_station_point('E07000008', '38',
+            Point(0.1572, 52.2003, srid=4326))
 
 
         print("..done")
