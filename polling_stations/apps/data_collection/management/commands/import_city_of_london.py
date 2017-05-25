23b4e41d91ed0694c187c2cf589c1fdf9d966cea
chris48s <chris.shaw480@gmail.com>
2017-05-25 12:06:48 +0100

    move 'n/a' handling into base Halarose class

--- polling_stations/apps/data_collection/management/commands/import_city_of_london.py
+++ polling_stations/apps/data_collection/management/commands/import_city_of_london.py
@@ -6,8 +6,3 @@ class Command(BaseHalaroseCsvImporter):
     stations_name   = 'parl.2017-06-08/Version 1/polling_station_export-2017-05-18.csv'
     elections       = ['parl.2017-06-08']
     csv_encoding    = 'windows-1252'
-
-    def station_record_to_dict(self, record):
-        if getattr(record, self.station_id_field).strip() == 'n/a':
-            return None
-        return super().station_record_to_dict(record)
