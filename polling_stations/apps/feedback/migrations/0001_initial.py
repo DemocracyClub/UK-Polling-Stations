3115896e5ffecb1d8ddbcb14a11db69bd0ff6195
chris48s <chris.shaw480@gmail.com>
2017-05-26 11:00:28 +0100

    add 'did you find this useful' feedback form

---
+++ polling_stations/apps/feedback/migrations/0001_initial.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django_extensions.db.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Feedback',
+            fields=[
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
+                ('created', django_extensions.db.fields.CreationDateTimeField(auto_now_add=True, verbose_name='created')),
+                ('modified', django_extensions.db.fields.ModificationDateTimeField(verbose_name='modified', auto_now=True)),
+                ('found_useful', models.CharField(choices=[('YES', 'Yes'), ('NO', 'No')], max_length=100, blank=True)),
+                ('comments', models.TextField(blank=True)),
+                ('source_url', models.CharField(max_length=800, blank=True)),
+                ('token', models.CharField(max_length=100, blank=True)),
+            ],
+            options={
+                'ordering': ('-modified', '-created'),
+                'abstract': False,
+                'get_latest_by': 'modified',
+            },
+        ),
+    ]
---
+++ polling_stations/apps/feedback/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+from django_extensions.db.models import TimeStampedModel
+
+FOUND_USEFUL_CHOICES = (
+    ('YES', 'Yes'),
+    ('NO', 'No'),
+)
+
+class Feedback(TimeStampedModel):
+    found_useful = models.CharField(
+        blank=True,
+        max_length=100,
+        choices=FOUND_USEFUL_CHOICES,
+    )
+    comments = models.TextField(blank=True)
+    source_url = models.CharField(blank=True, max_length=800)
+    token = models.CharField(blank=True, max_length=100)
