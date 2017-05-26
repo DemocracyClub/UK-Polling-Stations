2d5c8a2546732185e076b77e005893f260a199fb
chris48s <chris.shaw480@gmail.com>
2017-05-26 16:22:45 +0100

    update Barking & Dagenham import script for parl.2017-06-08

--- polling_stations/apps/data_collection/management/commands/import_barking_and_dagenham.py
+++ polling_stations/apps/data_collection/management/commands/import_barking_and_dagenham.py
@@ -9,8 +9,9 @@ class Command(BaseShpStationsShpDistrictsImporter):
     """
     council_id = 'E09000002'
     districts_name = 'Barking_n_Dag_completed_PDs_region_updated/Barking_n_Dag_completed_PDs_region_updated'
-    stations_name = 'Polling_Stations2016/Polling_Stations2016'
-    elections = ['ref.2016-06-23']
+    stations_name = 'parl.2017-06-08/Version 1/Polling_Stations2017DemocracyClub'
+    elections = ['parl.2017-06-08']
+    seen_stations = set()
 
     def district_record_to_dict(self, record):
         return {
@@ -19,10 +20,18 @@ class Command(BaseShpStationsShpDistrictsImporter):
         }
 
     def station_record_to_dict(self, record):
-        postcode = " ".join(record[3].split(' ')[-2:])
+
+        address = record[3].strip()
+        postcode = " ".join(address.split(' ')[-2:])
+        code = record[4].strip()
+
+        if code in self.seen_stations:
+            return None
+
+        self.seen_stations.add(code)
         return {
-            'internal_council_id': record[1],
-            'polling_district_id': record[4],
+            'internal_council_id': code,
+            'polling_district_id': code,
             'postcode' : postcode,
-            'address' : record[3]
+            'address' : address,
         }
