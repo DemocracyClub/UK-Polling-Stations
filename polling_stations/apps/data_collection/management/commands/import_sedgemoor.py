7d4bfe7dc44ab047a43300f0cbb74f06f83ec311
chris48s <chris.shaw480@gmail.com>
2017-05-28 17:39:47 +0100

    update Sedgemoor import script for parl.2017-06-08 (closes #954)

--- polling_stations/apps/data_collection/management/commands/import_sedgemoor.py
+++ polling_stations/apps/data_collection/management/commands/import_sedgemoor.py
@@ -2,9 +2,6 @@ from data_collection.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id      = 'E07000188'
-    addresses_name  = 'Sedgemoor_polling_station_export-2017-02-24.csv'
-    stations_name   = 'Sedgemoor_polling_station_export-2017-02-24.csv'
-    elections       = [
-        'local.somerset.2017-05-04',
-        'parl.2017-06-08'
-    ]
+    addresses_name  = 'parl.2017-06-08/Version 1/polling_station_export-2017-05-25.csv'
+    stations_name   = 'parl.2017-06-08/Version 1/polling_station_export-2017-05-25.csv'
+    elections       = ['parl.2017-06-08']
