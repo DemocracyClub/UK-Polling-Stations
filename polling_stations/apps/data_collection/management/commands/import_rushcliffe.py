088605e784cdb787fa8055229d1d050d1dca2c8d
chris48s <chris.shaw480@gmail.com>
2017-05-28 17:39:47 +0100

    update Rushcliffe import script for parl.2017-06-08 (closes #952)

--- polling_stations/apps/data_collection/management/commands/import_rushcliffe.py
+++ polling_stations/apps/data_collection/management/commands/import_rushcliffe.py
@@ -2,11 +2,7 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = 'E07000176'
-    addresses_name = 'Democracy_Club__04May2017 (5).tsv'
-    stations_name = 'Democracy_Club__04May2017 (5).tsv'
-    elections = [
-        'local.nottinghamshire.2017-05-04',
-        'local.rushcliffe.2017-05-04',
-        'parl.2017-06-08'
-    ]
+    addresses_name = 'parl.2017-06-08/Version 1/Rushcliffe Democracy_Club__08June2017-Rushcliffe.TSV'
+    stations_name = 'parl.2017-06-08/Version 1/Rushcliffe Democracy_Club__08June2017-Rushcliffe.TSV'
+    elections = ['parl.2017-06-08']
     csv_delimiter = '\t'
