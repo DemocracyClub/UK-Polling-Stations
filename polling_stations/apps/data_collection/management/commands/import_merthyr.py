6347e941814e54fb4efed6cea8c79bbbb776ebba
chris48s <chris.shaw480@gmail.com>
2017-05-28 17:39:47 +0100

    update Merthyr import script for parl.2017-06-08 (closes #938)

--- polling_stations/apps/data_collection/management/commands/import_merthyr.py
+++ polling_stations/apps/data_collection/management/commands/import_merthyr.py
@@ -2,10 +2,7 @@ from data_collection.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id      = 'W06000024'
-    addresses_name  = 'Merthyr_polling_station_export-2017-04-13.csv'
-    stations_name   = 'Merthyr_polling_station_export-2017-04-13.csv'
-    elections       = [
-        'local.merthyr-tydfil.2017-05-04',
-        'parl.2017-06-08'
-    ]
+    addresses_name  = 'parl.2017-06-08/Version 1/Merthyr Tydfil polling_station_export-2017-05-25.csv'
+    stations_name   = 'parl.2017-06-08/Version 1/Merthyr Tydfil polling_station_export-2017-05-25.csv'
+    elections       = ['parl.2017-06-08']
     csv_encoding    = 'latin-1'
