e02c4a9b39a11af0dad6312abcd20855744d5344
chris48s <chris.shaw480@gmail.com>
2017-05-28 17:39:30 +0100

    update Ashford import script for parl.2017-06-08 (closes #946)

--- polling_stations/apps/data_collection/management/commands/import_ashford.py
+++ polling_stations/apps/data_collection/management/commands/import_ashford.py
@@ -2,9 +2,7 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = 'E07000105'
-    addresses_name = 'Ashford_Democracy_Club__04May2017.CSV'
-    stations_name = 'Ashford_Democracy_Club__04May2017.CSV'
-    elections = [
-        'local.kent.2017-05-04',
-        'parl.2017-06-08'
-    ]
+    addresses_name = 'parl.2017-06-08/Version 1/Ashford Democracy_Club__08June2017.tsv'
+    stations_name = 'parl.2017-06-08/Version 1/Ashford Democracy_Club__08June2017.tsv'
+    elections = ['parl.2017-06-08']
+    csv_delimiter = '\t'
