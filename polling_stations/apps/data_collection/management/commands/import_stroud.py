6e4fbf23dd9d77955f55ca9b77684b472d7d0f2f
chris48s <chris.shaw480@gmail.com>
2017-05-28 17:39:47 +0100

    update Stroud import script for parl.2017-06-08 (closes #944)

--- polling_stations/apps/data_collection/management/commands/import_stroud.py
+++ polling_stations/apps/data_collection/management/commands/import_stroud.py
@@ -2,10 +2,8 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = 'E07000082'
-    addresses_name = 'democracy_club__04may2017 (4).csv'
-    stations_name = 'democracy_club__04may2017 (4).csv'
-    elections = [
-        'local.gloucestershire.2017-05-04',
-        'parl.2017-06-08'
-    ]
+    addresses_name = 'parl.2017-06-08/Version 1/Stroud Democracy Club - Where Do I Vote.TSV'
+    stations_name = 'parl.2017-06-08/Version 1/Stroud Democracy Club - Where Do I Vote.TSV'
+    elections = ['parl.2017-06-08']
     csv_encoding = 'latin-1'
+    csv_delimiter = '\t'
