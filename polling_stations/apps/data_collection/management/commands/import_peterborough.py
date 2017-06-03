83e4aa9e482635c72e7787b40311542b2f189fb3
chris48s <chris.shaw480@gmail.com>
2017-06-03 23:02:04 +0100

    Update import script for Peterborough (closes #998)

--- polling_stations/apps/data_collection/management/commands/import_peterborough.py
+++ polling_stations/apps/data_collection/management/commands/import_peterborough.py
@@ -2,11 +2,8 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = 'E06000031'
-    addresses_name = 'Democracy_Club__04May2017_peterborough.tsv'
-    stations_name = 'Democracy_Club__04May2017_peterborough.tsv'
-    elections = [
-        'mayor.cambridgeshire-and-peterborough.2017-05-04',
-        'parl.2017-06-08'
-    ]
+    addresses_name = 'parl.2017-06-08/Version 1/Democracy_Club__08June2017 (1).tsvv'
+    stations_name = 'parl.2017-06-08/Version 1/Democracy_Club__08June2017 (1).tsvv'
+    elections = ['parl.2017-06-08']
     csv_delimiter = '\t'
     csv_encoding = 'latin-1'
