00977180b40d4409d5e50660d06a67199c7bb8fc
chris48s <chris.shaw480@gmail.com>
2017-06-03 23:02:04 +0100

    Update import script for City of London (closes #996)

--- polling_stations/apps/data_collection/management/commands/import_city_of_london.py
+++ polling_stations/apps/data_collection/management/commands/import_city_of_london.py
@@ -2,7 +2,7 @@ from data_collection.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id      = 'E09000001'
-    addresses_name  = 'parl.2017-06-08/Version 1/polling_station_export-2017-05-18.csv'
-    stations_name   = 'parl.2017-06-08/Version 1/polling_station_export-2017-05-18.csv'
+    addresses_name  = 'parl.2017-06-08/Version 2/polling_station_export-2017-06-02.csv'
+    stations_name   = 'parl.2017-06-08/Version 2/polling_station_export-2017-06-02.csv'
     elections       = ['parl.2017-06-08']
     csv_encoding    = 'windows-1252'
