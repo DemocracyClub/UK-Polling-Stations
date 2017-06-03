d61f3eb1b8fc7fe01d5b2e8b19bbf1037bfec839
chris48s <chris.shaw480@gmail.com>
2017-06-03 23:02:04 +0100

    Update import script for Warrington (closes #882)

--- polling_stations/apps/data_collection/management/commands/import_warrington.py
+++ polling_stations/apps/data_collection/management/commands/import_warrington.py
@@ -2,7 +2,7 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = 'E06000007'
-    addresses_name = 'parl.2017-06-08/Version 1/Democracy_Club__08June2017 (7).tsv'
-    stations_name = 'parl.2017-06-08/Version 1/Democracy_Club__08June2017 (7).tsv'
+    addresses_name = 'parl.2017-06-08/Version 3/Warrington Democracy_Club__08June2017.tsv'
+    stations_name = 'parl.2017-06-08/Version 3/Warrington Democracy_Club__08June2017.tsv'
     elections = ['parl.2017-06-08']
     csv_delimiter = '\t'
