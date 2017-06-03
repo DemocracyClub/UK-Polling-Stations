cd72e7830ed18d2a55d09638c1e1c93a1e00e14a
chris48s <chris.shaw480@gmail.com>
2017-06-03 23:02:04 +0100

    Fix dodgy point in Chelmsford

--- polling_stations/apps/data_collection/management/commands/import_chelmsford.py
+++ polling_stations/apps/data_collection/management/commands/import_chelmsford.py
@@ -7,3 +7,10 @@ class Command(BaseXpressDemocracyClubCsvImporter):
     elections = ['parl.2017-06-08']
     csv_delimiter = '\t'
     csv_encoding = 'latin-1'
+
+    def station_record_to_dict(self, record):
+
+        if record.polling_place_id == '7695':
+            record = record._replace(polling_place_postcode = 'CM1 1FG')
+
+        return super().station_record_to_dict(record)
