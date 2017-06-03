9a9a13d0c05adb50a436f186fcf44702ae9d47ad
chris48s <chris.shaw480@gmail.com>
2017-06-03 23:02:04 +0100

    Update import script for Solihull (closes #992)

--- polling_stations/apps/data_collection/management/commands/import_solihull.py
+++ polling_stations/apps/data_collection/management/commands/import_solihull.py
@@ -2,10 +2,7 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = 'E08000029'
-    addresses_name = 'Democracy_Club__04May2017 (Solihull).tsv'
-    stations_name = 'Democracy_Club__04May2017 (Solihull).tsv'
-    elections = [
-        'mayor.west-midlands.2017-05-04',
-        'parl.2017-06-08'
-    ]
+    addresses_name = 'parl.2017-06-08/Version 2/Democracy_Club__08June2017 (26).tsv'
+    stations_name = 'parl.2017-06-08/Version 2/Democracy_Club__08June2017 (26).tsv'
+    elections = ['parl.2017-06-08']
     csv_delimiter = '\t'
