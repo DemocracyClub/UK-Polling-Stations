db3702f7ff975ee480dc4db3cfc33e13290c0233
chris48s <chris.shaw480@gmail.com>
2017-06-03 23:02:04 +0100

    Update import script for Maidstone (closes #986)

--- polling_stations/apps/data_collection/management/commands/import_maidstone.py
+++ polling_stations/apps/data_collection/management/commands/import_maidstone.py
@@ -1,28 +1,9 @@
-from data_collection.management.commands import BaseShpStationsShpDistrictsImporter
+from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
 
-class Command(BaseShpStationsShpDistrictsImporter):
-    srid = 27700
+class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = 'E07000110'
-    districts_name = 'shp/maidstone_polling_districts'
-    stations_name = 'shp/maidstone_polling_stations.shp'
-    elections = [
-        'local.kent.2017-05-04',
-        'parl.2017-06-08'
-    ]
-
-    def district_record_to_dict(self, record):
-        return {
-            'internal_council_id': record[0].strip(),
-            'name': record[1].strip(),
-            'polling_station_id': record[0].strip(),
-        }
-
-    def station_record_to_dict(self, record):
-        address_parts = record[3].strip().split(", ")
-        address_parts.insert(0, record[2].strip())
-        address = "\n".join(address_parts)
-        return {
-            'internal_council_id': record[0].strip(),
-            'postcode': record[18].strip(),
-            'address': address,
-        }
+    addresses_name = 'parl.2017-06-08/Version 1/Maidstone Democracy_Club__08June2017.tsv'
+    stations_name = 'parl.2017-06-08/Version 1/Maidstone Democracy_Club__08June2017.tsv'
+    elections = ['parl.2017-06-08']
+    csv_delimiter = '\t'
+    csv_encoding = 'windows-1252'
