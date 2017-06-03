c788a16f76a45972365f3dac9b2937a8bf938b93
chris48s <chris.shaw480@gmail.com>
2017-06-03 23:02:04 +0100

    Update import script for Middlesbrough (closes #982)

--- polling_stations/apps/data_collection/management/commands/import_middlesbrough.py
+++ polling_stations/apps/data_collection/management/commands/import_middlesbrough.py
@@ -2,10 +2,7 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = 'E06000002'
-    addresses_name = 'Democracy_Club__04May2017 (6).tsv'
-    stations_name = 'Democracy_Club__04May2017 (6).tsv'
-    elections = [
-        'mayor.tees-valley.2017-05-04',
-        'parl.2017-06-08'
-    ]
+    addresses_name = 'parl.2017-06-08/Version 1/Middlesbrough Democracy_Club__08June2017.tsv'
+    stations_name = 'parl.2017-06-08/Version 1/Middlesbrough Democracy_Club__08June2017.tsv'
+    elections = ['parl.2017-06-08']
     csv_delimiter = '\t'
