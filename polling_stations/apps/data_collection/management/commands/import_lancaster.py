871d6ed2425a90b2179fd5c485dff1bfdcc34f40
chris48s <chris.shaw480@gmail.com>
2017-06-03 23:02:04 +0100

    Update import script for Lancaster (closes #987)

--- polling_stations/apps/data_collection/management/commands/import_lancaster.py
+++ polling_stations/apps/data_collection/management/commands/import_lancaster.py
@@ -1,39 +1,8 @@
-from data_collection.morph_importer import BaseMorphApiImporter
+from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
 
-class Command(BaseMorphApiImporter):
-
-    srid = 4326
-    districts_srid  = 4326
+class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = 'E07000121'
+    addresses_name = 'parl.2017-06-08/Version 1/Lancaster (and Fleetwood) && (Morecambe and Lunesdale) Democracy_Club__08June2017.tsv'
+    stations_name = 'parl.2017-06-08/Version 1/Lancaster (and Fleetwood) && (Morecambe and Lunesdale) Democracy_Club__08June2017.tsv'
     elections = ['parl.2017-06-08']
-    scraper_name = 'wdiv-scrapers/DC-PollingStations-Lancaster'
-    geom_type = 'geojson'
-    seen_stations = set()
-
-    def district_record_to_dict(self, record):
-        poly = self.extract_geometry(record, self.geom_type, self.get_srid('districts'))
-        return {
-            'internal_council_id': record['PD_REF'],
-            'name'               : record['PD_NAME'],
-            'area'               : poly,
-            'polling_station_id' : record['PD_REF'],
-        }
-
-    def station_record_to_dict(self, record):
-        location = self.extract_geometry(record, self.geom_type, self.get_srid('stations'))
-        stations = []
-        codes = record['DISTRICT'].split(' ')
-
-        for code in codes:
-            if (code, record['POLLING_PL']) in self.seen_stations:
-                stations.append(None)
-            else:
-                self.seen_stations.add((code, record['POLLING_PL']))
-                stations.append({
-                    'internal_council_id': code,
-                    'postcode':            '',
-                    'address':             record['POLLING_PL'],
-                    'location':            location,
-                })
-
-        return stations
+    csv_delimiter = '\t'
