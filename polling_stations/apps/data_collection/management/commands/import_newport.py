2d84fe9b0787ccde90f5df3217ef6e653878e562
chris48s <chris.shaw480@gmail.com>
2017-06-04 16:08:12 +0100

    remove Newport election id (data issues)

--- polling_stations/apps/data_collection/management/commands/import_newport.py
+++ polling_stations/apps/data_collection/management/commands/import_newport.py
@@ -4,6 +4,6 @@ class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = 'W06000022'
     addresses_name = 'parl.2017-06-08/Version 1/Newport Democracy_Club__08June2017.tsv08June2017.tsv'
     stations_name = 'parl.2017-06-08/Version 1/Newport Democracy_Club__08June2017.tsv08June2017.tsv'
-    elections = ['parl.2017-06-08']
+    #elections = ['parl.2017-06-08']
     csv_delimiter = '\t'
     csv_encoding = 'latin-1'
