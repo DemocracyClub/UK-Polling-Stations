1e16ff115ba8dd034e6004624ce0867230f5ef98
chris48s <chris.shaw480@gmail.com>
2017-06-05 17:58:58 +0100

    update Breckland import script for parl.2017-06-08 (closes #995)

--- polling_stations/apps/data_collection/management/commands/import_breckland.py
+++ polling_stations/apps/data_collection/management/commands/import_breckland.py
@@ -1,13 +1,10 @@
-from data_collection.management.commands import BaseXpressWebLookupCsvImporter
+from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
 
-class Command(BaseXpressWebLookupCsvImporter):
+class Command(BaseXpressDemocracyClubCsvImporter):
     council_id      = 'E07000143'
-    addresses_name  = 'May 2017/BrecklandPropertyPostCodePollingStationWebLookup-2017-02-20.TSV'
-    stations_name   = 'May 2017/BrecklandPropertyPostCodePollingStationWebLookup-2017-02-20.TSV'
-    elections       = [
-        'local.norfolk.2017-05-04',
-        #'parl.2017-06-08'
-    ]
+    addresses_name  = 'parl.2017-06-08/Version 1/Breckland Update Democracy_Club__08June2017.tsv'
+    stations_name   = 'parl.2017-06-08/Version 1/Breckland Update Democracy_Club__08June2017.tsv'
+    elections       = ['parl.2017-06-08']
     csv_delimiter = '\t'
 
     def station_record_to_dict(self, record):
@@ -16,8 +13,8 @@ class Command(BaseXpressWebLookupCsvImporter):
         File supplied contained obviously inaccurate points
         remove and fall back to geocoding
         """
-        if record.pollingplaceid in ['5151', '5370', '5418', '5319']:
-            record = record._replace(pollingplaceeasting = '0')
-            record = record._replace(pollingplacenorthing = '0')
+        if record.polling_place_id in ['5875', '5779', '6008', '5926']:
+            record = record._replace(polling_place_easting = '0')
+            record = record._replace(polling_place_northing = '0')
 
         return super().station_record_to_dict(record)
