14b0af75ee6b94f0501c8abe455a7a698cab8f85
chris48s <chris.shaw480@gmail.com>
2017-06-05 17:59:43 +0100

    add import script for Mansfield

---
+++ polling_stations/apps/data_collection/management/commands/import_mansfield.py
@@ -0,0 +1,7 @@
+from data_collection.management.commands import BaseHalaroseCsvImporter
+
+class Command(BaseHalaroseCsvImporter):
+    council_id      = 'E07000174'
+    addresses_name  = 'parl.2017-06-08/Version 1/polling_station_export-2017-06-05 2.csv'
+    stations_name   = 'parl.2017-06-08/Version 1/polling_station_export-2017-06-05 2.csv'
+    elections       = ['parl.2017-06-08']
