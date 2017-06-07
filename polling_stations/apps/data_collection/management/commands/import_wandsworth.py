453adf93d305b103755ce37b71c54da5eaac9eac
chris48s <chris.shaw480@gmail.com>
2017-06-07 13:26:03 +0100

    add import script for Wandsworth

--- polling_stations/apps/data_collection/management/commands/import_wandsworth.py
+++ polling_stations/apps/data_collection/management/commands/import_wandsworth.py
@@ -1,8 +1,8 @@
-from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
+from data_collection.management.commands import BaseXpressWebLookupCsvImporter
 
-class Command(BaseXpressDemocracyClubCsvImporter):
+class Command(BaseXpressWebLookupCsvImporter):
     council_id = 'E09000032'
-    addresses_name = 'parl.2017-06-08/Version 1/Democracy_Club__08June2017 Wandsworth.tsv'
-    stations_name = 'parl.2017-06-08/Version 1/Democracy_Club__08June2017 Wandsworth.tsv'
-    #elections = ['parl.2017-06-08']
+    addresses_name = 'parl.2017-06-08/Version 2/PropertyPostCodePollingStationWebLookup-2017-05-03 (1).TSV'
+    stations_name = 'parl.2017-06-08/Version 2/PropertyPostCodePollingStationWebLookup-2017-05-03 (1).TSV'
+    elections = ['parl.2017-06-08']
     csv_delimiter = '\t'
