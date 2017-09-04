2bb71517a3c97ceb85d3587fc62cb1b33a2980be
chris48s <chris.shaw480@gmail.com>
2017-09-04 15:08:40 +0100

    split front end code into 2 pipelines
    This allows us to only load all the front end code
    relating to map display on the relevant page, which
    means the initial page impression will be quicker to load.

--- polling_stations/settings/base.py
+++ polling_stations/settings/base.py
@@ -99,20 +99,29 @@ PIPELINE = get_pipeline_settings(
     extra_css=[
         'custom_css/style.scss',
         'font-awesome/css/font-awesome.min.css',
+    ],
+    extra_js=[],
+)
+PIPELINE['JS_COMPRESSOR'] = 'pipeline.compressors.uglifyjs.UglifyJSCompressor'
+PIPELINE['UGLIFYJS_BINARY'] = 'node_modules/uglify-js/bin/uglifyjs'
+PIPELINE['UGLIFYJS_ARGUMENTS'] = '--no-mangle'
+PIPELINE['STYLESHEETS']['map'] = {
+    'source_filenames': [
         'leaflet/dist/leaflet.css',
         'custom_css/map.css',
         'leaflet-extra-markers/dist/css/leaflet.extra-markers.min.css',
     ],
-    extra_js=[
+    'output_filename': 'css/map.css',
+}
+PIPELINE['JAVASCRIPT']['map'] = {
+    'source_filenames': [
         'leaflet/dist/leaflet.js',
         'leaflet-extra-markers/dist/js/leaflet.extra-markers.min.js',
         '@mapbox/polyline/src/polyline.js',
         'custom_js/polyline_global.js',
     ],
-)
-PIPELINE['JS_COMPRESSOR'] = 'pipeline.compressors.uglifyjs.UglifyJSCompressor'
-PIPELINE['UGLIFYJS_BINARY'] = 'node_modules/uglify-js/bin/uglifyjs'
-PIPELINE['UGLIFYJS_ARGUMENTS'] = '--no-mangle'
+    'output_filename': 'js/map.js',
+}
 COMPRESS_CSS_FILTERS = ['compressor.filters.css_default.CssAbsoluteFilter']
 
 
