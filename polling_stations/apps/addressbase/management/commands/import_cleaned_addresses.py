cbe211cc72f1445c3784c60ed93958088d8fc291
chris48s <chris.shaw480@gmail.com>
2017-09-27 11:53:37 +0100

    make table_name in import_cleaned_addresses a property
    If we extend AbstractAddress we will also need some way
    to feed a custom table name into this bit of raw sql.
    
    Not hard-coding it is a good first step

--- polling_stations/apps/addressbase/management/commands/import_cleaned_addresses.py
+++ polling_stations/apps/addressbase/management/commands/import_cleaned_addresses.py
@@ -6,17 +6,18 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
 
+    table_name = 'addressbase_address'
+
     def add_arguments(self, parser):
         parser.add_argument(
             'cleaned_ab_path',
             help='The path to the folder containing the cleaned AddressBase CSVs'
         )
 
-
     def handle(self, *args, **kwargs):
         cursor = connection.cursor()
         print("clearing existing data..")
-        cursor.execute("TRUNCATE TABLE addressbase_address;")
+        cursor.execute("TRUNCATE TABLE %s;" % (self.table_name))
 
         cleaned_file_path = os.path.abspath(os.path.join(
             kwargs['cleaned_ab_path'],
@@ -26,8 +27,8 @@ class Command(BaseCommand):
         print("importing from %s.." % (cleaned_file_path))
 
         cursor.execute("""
-            COPY addressbase_address (UPRN,address,postcode,location)
-            FROM '{}' (FORMAT CSV, DELIMITER ',', quote '"');
-        """.format(cleaned_file_path))
+            COPY {0} (UPRN,address,postcode,location)
+            FROM '{1}' (FORMAT CSV, DELIMITER ',', quote '"');
+        """.format(self.table_name, cleaned_file_path))
 
         print("...done")
