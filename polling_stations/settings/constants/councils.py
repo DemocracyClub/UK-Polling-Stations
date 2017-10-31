ef7619668360a419d3effac214a98cf348a68ecb
chris48s <chris.shaw480@gmail.com>
2017-10-31 14:45:32 +0000

    rewrite councils import to get boundaries from ONS not mapit

--- polling_stations/settings/constants/councils.py
+++ polling_stations/settings/constants/councils.py
@@ -1,7 +1,8 @@
 # settings for councils scraper
 
-GOV_UK_LA_URL = "https://www.registertovote.service.gov.uk/register-to-vote/local-authority/"  # noqa
 YVM_LA_URL = "https://www.yourvotematters.co.uk/_design/nested-content/results-page2/search-voting-locations-by-districtcode?queries_distcode_query="  # noqa
+GB_BOUNDARIES_URL = "https://opendata.arcgis.com/datasets/3943c2114d764294a7c0079c4020d558_1.geojson"
+NI_BOUNDARIES_URL = "https://opendata.arcgis.com/datasets/bba0392d214b4ac0a8e0d559cdf6013f_2.geojson"
 
 COUNCIL_TYPES = [
     "LBO",
