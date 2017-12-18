a4307088a56bf41bb21a7bee2ea4e798c6c4bde2
chris48s <chris.shaw480@gmail.com>
2017-12-18 10:21:04 +0000

    ignore group ids when working out if there is an election
    If we've got a (by-)election happening in one ward,
    querying EE with any postcode in that local auth
    will return the 'organisation group' election object
    but there is only an election happening in the
    specific ward that has a 'ballot' type object.

--- polling_stations/apps/data_finder/tests/test_ee_wrapper.py
+++ polling_stations/apps/data_finder/tests/test_ee_wrapper.py
@@ -10,11 +10,34 @@ def get_data_exception(self, postcode):
 def get_data_no_elections(self, postcode):
     return []
 
+def get_data_only_group(self, postcode):
+    return [
+        { 'election_title': 'some election', 'group_type': 'election' },
+        { 'election_title': 'some election', 'group_type': 'organisation' }
+    ]
+
+def get_data_group_and_ballot(self, postcode):
+    return [
+        { 'election_title': 'some election', 'group_type': 'organisation' },
+        { 'election_title': 'some election', 'group_type': None }
+    ]
+
 def get_data_with_elections(self, postcode):
     return [
-        {'election_title': 'some election'},  # no explanation key
-        {'election_title': 'some election', 'explanation': None},  # null explanation key
-        {'election_title': 'some election', 'explanation': 'some text'},  # explanation key contains text
+        {
+            'election_title': 'some election',
+            'group_type': 'organisation'
+        },  # no explanation key
+        {
+            'election_title': 'some election',
+            'group_type': None,
+            'explanation': None  # null explanation key
+        },
+        {
+            'election_title': 'some election',
+            'group_type': None,
+            'explanation': 'some text'  # explanation key contains text
+        },
     ]
 
 class EveryElectionWrapperTest(TestCase):
@@ -41,3 +64,17 @@ class EveryElectionWrapperTest(TestCase):
         self.assertEqual([
             {'title': 'some election', 'explanation': 'some text'}
         ], ee.get_explanations())
+
+    @mock.patch("data_finder.helpers.EveryElectionWrapper.get_data", get_data_only_group)
+    def test_elections_only_group(self):
+        ee = EveryElectionWrapper('AA11AA')
+        self.assertTrue(ee.request_success)
+        self.assertFalse(ee.has_election())
+        self.assertEqual([], ee.get_explanations())
+
+    @mock.patch("data_finder.helpers.EveryElectionWrapper.get_data", get_data_group_and_ballot)
+    def test_elections_group_and_ballot(self):
+        ee = EveryElectionWrapper('AA11AA')
+        self.assertTrue(ee.request_success)
+        self.assertTrue(ee.has_election())
+        self.assertEqual([], ee.get_explanations())
