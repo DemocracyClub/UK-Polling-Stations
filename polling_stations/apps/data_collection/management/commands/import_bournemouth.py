f2a0b091cb62087d33a2eef52ee4e9b580de655d
chris48s <chris.shaw480@gmail.com>
2017-12-18 10:28:44 +0000

    add election id for bournemouth by election

--- polling_stations/apps/data_collection/management/commands/import_bournemouth.py
+++ polling_stations/apps/data_collection/management/commands/import_bournemouth.py
@@ -4,5 +4,8 @@ class Command(BaseHalaroseCsvImporter):
     council_id      = 'E06000028'
     addresses_name  = 'parl.2017-06-08/Version 1/polling_station_export-2017-05-09.csv'
     stations_name   = 'parl.2017-06-08/Version 1/polling_station_export-2017-05-09.csv'
-    elections       = ['parl.2017-06-08']
+    elections       = [
+        'parl.2017-06-08',
+        'local.bournemouth.throop-and-muscliff.by.2018-01-18'
+    ]
     csv_encoding    = 'windows-1252'
