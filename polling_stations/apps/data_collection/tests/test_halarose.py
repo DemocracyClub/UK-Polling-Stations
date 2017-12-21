9c9449fa21b2de851fd295321d6355911bdc04c6
chris48s <chris.shaw480@gmail.com>
2017-12-21 15:31:03 +0000

    add tests for halarose base class
    and tidy up a few bits of edge case handling

---
+++ polling_stations/apps/data_collection/tests/test_halarose.py
@@ -0,0 +1,70 @@
+from django.test import TestCase
+
+from councils.models import Council
+from data_collection.tests.stubs import stub_halaroseimport
+from pollingstations.models import PollingStation, ResidentialAddress
+
+
+class HalaroseImportTests(TestCase):
+
+    opts = {
+        'noclean': False,
+        'nochecks': True,
+        'verbosity': 0
+    }
+
+    def setUp(self):
+        Council.objects.update_or_create(pk='X01000000')
+        cmd = stub_halaroseimport.Command()
+        cmd.handle(**self.opts)
+
+    def test_addresses(self):
+        addresses = ResidentialAddress.objects\
+                                      .filter(council_id='X01000000')\
+                                      .order_by('address')\
+                                      .values_list('address', flat=True)
+
+        # we should have inserted 3 addresses
+        # and discarded 3 due to error conditions
+        self.assertEqual(3, len(addresses))
+        expected = set([
+            '1 Heol Elfed\nGorseinon\nSwansea',
+            '47 Heol Dylan\nGorseinon\nSwansea',
+            '2, Dogkennel Farm Cottages',
+        ])
+        self.assertEqual(set(addresses), expected)
+
+    def test_station_ids(self):
+        self.assertEqual('',
+            ResidentialAddress\
+                .objects\
+                .get(slug='x01000000-2-dogkennel-farm-cottages-ba152bb')\
+                .polling_station_id
+        )
+        self.assertEqual('10-penyrheol-boxing-club',
+            ResidentialAddress\
+                .objects\
+                .get(slug='x01000000-10-penyrheol-boxing-club-1-heol-elfed-gorseinon-swansea-sa44gh')\
+                .polling_station_id
+        )
+        self.assertEqual('10-penyrheol-boxing-club',
+            ResidentialAddress\
+                .objects\
+                .get(slug='x01000000-10-penyrheol-boxing-club-47-heol-dylan-gorseinon-swansea-sa44lr')\
+                .polling_station_id
+        )
+
+    def test_stations(self):
+        stations = PollingStation.objects\
+                                 .filter(council_id='X01000000')\
+                                 .order_by('internal_council_id')\
+                                 .values_list('address', flat=True)
+
+        # we inserted 2 stations, even though only one of them
+        # had no valid addresses associated with it
+        self.assertEqual(2, len(stations))
+        expected = set([
+            'Penyrheol Boxing Club\nGower View Road\nPenyrheol\nSwansea',
+            'St Ambrose Church Hall\nWest Cliff Road\nBournemouth'
+        ])
+        self.assertEqual(set(stations), expected)
