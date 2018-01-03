cce5ba46e2e2466bb0038ee0ae5d3655ccf057d4
chris48s <chris.shaw480@gmail.com>
2018-01-03 17:18:45 +0000

    core.context_processors -> template.context_processors

--- polling_stations/settings/base.py
+++ polling_stations/settings/base.py
@@ -108,12 +108,12 @@ MIDDLEWARE_CLASSES = (
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.core.context_processors.debug",
-    "django.core.context_processors.i18n",
-    "django.core.context_processors.media",
-    "django.core.context_processors.static",
-    'django.core.context_processors.request',
-    "django.core.context_processors.tz",
+    "django.template.context_processors.debug",
+    "django.template.context_processors.i18n",
+    "django.template.context_processors.media",
+    "django.template.context_processors.static",
+    'django.template.context_processors.request',
+    "django.template.context_processors.tz",
     "django.contrib.messages.context_processors.messages",
     "django.contrib.auth.context_processors.auth",
     'dc_theme.context_processors.dc_theme_context',
