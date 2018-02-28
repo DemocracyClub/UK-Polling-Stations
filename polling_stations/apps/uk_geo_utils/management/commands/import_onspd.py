6c3f2c1e4ecadf495b343e75a25e49af9b7e4bdc
chris48s <chris.shaw480@gmail.com>
2018-02-28 17:03:55 +0000

    update ONSPD model and import script for Feb 2018 release

--- polling_stations/apps/uk_geo_utils/management/commands/import_onspd.py
+++ polling_stations/apps/uk_geo_utils/management/commands/import_onspd.py
@@ -34,13 +34,12 @@ class Command(BaseCommand):
             fp = open(f, 'r')
             cursor.copy_expert("""
                 COPY %s (
-                pcd, pcd2, pcds, dointr, doterm, oscty, oslaua, osward,
-                usertype, oseast1m, osnrth1m, osgrdind, oshlthau, hro, ctry,
-                gor, streg, pcon, eer, teclec, ttwa, pct, nuts, psed, cened,
-                edind, oshaprev, lea, oldha, wardc91, wardo91, ward98,
-                statsward, oa01, casward, park, lsoa01, msoa01, ur01ind, oac01,
-                oldpct, oa11, lsoa11, msoa11, parish, wz11, ccg, bua11,
-                buasd11, ru11ind, oac11, lat, long, lep1, lep2, pfa, imd
+                pcd, pcd2, pcds, dointr, doterm, oscty, ced, oslaua, osward,
+                parish, usertype, oseast1m, osnrth1m, osgrdind, oshlthau,
+                nhser, ctry, rgn, pcon, eer, teclec, ttwa, pct, nuts,
+                statsward, oa01, casward, park, lsoa01, msoa01, ur01ind,
+                oac01, oa11, lsoa11, msoa11, wz11, ccg, bua11, buasd11,
+                ru11ind, oac11, lat, long, lep1, lep2, pfa, imd
                 ) FROM STDIN (FORMAT CSV, DELIMITER ',', quote '"', HEADER);
             """ % (self.table_name), fp)
 
