5a70de5b90dd9f22803586411eb421adf3d624b4
chris48s <chris.shaw480@gmail.com>
2018-03-07 16:47:19 +0000

    update import script for Southampton (closes #1159)
    switch to getting data from GitHub

--- polling_stations/apps/data_collection/management/commands/import_southampton.py
+++ polling_stations/apps/data_collection/management/commands/import_southampton.py
@@ -12,9 +12,9 @@ class Command(BaseApiKmlStationsKmlDistrictsImporter):
     srid             = 4326
     districts_srid   = 4326
     council_id       = 'E06000045'
-    districts_url    = 'http://www.southampton.gov.uk/geoserver/wms?LAYERS=SCC%3APOLLING_DISTRICTS&TRANSPARENT=TRUE&STYLES=Polling_Districts_Labelled&HOVER=false&FORMAT=kml&SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&SRS=EPSG%3A27700&BBOX=436000.0,108500.0,448000.0,118000.0&WIDTH=867&HEIGHT=426'
-    stations_url     = 'http://www.southampton.gov.uk/geoserver/wms?LAYERS=SCC%3APOLLING_STATIONS&TRANSPARENT=TRUE&STYLES=Polling_Stations_Labelled&HOVER=false&FORMAT=kml&SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&SRS=EPSG%3A27700&BBOX=436000.0,108500.0,448000.0,118000.0&WIDTH=867&HEIGHT=426'
-    elections        = ['parl.2017-06-08']
+    districts_url    = 'https://raw.githubusercontent.com/wdiv-scrapers/data/master/E06000045/districts.kml'
+    stations_url     = 'https://raw.githubusercontent.com/wdiv-scrapers/data/master/E06000045/stations.kml'
+    elections        = ['local.2018-05-03']
 
 
     def extract_info_from_district_description(self, description):
