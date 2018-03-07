9a9df7e4aba8e8bf1fd4e6f66308c96c532dc3e8
chris48s <chris.shaw480@gmail.com>
2018-03-07 16:41:56 +0000

    add import script for Brent (closes #1166)

--- polling_stations/apps/data_collection/management/commands/import_brent.py
+++ polling_stations/apps/data_collection/management/commands/import_brent.py
@@ -2,7 +2,7 @@ from data_collection.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id      = 'E09000005'
-    addresses_name  = 'parl.2017-06-08/Version 1/polling_station_export-2017-05-26.csv'
-    stations_name   = 'parl.2017-06-08/Version 1/polling_station_export-2017-05-26.csv'
-    elections       = ['parl.2017-06-08']
+    addresses_name  = 'local.2018-05-03/Version 1/polling_station_export-2018-02-20 Brent.csv'
+    stations_name   = 'local.2018-05-03/Version 1/polling_station_export-2018-02-20 Brent.csv'
+    elections       = ['local.2018-05-03']
     csv_encoding    = 'windows-1252'
