053f0ba3191c1a3202b70c7afbe07cb978787059
chris48s <chris.shaw480@gmail.com>
2018-03-07 16:48:42 +0000

    update import script for Walsall (closes #1153)

--- polling_stations/apps/data_collection/management/commands/import_walsall.py
+++ polling_stations/apps/data_collection/management/commands/import_walsall.py
@@ -2,10 +2,7 @@ from data_collection.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id      = 'E08000030'
-    addresses_name  = 'polling_station_export-2017-05-02.csv'
-    stations_name   = 'polling_station_export-2017-05-02.csv'
-    elections       = [
-        'mayor.west-midlands.2017-05-04',
-        'parl.2017-06-08'
-    ]
-    csv_encoding    = 'latin-1'
+    addresses_name  = 'local.2018-05-03/Version 1/polling_station_export-2018-02-15.csv'
+    stations_name   = 'local.2018-05-03/Version 1/polling_station_export-2018-02-15.csv'
+    elections       = ['local.2018-05-03']
+    csv_encoding    = 'windows-1252'
