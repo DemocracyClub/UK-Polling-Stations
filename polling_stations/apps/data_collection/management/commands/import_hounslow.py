58e6dda00ab3a5a36e255c9511fb4292ed3de1c3
chris48s <chris.shaw480@gmail.com>
2018-03-07 16:43:00 +0000

    add import script for Hounslow (closes #1155)

--- polling_stations/apps/data_collection/management/commands/import_hounslow.py
+++ polling_stations/apps/data_collection/management/commands/import_hounslow.py
@@ -5,9 +5,9 @@ from pollingstations.models import PollingStation
 
 class Command(BaseHalaroseCsvImporter):
     council_id      = 'E09000018'
-    addresses_name  = 'parl.2017-06-08/Version 1/polling_station_export-2017-05-09 (2).csv'
-    stations_name   = 'parl.2017-06-08/Version 1/polling_station_export-2017-05-09 (2).csv'
-    elections       = ['parl.2017-06-08']
+    addresses_name  = 'local.2018-05-03/Version 1/polling_station_export-2018-02-21 (1) Hounslow.csv'
+    stations_name   = 'local.2018-05-03/Version 1/polling_station_export-2018-02-21 (1) Hounslow.csv'
+    elections       = ['local.2018-05-03']
     csv_encoding    = 'windows-1252'
 
     def get_station_hash(self, record):
@@ -20,7 +20,7 @@ class Command(BaseHalaroseCsvImporter):
     def post_import(self):
         filepath = os.path.join(
             self.base_folder_path,
-            'parl.2017-06-08/Version 1/Hounslow Polling Station grid refs.csv'
+            'local.2018-05-03/Version 1/2018 Hounslow polling stations - GIS co-ordinates.csv'
         )
         gridrefs = self.get_data('csv', filepath)
 
@@ -37,6 +37,10 @@ class Command(BaseHalaroseCsvImporter):
                     float(record.cntr_y),
                     srid=27700
                 )
+                self.check_station_point({
+                    'location': station.location,
+                    'council_id': station.council_id,
+                })
                 station.save()
             else:
                 print("Could not find station id " + self.get_station_hash(record))
