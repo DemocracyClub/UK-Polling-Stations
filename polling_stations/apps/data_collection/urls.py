c023688a179bc49d878334f2278bfdd72da7d4c9
chris48s <chris.shaw480@gmail.com>
2018-03-20 20:46:21 +0000

    remove route for league table
    one day I will fix this...
    ..but not today

--- polling_stations/apps/data_collection/urls.py
+++ polling_stations/apps/data_collection/urls.py
@@ -2,9 +2,8 @@ from django.conf.urls import include, url
 from django.conf import settings
 from django.views.decorators.cache import cache_page
 
-from .views import LeagueTable, data_quality
+from .views import data_quality
 
 urlpatterns = [
-    url(r'^$', LeagueTable.as_view(), name='league_table'),
     url(r'data_quality/(?P<council_id>.+)/$', data_quality, name='data_quality'),
 ]
