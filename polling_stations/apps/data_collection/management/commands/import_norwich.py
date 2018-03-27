dcc9ea2573205de9a6662aaa06d5ae1bb93d0a22
chris48s <chris.shaw480@gmail.com>
2018-03-27 11:08:25 +0100

    import script for Norwich (closes #1211)

--- polling_stations/apps/data_collection/management/commands/import_norwich.py
+++ polling_stations/apps/data_collection/management/commands/import_norwich.py
@@ -2,9 +2,18 @@ from data_collection.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id      = 'E07000148'
-    addresses_name  = 'parl.2017-06-08/Version 1/polling_station_export-2017-05-15.csv'
-    stations_name   = 'parl.2017-06-08/Version 1/polling_station_export-2017-05-15.csv'
-    elections       = [
-        'local.norfolk.2017-05-04',
-        'parl.2017-06-08'
-    ]
+    addresses_name  = 'local.2018-05-03/Version 1/polling_station_export-2018-03-06.csv'
+    stations_name   = 'local.2018-05-03/Version 1/polling_station_export-2018-03-06.csv'
+    elections       = ['local.2018-05-03']
+
+    def address_record_to_dict(self, record):
+
+        if record.housepostcode == 'NR1 2EE':
+            return None
+
+        if record.houseid == '71720':
+            rec = super().address_record_to_dict(record)
+            rec['postcode'] = 'NR1 4AB'
+            return rec
+
+        return super().address_record_to_dict(record)
