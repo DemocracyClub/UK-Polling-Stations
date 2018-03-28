ce4396c796fd39323823d86b82559b145e9ef0e5
chris48s <chris.shaw480@gmail.com>
2018-03-28 18:45:58 +0100

    import script for Hart (closes #1181)

--- polling_stations/apps/data_collection/management/commands/import_hart.py
+++ polling_stations/apps/data_collection/management/commands/import_hart.py
@@ -2,15 +2,26 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = 'E07000089'
-    addresses_name = 'parl.2017-06-08/Version 1/Hart DC General Election polling place 120517.TSV'
-    stations_name = 'parl.2017-06-08/Version 1/Hart DC General Election polling place 120517.TSV'
-    elections = ['parl.2017-06-08']
+    addresses_name = 'local.2018-05-03/Version 2/Democracy_Club__03May2018.tsv'
+    stations_name = 'local.2018-05-03/Version 2/Democracy_Club__03May2018.tsv'
+    elections = ['local.2018-05-03']
     csv_delimiter = '\t'
 
     def station_record_to_dict(self, record):
 
-        if record.polling_place_id == '1914':
+        if record.polling_place_id == '2115':
             record = record._replace(polling_place_easting = '479224')
             record = record._replace(polling_place_northing = '154016')
 
+        if record.polling_place_id == '2338':
+            record = record._replace(polling_place_easting = '0')
+            record = record._replace(polling_place_northing = '0')
+
         return super().station_record_to_dict(record)
+
+    def address_record_to_dict(self, record):
+
+        if record.addressline6 == 'RG27 9QP':
+            return None
+
+        return super().address_record_to_dict(record)
