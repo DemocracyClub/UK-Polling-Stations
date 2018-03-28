370d89109afb2bed7396fde34ce1e5ea0171a018
chris48s <chris.shaw480@gmail.com>
2018-03-28 17:19:41 +0100

    import script for Reading (closes #1226)

--- polling_stations/apps/data_collection/management/commands/import_reading.py
+++ polling_stations/apps/data_collection/management/commands/import_reading.py
@@ -2,7 +2,25 @@ from data_collection.management.commands import BaseXpressDCCsvInconsistentPostc
 
 class Command(BaseXpressDCCsvInconsistentPostcodesImporter):
     council_id = 'E06000038'
-    addresses_name = 'parl.2017-06-08/Version 1/Democracy_Club__08June2017 (17).tsv'
-    stations_name = 'parl.2017-06-08/Version 1/Democracy_Club__08June2017 (17).tsv'
-    elections = ['parl.2017-06-08']
-    csv_delimiter = '\t'
+    addresses_name = 'local.2018-05-03/Version 1/Democracy_Club__03May2018 Reading.CSV'
+    stations_name = 'local.2018-05-03/Version 1/Democracy_Club__03May2018 Reading.CSV'
+    elections = ['local.2018-05-03']
+    csv_delimiter = ','
+
+    def address_record_to_dict(self, record):
+        if record.property_urn.strip() in ['310082434', '310082435']:
+            rec = super().address_record_to_dict(record)
+            rec['postcode'] = 'RG2 6AB'
+            return rec
+
+        if record.property_urn.strip() == '310074687':
+            rec = super().address_record_to_dict(record)
+            rec['postcode'] = 'RG1 5NN'
+            return rec
+
+        if record.property_urn.strip() == '310080353':
+            rec = super().address_record_to_dict(record)
+            rec['postcode'] = 'RG6 1DJ'
+            return rec
+
+        return super().address_record_to_dict(record)
