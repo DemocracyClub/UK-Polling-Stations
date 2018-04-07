2627309fcb53344b8fce75b883c26aa6403ddc82
chris48s <chris.shaw480@gmail.com>
2018-04-07 11:11:15 +0100

    import script for Milton Keynes (closes #1255)

--- polling_stations/apps/data_collection/management/commands/import_milton_keynes.py
+++ polling_stations/apps/data_collection/management/commands/import_milton_keynes.py
@@ -2,7 +2,22 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = 'E06000042'
-    addresses_name = 'parl.2017-06-08/Version 1/Democracy_Club__08June2017 (1).tsv'
-    stations_name = 'parl.2017-06-08/Version 1/Democracy_Club__08June2017 (1).tsv'
-    elections = ['parl.2017-06-08']
+    addresses_name = 'local.2018-05-03/Version 1/Democracy_Club__03May2018.tsv'
+    stations_name = 'local.2018-05-03/Version 1/Democracy_Club__03May2018.tsv'
+    elections = ['local.2018-05-03']
     csv_delimiter = '\t'
+
+    def station_record_to_dict(self, record):
+        if record.polling_place_id == '5280':
+            record = record._replace(polling_place_postcode='MK3 5NG')
+        return super().station_record_to_dict(record)
+
+    def address_record_to_dict(self, record):
+
+        if record.addressline6.strip() == 'MK18 1AT':
+            return None
+
+        if record.addressline6.strip() == 'MK10 7JD':
+            return None
+
+        return super().address_record_to_dict(record)
