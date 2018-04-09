76608d983e938b7b5fa8671727234bca4dbbac72
chris48s <chris.shaw480@gmail.com>
2018-04-09 09:28:52 +0100

    import script for Sefton (closes #1264)

--- polling_stations/apps/data_collection/management/commands/import_sefton.py
+++ polling_stations/apps/data_collection/management/commands/import_sefton.py
@@ -2,6 +2,30 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = 'E08000014'
-    addresses_name = 'parl.2017-06-08/Version 1/Democracy_Club__08June2017 2.CSV'
-    stations_name = 'parl.2017-06-08/Version 1/Democracy_Club__08June2017 2.CSV'
-    elections = ['parl.2017-06-08']
+    addresses_name = 'local.2018-05-03/Version 1/Democracy_Club__03May2018.tsv'
+    stations_name = 'local.2018-05-03/Version 1/Democracy_Club__03May2018.tsv'
+    elections = ['local.2018-05-03']
+    csv_delimiter = '\t'
+
+    def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip('0')
+
+        if record.addressline6 == 'L20 9AH':
+            return None
+
+        if record.addressline6 == 'L37 7AQ':
+            return None
+
+        if record.addressline6 == 'L20 6EA':
+            return None
+
+        if record.addressline6 == 'PR9 9JH':
+            return None
+
+        if record.addressline6 == 'L20 5AB':
+            return None
+
+        if record.addressline6 == 'L38 7JB':
+            return None
+
+        return super().address_record_to_dict(record)
