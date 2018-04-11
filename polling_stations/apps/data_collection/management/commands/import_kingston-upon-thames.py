1f3aaff5170d8887b20cac175cea7a760f039b20
chris48s <chris.shaw480@gmail.com>
2018-04-11 13:56:47 +0100

    import script for Kingston-upon-Thames (closes #1274)

--- polling_stations/apps/data_collection/management/commands/import_kingston-upon-thames.py
+++ polling_stations/apps/data_collection/management/commands/import_kingston-upon-thames.py
@@ -1,8 +1,26 @@
-from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
+from django.contrib.gis.geos import Point
+from data_collection.management.commands import BaseDemocracyCountsCsvImporter
 
-class Command(BaseXpressDemocracyClubCsvImporter):
+class Command(BaseDemocracyCountsCsvImporter):
     council_id = 'E09000021'
-    addresses_name = 'parl.2017-06-08/Version 1/Democracy_Club__08June2017 (10).tsv'
-    stations_name = 'parl.2017-06-08/Version 1/Democracy_Club__08June2017 (10).tsv'
-    elections = ['parl.2017-06-08']
-    csv_delimiter = '\t'
+    addresses_name = 'local.2018-05-03/Version 2/Democracy Club - Polling Districts Kingston.csv'
+    stations_name = 'local.2018-05-03/Version 2/Democracy Club - Polling Stations Kingston.csv'
+    elections = ['local.2018-05-03']
+
+    def address_record_to_dict(self, record):
+
+        if record.postcode == 'KT1 3JU':
+            return None
+        if record.postcode == 'KT1 1HG':
+            return None
+
+        return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+
+        rec = super().station_record_to_dict(record)
+
+        if rec['internal_council_id'] in ['T_71', 'T_72']:
+            rec['location'] = Point(-0.3009045, 51.4146479, srid=4326)
+
+        return rec
