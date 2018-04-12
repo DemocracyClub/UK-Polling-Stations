b793f95399a0936f1b5550d72e868d17bd771063
chris48s <chris.shaw480@gmail.com>
2018-04-12 12:42:27 +0100

    update Walsall data (closes #1291)

--- polling_stations/apps/data_collection/management/commands/import_walsall.py
+++ polling_stations/apps/data_collection/management/commands/import_walsall.py
@@ -1,3 +1,4 @@
+from django.contrib.gis.geos import Point
 from data_collection.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
@@ -6,6 +7,31 @@ class Command(BaseHalaroseCsvImporter):
     stations_name   = 'local.2018-05-03/Version 2/polling_station_export-2018-03-12.csv'
     elections       = ['local.2018-05-03']
     csv_encoding    = 'windows-1252'
+
+    def get_station_hash(self, record):
+        return "-".join([
+            record.pollingstationnumber.strip(),
+        ])
+
+    def station_record_to_dict(self, record):
+
+        # Changes requested by email
+        if record.pollingstationnumber == '24':
+            record = record._replace(pollingstationname='Community Room')
+            record = record._replace(pollingstationaddress_1='Mattersley Court')
+            record = record._replace(pollingstationaddress_2='Cresswell Crescent')
+            record = record._replace(pollingstationaddress_3='Walsall')
+            record = record._replace(pollingstationaddress_4='')
+            record = record._replace(pollingstationaddress_5='')
+            record = record._replace(pollingstationpostcode='WS3 2US')
+
+        if record.pollingstationnumber == '70':
+            record = record._replace(pollingstationpostcode='WS3 4JN')
+
+        rec = super().station_record_to_dict(record)
+        if rec['internal_council_id'] == '70':
+            rec['location'] = Point(-1.971183, 52.625281, srid=4326)
+        return rec
 
     def address_record_to_dict(self, record):
         if record.houseid in ['123217', '123218', '123219']:
