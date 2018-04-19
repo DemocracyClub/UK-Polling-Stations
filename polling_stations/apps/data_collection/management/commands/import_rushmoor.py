66e06c3b0e0d55dcb1c62fbba7ea0596013483d5
chris48s <chris.shaw480@gmail.com>
2018-04-19 11:26:28 +0100

    import script for Rushmoor (closes #1318)

---
+++ polling_stations/apps/data_collection/management/commands/import_rushmoor.py
@@ -0,0 +1,8 @@
+from data_collection.management.commands import BaseXpressDCCsvInconsistentPostcodesImporter
+
+class Command(BaseXpressDCCsvInconsistentPostcodesImporter):
+    council_id = 'E07000092'
+    addresses_name = 'local.2018-05-03/Version 1/Democracy_Club__03May2018.tsv'
+    stations_name = 'local.2018-05-03/Version 1/Democracy_Club__03May2018.tsv'
+    elections = ['local.2018-05-03']
+    csv_delimiter = '\t'
