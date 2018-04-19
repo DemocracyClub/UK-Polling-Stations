e2a69ae6c00b82d030b1d7a2ff3afee5ac3626d2
chris48s <chris.shaw480@gmail.com>
2018-04-19 11:11:12 +0100

    import script for Merton (closes #1311)

--- polling_stations/apps/data_collection/management/commands/import_merton.py
+++ polling_stations/apps/data_collection/management/commands/import_merton.py
@@ -2,7 +2,16 @@ from data_collection.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id      = 'E09000024'
-    addresses_name  = 'parl.2017-06-08/Version 2/polling_station_export-2017-05-09 4.csv'
-    stations_name   = 'parl.2017-06-08/Version 2/polling_station_export-2017-05-09 4.csv'
-    elections       = ['parl.2017-06-08']
+    addresses_name  = 'local.2018-05-03/Version 1/polling_station_export-2018-04-16.csv'
+    stations_name   = 'local.2018-05-03/Version 1/polling_station_export-2018-04-16.csv'
+    elections       = ['local.2018-05-03']
     csv_encoding    = 'windows-1252'
+
+    def address_record_to_dict(self, record):
+        if record.housepostcode == 'CR4 4JA':
+            return None
+
+        if record.houseid == '30851':
+            return None
+
+        return super().address_record_to_dict(record)
