726e99989e695939eac9d22e7caae2591896bba9
chris48s <chris.shaw480@gmail.com>
2018-04-23 09:29:47 +0100

    import script for Enfield (closes #1335)

--- polling_stations/apps/data_collection/management/commands/import_enfield.py
+++ polling_stations/apps/data_collection/management/commands/import_enfield.py
@@ -2,7 +2,27 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = 'E09000010'
-    addresses_name = 'parl.2017-06-08/Version 1/Democracy_Club__08June2017 (20).tsv'
-    stations_name = 'parl.2017-06-08/Version 1/Democracy_Club__08June2017 (20).tsv'
-    elections = ['parl.2017-06-08']
-    csv_delimiter = '\t'
+    addresses_name = 'local.2018-05-03/Version 1/Democracy Counts Polling Place Lookup Enfield.csv'
+    stations_name = 'local.2018-05-03/Version 1/Democracy Counts Polling Place Lookup Enfield.csv'
+    elections = ['local.2018-05-03']
+    csv_delimiter = ','
+
+    def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip('0')
+
+        if uprn == '207086112':
+            rec = super().address_record_to_dict(record)
+            rec['postcode'] = 'N13 4AP'
+            return rec
+
+        if uprn == '207017348':
+            rec = super().address_record_to_dict(record)
+            rec['postcode'] = 'N21 2AU'
+            return rec
+
+        if uprn == '207083595':
+            rec = super().address_record_to_dict(record)
+            rec['postcode'] = 'N21 2SD'
+            return rec
+
+        return super().address_record_to_dict(record)
