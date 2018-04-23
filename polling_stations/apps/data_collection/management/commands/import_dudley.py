2f3ffdc192b868b9f563c72db8d057c764622131
chris48s <chris.shaw480@gmail.com>
2018-04-23 09:26:00 +0100

    import script for Dudley (closes #1278)

--- polling_stations/apps/data_collection/management/commands/import_dudley.py
+++ polling_stations/apps/data_collection/management/commands/import_dudley.py
@@ -1,8 +1,31 @@
-from data_collection.management.commands import BaseXpressWebLookupCsvImporter
+from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
 
-class Command(BaseXpressWebLookupCsvImporter):
+class Command(BaseXpressDemocracyClubCsvImporter):
     council_id      = 'E08000027'
-    addresses_name  = 'parl.2017-06-08/Version 1/PropertyPostCodePollingStationWebLookup-2017-05-17.TSV'
-    stations_name   = 'parl.2017-06-08/Version 1/PropertyPostCodePollingStationWebLookup-2017-05-17.TSV'
-    elections       = ['parl.2017-06-08']
+    addresses_name  = 'local.2018-05-03/Version 1/Democracy_Club__03May2018 Dudley.tsv'
+    stations_name   = 'local.2018-05-03/Version 1/Democracy_Club__03May2018 Dudley.tsv'
+    elections       = ['local.2018-05-03']
     csv_delimiter   = '\t'
+    csv_encoding    = 'windows-1252'
+
+    def station_record_to_dict(self, record):
+
+        if record.polling_place_id == '18095':
+            record = record._replace(polling_place_postcode='DY3 3AB')
+
+        return super().station_record_to_dict(record)
+
+    def address_record_to_dict(self, record):
+
+        uprn = record.property_urn.strip().lstrip('0')
+
+        if record.addressline6.strip() == 'B63 4BN':
+            return None
+        if record.addressline6.strip() == 'B62 9EN':
+            return None
+        if record.addressline6.strip() == 'DY5 2HN':
+            return None
+        if record.addressline6.strip() == 'DY6 7JS':
+            return None
+
+        return super().address_record_to_dict(record)
