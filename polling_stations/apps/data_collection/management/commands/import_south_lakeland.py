59f3b71e2afa82bfcd4f3d8ba5b1d3deb8c71ea4
chris48s <chris.shaw480@gmail.com>
2018-04-25 16:21:21 +0100

    postcode correction for South Lakeland

--- polling_stations/apps/data_collection/management/commands/import_south_lakeland.py
+++ polling_stations/apps/data_collection/management/commands/import_south_lakeland.py
@@ -1,3 +1,4 @@
+from django.contrib.gis.geos import Point
 from data_collection.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
@@ -13,3 +14,13 @@ class Command(BaseHalaroseCsvImporter):
         record = record._replace(uprn='')
 
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+
+        if record.pollingstationnumber == '109':
+            record = record._replace(pollingstationpostcode='LA8 9LE')
+            rec = super().station_record_to_dict(record)
+            rec['location'] = Point(-2.709031, 54.398317, srid=4326)
+            return rec
+
+        return super().station_record_to_dict(record)
