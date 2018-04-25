83debd816f93d7938896af6baf5edae07691d905
chris48s <chris.shaw480@gmail.com>
2018-04-25 16:16:20 +0100

    add grid refs for Haringey (closes #1343)

--- polling_stations/apps/data_collection/management/commands/import_haringey.py
+++ polling_stations/apps/data_collection/management/commands/import_haringey.py
@@ -1,4 +1,7 @@
+import os
+from django.contrib.gis.geos import Point
 from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
+from pollingstations.models import PollingStation
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = 'E09000014'
@@ -6,6 +9,8 @@ class Command(BaseXpressDemocracyClubCsvImporter):
     stations_name = 'local.2018-05-03/Version 1/Democracy_Club__03May2018 Haringey.tsv'
     elections = ['local.2018-05-03']
     csv_delimiter = '\t'
+
+    station_id_field = 'polling_place_district_reference'
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip('0')
@@ -41,3 +46,36 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             return rec
 
         return super().address_record_to_dict(record)
+
+    # Haringey have supplied an additional file with
+    # better grid references for the polling stations
+    def post_import(self):
+        filepath = os.path.join(
+            self.base_folder_path,
+            'local.2018-05-03/Version 2/Polling_Stations_2018-eastings-and-northings Haringey.csv'
+        )
+        self.csv_delimiter = ','
+        gridrefs = self.get_data('csv', filepath)
+
+        print("Updating grid refs...")
+        for record in gridrefs:
+            district_id = record.districts.strip()
+            stations = PollingStation.objects.filter(
+                council_id=self.council_id,
+                internal_council_id=district_id
+            )
+            if len(stations) == 1:
+                station = stations[0]
+                station.location = Point(
+                    float(record.east),
+                    float(record.north),
+                    srid=27700
+                )
+                self.check_station_point({
+                    'location': station.location,
+                    'council_id': station.council_id,
+                })
+                station.save()
+            else:
+                print("Could not find station id " + district_id)
+        print("...done")
