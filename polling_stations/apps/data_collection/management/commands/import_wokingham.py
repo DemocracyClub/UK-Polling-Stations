a4405c1640fdd61660affbe7a8e5be5cfad63083
chris48s <chris.shaw480@gmail.com>
2018-04-26 10:30:02 +0100

    Wokingham postcode corrections

--- polling_stations/apps/data_collection/management/commands/import_wokingham.py
+++ polling_stations/apps/data_collection/management/commands/import_wokingham.py
@@ -6,3 +6,13 @@ class Command(BaseXpressDemocracyClubCsvImporter):
     stations_name = 'local.2018-05-03/Version 1/Democracy_Club__03May2018 (1) Wokingham.csv'
     elections = ['local.2018-05-03']
     csv_delimiter = ','
+
+    def station_record_to_dict(self, record):
+
+        if record.polling_place_id == '1347':
+            record = record._replace(polling_place_postcode = 'RG6 3HE')
+
+        if record.polling_place_id == '1371':
+            record = record._replace(polling_place_postcode = 'RG40 1XS')
+
+        return super().station_record_to_dict(record)
