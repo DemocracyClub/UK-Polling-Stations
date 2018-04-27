8c801c6cd64bd42c43cf416c15da7b5146990f76
chris48s <chris.shaw480@gmail.com>
2018-04-27 14:10:32 +0100

    import script for Harrow (closes #1349)

--- polling_stations/apps/data_collection/management/commands/import_harrow.py
+++ polling_stations/apps/data_collection/management/commands/import_harrow.py
@@ -2,9 +2,9 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = 'E09000015'
-    addresses_name = 'parl.2017-06-08/Version 1/Democracy_Club__08June2017 (19).tsv'
-    stations_name = 'parl.2017-06-08/Version 1/Democracy_Club__08June2017 (19).tsv'
-    elections = ['parl.2017-06-08']
+    addresses_name = 'local.2018-05-03/Version 1/Democracy_Club__03May2018 Harrow 1.tsv'
+    stations_name = 'local.2018-05-03/Version 1/Democracy_Club__03May2018 Harrow 1.tsv'
+    elections = ['local.2018-05-03']
     csv_delimiter = '\t'
 
     def station_record_to_dict(self, record):
@@ -13,3 +13,13 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             record = record._replace(polling_place_name = record.polling_place_name[:-1])
 
         return super().station_record_to_dict(record)
+
+    def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip('0')
+
+        if uprn == '100023030633':
+            rec = super().address_record_to_dict(record)
+            rec['postcode'] = 'HA2 9JE'
+            return rec
+
+        return super().address_record_to_dict(record)
