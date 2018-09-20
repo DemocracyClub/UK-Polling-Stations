62404e572518c8a2a5d1b90a3dfea6f337b93e84
chris48s <chris.shaw480@gmail.com>
2018-09-20 14:32:22 +0100

    import scripts for Broxtowe and Moray

--- polling_stations/apps/data_collection/management/commands/import_broxtowe.py
+++ polling_stations/apps/data_collection/management/commands/import_broxtowe.py
@@ -1,28 +1,45 @@
-from data_collection.management.commands import BaseShpStationsShpDistrictsImporter
+from data_collection.github_importer import BaseGitHubImporter
 
-class Command(BaseShpStationsShpDistrictsImporter):
-    srid = 27700
+class Command(BaseGitHubImporter):
+    srid = 4326
     council_id = 'E07000172'
-    districts_name = 'parl.2017-06-08/Version 1/Broxtowe_Polling_Information'
-    stations_name = 'parl.2017-06-08/Version 1/Broxtowe_Polling_Stations.shp'
-    elections = ['parl.2017-06-08']
+    elections = []
+    scraper_name = 'wdiv-scrapers/DC-PollingStations-Broxtowe'
+    geom_type = 'geojson'
 
     def district_record_to_dict(self, record):
+        poly = self.extract_geometry(
+            record,
+            self.geom_type,
+            self.get_srid('districts')
+        )
+
         return {
-            'internal_council_id': record[5].strip(),
-            'name': record[5].strip(),
-            'polling_station_id': record[5].strip(),
+            'internal_council_id': record['NAME'],
+            'name': ' - '.join([record['WARD_NAME'], record['NAME']]),
+            'area': poly,
+            'polling_station_id': record['NAME'],
         }
 
     def station_record_to_dict(self, record):
-        # attempt to lay the address out a bit more nicely
-        address = record[2]\
-            .strip()\
-            .replace(" Nottinghamshire ", "\nNottinghamshire\n")\
-            .replace(" Nottinghamshire", "\nNottinghamshire")
+        location = self.extract_geometry(
+            record,
+            self.geom_type,
+            self.get_srid('stations')
+        )
+
+        id_ = record['POLL_DIST']
+        if len(id_.split(' ')) > 1:
+            id_ = id_.split(' ')[0]
+
+        if record['ADDRESS'].lower().startswith(record['LABEL'].lower()):
+            address = record['ADDRESS']
+        else:
+            address = "\n".join([record['LABEL'], record['ADDRESS']])
 
         return {
-            'internal_council_id': record[4].strip(),
-            'postcode': record[3].strip(),
+            'internal_council_id': id_,
             'address': address,
+            'postcode': '',
+            'location': location,
         }
--- polling_stations/apps/data_collection/management/commands/import_moray.py
+++ polling_stations/apps/data_collection/management/commands/import_moray.py
@@ -1,9 +1,36 @@
-from data_collection.management.commands import BaseScotlandSpatialHubImporter
+from data_collection.github_importer import BaseGitHubImporter
 
-class Command(BaseScotlandSpatialHubImporter):
+class Command(BaseGitHubImporter):
+    srid = 4326
     council_id = 'S12000020'
-    council_name = 'Moray'
-    elections = [
-        'local.moray.2017-05-04',
-        'parl.2017-06-08'
-    ]
+    elections = []
+    scraper_name = 'wdiv-scrapers/DC-PollingStations-Moray'
+    geom_type = 'geojson'
+
+    def district_record_to_dict(self, record):
+        poly = self.extract_geometry(
+            record,
+            self.geom_type,
+            self.get_srid('districts')
+        )
+
+        return {
+            'internal_council_id': record['PD_CODE'],
+            'name': record['PD_CODE'],
+            'area': poly,
+            'polling_station_id': record['PD_CODE'],
+        }
+
+    def station_record_to_dict(self, record):
+        location = self.extract_geometry(
+            record,
+            self.geom_type,
+            self.get_srid('stations')
+        )
+
+        return {
+            'internal_council_id': record['PD_CODE'],
+            'address': record['Addr'],
+            'postcode': '',
+            'location': location,
+        }
