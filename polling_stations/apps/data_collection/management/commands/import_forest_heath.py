8f9caf06e307da10aec009615f1db59b0daa7c0c
chris48s <chris.shaw480@gmail.com>
2019-01-08 11:38:23 +0000

    delete old import scripts

--- polling_stations/apps/data_collection/management/commands/import_forest_heath.py
+++
@@ -1,8 +0,0 @@
-from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
-
-
-class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000201"
-    addresses_name = "Forest_Heath_split.csv"
-    stations_name = "Forest_Heath_split.csv"
-    elections = ["local.suffolk.2017-05-04", "parl.2017-06-08"]
--- polling_stations/apps/data_collection/management/commands/import_st_edmundsbury.py
+++
@@ -1,8 +0,0 @@
-from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
-
-
-class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000204"
-    addresses_name = "St_Edmundsbury_split.csv"
-    stations_name = "St_Edmundsbury_split.csv"
-    elections = ["local.suffolk.2017-05-04", "parl.2017-06-08"]
