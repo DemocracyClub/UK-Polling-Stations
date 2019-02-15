2e46888ddfaa0bf0eb582d6d9684f1931ad2cc7e
chris48s <chris.shaw480@gmail.com>
2019-02-15 15:55:24 +0000

    import script for Ashfield (closes #1480)

--- polling_stations/apps/data_collection/management/commands/import_ashfield.py
+++ polling_stations/apps/data_collection/management/commands/import_ashfield.py
@@ -1,56 +1,38 @@
-from django.db import transaction
-from django.db import connection
-from data_collection.management.commands import BaseShpStationsShpDistrictsImporter
-from pollingstations.models import PollingDistrict
-
-"""
-Ashfield publish their data on data.gov.uk :D
-..but they publish Excel and MapInfo binaries :(
-
-I've converted these to CSV and SHP format
-and uploaded that data to Amazon S3 for import purposes
-
-Additionally there's a hashes only scraper at
-https://github.com/wdiv-scrapers/DC-PollingStations-Ashfield
-polling the URLs to look for changes.
-"""
+from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
-class Command(BaseShpStationsShpDistrictsImporter):
+class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E07000170"
-    srid = 27700
-    districts_srid = 27700
-    districts_name = "New data/shp/polling_districts"
-    stations_name = "New data/shp/polling_stations.shp"
-    elections = ["local.nottinghamshire.2017-05-04", "parl.2017-06-08"]
+    addresses_name = (
+        "local.2019-05-02/Version 2/Democracy_Club__02May2019(REVISED)Ashf.tsv"
+    )
+    stations_name = (
+        "local.2019-05-02/Version 2/Democracy_Club__02May2019(REVISED)Ashf.tsv"
+    )
+    elections = ["local.2019-05-02"]
+    csv_delimiter = "\t"
 
-    def district_record_to_dict(self, record):
-        return {
-            "internal_council_id": str(record[0]).strip(),
-            "name": "%s - %s" % (str(record[1]).strip(), str(record[0]).strip()),
-            "polling_station_id": str(record[3]).strip(),
-        }
+    def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
 
-    def station_record_to_dict(self, record):
-        return {
-            "internal_council_id": str(record[0]).strip(),
-            "address": "%s\n%s" % (str(record[2]).strip(), str(record[3]).strip()),
-            "postcode": str(record[4]).strip(),
-        }
+        if uprn == "10001344107":
+            rec = super().address_record_to_dict(record)
+            rec["postcode"] = "NG15 8JA"
+            return rec
 
-    @transaction.atomic
-    def post_import(self):
-        # fix self-intersecting polygon
-        print("running fixup SQL")
-        table_name = PollingDistrict()._meta.db_table
+        if uprn == "10001337112":
+            rec = super().address_record_to_dict(record)
+            rec["postcode"] = "NG17 4FU"
+            return rec
 
-        cursor = connection.cursor()
-        cursor.execute(
-            """
-            UPDATE {0}
-            SET area=ST_Multi(ST_CollectionExtract(ST_MakeValid(area), 3))
-            WHERE NOT ST_IsValid(area);
-            """.format(
-                table_name
-            )
-        )
+        if uprn == "10001342248":
+            rec = super().address_record_to_dict(record)
+            rec["postcode"] = "NG17 1FU"
+            return rec
+
+        if uprn == "10070852165":
+            rec = super().address_record_to_dict(record)
+            rec["postcode"] = "NG17 8HS"
+            return rec
+
+        return super().address_record_to_dict(record)
