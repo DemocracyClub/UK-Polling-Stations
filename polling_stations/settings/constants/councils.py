0ddc4d8109647b55913afa4d2ec1fc8ec15f8e87
chris48s <chris.shaw480@gmail.com>
2019-02-19 12:52:44 +0000

    update ONS local auths data (again)

--- polling_stations/settings/constants/councils.py
+++ polling_stations/settings/constants/councils.py
@@ -2,7 +2,7 @@
 
 YVM_LA_URL = "https://www.yourvotematters.co.uk/_design/nested-content/results-page2/search-voting-locations-by-districtcode?queries_distcode_query="  # noqa
 BOUNDARIES_URL = (
-    "https://opendata.arcgis.com/datasets/e50a6790161743038d586e1a9494d9ff_1.geojson"
+    "https://opendata.arcgis.com/datasets/b2d5f4f8e9eb469bb22af910bdc1de22_1.geojson"
 )
 
 
