0766a2440717fac41e3a7e7b676a7c07babb08e7
chris48s <chris.shaw480@gmail.com>
2019-02-21 16:46:31 +0000

    import Calderdale (closes #1536)

--- polling_stations/apps/data_collection/management/commands/import_calderdale.py
+++ polling_stations/apps/data_collection/management/commands/import_calderdale.py
@@ -1,15 +1,12 @@
-from django.db import transaction
-from data_collection.data_types import StationSet
 from data_collection.geo_utils import fix_bad_polygons
 from data_collection.management.commands import BaseShpStationsShpDistrictsImporter
 
 
 class Command(BaseShpStationsShpDistrictsImporter):
     council_id = "E08000033"
-    districts_name = "local.2018-05-03/Version 1/POLLING_DISTRICTS_region.shp"
-    stations_name = "local.2018-05-03/Version 1/POLLING_STATIONS_region.shp"
-    elections = ["local.2018-05-03"]
-    station_addresses = {}
+    districts_name = "local.2019-05-02/Version 1/Polling Districts 2019 Shp/POLLING_DISTRICTS_2019_region.shp"
+    stations_name = "local.2019-05-02/Version 1/Polling Stations Shp 2019/POLLING_STATIONS_region.shp"
+    elections = ["local.2019-05-02"]
 
     def parse_string(self, text):
         try:
@@ -20,14 +17,6 @@ class Command(BaseShpStationsShpDistrictsImporter):
     def district_record_to_dict(self, record):
         code = self.parse_string(record[0])
         name = self.parse_string(record[1])
-        address = self.parse_string(record[5])
-
-        """
-        Some of the districts don't have a corresponding station point
-        in the stations file but every district has a station address.
-        We can use these to fill the gaps in later.
-        """
-        self.station_addresses[code] = address
         return {"internal_council_id": code, "name": name, "polling_station_id": code}
 
     def station_record_to_dict(self, record):
@@ -37,30 +26,7 @@ class Command(BaseShpStationsShpDistrictsImporter):
         if code == "" and address == "":
             return None
 
-        # remove station addresses from the districts file
-        # as we find them in the stations file
-        if code in self.station_addresses:
-            del (self.station_addresses[code])
-
         return {"internal_council_id": code, "address": address, "postcode": ""}
 
-    @transaction.atomic
-    def fill_the_blanks(self):
-        # mop up any districts where we have a station address
-        # attached to a district code but no point
-        self.stations = StationSet()
-        for code in self.station_addresses:
-            self.add_polling_station(
-                {
-                    "internal_council_id": code,
-                    "postcode": "",
-                    "address": self.station_addresses[code],
-                    "location": None,
-                    "council": self.council,
-                }
-            )
-        self.stations.save()
-
     def post_import(self):
-        self.fill_the_blanks()
         fix_bad_polygons()
