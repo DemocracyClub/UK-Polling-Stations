8392edd67c97673db9ccab23779e3ccb3ee187cf
GeoWill <roper.william@gmail.com>
2019-02-21 14:45:01 +0100

    Import script for Tamworth (closes #1506)

--- polling_stations/apps/data_collection/management/commands/import_tamworth.py
+++ polling_stations/apps/data_collection/management/commands/import_tamworth.py
@@ -3,11 +3,16 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E07000199"
-    addresses_name = (
-        "local.2018-05-03/Version 1/Democracy_Club__03May2018 (1) Tamworth.tsv"
-    )
-    stations_name = (
-        "local.2018-05-03/Version 1/Democracy_Club__03May2018 (1) Tamworth.tsv"
-    )
-    elections = ["local.2018-05-03"]
+    addresses_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019Tamworth.tsv"
+    stations_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019Tamworth.tsv"
+    elections = ["local.2019-05-02"]
     csv_delimiter = "\t"
+
+    def address_record_to_dict(self, record):
+        rec = super().address_record_to_dict(record)
+
+        # Uprn and address match to abp, but council postcode probably incorrect.
+        if record.property_urn.strip().lstrip("0") == "394034102":
+            rec["postcode"] = "B78 3XD"
+
+        return rec
