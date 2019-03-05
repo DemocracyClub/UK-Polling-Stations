aa20f8099622ace9eb24458e75205a9653342d9d
chris48s <chris.shaw480@gmail.com>
2019-03-05 16:52:40 +0000

    import Eastleigh (closes #1540)

--- polling_stations/apps/data_collection/management/commands/import_eastleigh.py
+++ polling_stations/apps/data_collection/management/commands/import_eastleigh.py
@@ -1,31 +1,35 @@
-from django.contrib.gis.geos import Point
 from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E07000086"
-    addresses_name = (
-        "local.2018-05-03/Version 1/Democracy_Club__03May2018 Eastleigh.tsv"
-    )
-    stations_name = "local.2018-05-03/Version 1/Democracy_Club__03May2018 Eastleigh.tsv"
-    elections = ["local.2018-05-03"]
+    addresses_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019Eastleigh.tsv"
+    stations_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019Eastleigh.tsv"
+    elections = ["local.2019-05-02"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
-
         uprn = record.property_urn.strip().lstrip("0")
+        rec = super().address_record_to_dict(record)
 
         if uprn == "100060324905":
             return None
 
-        return super().address_record_to_dict(record)
+        if uprn in [
+            "100060294726",  # SO533DA -> SO533HB : 242 Bournemouth Road, Chandler`s Ford, Eastleigh
+            "10009593644",  # SO533DA -> SO533AD : Dovecote, Howard Close, Chandler`s Ford, Eastleigh
+        ]:
+            rec["accept_suggestion"] = True
+
+        return rec
 
     def station_record_to_dict(self, record):
         if record.polling_place_uprn == "100062644887":
             record = record._replace(polling_place_postcode="SO53 2FT")
 
-        rec = super().station_record_to_dict(record)
-        if record.polling_place_uprn == "10009592012":
-            rec["location"] = Point(-1.3198333, 50.8591061, srid=4326)
+        # use UPRN for this one - E/N is in the sea
+        if record.polling_place_id == "4095":
+            record = record._replace(polling_place_easting="0")
+            record = record._replace(polling_place_northing="0")
 
-        return rec
+        return super().station_record_to_dict(record)
