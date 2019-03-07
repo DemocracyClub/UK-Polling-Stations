9bb87e014fc55d4e7123f316ecbfa43a9b38e632
chris48s <chris.shaw480@gmail.com>
2019-03-07 16:16:47 +0000

    import Salford (closes #1493)

--- polling_stations/apps/data_collection/management/commands/import_salford.py
+++ polling_stations/apps/data_collection/management/commands/import_salford.py
@@ -1,31 +1,39 @@
-from data_collection.github_importer import BaseGitHubImporter
+from django.contrib.gis.geos import Point
+from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
-class Command(BaseGitHubImporter):
-
-    srid = 4326
-    districts_srid = 4326
+class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E08000006"
-    elections = ["local.2018-05-03"]
-    scraper_name = "wdiv-scrapers/DC-PollingStations-Salford"
-    geom_type = "geojson"
+    addresses_name = (
+        "local.2019-05-02/Version 1/Democracy_Club__02May2019 SalfordCC.tsv"
+    )
+    stations_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019 SalfordCC.tsv"
+    elections = ["local.2019-05-02"]
+    csv_delimiter = "\t"
 
-    def district_record_to_dict(self, record):
-        poly = self.extract_geometry(record, self.geom_type, self.get_srid("districts"))
-        return {
-            "internal_council_id": record["code"],
-            "name": record["code"],
-            "area": poly,
-        }
+    def address_record_to_dict(self, record):
+        rec = super().address_record_to_dict(record)
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "100012471369",  # M380EL -> M280EL : Peel Mill Cottage, Peel Lane, Little Hulton, Worsley
+            "100011389722",  # M272BU -> M276BU : Heath Cottage, 119 Station Road, Pendlebury
+            "200001505152",  # M65RA -> M65YD : The Vicarage, 43 Derby Road, Seedley, Salford
+        ]:
+            rec["accept_suggestion"] = True
+
+        if uprn in [
+            "100012469873",  # M307RR -> M307QH : Moss Lane Farm, Barton Moss Road, Eccles
+            "100011405102",  # M65LS -> M67JT : 39 Duchy Street, Salford
+        ]:
+            rec["accept_suggestion"] = False
+
+        return rec
 
     def station_record_to_dict(self, record):
-        location = self.extract_geometry(
-            record, self.geom_type, self.get_srid("stations")
-        )
-        return {
-            "internal_council_id": record["polling_district"],
-            "postcode": "",
-            "address": record["station_location"],
-            "location": location,
-            "polling_district_id": record["polling_district"],
-        }
+        rec = super().station_record_to_dict(record)
+
+        if record.polling_place_id == "3897":
+            rec["location"] = Point(-2.41516389, 53.51105357, srid=4326)
+
+        return rec
