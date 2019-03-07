246cec6c0fe801d7d5e5e988a900725131891be9
chris48s <chris.shaw480@gmail.com>
2019-03-07 16:22:28 +0000

    import Harlow (closes #1567)

---
+++ polling_stations/apps/data_collection/management/commands/import_harlow.py
@@ -0,0 +1,19 @@
+from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
+
+
+class Command(BaseXpressDemocracyClubCsvImporter):
+    council_id = "E07000073"
+    addresses_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019 Harlow.CSV"
+    stations_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019 Harlow.CSV"
+    elections = ["local.2019-05-02"]
+
+    def address_record_to_dict(self, record):
+        rec = super().address_record_to_dict(record)
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "200002566340"  # CM179LU -> CM179DS : The Farmhouse, London Road, Harlow, Essex
+        ]:
+            rec["accept_suggestion"] = False
+
+        return rec
