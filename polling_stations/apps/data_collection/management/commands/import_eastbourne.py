6d6791a283c2c9d020992d507c1ebd7e5795b602
chris48s <chris.shaw480@gmail.com>
2019-03-07 16:11:58 +0000

    import Eastbourne (closes #1559)

--- polling_stations/apps/data_collection/management/commands/import_eastbourne.py
+++ polling_stations/apps/data_collection/management/commands/import_eastbourne.py
@@ -4,9 +4,27 @@ from data_collection.management.commands import BaseHalaroseCsvImporter
 class Command(BaseHalaroseCsvImporter):
     council_id = "E07000061"
     addresses_name = (
-        "parl.2017-06-08/Version 1/Eastbourne polling_station_export-2017-05-25.csv"
+        "local.2019-05-02/Version 1/polling_station_export-2019-02-26Eastb.csv"
     )
     stations_name = (
-        "parl.2017-06-08/Version 1/Eastbourne polling_station_export-2017-05-25.csv"
+        "local.2019-05-02/Version 1/polling_station_export-2019-02-26Eastb.csv"
     )
-    elections = ["parl.2017-06-08"]
+    elections = ["local.2019-05-02"]
+
+    def address_record_to_dict(self, record):
+        rec = super().address_record_to_dict(record)
+        uprn = record.uprn.strip().lstrip("0")
+
+        if uprn in [
+            "10010661279",  # BN208EJ -> BN207EJ : FLAT 6 8 CARLISLE ROAD, EASTBOURNE, EAST SUSSEX
+            "10010663461",  # BN211HG -> BN211HH : THE LAMBE INN HIGH STREET, EASTBOURNE, EAST SUSSEX
+        ]:
+            rec["accept_suggestion"] = True
+
+        if uprn in [
+            "10024140467",  # BN213EU -> BN228AG : 143 / 145 LANGNEY ROAD, EASTBOURNE, EAST SUSSEX
+            "100061919876",  # BN208NH -> BN208NR : THE DRIVE 153 VICTORIA DRIVE, EASTBOURNE, EAST SUSSEX
+        ]:
+            rec["accept_suggestion"] = False
+
+        return rec
