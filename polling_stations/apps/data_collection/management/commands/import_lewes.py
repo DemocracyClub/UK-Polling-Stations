701fe66569e1793378d7bd531cd5fecf7dfd6cc0
chris48s <chris.shaw480@gmail.com>
2019-03-07 16:24:04 +0000

    remove Lewes election id
    update pending

--- polling_stations/apps/data_collection/management/commands/import_lewes.py
+++ polling_stations/apps/data_collection/management/commands/import_lewes.py
@@ -9,7 +9,7 @@ class Command(BaseHalaroseCsvImporter):
     stations_name = (
         "local.2019-05-02/Version 1/polling_station_export-2019-02-18Lewes.csv"
     )
-    elections = ["local.2019-05-02"]
+    elections = []
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
