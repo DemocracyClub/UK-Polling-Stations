9280b54f86258c03dc927a1edfde23e6f2cab3da
chris48s <chris.shaw480@gmail.com>
2019-03-13 16:49:58 +0000

    import Dover (closes #1642)

--- polling_stations/apps/data_collection/management/commands/import_dover.py
+++ polling_stations/apps/data_collection/management/commands/import_dover.py
@@ -1,37 +1,50 @@
-from django.contrib.gis.geos import Point
+from django.contrib.gis.geos import MultiPoint
 from data_collection.github_importer import BaseGitHubImporter
 
 
 class Command(BaseGitHubImporter):
 
-    srid = 27700
+    srid = 4326
     districts_srid = 4326
     council_id = "E07000108"
-    elections = ["parl.2017-06-08"]
+    elections = ["local.2019-05-02"]
     scraper_name = "wdiv-scrapers/DC-PollingStations-Dover"
     geom_type = "geojson"
 
     def district_record_to_dict(self, record):
         poly = self.extract_geometry(record, self.geom_type, self.get_srid("districts"))
+        code = record["district"].split("-")[0].strip()
+
+        # PEX1 and PEX2 cover the exact same area
+        # PSL1 and PSL2 cover the exact same area
+        if code in ["PEX2", "PSL2"]:
+            return None
+
         return {
-            "internal_council_id": record["code"],
+            "internal_council_id": code,
             "name": record["district"],
             "area": poly,
-            "polling_station_id": record["code"],
+            "polling_station_id": code,
         }
 
     def station_record_to_dict(self, record):
-
-        location = Point(
-            float(record["EASTING"]),
-            float(record["NORTHING"]),
-            srid=self.get_srid("stations"),
+        location = self.extract_geometry(
+            record, self.geom_type, self.get_srid("stations")
         )
-        address = "\n".join([record["NAME_OF_PO"], record["LOCATION"]])
+        if isinstance(location, MultiPoint) and len(location) == 1:
+            location = location[0]
 
-        return {
-            "internal_council_id": record["POLLING_DI"],
-            "postcode": record["POSTCODE"],
-            "address": address,
-            "location": location,
-        }
+        address = "\n".join([record["NAME_OF_PO"], record["LOCATION"]])
+        codes = record["POLLING_DI"].split("&")
+
+        stations = []
+        for code in codes:
+            stations.append(
+                {
+                    "internal_council_id": code.strip(),
+                    "postcode": record["POSTCODE"],
+                    "address": address,
+                    "location": location,
+                }
+            )
+        return stations
