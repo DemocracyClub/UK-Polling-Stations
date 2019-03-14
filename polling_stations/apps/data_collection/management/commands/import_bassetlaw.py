a388e73a7cfbec55ed6dddf1450c165048495984
chris48s <chris.shaw480@gmail.com>
2019-03-14 13:33:17 +0000

    import Bassetlaw (closes #1548)

--- polling_stations/apps/data_collection/management/commands/import_bassetlaw.py
+++ polling_stations/apps/data_collection/management/commands/import_bassetlaw.py
@@ -1,3 +1,4 @@
+from django.contrib.gis.geos import Point
 from data_collection.management.commands import (
     BaseXpressDCCsvInconsistentPostcodesImporter,
 )
@@ -5,9 +6,33 @@ from data_collection.management.commands import (
 
 class Command(BaseXpressDCCsvInconsistentPostcodesImporter):
     council_id = "E07000171"
-    addresses_name = (
-        "parl.2017-06-08/Version 1/Bassetlaw Democracy_Club__08June2017.tsv"
-    )
-    stations_name = "parl.2017-06-08/Version 1/Bassetlaw Democracy_Club__08June2017.tsv"
-    elections = ["parl.2017-06-08"]
+    addresses_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019Basset.tsv"
+    stations_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019Basset.tsv"
+    elections = ["local.2019-05-02"]
     csv_delimiter = "\t"
+
+    def address_record_to_dict(self, record):
+
+        if record.property_urn == "10013978026":
+            record = record._replace(addressline6="NG22 0GW")
+
+        if record.property_urn == "10093191105":
+            record = record._replace(addressline6="S81 7SN")
+
+        # All of the UPRN data from Bassetlaw is a bit dubious.
+        # For safety I'm just going to ignore them all
+        record = record._replace(property_urn="")
+
+        if record.addressline6 in ["DN22 8AH", "DN22 0NP", "DN22 0JZ"]:
+            return None
+
+        return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        rec = super().station_record_to_dict(record)
+
+        # Manton Parish Hall
+        if record.polling_place_id == "8187":
+            rec["location"] = Point(-1.1105063, 53.2957291, srid=4326)
+
+        return rec
