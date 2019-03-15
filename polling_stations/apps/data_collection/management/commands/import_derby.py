3a61da830eadcd70642af57dd6b681dd43f01eb9
chris48s <chris.shaw480@gmail.com>
2019-03-15 16:42:48 +0000

    import Derby (closes #1553)

--- polling_stations/apps/data_collection/management/commands/import_derby.py
+++ polling_stations/apps/data_collection/management/commands/import_derby.py
@@ -1,44 +1,61 @@
-from django.contrib.gis.geos import Point
 from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E06000015"
-    addresses_name = "local.2018-05-03/Version 1/Democracy_Club__03May2018 Derby.csv"
-    stations_name = "local.2018-05-03/Version 1/Democracy_Club__03May2018 Derby.csv"
-    elections = ["local.2018-05-03"]
+    addresses_name = "local.2019-05-02/Version 2/Democracy_Club__02May2019derby.CSV"
+    stations_name = "local.2019-05-02/Version 2/Democracy_Club__02May2019derby.CSV"
+    elections = ["local.2019-05-02"]
     csv_delimiter = ","
 
     def address_record_to_dict(self, record):
+        rec = super().address_record_to_dict(record)
+        uprn = record.property_urn.strip().lstrip("0")
 
-        # All of the UPRN data from Derby is a bit dubious.
-        # I think they might have opened the CSV in Excel and accidentally
-        # 'rounded' all the UPRNs with more than N digits in them
-        # For safety I'm just going to ignore them all
-        record = record._replace(property_urn="")
+        if (
+            record.addressline1.strip() == "Flat 1"
+            and record.addressline2.strip() == "48 Bedford Street"
+            and record.addressline3.strip() == "Derby"
+        ):
+            rec["postcode"] = "DE22 3PB"
 
-        # given we can't rely on the UPRNs, we'll have to
-        # throw the baby out with the bathwater a bit here
-        bad_postcodes = [
-            "DE24 3LT",
-            "DE23 4AA",
-            "DE23 6PA",
-            "DE22 2NX",
-            "DE23 6GH",
-            "DE24 8BE",
-            "DE24 8RF",
-            "DE22 3FZ",
-            "DE21 2NL",
-        ]
+        if (
+            record.addressline1.strip() == "43A Coronation Avenue"
+            and record.addressline2.strip() == "Alvaston"
+            and record.addressline3.strip() == "Derby"
+        ):
+            rec["postcode"] = "DE24 0LR"
 
-        if record.addressline6.strip() in bad_postcodes:
+        if (
+            record.addressline1.strip() == "Flat 2"
+            and record.addressline2.strip() == "70 Wilkins Drive"
+            and record.addressline3.strip() == "Allenton"
+            and record.addressline4.strip() == "Derby"
+        ):
+            rec["postcode"] = "DE24 8LU"
+
+        if uprn == "10010687582":
+            rec["postcode"] = "DE22 4LT"
+
+        if record.addressline6.strip() == "DE23 6GH":
             return None
 
-        return super().address_record_to_dict(record)
+        if uprn in [
+            "100032007602",  # DE223FZ -> DE223SZ : Basement Flat, Flat 1, Rear of 302 Abbey Street, Derby
+            "100032008969",  # DE234AA -> DE236AD : International Hotel, 288-290 Burton Road, Derby
+            "10010687003",  # DE236SX -> DE238SX : Flat c, 202 St Thomas` Road, Derby
+        ]:
+            rec["accept_suggestion"] = True
 
-    def station_record_to_dict(self, record):
-        rec = super().station_record_to_dict(record)
-        # better grid ref
-        if rec["internal_council_id"] == "6652":
-            rec["location"] = Point(-1.4980912, 52.9057198, srid=4326)
+        if uprn in [
+            "100032256980",  # DE11RZ -> DE11RY : 112 Green Lane, Derby
+            "100030362297",  # DE231HD -> DE231LH : 420A Stenson Road, Sunnyhill, Derby
+            "10010685239",  # DE240UQ -> DE248UQ : The Needles, Bembridge Drive, Alvaston, Derby
+            "100030323067",  # DE221GZ -> DE231DG : 20 Highfield Road, Derby
+            "100030301767",  # DE39FT -> DE236WG : 20 Chestnut Avenue, Mickleover, Derby
+            "100030342785",  # DE217GZ -> DE216AN : 211 Nottingham Road, Spondon, Derby
+            "10071154563",  # DE13GB -> DE13DZ : Flat Above The Flowerpot, 23 King Street, Derby
+        ]:
+            rec["accept_suggestion"] = False
+
         return rec
