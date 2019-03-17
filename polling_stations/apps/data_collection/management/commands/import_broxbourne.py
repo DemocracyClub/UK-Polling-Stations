319dada98105cdf554db7bf2af0aa9a86d441531
chris48s <chris.shaw480@gmail.com>
2019-03-17 19:44:19 +0000

    import Broxbourne (closes #1603)

--- polling_stations/apps/data_collection/management/commands/import_broxbourne.py
+++ polling_stations/apps/data_collection/management/commands/import_broxbourne.py
@@ -3,15 +3,26 @@ from data_collection.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "E07000095"
-    addresses_name = "local.2018-05-03/Version 1/Democracy Club - Polling Districts-2018- Broxbourne.csv"
-    stations_name = "local.2018-05-03/Version 1/Democracy Club - Polling Stations-2018 - Broxbourne 2.csv"
-    elections = ["local.2018-05-03"]
+    addresses_name = "local.2019-05-02/Version 1/Democracy club- polling districts for Borough of Broxbourne.csv"
+    stations_name = "local.2019-05-02/Version 1/Democracy club- polling stations for Borough of Broxbourne.csv"
+    elections = ["local.2019-05-02"]
 
-    def station_record_to_dict(self, record):
+    def address_record_to_dict(self, record):
+        rec = super().address_record_to_dict(record)
+        uprn = record.uprn.strip().lstrip("0")
 
-        # Point supplied for Broxbourne School is miles off
-        if record.stationcode == "ACC_1":
-            record = record._replace(xordinate="")
-            record = record._replace(yordinate="")
+        if uprn in [
+            "148036797",  # EN89DG -> EN89DQ : 3 LEIGHTON COURT, TURNERS HILL, CHESHUNT, HERTFORDSHIRE
+            "148048401",  # EN88JJ -> EN88EB : 4A THEOBALDS COURT, CROSSBROOK STREET, CHESHUNT, HERTFORDSHIRE
+            "148004180",  # EN80JB -> EN80HT : 28 FLAMSTEAD END ROAD, CHESHUNT, HERTFORDSHIRE
+            "148004473",  # EN107QQ -> EN107FF : HILL CROSS FARM, HOLY CROSS HILL, WORMLEY, HERTFORDSHIRE
+        ]:
+            rec["accept_suggestion"] = True
 
-        return super().station_record_to_dict(record)
+        if uprn in [
+            "148048304",  # EN88JJ -> EN118JJ : 3 ST GEORGES HOUSE, 100 CROSSBROOK STREET, CHESHUNT, HERTFORDSHIRE
+            "148046488",  # EN106JJ -> EN106HT : 55A HIGH ROAD, WORMLEY, HERTFORDSHIRE
+        ]:
+            rec["accept_suggestion"] = False
+
+        return rec
