2b875d2fed45ef54fa8f90d63a6ac9d9f622756f
chris48s <chris.shaw480@gmail.com>
2019-03-17 19:44:43 +0000

    import Luton (closes #1579)

--- polling_stations/apps/data_collection/management/commands/import_luton.py
+++ polling_stations/apps/data_collection/management/commands/import_luton.py
@@ -1,9 +1,34 @@
-from data_collection.management.commands import BaseHalaroseCsvImporter
+from data_collection.management.commands import BaseDemocracyCountsCsvImporter
 
 
-class Command(BaseHalaroseCsvImporter):
+class Command(BaseDemocracyCountsCsvImporter):
     council_id = "E06000032"
-    addresses_name = "parl.2017-06-08/Version 1/polling_station_export-2017-05-10.csv"
-    stations_name = "parl.2017-06-08/Version 1/polling_station_export-2017-05-10.csv"
-    elections = ["parl.2017-06-08"]
+    addresses_name = (
+        "local.2019-05-02/Version 1/Democracy Club - Polling DistrictsLuton.csv"
+    )
+    stations_name = (
+        "local.2019-05-02/Version 1/Democracy Club - Polling StationsLuton.csv"
+    )
+    elections = ["local.2019-05-02"]
     csv_encoding = "windows-1252"
+
+    def address_record_to_dict(self, record):
+        rec = super().address_record_to_dict(record)
+        uprn = record.uprn.strip().lstrip("0")
+
+        if uprn in [
+            "10001038309",  # LU40HF -> LU40FH : 228 Servite Court, Morecambe Close, Luton
+            "100080152018",  # LU49LQ -> LU49LF : 54 High Street, Leagrave, Luton
+            "100080152011",  # LU49LQ -> LU49LF : 36 High Street, Leagrave, Luton
+            "100080152012",  # LU49LQ -> LU49LF : 40 High Street, Leagrave, Luton
+            "100080152013",  # LU49LQ -> LU49LF : 42 High Street, Leagrave, Luton
+            "100080152015",  # LU49LQ -> LU49LF : 46 High Street, Leagrave, Luton
+            "100080152016",  # LU49LQ -> LU49LF : 50 High Street, Leagrave, Luton
+            "100080152017",  # LU49LQ -> LU49LF : 52 High Street, Leagrave, Luton
+        ]:
+            rec["accept_suggestion"] = True
+
+        if uprn in ["100080171226"]:  # LU15HN -> LU11QZ : 49 Wilsden Avenue, Luton
+            rec["accept_suggestion"] = False
+
+        return rec
