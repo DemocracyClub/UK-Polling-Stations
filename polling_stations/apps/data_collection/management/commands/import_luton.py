df57f12c8b5ddfca5e2d50510d1f1c9432fba3d4
chris48s <chris.shaw480@gmail.com>
2019-03-21 16:37:07 +0000

    update Luton (closes #1691)

--- polling_stations/apps/data_collection/management/commands/import_luton.py
+++ polling_stations/apps/data_collection/management/commands/import_luton.py
@@ -3,18 +3,18 @@ from data_collection.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "E06000032"
-    addresses_name = (
-        "local.2019-05-02/Version 1/Democracy Club - Polling DistrictsLuton.csv"
-    )
-    stations_name = (
-        "local.2019-05-02/Version 1/Democracy Club - Polling StationsLuton.csv"
-    )
+    addresses_name = "local.2019-05-02/Version 2/Democracy Club - Polling Districts.csv"
+    stations_name = "local.2019-05-02/Version 2/Democracy Club - Polling Stations.csv"
     elections = ["local.2019-05-02"]
     csv_encoding = "windows-1252"
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
         uprn = record.uprn.strip().lstrip("0")
+
+        if uprn == "10090734899":
+            rec["postcode"] = "LU4 9BN"
+            rec["accept_suggestion"] = False
 
         if uprn in [
             "10001038309",  # LU40HF -> LU40FH : 228 Servite Court, Morecambe Close, Luton
@@ -25,6 +25,8 @@ class Command(BaseDemocracyCountsCsvImporter):
             "100080152015",  # LU49LQ -> LU49LF : 46 High Street, Leagrave, Luton
             "100080152016",  # LU49LQ -> LU49LF : 50 High Street, Leagrave, Luton
             "100080152017",  # LU49LQ -> LU49LF : 52 High Street, Leagrave, Luton
+            "200003273279",  # LU31TL -> LU27AU : SCHOOL HOUSE BARNFIELD COLLEGE, BARNFIELD AVENUE, LUTON
+            "100080152039",  # LU49LQ -> LU49LJ : 130 HIGH STREET, LUTON
         ]:
             rec["accept_suggestion"] = True
 
