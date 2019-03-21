21b76b6e91af4421827914def5f7ab0cd275ca33
chris48s <chris.shaw480@gmail.com>
2019-03-21 16:38:18 +0000

    update Mansfield (closes #1689)

--- polling_stations/apps/data_collection/management/commands/import_mansfield.py
+++ polling_stations/apps/data_collection/management/commands/import_mansfield.py
@@ -1,3 +1,4 @@
+from django.contrib.gis.geos import Point
 from data_collection.management.commands import BaseHalaroseCsvImporter
 
 
@@ -6,6 +7,12 @@ class Command(BaseHalaroseCsvImporter):
     addresses_name = "local.2019-05-02/Version 1/polling_station_export-2019-02-12.csv"
     stations_name = "local.2019-05-02/Version 1/polling_station_export-2019-02-12.csv"
     elections = ["local.2019-05-02"]
+
+    def station_record_to_dict(self, record):
+        rec = super().station_record_to_dict(record)
+        if rec["internal_council_id"] == "7-meden-vale-methodist-church":
+            rec["location"] = Point(-1.1309629, 53.2221165, srid=4326)
+        return rec
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
