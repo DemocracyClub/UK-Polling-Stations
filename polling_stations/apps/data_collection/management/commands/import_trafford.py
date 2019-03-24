dd125507f8303570262cdf6e77805f6b9007c6b5
chris48s <chris.shaw480@gmail.com>
2019-03-24 14:48:21 +0000

    import Trafford (closes #1637)

--- polling_stations/apps/data_collection/management/commands/import_trafford.py
+++ polling_stations/apps/data_collection/management/commands/import_trafford.py
@@ -3,24 +3,40 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E08000009"
-    addresses_name = "local.2018-05-03/Version 2/Democracy_Club__03May2018.tsv"
-    stations_name = "local.2018-05-03/Version 2/Democracy_Club__03May2018.tsv"
-    elections = ["local.2018-05-03"]
+    addresses_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019Trafford.tsv"
+    stations_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019Trafford.tsv"
+    elections = ["local.2019-05-02"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
-
+        rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
-        if record.addressline6 == "M33 5RJ":
-            return None
+        if uprn == "200000333851":
+            rec["postcode"] = "M41 6AS"
 
-        if uprn == "100012491068":
-            rec = super().address_record_to_dict(record)
-            rec["postcode"] = "WA14 5RJ"
-            return rec
+        if uprn == "10070456948":
+            rec["accept_suggestion"] = False
 
-        if record.addressline6 == "WA13 9TY":
-            return None
+        if uprn in [
+            "100012491068",  # WA144RJ -> WA145RJ : 1 Red House Cottages, Red House Lane, Dunham Massey
+            "10070444322",  # WA145UX -> WA145UZ : Apartment 1, 2 Badger Road, West Timperley, Altrincham
+            "10070444323",  # WA145UX -> WA145UZ : Apartment 2, 2 Badger Road, West Timperley, Altrincham
+            "10070444324",  # WA145UX -> WA145UZ : Apartment 3, 2 Badger Road, West Timperley, Altrincham
+            "100012495530",  # M314WJ -> M314NL : Carlton, 29 Warburton Lane, Partington
+            "200000333945",  # M416JU -> M416DU : 83 Irlam Road, Flixton
+            "100011674930",  # M416JS -> M416GT : 12 The Avenue, Flixton
+            "100012496466",  # M337WH -> M337QH : Bridge Inn, Dane Road, Sale
+        ]:
+            rec["accept_suggestion"] = True
 
-        return super().address_record_to_dict(record)
+        if uprn in [
+            "10070460306",  # WA158UH -> WA158SZ : 3 Great Oak Drive, Altrincham
+            "200000340318",  # M337RE -> M337TY : Apartment 18, 100 Washway Road, Sale
+            "100012491343",  # WA143HD -> WA143HF : Woodhatch, South Downs Road, Bowdon
+            "100011654153",  # M417DQ -> M417DN : 50 Davyhulme Road, Davyhulme
+            "100011700864",  # M332UW -> M334RZ : 264 Norris Road, Sale
+        ]:
+            rec["accept_suggestion"] = False
+
+        return rec
