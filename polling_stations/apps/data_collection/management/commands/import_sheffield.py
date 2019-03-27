27b86540db3c409f42b40cc9b205a0a2b000e11b
chris48s <chris.shaw480@gmail.com>
2019-03-27 17:20:04 +0000

    import Sheffield (closes #1669)

--- polling_stations/apps/data_collection/management/commands/import_sheffield.py
+++ polling_stations/apps/data_collection/management/commands/import_sheffield.py
@@ -1,48 +1,69 @@
-import os
-from django.contrib.gis.geos import Point
 from data_collection.management.commands import BaseHalaroseCsvImporter
-from pollingstations.models import PollingStation
 
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "E08000019"
-    addresses_name = "local.2018-05-03/Version 1/polling_station_export-2018-03-16.csv"
-    stations_name = "local.2018-05-03/Version 1/polling_station_export-2018-03-16.csv"
-    elections = ["local.2018-05-03", "mayor.sheffield-city-ca.2018-05-03"]
+    addresses_name = (
+        "local.2019-05-02/Version 1/polling_station_export-2019-03-19sheff.csv"
+    )
+    stations_name = (
+        "local.2019-05-02/Version 1/polling_station_export-2019-03-19sheff.csv"
+    )
+    elections = ["local.2019-05-02"]
     csv_encoding = "windows-1252"
 
-    def get_station_hash(self, record):
-        return "-".join([record.pollingstationnumber.strip()])
-
     def address_record_to_dict(self, record):
-        if record.houseid == "12006669":
+        rec = super().address_record_to_dict(record)
+        uprn = record.uprn.strip().lstrip("0")
+
+        if record.housepostcode == "S20 3FS":
             return None
-        return super().address_record_to_dict(record)
 
-    # Sheffield have supplied an additional file with
-    # better grid references for the polling stations
-    def post_import(self):
-        filepath = os.path.join(
-            self.base_folder_path,
-            "local.2018-05-03/Version 1/Polling_Stations with GIS coordinate Sheffield.csv",
-        )
-        gridrefs = self.get_data("csv", filepath)
+        if record.houseid == "3071121":
+            rec["postcode"] = "S17 4AT"
+            rec["accept_suggestion"] = False
 
-        print("Updating grid refs...")
-        for record in gridrefs:
-            stations = PollingStation.objects.filter(
-                council_id=self.council_id,
-                internal_council_id=self.get_station_hash(record),
-            )
-            if len(stations) == 1:
-                station = stations[0]
-                station.location = Point(
-                    float(record.easting), float(record.northing), srid=27700
-                )
-                self.check_station_point(
-                    {"location": station.location, "council_id": station.council_id}
-                )
-                station.save()
-            else:
-                print("Could not find station id " + self.get_station_hash(record))
-        print("...done")
+        if uprn in [
+            "10022927869",  # S24NA -> S14RQ : Apartment 7 2 Mary Street, Sheffield
+            "100050961874",  # S118TB -> S118TD : Flat Over 782 Ecclesall Road, Sheffield
+            "10003572911",  # S80RL -> S80RN : Flat Over 67 Chesterfield Road, Sheffield
+            "100051115645",  # S80PL -> S80NT : Apartment 2 67 Smithy Wood Crescent, Sheffield
+            "100051115646",  # S80PL -> S80NT : Apartment 1 67 Smithy Wood Crescent, Sheffield
+            "100051072577",  # S117AA -> S118ZP : Over 398 Sharrow Vale Road, Sheffield
+        ]:
+            rec["accept_suggestion"] = True
+
+        if uprn in [
+            "100052084411",  # S118RQ -> S118RP : Coach House 11 Brocco Bank, Sheffield
+            "10023155059",  # S11HA -> S57EH : Archer Project Sheffield Cathedral Church Street, Sheffield
+            "10090614322",  # S80ZH -> S87LD : 88 Chippinghouse Road, Sheffield
+            "100051004490",  # S117TG -> S103TE : School House Ivy Cottage Lane, Sheffield
+            "100050928743",  # S104PE -> S104QX : Soughley Cottage Brown Hills Lane, Sheffield
+            "10003573730",  # S66GH -> S66GD : Fern Hill Hollow Meadows, Sheffield
+            "10013159987",  # S66GQ -> S66LJ : 1 Trouble Wood Lane, Sheffield
+            "10013159991",  # S66GQ -> S66LJ : 2 Trouble Wood Lane, Sheffield
+            "10094032044",  # S66GQ -> S66LJ : 3 Trouble Wood Lane, Sheffield
+            "100051072776",  # S124LW -> S124LT : The Fairway 162 Sheffield Road, Hackenthorpe, Sheffield
+            "200002991853",  # S137PL -> S124LP : 1 Beighton Road, Sheffield
+            "200003018213",  # S101QS -> S101QN : 8 Northfield Road, Sheffield
+            "10003576025",  # S64HA -> S64GW : Over 49 Middlewood Road, Sheffield
+            "200003024433",  # S62DH -> S64HB : Hillsborough Park Lodge 947 Penistone Road, Sheffield
+            "200003000612",  # S25RS -> S25QQ : 192 Long Henry Row, Sheffield
+            "200003000616",  # S25RS -> S25QQ : 196 Long Henry Row, Sheffield
+            "200002999863",  # S25RH -> S25QQ : 176 Norwich Row, Sheffield
+            "200002999864",  # S25RH -> S25QQ : 177 Norwich Row, Sheffield
+            "200002999874",  # S25RH -> S25QQ : 187 Norwich Row, Sheffield
+            "200002999883",  # S25RH -> S25QQ : 196 Norwich Row, Sheffield
+            "200002999897",  # S25RH -> S25QQ : 210 Norwich Row, Sheffield
+            "200002999315",  # S25RH -> S25QQ : 222 Norwich Row, Sheffield
+            "200002999318",  # S25RH -> S25QQ : 225 Norwich Row, Sheffield
+            "200002999858",  # S25RG -> S25QQ : 98 Norwich Row, Sheffield
+            "200003016406",  # S66LJ -> S66LH : Ashley Cottage High Bradfield, Sheffield
+            "200003021852",  # S363ZB -> S363ZA : Yew Trees Cottage Yew Trees Lane, Bolsterstone, Sheffield
+            "200003021853",  # S363ZB -> S363ZA : Yew Trees Farm Yew Trees Lane, Bolsterstone, Sheffield
+            "100050981171",  # S361LL -> S364GH : The Cruck Barn Green Farm Green Lane, Stocksbridge, Sheffield
+            "200003025132",  # S362PR -> S362NR : The Boskins Royd Farm Carr Road, Deepcar, Sheffield
+        ]:
+            rec["accept_suggestion"] = False
+
+        return rec
