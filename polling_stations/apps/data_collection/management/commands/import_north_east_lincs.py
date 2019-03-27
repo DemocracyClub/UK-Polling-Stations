a64a23c9e37214676a49ee10ad4b74401a8dfdfc
chris48s <chris.shaw480@gmail.com>
2019-03-27 17:21:48 +0000

    import North East Lincolnshire (closes #1658)

--- polling_stations/apps/data_collection/management/commands/import_north_east_lincs.py
+++ polling_stations/apps/data_collection/management/commands/import_north_east_lincs.py
@@ -3,7 +3,33 @@ from data_collection.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "E06000012"
-    addresses_name = "local.2018-05-03/Version 2/polling_station_export-2018-04-12.csv"
-    stations_name = "local.2018-05-03/Version 2/polling_station_export-2018-04-12.csv"
-    elections = ["local.2018-05-03"]
+    addresses_name = (
+        "local.2019-05-02/Version 1/polling_station_export-2019-03-15NELC.csv"
+    )
+    stations_name = (
+        "local.2019-05-02/Version 1/polling_station_export-2019-03-15NELC.csv"
+    )
+    elections = ["local.2019-05-02"]
     csv_encoding = "windows-1252"
+
+    def address_record_to_dict(self, record):
+        rec = super().address_record_to_dict(record)
+        uprn = record.uprn.strip().lstrip("0")
+
+        if record.housepostcode.strip() == "DN333 3E":
+            rec["postcode"] = "DN33 3EN"
+
+        if uprn in [
+            "11073783"  # DN364QR -> DN364QQ : 298 STATION ROAD, NEW WALTHAM, NORTH EAST LINCOLNSHIRE
+        ]:
+            rec["accept_suggestion"] = True
+
+        if uprn in [
+            "11073743",  # DN364RX -> DN364RY : THE BUNGALOW WALTHAM HOUSE LOUTH ROAD, WALTHAM, NORTH EAST LINCOLNSHIRE
+            "11076295",  # DN357AB -> DN357HE : GROUND FLOOR REAR FLAT 22 GRIMSBY ROAD, CLEETHORPES, NORTH EAST LINCOLNSHIRE
+            "11019854",  # DN370HU -> DN370JA : BRIAR FARM CHEAPSIDE, WALTHAM, NORTH EAST LINCOLNSHIRE
+            "11053301",  # DN370HU -> DN370HT : BRIGSLEY TOP FARM CHEAPSIDE, WALTHAM, NORTH EAST LINCOLNSHIRE
+        ]:
+            rec["accept_suggestion"] = False
+
+        return rec
