1732745ed041fb9ccebb944d0e6f9b320923cc26
chris48s <chris.shaw480@gmail.com>
2019-03-28 11:36:58 +0000

    import Rugby (closes #1671)

--- polling_stations/apps/data_collection/management/commands/import_rugby.py
+++ polling_stations/apps/data_collection/management/commands/import_rugby.py
@@ -3,17 +3,21 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E07000220"
-    addresses_name = "local.2018-05-03/Version 1/Democracy_Club__03May2018 Rugby.tsv"
-    stations_name = "local.2018-05-03/Version 1/Democracy_Club__03May2018 Rugby.tsv"
-    elections = ["local.2018-05-03"]
+    addresses_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019ruggers.tsv"
+    stations_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019ruggers.tsv"
+    elections = ["local.2019-05-02"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
+        rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
-        if uprn == "10010523748":
-            rec = super().address_record_to_dict(record)
-            rec["postcode"] = "CV22 7EX"
-            return rec
+        if uprn == "10010524278":
+            rec["postcode"] = "CV7 9LX"
 
-        return super().address_record_to_dict(record)
+        if uprn in [
+            "100070189342"  # CV227JT -> CV227JS : 83 Lawford Lane, Bilton, Rugby
+        ]:
+            rec["accept_suggestion"] = False
+
+        return rec
