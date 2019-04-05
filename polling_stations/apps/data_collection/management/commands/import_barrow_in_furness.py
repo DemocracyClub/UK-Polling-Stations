9ff4be5f9f441df874411a3302ca69bb96dd23ee
chris48s <chris.shaw480@gmail.com>
2019-04-05 16:30:07 +0100

    import Barrow-in-Furness (closes #1680)

--- polling_stations/apps/data_collection/management/commands/import_barrow_in_furness.py
+++ polling_stations/apps/data_collection/management/commands/import_barrow_in_furness.py
@@ -1,32 +1,32 @@
-from data_collection.github_importer import BaseGitHubImporter
+from data_collection.management.commands import BaseDemocracyCountsCsvImporter
 
 
-class Command(BaseGitHubImporter):
-
-    srid = 4326
-    districts_srid = 4326
+class Command(BaseDemocracyCountsCsvImporter):
     council_id = "E07000027"
-    elections = ["parl.2017-06-08"]
-    scraper_name = "wdiv-scrapers/DC-PollingStations-BarrowInFurness"
-    geom_type = "geojson"
+    addresses_name = "local.2019-05-02/Version 1/DemocracyClubPollingDistrictsbdc.csv"
+    stations_name = "local.2019-05-02/Version 1/DemocracyClubPollingStationsbdc.csv"
+    elections = ["local.2019-05-02"]
 
-    def district_record_to_dict(self, record):
-        poly = self.extract_geometry(record, self.geom_type, self.get_srid("districts"))
-        return {
-            "internal_council_id": record["INFO"],
-            "name": record["INFO"],
-            "area": poly,
-            "polling_station_id": record["INFO"],
-        }
+    def address_record_to_dict(self, record):
+        rec = super().address_record_to_dict(record)
+        uprn = record.uprn.strip().lstrip("0")
 
-    def station_record_to_dict(self, record):
-        location = self.extract_geometry(
-            record, self.geom_type, self.get_srid("stations")
-        )
-        return {
-            "internal_council_id": record["POLLING_DISTRICT"],
-            "postcode": "",
-            "address": "%s\n%s"
-            % (record["POLLING_STATION"], record["POLLING_ADDRESS"]),
-            "location": location,
-        }
+        if uprn in [
+            "36025885"  # LA130PJ -> LA130PG : West Gate Cottage, Manor Road, Barrow-in-Furness
+        ]:
+            rec["accept_suggestion"] = True
+
+        if uprn in [
+            "36015231",  # LA141BS -> LA141DU : 289 Rawlinson Street, Barrow-in-Furness
+            "36016015",  # LA141DU -> LA145AY : 26 Crellin Street, Barrow-in-Furness
+            "36025851",  # LA139HY -> LA139LT : 81 Newbarns Village, Hollow Lane, Barrow-in-Furness
+            "36047048",  # LA130UF -> LA142UJ : 7 Rosewood Grove, Barrow-in-Furness
+            "36030436",  # LA158BQ -> LA158BD : 43 Mount Pleasant, Skelgate, Dalton-in-Furness
+            "36034298",  # LA130PD -> LA130PB : North Lodge, Abbey Road, Dalton-in-Furness
+            "36031188",  # LA130NF -> LA130NX : The Bungalow, Greystone Lane, Dalton-in-Furness
+            "36031184",  # LA130NF -> LA130ED : North Stank Farm, Newton Cross Road, Newton-in-Furness
+            "36033063",  # LA158RP -> LA158HY : 40 Bankside, Broughton Road, Dalton-in-Furness
+        ]:
+            rec["accept_suggestion"] = False
+
+        return rec
