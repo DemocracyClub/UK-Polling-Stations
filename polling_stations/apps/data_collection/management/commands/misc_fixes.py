b057acf4ef56dbf8961e4fba0cc7e4b3f3f2569a
chris48s <chris.shaw480@gmail.com>
2019-04-23 16:24:28 +0100

    remove ad-hoc fixes
    these are up-to-date on YVM now

--- polling_stations/apps/data_collection/management/commands/misc_fixes.py
+++ polling_stations/apps/data_collection/management/commands/misc_fixes.py
@@ -22,18 +22,6 @@ def update_station_point(council_id, station_id, point):
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
 
-        print("updating Southend email...")
-        southend = Council.objects.get(pk="E06000033")
-        southend.email = "elections@southend.gov.uk"
-        southend.save()
-        print("..updated")
-
-        print("updating Wyre Forest email...")
-        wyre_forest = Council.objects.get(pk="E07000239")
-        wyre_forest.email = "customerservice@wyreforestdc.gov.uk"
-        wyre_forest.save()
-        print("..updated")
-
         print("updating Watford address...")
         watford = Council.objects.get(pk="E07000103")
         watford.address = "Electoral Services Office\nRoom 22\nTown Hall\nWatford"
