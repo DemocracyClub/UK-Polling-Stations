b7752868a3e641ccacc5a2dbe1cfe10497de6203
GeoWill <roper.william@gmail.com>
2019-04-25 12:42:32 +0200

    Import script for Croydon (closes #1830)

--- polling_stations/apps/data_collection/management/commands/misc_fixes.py
+++ polling_stations/apps/data_collection/management/commands/misc_fixes.py
@@ -67,6 +67,7 @@ class Command(BaseCommand):
             "10024288189",
             "10023117526",
             "10009922833",
+            "10014047099",
         ]
         addresses = Address.objects.filter(pk__in=bad_uprns)
         for address in addresses:
