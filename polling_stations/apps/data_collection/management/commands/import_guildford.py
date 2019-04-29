ecc782616171042ae1cc723d25c2dd591fbe4709
chris48s <chris.shaw480@gmail.com>
2019-04-29 20:01:00 +0100

    fix Guildford import script

--- polling_stations/apps/data_collection/management/commands/import_guildford.py
+++ polling_stations/apps/data_collection/management/commands/import_guildford.py
@@ -11,7 +11,26 @@ class Command(BaseGitHubImporter):
     scraper_name = "wdiv-scrapers/DC-PollingStations-Guildford"
     geom_type = "gml"
 
+    def replace_schema(self, record):
+        """
+        Shoddy workaround for:
+        Failed to connect to www2.guildford.gov.uk port 80: Connection timed out
+
+        This is *definitely* not the correct way to do this
+        but it gets the job done
+        """
+        record["geometry"] = record["geometry"].replace(
+            "http://www2.guildford.gov.uk/ishare5.2.web/getows.ashx?mapsource=GBC/Inspire&amp;service=WFS&amp;SERVICE=WFS&amp;VERSION=1.1.0&amp;REQUEST=DescribeFeatureType&amp;TYPENAME=polling_districts&amp;OUTPUTFORMAT=XMLSCHEMA",
+            "",
+        )
+        record["geometry"] = record["geometry"].replace(
+            "http://www2.guildford.gov.uk/ishare5.2.web/getows.ashx?mapsource=GBC/Inspire&amp;service=WFS&amp;SERVICE=WFS&amp;VERSION=1.1.0&amp;REQUEST=DescribeFeatureType&amp;TYPENAME=polling_places&amp;OUTPUTFORMAT=XMLSCHEMA",
+            "",
+        )
+        return record
+
     def district_record_to_dict(self, record):
+        record = self.replace_schema(record)
         poly = self.extract_geometry(record, self.geom_type, self.get_srid("districts"))
         return {
             "internal_council_id": record["register"],
@@ -21,6 +40,7 @@ class Command(BaseGitHubImporter):
         }
 
     def station_record_to_dict(self, record):
+        record = self.replace_schema(record)
         location = self.extract_geometry(
             record, self.geom_type, self.get_srid("stations")
         )
