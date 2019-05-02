55bf16cf0667df7a06b2ae5b90a61d0e3dd6f35e
chris48s <chris.shaw480@gmail.com>
2019-05-02 11:39:18 +0100

    updated file for South Kesteven

--- polling_stations/apps/data_collection/management/commands/import_south_kesteven.py
+++ polling_stations/apps/data_collection/management/commands/import_south_kesteven.py
@@ -3,8 +3,8 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E07000141"
-    addresses_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019 skest.tsv"
-    stations_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019 skest.tsv"
+    addresses_name = "local.2019-05-02/Version 2/Democracy_Club__02May2019.tsv"
+    stations_name = "local.2019-05-02/Version 2/Democracy_Club__02May2019.tsv"
     elections = ["local.2019-05-02"]
     csv_delimiter = "\t"
 
