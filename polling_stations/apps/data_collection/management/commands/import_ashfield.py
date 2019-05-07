f0831a72944d0685279b504fe62a071a7bf8b67c
GeoWill <roper.william@gmail.com>
2019-05-07 15:46:53 +0100

    Import script for Ashfield (closes #1839)

--- polling_stations/apps/data_collection/management/commands/import_ashfield.py
+++ polling_stations/apps/data_collection/management/commands/import_ashfield.py
@@ -4,20 +4,35 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E07000170"
     addresses_name = (
-        "local.2019-05-02/Version 2/Democracy_Club__02May2019(REVISED)Ashf.tsv"
+        "europarl.2019-05-23/Version 1/Democracy_Club__23May2019Ashfield.tsv"
     )
+
     stations_name = (
-        "local.2019-05-02/Version 2/Democracy_Club__02May2019(REVISED)Ashf.tsv"
+        "europarl.2019-05-23/Version 1/Democracy_Club__23May2019Ashfield.tsv"
     )
-    elections = ["local.2019-05-02"]
+
+    elections = ["europarl.2019-05-23"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
+        rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
-        if uprn in ["100031234554", "100031241226", "10001342248", "10070852165"]:
-            rec = super().address_record_to_dict(record)
-            rec["accept_suggestion"] = True
-            return rec
+        if uprn == "200003310492":
+            rec["postcode"] = "DE554PB"
+            rec["accept_suggestion"] = False
 
-        return super().address_record_to_dict(record)
+        if uprn in [
+            "100031234554",  # NG177GX -> NG177HJ : 1A Oak Street, Kirkby In Ashfield, Nottingham
+            "10070852165",  # NG175HS -> NG178HS : Flat 6, 5 Borders Avenue, Kirkby In Ashfield, Nottingham
+            "100031241226",  # NG178JR -> NG178JT : Waverley House, The Hill, Kirkby In Ashfield, Nottingham
+            "10001342248",  # NG177FU -> NG171FU : The Old Coach House, Clumber Street, Sutton In Ashfield
+        ]:
+            rec["accept_suggestion"] = True
+
+        if uprn in [
+            "100031229455"  # NG178AG -> NG172PA : 4 King Street, Kirkby In Ashfield, Nottingham
+        ]:
+            rec["accept_suggestion"] = False
+
+        return rec
