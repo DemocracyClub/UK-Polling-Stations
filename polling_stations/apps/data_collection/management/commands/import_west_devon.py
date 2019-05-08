19baa45fcdb67e8124fb334ae1d00dabe45916f8
mtravis <matt.travis1@gmail.com>
2019-05-08 16:39:23 +0100

    import script for West Devon (closes #1848)

--- polling_stations/apps/data_collection/management/commands/import_west_devon.py
+++ polling_stations/apps/data_collection/management/commands/import_west_devon.py
@@ -3,14 +3,17 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E07000047"
-    addresses_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019WD.tsv"
-    stations_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019WD.tsv"
-    elections = ["local.2019-05-02"]
+    addresses_name = "europarl.2019-05-23/Version 1/Democracy_Club__23May2019WD.tsv"
+    stations_name = "europarl.2019-05-23/Version 1/Democracy_Club__23May2019WD.tsv"
+    elections = ["europarl.2019-05-23"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn == "10013755070":
+            rec["postcode"] = "EX201FQ"
 
         if uprn in [
             "10001329690"  # EX201SQ -> EX201JJ : Beechwood, Upcott Hill, Okehampton, Devon
