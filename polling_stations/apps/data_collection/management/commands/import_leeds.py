e9539786cfea4cefb5a06a6576d9e3078321f5d2
chris48s <chris.shaw480@gmail.com>
2019-05-08 16:40:55 +0100

    import Leeds (closes #1861)

--- polling_stations/apps/data_collection/management/commands/import_leeds.py
+++ polling_stations/apps/data_collection/management/commands/import_leeds.py
@@ -4,39 +4,18 @@ from data_collection.management.commands import BaseXpressWebLookupCsvImporter
 
 class Command(BaseXpressWebLookupCsvImporter):
     council_id = "E08000035"
-    addresses_name = "local.2019-05-02/Version 3/Democracy_Club_2.5.2019.CSV"
-    stations_name = "local.2019-05-02/Version 3/Democracy_Club_2.5.2019.CSV"
-    elections = ["local.2019-05-02"]
+    addresses_name = (
+        "europarl.2019-05-23/Version 1/Democracy_Club_Leeds_Euro.26.4.2019.CSV"
+    )
+    stations_name = (
+        "europarl.2019-05-23/Version 1/Democracy_Club_Leeds_Euro.26.4.2019.CSV"
+    )
+    elections = ["europarl.2019-05-23"]
 
     def station_record_to_dict(self, record):
 
-        """
-        Sorry to bother you but we have come across an error in the data that
-        we sent to you the Polling Stations for BHB & BHK have been transposed
-        BHB should be Mill Hill Unitarian Chapel
-        BHK should be Holbeck Working Mens Club
-        """
-
-        if record.pollingdistrictreference == "BHK":
-            record = record._replace(pollingplaceaddress1="Holbeck Working Mens Club")
-            record = record._replace(pollingplaceaddress2="(The Lounge)")
-            record = record._replace(pollingplaceaddress3="Jenkinson Lawn")
-            record = record._replace(pollingplaceaddress4="Holbeck")
-            record = record._replace(pollingplaceaddress5="Leeds")
-            record = record._replace(pollingplaceaddress6="")
-            record = record._replace(pollingplaceaddress7="LS11 9QX")
-
-        if record.pollingdistrictreference == "BHB":
-            record = record._replace(pollingplaceaddress1="Mill Hill Unitarian Chapel")
-            record = record._replace(pollingplaceaddress2="City Square")
-            record = record._replace(pollingplaceaddress3="Leeds")
-            record = record._replace(pollingplaceaddress4="")
-            record = record._replace(pollingplaceaddress5="")
-            record = record._replace(pollingplaceaddress6="")
-            record = record._replace(pollingplaceaddress7="LS1 5EB")
-
         # user error report
-        if record.pollingplaceid == "93":
+        if record.pollingplaceid == "5804":
             rec = super().station_record_to_dict(record)
             rec["location"] = Point(-1.580224, 53.852590, srid=4326)
             return rec
