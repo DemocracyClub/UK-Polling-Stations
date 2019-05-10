06f3628e481df4f0efecf7d644e7178bc3ee2276
GeoWill <roper.william@gmail.com>
2019-05-10 13:06:01 +0100

    Import script for Milton Keynes (closes #1885)

--- polling_stations/apps/data_collection/management/commands/import_milton_keynes.py
+++ polling_stations/apps/data_collection/management/commands/import_milton_keynes.py
@@ -3,20 +3,23 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E06000042"
-    addresses_name = "local.2018-05-03/Version 1/Democracy_Club__03May2018.tsv"
-    stations_name = "local.2018-05-03/Version 1/Democracy_Club__03May2018.tsv"
-    elections = ["local.2018-05-03"]
+    addresses_name = "europarl.2019-05-23/Version 1/Democracy_Club__23May2019MK.tsv"
+    stations_name = "europarl.2019-05-23/Version 1/Democracy_Club__23May2019MK.tsv"
+    elections = ["europarl.2019-05-23"]
     csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
-        if record.polling_place_id == "5280":
-            record = record._replace(polling_place_postcode="MK3 5NG")
+
+        if record.polling_place_id == "7179":
+            record = record._replace(polling_place_easting="0")
+            record = record._replace(polling_place_northing="0")
+        if record.polling_place_id == "7258":
+            record = record._replace(polling_place_easting="0")
+            record = record._replace(polling_place_northing="0")
+
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-
-        if record.addressline6.strip() == "MK18 1AT":
-            return None
 
         if record.addressline6.strip() == "MK10 7JD":
             return None
