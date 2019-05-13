8095d5183211582fcc6d8b9fb1b7b9db19c8e7de
mtravis <matt.travis1@gmail.com>
2019-05-13 15:15:45 +0100

    import script for Daventry & misc_fixes update for incorrect uprn (closes #1904)

--- polling_stations/apps/data_collection/management/commands/import_daventry.py
+++ polling_stations/apps/data_collection/management/commands/import_daventry.py
@@ -3,26 +3,16 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E07000151"
-    addresses_name = (
-        "local.2018-05-03/Version 1/Democracy_Club__03May2018 (1) Daventry.tsv"
-    )
-    stations_name = (
-        "local.2018-05-03/Version 1/Democracy_Club__03May2018 (1) Daventry.tsv"
-    )
-    elections = ["local.2018-05-03"]
+    addresses_name = "europarl.2019-05-23/Version 1/Democracy_Club__23May2019daven.tsv"
+    stations_name = "europarl.2019-05-23/Version 1/Democracy_Club__23May2019daven.tsv"
+    elections = ["europarl.2019-05-23"]
     csv_delimiter = "\t"
-    csv_encoding = "windows-1252"
 
     def address_record_to_dict(self, record):
-
+        rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
-        if uprn == "28052804":
-            rec = super().address_record_to_dict(record)
-            rec["postcode"] = "NN6 8HN"
-            return rec
+        if uprn == "28052662":
+            rec["postcode"] = "NN113QJ"
 
-        if record.addressline6 == "NN6 6JQ":
-            return None
-
-        return super().address_record_to_dict(record)
+        return rec
