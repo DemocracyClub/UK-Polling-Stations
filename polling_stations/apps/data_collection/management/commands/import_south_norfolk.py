ddcd207aba5175cab250f331c8713593d275022a
chris48s <chris.shaw480@gmail.com>
2019-05-13 16:26:58 +0100

    import South Norfolk (closes #1953)

--- polling_stations/apps/data_collection/management/commands/import_south_norfolk.py
+++ polling_stations/apps/data_collection/management/commands/import_south_norfolk.py
@@ -5,8 +5,24 @@ class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E07000149"
     addresses_name = "local.2019-05-02/Version 1/SouthNorfolk2019PollingStation Report for Democracy Club Website.csv"
     stations_name = "local.2019-05-02/Version 1/SouthNorfolk2019PollingStation Report for Democracy Club Website.csv"
-    elections = ["local.2019-05-02"]
+    elections = ["europarl.2019-05-23"]
     csv_delimiter = ","
+
+    def station_record_to_dict(self, record):
+
+        # Station change for EU election
+        if record.polling_place_id == "9402":
+            record = record._replace(polling_place_name="Costessey")
+            record = record._replace(polling_place_address_1="Breckland Hall")
+            record = record._replace(polling_place_address_2="Breckland Road")
+            record = record._replace(polling_place_address_3="New Costessey")
+            record = record._replace(polling_place_address_4="")
+            record = record._replace(polling_place_postcode="NR5 0RW")
+            record = record._replace(polling_place_easting="0")
+            record = record._replace(polling_place_northing="0")
+            record = record._replace(polling_place_uprn="")
+
+        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
