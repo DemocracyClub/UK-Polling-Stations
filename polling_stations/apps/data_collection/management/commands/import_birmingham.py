827ba7b26e486eb344362f7f6598e355e2a8bf6f
chris48s <chris.shaw480@gmail.com>
2019-05-14 16:42:24 +0100

    import Birmingham (closes #1935)

--- polling_stations/apps/data_collection/management/commands/import_birmingham.py
+++ polling_stations/apps/data_collection/management/commands/import_birmingham.py
@@ -3,40 +3,45 @@ from data_collection.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "E08000025"
-    addresses_name = "local.2018-05-03/Version 3/polling_station_export-2018-04-17.csv"
-    stations_name = "local.2018-05-03/Version 3/polling_station_export-2018-04-17.csv"
-    elections = ["local.2018-05-03"]
+    addresses_name = (
+        "europarl.2019-05-23/Version 1/polling_station_export-2019-04-30.csv"
+    )
+    stations_name = (
+        "europarl.2019-05-23/Version 1/polling_station_export-2019-04-30.csv"
+    )
+    elections = ["europarl.2019-05-23"]
     csv_encoding = "windows-1252"
 
     def address_record_to_dict(self, record):
-        if record.housepostcode == "B71 1BX":
-            return None
+        uprn = record.uprn.strip()
+        rec = super().address_record_to_dict(record)
 
-        if record.housepostcode == "B15 3QD":
-            return None
+        if uprn == "100071465044":
+            rec["postcode"] = "B44 0NN"
 
-        if record.housepostcode == "B31 2FL":
-            return None
+        if uprn == "10093745957":
+            rec["postcode"] = "B36 8HU"
 
-        if record.housepostcode == "B31 3JE":
-            return None
+        if uprn == "10023294797":
+            rec["postcode"] = "B72 1BX"
 
-        if record.housepostcode == "B32 2AQ":
+        if record.housepostcode in ["B11 3EY", "B13 9BT", "B27 6BY"]:
             return None
-
-        if record.housepostcode == "B6 6JU":
+        if uprn in [
+            "100070369867",
+            "10090247576",
+            "10090247575",
+            "10090247574",
+            "10090247573",
+            "10090247571",
+            "10090247570",
+            "10090247577",
+            "10090247569",
+            "10090436086",
+        ]:
             return None
 
         if record.houseid == "48553":
             return None
 
-        if record.housepostcode == "B9 4DS":
-            return None
-
-        if record.housepostcode == "B11 3EY":
-            return None
-
-        if record.housepostcode == "B15 3AY":
-            return None
-
-        return super().address_record_to_dict(record)
+        return rec
