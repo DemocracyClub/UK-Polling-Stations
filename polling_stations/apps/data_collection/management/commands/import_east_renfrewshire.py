fe5aff29ba142d14c30d8fce4b5e12ce34ec2b58
GeoWill <roper.william@gmail.com>
2019-05-15 12:06:11 +0100

    Import script for East Renfrewshire

--- polling_stations/apps/data_collection/management/commands/import_east_renfrewshire.py
+++ polling_stations/apps/data_collection/management/commands/import_east_renfrewshire.py
@@ -4,4 +4,4 @@ from data_collection.management.commands import BaseScotlandSpatialHubImporter
 class Command(BaseScotlandSpatialHubImporter):
     council_id = "S12000011"
     council_name = "East Renfrewshire"
-    elections = ["local.east-renfrewshire.2017-05-04", "parl.2017-06-08"]
+    elections = ["europarl.2019-05-23"]
