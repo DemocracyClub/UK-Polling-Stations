9c09f3545c4ca59ab13ca5da97a349e58dff5a83
GeoWill <roper.william@gmail.com>
2019-05-15 14:51:22 +0100

    Import Script for Highland
    The UPRNs added to 'bad_uprns' in misc_fixes.py have not been all been checked
    as bad, rather it is all uprns that are >16km from the polling place.
    This reduces the number of address pickers by throwing out some actual bad
    uprns, but it probably includes some good uprns too. As such it would be good to
    revert this after May 23rd.

--- polling_stations/apps/data_collection/management/commands/import_highland.py
+++ polling_stations/apps/data_collection/management/commands/import_highland.py
@@ -4,7 +4,4 @@ from data_collection.management.commands import BaseScotlandSpatialHubImporter
 class Command(BaseScotlandSpatialHubImporter):
     council_id = "S12000017"
     council_name = "Highland"
-    elections = [
-        "local.highland.2017-05-04",
-        #'parl.2017-06-08'
-    ]
+    elections = ["europarl.2019-05-23"]
