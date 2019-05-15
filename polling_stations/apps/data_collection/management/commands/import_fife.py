e74d73ea967787953f67252531e01322fb373e8a
GeoWill <roper.william@gmail.com>
2019-05-15 13:57:21 +0100

    Import script for Fife

--- polling_stations/apps/data_collection/management/commands/import_fife.py
+++ polling_stations/apps/data_collection/management/commands/import_fife.py
@@ -1,39 +1,13 @@
-from data_collection.github_importer import BaseGitHubImporter
-
-"""
-Note:
-This importer provides coverage for 190/193 districts
-due to incomplete/poor quality data
-"""
+from data_collection.management.commands import BaseScotlandSpatialHubImporter
 
 
-class Command(BaseGitHubImporter):
-
-    srid = 4326
-    districts_srid = 4326
+class Command(BaseScotlandSpatialHubImporter):
     council_id = "S12000047"
-    elections = ["parl.2017-06-08"]
-    scraper_name = "wdiv-scrapers/DC-PollingStations-Fife"
-    geom_type = "geojson"
+    council_name = "Fife"
+    elections = ["europarl.2019-05-23"]
 
     def district_record_to_dict(self, record):
-        poly = self.extract_geometry(record, self.geom_type, self.get_srid("districts"))
-
-        return {
-            "internal_council_id": record["CODE"],
-            "name": record["NAME"],
-            "area": poly,
-            "polling_station_id": record["CODE"],
-        }
-
-    def station_record_to_dict(self, record):
-        location = self.extract_geometry(
-            record, self.geom_type, self.get_srid("stations")
-        )
-
-        return {
-            "internal_council_id": record["CODE"],
-            "address": record["POLLING_PLACE"],
-            "postcode": "",
-            "location": location,
-        }
+        if record[0].startswith("999ZZ"):  # Fife reservoirs
+            return None
+        rec = super().district_record_to_dict(record)
+        return rec
