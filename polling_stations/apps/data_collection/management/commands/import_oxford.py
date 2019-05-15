73273db967872bac246d1f212236136b06c0d0c6
mtravis <matt.travis1@gmail.com>
2019-05-15 15:14:43 +0100

    import script for Oxford (closes #1858)

--- polling_stations/apps/data_collection/management/commands/import_oxford.py
+++ polling_stations/apps/data_collection/management/commands/import_oxford.py
@@ -4,38 +4,33 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E07000178"
-    addresses_name = "local.2018-05-03/Version 1/Democracy_Club__03May2018 Oxford.tsv"
-    stations_name = "local.2018-05-03/Version 1/Democracy_Club__03May2018 Oxford.tsv"
-    elections = ["local.2018-05-03"]
+    addresses_name = "europarl.2019-05-23/Version 1/Democracy_Club__23May2019ox.tsv"
+    stations_name = "europarl.2019-05-23/Version 1/Democracy_Club__23May2019ox.tsv"
+    elections = ["europarl.2019-05-23"]
     csv_delimiter = "\t"
 
-    def station_record_to_dict(self, record):
-        rec = super().station_record_to_dict(record)
-
-        # more accurate point for St Alban's Hall
-        if rec["internal_council_id"] == "4699":
-            rec["location"] = Point(-1.232920, 51.740993, srid=4326)
-
-        return rec
-
     def address_record_to_dict(self, record):
-
+        rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
+        if uprn == "200004681758":
+            rec["postcode"] = "OX2 6TQ"
+
+        if uprn == "10093785403":
+            rec["postcode"] = "OX4 4RS"
+
+        if uprn == "10091102017":
+            rec["postcode"] = "OX3 0SN"
+
         if uprn == "10013991465":
-            rec = super().address_record_to_dict(record)
+
             rec["postcode"] = "OX4 2NH"
-            return rec
 
         if uprn == "10024243546":
-            rec = super().address_record_to_dict(record)
             rec["postcode"] = "OX4 3DQ"
-            return rec
 
         if uprn in ["10091102003", "10091102004", "10091102005"]:
-            rec = super().address_record_to_dict(record)
             rec["postcode"] = "OX4 3QD"
-            return rec
 
         if uprn == "100120827844":
             return None
@@ -43,4 +38,29 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         if uprn == "10024245588":
             return None
 
-        return super().address_record_to_dict(record)
+        return rec
+
+    def station_record_to_dict(self, record):
+
+        # Baptist Church Room, Godstow Road, Wolvercote
+        if record.polling_place_id == "4904":
+            record = record._replace(polling_place_postcode="OX2 8PG")
+
+        # Aspire, Former St Thomas School, Osney Lane
+        if record.polling_place_id == "5124":
+            record = record._replace(polling_place_postcode="OX1 1NJ")
+
+        # St Margaret`s Centre Hall, 30 Polstead Road
+        if record.polling_place_id == "4929":
+            record = record._replace(polling_place_postcode="OX2 6TN")
+
+        # Littlemore Community Centre, Giles Road
+        if record.polling_place_id == "5116":
+            record = record._replace(polling_place_postcode="OX4 4NW")
+
+        rec = super().station_record_to_dict(record)
+        # more accurate point for St Alban's Hall
+        if rec["internal_council_id"] == "4995":
+            rec["location"] = Point(-1.232920, 51.740993, srid=4326)
+
+        return rec
