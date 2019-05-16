b7c07e9813f1e805de89db5e329f81925f8bab74
GeoWill <roper.william@gmail.com>
2019-05-16 15:16:37 +0100

    Import script for Edinburgh

--- polling_stations/apps/data_collection/management/commands/import_edinburgh.py
+++ polling_stations/apps/data_collection/management/commands/import_edinburgh.py
@@ -1,12 +1,16 @@
 from data_collection.github_importer import BaseGitHubImporter
 
+"""
+There's no polling station for district EE16L which covers the hospital at Little France
+"""
+
 
 class Command(BaseGitHubImporter):
 
     srid = 4326
     districts_srid = 4326
     council_id = "S12000036"
-    elections = ["parl.2017-06-08"]
+    elections = ["europarl.2019-05-23"]
     scraper_name = "wdiv-scrapers/DC-PollingStations-Edinburgh"
     geom_type = "geojson"
 
@@ -29,12 +33,51 @@ class Command(BaseGitHubImporter):
 
         stations = []
         for code in codes:
-            stations.append(
-                {
-                    "internal_council_id": code,
-                    "address": "\n".join([record["Polling__1"], record["Address_1"]]),
-                    "postcode": "",
-                    "location": location,
-                }
-            )
+            if (
+                code == "EC11H"
+            ):  # Polling station address changed to match code in Council email
+                stations.append(
+                    {
+                        "internal_council_id": code,
+                        "address": "City Chambers,\nHigh Street,\nEdinburgh,\nEH1 1YJ",
+                        "postcode": "",
+                        "location": None,
+                    }
+                )
+            elif (
+                code == "NC05D"
+            ):  # Polling station address changed to match code in Council email
+                stations.append(
+                    {
+                        "internal_council_id": code,
+                        "address": "FetLor Youth Club,\n122 Crewe Road South,\nEdinburgh,\nEH4 2NY",
+                        "postcode": "",
+                        "location": None,
+                    }
+                )
+
+            elif (
+                code == "EN12K"
+            ):  # Polling station address changed to match code in Council email
+                stations.append(
+                    {
+                        "internal_council_id": code,
+                        "address": "Norton Park Conference Centre,\n53 Albion Road,\nEdinburgh,\nEH7 5QY",
+                        "postcode": "",
+                        "location": None,
+                    }
+                )
+            elif code == "EE16L":  # Code not present in Council email
+                pass
+            else:
+                stations.append(
+                    {
+                        "internal_council_id": code,
+                        "address": "\n".join(
+                            [record["Polling__1"], record["Address_1"]]
+                        ),
+                        "postcode": "",
+                        "location": location,
+                    }
+                )
         return stations
