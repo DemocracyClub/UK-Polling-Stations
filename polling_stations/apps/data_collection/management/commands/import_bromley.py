bef507734a5a31df75ac6984439b989397960e86
chris48s <chris.shaw480@gmail.com>
2019-05-16 16:36:45 +0100

    import Bromley (closes #1914)

--- polling_stations/apps/data_collection/management/commands/import_bromley.py
+++ polling_stations/apps/data_collection/management/commands/import_bromley.py
@@ -3,23 +3,11 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E09000006"
-    addresses_name = "local.2018-05-03/Version 1/Democracy_Club__03May2018 (1).tsv"
-    stations_name = "local.2018-05-03/Version 1/Democracy_Club__03May2018 (1).tsv"
-    elections = ["local.2018-05-03"]
+    addresses_name = "europarl.2019-05-23/Version 1/Democracy_Club__23May2019brom.tsv"
+    stations_name = "europarl.2019-05-23/Version 1/Democracy_Club__23May2019brom.tsv"
+    elections = ["europarl.2019-05-23"]
     csv_delimiter = "\t"
     csv_encoding = "windows-1252"
-
-    def station_record_to_dict(self, record):
-
-        if record.polling_place_id == "8547":
-            record = record._replace(polling_place_easting="537322")
-            record = record._replace(polling_place_northing="170426")
-
-        if record.polling_place_id == "8694":
-            record = record._replace(polling_place_easting="542751")
-            record = record._replace(polling_place_northing="172102")
-
-        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
 
