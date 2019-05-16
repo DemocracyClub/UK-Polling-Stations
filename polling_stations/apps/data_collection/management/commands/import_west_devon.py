9940a797d661c87ce39e50f8db7d05092917c92c
Matt Travis <matt.travis1@gmail.com>
2019-05-16 09:55:35 +0100

    import script for West Devon (closes #1848) (#1945)

--- polling_stations/apps/data_collection/management/commands/import_west_devon.py
+++ polling_stations/apps/data_collection/management/commands/import_west_devon.py
@@ -3,9 +3,9 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E07000047"
-    addresses_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019WD.tsv"
-    stations_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019WD.tsv"
-    elections = ["local.2019-05-02"]
+    addresses_name = "europarl.2019-05-23/Version 1/Democracy_Club__23May2019WD.tsv"
+    stations_name = "europarl.2019-05-23/Version 1/Democracy_Club__23May2019WD.tsv"
+    elections = ["europarl.2019-05-23"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -17,4 +17,24 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         ]:
             rec["accept_suggestion"] = False
 
+        if uprn == "10013755070":
+            rec["postcode"] = "EX201FQ"
+
         return rec
+
+    def station_record_to_dict(self, record):
+
+        if record.polling_place_id == "6451":
+            record = record._replace(polling_place_uprn="10013752301")
+
+        if record.polling_place_id == "6477":
+            record = record._replace(polling_place_uprn="10013752301")
+
+        if record.polling_place_id == "6524":
+            record = record._replace(polling_place_uprn="10013752301")
+
+        if record.polling_place_id == "6502":
+            record = record._replace(polling_place_uprn="10001329795")
+            record = record._replace(polling_place_postcode="")
+
+        return super().station_record_to_dict(record)
