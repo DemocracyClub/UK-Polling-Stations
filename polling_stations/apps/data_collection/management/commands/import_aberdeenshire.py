59e5feed2ae62784095f9392076baedcfdd630d7
chris48s <chris48s@users.noreply.github.com>
2019-05-20 09:27:41 +0100

    import Aberdeenshire (#2020)

--- polling_stations/apps/data_collection/management/commands/import_aberdeenshire.py
+++ polling_stations/apps/data_collection/management/commands/import_aberdeenshire.py
@@ -4,4 +4,12 @@ from data_collection.management.commands import BaseScotlandSpatialHubImporter
 class Command(BaseScotlandSpatialHubImporter):
     council_id = "S12000034"
     council_name = "Aberdeenshire"
-    elections = ["local.aberdeenshire.2017-05-04", "parl.2017-06-08"]
+    elections = ["europarl.2019-05-23"]
+
+    def station_record_to_dict(self, record):
+        rec = super().station_record_to_dict(record)
+        if record[0] == "EG1202":  # District file and Stations file disagree on station
+            return None
+        if record[0] == "EG1106":  # District file and Stations file disagree on station
+            return None
+        return rec
