07650b1e15573882897cb66f614dcf2ab8c2171f
GeoWill <roper.william@gmail.com>
2019-05-21 10:36:09 +0200

    Import script for Runnymede (closes #1983)

--- polling_stations/apps/data_collection/management/commands/import_runnymede.py
+++ polling_stations/apps/data_collection/management/commands/import_runnymede.py
@@ -3,9 +3,9 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E07000212"
-    addresses_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019Runny.tsv"
-    stations_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019Runny.tsv"
-    elections = ["local.2019-05-02"]
+    addresses_name = "europarl.2019-05-23/Version 1/Democracy_Club__23May2019runny.tsv"
+    stations_name = "europarl.2019-05-23/Version 1/Democracy_Club__23May2019runny.tsv"
+    elections = ["europarl.2019-05-23"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -16,12 +16,7 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "100061498464",  # TW200BJ -> TW200BQ : Chestnuts, Royal Holloway University - Halls of Residence, Englefield Green, Egham, Surrey
             "200001732773",  # TW208HJ -> TW200HJ : Brook Lodge, Wick Road, Englefield Green, Egham, Surrey
             "10002019662",  # KT153NT -> KT153QE : Accomodation at The Black Prince, Woodham Lane
-            "100061503485",  # TW209UY -> TW209UX : Hogsters Farm, Stroude Road
-            "100061499461",  # TW208LQ -> TW208QN : Rose Cottage, Green Road, Thorpe, Egham, Surrey
         ]:
             rec["accept_suggestion"] = True
-
-        if uprn in []:
-            rec["accept_suggestion"] = False
 
         return rec
