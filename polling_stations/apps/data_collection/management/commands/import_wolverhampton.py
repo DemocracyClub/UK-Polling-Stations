fdd93dab191824b4b836a60b2a211e294134b87d
chris48s <chris.shaw480@gmail.com>
2019-05-22 12:25:22 +0100

    last minute Wolverhampton import

--- polling_stations/apps/data_collection/management/commands/import_wolverhampton.py
+++ polling_stations/apps/data_collection/management/commands/import_wolverhampton.py
@@ -6,7 +6,7 @@ class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E08000031"
     addresses_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019Wolves.tsv"
     stations_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019Wolves.tsv"
-    elections = ["local.2019-05-02"]
+    elections = ["local.2019-05-02", "europarl.2019-05-23"]
     csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
