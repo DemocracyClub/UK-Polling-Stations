e795eae888720d0ff1f2af8d02d9a868e74476a0
chris48s <chris.shaw480@gmail.com>
2019-09-02 10:57:26 +0100

    update Pembrokeshire import script

--- polling_stations/apps/data_collection/management/commands/import_pembrokeshire.py
+++ polling_stations/apps/data_collection/management/commands/import_pembrokeshire.py
@@ -6,14 +6,14 @@ class Command(BaseGitHubImporter):
     srid = 4326
     districts_srid = 4326
     council_id = "W06000009"
-    elections = ["parl.2017-06-08"]
+    elections = []
     scraper_name = "wdiv-scrapers/DC-PollingStations-Pembrokeshire"
     geom_type = "geojson"
 
     def district_record_to_dict(self, record):
         poly = self.extract_geometry(record, self.geom_type, self.get_srid("districts"))
         return {
-            "internal_council_id": record["DistrictID"],
+            "internal_council_id": record["DistrictRef"],
             "name": record["DistrictName"],
             "area": poly,
             "polling_station_id": record["StationID"],
@@ -24,7 +24,7 @@ class Command(BaseGitHubImporter):
             record, self.geom_type, self.get_srid("stations")
         )
         return {
-            "internal_council_id": record["StationID"],
+            "internal_council_id": record["pk"].split(".")[1],
             "postcode": "",
             "address": record["StationName"],
             "location": location,
