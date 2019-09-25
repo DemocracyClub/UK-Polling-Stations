71cca2acefa1e3b7d2b2b301ef62165aba718957
chris48s <chris.shaw480@gmail.com>
2019-09-25 14:08:05 +0100

    import script for Luton

--- polling_stations/apps/data_collection/management/commands/import_luton.py
+++ polling_stations/apps/data_collection/management/commands/import_luton.py
@@ -3,28 +3,21 @@ from data_collection.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "E06000032"
-    addresses_name = "local.2019-05-02/Version 2/Democracy Club - Polling Districts.csv"
-    stations_name = "local.2019-05-02/Version 2/Democracy Club - Polling Stations.csv"
-    elections = ["local.2019-05-02"]
+    addresses_name = "parl.maybe/Version 1/luton-DC - Polling Districts.csv"
+    stations_name = "parl.maybe/Version 1/luton-DC - Polling Stations.csv"
+    elections = ["parl.maybe"]
     csv_encoding = "windows-1252"
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
         uprn = record.uprn.strip().lstrip("0")
 
-        if uprn == "10090734899":
-            rec["postcode"] = "LU4 9BN"
+        if uprn == "10001037360":
+            rec["postcode"] = "LU2 7QG"
             rec["accept_suggestion"] = False
 
         if uprn in [
             "10001038309",  # LU40HF -> LU40FH : 228 Servite Court, Morecambe Close, Luton
-            "100080152018",  # LU49LQ -> LU49LF : 54 High Street, Leagrave, Luton
-            "100080152011",  # LU49LQ -> LU49LF : 36 High Street, Leagrave, Luton
-            "100080152012",  # LU49LQ -> LU49LF : 40 High Street, Leagrave, Luton
-            "100080152013",  # LU49LQ -> LU49LF : 42 High Street, Leagrave, Luton
-            "100080152015",  # LU49LQ -> LU49LF : 46 High Street, Leagrave, Luton
-            "100080152016",  # LU49LQ -> LU49LF : 50 High Street, Leagrave, Luton
-            "100080152017",  # LU49LQ -> LU49LF : 52 High Street, Leagrave, Luton
             "200003273279",  # LU31TL -> LU27AU : SCHOOL HOUSE BARNFIELD COLLEGE, BARNFIELD AVENUE, LUTON
             "100080152039",  # LU49LQ -> LU49LJ : 130 HIGH STREET, LUTON
         ]:
