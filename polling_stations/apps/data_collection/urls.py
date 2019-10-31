7623652637900bd1365eaa0c7a00f9544b4294e8
Alexander Dutton <alex.dutton@mysociety.org>
2019-10-31 14:51:35 +0000

    Remove data quality views in favour of the dashboard.
    This includes a redirect from the old council view URL.

--- polling_stations/apps/data_collection/urls.py
+++ polling_stations/apps/data_collection/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
-from django.urls import reverse_lazy
 from django.views.generic import RedirectView
 
-from .views import data_quality
 
 urlpatterns = [
-    url(r"^$", RedirectView.as_view(url=reverse_lazy("home")), name="league_table"),
-    url(r"data_quality/(?P<council_id>.+)/$", data_quality, name="data_quality"),
+    url(r"^$", RedirectView.as_view(pattern_name="home"), name="league_table"),
+    url(
+        r"data_quality/(?P<pk>.+)/$",
+        RedirectView.as_view(pattern_name="dashboard:council_detail"),
+        name="data_quality",
+    ),
 ]
