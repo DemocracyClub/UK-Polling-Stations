878117ddb35795c29245f685e93b61fae41137d8
chris48s <chris.shaw480@gmail.com>
2019-11-10 19:03:43 +0000

    import East Hampshire (closes #2150)

--- polling_stations/apps/data_collection/management/commands/import_east_hampshire.py
+++ polling_stations/apps/data_collection/management/commands/import_east_hampshire.py
@@ -4,14 +4,16 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E07000085"
-    addresses_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019easthants.tsv"
-    stations_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019easthants.tsv"
-    elections = ["local.2019-05-02"]
+    addresses_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019EHDC.tsv"
+    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019EHDC.tsv"
+    elections = ["parl.2019-12-12"]
     csv_delimiter = "\t"
+    allow_station_point_from_postcode = True
 
     def station_record_to_dict(self, record):
+        # Jubilee Hall, Crouch Lane
         rec = super().station_record_to_dict(record)
-        if record.polling_place_id == "10278":
+        if record.polling_place_id == "11962":
             rec["location"] = Point(-1.013446, 50.916221, srid=4326)
         return rec
 
@@ -31,11 +33,8 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
         if uprn in [
             "10009815781",  # GU344ND -> GU344NE : The Lodge, Coldrey Farm, Lower Froyle, Alton, Hants
-            "1710006647",  # GU345BJ -> GU345QQ : Assissi, The Shrave, Four Marks, Alton, Hants
             "100060269031",  # GU358DS -> GU358DB : Newlands, Headley Hill Road, Headley Down, Bordon, Hants
-            "100060260403",  # GU344BS -> GU344FG : Cadnams Farm, Anstey Lane, Alton, Hants
             "100062342567",  # GU307RN -> GU307RX : 2 The Oast Houses, Headley Lane, Passfield, Liphook, Hants
-            "1710007913",  # GU343ET -> GU343DE : Greenmore, Brightstone Lane, Lower Farringdon, Alton, Hants
             "1710006656",  # GU345BJ -> GU345FX : Oakdene, 2 handyside place, The Shrave, Four Marks, Alton, Hants
             "1710049581",  # GU350QB -> GU350QL : 27 Grayshott Laurels, Lindford, Bordon, Hants
             "100062321534",  # GU102QG -> GU102QE : Kingsmead, Frensham Lane, Churt, Farnham, Surrey
