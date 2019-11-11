3bd94c55e1565388b385b3eb0ec8a14d506bd802
GeoWill <roper.william@gmail.com>
2019-11-11 15:16:26 +0000

    Import script for Monmouthshire (closes #2157)

--- polling_stations/apps/data_collection/management/commands/import_monmouthshire.py
+++ polling_stations/apps/data_collection/management/commands/import_monmouthshire.py
@@ -1,36 +1,18 @@
-from django.contrib.gis.geos import Point
-from data_collection.management.commands import BaseShpStationsShpDistrictsImporter
+from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
-class Command(BaseShpStationsShpDistrictsImporter):
-    srid = 27700
+class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "W06000021"
-    districts_name = "polling_district"
-    stations_name = "polling_station.shp"
-    elections = ["local.monmouthshire.2017-05-04", "parl.2017-06-08"]
+    addresses_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019mon.tsv"
+    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019mon.tsv"
+    elections = ["parl.2019-12-12"]
+    csv_delimiter = "\t"
+    allow_station_point_from_postcode = False
 
-    def district_record_to_dict(self, record):
-        return {
-            "internal_council_id": str(record[1]).strip(),
-            "name": str(record[1]).strip(),
-            "polling_station_id": record[3],
-        }
+    def address_record_to_dict(self, record):
+        rec = super().address_record_to_dict(record)
+        if record.addressline1 == "Bryn Cwrt":
+            rec["postcode"] = "NP44 2DB"
+            rec["uprn"] = "10033347654"
 
-    def station_record_to_dict(self, record):
-
-        station = {
-            "internal_council_id": record[0],
-            "postcode": "",
-            "address": "%s\n%s" % (record[2].strip(), record[4].strip()),
-        }
-
-        if str(record[1]).strip() == "10033354925":
-            """
-            There is a dodgy point in this file.
-            It has too many digits for a UK national grid reference.
-
-            Joe queried, Monmouthshire provided this corrected point by email
-            """
-            station["location"] = Point(335973, 206322, srid=27700)
-
-        return station
+        return rec
