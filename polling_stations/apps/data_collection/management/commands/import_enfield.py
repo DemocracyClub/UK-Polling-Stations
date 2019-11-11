69cc1e685fa3fcc5aaacf7e6bd6c26da2bc818a7
GeoWill <roper.william@gmail.com>
2019-11-11 16:02:48 +0000

    Import script for Enfield (closes #2158)

--- polling_stations/apps/data_collection/management/commands/import_enfield.py
+++ polling_stations/apps/data_collection/management/commands/import_enfield.py
@@ -3,31 +3,34 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E09000010"
-    addresses_name = (
-        "local.2018-05-03/Version 1/Democracy Counts Polling Place Lookup Enfield.csv"
-    )
-    stations_name = (
-        "local.2018-05-03/Version 1/Democracy Counts Polling Place Lookup Enfield.csv"
-    )
-    elections = ["local.2018-05-03"]
-    csv_delimiter = ","
+    addresses_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019en.CSV"
+    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019en.CSV"
+    elections = ["parl.2019-12-12"]
+    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
+        rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
-        if uprn == "207086112":
-            rec = super().address_record_to_dict(record)
-            rec["postcode"] = "N13 4AP"
-            return rec
+        if uprn == "207021414":  # 5 College Close, London, N182X
+            rec["postcode"] = "N182XU"
 
-        if uprn == "207017348":
-            rec = super().address_record_to_dict(record)
-            rec["postcode"] = "N21 2AU"
-            return rec
+        # 1-4 Avis house, N97BQZ
+        if uprn in ["207021452", "207021453", "207021454", "207021455"]:
+            rec["postcode"] = "N97BQ"
 
-        if uprn == "207083595":
-            rec = super().address_record_to_dict(record)
-            rec["postcode"] = "N21 2SD"
-            return rec
+        if uprn in ["207016774"]:  # FLAT 2 928 HERTFORD ROAD, ENFIELD
+            return None
 
-        return super().address_record_to_dict(record)
+        return rec
+
+    def station_record_to_dict(self, record):
+
+        if record.polling_place_id == "6566":  # Enfield Highway Community Centre
+            record = record._replace(polling_place_easting="535189")
+            record = record._replace(polling_place_northing="197091")
+
+        if record.polling_place_id == "6648":  # Christ Church Southgate
+            record = record._replace(polling_place_address_1="The Green")
+
+        return super().station_record_to_dict(record)
