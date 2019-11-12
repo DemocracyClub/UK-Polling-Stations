3d95d3b61bbce6e9a5eb29a52545dec351fb362d
GeoWill <roper.william@gmail.com>
2019-11-12 17:50:54 +0000

    Import script for Huntingdonshire (closes #2186)

--- polling_stations/apps/data_collection/management/commands/import_huntingdonshire.py
+++ polling_stations/apps/data_collection/management/commands/import_huntingdonshire.py
@@ -3,14 +3,19 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E07000011"
-    addresses_name = "europarl.2019-05-23/Version 1/Democracy_Club__23May2019hunt.tsv"
-    stations_name = "europarl.2019-05-23/Version 1/Democracy_Club__23May2019hunt.tsv"
-    elections = ["europarl.2019-05-23"]
+    addresses_name = (
+        "parl.2019-12-12/Version 1/Democracy_Club__12December2019hunting.tsv"
+    )
+    stations_name = (
+        "parl.2019-12-12/Version 1/Democracy_Club__12December2019hunting.tsv"
+    )
+    elections = ["parl.2019-12-12"]
     csv_delimiter = "\t"
+    allow_station_point_from_postcode = False
 
     def station_record_to_dict(self, record):
 
-        if record.polling_place_id == "6265":  # Berkley Street Methodist Church
+        if record.polling_place_id == "6703":  # Berkley Street Methodist Church
             record = record._replace(polling_place_postcode="PE19 2NB")
 
         return super().station_record_to_dict(record)
