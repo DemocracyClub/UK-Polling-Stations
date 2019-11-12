c97d9b6c878cb82b2f112bb49859bb96b58ef2ce
GeoWill <roper.william@gmail.com>
2019-11-12 17:33:15 +0000

    Import script for Halton Borough Council (closes #2184)

--- polling_stations/apps/data_collection/management/commands/import_halton.py
+++ polling_stations/apps/data_collection/management/commands/import_halton.py
@@ -1,42 +1,37 @@
-from django.contrib.gis.geos import Point
 from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E06000006"
-    addresses_name = "local.2018-05-03/Version 1/Democracy_Club__03May2018.tsv"
-    stations_name = "local.2018-05-03/Version 1/Democracy_Club__03May2018.tsv"
-    elections = ["local.2018-05-03"]
+    addresses_name = (
+        "parl.2019-12-12/Version 1/Democracy_Club__12December2019halton.tsv"
+    )
+    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019halton.tsv"
+    elections = ["parl.2019-12-12"]
     csv_delimiter = "\t"
+    allow_station_point_from_postcode = False
 
     def station_record_to_dict(self, record):
 
-        if record.polling_place_id == "298":
-            record = record._replace(polling_place_postcode="WA7 5DJ")
-
-        if record.polling_place_id == "268":
+        if record.polling_place_id == "1564":  # Scout Hut, Hall Avenue, Widnes
             record = record._replace(polling_place_postcode="WA8 4PU")
-
-        if record.polling_place_id == "320":
-            record = record._replace(polling_place_postcode="WA7 2FL")
-            rec = super().station_record_to_dict(record)
-            rec["location"] = Point(-2.701970, 53.320739, srid=4326)
-            return rec
-
-        if record.polling_place_id == "388":
-            record = record._replace(polling_place_postcode="WA7 6JW")
 
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
+        rec = super().address_record_to_dict(record)
+
         uprn = record.property_urn.strip().lstrip("0")
 
-        if record.addressline6 == "WA8 8SF":
-            return None
+        if uprn in [
+            "10090873193",  # Flat 1 Victoria House, 20 Ann Street West
+            "100010231610",  # Flat 2, 1 South Road, Runcorn
+        ]:
+            rec["accept_suggestion"] = False
 
         if uprn in ["10010612061", "10010612062", "10010612064"]:
             rec = super().address_record_to_dict(record)
             rec["postcode"] = "WA7 6HG"
             return rec
 
-        return super().address_record_to_dict(record)
+        return rec
