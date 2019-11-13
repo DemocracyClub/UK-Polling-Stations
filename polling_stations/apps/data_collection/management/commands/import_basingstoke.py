084219549c3c78eefaede30d755b298deb5e891d
GeoWill <roper.william@gmail.com>
2019-11-13 10:51:32 +0000

    Import script for Basingstoke (closes #2189)

--- polling_stations/apps/data_collection/management/commands/import_basingstoke.py
+++ polling_stations/apps/data_collection/management/commands/import_basingstoke.py
@@ -3,17 +3,17 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E07000084"
-    addresses_name = "europarl.2019-05-23/Version 1/Democracy_Club__23May2019basing.tsv"
-    stations_name = "europarl.2019-05-23/Version 1/Democracy_Club__23May2019basing.tsv"
-    elections = ["europarl.2019-05-23"]
+    addresses_name = (
+        "parl.2019-12-12/Version 1/Democracy_Club__12December2019basing.tsv"
+    )
+    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019basing.tsv"
+    elections = ["parl.2019-12-12"]
     csv_delimiter = "\t"
+    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
         rec = super().address_record_to_dict(record)
-
-        if uprn == "10008485351":
-            rec["postcode"] = "RG26 5HL"
 
         if uprn in ["100062463351"]:
             rec["accept_suggestion"] = True
