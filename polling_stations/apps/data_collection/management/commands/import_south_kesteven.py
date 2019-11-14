0466e9cb9cd46fe1e37d92265eb0c413ca0d0fc7
Alexander Dutton <alex.dutton@mysociety.org>
2019-11-14 12:09:30 +0000

    Import South Kesteven (closes #2195)

--- polling_stations/apps/data_collection/management/commands/import_south_kesteven.py
+++ polling_stations/apps/data_collection/management/commands/import_south_kesteven.py
@@ -3,10 +3,11 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E07000141"
-    addresses_name = "europarl.2019-05-23/Version 1/Democracy_Club__23May2019SK.tsv"
-    stations_name = "europarl.2019-05-23/Version 1/Democracy_Club__23May2019SK.tsv"
-    elections = ["europarl.2019-05-23"]
+    addresses_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019SK.tsv"
+    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019SK.tsv"
+    elections = ["parl.2019-12-12"]
     csv_delimiter = "\t"
+    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
@@ -21,8 +22,11 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         if uprn == "10007257235":
             rec["postcode"] = "PE9 4NT"
 
+        if record.post_code in ("NG31 8NH",):
+            # Assigned polling station looks a bit suss
+            return None
+
         if uprn in [
-            "10007246648",  # PE100TT -> PE100UH : The Old Gatehouse, Fen Road, Dunsby, Bourne, Lincs
             "100030920062",  # NG321LT -> NG321LD : Bullimore`s Cottage, 89 Main Street, Denton, Grantham, Lincs
             "10007253051",  # NG321AT -> NG321AN : The Corner Cottage, 5 Hungerton Hollow, Grantham, Lincs
             "10007246148",  # NG321LT -> NG321LY : 42 Main Street, Woolsthorpe-By-Belvoir, Grantham, Lincs
@@ -43,16 +47,15 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "10007252178",  # NG323AY -> NG323SB : 4 Main Street, Sudbrook, Grantham, Lincs.
             "100030919971",  # NG322LW -> NG322NH : Corner House, Main Road, Barkston, Grantham, Lincs
             "10007242167",  # NG322DU -> NG322EF : Willow Top Farm, Gonerby Lane, Allington, Grantham, Lincs
-            "10007263149",  # PE91JP -> PE92AP : Flat 1 Caymen House, Foundry Road, Stamford, Lincs
-            "10007263150",  # PE91JP -> PE92AP : Flat 2 Caymen House, Foundry Road, Stamford, Lincs
-            "10007263151",  # PE91JP -> PE92AP : Flat 3 Caymen House, Foundry Road, Stamford, Lincs
-            "10007263152",  # PE91JP -> PE92AP : Flat 4 Caymen House, Foundry Road, Stamford, Lincs
+            "10007246399",  # NG334SP -> NG334SR : The Barn, Porters Lodge Farm, Morkery Lane, Castle Bytham, Grantham, Lincs.
+            "100030904285",  # PE109AE -> PE109EA : Flat Over Bank, R/o 4 North Street, Bourne, Lincs
         ]:
             rec["accept_suggestion"] = True
 
         if uprn in [
             "10007248507",  # NG321LH -> NG318BY : Park Cottage, Park Lane, Denton, Grantham, Lincs
             "200001873982",  # PE91QP -> PE91QL : Hazel Grove, Emlyns Street, Stamford, Lincs
+            "10007257592",  # PE100TT -> PE100TG : Gatehouse Cottage, Fen Road, Dowsby, Bourne, Lincs
         ]:
             rec["accept_suggestion"] = False
 
