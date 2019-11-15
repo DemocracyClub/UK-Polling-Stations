be7c26b2d875cdc29e1ba5a4e37b03bce81ceab0
GeoWill <roper.william@gmail.com>
2019-11-15 10:08:31 +0000

    Import script for Rochdale (closes #2199)

--- polling_stations/apps/data_collection/management/commands/import_rochdale.py
+++ polling_stations/apps/data_collection/management/commands/import_rochdale.py
@@ -4,26 +4,20 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E08000005"
-    addresses_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019Rochdale.tsv"
-    stations_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019Rochdale.tsv"
-    elections = ["local.2019-05-02"]
+    addresses_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019.tsv"
+    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019.tsv"
+    elections = ["parl.2019-12-12"]
     csv_delimiter = "\t"
+    allow_station_point_from_postcode = False
 
     def station_record_to_dict(self, record):
 
-        # Mobile Unit at Black Dog Pub
-        if record.polling_place_id == "2829":
-            record = record._replace(polling_place_postcode="OL12 7JG")
-            rec = super().station_record_to_dict(record)
-            rec["location"] = Point(-2.186986, 53.631252, srid=4326)
-            return rec
-
-        # Bowls Pavilion
-        if record.polling_place_id == "2577":
+        # Bowls Pavilion - Postcode gives wrong geocode
+        if record.polling_place_id == "3132":
             record = record._replace(polling_place_postcode="")
 
         # The Coach House
-        if record.polling_place_id == "2469":
+        if record.polling_place_id == "3348":
             rec = super().station_record_to_dict(record)
             rec["location"] = Point(-2.0961003, 53.6444469, srid=4326)
             return rec
@@ -49,22 +43,28 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         if uprn == "23081457":
             rec["postcode"] = "OL12 9AA"
 
-        if record.addressline6.strip() == "OL16 3BX":
-            return None
-
-        if record.addressline6.strip() == "M24 5TG":
-            return None
-
         if uprn == "23099282":
             rec["postcode"] = "OL11 3AU"
 
-        if record.addressline6.strip() == "OL16 5SJ":
-            return None
-
-        if record.addressline6.strip() == "M24 2YS":
-            return None
-
         if uprn in ["23095362", "23095226"]:
             rec["postcode"] = "OL12 9NE"
+
+        # Not quite right, safer to throw away
+        if record.addressline6.strip() in [
+            "OL16 3BX",
+            "OL16 5SJ",
+            "M24 2YS",
+            "M24 5TG",
+            "M24 5TN",
+        ] or uprn in [
+            "10023363964",
+            "23108014",
+            "10090922210",
+            "10090922501",
+            "10090922502",
+            "10023364069",
+            "10023364206",
+        ]:
+            return None
 
         return rec
