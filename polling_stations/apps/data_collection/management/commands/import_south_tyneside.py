dc77517a4bfc35e8280f41c2a80f5e5c6843a512
Alexander Dutton <alex.dutton@mysociety.org>
2019-11-15 12:02:57 +0000

    Import South Tyneside (closes #2244)

--- polling_stations/apps/data_collection/management/commands/import_south_tyneside.py
+++ polling_stations/apps/data_collection/management/commands/import_south_tyneside.py
@@ -3,12 +3,23 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E08000023"
-    addresses_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019sts.tsv"
-    stations_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019sts.tsv"
-    elections = ["local.2019-05-02", "europarl.2019-05-23"]
+    addresses_name = (
+        "parl.2019-12-12/Version 1/Democracy_Club__12December2019s tynes.tsv"
+    )
+    stations_name = (
+        "parl.2019-12-12/Version 1/Democracy_Club__12December2019s tynes.tsv"
+    )
+    elections = ["parl.2019-12-12"]
     csv_delimiter = "\t"
+    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
+        if (
+            record.addressline1.endswith(" Long Row")
+            and record.addressline6 == "NE31 1JA"
+        ):
+            record = record._replace(addressline6="NE33 1JA")
+
         rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
