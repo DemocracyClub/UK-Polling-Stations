3c3365c87ec786b48b3aa6b2c8481eb6bdb0e59b
GeoWill <roper.william@gmail.com>
2019-11-15 16:16:52 +0000

    Import script for Isle of Wight (closes #2206)

--- polling_stations/apps/data_collection/management/commands/import_isle_of_wight.py
+++ polling_stations/apps/data_collection/management/commands/import_isle_of_wight.py
@@ -3,24 +3,27 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E06000046"
-    addresses_name = "europarl.2019-05-23/Version 1/iow.gov.uk-1556295628000-.tsv"
-    stations_name = "europarl.2019-05-23/Version 1/iow.gov.uk-1556295628000-.tsv"
-    elections = ["europarl.2019-05-23"]
+    addresses_name = "parl.2019-12-12/Version 1/iow.gov.uk-1573036635000-.tsv"
+    stations_name = "parl.2019-12-12/Version 1/iow.gov.uk-1573036635000-.tsv"
+    elections = ["parl.2019-12-12"]
     csv_delimiter = "\t"
     csv_encoding = "windows-1252"
+    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
         if record.addressline1 == "Le Sage":
             rec["postcode"] = "PO390AD"
-        if record.addressline1 == "17 Paddock Road":
-            rec["postcode"] = "PO376NZ"
+
         if record.addressline1 == "Rookley Holiday Park":
             rec["postcode"] = "PO383LU"
         if (
             record.addressline1 == "Flat 15 Solent House"
             and record.addressline6 == "PO30 5TG"
         ):
+            return None
+
+        if record.addressline1 == "Down House":
             return None
 
         return rec
