ee3edd4cbb1151ea3a2e5db11295724ba383c61e
GeoWill <roper.william@gmail.com>
2019-11-15 12:41:25 +0000

    Import script for Stockton-on-Tees (closes #2201)

--- polling_stations/apps/data_collection/management/commands/import_stockton.py
+++ polling_stations/apps/data_collection/management/commands/import_stockton.py
@@ -3,7 +3,28 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E06000004"
-    addresses_name = "local.2019-05-02/Version 2/stockton.gov.uk-1554713389000.tsv"
-    stations_name = "local.2019-05-02/Version 2/stockton.gov.uk-1554713389000.tsv"
-    elections = ["local.2019-05-02"]
+    addresses_name = "parl.2019-12-12/Version 1/stockton.gov.uk-1573042249000-.tsv"
+    stations_name = "parl.2019-12-12/Version 1/stockton.gov.uk-1573042249000-.tsv"
+    elections = ["parl.2019-12-12"]
     csv_delimiter = "\t"
+    allow_station_point_from_postcode = False
+
+    def address_record_to_dict(self, record):
+        rec = super().address_record_to_dict(record)
+        uprn = record.property_urn.strip().lstrip("0")
+
+        # Probable new builds throwing centroid outside of LA warning.
+        if record.post_code == "TS21 3FJ":
+            return None
+
+        if uprn in [
+            "10013728465",  # TS225AS -> TS225ER : Flat The Whitehouse, Whitehouse Road, Billingham
+        ]:
+            rec["accept_suggestion"] = True
+
+        if uprn in [
+            "10009306411",  # TS160QH -> TS160QT : Orchard Grange, A67 Eaglescliffe, Eaglescliffe, Stockton-on-Tees
+        ]:
+            rec["accept_suggestion"] = False
+
+        return rec
