e67b88c30e7fc5065790274ed4c2c91e657a12ff
chris48s <chris.shaw480@gmail.com>
2019-11-19 17:13:49 +0000

    import Mansfield (closes #2196)

--- polling_stations/apps/data_collection/management/commands/import_mansfield.py
+++ polling_stations/apps/data_collection/management/commands/import_mansfield.py
@@ -1,20 +1,18 @@
+import os
 from django.contrib.gis.geos import Point
 from data_collection.management.commands import BaseHalaroseCsvImporter
+from pollingstations.models import PollingStation
 
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "E07000174"
-    addresses_name = "local.2019-05-02/Version 1/polling_station_export-2019-02-12.csv"
-    stations_name = "local.2019-05-02/Version 1/polling_station_export-2019-02-12.csv"
-    elections = ["local.2019-05-02"]
+    addresses_name = "parl.2019-12-12/Version 2/polling_station_export-2019-11-12.csv"
+    stations_name = "parl.2019-12-12/Version 2/polling_station_export-2019-11-12.csv"
+    elections = ["parl.2019-12-12"]
+    allow_station_point_from_postcode = False
 
-    def station_record_to_dict(self, record):
-        rec = super().station_record_to_dict(record)
-        if rec["internal_council_id"] == "7-meden-vale-methodist-church":
-            rec["location"] = Point(-1.1309629, 53.2221165, srid=4326)
-        if rec["internal_council_id"] == "18-chesterfield-road-south-methodist-church":
-            rec["location"] = Point(-1.209920, 53.154687, srid=4326)
-        return rec
+    def get_station_hash(self, record):
+        return "-".join([record.pollingstationnumber.strip(),])
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
@@ -23,7 +21,29 @@ class Command(BaseHalaroseCsvImporter):
         if uprn == "10091484857":
             rec["postcode"] = "NG19 7NH"
 
-        if record.housepostcode == "NG19 0BZ":
-            return None
-
         return rec
+
+    def post_import(self):
+        filepath = os.path.join(
+            self.base_folder_path, "parl.2019-12-12/Version 2/Polling Station.csv"
+        )
+        gridrefs = self.get_data("csv", filepath)
+
+        print("Updating grid refs...")
+        for record in gridrefs:
+            stations = PollingStation.objects.filter(
+                council_id=self.council_id,
+                internal_council_id="-".join([record.number.strip()]),
+            )
+            if len(stations) == 1:
+                station = stations[0]
+                station.location = Point(
+                    float(record.origin_x), float(record.origin_y), srid=27700
+                )
+                self.check_station_point(
+                    {"location": station.location, "council_id": station.council_id,}
+                )
+                station.save()
+            else:
+                print("Could not find station id " + "-".join([record.number.strip()]))
+        print("...done")
