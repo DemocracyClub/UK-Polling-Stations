d9081d3a43e05422eb71b73a9ae29f3b39ff8380
GeoWill <roper.william@gmail.com>
2019-11-19 18:06:25 +0000

    Import script for Hart (closes #2211)

--- polling_stations/apps/data_collection/management/commands/import_hart.py
+++ polling_stations/apps/data_collection/management/commands/import_hart.py
@@ -3,14 +3,18 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E07000089"
-    addresses_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019Hart.tsv"
-    stations_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019Hart.tsv"
-    elections = ["local.2019-05-02", "europarl.2019-05-23"]
+    addresses_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019Hart.tsv"
+    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019Hart.tsv"
+    elections = ["parl.2019-12-12"]
     csv_delimiter = "\t"
+    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in ["10093265032", "100060398295", "10093264107"]:
+            return None
 
         if uprn in [
             "10008959585",  # GU179LW -> GU466AT : Leafy Oak Farmhouse, Cobbetts Lane, Blackwater, Camberley
@@ -18,15 +22,14 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "10008948691",  # RG278EF -> RG278SH : Taplins Farm Stables, Church Lane, Hartley Wintney, Hook
             "200001006098",  # GU105HJ -> RG291TL : Quern Farm, Well Road, Crondall, Farnham
             "100060406353",  # GU527UA -> GU467RX : 94A Reading Road South, Fleet, Hampshire
-            "200001000332",  # RG279JJ -> RG278JD : The Annexe, 1 Yew Tree Cottage, Hook Common, Hook
         ]:
             rec["accept_suggestion"] = False
 
         return rec
 
     def station_record_to_dict(self, record):
-
-        if record.polling_place_id == "2487":
+        # Hart Leisure Centre
+        if record.polling_place_id == "2903":
             record = record._replace(polling_place_easting="479224")
             record = record._replace(polling_place_northing="154016")
 
