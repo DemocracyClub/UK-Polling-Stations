084f613477d6845e0fa3b98f1024a7736be4ba21
GeoWill <roper.william@gmail.com>
2019-11-20 15:13:43 +0000

    Import script for City of London (closes #2230)

--- polling_stations/apps/data_collection/management/commands/import_city_of_london.py
+++ polling_stations/apps/data_collection/management/commands/import_city_of_london.py
@@ -3,6 +3,14 @@ from data_collection.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "E09000001"
-    addresses_name = "europarl.2019-05-23/Version 1/Data for Democracy Club.csv"
-    stations_name = "europarl.2019-05-23/Version 1/Data for Democracy Club.csv"
-    elections = ["europarl.2019-05-23"]
+    addresses_name = "parl.2019-12-12/Version 1/polling_station_export-2019-11-06.csv"
+    stations_name = "parl.2019-12-12/Version 1/polling_station_export-2019-11-06.csv"
+    elections = ["parl.2019-12-12"]
+
+    def address_record_to_dict(self, record):
+        rec = super().address_record_to_dict(record)
+
+        if record.housepostcode == "EC4Y 9BE":
+            return None
+
+        return rec
