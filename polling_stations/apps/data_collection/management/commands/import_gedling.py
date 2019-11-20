f2a9ef7467f2f99b0ac0c56103f881a3680b193d
Alexander Dutton <alex.dutton@mysociety.org>
2019-11-20 15:44:02 +0000

    Import Gedling (closes #2225)

--- polling_stations/apps/data_collection/management/commands/import_gedling.py
+++ polling_stations/apps/data_collection/management/commands/import_gedling.py
@@ -3,23 +3,34 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E07000173"
-    addresses_name = "europarl.2019-05-23/Version 1/Democracy_Club__23May2019.tsv"
-    stations_name = "europarl.2019-05-23/Version 1/Democracy_Club__23May2019.tsv"
-    elections = ["europarl.2019-05-23"]
+    addresses_name = (
+        "parl.2019-12-12/Version 1/Democracy_Club__12December2019gedling.tsv"
+    )
+    stations_name = (
+        "parl.2019-12-12/Version 1/Democracy_Club__12December2019gedling.tsv"
+    )
+    elections = ["parl.2019-12-12"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
-        if record.addressline6 == "NG15 8JT":
+        if record.addressline6 in ("NG5 8PQ", "NG4 4PS"):
+            # Unexpected polling stations for one or more properties. Removing just those properties might give
+            # misleading results for the postcode if the polling station was correct.
             return None
 
-        if uprn == "10035280929":
-            rec["postcode"] = "NG6 8YX"
+        if uprn in [
+            "10035282319",
+            "100032276046",
+            "100032123022",
+            "100031353751",
+        ]:
+            return None
 
-        if uprn == "10035287498":
-            rec["postcode"] = "NG14 6SE"
+        if record.addressline1 == "328A Carlton Hill":
+            return None
 
         if uprn in [
             "10035281242",  # NG35TQ -> NG35QQ : Flat 2 840 Woodborough Road, Mapperley, Nottinghamshire
@@ -28,7 +39,6 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "10035281244",  # NG35TQ -> NG35QQ : Flat 4 840 Woodborough Road, Mapperley, Nottinghamshire
             "10035281246",  # NG35TQ -> NG35QQ : Flat 5 840 Woodborough Road, Mapperley, Nottinghamshire
             "10035281247",  # NG35TQ -> NG35QQ : Flat 6 840 Woodborough Road, Mapperley, Nottinghamshire
-            "10002895726",  # NG58NP -> NG68NP : 2 Rhodes Way, Bestwood, Nottinghamshire
             "100031358592",  # NG41LX -> NG41LZ : 64 Ernest Road, Carlton, Nottinghamshire
             "100032100963",  # NG158GD -> NG159HP : The Gatehouse Lodge, Newstead, Nottinghamshire
             "10002888179",  # NG158GD -> NG158GE : Mount Charlotte, Newstead Abbey Park, Ravenshead, Nottinghamshire
@@ -36,9 +46,10 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "100031366280",  # NG36BN -> NG36BS : 108 Kent Road, Mapperley, Nottinghamshire
             "100031360828",  # NG57BN -> NG57BP : Ernehale Lodge Nursing Home, Furlong Street, Arnold, Nottinghamshire
             "100031370521",  # NG58PB -> NG58PE : Wernbank Cottage, Mansfield Road, Bestwood, Nottinghamshire
+            "10002896029",  # NG36HE -> NG36HG : Flat above The Punchbowl, 214 Porchester Road, Mapperley, Nottingham
         ]:
             rec["accept_suggestion"] = True
-
+        #
         if uprn in [
             "200001152310",  # NG44FL -> NG44FG : 267A Westdale Lane East, Carlton, Nottinghamshire
             "200001152316",  # NG44FN -> NG44FG : 273A Westdale Lane East, Carlton, Nottinghamshire
@@ -55,7 +66,7 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "100032099652",  # NG146FG -> NG146FN : The Flat Oscar`s Lounge & Restaurant,, Main Street, Calverton, Nottinghamshire
             "100031348280",  # NG146FR -> NG146ED : The Poplars, Bonner Hill, Calverton, Nottinghamshire
             "10002896692",  # NG58PJ -> NG158FJ : Miller and Carter Steakhouse, Mansfield Road, Arnold, Nottingham
-            "10002896029",  # NG36HE -> NG36HG : 214 Porchester Road, Mapperley, Nottingham, Nottinghamshire
+            "10035287615",  # NG158JU -> NG44LU : 31 Askew Road, Linby, Nottinghamshire
         ]:
             rec["accept_suggestion"] = False
 
