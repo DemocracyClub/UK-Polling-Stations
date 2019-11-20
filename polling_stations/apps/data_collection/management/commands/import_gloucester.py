0b9b8578c7e15671b58b1a4d93d30cdf0a70475e
Alexander Dutton <alex.dutton@mysociety.org>
2019-11-20 10:18:30 +0000

    Import Gloucester (closes #2209)

--- polling_stations/apps/data_collection/management/commands/import_gloucester.py
+++ polling_stations/apps/data_collection/management/commands/import_gloucester.py
@@ -5,22 +5,26 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E07000081"
     addresses_name = (
-        "europarl.2019-05-23/Version 2/Democracy_Club__23May2019 v6glos.tsv"
+        "parl.2019-12-12/Version 1/Democracy_Club__12December2019glouces.tsv"
     )
-    stations_name = "europarl.2019-05-23/Version 2/Democracy_Club__23May2019 v6glos.tsv"
-    elections = ["europarl.2019-05-23"]
+    stations_name = (
+        "parl.2019-12-12/Version 1/Democracy_Club__12December2019glouces.tsv"
+    )
+    elections = ["parl.2019-12-12"]
     csv_delimiter = "\t"
+    allow_station_point_from_postcode = False
 
     def station_record_to_dict(self, record):
 
         # St Paul and St Stephen Church
-        if record.polling_place_id == "2663":
+        if record.polling_place_id == "3200":
             record = record._replace(polling_place_postcode="GL1 5AL")
             rec = super().station_record_to_dict(record)
             rec["location"] = Point(-2.245029, 51.854501, srid=4326)
             return rec
 
-        if record.polling_place_id == "2599":
+        # St Lawrence Church Centre
+        if record.polling_place_id == "3145":
             rec = super().station_record_to_dict(record)
             rec["location"] = Point(-2.206526, 51.858234, srid=4326)
             return rec
@@ -29,8 +33,23 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
+        uprn = record.property_urn.strip().lstrip("0")
 
-        if record.addressline1.strip() == "294 Painswick Road":
-            rec["postcode"] = "GL4 5DE"
+        if uprn in [
+            "10007318936",  # GL24DE -> GL45DE : 294 Painswick Road, Gloucester
+            "10007318921",  # GL46JR -> GL46JB : 24B Bazeley Road, Gloucester
+            "10007304835",  # GL13NP -> GL13NF : 1 Picton House, Wellington Parade, Gloucester
+        ]:
+            rec["accept_suggestion"] = True
+
+        if uprn in [
+            "10007317909",  # GL46LP -> GL46ES : 48A Birchall Avenue, Gloucester
+            "10007319078",  # GL12NZ -> GL46PW : 78A Westgate Street, Gloucester
+            "10007310053",  # GL15AL -> GL15AN : Flat 3, 6 Park End Road, Gloucester
+            "100120469680",  # GL20JT -> GL12QL : 115 Elmleaze, Gloucester
+            "100120474388",  # GL14PJ -> GL14PN : 13 Lattistep Court, Hatherley Road, Gloucester
+            "100121248175",  # GL15PT -> GL15PX : 24B Seymour Road, Gloucester
+        ]:
+            rec["accept_suggestion"] = False
 
         return rec
