81d4c5c62ab6d1a9c5b033372d3082c1f3e0947a
GeoWill <roper.william@gmail.com>
2019-11-21 17:18:56 +0000

    Import script for Herefordshire (closes #2290)

--- polling_stations/apps/data_collection/management/commands/import_herefordshire.py
+++ polling_stations/apps/data_collection/management/commands/import_herefordshire.py
@@ -1,30 +1,47 @@
 from data_collection.management.commands import BaseHalaroseCsvImporter
+from django.contrib.gis.geos import Point
 
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "E06000019"
     addresses_name = (
-        "local.2019-05-02/Version 1/polling_station_export-2019-03-19Here.csv"
+        "parl.2019-12-12/Version 1/polling_station_export-2019-11-08here.csv"
     )
     stations_name = (
-        "local.2019-05-02/Version 1/polling_station_export-2019-03-19Here.csv"
+        "parl.2019-12-12/Version 1/polling_station_export-2019-11-08here.csv"
     )
-    elections = ["local.2019-05-02"]
-    csv_encoding = "windows-1252"
+    elections = ["parl.2019-12-12"]
+    allow_station_point_from_postcode = False
+
+    def station_record_to_dict(self, record):
+        rec = super().station_record_to_dict(record)
+
+        if record.pollingstationname == "Weobley Village Hall":
+            rec["location"] = Point(-2.869103, 52.159915, srid=4326)
+
+        if record.pollingstationname == "Richards Castle Village Hall":
+            rec["location"] = Point(-2.74213, 52.32607, srid=4326)
+
+        return rec
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
+        uprn = record.uprn.strip().lstrip("0")
 
         if record.housepostcode.strip() == "HR9 7RA":
             return None
 
-        if record.houseid in ["9012185", "9012186"]:
+        if uprn in ["10007369349", "10091655370"]:
+
             return None
 
-        if record.houseid == "3072006":
+        if record.houseid in [
+            "3072006",
+            "9010513",
+        ]:
             return None
 
-        if record.housepostcode.strip() == "HR1 2PJ":
+        if record.housepostcode.strip() in ["HR1 2PJ", "HR4 8FH"]:
             return None
 
         return rec
