28ee9ebf57764478d542693b1c92005d6ee8bb30
GeoWill <roper.william@gmail.com>
2019-11-21 11:24:06 +0000

    Import script for Castle Point (closes #2235)

--- polling_stations/apps/data_collection/management/commands/import_castle_point.py
+++ polling_stations/apps/data_collection/management/commands/import_castle_point.py
@@ -1,34 +1,31 @@
-from django.contrib.gis.geos import Point
-from data_collection.management.commands import (
-    BaseXpressDCCsvInconsistentPostcodesImporter,
-)
+from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
-class Command(BaseXpressDCCsvInconsistentPostcodesImporter):
+class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E07000069"
     addresses_name = (
-        "local.2018-05-03/Version 1/Democracy_Club__03May2018 Castlepoint.tsv"
+        "parl.2019-12-12/Version 1/Democracy_Club__12December2019castle.tsv"
     )
-    stations_name = (
-        "local.2018-05-03/Version 1/Democracy_Club__03May2018 Castlepoint.tsv"
-    )
-    elections = ["local.2018-05-03"]
+    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019castle.tsv"
+    elections = ["parl.2019-12-12"]
     csv_delimiter = "\t"
-
-    def station_record_to_dict(self, record):
-        rec = super().station_record_to_dict(record)
-
-        if rec["internal_council_id"] == "1799":
-            rec["location"] = Point(0.605786, 51.554074, srid=4326)
-
-        return rec
+    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
+        rec = super().address_record_to_dict(record)
 
+        if uprn in [
+            "10004934986",
+            "100090361765",
+            "10004940667",
+            "10004941055",
+        ]:
+            return None
+
+        if record.addressline1 == "Caravan 1112A Thorney Bay Park":
+            rec["postcode"] = "SS8 0DB"
         if uprn == "10004937052":
-            rec = super().address_record_to_dict(record)
             rec["postcode"] = "SS8 7SL"
-            return rec
 
-        return super().address_record_to_dict(record)
+        return rec
