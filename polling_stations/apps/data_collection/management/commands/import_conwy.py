0c257b9233fd21331242dd2b9cb5816299cf5212
GeoWill <roper.william@gmail.com>
2019-11-21 16:31:13 +0000

    Import script for Conwy (closes #2289)

--- polling_stations/apps/data_collection/management/commands/import_conwy.py
+++ polling_stations/apps/data_collection/management/commands/import_conwy.py
@@ -3,6 +3,12 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "W06000003"
-    addresses_name = "europarl.2019-05-23/Version 1/Democracy_Club__23May2019_Conwy.CSV"
-    stations_name = "europarl.2019-05-23/Version 1/Democracy_Club__23May2019_Conwy.CSV"
-    elections = ["europarl.2019-05-23"]
+    addresses_name = (
+        "parl.2019-12-12/Version 2/Democracy_Club__12December2019_20.11.2019.tsv"
+    )
+    stations_name = (
+        "parl.2019-12-12/Version 2/Democracy_Club__12December2019_20.11.2019.tsv"
+    )
+    elections = ["parl.2019-12-12"]
+    csv_delimiter = "\t"
+    allow_station_point_from_postcode = False
