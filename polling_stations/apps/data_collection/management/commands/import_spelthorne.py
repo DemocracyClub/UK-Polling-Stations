ad31b7037f4ec2175282e27759abd8c364c12950
GeoWill <roper.william@gmail.com>
2019-11-22 11:50:52 +0000

    Import script for Spelthorne (closes #2296)

--- polling_stations/apps/data_collection/management/commands/import_spelthorne.py
+++ polling_stations/apps/data_collection/management/commands/import_spelthorne.py
@@ -4,20 +4,31 @@ from data_collection.management.commands import BaseHalaroseCsvImporter
 class Command(BaseHalaroseCsvImporter):
     council_id = "E07000213"
     addresses_name = (
-        "local.2019-05-02/Version 1/polling_station_export Spelthorne-2019-02-07.csv"
+        "parl.2019-12-12/Version 1/polling_station_export-2019-11-09spel.csv"
     )
     stations_name = (
-        "local.2019-05-02/Version 1/polling_station_export Spelthorne-2019-02-07.csv"
+        "parl.2019-12-12/Version 1/polling_station_export-2019-11-09spel.csv"
     )
-    elections = ["local.2019-05-02", "europarl.2019-05-23"]
-    match_threshold = 98
+    elections = ["parl.2019-12-12"]
+    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
+        rec = super().address_record_to_dict(record)
 
-        if uprn in ["33040703", "33040704", "33048602", "33051119"]:
-            rec = super().address_record_to_dict(record)
+        if record.housepostcode in ["TW18 0OO", "TW17 0OO"]:
+            return None
+
+        if record.housepostcode == "TW18 3AE":
+            rec["postcode"] = "TW153AE"
+
+        if uprn in [
+            "33048602",  # TW151AG -> TW152AG : Flat 8, 54 St Michaels Road, Ashford
+            "33051119",  # TW178AN -> TW178NF : Bude Haven, Riverside, Penny Lane, Shepperton
+            "33040704",  # TW184JQ -> TW184JX : Hengrove Farm, 324B London Road, Ashford
+            "33040703",  # TW184JQ -> TW184JX : 324A London Road, Ashford
+            "33036070",  # TW182LF -> TW182LE : Towpath House, Riverside Road, Staines-Upon-Thames
+        ]:
             rec["accept_suggestion"] = True
-            return rec
 
-        return super().address_record_to_dict(record)
+        return rec
