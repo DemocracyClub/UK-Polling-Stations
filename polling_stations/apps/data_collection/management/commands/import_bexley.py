af1d8a549cf444e33d094bce8582758ba13b2652
Alexander Dutton <alex.dutton@mysociety.org>
2019-11-22 18:06:00 +0000

    Import Bexley (closes #2301)

--- polling_stations/apps/data_collection/management/commands/import_bexley.py
+++ polling_stations/apps/data_collection/management/commands/import_bexley.py
@@ -1,39 +1,44 @@
-from data_collection.management.commands import BaseXpressWebLookupCsvImporter
+from data_collection.ems_importers import BaseXpressDemocracyClubCsvImporter
 
 
-class Command(BaseXpressWebLookupCsvImporter):
+class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E09000004"
-    addresses_name = "local.2018-05-03/Version 1/PropertyPostCodePollingStationWebLookup-2018-03-21.TSV"
-    stations_name = "local.2018-05-03/Version 1/PropertyPostCodePollingStationWebLookup-2018-03-21.TSV"
-    elections = ["local.2018-05-03"]
+    addresses_name = (
+        "parl.2019-12-12/Version 1/L B Bexley - Democracy_Club__12December2019.tsv"
+    )
+    stations_name = (
+        "parl.2019-12-12/Version 1/L B Bexley - Democracy_Club__12December2019.tsv"
+    )
+    elections = ["parl.2019-12-12"]
     csv_delimiter = "\t"
+    allow_station_point_from_postcode = False
 
     def station_record_to_dict(self, record):
-
-        # Correction requested
-        if record.pollingplaceid == "843":
-            record = record._replace(pollingplaceaddress2="Belmont Road")
-            record = record._replace(pollingplaceaddress3="")
-            record = record._replace(pollingplaceaddress4="")
-            record = record._replace(pollingplaceaddress5="")
-            record = record._replace(pollingplaceaddress6="")
-            record = record._replace(pollingplaceaddress7="DA8 1LB")
-
         # Point supplied for Footscray Baptist Church is miles off
-        if record.pollingplaceid == "869":
-            record = record._replace(pollingplaceeasting="0")
-            record = record._replace(pollingplacenorthing="0")
-
-        # Correction requested
-        if record.pollingplaceid == "878":
-            record = record._replace(pollingplaceaddress1="Temp Poll Stn (SN1B/E)")
-            record = record._replace(pollingplaceaddress2="Reddy Road")
-            record = record._replace(pollingplaceaddress3="Slade Green")
-            record = record._replace(pollingplaceaddress4="")
-            record = record._replace(pollingplaceaddress5="")
-            record = record._replace(pollingplaceaddress6="")
-            record = record._replace(pollingplaceaddress7="DA8 2AY")
-            record = record._replace(pollingplaceeasting="0")
-            record = record._replace(pollingplacenorthing="0")
-
+        if record.polling_place_id == "1804":
+            record = record._replace(
+                polling_place_easting="547145", polling_place_northing="171147"
+            )
         return super().station_record_to_dict(record)
+
+    def address_record_to_dict(self, record):
+        rec = super().address_record_to_dict(record)
+        uprn = record.property_urn.lstrip("0")
+
+        if record.addressline6.strip() in ("DA14 6NG", "DA14 6NE"):
+            # The only 6NG property should be 6NE, which is itself split across stations in a surprising way
+            return None
+
+        if uprn in [
+            "10090793686",  # DA74AQ -> DA74QW : 2A Pickford Road, Bexleyheath, Kent
+            "100020221033",  # DA14RN -> DA14RS : 23A Iron Mill Lane, Crayford, Kent
+            "100020221034",  # DA14RN -> DA14RS : 23 Iron Mill Lane, Crayford, Kent
+        ]:
+            rec["accept_suggestion"] = True
+
+        if uprn in [
+            "100023522937",  # DA68DP -> DA68HZ : 9 Standard Road, Bexleyheath, Kent
+        ]:
+            rec["accept_suggestion"] = False
+
+        return rec
