38b0672f5fba50e0e17fe653c5a9330aec056072
GeoWill <roper.william@gmail.com>
2019-11-25 17:10:59 +0000

    Import script for Cardiff (closes #2303)

--- polling_stations/apps/data_collection/management/commands/import_cardiff.py
+++ polling_stations/apps/data_collection/management/commands/import_cardiff.py
@@ -1,78 +1,32 @@
-"""
-Import Cardiff
-"""
-from django.db import transaction
-from django.db import connection
-
-from data_collection.management.commands import BaseShpStationsShpDistrictsImporter
-from pollingstations.models import PollingDistrict
+from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
-class Command(BaseShpStationsShpDistrictsImporter):
-    """
-    Imports the Polling Station data from Braintree
-    """
+class Command(BaseXpressDemocracyClubCsvImporter):
 
     council_id = "W06000015"
-    districts_name = "Polling Districts_region"
-    stations_name = "Polling Stations_font_point.shp"
-    elections = ["local.cardiff.2017-05-04", "parl.2017-06-08"]
+    addresses_name = (
+        "parl.2019-12-12/Version 1/Democracy_Club__12December2019cardiff.tsv"
+    )
+    stations_name = (
+        "parl.2019-12-12/Version 1/Democracy_Club__12December2019cardiff.tsv"
+    )
+    elections = ["parl.2019-12-12"]
+    csv_delimiter = "\t"
+    csv_encoding = "windows-1252"
+    allow_station_point_from_postcode = False
 
-    def parse_string(self, text):
-        try:
-            return text.strip().decode("latin-1")
-        except AttributeError:
-            return text.strip()
+    def address_record_to_dict(self, record):
+        rec = super().address_record_to_dict(record)
+        uprn = record.property_urn.strip().lstrip("0")
 
-    def district_record_to_dict(self, record):
-        code = self.parse_string(record[0])
-
-        """
-        Joe received following from Cardiff:
-
-        Two polling districts SJ and QK
-        The following streets are affected
-          - SJ Duncan Close
-          - QK Bethania Row
-        but no updated data: exclude these 2 districts
-        because we don't have the new boundaries
-        """
-        if code == "SJ" or code == "QK":
+        if uprn == "10023549342":
+            rec["postcode"] = "CF240ES"
+        if uprn == "200001679322":
+            rec["postcode"] = "CF118HP"
+        if record.addressline6 in ["CF72 8NS", "CF14 9UA"]:
             return None
 
-        return {"internal_council_id": code, "name": code}
+        if uprn in ["200001679752", "10002509644"]:
+            return None
 
-    def station_record_to_dict(self, record):
-        unique_id = "-".join(
-            (self.parse_string(record[0]), self.parse_string(record[1]))
-        ).strip()
-        return {
-            "internal_council_id": unique_id,
-            "postcode": "",
-            "address": self.parse_string(record[2]),
-            "polling_district_id": self.parse_string(record[1]),
-        }
-
-    @transaction.atomic
-    def post_import(self):
-        """
-        This data isn't great – the polygons seem to be corrupt in some way.
-
-        PostGIS can fix them though!
-        """
-        print("running fixup SQL")
-        table_name = PollingDistrict()._meta.db_table
-
-        cursor = connection.cursor()
-        cursor.execute(
-            """
-            UPDATE {0}
-            SET area=ST_Multi(ST_CollectionExtract(ST_MakeValid(area), 3))
-            WHERE NOT ST_IsValid(area);
-            """.format(
-                table_name
-            )
-        )
-        # Note the delibarate use of `.format` above – we don't want the table
-        # names in quotes.  Use `%s` and a list as a 2nd arg to execute
-        # if you're adding values at all.
+        return rec
