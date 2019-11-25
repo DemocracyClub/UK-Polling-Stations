fd1eef49621335af132f920555c974b3b0982dca
GeoWill <roper.william@gmail.com>
2019-11-25 11:14:20 +0000

    Import script for Derbyshire Dales (closes #2293)

--- polling_stations/apps/data_collection/management/commands/import_derbyshire_dales.py
+++ polling_stations/apps/data_collection/management/commands/import_derbyshire_dales.py
@@ -3,9 +3,10 @@ from data_collection.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "E07000035"
-    addresses_name = "local.2019-05-02/Version 2/DerbyDalesDC polling districts.csv"
-    stations_name = "local.2019-05-02/Version 2/DerbyDalesDC polling stations.csv"
-    elections = ["local.2019-05-02"]
+    addresses_name = "parl.2019-12-12/Version 1/Democracy Club - Polling Districts - Derbyshire Dales.csv"
+    stations_name = "parl.2019-12-12/Version 1/Democray Club - Polling Stations - Derbyshire Dales.csv"
+    elections = ["parl.2019-12-12"]
+    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
