4d66fb5550991bc93b06361aee33457fb1e2b6aa
GeoWill <roper.william@gmail.com>
2019-11-25 11:49:52 +0000

    Import script for Hartlepool (closes #2243)

--- polling_stations/apps/data_collection/management/commands/import_hartlepool.py
+++ polling_stations/apps/data_collection/management/commands/import_hartlepool.py
@@ -1,61 +1,44 @@
 from django.contrib.gis.geos import Point
 from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
-from data_collection.addresshelpers import format_residential_address
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E06000001"
-    addresses_name = "local.2019-05-02/Version 1/Polling Station Address Information 02 May 2019 Hartlepool.csv"
-    stations_name = "local.2019-05-02/Version 1/Polling Station Address Information 02 May 2019 Hartlepool.csv"
-    elections = ["local.2019-05-02"]
-
-    # Hartlepool use Xpress, but they've provided a slightly trimmed down
-    # version of the export. We need to customise a bit..
-    station_uprn_field = None
+    addresses_name = "parl.2019-12-12/Version 3/Democracy_Club__12December 2019.TSV"
+    stations_name = "parl.2019-12-12/Version 3/Democracy_Club__12December 2019.TSV"
+    elections = ["parl.2019-12-12"]
+    csv_delimiter = "\t"
+    allow_station_point_from_postcode = False
 
     def station_record_to_dict(self, record):
+        rec = super().station_record_to_dict(record)
+        if record.polling_place_id == "8878":
 
-        if record.polling_place_id == "8017":
-            record = record._replace(polling_place_name="Warrior Park Care Home")
-            record = record._replace(polling_place_address_1="Queen Street")
-            record = record._replace(polling_place_address_2="Seaton Carew")
-            record = record._replace(polling_place_address_3="Stockport")
-            record = record._replace(polling_place_address_4="Hartlepool")
-            record = record._replace(polling_place_postcode="TS25 1EZ")
             rec = super().station_record_to_dict(record)
             rec["location"] = Point(-1.191945, 54.664709, srid=4326)
-            return rec
 
-        return super().station_record_to_dict(record)
+        return rec
 
     def address_record_to_dict(self, record):
-        if record.post_code.strip() == "":
-            return None
-        address = format_residential_address(
-            [
-                record.addressline1,
-                record.addressline2,
-                record.addressline3,
-                record.addressline4,
-            ]
-        )
-        uprn = getattr(record, self.residential_uprn_field).strip().lstrip("0")
-        postcode = record.post_code.strip()
-        ret = {
-            "address": address.strip(),
-            "postcode": postcode,
-            "polling_station_id": getattr(record, self.station_id_field).strip(),
-            "uprn": uprn,
-        }
+        rec = super().address_record_to_dict(record)
+        uprn = record.property_urn.strip().lstrip("0")
 
         # corrections
-        if postcode in ["TS25 2HE", "TS24 0HJ"]:
+        if record.addressline6 in [
+            "TS25 5DF",
+            "TS25 2HE",
+        ]:
+            return None
+
+        if uprn in ["100110020815", "100110009750", "10090070990", "100110020667"]:
             return None
 
         if uprn in ["10090068484", "10009734034", "100110786034"]:
-            ret["accept_suggestion"] = True
+            rec["accept_suggestion"] = True
 
-        if uprn in ["100110673453", "100110673049"]:
-            ret["accept_suggestion"] = False
+        if uprn in [
+            "100110673453",
+        ]:
+            rec["accept_suggestion"] = False
 
-        return ret
+        return rec
