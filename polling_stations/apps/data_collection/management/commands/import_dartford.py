ec061648a76ebd9a7fb33d29b51e064c3e0edec8
GeoWill <roper.william@gmail.com>
2019-11-26 14:30:20 +0000

    Import script for Dartford (closes #2317)

--- polling_stations/apps/data_collection/management/commands/import_dartford.py
+++ polling_stations/apps/data_collection/management/commands/import_dartford.py
@@ -3,44 +3,33 @@ from data_collection.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "E07000107"
-    addresses_name = (
-        "europarl.2019-05-23/Version 1/polling_station_export-2019-05-06.csv"
-    )
-    stations_name = (
-        "europarl.2019-05-23/Version 1/polling_station_export-2019-05-06.csv"
-    )
-    elections = ["europarl.2019-05-23"]
+    addresses_name = "parl.2019-12-12/Version 1/polling_station_export-2019-11-07.csv"
+    stations_name = "parl.2019-12-12/Version 1/polling_station_export-2019-11-07.csv"
+    elections = ["parl.2019-12-12"]
+    allow_station_point_from_postcode = False
 
     def station_record_to_dict(self, record):
-        rec = super().station_record_to_dict(record)
-        if not rec:
-            return rec
-        if rec["internal_council_id"] in [
-            "54-hodsoll-street-ridley-village-hall",
-            "57-fawkham-and-hartley-church-centre",
+        # These are single records listed as 'Other Electors Address'
+        # no addresses are assigned. So probably don't cause problems, but
+        # dropping them to squash warnings.
+        if (record.pollingstationnumber, record.pollingstationname) in [
+            ("63", "Hodsoll Street & Ridley Village Hall"),
+            ("64", "Longfield & Hartley Scout HQ"),
+            ("66", "Fawkham and Hartley Church Centre"),
         ]:
             return None
-        return rec
+        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
         uprn = record.uprn.strip().lstrip("0")
 
-        if record.pollingstationnumber == "n/a":
-            return None
-
         if record.houseid.strip() in ["9004672", "9004673"]:
             rec["postcode"] = "DA2 7AP"
 
         if uprn in [
-            "10023438353",  # DA11JB -> DA11HS : Flat Above The Plough 110 Lowfield Street, Dartford, Kent
             "100060873013",  # DA28AX -> DA28DL : Beacon House Shellbank Lane, Bean, Dartford, Kent
         ]:
             rec["accept_suggestion"] = True
-
-        if uprn in [
-            "100060862814"  # DA99XT -> DA13JB : 104 Havelock Drive, Greenhithe, Kent
-        ]:
-            rec["accept_suggestion"] = False
 
         return rec
