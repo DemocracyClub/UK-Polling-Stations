c5fbc85ebc72b6814258294563f916da19bddccb
chris48s <chris.shaw480@gmail.com>
2019-11-27 16:58:22 +0000

    import Vale of White Horse (closes #2351)

--- polling_stations/apps/data_collection/management/commands/import_vale_of_white_horse.py
+++ polling_stations/apps/data_collection/management/commands/import_vale_of_white_horse.py
@@ -3,98 +3,57 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E07000180"
+    # single file merged for South Oxfordshire and Value of White Horse
+    # we'll split out the bits we need
     addresses_name = (
-        "local.2019-05-02/Version 1/Democracy Club Data Vale of White Horse.tsv"
+        "parl.2019-12-12/Version 1/Democracy_Club__12December2019vale and ox.CSV"
     )
     stations_name = (
-        "local.2019-05-02/Version 1/Democracy Club Data Vale of White Horse.tsv"
+        "parl.2019-12-12/Version 1/Democracy_Club__12December2019vale and ox.CSV"
     )
-    elections = ["europarl.2019-05-23"]
-    csv_delimiter = "\t"
+    elections = ["parl.2019-12-12"]
+    csv_delimiter = ","
+    allow_station_point_from_postcode = False
 
     def station_record_to_dict(self, record):
-
-        # 2x station changes for EU Parl elections
-        if record.polling_place_id == "6990":
-            record = record._replace(polling_place_name="Reading Room")
-            record = record._replace(polling_place_address_1="Littleworth")
-            record = record._replace(polling_place_address_2="")
-            record = record._replace(polling_place_address_3="")
-            record = record._replace(polling_place_address_4="")
-            record = record._replace(polling_place_postcode="SN7 8ED")
-            record = record._replace(polling_place_easting="0")
-            record = record._replace(polling_place_northing="0")
-            record = record._replace(polling_place_uprn="")
-
-        if record.polling_place_id == "6968":
-            record = record._replace(polling_place_name="St Michaels Church Hall")
-            record = record._replace(
-                polling_place_address_1="The Church of St Michaels and All Angels Church"
-            )
-            record = record._replace(polling_place_address_2="Church Lane")
-            record = record._replace(polling_place_address_3="")
-            record = record._replace(polling_place_address_4="")
-            record = record._replace(polling_place_postcode="OX13 6SN")
-            record = record._replace(polling_place_easting="0")
-            record = record._replace(polling_place_northing="0")
-            record = record._replace(polling_place_uprn="")
+        # discard records in South Oxfordshire
+        if record.polling_place_district_reference.startswith(
+            "L"
+        ) or record.polling_place_district_reference.startswith("R"):
+            return None
 
         # Polling place grid ref wrong.
-        if record.polling_place_id in ["6822"]:
+        if record.polling_place_id in ["8409"]:
             record = record._replace(polling_place_easting="0")
             record = record._replace(polling_place_northing="0")
 
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
+        # discard records in South Oxfordshire
+        if record.polling_place_district_reference.startswith(
+            "L"
+        ) or record.polling_place_district_reference.startswith("R"):
+            return None
+
         rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
-
-        # postcodes with implausible pollingstations.
-        if uprn in [
-            "10093199584",
-            "10093199586",
-            "10094276967",
-            "10093199585",
-            "10093199588",
-            "10094276965",
-            "10094276964",
-            "10093199587",
-            "10094276966",
-            "10094276968",
-        ]:
-            return None
 
         if uprn in [
             "10093195669"  # OX29AH -> OX29PH : 136, Apt 2, Cumnor Hill, Cumnor, Oxford, Oxfordshire
         ]:
             rec["accept_suggestion"] = True
 
-        if uprn in [
-            "10093196551"  # OX116GE -> OX110ED : 5 Mezereon Spur, Mezereon Spur, Harwell, Didcot
-        ]:
-            rec["accept_suggestion"] = False
+        if uprn == "10014079091":
+            rec["postcode"] = "OX12 0ED"
 
-        # Addressbase georeferencing incorrect
-        if uprn in [
-            "10093196937",
-            "10093196928",
-            "10093196926",
-            "10093196931",
-            "10093196930",
-            "10093196929",
-            "10093196936",
-            "10093196940",
-            "10093196939",
-            "10093196933",
-            "10093196925",
-            "10093196941",
-            "10093196935",
-            "10093196932",
-            "10093196938",
-            "10093196927",
-            "10093196934",
-        ]:
-            rec["accept_suggestion"] = False
+        if uprn == "10014027599":
+            rec["postcode"] = "OX12 8AU"
+
+        if record.post_code == "SN7 8FG":
+            return None
+
+        if uprn == "10014082062":
+            return None
 
         return rec
