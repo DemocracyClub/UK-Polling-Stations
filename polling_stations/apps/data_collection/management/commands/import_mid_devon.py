9e70a6a77da25c15a4a67daeb348494037e5e591
GeoWill <roper.william@gmail.com>
2019-11-29 14:33:27 +0000

    Import script for Mid Devon (closes #2394)

--- polling_stations/apps/data_collection/management/commands/import_mid_devon.py
+++ polling_stations/apps/data_collection/management/commands/import_mid_devon.py
@@ -1,48 +1,38 @@
 from django.contrib.gis.geos import Point
-from data_collection.management.commands import (
-    BaseXpressDCCsvInconsistentPostcodesImporter,
-)
+from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
-class Command(BaseXpressDCCsvInconsistentPostcodesImporter):
+class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E07000042"
-    addresses_name = (
-        "europarl.2019-05-23/Version 1/Democracy_Club__23May2019.tsv3May2019.tsv"
-    )
-    stations_name = (
-        "europarl.2019-05-23/Version 1/Democracy_Club__23May2019.tsv3May2019.tsv"
-    )
-    elections = ["europarl.2019-05-23"]
+    addresses_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019MD.tsv"
+    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019MD.tsv"
+    elections = ["parl.2019-12-12"]
     csv_delimiter = "\t"
     csv_encoding = "windows-1252"
+    allow_station_point_from_postcode = False
 
     def station_record_to_dict(self, record):
+        rec = super().station_record_to_dict(record)
 
         # Chawleigh Jubilee Hall
-        if record.polling_place_id == "7379":
-            record = record._replace(polling_place_easting="0")
-            record = record._replace(polling_place_northing="0")
-            rec = super().station_record_to_dict(record)
+        if record.polling_place_id == "7809":
             rec["location"] = None
-            return rec
 
         # Oakford Village Hall
-        if record.polling_place_id == "7280":
-            record = record._replace(polling_place_easting="0")
-            record = record._replace(polling_place_northing="0")
-            rec = super().station_record_to_dict(record)
+        if record.polling_place_id == "7949":
             rec["location"] = Point(-3.550351, 50.982061, srid=4326)
-            return rec
 
-        return super().station_record_to_dict(record)
+        return rec
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
-        uprn = record.property_urn.strip().lstrip("0")
 
-        if uprn in [
-            "10035356543"  # EX153EY -> EX153EX : The Old Oaks, Chapel Hill, Uffculme, Cullompton
+        if record.addressline6 in [
+            "EX16 8RA",
+            "EX16 7RW",
+            "EX17 3QQ",
+            "EX5 4LA",
         ]:
-            rec["accept_suggestion"] = False
+            return None
 
         return rec
