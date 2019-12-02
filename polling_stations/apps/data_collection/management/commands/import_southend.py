c91767e1488729d9f08d8a38a453f293a81cb689
GeoWill <roper.william@gmail.com>
2019-12-02 15:22:36 +0000

    Import script for Southend-on-Sea (closes #2406)

--- polling_stations/apps/data_collection/management/commands/import_southend.py
+++ polling_stations/apps/data_collection/management/commands/import_southend.py
@@ -1,17 +1,26 @@
-from data_collection.management.commands import (
-    BaseXpressDCCsvInconsistentPostcodesImporter,
-)
+from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
-class Command(BaseXpressDCCsvInconsistentPostcodesImporter):
+class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E06000033"
-    addresses_name = "local.2019-05-02/Version 1/Southend-on-Sea XpressDC.csv"
-    stations_name = "local.2019-05-02/Version 1/Southend-on-Sea XpressDC.csv"
-    elections = ["local.2019-05-02", "europarl.2019-05-23"]
+    addresses_name = (
+        "parl.2019-12-12/Version 1/polling station finder UKP 2019south.csv"
+    )
+    stations_name = "parl.2019-12-12/Version 1/polling station finder UKP 2019south.csv"
+    elections = ["parl.2019-12-12"]
+    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
+
+        if record.addressline6 in [
+            "SS2 5NJ",
+            "SS0 7JG",
+            "SS1 2PE",
+            "SS9 4RP",
+        ]:
+            return None
 
         if uprn in [
             "100090684611",  # SS26UY -> SS26UH : Flat above, 281 Eastwoodbury Lane, Southend-on-Sea, Essex
@@ -24,7 +33,6 @@ class Command(BaseXpressDCCsvInconsistentPostcodesImporter):
             "10024162959",  # SS24XA -> SS24UZ : Thorpe Bay High School House, Southchurch Boulevard, Southend-on-Sea, Essex
             "100090662005",  # SS94RP -> SS94RY : 124 Eastwood Old Road, Leigh-on-Sea, Essex
             "100091605320",  # SS94RP -> SS94RY : Flat Above, 112 Eastwood Old Road, Leigh-on-Sea, Essex
-            "100090662492",  # SS94LR -> SS94LT : 297 Eastwood Road North, Leigh-on-Sea, Essex
             "100090671177",  # SS92HE -> SS92HW : 99 Rectory Grove, Leigh-on-Sea, Essex
             "100091283942",  # SS91NH -> SS91NJ : 32 Chalkwell Park Drive, Leigh-on-Sea, Essex
             "10024286458",  # SS08LL -> SS08LR : 12A The Vestry, Kings Road, Westcliff-on-Sea, Essex
@@ -34,9 +42,6 @@ class Command(BaseXpressDCCsvInconsistentPostcodesImporter):
             "100091581635",  # SS00AA -> SS09UN : 77B Southbourne Grove, Westcliff-on-Sea, Essex
             "100091291176",  # SS12RG -> SS12FJ : 18 Wesley Court, Southchurch Avenue, Southend-on-Sea, Essex
         ]:
-            rec["accept_suggestion"] = False
-
-        if uprn in ["10024288188", "10024288190", "10024288189"]:
             rec["accept_suggestion"] = False
 
         return rec
