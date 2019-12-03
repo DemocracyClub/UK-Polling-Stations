4acd8a78516f6c811efc172b64152da0daaf1799
Alex Dutton <alex@cottagelabs.com>
2019-12-03 14:38:10 +0000

    Import Rushcliffe (closes #2419)

--- polling_stations/apps/data_collection/management/commands/import_rushcliffe.py
+++ polling_stations/apps/data_collection/management/commands/import_rushcliffe.py
@@ -3,18 +3,19 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E07000176"
-    addresses_name = "europarl.2019-05-23/Version 1/Democracy_Club__23May2019rush.CSV"
-    stations_name = "europarl.2019-05-23/Version 1/Democracy_Club__23May2019rush.CSV"
-    elections = ["europarl.2019-05-23"]
+    addresses_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019.tsv"
+    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019.tsv"
+    elections = ["parl.2019-12-12"]
+    csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
 
         # these co-ordinates were a missing a digit - fixes carried over from local.2019-05-02
-        if record.polling_place_id == "4346":  # Bingham Methodist Centre
+        if record.polling_place_id == "4566":  # Bingham Methodist Centre
             record = record._replace(polling_place_easting="470360")
-        if record.polling_place_id == "4390":  # Edwalton Church Hall
+        if record.polling_place_id == "4660":  # Edwalton Church Hall
             record = record._replace(polling_place_easting="459770")
-        if record.polling_place_id == "4525":  # West Bridgford Baptist Church
+        if record.polling_place_id == "4785":  # West Bridgford Baptist Church
             record = record._replace(polling_place_easting="458380")
 
         return super().station_record_to_dict(record)
@@ -23,10 +24,8 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
-        if uprn == "3040065651":
-            rec["postcode"] = "NG12 4DF"
-
-        if uprn == "3040069418":
-            rec["postcode"] = "NG23 5RW"
+        if uprn == "3040072549":
+            rec["postcode"] = "NG2 7PQ"
+            rec["accept_suggestion"] = False
 
         return rec
