4b285ae0da8c1618983d6d82751cf8ccce9b1fe8
GeoWill <roper.william@gmail.com>
2019-12-03 14:32:25 +0000

    Import script for Bromsgrove (closes #2420)

--- polling_stations/apps/data_collection/management/commands/import_bromsgrove.py
+++ polling_stations/apps/data_collection/management/commands/import_bromsgrove.py
@@ -4,10 +4,19 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E07000234"
     addresses_name = (
-        "parl.2017-06-08/Version 2/Bromsgrove Democracy_Club__08June2017 BDC.tsv17.tsv"
+        "parl.2019-12-12/Version 1/Democracy_Club__12December2019- Bromsgrove.CSV"
     )
     stations_name = (
-        "parl.2017-06-08/Version 2/Bromsgrove Democracy_Club__08June2017 BDC.tsv17.tsv"
+        "parl.2019-12-12/Version 1/Democracy_Club__12December2019- Bromsgrove.CSV"
     )
-    elections = ["parl.2017-06-08"]
-    csv_delimiter = "\t"
+    elections = ["parl.2019-12-12"]
+    allow_station_point_from_postcode = False
+
+    def address_record_to_dict(self, record):
+        rec = super().address_record_to_dict(record)
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in ["100120569259", "10090741909"]:
+            return None
+
+        return rec
