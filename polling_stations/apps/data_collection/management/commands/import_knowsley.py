a119ba1a3ab4ea8e865034410fe20bb3450c4543
GeoWill <roper.william@gmail.com>
2019-12-03 13:02:46 +0000

    Import script for Knowsley (closes #2415)

--- polling_stations/apps/data_collection/management/commands/import_knowsley.py
+++ polling_stations/apps/data_collection/management/commands/import_knowsley.py
@@ -3,14 +3,24 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E08000011"
-    addresses_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019Knowsley.CSV"
-    stations_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019Knowsley.CSV"
-    elections = ["local.2019-05-02", "europarl.2019-05-23"]
-    csv_delimiter = ","
+    addresses_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019know.tsv"
+    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019know.tsv"
+    elections = ["parl.2019-12-12"]
+    csv_delimiter = "\t"
+    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn == "40038051":
+            return None
+
+        if record.addressline1 == "14A Katherine Walk":
+            return None
+
+        if record.addressline6 == "L34 6JQ":
+            return None
 
         if uprn in [
             "40061353",
@@ -21,11 +31,9 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "40008723",  # L266LB -> L266LA
             "40008724",  # L266LB -> L266LA
             "40008729",  # L266LB -> L267YE
-            "40075219",  # L266LB -> L266LA
             "40076082",  # L266LB -> L266LA
             "40076083",  # L266LB -> L266LA
             "40083391",  # L321BB -> L321BJ
-            "40021110",  # L346HF -> L346HB
             "40038051",  # L364HS -> L365ST
             "40076030",  # L365UZ -> L365UY
             "40042146",  # L365YR -> L365UY
@@ -34,8 +42,5 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "40056758",  # L368EL -> L368HW
         ]:
             rec["accept_suggestion"] = True
-
-        if uprn in []:
-            rec["accept_suggestion"] = False
 
         return rec
