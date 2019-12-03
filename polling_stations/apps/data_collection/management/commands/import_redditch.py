55da994e839cbc72a67192f3dc27d1b50a8f690e
Alex Dutton <alex@cottagelabs.com>
2019-12-03 15:36:03 +0000

    Import Redditch (closes #2421)

--- polling_stations/apps/data_collection/management/commands/import_redditch.py
+++ polling_stations/apps/data_collection/management/commands/import_redditch.py
@@ -4,8 +4,34 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E07000236"
     addresses_name = (
-        "parl.2017-06-08/Version 3/RBC UKPGE Democracy_Club__08June2017.tsv"
+        "parl.2019-12-12/Version 1/Democracy_Club__12December2019-Redditch.CSV"
     )
-    stations_name = "parl.2017-06-08/Version 3/RBC UKPGE Democracy_Club__08June2017.tsv"
-    elections = ["parl.2017-06-08"]
-    csv_delimiter = "\t"
+    stations_name = (
+        "parl.2019-12-12/Version 1/Democracy_Club__12December2019-Redditch.CSV"
+    )
+    elections = ["parl.2019-12-12"]
+    allow_station_point_from_postcode = False
+
+    def address_record_to_dict(self, record):
+        if record.addressline6 == "B96 6RN":
+            return None  # centroid outside authority
+
+        if (record.addressline1, record.addressline2) == (
+            "4A High Street",
+            "Feckenham",
+        ):
+            record = record._replace(property_urn="100121268977")
+
+        if record.addressline6 == "B98 9BH":
+            record = record._replace(addressline6="B98 9BJ")
+        if record.addressline6 == "B67 6QA":
+            record = record._replace(addressline6="B97 6QA")
+
+        rec = super().address_record_to_dict(record)
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn == "100120640565":
+            rec["postcode"] = "B98 9PJ"
+            rec["accept_suggestion"] = False
+
+        return rec
