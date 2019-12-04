a662e3947aaf6e70521197f7279f9f5d32d981e4
GeoWill <roper.william@gmail.com>
2019-12-04 13:46:08 +0000

    Import script for Sutton (closes #2454)

--- polling_stations/apps/data_collection/management/commands/import_sutton.py
+++ polling_stations/apps/data_collection/management/commands/import_sutton.py
@@ -5,12 +5,13 @@ from data_collection.management.commands import BaseDemocracyCountsCsvImporter
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "E09000029"
     addresses_name = (
-        "europarl.2019-05-23/Version 1/Democracy Club - Polling Districts.csv"
+        "parl.2019-12-12/Version 1/Polling Districts Sutton 12 December 2019.csv"
     )
     stations_name = (
-        "europarl.2019-05-23/Version 1/Democracy Club - Polling Stations.csv"
+        "parl.2019-12-12/Version 1/Polling Stations Sutton 12 December 2019.csv"
     )
-    elections = ["europarl.2019-05-23"]
+    elections = ["parl.2019-12-12"]
+    allow_station_point_from_postcode = False
 
     def station_record_to_dict(self, record):
 
@@ -22,3 +23,12 @@ class Command(BaseDemocracyCountsCsvImporter):
             return rec
 
         return super().station_record_to_dict(record)
+
+    def address_record_to_dict(self, record):
+        rec = super().address_record_to_dict(record)
+        uprn = record.uprn.strip().lstrip("0")
+
+        if uprn == "5870041842":
+            return None
+
+        return rec
