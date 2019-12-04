a8744c6d583a0da7f26be6e1ca144777f538e9ba
GeoWill <roper.william@gmail.com>
2019-12-04 11:30:44 +0000

    Import script for Lewes (closes #2450)

--- polling_stations/apps/data_collection/management/commands/import_lewes.py
+++ polling_stations/apps/data_collection/management/commands/import_lewes.py
@@ -3,9 +3,10 @@ from data_collection.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "E07000063"
-    addresses_name = "local.2019-05-02/Version 3/polling_station_export-2019-03-13.csv"
-    stations_name = "local.2019-05-02/Version 3/polling_station_export-2019-03-13.csv"
-    elections = ["local.2019-05-02"]
+    addresses_name = "parl.2019-12-12/Version 1/polling_station_export-2019-11-22.csv"
+    stations_name = "parl.2019-12-12/Version 1/polling_station_export-2019-11-22.csv"
+    elections = ["parl.2019-12-12"]
+    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
@@ -17,7 +18,7 @@ class Command(BaseHalaroseCsvImporter):
         ]:
             rec["accept_suggestion"] = False
 
-        if record.housepostcode == "RH17 7QH":
+        if record.housepostcode in ["RH17 7QH", "BN8 4AA"]:
             return None
 
         return rec
@@ -25,17 +26,17 @@ class Command(BaseHalaroseCsvImporter):
     def station_record_to_dict(self, record):
         rec = super().station_record_to_dict(record)
         if rec["internal_council_id"] in [
-            "62-alciston-and-selmeston-village-hall",
-            "69-alfriston-war-memorial-hall",
-            "63-berwick-village-hall",
-            "64-hayton-baker-hall",
-            "65-deanland-wood-social-centre",
-            "70-east-dean-village-hall",
-            "71-wilmington-village-hall",
-            "72-litlington-village-hall",
-            "66-polegate-community-centre",
-            "67-polegate-community-centre",
-            "68-st-georges-church-hall",
+            "38-alciston-and-selmeston-village-hall",
+            "45-alfriston-war-memorial-hall",
+            "39-berwick-village-hall",
+            "40-hayton-baker-hall",
+            "41-deanland-wood-social-centre",
+            "46-east-dean-village-hall",
+            "47-wilmington-village-hall",
+            "48-litlington-village-hall",
+            "42-polegate-community-centre",
+            "43-polegate-community-centre",
+            "44-st-georges-church-hall",
         ]:
             # all these have 0 properties assigned
             # and are in Wealden
