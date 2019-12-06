5be3dbc5a19eac518a088df592e1a43536182414
GeoWill <roper.william@gmail.com>
2019-12-06 11:18:02 +0000

    Import script for Tandridge (closes #2461)

--- polling_stations/apps/data_collection/management/commands/import_tandridge.py
+++ polling_stations/apps/data_collection/management/commands/import_tandridge.py
@@ -3,13 +3,10 @@ from data_collection.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "E07000215"
-    addresses_name = (
-        "local.2019-05-02/Version 1/polling_station_export-2019-02-28tandb.csv"
-    )
-    stations_name = (
-        "local.2019-05-02/Version 1/polling_station_export-2019-02-28tandb.csv"
-    )
-    elections = ["local.2019-05-02"]
+    addresses_name = "parl.2019-12-12/Version 1/polling_station_export-2019-11-26.csv"
+    stations_name = "parl.2019-12-12/Version 1/polling_station_export-2019-11-26.csv"
+    elections = ["parl.2019-12-12"]
+    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
@@ -18,8 +15,22 @@ class Command(BaseHalaroseCsvImporter):
             rec["accept_suggestion"] = True
         if record.houseid == "37412":
             rec["accept_suggestion"] = False
-
-        if record.houseid in ["8211", "8212"]:
-            rec["accept_suggestion"] = False
+        if record.housepostcode in ["CR8 5AT", "RH7 6BL"]:
+            return None
 
         return rec
+
+    def station_record_to_dict(self, record):
+
+        # Outside the LA area
+        if record.pollingstationnumber in [
+            "38",
+            "40",
+            "35",
+            "34",
+            "41",
+            "37",
+        ]:
+            return None
+
+        return super().station_record_to_dict(record)
