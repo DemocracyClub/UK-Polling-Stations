12cf4c6099ddc668e334c26193a8a51bbbeb1897
GeoWill <roper.william@gmail.com>
2019-12-06 10:14:35 +0000

    Import script for Bracknell Forest (closes #2459)

--- polling_stations/apps/data_collection/management/commands/import_bracknell_forest.py
+++ polling_stations/apps/data_collection/management/commands/import_bracknell_forest.py
@@ -1,11 +1,29 @@
 from data_collection.management.commands import BaseXpressDemocracyClubCsvImporter
+from django.contrib.gis.geos import Point
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E06000036"
-    addresses_name = "europarl.2019-05-23/Version 1/Democracy_Club__23May2019BF.tsv"
-
-    stations_name = "europarl.2019-05-23/Version 1/Democracy_Club__23May2019BF.tsv"
-
-    elections = ["europarl.2019-05-23"]
+    addresses_name = "parl.2019-12-12/Version 1/merged.tsv"
+    stations_name = "parl.2019-12-12/Version 1/merged.tsv"
+    elections = ["parl.2019-12-12"]
     csv_delimiter = "\t"
+    allow_station_point_from_postcode = False
+
+    def station_record_to_dict(self, record):
+
+        # Sandhurst School Sports Hall
+        if record.polling_place_id == "4381":
+            rec = super().station_record_to_dict(record)
+            rec["location"] = Point(-0.782442, 51.350179, srid=4326)
+            return rec
+
+        return super().station_record_to_dict(record)
+
+    def address_record_to_dict(self, record):
+        rec = super().address_record_to_dict(record)
+
+        if record.addressline6 == "RG40 3YZ":
+            return None
+
+        return rec
