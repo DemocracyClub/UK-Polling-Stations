c16dc5eb66a13741b4a0f2385c2ea4cce096d118
GeoWill <roper.william@gmail.com>
2019-12-06 12:07:00 +0000

    Import script for Cheshire West and Chester (closes #2474)

--- polling_stations/apps/data_collection/management/commands/import_cheshire_west.py
+++ polling_stations/apps/data_collection/management/commands/import_cheshire_west.py
@@ -3,15 +3,23 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E06000050"
-    addresses_name = "europarl.2019-05-23/Version 1/Democracy_Club__23May2019CWC.CSV"
-    stations_name = "europarl.2019-05-23/Version 1/Democracy_Club__23May2019CWC.CSV"
-    elections = ["europarl.2019-05-23"]
+    addresses_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019ches.tsv"
+    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019ches.tsv"
+    elections = ["parl.2019-12-12"]
+    csv_delimiter = "\t"
+    allow_station_point_from_postcode = False
 
     def station_record_to_dict(self, record):
 
         # "Ashton Hayes Parish Room" Correction brought forward from local.2019-05-02
-        if record.polling_place_id == "3673":
+        if record.polling_place_id == "5217":
             record = record._replace(polling_place_postcode="CH3 8BJ")
+
+        # based on misc fixes in commit: 972106
+        if record.polling_place_id == "5375":
+            rec = super().station_record_to_dict(record)
+            rec["location"] = None
+            return rec
 
         return super().station_record_to_dict(record)
 
@@ -21,8 +29,6 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
         if uprn in [
             "10013575202",  # CH661NZ -> CH661NT : Station House, Station Road, Little Sutton, Ellesmere Port, Cheshire
-            "100012372045",  # WA169JQ -> WA169JH : 1 Laburnum Cottages, Middlewich Road, Allostock, Knutsford, Cheshire
-            "100012372046",  # WA169JQ -> WA169JH : 2 Laburnum Cottages, Middlewich Road, Allostock, Knutsford, Cheshire
             "200003238279",  # CW82NQ -> CW72NQ : 3 Salterswall Cottages, Chester Road, Winsford, Cheshire
             "200003237975",  # CW60JQ -> CW69AU : Boot House Farm, Hall Lane, Rushton, Tarporley, Cheshire
             "100012807237",  # WA60AH -> WA60HS : 2 Meadow Farm Cottages, Lower Rake Lane, Helsby, Frodsham, Cheshire
@@ -30,7 +36,6 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             rec["accept_suggestion"] = True
 
         if uprn in [
-            "10091641303",  # WA169ND -> CH12NW : 2 The Barns, Booth Bed Lane, Allostock, Knutsford, Cheshire
             "200000832753",  # CH36EA -> CH36EE : The Bungalow,Oakfield Nurseries, Saighton Lane, Huntington, Chester, Cheshire
             "200000829308",  # CW60RP -> CH38BH : Weldon Farm, Dog Lane, Kelsall, Tarporley, Cheshire
             "100012353094",  # CW60HA -> CW60ET : The Woodlands, Duddon Heath, Duddon, Tarporley, Cheshire
