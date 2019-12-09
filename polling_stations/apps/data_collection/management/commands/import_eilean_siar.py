e19de6ec2240d0b35633668adf8428c7aabad206
GeoWill <roper.william@gmail.com>
2019-12-09 12:13:48 +0000

    Import script for Eilean Siar

--- polling_stations/apps/data_collection/management/commands/import_eilean_siar.py
+++ polling_stations/apps/data_collection/management/commands/import_eilean_siar.py
@@ -1,22 +1,14 @@
 from data_collection.management.commands import BaseScotlandSpatialHubImporter
 
-"""
-Note:
-This importer provides coverage for 45/47 districts
-due to incomplete/poor quality data
-"""
-
 
 class Command(BaseScotlandSpatialHubImporter):
     council_id = "S12000013"
     council_name = "Eilean Siar"
-    elections = ["europarl.2019-05-23"]
+    elections = ["parl.2019-12-12"]
 
     def station_record_to_dict(self, record):
-        record[0] = self.parse_string(record[0]).replace("O", "0")
 
-        # exclude duplicate district code
-        if record[0] == "E09E":
-            return None
-
-        return super().station_record_to_dict(record)
+        rec = super().station_record_to_dict(record)
+        if rec:
+            rec["internal_council_id"] = record[0].replace("EO", "E0")
+        return rec
