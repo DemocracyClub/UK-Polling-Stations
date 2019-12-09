0a5b09799ddb9fb74d274ec839b3e48555a15ff3
GeoWill <roper.william@gmail.com>
2019-12-09 17:36:51 +0000

    Import script for Scottish Borders

--- polling_stations/apps/data_collection/management/commands/import_scottish_borders.py
+++ polling_stations/apps/data_collection/management/commands/import_scottish_borders.py
@@ -4,4 +4,23 @@ from data_collection.management.commands import BaseScotlandSpatialHubImporter
 class Command(BaseScotlandSpatialHubImporter):
     council_id = "S12000026"
     council_name = "Scottish Borders"
-    elections = []
+    elections = ["parl.2019-12-12"]
+
+    def district_record_to_dict(self, record):
+
+        # Districts with no Station
+        if record[0] in ["03I", "08E", "11E"]:
+            return None
+
+        return super().district_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+
+        record[0] = record[0].zfill(3)
+        rec = super().station_record_to_dict(record)
+
+        if rec and record[0] == "09C":
+            rec["address"] = "ROXBURGH FORMER SCHOOL, ROXBURGH, KELSO, TD5 8LZ"
+            rec["location"] = None
+
+        return rec
