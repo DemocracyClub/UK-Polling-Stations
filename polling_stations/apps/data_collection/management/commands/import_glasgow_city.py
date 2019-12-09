85342d179e5b4478e41dd84526cbdcde0cf4ab4e
GeoWill <roper.william@gmail.com>
2019-12-09 15:38:09 +0000

    Import script for Glasgow City

--- polling_stations/apps/data_collection/management/commands/import_glasgow_city.py
+++ polling_stations/apps/data_collection/management/commands/import_glasgow_city.py
@@ -1,89 +1,70 @@
 from data_collection.management.commands import BaseScotlandSpatialHubImporter
 
-"""
-Note:
-This importer provides coverage for 245/258 districts
-due to incomplete/poor quality data
-"""
-
 
 class Command(BaseScotlandSpatialHubImporter):
-    council_id = "S12000046"
+    council_id = "S12000049"
     council_name = "Glasgow City"
-    elections = []
+    elections = ["parl.2019-12-12"]
 
     def district_record_to_dict(self, record):
-        """
-        There's been a boundary change, some datasets have caught up,
-        others haven't. Conveniently the 'transfer area' from Glagow to
-        North Lanarkshire corresponds neatly to polling district PR2021.
-        So for now we're throwing it away.
-        """
-        if record[0] == "PR2021":
+        # District that falls on Glasgow/N.Lanarkshire boundary change.
+        # Ignore it for now.
+
+        if record[0] == "NL43 (part)":
             return None
 
         return super().district_record_to_dict(record)
 
     def station_record_to_dict(self, record):
-        """
-        Throw away the polling station that corresponds to discarded
-        polling district as well.
-        """
-        if record[0] == "PR2021":
+
+        # District that falls on Glasgow/N.Lanarkshire boundary change.
+        # Ignore it for now.
+        if record[0] == "NL43":
             return None
 
-        # The following stations have a diagreement between the stations file and the districts file. So for now we'll throw the stations away.
+        # Doesn't appear in Districts shapefile
+        if record[0] == "ES0117":
+            return None
 
-        if (
-            record[0] == "AN3723"
-        ):  # Jordanhill Parish Church Hall, Munro Road (28 Woodend Drive), G13 1QT vs   KELVINDALE PRIMARY SCHOOL, 11 DORCHESTER PLACE, G12 0BP 55
-            return None
-        if (
-            record[0] == "CC1007"
-        ):  # Our Lady of the Annunciation Primary School, 80 Friarton Road, G43 2PR  vs NEWLANDS SOUTH PARISH CHURCH HALL, 37 RIVERSIDE ROAD, G43 2EG   55
-            return None
-        if (
-            record[0] == "KE2822"
-        ):  # St Mungo's Primary School, 45 Parson Street, G4 0RZ vs LADYWELL COMMUNITY HALL, 32A DRYGATE, G4 0YB    44
-            return None
-        if (
-            record[0] == "KE1416"
-        ):  # Dunard Street Primary School, 65 Dunard Street, G20 6RL vs MACKINTOSH CHURCH, 870 GARSCUBE ROAD, G20 7EL   44
-            return None
-        if (
-            record[0] == "KE1215"
-        ):  # Belhaven Nursery, 54 Kelvinside Avenue, G20 6PY vs KELBOURNE PARK PRIMARY SCHOOL, 109 HOTSPUR STREET, G20 8LH  42
-            return None
-        if (
-            record[0] == "MS1515"
-        ):  # Kelvindale Primary School, 11 Dorchester Place, G12 0BP vs ST MARY'S (MARYHILL) PRIMARY SCHOOL, 2 KILMUN STREET, G20 0EL   38
-            return None
-        if (
-            record[0] == "CC0407"
-        ):  # City Building Depot, 32 Greenholme Street, G44 4DU vs  CATHCART CONGREGATIONAL CHURCH HALL, 15 GARRY STREET, G44 4AY   38
-            return None
-        if (
-            record[0] == "PR3717"
-        ):  # St Philomena's Primary School, 21 Robroyston Road, G33 1EA vs  ALL SAINTS SECONDARY SCHOOL, 299 RYEHILL ROAD, G21 3EN  37
-            return None
-        if (
-            record[0] == "MS4416"
-        ):  # St Blane's Primary School, 23 Arrcoher Drive, G23 5QB vs   CADDER PRIMARY SCHOOL, 60 HERMA STREET, G23 5AR 34
-            return None
-        if (
-            record[0] == "MS2016"
-        ):  # St Blane's Primary School, 23 Arrocher Drive, G23 5QB vs   CADDER PRIMARY SCHOOL, 60 HERMA STREET, G23 5AR 34
-            return None
-        if (
-            record[0] == "KE2311"
-        ):  # Gaelic School, 147 Berkeley Street, G3 7HP vs  HILLHEAD PRIMARY SCHOOL, 110 OTAGO STREET, G12 8NS  29
-            return None
-        if (
-            record[0] == "PK1703"
-        ):  # Ashpark Primary School, 75 Kyleakin Road, G46 8DQ vs   DARNLEY PRIMARY SCHOOL, 169 GLEN MORRISTON ROAD, G53 7HT    26
-            return None
-        if (
-            record[0] == "PR3517"
-        ):  # All Saints Primary School, 299 Ryehill Road, G21 3EN vs    ALL SAINTS SECONDARY SCHOOL, 299 RYEHILL ROAD, G21 3EN  6
-            return None
-        return super().station_record_to_dict(record)
+        # Corrections based on checking notice of polling place
+        addresses = {
+            "KE0310": "GARNETBANK PRIMARY SCHOOL, 231 RENFREW STREET",
+            "SH1609": "SACRED HEART PRIMARY SCHOOL, 31 REID STREET",
+            "SS0706": "ST ALBERT'S PRIMARY SCHOOL, 34 MAXWELL DRIVE",
+            "SS1508": "ST BRIDE'S PRIMARY SCHOOL, 85 CRAIGIE STREET",
+            "SS1607": "ST BRIGID'S PRIMARY SCHOOL, 4 GLENMORE AVENUE",
+            "SS1908": "BLACKFRIARS PRIMARY SCHOOL, 310 CUMBERLAND STREET",
+            "PR0318": "BUDHILL FAMILY LEARNING CENTRE PRE 5 CE CENTRE, 1A HALLHILL ROAD",
+            "PR2621": "BLAIRTUMMOCK HOUSING ASSOCIATION, COMMUNITY HALL, 45 BOYNDIE ROAD",
+            "SH0620": "CALEDONIA PRIMARY SCHOOL, 10 CALDERWOOD DRIVE",
+            "SH0720": "CALEDONIA PRIMARY SCHOOL, 10 CALDERWOOD DRIVE",
+            "SH3119": "EASTBANK ACADEMY, 26 ACADEMY STREET",
+            "SH3319": "ST JOSEPH'S CHURCH HALL, 16 FULLARTON AVENUE",
+            "KE1311": "BELHAVEN NURSERY, 54 KELVINSIDE AVENUE",
+            "MS0516": "SCARAWAY NURSERY, 24 SHAPINSAY STREET",
+            "MS3817": "SPRINGBURN LIBRARY, 10 KAY STREET",
+            "PR3121": "RUCHAZIE COMMUNITY CENTRE, 441 GARTLOCH ROAD",
+            "AN0114": "ANTONINE PRIMARY SCHOOL, 3 ABBOTSHALL AVENUE",
+            "AN0814": "ST MARGARET'S PARISH CHURCH HALL, 2000 GREAT WESTERN ROAD",
+            "AN2613": "SCOTSTOUN PRIMARY SCHOOL, 26 ORMISTON AVENUE, G14 9HN",
+            "AN3312": "BROOMHILL COMMUNITY CHURCH OF THE NAZARENE, 95 BROOMHILL DRIVE (ENTER FROM NORBY ROAD)",
+            "KE2523": "HYNDLAND PRIMARY SCHOOL, 36 FORTROSE STREET",
+            "PK0506": "CRAIGTON PRIMARY SCHOOL, 5 MORVEN STREET",
+            "PK0803": "LINTHAUGH NURSERY SCHOOL, 533 CROOKSTON ROAD",
+            "PK0903": "LINTHAUGH NURSERY SCHOOL, 533 CROOKSTON ROAD",
+            "PK1403": "LINTHAUGH NURSERY SCHOOL, 533 CROOKSTON ROAD",
+            "PK2504": "LOURDES PRIMARY SCHOOL, 144 BERRYKNOWES ROAD",
+            "PK2604": "LOURDES PRIMARY SCHOOL, 144 BERRYKNOWES ROAD",
+            "PK3304": "NETHERCRAIGS (GLASGOW CLUB), 355 CORKERHILL ROAD",
+            "SS0205": "IBROX PRIMARY SCHOOL, 40 HINSHELWOOD DRIVE",
+        }
+
+        rec = super().station_record_to_dict(record)
+
+        if rec:
+            try:
+                rec["address"] = addresses[rec["internal_council_id"]]
+                rec["location"] = None
+                return rec
+            except KeyError:
+                return rec
