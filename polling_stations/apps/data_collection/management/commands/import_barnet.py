c5ff081489c8c499c9fdf76c69dd965bc01ab812
chris48s <chris.shaw480@gmail.com>
2019-12-10 13:08:35 +0000

    import Barnet (closes #2416)

--- polling_stations/apps/data_collection/management/commands/import_barnet.py
+++ polling_stations/apps/data_collection/management/commands/import_barnet.py
@@ -4,18 +4,28 @@ from data_collection.management.commands import BaseDemocracyCountsCsvImporter
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "E09000003"
     addresses_name = (
-        "europarl.2019-05-23/Version 1/Decmoracy Club - Polling Districts.csv"
+        "parl.2019-12-12/Version 2/Democracy Club - Polling Districts_UKPGE.csv"
     )
-    stations_name = (
-        "europarl.2019-05-23/Version 1/Democracy Club - Polling Stations.csv"
-    )
-    elections = ["europarl.2019-05-23"]
+    stations_name = "parl.2019-12-12/Version 2/Democracy Club - Polling Stations_2.csv"
+    elections = ["parl.2019-12-12"]
+    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
         uprn = record.uprn.strip().lstrip("0")
 
         if uprn == "200196535":
-            rec["accept_suggestion"] = False
+            return None
+
+        if record.postcode.strip() in [
+            "N3 1QT",
+            "NW2 2NA",
+            "N11 3DD",
+            "EN4 8QZ",
+            "EN4 8TF",
+            "N12 7FB",
+            "N12 7EJ",
+        ]:
+            return None
 
         return rec
