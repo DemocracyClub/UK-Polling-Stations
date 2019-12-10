6cffd71cc62dbed2fb869d8f9862303b0d08eb66
GeoWill <roper.william@gmail.com>
2019-12-10 17:11:59 +0000

    Import script Argyll and Bute

--- polling_stations/apps/data_collection/management/commands/import_argyll_and_bute.py
+++ polling_stations/apps/data_collection/management/commands/import_argyll_and_bute.py
@@ -4,7 +4,7 @@ from data_collection.management.commands import BaseScotlandSpatialHubImporter
 class Command(BaseScotlandSpatialHubImporter):
     council_id = "S12000035"
     council_name = "Argyll and Bute"
-    elections = ["europarl.2019-05-23"]
+    elections = ["parl.2019-12-12"]
 
     def station_record_to_dict(self, record):
         rec = super().station_record_to_dict(record)
@@ -17,23 +17,9 @@ class Command(BaseScotlandSpatialHubImporter):
                     "postcode": rec["postcode"],
                     "address": rec["address"],
                 }
-                stations.append(new_rec)
 
-                # The following two stations have two districts voting at them.
-                # There are separate codes in the district shapfile so this creates them
-                if code == "AA83":
-                    hunters_quay = {
-                        "internal_council_id": "AA83A",
-                        "postcode": rec["postcode"],
-                        "address": rec["address"],
-                    }
-                    stations.append(hunters_quay)
-                if code.strip() == "AA06":
-                    bellochantuy_kilchenzie = {
-                        "internal_council_id": "AA07",
-                        "postcode": rec["postcode"],
-                        "address": rec["address"],
-                    }
-                    stations.append(bellochantuy_kilchenzie)
+                if code == "AA89":
+                    new_rec["address"] = new_rec["address"].replace("?", ",")
+                stations.append(new_rec)
             return stations
         return rec
