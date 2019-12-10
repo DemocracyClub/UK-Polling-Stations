3d5f1c528753095f33a6e132f66afc33789aea13
GeoWill <roper.william@gmail.com>
2019-12-10 18:25:37 +0000

    Import script for East Ayrshire

--- polling_stations/apps/data_collection/management/commands/import_east_ayrshire.py
+++ polling_stations/apps/data_collection/management/commands/import_east_ayrshire.py
@@ -1,19 +1,54 @@
 from data_collection.management.commands import BaseScotlandSpatialHubImporter
 
-"""
-Note:
-This importer provides coverage for 66/81 districts
-due to incomplete/poor quality data
-"""
-
 
 class Command(BaseScotlandSpatialHubImporter):
     council_id = "S12000008"
     council_name = "East Ayrshire"
-    elections = ["local.east-ayrshire.2017-05-04", "parl.2017-06-08"]
+    elections = ["parl.2019-12-12"]
 
     def station_record_to_dict(self, record):
-        # exclude duplicate district code
-        if str(record[1]).strip() == "E706":
-            return None
-        return super().station_record_to_dict(record)
+        def duplicate_records(rec, codes, stations):
+            for code in codes:
+                stations.append(
+                    {
+                        "internal_council_id": code,
+                        "postcode": rec["postcode"],
+                        "address": rec["address"],
+                    }
+                )
+            return stations
+
+        rec = super().station_record_to_dict(record)
+        if rec:
+            stations = [rec]
+
+            # Updates from SoPP review.
+            if rec["internal_council_id"] == "E104":
+                stations = duplicate_records(rec, ["E106", "E107"], stations)
+            if rec["internal_council_id"] == "E110":
+                stations = duplicate_records(rec, ["E606"], stations)
+            if rec["internal_council_id"] == "E201":
+                stations = duplicate_records(rec, ["E205"], stations)
+            if rec["internal_council_id"] == "E203":
+                stations = duplicate_records(rec, ["E204"], stations)
+            if rec["internal_council_id"] == "E304":
+                stations = duplicate_records(rec, ["E306"], stations)
+            if rec["internal_council_id"] == "E305":
+                stations = duplicate_records(rec, ["E309"], stations)
+            if rec["internal_council_id"] == "E307":
+                stations = duplicate_records(rec, ["E310"], stations)
+            if rec["internal_council_id"] == "E313":
+                stations = duplicate_records(rec, ["E401"], stations)
+            if rec["internal_council_id"] == "E411":
+                stations = duplicate_records(rec, ["E403", "E404", "E405"], stations)
+            if rec["internal_council_id"] == "E603":
+                stations = duplicate_records(rec, ["E607"], stations)
+            if rec["internal_council_id"] == "E701":
+                stations = duplicate_records(rec, ["E710"], stations)
+            if rec["internal_council_id"] == "E704":
+                stations = duplicate_records(rec, ["E807"], stations)
+            if rec["internal_council_id"] == "E708":
+                stations = duplicate_records(rec, ["E808"], stations)
+
+            return stations
+        return rec
