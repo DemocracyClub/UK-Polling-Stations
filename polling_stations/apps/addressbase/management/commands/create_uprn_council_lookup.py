6f3392c51d00f87f954755a17ed063ff9b1a5a36
GeoWill <roper.william@gmail.com>
2020-02-14 15:41:00 +0000

    A command to make a csv of UPRNs and councils
    The purpose of this is to create a file which can be used to build a
    table of the same. At the moment we use addressbase_onsud for this.
    We want to get rid of addressbase_onsud because it introduces one more
    source of uprns and addresses, this:
      - makes reasoning about issues when writing import scripts harder
      - means we don't ignore UPRNs in addressbase that are missing from
        ONSUD.
    
    We're doing this dance with outputting a file to be imported and then
    importing it into a different table to avoid having to rewrite to much,
    as that's what we already do but with ONSUD. It's worth noting the
    the reason we don't update the addressbase_address table with UPRNs is
    it's really slow!

---
+++ polling_stations/apps/addressbase/management/commands/create_uprn_council_lookup.py
@@ -0,0 +1,76 @@
+from django.core.management.base import BaseCommand
+from django.db import connection
+from pathlib import Path
+
+
+class Command(BaseCommand):
+    """
+    This creates a lookup csv of uprn and council GSS codes.
+    This can then be imported using 'import_uprn_council_lookup'.
+    """
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "-d", "--destination", help="Path to write csv to", default=None
+        )
+
+    def handle(self, *args, **kwargs):
+
+        self.cursor = connection.cursor()
+        # Set where we'll write the join query to.
+        if kwargs["destination"]:
+            self.dst = Path(kwargs["destination"]).open("w")
+        else:
+            self.dst = Path("./uprn-to-councils.csv").open("w")
+
+        # Build the temporary subdivided council polygon table
+        # See http://blog.cleverelephant.ca/2019/11/subdivide.html for why we're doing this.
+        # tl;dr point in polygon lookups are super fast on small geometries.
+        self.stdout.write("Creating subdivided councils table...")
+        self.cursor.execute(
+            """
+            DROP TABLE IF EXISTS councils_council_subdivided;
+            """
+        )
+        self.cursor.execute(
+            """
+            CREATE TABLE councils_council_subdivided AS
+            SELECT council_id, st_subdivide(area) AS geom
+            FROM councils_council;
+            """
+        )
+
+        # spatial join between councils_council_subdivided and addressbase
+        # & dump out a CSV file
+        self.stdout.write("Joining addresses to councils...")
+        self.cursor.copy_expert(
+            """
+            COPY (SELECT
+                    a.uprn as uprn,
+                    c.council_id as lad
+                FROM
+                    addressbase_address a 
+                    JOIN 
+                    councils_council_subdivided c
+                    ON
+                    ST_Covers(c.geom, a.location)
+                    ) 
+                TO STDOUT
+                
+            with CSV HEADER DELIMITER ',';
+            """,
+            self.dst,
+        )
+        self.stdout.write(f"Output written to: {self.dst.name}")
+
+        # Drop councils_council_subdivided
+        self.stdout.write("Dropping subdivided councils table...")
+        self.cursor.execute(
+            """
+            DROP TABLE councils_council_subdivided;
+            """
+        )
+
+        self.stdout.write(
+            f"To import this data run: python manage.py import_uprn_council_lookup {self.dst.name}"
+        )
