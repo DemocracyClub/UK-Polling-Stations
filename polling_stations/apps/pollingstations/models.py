46c23aa937cde2984af2159f152af3cb249a1e8b
GeoWill <roper.william@gmail.com>
2020-02-19 12:55:07 +0000

    Remove 'eer' as lookup for the CustomFinderManager
    Using ONSUD allowed us to route requests to custom PSFs based on any
    GSS code. Now the look-up has to be by Council. Which means there
    should be a CustomFinder for each local authority.

--- polling_stations/apps/pollingstations/models.py
+++ polling_stations/apps/pollingstations/models.py
@@ -141,9 +141,7 @@ class ResidentialAddress(models.Model):
 class CustomFinderManager(models.Manager):
     def get_custom_finder(self, geocoder, postcode):
         try:
-            finder = self.get(
-                pk__in=[geocoder.get_code("lad"), geocoder.get_code("eer")]
-            )
+            finder = self.get(pk__in=[geocoder.get_code("lad")])
             finder.message = _(finder.message)
             """
             EONI's poling station finder requires postcode to have a space :(
