e5c74a8274ca86fa11ee3229d18f42f9ed2d1778
chris48s <chris.shaw480@gmail.com>
2020-02-25 16:57:46 +0000

    update BOUNDARIES_URL
    https://opendata.arcgis.com/datasets/2b95585accc4437b97d766f31c5568cb_0.geojson
    has been throwing an error for ages
    and I've given up on ONS support

--- polling_stations/settings/constants/councils.py
+++ polling_stations/settings/constants/councils.py
@@ -1,9 +1,7 @@
 # settings for councils scraper
 
 YVM_LA_URL = "https://www.yourvotematters.co.uk/_design/nested-content/results-page2/search-voting-locations-by-districtcode?queries_distcode_query="  # noqa
-BOUNDARIES_URL = (
-    "https://opendata.arcgis.com/datasets/2b95585accc4437b97d766f31c5568cb_0.geojson"
-)
+BOUNDARIES_URL = "https://ons-cache.s3.amazonaws.com/Local_Authority_Districts_April_2019_Boundaries_UK_BFE.geojson"
 
 
 # The data we need won't all be
