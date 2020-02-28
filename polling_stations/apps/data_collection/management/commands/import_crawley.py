0105fe317dd3ba1a22c041cacf3b0f4abf6374c9
GeoWill <roper.william@gmail.com>
2020-02-28 16:03:00 +0000

    Import script for Crawley (closes #2563)

--- polling_stations/apps/data_collection/management/commands/import_crawley.py
+++ polling_stations/apps/data_collection/management/commands/import_crawley.py
@@ -3,22 +3,17 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E07000226"
-    addresses_name = (
-        "parl.2019-12-12/Version 1/Democracy_Club__12December2019crawley.tsv"
-    )
-    stations_name = (
-        "parl.2019-12-12/Version 1/Democracy_Club__12December2019crawley.tsv"
-    )
-    elections = ["parl.2019-12-12/Version 1"]
+    addresses_name = "2020-02-04T10:49:54.706223/Democracy_Club__07May2020craw.tsv"
+    stations_name = "2020-02-04T10:49:54.706223/Democracy_Club__07May2020craw.tsv"
+    elections = ["2020-05-07"]
     csv_delimiter = "\t"
-    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
-        if record.addressline6 == "RH10 3HW":
-            rec["postcode"] = "RH10 3GW"
+        if record.addressline1 == "7B Crawley Foyer":
+            rec["postcode"] == "RH11 7AU"
 
         if uprn in [
             "10024122201"  # RH110EA -> RH110AE : 50A Ifield Drive, Ifield, Crawley
@@ -30,15 +25,18 @@ class Command(BaseXpressDemocracyClubCsvImporter):
     def get_station_point(self, record):
 
         # Furnace Green Community Centre
-        if record.polling_place_id == "774":
-            return None
+        if record.polling_place_id == "910":
+            record = record._replace(polling_place_easting="528414")
+            record = record._replace(polling_place_northing="135782")
 
         # Wakehams Green Community Centre
-        if record.polling_place_id == "789":
-            return None
+        if record.polling_place_id == "892":
+            record = record._replace(polling_place_easting="529968")
+            record = record._replace(polling_place_northing="138172")
 
         # Southgate West Community Centre
-        if record.polling_place_id == "795":
-            return None
+        if record.polling_place_id == "898":
+            record = record._replace(polling_place_easting="526307")
+            record = record._replace(polling_place_northing="135610")
 
         return super().get_station_point(record)
