9a1b923fa90080df13dac806c28e8dd0bf629ceb
GeoWill <roper.william@gmail.com>
2020-02-28 12:10:44 +0000

    Import script for Dacorum (closes #2559)

--- polling_stations/apps/data_collection/management/commands/import_dacorum.py
+++ polling_stations/apps/data_collection/management/commands/import_dacorum.py
@@ -3,22 +3,24 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E07000096"
-    addresses_name = (
-        "parl.2019-12-12/Version 2/Democracy_Club__12December2019dacorum.CSV"
-    )
-    stations_name = (
-        "parl.2019-12-12/Version 2/Democracy_Club__12December2019dacorum.CSV"
-    )
-    elections = ["parl.2019-12-12"]
+    addresses_name = "2020-02-03T10:27:29.701109/Democracy_Club__07May2020Dacorum.CSV"
+    stations_name = "2020-02-03T10:27:29.701109/Democracy_Club__07May2020Dacorum.CSV"
+    elections = ["2020-05-07"]
     csv_delimiter = ","
-    allow_station_point_from_postcode = False
+
+    def station_record_to_dict(self, record):
+
+        if record.polling_place_id == "1297":
+            record = record._replace(polling_place_easting="507211")
+            record = record._replace(polling_place_northing="204366")
+
+        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
 
         if record.addressline6 in [
             "AL3 8LR",
-            "HP4 2RS",
         ]:
             return None
 
