41b8eec807abc132ff49b87093022f8ce9d55e3c
GeoWill <roper.william@gmail.com>
2020-03-12 15:59:36 +0000

    Import script for Gosport (closes #2608)

--- polling_stations/apps/data_collection/management/commands/import_gosport.py
+++ polling_stations/apps/data_collection/management/commands/import_gosport.py
@@ -3,19 +3,17 @@ from data_collection.management.commands import BaseXpressDemocracyClubCsvImport
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "E07000088"
-    addresses_name = (
-        "parl.2019-12-12/Version 1/2019 PGE - Democracy_Club__12December2019gosport.TSV"
-    )
-    stations_name = (
-        "parl.2019-12-12/Version 1/2019 PGE - Democracy_Club__12December2019gosport.TSV"
-    )
-    elections = ["parl.2019-12-12"]
+    addresses_name = "2020-02-24T14:38:24.107761/2020 LGE & PCC - Borough of Gosport Polling Stations for Democracy_Club__07May2020.tsv"
+    stations_name = "2020-02-24T14:38:24.107761/2020 LGE & PCC - Borough of Gosport Polling Stations for Democracy_Club__07May2020.tsv"
+    elections = ["2020-05-07"]
     csv_delimiter = "\t"
-    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in ["37042423", "37043527"]:
+            return None
 
         if uprn in [
             "37013642",  # PO122BY -> PO123BY : Duncan Block, HMS Sultan, Military Road, Gosport, Hampshire
@@ -44,6 +42,7 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "37013642",  # PO122BY -> PO123BY : Mountbatten Block, HMS Sultan, Military Road, Gosport, Hampshire
             "37042796",  # PO122BY -> PO123BG : The Wardroom, HMS Sultan, Military Road, Gosport, Hampshire
             "37013642",  # PO122BY -> PO123BY : Lefanu Block, HMS Sultan, Military Road, Gosport, Hampshire
+            "37042495",  # PO122AB -> PO121HJ : Dolphin House, Officers Mess Fort Blockhouse, Haslar Road, Gosport, Hampshire
         ]:
             rec["accept_suggestion"] = True
 
