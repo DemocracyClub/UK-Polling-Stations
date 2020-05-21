6596b538d074e5188016ade32f39bfca21ac8101
GeoWill <roper.william@gmail.com>
2020-05-21 11:58:34 +0100

    Add a teardown command to data_importers app
    This is quick if there aren't too many polling station ids in the
    uprntocouncil table. If it get's slower the fuller it gets. This
    shouldn't be a problem when writing import scripts as long as one
    runs teardown fairly regularly.

--- polling_stations/apps/data_importers/management/commands/teardown.py
+++ polling_stations/apps/data_importers/management/commands/teardown.py
@@ -1,12 +1,15 @@
 from django.apps import apps
 from django.core.management.base import BaseCommand
 from django.db import connection
+
+from addressbase.models import UprnToCouncil
 from councils.models import Council
-from data_collection.models import DataQuality
-from pollingstations.models import PollingStation, PollingDistrict, ResidentialAddress
+from data_importers.models import DataQuality
+from pollingstations.models import PollingStation, PollingDistrict
 
 """
-Clear PollingDistrict, PollingStation and ResidentialAddress models
+Clear PollingDistrict and PollingStation models
+Clear polling_station_id field in UprnToCouncil model
 Clear report, num_addresses, num_districts and num_stations
 fields in DataQuality model
 """
@@ -43,26 +46,27 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         """
         Manually run system checks for the
-        'data_collection' and 'pollingstations' apps
+        'data_importers' and 'pollingstations' apps
         Management commands can ignore checks that only apply to
         the apps supporting the website part of the project
         """
         self.check(
             [
-                apps.get_app_config("data_collection"),
+                apps.get_app_config("data_importers"),
                 apps.get_app_config("pollingstations"),
             ]
         )
 
         if kwargs["council"]:
-            council_id = kwargs["council"][0]
+            council_id = kwargs["council"]
             print("Deleting data for council %s..." % (council_id))
             # check this council exists
             Council.objects.get(pk=council_id)
 
             PollingStation.objects.filter(council=council_id).delete()
             PollingDistrict.objects.filter(council=council_id).delete()
-            ResidentialAddress.objects.filter(council=council_id).delete()
+
+            UprnToCouncil.objects.filter(lad=council_id).update(polling_station_id="")
 
             dq = DataQuality.objects.get(council_id=council_id)
             dq.report = ""
@@ -76,10 +80,13 @@ class Command(BaseCommand):
             print("Deleting ALL data...")
             PollingDistrict.objects.all().delete()
             PollingStation.objects.all().delete()
-            ResidentialAddress.objects.all().delete()
+
             # use raw SQL so we don't have to loop over every single record one-by-one
             cursor = connection.cursor()
             cursor.execute(
-                "UPDATE data_collection_dataquality SET report='', num_addresses=0, num_districts=0, num_stations=0"
+                "UPDATE addressbase_uprntocouncil SET polling_station_id='' WHERE polling_station_id != ''"
+            )
+            cursor.execute(
+                "UPDATE data_importers_dataquality SET report='', num_addresses=0, num_districts=0, num_stations=0"
             )
             print("..done")
