dcbcd7434b8b4199242a479d187d2b833ca6ffcc
Sym Roe <sym.roe@talusdesign.co.uk>
2020-08-09 15:15:12 +0100

    Set the EC API URL

--- polling_stations/settings/constants/councils.py
+++ polling_stations/settings/constants/councils.py
@@ -2,7 +2,9 @@
 
 YVM_LA_URL = "https://www.yourvotematters.co.uk/_design/nested-content/results-page2/search-voting-locations-by-districtcode?queries_distcode_query="  # noqa
 BOUNDARIES_URL = "https://ons-cache.s3.amazonaws.com/Local_Authority_Districts_April_2019_Boundaries_UK_BFE.geojson"
-EC_COUNCIL_CONTACT_DETAILS_API_URL = ""
+EC_COUNCIL_CONTACT_DETAILS_API_URL = (
+    "https://electoralcommission.org.uk/api/v1/data/local-authorities.json"
+)
 
 
 OLD_TO_NEW_MAP = {}
