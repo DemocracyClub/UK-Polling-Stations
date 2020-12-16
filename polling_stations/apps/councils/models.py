fce83ce3a6c0b7c22ea517c3cc1c0096bc989f48
GeoWill <roper.william@gmail.com>
2020-12-16 17:49:15 +0000

    Add nation property to council model
    Also fix up reg/gss code issues in test fixtures.

--- polling_stations/apps/councils/models.py
+++ polling_stations/apps/councils/models.py
@@ -1,3 +1,5 @@
+import re
+
 from django.contrib.gis.db import models
 from django.contrib.postgres.fields import ArrayField
 
@@ -34,3 +36,50 @@ class Council(models.Model):
 
     class Meta:
         ordering = ("name",)
+
+    @property
+    def nation(self):
+        nations_lookup = {
+            "E": "England",
+            "W": "Wales",
+            "S": "Scotland",
+            "N": "Northern Ireland",
+        }
+        # A GSS code is:
+        #   'ANN' + 'NNNNNN' where 'A' is one of 'ENSW' and 'N' is 0-9.
+        #   'ANN' section is the Entity Code.
+        # We want to look for identifiers that look like GSS codes
+        # for the types of organisations that manage elections.
+
+        # Ref: https://geoportal.statistics.gov.uk/search?collection=Dataset&sort=-created&tags=all(PRD_RGC)
+        gss_pattern = re.compile(
+            """
+            ^        # Start of string
+            (        # Entity Codes:
+                E06   # Unitary Authorities (England)
+              | E07   # Non-metropolitan Districts (England)
+              | E08   # Metropolitan Districts (England)
+              | E09   # London Boroughs (England)
+              | N09   # Local Government Districts (Northern Ireland)
+              | S12   # Council Areas (Scotland)
+              | W06   # Unitary Authorities (Wales)
+            )
+            [0-9]{6} # id
+            $        # End of string
+            """,
+            re.VERBOSE,
+        )
+        identifier_matches = [
+            identifier
+            for identifier in self.identifiers
+            if re.match(gss_pattern, identifier)
+        ]
+        identifier_nations = set(
+            nations_lookup[identifier[0]]
+            for identifier in identifier_matches
+            if identifier
+        )
+        if len(identifier_nations) == 1:
+            return identifier_nations.pop()
+        else:
+            return ""
