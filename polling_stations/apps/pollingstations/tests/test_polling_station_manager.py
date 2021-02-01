db903e7cfff8318315828f4080e581c10aeaa2c5
GeoWill <roper.william@gmail.com>
2021-02-01 21:07:49 +0000

    Fix pollingstations tests

--- polling_stations/apps/pollingstations/tests/test_polling_station_manager.py
+++ polling_stations/apps/pollingstations/tests/test_polling_station_manager.py
@@ -1,9 +1,31 @@
+from django.core.management import call_command
 from django.test import TestCase
+
+from councils.tests.factories import CouncilFactory
 from pollingstations.models import PollingStation
 
 
 class PollingStationsStationIdTest(TestCase):
-    fixtures = ["test_polling_stations_station_id.json"]
+    @classmethod
+    def setUpTestData(cls):
+        CouncilFactory(
+            council_id="ABC",
+            name="ABC council",
+            identifiers=["X01000001"],
+            geography__geography=None,
+        )
+        CouncilFactory(
+            council_id="DEF",
+            name="DEF council",
+            identifiers=["X01000002"],
+            geography__geography=None,
+        )
+
+        call_command(  # Hack to avoid converting all fixtures to factories
+            "loaddata",
+            "polling_stations/apps/pollingstations/fixtures/test_polling_stations_station_id.json",
+            verbosity=0,
+        )
 
     def test_formatted_address(self):
         station = PollingStation.objects.get(pk=1)
