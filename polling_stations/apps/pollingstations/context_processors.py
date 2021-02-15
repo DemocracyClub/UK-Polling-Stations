eaaa1a23eadb0cb1844222fbacbc099b7bf869a6
GeoWill <roper.william@gmail.com>
2021-02-15 16:57:52 +0000

    Include election date in html headers if available

--- polling_stations/apps/pollingstations/context_processors.py
+++ polling_stations/apps/pollingstations/context_processors.py
@@ -1,12 +1,24 @@
 from django.conf import settings
+from django.utils.datetime_safe import datetime
 
 
 def google_analytics(request):
     return {"disable_ga": getattr(settings, "DISABLE_GA", False)}
 
 
+NEXT_CHARISMATIC_ELECTION_DATE = getattr(
+    settings, "NEXT_CHARISMATIC_ELECTION_DATE", None
+)
+NEXT_CHARISMATIC_ELECTION_DATE = (
+    datetime.strptime(NEXT_CHARISMATIC_ELECTION_DATE, "%Y-%m-%d")
+    if NEXT_CHARISMATIC_ELECTION_DATE
+    else None
+)
+
+
 def global_settings(request):
     return {
         "RAVEN_CONFIG": getattr(settings, "RAVEN_CONFIG", None),
         "SERVER_ENVIRONMENT": getattr(settings, "SERVER_ENVIRONMENT", None),
+        "NEXT_CHARISMATIC_ELECTION_DATE": NEXT_CHARISMATIC_ELECTION_DATE,
     }
