9c07b57fefd5392ce2185f0d7169901a0d686da3
GeoWill <roper.william@gmail.com>
2021-02-24 13:24:08 +0000

    Check for split postcodes and warn.
    The probable course of action here will be to remove any records with
    those postcodes in the 'address_record_to_dict' method.

--- polling_stations/apps/data_importers/tests/test_address_list.py
+++ polling_stations/apps/data_importers/tests/test_address_list.py
@@ -1,10 +1,17 @@
 from django.test import TestCase
+
+from addressbase.tests.factories import AddressFactory
 from data_importers.data_types import AddressList
 
 
 class MockLogger:
+    logs = []
+
     def log_message(self, level, message, variable=None, pretty=False):
-        pass
+        self.logs.append(message)
+
+    def clear_logs(self):
+        self.logs = []
 
 
 class AddressListTest(TestCase):
@@ -395,6 +402,151 @@ class AddressListTest(TestCase):
             address_list.append(el)
         expected = ["AA11AA"]
         self.assertListEqual(expected, address_list.get_council_split_postcodes())
+
+    def test_remove_records_missing_uprns(self):
+        in_list = [
+            {
+                "address": "foo",
+                "postcode": "AA11AA",
+                "council": "AAA",
+                "polling_station_id": "01",
+                "uprn": "1",
+            },
+            {  # Doesn't need a uprn - this should be added
+                "address": "bar",
+                "postcode": "AA11AA",
+                "council": "AAA",
+                "polling_station_id": "01",
+                "uprn": "",
+            },
+            {  # Does need a postcode - this shouldn't
+                "address": "baz",
+                "postcode": "",
+                "council": "AAA",
+                "polling_station_id": "01",
+                "uprn": "1",
+            },
+        ]
+        expected = [
+            {
+                "address": "foo",
+                "postcode": "AA11AA",
+                "council": "AAA",
+                "polling_station_id": "01",
+                "uprn": "1",
+            },
+        ]
+        address_list = AddressList(MockLogger())
+        for el in in_list:
+            address_list.append(el)
+        address_list.remove_records_missing_uprns()
+        self.assertEqual(expected, address_list.elements)
+
+    def test_check_split_postcodes_are_split(self):
+        """
+        AddressBase                      | Council Data
+                                         |
+        uprn | Address        | Postcode | uprn | Address       | Postcode | Station
+        ---------------------------------|------------------------------------------
+        123  | 1 Foo Street   | AA11AA   | 123  | 1 Foo Street  | AA11AA   | A1
+        124  | 2 Foo Street   | AA11AA   | 124  | 2 Foo Street  | AA11AA   | A1
+        125  | 3 Foo Street   | AA11AA   | 125  | 3 Foo Street  | AA22AA   | A2
+        ---------------------------------|------------------------------------------
+        223  | 1 Bar Street   | BB11BB   | 223  | 1 Bar Street  | BB11BB   | B1
+        224  | 2 Bar Street   | BB11BB   | 224  | 2 Bar Street  | BB11BB   | B1
+                                         | 225  | 3 Bar Street  | BB11BB   | B2
+        ---------------------------------|------------------------------------------
+        323  | 1 Baz Street   | CC11CC   | 323  | 1 Baz Street  | CC11CC   | C1
+        324  | 2 Baz Street   | CC11CC   | 324  | 2 Baz Street  | CC11CC   | C1
+        325  | 3 Baz Street   | CC22CC   | 325  | 3 Baz Street  | CC11CC   | C2
+
+        BB11BB and CC11CC are the ones we won't think are split, but are really.
+        Our checks should remove 225 and 325 because there aren't matches for them
+        in addressbase. Therefore they're not in the in_list.
+        """
+
+        in_list = [
+            {
+                "address": "1 Foo Street",
+                "postcode": "AA11AA",
+                "council": "AAA",
+                "polling_station_id": "A1",
+                "uprn": "123",
+            },
+            {
+                "address": "2 Foo Street",
+                "postcode": "AA11AA",
+                "council": "AAA",
+                "polling_station_id": "A1",
+                "uprn": "124",
+            },
+            {
+                "address": "3 Foo Street",
+                "postcode": "AA22AA",
+                "council": "AAA",
+                "polling_station_id": "A2",
+                "uprn": "125",
+            },
+            {
+                "address": "1 Bar Street",
+                "postcode": "BB11BB",
+                "council": "AAA",
+                "polling_station_id": "B1",
+                "uprn": "223",
+            },
+            {
+                "address": "2 Bar Street",
+                "postcode": "BB11BB",
+                "council": "AAA",
+                "polling_station_id": "B1",
+                "uprn": "224",
+            },
+            {
+                "address": "1 Baz Street",
+                "postcode": "CC11CC",
+                "council": "AAA",
+                "polling_station_id": "C1",
+                "uprn": "323",
+            },
+            {
+                "address": "2 Baz Street",
+                "postcode": "CC11CC",
+                "council": "AAA",
+                "polling_station_id": "C1",
+                "uprn": "324",
+            },
+        ]
+
+        addressbase = [
+            # AA11A
+            {"uprn": "123", "address": "1 Foo Street", "postcode": "AA11AA"},
+            {"uprn": "124", "address": "2 Foo Street", "postcode": "AA11AA"},
+            {"uprn": "125", "address": "3 Foo Street", "postcode": "AA11AA"},
+            # BB11BB
+            {"uprn": "223", "address": "1 Bar Street ", "postcode": "BB11BB"},
+            {"uprn": "224", "address": "2 Bar Street ", "postcode": "BB11BB"},
+            # CC11CC
+            {"uprn": "323", "address": "1 Baz Street", "postcode": "CC11CC"},
+            {"uprn": "324", "address": "2 Baz Street", "postcode": "CC11CC"},
+            # CC22CC
+            {"uprn": "325", "address": "3 Baz Street", "postcode": "CC22CC"},
+        ]
+        for address in addressbase:
+            AddressFactory(**address)
+        address_list = AddressList(MockLogger())
+        address_list.logger.clear_logs()
+        for el in in_list:
+            address_list.append(el)
+
+        split_postcodes = ["BB11BB", "CC11CC"]
+        address_list.check_split_postcodes_are_split(split_postcodes)
+        self.assertListEqual(
+            address_list.logger.logs,
+            [
+                "The same polling station will be assigned to all addresses in BB11BB, but BB11BB has more than one polling station assigned in council data.",
+                "The same polling station will be assigned to all addresses in CC11CC, but CC11CC has more than one polling station assigned in council data.",
+            ],
+        )
 
     def test_check_records(self):
         pass
