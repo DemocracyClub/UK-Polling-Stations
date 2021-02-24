15bbaeaed2493bb08c661eca10200983ea294d22
smsmith97 <sean.m.smith97@outlook.com>
2021-02-24 13:59:51 +0000

    import script for wyre (closes #3012)

--- polling_stations/apps/data_importers/management/commands/import_wyre.py
+++ polling_stations/apps/data_importers/management/commands/import_wyre.py
@@ -1,22 +1,23 @@
-from django.contrib.gis.geos import Point
 from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000128"
-    addresses_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019.tsv"
-    stations_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019.tsv"
-    elections = ["local.2019-05-02", "europarl.2019-05-23"]
-    csv_delimiter = "\t"
+    council_id = "WYR"
+    addresses_name = "2021-02-15T11:25:19.329853/Wyre Democracy_Club__06May2021.CSV"
+    stations_name = "2021-02-15T11:25:19.329853/Wyre Democracy_Club__06May2021.CSV"
+    elections = ["2021-05-06"]
+    csv_delimiter = ","
     csv_encoding = "windows-1252"
 
     def station_record_to_dict(self, record):
 
-        if record.polling_place_id == "3393":
-            record = record._replace(polling_place_postcode="PR3 0JY")
-            rec = super().station_record_to_dict(record)
-            rec["location"] = Point(-2.800374, 53.926697, srid=4326)
-            return rec
+        if record.polling_place_id == "4304":  # Charles Saer Community Primary School
+            record = record._replace(polling_place_postcode="FY7 8DD")
+
+        if (
+            record.polling_place_id == "4295"
+        ):  # Senior Citizens Assoc Hall, 190 Victoria Road West, Thornton Cleveleys - FY5 7JA ==> FY5 3NG
+            record = record._replace(polling_place_postcode="FY5 3NG")
 
         return super().station_record_to_dict(record)
 
@@ -24,16 +25,20 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
-        if uprn in [
-            "100012423379",  # PR30HA -> PR31QB : Sturzaker House Farm, Stones Lane, Catterall, Preston
-            "100012422528",  # PR31NL -> PR31PH : Conway, Lancaster Road, Cabus, Preston
-            "100012422994",  # PR30JX -> PR30JU : Brookfold Farm, Park Lane, Forton, Preston
-            "10003512835",  # PR30HX -> PR30TB : Keepers Cottage Kirkland Hall Farm, The Avenue, Churchtown
-            "10003512836",  # PR30HX -> PR30TB : Kirkland Hall Farm, The Avenue, Churchtown, Preston
-            "10034082836",  # PR30LB -> PR30LD : Cogie Hill Farm, Island Lane, Winmarleigh, Preston
-            "10003517314",  # FY67SW -> FY68AR : 8 Garstang Road West, Poulton-Le-Fylde
-            "10034089363",  # PR31UN -> PR31UL : Brook Cottage, Oakenclough Road, Scorton, Preston
+        if record.post_code in [
+            "FY6 8FS",  # Astray postcode
+            "FY6 7GH",  # Astray postcode
+            "FY7 6FJ",  # Astray postcode
+            "PR3 1TS",  # Astray postcode
+            "FY5 3LS",  # Postcode split over large distance
+            "FY6 9EX",  # Postcode split over large distance
         ]:
-            rec["accept_suggestion"] = False
+            return None
+
+        if uprn in [
+            "10034088556",  # FLAT AT GREAT SEASONS GARSTANG BYPASS ROAD, GARSTANG, PR3 1PH
+            "10093998387",  # 97A CROSTON ROAD, GARSTANG, PR3 0HQ - postcode likely should be PR3 1HQ
+        ]:
+            return None
 
         return rec
