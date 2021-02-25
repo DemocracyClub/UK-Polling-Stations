b1e26ff4c515ebc3dc54d41cbe90a4f9ccef0018
GeoWill <roper.william@gmail.com>
2021-02-25 15:09:39 +0000

    Import script for Folkestone and Hythe (closes #3004)

--- polling_stations/apps/data_importers/management/commands/import_folkestone_hythe.py
+++ polling_stations/apps/data_importers/management/commands/import_folkestone_hythe.py
@@ -3,71 +3,40 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000112"
-    addresses_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019 F&H.tsv"
-    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019 F&H.tsv"
-    elections = ["parl.2019-12-12"]
+    council_id = "SHE"
+    addresses_name = "/home/will/cloud/aws/democracy-club/225244788755/pollingstations-data/SHE/2021-02-10T16:39:37.576273/Democracy_Club__06May2021.tsv"
+    stations_name = "/home/will/cloud/aws/democracy-club/225244788755/pollingstations-data/SHE/2021-02-10T16:39:37.576273/Democracy_Club__06May2021.tsv"
+    elections = ["2021-06-05"]
     csv_delimiter = "\t"
-    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
-        if uprn == "50102150":
-            rec["postcode"] = "CT20 2EF"
-            rec["accept_suggestion"] = False
-
-        if uprn in [
-            "50104978",  # TN299AU -> TN299UA : Headmasters House, Rhee Wall, Brenzett, Romney Marsh, Kent
-            "50042070",  # CT202NQ -> CT202QN : Flat D, 104 Cheriton Road, Folkestone, Kent
-        ]:
-            rec["accept_suggestion"] = True
-
+        if record.addressline6 in ["CT20 3RE", "TN29 9AU"]:
+            return None
+        if uprn == "50124434":
+            return None
         return rec
 
     def station_record_to_dict(self, record):
         uprns = {
-            "6299": "50032257",  # Wards Hotel
-            "6291": "50046311",  # St Marys Primary School
-            "6212": "50102495",  # Hawkinge Community Centre
-            "6226": "50103399",  # Newington Village Hall
-            "6197": "50021990",  # Elham Village Hall
-            "6245": "50011466",  # Bodsham Primary School
-            "6366": "50011480",  # The Neptune
-            "6394": "50103652",  # The Star Inn
-            "6391": "50001094",  # Rose & Crown
+            "6872": "50032257",  # Wards Hotel
+            "6888": "50102495",  # Hawkinge Community Centre
+            "6899": "50103399",  # Newington Village Hall
+            "6876": "50021990",  # Elham Village Hall
+            "6922": "50011466",  # Bodsham Primary School
+            "7008": "50011480",  # The Neptune
+            "7056": "50103652",  # The Star Inn
+            "7053": "50001094",  # Rose & Crown
         }
 
         gridrefs = {
-            "6251": Point(1.0357008, 51.1383074, srid=4326),  # Peace Room
-            "6373": Point(0.9963577, 51.048795, srid=4326),  # Burmarsh Church Hall
-            "6376": Point(0.8502587, 51.0086578, srid=4326),  # Brenzett Village Hall
-            "6319": Point(1.119211, 51.072634, srid=4326),  # The Fountain Pub
+            "6928": Point(1.0357008, 51.1383074, srid=4326),  # Peace Room
+            "7036": Point(0.9963577, 51.048795, srid=4326),  # Burmarsh Church Hall
+            "7039": Point(0.8502587, 51.0086578, srid=4326),  # Brenzett Village Hall
+            "6862": Point(1.119211, 51.072634, srid=4326),  # The Fountain Pub
         }
-
-        """
-        Email
-
-        Please note that WDM5 – Ivychurch Parish has had a last minute change
-        of polling station, it is now:
-        St Georges Church, Ashford Road, Ivychurch, Romney Marsh, TN29 0AL
-        Can you please update accordingly. Thanks in adv.
-        """
-        if record.polling_place_id == "6383":
-            record = record._replace(polling_place_name="St Georges Church")
-            record = record._replace(polling_place_address_1="Ashford Road")
-            record = record._replace(polling_place_address_2="Ivychurch")
-            record = record._replace(polling_place_address_3="Romney Marsh")
-            record = record._replace(polling_place_address_4="")
-            record = record._replace(polling_place_postcode="TN29 0AL")
-            record = record._replace(polling_place_uprn="")
-            record = record._replace(polling_place_easting="0")
-            record = record._replace(polling_place_northing="0")
-
-        # Correction via email
-        if record.polling_place_id == "6400":
-            record = record._replace(polling_place_postcode="TN29 0NX")
 
         if record.polling_place_id in uprns:
             record = record._replace(polling_place_uprn=uprns[record.polling_place_id])
