6756a0c378d6bf0d9893d4893daf718f072bcef3
smsmith97 <sean.m.smith97@outlook.com>
2021-02-25 14:05:38 +0000

    import script for east northamptonshire (closes #3026)

--- polling_stations/apps/data_importers/management/commands/import_east_northamptonshire.py
+++ polling_stations/apps/data_importers/management/commands/import_east_northamptonshire.py
@@ -2,24 +2,28 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000152"
-    addresses_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019.tsv"
-    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019.tsv"
-    elections = ["parl.2019-12-12"]
+    council_id = "ENO"
+    addresses_name = "2021-02-18T16:04:59.003549/Democracy_Club__06May2021.tsv"
+    stations_name = "2021-02-18T16:04:59.003549/Democracy_Club__06May2021.tsv"
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
-    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
-        if uprn == "10001197605":
-            rec["postcode"] = "NN10 9XH"
-
-        if uprn == "10090611778":
+        if uprn in [
+            "10093005561"  # MOONBEAM WOODFORD RIVERSIDE MARINA ADDINGTON ROAD, WOODFORD
+        ]:
             return None
 
-        if uprn in ["200000731079", "10001197785", "10001198047"]:
-            rec["accept_suggestion"] = True
+        if record.addressline6 in ["NN9 5UF", "NN14 3BY", "NN10 9JD", "NN10 9NJ"]:
+            return None
 
         return rec
+
+    def station_record_to_dict(self, record):
+        # The Albert Underwood Room, St Peters Church, Church Street, Irthlingborough, Wellingborough
+        if record.polling_place_id == "10161":
+            record = record._replace(polling_place_postcode="")
+        return super().station_record_to_dict(record)
