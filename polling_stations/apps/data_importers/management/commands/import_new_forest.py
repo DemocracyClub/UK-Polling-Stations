315f55cebd50933561ecbd95a33f9e9794719fa7
smsmith97 <sean.m.smith97@outlook.com>
2021-02-26 11:56:16 +0000

    import script for new forest (closes #3045)

--- polling_stations/apps/data_importers/management/commands/import_new_forest.py
+++ polling_stations/apps/data_importers/management/commands/import_new_forest.py
@@ -1,36 +1,25 @@
-from django.contrib.gis.geos import Point
 from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000091"
-    addresses_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019nf.tsv"
-    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019nf.tsv"
-    elections = ["parl.2019-12-12"]
+    council_id = "NEW"
+    addresses_name = "2021-02-24T12:13:04.627288/Democracy_Club__06May2021.tsv"
+    stations_name = "2021-02-24T12:13:04.627288/Democracy_Club__06May2021.tsv"
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
     csv_encoding = "windows-1252"
-    allow_station_point_from_postcode = False
 
     def station_record_to_dict(self, record):
 
-        # correction: https://trello.com/c/R5XolnQC
-        if record.polling_place_id == "7903":  # New Milton Cricket Club
-            record = record._replace(polling_place_postcode="BH25 5SU")
-
-        # user issue report #195
-        if record.polling_place_id == "7942":  # Sandleheath Village Hall
-            rec = super().station_record_to_dict(record)
-            rec["location"] = Point(-1.817931, 50.931645, srid=4326)
-            return rec
-
         # corrections from council
-        if record.polling_place_id == "7953":  # Woodgreen Village Hall
-            record = record._replace(polling_place_easting="417079.23")
-            record = record._replace(polling_place_northing="117684.73")
-        if record.polling_place_id == "7836":  # Fordingbridge Town Hall
+        if (
+            record.polling_place_id == "8374"
+        ):  # Fordingbridge Town Hall, 63 High Street, Fordingbridge, SP6 1AS
             record = record._replace(polling_place_easting="414713.35")
             record = record._replace(polling_place_northing="114134.91")
-        if record.polling_place_id == "7784":  # Totton & Eling Cricket Club
+        if (
+            record.polling_place_id == "8523"
+        ):  # Totton & Eling Cricket Club, Southern Gardens, Ringwood Road, Totton, Southampton, SO40 8RW
             record = record._replace(polling_place_easting="435321.7")
             record = record._replace(polling_place_northing="113089.68")
 
@@ -40,7 +29,14 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
-        if uprn in ["10007461147", "100062214501"]:
+        if uprn in [
+            "10093932670",  # NEWTOWN LANE FARM, NEWTOWN LANE, MOCKBEGGAR, RINGWOOD, BH24 3NN
+            "100062214501",  # FORTUNE CENTRE OF RIDING THERAPY, WOOTON HALL FARM, TIPTOE ROAD, NEW MILTON, BH25 5SJ
+            "10013640427",  # ACORNS 47 BELMORE LANE, LYMINGTON, SO41 3NR
+        ]:
+            return None
+
+        if record.addressline6 in ["SO45 4PA"]:
             return None
 
         return rec
