f30a2fdc0be34d364b9072becbf17d968db17c59
smsmith97 <sean.m.smith97@outlook.com>
2021-03-02 13:54:16 +0000

    import script for waverley (closes #3064)

--- polling_stations/apps/data_importers/management/commands/import_waverley.py
+++ polling_stations/apps/data_importers/management/commands/import_waverley.py
@@ -2,38 +2,30 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000216"
-    addresses_name = "parl.2019-12-12/Version 3/merged.tsv"
-    stations_name = "parl.2019-12-12/Version 3/merged.tsv"
-    elections = ["parl.2019-12-12"]
+    council_id = "WAE"
+    addresses_name = "2021-03-01T10:16:29.232903/Democracy_Club__06May2021.tsv"
+    stations_name = "2021-03-01T10:16:29.232903/Democracy_Club__06May2021.tsv"
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
-    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
-        if uprn == "100062163595":
+
+        if uprn in [
+            "100061614139",  # KINGSLEY, CATTESHALL LANE, GODALMING
+            "200001293683",  # 1 GREEN LANE VILLAS, GREEN LANE, CHURT, FARNHAM
+            "100061610563",  # 75 UPPER HALE ROAD, FARNHAM
+        ]:
             return None
 
-        if uprn in [
-            "200001286366",  # GU50LG -> GU50LH : The Old Cottage, Grafham, Bramley, Guildford, Surrey
-            "200003086280",  # GU84BH -> GU84BX : Gorebridge House, Loxhill, Godalming, Surrey
-            "100061616798",  # GU85UJ -> GU85LA : Brook Grange, Haslemere Road, Brook, Godalming, Surrey
-            "10013894366",  # GU99JT -> GU97JT : 5 Stoke House, 17 St James Terrace, Farnham, Surrey
-            "100062163296",  # GU71SX -> GU71XS : 10 Lawnwood Court, Catteshall Lane, Godalming, Surrey
-            "100061625643",  # GU50TJ -> GU50TP : Thanescroft, Lords Hill Common, Shamley Green, Guildford, Surrey
-            "200001294523",  # GU50LG -> GU50LH : Rushett Farm Cottage, Rushett Common, Bramley, Guildford, Surrey
-            "200001537439",  # GU50SU -> GU68QY : Willinghurst Lodge, Willinghurst Estate, Shamley Green, Guildford, Surrey
+        if record.addressline6 in [
+            "GU9 9JT",
+            "GU9 0HR",
+            "GU10 2JT",
+            "GU8 4BH",
+            "GU7 3LG",
+            "GU7 1LN",
         ]:
-            rec["accept_suggestion"] = True
+            return None
 
-        if uprn in [
-            "10013894393",  # GU103QY -> GU98DR : 3 Tattingstone Close, Lower Bourne, Farnham, Surrey
-            "100062339874",  # GU97DR -> GU272AJ : Merriott House, 26 West Street, Farnham, Surrey
-            "100062339873",  # GU97DR -> GU272AH : 24 West Street, Farnham, Surrey
-            "100062339881",  # GU104BT -> GU272HG : 27 High Street, Rowledge, Farnham, Surrey
-            "200001536207",  # GU103BS -> GU102AZ : Keepers Cottage Pierrepont Farm, Priory Lane, Frensham, Farnham, Surrey
-        ]:
-            rec["accept_suggestion"] = False
-
-        return rec
+        return super().address_record_to_dict(record)
