c1cef7d6c732d11b5045694ccd72332798c8155d
smsmith97 <sean.m.smith97@outlook.com>
2021-03-02 10:58:51 +0000

    import script for cotswold (closes #3040)

--- polling_stations/apps/data_importers/management/commands/import_cotswold.py
+++ polling_stations/apps/data_importers/management/commands/import_cotswold.py
@@ -2,27 +2,38 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000079"
-    addresses_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019cots.tsv"
-    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019cots.tsv"
-    elections = ["parl.2019-12-12"]
+    council_id = "COT"
+    addresses_name = "2021-02-23T13:29:42.051423/Democracy_Club__06May2021.tsv"
+    stations_name = "2021-02-23T13:29:42.051423/Democracy_Club__06May2021.tsv"
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
     csv_encoding = "windows-1252"
-    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
+        uprn = record.property_urn.strip().lstrip("0")
 
-        if record.addressline1 == "Flat Above Garage, Treetops":
-            rec["postcode"] = "GL560HE"
+        if uprn in [
+            "10023475981",  # SELWORTHY, MORETON ROAD, STOW ON THE WOLD, CHELTENHAM
+            "10013878640",  # BARTON MEADOW LODGE, GUITING POWER, CHELTENHAM
+            "10093270137",  # WOODLAND BARN FARM, WHITEWAY, CIRENCESTER
+            "10013883455",  # BURGAGE END FARMINGTON ROAD, NORTHLEACH
+        ]:
+            return None
 
-        return rec
+        if record.addressline6 in [
+            "GL8 8SQ",
+            "GL7 7EX",
+            "GL7 7BA",
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
 
         # source: https://britishlistedbuildings.co.uk/101340859-village-hall-icomb
         # Icomb Village Hall
-        if record.polling_place_id == "16018":
+        if record.polling_place_id == "17236":
             record = record._replace(polling_place_northing="222640")
 
         return super().station_record_to_dict(record)
