b0d625a5b74ad0c286d6b8583a0eb5071cd7ddf1
smsmith97 <sean.m.smith97@outlook.com>
2021-03-03 16:55:12 +0000

    import script for hart (closes #3082)

--- polling_stations/apps/data_importers/management/commands/import_hart.py
+++ polling_stations/apps/data_importers/management/commands/import_hart.py
@@ -2,30 +2,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000089"
-    addresses_name = "2020-02-13T16:27:39.613267/Democracy_Club__07May2020.tsv"
-    stations_name = "2020-02-13T16:27:39.613267/Democracy_Club__07May2020.tsv"
-    elections = ["2020-05-07"]
+    council_id = "HAT"
+    addresses_name = (
+        "2021-03-03T10:59:35.556539/Hart District Democracy_Club_06May2021.tsv"
+    )
+    stations_name = (
+        "2021-03-03T10:59:35.556539/Hart District Democracy_Club_06May2021.tsv"
+    )
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
-
-    def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "200001000440",  # RG278BU -> RG291JR : Chevertons Farm, Odiham Road, Winchfield, Hook
-            "10008948691",  # RG278EF -> RG278SH : Taplins Farm Stables, Church Lane, Hartley Wintney, Hook
-            "200001006098",  # GU105HJ -> RG291TL : Quern Farm, Well Road, Crondall, Farnham
-            "100060406353",  # GU527UA -> GU467RX : 94A Reading Road South, Fleet, Hampshire
-        ]:
-            rec["accept_suggestion"] = False
-
-        return rec
-
-    def station_record_to_dict(self, record):
-        # Hart Leisure Centre
-        if record.polling_place_id == "3297":
-            record = record._replace(polling_place_easting="479224")
-            record = record._replace(polling_place_northing="154016")
-
-        return super().station_record_to_dict(record)
