d281400772105bd4937f83fbe6cf45ae41f9e929
smsmith97 <sean.m.smith97@outlook.com>
2021-03-03 15:12:33 +0000

    import script for hambleton (closes #3076)

--- polling_stations/apps/data_importers/management/commands/import_hambleton.py
+++ polling_stations/apps/data_importers/management/commands/import_hambleton.py
@@ -2,35 +2,29 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000164"
-    addresses_name = "parl.2019-12-12/Version 1/merged.tsv"
-    stations_name = "parl.2019-12-12/Version 1/merged.tsv"
-    elections = ["parl.2019-12-12"]
+    council_id = "HAE"
+    addresses_name = "2021-03-02T16:29:16.566032/Democracy_Club__06May2021 Hambleton District Council.tsv"
+    stations_name = "2021-03-02T16:29:16.566032/Democracy_Club__06May2021 Hambleton District Council.tsv"
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
-    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "10001280326",  # TS97DA -> TS97BZ : Hazel Grove, Whorl Hill, Faceby, Middlesbrough
-            "10001283328",  # YO612QG -> YO612QQ : Woodmans Cottage, Pilmoor, York
-            "10001287245",  # DL63TE -> DL63PL : Foxton Grange, Kirby Sigston, Northallerton
-            "100050359857",  # DL82PD -> DL82PE : Oaklands Long Lane, Well, Bedale
-            "10008642909",  # TS95LJ -> TS95LL : Seamer Moor Bungalow Seamer Moor Farm, Seamer Moor, Seamer, Middlesbrough
-            "10013390894",  # TS96QE -> TS96QD : Caravan B Stanley Grange Stud, Great Ayton, Middlesbrough
-            "10070731545",  # YO613QG -> YO613QE : Kyle Farm, Husthwaite Road, Easingwold, York
-            "200001927445",  # TS95LN -> TS95LL : Falklands Wynn, Holme Lane, Seamer
-            "200003294819",  # YO613PR -> YO613PU : Hollybush Farm, Myra Bank Lane, Crayke, York
-            "200003297809",  # DL63HR -> DL63HS : Greenacre, Gold Hill Loop Road, Swainby, Northallerton
-            "200003297811",  # DL63HR -> DL63HS : Longlands, Gold Gate Lane, Swainby, Northallerton
+            "200003295948",  # CHAPPEL VIEW PONY STUD, TUNSTALL LANE, NUNTHORPE, MIDDLESBROUGH
         ]:
-            rec["accept_suggestion"] = True
+            return None
 
-        if uprn in [
-            "10070735117",  # YO71AS -> YO613HY : Flat Above, 97 Long Street, Thirsk
-        ]:
-            rec["accept_suggestion"] = False
+        if record.addressline6 in ["YO7 3NG", "DL7 8DA"]:
+            return None
 
-        return rec
+        return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        if (
+            record.polling_place_id == "8653"
+        ):  # Thirkleby Parish Hall Great Thirkleby Thirsk - https://trello.com/c/uAHT7T8d/330-hambleton
+            record = record._replace(polling_place_postcode="YO7 2AT")
+
+        return super().station_record_to_dict(record)
