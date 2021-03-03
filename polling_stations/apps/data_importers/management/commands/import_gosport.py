84c85246c553396dd6f5b8a741fce5701414ac4d
smsmith97 <sean.m.smith97@outlook.com>
2021-03-03 15:44:27 +0000

    import script for gosport (close #3078)

--- polling_stations/apps/data_importers/management/commands/import_gosport.py
+++ polling_stations/apps/data_importers/management/commands/import_gosport.py
@@ -2,49 +2,14 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000088"
-    addresses_name = (
-        "parl.2019-12-12/Version 1/2019 PGE - Democracy_Club__12December2019gosport.TSV"
-    )
-    stations_name = (
-        "parl.2019-12-12/Version 1/2019 PGE - Democracy_Club__12December2019gosport.TSV"
-    )
-    elections = ["parl.2019-12-12"]
+    council_id = "GOS"
+    addresses_name = "2021-03-02T17:11:51.716352/2021 LGE & PCC - Gosport Democracy_Club__06May2021 v2.tsv"
+    stations_name = "2021-03-02T17:11:51.716352/2021 LGE & PCC - Gosport Democracy_Club__06May2021 v2.tsv"
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
-    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
-        uprn = record.property_urn.strip().lstrip("0")
+        if record.addressline6 in ["PO13 0EW"]:
+            return None
 
-        if uprn in [
-            "37013642",  # PO122BY -> PO123BY : Duncan Block, HMS Sultan, Military Road, Gosport, Hampshire
-            "37013642",  # PO122BY -> PO123BY : Hood Block, HMS Sultan, Military Road, Gosport, Hampshire
-            "37013642",  # PO122BY -> PO123BY : Onslow Block, HMS Sultan, Military Road, Gosport, Hampshire
-            "37013642",  # PO122BY -> PO123BY : Ussher Block, HMS Sultan, Military Road, Gosport, Hampshire
-            "37013642",  # PO122BY -> PO123BY : Beaufort Block, HMS Sultan, Military Road, Gosport, Hampshire
-            "37013642",  # PO122BY -> PO123BY : Ramsey Block, HMS Sultan, Military Road, Gosport, Hampshire
-            "37013642",  # PO122BY -> PO123BY : Rodney Block, HMS Sultan, Military Road, Gosport, Hampshire
-            "37013642",  # PO122BY -> PO123BY : Cunningham Block, HMS Sultan, Military Road, Gosport, Hampshire
-            "37013642",  # PO122BY -> PO123BY : Inman Block, HMS Sultan, Military Road, Gosport, Hampshire
-            "37013642",  # PO122BY -> PO123BY : Nelson Block, HMS Sultan, Military Road, Gosport, Hampshire
-            "37013642",  # PO122BY -> PO123BY : Oates Block, HMS Sultan, Military Road, Gosport, Hampshire
-            "37013642",  # PO122BY -> PO123BY : Vian Block, HMS Sultan, Military Road, Gosport, Hampshire
-            "37013642",  # PO122BY -> PO123BY : Yarmouth Block, HMS Sultan, Military Road, Gosport, Hampshire
-            "37013642",  # PO122BY -> PO123BY : Keyes Block, HMS Sultan, Military Road, Gosport, Hampshire
-            "37013642",  # PO122BY -> PO123BY : JRAC Block, HMS Sultan, Military Road, Gosport, Hampshire
-            "37013642",  # PO122BY -> PO123BY : Esmonde Block, HMS Sultan, Military Road, Gosport, Hampshire
-            "37013642",  # PO122BY -> PO123BY : Whitworth Block, HMS Sultan, Military Road, Gosport, Hampshire
-            "37013642",  # PO122BY -> PO123BY : Benbow Block, HMS Sultan, Military Road, Gosport, Hampshire
-            "37013642",  # PO122BY -> PO123BY : Sommerville Block, HMS Sultan, Military Road, Gosport, Hampshire
-            "37013642",  # PO122BY -> PO123BY : Phoebe Block, HMS Sultan, Military Road, Gosport, Hampshire
-            "37013642",  # PO122BY -> PO123BY : Quiberon Block, HMS Sultan, Military Road, Gosport, Hampshire
-            "37013642",  # PO122BY -> PO123BY : Salisbury Block, HMS Sultan, Military Road, Gosport, Hampshire
-            "37013642",  # PO122BY -> PO123BY : Jervis Block, HMS Sultan, Military Road, Gosport, Hampshire
-            "37013642",  # PO122BY -> PO123BY : Mountbatten Block, HMS Sultan, Military Road, Gosport, Hampshire
-            "37042796",  # PO122BY -> PO123BG : The Wardroom, HMS Sultan, Military Road, Gosport, Hampshire
-            "37013642",  # PO122BY -> PO123BY : Lefanu Block, HMS Sultan, Military Road, Gosport, Hampshire
-        ]:
-            rec["accept_suggestion"] = True
-
-        return rec
+        return super().address_record_to_dict(record)
