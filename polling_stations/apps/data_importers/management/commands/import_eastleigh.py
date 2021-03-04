869ba52a18a16e076c88c6326d2ddc9366f1deaf
smsmith97 <sean.m.smith97@outlook.com>
2021-03-04 16:06:33 +0000

    import script for eastleigh (closes #3087)

--- polling_stations/apps/data_importers/management/commands/import_eastleigh.py
+++ polling_stations/apps/data_importers/management/commands/import_eastleigh.py
@@ -2,46 +2,39 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000086"
-    addresses_name = "parl.2019-12-12/Version 1/Parliamentary Election - Democracy_Club__12December2019east.tsv"
-    stations_name = "parl.2019-12-12/Version 1/Parliamentary Election - Democracy_Club__12December2019east.tsv"
-    elections = ["parl.2019-12-12"]
+    council_id = "EAT"
+    addresses_name = "2021-03-03T12:56:56.264077/Eastleigh Borough Council Democracy_Club__06May2021 (1).tsv"
+    stations_name = "2021-03-03T12:56:56.264077/Eastleigh Borough Council Democracy_Club__06May2021 (1).tsv"
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
-    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
-        rec = super().address_record_to_dict(record)
 
-        # Implausible looking UPRN geocodes. Throw away rather than investigate.
         if uprn in [
-            "10091136033",
-            "100060322293",
-            "10091136173",
-            "10009593531",
-            "10012197923",
+            "100060322293",  # 18 LEWRY CLOSE, HEDGE END, SOUTHAMPTON
+            "10094385410",  # 44A KELBURN CLOSE, CHANDLER'S FORD
+            "10012197923",  # 300 CHESTNUT AVENUE, CHANDLER'S FORD, EASTLEIGH
+            "10009593531",  # FLAT 1 85 TWYFORD ROAD, EASTLEIGH
+            "10094385283",  # 12 JACOBS CLOSE, BURSLEDON
         ]:
             return None
 
-        if uprn in [
-            "100060294726",  # SO533DA -> SO533HB : 242 Bournemouth Road, Chandler`s Ford, Eastleigh
-            "10009593644",  # SO533DA -> SO533AD : Dovecote, Howard Close, Chandler`s Ford, Eastleigh
-            "100060302836",  # SO532FG -> SO535BZ : 138 Kingsway, Chandler`s Ford, Eastleigh
-            "10009589400",  # SO507DE -> SO507DF : The Chalet, Moon River Pines, Fir Tree Lane, Horton Heath, Eastleigh
-            "10091136799",  # SO303FA -> SO532HL : 8a Clanfield Close, Chandler`s Ford, Eastleigh
-        ]:
-            rec["accept_suggestion"] = True
+        if record.addressline6 in ["SO31 5FH", "SO30 3FA", "SO53 2FG"]:
+            return None
 
-        return rec
+        return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
-        if record.polling_place_id == "4629":  # Abbey Hall, Victoria Road.
-            record = record._replace(polling_place_easting="445232")
+        if (
+            record.polling_place_id == "5150"
+        ):  # Abbey Hall Victoria Road Netley Abbey Southampton SO31 5FA
+            record = record._replace(polling_place_easting="445235")
             record = record._replace(polling_place_northing="108734")
 
         if (
-            record.polling_place_uprn == "100062644887"
-        ):  # Chandler's Ford Community Centre
+            record.polling_place_id == "5105"
+        ):  # Chandler's Ford Community Centre, Hursley Road, Chandler's Ford, Eastleigh, SO53 2FS
             record = record._replace(polling_place_postcode="SO53 2FT")
 
         return super().station_record_to_dict(record)
