7191b5e6937b5bebb1aa5c963c5ee25feb4e2f61
GeoWill <roper.william@gmail.com>
2021-03-05 16:22:03 +0000

    Import script for Moray

--- polling_stations/apps/data_importers/management/commands/import_moray.py
+++ polling_stations/apps/data_importers/management/commands/import_moray.py
@@ -3,8 +3,8 @@ from data_importers.github_importer import BaseGitHubImporter
 
 class Command(BaseGitHubImporter):
     srid = 4326
-    council_id = "S12000020"
-    elections = ["parl.2019-12-12"]
+    council_id = "MRY"
+    elections = ["2021-05-06"]
     scraper_name = "wdiv-scrapers/DC-PollingStations-Moray"
     geom_type = "geojson"
 
@@ -13,7 +13,7 @@ class Command(BaseGitHubImporter):
 
         return {
             "internal_council_id": record["PD_CODE"],
-            "name": record["PD_CODE"],
+            "name": record["PD_NAME"],
             "area": poly,
             "polling_station_id": record["PD_CODE"],
         }
