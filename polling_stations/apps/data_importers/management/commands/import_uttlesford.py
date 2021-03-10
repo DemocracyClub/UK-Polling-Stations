75865c65433206a4d25be19f38955f261b92f72c
smsmith97 <sean.m.smith97@outlook.com>
2021-03-10 14:32:19 +0000

    import script for uttlesford (closes #3132)

--- polling_stations/apps/data_importers/management/commands/import_uttlesford.py
+++ polling_stations/apps/data_importers/management/commands/import_uttlesford.py
@@ -1,55 +1,68 @@
-import os
-from data_importers.base_importers import CsvMixin
-from data_importers.github_importer import BaseGitHubImporter
-from data_finder.helpers import geocode_point_only, PostcodeError
+from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
-class Command(BaseGitHubImporter, CsvMixin):
-
-    srid = 27700
-    districts_srid = 27700
-    council_id = "E07000077"
-    elections = ["europarl.2019-05-23"]
-    scraper_name = "wdiv-scrapers/DC-PollingStations-Uttlesford"
-    geom_type = "geojson"
-    # districts file has station address and UPRN for district
-    # parse the districts file twice
-    stations_query = "districts"
-    # ..and then the postcodes for the polling stations
-    # are in a CSV file on S3
-    local_files = True
-    csv_encoding = "utf-8"
+class Command(BaseXpressDemocracyClubCsvImporter):
+    council_id = "UTT"
+    addresses_name = "2021-03-09T11:57:50.428803/Democracy_Club__06May2021_UDC.CSV"
+    stations_name = "2021-03-09T11:57:50.428803/Democracy_Club__06May2021_UDC.CSV"
+    elections = ["2021-05-06"]
     csv_delimiter = ","
 
-    def pre_import(self):
-        postcodes_file = os.path.join(
-            self.base_folder_path,
-            "europarl.2019-05-23/Version 1/Uttlesford Postcodes.csv",
-        )
-        postcodes = self.get_data("csv", postcodes_file)
-        self.postcodes = {pc.district_code: pc.postcode for pc in postcodes}
+    def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
 
-    def district_record_to_dict(self, record):
-        poly = self.extract_geometry(record, self.geom_type, self.get_srid("districts"))
-        return {
-            "internal_council_id": record["PD_ID"],
-            "name": record["PD_ID"],
-            "area": poly,
-            "polling_station_id": record["PD_ID"],
-        }
+        if uprn in [
+            "200004270735",  # "THE BARN, PLEDGDON GREEN, HENHAM, BISHOP'S STORTFORD"
+            "10002182834",  # ANNEXE AT PLEDGDON LODGE BRICK END ROAD, HENHAM
+            "200004261749",  # B LODGE, EASTON LODGE, LITTLE EASTON, DUNMOW
+            "100091278781",  # GREENWOOD, CHURCH ROAD, CHRISHALL, ROYSTON
+            "10090833371",  # NEW FARM ARKESDEN ROAD, WENDENS AMBO
+            "100091276459",  # 6 CHICKNEY ROAD, HENHAM, BISHOP'S STORTFORD
+            "100091277104",  # ARCHWAYS OLD MEAD ROAD, HENHAM
+        ]:
+            return None
+
+        if record.addressline6 in ["CM22 6FG", "CM22 6TW"]:
+            return None
+
+        return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
-        postcode = self.postcodes[record["PD_ID"]]
+        # St. Mary`s CoE Foundation Primary School, Stansted School Hall Hampton Road Stansted CM24 8FE
+        if record.polling_place_id == "688":
+            record = record._replace(polling_place_uprn="10090833547")
 
-        try:
-            location_data = geocode_point_only(postcode)
-            location = location_data.centroid
-        except PostcodeError:
-            location = None
+        # R A Butler School, R A Butler Academy - School Hall, South Road, Saffron Walden
+        if record.polling_place_id == "676":
+            record = record._replace(polling_place_uprn="200004267358")
 
-        return {
-            "internal_council_id": record["PD_ID"],
-            "address": record["Polling_St"],
-            "postcode": postcode,
-            "location": location,
-        }
+        # Chrishall New Village Hall Crawley End Chrishall Royston SG8 8QJ
+        if record.polling_place_id == "637":
+            record = record._replace(polling_place_uprn="200004259626")
+
+        # Ashdon Village Hall Radwinter Road Ashdon Saffron Walden
+        if record.polling_place_id == "547":
+            record = record._replace(polling_place_postcode="CB10 2HA")
+
+        # Removing postcodes where council data is incorrect or inconclusive
+        if record.polling_place_id in [
+            "552",  # Hadstock Village Hall,  Church Lane, Hadstock
+            "556",  # Sewards End Village Hall,  Radwinter Road, Sewards End, Saffron Walden
+            "565",  # Hatfield Heath Village Hall,  The Heath, Hatfield Heath, Bishop`s Stortford
+            "577",  # Langley Community Centre,  Langley Upper Green, Saffron Walden
+            "584",  # The Community Hall at Carver Barracks,  Off Elder Street, Wimbish, Saffron Walden
+            "587",  # Wimbish Village Hall,  Tye Green, Wimbish, Saffron Walden
+            "621",  # Hatfield Broad Oak Village Hall,  Cage End, Hatfield Broad Oak, Bishop`s Stortford
+            "632",  # Leaden Roding Village Hall,  Stortford Road,  Leaden Roding,  Dunmow
+            "642",  # Elmdon Village Hall,  Cross Hill, Elmdon, Saffron Walden
+            "668",  # Council Offices,  London Road,  London Road, Saffron Walden, Essex
+            "685",  # St. Mary`s Church Hall,  Birchanger Lane, Birchanger, Bishops Stortford
+            "694",  # Farnham Village Hall,  Rectory Lane, Farnham, Bishop`s Stortford
+            "702",  # Broxted Village Hall,  Browns End Road, Broxted, Dunmow
+            "704",  # Little Canfield Village Hall,  Stortford Road, Little Canfield, Dunmow
+            "707",  # Mole Hill Green Village Hall,  Mole Hill Green, Takeley, Bishop`s Stortford
+            "718",  # Great Easton Village Hall,  Rebecca Meade, Great Easton, Dunmow
+        ]:
+            record = record._replace(polling_place_postcode="")
+
+        return super().station_record_to_dict(record)
