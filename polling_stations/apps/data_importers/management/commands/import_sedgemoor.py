6935a50c8194338bd9b927aa169f007b7c93a171
GeoWill <roper.william@gmail.com>
2021-03-12 15:36:23 +0000

    Import script for Sedgemoor (closes #3105)

--- polling_stations/apps/data_importers/management/commands/import_sedgemoor.py
+++ polling_stations/apps/data_importers/management/commands/import_sedgemoor.py
@@ -2,28 +2,29 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 
 class Command(BaseHalaroseCsvImporter):
-    council_id = "E07000188"
-    addresses_name = "2020-02-03T10:23:47.031505/polling_station_export-2020-01-29.csv"
-    stations_name = "2020-02-03T10:23:47.031505/polling_station_export-2020-01-29.csv"
-    elections = ["2020-05-07"]
+    council_id = "SEG"
+    addresses_name = "2021-03-04T13:46:19.027597/polling_station_export-2021-03-04.csv"
+    stations_name = "2021-03-04T13:46:19.027597/polling_station_export-2021-03-04.csv"
+    elections = ["2021-05-06"]
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip()
-        rec = super().address_record_to_dict(record)
-
-        if uprn == "10009328603":
-            rec["postcode"] = "TA5 1JG"
-
-        if uprn == "200000451332":
-            rec["postcode"] = "TA7 0SD"
-
-        if uprn == "200000450011":
-            rec["postcode"] = "BS26 2HU"
-
         if record.housepostcode in [
-            "TA5 1NQ",
+            "TA6 5NL",
+            "TA6 4HB",
+            "TA5 2PF",
+            "TA6 6RZ",
+            "TA6 6QH",
+            "TA6 6GD",
+            "TA5 2BE",
+            "BS24 0HT",
+            "TA6 7AW",
+            "TA5 2NH",
             "TA5 1JW",
         ]:
             return None
 
-        return rec
+        if uprn == "10009328603":
+            record = record._replace(housepostcode="TA5 1JG")
+
+        return super().address_record_to_dict(record)
