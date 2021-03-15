ba33c26b48975f8bad50bb45a06b144701c32c17
smsmith97 <sean.m.smith97@outlook.com>
2021-03-15 10:18:17 +0000

    import script for west devon (closes #3160)

--- polling_stations/apps/data_importers/management/commands/import_west_devon.py
+++ polling_stations/apps/data_importers/management/commands/import_west_devon.py
@@ -2,33 +2,44 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000047"
-    addresses_name = "parl.2019-12-12/Version 1/merged.tsv"
-    stations_name = "parl.2019-12-12/Version 1/merged.tsv"
-    elections = ["parl.2019-12-12"]
+    council_id = "WDE"
+    addresses_name = (
+        "2021-03-12T10:39:08.998016/West Devon Democracy_Club__06May2021.tsv"
+    )
+    stations_name = (
+        "2021-03-12T10:39:08.998016/West Devon Democracy_Club__06May2021.tsv"
+    )
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
-    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "10001329690"  # EX201SQ -> EX201JJ : Beechwood, Upcott Hill, Okehampton, Devon
+            "10013758398",  # MOWHAY BARN, HIGHER CARLEY BARNS, LIFTON
+            "10001332226",  # BERRYDOWN STABLES, EXBOURNE, OKEHAMPTON
+            "10013751665",  # HOMEFIELD, DREWSTEIGNTON, EXETER
+            "10001330732",  # ROWDEN, BRIDESTOWE, OKEHAMPTON
         ]:
-            rec["accept_suggestion"] = False
+            return None
 
-        return rec
+        if record.addressline6 in [
+            "EX20 3NX",
+            "EX20 3NW",
+            "EX20 1QB",
+            "EX20 2TP",
+            "EX20 2SD",
+            "EX20 1SY",
+            "EX20 2NF",
+            "PL19 9HA",
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
-
-        # Charter Hall (Polling Station No.1-4)
-        if record.polling_place_id in ["6965", "6975", "6971", "6973"]:
-            record = record._replace(polling_place_uprn="10013752301")
-
-        # Milton Abbot Village Halls
-        if record.polling_place_id == "7237":
-            record = record._replace(polling_place_uprn="10001329795")
-            record = record._replace(polling_place_postcode="")
+        # Lifton Community Centre Park Wood Rise Lifton PL16 0AL
+        if record.polling_place_id == "8001":
+            record = record._replace(polling_place_postcode="PL16 0LA")
 
         return super().station_record_to_dict(record)
