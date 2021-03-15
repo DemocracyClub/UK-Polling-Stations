97489bc8c364e35342f04a0cbdd06633a3cba89b
smsmith97 <sean.m.smith97@outlook.com>
2021-03-15 15:26:26 +0000

    import script for oadby (closes #3177)

--- polling_stations/apps/data_importers/management/commands/import_oadby.py
+++ polling_stations/apps/data_importers/management/commands/import_oadby.py
@@ -2,27 +2,24 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000135"
-    addresses_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019O&W.tsv"
-    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019O&W.tsv"
-    elections = ["parl.2019-12-12"]
-    csv_delimiter = "\t"
-    allow_station_point_from_postcode = False
+    council_id = "OAD"
+    addresses_name = "2021-03-16T16:05:47.817797/Democracy_Club__06May2021.csv"
+    stations_name = "2021-03-16T16:05:47.817797/Democracy_Club__06May2021.csv"
+    elections = ["2021-05-06"]
+    csv_delimiter = ","
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "100030582044",  # LE22DB -> LE25DB : 36-38 Bankart Avenue, Oadby, Leicester
-            "200001140229",  # LE25AW -> LE24LZ : Flat 1 St Pauls Court, Chapel Street, Oadby, Leicester
-            "200001140230",  # LE25AW -> LE24LZ : Flat 2 St Pauls Court, Chapel Street, Oadby, Leicester
-            "200001140231",  # LE25AW -> LE24LZ : Flat 3 St Pauls Court, Chapel Street, Oadby, Leicester
-            "200001140232",  # LE25AW -> LE24LZ : Flat 4 St Pauls Court, Chapel Street, Oadby, Leicester
-            "200001140233",  # LE25AW -> LE24LZ : Flat 5 St Pauls Court, Chapel Street, Oadby, Leicester
-            "10010147237",  # LE25QP -> LE182LE : 9 Honeywell Close, Oadby, Leicester
-            "10010147209",  # LE183QH -> LE181DZ : 12A Waterloo Crescent, Wigston, Leicestershire
+            "100030597499",  # 69A LONG STREET, WIGSTON
+            "10010149199",  # 67A LONG STREET, WIGSTON
+            "100030597496",  # OADBY LODGE YOUTH HOSTEL GARTREE ROAD, OADBY
+            "10010146840",  # 46A LONG STREET, WIGSTON
         ]:
-            rec["accept_suggestion"] = False
+            return None
 
-        return rec
+        if record.addressline6 in ["LE18 2GQ"]:
+            return None
+
+        return super().address_record_to_dict(record)
