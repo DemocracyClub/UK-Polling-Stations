74c82a4ab3f49f707eff41959317011c33974d15
smsmith97 <sean.m.smith97@outlook.com>
2021-03-17 16:49:33 +0000

    import script for selby (closes #3210)

--- polling_stations/apps/data_importers/management/commands/import_selby.py
+++ polling_stations/apps/data_importers/management/commands/import_selby.py
@@ -2,28 +2,33 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000169"
-    addresses_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019selby.CSV"
-    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019selby.CSV"
-    elections = ["parl.2019-12-12"]
-    allow_station_point_from_postcode = False
-
-    def station_record_to_dict(self, record):
-        if record.polling_place_id == "4929":
-            # Selby Rugby Club Sandhill Lane Selby
-            record = record._replace(polling_place_postcode="YO8 4JP")
-        if record.polling_place_id == "4923":
-            # Lady Popplewell Centre, Beechwood Close, Sherburn in Elmet, Leeds
-            record = record._replace(polling_place_postcode="LS25 6HU")
-        return super().station_record_to_dict(record)
+    council_id = "SEL"
+    addresses_name = "2021-03-16T14:34:21.177117/Democracy_Club__06May2021 (1).tsv"
+    stations_name = "2021-03-16T14:34:21.177117/Democracy_Club__06May2021 (1).tsv"
+    elections = ["2021-05-06"]
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
-        if record.addressline6 in [
-            "LS24 9PA",
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "200001023825",  # WILLOW FARM, DONCASTER ROAD, TADCASTER
+            "200001017730",  # WHITEMOOR GRANGE WHITEMOOR LANE, BARLBY, SELBY
         ]:
             return None
 
-        if (record.addressline1, record.addressline2) == ("Rose Lodge", "Oakwood Park"):
-            record = record._replace(property_urn="10093092239")
+        if record.addressline6 in ["YO8 8FH", "YO8 3EH", "LS24 9HH", "YO8 3ED"]:
+            return None
 
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # Selby Rugby Club Sandhill Lane Selby YO8 4HP
+        if record.polling_place_id == "6168":
+            record = record._replace(polling_place_postcode="YO8 4JP")
+
+        # Lady Popplewell Centre Beechwood Close Sherburn in Elmet Leeds LS25 6HO
+        if record.polling_place_id == "6162":
+            record = record._replace(polling_place_postcode="LS25 6HU")
+
+        return super().station_record_to_dict(record)
