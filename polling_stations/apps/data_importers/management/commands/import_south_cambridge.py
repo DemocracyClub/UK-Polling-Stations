e28aff40e23b0432c19a12139befad5b9fd80f8d
smsmith97 <sean.m.smith97@outlook.com>
2021-03-17 13:25:40 +0000

    import script for south cambridge (closes #3202)

--- polling_stations/apps/data_importers/management/commands/import_south_cambridge.py
+++ polling_stations/apps/data_importers/management/commands/import_south_cambridge.py
@@ -2,56 +2,33 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000012"
-    addresses_name = "parl.2019-12-12/Version 1/merged.tsv"
-    stations_name = "parl.2019-12-12/Version 1/merged.tsv"
-    elections = ["parl.2019-12-12"]
+    council_id = "SCA"
+    addresses_name = (
+        "2021-03-16T10:56:43.784190/South Cambridgeshire Democracy_Club__06May2021.tsv"
+    )
+    stations_name = (
+        "2021-03-16T10:56:43.784190/South Cambridgeshire Democracy_Club__06May2021.tsv"
+    )
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
-    allow_station_point_from_postcode = False
-
-    def station_record_to_dict(self, record):
-        if record.polling_place_id == "7342":
-            record = record._replace(polling_place_postcode="CB8 0XF")
-
-        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
-        if uprn in ["200001850767", "10091623496", "10091623497", "10091624790"]:
-            rec["postcode"] = "CB234NL"
-            rec["accept_suggestion"] = False
-
-        if uprn in ["100091203868"]:
+        if uprn in [
+            "10008082519",  # DWELLING AT REAR 43 CAMBRIDGE ROAD, WIMPOLE
+            "10033031432",  # BURY LANE HOUSE, BURY LANE, MELBOURN, ROYSTON
+            "10008079271",  # GREENGAGE COTTAGE, BURY LANE, MELBOURN, ROYSTON
+            "10094259590",  # CROWDEAN, CROWDEAN LANE, ELSWORTH, CAMBRIDGE
+            "100091203868",  # ANNEXE CLARE BARN MAIN STREET, CALDECOTE
+            "200001812525",  # THE GATE HOUSE, ST. NEOTS ROAD, DRY DRAYTON, CAMBRIDGE
+            "100091203790",  # FOSTERS LONG DROVE, COTTENHAM
+            "100090163907",  # 34 WOODFIELD ROAD, HIGHFIELDS CALDECOTE, CAMBRIDGE
+            "100090161038",  # 10 THE PASTURES, HARSTON, CAMBRIDGE
+        ]:
             return None
 
-        if uprn in [
-            "10003188510",  # CB259NW -> CB259PG : 1 Goose Hall Farm, Ely Road, Waterbeach, Cambridge
-            "10003183888",  # CB259PH -> CB259PW : School Farm House, Ely Road, Chittering, Cambridge
-            "10033037257",  # CB231AA -> CB231NF : 13 Wisbey`s Yard, Haslingfield, Cambridge
-            "10033037258",  # CB231AA -> CB231NF : 14 Wisbey`s Yard, Haslingfield, Cambridge
-            "10033037259",  # CB231AA -> CB231NF : 15 Wisbey`s Yard, Haslingfield, Cambridge
-            "10033037260",  # CB231AA -> CB231NF : 16 Wisbey`s Yard, Haslingfield, Cambridge
-            "100091204382",  # CB227NJ -> CB227NH : 2 New Farm, Royston Road, Harston, Cambridge
-            "100091204381",  # CB227NJ -> CB227NH : 1 New Farm, Royston Road, Harston, Cambridge
-            "10003195547",  # CB223AY -> CB223AE : Magog House, Cambridge Road, Babraham, Cambridge
-            "10003193988",  # SG85JR -> SG85JS : Calvisi, Old North Road, Bassingbourn, Royston
-            "10008082645",  # SG85JR -> SG85SR : Flat 4 Taunus, Old North Road, Bassingbourn, Royston
-            "10008080949",  # SG85JR -> SG85SR : Flat 2 Taunus, Old North Road, Bassingbourn, Royston
-            "10003193066",  # SG80QN -> SG85NT : St. Johnsbury, Bassingbourn Road, Litlington, Royston
-            "10003195581",  # SG80HP -> SG80HT : Penquite, Flecks Lane, Wendy Road, Shingay-Cum-Wendy, Royston
-            "10003202065",  # CB223XE -> CB223EH : Roxborough, 2A Brookfield Road, Sawston, Cambridge
-            "100091202269",  # CB225ED -> CB225BP : Stapleford Grange, Stapleford, Cambridge
-            "10003182031",  # CB89LD -> CB89LE : 171 Carlton Green Road, Carlton, Newmarket
-            "10003182035",  # CB89LD -> CB89LE : Rood Hall, Carlton Green Road, Carlton, Newmarket
-            "100091204465",  # CB215NE -> CB215PF : Wadlow Farm House, Six Mile Bottom Road, West Wratting, Cambridge
-        ]:
-            rec["accept_suggestion"] = True
+        if record.addressline6 in ["CB23 6LE", "CB24 4QG"]:
+            return None
 
-        if uprn in [
-            "100090161038"  # CB237XA -> CB227AD : 10 The Pastures, Hardwick, Cambridge
-        ]:
-            rec["accept_suggestion"] = False
-
-        return rec
+        return super().address_record_to_dict(record)
