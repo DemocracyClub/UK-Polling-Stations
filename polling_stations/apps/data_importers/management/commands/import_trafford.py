9b07cb18f6c080554af9fefc5dccbf668996f5a4
smsmith97 <sean.m.smith97@outlook.com>
2021-03-18 16:06:26 +0000

    import script for trafford (closes #3233)

--- polling_stations/apps/data_importers/management/commands/import_trafford.py
+++ polling_stations/apps/data_importers/management/commands/import_trafford.py
@@ -2,55 +2,44 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E08000009"
-    addresses_name = (
-        "parl.2019-12-12/Version 1/Democracy_Club__12December2019trafford.CSV"
-    )
-    stations_name = (
-        "parl.2019-12-12/Version 1/Democracy_Club__12December2019trafford.CSV"
-    )
-    elections = ["parl.2019-12-12"]
+    council_id = "TRF"
+    addresses_name = "2021-03-17T16:41:00.619202/Democracy_Club__06May2021.CSV"
+    stations_name = "2021-03-17T16:41:00.619202/Democracy_Club__06May2021.CSV"
+    elections = ["2021-05-06"]
     csv_delimiter = ","
-    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
-        if uprn == "200000333851":
-            rec["postcode"] = "M41 6AS"
-
-        if record.addressline1 == "78B High Elm Road":
-            rec["postcode"] = "WA15 0HX"
-
         if uprn in [
-            "10070405338",
-            "10070405338",
-            "100011646326",
-            "100011693362",
-            "100011622567",
-            "100011679026",
+            "10070444818",  # FOUR OAKS CARE HOME, 28 WOOD LANE, PARTINGTON, MANCHESTER
+            "100011622567",  # 52B ARDERNE ROAD, TIMPERLEY, ALTRINCHAM
+            "10070457195",  # 86 HEYES LANE, TIMPERLEY, ALTRINCHAM
+            "10070457196",  # 4A BROOMFIELD LANE, HALE
+            "200000333947",  # 161 STOCKPORT ROAD, TIMPERLEY, ALTRINCHAM
+            "100011646326",  # 5 SOUTH CROSTON STREET, MANCHESTER
+            "100011640670",  # 3 SOUTH CROSTON STREET, MANCHESTER
+            "10070457194",  # 238 AYRES ROAD, MANCHESTER
+            "100012699156",  # DOG BEAUTY, 96 PARK ROAD, TIMPERLEY, ALTRINCHAM
+            "10070408519",  # 7 SOUTH CROSTON STREET, MANCHESTER
+            "10070405338",  # 183A CROSS STREET, SALE
+            "100011637164",  # RAILWAY TAVERN, IRLAM ROAD, URMSTON, MANCHESTER
+            "100011632467",  # ST ANTONYS ROMAN CATHOLIC CHURCH ELEVENTH STREET, TRAFFORD PARK
+            "100012489906",  # VALE LODGE, GRANGE ROAD, BOWDON, ALTRINCHAM
+            "100011679026",  # 17 WOOD LANE, PARTINGTON, MANCHESTER
+            "100011693362",  # 300 MANOR AVENUE, SALE
         ]:
             return None
 
-        if uprn in [
-            "100012491068",  # WA144RJ -> WA145RJ : 1 Red House Cottages, Red House Lane, Dunham Massey
-            "10070444322",  # WA145UX -> WA145UZ : Apartment 1, 2 Badger Road, West Timperley, Altrincham
-            "10070444323",  # WA145UX -> WA145UZ : Apartment 2, 2 Badger Road, West Timperley, Altrincham
-            "10070444324",  # WA145UX -> WA145UZ : Apartment 3, 2 Badger Road, West Timperley, Altrincham
-            "100012495530",  # M314WJ -> M314NL : Carlton, 29 Warburton Lane, Partington
-            "200000333945",  # M416JU -> M416DU : 83 Irlam Road, Flixton
-            "100011674930",  # M416JS -> M416GT : 12 The Avenue, Flixton
-            "100012496466",  # M337WH -> M337QH : Bridge Inn, Dane Road, Sale]
+        if record.addressline6 in [
+            "M41 9JA",
+            "M33 5BA",
+            "WA14 4RJ",
+            "WA14 2AL",
+            "M33 2BT",
+            "M41 6JU",
+            "M33 6NH",
         ]:
-            rec["accept_suggestion"] = True
+            return None
 
-        if uprn in [
-            "10070460306",  # WA158UH -> WA158SZ : 3 Great Oak Drive, Altrincham
-            "200000340318",  # M337RE -> M337TY : Apartment 18, 100 Washway Road, Sale
-            "100011700864",  # M332UW -> M334RZ : 264 Norris Road, Sale
-            "10070456948",  # 31A Ludford Grove, Sale
-        ]:
-            rec["accept_suggestion"] = False
-
-        return rec
+        return super().address_record_to_dict(record)
