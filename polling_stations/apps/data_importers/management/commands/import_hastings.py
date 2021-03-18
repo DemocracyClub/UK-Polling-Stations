a499bfe7378fdf60bf062d11a85663a516db1270
smsmith97 <sean.m.smith97@outlook.com>
2021-03-18 15:12:18 +0000

    import script for hastings (closes #3232)

--- polling_stations/apps/data_importers/management/commands/import_hastings.py
+++ polling_stations/apps/data_importers/management/commands/import_hastings.py
@@ -1,22 +1,35 @@
-from data_importers.management.commands import BaseHalaroseCsvImporter
+from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
-class Command(BaseHalaroseCsvImporter):
-    council_id = "E07000062"
-    addresses_name = "local.2018-05-03/Version 1/polling_station_export-2018-03-12.csv"
-    stations_name = "local.2018-05-03/Version 1/polling_station_export-2018-03-12.csv"
-    elections = ["local.2018-05-03"]
+class Command(BaseXpressDemocracyClubCsvImporter):
+    council_id = "HAS"
+    addresses_name = "2021-03-17T16:31:21.505378/Democracy_Club__06May2021.tsv"
+    stations_name = "2021-03-17T16:31:21.505378/Democracy_Club__06May2021.tsv"
+    elections = ["2021-05-06"]
+    csv_delimiter = "\t"
     csv_encoding = "windows-1252"
 
     def address_record_to_dict(self, record):
-        if record.houseid == "3000023":
-            rec = super().address_record_to_dict(record)
-            rec["postcode"] = "TN38 0BN"
-            return rec
+        uprn = record.property_urn.strip().lstrip("0")
 
-        if record.houseid == "5000237":
-            rec = super().address_record_to_dict(record)
-            rec["postcode"] = "TN37 6BP"
-            return rec
+        if uprn in [
+            "100062577569",  # THE ROYAL ALBERT, 293 BATTLE ROAD, ST. LEONARDS-ON-SEA
+            "10002500023",  # BEECH VIEW, CAMPKIN GARDENS, ST. LEONARDS-ON-SEA
+            "10070607789",  # 120 BATTLE ROAD, ST. LEONARDS-ON-SEA
+            "100062577571",  # NUMBER 46 GUEST HOUSE, 46 CAMBRIDGE GARDENS, HASTINGS
+            "10002501567",  # 285 ELPHINSTONE ROAD, HASTINGS
+        ]:
+            return None
+
+        if record.addressline6 in ["TN38 9EL", "TN37 7JL"]:
+            return None
 
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+
+        # Downs Farm Community Centre Crowborough Road Hastings TN35 5EA
+        if record.polling_place_id == "887":
+            record = record._replace(polling_place_postcode="TN35 5EE")
+
+        return super().station_record_to_dict(record)
