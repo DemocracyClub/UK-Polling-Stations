19da9314a425e4d2e0e9009443bf3935bbf5a2e1
smsmith97 <sean.m.smith97@outlook.com>
2021-03-18 10:57:08 +0000

    import script for central bedfordshire (closes #3214)

--- polling_stations/apps/data_importers/management/commands/import_central_bedfordshire.py
+++ polling_stations/apps/data_importers/management/commands/import_central_bedfordshire.py
@@ -3,51 +3,59 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
 
-    council_id = "E06000056"
-    addresses_name = (
-        "parl.2019-12-12/Version 2/Democracy_Club__12December2019 polling stations.csv"
-    )
-    stations_name = (
-        "parl.2019-12-12/Version 2/Democracy_Club__12December2019 polling stations.csv"
-    )
-    elections = ["parl.2019-12-12"]
+    council_id = "CBF"
+    addresses_name = "2021-03-16T16:10:23.712198/Democracy_Club__06May2021.CSV"
+    stations_name = "2021-03-16T16:10:23.712198/Democracy_Club__06May2021.CSV"
+    elections = ["2021-05-06"]
     csv_delimiter = ","
-    allow_station_point_from_postcode = False
 
     def station_record_to_dict(self, record):
-
-        # Haynes Village Hall
-        if record.polling_place_id == "13358":
+        # Haynes Village Hall Northwood End Road Haynes Beds MK45 3QB
+        if record.polling_place_id == "14700":
             record = record._replace(polling_place_easting="510086")
             record = record._replace(polling_place_northing="242021")
+
+        # Flitton Church Hall Brook Lane Flitton Beds MK45 5EJ - removing misleading point
+        if record.polling_place_id == "14663":
+            record = record._replace(polling_place_easting="")
+            record = record._replace(polling_place_northing="")
 
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
-        # most of these UPRNs are junk
-        if uprn.endswith("0000000"):
-            rec["uprn"] = ""
-
-        if record.addressline1 == "Meadow Lake":
-            rec["postcode"] = "SG191NU"
-        if record.addressline1 in ["4 Bunch O`nuts", "3 Bunch O`nuts"]:
-            rec["postcode"] = "LU5 5DX"
-
         if uprn in [
-            "10093351429",
-            "10014618845",
+            "100080999932",  # 4 BREWERS HILL ROAD, DUNSTABLE
+            "10094352855",  # 11 STRATTON, MARSTON MORETAINE, BEDFORD
+            "10094352856",  # 12 BREWERS HILL ROAD, DUNSTABLE
+            "10094352858",  # 10 BREWERS HILL ROAD, DUNSTABLE
+            "10014618845",  # 6 BREWERS HILL ROAD, DUNSTABLE
+            "10094352859",  # 8 BREWERS HILL ROAD, DUNSTABLE
+            "10094352857",  # 2 BREWERS HILL ROAD, DUNSTABLE
+            "100081002043",  # 2A BREWERS HILL ROAD, DUNSTABLE
+            "10094352860",  # 12A BREWERS HILL ROAD, DUNSTABLE
+            "10094352854",  # LONDON LODGE, WOBURN, MILTON KEYNES
+            "10094352861",  # PARK FARM, HAZELWOOD LANE, AMPTHILL, BEDFORD
+            "100081002361",  # THE BUTTERFLY, GREAT NORTH ROAD, SANDY
         ]:
             return None
 
-        if uprn in [
-            "10000861447",  # MK430LN -> MK430LP : 110B Lower Shelton Road, Marston Moretaine, Beds
-            "10000799231",  # MK430LN -> MK430LP : 110 Lower Shelton Road, Marston Moretaine, Beds
-            "10002278170",  # LU63RG -> LU63JH : Aveldone House, 70-74 Common Road, Kensworth, Dunstable, Beds.
-            "10001023325",  # LU55AD -> LU55ES : 48A Houghton Road, Dunstable, Beds.
+        if record.addressline6 in [
+            "SG18 8GF",
+            "SG5 4PS",
+            "SG18 9TA",
+            "LU6 3NB",
+            "MK45 1QX",
+            "SG18 8JD",
+            "MK45 1XL",
+            "SG18 9SY",
+            "MK45 1AX",
+            "SG18 0JL",
+            "MK45 1QZ",
+            "MK45 1JA",
+            "LU7 4AY",
         ]:
-            rec["accept_suggestion"] = False
+            return None
 
-        return rec
+        return super().address_record_to_dict(record)
