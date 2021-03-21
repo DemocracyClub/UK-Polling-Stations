2d5a249c9789ed71dbf5b1a96b50479a971f5588
GeoWill <roper.william@gmail.com>
2021-03-21 09:45:13 +0000

    Import script for South Norfolk (closes #3108)

--- polling_stations/apps/data_importers/management/commands/import_south_norfolk.py
+++ polling_stations/apps/data_importers/management/commands/import_south_norfolk.py
@@ -1,40 +1,14 @@
-from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
+from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 
-class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000149"
-    addresses_name = "parl.2019-12-12/Version 2/Democracy_Club__12December2019.tsv"
-    stations_name = "parl.2019-12-12/Version 2/Democracy_Club__12December2019.tsv"
-    elections = ["parl.2019-12-12"]
-    csv_delimiter = "\t"
-    allow_station_point_from_postcode = False
+class Command(BaseDemocracyCountsCsvImporter):
+    council_id = "SNO"
+    addresses_name = "2021-03-04T15:54:30.296900/South Norfolk Democracy Club - Polling Districts - Election ID 7 County Council.csv"
+    stations_name = "2021-03-04T15:54:30.296900/South Norfolk Democracy Club - Polling Stations - Election ID 7 County Council.csv"
+    elections = ["2021-05-06"]
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if record.addressline6 == "NR18 9FB":
+        if record.postcode in ["IP21 4QR", "IP22 4JW", "NR18 0TR"]:
             return None
 
-        if (record.addressline1, record.addressline6) in [
-            ("Acers", "NR9 3LT"),
-            ("24 Bank Street", "NR18 0BX"),
-        ]:
-            return None
-
-        if record.addressline6 in [
-            "NR14 6TF",
-            "NR14 7UU",
-        ]:
-            return None
-
-        if uprn in [
-            "2630159192",
-            "2630101566",
-            "2630106361",
-            "2630164300",
-            "2630122694",
-        ]:
-            rec["accept_suggestion"] = True
-
-        return rec
+        return super().address_record_to_dict(record)
