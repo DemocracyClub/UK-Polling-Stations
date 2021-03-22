cb9959c4570ee7769facc4bec15f172ad0e4be36
GeoWill <roper.william@gmail.com>
2021-03-22 22:09:25 +0000

    Import script for Barrow in Furness (closes #3127)

--- polling_stations/apps/data_importers/management/commands/import_barrow_in_furness.py
+++ polling_stations/apps/data_importers/management/commands/import_barrow_in_furness.py
@@ -2,36 +2,25 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 
 class Command(BaseDemocracyCountsCsvImporter):
-    council_id = "E07000027"
-    addresses_name = "parl.2019-12-12/Version 1/Democracy Club - Polling Districts.csv"
-    stations_name = "parl.2019-12-12/Version 1/Democracy Club - Polling Stations.csv"
-    elections = ["parl.2019-12-12"]
+    council_id = "BAR"
+    addresses_name = "2021-03-08T19:54:15.110811/Barrow in Furness Democracy Club - Polling Districts (2).csv"
+    stations_name = "2021-03-08T19:54:15.110811/Barrow in Furness Democracy Club - Polling Stations (2).csv"
+    elections = ["2021-05-06"]
     allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
-        uprn = record.uprn.strip().lstrip("0")
+        if record.postcode in ["LA13 9SF", "LA13 0NF"]:
+            return None
 
-        if uprn in [
-            "36016015",  # LA141DU -> LA145AY : 26 Crellin Street, Barrow-in-Furness
-            "36025851",  # LA139HY -> LA139LT : 81 Newbarns Village, Hollow Lane, Barrow-in-Furness
-            "36047048",  # LA130UF -> LA142UJ : 7 Rosewood Grove, Barrow-in-Furness
-            "36030436",  # LA158BQ -> LA158BD : 43 Mount Pleasant, Skelgate, Dalton-in-Furness
-            "36034298",  # LA130PD -> LA130PB : North Lodge, Abbey Road, Dalton-in-Furness
-            "36031188",  # LA130NF -> LA130NX : The Bungalow, Greystone Lane, Dalton-in-Furness
-            "36031184",  # LA130NF -> LA130ED : North Stank Farm, Newton Cross Road, Newton-in-Furness
-            "36033063",  # LA158RP -> LA158HY : 40 Bankside, Broughton Road, Dalton-in-Furness
-        ]:
-            rec["accept_suggestion"] = False
-
-        return rec
+        return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
-
+        # ASKAM COMMUNITY CENTRE
         if record.stationcode in ["LC/1", "LC/2"]:
             record = record._replace(xordinate="321436")
             record = record._replace(yordinate="477564")
 
+        # VICKERSTOWN METHODIST CHURCH
         if record.stationcode == "AA/1":
             record = record._replace(xordinate="318608")
             record = record._replace(yordinate="468975")
