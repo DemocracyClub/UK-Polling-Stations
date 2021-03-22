0ab787da73bdb8fdfd6a95192bf4861a982a964c
smsmith97 <sean.m.smith97@outlook.com>
2021-03-22 12:18:30 +0000

    import script for aylesbury_vale (closes #3246)

--- polling_stations/apps/data_importers/management/commands/import_aylesbury_vale.py
+++ polling_stations/apps/data_importers/management/commands/import_aylesbury_vale.py
@@ -2,13 +2,32 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000004"
-    addresses_name = (
-        "parl.2019-12-12/Version 1/Aylesbury Vale DC Democracy_Club__12December2019.TSV"
-    )
-    stations_name = (
-        "parl.2019-12-12/Version 1/Aylesbury Vale DC Democracy_Club__12December2019.TSV"
-    )
-    elections = ["parl.2019-12-12"]
+    council_id = "AYL"
+    addresses_name = "2021-03-18T17:32:05.640816/Bucks_dedupe.tsv"
+    stations_name = "2021-03-18T17:32:05.640816/Bucks_dedupe.tsv"
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
-    allow_station_point_from_postcode = False
+
+    def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "766039022",  # MANOR FARM, HILLESDEN ROAD, GAWCOTT, BUCKINGHAM
+            "766258964",  # LOWER FARM, BERRYFIELDS GATED ROAD, QUARRENDON, AYLESBURY
+            "10095501236",  # NARROWBOAT GENTLY BENTLEY, AYLESBURY BASIN, EXCHANGE STREET, AYLESBURY
+            "10095500186",  # 35 YORK PLACE, AYLESBURY
+            "766297815",  # THE WHITELEAF CENTRE, BIERTON ROAD, AYLESBURY
+            "766259070",  # NB ODIN AYLESBURY BASIN EXCHANGE STREET, AYLESBURY
+        ]:
+            return None
+
+        if record.addressline6 in [
+            "MK18 1PJ",
+            "HP21 9HY",
+            "MK17 0EW",
+            "HP20 1UR",
+            "MK18 3JZ",
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
