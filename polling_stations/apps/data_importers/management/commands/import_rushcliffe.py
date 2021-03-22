24daf50d5d9f4f96e77f99522203597b40fd354b
smsmith97 <sean.m.smith97@outlook.com>
2021-03-22 10:37:39 +0000

    import script for rushcliffe (closes #3242)

--- polling_stations/apps/data_importers/management/commands/import_rushcliffe.py
+++ polling_stations/apps/data_importers/management/commands/import_rushcliffe.py
@@ -2,30 +2,43 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000176"
-    addresses_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019.tsv"
-    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019.tsv"
-    elections = ["parl.2019-12-12"]
+    council_id = "RUS"
+    addresses_name = "2021-03-18T15:04:35.902256/Democracy_Club__06May2021.tsv"
+    stations_name = "2021-03-18T15:04:35.902256/Democracy_Club__06May2021.tsv"
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
 
-        # these co-ordinates were a missing a digit - fixes carried over from local.2019-05-02
-        if record.polling_place_id == "4566":  # Bingham Methodist Centre
+        # Bingham Methodist Centre, Union Street, Bingham, Nottingham
+        if record.polling_place_id == "5064":
             record = record._replace(polling_place_easting="470360")
-        if record.polling_place_id == "4660":  # Edwalton Church Hall
+
+        # Edwalton Church Hall, Vicarage Green, Edwalton
+        if record.polling_place_id == "5244":
             record = record._replace(polling_place_easting="459770")
-        if record.polling_place_id == "4785":  # West Bridgford Baptist Church
+
+        # West Bridgford Baptist Church, Melton Road, West Bridgford
+        if record.polling_place_id == "5252":
             record = record._replace(polling_place_easting="458380")
 
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
-        if uprn == "3040072549":
-            rec["postcode"] = "NG2 7PQ"
-            rec["accept_suggestion"] = False
+        if uprn in [
+            "3040046829",  # WILLOW BANK, ZOUCH FARM, MAIN STREET, ZOUCH, LOUGHBOROUGH
+            "3040055436",  # 1A THE COTTAGE, GOTHAM ROAD, EAST LEAKE, LOUGHBOROUGH
+            "3040057547",  # WEST END, LANGAR ROAD, BARNSTONE, NOTTINGHAM
+            "3040046757",  # WINDY RIDGE, LANGAR ROAD, BARNSTONE, NOTTINGHAM
+            "3040052762",  # DUKERIES TEXTILES, 15A MELBOURNE ROAD, WEST BRIDGFORD, NOTTINGHAM
+            "3040012196",  # 11 WILLOW ROAD, COTGRAVE, NOTTINGHAM
+            "3040072549",  # PROPERTY ON FIRST AND SECOND FLOOR 43 MUSTERS ROAD, WEST BRIDGFORD
+        ]:
+            return None
 
-        return rec
+        if record.addressline6 in ["NG11 6NY", "NG2 5JT"]:
+            return None
+
+        return super().address_record_to_dict(record)
