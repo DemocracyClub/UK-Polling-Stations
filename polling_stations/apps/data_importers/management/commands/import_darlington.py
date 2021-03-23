ff69d70470a89f062a5049638fdc914d47ff257b
smsmith97 <sean.m.smith97@outlook.com>
2021-03-23 14:17:15 +0000

    import script for darlington (closes #3258)

--- polling_stations/apps/data_importers/management/commands/import_darlington.py
+++ polling_stations/apps/data_importers/management/commands/import_darlington.py
@@ -3,20 +3,16 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E06000005"
-    addresses_name = (
-        "parl.2019-12-12/Version 1/Democracy_Club__12December2019darling.tsv"
-    )
-    stations_name = (
-        "parl.2019-12-12/Version 1/Democracy_Club__12December2019darling.tsv"
-    )
-    elections = ["parl.2019-12-12"]
+    council_id = "DAL"
+    addresses_name = "2021-03-19T13:08:53.439791/darlington.gov.uk-1616156390000-.tsv"
+    stations_name = "2021-03-19T13:08:53.439791/darlington.gov.uk-1616156390000-.tsv"
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
-    allow_station_point_from_postcode = False
 
     def station_record_to_dict(self, record):
-
-        if record.polling_place_id == "5634":  #  The Reading Room
+        # The Reading Room Neasham Darlington DL2 1PH
+        if record.polling_place_id == "6423":
+            record = record._replace(polling_place_postcode="DL2 1QX")
             rec = super().station_record_to_dict(record)
             rec["location"] = Point(-1.49308179, 54.48766450, srid=4326)
             return rec
@@ -24,26 +20,18 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
-        if uprn == "10013318194":  # Suspicious postcode
+        if uprn in [
+            "10003083588",  # ROCK COTTAGE, PIERCEBRIDGE, DARLINGTON
+            "10003083581",  # DENTON GRANGE WEST, PIERCEBRIDGE, DARLINGTON
+            "10003083582",  # DENTON GRANGE WEST COTTAGE, PIERCEBRIDGE, DARLINGTON
+            "10003082878",  # PRO SEW, 98 ELDON STREET, DARLINGTON
+            "10013318194",  # 28A YARM ROAD, DARLINGTON
+        ]:
             return None
 
-        if uprn in [
-            "10013315063",  # DL14EP -> DL14ER : Willow Green Care Home, Eastbourne Road, Darlington
-            "200002723491",  # DL22XJ -> DL22UF : Elm Bank Cottage, Houghton Bank, Heighington (Part)
-        ]:
-            rec["accept_suggestion"] = True
+        if record.addressline6 in ["DL2 2XX"]:
+            return None
 
-        if uprn in [
-            "10013312294",  # DL13JU -> DL21RL : Little Burdon Farm, Sadberge
-            "10013312299",  # DL13JU -> DL21RL : Little Burdon Farm Cottage, Sadberge
-            "200002722478",  # DL13LA -> DL13LB : Orchard House, The Green, Brafferton
-            "100110749824",  # DL21QB -> DL21QG : Ivy House, Snipe Lane, Hurworth
-            "100110718839",  # DL21QB -> DL22SA : Blackwell Moor Farm, Snipe Lane, Hurworth
-            "10013315658",  # DL37LU -> DL11TZ : Managers Accommodation, 17 Post House Wynd, Darlington
-        ]:
-            rec["accept_suggestion"] = False
-
-        return rec
+        return super().address_record_to_dict(record)
