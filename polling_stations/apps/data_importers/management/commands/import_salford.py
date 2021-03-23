ad822fcb2b8eeb6d386f1ef8a2bb01685871e5ab
GeoWill <roper.william@gmail.com>
2021-03-23 18:46:16 +0000

    Import script for Salford

--- polling_stations/apps/data_importers/management/commands/import_salford.py
+++ polling_stations/apps/data_importers/management/commands/import_salford.py
@@ -6,8 +6,8 @@ class Command(BaseGitHubImporter):
 
     srid = 4326
     districts_srid = 4326
-    council_id = "E08000006"
-    elections = ["2020-05-07"]
+    council_id = "SLF"
+    elections = ["2021-05-06"]
     scraper_name = "wdiv-scrapers/DC-PollingStations-Salford"
     geom_type = "geojson"
 
