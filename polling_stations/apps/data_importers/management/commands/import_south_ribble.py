ae6b2da581332ca42b800e8d88212bc316864bd5
GeoWill <roper.william@gmail.com>
2021-03-23 08:36:00 +0000

    Didn't git add before committing

--- polling_stations/apps/data_importers/management/commands/import_south_ribble.py
+++ polling_stations/apps/data_importers/management/commands/import_south_ribble.py
@@ -10,8 +10,8 @@ class Command(BaseHalaroseCsvImporter):
         "2021-03-09T23:18:28.806724/South Ribble polling_station_export-2021-03-09.csv"
     )
     elections = ["2021-05-06"]
-    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
         if record.housepostcode in ["PR5 5AU", "PR5 4TB"]:
             return None
+        return super().address_record_to_dict(record)
