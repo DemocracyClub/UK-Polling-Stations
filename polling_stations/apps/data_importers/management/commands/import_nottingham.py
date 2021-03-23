9e877d7ab115d77152ae1a7b8d217603e11ed57b
GeoWill <roper.william@gmail.com>
2021-03-23 17:55:28 +0000

    Import script for Nottingham

--- polling_stations/apps/data_importers/management/commands/import_nottingham.py
+++ polling_stations/apps/data_importers/management/commands/import_nottingham.py
@@ -5,8 +5,8 @@ class Command(BaseGitHubImporter):
 
     srid = 4326
     districts_srid = 4326
-    council_id = "E06000018"
-    elections = ["2020-05-07"]
+    council_id = "NGM"
+    elections = ["2021-05-06"]
     scraper_name = "wdiv-scrapers/DC-PollingStations-Nottingham"
     geom_type = "geojson"
 
