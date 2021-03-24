a58ad947ca6b1c261c409bae86ba29171b382a03
smsmith97 <sean.m.smith97@outlook.com>
2021-03-24 12:29:13 +0000

    import script for vale of white horse (closes #3192)

--- polling_stations/apps/data_importers/management/commands/import_vale_of_white_horse.py
+++ polling_stations/apps/data_importers/management/commands/import_vale_of_white_horse.py
@@ -2,20 +2,53 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000180"
-    # single file merged for South Oxfordshire and Value of White Horse
-    # we'll split out the bits we need
-    addresses_name = (
-        "parl.2019-12-12/Version 1/Democracy_Club__12December2019vale and ox.CSV"
-    )
-    stations_name = (
-        "parl.2019-12-12/Version 1/Democracy_Club__12December2019vale and ox.CSV"
-    )
-    elections = ["parl.2019-12-12"]
-    csv_delimiter = ","
-    allow_station_point_from_postcode = False
+    council_id = "VAL"
+    addresses_name = "2021-03-18T11:03:17.493346/UPDATED Democracy_Club__06May2021.TSV"
+    stations_name = "2021-03-18T11:03:17.493346/UPDATED Democracy_Club__06May2021.TSV"
+    elections = ["2021-05-06"]
+    csv_delimiter = "\t"
+    csv_encoding = "windows-1252"
+
+    def address_record_to_dict(self, record):
+        # discard records in South Oxfordshire
+        if record.polling_place_district_reference.startswith(
+            "L"
+        ) or record.polling_place_district_reference.startswith("R"):
+            return None
+
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "10014028754",  # CHURCH PATH FARM, FARINGDON
+            "10014025441",  # CROSS BARGAIN COTTAGE, KINGSTON LISLE, WANTAGE
+            "10093199254",  # FLAT 2 12 BATH STREET, ABINGDON
+            "10014070551",  # GINGE MANOR STABLES, GINGE, WANTAGE
+            "10093199253",  # FLAT 1 12 BATH STREET, ABINGDON
+        ]:
+            return None
+
+        if record.addressline6 in [
+            "OX11 6DA",
+            "OX11 6DG",
+            "OX11 9NX",
+            "OX13 5GW",
+            "SN7 7SX",
+            "OX11 6EQ",
+            "SN7 8DJ",
+            "OX14 5DL",
+            "OX13 5PS",
+            "OX14 4DS",
+            "OX12 7FS",
+            "OX13 5GL",
+            "OX14 4BF",
+            "SN7 8QH",
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
+
         # discard records in South Oxfordshire
         if record.polling_place_district_reference.startswith(
             "L"
@@ -26,34 +59,16 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         if record.polling_place_id in ["8409"]:
             record = record._replace(polling_place_easting="0")
             record = record._replace(polling_place_northing="0")
+        # Church of The Holy Ascension Littleworth Faringdon
+        if record.polling_place_id == "11738":
+            record = record._replace(polling_place_easting="431246")
+            record = record._replace(polling_place_northing="197151")
+            record = record._replace(polling_place_postcode="SN7 8EF")
+
+        # Radley Church Room Church Road Radley OX14 2JE
+        if record.polling_place_id == "11228":
+            record = record._replace(polling_place_easting="452231")
+            record = record._replace(polling_place_northing="199390")
+            record = record._replace(polling_place_postcode="OX14 3QF")
 
         return super().station_record_to_dict(record)
-
-    def address_record_to_dict(self, record):
-        # discard records in South Oxfordshire
-        if record.polling_place_district_reference.startswith(
-            "L"
-        ) or record.polling_place_district_reference.startswith("R"):
-            return None
-
-        rec = super().address_record_to_dict(record)
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "10093195669"  # OX29AH -> OX29PH : 136, Apt 2, Cumnor Hill, Cumnor, Oxford, Oxfordshire
-        ]:
-            rec["accept_suggestion"] = True
-
-        if uprn == "10014079091":
-            rec["postcode"] = "OX12 0ED"
-
-        if uprn == "10014027599":
-            rec["postcode"] = "OX12 8AU"
-
-        if record.post_code == "SN7 8FG":
-            return None
-
-        if uprn == "10014082062":
-            return None
-
-        return rec
