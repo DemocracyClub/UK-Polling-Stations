cf7c0d4dc4c34fa710020344b33a2968385cd66e
smsmith97 <sean.m.smith97@outlook.com>
2021-03-24 16:32:35 +0000

    import script for harrogate (closes #3257)

--- polling_stations/apps/data_importers/management/commands/import_harrogate.py
+++ polling_stations/apps/data_importers/management/commands/import_harrogate.py
@@ -2,29 +2,49 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000165"
-    addresses_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019harro.tsv"
-    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019harro.tsv"
-    elections = ["parl.2019-12-12"]
-    csv_delimiter = "\t"
-    allow_station_point_from_postcode = False
-
-    def station_record_to_dict(self, record):
-
-        # Samwaies Hall
-        if record.polling_place_id == "13135":
-            record = record._replace(polling_place_postcode="")
-
-        return super().station_record_to_dict(record)
+    council_id = "HAG"
+    addresses_name = (
+        "2021-03-24T11:03:45.408151/Democracy_Club__06May2021 amendments.CSV"
+    )
+    stations_name = (
+        "2021-03-24T11:03:45.408151/Democracy_Club__06May2021 amendments.CSV"
+    )
+    elections = ["2021-05-06"]
+    csv_delimiter = ","
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
-        rec = super().address_record_to_dict(record)
 
-        if uprn == "100052009106":
-            rec["postcode"] = "HG3 5AT"
-
-        if record.addressline6.strip() == "YO7 4ED":
+        if uprn in [
+            "100050407904",  # GROVE HOUSE BUNGALOW, SKIPTON ROAD, HARROGATE
+            "100050429604",  # HAWTHORNS, STUDLEY ROAD, RIPON
+            "10093034565",  # OAK HOUSE PENNY POT LANE TO CENTRAL HOUSE FARM, HAMPSTHWAITE
+        ]:
             return None
 
-        return rec
+        if record.addressline6 in [
+            "HG3 5QF",
+            "HG3 3AT",
+            "HG3 3JQ",
+            "HG2 9LJ",
+            "HG1 4JW",
+            "HG2 9NW",
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # North Stainley Village Hall North Stainley Ripon
+        if record.polling_place_id == "14714":
+            record = record._replace(polling_place_postcode="HG4 3JT")
+
+        # Samwaies Hall Main Street Wath Ripon HG4 5AT
+        if record.polling_place_id == "14943":
+            record = record._replace(polling_place_postcode="HG4 5ET")
+
+        # Dishforth Village Hall Main Street Dishforth
+        if record.polling_place_id == "14932":
+            record = record._replace(polling_place_postcode="YO7 3JU")
+
+        return super().station_record_to_dict(record)
