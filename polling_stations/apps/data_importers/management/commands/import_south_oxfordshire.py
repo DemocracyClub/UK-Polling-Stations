79607fc0a596caad186c89a30fc3d29dc657cc46
smsmith97 <sean.m.smith97@outlook.com>
2021-03-24 11:15:40 +0000

    import script for south oxfordshire (closes #3190)

--- polling_stations/apps/data_importers/management/commands/import_south_oxfordshire.py
+++ polling_stations/apps/data_importers/management/commands/import_south_oxfordshire.py
@@ -2,48 +2,46 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000179"
-    # single file merged for South Oxfordshire and Value of White Horse
-    # we'll split out the bits we need
-    addresses_name = (
-        "parl.2019-12-12/Version 1/Democracy_Club__12December2019vale and ox.CSV"
-    )
-    stations_name = (
-        "parl.2019-12-12/Version 1/Democracy_Club__12December2019vale and ox.CSV"
-    )
-    elections = ["parl.2019-12-12"]
-    csv_delimiter = ","
-    allow_station_point_from_postcode = False
-
-    def station_record_to_dict(self, record):
-        # discard records in Vale of White Horse
-        if record.polling_place_district_reference.startswith(
-            "S"
-        ) or record.polling_place_district_reference.startswith("X"):
-            return None
-
-        if record.polling_place_id == "8737":
-            record = record._replace(polling_place_uprn="")
-
-        return super().station_record_to_dict(record)
+    council_id = "SOX"
+    addresses_name = "2021-03-18T11:05:52.448256/UPDATED Democracy_Club__06May2021.TSV"
+    stations_name = "2021-03-18T11:05:52.448256/UPDATED Democracy_Club__06May2021.TSV"
+    elections = ["2021-05-06"]
+    csv_delimiter = "\t"
+    csv_encoding = "windows-1252"
 
     def address_record_to_dict(self, record):
-        # discard records in Vale of White Horse
-        if record.polling_place_district_reference.startswith(
-            "S"
-        ) or record.polling_place_district_reference.startswith("X"):
-            return None
-
-        rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
-        if record.post_code == "OX49 5BZ" or uprn == "100120897264":
+        if uprn in [
+            "100121305054",  # THE OXFORDSHIRE, RYCOTE LANE, MILTON COMMON, THAME
+            "100120895687",  # THE OLD PUMP HOUSE, KINGSEY ROAD, THAME
+        ]:
             return None
 
-        if uprn == "100120888662":
-            rec["postcode"] = "OX14 3DX"
+        if record.addressline6 in [
+            "OX11 8JW",
+            "OX11 9EW",
+            "RG9 2HZ",
+            "RG9 4PX",
+            "OX11 7PT",
+            "OX11 6DE",
+            "OX10 0FJ",
+            "RG8 0BS",
+            "OX11 8RE",
+            "OX9 2AD",
+            "OX11 8AU",
+            "OX11 7DT",
+            "OX11 6JL",
+            "OX11 6ES",
+        ]:
+            return None
 
-        if uprn == "100120860708":
-            rec["postcode"] = "OX11 9UX"
+        return super().address_record_to_dict(record)
 
-        return rec
+    def station_record_to_dict(self, record):
+        # Thame Snooker Club 1a Saint Andrew Court Wellington Street Thame OX9 3WT
+        if record.polling_place_id == "11459":
+            record = record._replace(polling_place_easting="")
+            record = record._replace(polling_place_northing="")
+
+        return super().station_record_to_dict(record)
