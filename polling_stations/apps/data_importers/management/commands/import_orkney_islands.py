9b6fa4d650a7fab93099a7f16e3fed89ae02769c
smsmith97 <sean.m.smith97@outlook.com>
2021-03-24 16:53:07 +0000

    import script for orkney islands (closes #3312)

--- polling_stations/apps/data_importers/management/commands/import_orkney_islands.py
+++ polling_stations/apps/data_importers/management/commands/import_orkney_islands.py
@@ -1,49 +1,13 @@
-from data_importers.management.commands import BaseScotlandSpatialHubImporter
+from data_importers.management.commands import BaseHalaroseCsvImporter
 
 
-station_map = {
-    "134013877": [
-        "AA",
-        "BA",
-        "CD",
-        "CE",
-        "EB",
-        "FA",
-        "FB",
-        "FC",
-        "FD",
-        "FE",
-        "FF",
-        "FG",
-        "FH",
-    ],
-    "134010608": ["BB"],
-    "134017823": ["CA", "CB", "CC"],
-    "134014778": ["DA"],
-    "134016678": ["DB"],
-    "134015948": ["DC"],
-    "134017419": ["DD"],
-    "134014556": ["EA"],
-    "134015074": ["EC"],
-    "134014675": ["ED"],
-}
-
-
-class Command(BaseScotlandSpatialHubImporter):
-    council_id = "S12000023"
-    council_name = "Orkney Islands"
-    elections = ["parl.2019-12-12"]
-
-    def station_record_to_dict(self, record):
-        council_name = self.parse_string(record[2])
-        if council_name != self.council_name:
-            return None
-
-        codes = station_map[self.parse_string(record[4])]
-        address = self.parse_string(record[3])
-        stations = []
-        for code in codes:
-            stations.append(
-                {"internal_council_id": code, "postcode": "", "address": address}
-            )
-        return stations
+class Command(BaseHalaroseCsvImporter):
+    council_id = "ORK"
+    addresses_name = (
+        "2021-03-23T12:13:35.698255/Orkney polling_station_export-2021-03-23.csv"
+    )
+    stations_name = (
+        "2021-03-23T12:13:35.698255/Orkney polling_station_export-2021-03-23.csv"
+    )
+    elections = ["2021-05-06"]
+    csv_delimiter = ","
