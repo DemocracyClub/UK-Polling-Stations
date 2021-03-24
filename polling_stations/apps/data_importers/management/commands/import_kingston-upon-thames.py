ab28e37b7ede2c21d145fd0dbede138d05bdfff1
GeoWill <roper.william@gmail.com>
2021-03-24 18:01:27 +0000

    Import script for Kingston upon Thames

--- polling_stations/apps/data_importers/management/commands/import_kingston-upon-thames.py
+++ polling_stations/apps/data_importers/management/commands/import_kingston-upon-thames.py
@@ -1,8 +1,32 @@
-from data_importers.management.commands import BaseDemocracyCountsCsvImporter
+from data_importers.github_importer import BaseGitHubImporter
 
 
-class Command(BaseDemocracyCountsCsvImporter):
-    council_id = "E09000021"
-    stations_name = "parl.2019-12-12/Version 1/stations-merged.csv"
-    addresses_name = "parl.2019-12-12/Version 1/addresses-merged.csv"
-    elections = ["parl.2019-12-12"]
+class Command(BaseGitHubImporter):
+    srid = 4326
+    districts_srid = 4326
+    council_id = "KTT"
+    elections = ["2021-05-06"]
+    scraper_name = "wdiv-scrapers/DC-PollingStations-Kingston"
+    geom_type = "geojson"
+
+    def district_record_to_dict(self, record):
+        poly = self.extract_geometry(record, self.geom_type, self.get_srid("districts"))
+
+        return {
+            "internal_council_id": record["CODE"],
+            "name": record["CODE"],
+            "area": poly,
+            "polling_station_id": record["CODE"],
+        }
+
+    def station_record_to_dict(self, record):
+        location = self.extract_geometry(
+            record, self.geom_type, self.get_srid("stations")
+        )
+
+        return {
+            "internal_council_id": record["CODE"],
+            "address": record["ADDRESS"],
+            "postcode": "",
+            "location": location,
+        }
