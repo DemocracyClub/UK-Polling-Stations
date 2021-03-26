075057b847a25a629a3d5b7a786c4750bec27619
GeoWill <roper.william@gmail.com>
2021-03-26 12:21:53 +0000

    Import script for Barnet (closes #3355)

--- polling_stations/apps/data_importers/management/commands/import_barnet.py
+++ polling_stations/apps/data_importers/management/commands/import_barnet.py
@@ -2,30 +2,33 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 
 class Command(BaseDemocracyCountsCsvImporter):
-    council_id = "E09000003"
-    addresses_name = (
-        "parl.2019-12-12/Version 2/Democracy Club - Polling Districts_UKPGE.csv"
-    )
-    stations_name = "parl.2019-12-12/Version 2/Democracy Club - Polling Stations_2.csv"
-    elections = ["parl.2019-12-12"]
-    allow_station_point_from_postcode = False
+    council_id = "BNE"
+    addresses_name = "2021-03-25T11:42:15.781785/Democracy Club_Polling Districts.csv"
+    stations_name = "2021-03-25T11:42:15.781785/Democracy Club_Polling Stations.csv"
+    elections = ["2021-05-06"]
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
         uprn = record.uprn.strip().lstrip("0")
-
-        if uprn == "200196535":
-            return None
-
         if record.postcode.strip() in [
-            "N3 1QT",
-            "NW2 2NA",
+            "NW7 4SB",
+            "HA8 7LZ",
             "N11 3DD",
             "EN4 8QZ",
-            "EN4 8TF",
-            "N12 7FB",
-            "N12 7EJ",
+            "NW11 7ND",
         ]:
             return None
 
-        return rec
+        if uprn in [
+            "10091038621",  # FLAT 1 129 HENDON WAY, LONDON
+            "10091038625",  # FLAT 5 129 HENDON WAY, LONDON
+            "10091038623",  # FLAT 3 129 HENDON WAY, LONDON
+            "10091041237",  # FLAT 3, 238 EAST BARNET ROAD, BARNET
+            "10091041238",  # FLAT 4, 238 EAST BARNET ROAD, BARNET
+            "200163833",  # FLAT 1 41 THE GROVE, FINCHLEY, LONDON
+            "200163834",  # FLAT 2 41 THE GROVE, FINCHLEY, LONDON
+            "200163838",  # FLAT 6 41 THE GROVE, FINCHLEY, LONDON
+            "200196535",  # 1 NINA WALK, LONDON, N20 0RB
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
