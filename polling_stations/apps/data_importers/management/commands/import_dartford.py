b9e5b4a12c20639497846ac3afcf9557757eaf04
smsmith97 <sean.m.smith97@outlook.com>
2021-03-30 14:10:52 +0100

    import script for dartford (closes #3163)

--- polling_stations/apps/data_importers/management/commands/import_dartford.py
+++ polling_stations/apps/data_importers/management/commands/import_dartford.py
@@ -2,34 +2,32 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 
 class Command(BaseHalaroseCsvImporter):
-    council_id = "E07000107"
-    addresses_name = "parl.2019-12-12/Version 1/polling_station_export-2019-11-07.csv"
-    stations_name = "parl.2019-12-12/Version 1/polling_station_export-2019-11-07.csv"
-    elections = ["parl.2019-12-12"]
-    allow_station_point_from_postcode = False
-
-    def station_record_to_dict(self, record):
-        # These are single records listed as 'Other Electors Address'
-        # no addresses are assigned. So probably don't cause problems, but
-        # dropping them to squash warnings.
-        if (record.pollingstationnumber, record.pollingstationname) in [
-            ("63", "Hodsoll Street & Ridley Village Hall"),
-            ("64", "Longfield & Hartley Scout HQ"),
-            ("66", "Fawkham and Hartley Church Centre"),
-        ]:
-            return None
-        return super().station_record_to_dict(record)
+    council_id = "DAR"
+    addresses_name = "2021-03-12T12:25:27.291565/polling_station_export-2021-03-12.csv"
+    stations_name = "2021-03-12T12:25:27.291565/polling_station_export-2021-03-12.csv"
+    elections = ["2021-05-06"]
+    csv_delimiter = ","
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
         uprn = record.uprn.strip().lstrip("0")
 
-        if record.houseid.strip() in ["9004672", "9004673"]:
-            rec["postcode"] = "DA2 7AP"
-
         if uprn in [
-            "100060873013",  # DA28AX -> DA28DL : Beacon House Shellbank Lane, Bean, Dartford, Kent
+            "10094156345",  # 447A PRINCES ROAD, DARTFORD
+            "200000533812",  # THE NOOK, POWDER MILL LANE, DARTFORD
+            "200000538710",  # THE COACH HOUSE, HAWLEY ROAD, DARTFORD
         ]:
-            rec["accept_suggestion"] = True
+            return None
 
-        return rec
+        if record.housepostcode in ["DA9 9XT"]:
+            return None
+
+        return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+
+        # 8th Dartford Scout Hall (PRI5) Oakfield Lane Dartford Kent DA1 2EU and
+        # 8th Dartford Scout Hall (WSH5) Oakfield Lane Dartford Kent DA1 2EU
+        if "8th Dartford Scout Hall" in record.pollingstationname:
+            record = record._replace(pollingstationpostcode="DA1 2SP")
+
+        return super().station_record_to_dict(record)
