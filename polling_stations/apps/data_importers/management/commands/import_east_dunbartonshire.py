3802a4eb8c8f0fee0fc162af704f5ca9d94ead99
smsmith97 <sean.m.smith97@outlook.com>
2021-03-30 11:23:09 +0100

    import script for east dunbartonshire (closes #3275)

---
+++ polling_stations/apps/data_importers/management/commands/import_east_dunbartonshire.py
@@ -0,0 +1,41 @@
+from data_importers.management.commands import BaseHalaroseCsvImporter
+
+
+class Command(BaseHalaroseCsvImporter):
+    council_id = "EDU"
+    addresses_name = "2021-03-22T09:26:21.784038/polling_station_export-2021-03-21.csv"
+    stations_name = "2021-03-22T09:26:21.784038/polling_station_export-2021-03-21.csv"
+    elections = ["2021-05-06"]
+    csv_delimiter = ","
+
+    def address_record_to_dict(self, record):
+        uprn = record.uprn.strip().lstrip("0")
+
+        if uprn in [
+            "132016488",  # WINDYKNOWE COTTAGE, STATION ROAD, LENNOXTOWN, GLASGOW
+            "132043041",  # BRAES O YETTS FARM, KIRKINTILLOCH, GLASGOW
+            "132023751",  # 4 LENZIE ROAD, KIRKINTILLOCH, GLASGOW
+            "132023752",  # 6 LENZIE ROAD, KIRKINTILLOCH, GLASGOW
+            "132023979",  # PARKVIEW, LENZIE ROAD, KIRKINTILLOCH, GLASGOW
+            "132023978",  # KIRROUGHTREE, LENZIE ROAD, KIRKINTILLOCH, GLASGOW
+            "132008358",  # ORCHARDLEA, TORRANCE, GLASGOW
+            "132007850",  # 145 BALMUILDY ROAD, BISHOPBRIGGS, GLASGOW
+            "132008282",  # HILTON COTTAGE, BALMUILDY ROAD, BISHOPBRIGGS, GLASGOW
+            "132008265",  # HILTON FARM COTTAGE, BALMUILDY ROAD, BISHOPBRIGGS, GLASGOW
+            "132001702",  # 129 MILNGAVIE ROAD, BEARSDEN, GLASGOW
+        ]:
+            return None
+
+        if record.housepostcode in [
+            "G66 7HE",
+            "G66 7HL",
+            "G66 7AS",
+            "G64 2DF",
+            "G64 4DQ",
+            "G66 4QH",
+            "G64 1FR",
+            "G61 1RS",
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
