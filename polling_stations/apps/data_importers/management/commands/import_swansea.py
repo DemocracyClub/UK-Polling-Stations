2807fa297f9dfc2fe70c19ceb5f35ede0a9b19aa
smsmith97 <sean.m.smith97@outlook.com>
2021-03-31 15:30:51 +0100

    import script for swansea (closes #3213)

--- polling_stations/apps/data_importers/management/commands/import_swansea.py
+++ polling_stations/apps/data_importers/management/commands/import_swansea.py
@@ -2,33 +2,61 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 
 class Command(BaseHalaroseCsvImporter):
-    council_id = "W06000011"
-    addresses_name = "parl.2019-12-12/Version 2/polling_station_export-2019-11-06.csv"
-    stations_name = "parl.2019-12-12/Version 2/polling_station_export-2019-11-06.csv"
-    elections = ["parl.2019-12-12"]
-    csv_encoding = "latin-1"
-    allow_station_point_from_postcode = False
-
-    def station_record_to_dict(self, record):
-        if record.pollingstationaddress_5 == "mail@clydach.wales":
-            record = record._replace(pollingstationaddress_5="")
-
-        return super().station_record_to_dict(record)
+    council_id = "SWA"
+    addresses_name = "2021-04-08T09:24:44.681755/polling_station_export-2021-04-06.csv"
+    stations_name = "2021-04-08T09:24:44.681755/polling_station_export-2021-04-06.csv"
+    elections = ["2021-05-06"]
+    csv_delimiter = ","
+    csv_encoding = "windows-1252"
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
         uprn = record.uprn.strip().lstrip("0")
 
-        if record.housepostcode == "SA2 OSF":
-            rec["postcode"] = "SA2 0SF"
-        if record.housepostcode == "SA2 OLY":
-            rec["postcode"] = "SA2 0LY"
-        if record.housepostcode == "SA2 OPN":
-            rec["postcode"] = "SA2 0PN"
-        if record.housepostcode == "SAG 5PG":
-            rec["postcode"] = "SA6 5PG"
+        if uprn in [
+            "10010059192",  # CARAVAN 81 BURROWS CARAVAN SITE A4118 FROM JUNCTION WITH PORT EYNON BUS TURNING CIRCLE TO JUNCTION WITH CLASSIFIED SECTION B4247, PORT EYNON, SWANSEA
+            "10010038594",  # THE FARMHOUSE, CEFN GORWYDD FAWR, GOWERTON, SWANSEA
+            "10010041547",  # FLAT, 9 HIGH STREET, GORSEINON, SWANSEA
+            "10011729497",  # FLAT THE MARY DILLWYN FFORDD CYNORE, FFORESTFACH, SWANSEA
+            "10010059620",  # PENTRE COTTAGE, PENTRE ROAD, PONTARDDULAIS, SWANSEA
+            "10010043604",  # CARAVAN CWMDULAIS FARM UNCLASSIFIED SECTION-Y920, PONTARDDULAIS, SWANSEA
+            "100101045973",  # FIRST FLOOR FLAT 4 WALTER ROAD, CITY CENTRE, SWANSEA
+            "10010035134",  # FLAT 1 141 TERRACE ROAD, MOUNT PLEASANT, SWANSEA
+            "100100360550",  # FLAT 2 141 TERRACE ROAD, MOUNT PLEASANT, SWANSEA
+            "100100360551",  # FLAT 3 141 TERRACE ROAD, MOUNT PLEASANT, SWANSEA
+            "10010037937",  # MAISONETTE FLAT - BOTTOM FLAT 143 TERRACE ROAD, MOUNT PLEASANT, SWANSEA
+            "100100360552",  # FIRST FLOOR FLAT TOP FLAT 143 TERRACE ROAD, MOUNT PLEASANT, SWANSEA
+            "10010059308",  # CARAVAN EAST MOOR B4247 FROM HILL VIEW TO KIMLEY MOOR FARM, RHOSSILI, SWANSEA
+            "10010045307",  # THE NATIONAL TRUST, THE OLD RECTORY UNCLASSIFIED SECTION-Y2402, RHOSSILI, SWANSEA
+            "10010059384",  # COTTAGE AT ROBINS NEST UNCLASSIFIED SECTION-Y2085, PENMAEN, SWANSEA
+        ]:
+            return None
 
-        if uprn in ["10010062487", "10010062488"]:
-            rec["accept_suggestion"] = False
+        if record.housepostcode in [
+            "SA5 4NN",
+            "SA4 3QX",
+            "SA6 5JS",
+            "SA5 7PH",
+            "SA5 7DR",
+            "SA5 7HY",
+            "SA6 6BW",
+            "SA1 8PN",
+            "SA3 1AS",
+            "SA3 3JS",
+            "SA3 4QE",
+            "SA1 6NQ",
+            "SA6 6QA",
+            "SA1 6JP",
+        ]:
+            return None
 
-        return rec
+        return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # Llangyfelach Church Hall Swansea Road Llangyfelach Swansea
+        if (record.pollingstationnumber, record.pollingstationname) == (
+            "7",
+            "Llangyfelach Church Hall",
+        ):
+            record = record._replace(pollingstationpostcode="SA5 7JD")
+
+        return super().station_record_to_dict(record)
