7e5e8ac5f530c8efff6c77f8b0a09a71e677b417
smsmith97 <sean.m.smith97@outlook.com>
2021-03-31 16:53:58 +0100

    import script for bridgend (closes #3220)

--- polling_stations/apps/data_importers/management/commands/import_bridgend.py
+++ polling_stations/apps/data_importers/management/commands/import_bridgend.py
@@ -2,28 +2,52 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 
 class Command(BaseHalaroseCsvImporter):
-    council_id = "W06000013"
+    council_id = "BGE"
     addresses_name = (
-        "parl.2019-12-12/Version 1/polling_station_export-2019-11-08bridgend.csv"
+        "2021-03-17T10:33:16.382690/Bridgend polling_station_export-2021-03-16.csv"
     )
     stations_name = (
-        "parl.2019-12-12/Version 1/polling_station_export-2019-11-08bridgend.csv"
+        "2021-03-17T10:33:16.382690/Bridgend polling_station_export-2021-03-16.csv"
     )
-    elections = ["parl.2019-12-12"]
-    allow_station_point_from_postcode = False
+    elections = ["2021-05-06"]
+    csv_delimiter = ","
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
-        rec = super().address_record_to_dict(record)
 
-        if uprn == "10090816154":
-            rec["postcode"] = "CF365HN"
-        if uprn == "10090815370":
-            rec["postcode"] = "CF320EP"
-        if uprn == "10090814759":
-            rec["postcode"] = "CF356FR"
-
-        if record.housepostcode in ["CF33 4RY", "CF32 0NR"]:
+        if uprn in [
+            "200002664482",  # YNYSLAS UCHAF FARM, BLACKMILL, BRIDGEND
+            "200001775090",  # GLYNTAWEL HOUSE, PANT HIRWAUN, BRYNCETHIN, BRIDGEND
+            "10013368189",  # BALLAS BARNS, HEOL Y SHEET, STORMY DOWN, PYLE, BRIDGEND
+        ]:
             return None
 
-        return rec
+        if record.housepostcode in [
+            "CF31 1NP",
+            "CF31 5FD",
+            "CF34 0UF",
+            "CF35 6HZ",
+            "CF34 9BT",
+            "CF34 0BF",
+            "CF34 0DY",
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # Llangeinor RFC Llangeinor FC Bettws Road Llangeinor CF32 8FG
+        if (record.pollingstationnumber, record.pollingstationpostcode) == (
+            "52",
+            "CF32 8FG",
+        ):
+            record = record._replace(pollingstationpostcode="")
+
+        # Pyle Rugby Football Club Brynglas Terrace Pyle Bridgend CF36 6AG
+        if (record.pollingstationnumber, record.pollingstationpostcode) == (
+            "33",
+            "CF36 6AG",
+        ):
+            record = record._replace(pollingstationpostcode="")
+
+        return super().station_record_to_dict(record)
