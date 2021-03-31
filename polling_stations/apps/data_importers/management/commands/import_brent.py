3569387e838553f61012eaa9c4e30aa682356f80
GeoWill <roper.william@gmail.com>
2021-03-31 14:35:38 +0100

    Import script for Brent (closes #3316)

--- polling_stations/apps/data_importers/management/commands/import_brent.py
+++ polling_stations/apps/data_importers/management/commands/import_brent.py
@@ -2,7 +2,27 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 
 class Command(BaseDemocracyCountsCsvImporter):
-    council_id = "E09000005"
-    addresses_name = "europarl.2019-05-23/Version 1/DC PD.csv"
-    stations_name = "europarl.2019-05-23/Version 1/DC PS.csv"
-    elections = ["europarl.2019-05-23"]
+    council_id = "BEN"
+    addresses_name = "2021-03-30T11:24:08.732705/Brent DC -polling districts.csv"
+    stations_name = "2021-03-30T11:24:08.732705/Brent DC -polling stations (2).csv"
+    elections = ["2021-05-06"]
+
+    def station_record_to_dict(self, record):
+        if record.stationcode == "CDU4_1":
+            record = record._replace(yordinate="184797")
+            record = record._replace(xordinate="521445")
+
+        return super().station_record_to_dict(record)
+
+    def address_record_to_dict(self, record):
+        uprn = record.uprn.strip().lstrip("0")
+        if uprn in [
+            "202228638",
+            "202038378",
+            "202228580",
+            "202053053",
+            "202053054",
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
