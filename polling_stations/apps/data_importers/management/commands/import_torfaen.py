609b71e0aa105b74abaf93eb32d7bf31696a11d6
smsmith97 <sean.m.smith97@outlook.com>
2021-03-31 11:10:23 +0100

    import script for torfaen (closes #3182)

--- polling_stations/apps/data_importers/management/commands/import_torfaen.py
+++ polling_stations/apps/data_importers/management/commands/import_torfaen.py
@@ -1,69 +1,52 @@
-from addressbase.models import Address
-from uk_geo_utils.helpers import Postcode
-from data_importers.base_importers import BaseCsvStationsCsvAddressesImporter
-from data_importers.addresshelpers import (
-    format_residential_address,
-    format_polling_station_address,
-)
+from data_importers.management.commands import BaseHalaroseCsvImporter
 
 
-class Command(BaseCsvStationsCsvAddressesImporter):
-    council_id = "W06000020"
-    addresses_name = "parl.2019-12-12/Version 1/DEMOCRACY CLUB Finaltor.csv"
-    stations_name = "parl.2019-12-12/Version 1/DEMOCRACY CLUB Finaltor.csv"
-    elections = ["parl.2019-12-12"]
-
-    def get_station_hash(self, record):
-        return "-".join([record.polling_district.strip()])
-
-    def get_station_address(self, record):
-        return format_polling_station_address(
-            [
-                record.polling_station_name.strip(),
-                record.polling_station_address1.strip(),
-                record.polling_station_address2.strip(),
-                record.polling_station_address3.strip(),
-                record.polling_station_address4.strip(),
-                record.polling_station_address5.strip(),
-                record.polling_station_address6.strip(),
-                record.polling_station_address7.strip(),
-                record.polling_station_address8.strip(),
-            ]
-        )
-
-    def get_station_point(self, record):
-        uprn = record.polling_station_uprn.lstrip("0").strip()
-        try:
-            ab_rec = Address.objects.get(uprn=uprn)
-            return ab_rec.location
-        except Address.DoesNotExist:
-            return None
-
-    def station_record_to_dict(self, record):
-        location = self.get_station_point(record)
-        return {
-            "internal_council_id": record.polling_district.strip(),
-            "postcode": record.polling_station_address9.strip(),
-            "address": self.get_station_address(record),
-            "location": location,
-        }
+class Command(BaseHalaroseCsvImporter):
+    council_id = "TOF"
+    addresses_name = (
+        "2021-03-15T11:44:05.081204/Torfaen polling_station_export-2021-03-15.csv"
+    )
+    stations_name = (
+        "2021-03-15T11:44:05.081204/Torfaen polling_station_export-2021-03-15.csv"
+    )
+    elections = ["2021-05-06"]
+    csv_delimiter = ","
 
     def address_record_to_dict(self, record):
-        address = format_residential_address(
-            [
-                record.elector_address1.strip(),
-                record.elector_address2.strip(),
-                record.elector_address3.strip(),
-                record.elector_address4.strip(),
-                record.elector_address5.strip(),
-                record.elector_address6.strip(),
-                record.elector_address7.strip(),
-                record.elector_address8.strip(),
-            ]
-        )
+        uprn = record.uprn.strip().lstrip("0")
 
-        return {
-            "address": address.strip(),
-            "postcode": Postcode(record.elector_address9.strip()).without_space,
-            "polling_station_id": record.polling_district.strip(),
-        }
+        if uprn in [
+            "10013477024",  # AWEL MYNYDD, MANOR ROAD, ABERSYCHAN, PONTYPOOL
+            "200002953610",  # YNYS NEWYDD, VALENTINE ROAD, COED YR EOS, ABERSYCHAN, PONTYPOOL
+            "200002953605",  # TY COED YR EOS, VALENTINE ROAD, COED YR EOS, ABERSYCHAN, PONTYPOOL
+            "200002953910",  # PARK HOUSE FARM, GRAIG ROAD, UPPER CWMBRAN, CWMBRAN
+            "10013477141",  # GELLI FAWR FARM, HENLLYS, CWMBRAN
+            "10013478138",  # PENWYRLOD FARM PEN Y WYRLOD FARM ACCESS, HENLLYS, CWMBRAN
+        ]:
+            return None
+
+        if record.housepostcode in [
+            "NP4 7NW",
+            "NP4 8LG",
+            "NP44 1LE",
+            "NP44 4QS",
+            "NP4 6TX",
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # FORGESIDE COMMUNITY CENTRE FORGESIDE COMMUNITY CENTRE BFORGESIDE BLAENAVON TORFAEN NP4 9BD
+        if record.pollingstationname == "FORGESIDE COMMUNITY CENTRE":
+            record = record._replace(pollingstationpostcode="NP4 9DH")
+
+        # ST MARYS CHURCH HALL BRYN EGLWYS CROESYCEILIOG CWMBRAN TORFAEN NP44  2E - changes two stations with same address
+        if record.pollingstationname == "ST MARYS CHURCH HALL":
+            record = record._replace(pollingstationpostcode="NP44  2EJ")
+
+        # THORNHILL4UTOO THORNHILL COMMUNITY CENTRE LEADON COURT THORNHILL CWMBRAN TORFAEN NP44 5YZ
+        if record.pollingstationname == "THORNHILL4UTOO":
+            record = record._replace(pollingstationpostcode="NP44 5TZ")
+
+        return super().station_record_to_dict(record)
