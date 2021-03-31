a0114428a41005d1e4413b15a3909d6d9fe18221
smsmith97 <sean.m.smith97@outlook.com>
2021-03-31 12:34:17 +0100

    import script for norwhich (closes #3186)

--- polling_stations/apps/data_importers/management/commands/import_norwich.py
+++ polling_stations/apps/data_importers/management/commands/import_norwich.py
@@ -2,29 +2,34 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 
 class Command(BaseHalaroseCsvImporter):
-    council_id = "E07000148"
+    council_id = "NOW"
     addresses_name = (
-        "parl.2019-12-12/Version 1/polling_station_export-2019-11-08Norwich.csv"
+        "2021-03-24T11:30:58.947611/Norwich new polling_station_export-2021-03-24.csv"
     )
     stations_name = (
-        "parl.2019-12-12/Version 1/polling_station_export-2019-11-08Norwich.csv"
+        "2021-03-24T11:30:58.947611/Norwich new polling_station_export-2021-03-24.csv"
     )
-    elections = ["parl.2019-12-12"]
-    allow_station_point_from_postcode = False
+    elections = ["2021-05-06"]
+    csv_delimiter = ","
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
         uprn = record.uprn.strip().lstrip("0")
 
-        if record.housepostcode.strip() in ["NR4 7FW", "RG8 0RR", "NR6 5RP", "NR1 2EB"]:
-            return None
-
-        if uprn in ["10093501268", "100091562008"]:
-            return None
-
         if uprn in [
-            "100091553263"  # NR23AT -> NR23AU : ST JOHNS HOUSE 38 HEIGHAM ROAD, NORWICH
+            "100091562008",  # 10 SPROWSTON ROAD, NORWICH
+            "10093501268",  # 481D SPROWSTON ROAD, NORWICH
+            "100091339250",  # FIRST FLOOR FLAT 70 SILVER ROAD, NORWICH
+            "100090915857",  # 9 OAK STREET, NORWICH
+            "100090915856",  # 7 OAK STREET, NORWICH
+            "100090915855",  # 5 OAK STREET, NORWICH
+            "100090915854",  # 3 OAK STREET, NORWICH
+            "100090890924",  # 32 BRITANNIA ROAD, NORWICH
+            "100090890926",  # 34 BRITANNIA ROAD, NORWICH
+            "100090890928",  # 36 BRITANNIA ROAD, NORWICH
         ]:
-            rec["accept_suggestion"] = True
+            return None
 
-        return rec
+        if record.housepostcode in ["NR2 3AT", "NR4 7FW"]:
+            return None
+
+        return super().address_record_to_dict(record)
