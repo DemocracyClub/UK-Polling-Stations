3d2b053d4d93cd44a41319c612fdf4493a443d33
smsmith97 <sean.m.smith97@outlook.com>
2021-03-31 11:53:36 +0100

    import script for copeland (closes #3184)

--- polling_stations/apps/data_importers/management/commands/import_copeland.py
+++ polling_stations/apps/data_importers/management/commands/import_copeland.py
@@ -3,9 +3,10 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "COP"
-    addresses_name = "2021-03-03T13:29:48.558281/polling_station_export-2021-03-02.csv"
-    stations_name = "2021-03-03T13:29:48.558281/polling_station_export-2021-03-02.csv"
+    addresses_name = "2021-03-15T11:52:14.388522/polling_station_export-2021-03-15.csv"
+    stations_name = "2021-03-15T11:52:14.388522/polling_station_export-2021-03-15.csv"
     elections = ["2021-05-06"]
+    csv_delimiter = ","
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
@@ -18,10 +19,18 @@ class Command(BaseHalaroseCsvImporter):
             "CA25 5LN",
             "CA25 5LH",
             "CA22 2TD",
+            "CA28 6AQ",
         ]:
             return None
 
-        if uprn in ["10000890435"]:
+        if uprn in [
+            "10000899045",  # STABLE COTTAGE, STEEL GREEN, MILLOM
+            "10000897322",  # STEEL GREEN HOUSE, STEEL GREEN, MILLOM
+            "10000906979",  # FLAT AT HERDWICKS STEEL GREEN, MILLOM
+            "10000898773",  # LANGLEY FARM, BOOTLE, MILLOM
+            "10000890830",  # FLOSH FARM CLEATOR, CLEATOR
+            "10000901390",  # FURNACE COTTAGE, DUDDON BRIDGE, BROUGHTON-IN-FURNESS
+        ]:
             return None
 
         return super().address_record_to_dict(record)
