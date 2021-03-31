acba6b76bb3cf8020e68f99f41c176f7bf4ef2a3
GeoWill <roper.william@gmail.com>
2021-03-31 17:42:33 +0100

    Import script for West Dunbartonshire (closes #3274)

--- polling_stations/apps/data_importers/management/commands/import_west_dunbartonshire.py
+++ polling_stations/apps/data_importers/management/commands/import_west_dunbartonshire.py
@@ -1,27 +1,27 @@
-from data_importers.management.commands import BaseScotlandSpatialHubImporter
+from data_importers.ems_importers import BaseHalaroseCsvImporter
 
 
-class Command(BaseScotlandSpatialHubImporter):
-    council_id = "S12000039"
-    council_name = "West Dunbartonshire"
-    elections = ["parl.2019-12-12"]
+class Command(BaseHalaroseCsvImporter):
+    council_id = "WDU"
+    addresses_name = "2021-03-22T09:25:02.768761/polling_station_export-2021-03-21.csv"
+    stations_name = "2021-03-22T09:25:02.768761/polling_station_export-2021-03-21.csv"
+    elections = ["2021-05-06"]
+
+    def address_record_to_dict(self, record):
+        if record.housepostcode in [
+            "G81 3PY",
+            "G81 5AW",
+            "G81 5BL",
+            "G60 5DP",
+            "G82 3LE",
+            "G82 3EY",
+            "G82 4JS",
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
-
-        # Corrections based on checking notice of polling place
-        addresses = {
-            "LE8D": "DALMONACH MOBILE POLLING PLACE, FIRST AVENUE, BONHILL, G83 9AU",
-            "DU13D": "WESTBRIDGEND MOBILE POLLING PLACE, WESTBRIDGEND, DUMBARTON, G82 4BJ",
-            "CC5C": "OUR LADY OF LORETTO PRIMARY SCHOOL, CASTLE SQUARE, CASTLE STREET, DALMUIR, CLYDEBANK, G81 4HN",
-        }
-        rec = super().station_record_to_dict(record)
-
-        if rec:
-            try:
-                rec["address"] = addresses[rec["internal_council_id"]]
-                rec["location"] = None
-                return rec
-            except KeyError:
-                return rec
-
-        return rec
+        if record.adminarea != "WEST DUNBARTONSHIRE":
+            return None
+        return super().station_record_to_dict(record)
