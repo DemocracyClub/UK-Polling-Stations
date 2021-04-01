ec871d52d951e93faa11c0e56660a9dca10ffe5f
smsmith97 <sean.m.smith97@outlook.com>
2021-04-01 13:26:04 +0100

    import script for tameside (closes #3263)

--- polling_stations/apps/data_importers/management/commands/import_tameside.py
+++ polling_stations/apps/data_importers/management/commands/import_tameside.py
@@ -2,104 +2,61 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E08000008"
-    addresses_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019tame.tsv"
-    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019tame.tsv"
-    elections = ["parl.2019-12-12"]
+    council_id = "TAM"
+    addresses_name = "2021-03-19T15:38:04.940947/Democracy_Club__06May2021.tsv"
+    stations_name = "2021-03-19T15:38:04.940947/Democracy_Club__06May2021.tsv"
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
-    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
-        rec = super().address_record_to_dict(record)
-        postcode = record.addressline6
 
-        # These fixes mostly come from locals May 2019, checking a few of them suggests
-        # they're still relevant for GE Dec 2019 so just going to leave them here for now.
         if uprn in [
-            "100012486537",
-            "100012486572",
-            "100012776038",
-            "100012777167",
-            "10014259385",
-            "10090073761",
-            "10090073763",
-            "10090073764",
-            "200004407864",
-            "200004407867",
-            "200004410055",
-            "200004410061",
-            "200004410550",
-            "200004410656",
-            "200004422675",
-            "200004423681",
-            "200004409546",
-            "200004419687",
-            "200004419357",
-            "200004419358",
-            "200004401602",
-            "10090078250",
-            "100012488602",
-            "200004406539",
-            "200004406539",
-            "100011576995",
-        ] or postcode in [
-            "SK15 3QR",
-            "SK14 6SG",
-            "SK14 5RF",
-            "SK14 1EY",
-            "SK14 4XE",
-            "SK14 4EG",
-            "M34 2WS",
-            "OL6 7FU",
-            "M43 6LX",
-            "SK14 3EB",
-            "SK14 3HD",
+            "10003436095",  # ASH TREE FARM, HOBSON MOOR ROAD, MOTTRAM, HYDE
+            "200004419358",  # FLAT 2 34 HUDSON ROAD, HYDE
+            "10090077959",  # FLAT 3, 14 VICTORIA STREET, DENTON, MANCHESTER
+            "10090077958",  # FLAT 2, 14 VICTORIA STREET, DENTON, MANCHESTER
+            "10090077957",  # FLAT 1, 14 VICTORIA STREET, DENTON, MANCHESTER
+            "10090078212",  # COTTAGE REAR OF 49 FAIRFIELD SQUARE, DROYLSDEN
+            "100011591969",  # 39 FAIRFIELD SQUARE, DROYLSDEN, MANCHESTER
+            "100011584362",  # 1 ARRAS GROVE, DENTON, MANCHESTER
+            "100011584364",  # 3 ARRAS GROVE, DENTON, MANCHESTER
+            "100011584363",  # 2 ARRAS GROVE, DENTON, MANCHESTER
+            "100011584365",  # 4 ARRAS GROVE, DENTON, MANCHESTER
+            "10003439283",  # INDIAN PLAZA, 90 AUDENSHAW ROAD, AUDENSHAW, MANCHESTER
+            "100012484582",  # THE BOUNDARY, 2 AUDENSHAW ROAD, AUDENSHAW, MANCHESTER
+            "100012488602",  # THE VALLEY VIEW, HUDDERSFIELD ROAD, MILLBROOK, STALYBRIDGE
+            "200004406539",  # FLAT 2 BANK MILL MANCHESTER ROAD, MOSSLEY
+            "10090073761",  # 19A WELLINGTON STREET, ASHTON-UNDER-LYNE
+            "10090073763",  # 21A WELLINGTON STREET, ASHTON-UNDER-LYNE
+            "10090073764",  # 21B WELLINGTON STREET, ASHTON-UNDER-LYNE
+            "200002857633",  # PORTLAND BASIN MARINA, LOWER ALMA STREET, DUKINFIELD
+            "10093148018",  # 5 MEADOW VIEW GARDENS, DROYLSDEN
+            "10090075504",  # 5 EDMUND STREET, DROYLSDEN
+            "10090075502",  # 2 EDMUND STREET, DROYLSDEN, MANCHESTER
+            "10090075503",  # 4 EDMUND STREET, DROYLSDEN, MANCHESTER
+            "200004409037",  # FLAT 4 62 STAMFORD STREET EAST, ASHTON-UNDER-LYNE
+            "200004419592",  # FLAT 6 62 STAMFORD STREET EAST, ASHTON-UNDER-LYNE
+            "10014258699",  # 193 MOSSLEY ROAD, ASHTON-UNDER-LYNE
+            "10014258700",  # 195 MOSSLEY ROAD, ASHTON-UNDER-LYNE
+            "100012777167",  # 36 LORD STREET, STALYBRIDGE
+            "100011576995",  # 3 MOTTRAM MOOR, HOLLINGWORTH, HYDE
+            "10003441482",  # 78A STOCKPORT ROAD, ASHTON-UNDER-LYNE
+            "10014255906",  # THE HOLLIES BUNGALOW, BOYDS WALK, DUKINFIELD
         ]:
             return None
 
-        if uprn in [
-            "10014259345",  # SK153AE -> SK153AD : 2 Portland Chambers, 1 Mottram Road, Stalybridge, Cheshire
-            "100011618417",  # SK151AD -> SK153AD : 6A Portland Place, Mottram Road, Stalybridge, Cheshire
-            "10003434537",  # OL59PH -> OL50BA : The Penthouse, Stamford Road, Mossley, Ashton-under-Lyne, Lancashire
-            "100012776537",  # SK145QG -> SK145QN : Cemetery Lodge, Stockport Road, Hyde, Cheshire
-            "100011609197",  # M346AJ -> M346AF : 75A Town Lane, Denton, Manchester
-            "100012487649",  # M437LN -> M437FS : Meadow View Farm, Lumb Lane, Droylsden, Manchester
-            "100011536589",  # OL66NR -> OL69NR : 4 Albemarle Street, Ashton-under-Lyne, Lancashire
-            "100011536590",  # OL66NR -> OL69NR : 6 Albemarle Street, Ashton-under-Lyne, Lancashire
-            "100011536591",  # OL66NR -> OL69NR : 8 Albemarle Street, Ashton-under-Lyne, Lancashire
-            "100011536592",  # OL66NR -> OL69NR : 12 Albemarle Street, Ashton-under-Lyne, Lancashire
-            "100011536593",  # OL66NR -> OL69NR : 14 Albemarle Street, Ashton-under-Lyne, Lancashire
-            "100011536594",  # OL66NR -> OL69NR : 16 Albemarle Street, Ashton-under-Lyne, Lancashire
-            "100011536595",  # OL66NR -> OL69NR : 18 Albemarle Street, Ashton-under-Lyne, Lancashire
-            "100011536596",  # OL66NR -> OL69NR : 20 Albemarle Street, Ashton-under-Lyne, Lancashire
-            "100011536597",  # OL66NR -> OL69NR : 22 Albemarle Street, Ashton-under-Lyne, Lancashire
-            "100011536598",  # OL66NR -> OL69NR : 24 Albemarle Street, Ashton-under-Lyne, Lancashire
-            "100011536599",  # OL66NR -> OL69NR : 26 Albemarle Street, Ashton-under-Lyne, Lancashire
-            "10090075402",  # M437PU -> M437ZA : 1 Bridgewater House, Somerset Road, Droylsden, Manchester
-            "10090075327",  # M437PU -> M437ZA : 2 Bridgewater House, Somerset Road, Droylsden, Manchester
-            "10090075403",  # M437PU -> M437ZA : 3 Bridgewater House, Somerset Road, Droylsden, Manchester
-            "10090075328",  # M437PU -> M437ZA : 4 Bridgewater House, Somerset Road, Droylsden, Manchester
-            "10090075404",  # M437PU -> M437ZA : 5 Bridgewater House, Somerset Road, Droylsden, Manchester
-            "10090075329",  # M437PU -> M437ZA : 6 Bridgewater House, Somerset Road, Droylsden, Manchester
-            "200004409564",  # OL79DR -> OL79AA : Flat 1 The Raynors, Taunton Road, Ashton-under-Lyne, Lancashire
-            "200004409565",  # OL79DR -> OL79AA : Flat 2 The Raynors, Taunton Road, Ashton-under-Lyne, Lancashire
-            "200004424803",  # OL79DR -> OL79AA : Flat 3 The Raynors, Taunton Road, Ashton-under-Lyne, Lancashire
-            "200004409566",  # OL79DR -> OL79AA : Flat 4 The Raynors, Taunton Road, Ashton-under-Lyne, Lancashire
-            "200004424804",  # OL79DR -> OL79AA : Flat 5 The Raynors, Taunton Road, Ashton-under-Lyne, Lancashire
-            "200004409568",  # OL79DR -> OL79AA : Flat 6 The Raynors, Taunton Road, Ashton-under-Lyne, Lancashire
-            "200004409569",  # OL79DR -> OL79AA : Flat 7 The Raynors, Taunton Road, Ashton-under-Lyne, Lancashire
-            "200004424805",  # OL79DR -> OL79AA : Flat 8 The Raynors, Taunton Road, Ashton-under-Lyne, Lancashire
-            "200004409570",  # OL79DR -> OL79AA : Flat 9 The Raynors, Taunton Road, Ashton-under-Lyne, Lancashire
-            "200004409571",  # OL79DR -> OL79AA : Flat 10 The Raynors, Taunton Road, Ashton-under-Lyne, Lancashire
-            "200004424812",  # OL79DR -> OL79AA : Flat 11 The Raynors, Taunton Road, Ashton-under-Lyne, Lancashire
+        if record.addressline6 in [
+            "OL6 9LS",
+            "M43 7ZD",
+            "M34 7RZ",
+            "SK14 3EB",
+            "M34 5US",
+            "OL6 7FU",
+            "OL7 9NZ",
+            "M34 5SH",
+            "OL5 9PN",
         ]:
-            rec["accept_suggestion"] = True
+            return None
 
-        if uprn in [
-            "200004419184",  # SK153AD -> SK153AE : 8C Portland Place, Mottram Road, Stalybridge, Cheshire
-            "10090073715",  # OL66LX -> OL67TX : Flat 9, 213 Mossley Road, Ashton-under-Lyne, Lancashire
-        ]:
-            rec["accept_suggestion"] = False
-
-        return rec
+        return super().address_record_to_dict(record)
