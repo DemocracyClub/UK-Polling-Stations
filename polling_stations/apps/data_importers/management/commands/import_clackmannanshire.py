a8186ca5c2c6573deaf9bc1bcb148bb960b8c745
GeoWill <roper.william@gmail.com>
2021-04-02 13:39:37 +0100

    Import script for Clackmannanshire (closes #3456)

--- polling_stations/apps/data_importers/management/commands/import_clackmannanshire.py
+++ polling_stations/apps/data_importers/management/commands/import_clackmannanshire.py
@@ -1,7 +1,34 @@
-from data_importers.management.commands import BaseScotlandSpatialHubImporter
+from data_importers.ems_importers import BaseHalaroseCsvImporter
+
+CLK_ADMINAREAS = (
+    "Alloa",
+    "Alva",
+    "Clackmannan",
+    "Clackmannanshire Ward 1",
+    "Clackmannanshire Ward 2",
+    "Clackmannanshire Ward 3",
+    "Clackmannanshire Ward 4",
+    "Clackmannanshire Ward 5",
+    "Dollar",
+    "Menstrie",
+    "Tillicoultry",
+)
 
 
-class Command(BaseScotlandSpatialHubImporter):
-    council_id = "S12000005"
-    council_name = "Clackmannanshire"
-    elections = ["parl.2019-12-12"]
+class Command(BaseHalaroseCsvImporter):
+    council_id = "CLK"
+    addresses_name = "2021-04-01T20:38:39.906716/Central Scotland polling_station_export-2021-03-31.csv"
+    stations_name = "2021-04-01T20:38:39.906716/Central Scotland polling_station_export-2021-03-31.csv"
+    elections = ["2021-05-06"]
+
+    def station_record_to_dict(self, record):
+        if record.adminarea not in CLK_ADMINAREAS:
+            return None
+
+        return super().station_record_to_dict(record)
+
+    def address_record_to_dict(self, record):
+        if record.adminarea not in CLK_ADMINAREAS:
+            return None
+
+        return super().address_record_to_dict(record)
