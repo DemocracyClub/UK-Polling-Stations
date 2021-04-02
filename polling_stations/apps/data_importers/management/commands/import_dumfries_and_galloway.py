1d3b0e7f38af407c254f8a1cd8fb8f32fab3b716
GeoWill <roper.william@gmail.com>
2021-04-02 13:15:50 +0100

    Import script for Dumfries and Galloway (closes #3448)

--- polling_stations/apps/data_importers/management/commands/import_dumfries_and_galloway.py
+++ polling_stations/apps/data_importers/management/commands/import_dumfries_and_galloway.py
@@ -1,30 +1,12 @@
-from data_importers.management.commands import BaseScotlandSpatialHubImporter
+from data_importers.ems_importers import BaseDemocracyCountsCsvImporter
 
 
-class Command(BaseScotlandSpatialHubImporter):
-    council_id = "S12000006"
-    council_name = "Dumfries and Galloway"
-    elections = ["parl.2019-12-12"]
-
-    def station_record_to_dict(self, record):
-
-        # Corrections based on checking notice of polling place
-        addresses = {
-            "02J1": "COMMUNITY YOUTH CENTRE, PORT WILLIAM",
-            "05F1": "TROQUEER COMMUNITY CENTRE, TROQUEER",
-            "06G1": "DUMFRIES NORTH WEST CHURCH HALL",
-            "08F2": "Tinwald Parish Hall",
-            "12H3": "Middlebie Community Centre",
-        }
-
-        rec = super().station_record_to_dict(record)
-
-        if rec:
-            try:
-                rec["address"] = addresses[rec["internal_council_id"]]
-                rec["location"] = None
-                return rec
-            except KeyError:
-                return rec
-
-        return rec
+class Command(BaseDemocracyCountsCsvImporter):
+    council_id = "DGY"
+    addresses_name = (
+        "2021-04-01T10:19:31.362798/D&GPollingDistrics_DemocractClub250321.csv"
+    )
+    stations_name = (
+        "2021-04-01T10:19:31.362798/D&GPollingStations_DemocractClub250321.csv"
+    )
+    elections = ["2021-05-06"]
