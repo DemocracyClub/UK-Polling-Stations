b9c1a41cc91dbf7ca03211fc432b467983096c22
GeoWill <roper.william@gmail.com>
2021-04-05 10:55:28 +0100

    Import script for Luton (closes #3222)

--- polling_stations/apps/data_importers/management/commands/import_luton.py
+++ polling_stations/apps/data_importers/management/commands/import_luton.py
@@ -2,24 +2,20 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 
 class Command(BaseDemocracyCountsCsvImporter):
-    council_id = "E06000032"
-    addresses_name = "parl.2019-12-12/Version 1/DC - Polling Districts2.csv"
-    stations_name = "parl.2019-12-12/Version 1/DC - Polling Stations.csv"
-    elections = ["parl.2019-12-12"]
-    csv_encoding = "windows-1252"
-    allow_station_point_from_postcode = False
+    council_id = "LUT"
+    addresses_name = "2021-03-17T12:07:50.769847/Luton DC - Polling Districts.csv"
+    stations_name = "2021-03-17T12:07:50.769847/Luton DC - Polling Stations.csv"
+    elections = ["2021-05-06"]
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
         uprn = record.uprn.strip().lstrip("0")
+        self.postcode_field = "pcode"
+        if record.pcode in ["LU3 1BU", "LU1 1BE"]:
+            return None
+        if uprn in ["10001037360", "200003278390"]:
+            return None
+        return super().address_record_to_dict(record)
 
-        if uprn == "10001037360":
-            rec["postcode"] = "LU2 7QG"
-            rec["accept_suggestion"] = False
-
-        if uprn in [
-            "200003273279",  # LU31TL -> LU27AU : SCHOOL HOUSE BARNFIELD COLLEGE, BARNFIELD AVENUE, LUTON
-        ]:
-            rec["accept_suggestion"] = True
-
-        return rec
+    def station_record_to_dict(self, record):
+        self.postcode_field = "postcode"
+        return super().station_record_to_dict(record)
