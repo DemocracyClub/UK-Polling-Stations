16ca6371caae02c5386aec697601cc6bbcc40b7f
smsmith97 <sean.m.smith97@outlook.com>
2021-04-07 14:44:48 +0100

    import script for pendle (closes #3288)

--- polling_stations/apps/data_importers/management/commands/import_pendle.py
+++ polling_stations/apps/data_importers/management/commands/import_pendle.py
@@ -2,39 +2,49 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000122"
-    addresses_name = (
-        "parl.2019-12-12/Version 1/Democracy_Club__12December2019pendle.tsv"
-    )
-    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019pendle.tsv"
-    elections = ["parl.2019-12-12"]
+    council_id = "PEN"
+    addresses_name = "2021-03-22T11:55:12.927152/Pendle Democracy_Club__06May2021.tsv"
+    stations_name = "2021-03-22T11:55:12.927152/Pendle Democracy_Club__06May2021.tsv"
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
-    allow_station_point_from_postcode = False
 
     def station_record_to_dict(self, record):
-        if record.polling_place_id == "3538":
-            # Rolls Royce Leisure
+        # Rolls Royce Leisure, Skipton Road, Barnoldswick BB18 5RU
+        if record.polling_place_id == "4117":
             record = record._replace(polling_place_postcode="BB18 6HJ")
-        if record.polling_place_id == "3638":
-            # New Life Christian Centre
-            record = record._replace(polling_place_postcode="BB8 0HP")
-        if record.polling_place_id == "3522":
-            # Thomas Street Bowling Pavilion, Percy Street, Nelson
+
+        # Thomas Street Bowling Pavilion, Percy Street, Nelson BB9 9AY
+        if record.polling_place_id == "4099":
             record = record._replace(
                 polling_place_postcode="BB9 9BZ",
                 polling_place_northing=437261,
                 polling_place_easting=386085,
             )
 
-        # https://trello.com/c/nwGJpZV0
-        if record.polling_place_id == "3535":
-            record = record._replace(polling_place_name="Barnoldswick Civic Hall")
-            record = record._replace(polling_place_address_1="Station Road")
-            record = record._replace(polling_place_address_2="Barnoldswick")
+        # Baptist School Guilford Street Brierfield BB9 5LH - moving postcode to correct place
+        if record.polling_place_id == "4106":
+            record = record._replace(
+                polling_place_postcode=record.polling_place_address_3
+            )
             record = record._replace(polling_place_address_3="")
-            record = record._replace(polling_place_address_4="")
-            record = record._replace(polling_place_postcode="BB18 6NA")
-            record = record._replace(polling_place_easting="0")
-            record = record._replace(polling_place_northing="0")
 
         return super().station_record_to_dict(record)
+
+    def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "10090962922",  # THE CHALET SANSBURY FARM SOUTHFIELD LANE, SOUTHFIELD
+            "10090962921",  # THE BUNGALOW SANSBURY FARM SOUTHFIELD LANE, SOUTHFIELD
+            "100012400513",  # FLAT 3, REEDYFORD COTTAGE, SCOTLAND ROAD, NELSON
+            "100012400515",  # FLAT 5, REEDYFORD COTTAGE, SCOTLAND ROAD, NELSON
+            "100012400512",  # FLAT 2, REEDYFORD COTTAGE, SCOTLAND ROAD, NELSON
+            "100012400514",  # FLAT 4, REEDYFORD COTTAGE, SCOTLAND ROAD, NELSON
+            "100012400511",  # FLAT 1, REEDYFORD COTTAGE, SCOTLAND ROAD, NELSON
+        ]:
+            return None
+
+        if record.addressline6 in ["BB9 0RQ", "BB8 7JP", "BB8 0JR"]:
+            return None
+
+        return super().address_record_to_dict(record)
