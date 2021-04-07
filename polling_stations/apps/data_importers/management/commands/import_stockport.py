0ccd7d95cb546091e6c2a96362ccaabf9e5d57d3
smsmith97 <sean.m.smith97@outlook.com>
2021-04-07 12:32:06 +0100

    import script for stockport (closes #3286)

--- polling_stations/apps/data_importers/management/commands/import_stockport.py
+++ polling_stations/apps/data_importers/management/commands/import_stockport.py
@@ -2,71 +2,65 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E08000007"
-    addresses_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019Stockport.tsv"
-    stations_name = "local.2019-05-02/Version 1/Democracy_Club__02May2019Stockport.tsv"
-    elections = ["europarl.2019-05-23"]
+    council_id = "SKP"
+    addresses_name = "2021-03-22T11:08:59.256823/Democracy_Club__06May2021.tsv"
+    stations_name = "2021-03-22T11:08:59.256823/Democracy_Club__06May2021.tsv"
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
-
-    def station_record_to_dict(self, record):
-
-        if record.polling_place_id == "7780":
-            record = record._replace(polling_place_name="Centenary Hall")
-            record = record._replace(polling_place_address_1="Benja Fold")
-            record = record._replace(polling_place_address_2="Off Ack Lane East")
-            record = record._replace(polling_place_address_3="Stockport")
-            record = record._replace(polling_place_address_4="")
-
-        return super().station_record_to_dict(record)
+    csv_encoding = "windows-1252"
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
-        # Correction made because addresses are missing uprns in the csv.
-        if record.addressline6 == "SK7 5AJ" and record.polling_place_id == "7908":
-            rec["postcode"] = "SK8 6AJ"
-
-        # matched to addressbase record by eye
         if uprn in [
-            "10009672777",  # SK26NG -> SK26ND
-            "10090543095",
-            "10007621290",  # SK26QX -> SK26QZ
-            "100011519677",  # SK26QX -> SK26QZ
-            "100011486287",  # SK25PG -> SK14DG
-            "100012478853",  # SK68DR -> SK68AA
-            "100012480964",  # SK63DP -> SK63DR
-            "100012478760",  # SK76AF -> SK76LU
-            "100012478766",
-            "10090546401",  # SK74AW -> SK74RF
-            "100012481439",  # SK61NL -> SK61RJ
-            "100012786198",  # SK65BB -> SK66AW
-            "100012483235",  # SK65NY -> SK65DF
-            "10000017775",  # SK67HB -> SK65HB
-            "100011531719",  # SK45BS -> SK45BT
-            "100011502100",  # SK11EW -> SK11ES
-            "100011502101",
-            "100012788925",  # SK83LA -> SK83LL
-            "100012788926",
-            "100012788927",
-            "100012788928",
-            "100012788929",
-            "100012769647",  # SK12QE -> SK12JT
-            "200000785972",  # SK64HT -> SK64DS
-            "100012788954",  # SK83DQ -> SK83QA
-            "100012786072",  # SK66LP -> SK65LP
-            "10090543047",  # SK82NP -> SK82NZ
-            "100011483808",  # SK65EL -> SK65EJ
-            "10002096813",  # SK44NZ -> SK56AZ
-            "100011523163",  # SK66AB -> SK66BD
-            "100011523164",
+            "10002096787",  # FLAT TORKINGTON ROAD STABLES TORKINGTON ROAD, HAZEL GROVE, STOCKPORT
+            "10093012711",  # 155A STOCKPORT ROAD, MARPLE, STOCKPORT
+            "100011526060",  # 27 THE CLOUGH, STOCKPORT
+            "100011526066",  # 33 THE CLOUGH, STOCKPORT
+            "100011526058",  # 25 THE CLOUGH, STOCKPORT
+            "100011526064",  # 31 THE CLOUGH, STOCKPORT
+            "100011526056",  # 23 THE CLOUGH, STOCKPORT
+            "100011526062",  # 29 THE CLOUGH, STOCKPORT
+            "10002096735",  # 16B MARPLE ROAD, OFFERTON, STOCKPORT
+            "100011432786",  # 6 DEMMINGS ROAD, CHEADLE
+            "100011432788",  # 8 DEMMINGS ROAD, CHEADLE
         ]:
-            rec["accept_suggestion"] = True
+            return None
 
-        if uprn in [
-            "10093012146",  # SK41NN -> SK14NN
-            "10093012146",  # SK41NN -> SK14NN
+        if record.addressline6 in [
+            "SK8 3TJ",
+            "SK8 6BG",
+            "SK8 3DQ",
+            "SK6 2BD",
+            "SK6 1AY",
+            "SK6 1NL",
+            "SK7 4NX",
+            "SK7 4AJ",
+            "SK7 4DF",
+            "SK7 2AA",
+            "SK6 6LP",
+            "SK6 5NY",
+            "SK6 6DF",
+            "SK6 3BT",
+            "SK7 3LZ",
+            "SK6 4EN",
+            "SK6 4HT",
+            "SK6 1JG",
+            "SK5 6XD",
+            "SK5 7BJ",
+            "SK4 5DA",
+            "SK2 7HJ",
+            "SK2 6LW",
+            "SK6 7DH",
         ]:
-            rec["accept_suggestion"] = False
+            return None
 
-        return rec
+        return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # Kingsway School (Lower) Broadway Campus High Grove Road Cheadle SK8 1NP
+        if record.polling_place_id == "10415":
+            record = record._replace(polling_place_easting="")
+            record = record._replace(polling_place_northing="")
+
+        return super().station_record_to_dict(record)
