7d01a92b9ff5f6ee41e82a57919021f21b20c271
GeoWill <roper.william@gmail.com>
2021-04-09 16:02:52 +0100

    Import script for Perth and Kinross

--- polling_stations/apps/data_importers/management/commands/import_perth_and_kinross.py
+++ polling_stations/apps/data_importers/management/commands/import_perth_and_kinross.py
@@ -5,13 +5,10 @@ from data_importers.github_importer import BaseGitHubImporter
 class Command(BaseGitHubImporter):
 
     srid = 4326
-    council_id = "S12000048"
-    elections = ["parl.2019-12-12"]
+    council_id = "PKN"
+    elections = ["2021-05-06"]
     scraper_name = "wdiv-scrapers/DC-PollingStations-PerthAndKinross"
     geom_type = "geojson"
-    # districts file has station address and UPRN for district
-    # parse the districts file twice
-    stations_query = "districts"
 
     def geocode_from_uprn(self, uprn):
         uprn = str(uprn).lstrip("0").strip()
@@ -20,25 +17,31 @@ class Command(BaseGitHubImporter):
 
     def district_record_to_dict(self, record):
         poly = self.extract_geometry(record, self.geom_type, self.get_srid("districts"))
-        return {
-            "internal_council_id": record["CODE"],
-            "name": record["CODE"],
-            "area": poly,
-            "polling_station_id": record["CODE"],
-        }
-
-    def station_record_to_dict(self, record):
-        try:
-            location = self.geocode_from_uprn(record["UPRN"])
-        except Address.DoesNotExist:
-            location = None
-
-        if record["CODE"] == "tba":
+        if record["PPD_CODE"] == "tba":
             return None
 
         return {
-            "internal_council_id": record["CODE"],
-            "address": record["ADDRESS"],
-            "postcode": "",
-            "location": location,
+            "internal_council_id": record["PPD_CODE"],
+            "name": record["PPD_CODE"],
+            "area": poly,
+            "polling_station_id": record["PPD_CODE"],
         }
+
+    def station_record_to_dict(self, record):
+        location = self.extract_geometry(
+            record, self.geom_type, self.get_srid("districts")
+        )
+
+        address = f'{record["POLL_PLACE"]}, {record["PPD_NAMES"]}'
+        codes = record["PPD_CODES"].split(",")
+        stations = []
+        for code in codes:
+            stations.append(
+                {
+                    "internal_council_id": code.strip(),
+                    "address": address,
+                    "postcode": "",
+                    "location": location,
+                }
+            )
+        return stations
