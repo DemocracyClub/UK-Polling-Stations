6bd84bc904c6642420f7681c265e776ed2fb35b4
GeoWill <roper.william@gmail.com>
2021-04-09 16:15:05 +0100

    Import script for Tonbridge Malling

--- polling_stations/apps/data_importers/management/commands/import_tonbridge.py
+++ polling_stations/apps/data_importers/management/commands/import_tonbridge.py
@@ -5,14 +5,15 @@ class Command(BaseGitHubImporter):
 
     srid = 4326
     districts_srid = 4326
-    council_id = "E07000115"
-    elections = ["parl.2019-12-12"]
+    council_id = "TON"
+    elections = ["2021-05-06"]
     scraper_name = "wdiv-scrapers/DC-PollingStations-TonbridgeMalling"
     geom_type = "geojson"
 
     def district_record_to_dict(self, record):
         poly = self.extract_geometry(record, self.geom_type, self.get_srid("districts"))
-
+        if record["OBJECTID"] == 54 and record["PROPOSED_1"] == "Kings Hill South":
+            record["PROPOSED_P"] = "TKB"
         return {
             "internal_council_id": record["PROPOSED_P"],
             "name": record["PROPOSED_1"],
@@ -23,10 +24,10 @@ class Command(BaseGitHubImporter):
         location = self.extract_geometry(
             record, self.geom_type, self.get_srid("stations")
         )
-        address = record["ADDRESS"]
+        address = record["ADDRESS_1"]
 
         return {
-            "internal_council_id": record["OBJECTID_1"],
+            "internal_council_id": record["OBJECTID"],
             "address": address,
             "postcode": "",
             "location": location,
