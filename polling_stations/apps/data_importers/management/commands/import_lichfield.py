5e64b9a340d76b61dcb1ebb6c263825b8252a355
smsmith97 <sean.m.smith97@outlook.com>
2021-04-09 12:32:53 +0100

    import script for lichfield (closes #3331)

--- polling_stations/apps/data_importers/management/commands/import_lichfield.py
+++ polling_stations/apps/data_importers/management/commands/import_lichfield.py
@@ -2,54 +2,54 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000194"
-    addresses_name = "parl.2019-12-12/Version 1/merged.tsv"
-    stations_name = "parl.2019-12-12/Version 1/merged.tsv"
-    elections = ["parl.2019-12-12"]
+    council_id = "LIF"
+    addresses_name = (
+        "2021-03-24T10:59:01.635563/Lichfield Democracy_Club__06May2021.tsv"
+    )
+    stations_name = "2021-03-24T10:59:01.635563/Lichfield Democracy_Club__06May2021.tsv"
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
     csv_encoding = "windows-1252"
-    allow_station_point_from_postcode = False
 
     def station_record_to_dict(self, record):
-        if record.polling_place_id == "4925":
-            # Guildroom, Guildhall, Bore Street, Lichfield
+        # Guildroom, Guildhall, Bore Street, Lichfield
+        if record.polling_place_id == "5596":
             record = record._replace(polling_place_postcode="WS13 6LU")
-        if record.polling_place_id == "4912":
-            # Mobile Polling Station, Staffs University West Car Park, Monks Close, Lichfield
+
+        # Mobile Polling Station, Staffs University West Car Park, Monks Close, Lichfield
+        if record.polling_place_id == "5583":
             record = record._replace(
                 polling_place_easting=411530, polling_place_northing=309223
             )
-        # Locations for these because I investigated why they had the same postcode and it turns out they're adjacent
-        # properties
-        if record.polling_place_id == "4924":
-            # Holy Cross, Community Meeting Room, Holy Cross Church
+
+        # Holy Cross, Community Meeting Room, Holy Cross Church
+        if record.polling_place_id == "5595":
             record = record._replace(
                 polling_place_easting=411921, polling_place_northing=308789
             )
-        if record.polling_place_id == "4920":
-            # Holy Cross Parish Hall
+
+        # Holy Cross Parish Hall
+        if record.polling_place_id == "5591":
             record = record._replace(
                 polling_place_easting=411908, polling_place_northing=308797
             )
+
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "10013216157"  # WS70HZ -> WS70HT : The Coach House, Edial House Farm, Lichfield Road, Burntwood, Staffs
+            "10002775637",  # HORSEY LANE LODGE, HORSEY LANE, RUGELEY
+            "100031701504",  # MILL DAM HOUSE, MILL LANE, ALDRIDGE, WALSALL
+            "100031713231",  # THE GRANARY MILL GREEN FARM MILL LANE, ALDRIDGE, WALSALL
+            "10002767279",  # 4 WISSAGE ROAD, LICHFIELD
+            "10002767278",  # 2 WISSAGE ROAD, LICHFIELD
+            "100031692345",  # MILL ACRES HOUSE, DAISY LANE, ALREWAS, BURTON-ON-TRENT
         ]:
-            rec["accept_suggestion"] = True
+            return None
 
-        if uprn in [
-            "10002777436",  # B783TW -> B783SR : Longwood House, Drayton Manor Park, Tamworth, Staffs
-            "10024113376",  # B783TW -> B783TJ : Manor Lodge, Drayton Manor Park, Tamworth, Staffs
-            "10013216554",  # WS70BJ -> WS70BG : White Swan, 2 Cannock Road, Burntwood, Staffs
-            "100032225996",  # WS140ET -> WS140EU : Hilton Studio, Pouk Lane, Hilton, Lichfield, Staffs
-            "100031687250",  # WS70HY -> WS70HZ : Edial House, 415 Lichfield Road, Burntwood, Staffs
-            "10013848296",  # WS140EN -> WS140ER : Lynn Lane Farm, Lynn Lane, Shenstone, Lichfield, Staffs
-        ]:
-            rec["accept_suggestion"] = False
+        if record.addressline6 in ["B78 3AU", "WS7 0HZ"]:
+            return None
 
-        return rec
+        return super().address_record_to_dict(record)
