086a95771da502f9d2495ea7c3605f89ff929d79
Alex Dutton <alex@cottagelabs.com>
2021-04-09 16:05:24 +0100

    Import Denbighshire (closes #3337)

--- polling_stations/apps/data_importers/management/commands/import_denbighshire.py
+++ polling_stations/apps/data_importers/management/commands/import_denbighshire.py
@@ -2,35 +2,19 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "W06000004"
-    addresses_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019Denb.tsv"
-    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019Denb.tsv"
+    council_id = "DEN"
+    addresses_name = (
+        "2021-03-24T11:39:12.706041/Denbighshire Democracy_Club__06May2021.tsv"
+    )
+    stations_name = (
+        "2021-03-24T11:39:12.706041/Denbighshire Democracy_Club__06May2021.tsv"
+    )
     csv_delimiter = "\t"
     csv_encoding = "windows-1252"
-    elections = ["parl.2019-12-12"]
-    allow_station_point_from_postcode = False
+    elections = ["2021-05-06"]
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
+        if record.addressline6 in ["LL18 4DP"]:
+            return None  # split
 
-        uprn = record.property_urn.lstrip("0")
-
-        if uprn in [
-            "10023749403",  # LL210HN -> LL210HW : Bryn Ffynnon Bungalow, Corwen, Sir Ddinbych/Denbighshire
-            "10091598193",  # LL184DY -> LL184QA : D 57 New Pines Caravan Park, Dyserth Road, Rhyl
-            "10023752567",  # LL184DY -> LL184QJ : E 67 New Pines Caravan Park, Dyserth Road, Rhyl
-            "10091598194",  # LL184DY -> LL184QA : E 82 New Pines Caravan Park, Dyserth Road, Rhyl
-            "10091601258",  # LL184DY -> LL184QA : D 42 New Pines Caravan Park, Dyserth Road, Rhyl
-            "200004303814",  # LL181PH -> LL181PG : Flat 2, 20 Aquarium Street, Rhyl
-            "200004293591",  # CH74DE -> CH74DD : Ty Minffordd, Eryrys, Yr Wyddgrug/Mold
-            "10023751330",  # LL152NL -> LL152NA : Pencoed, Clawddnewydd, Rhuthun/Ruthin
-        ]:
-            rec["accept_suggestion"] = True
-
-        if uprn in [
-            # Wrong postcode in addressbase
-            "10093589383",  # LL170HT -> LL170TH : Dolafon Lodge, Rhyl Road, Llanelwy/St Asaph
-        ]:
-            rec["accept_suggestion"] = False
-
-        return rec
+        return super().address_record_to_dict(record)
