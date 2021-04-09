6de798f7a3ca876defe5339eb59715dfdf841499
smsmith97 <sean.m.smith97@outlook.com>
2021-04-09 15:31:15 +0100

    import script for bristol (closes #3345)

--- polling_stations/apps/data_importers/management/commands/import_bristol.py
+++ polling_stations/apps/data_importers/management/commands/import_bristol.py
@@ -1,98 +1,23 @@
-import re
-from data_importers.github_importer import BaseGitHubImporter
-from data_importers.addresshelpers import format_polling_station_address
+from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
-class Command(BaseGitHubImporter):
+class Command(BaseXpressDemocracyClubCsvImporter):
+    council_id = "BST"
+    addresses_name = "2021-03-24T17:38:12.449323/bristol_deduped.tsv"
+    stations_name = "2021-03-24T17:38:12.449323/bristol_deduped.tsv"
+    elections = ["2021-05-06"]
+    csv_delimiter = "\t"
+    csv_encoding = "windows-1252"
 
-    srid = 4326
-    districts_srid = 4326
-    council_id = "E06000023"
-    elections = ["europarl.2019-05-23"]
-    scraper_name = "wdiv-scrapers/DC-PollingStations-Bristol"
-    geom_type = "geojson"
-    stations_query = "stations_opend"
-    districts_query = "districts_opend"
+    def address_record_to_dict(self, record):
+        if record.post_code in ["BS6 7AT"]:
+            return None
 
-    def district_record_to_dict(self, record):
-        poly = self.extract_geometry(record, self.geom_type, self.get_srid("districts"))
-        return {
-            "internal_council_id": record["polling_dist_id"].strip(),
-            "name": record["polling_dist_id"].strip(),
-            "area": poly,
-            "polling_station_id": record["polling_dist_id"].strip(),
-        }
-
-    def extract_codes(self, text):
-        """
-        Bristol's data does tell us about stations that serve multiple stations
-        but not in a very nice way e.g:
-
-        CEND and CENB
-        SGWD & SGWB
-        Used by CLIB and CLIA
-        Used by REDC amd REDG
-        Used by BEDA & BEDC
-        STWB, STWA1 and STWA2
-
-        Attempt to make sense of this
-        """
-        stations = text
-        stations = stations.replace("Used by", "")
-        stations = stations.replace("Used bu", "")
-        stations = stations.replace("Used for", "")
-        codes = []
-        if "and" in stations:
-            codes = re.split("and|,", stations)
-        elif "amd" in stations:
-            codes = re.split("amd|,", stations)
-        elif "&" in stations:
-            codes = re.split("&|,", stations)
-        else:
-            raise ValueError("Could not parse 'DUAL_STN' field: %s" % text)
-
-        codes = [code.strip() for code in codes]
-
-        if len(codes) < 2:
-            raise ValueError("Could not parse 'DUAL_STN' field: %s" % text)
-
-        return codes
+        return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
-        location = self.extract_geometry(
-            record, self.geom_type, self.get_srid("stations")
-        )
+        # St Anne`s Infants School Community Room Bloomfield Road Brislington Bristol BS4 4EJ
+        if record.polling_place_id == "12587":
+            record = record._replace(polling_place_postcode="")
 
-        address = format_polling_station_address(
-            [
-                record["pao"] if record["pao"] else "",
-                record["street"] if record["street"] else "",
-                record["locality"] if record["locality"] else "",
-            ]
-        )
-        postcode = ""
-        if record["postcode"]:
-            postcode = record["postcode"].strip()
-
-        if record["dual_stn"]:
-            codes = self.extract_codes(record["dual_stn"])
-        else:
-            codes = [record["polling_district"].strip()]
-
-        if (
-            record["pao"] == "Stockwood Library"
-            and record["polling_district"].strip() == "STWD"
-        ):
-            codes = ["STWE"]
-
-        stations = []
-        for code in codes:
-            stations.append(
-                {
-                    "internal_council_id": code,
-                    "postcode": postcode,
-                    "address": address,
-                    "location": location,
-                }
-            )
-        return stations
+        return super().station_record_to_dict(record)
