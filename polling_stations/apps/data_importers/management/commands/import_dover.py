3eb529c144099b5340fee6fc26327bf0c4eeea56
GeoWill <roper.william@gmail.com>
2021-04-09 15:52:32 +0100

    Import script for Dover

--- polling_stations/apps/data_importers/management/commands/import_dover.py
+++ polling_stations/apps/data_importers/management/commands/import_dover.py
@@ -1,25 +1,19 @@
-from django.contrib.gis.geos import Point, MultiPoint
+from django.contrib.gis.geos import MultiPoint
 from data_importers.github_importer import BaseGitHubImporter
-from pollingstations.models import ResidentialAddress
 
 
 class Command(BaseGitHubImporter):
 
     srid = 4326
     districts_srid = 4326
-    council_id = "E07000108"
-    elections = ["parl.2019-12-12"]
+    council_id = "DOV"
+    elections = ["2021-05-06"]
     scraper_name = "wdiv-scrapers/DC-PollingStations-Dover"
     geom_type = "geojson"
 
     def district_record_to_dict(self, record):
         poly = self.extract_geometry(record, self.geom_type, self.get_srid("districts"))
         code = record["district"].split("-")[0].strip()
-
-        # PEX1 and PEX2 cover the exact same area
-        # PSL1 and PSL2 cover the exact same area
-        if code in ["PEX2", "PSL2"]:
-            return None
 
         return {
             "internal_council_id": code,
@@ -29,6 +23,8 @@ class Command(BaseGitHubImporter):
         }
 
     def station_record_to_dict(self, record):
+        if record["POLLING_DI"] == "PSHN":
+            return None
         location = self.extract_geometry(
             record, self.geom_type, self.get_srid("stations")
         )
@@ -37,7 +33,6 @@ class Command(BaseGitHubImporter):
 
         address = "\n".join([record["NAME_OF_PO"], record["LOCATION"]])
         codes = record["POLLING_DI"].split("&")
-
         stations = []
         for code in codes:
             stations.append(
@@ -49,17 +44,3 @@ class Command(BaseGitHubImporter):
                 }
             )
         return stations
-
-    def post_import(self):
-        # user error report #61
-        # https://trello.com/c/LFxuqKjY
-        # say "we don't know" for this one postcode
-        ResidentialAddress.objects.create(
-            address="CT162GZ",
-            postcode="CT162GZ",
-            polling_station_id="",
-            council_id=self.council_id,
-            slug="CT162GZ",
-            location=Point(1.291082, 51.152949, srid=4326),
-            uprn="",
-        )
