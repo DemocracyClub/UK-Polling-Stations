ffa3910d3ea1623da2e3b76c516b71850fb6035b
smsmith97 <sean.m.smith97@outlook.com>
2021-04-12 11:56:13 +0100

    import script for hammersmith and fulham (closes #3545)

--- polling_stations/apps/data_importers/management/commands/import_hammersmith_and_fulham.py
+++ polling_stations/apps/data_importers/management/commands/import_hammersmith_and_fulham.py
@@ -2,37 +2,34 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 
 class Command(BaseHalaroseCsvImporter):
-    council_id = "E09000013"
-    addresses_name = (
-        "parl.2019-12-12/Version 1/polling_station_export-2019-11-16H&F.csv"
-    )
-    stations_name = "parl.2019-12-12/Version 1/polling_station_export-2019-11-16H&F.csv"
-    elections = ["parl.2019-12-12"]
-    allow_station_point_from_postcode = False
+    council_id = "HMF"
+    addresses_name = "2021-04-12T09:04:13.301934/polling_station_export-2021-04-11.csv"
+    stations_name = "2021-04-12T09:04:13.301934/polling_station_export-2021-04-11.csv"
+    elections = ["2021-05-06"]
+    csv_delimiter = ","
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
         uprn = record.uprn.strip().lstrip("0")
 
-        if uprn == "34157887":
-            rec["postcode"] = "W60BY"
-
         if uprn in [
-            "34148072",
-            "34136009",
-            "34135797",
-            "34129561",
-            "34155328",
+            "34150747",  # FLAT 1, 62A RYLETT ROAD, LONDON
+            "34150748",  # FLAT 2, 62A RYLETT ROAD, LONDON
+            "34152305",  # 2A NITON STREET, LONDON
+            "34155328",  # 29C MUNSTER ROAD, LONDON
+            "34136009",  # UPPER FLOOR INNER LODGE ST MARY'S CEMETERY HARROW ROAD, WEMBLEY
+            "34135797",  # GROUND FLOOR INNER LODGE ST MARY'S CEMETERY HARROW ROAD, WEMBLEY
+            "34146774",  # THIRD FLOOR FLAT 95 HAMMERSMITH GROVE, LONDON
         ]:
-            return None
-
-        if (record.housenumber, record.streetname) == ("17A", "Hannell Road"):
             return None
 
         if record.housepostcode in [
-            "NW10 6FG",
-            "W12 8HH",
+            "W14 9DS",
+            "W6 9HJ",
+            "SW6 2EF",
+            "SW6 2GG",
+            "SW6 2GN",
+            "W14 8UZ",
         ]:
             return None
 
-        return rec
+        return super().address_record_to_dict(record)
