71e35ba76fff279054669ca7918ad108e2eeef23
smsmith97 <sean.m.smith97@outlook.com>
2021-04-13 14:30:40 +0100

    import script for bromley (closes #3567)

--- polling_stations/apps/data_importers/management/commands/import_bromley.py
+++ polling_stations/apps/data_importers/management/commands/import_bromley.py
@@ -1,26 +1,14 @@
-from django.contrib.gis.geos import Point
 from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E09000006"
-    addresses_name = "europarl.2019-05-23/Version 1/Democracy_Club__23May2019brom.tsv"
-    stations_name = "europarl.2019-05-23/Version 1/Democracy_Club__23May2019brom.tsv"
-    elections = ["europarl.2019-05-23"]
-    csv_delimiter = "\t"
+    council_id = "BRY"
+    addresses_name = (
+        "2021-04-13T11:41:45.130802/Bromley Democracy_Club__06May2021 (1).CSV"
+    )
+    stations_name = (
+        "2021-04-13T11:41:45.130802/Bromley Democracy_Club__06May2021 (1).CSV"
+    )
+    elections = ["2021-05-06"]
+    csv_delimiter = ","
     csv_encoding = "windows-1252"
-
-    def station_record_to_dict(self, record):
-        rec = super().station_record_to_dict(record)
-
-        if record.polling_place_id == "9036":
-            rec["location"] = Point(-0.058087, 51.403252, srid=4326)
-
-        return rec
-
-    def address_record_to_dict(self, record):
-
-        if record.addressline6 == "BR7 6HL":
-            return None
-
-        return super().address_record_to_dict(record)
