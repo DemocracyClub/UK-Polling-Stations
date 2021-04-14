3a744871525e7f68b466860c1492f3bb93a6e8d3
GeoWill <roper.william@gmail.com>
2021-04-14 14:07:51 +0100

    Import script for West Lothian (closes #3278)

--- polling_stations/apps/data_importers/management/commands/import_west_lothian.py
+++ polling_stations/apps/data_importers/management/commands/import_west_lothian.py
@@ -1,52 +1,55 @@
-from data_importers.management.commands import BaseScotlandSpatialHubImporter
-from data_importers.slugger import Slugger
-
-"""
-Note:
-This importer provides coverage for 91/96 districts
-due to incomplete/poor quality data
-
-Districts LFL6B, LFL6L, LVL7X, LVL8Z and LVV3D missing polling place
-
-"""
+from data_importers.ems_importers import BaseHalaroseCsvImporter
 
 
-class Command(BaseScotlandSpatialHubImporter):
-    council_id = "S12000040"
-    council_name = "West Lothian"
-    elections = []
-    station_map = {}
-    seen_codes = set()
+class Command(BaseHalaroseCsvImporter):
+    council_id = "WLN"
+    addresses_name = "2021-04-14T09:51:22.820489/edinburgh_lothians_polling_station_export-2021-04-13.csv"
+    stations_name = "2021-04-14T09:51:22.820489/edinburgh_lothians_polling_station_export-2021-04-13.csv"
+    elections = ["2021-05-06"]
+    csv_encoding = "windows-1252"
 
-    def district_record_to_dict(self, record):
-        if record[2] == self.council_name:
-            station_slug = Slugger.slugify(record[3])
-            if station_slug == "whitburn-bowling-club":
-                station_slug = "whitburn-bowling-club-club"
-            elif station_slug == "livingston-village-primary-school":
-                station_slug = "livingston-village-primary"
-            elif station_slug == "uphall-station-institute-hall":
-                station_slug = "uphall-station-institue-hall"
+    def address_record_to_dict(self, record):
+        station_hash = self.get_station_hash(record)
 
-            self.station_map.setdefault(station_slug, []).append(record[0])
-            return super().district_record_to_dict(record)
+        if record.adminarea.lower().strip() != "west lothian":
+            return None
+        if station_hash in (
+            "142-kirknewton-village-hall",
+            "147-centenary-hall",
+            "80-craig-inn-centre",
+            "0-echline-primary-school",
+            "343-echline-primary-school",
+            "0-kirkliston-community-centre",
+            "352-kirkliston-community-centre",
+        ):
+            return None
+        if record.housepostcode in (
+            "EH47 0EY",
+            "EH48 2GT",
+            "EH48 3HL",
+            "EH47 7NL",
+            "EH54 8FF",
+            "EH53 0GJ",
+        ):
+            return None
+
+        return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
-        if record[2] == self.council_name:
-            try:
-                station_slug = Slugger.slugify(record[3].split(",")[0])
-                codes = self.station_map[station_slug]
-            except KeyError:
-                return None
+        station_hash = self.get_station_hash(record)
+        if record.adminarea.lower().strip() != "west lothian":
+            return None
 
-            stations = []
-            for code in codes:
-                if code not in self.seen_codes:
-                    self.seen_codes.add(code)
-                    rec = {
-                        "internal_council_id": code,
-                        "postcode": "",
-                        "address": record[3],
-                    }
-                    stations.append(rec)
-            return stations
+        if station_hash in [
+            "142-kirknewton-village-hall",
+            "147-centenary-hall",
+            "80-craig-inn-centre",
+            "0-echline-primary-school",
+            "343-echline-primary-school",
+            "0-kirkliston-community-centre",
+            "352-kirkliston-community-centre",
+        ]:
+            print(station_hash)
+            return None
+
+        return super().station_record_to_dict(record)
