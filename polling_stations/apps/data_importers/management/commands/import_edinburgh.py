c5c9c1cd4290bbb0bc67d3552cafbd98c0aae60e
GeoWill <roper.william@gmail.com>
2021-04-14 15:27:18 +0100

    import script for edinburgh (closes #3279)

--- polling_stations/apps/data_importers/management/commands/import_edinburgh.py
+++ polling_stations/apps/data_importers/management/commands/import_edinburgh.py
@@ -1,41 +1,20 @@
-import tempfile
-import urllib.request
+from data_importers.geo_utils import fix_bad_polygons
 from data_importers.github_importer import BaseGitHubImporter
 
 
 class Command(BaseGitHubImporter):
     srid = 4326
     districts_srid = 4326
-    council_id = "S12000036"
-    elections = ["parl.2019-12-12"]
+    council_id = "EDH"
+    elections = ["2021-05-06"]
     scraper_name = "wdiv-scrapers/DC-PollingStations-Edinburgh"
     geom_type = "geojson"
 
-    def get_stations(self):
-        with tempfile.NamedTemporaryFile() as tmp:
-            urllib.request.urlretrieve(self.stations_url, tmp.name)
-            stations = self.get_data(self.stations_filetype, tmp.name)
-            # ad-hoc fixes for parl.2019-12-12
-            stations.append(
-                {
-                    "Polling__1": "Assembly Rooms",
-                    "Address_1": "54 George Street",
-                    "geometry": '{ "type": "Feature", "geometry": null }',
-                    "LG_PP": "NC11A",
-                }
-            )
-            stations.append(
-                {
-                    "Polling__1": "Dalmeny Parish Church Hall",
-                    "Address_1": "Main Street",
-                    "geometry": '{ "type": "Feature", "geometry": null }',
-                    "LG_PP": "WW01C",
-                }
-            )
-            return stations
-
     def district_record_to_dict(self, record):
         poly = self.extract_geometry(record, self.geom_type, self.get_srid("districts"))
+
+        if record["Code2016"] == "EE11K":
+            record["Code2016"] = "EN11K"
 
         return {
             "internal_council_id": record["Code2016"],
@@ -53,6 +32,9 @@ class Command(BaseGitHubImporter):
 
         stations = []
         for code in codes:
+            # Slateford Longstone Parish Church Hall
+            if code == "SWP07N":
+                location = None
             station = {
                 "internal_council_id": code,
                 "address": "\n".join([record["Polling__1"], record["Address_1"]]),
@@ -62,3 +44,6 @@ class Command(BaseGitHubImporter):
             stations.append(station)
 
         return stations
+
+    def post_import(self):
+        fix_bad_polygons()
