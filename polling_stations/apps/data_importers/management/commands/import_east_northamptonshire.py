51808014abe57e18ece6a1794fd04d680647a164
smsmith97 <sean.m.smith97@outlook.com>
2021-04-15 12:42:49 +0100

    new file for East Northamptonshire (closes #3593)

--- polling_stations/apps/data_importers/management/commands/import_east_northamptonshire.py
+++ polling_stations/apps/data_importers/management/commands/import_east_northamptonshire.py
@@ -3,13 +3,16 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "ENO"
-    addresses_name = "2021-02-18T16:04:59.003549/Democracy_Club__06May2021.tsv"
-    stations_name = "2021-02-18T16:04:59.003549/Democracy_Club__06May2021.tsv"
+    addresses_name = (
+        "2021-03-26T11:17:25.639067/Democracy_Club__06May2021-2nd run 25 3 2021.tsv"
+    )
+    stations_name = (
+        "2021-03-26T11:17:25.639067/Democracy_Club__06May2021-2nd run 25 3 2021.tsv"
+    )
     elections = ["2021-05-06"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
@@ -17,13 +20,16 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         ]:
             return None
 
-        if record.addressline6 in ["NN9 5UF", "NN14 3BY", "NN10 9JD", "NN10 9NJ"]:
+        if record.addressline6 in [
+            "NN9 5UF",
+            "NN14 3BY",
+            "NN10 9JD",
+            "NN10 9NJ",
+            "NN14 3HB",
+            "PE8 5BA",
+            "NN10 6EU",
+            "NN10 0NZ",
+        ]:
             return None
 
-        return rec
-
-    def station_record_to_dict(self, record):
-        # The Albert Underwood Room, St Peters Church, Church Street, Irthlingborough, Wellingborough
-        if record.polling_place_id == "10161":
-            record = record._replace(polling_place_postcode="")
-        return super().station_record_to_dict(record)
+        return super().address_record_to_dict(record)
