03b22bd947741eb1eb281680decb4cdeea7cc500
GeoWill <roper.william@gmail.com>
2021-04-15 15:16:08 +0100

    Import script for Derbyshire Dales (closes #3397)

--- polling_stations/apps/data_importers/management/commands/import_derbyshire_dales.py
+++ polling_stations/apps/data_importers/management/commands/import_derbyshire_dales.py
@@ -2,17 +2,11 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 
 class Command(BaseDemocracyCountsCsvImporter):
-    council_id = "E07000035"
-    addresses_name = "parl.2019-12-12/Version 1/Democracy Club - Polling Districts - Derbyshire Dales.csv"
-    stations_name = "parl.2019-12-12/Version 1/Democray Club - Polling Stations - Derbyshire Dales.csv"
-    elections = ["parl.2019-12-12"]
-    allow_station_point_from_postcode = False
-
-    def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
-        uprn = record.uprn.strip().lstrip("0")
-
-        if uprn in ["10010331972", "10010331834", "10070090689"]:
-            rec["accept_suggestion"] = True
-
-        return rec
+    council_id = "DEB"
+    addresses_name = (
+        "2021-03-29T10:09:54.269326/Derbyshire Dales - Polling Districts Data.csv"
+    )
+    stations_name = (
+        "2021-03-29T10:09:54.269326/Derbyshire Dales - Polling Station Data.csv"
+    )
+    elections = ["2021-05-06"]
