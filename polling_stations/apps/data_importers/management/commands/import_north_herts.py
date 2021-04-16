d0134948e109bcf3a69732c6f601b2ccc29cc39c
GeoWill <roper.william@gmail.com>
2021-04-16 16:53:14 +0100

    Import script for North Herts (closes #3034)

--- polling_stations/apps/data_importers/management/commands/import_north_herts.py
+++ polling_stations/apps/data_importers/management/commands/import_north_herts.py
@@ -3,20 +3,13 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000099"
-    addresses_name = (
-        "parl.2019-12-12/Version 1/Elections@north-herts.gov.uk-1574441053000-.CSV"
-    )
-    stations_name = (
-        "parl.2019-12-12/Version 1/Elections@north-herts.gov.uk-1574441053000-.CSV"
-    )
-    elections = ["parl.2019-12-12"]
-    csv_delimiter = ","
-    allow_station_point_from_postcode = False
+    council_id = "NHE"
+    addresses_name = "2021-04-08T14:05:44.688002/Democracy_Club__06May2021.CSV"
+    stations_name = "2021-04-08T14:05:44.688002/Democracy_Club__06May2021.CSV"
+    elections = ["2021-05-06"]
 
     def station_record_to_dict(self, record):
-
-        if record.polling_place_id == "7364":  # Studlands Rise First School
+        if record.polling_place_id == "7979":  # Studlands Rise First School
             rec = super().station_record_to_dict(record)
             rec["location"] = Point(-0.013935, 52.045835, srid=4326)
             return rec
@@ -24,22 +17,7 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if record.addressline6 == "LU2 8NH":
+        if record.addressline6 in ["SG8 5AL", "SG8 9JU", "LU2 8NH"]:
             return None
 
-        if uprn in [
-            "10013379572",  # SG85AB -> SG85AN : 28A Stamford Yard, Kneesworth Street, Royston, Herts
-            "100081260214",  # SG89JU -> SG89JS : 25 Market Hill, Royston, Herts
-        ]:
-            rec["accept_suggestion"] = True
-
-        if uprn in [
-            "100081125858",  # SG75HY -> SG75HU : The Cuckoo Poultry Farm, Hinxworth Road, Ashwell, Baldock, Herts.
-            "10070043607",  # SG85AA -> SG85AQ : 7 Kneesworth Street, Royston, Herts
-        ]:
-            rec["accept_suggestion"] = False
-
-        return rec
+        return super().address_record_to_dict(record)
