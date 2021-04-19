e1582e68c04dd4e5df6aa8fc6d1b81ce80cfa1e5
smsmith97 <sean.m.smith97@outlook.com>
2021-04-19 12:42:53 +0100

    import script for croydon (closes #3621)

--- polling_stations/apps/data_importers/management/commands/import_croydon.py
+++ polling_stations/apps/data_importers/management/commands/import_croydon.py
@@ -1,91 +1,50 @@
-from data_importers.management.commands import (
-    BaseXpressDCCsvInconsistentPostcodesImporter,
-)
+from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
-class Command(BaseXpressDCCsvInconsistentPostcodesImporter):
-    council_id = "E09000008"
-    addresses_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019.tsv"
-    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019.tsv"
-
-    elections = ["parl.2019-12-12"]
+class Command(BaseXpressDemocracyClubCsvImporter):
+    council_id = "CRY"
+    addresses_name = (
+        "2021-04-16T11:52:42.569584/Croydon 3Democracy_Club__06May2021.tsv V2.tsv"
+    )
+    stations_name = (
+        "2021-04-16T11:52:42.569584/Croydon 3Democracy_Club__06May2021.tsv V2.tsv"
+    )
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
-    allow_station_point_from_postcode = False
-
-    def station_record_to_dict(self, record):
-        if record.polling_place_id == "11078":
-            record = record._replace(polling_place_easting="536768")
-            record = record._replace(polling_place_northing="162439")
-
-        return super().station_record_to_dict(record)
+    csv_encoding = "windows-1252"
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
-        if uprn == "10093756524":  # Flat 4 Boileau Court, 449A Purley Way, Croydon
-            rec["postcode"] = "CR04RG"
-            rec["accept_suggestion"] = False
-        if uprn == "10093048086":  # Flat 14, 3 Church Road, Croydon
-            rec["postcode"] = "CR01FP"
-            rec["accept_suggestion"] = False
-        if uprn == "10093050442":
-            rec["postcode"] = "CR05HL"  # Flat 6 Coach House, Oaks Road, Croydon
-            rec["accept_suggestion"] = False
-        if record.post_code == "CR0 7Q":
-            rec["postcode"] = "CR0 7QN"
-
-        if record.addressline6 in [
-            "CR0 7UR",
-            "CR0 5SH",
-            "CR0 5BA",
-            "CR2 0SA",
-            "CR8 4HE",
+        if uprn in [
+            "200001221192",  # 141 BRIGHTON ROAD, PURLEY
+            "10093049201",  # 16A LIMPSFIELD ROAD, SOUTH CROYDON
+            "10093049200",  # 16 LIMPSFIELD ROAD, SOUTH CROYDON
+            "100020680814",  # FLAT 2 9A LIMPSFIELD ROAD, SOUTH CROYDON
+            "100020680813",  # FLAT 1 9A LIMPSFIELD ROAD, SOUTH CROYDON
+            "100020680810",  # 3A LIMPSFIELD ROAD, SOUTH CROYDON
+            "100020584917",  # WEST LODGE, BISHOPS WALK, CROYDON
+            "100020676357",  # 18A CROHAM ROAD, SOUTH CROYDON
+            "100022908056",  # 4 HIGH TREES, CROYDON
+            "100022908054",  # 2 HIGH TREES, CROYDON
+            "100022908055",  # 3 HIGH TREES, CROYDON
+            "10093046375",  # 237A SYDENHAM ROAD, CROYDON
+            "100020690731",  # 39 BRICKFIELD ROAD, THORNTON HEATH
+            "100020572288",  # 71 BRIGHTON ROAD, COULSDON
         ]:
             return None
 
-        if uprn in [
-            "100020571691",  # CR35QX -> CR35QS : Wentworth, Fox Lane, Caterham, Surrey
-            "100020640357",  # SE255BQ -> SE255DF : 1 Argyll Court, 130 Birchanger Road, South Norwood, London
-            "100020640358",  # SE255BQ -> SE255DF : 2 Argyll Court, 130 Birchanger Road, South Norwood, London
-            "100020640359",  # SE255BQ -> SE255DF : 3 Argyll Court, 130 Birchanger Road, South Norwood, London
-            "100020640360",  # SE255BQ -> SE255DF : 4 Argyll Court, 130 Birchanger Road, South Norwood, London
-            "100020640361",  # SE255BQ -> SE255DF : 5 Argyll Court, 130 Birchanger Road, South Norwood, London
-            "100020640362",  # SE255BQ -> SE255DF : 6 Argyll Court, 130 Birchanger Road, South Norwood, London
-            "100020640363",  # SE255BQ -> SE255DF : 7 Argyll Court, 130 Birchanger Road, South Norwood, London
-            "100020640364",  # SE255BQ -> SE255DF : 8 Argyll Court, 130 Birchanger Road, South Norwood, London
-            "100020640365",  # SE255BQ -> SE255DF : 9 Argyll Court, 130 Birchanger Road, South Norwood, London
-            "100023258863",  # SE254PL -> SE254PT : Ground Floor Flat, 134 Portland Road, South Norwood, London
-            "100023259899",  # SE254UF -> SE254UN : 49 Portland Road, South Norwood, London
-            "10014049899",  # SE254UF -> SE254UN : 49A Portland Road, South Norwood, London
-            "10090380070",  # CR01HD -> CR01RA : Ground Floor, 18 St. Peter`s Road, Croydon
-            "200001203840",  # CR35QX -> CR35QS : The Bungalow, The Fox Inn, Fox Lane, Caterham, Surrey
-            "200001218459",  # CR78SB -> CR78SD : Rear Flat, 434 Whitehorse Road, Thornton Heath
+        if record.addressline6 in [
+            "CR7 7JJ",
+            "CR0 1ND",
+            "CR0 5RN",
+            "CR0 2JH",
+            "CR2 0JB",
+            "CR8 2BA",
+            "CR0 0NX",
+            "CR0 8RG",
+            "SE19 3FB",
         ]:
-            rec["accept_suggestion"] = True
+            return None
 
-        if uprn in [
-            "10000105571",  # SW164BZ -> SW164AN : 1396B London Road, Norbury, London
-            "10001008215",  # CR85JE -> CR82DB : 18A Godstone Road, Kenley
-            "100020607347",  # CR82PA -> CR02BD : 42 Lansdowne Road, Purley
-            "100020607351",  # CR82PA -> CR02BD : 44 Lansdowne Road, Purley
-            "100020607409",  # CR82PE -> CR02BF : 81 Lansdowne Road, Purley
-            "100020607420",  # CR82PE -> CR02BN : 95 Lansdowne Road, Purley
-            "100020664258",  # CR84DU -> CR82HF : 17 Callow Field, Purley
-            "100022902871",  # CR82PD -> CR02BX : 5 Lansdowne Road, Purley
-            "100022902872",  # CR82PA -> CR02BX : 2 Lansdowne Road, Purley
-            "100022902929",  # CR82PD -> CR02BX : 17 Lansdowne Road, Purley
-            "100022903052",  # CR82PA -> CR02BD : 46 Lansdowne Road, Purley
-            "100022903053",  # CR82PD -> CR02BE : 45 Lansdowne Road, Purley
-            "100022904030",  # CR82PA -> CR02BD : 12 Lansdowne Road, Purley
-            "100022904031",  # CR82PA -> CR02BD : 22 Lansdowne Road, Purley
-            "100023534232",  # CR51EB -> CR52NH : Flat rear of, 266-268 Coulsdon Road, Coulsdon
-            "10014051126",  # CR52NL -> CR53BP : 1 Well Cottage, Fourth Drive, Coulsdon
-            "200001186634",  # CR01NA -> CR01ND : 58 High Street, Croydon
-            "200001205005",  # CR82PA -> CR02BD : 16 Lansdowne Road, Purley
-            "200001207021",  # CR82PA -> CR02BD : 18 Lansdowne Road, Purley
-            "10090380070",
-        ]:
-            rec["accept_suggestion"] = False
-
-        return rec
+        return super().address_record_to_dict(record)
