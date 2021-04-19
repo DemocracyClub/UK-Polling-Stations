755348d827f31ff546291bc5526d24198f0a57d3
smsmith97 <sean.m.smith97@outlook.com>
2021-04-19 14:00:32 +0100

    import script for merton (closes #3623)

--- polling_stations/apps/data_importers/management/commands/import_merton.py
+++ polling_stations/apps/data_importers/management/commands/import_merton.py
@@ -2,17 +2,26 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 
 class Command(BaseHalaroseCsvImporter):
-    council_id = "E09000024"
-    addresses_name = "local.2018-05-03/Version 1/polling_station_export-2018-04-16.csv"
-    stations_name = "local.2018-05-03/Version 1/polling_station_export-2018-04-16.csv"
-    elections = ["local.2018-05-03"]
+    council_id = "MRT"
+    addresses_name = (
+        "2021-04-16T12:08:03.317001/polling_station_export-2021-04-15 Merton.csv"
+    )
+    stations_name = (
+        "2021-04-16T12:08:03.317001/polling_station_export-2021-04-15 Merton.csv"
+    )
+    elections = ["2021-05-06"]
+    csv_delimiter = ","
     csv_encoding = "windows-1252"
 
     def address_record_to_dict(self, record):
-        if record.housepostcode == "CR4 4JA":
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "48108063",  # HOUSE 159 COMMONSIDE EAST, MITCHAM
+        ]:
             return None
 
-        if record.houseid == "30851":
+        if record.housepostcode in ["KT3 6LZ", "SW19 2FJ", "SM4 6BE", "SM4 6BB"]:
             return None
 
         return super().address_record_to_dict(record)
