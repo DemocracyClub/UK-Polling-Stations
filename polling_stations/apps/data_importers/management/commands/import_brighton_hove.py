1e24e48d86a4d568d1a100f20841a0d874ae9358
Alex Dutton <alex@cottagelabs.com>
2021-04-20 17:39:08 +0100

    Import Brighton and Hove (closes #3430)
    > UPRNS ASSIGNED STATION ID        : 137829
    >  - As % of uprns in addressbase  : 85.5%
    >  - As % of rows in council csv   : 87.0%
    
    Looks a bit low, but probably fine?

--- polling_stations/apps/data_importers/management/commands/import_brighton_hove.py
+++ polling_stations/apps/data_importers/management/commands/import_brighton_hove.py
@@ -2,40 +2,20 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E06000043"
+    council_id = "BNH"
     addresses_name = (
-        "parl.2019-12-12/Version 1/Democracy_Club__12December2019bright.TSV"
+        "2021-03-31T10:12:51.372204/Brighton and Hove Democracy_Club__06May2021.tsv"
     )
-    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019bright.TSV"
-    elections = ["parl.2019-12-12"]
+    stations_name = (
+        "2021-03-31T10:12:51.372204/Brighton and Hove Democracy_Club__06May2021.tsv"
+    )
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
-    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
-        uprn = record.property_urn.strip().lstrip("0")
 
-        # user error report #206
-        if record.addressline6.strip() == "BN3 1DN":
-            return None
-
-        if uprn in [
-            "22249017",  # BN13RH -> BN13PJ : Flat 1, 22 Buckingham Road, Brighton
-            "22200182",  # BN33PY -> BN33JN : Maisonette (1ST/2ND/F), 64 Wilbury Road, Hove
-            "22132382",  # BN34HB -> BN34HA : Ground Floor, 26 Braemore Road, Hove
-            "22125165",  # BN15DQ -> BN37BE : First Floor, 37 Old Shoreham Road, Brighton
-            "22258781",  # BN20AL -> BN20AJ : Deputy House Mistress Flat New House, Brighton College, Eastern Road, Brighton
-        ]:
-            rec["accept_suggestion"] = True
-
-        if uprn in [
-            "22259748",  # BN21AU -> BN21PD : Flat 2 (First Rear), 21 Burlington Street, Brighton
-            "22253791",  # BN21TN -> BN14QE : Flat At, 16 Madeira Place, Brighton
-            "22131280",  # BN31AE -> BN12PG : Lower Flat, 15 Western Road, Hove
-            # addressbase errors - TODO: report to OS
-            "22125165",
-            "22177543",
-        ]:
-            rec["accept_suggestion"] = False
+        if record.addressline6 in ["BN2 4RF", "BN1 7ET", "BN1 8NF", "BN1 3AE"]:
+            return None  # split
 
         return rec
