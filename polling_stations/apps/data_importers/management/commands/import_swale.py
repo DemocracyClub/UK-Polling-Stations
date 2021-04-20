2f489db038206f3466c95fde5ee265f98ecfe665
GeoWill <roper.william@gmail.com>
2021-04-20 16:05:38 +0100

    Import script for Swale (closes #3645)

--- polling_stations/apps/data_importers/management/commands/import_swale.py
+++ polling_stations/apps/data_importers/management/commands/import_swale.py
@@ -2,38 +2,27 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 
 class Command(BaseHalaroseCsvImporter):
-    council_id = "E07000113"
-    addresses_name = (
-        "parl.2019-12-12/Version 1/polling_station_export-2019-11-04swale.csv"
-    )
-    stations_name = (
-        "parl.2019-12-12/Version 1/polling_station_export-2019-11-04swale.csv"
-    )
-    elections = ["parl.2019-12-12"]
-    allow_station_point_from_postcode = False
+    council_id = "SWL"
+    addresses_name = "2021-04-19T15:31:29.401515/polling_station_export-2021-03-02.csv"
+    stations_name = "2021-04-19T15:31:29.401515/polling_station_export-2021-03-02.csv"
+    elections = ["2021-05-06"]
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
         uprn = record.uprn.strip().lstrip("0")
+        if record.housepostcode in [
+            "ME13 7NB",
+            "ME10 2EF",
+            "ME12 2HP",
+            "ME12 2SG",
+            "ME10 3TU",
+            "ME12 1TF",
+        ]:
+            return None
 
         if uprn in [
-            "200001875126",  # ME122HP -> ME122LZ : Morland House Augustine Road, Minster-on-Sea, Sheerness, Kent
-            "100061078702",  # ME122LE -> ME122LA : Uppal Villa Minster Drive, Minster-on-Sea, Sheerness, Kent
-            "100061081138",  # ME122LS -> ME122LX : 150 Scarborough Drive, Minster-on-Sea, Sheerness, Kent
-            "10023197317",  # ME122SG -> ME122SH : 1A Baldwin Cottage Baldwin Road, Minster-on-Sea, Sheerness, Kent
-            "10023197934",  # ME122LT -> ME122LX : 152 Scarborough Drive, Minster-on-Sea, Sheerness, Kent
-            "100061080835",  # ME123JE -> ME123HT : 1A Rosemary Avenue, Minster-on-Sea, Sheerness, Kent
-            "10023200030",  # ME103TU -> ME123TU : 28 Petunia Avenue, Minster-on-Sea, Sheerness, Kent
-            "100061083074",  # ME122SG -> ME122SD : Llamedos The Glen, Minster-on-Sea, Sheerness, Kent
-            "200002539987",  # ME101NL -> ME101NS : Flat L 94 London Road, Sittingbourne, Kent
+            "200002533735",  # POPPINGTON BUNGALOW, WHITE HILL, SELLING, FAVERSHAM
+            "200002536488",  # SIMEL HOUSE, MINTCHING WOOD LANE, KINGSDOWN, SITTINGBOURNE
         ]:
-            rec["accept_suggestion"] = True
+            return None
 
-        if uprn in [
-            "100062087806",  # ME121TP -> ME122RT : Flat 2 36A Broadway, Sheerness, Kent
-            "100062087803",  # ME121TP -> ME122RT : Flat 1 36A Broadway, Sheerness, Kent
-            "10023196555",  # ME122DH -> ME121AG : 3 The Crescent Parklands Village The Broadway, Minster-on-Sea, Sheerness, Kent
-        ]:
-            rec["accept_suggestion"] = False
-
-        return rec
+        return super().address_record_to_dict(record)
