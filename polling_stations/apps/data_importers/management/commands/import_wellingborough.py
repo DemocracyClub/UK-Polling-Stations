ccc585fbbc7839c2a6c7b8449bb1d236f2f75653
Alex Dutton <alex@cottagelabs.com>
2021-04-20 18:13:05 +0100

    Import Wellingborough (closes #3431)

--- polling_stations/apps/data_importers/management/commands/import_wellingborough.py
+++ polling_stations/apps/data_importers/management/commands/import_wellingborough.py
@@ -2,13 +2,31 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000156"
+    council_id = "WEL"
     addresses_name = (
-        "parl.2019-12-12/Version 1/Democracy_Club__12December2019welling.csv"
+        "2021-03-31T10:18:02.280304/Wellingborough Democracy_Club__06May2021 (2).tsv"
     )
     stations_name = (
-        "parl.2019-12-12/Version 1/Democracy_Club__12December2019welling.csv"
+        "2021-03-31T10:18:02.280304/Wellingborough Democracy_Club__06May2021 (2).tsv"
     )
-    elections = ["parl.2019-12-12"]
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
-    allow_station_point_from_postcode = False
+
+    def address_record_to_dict(self, record):
+        if record.addressline6 in [
+            "NN8 3JX",
+            "NN29 7NL",
+            "NN29 7AW",
+            "NN8 4WG",
+            "NN8 1SH",
+            "NN7 1NP",
+            "NN6 0FT",
+        ]:
+            return None  # split
+
+        if record.property_urn.lstrip(" 0") in [
+            "10024133595",  # long distance to polling station; leads to NN6 0FT split
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
