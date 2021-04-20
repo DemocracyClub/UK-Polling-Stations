3210465311dac41068713bfa0e0636e946fcc7e6
GeoWill <roper.william@gmail.com>
2021-04-20 12:14:01 +0100

    Import script for Richmond (closes #3613)

--- polling_stations/apps/data_importers/management/commands/import_richmond.py
+++ polling_stations/apps/data_importers/management/commands/import_richmond.py
@@ -2,9 +2,18 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E09000027"
-    addresses_name = "local.2018-05-03/Version 2/Democracy_Club__03May2018 Richmond.CSV"
-    stations_name = "local.2018-05-03/Version 2/Democracy_Club__03May2018 Richmond.CSV"
-    elections = ["local.2018-05-03"]
-    csv_delimiter = ","
+    council_id = "RIC"
+    addresses_name = "2021-04-16T09:27:30.944565/Democracy_Club__06May2021.tsv"
+    stations_name = "2021-04-16T09:27:30.944565/Democracy_Club__06May2021.tsv"
+    elections = ["2021-05-06"]
+    csv_delimiter = "\t"
     csv_encoding = "windows-1252"
+
+    def address_record_to_dict(self, record):
+        if record.addressline6 in [
+            "TW12 2SB",
+            "SW13 9RF",
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
