aff39ece24fb3ec5047d7f920a42ac51178c06e8
Alex Dutton <alex@cottagelabs.com>
2021-04-21 12:25:50 +0100

    Import Solihull (closes #3493)

--- polling_stations/apps/data_importers/management/commands/import_solihull.py
+++ polling_stations/apps/data_importers/management/commands/import_solihull.py
@@ -3,91 +3,79 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E08000029"
-    addresses_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019.CSV"
-    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019.CSV"
-    elections = ["parl.2019-12-12"]
-    allow_station_point_from_postcode = False
+    council_id = "SOL"
+    addresses_name = "2021-04-07T11:17:55.688819/Democracy_Club__06May2021.CSV"
+    stations_name = "2021-04-07T11:17:55.688819/Democracy_Club__06May2021.CSV"
+    elections = ["2021-05-06"]
 
     def station_record_to_dict(self, record):
+        # Whar Hall Road Community Centre's postcode doesn't match AddressBase, but
+        # online sources agree with the council. As the postcodes are adjacent, we'll
+        # leave this unmodified from what's in the CSV. First noted in 2019, and checked
+        # and carried across for 2021.
+
         # These polling places have a UPRN, and the addressbase postcode doesn't match
         # the postcode from the council. In these cases the addressbase postcode matches
         # the postcode used on the venue's website.
-        # Online references toStation ID 7680 (Whar Hall Road Community Centre) don't
-        # align with addressbase, but the postcodes are adjacent. So leaving postcode
-        # as is in the CSV.
-        if record.polling_place_id == "7518":  # Barston Memorial Institute
-            record = record._replace(polling_place_postcode="B92 0JU")
-        if record.polling_place_id == "7550":  # St Clements Church
-            record = record._replace(polling_place_postcode="B36 0BA")
-        if record.polling_place_id == "7561":  # Kingshurst Evangelical Church
+        if record.polling_place_id == "8918":  # KEC Church Centre
             record = record._replace(polling_place_postcode="B37 6NP")
+        # Fixes carried forward
+        if record.polling_place_id == "8877":  # Barston Memorial Institute
+            record = record._replace(polling_place_postcode="B92 0JU")
+        if record.polling_place_id == "8907":  # St Clements Church
+            record = record._replace(polling_place_postcode="B36 0BA")
         if (
-            record.polling_place_id == "7626"
+            record.polling_place_id == "9001"
         ):  # The Royal British Legion (Knowle) Club Limited
             record = record._replace(polling_place_postcode="B93 9LU")
-        if record.polling_place_id == "7660":  # Woodlands Campus
+        if record.polling_place_id == "9138":  # Woodlands Campus
             record = record._replace(polling_place_postcode="B36 0NF")
 
         # Fixes carried forward
         # Three Trees Community Centre
-        if record.polling_place_id == "7571":
+        if record.polling_place_id == "8928":
             record = record._replace(polling_place_uprn="100071461342")
         # Dorridge Methodist Church
-        if record.polling_place_id == "7586":
+        if record.polling_place_id == "8945":
             record = record._replace(polling_place_uprn="100071001475")
 
         rec = super().station_record_to_dict(record)
 
         # Tudor Grange Leisure Centre
-        if record.polling_place_id == "7726":
+        if record.polling_place_id == "9156":
             rec["location"] = Point(-1.7881577, 52.4124167, srid=4326)
 
         # Catherine de Barnes Village Hall
-        if record.polling_place_id == "7515":
+        if record.polling_place_id == "8873":
             rec["location"] = Point(-1.7382134, 52.4203089, srid=4326)
 
         return rec
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn == "10090949380":
-            rec["postcode"] = "B93 0FH"
-
-        if (record.addressline1, record.addressline2) == (
-            "101 Noble Way",
-            "Cheswick Green",
-        ):
-            rec["uprn"] = "10090950327"
-            rec["accept_suggestion"] = True
-
-        if uprn in [
-            "10090945527",  # B377RN -> B376RL : 3C Woodlands Way, Chelmsley Wood
-            "10090945525",  # B377RN -> B376RL : 3A Woodlands Way, Chelmsley Wood
-        ]:
-            rec["accept_suggestion"] = True
 
         if record.addressline6 in [
-            "B90 4AY",  # stray odd-looking property
-            "CV7 7HL",  # single property with spurious-looking station
+            "B37 7WD",  # 10 properties embedded in another area
         ]:
             return None
 
-        if uprn in [
-            "100071001341",  # B911DA -> B911JW : 90 Grange Road, Solihull
-            "10090946742",  # B901FT -> B930EJ : Apartment 16, Leasowes House, 3 Main Street, Dickens Heath, Solihull
-            "10090948318",  # B901GL -> B913AB : Apartment 5, Market Court, 61 Old Dickens Heath Road, Shirley, Solihull
-            "10090947804",  # CV49BN -> B901FT : 12 Eagle Drive, Solihull
-            "200003834455",  # B927AW -> B927AH : St Michaels Residential Home, 251 Warwick Road, Solihull
-            "10090946771",  # B920JP -> B930FD : Caravan Firs Farm, Barston Lane, Solihull
-            "10090948319",  # B912AW -> B913AB : Flat 2, 58 Lode Lane, Solihull
-            "100070965323",  # B376ES -> B376EU : 77 Overgreen Drive, Kingshurst
-            "100070965320",  # B376ES -> B376EU : 77A Overgreen Drive, Kingshurst
-            "100070965321",  # B376ES -> B376EU : 77B Overgreen Drive, Kingshurst
-            "100070965322",  # B376ES -> B376EU : 77C Overgreen Drive, Kingshurst
+        if record.addressline6 in [
+            "B37 7RN",
+            "B37 7HW",
+            "B37 7LY",
+            "B92 8NA",
+            "B93 8PP",
+            "B37 6EU",
+            "B93 9JN",
+            "CV7 7EG",
+            "CV7 7DG",
+            "CV7 7GY",
+            "B91 1UQ",
+            "B90 3QQ",
+            "B90 3EE",
+            "B91 2UN",
+            "B91 2BP",
         ]:
-            rec["accept_suggestion"] = False
+            return None  # split
 
         return rec
