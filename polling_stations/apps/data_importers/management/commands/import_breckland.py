71e3ba6d63be2cd37dc824c8c709abef2058d61a
smsmith97 <sean.m.smith97@outlook.com>
2021-04-21 14:53:57 +0100

    import script for breckland (closes #3508)

--- polling_stations/apps/data_importers/management/commands/import_breckland.py
+++ polling_stations/apps/data_importers/management/commands/import_breckland.py
@@ -1,64 +1,60 @@
-from uk_geo_utils.helpers import Postcode
 from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000143"
-    addresses_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019breck.tsv"
-    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019breck.tsv"
-    elections = ["parl.2019-12-12"]
+    council_id = "BRE"
+    addresses_name = "2021-04-07T16:45:08.424818/Democracy_Club__06May2021.tsv"
+    stations_name = "2021-04-07T16:45:08.424818/Democracy_Club__06May2021.tsv"
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
     csv_encoding = "windows-1252"
 
     def station_record_to_dict(self, record):
 
-        if record.polling_place_id in ["8277", "8377", "8521"]:
+        if record.polling_place_id in [
+            "10265",  # Rocklands Village Hall The Street Rocklands
+            "10216",  # Ovington Village Hall Church Road Ovington
+            "10163",  # Oxborough Village Hall Swaffham Road Oxborough
+        ]:
             # The E/N points are the wrong way round for these 3
-            # perform the old switcheroo
+            # perform the ol' switcheroo
             easting = record.polling_place_northing
             northing = record.polling_place_easting
             record = record._replace(polling_place_easting=easting)
             record = record._replace(polling_place_northing=northing)
 
-        if record.polling_place_id == "8440":
-            # this one is just miles off
-            # remove it and fall back to geocoding by postcode
-            record = record._replace(polling_place_easting="0")
-            record = record._replace(polling_place_northing="0")
-
-        if record.polling_place_id == "8147":
+        # Dereham Town Football Club Aldiss Park Norwich Road, Dereham
+        if record.polling_place_id == "9911":
             record = record._replace(polling_place_easting="601038")
             record = record._replace(polling_place_northing="313511")
+
+        # The Red Lion East Church Street Kenninghall NR16 2EP
+        if record.polling_place_id == "9996":
+            record = record._replace(polling_place_easting="")
+            record = record._replace(polling_place_northing="")
 
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
-        if uprn == "10011992099":
-            rec["postcode"] = "PE32 2DQ"
-
-        if Postcode(record.addressline6).with_space == "IP24 1QS":
+        if uprn in [
+            "100091495478",  # M LAMBERT PLANT LTD, MILL POND FARM, THETFORD ROAD, GARBOLDISHAM, DISS
+            "10011976645",  # HAWTHORNS, BOTTLE CORNER, BESTHORPE, ATTLEBOROUGH
+            "10011993736",  # 1A SCARNING FEN, DEREHAM
+            "10011990571",  # THE SPINNEY, SCHOOL ROAD, BRISLEY, DEREHAM
+        ]:
             return None
 
-        if (
-            record.addressline1 == "122/123"
-            and record.addressline2 == "Green Lane Quidenham"
-            and record.addressline3 == "Norwich"
-            and record.addressline4 == "Norfolk"
-        ):
+        if record.post_code in [
+            "IP25 6TF",
+            "IP25 7BS",
+            "IP24 2JJ",
+            "IP25 2JB",
+            "IP25 6XY",
+            "IP25 6YU",
+            "PE37 7JP",
+        ]:
             return None
 
-        if uprn in [
-            "10011984733"  # PE322NB -> PE322RP : Lilac Cottages, 2 Litcham Road, Beeston, Kings Lynn, Norfolk
-        ]:
-            rec["accept_suggestion"] = True
-
-        if uprn in [
-            "100091305655",  # NR171DY -> NR171EA : Red Lodge, Watton Road, Shropham, Attleborough, Norfolk
-            "100090787475",  # IP257AX -> IP257BL : Flint Cottage, Hale Road, Ashill, Thetford, Norfolk
-        ]:
-            rec["accept_suggestion"] = False
-
-        return rec
+        return super().address_record_to_dict(record)
