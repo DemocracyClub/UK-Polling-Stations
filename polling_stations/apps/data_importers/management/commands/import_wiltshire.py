d886e669e6a381228ec985ff52abadcbb5c801d0
Alex Dutton <alex@cottagelabs.com>
2021-04-21 11:23:33 +0100

    Import Wiltshire (closes #3595)

--- polling_stations/apps/data_importers/management/commands/import_wiltshire.py
+++ polling_stations/apps/data_importers/management/commands/import_wiltshire.py
@@ -1,35 +1,89 @@
-from django.contrib.gis.geos import Point
 from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E06000054"
-    addresses_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019wilt.tsv"
-    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019wilt.tsv"
-    elections = ["parl.2019-12-12"]
+    council_id = "WIL"
+    addresses_name = "2021-04-14T17:40:57.372275/wiltshire-deduped.tsv"
+    stations_name = "2021-04-14T17:40:57.372275/wiltshire-deduped.tsv"
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
-    allow_station_point_from_postcode = False
+
+    # Checked 63862 (out of council area) and 64258/64219 proximity.
+
+    # These were found because they weren't geocodeable, and so are presumably wrong
+    station_postcode_fixes = {
+        # Easterton Village Hall, Easterton, Devizes
+        # https://wvha.org.uk/listing/easterton-village-hall/
+        ("63510", "SN10 4P"): "SN10 4PS",
+        # Edington Parish Hall, Edington, Westbury
+        ("64399", ""): "",
+        # Tidworth Community Centre, Wylye Road Tidworth
+        # https://tidworthtowncouncil.gov.uk/tidworth-community-centre/
+        ("63590", "SP9 7QH"): "SP9 7QQ",
+        # Beanacre Church Schoolroom, Beanacre, Melksham
+        ("64478", "SN12"): "",
+        # Great Bedwyn Cricket Club, Frog Lane, Great Bedwyn, Marlborough
+        # http://www.wccl.org.uk/team_info.php?id=1247
+        ("63542", "SN8 3PD"): "SN8 3PB",
+        # Wilton Community Centre, West Street, Wilton, Salisbury
+        # https://wvha.org.uk/listing/wilton-community-centre/
+        ("63870", "SP2 0DJ"): "SP2 0DG",
+        # Easton Royal Village Hall, Easton Royal, Pewsey
+        # https://wvha.org.uk/listing/easton-royal-village-hall/
+        ("63514", ""): "SN9 5LY",
+        # Baydon Young Peoples Hall, Baydon, Marlborough
+        # http://www.baydon.org/BYPA.htm
+        ("63447", ""): "SN8 2JE",
+    }
 
     def station_record_to_dict(self, record):
-        # user issue report #126
-        # Bradford on Avon Youth Development Centre
-        if record.polling_place_id == "58067":
-            rec = super().station_record_to_dict(record)
-            rec["location"] = Point(-2.250676, 51.341552, srid=4326)
-            return rec
+        if (
+            record.polling_place_id,
+            record.polling_place_postcode,
+        ) in self.station_postcode_fixes:
+            record = record._replace(
+                polling_place_postcode=self.station_postcode_fixes[
+                    (record.polling_place_id, record.polling_place_postcode)
+                ]
+            )
 
         return super().station_record_to_dict(record)
 
+    #
     def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
+        if record.addressline6.strip() in [
+            "SN8 4AF",
+            "SN10 4AD",
+            "SN10 3SQ",
+            "SN8 1QB",
+            "SP4 9QE",
+            "SN10 3DD",
+            "SN10 5HE",
+            "SN10 2PA",
+            "SN8 3DY",
+            "SP5 2BZ",
+            "SP5 2NL",
+            "SP5 2DT",
+            "SP4 8JD",
+            "SP4 7PB",
+            "SN11 0PQ",
+            "SN5 0AB",
+            "SN16 9ES",
+            "BA13 4LY",
+            "SN15 3SX",
+            "SN11 8EJ",
+            "SN15 5EY",
+            "BA14 7DW",
+            "SP4 7FF",
+            "SN15 3RW",
+            "SN5 4HB",
+            "SN8 4NR",
+            "SN14 6HT",
+            "SN10 3EZ",
+            "SP3 6DY",
+            "SN10 4QP",
+            "SN8 1HG",
+        ]:
+            return None  # split
 
-        if (record.addressline1, record.addressline2, record.addressline3) == (
-            "Hedgerow Stables",
-            "Paget Road",
-            "Ludgershall",
-        ) and uprn == "10013826068":
-            return None
-
-        rec = super().address_record_to_dict(record)
-
-        return rec
+        return super().address_record_to_dict(record)
