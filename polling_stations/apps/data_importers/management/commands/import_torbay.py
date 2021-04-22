135aa5ddbf7f8ede82057b117f5dd917c2303c73
smsmith97 <sean.m.smith97@outlook.com>
2021-04-22 12:13:50 +0100

    import script for torbay (closes #3529)

--- polling_stations/apps/data_importers/management/commands/import_torbay.py
+++ polling_stations/apps/data_importers/management/commands/import_torbay.py
@@ -3,88 +3,72 @@ from django.contrib.gis.geos import Point
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E06000027"
-    addresses_name = "parl.2019-12-12/Version 1/merged.tsv"
-    stations_name = "parl.2019-12-12/Version 1/merged.tsv"
-    elections = ["parl.2019-12-12"]
+    council_id = "TOB"
+    addresses_name = "2021-04-09T11:31:36.785601/Democracy_Club__06May2021.tsv"
+    stations_name = "2021-04-09T11:31:36.785601/Democracy_Club__06May2021.tsv"
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
-    allow_station_point_from_postcode = False
+    csv_encoding = "windows-1252"
 
     def station_record_to_dict(self, record):
-        if record.polling_place_id == "7553":
+
+        # Central Garage Showroom Milton Street Brixham TQ5 OBY
+        if record.polling_place_id == "8248":
             record = record._replace(polling_place_postcode="TQ5 0BX")
 
-        if record.polling_place_id == "7530":
-            record = record._replace(polling_place_postcode="TQ5 9HW")
+        # Paignton SNU Spiritualist Church Hall Manor Corner Torquay Road Paignton TQ3 1JB
+        if record.polling_place_id == "8230":
+            record = record._replace(polling_place_postcode="TQ3 2JB")
+
+        # St Annes Hall Babbacombe Road Torquay TQ1 3UH
+        if record.polling_place_id == "8242":
+            record = record._replace(polling_place_postcode="")
 
         rec = super().station_record_to_dict(record)
 
-        if record.polling_place_id == "7418":
+        # Mobile Station at DFS Car Park Willows Retail Park Nicholson Road Torquay TQ2 7TD
+        if record.polling_place_id == "8181":
             rec["location"] = Point(-3.556830, 50.488924, srid=4326)
 
         return rec
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
-        # This was a last minute import,
-        # so I've taken a fairly coarse approach to throwing things away...
-        if record.addressline6 in [
-            "TQ2 6XA",
-            "TQ4 6JX",
-            "TQ5 0BX",
-            "TQ4 6LH",
-            "TQ5 0AE",
-            "TQ1 3QF",
-            "TQ4",
+        if uprn in [
+            "10024001656",  # GATEHOUSE COTTAGE MUDSTONE LANE, BRIXHAM
+            "100040534883",  # ZOO AND GARDENS NORTH LODGE TOTNES ROAD, PAIGNTON
+            "10094527880",  # 140A TOTNES ROAD, PAIGNTON
+            "100040529632",  # 16 OLD TORQUAY ROAD, PAIGNTON
+            "100040526403",  # SMALLCOMBE, KINGS ASH ROAD, PAIGNTON
+            "10093142379",  # 73A BLATCHCOMBE ROAD, PAIGNTON
+            "10024003083",  # 30 SHORTON ROAD, PAIGNTON
+            "100040532454",  # 28 SHORTON ROAD, PAIGNTON
+            "10094526711",  # BARWOOD MEWS LOWER WARBERRY ROAD, TORQUAY
+            "100041187053",  # TREETOPS, LOWER WARBERRY ROAD, TORQUAY
+            "100040548856",  # FLAT 7, WARBERRY HOUSE, LOWER WARBERRY ROAD, TORQUAY
+            "100040548855",  # FLAT 6, WARBERRY HOUSE, LOWER WARBERRY ROAD, TORQUAY
+            "100040548852",  # FLAT 1, WARBERRY HOUSE, LOWER WARBERRY ROAD, TORQUAY
+            "100040548854",  # FLAT 5, WARBERRY HOUSE, LOWER WARBERRY ROAD, TORQUAY
+            "100040539413",  # FLAT A, CLIFTON HOUSE, BRADDONS HILL ROAD EAST, TORQUAY
+            "100040539414",  # FLAT B, CLIFTON HOUSE, BRADDONS HILL ROAD EAST, TORQUAY
+            "10094527881",  # THE MEADOWS, AVENUE ROAD, TORQUAY
+            "10093141340",  # FLAT 2 ODDICOMBE HALL BABBACOMBE DOWNS ROAD, ST MARYCHURCH, TORQUAY
+            "100040538743",  # DOWNSVIEW CORNER, BEDFORD ROAD, TORQUAY
+            "10002985724",  # 8B ST. PAULS ROAD, TORQUAY
+            "100040547447",  # 42 ISAACS ROAD, TORQUAY
+            "100040541253",  # CHERRY BLOSSOM FARM, CLADDON LANE, MAIDENCOMBE, TORQUAY
+            "100041198763",  # 308 DARTMOUTH ROAD, PAIGNTON
+            "200002083269",  # PONTOON ADJ PRINCESS PIER, PONTOON ADJ PRINCESS PIER TORBAY ROAD, TORQUAY
+            "100041200459",  # HOMELANDS, BERRY HEAD ROAD, BRIXHAM
+            "100040513373",  # JESMOND COTTAGE MILTON STREET, BRIXHAM
+            "100040519265",  # SUNNYBANK BRIXHAM ROAD, PAIGNTON
+            "100040520900",  # THE MANSE CLIFTON ROAD, PAIGNTON
+            "100041189794",  # THE OLD DAIRY, TEIGNMOUTH ROAD, MAIDENCOMBE, TORQUAY
         ]:
             return None
 
-        if uprn in [
-            "100040519265",
-            "200002083269",
-            "10024003870",
-            "10002991569",
-            "100040529632",
-            "100040562013",
-            "100040520900",
-            "10093141050",
-            "10093141805",
-            "10093142379",
-            "100040537640",
-            "100041189794",
-            "10002991567",
-            "10002991571",
-        ]:
+        if record.addressline6 in ["TQ1 4QZ"]:
             return None
 
-        if uprn in [
-            "100041196260",  # TQ26AS -> TQ26AP : Millbrook House Hotel, Old Mill Road, Torquay
-            "200001778568",  # TQ14AF -> TQ14AG : 1C Magdalene Rd Rear Of, 17 Upton Road, Torquay
-            "100040561357",  # TQ14LR -> TQ14LH : Flat 2 Westhill Court, 12 Westhill Avenue, Torquay
-            "100040561358",  # TQ14LR -> TQ14LH : Flat 3 Westhill Court, 12 Westhill Avenue, Torquay
-            "100040559288",  # TQ11EJ -> TQ11EG : 2 Apsley House, Torwood Gardens Road, Torquay
-            "100040559289",  # TQ11EJ -> TQ11EG : 3 Apsley House, Torwood Gardens Road, Torquay
-            "10002989359",  # TQ50BX -> TQ50BP : The Hayloft, 26 Milton Street, Brixham
-        ]:
-
-            rec["accept_suggestion"] = True
-
-        if uprn in [
-            "100040523809",  # TQ26AU -> TQ46AU : Cottage C, 1 Old Mill Road, Torquay
-            "100041187152",  # TQ12EA -> TQ11BN : Cottage 1 Sundial Lodge, Park Hill Road, Torquay
-            "100041187013",  # TQ12EA -> TQ11BN : Mews 8 Sundial Lodge, Park Hill Road, Torquay
-            "100040534293",  # TQ32HW -> TQ32SF : Bottom Flat, 239 Torquay Road, Paignton
-            "10000012748",  # TQ32HT -> TQ50EH : 1 Manor Court, Manor Road, Paignton
-            "10000012807",  # TQ32HT -> TQ50EH : 7 Manor Court, Manor Road, Paignton
-            "10000012810",  # TQ32HT -> TQ50EH : 4 Manor Court, Manor Road, Paignton
-            "100041195363",  # TQ32HT -> TQ25BZ : 9 Manor Court, Manor Road, Paignton
-            "10000012832",  # TQ32HT -> TQ50EH : 5 Manor Court, Manor Road, Paignton
-            "10000012760",  # TQ32HT -> TQ50EH : 2 Manor Court, Manor Road, Paignton
-            "10000012746",  # TQ32HT -> TQ50EH : 3 Manor Court, Manor Road, Paignton
-            "10000012765",  # TQ32HT -> TQ50EH : 6 Manor Court, Manor Road, Paignton
-        ]:
-            rec["accept_suggestion"] = False
-
-        return rec
+        return super().address_record_to_dict(record)
