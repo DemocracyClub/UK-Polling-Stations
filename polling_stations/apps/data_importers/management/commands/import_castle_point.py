2c13f48ff2aa7903bdbaa7d5752f06231da9eed3
smsmith97 <sean.m.smith97@outlook.com>
2021-04-22 12:35:19 +0100

    import script for castle point (closes #3544)

--- polling_stations/apps/data_importers/management/commands/import_castle_point.py
+++ polling_stations/apps/data_importers/management/commands/import_castle_point.py
@@ -2,30 +2,34 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000069"
-    addresses_name = (
-        "parl.2019-12-12/Version 1/Democracy_Club__12December2019castle.tsv"
-    )
-    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019castle.tsv"
-    elections = ["parl.2019-12-12"]
+    council_id = "CAS"
+    addresses_name = "2021-04-12T09:02:05.837447/Democracy_Club__06May2021.tsv"
+    stations_name = "2021-04-12T09:02:05.837447/Democracy_Club__06May2021.tsv"
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
-    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
-        rec = super().address_record_to_dict(record)
 
         if uprn in [
-            "10004934986",
-            "100090361765",
-            "10004940667",
-            "10004941055",
+            "100090361765",  # 35 LONDON ROAD, BENFLEET
+            "100091600419",  # THE GRANGE, GRANGE ROAD, BENFLEET
+            "10004940533",  # CARAVAN 9E2 THORNEY BAY CARAVAN PARK THORNEY BAY ROAD, CANVEY ISLAND
+            "10004940535",  # CARAVAN 9I3 THORNEY BAY CARAVAN PARK THORNEY BAY ROAD, CANVEY ISLAND
+            "10004940532",  # CARAVAN 8A1 THORNEY BAY CARAVAN PARK THORNEY BAY ROAD, CANVEY ISLAND
+            "10004940527",  # CARAVAN 8F5 THORNEY BAY CARAVAN PARK THORNEY BAY ROAD, CANVEY ISLAND
+            "10004934986",  # 5 STATION ROAD, CANVEY ISLAND
+            "100090382770",  # 62 WAARDEN ROAD, CANVEY ISLAND
         ]:
             return None
 
-        if record.addressline1 == "Caravan 1112A Thorney Bay Park":
-            rec["postcode"] = "SS8 0DB"
-        if uprn == "10004937052":
-            rec["postcode"] = "SS8 7SL"
+        if record.addressline6 in [
+            "SS8 9JH",
+            "SS8 8HN",
+            "SS8 7RN",
+            "SS8 9SL",
+            "SS7 3PJ",
+        ]:
+            return None
 
-        return rec
+        return super().address_record_to_dict(record)
