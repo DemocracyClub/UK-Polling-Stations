c1d508ba6396d765e24e69c41564b29946dcf6a5
smsmith97 <sean.m.smith97@outlook.com>
2021-04-23 11:36:14 +0100

    import script for coventry (closes #3569)

--- polling_stations/apps/data_importers/management/commands/import_coventry.py
+++ polling_stations/apps/data_importers/management/commands/import_coventry.py
@@ -2,83 +2,75 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E08000026"
-    addresses_name = "parl.2019-12-12/Version 2/Democracy_Club__12December2019.tsv"
-    stations_name = "parl.2019-12-12/Version 2/Democracy_Club__12December2019.tsv"
-    elections = ["parl.2019-12-12"]
+    council_id = "COV"
+    addresses_name = "2021-04-13T12:12:57.429911/Coventry_deduped.tsv"
+    stations_name = "2021-04-13T12:12:57.429911/Coventry_deduped.tsv"
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
-    allow_station_point_from_postcode = False
 
     def station_record_to_dict(self, record):
-        # Community Centre, Warwickshire Shopping Park
-        if record.polling_place_id == "11876":
+        # Community Centre Warwickshire Shopping Park Kynner Way Binley Coventry CV3 2BS
+        if record.polling_place_id == "13766":
             record = record._replace(polling_place_postcode="CV3 2SB")
 
-        # Parkgate Primary School
-        if record.polling_place_id == "11954":
+        # Parkgate Primary School Parkgate Road Coventry CV5 7LR
+        if record.polling_place_id == "13490":
             record = record._replace(polling_place_postcode="CV6 4GF")
 
-        if record.polling_place_id == "12190":
+        # St. James Church Hall Westcotes Coventry CV4 9BD
+        if record.polling_place_id == "13700":
             record = record._replace(polling_place_easting="429643")
             record = record._replace(polling_place_northing="278413")
 
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
-        if record.addressline6.strip() in [
-            "CV3 1QH",
-            "CV4 9AJ",
-            "CV4 9YJ",
-            "CV6 2GE",
-            "CV6 2EQ",
+        if uprn in [
+            "10091717520",  # 224 TORRINGTON AVENUE, COVENTRY
+            "100071515845",  # 2A NEWCOMBE ROAD, COVENTRY
+            "10094849274",  # FLAT 3 279 WALSGRAVE ROAD, COVENTRY
+            "10094849275",  # FLAT 4 279 WALSGRAVE ROAD, COVENTRY
+            "10094849273",  # FLAT 2 279 WALSGRAVE ROAD, COVENTRY
+            "10093949639",  # FLAT 3 119 WALSGRAVE ROAD, COVENTRY
+            "10093948773",  # GROUND FLOOR FLAT 119 WALSGRAVE ROAD, COVENTRY
+            "10093948774",  # FIRST FLOOR FLAT 119 WALSGRAVE ROAD, COVENTRY
+            "10091718353",  # 112 WOODWAY LANE, COVENTRY
+            "10024028582",  # FLAT ABOVE THE WHEEL 624 SEWALL HIGHWAY, COVENTRY
+            "10095510481",  # GROUND FLOOR FLAT 658 FOLESHILL ROAD, COVENTRY
+            "10095510480",  # FIRST FLOOR FLAT 658 FOLESHILL ROAD, COVENTRY
+            "10024030569",  # FLAT BUTTS RETREAT 126 BUTTS, COVENTRY
+            "10094852182",  # ANNEXE 358 BROAD LANE, COVENTRY
+            "10095509721",  # FLAT ABOVE BROAD LANE TRADING ESTATE BANNER LANE, COVENTRY
+            "10095510461",  # GROUND FLOOR FLAT 230 HOLBROOK LANE, COVENTRY
+            "10014006474",  # SCHOOL HOUSE WHITMORE PARK PRIMARY SCHOOL HALFORD LANE, COVENTRY
+            "10023032376",  # GROUND FLOOR FLAT 280 CHEVERAL AVENUE, COVENTRY
+            "10023032377",  # FIRST FLOOR FLAT 280 CHEVERAL AVENUE, COVENTRY
+            "10095510454",  # GROUND FLOOR FLAT 2C KENPAS HIGHWAY, COVENTRY
+            "10094853479",  # FLAT C 86 WALSGRAVE ROAD, COVENTRY
+            "10014007628",  # WESTWOOD SOCIAL BUILDING UNIVERSITY OF WARWICK KIRBY CORNER ROAD, COVENTRY
         ]:
             return None
 
-        if uprn == "100071317687":
-            rec["postcode"] = "CV12LL"
-
-        if uprn in [
-            "100071319791",  # CV59AP -> CV59AR : The Cottage, Pickford Grange Lane, Coventry
-            "100071367442",  # CV24ED -> CV24EB : 74 Walsgrave Road, Coventry
-            "10024031054",  # CV36BQ -> CV36PB : 54 Kenpas Highway, Coventry
-            "10024625018",  # CV35FE -> CV12NJ : Annex at 39 Quinton Road, Coventry
-            "10024029198",  # CV14AQ -> CV13BW : Flat F, 4 Barras Lane, Coventry
-            "100071316737",  # CV62AG -> CV62AL : The Nugget, Hollyfast Road, Coventry
+        if record.addressline6 in [
+            "CV3 3FA",
+            "CV3 1QH",
+            "CV2 3FD",
+            "CV2 1TA",
+            "CV2 2XJ",
+            "CV2 2XL",
+            "CV6 7FA",
+            "CV5 9NQ",
+            "CV5 9NR",
+            "CV7 8NJ",
+            "CV2 1PX",
+            "CV4 9LP",
+            "CV4 9YJ",
+            "CV2 1FX",
+            "CV6 5NU",
+            "CV1 4GZ",
         ]:
-            rec["accept_suggestion"] = True
+            return None
 
-        if uprn in [
-            "100071505028",  # CV35AX -> CV35DU : Cornerpost Public House, Watercall Avenue, Coventry
-            "10024621865",  # CV64GR -> CV78RH : 26 Gospel Oak Road, Coventry
-            "10024621863",  # CV64GS -> CV78RH : 1 Gospel Oak Road, Coventry
-            "100070634208",  # CV24LJ -> CV24LS : 17 Clay Lane, Stoke, Coventry
-            "100070624242",  # CV32DS -> CV32HZ : 13 Brinklow Road, Coventry
-            "100070715099",  # CV57BP -> CV57LJ : Tiber Cottage, Tiber Close, Coventry
-            "10024028272",  # CV66BL -> CV62BL : FLAT ABOVE, 267 - 269 Bedworth Road, Coventry
-            "200001562367",  # CV63BP -> CV63HL : Caretakers Flat Westfield House, Radford Road, Coventry
-            "10024620573",  # CV21UR -> CV61PR : 12 Curlew Close, Coventry
-            "10024621864",  # CV64GR -> CV78RH : 58 Gospel Oak Road, Coventry
-        ]:
-            rec["accept_suggestion"] = False
-
-        if uprn in [
-            # Addressbase issues - TODO: report to OS
-            "10023038958",
-            "100070637150",
-            "10024027630",
-            "100071317687",
-            "10024027756",
-            "10023038958",
-            "10023034814",
-            "10023034813",
-            "10023034811",
-            "10023034810",
-            "10023034809",
-            "10023034808",
-        ]:
-            rec["accept_suggestion"] = False
-
-        return rec
+        return super().address_record_to_dict(record)
