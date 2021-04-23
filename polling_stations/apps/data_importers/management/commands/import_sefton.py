3165888ac8e8da3945ccfd32b5bb759bc5c02e69
smsmith97 <sean.m.smith97@outlook.com>
2021-04-23 16:46:55 +0100

    import script for sefton (closes #3592)

--- polling_stations/apps/data_importers/management/commands/import_sefton.py
+++ polling_stations/apps/data_importers/management/commands/import_sefton.py
@@ -2,29 +2,42 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E08000014"
-    addresses_name = "local.2018-05-03/Version 1/Democracy_Club__03May2018.tsv"
-    stations_name = "local.2018-05-03/Version 1/Democracy_Club__03May2018.tsv"
-    elections = ["local.2018-05-03"]
+    council_id = "SFT"
+    addresses_name = "2021-04-14T17:32:40.128471/Sefton.tsv"
+    stations_name = "2021-04-14T17:32:40.128471/Sefton.tsv"
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
-        if record.addressline6 == "L20 9AH":
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "41063021",  # 8 MOOR LANE, THORNTON, LIVERPOOL
+            "41215760",  # 471A LORD STREET, SOUTHPORT
+        ]:
             return None
 
-        if record.addressline6 == "L37 7AQ":
-            return None
-
-        if record.addressline6 == "L20 6EA":
-            return None
-
-        if record.addressline6 == "PR9 9JH":
-            return None
-
-        if record.addressline6 == "L20 5AB":
-            return None
-
-        if record.addressline6 == "L38 7JB":
+        if record.addressline6 in [
+            "L23 7TX",
+            "L20 6AQ",
+            "L30 7PD",
+            "L20 6EA",
+            "L9 5AD",
+            "L31 2JE",
+            "L31 2JF",
+        ]:
             return None
 
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+
+        # Bootle Main Library 220 Stanley Road Bootle L20 3GN
+        if record.polling_place_id == "7799":
+            record = record._replace(polling_place_postcode="")
+
+        # Ford Lane Community Centre Ford Lane Litherland Liverpool L21 7LX
+        if record.polling_place_id == "7737":
+            record = record._replace(polling_place_postcode="")
+
+        return super().station_record_to_dict(record)
