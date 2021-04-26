996a82c48947ad2a563be78af8934c1b06b7d61d
smsmith97 <sean.m.smith97@outlook.com>
2021-04-26 15:16:39 +0100

    import script for newcastle under lyme (closes #3625)

--- polling_stations/apps/data_importers/management/commands/import_newcastle_under_lyme.py
+++ polling_stations/apps/data_importers/management/commands/import_newcastle_under_lyme.py
@@ -2,37 +2,61 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000195"
-    addresses_name = "local.2018-05-03/Version 1/Democracy_Club__03May2018.tsv"
-    stations_name = "local.2018-05-03/Version 1/Democracy_Club__03May2018.tsv"
-    elections = ["local.2018-05-03"]
+    council_id = "NEC"
+    addresses_name = (
+        "2021-04-16T12:45:06.761928/Newcastle under lyme Democracy_Club__06May2021.tsv"
+    )
+    stations_name = (
+        "2021-04-16T12:45:06.761928/Newcastle under lyme Democracy_Club__06May2021.tsv"
+    )
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
 
-        # Postcode supplied for Higherland Methodist Church is incorrect
-        # remove the grid ref and we'll fall back to UPRN
-        if record.polling_place_id == "488":
-            record = record._replace(polling_place_easting="0")
-            record = record._replace(polling_place_northing="0")
+        # Higherland Methodist Church Hall Higherland Newcastle Staffs ST5 2TF
+        if record.polling_place_id == "1927":
+            record = record._replace(polling_place_easting="")
+            record = record._replace(polling_place_northing="")
 
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-
         uprn = record.property_urn.strip().lstrip("0")
 
-        if record.addressline6 == "CW9 9PW":
+        if uprn in [
+            "100032219337",  # ARDENNE THE AVENUE, KIDSGROVE
+            "10024255961",  # 4B WARWICK CLOSE, KIDSGROVE, STOKE-ON-TRENT
+            "10024255960",  # 4A WARWICK CLOSE, KIDSGROVE, STOKE-ON-TRENT
+            "200004607891",  # STORE 3 LAWSON TERRACE, PORTHILL, NEWCASTLE UNDER LYME
+            "10002238202",  # 3 LAWSON TERRACE, NEWCASTLE
+            "200002872601",  # 125A LIVERPOOL ROAD, NEWCASTLE
+            "100031730123",  # 79 KNUTTON LANE, NEWCASTLE UNDER LYME
+            "100031730122",  # 77 KNUTTON LANE, NEWCASTLE UNDER LYME
+            "200001160997",  # SUITE 2 HANOVER COURT HANOVER STREET, NEWCASTLE UNDER LYME
+            "200004616065",  # 2 EARDLEYEND ROAD, NEWCASTLE UNDER LYME
+            "200004617440",  # 93-94 LEYCETT LANE, LEYCETT, NEWCASTLE UNDER LYME
+            "200004602621",  # AUSTIN LEYCETT LANE, SILVERDALE, NEWCASTLE UNDER LYME
+            "10002237282",  # WILLOUGHBRIDGE WELLS LODGE, WILLOUGHBRIDGE, MARKET DRAYTON
+            "200004612280",  # CARAVAN RED STREET STABLES TALKE ROAD, BRADWELL, NEWCASTLE UNDER LYME
+        ]:
             return None
 
-        if uprn == "200004611540":
-            rec = super().address_record_to_dict(record)
-            rec["postcode"] = "TF9 4JG"
-            return rec
-
-        if uprn == "200004601964":
-            rec = super().address_record_to_dict(record)
-            rec["postcode"] = "CW3 9LE"
-            return rec
+        if record.addressline6 in [
+            "ST7 1DY",
+            "ST5 8DS",
+            "ST5 3ES",
+            "ST5 7ES",
+            "ST5 2TP",
+            "ST5 2HN",
+            "ST5 2PU",
+            "ST5 1AU",
+            "ST5 4DU",
+            "TF9 4EY",
+            "ST7 1LH",
+            "ST5 1HN",
+            "ST5 6AL",
+        ]:
+            return None
 
         return super().address_record_to_dict(record)
