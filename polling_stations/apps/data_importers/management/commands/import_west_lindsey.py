d3edafdd499ac9bc560232a83763e76fa572eb67
smsmith97 <sean.m.smith97@outlook.com>
2021-04-26 11:28:17 +0100

    import script for west lindsey (closes #3619)

--- polling_stations/apps/data_importers/management/commands/import_west_lindsey.py
+++ polling_stations/apps/data_importers/management/commands/import_west_lindsey.py
@@ -2,9 +2,37 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000142"
-    addresses_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019WL.tsv"
-    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019WL.tsv"
-    elections = ["parl.2019-12-12"]
-    csv_delimiter = "\t"
-    allow_station_point_from_postcode = False
+    council_id = "WLI"
+    addresses_name = (
+        "2021-04-16T11:36:59.020893/West Lindsay Democracy_Club__06May2021 (1).CSV"
+    )
+    stations_name = (
+        "2021-04-16T11:36:59.020893/West Lindsay Democracy_Club__06May2021 (1).CSV"
+    )
+    elections = ["2021-05-06"]
+    csv_delimiter = ","
+    csv_encoding = "windows-1252"
+
+    def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "10090698579",  # HORSESHOE BARN, LISSINGLEY LANE, LISSINGTON, LINCOLN
+            "10090696854",  # HORIZON BARN, BULLY HILL TOP, TEALBY, MARKET RASEN
+            "10013812180",  # 1 NETTLETON PARK MOORTOWN ROAD, NETTLETON, MARKET RASEN
+        ]:
+            return None
+
+        if record.addressline6 in [
+            "LN7 6JD",
+            "LN1 2XG",
+            "LN1 2PL",
+            "LN2 2YX",
+            "LN8 3SF",
+            "LN2 3PD",
+            "LN2 3RS",
+            "DN21 2PL",
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
