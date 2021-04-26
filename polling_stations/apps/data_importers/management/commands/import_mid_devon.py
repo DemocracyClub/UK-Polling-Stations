d4f3080677f835b3885886d9ae63f8c1e68e409d
smsmith97 <sean.m.smith97@outlook.com>
2021-04-26 10:14:49 +0100

    import script for mid devon (closes #3617)

--- polling_stations/apps/data_importers/management/commands/import_mid_devon.py
+++ polling_stations/apps/data_importers/management/commands/import_mid_devon.py
@@ -3,36 +3,70 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000042"
-    addresses_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019MD.tsv"
-    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019MD.tsv"
-    elections = ["parl.2019-12-12"]
+    council_id = "MDE"
+    addresses_name = (
+        "2021-04-16T11:24:22.888592/Mid Devon Democracy_Club__06May2021.tsv"
+    )
+    stations_name = "2021-04-16T11:24:22.888592/Mid Devon Democracy_Club__06May2021.tsv"
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
     csv_encoding = "windows-1252"
-    allow_station_point_from_postcode = False
 
     def station_record_to_dict(self, record):
+
+        if record.polling_place_id in [
+            "8700",  # Cullompton Town Hall, High Street, Cullompton EX15 1AB
+            "8634",  # Chawleigh Jubilee Hall, Chawleigh EX18 7HH
+            "8628",  # Thorverton Memorial Hall, Silver Street, Thorverton EX5 5LT
+            "8615",  # Shobrooke Village Hall, Shobrooke EX17 1AT
+            "8583",  # Crediton Congregational Church, High Street, Crediton EX17 3LB
+        ]:
+            record = record._replace(polling_place_easting="")
+            record = record._replace(polling_place_northing="")
+
+        # Crediton United Football Club Lords Meadow Sports Centre Commercial Road Crediton EX17 IER
+        if record.polling_place_id == "8850":
+            record = record._replace(polling_place_postcode="EX17 1ER")
+
         rec = super().station_record_to_dict(record)
 
-        # Chawleigh Jubilee Hall
-        if record.polling_place_id == "7809":
-            rec["location"] = None
-
-        # Oakford Village Hall
-        if record.polling_place_id == "7949":
+        # Oakford Village Hall, Oakford EX16 9EW
+        if record.polling_place_id == "8742":
             rec["location"] = Point(-3.550351, 50.982061, srid=4326)
 
         return rec
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
+        uprn = record.property_urn.strip().lstrip("0")
 
-        if record.addressline6 in [
-            "EX16 8RA",
-            "EX16 7RW",
-            "EX17 3QQ",
-            "EX5 4LA",
+        if uprn in [
+            "10024639781",  # FOXHOLES FARM, CLAYHIDON, CULLOMPTON
+            "10093536858",  # MOBILE HOME THE OLD OAKS CHAPEL HILL, UFFCULME
+            "10035356543",  # THE OLD OAKS, CHAPEL HILL, UFFCULME, CULLOMPTON
+            "10002162361",  # POTTERS, SHILLINGFORD, TIVERTON
+            "10093536511",  # RAMSTORLAND STAG VIEW, STOODLEIGH, TIVERTON
+            "200004004367",  # CARAVANS 3 AND 4 THREE ACRES UFFCULME ROAD, WILLAND
+            "200004004366",  # CARAVANS 1 AND 2 THREE ACRES UFFCULME ROAD, WILLAND
+            "10009446166",  # DYERS, BICKLEIGH, TIVERTON
+            "200004001948",  # BACKSWOOD FARM, BICKLEIGH, TIVERTON
+            "200004001949",  # BACKSWOOD FARM COTTAGE LANE TO BACKSWOOD FARM, BICKLEIGH
+            "100040361739",  # FLAT 1 10 BRIDGE STREET, TIVERTON
+            "10093534226",  # FLAT 2 10 BRIDGE STREET, TIVERTON
+            "10002162357",  # RADDS COTTAGE, CALVERLEIGH, TIVERTON
+            "200004002192",  # PRIMROSE LODGE, LOXBEARE, TIVERTON
+            "10024639061",  # THE BIRDHOUSE, TEMPLETON, TIVERTON
+            "10009448630",  # MAYFIELD HOUSE, TEMPLETON, TIVERTON
+            "10002165647",  # POUND CASTLE, BICKLEIGH, TIVERTON
+            "200004005422",  # GUNSTONE LODGE GUNSTONE PARK ROAD FROM GUNSTONE CROSS TO GUNSTONE MILL CROSS, GUNSTONE
+            "200004006381",  # GUNSTONE PARK, GUNSTONE, CREDITON
+            "10093533623",  # RAVENS WOOD, YEOFORD, CREDITON
+            "200004002829",  # POND VIEW, UFFCULME, CULLOMPTON
         ]:
             return None
 
-        return rec
+        if record.addressline6 in [
+            "EX16 7RW",
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
