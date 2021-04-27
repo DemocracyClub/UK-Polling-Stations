34772cc2a9f8269617b6072b62ddfb93b46ba899
smsmith97 <sean.m.smith97@outlook.com>
2021-04-27 09:49:50 +0100

    import script for gateshead (closes #3720)

--- polling_stations/apps/data_importers/management/commands/import_gateshead.py
+++ polling_stations/apps/data_importers/management/commands/import_gateshead.py
@@ -2,30 +2,26 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E08000037"
-    addresses_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019gates.tsv"
-    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019gates.tsv"
-    elections = ["parl.2019-12-12"]
+    council_id = "GAT"
+    addresses_name = "2021-04-26T17:25:33.216720/Democracy_Club__06May2021.tsv"
+    stations_name = "2021-04-26T17:25:33.216720/Democracy_Club__06May2021.tsv"
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
-    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "100000084114",  # NE404NG -> NE404NB : Allendale House, Garden Terrace, Crawcrook, Ryton
-            "100000083763",  # NE403HF -> NE403RS : Orchard House, Lane Head, Ryton
-            "10024187613",  # NE95XP -> NE95XB : Spion Kop North, Primrose Hill, Sheriff Hill, Gateshead
-            "10024187614",  # NE95XP -> NE95XB : Landscape Cottage, Primrose Hill, Sheriff Hill, Gateshead
-            "100000057892",  # NE108UJ -> NE404FA : 3 Stanton Close, Wardley, Gateshead
-            "100000057890",  # NE108UJ -> NE404FA : 1 Stanton Close, Wardley, Gateshead
-            "100000057894",  # NE108UJ -> NE404FA : 5 Stanton Close, Wardley, Gateshead
-            "100000057900",  # NE108UJ -> NE404FA : 11 Stanton Close, Wardley, Gateshead
-            "100000057896",  # NE108UJ -> NE404FA : 7 Stanton Close, Wardley, Gateshead
-            "100000057891",  # NE108UJ -> NE404FA : 2 Stanton Close, Wardley, Gateshead
-            "100000086887",  # NE403PG -> NE403PL : Tower Lodge, Whitewell Lane, Ryton
+            "10022984423",  # HIGH EIGHTON FARM HOUSE BLACK LANE, HARLOW GREEN, GATESHEAD
+            "10024188004",  # 168 SHERIFFS HIGHWAY, GATESHEAD
+            "100000029999",  # 125 ELLISON ROAD, GATESHEAD
+            "10093488596",  # BULLERWELL HOUSE ASHTREE LANE, HIGH SPEN, ROWLANDS GILL
+            "10070837301",  # 1A SPLIT CROW ROAD, GATESHEAD
+            "10070836935",  # RESIDENTIAL ACCOMMODATION COACH AND HORSES INN DURHAM ROAD, BIRTLEY
         ]:
-            rec["accept_suggestion"] = False
+            return None
 
-        return rec
+        if record.addressline6 in ["NE9 5XP", "NE9 6JR", "NE10 9RZ"]:
+            return None
+
+        return super().address_record_to_dict(record)
