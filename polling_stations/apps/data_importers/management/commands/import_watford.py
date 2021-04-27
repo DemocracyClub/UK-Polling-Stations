afbbe8c1d5268b8749b6945761db36d35c309089
smsmith97 <sean.m.smith97@outlook.com>
2021-04-27 11:38:01 +0100

    import script for watford (closes #3637)

--- polling_stations/apps/data_importers/management/commands/import_watford.py
+++ polling_stations/apps/data_importers/management/commands/import_watford.py
@@ -2,38 +2,25 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 
 class Command(BaseHalaroseCsvImporter):
-    council_id = "E07000103"
+    council_id = "WAT"
     addresses_name = (
-        "parl.2019-12-12/Version 1/polling_station_export-2019-11-08watford.csv"
+        "2021-04-19T09:17:40.372170/watford polling_station_export-2021-04-18.csv"
     )
     stations_name = (
-        "parl.2019-12-12/Version 1/polling_station_export-2019-11-08watford.csv"
+        "2021-04-19T09:17:40.372170/watford polling_station_export-2021-04-18.csv"
     )
-    elections = ["parl.2019-12-12"]
-    allow_station_point_from_postcode = False
+    elections = ["2021-05-06"]
+    csv_delimiter = ","
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
 
-        if uprn in ["100080947255", "100080947259"]:
-            rec = super().address_record_to_dict(record)
-            rec["postcode"] = "WD18 7JT"
-            return rec
-
-        return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # These are single records listed as 'Other Electors Address'
-        # no addresses are assigned. So probably don't cause problems, but
-        # dropping them to squash warnings.
-        if (record.pollingstationnumber, record.pollingstationname) in [
-            ("61", "Manor House Sports & Social Centre"),
-            ("62", "Scout Hut"),
-            ("63", "Tanners Wood Hall"),
-            ("65", "YMCA Woodlands Centre"),
-            ("67", "Coates Way JMI and Nursery School"),
-            ("69", "Oxhey Hall Community Association Hall"),
-            ("68", "Leavesden Green Infants Hall"),
+        if uprn in [
+            "10023338176",  # 49 WILMINGTON CLOSE, WATFORD
         ]:
             return None
-        return super().station_record_to_dict(record)
+
+        if record.housepostcode in ["WD24 5DB", "WD17 4JT", "WD18 7BS"]:
+            return None
+
+        return super().address_record_to_dict(record)
