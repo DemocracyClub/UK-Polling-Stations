4547fa36a2d7d5c4286b3bf25d62a14171ea4f11
smsmith97 <sean.m.smith97@outlook.com>
2021-04-27 16:05:55 +0100

    import script for west lancashire (closes #3664)

--- polling_stations/apps/data_importers/management/commands/import_west_lancashire.py
+++ polling_stations/apps/data_importers/management/commands/import_west_lancashire.py
@@ -2,85 +2,58 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000127"
-    addresses_name = (
-        "parl.2019-12-12/Version 1/West lancashire Democracy_Club__12December2019.TSV"
-    )
-    stations_name = (
-        "parl.2019-12-12/Version 1/West lancashire Democracy_Club__12December2019.TSV"
-    )
-    elections = ["parl.2019-12-12"]
+    council_id = "WLA"
+    addresses_name = "2021-04-20T12:09:13.316028/WLBC-Democracy_Club__06May2021.tsv"
+    stations_name = "2021-04-20T12:09:13.316028/WLBC-Democracy_Club__06May2021.tsv"
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
-    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
-        if uprn == "10012355528":
-            return None
-
-        if record.addressline6.strip() == "L33 3AL":
-            return None
-
-        if record.addressline6.strip() in (
-            "PR8 5JE",
-            "PR4 6SA",
-            "L39 8TR",
-            "L40 6JP",
-            "PR4 6XA",
-        ):
-            # One or more properties has a wrong-looking polling station, so play it safe
-            return None
-
-        if record.addressline6.strip() == "PR9 8DF":
-            # Riverside Caravan Park, which has lots of suggested postcode fixes. Traditionally we have said no to
-            # these
-            rec["accept_suggestion"] = False
-
         if uprn in [
-            "10012347931",  # L408HQ -> L408HR : Quarry Barn, Pinfold Lane, Scarisbrick, Ormskirk
-            "100012818795",  # WN69PS -> WN69QB : 9 Highmoor, Whittle Lane, Wrightington, Wigan
-            "100012414932",  # L399EE -> L399EG : Boundary Farm, Graveyard Lane, Bickerstaffe, Ormskirk
-            "10012341232",  # L401UH -> L401UA : Ivy Cottage, Holmeswood Road, Holmeswood, Ormskirk
-            "100012417385",  # WN86SH -> WN87SH : Tildesley Farmhouse, Elmers Green Lane, Skelmersdale
-            "100012657720",  # L408JB -> L408JR : Langley Brook Farm, Langley Road, Burscough, Ormskirk
-            "100010675945",  # WN88EN -> WN88PU : 83 School Lane, Skelmersdale
-            "200001130134",  # WN87XA -> WN87XF : Crossing Cottage, Ferrett Lane, Lathom, Ormskirk
-            "200002847950",  # L398RL -> L398BB : Meadowbank Barn, Halsall Lane, Halsall, Ormskirk
+            "10012357451",  # HILLCREST BARN, LEES LANE, DALTON, WIGAN
+            "10012340452",  # 257 ELMERS GREEN LANE, DALTON, WIGAN
+            "10012344231",  # 143D LIVERPOOL ROAD, BICKERSTAFFE, ORMSKIRK
+            "10012343974",  # 1 THE PUMP HOUSE, SCARTH HILL LANE, LATHOM, ORMSKIRK
+            "10012343975",  # 2 THE PUMP HOUSE, SCARTH HILL LANE, LATHOM, ORMSKIRK
+            "10012343976",  # 3 THE PUMP HOUSE, SCARTH HILL LANE, LATHOM, ORMSKIRK
+            "10012343977",  # 4 THE PUMP HOUSE, SCARTH HILL LANE, LATHOM, ORMSKIRK
+            "10012356512",  # ANNEX HEYES COTTAGE MEADOW LANE, LATHOM
+            "10012357871",  # WILLOW BARN, GREEN LANE, TARLETON, PRESTON
+            "10012363739",  # 37 BOLD LANE, AUGHTON, ORMSKIRK
+            "10012353358",  # 24 SOUTHPORT ROAD, SCARISBRICK, SOUTHPORT
+            "10012348052",  # GREEN LANE FARM, GREEN LANE, TARLETON, PRESTON
+            "10012363317",  # FLAT 2 MEANYGATE FARM COMMON LANE, SCARISBRICK
+            "10012340398",  # 177A CHAPEL ROAD, HESKETH BANK, PRESTON
+            "200002461545",  # DOUGLAS LODGE, CHAPEL ROAD, HESKETH BANK, PRESTON
+            "200002854842",  # ASTLAND BARN, CHAPEL ROAD, HESKETH BANK, PRESTON
+            "200002471251",  # RIBBLE COTTAGE, CHAPEL ROAD, HESKETH BANK, PRESTON
+            "10012348092",  # 5 TATLOCKS GRANGE, ORMSKIRK
         ]:
-            rec["accept_suggestion"] = True
-        #
-        if uprn in [
-            "100010684796",  # WN80AA -> WN80AB : 168 Ormskirk Road, Up Holland, Skelmersdale
-            "100010684616",  # WN80AG -> WN80AH : 11 Ormskirk Road, Up Holland, Skelmersdale
-            "100010684618",  # WN80AG -> WN80AH : 13 Ormskirk Road, Up Holland, Skelmersdale
-            "100010684620",  # WN80AG -> WN80AH : 15 Ormskirk Road, Up Holland, Skelmersdale
-            "100010684622",  # WN80AG -> WN80AH : 17 Ormskirk Road, Up Holland, Skelmersdale
-            "100010684609",  # WN80AG -> WN80AH : 3 Ormskirk Road, Up Holland, Skelmersdale
-            "100010684611",  # WN80AG -> WN80AH : 5 Ormskirk Road, Up Holland, Skelmersdale
-            "100010684612",  # WN80AG -> WN80AH : 7 Ormskirk Road, Up Holland, Skelmersdale
-            "100012654494",  # L397HS -> L397HQ : Rose Croft, Broad Lane, Downholland, Ormskirk
-            "100012821133",  # WN88AU -> WN80PB : 21 Liverpool Road, Skelmersdale
-            "100012657542",  # L407SN -> L407SL : The Homestead Merridale Farm, High Lane, Ormskirk
-            "100012415199",  # L407SN -> L407SL : The Mount, High Lane, Ormskirk
-            "10012356919",  # WN80HS -> WN89QS : 93 Dearden Way, Up Holland, Skelmersdale
-            "10012344656",  # L406JA -> L406JT : Ottershead Barn, Dicks Lane, Lathom, Ormskirk
-            "100012414799",  # L406JA -> L406JT : Whitestone Cottage, Dicks Lane, Lathom, Ormskirk
-            "100012418034",  # WN69EN -> WN69EQ : Glen View, Hall Lane, Wrightington, Wigan
-            "100010677297",  # WN80EW -> WN80EN : Hallbridge Farmhouse, 59 Dingle Road, Up Holland, Skelmersdale
-            "100010682656",  # WN80QY -> WN80QZ : Pearsons Farm, Lafford Lane, Up Holland, Skelmersdale
-            "100012657092",  # L405SW -> L405UZ : Lyme House, Junction Lane, Burscough, Ormskirk
-            "10012357601",  # L393BH -> L393BW : 14 Park Road, Ormskirk
-            # Wrong postcode in AddressBase
-            "100012416134",  # L390HF -> L390HP : The Flat Quattros, Rainford Road, Bickerstaffe, Ormskirk
-            "10012363368",  # WN89HU -> WN89BH : 1 Beechtrees Mews, Beechtrees, Skelmersdale
-            "10012363370",  # WN89HU -> WN89BH : 3 Beechtrees Mews, Beechtrees, Skelmersdale
-            "10012363372",  # WN89HU -> WN89BH : 5 Beechtrees Mews, Beechtrees, Skelmersdale
-            "10012363374",  # WN89HU -> WN89BH : 7 Beechtrees Mews, Beechtrees, Skelmersdale
-            "10012363376",  # WN89HU -> WN89BH : 9 Beechtrees Mews, Beechtrees, Skelmersdale
-            "10012363378",  # WN89HU -> WN89BH : 11 Beechtrees Mews, Beechtrees, Skelmersdale
-        ]:
-            rec["accept_suggestion"] = False
+            return None
 
-        return rec
+        if record.addressline6 in [
+            "L40 5BE",
+            "L39 2EJ",
+            "L40 8JB",
+            "L40 9QL",
+            "L40 1UA",
+            "WN8 8LL",
+            "WN8 9HU",
+            "WN8 6QG",
+            "L40 8HQ",
+            "L39 5BP",
+            "WN8 0AG",
+            "WN8 6SH",
+            "WN8 7XA",
+            "L39 4QJ",
+            "L39 0EG",
+            "L40 6JA",
+            "PR4 6SE",
+            "L39 8SR",
+            "L39 1RB",
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
