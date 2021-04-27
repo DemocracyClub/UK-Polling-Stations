c91bab1dd1f4e74df83f8ba485b430795836cef3
smsmith97 <sean.m.smith97@outlook.com>
2021-04-27 16:11:42 +0100

    import script for dudley (closes #3656)

--- polling_stations/apps/data_importers/management/commands/import_dudley.py
+++ polling_stations/apps/data_importers/management/commands/import_dudley.py
@@ -2,42 +2,48 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E08000027"
-    addresses_name = "parl.2019-12-12/Version 1/merged.tsv"
-    stations_name = "parl.2019-12-12/Version 1/merged.tsv"
-    elections = ["parl.2019-12-12"]
-    csv_delimiter = "\t"
+    council_id = "DUD"
+    addresses_name = (
+        "2021-04-20T10:07:18.089598/Dudley Copy of Democracy_Club__06May2021.csv"
+    )
+    stations_name = (
+        "2021-04-20T10:07:18.089598/Dudley Copy of Democracy_Club__06May2021.csv"
+    )
+    elections = ["2021-05-06"]
+    csv_delimiter = ","
     csv_encoding = "windows-1252"
-    allow_station_point_from_postcode = False
-
-    def station_record_to_dict(self, record):
-
-        # St Andrews District Church
-        if record.polling_place_id == "22550":
-            record = record._replace(polling_place_postcode="DY3 3AB")
-
-        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "90146449",  # DY69LJ -> DY69NW : Swan Hotel, Stream Road, Kingswinford, West Midlands
-            "90163017",  # DY13EP -> DY11EP : 2 The Old Court House, 3 Priory Street, Dudley, West Midlands
+            "90157092",  # 74 HAYES LANE, STOURBRIDGE
+            "90159047",  # THE JAYS, HYPERION ROAD, STOURTON, STOURBRIDGE
+            "90153608",  # NEW BROMLEY FARM, BROMLEY LANE, KINGSWINFORD
+            "90127392",  # FLAT A 106 STOURBRIDGE ROAD, DUDLEY
+            "90135708",  # 106B STOURBRIDGE ROAD, DUDLEY
+            "90142681",  # 106C STOURBRIDGE ROAD, DUDLEY
+            "90163097",  # 50 WOLVERHAMPTON STREET, DUDLEY
+            "90163098",  # 51 WOLVERHAMPTON STREET, DUDLEY
+            "90163100",  # 52 WOLVERHAMPTON STREET, DUDLEY
+            "90150770",  # HICKMERELANDS FARM, HICKMERELANDS, DUDLEY
+            "90161895",  # FIRST FLOOR FLAT 166-167 WOLVERHAMPTON STREET, DUDLEY
         ]:
-            rec["accept_suggestion"] = True
-
-        if uprn in [
-            "90153005",  # WV149AR -> WV149LE : Foxgloves, Elmdale Road, Coseley, West Midlands
-            "90213042",  # DY81DX -> DY83DF : Flat Above, 78 High Street, Stourbridge, West Midlands
-            "90214339",  # B629EN -> B634BN : 23 Hay Barn Close, Halesowen, West Midlands
-            "90214017",  # B629EN -> DY84GF : 12 Hay Barn Close, Halesowen, West Midlands
-            "90156425",  # B629EJ -> B629LB : 161 Long Lane, Halesowen, West Midlands
-        ]:
-            rec["accept_suggestion"] = False
-
-        if uprn == "90161895":
             return None
 
-        return rec
+        if record.addressline6 in ["DY1 3EP", "DY5 2PA"]:
+            return None
+
+        return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+
+        # Caravan on Land Adjoining Black Horse Illey Lane Illey Halesowen B62 0JH
+        if record.polling_place_id == "24813":
+            record = record._replace(polling_place_postcode="B62 0HJ")
+
+        # Rufford Primary School Bredon Avenue Stourbridge DY9 7RN
+        if record.polling_place_id == "24895":
+            record = record._replace(polling_place_postcode="DY9 7NR")
+
+        return super().station_record_to_dict(record)
