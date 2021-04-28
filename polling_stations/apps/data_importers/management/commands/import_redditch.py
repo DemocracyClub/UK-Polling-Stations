0e2a33857f8662bfbdc8e00bcd6328815d2429c4
Alex Dutton <alex@cottagelabs.com>
2021-04-28 10:04:56 +0100

    Import Redditch (closes #3700)

--- polling_stations/apps/data_importers/management/commands/import_redditch.py
+++ polling_stations/apps/data_importers/management/commands/import_redditch.py
@@ -2,36 +2,38 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000236"
-    addresses_name = (
-        "parl.2019-12-12/Version 1/Democracy_Club__12December2019-Redditch.CSV"
-    )
-    stations_name = (
-        "parl.2019-12-12/Version 1/Democracy_Club__12December2019-Redditch.CSV"
-    )
-    elections = ["parl.2019-12-12"]
-    allow_station_point_from_postcode = False
+    council_id = "RED"
+    addresses_name = "2021-04-23T10:48:03.563923/Democracy_Club__06May2021 Redditch.CSV"
+    stations_name = "2021-04-23T10:48:03.563923/Democracy_Club__06May2021 Redditch.CSV"
+    elections = ["2021-05-06"]
+
+    def station_record_to_dict(self, record):
+        if record.polling_place_id == "7070":
+            # Matchborough Meeting Rooms
+            # Misplaced; defer to AddressBase
+            record = record._replace(
+                polling_place_uprn="200002867453",
+                polling_place_easting="",
+                polling_place_northing="",
+            )
+        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-        if record.addressline6 == "B96 6RN":
-            return None  # centroid outside authority
-
-        if (record.addressline1, record.addressline2) == (
-            "4A High Street",
-            "Feckenham",
-        ):
-            record = record._replace(property_urn="100121268977")
-
-        if record.addressline6 == "B98 9BH":
-            record = record._replace(addressline6="B98 9BJ")
-        if record.addressline6 == "B67 6QA":
-            record = record._replace(addressline6="B97 6QA")
-
-        rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
-        if uprn == "100120640565":
-            rec["postcode"] = "B98 9PJ"
-            rec["accept_suggestion"] = False
+        if uprn in [
+            "100120623245",
+            "200003090064",
+            "200003090054",
+            "200003090054",
+        ]:
+            return None  # embedded in another district
 
-        return rec
+        if record.addressline6 == "B67 6QA":
+            # carried over postcode fix
+            record = record._replace(addressline6="B97 6QA")
+
+        if record.addressline6 in ["B98 8PX", "B97 6AQ"]:
+            return None  # split
+
+        return super().address_record_to_dict(record)
