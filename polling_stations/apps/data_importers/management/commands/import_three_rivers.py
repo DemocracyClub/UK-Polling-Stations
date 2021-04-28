511dbeb73ea22ec062396cefa58ab84791b44aca
smsmith97 <sean.m.smith97@outlook.com>
2021-04-28 10:43:02 +0100

    import script for three rivers (closes #3657)

--- polling_stations/apps/data_importers/management/commands/import_three_rivers.py
+++ polling_stations/apps/data_importers/management/commands/import_three_rivers.py
@@ -2,25 +2,20 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000102"
-    addresses_name = "parl.2019-12-12/Version 1/merged.tsv"
-    stations_name = "parl.2019-12-12/Version 1/merged.tsv"
-    elections = ["parl.2019-12-12"]
+    council_id = "THE"
+    addresses_name = (
+        "2021-04-20T10:19:27.612662/Three rivers Democracy_Club__06May2021.tsv"
+    )
+    stations_name = (
+        "2021-04-20T10:19:27.612662/Three rivers Democracy_Club__06May2021.tsv"
+    )
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
     csv_encoding = "latin-1"
-    allow_station_point_from_postcode = False
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
-        uprn = record.property_urn.strip().lstrip("0")
 
-        if uprn == "10090789736":
-            rec["postcode"] = "WD5 0GF"
+        if record.addressline6 in ["WD3 5HD", "WD25 0JX", "WD19 7BU"]:
+            return None
 
-        if uprn in [
-            "100081289658",  # WD35LW -> WD35LL : Viewpoint, Old Common Road, Chorleywood, Rickmansworth, Hertfordshire
-            "200000943009",  # WD35NL -> WD33AN : 3A New Parade, Chorleywood, Rickmansworth, Hertfordshire
-        ]:
-            rec["accept_suggestion"] = False
-
-        return rec
+        return super().address_record_to_dict(record)
