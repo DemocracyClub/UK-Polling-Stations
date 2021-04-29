eed71cf2a3441b0bb9824b5badece71eaf8edec5
Alex Dutton <alex@cottagelabs.com>
2021-04-29 14:48:43 +0100

    Mark more strings for translation

--- polling_stations/apps/bug_reports/models.py
+++ polling_stations/apps/bug_reports/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django_extensions.db.models import TimeStampedModel
+from django.utils.translation import gettext_lazy as _
 
 
 STATUS_CHOICES = (("OPEN", "Open"), ("RESOLVED", "Resolved"))
@@ -10,13 +11,15 @@ REPORT_TYPES = (("OTHER", "Other"),)
 class BugReport(TimeStampedModel):
     description = models.TextField(
         blank=False,
-        verbose_name="Provide information about the problem to help us improve our service:",
+        verbose_name=_(
+            "Provide information about the problem to help us improve our service:"
+        ),
     )
     source_url = models.CharField(blank=True, max_length=800)
     status = models.CharField(blank=False, max_length=100, choices=STATUS_CHOICES)
     user_agent = models.TextField(blank=True)
     source = models.CharField(blank=True, max_length=100)
     email = models.EmailField(
-        blank=True, max_length=100, verbose_name="(Optional) Email address:"
+        blank=True, max_length=100, verbose_name=_("(Optional) Email address:")
     )
     report_type = models.CharField(blank=False, max_length=100, choices=REPORT_TYPES)
