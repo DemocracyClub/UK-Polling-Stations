9d9f71abc52f0cdf46649e4ed472eb0f5314b002
smsmith97 <sean.m.smith97@outlook.com>
2021-04-29 10:37:31 +0100

    Station change in Fife

--- polling_stations/apps/data_importers/management/commands/import_fife.py
+++ polling_stations/apps/data_importers/management/commands/import_fife.py
@@ -6,3 +6,17 @@ class Command(BaseDemocracyCountsCsvImporter):
     addresses_name = "2021-04-12T09:21:22.730934/Fife E8 DC Polling Districts.csv"
     stations_name = "2021-04-12T09:21:22.730934/Fife E8 DC Polling Stations.csv"
     elections = ["2021-05-06"]
+
+    def station_record_to_dict(self, record):
+
+        # LOCHGELLY TOWN HALL BANK STREET LOCHGELLY KY5 9QQ
+        if record.stationcode in [
+            "130",
+            "131",
+            "132",
+        ]:
+            record = record._replace(
+                placename="LOCHGELLY CENTRE", add1="BANK STREET", postcode=" KY5 9RD"
+            )
+
+        return super().station_record_to_dict(record)
