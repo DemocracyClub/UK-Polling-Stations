d7012ac4353ede29f37fb6e0d5ee7a71c2cb93e5
smsmith97 <sean.m.smith97@outlook.com>
2021-04-29 12:05:21 +0100

    import script for king`s lynn & west norfolk (closes #3750)

--- polling_stations/apps/data_importers/management/commands/import_kings_lynn.py
+++ polling_stations/apps/data_importers/management/commands/import_kings_lynn.py
@@ -1,45 +1,39 @@
-from django.contrib.gis.geos import Point
 from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000146"
-    stations_name = "parl.2019-12-12/Version 1/west-norfolk.gov.uk-1572885849000-.tsv"
-    addresses_name = "parl.2019-12-12/Version 1/west-norfolk.gov.uk-1572885849000-.tsv"
-    elections = ["parl.2019-12-12"]
-    csv_delimiter = "\t"
-    allow_station_point_from_postcode = False
-
-    def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn == "10024107639":
-            rec["postcode"] = ""
-            rec["accept_suggestion"] = False
-
-        if record.addressline1 == "8 Lions Close":
-            rec["postcode"] = "PE38 0AT"
-
-        return rec
+    council_id = "KIN"
+    addresses_name = "2021-04-29T10:57:28.594826/Democracy-Club-Data-West-Norfolk.csv"
+    stations_name = "2021-04-29T10:57:28.594826/Democracy-Club-Data-West-Norfolk.csv"
+    elections = ["2021-05-06"]
+    csv_delimiter = ","
 
     def station_record_to_dict(self, record):
 
-        # Dersingham Village Centre
-        if record.polling_place_id == "17923":
-            rec = super().station_record_to_dict(record)
-            rec["location"] = Point(0.512389, 52.843528, srid=4326)
-            return rec
-
-        # Windsor Park, KING`S LYNN
-        if record.polling_place_id == "17867":
-            rec = super().station_record_to_dict(record)
-            rec["location"] = Point(0.404833, 52.748556, srid=4326)
-            return rec
-
-        if record.polling_place_id == "18049":
+        # Lakes End Village Hall Main Road Lakes End WISBECH PE14 9QL
+        if record.polling_place_id == "20394":
             record = record._replace(polling_place_postcode="PE14 9QH")
-            record = record._replace(polling_place_easting="0")
-            record = record._replace(polling_place_northing="0")
 
         return super().station_record_to_dict(record)
+
+    def address_record_to_dict(self, record):
+
+        if record.addressline6 in [
+            "PE30 4XW",
+            "PE30 4XW",
+            "PE31 6HJ",
+            "PE38 9QZ",
+            "PE14 7QT",
+            "PE14 8JN",
+            "PE34 4RD",
+            "PE34 3BJ",
+            "PE34 4EX",
+            "PE30 3LE",
+            "PE33 9PN",
+            "PE30 3BG",
+            "PE14 7LB",
+            "PE14 7EU",
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
