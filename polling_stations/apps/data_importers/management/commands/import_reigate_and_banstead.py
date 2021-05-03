3c6450e964cc02d66e35bd6f6c7a6c0c302e099b
GeoWill <roper.william@gmail.com>
2021-05-03 12:21:44 +0100

    Import script for Reigate and Banstead (closes #3763)

--- polling_stations/apps/data_importers/management/commands/import_reigate_and_banstead.py
+++ polling_stations/apps/data_importers/management/commands/import_reigate_and_banstead.py
@@ -2,26 +2,26 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000211"
-    addresses_name = (
-        "europarl.2019-05-23/Version 2/Democracy_Club__23May2019Reigate.tsv"
-    )
-    stations_name = "europarl.2019-05-23/Version 2/Democracy_Club__23May2019Reigate.tsv"
-    elections = ["europarl.2019-05-23"]
+    council_id = "REI"
+    addresses_name = "2021-04-30T16:00:10.118165/Democracy_Club__06May2021.tsv"
+    stations_name = "2021-04-30T16:00:10.118165/Democracy_Club__06May2021.tsv"
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
 
-    def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
-        uprn = record.property_urn.strip().lstrip("0")
+    def station_record_to_dict(self, record):
+        if record.polling_place_id == "4151":
+            record = record._replace(polling_place_postcode="")
 
-        if uprn in [
-            "68178079",
-            "68178081",
-            "68178085",
-            "68178086",
-            "68178087",
-            "68178088",
+        return super().station_record_to_dict(record)
+
+    def address_record_to_dict(self, record):
+
+        if record.addressline6 in [
+            "RH1 1PH",
+            "RH1 2JG",
+            "RH2 9JB",
+            "RH6 9BS",
+            "RH6 8QG",
         ]:
             return None
-
-        return rec
+        return super().address_record_to_dict(record)
