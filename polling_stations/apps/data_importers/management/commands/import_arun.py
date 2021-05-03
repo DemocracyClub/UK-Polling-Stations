26c86589a1a441416de292ffa9013733d926812c
smsmith97 <sean.m.smith97@outlook.com>
2021-05-03 10:04:07 +0100

    import script for arun (closes #3314)

--- polling_stations/apps/data_importers/management/commands/import_arun.py
+++ polling_stations/apps/data_importers/management/commands/import_arun.py
@@ -2,11 +2,15 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000224"
-    addresses_name = (
-        "parl.2019-12-12/Version 1/elections@arun.gov.uk-1572944623000-.tsv"
-    )
-    stations_name = "parl.2019-12-12/Version 1/elections@arun.gov.uk-1572944623000-.tsv"
-    elections = ["parl.2019-12-12"]
+    council_id = "ARU"
+    addresses_name = "2021-05-01T06:54:35.189732/Democracy_Club__06May2021(2).tsv"
+    stations_name = "2021-05-01T06:54:35.189732/Democracy_Club__06May2021(2).tsv"
+    elections = ["2021-05-06"]
     csv_delimiter = "\t"
-    allow_station_point_from_postcode = False
+
+    def address_record_to_dict(self, record):
+
+        if record.addressline6 in ["PO21 1JB"]:
+            return None
+
+        return super().address_record_to_dict(record)
