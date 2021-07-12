f2bb5a2b652909aa5816c250a3bd89d80354a7ec
Virginia Dooley <vcdooley@gmail.com>
2021-07-12 13:12:33 +0100

    Update logo src and add favicon

--- polling_stations/settings/base.py
+++ polling_stations/settings/base.py
@@ -261,7 +261,7 @@ CORS_URLS_REGEX = r"^/(api|embed)/.*$"
 
 INTERNAL_IPS = "127.0.0.1"
 SITE_TITLE = _("Where Do I Vote?")
-SITE_LOGO = "images/logo-with-text.png"
+SITE_LOGO = "images/logo_icon.svg"
 SITE_LOGO_WIDTH = "390px"
 
 TEST_RUNNER = "django.test.runner.DiscoverRunner"
