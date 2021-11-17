8003cc927130379af35ab6bd19e310e57924985f
GeoWill <roper.william@gmail.com>
2021-11-17 12:00:00 +0000

    More imports for Nov

--- polling_stations/apps/data_importers/management/commands/import_harrogate.py
+++ polling_stations/apps/data_importers/management/commands/import_harrogate.py
@@ -3,15 +3,10 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "HAG"
-    addresses_name = (
-        "2021-03-24T11:03:45.408151/Democracy_Club__06May2021 amendments.CSV"
-    )
-    stations_name = (
-        "2021-03-24T11:03:45.408151/Democracy_Club__06May2021 amendments.CSV"
-    )
-    elections = ["2021-05-06"]
-    csv_delimiter = ","
-
+    addresses_name = "2021-11-16T10:10:34.996455/Democracy_Club__25November2021 (1).CSV"
+    stations_name = "2021-11-16T10:10:34.996455/Democracy_Club__25November2021 (1).CSV"
+    elections = ["2021-11-25"]
+    #
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
@@ -25,26 +20,11 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         if record.addressline6 in [
             "HG3 5QF",
             "HG3 3AT",
-            "HG3 3JQ",
             "HG2 9LJ",
             "HG1 4JW",
             "HG2 9NW",
+            "YO51 9LN",
         ]:
             return None
 
         return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # North Stainley Village Hall North Stainley Ripon
-        if record.polling_place_id == "14714":
-            record = record._replace(polling_place_postcode="HG4 3JT")
-
-        # Samwaies Hall Main Street Wath Ripon HG4 5AT
-        if record.polling_place_id == "14943":
-            record = record._replace(polling_place_postcode="HG4 5ET")
-
-        # Dishforth Village Hall Main Street Dishforth
-        if record.polling_place_id == "14932":
-            record = record._replace(polling_place_postcode="YO7 3JU")
-
-        return super().station_record_to_dict(record)
--- polling_stations/apps/data_importers/management/commands/import_nuneaton_and_bedworth.py
+++ polling_stations/apps/data_importers/management/commands/import_nuneaton_and_bedworth.py
@@ -3,27 +3,7 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "NUN"
-    addresses_name = "2021-04-08T14:20:43.445604/Democracy_Club__06May2021.tsv"
-    stations_name = "2021-04-08T14:20:43.445604/Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
+    addresses_name = "2021-11-17T11:26:12.478111/Democracy_Club__25November2021.tsv"
+    stations_name = "2021-11-17T11:26:12.478111/Democracy_Club__25November2021.tsv"
+    elections = ["2021-11-25"]
     csv_delimiter = "\t"
-
-    def station_record_to_dict(self, record):
-
-        return super().station_record_to_dict(record)
-
-    def address_record_to_dict(self, record):
-        if record.addressline6.strip() in [
-            "CV10 7BE",
-            "CV10 9QF",
-            "CV12 9HJ",
-            "CV7 9NQ",
-            "CV11 6NL",
-            "CV11 6JF",
-            "CV11 6JE",
-            "CV11 4NW",
-            "CV10 9FG",
-        ]:
-            return None  # split
-
-        return super().address_record_to_dict(record)
