f52ffbfdeada0c762ad9449dd169fb9cc244d491
GeoWill <roper.william@gmail.com>
2022-01-20 11:30:32 +0000

    Import script for parl.southend-west.by.2022-02-03

--- polling_stations/apps/data_importers/management/commands/import_southend.py
+++ polling_stations/apps/data_importers/management/commands/import_southend.py
@@ -3,32 +3,22 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SOS"
-    addresses_name = "2021-03-29T13:06:29.813153/Southend.csv"
-    stations_name = "2021-03-29T13:06:29.813153/Southend.csv"
-    elections = ["2021-05-06"]
+    addresses_name = "2022-01-13T16:25:37.997169/Democracy_Club__03February2022.tsv"
+    stations_name = "2022-01-13T16:25:37.997169/Democracy_Club__03February2022.tsv"
+    elections = ["2022-02-03"]
+    csv_delimiter = "\t"
+    csv_encoding = "windows-1252"
 
     def address_record_to_dict(self, record):
         if record.addressline6 in [
-            "SS0 7JG",
-            "SS1 2PE",
-            "SS9 4RP",
-            "SS0 0BD",
-            "SS0 8LL",
-            "SS0 9HW",
-            "SS1 2ER",
-            "SS1 2TD",
+            "SS0 0NP",
             "SS2 6UY",
-            "SS3 9QH",
+            "SS9 1LN",
             "SS9 1NH",
             "SS9 1QY",
             "SS9 1RP",
-            "SS9 1SP",
-            "SS9 2HE",
-            "SS9 2PP",
-            "SS9 3BG",
-            "SS9 3NQ",
+            "SS9 3BG" "SS9 3BG",
             "SS9 5EW",
-            "SS3 9RJ",
         ]:
             return None
 
