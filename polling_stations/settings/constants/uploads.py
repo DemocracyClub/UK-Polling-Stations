445bb3602306b20637064aa7331a01e5542f4c0e
GeoWill <roper.william@gmail.com>
2022-01-31 12:46:23 +0000

    Add method on Upload to make PR

--- polling_stations/settings/constants/uploads.py
+++ polling_stations/settings/constants/uploads.py
@@ -1,2 +1,5 @@
 MAX_FILE_SIZE = 150000000  # 150Mb
 S3_UPLOADS_BUCKET = "pollingstations-uploads-dev"
+GITHUB_USERNAME = "polling-bot-4000"
+GITHUB_EMAIL = "developers@democracyclub.org.uk"
+GITHUB_REPO = "DemocracyClub/UK-Polling-Stations"
