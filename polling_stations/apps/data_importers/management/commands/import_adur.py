951ffeff8e56b34127a8ee533858b58ab97f5dfc
polling-bot-4000 <developers@democracyclub.org.uk>
2022-02-07 09:24:11 +0000

    TEST/Import script for Adur District (2022-05-05)

--- polling_stations/apps/data_importers/management/commands/import_adur.py
+++ polling_stations/apps/data_importers/management/commands/import_adur.py
@@ -3,13 +3,8 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "ADU"
-    addresses_name = "2021-02-15T11:20:20.221841/ADCDemocracy_Club__06May2021.tsv"
-    stations_name = "2021-02-15T11:20:20.221841/ADCDemocracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
+    addresses_name = "2022-05-05/2022-02-07T09:23:53.674215/Democracy_Club__03February2022.tsv"
+    stations_name = "2022-05-05/2022-02-07T09:23:53.674215/Democracy_Club__03February2022.tsv"
+    elections = ['2022-05-05']
+    csv_encoding = "windows-1252"
     csv_delimiter = "\t"
-
-    def address_record_to_dict(self, record):
-        if record.addressline6 == "BN43 6DF":
-            return None
-
-        return super().address_record_to_dict(record)
