b423b86efa59bee3cde17d2d2d884f4f4a10dc84
polling-bot-4000 <developers@democracyclub.org.uk>
2022-02-07 11:39:15 +0000

    TEST/Import script for Southend-on-Sea (2022-02-24)

--- polling_stations/apps/data_importers/management/commands/import_southend.py
+++ polling_stations/apps/data_importers/management/commands/import_southend.py
@@ -3,8 +3,8 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SOS"
-    addresses_name = "2022-02-24/2022-02-06T16:29:25.993641/Democracy_Club__03February2022.tsv"
-    stations_name = "2022-02-24/2022-02-06T16:29:25.993641/Democracy_Club__03February2022.tsv"
+    addresses_name = "2022-02-24/2022-02-07T11:39:07.829749/Democracy_Club__03February2022.tsv"
+    stations_name = "2022-02-24/2022-02-07T11:39:07.829749/Democracy_Club__03February2022.tsv"
     elections = ['2022-02-24']
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
