1d3fb4e52fa92e013cc76c2f633b674952705df7
polling-bot-4000 <developers@democracyclub.org.uk>
2022-02-07 17:10:08 +0000

    TEST/Import script for South Tyneside (2022-05-05)

--- polling_stations/apps/data_importers/management/commands/import_south_tyneside.py
+++ polling_stations/apps/data_importers/management/commands/import_south_tyneside.py
@@ -3,23 +3,8 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "STY"
-    addresses_name = "2021-03-17T14:46:18.378677/Democracy_Club__06May2021.tsv"
-    stations_name = "2021-03-17T14:46:18.378677/Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
+    addresses_name = "2022-05-05/2022-02-07T17:10:01.690596/wdiv-upload-test.tsv"
+    stations_name = "2022-05-05/2022-02-07T17:10:01.690596/wdiv-upload-test.tsv"
+    elections = ['2022-05-05']
+    csv_encoding = "windows-1252"
     csv_delimiter = "\t"
-
-    def station_record_to_dict(self, record):
-
-        # Removing undesired text
-        if "NEW STATION" in record.polling_place_address_1:
-            record = record._replace(polling_place_address_1="")
-        if "NEW STATION" in record.polling_place_address_2:
-            record = record._replace(polling_place_address_2="")
-
-        return super().station_record_to_dict(record)
-
-    def address_record_to_dict(self, record):
-        if record.addressline6 in ["NE31 2XF", "NE34 8AE"]:
-            return None
-
-        return super().address_record_to_dict(record)
