f2a7fe543aa338e81bea692b8267154e64e7478d
GeoWill <roper.william@gmail.com>
2022-02-07 10:37:31 +0000

    Make sure UserCouncil is created in logger db

--- polling_stations/apps/file_uploads/utils.py
+++ polling_stations/apps/file_uploads/utils.py
@@ -14,10 +14,12 @@ def assign_councils_to_user(user):
     Adds rows to the join table between User and Council
     """
     email_domain = user.email.rsplit("@", 1)[1]
-    councils = Council.objects.filter(
+    councils = Council.objects.using("logger").filter(
         Q(electoral_services_email__contains=email_domain)
         | Q(registration_email__contains=email_domain)
     )
 
     for council in councils:
-        UserCouncils.objects.update_or_create(user=user, council=council)
+        UserCouncils.objects.using("logger").update_or_create(
+            user=user, council=council
+        )
