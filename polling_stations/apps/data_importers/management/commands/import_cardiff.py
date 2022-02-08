5305168ad07c0f1f15e8b1737c2a2949bd2e18a1
polling-bot-4000 <developers@democracyclub.org.uk>
2022-02-08 14:30:10 +0000

    Import script for City of Cardiff (2022-02-17)

--- polling_stations/apps/data_importers/management/commands/import_cardiff.py
+++ polling_stations/apps/data_importers/management/commands/import_cardiff.py
@@ -2,30 +2,9 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-
     council_id = "CRF"
-    addresses_name = "2021-03-24T16:24:49.782410/cardiff_deduped.tsv"
-    stations_name = "2021-03-24T16:24:49.782410/cardiff_deduped.tsv"
-    elections = ["2021-05-06"]
-    csv_delimiter = "\t"
+    addresses_name = "2022-02-17/2022-02-08T14:29:57.137736/wdiv-upload-test.tsv"
+    stations_name = "2022-02-17/2022-02-08T14:29:57.137736/wdiv-upload-test.tsv"
+    elections = ['2022-02-17']
     csv_encoding = "windows-1252"
-
-    def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn == "100100110392":
-            return None
-
-        if record.addressline6 in [
-            "CF24 3DZ",
-            "CF3 0UH",
-            "CF14 2FN",
-            "CF14 6PE",
-            "CF24 4RU",
-            "CF3 4LL",
-            "CF5 6HF",
-            "CF14 9UA",
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
+    csv_delimiter = "\t"
