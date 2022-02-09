914dc0fd984a291c84c761ccce6a651f5abb12a4
polling-bot-4000 <developers@democracyclub.org.uk>
2022-02-09 16:30:41 +0000

    Import script for Nuneaton and Bedworth (2022-05-05)

--- polling_stations/apps/data_importers/management/commands/import_nuneaton_and_bedworth.py
+++ polling_stations/apps/data_importers/management/commands/import_nuneaton_and_bedworth.py
@@ -3,7 +3,7 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "NUN"
-    addresses_name = "2021-11-17T11:26:12.478111/Democracy_Club__25November2021.tsv"
-    stations_name = "2021-11-17T11:26:12.478111/Democracy_Club__25November2021.tsv"
-    elections = ["2021-11-25"]
+    addresses_name = "2022-05-05/2022-02-09T16:30:28.635962/Democracy_Club__05May2022.tsv"
+    stations_name = "2022-05-05/2022-02-09T16:30:28.635962/Democracy_Club__05May2022.tsv"
+    elections = ['2022-05-05']
     csv_delimiter = "\t"
