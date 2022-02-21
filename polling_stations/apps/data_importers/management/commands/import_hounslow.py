c6489b1b7e02a6761c3d4d3a88eeb0eb68a2e492
polling-bot-4000 <developers@democracyclub.org.uk>
2022-02-21 16:50:37 +0000

    Import script for London of Hounslow (2022-05-05)

--- polling_stations/apps/data_importers/management/commands/import_hounslow.py
+++ polling_stations/apps/data_importers/management/commands/import_hounslow.py
@@ -3,37 +3,7 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "HNS"
-    addresses_name = (
-        "2021-04-01T10:43:33.063903/Democracy_Club__06May2021 - Hounslow.CSV"
-    )
-    stations_name = (
-        "2021-04-01T10:43:33.063903/Democracy_Club__06May2021 - Hounslow.CSV"
-    )
-    elections = ["2021-05-06"]
-    csv_delimiter = ","
+    addresses_name = "2022-05-05/2022-02-21T16:49:40.581288/Democracy_Club__05May2022.CSV"
+    stations_name = "2022-05-05/2022-02-21T16:49:40.581288/Democracy_Club__05May2022.CSV"
+    elections = ['2022-05-05']
     csv_encoding = "windows-1252"
-
-    def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "100021554382",  # 104 MARTINDALE ROAD, HOUNSLOW
-            "100021539525",  # 208B BATH ROAD, HOUNSLOW
-            "10090801431",  # FLAT 5 95 MASWELL PARK ROAD, HOUNSLOW
-            "10090801430",  # FLAT 4 95 MASWELL PARK ROAD, HOUNSLOW
-            "100021580780",  # FLAT 2 392 CHISWICK HIGH ROAD, CHISWICK, LONDON
-            "100021514552",  # FLAT 1 36 HAMILTON ROAD, BRENTFORD
-        ]:
-            return None
-
-        if record.addressline6 in [
-            "TW4 6DH",
-            "TW3 3DW",
-            "TW3 3TU",
-            "TW13 5JE",
-            "W4 4EU",
-            "TW13 6AB",
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
