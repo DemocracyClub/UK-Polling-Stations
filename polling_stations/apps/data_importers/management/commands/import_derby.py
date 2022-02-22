32141ea6af8f68cf4a0581e0ddc650a6a2b424d3
polling-bot-4000 <developers@democracyclub.org.uk>
2022-02-22 14:57:01 +0000

    Import script for Derby (2022-05-05) (closes #4151)

--- polling_stations/apps/data_importers/management/commands/import_derby.py
+++ polling_stations/apps/data_importers/management/commands/import_derby.py
@@ -3,34 +3,29 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "DER"
-    addresses_name = "2021-03-12T15:52:38.494809/Democracy_Club__06May2021.tsv"
-    stations_name = "2021-03-12T15:52:38.494809/Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
+    addresses_name = (
+        "2022-05-05/2022-02-22T14:55:56.122456/Democracy_Club__05May2022.tsv"
+    )
+    stations_name = (
+        "2022-05-05/2022-02-22T14:55:56.122456/Democracy_Club__05May2022.tsv"
+    )
+    elections = ["2022-05-05"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "200001135441",  # FLAT THE HONEYCOMB LADYBANK ROAD, DERBY
-            "10010685086",  # MORTGAGE FORCE LTD, 65A FRIAR GATE, DERBY
-            "100030364800",  # 134 STATION ROAD, MICKLEOVER, DERBY
-            "100030364798",  # 142 STATION ROAD, MICKLEOVER, DERBY
-            "100030329310",  # FLAT 631 HARVEY ROAD, DERBY
-            "100030361764",  # 68 TADDINGTON ROAD, CHADDESDEN, DERBY
-            "100030361769",  # 66 TADDINGTON ROAD, CHADDESDEN, DERBY
-            "10010614113",  # STONE FARM, SINFIN MOOR, DERBY
-            "10010623517",  # EDALE HOUSE ROYAL SCHOOL FOR THE DEAF 180 ASHBOURNE ROAD, DERBY
+            "100030300432",  # FIRST FLOOR FLAT 28 CHAPEL STREET, SPONDON, DERBY
         ]:
             return None
 
         if record.addressline6 in [
             "DE23 6AR",
-            "DE1 1RX",
-            "DE1 3NZ",
             "DE24 0LU",
             "DE21 4HF",
-            "DE22 3FZ",
+            "DE24 9HW",
+            "DE21 4AW",
         ]:
             return None
 
