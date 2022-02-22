fae9b64657a3d9437442867f8e3384a5dead0f36
polling-bot-4000 <developers@democracyclub.org.uk>
2022-02-22 10:39:59 +0000

    Import script for London of Richmond Upon Thames (2022-05-05)

--- polling_stations/apps/data_importers/management/commands/import_richmond.py
+++ polling_stations/apps/data_importers/management/commands/import_richmond.py
@@ -3,16 +3,20 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "RIC"
-    addresses_name = "2021-04-16T09:27:30.944565/Democracy_Club__06May2021.tsv"
-    stations_name = "2021-04-16T09:27:30.944565/Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
-    csv_delimiter = "\t"
+    addresses_name = (
+        "2022-05-05/2022-02-22T10:39:44.138903/Democracy_Club__05May2022.tsv"
+    )
+    stations_name = (
+        "2022-05-05/2022-02-22T10:39:44.138903/Democracy_Club__05May2022.tsv"
+    )
+    elections = ["2022-05-05"]
     csv_encoding = "windows-1252"
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         if record.addressline6 in [
             "TW12 2SB",
-            "SW13 9RF",
+            "TW2 5NJ",
         ]:
             return None
 
