5a8c476a8c8c734d74baeae940c596f6c86287f4
polling-bot-4000 <developers@democracyclub.org.uk>
2022-02-22 14:39:51 +0000

    Import script for St Helens (2022-05-05) (closes #4147)

--- polling_stations/apps/data_importers/management/commands/import_st_helens.py
+++ polling_stations/apps/data_importers/management/commands/import_st_helens.py
@@ -3,14 +3,18 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SHN"
-    addresses_name = "2021-04-06T14:46:11.580651/Democracy_Club__06May2021.tsv"
-    stations_name = "2021-04-06T14:46:11.580651/Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
+    addresses_name = (
+        "2022-05-05/2022-02-22T14:39:34.210989/Democracy_Club__05May2022 (1).tsv"
+    )
+    stations_name = (
+        "2022-05-05/2022-02-22T14:39:34.210989/Democracy_Club__05May2022 (1).tsv"
+    )
+    elections = ["2022-05-05"]
     csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
         # Sutton Village Church, Herbert Street
-        if record.polling_place_id == "4264":
+        if record.polling_place_id == "4714":
             record = record._replace(polling_place_easting="0")
             record = record._replace(polling_place_northing="0")
 
@@ -19,16 +23,14 @@ class Command(BaseXpressDemocracyClubCsvImporter):
     def address_record_to_dict(self, record):
 
         uprn = record.property_urn.strip().lstrip("0")
-        if uprn in ["39081348"]:
+        if uprn in [
+            "39097215",  # 8 PARK AVENUE NORTH, NEWTON-LE-WILLOWS, WA12 8HL
+        ]:
             return None
         if record.addressline6 in [
-            "WA9 4QJ",
-            "WA11 0LA",
-            "WA11 9TB",
             "WA9 3RR",
             "WA9 3UF",
-            "WA10 1JT",
-            "WA9 3ZL",
+            "WA10 1HT",
         ]:
             return None
         return super().address_record_to_dict(record)
