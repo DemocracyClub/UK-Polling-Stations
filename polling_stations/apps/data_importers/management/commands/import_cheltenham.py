f28986efc29abcc79a8eeae355b0416ff8e0a88b
polling-bot-4000 <developers@democracyclub.org.uk>
2022-02-25 12:49:09 +0000

    Import script for Cheltenham (2022-05-05) (closes #4165)

--- polling_stations/apps/data_importers/management/commands/import_cheltenham.py
+++ polling_stations/apps/data_importers/management/commands/import_cheltenham.py
@@ -4,32 +4,23 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 class Command(BaseHalaroseCsvImporter):
     council_id = "CHT"
     addresses_name = (
-        "2021-03-25T14:01:18.317528/Cheltenham polling_station_export-2021-03-24.csv"
+        "2022-05-05/2022-02-25T12:48:35.558843/polling_station_export-2022-02-25.csv"
     )
     stations_name = (
-        "2021-03-25T14:01:18.317528/Cheltenham polling_station_export-2021-03-24.csv"
+        "2022-05-05/2022-02-25T12:48:35.558843/polling_station_export-2022-02-25.csv"
     )
-    elections = ["2021-05-06"]
-    csv_delimiter = ","
+    elections = ["2022-05-05"]
 
     def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
-
-        if uprn in [
-            "200002686059",  # 3A CLARENCE ROAD, CHELTENHAM
-            "100120409897",  # BUXTON, NAUNTON LANE, CHELTENHAM
-            "200001494338",  # BASEMENT FLAT, NORTHWICK, DOURO ROAD, CHELTENHAM
-            "10091671112",  # FLAT ABOVE, SOUND & VISION EXPRESS LTD, MEAD ROAD, CHELTENHAM
-        ]:
-            return None
 
         if record.housepostcode in [
+            "GL50 2RF",
+            "GL52 6RN",
             "GL52 2ES",
             "GL53 7AJ",
-            "GL52 6RN",
+            "GL50 3RB",
             "GL53 0HL",
             "GL50 2DZ",
-            "GL50 3RB",
         ]:
             return None
 
