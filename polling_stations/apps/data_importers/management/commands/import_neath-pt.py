928e7cbe438fb736248e91b8947278d17a60d72b
polling-bot-4000 <developers@democracyclub.org.uk>
2022-02-28 14:59:27 +0000

    Import script for Neath Port Talbot County (2022-05-05) (closes #4171)

--- polling_stations/apps/data_importers/management/commands/import_neath-pt.py
+++ polling_stations/apps/data_importers/management/commands/import_neath-pt.py
@@ -1,17 +1,17 @@
-from data_importers.management.commands import BaseHalaroseCsvImporter
 from django.contrib.gis.geos import Point
+
+from data_importers.management.commands import BaseHalaroseCsvImporter
 
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "NTL"
     addresses_name = (
-        "2021-03-25T10:36:12.245581/Neath PT polling_station_export-2021-03-23.csv"
+        "2022-05-05/2022-02-28T14:59:06.740989/polling_station_export-2022-02-28.csv"
     )
     stations_name = (
-        "2021-03-25T10:36:12.245581/Neath PT polling_station_export-2021-03-23.csv"
+        "2022-05-05/2022-02-28T14:59:06.740989/polling_station_export-2022-02-28.csv"
     )
-    elections = ["2021-05-06"]
-    csv_delimiter = ","
+    elections = ["2022-05-05"]
 
     def station_record_to_dict(self, record):
 
@@ -22,7 +22,7 @@ class Command(BaseHalaroseCsvImporter):
 
         # Godrergraig Workingmens Club Glanyrafon Road Ystalyfera SA9 2HA
         if (
-            record.pollingstationnumber == "22"
+            record.pollingstationnumber == "72"
             and record.pollingstationpostcode == "SA9 2HA"
         ):
             record = record._replace(pollingstationpostcode="SA9 2DE")
@@ -38,34 +38,15 @@ class Command(BaseHalaroseCsvImporter):
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
-
-        if uprn in [
-            "10009182194",  # FFRWD VALE BUNGALOW, DWR Y FELIN ROAD, NEATH
-            "100100609132",  # 35B PENYWERN ROAD, NEATH
-            "10009177319",  # MAES MELYN BUNGALOW, DRUMMAU ROAD, SKEWEN, NEATH
-            "100101040244",  # 11A REGENT STREET EAST, NEATH
-            "100100598607",  # 134 SHELONE ROAD, NEATH
-            "10023946967",  # 134A SHELONE ROAD, BRITON FERRY
-            "100100600419",  # 113 GNOLL PARK ROAD, NEATH
-            "100100991905",  # BROOKLYN, TONMAWR ROAD, PONTRHYDYFEN, PORT TALBOT
-            "10009184466",  # CILGARN FARM COTTAGE, CWMAVON, PORT TALBOT
-            "10009186526",  # TYN Y CAEAU, MARGAM ROAD, MARGAM, PORT TALBOT
-            "10014164971",  # FLAT TYN-Y-CAEAU MARGAM ROAD, MARGAM
-            "10009184513",  # 1 ALMS HOUSE, MARGAM, PORT TALBOT
-        ]:
-            return None
-
         if record.housepostcode in [
-            "SA12 8EP",
-            "SA10 9DJ",
             "SA10 6DE",
-            "SA11 3PW",
+            "SA10 9DJ",
             "SA11 1TS",
             "SA11 1TW",
-            "SA12 9ST",
+            "SA11 3PW",
+            "SA12 8EP",
             "SA8 4PX",
-            "SA11 3QE",
+            "SA8 4TS",
         ]:
             return None
 
