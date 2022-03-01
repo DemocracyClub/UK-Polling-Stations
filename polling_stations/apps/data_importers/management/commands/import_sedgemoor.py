913ee6f62888766927ec489480a85e585608c399
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-01 14:24:43 +0000

    Import script for Sedgemoor District (2022-05-05) (closes #4177)

--- polling_stations/apps/data_importers/management/commands/import_sedgemoor.py
+++ polling_stations/apps/data_importers/management/commands/import_sedgemoor.py
@@ -3,28 +3,35 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "SEG"
-    addresses_name = "2021-03-04T13:46:19.027597/polling_station_export-2021-03-04.csv"
-    stations_name = "2021-03-04T13:46:19.027597/polling_station_export-2021-03-04.csv"
-    elections = ["2021-05-06"]
+    addresses_name = (
+        "2022-05-05/2022-03-01T14:24:32.538939/polling_station_export-2022-02-28.csv"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-01T14:24:32.538939/polling_station_export-2022-02-28.csv"
+    )
+    elections = ["2022-05-05"]
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip()
         if record.housepostcode in [
-            "TA6 5NL",
-            "TA6 4HB",
-            "TA5 2PF",
-            "TA6 6RZ",
-            "TA6 6QH",
-            "TA6 6GD",
+            "BS26 2DX",
+            "BS27 3PA",
             "TA5 2BE",
-            "BS24 0HT",
-            "TA6 7AW",
-            "TA5 2NH",
-            "TA5 1JW",
+            "TA5 2PF",
+            "TA6 4HB",
+            "TA6 5NL",
+            "TA6 6GD",
+            "TA6 6QH",
+            "TA6 6RZ",
+            "TA6 7BS",
         ]:
             return None
 
-        if uprn == "10009328603":
-            record = record._replace(housepostcode="TA5 1JG")
+        if uprn in [
+            "10090855892",  # PUT HOUSE, FIDDINGTON, BRIDGWATER, TA51JW
+            "100040899479",  # 135A TAUNTON ROAD, BRIDGWATER, TA6 6BD
+        ]:
+
+            return None
 
         return super().address_record_to_dict(record)
