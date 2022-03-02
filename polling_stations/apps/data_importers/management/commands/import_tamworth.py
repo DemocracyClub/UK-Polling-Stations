bd777e1c7135feef33fdaa33d479099fdb49b069
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-02 14:26:36 +0000

    Import script for Tamworth (2022-05-05) (closes #4187)

--- polling_stations/apps/data_importers/management/commands/import_tamworth.py
+++ polling_stations/apps/data_importers/management/commands/import_tamworth.py
@@ -3,7 +3,19 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "TAW"
-    addresses_name = "2021-01-25T10:50:55.656955/Tamworth_Borough_Council_Democracy_Club__06May2021.tsv"
-    stations_name = "2021-01-25T10:50:55.656955/Tamworth_Borough_Council_Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
+    addresses_name = "2022-05-05/2022-03-02T14:26:27.337999/Tamworth_BC_Democracy_Club__05May2022.tsv"
+    stations_name = "2022-05-05/2022-03-02T14:26:27.337999/Tamworth_BC_Democracy_Club__05May2022.tsv"
+    elections = ["2022-05-05"]
     csv_delimiter = "\t"
+
+    def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "394040617",  # 77C AMINGTON ROAD, TAMWORTH, B77 3LN
+        ]:
+            return None
+        if record.addressline6 in ["B79 0BY"]:
+            return None
+
+        return super().address_record_to_dict(record)
