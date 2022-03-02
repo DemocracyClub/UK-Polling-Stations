decc9d0ed0e6b1439d3f2f106497a58508851990
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-02 14:17:32 +0000

    Import script for Rossendale (2022-05-05) (closes #4185)

--- polling_stations/apps/data_importers/management/commands/import_rossendale.py
+++ polling_stations/apps/data_importers/management/commands/import_rossendale.py
@@ -3,31 +3,20 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "ROS"
-    addresses_name = "2021-03-26T12:03:10.152976/Democracy_Club__06May2021.tsv"
-    stations_name = "2021-03-26T12:03:10.152976/Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
+    addresses_name = (
+        "2022-05-05/2022-03-02T14:17:20.976699/Democracy_Club__05May2022.tsv"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-02T14:17:20.976699/Democracy_Club__05May2022.tsv"
+    )
+    elections = ["2022-05-05"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "10014333480",  # HEIGHT END COTTAGE HEIGHT END FARM BACUP OLD ROAD, BACUP
-        ]:
-            return None
 
         if record.addressline6 in [
-            "OL12 8NU",
-            "BB4 4NZ",
-            "OL13 9BT",
-            "BB4 7TD",
             "BB4 8TT",
             "OL13 0QR",
-            "OL13 9QA",
-            "BB4 9UD",
-            "BB4 9LR",
-            "BB4 7ND",
-            "OL13 0PS",
             "BB4 9QR",
         ]:
             return None
