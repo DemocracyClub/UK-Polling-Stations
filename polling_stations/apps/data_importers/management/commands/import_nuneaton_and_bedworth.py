8fe1e9aca4c48f0f443d40dc984445807295bc7e
GeoWill <roper.william@gmail.com>
2022-03-02 21:38:57 +0000

    Import script for Nuneaton and Bedworth (closes #41020

--- polling_stations/apps/data_importers/management/commands/import_nuneaton_and_bedworth.py
+++ polling_stations/apps/data_importers/management/commands/import_nuneaton_and_bedworth.py
@@ -3,7 +3,26 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "NUN"
-    addresses_name = "2021-11-17T11:26:12.478111/Democracy_Club__25November2021.tsv"
-    stations_name = "2021-11-17T11:26:12.478111/Democracy_Club__25November2021.tsv"
-    elections = ["2021-11-25"]
+    addresses_name = (
+        "2022-05-05/2022-02-09T16:30:28.635962/Democracy_Club__05May2022.tsv"
+    )
+    stations_name = (
+        "2022-05-05/2022-02-09T16:30:28.635962/Democracy_Club__05May2022.tsv"
+    )
+    elections = ["2022-05-05"]
     csv_delimiter = "\t"
+
+    def address_record_to_dict(self, record):
+        rec = super().address_record_to_dict(record)
+
+        if record.addressline6 in [
+            "CV10 9QF",
+            "CV7 9NQ",
+            "CV11 6NL",
+            "CV11 6JF",
+            "CV11 6JE",
+            "CV11 4NW",
+        ]:
+            return None
+
+        return rec
