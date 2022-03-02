39416274a364ae853ad4bc9dccef79fff51bc20c
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-02 14:55:12 +0000

    Import script for Stockport (2022-05-05) (closes #4191)

--- polling_stations/apps/data_importers/management/commands/import_stockport.py
+++ polling_stations/apps/data_importers/management/commands/import_stockport.py
@@ -3,54 +3,20 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SKP"
-    addresses_name = "2021-03-22T11:08:59.256823/Democracy_Club__06May2021.tsv"
-    stations_name = "2021-03-22T11:08:59.256823/Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
-    csv_delimiter = "\t"
+    addresses_name = (
+        "2022-05-05/2022-03-02T15:12:55.643036/Democracy_Club__05May2022.tsv"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-02T15:12:55.643036/Democracy_Club__05May2022.tsv"
+    )
+    elections = ["2022-05-05"]
     csv_encoding = "windows-1252"
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "10002096787",  # FLAT TORKINGTON ROAD STABLES TORKINGTON ROAD, HAZEL GROVE, STOCKPORT
-            "10093012711",  # 155A STOCKPORT ROAD, MARPLE, STOCKPORT
-            "100011526060",  # 27 THE CLOUGH, STOCKPORT
-            "100011526066",  # 33 THE CLOUGH, STOCKPORT
-            "100011526058",  # 25 THE CLOUGH, STOCKPORT
-            "100011526064",  # 31 THE CLOUGH, STOCKPORT
-            "100011526056",  # 23 THE CLOUGH, STOCKPORT
-            "100011526062",  # 29 THE CLOUGH, STOCKPORT
-            "10002096735",  # 16B MARPLE ROAD, OFFERTON, STOCKPORT
-            "100011432786",  # 6 DEMMINGS ROAD, CHEADLE
-            "100011432788",  # 8 DEMMINGS ROAD, CHEADLE
-        ]:
-            return None
 
         if record.addressline6 in [
             "SK8 3TJ",
-            "SK8 6BG",
-            "SK8 3DQ",
-            "SK6 2BD",
-            "SK6 1AY",
-            "SK6 1NL",
-            "SK7 4NX",
-            "SK7 4AJ",
-            "SK7 4DF",
-            "SK7 2AA",
-            "SK6 6LP",
-            "SK6 5NY",
-            "SK6 6DF",
-            "SK6 3BT",
-            "SK7 3LZ",
-            "SK6 4EN",
-            "SK6 4HT",
-            "SK6 1JG",
-            "SK5 6XD",
-            "SK5 7BJ",
-            "SK4 5DA",
-            "SK2 7HJ",
-            "SK2 6LW",
             "SK6 7DH",
         ]:
             return None
@@ -62,21 +28,5 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         if record.polling_place_id == "10415":
             record = record._replace(polling_place_easting="")
             record = record._replace(polling_place_northing="")
-
-        # Station change from Council
-        # Change: 1st Romiley Scout Group Heys Avenue Romiley SK6 4H
-        # To:The Cherry Tree Project Hub, 2 Cherry Tree Lane, Romiley SK6 4ER
-        if record.polling_place_id == "10373":
-            record = record._replace(
-                polling_place_name="The Cherry Tree Project Hub",
-                polling_place_address_1="2 Cherry Tree Lane",
-                polling_place_address_2="Romiley",
-                polling_place_address_3="",
-                polling_place_address_4="",
-                polling_place_postcode="SK6 4ER",
-                polling_place_easting="395171",
-                polling_place_northing="390901",
-                polling_place_uprnproperty_urn="100011468406",
-            )
 
         return super().station_record_to_dict(record)
