fc19276d47741124248f9893a954b6e9afb1f8ec
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-03 14:49:36 +0000

    Import script for Runnymede (2022-05-05) (closes #4200)

--- polling_stations/apps/data_importers/management/commands/import_runnymede.py
+++ polling_stations/apps/data_importers/management/commands/import_runnymede.py
@@ -4,22 +4,15 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "RUN"
     addresses_name = (
-        "2021-03-03T10:09:37.682656/Runnymede Democracy_Club__06May2021 (1).tsv"
+        "2022-05-05/2022-03-03T14:49:14.647146/Democracy_Club__05May2022.tsv"
     )
     stations_name = (
-        "2021-03-03T10:09:37.682656/Runnymede Democracy_Club__06May2021 (1).tsv"
+        "2022-05-05/2022-03-03T14:49:14.647146/Democracy_Club__05May2022.tsv"
     )
-    elections = ["2021-05-06"]
+    elections = ["2022-05-05"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "10002019662",  # MOBILE HOME GREENACRES BITTAMS LANE, CHERTSEY
-            "10092959794",  # ACCOMMODATION AT THE BLACK PRINCE WOODHAM LANE, ADDLESTONE
-        ]:
-            return None
 
         if record.addressline6 in ["KT16 8QD", "KT16 8AG"]:
             return None
