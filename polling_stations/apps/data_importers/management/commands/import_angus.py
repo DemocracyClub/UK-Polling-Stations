a875dd138b3abc5106118787dfe045e614e2f6fb
GeoWill <roper.william@gmail.com>
2022-03-03 12:45:19 +0000

    Import Script for Angus (2022-05-05) (closes #4118)

--- polling_stations/apps/data_importers/management/commands/import_angus.py
+++ polling_stations/apps/data_importers/management/commands/import_angus.py
@@ -4,28 +4,21 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 class Command(BaseHalaroseCsvImporter):
     council_id = "ANS"
     addresses_name = (
-        "2021-03-04T10:52:18.139732/Angus polling_station_export-2021-03-04.csv"
+        "2022-05-05/2022-02-11T11:29:59.770553/polling_station_export-2022-02-03.csv"
     )
     stations_name = (
-        "2021-03-04T10:52:18.139732/Angus polling_station_export-2021-03-04.csv"
+        "2022-05-05/2022-02-11T11:29:59.770553/polling_station_export-2022-02-03.csv"
     )
-    elections = ["2021-05-06"]
+    elections = ["2022-05-05"]
 
-    def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
 
-        if record.housepostcode in [
-            "DD9 7EZ",
-            "DD8 2SF",
-            "DD8 4QB",
-            "DD8 5PP",
-            "DD8 2TJ",
-            "DD11 3ET",
-            "DD8 4QH",
-        ]:
-            return None
+def address_record_to_dict(self, record):
+    uprn = record.uprn.strip().lstrip("0")
 
-        if uprn in ["117078908", "117116533"]:
-            return None
+    if record.housepostcode in ["DD9 7EZ", "DD8 2SF", "DD8 5PP", "DD8 2TJ"]:
+        return None
 
-        return super().address_record_to_dict(record)
+    if uprn in ["117116533"]:
+        return None
+
+    return super().address_record_to_dict(record)
