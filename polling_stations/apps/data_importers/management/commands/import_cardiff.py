ec8633dc5d32f0719f9edf96e5b6f02e407e06fd
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-03 10:12:41 +0000

    Import script for City of Cardiff (2022-05-05) (closes #4196)

--- polling_stations/apps/data_importers/management/commands/import_cardiff.py
+++ polling_stations/apps/data_importers/management/commands/import_cardiff.py
@@ -2,30 +2,20 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-
     council_id = "CRF"
-    addresses_name = "2021-03-24T16:24:49.782410/cardiff_deduped.tsv"
-    stations_name = "2021-03-24T16:24:49.782410/cardiff_deduped.tsv"
-    elections = ["2021-05-06"]
-    csv_delimiter = "\t"
+    addresses_name = (
+        "2022-05-05/2022-03-03T10:11:12.032905/Democracy_Club__05May2022.tsv"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-03T10:11:12.032905/Democracy_Club__05May2022.tsv"
+    )
+    elections = ["2022-05-05"]
     csv_encoding = "windows-1252"
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
 
-        if uprn == "100100110392":
-            return None
-
-        if record.addressline6 in [
-            "CF24 3DZ",
-            "CF3 0UH",
-            "CF14 2FN",
-            "CF14 6PE",
-            "CF24 4RU",
-            "CF3 4LL",
-            "CF5 6HF",
-            "CF14 9UA",
-        ]:
+        if record.addressline6 in ["CF24 2DG", "CF3 4LL", "CF14 9UA"]:
             return None
 
         return super().address_record_to_dict(record)
