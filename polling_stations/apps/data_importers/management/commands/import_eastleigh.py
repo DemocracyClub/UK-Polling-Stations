00bc3700c4af1fa4780b365833401165e76c6d05
GeoWill <roper.william@gmail.com>
2022-03-03 15:49:06 +0000

    Import script for Eastleigh (2022-05-05) (closes #4141)

--- polling_stations/apps/data_importers/management/commands/import_eastleigh.py
+++ polling_stations/apps/data_importers/management/commands/import_eastleigh.py
@@ -3,37 +3,24 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "EAT"
-    addresses_name = "2021-03-03T12:56:56.264077/Eastleigh Borough Council Democracy_Club__06May2021 (1).tsv"
-    stations_name = "2021-03-03T12:56:56.264077/Eastleigh Borough Council Democracy_Club__06May2021 (1).tsv"
-    elections = ["2021-05-06"]
+    addresses_name = (
+        "2022-05-05/2022-02-22T09:04:34.675651/Democracy_Club__05May2022.tsv"
+    )
+    stations_name = (
+        "2022-05-05/2022-02-22T09:04:34.675651/Democracy_Club__05May2022.tsv"
+    )
+    elections = ["2022-05-05"]
     csv_delimiter = "\t"
-
-    def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "100060322293",  # 18 LEWRY CLOSE, HEDGE END, SOUTHAMPTON
-            "10094385410",  # 44A KELBURN CLOSE, CHANDLER'S FORD
-            "10012197923",  # 300 CHESTNUT AVENUE, CHANDLER'S FORD, EASTLEIGH
-            "10009593531",  # FLAT 1 85 TWYFORD ROAD, EASTLEIGH
-            "10094385283",  # 12 JACOBS CLOSE, BURSLEDON
-        ]:
-            return None
-
-        if record.addressline6 in ["SO31 5FH", "SO30 3FA", "SO53 2FG"]:
-            return None
-
-        return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
         if (
-            record.polling_place_id == "5150"
+            record.polling_place_id == "5363"
         ):  # Abbey Hall Victoria Road Netley Abbey Southampton SO31 5FA
             record = record._replace(polling_place_easting="445235")
             record = record._replace(polling_place_northing="108734")
 
         if (
-            record.polling_place_id == "5105"
+            record.polling_place_id == "5351"
         ):  # Chandler's Ford Community Centre, Hursley Road, Chandler's Ford, Eastleigh, SO53 2FS
             record = record._replace(polling_place_postcode="SO53 2FT")
 
