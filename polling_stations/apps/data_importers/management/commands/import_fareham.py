532ea8b5e9bd3680d892b293e8b27dcc8dfb9067
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-04 14:15:39 +0000

    Import script for Fareham (2022-05-05) (closes #4205)

--- polling_stations/apps/data_importers/management/commands/import_fareham.py
+++ polling_stations/apps/data_importers/management/commands/import_fareham.py
@@ -4,23 +4,13 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "FAR"
     addresses_name = (
-        "2021-02-17T11:13:08.624493/Fareham BC Democracy_Club__06May2021.CSV"
+        "2022-05-05/2022-03-04T14:15:27.595084/Democracy_Club__05May2022.tsv"
     )
     stations_name = (
-        "2021-02-17T11:13:08.624493/Fareham BC Democracy_Club__06May2021.CSV"
+        "2022-05-05/2022-03-04T14:15:27.595084/Democracy_Club__05May2022.tsv"
     )
-    elections = ["2021-05-06"]
-    csv_delimiter = ","
-    csv_encoding = "windows-1252"
-
-    def station_record_to_dict(self, record):
-
-        if (
-            record.polling_place_id == "6286"
-        ):  # Titchfield Parish Rooms, High Street, Titchfield, Fareham, PO14 4AB
-            record = record._replace(polling_place_postcode="PO14 4AQ")
-
-        return super().station_record_to_dict(record)
+    elections = ["2022-05-05"]
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
@@ -28,7 +18,6 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         if record.post_code in [
             "SO31 7BJ",
             "PO16 7LR",
-            "SO31 6BH",
             "PO14 4QS",
         ]:
             return None
