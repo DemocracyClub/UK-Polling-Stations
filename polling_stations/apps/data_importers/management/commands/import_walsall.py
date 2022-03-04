73e8176ad25c89128fe0935676f39435163a2874
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-04 14:27:10 +0000

    Import script for Walsall (2022-05-05) (closes #4207)

--- polling_stations/apps/data_importers/management/commands/import_walsall.py
+++ polling_stations/apps/data_importers/management/commands/import_walsall.py
@@ -6,44 +6,39 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 class Command(BaseHalaroseCsvImporter):
     council_id = "WLL"
     addresses_name = (
-        "2021-03-30T11:27:49.115128/Walsall New polling_station_export-2021-03-29.csv"
+        "2022-05-05/2022-03-04T14:26:29.368353/polling_station_export-2022-03-03.csv"
     )
     stations_name = (
-        "2021-03-30T11:27:49.115128/Walsall New polling_station_export-2021-03-29.csv"
+        "2022-05-05/2022-03-04T14:26:29.368353/polling_station_export-2022-03-03.csv"
     )
-    elections = ["2021-05-06"]
-    csv_delimiter = ","
+    elections = ["2022-05-05"]
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
 
         if uprn in [
-            "10090065721",  # 279 HIGH STREET, BROWNHILLS, WALSALL
-            "200002877500",  # 43A HIGH STREET, BROWNHILLS, WALSALL
             "10093460948",  # CASTLEHILL SPECIALIST CARE HOME, 390 CHESTER ROAD, WALSALL
             "100071550039",  # LIVING AREA 5 JOHN STREET, WALSALL
             "100071072854",  # 59 NEW STREET, SHELFIELD, WALSALL
             "100071072810",  # 9A NEW STREET, SHELFIELD, WALSALL
-            "10013665979",  # FLATS 2 AND 4 32 LYSWAYS STREET, WALSALL
-            "10090903152",  # REAR OF 26, CALDMORE GREEN, WALSALL
             "10090902979",  # 235 STAFFORD STREET, WALSALL
-            "10013665316",  # YMCA, PREMSONS HOUSE, GREEN LANE, WALSALL
-            "200003317397",  # CHRIST CHURCH RECTORY, BLAKENALL HEATH, WALSALL
             "10090903310",  # FLAT 1 73 STAFFORD STREET, WILLENHALL
+            "10013665979",  # FLATS 2 AND 4 32 LYSWAYS STREET, WALSALL
+            "200002877500",  # 43A HIGH STREET, BROWNHILLS, WALSALL
         ]:
             return None
 
         if record.housepostcode in [
-            "WS3 2DX",
-            "WS3 3XA",
-            "WV12 4BZ",
-            "WV12 4BZ",
             "WS1 3LD",
-            "WS4 1GB",
+            "WS2 7LU",
+            "WS3 1FJ",
+            "WS3 2DX",
+            "WS3 5AE",
             "WS4 1GA",
+            "WV12 4BZ",
             "WV12 5QA",
-            "WS10 7TG",
             "WV12 5YH",
+            "WS10 7TG",
         ]:
             return None
 
