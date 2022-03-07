bc0e1f151adb5059a957689f3e5b617926b5ac35
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-07 15:47:35 +0000

    Import script for Gosport (2022-05-05) (closes #4221)

--- polling_stations/apps/data_importers/management/commands/import_gosport.py
+++ polling_stations/apps/data_importers/management/commands/import_gosport.py
@@ -1,15 +1,14 @@
-from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
+from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 
-class Command(BaseXpressDemocracyClubCsvImporter):
+class Command(BaseDemocracyCountsCsvImporter):
     council_id = "GOS"
-    addresses_name = "2021-03-02T17:11:51.716352/2021 LGE & PCC - Gosport Democracy_Club__06May2021 v2.tsv"
-    stations_name = "2021-03-02T17:11:51.716352/2021 LGE & PCC - Gosport Democracy_Club__06May2021 v2.tsv"
-    elections = ["2021-05-06"]
-    csv_delimiter = "\t"
+    addresses_name = "2022-05-05/2022-03-07T15:47:28.644792/2022 Borough of Gosport - Democracy Club - Polling Districts v1 (07 03 2022).csv"
+    stations_name = "2022-05-05/2022-03-07T15:47:28.644792/2022 Borough of Gosport - Democracy Club - Polling Stations v1 (07 03 2022).csv"
+    elections = ["2022-05-05"]
 
     def address_record_to_dict(self, record):
-        if record.addressline6 in ["PO13 0EW"]:
+        if record.addressline6 in ["PO12 2EH"]:
             return None
 
         return super().address_record_to_dict(record)
