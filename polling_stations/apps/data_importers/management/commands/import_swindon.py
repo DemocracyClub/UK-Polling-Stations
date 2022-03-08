a55369539486b1ccb36880c87461937bd2e9899b
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-08 10:01:19 +0000

    Import script for Swindon (2022-05-05) (closes #4227)

--- polling_stations/apps/data_importers/management/commands/import_swindon.py
+++ polling_stations/apps/data_importers/management/commands/import_swindon.py
@@ -1,60 +1,33 @@
-from data_importers.ems_importers import BaseXpressDemocracyClubCsvImporter
+from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SWD"
-    addresses_name = "2021-07-26/SwindonDemocracy_Club__19August2021.tsv"
-    stations_name = "2021-07-26/SwindonDemocracy_Club__19August2021.tsv"
-    elections = ["2021-08-19"]
+    addresses_name = (
+        "2022-05-05/2022-03-08T10:01:03.456828/Democracy_Club__05May2022.tsv"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-08T10:01:03.456828/Democracy_Club__05May2022.tsv"
+    )
+    elections = ["2022-05-05"]
     csv_delimiter = "\t"
-
-    def station_record_to_dict(self, record):
-        if record.polling_place_id == "10577":
-            # Central Community Centre Emlyn Square Entrance Railway Village Swindon
-            record = record._replace(polling_place_postcode="SN1 5BP")
-        if record.polling_place_id == "10369":
-            # Nythe Community Centre The Drive Swindon
-            record = record._replace(polling_place_postcode="SN3 3QA")
-        if record.polling_place_id == "10329":
-            # All Saints Church Southbrook Street Swindon
-            record = record._replace(polling_place_postcode="SN2 1HF")
-        if record.polling_place_id == "10357":
-            # The Snooker Room The Tawny Owl Queen Elizabeth Drive Taw Hill Swindon
-            record = record._replace(polling_place_postcode="SN25 1WR")
-        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
             "10090042967",  # 2 COMET WAY, WROUGHTON, SWINDON
-            "10009420836",  # KINGSHILL TRUST, KINGSHILL HOUSE, KENT ROAD, SWINDON
-            "10094330068",  # FLAT 5, 103 COMMERCIAL ROAD, SWINDON
             "10094328819",  # 170A RODBOURNE ROAD, SWINDON
-            "10010427210",  # 71 EASTBURY WAY, SWINDON
-            "10010424377",  # 69 EASTBURY WAY, SWINDON
-            "10010427209",  # 67 EASTBURY WAY, SWINDON
-            "10010427208",  # 65 EASTBURY WAY, SWINDON
-            "10010425158",  # 100 EASTBURY WAY, SWINDON
-            "10010425157",  # 98 EASTBURY WAY, SWINDON
-            "10010425156",  # 96 EASTBURY WAY, SWINDON
-            "10010425159",  # 102 EASTBURY WAY, SWINDON
+            "100121344239",  # CONLON, UNIT 16 GALTON WAY, KENDRICK INDUSTRIAL ESTATE, SWINDON
+            "200002920576",  # 549A CRICKLADE ROAD, SWINDON
         ]:
             return None
 
         if record.addressline6 in [
-            "SN25 1TX",
-            "SN26 8BZ",
-            "SN2 2AG",
-            "SN25 3EN",
             "SN25 3LR",
-            "SN1 3AS",
-            "SN1 5QP",
-            "SN3 2LQ",
-            "SN3 5EP",
+            "SN25 3EN",
             "SN6 7JY",
-            "SN25 2DZ",
-            "SN6 7DG",
+            "SN3 5EP",
         ]:
             return None
 
