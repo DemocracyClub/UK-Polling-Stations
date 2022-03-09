91760d6c0c634b1b0cd8d0d49b3fec58c481981f
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-09 17:05:34 +0000

    Import script for St Albans (2022-05-05) (closes #4254)

--- polling_stations/apps/data_importers/management/commands/import_st_albans.py
+++ polling_stations/apps/data_importers/management/commands/import_st_albans.py
@@ -1,73 +1,24 @@
-import json
-from django.contrib.gis.geos import GEOSGeometry
-from django.contrib.gis.geos.collections import MultiPolygon, Polygon
-from data_importers.geo_utils import fix_bad_polygons
-from data_importers.github_importer import BaseGitHubImporter
+from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
-class Command(BaseGitHubImporter):
-
-    srid = 27700
-    districts_srid = 27700
+class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SAL"
-    elections = ["2021-05-06"]
-    scraper_name = "wdiv-scrapers/DC-PollingStations-StAlbans"
-    geom_type = "geojson"
+    addresses_name = (
+        "2022-05-05/2022-03-09T17:04:51.766114/Democracy_Club__05May2022.tsv"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-09T17:04:51.766114/Democracy_Club__05May2022.tsv"
+    )
+    elections = ["2022-05-05"]
+    csv_delimiter = "\t"
 
-    def extract_poly_from_geometrycollection(self, geo_collection):
-        for feature in geo_collection:
-            if isinstance(feature, Polygon):
-                return self.clean_poly(
-                    GEOSGeometry(feature.wkt, srid=self.get_srid("districts"))
-                )
+    def address_record_to_dict(self, record):
+        if record.addressline6 in [
+            "AL3 5PR",
+            "AL4 0TP",
+            "AL4 0TG",
+            "AL3 5PD",
+        ]:
+            return None
 
-    def extract_json_geometry(self, record, srid):
-        geom = json.loads(record["geometry"])
-
-        # delete any junk which will cause us to throw
-        # Invalid number of points in LinearRing found 3 - must be 0 or >= 4
-        if geom["geometry"]["type"] == "MultiPolygon":
-            for i, poly in enumerate(geom["geometry"]["coordinates"]):
-                if len(poly[0]) < 4:
-                    del geom["geometry"]["coordinates"][i]
-
-        geojson = json.dumps(geom["geometry"])
-        poly = self.clean_poly(GEOSGeometry(geojson))
-        poly.srid = srid
-        return poly
-
-    def district_record_to_dict(self, record):
-        poly = self.extract_geometry(record, self.geom_type, self.get_srid("districts"))
-
-        if isinstance(poly, MultiPolygon):
-            area = poly
-        else:
-            area = self.extract_poly_from_geometrycollection(poly)
-
-        return {
-            "internal_council_id": record["DISTRICT"],
-            "name": record["WARD"] + " - " + record["DISTRICT"],
-            "area": area,
-            "polling_station_id": record["DISTRICT"],
-        }
-
-    def station_record_to_dict(self, record):
-        location = self.extract_geometry(
-            record, self.geom_type, self.get_srid("stations")
-        )
-
-        # Most stations are tiny circular polygons...
-        if location.geom_type == "Point":
-            point = location
-        else:
-            point = location.centroid
-
-        return {
-            "internal_council_id": record["PD"],
-            "address": record["LOCATION"],
-            "postcode": "",
-            "location": point,
-        }
-
-    def post_import(self):
-        fix_bad_polygons()
+        return super().address_record_to_dict(record)
