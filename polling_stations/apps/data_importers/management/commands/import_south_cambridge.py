6d0360828c76837b0a33dd3bf5c76871b750f0f7
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-09 15:39:53 +0000

    Import script for South Cambridgeshire (2022-05-05) (closes #4250)

--- polling_stations/apps/data_importers/management/commands/import_south_cambridge.py
+++ polling_stations/apps/data_importers/management/commands/import_south_cambridge.py
@@ -4,31 +4,24 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SCA"
     addresses_name = (
-        "2021-03-16T10:56:43.784190/South Cambridgeshire Democracy_Club__06May2021.tsv"
+        "2022-05-05/2022-03-09T15:38:47.461320/Democracy_Club__05May2022.tsv"
     )
     stations_name = (
-        "2021-03-16T10:56:43.784190/South Cambridgeshire Democracy_Club__06May2021.tsv"
+        "2022-05-05/2022-03-09T15:38:47.461320/Democracy_Club__05May2022.tsv"
     )
-    elections = ["2021-05-06"]
+    elections = ["2022-05-05"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
 
-        if uprn in [
-            "10008082519",  # DWELLING AT REAR 43 CAMBRIDGE ROAD, WIMPOLE
-            "10033031432",  # BURY LANE HOUSE, BURY LANE, MELBOURN, ROYSTON
-            "10008079271",  # GREENGAGE COTTAGE, BURY LANE, MELBOURN, ROYSTON
-            "10094259590",  # CROWDEAN, CROWDEAN LANE, ELSWORTH, CAMBRIDGE
-            "100091203868",  # ANNEXE CLARE BARN MAIN STREET, CALDECOTE
-            "200001812525",  # THE GATE HOUSE, ST. NEOTS ROAD, DRY DRAYTON, CAMBRIDGE
-            "100091203790",  # FOSTERS LONG DROVE, COTTENHAM
-            "100090163907",  # 34 WOODFIELD ROAD, HIGHFIELDS CALDECOTE, CAMBRIDGE
-            "100090161038",  # 10 THE PASTURES, HARSTON, CAMBRIDGE
+        if record.addressline6 in [
+            "CB24 3DS",
+            "CB23 3UG",
+            "SG8 0BD",
+            "CB21 5LF",
+            "CB23 1AA",
+            "CB22 5AN",
         ]:
-            return None
-
-        if record.addressline6 in ["CB23 6LE", "CB24 4QG"]:
             return None
 
         return super().address_record_to_dict(record)
