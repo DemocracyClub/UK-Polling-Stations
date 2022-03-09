accb3925356cfa5d457cabf30ecb97c9e2e86aa9
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-09 09:56:29 +0000

    Import script for Midlothian (2022-05-05) (closes #4238)

--- polling_stations/apps/data_importers/management/commands/import_midlothian.py
+++ polling_stations/apps/data_importers/management/commands/import_midlothian.py
@@ -1,52 +1,54 @@
-from data_importers.ems_importers import BaseHalaroseCsvImporter
+from addressbase.models import UprnToCouncil
+from data_importers.management.commands import BaseHalaroseCsvImporter
 
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "MLN"
     addresses_name = (
-        "2021-03-22T10:03:09.388418/lothian ERO polling_station_export-2021-03-21.csv"
+        "2022-05-05/2022-03-09T09:55:31.236684/polling_station_export-2022-03-02 2.csv"
     )
     stations_name = (
-        "2021-03-22T10:03:09.388418/lothian ERO polling_station_export-2021-03-21.csv"
+        "2022-05-05/2022-03-09T09:55:31.236684/polling_station_export-2022-03-02 2.csv"
     )
-    elections = ["2021-05-06"]
-    csv_delimiter = ","
-    csv_encoding = "windows-1252"
+    elections = ["2022-05-05"]
+
+    def pre_import(self):
+        # We need to consider rows that don't have a uprn when importing data.
+        # However there are lots of rows for other councils in this file.
+        # So build a list of stations from rows that do have UPRNS
+        # and then use that list of stations to make sure we check relevant rows, even if they don't have a UPRN
+
+        council_uprns = set(
+            UprnToCouncil.objects.filter(lad=self.council.geography.gss).values_list(
+                "uprn", flat=True
+            )
+        )
+        self.COUNCIL_STATIONS = set()
+        data = self.get_addresses()
+
+        for record in data:
+            if record.uprn in council_uprns:
+                self.COUNCIL_STATIONS.add(self.get_station_hash(record))
 
     def address_record_to_dict(self, record):
-        if (
-            record.adminarea != "Midlothian"
-            or (
-                record.town == "Musselburgh"
-                and record.pollingstationname != "Danderhall Leisure Centre"
-            )
-            or record.town == "Currie"
-            or record.town == "Newbridge"
-        ):
+        if self.get_station_hash(record) not in self.COUNCIL_STATIONS:
             return None
 
         if record.housepostcode in [
-            "EH20 9AA",
-            "EH20 9QA",
-            "EH22 5TH",
-            "EH23 4QA",
             "EH22 2EE",
+            "EH20 9AA",
+            "EH22 5TH",
+            "EH22 1SY",
+            "EH22 5BG",
+            "EH23 4QA",
+            "EH20 9QA",
         ]:
             return None
 
         return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
-        if (
-            record.adminarea != "Midlothian"
-            or (
-                record.town == "Musselburgh"
-                and record.pollingstationname != "Danderhall Leisure Centre"
-            )
-            or record.town == "Currie"
-            or record.town == "Newbridge"
-        ):
-
+        if self.get_station_hash(record) not in self.COUNCIL_STATIONS:
             return None
 
         return super().station_record_to_dict(record)
