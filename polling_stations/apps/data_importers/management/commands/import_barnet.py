430dee6dcc532b7e7db0f9d2a2b961935b6807a2
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-10 12:42:35 +0000

    Import script for Barnet (2022-05-05) (closes #4265)

--- polling_stations/apps/data_importers/management/commands/import_barnet.py
+++ polling_stations/apps/data_importers/management/commands/import_barnet.py
@@ -3,32 +3,15 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "BNE"
-    addresses_name = "2021-03-25T11:42:15.781785/Democracy Club_Polling Districts.csv"
-    stations_name = "2021-03-25T11:42:15.781785/Democracy Club_Polling Stations.csv"
-    elections = ["2021-05-06"]
+    addresses_name = "2022-05-05/2022-03-10T12:42:22.195442/Barnet_PDs_Data_220505.csv"
+    stations_name = (
+        "2022-05-05/2022-03-10T12:42:22.195442/Barnet_PollStations_Data_220505.csv"
+    )
+    elections = ["2022-05-05"]
 
     def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
         if record.postcode.strip() in [
-            "NW7 4SB",
-            "HA8 7LZ",
-            "N11 3DD",
-            "EN4 8QZ",
             "NW11 7ND",
         ]:
             return None
-
-        if uprn in [
-            "10091038621",  # FLAT 1 129 HENDON WAY, LONDON
-            "10091038625",  # FLAT 5 129 HENDON WAY, LONDON
-            "10091038623",  # FLAT 3 129 HENDON WAY, LONDON
-            "10091041237",  # FLAT 3, 238 EAST BARNET ROAD, BARNET
-            "10091041238",  # FLAT 4, 238 EAST BARNET ROAD, BARNET
-            "200163833",  # FLAT 1 41 THE GROVE, FINCHLEY, LONDON
-            "200163834",  # FLAT 2 41 THE GROVE, FINCHLEY, LONDON
-            "200163838",  # FLAT 6 41 THE GROVE, FINCHLEY, LONDON
-            "200196535",  # 1 NINA WALK, LONDON, N20 0RB
-        ]:
-            return None
-
         return super().address_record_to_dict(record)
