4188533dc661807a7bc51c0e346f21bb27ac00a8
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-10 14:33:43 +0000

    Import script for Shetland Islands (2022-05-05) (closes #4274)

--- polling_stations/apps/data_importers/management/commands/import_shetland_islands.py
+++ polling_stations/apps/data_importers/management/commands/import_shetland_islands.py
@@ -3,21 +3,27 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "ZET"
-    addresses_name = "2021-03-23T15:04:56.746808/polling_station_export-2021-03-19.csv"
-    stations_name = "2021-03-23T15:04:56.746808/polling_station_export-2021-03-19.csv"
-    elections = ["2021-05-06"]
-    csv_delimiter = ","
+    addresses_name = (
+        "2022-05-05/2022-03-10T14:33:34.834669/polling_station_export-2022-03-10.csv"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-10T14:33:34.834669/polling_station_export-2022-03-10.csv"
+    )
+    elections = ["2022-05-05"]
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
 
         if uprn in [
             "30100007505",  # NORTH BOOTH, HAROLDSWICK, UNST, SHETLAND
-            "30100000940",  # HOUSE A B9088 FROM HERRA X-ROADS TO BAELA JUNCTION, HOUBIE, FETLAR
+            "30100005132",  # BOOTH, OLLABERRY, SHETLAND
         ]:
             return None
 
-        if record.housepostcode in ["ZE2 9HD"]:
+        if record.housepostcode in [
+            "ZE1 0UX",
+            "ZE2 9HD",
+        ]:
             return None
 
         return super().address_record_to_dict(record)
