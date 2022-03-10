3c45a36b21a522ab92e1182a04d43edd6841fd46
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-10 11:16:57 +0000

    Import script for Royal of Kensington and Chelsea (2022-05-05) (closes #4263)

--- polling_stations/apps/data_importers/management/commands/import_kensington_and_chelsea.py
+++ polling_stations/apps/data_importers/management/commands/import_kensington_and_chelsea.py
@@ -3,25 +3,28 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "KEC"
-    addresses_name = "2021-03-24T11:28:23.977778/RBKC Democracy_Club__06May2021.CSV"
-    stations_name = "2021-03-24T11:28:23.977778/RBKC Democracy_Club__06May2021.CSV"
-    elections = ["2021-05-06"]
+    addresses_name = (
+        "2022-05-05/2022-03-10T11:16:41.646308/Democracy_Club__05May2022.CSV"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-10T11:16:41.646308/Democracy_Club__05May2022.CSV"
+    )
+    elections = ["2022-05-05"]
 
     def address_record_to_dict(self, record):
-        if record.addressline6 in ["W11 4HD", "W14 8BA", "SW1X 9SG", "SW3 5RP"]:
+        if record.addressline6 in [
+            "SW1X 9SG",
+            "SW7 4DW",
+            "W11 4JJ",
+            "W11 4HD",
+        ]:
             return None
 
         uprn = record.property_urn.strip().lstrip("0")
         if uprn in [
+            "217113295",  # DOCTORS SURGERY, 12 RADDINGTON ROAD, LONDON
             "217130227",  # BASEMENT AND GROUND FLOOR FLAT 20 TREGUNTER ROAD, LONDON
         ]:
             return None
 
-        if uprn == "217108045":
-            record = record._replace(addressline6="W8 5DH")
-        if (
-            record.addressline6 == "W11 4LY"
-            and record.addressline1 == "2B Drayson Mews"
-        ):
-            record = record._replace(addressline6="W8 4LY")
         return super().address_record_to_dict(record)
