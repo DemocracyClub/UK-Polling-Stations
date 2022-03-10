43e0e8a9e085683c7186230003b4ac2e54dc4ad6
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-10 15:06:22 +0000

    Import script for Kirklees (2022-05-05) (closes #4276)

--- polling_stations/apps/data_importers/management/commands/import_kirklees.py
+++ polling_stations/apps/data_importers/management/commands/import_kirklees.py
@@ -3,17 +3,32 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "KIR"
-    addresses_name = "2021-03-24T10:25:10.922382/Kirklees Democracy_Club__06May2021.tsv"
-    stations_name = "2021-03-24T10:25:10.922382/Kirklees Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
-    csv_delimiter = "\t"
+    addresses_name = (
+        "2022-05-05/2022-03-10T15:06:01.054793/Democracy_Club__05May2022.tsv"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-10T15:06:01.054793/Democracy_Club__05May2022.tsv"
+    )
+    elections = ["2022-05-05"]
     csv_encoding = "windows-1252"
+    csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
-        # New Mill Working Mens Club Sheffield Road New Mill Holmfirth HD9 7JT
-        if record.polling_place_id == "14458":
+        # Cleckheaton Methodist Church, Greenside Cleckheaton
+        if record.polling_place_id == "14951":
             record = record._replace(polling_place_easting="")
             record = record._replace(polling_place_northing="")
+
+        # Pre-School Room - Lydgate J & I School, Lydgate Road, Soothill
+        if record.polling_place_id == "14866":
+            record = record._replace(polling_place_easting="")
+            record = record._replace(polling_place_northing="")
+
+        # Marsden United Reform Church, Peel Street, Marsden
+        if record.polling_place_id == "15516":
+            record = record._replace(polling_place_easting="")
+            record = record._replace(polling_place_northing="")
+            record = record._replace(polling_place_uprn="")
 
         # Roberttown Community Centre
         # Entrance is on Church Rd
@@ -24,25 +39,10 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "10094118631",  # 6 HAND BANK LANE, LOWER HOPTON, MIRFIELD
-            "83192424",  # 54 WILDSPUR MILLS, NEW MILL, HOLMFIRTH
-            "83192423",  # 53 WILDSPUR MILLS, NEW MILL, HOLMFIRTH
-            "83192420",  # 50 WILDSPUR MILLS, NEW MILL, HOLMFIRTH
-            "83192421",  # 51 WILDSPUR MILLS, NEW MILL, HOLMFIRTH
-            "83192422",  # 52 WILDSPUR MILLS, NEW MILL, HOLMFIRTH
-        ]:
-            return None
 
         if record.addressline6 in [
-            "WF15 6NP",
-            "HD7 6DU",
-            "HD7 4NN",
-            "HD7 5XB",
-            "HD7 4DJ",
             "HD9 7EH",
+            "HD7 5XB",
         ]:
             return None
 
