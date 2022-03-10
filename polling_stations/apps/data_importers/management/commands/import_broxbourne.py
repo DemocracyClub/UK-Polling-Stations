8f8453d55bc62f22c3900355161160748c89d272
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-10 15:49:06 +0000

    Import script for Broxbourne (2022-05-05) (closes #4280)

--- polling_stations/apps/data_importers/management/commands/import_broxbourne.py
+++ polling_stations/apps/data_importers/management/commands/import_broxbourne.py
@@ -3,40 +3,35 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "BRX"
-    addresses_name = "2021-03-19T10:23:06.876474/Democracy club - polling districts 2021 Borough of Broxbourne.csv"
-    stations_name = "2021-03-19T10:23:06.876474/Democracy club - polling stations 2021 Borough of Broxbourne.csv"
-    elections = ["2021-05-06"]
+    addresses_name = "2022-05-05/2022-03-10T15:48:58.245362/Democracy Club - Polling Districts 2022 Borough of Broxbourne.csv"
+    stations_name = "2022-05-05/2022-03-10T15:48:58.245362/Democracy Club - Polling Stations 2022 Borough of Broxbourne.csv"
+    elections = ["2022-05-05"]
 
     def station_record_to_dict(self, record):
 
-        # Fairfields primary school
-        if record.stationcode in ["14A", "14"]:
+        # FAIRFIELDS PRIMARY  SCHOOL, (LITTLE FIELDS PRE SCHOOL)
+        if record.stationcode == "23":
             record = record._replace(xordinate="534213")
             record = record._replace(yordinate="203580")
 
-        # Flamstead End Hall
-        if record.stationcode in ["22", "22A"]:
+        # FLAMSTEAD END HALL, MAYO CLOSE
+        if record.stationcode == "22":
             record = record._replace(xordinate="535062")
             record = record._replace(yordinate="203335")
 
-        # Fairley Cross Hall, Rosedale Community Church
-        if record.stationcode in ["31", "16"]:
+        # FAIRLEY CROSS HALL, ROSEDALE COMMUNITY CHURCH
+        if record.stationcode == "27":
             record = record._replace(xordinate="534455")
             record = record._replace(yordinate="203051")
 
-        # HALSEY HALL
-        if record.stationcode in ["23", "23A", "21", "21A"]:
+        # HALSEY HALL, TURNERS HILL
+        if record.stationcode in ["16", "17", "17A"]:
             record = record._replace(xordinate="535906")
             record = record._replace(yordinate="201922")
 
-        # Christ Church, Church Hall, Trinity Lane, Waltham Cross, EN8 7ED
-        if record.stationcode == "24":
+        # CHRIST CHURCH, CHURCH HALL, TRINITY LANE, WALTHAM CROSS, EN8 7ED
+        if record.stationcode == "18":
             record = record._replace(xordinate="535999")
             record = record._replace(yordinate="201098")
-
-        # Hurst Drive Primary School, Hurst Drive, Waltham Cross EN8 8DH
-        if record.stationcode == "25":
-            record = record._replace(xordinate="535576")
-            record = record._replace(yordinate="200249")
 
         return super().station_record_to_dict(record)
