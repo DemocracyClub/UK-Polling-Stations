fe9c0f4a0d08851b0cf016e3b4c9c3d8e1e922c1
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-10 15:50:16 +0000

    Import script for Huntingdonshire (2022-05-05) (closes #4283)

--- polling_stations/apps/data_importers/management/commands/import_huntingdonshire.py
+++ polling_stations/apps/data_importers/management/commands/import_huntingdonshire.py
@@ -3,37 +3,27 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "HUN"
-    addresses_name = "2021-03-22T10:17:22.245616/Democracy_Club__06May2021 Friday.CSV"
-    stations_name = "2021-03-22T10:17:22.245616/Democracy_Club__06May2021 Friday.CSV"
-    elections = ["2021-05-06"]
-    csv_delimiter = ","
+    addresses_name = (
+        "2022-05-05/2022-03-10T15:49:21.235997/Democracy_Club__05May2022.tsv"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-10T15:49:21.235997/Democracy_Club__05May2022.tsv"
+    )
+    elections = ["2022-05-05"]
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "10094922851",  # CAMELLIA COTTAGE, LITTLE END ROAD, EATON SOCON, ST. NEOTS
-            "10094922852",  # 156B GREAT NORTH ROAD, EATON SOCON
-            "10093629694",  # 156A GREAT NORTH ROAD, EATON SOCON
-            "10093628232",  # 78 HARTFORD ROAD, HUNTINGDON
-            "10093626016",  # 1A ELM DRIVE, ST. IVES
-            "10090393780",  # 4A ST. AUDREY LANE, ST. IVES
-            "10090393781",  # 4C ST. AUDREY LANE, ST. IVES
-            "10090393783",  # 4 ST. AUDREY LANE, ST. IVES
-            "10090393782",  # 4B ST. AUDREY LANE, ST. IVES
-            "100091201367",  # THE HAWTHORNS, WOOD LANE, RAMSEY, HUNTINGDON
             "10091593436",  # 26D ALEXANDRA HOUSE HINCHINGBROOKE HOSPITAL HINCHINGBROOKE PARK ROAD, HUNTINGDON
-            "10012049676",  # NEW FARM, EARITH ROAD, COLNE, HUNTINGDON
         ]:
             return None
 
         if record.addressline6 in [
-            "PE26 1HA",
-            "PE27 5BZ",
+            "PE29 2NS",
             "PE19 1HW",
-            "PE26 2UB",
-            "PE28 2AS",
-            "PE29 2PL",
+            "PE27 6DT",
         ]:
             return None
 
@@ -41,22 +31,8 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
     def station_record_to_dict(self, record):
 
-        # Removing unwanted text from polling station name.
-        if "NEW POLLING STATION" in record.polling_place_name:
-            record = record._replace(
-                polling_place_name=record.polling_place_name.replace(
-                    "NEW POLLING STATION:", ""
-                )
-            )
-
         # Berkley Street Methodist Church, Berkley Street, Eynesbury, St. Neots, PE19 2HD
-        if record.polling_place_id == "7819":
+        if record.polling_place_id == "8397":
             record = record._replace(polling_place_postcode="PE19 2NB")
-
-        if record.polling_place_id in [
-            "7643"  # Ellington Village Hall, Ellington
-            "7620"  # Colne Community Hall, East Street, Colne
-        ]:
-            record = record._replace(polling_place_postcode="")
 
         return super().station_record_to_dict(record)
