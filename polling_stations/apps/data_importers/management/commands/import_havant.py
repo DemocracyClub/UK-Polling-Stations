fd011c8bd3eedd921e8fd745020577897d049557
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-10 13:42:21 +0000

    Import script for Havant (2022-05-05) (closes #4267)

--- polling_stations/apps/data_importers/management/commands/import_havant.py
+++ polling_stations/apps/data_importers/management/commands/import_havant.py
@@ -3,26 +3,23 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "HAA"
-    addresses_name = "2021-03-25T12:55:41.884654/Democracy_Club__06May2021.tsv"
-    stations_name = "2021-03-25T12:55:41.884654/Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
+    addresses_name = (
+        "2022-05-05/2022-03-10T13:42:05.933404/Democracy_Club__05May2022.tsv"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-10T13:42:05.933404/Democracy_Club__05May2022.tsv"
+    )
+    elections = ["2022-05-05"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "100062456253",  # 37 LONDON ROAD, COWPLAIN, WATERLOOVILLE
-        ]:
-            return None
 
         if record.addressline6 in [
             "PO9 2DT",
-            "PO9 3EZ",
             "PO8 8BB",
             "PO10 7NH",
-            "PO10 7HN",
             "PO8 9UB",
+            "PO11 9LA",
         ]:
             return None
 
