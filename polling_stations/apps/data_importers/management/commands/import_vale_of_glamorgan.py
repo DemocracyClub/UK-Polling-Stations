a2909e9a8a8d81247ad2f21751d52464c8712e28
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-11 14:01:54 +0000

    Import script for Vale of Glamorgan (2022-05-05) (closes #4296)

--- polling_stations/apps/data_importers/management/commands/import_vale_of_glamorgan.py
+++ polling_stations/apps/data_importers/management/commands/import_vale_of_glamorgan.py
@@ -1,16 +1,16 @@
-from data_importers.ems_importers import BaseDemocracyCountsCsvImporter
+from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 
 class Command(BaseDemocracyCountsCsvImporter):
-
     council_id = "VGL"
-    addresses_name = "2021-04-13T09:25:56.187046/Polling Districts.csv"
-    stations_name = "2021-04-13T09:25:56.187046/Polling Stations.csv"
-
-    elections = ["2021-05-06"]
+    addresses_name = "2022-05-05/2022-03-11T14:01:39.044454/democracy club polling districts vale of glamorgan.csv"
+    stations_name = "2022-05-05/2022-03-11T14:01:39.044454/democracy club polling stations vale of glamorgan.csv"
+    elections = ["2022-05-05"]
     csv_encoding = "windows-1252"
 
     def address_record_to_dict(self, record):
-        if record.postcode in ["CF62 6BA", "CF64 4TA", "CF71 7QR"]:
+        if record.postcode in [
+            "CF62 6BA",
+        ]:
             return None
         return super().address_record_to_dict(record)
