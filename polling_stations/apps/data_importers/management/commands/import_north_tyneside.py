71d14a69a47e6b8d34c8297b35e8f1449e1d2c50
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-11 10:11:01 +0000

    Import script for North Tyneside (2022-05-05) (closes #4290)

--- polling_stations/apps/data_importers/management/commands/import_north_tyneside.py
+++ polling_stations/apps/data_importers/management/commands/import_north_tyneside.py
@@ -4,24 +4,16 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "NTY"
     addresses_name = (
-        "2021-03-22T11:03:37.869995/North Tyneside Democracy_Club__06May2021.tsv"
+        "2022-05-05/2022-03-11T10:10:41.451729/Democracy_Club__05May2022.tsv"
     )
     stations_name = (
-        "2021-03-22T11:03:37.869995/North Tyneside Democracy_Club__06May2021.tsv"
+        "2022-05-05/2022-03-11T10:10:41.451729/Democracy_Club__05May2022.tsv"
     )
-    elections = ["2021-05-06"]
+    elections = ["2022-05-05"]
+    csv_encoding = "windows-1252"
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "47080850",  # FLAT ABOVE ALNWICK CASTLE SAVILLE STREET, NORTH SHIELDS
-            "47057152",  # LOW LIGHT HOUSE, FISH QUAY, NORTH SHIELDS
-            "47036550",  # FLAT AT 1 FRONT STREET, CHIRTON, NORTH SHIELDS
-        ]:
-            return None
-
         if record.addressline6 in ["NE12 8EE", "NE27 0XP"]:
             return None
 
