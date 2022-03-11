9f6d166fa8f6039814be7e26241aa1c0e1f48985
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-11 13:35:06 +0000

    Import script for Cherwell (2022-05-05) (closes #4292)

--- polling_stations/apps/data_importers/management/commands/import_cherwell.py
+++ polling_stations/apps/data_importers/management/commands/import_cherwell.py
@@ -3,115 +3,77 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "CHR"
-    addresses_name = (
-        "2021-04-16T12:59:18.049121/Re-submission - Democracy_Club__06May2021.tsv"
-    )
-    stations_name = (
-        "2021-04-16T12:59:18.049121/Re-submission - Democracy_Club__06May2021.tsv"
-    )
-    elections = ["2021-05-06"]
-    csv_delimiter = "\t"
+    addresses_name = "2022-05-05/2022-03-11T13:34:54.264817/Cherwell District Council - Democracy_Club__05May2022.tsv"
+    stations_name = "2022-05-05/2022-03-11T13:34:54.264817/Cherwell District Council - Democracy_Club__05May2022.tsv"
+    elections = ["2022-05-05"]
     csv_encoding = "windows-1252"
+    csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
         # Kidlington Youth Football Club
-        if record.polling_place_id == "23006":
+        if record.polling_place_id == "24875":
             record = record._replace(
                 polling_place_uprn="10011878924",
                 polling_place_easting="",
                 polling_place_northing="",
             )
 
-        # Following updates based on last year's corrections
-
-        # The Peoples Church, Horsefair, Banbury, Oxon
-        if record.polling_place_id == "22810":
-            record = record._replace(polling_place_easting="445258")
-            record = record._replace(polling_place_northing="240545")
-
         # West Bicester Community Centre, Bowmont Square, Bicester, Oxon
-        if record.polling_place_id == "22911":
+        if record.polling_place_id == "24693":
             record = record._replace(polling_place_easting="456935")
             record = record._replace(polling_place_northing="222867")
 
-        # Sibford Gower Village Hall
-        if record.polling_place_id == "23154":
-            record = record._replace(polling_place_easting="435614")
-            record = record._replace(polling_place_northing="237845")
-
-        # Drayton Village Hall
-        if record.polling_place_id == "23186":
-            record = record._replace(polling_place_easting="442908")
-            record = record._replace(polling_place_northing="241900")
-
         # St Mary the Virgin Church, Cottisford, Brackley, Northants
-        if record.polling_place_id == "23075":
+        if record.polling_place_id == "24828":
             record = record._replace(polling_place_easting="458727")
             record = record._replace(polling_place_northing="231062")
 
-        # The Church of St John the Baptist, Broadway, Kidlington, Oxon
-        if record.polling_place_id == "22985":
-            record = record._replace(polling_place_easting="449651")
-            record = record._replace(polling_place_northing="212578")
+        # Drayton Village Hall
+        if record.polling_place_id == "24717":
+            record = record._replace(polling_place_easting="442908")
+            record = record._replace(polling_place_northing="241900")
+
+        # Sibford Gower Village Hall
+        if record.polling_place_id == "24751":
+            record = record._replace(polling_place_easting="435614")
+            record = record._replace(polling_place_northing="237845")
 
         # Kidlington Baptist Church, High Street, Kidlington, Oxon
-        if record.polling_place_id == "23012":
+        if record.polling_place_id == "24895":
             record = record._replace(polling_place_easting="449488")
             record = record._replace(polling_place_northing="214376")
 
-        # Confirmed with Council
-        # Heyford Park Community Centre, Brice Road, Upper Heyford
-        if record.polling_place_id == "23115":
-            record = record._replace(polling_place_easting="451146")
-            record = record._replace(polling_place_northing="225741")
+        # The Church of St John the Baptist, Broadway, Kidlington, Oxon
+        if record.polling_place_id == "24883":
+            record = record._replace(polling_place_easting="449651")
+            record = record._replace(polling_place_northing="212578")
 
         # Pingle Field Pavilion, Pingle Field, Bicester
-        if record.polling_place_id == "22904":
+        if record.polling_place_id == "24685":
             record = record._replace(polling_place_postcode="OX26 6AU")
 
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         if record.addressline6 in [
-            "OX16 9JU",
-            "OX16 9QF",
             "OX16 5AW",
-            "OX16 2BN",
-            "OX16 2AS",
+            "OX16 9QF",
+            "OX25 5BH",
             "OX26 3EB",
             "OX26 3EZ",
-            "OX26 6BP",
-            "OX5 3DJ",
-            "OX25 3QU",
-            "OX5 1LZ",
-            "OX5 1AJ",
+            "OX26 6HB",
             "OX27 7AE",
+            "OX5 1LZ",
         ]:
-            return None  # split
+            return None
 
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            # Group 1
-            "10011909033",
-            "10011917099",
-            # Group 2
-            "10011924957",
-            "10011924949",
-            "10011924948",
-            "10011924947",
-            "10011924946",
-            # Group 3
-            "10011927441",
-            "10011927442",
-            "10011927443",
-            "10011927445",
-            "10011927446",
-            "10011927448",
-            "10011931230",
-            # Group 4
             "10011889761",
+            "100121291839",
+            "10011906163",
         ]:
-            return None  # long distance; crosses another polling district
+            return None
 
         return super().address_record_to_dict(record)
