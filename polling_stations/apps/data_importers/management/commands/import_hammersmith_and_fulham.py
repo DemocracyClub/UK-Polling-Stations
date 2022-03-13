ce63deafcd19acd63b086d315e724f7cf44c6e92
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-13 18:02:02 +0000

    Import script for Hammersmith and Fulham (2022-05-05) (closes #4309)

--- polling_stations/apps/data_importers/management/commands/import_hammersmith_and_fulham.py
+++ polling_stations/apps/data_importers/management/commands/import_hammersmith_and_fulham.py
@@ -3,32 +3,24 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "HMF"
-    addresses_name = "2021-04-12T09:04:13.301934/polling_station_export-2021-04-11.csv"
-    stations_name = "2021-04-12T09:04:13.301934/polling_station_export-2021-04-11.csv"
-    elections = ["2021-05-06"]
-    csv_delimiter = ","
+    addresses_name = (
+        "2022-05-05/2022-03-13T18:01:43.704952/polling_station_export-2022-03-13.csv"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-13T18:01:43.704952/polling_station_export-2022-03-13.csv"
+    )
+    elections = ["2022-05-05"]
 
     def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
-
-        if uprn in [
-            "34150747",  # FLAT 1, 62A RYLETT ROAD, LONDON
-            "34150748",  # FLAT 2, 62A RYLETT ROAD, LONDON
-            "34152305",  # 2A NITON STREET, LONDON
-            "34155328",  # 29C MUNSTER ROAD, LONDON
-            "34136009",  # UPPER FLOOR INNER LODGE ST MARY'S CEMETERY HARROW ROAD, WEMBLEY
-            "34135797",  # GROUND FLOOR INNER LODGE ST MARY'S CEMETERY HARROW ROAD, WEMBLEY
-            "34146774",  # THIRD FLOOR FLAT 95 HAMMERSMITH GROVE, LONDON
-        ]:
-            return None
 
         if record.housepostcode in [
             "W14 9DS",
-            "W6 9HJ",
-            "SW6 2EF",
-            "SW6 2GG",
-            "SW6 2GN",
+            "SW6 3QP",
+            "W6 7RP",
+            "SW6 7PT",
+            "SW6 6BU",
             "W14 8UZ",
+            "W6 0NQ",
         ]:
             return None
 
