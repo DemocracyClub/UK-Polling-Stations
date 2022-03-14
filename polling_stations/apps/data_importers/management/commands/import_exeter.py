7281a3df1da59add0c27192197a26c23375076d6
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-14 16:19:10 +0000

    Import script for Exeter (2022-05-05) (closes #4315)

--- polling_stations/apps/data_importers/management/commands/import_exeter.py
+++ polling_stations/apps/data_importers/management/commands/import_exeter.py
@@ -3,29 +3,28 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "EXE"
-    addresses_name = "2021-03-23T11:45:52.642475/Exeter Democracy_Club__06May2021.CSV"
-    stations_name = "2021-03-23T11:45:52.642475/Exeter Democracy_Club__06May2021.CSV"
-    elections = ["2021-05-06"]
-    csv_delimiter = ","
+    addresses_name = (
+        "2022-05-05/2022-03-14T16:19:01.344084/Democracy_Club__05May2022.tsv"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-14T16:19:01.344084/Democracy_Club__05May2022.tsv"
+    )
+    elections = ["2022-05-05"]
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
             "10091473295",  # EXE VIEW LODGE STOKE HILL, EXETER
-            "100041124933",  # THE GRANGE, HARTS LANE, EXETER
         ]:
             return None
 
         if record.addressline6 in [
-            "EX4 5AJ",
             "EX2 7AY",
-            "EX2 7TF",
-            "EX4 1RD",
             "EX4 9HE",
-            "EX4 1AS",
-            "EX2 8DQ",
-            "EX1 3FA",
+            "EX2 7TF",
+            "EX4 5AJ",
         ]:
             return None
 
