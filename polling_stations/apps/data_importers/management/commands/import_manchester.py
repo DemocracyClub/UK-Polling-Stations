43d623d40f8a8549004c7180b4d8993e0d0ef081
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-16 17:36:29 +0000

    Import script for Manchester (2022-05-05) (closes #4344)

--- polling_stations/apps/data_importers/management/commands/import_manchester.py
+++ polling_stations/apps/data_importers/management/commands/import_manchester.py
@@ -3,47 +3,42 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "MAN"
-    addresses_name = "2021-03-29T13:39:43.070287/manchester_deduped.tsv"
-    stations_name = "2021-03-29T13:39:43.070287/manchester_deduped.tsv"
-    elections = ["2021-05-06"]
-    csv_delimiter = "\t"
+    addresses_name = (
+        "2022-05-05/2022-03-16T17:36:14.196465/Democracy_Club__05May2022.tsv"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-16T17:36:14.196465/Democracy_Club__05May2022.tsv"
+    )
+    elections = ["2022-05-05"]
     csv_encoding = "windows-1252"
+    csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
 
         # Moston Methodist Church corner of Ilkley Street and Moston Lane Moston Manchester
-        if record.polling_place_id == "9002":
+        if record.polling_place_id == "9372":
             record = record._replace(
                 polling_place_easting="387244", polling_place_northing="401944"
             )
 
-        # Holy Trinity Church Hall Goodman Street Blackley Manchester M9 4BW
-        if record.polling_place_id == "8840":
+        # Holy Trinity Church Hall Goodman Street Blackley Manchester M9 4BW (386441,402233)
+        if record.polling_place_id == "9332":
             record = record._replace(polling_place_easting="")
             record = record._replace(polling_place_northing="")
+
+        if record.addressline1 in ["NEW POLLING STATION", "CHANGE OF STATION"]:
+            record = record._replace(addressline1="")
 
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "77123789",  # 2 ALMOND STREET, MANCHESTER
-            "77123796",  # 2 DAVY STREET, MANCHESTER
-        ]:
-            return None
 
         if record.addressline6 in [
             "M8 9AE",
             "M1 2PE",
             "M12 5PR",
-            "M20 1LU",
+            "M8 4RB",
             "M22 8BE",
-            "M15 4TN",
-            "M15 4TP",
-            "M15 4TW",
-            "M15 4TT",
-            "M15 4TU",
         ]:
             return None
 
