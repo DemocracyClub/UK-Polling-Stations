6b964c340f2ab46893ff408bd4c8ff32a0bb6b6f
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-16 12:09:38 +0000

    Import script for Gateshead (2022-05-05) (closes #4332)

--- polling_stations/apps/data_importers/management/commands/import_gateshead.py
+++ polling_stations/apps/data_importers/management/commands/import_gateshead.py
@@ -3,25 +3,20 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "GAT"
-    addresses_name = "2021-04-26T17:25:33.216720/Democracy_Club__06May2021.tsv"
-    stations_name = "2021-04-26T17:25:33.216720/Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
-    csv_delimiter = "\t"
+    addresses_name = (
+        "2022-05-05/2022-03-16T12:09:15.983600/Democracy_Club__05May2022.CSV"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-16T12:09:15.983600/Democracy_Club__05May2022.CSV"
+    )
+    elections = ["2022-05-05"]
 
     def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
 
-        if uprn in [
-            "10022984423",  # HIGH EIGHTON FARM HOUSE BLACK LANE, HARLOW GREEN, GATESHEAD
-            "10024188004",  # 168 SHERIFFS HIGHWAY, GATESHEAD
-            "100000029999",  # 125 ELLISON ROAD, GATESHEAD
-            "10093488596",  # BULLERWELL HOUSE ASHTREE LANE, HIGH SPEN, ROWLANDS GILL
-            "10070837301",  # 1A SPLIT CROW ROAD, GATESHEAD
-            "10070836935",  # RESIDENTIAL ACCOMMODATION COACH AND HORSES INN DURHAM ROAD, BIRTLEY
+        if record.addressline6 in [
+            "NE9 5XP",
+            "NE9 6JR",
         ]:
-            return None
-
-        if record.addressline6 in ["NE9 5XP", "NE9 6JR", "NE10 9RZ"]:
             return None
 
         return super().address_record_to_dict(record)
