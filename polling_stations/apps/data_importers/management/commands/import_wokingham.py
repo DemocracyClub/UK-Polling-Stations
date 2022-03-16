b11e2bdb7b1832e9aea9fad87290eddfc71c0ca5
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-16 15:20:45 +0000

    Import script for Wokingham (2022-05-05) (closes #4342)

--- polling_stations/apps/data_importers/management/commands/import_wokingham.py
+++ polling_stations/apps/data_importers/management/commands/import_wokingham.py
@@ -3,30 +3,29 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WOK"
-    addresses_name = "2021-04-12T09:10:21.537410/Democracy_Club__06May2021.tsv"
-    stations_name = "2021-04-12T09:10:21.537410/Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
-    csv_delimiter = "\t"
+    addresses_name = (
+        "2022-05-05/2022-03-16T15:20:21.336512/Democracy_Club__05May2022.tsv"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-16T15:20:21.336512/Democracy_Club__05May2022.tsv"
+    )
+    elections = ["2022-05-05"]
     csv_encoding = "windows-1252"
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
             "14008163",  # LOWER COTTAGE, WHITE HORSE LANE, FINCHAMPSTEAD, WOKINGHAM
-            "10094223552",  # 4 SHEERLANDS ROAD, ARBORFIELD
-            "14006244",  # FIELD VIEW, MOLE ROAD, SINDLESHAM, WOKINGHAM
-            "14063909",  # MAYS HILL LODGE, BEECH HILL ROAD, SPENCERS WOOD, READING
-            "10032929468",  # LANE END FARM HOUSE, SHINFIELD ROAD, SHINFIELD, READING
         ]:
             return None
 
         if record.addressline6 in [
-            "RG2 9LG",
             "RG6 4AG",
             "RG7 1NL",
-            "RG41 4EL",
             "RG10 9HN",
+            "RG7 1TB",
         ]:
             return None
 
@@ -35,7 +34,7 @@ class Command(BaseXpressDemocracyClubCsvImporter):
     def station_record_to_dict(self, record):
 
         # Civic Offices Shute End Wokingham Berkshire RG40 1WH
-        if record.polling_place_id == "2740":
+        if record.polling_place_id == "2891":
             record = record._replace(polling_place_postcode="")
 
         return super().station_record_to_dict(record)
