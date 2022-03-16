3dedfabc76ec4633dfcd2a05bab6b01611a51012
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-16 15:16:43 +0000

    Import script for Hillingdon (2022-05-05) (closes #4340)

--- polling_stations/apps/data_importers/management/commands/import_hillingdon.py
+++ polling_stations/apps/data_importers/management/commands/import_hillingdon.py
@@ -3,15 +3,19 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "HIL"
-    addresses_name = "2021-03-25T09:17:18.067946/Democracy_Club__06May2021.CSV"
-    stations_name = "2021-03-25T09:17:18.067946/Democracy_Club__06May2021.CSV"
-    elections = ["2021-05-06"]
-    csv_delimiter = ","
+    addresses_name = (
+        "2022-05-05/2022-03-16T15:16:10.701979/Democracy_Club__05May2022.tsv"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-16T15:16:10.701979/Democracy_Club__05May2022.tsv"
+    )
+    elections = ["2022-05-05"]
+    csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
 
         # Walter G Pomeroy Hall, Royal Lane, Hillingdon
-        if record.polling_place_id == "9559":
+        if record.polling_place_id == "11594":
             record = record._replace(polling_place_easting="506536")
             record = record._replace(polling_place_northing="181610")
 
@@ -21,33 +25,23 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "100021469325",  # 1 ELM VIEW HOUSE, SHEPISTON LANE, HAYES
-            "100023417375",  # WESTWAY FARM, CHARVILLE LANE, HAYES
-            "100022832219",  # 241 BALMORAL DRIVE, HAYES
-            "100021415478",  # 48A THE FAIRWAY, RUISLIP
-            "10022802358",  # MANAGERS FLAT BAITUL AMAN MOSQUE ROYAL LANE, UXBRIDGE
-            "100021461989",  # MANAGERS FLAT THE GEORGE HARVESTER BURY STREET, RUISLIP
+            "100022832219",
+            "100021461989",
         ]:
             return None
 
         if record.addressline6 in [
-            "UB10 0LF",
-            "UB7 9LW",
-            "UB3 3PF",
-            "UB10 0LE",
-            "UB10 8LF",
-            "UB8 2YF",
-            "UB9 6NA",
-            "UB8 2TZ",
-            "TW6 2AL",
-            "UB8 3DH",
-            "UB8 1GW",
-            "UB9 6QS",
-            "UB3 2FH",
-            "UB8 3QD",
-            "UB10 0QB",
+            "UB8 3DG",
             "HA4 0SE",
-            "HA6 1RL",
+            "UB10 0QB",
+            "UB3 2FH",
+            "UB3 3FN",
+            "UB3 3PF",
+            "UB7 9GA",
+            "UB8 3DH",
+            "UB8 3FE",
+            "UB8 3QD",
+            "UB8 3QT",
         ]:
             return None
 
