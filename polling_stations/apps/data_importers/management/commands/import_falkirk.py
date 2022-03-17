a55d3ad7e0f0ea0be469ac98894405515a080221
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-17 11:46:13 +0000

    Import script for Falkirk (2022-05-05) (closes #4351)

--- polling_stations/apps/data_importers/management/commands/import_falkirk.py
+++ polling_stations/apps/data_importers/management/commands/import_falkirk.py
@@ -1,62 +1,48 @@
-from data_importers.ems_importers import BaseHalaroseCsvImporter
-
-FAL_ADMINAREAS = (
-    "Bo'ness",
-    "Bonnybridge",
-    "Denny",
-    "Falkirk Ward 1",
-    "Falkirk Ward 2",
-    "Falkirk Ward 3",
-    "Falkirk Ward 4",
-    "Falkirk Ward 5",
-    "Falkirk Ward 6",
-    "Falkirk Ward 7",
-    "Falkirk Ward 8",
-    "Falkirk Ward 9",
-    "Falkirk",
-    "Grangemouth",
-    "Larbert",
-)
-FAL_INCLUDE_STATIONS = [
-    "Maddiston Old Folks Hall",
-    "Deanburn Primary School",
-    "Blackness Community Hall",
-]
-FAL_EXCLUDE_STATIONS = [
-    "Cowie Community Centre",
-    "Balfour Centre",
-    "Bannockburn Community Centre",
-]
+from addressbase.models import UprnToCouncil
+from data_importers.management.commands import BaseHalaroseCsvImporter
 
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "FAL"
-    addresses_name = "2021-04-01T20:39:48.865108/Central Scotland polling_station_export-2021-03-31.csv"
-    stations_name = "2021-04-01T20:39:48.865108/Central Scotland polling_station_export-2021-03-31.csv"
-    elections = ["2021-05-06"]
+    addresses_name = (
+        "2022-05-05/2022-03-17T11:45:51.431104/polling_station_export-2022-03-15.csv"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-17T11:45:51.431104/polling_station_export-2022-03-15.csv"
+    )
+    elections = ["2022-05-05"]
 
-    def station_record_to_dict(self, record):
-        if (
-            (record.adminarea not in FAL_ADMINAREAS)
-            and (record.pollingstationname not in FAL_INCLUDE_STATIONS)
-            or (record.pollingstationname in FAL_EXCLUDE_STATIONS)
-        ):
-            return None
-        return super().station_record_to_dict(record)
+    def pre_import(self):
+        # We need to consider rows that don't have a uprn when importing data.
+        # However there are lots of rows for other councils in this file.
+        # So build a list of stations from rows that do have UPRNS
+        # and then use that list of stations to make sure we check relevant rows, even if they don't have a UPRN
+
+        council_uprns = set(
+            UprnToCouncil.objects.filter(lad=self.council.geography.gss).values_list(
+                "uprn", flat=True
+            )
+        )
+        self.COUNCIL_STATIONS = set()
+        data = self.get_addresses()
+
+        for record in data:
+            if record.uprn in council_uprns:
+                self.COUNCIL_STATIONS.add(self.get_station_hash(record))
 
     def address_record_to_dict(self, record):
-        if (
-            (record.adminarea not in FAL_ADMINAREAS)
-            and (record.pollingstationname not in FAL_INCLUDE_STATIONS)
-            or (record.pollingstationname in FAL_EXCLUDE_STATIONS)
-        ):
+        if self.get_station_hash(record) not in self.COUNCIL_STATIONS:
             return None
-
         if record.housepostcode in [
-            "FK2 7FG",
             "FK6 5EP",
+            "FK2 7FG",
             "FK2 7YN",
         ]:
             return None
-
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        if self.get_station_hash(record) not in self.COUNCIL_STATIONS:
+            return None
+
+        return super().station_record_to_dict(record)
