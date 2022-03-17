7c10f4af3e9f178bd006064c74e48926456716cd
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-17 09:27:30 +0000

    Import script for West Oxfordshire (2022-05-05) (closes #4347)

--- polling_stations/apps/data_importers/management/commands/import_west_oxfordshire.py
+++ polling_stations/apps/data_importers/management/commands/import_west_oxfordshire.py
@@ -3,14 +3,19 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WOX"
-    addresses_name = "2021-03-15T11:16:34.226362/WODC - EC & Democracy Club Polling Place Look Up for 6 May 2021 (Rev).csv"
-    stations_name = "2021-03-15T11:16:34.226362/WODC - EC & Democracy Club Polling Place Look Up for 6 May 2021 (Rev).csv"
-    elections = ["2021-05-06"]
+    addresses_name = (
+        "2022-05-05/2022-03-17T09:27:17.244620/Democracy Club Export for 220505.tsv"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-17T09:27:17.244620/Democracy Club Export for 220505.tsv"
+    )
+    elections = ["2022-05-05"]
+    csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
 
         # Brize Norton Elder Bank Hall
-        if record.polling_place_id == "7318":
+        if record.polling_place_id == "7807":
             record = record._replace(polling_place_postcode="OX18 3PU")
             record = record._replace(polling_place_easting="430059")
             record = record._replace(polling_place_northing="207340")
@@ -18,10 +23,7 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-        if record.addressline6 in ["OX28 6DH"]:
+        if record.addressline6 in ["OX20 1RZ", "OX18 3HW", "OX18 3NU"]:
             return None  # split
-
-        if record.addressline6 in ["GL56 0SU"]:
-            return None  # spurious
 
         return super().address_record_to_dict(record)
