4a7e9ebc7fe66e1bf71aa7af65b9b73682e1c0ba
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-17 15:55:02 +0000

    Import script for Tandridge (2022-05-05) (closes #4357)

--- polling_stations/apps/data_importers/management/commands/import_tandridge.py
+++ polling_stations/apps/data_importers/management/commands/import_tandridge.py
@@ -3,33 +3,27 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "TAN"
-    addresses_name = "2021-03-16T13:46:43.742650/polling_station_export-2021-03-16.csv"
-    stations_name = "2021-03-16T13:46:43.742650/polling_station_export-2021-03-16.csv"
-    elections = ["2021-05-06"]
-    csv_delimiter = ","
+    addresses_name = (
+        "2022-05-05/2022-03-17T15:54:54.749645/polling_station_export-2022-03-17.csv"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-17T15:54:54.749645/polling_station_export-2022-03-17.csv"
+    )
+    elections = ["2022-05-05"]
+
+    # For some reason the export only has these fields for station address
+    station_address_fields = [
+        "pollingstationname",
+        "pollingstationaddress_1",
+        "pollingstationaddress_2",
+        "pollingstationaddress_3",
+    ]
 
     def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
-
-        if uprn in [
-            "100062498517",  # SWEET BRIAR BARN, NEWCHAPEL ROAD, LINGFIELD
-            "100062499148",  # SCHOOL HOUSE, TANDRIDGE LANE, TANDRIDGE, OXTED
-            "200000141065",  # OLD MOAT BARN, ARDENRUN, LINGFIELD
-            "200000146196",  # DERHILL, STATION ROAD, WOLDINGHAM, CATERHAM
+        if record.housepostcode in [
+            "CR3 6HG",
+            "RH7 6JH",
         ]:
             return None
 
-        if record.housepostcode in ["CR3 6HG", "RH7 6JH", "CR6 9LB", "TN16 2EX"]:
-            return None
-
         return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # St. Peter's Hall High Street Limpsfield Oxted RH8 ODG
-        if (record.pollingstationnumber, record.pollingstationpostcode) == (
-            "32",
-            "RH8 ODG",
-        ):
-            record = record._replace(pollingstationpostcode="RH8 0DG")
-
-        return super().station_record_to_dict(record)
