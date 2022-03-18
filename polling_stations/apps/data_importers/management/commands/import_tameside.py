05f11337692babe2dbe029edf3d5341d0268d5da
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-18 11:53:19 +0000

    Import script for Tameside (2022-05-05) (closes #4362)
    Co-authored-by: Alex Dutton <democracyclub@alexdutton.co.uk>

--- polling_stations/apps/data_importers/management/commands/import_tameside.py
+++ polling_stations/apps/data_importers/management/commands/import_tameside.py
@@ -3,59 +3,49 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "TAM"
-    addresses_name = "2021-03-19T15:38:04.940947/Democracy_Club__06May2021.tsv"
-    stations_name = "2021-03-19T15:38:04.940947/Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
+    addresses_name = (
+        "2022-05-05/2022-03-25T15:02:16.351806/Democracy_Club__05May2022.tsv"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-25T15:02:16.351806/Democracy_Club__05May2022.tsv"
+    )
+    elections = ["2022-05-05"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "10003436095",  # ASH TREE FARM, HOBSON MOOR ROAD, MOTTRAM, HYDE
+        if record.property_urn.lstrip("0") in [
+            # single property too far
+            "200004405547",
+            # another group too far
+            "10090073761",
+            "10090073763",
+            "10090073764",
+            "100012777279",
+            "200002032772",
+            # embedded in another area
+            "200004400819",
             "200004419358",  # FLAT 2 34 HUDSON ROAD, HYDE
-            "10090077959",  # FLAT 3, 14 VICTORIA STREET, DENTON, MANCHESTER
-            "10090077958",  # FLAT 2, 14 VICTORIA STREET, DENTON, MANCHESTER
-            "10090077957",  # FLAT 1, 14 VICTORIA STREET, DENTON, MANCHESTER
-            "10090078212",  # COTTAGE REAR OF 49 FAIRFIELD SQUARE, DROYLSDEN
-            "100011591969",  # 39 FAIRFIELD SQUARE, DROYLSDEN, MANCHESTER
-            "100011584362",  # 1 ARRAS GROVE, DENTON, MANCHESTER
-            "100011584364",  # 3 ARRAS GROVE, DENTON, MANCHESTER
-            "100011584363",  # 2 ARRAS GROVE, DENTON, MANCHESTER
-            "100011584365",  # 4 ARRAS GROVE, DENTON, MANCHESTER
-            "10003439283",  # INDIAN PLAZA, 90 AUDENSHAW ROAD, AUDENSHAW, MANCHESTER
-            "100012484582",  # THE BOUNDARY, 2 AUDENSHAW ROAD, AUDENSHAW, MANCHESTER
-            "100012488602",  # THE VALLEY VIEW, HUDDERSFIELD ROAD, MILLBROOK, STALYBRIDGE
             "200004406539",  # FLAT 2 BANK MILL MANCHESTER ROAD, MOSSLEY
-            "10090073761",  # 19A WELLINGTON STREET, ASHTON-UNDER-LYNE
-            "10090073763",  # 21A WELLINGTON STREET, ASHTON-UNDER-LYNE
-            "10090073764",  # 21B WELLINGTON STREET, ASHTON-UNDER-LYNE
-            "200002857633",  # PORTLAND BASIN MARINA, LOWER ALMA STREET, DUKINFIELD
-            "10093148018",  # 5 MEADOW VIEW GARDENS, DROYLSDEN
-            "10090075504",  # 5 EDMUND STREET, DROYLSDEN
-            "10090075502",  # 2 EDMUND STREET, DROYLSDEN, MANCHESTER
-            "10090075503",  # 4 EDMUND STREET, DROYLSDEN, MANCHESTER
-            "200004409037",  # FLAT 4 62 STAMFORD STREET EAST, ASHTON-UNDER-LYNE
-            "200004419592",  # FLAT 6 62 STAMFORD STREET EAST, ASHTON-UNDER-LYNE
-            "10014258699",  # 193 MOSSLEY ROAD, ASHTON-UNDER-LYNE
-            "10014258700",  # 195 MOSSLEY ROAD, ASHTON-UNDER-LYNE
-            "100012777167",  # 36 LORD STREET, STALYBRIDGE
-            "100011576995",  # 3 MOTTRAM MOOR, HOLLINGWORTH, HYDE
-            "10003441482",  # 78A STOCKPORT ROAD, ASHTON-UNDER-LYNE
-            "10014255906",  # THE HOLLIES BUNGALOW, BOYDS WALK, DUKINFIELD
+            # across another area
+            "10093148014",  # 1 MEADOW VIEW GARDENS, DROYLSDEN, MANCHESTER
+            "10093148018",  # 5 MEADOW VIEW GARDENS, DROYLSDEN, MANCHESTER
         ]:
             return None
 
         if record.addressline6 in [
             "OL6 9LS",
-            "M43 7ZD",
+            "SK15 1HF",
             "M34 7RZ",
-            "SK14 3EB",
-            "M34 5US",
-            "OL6 7FU",
-            "OL7 9NZ",
-            "M34 5SH",
-            "OL5 9PN",
+            "M43 7ZD",
+        ]:
+            return None  # split
+
+        if record.addressline6 in [
+            "SK14 3GD",  # embedded in another area. postcode centroid a long way from these residential properties
+            "SK14 3EB",  # embedded in another area
+            "SK14 3GU",  # dubious geolocation; too far; through another area
+            "M34 2WS",  # embedded in another area
+            "SK14 2BN",  # embedded in another area
         ]:
             return None
 
