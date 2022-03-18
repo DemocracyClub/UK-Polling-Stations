e2483f9dc91456031586bd0b29702615103fbc5e
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-18 12:10:57 +0000

    Import script for Watford (2022-05-05) (closes #4364)

--- polling_stations/apps/data_importers/management/commands/import_watford.py
+++ polling_stations/apps/data_importers/management/commands/import_watford.py
@@ -3,24 +3,19 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "WAT"
-    addresses_name = (
-        "2021-04-19T09:17:40.372170/watford polling_station_export-2021-04-18.csv"
-    )
-    stations_name = (
-        "2021-04-19T09:17:40.372170/watford polling_station_export-2021-04-18.csv"
-    )
-    elections = ["2021-05-06"]
-    csv_delimiter = ","
+    addresses_name = "2022-05-05/2022-03-28T12:27:39.290212/Eros_SQL_Output028.csv"
+    stations_name = "2022-05-05/2022-03-28T12:27:39.290212/Eros_SQL_Output028.csv"
+    elections = ["2022-05-05"]
 
     def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
+        # All UPRNs have a leading "'"
+        uprn = record.uprn.replace("'", "").strip()
+        record = record._replace(uprn=uprn)
 
-        if uprn in [
-            "10023338176",  # 49 WILMINGTON CLOSE, WATFORD
+        if record.housepostcode in [
+            "WD25 9AS",
+            "WD25 7DA",
+            "WD18 7BS",
         ]:
             return None
-
-        if record.housepostcode in ["WD24 5DB", "WD17 4JT", "WD18 7BS"]:
-            return None
-
         return super().address_record_to_dict(record)
