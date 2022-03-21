f285dbedf6f035daf0a6d48cfcfe3998eb4b7e89
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-21 14:44:49 +0000

    Import script for Greenwich (2022-05-05) (closes #4382)

--- polling_stations/apps/data_importers/management/commands/import_greenwich.py
+++ polling_stations/apps/data_importers/management/commands/import_greenwich.py
@@ -1,31 +1,27 @@
-from data_importers.management.commands import BaseHalaroseCsvImporter
+from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
-class Command(BaseHalaroseCsvImporter):
+class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "GRE"
-    addresses_name = "2021-04-12T08:58:49.743842/polling_station_export-2021-04-11.csv"
-    stations_name = "2021-04-12T08:58:49.743842/polling_station_export-2021-04-11.csv"
-    elections = ["2021-05-06"]
-    csv_delimiter = ","
-    csv_encoding = "windows-1252"
+    addresses_name = (
+        "2022-05-05/2022-03-21T14:44:29.868800/Democracy_Club__05May2022.tsv"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-21T14:44:29.868800/Democracy_Club__05May2022.tsv"
+    )
+    elections = ["2022-05-05"]
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
-
+        uprn = record.property_urn.strip().lstrip("0")
         if uprn in [
-            "10010199305",  # 4-5 HILLREACH, LONDON
-            "10010240758",  # 5 OGILBY STREET, LONDON
-            "10010240756",  # 1 OGILBY STREET, LONDON
-            "10010240757",  # 3 OGILBY STREET, LONDON
-            "100020974501",  # 102A HERBERT ROAD, LONDON
-            "100020980261",  # 67 KINGSLEY WOOD DRIVE, LONDON
-            "100020989060",  # 209 MOORDOWN, LONDON
             "10010227617",  # 292B PLUMSTEAD COMMON ROAD, PLUMSTEAD
-            "100020940599",  # 132 BEXLEY ROAD, LONDON
         ]:
             return None
 
-        if record.housepostcode in ["SE9 6SQ", "SE9 2BU", "SE10 0PR", "SE10 8GS"]:
+        if record.addressline6 in [
+            "SE9 2BU",
+        ]:
             return None
 
         return super().address_record_to_dict(record)
