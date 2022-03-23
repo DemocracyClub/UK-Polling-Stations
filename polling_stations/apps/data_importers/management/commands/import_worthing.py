3bfb261406e4d87385443e61a2ff67aa65bad9b8
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-23 16:15:31 +0000

    Import script for Worthing (2022-05-05) (closes #4421)

--- polling_stations/apps/data_importers/management/commands/import_worthing.py
+++ polling_stations/apps/data_importers/management/commands/import_worthing.py
@@ -3,21 +3,11 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WOT"
-    addresses_name = "2021-02-15T11:21:49.443491/WBCDemocracy_Club__06May2021.tsv"
-    stations_name = "2021-02-15T11:21:49.443491/WBCDemocracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
+    addresses_name = (
+        "2022-05-05/2022-03-23T16:15:23.604159/Democracy_Club__05May2022WBC.tsv"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-23T16:15:23.604159/Democracy_Club__05May2022WBC.tsv"
+    )
+    elections = ["2022-05-05"]
     csv_delimiter = "\t"
-
-    def station_record_to_dict(self, record):
-
-        # Moved to correct address => Richmond Room (adj. to Assembly Hall), Stoke Abbott Road, Worthing
-        if record.polling_place_id == "2171":
-            record = record._replace(polling_place_easting="514729")
-            record = record._replace(polling_place_northing="102975")
-
-        return super().station_record_to_dict(record)
-
-    def address_record_to_dict(self, record):
-        if record.addressline6 == "BN11 3FP":
-            return None
-        return super().address_record_to_dict(record)
