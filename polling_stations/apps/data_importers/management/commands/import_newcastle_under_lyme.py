530ae9ea7a666030a1026e2ff8cbc41b2aaffab2
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-23 13:32:34 +0000

    Import script for Newcastle Under Lyme (2022-05-05) (closes #4407)

--- polling_stations/apps/data_importers/management/commands/import_newcastle_under_lyme.py
+++ polling_stations/apps/data_importers/management/commands/import_newcastle_under_lyme.py
@@ -4,20 +4,24 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "NEC"
     addresses_name = (
-        "2021-04-16T12:45:06.761928/Newcastle under lyme Democracy_Club__06May2021.tsv"
+        "2022-05-05/2022-03-23T13:32:11.842979/Democracy_Club__05May2022.tsv"
     )
     stations_name = (
-        "2021-04-16T12:45:06.761928/Newcastle under lyme Democracy_Club__06May2021.tsv"
+        "2022-05-05/2022-03-23T13:32:11.842979/Democracy_Club__05May2022.tsv"
     )
-    elections = ["2021-05-06"]
+    elections = ["2022-05-05"]
     csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
 
         # Higherland Methodist Church Hall Higherland Newcastle Staffs ST5 2TF
-        if record.polling_place_id == "1927":
-            record = record._replace(polling_place_easting="")
-            record = record._replace(polling_place_northing="")
+        if record.polling_place_id == "2633":
+            record = record._replace(polling_place_easting="384574")
+            record = record._replace(polling_place_northing="345703")
+
+        # Salvation Army, Heathcote Street, Kidsgrove, Stoke on Trent
+        if record.polling_place_id == "2752":
+            record = record._replace(polling_place_postcode="ST7 4AA")
 
         return super().station_record_to_dict(record)
 
@@ -28,34 +32,20 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "100032219337",  # ARDENNE THE AVENUE, KIDSGROVE
             "10024255961",  # 4B WARWICK CLOSE, KIDSGROVE, STOKE-ON-TRENT
             "10024255960",  # 4A WARWICK CLOSE, KIDSGROVE, STOKE-ON-TRENT
-            "200004607891",  # STORE 3 LAWSON TERRACE, PORTHILL, NEWCASTLE UNDER LYME
-            "10002238202",  # 3 LAWSON TERRACE, NEWCASTLE
             "200002872601",  # 125A LIVERPOOL ROAD, NEWCASTLE
-            "100031730123",  # 79 KNUTTON LANE, NEWCASTLE UNDER LYME
-            "100031730122",  # 77 KNUTTON LANE, NEWCASTLE UNDER LYME
-            "200001160997",  # SUITE 2 HANOVER COURT HANOVER STREET, NEWCASTLE UNDER LYME
-            "200004616065",  # 2 EARDLEYEND ROAD, NEWCASTLE UNDER LYME
-            "200004617440",  # 93-94 LEYCETT LANE, LEYCETT, NEWCASTLE UNDER LYME
-            "200004602621",  # AUSTIN LEYCETT LANE, SILVERDALE, NEWCASTLE UNDER LYME
-            "10002237282",  # WILLOUGHBRIDGE WELLS LODGE, WILLOUGHBRIDGE, MARKET DRAYTON
+            "100032216255",  # CROSS HEATH NEWS, 121 LIVERPOOL ROAD, NEWCASTLE
             "200004612280",  # CARAVAN RED STREET STABLES TALKE ROAD, BRADWELL, NEWCASTLE UNDER LYME
+            "10024252556",  # BRIERYHURST FARM HOUSE, MOW LANE, MOW COP, STOKE-ON-TRENT
+            "10002238961",  # 2 BRIERYHURST FARM BUNGALOW THE HOLLOW, MOW COP
         ]:
             return None
 
         if record.addressline6 in [
-            "ST7 1DY",
-            "ST5 8DS",
-            "ST5 3ES",
-            "ST5 7ES",
             "ST5 2TP",
-            "ST5 2HN",
-            "ST5 2PU",
-            "ST5 1AU",
             "ST5 4DU",
-            "TF9 4EY",
-            "ST7 1LH",
-            "ST5 1HN",
-            "ST5 6AL",
+            "ST5 6JY",
+            "ST5 8DS",
+            "ST7 1DY",
         ]:
             return None
 
