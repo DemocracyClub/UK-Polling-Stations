9d55d8d724d6a21b2cbf215a3ddffc5d36d1faaa
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-23 17:31:49 +0000

    Import script for Hackney (2022-05-05) (closes #4431)

--- polling_stations/apps/data_importers/management/commands/import_hackney.py
+++ polling_stations/apps/data_importers/management/commands/import_hackney.py
@@ -3,52 +3,39 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "HCK"
-    addresses_name = (
-        "2021-04-14T17:18:14.166273/Democracy_club_with_coords_06May2021.csv"
-    )
-    stations_name = (
-        "2021-04-14T17:18:14.166273/Democracy_club_with_coords_06May2021.csv"
-    )
-    elections = ["2021-05-06"]
-    csv_encoding = "utf-8-sig"
+    addresses_name = "2022-05-05/2022-03-23T17:30:06.543100/Democracy_Club__05May2022-HackneyCouncilPollingStations.CSV"
+    stations_name = "2022-05-05/2022-03-23T17:30:06.543100/Democracy_Club__05May2022-HackneyCouncilPollingStations.CSV"
+    elections = ["2022-05-05"]
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "10008353260",  # BASEMENT FLAT, 75 ICKBURGH ROAD, LONDON
-            "10008322617",  # FLAT 2 357A WICK ROAD, HACKNEY, LONDON
-            "10008245689",  # SECOND FLOOR FLAT 126 KINGSLAND HIGH STREET, HACKNEY, LONDON
-            "10008349782",  # 3 MIAH HOUSE 149D BROOKE ROAD, HACKNEY, LONDON
-            "10008342758",  # 1 MIAH HOUSE 149D BROOKE ROAD, HACKNEY, LONDON
-            "10008342260",  # 20 MILLFIELDS PARADE, MILLFIELDS ROAD, LONDON
             "100021050062",  # BASEMENT AND GROUND FLOOR 100 LOWER CLAPTON ROAD, HACKNEY, LONDON
-            "10008294596",  # 1A SHEPHERDESS WALK, LONDON
-            "10008294592",  # FIRST FLOOR AND SECOND FLOOR FLAT 111 STOKE NEWINGTON ROAD, HACKNEY, LONDON
-            "10008294594",  # FLAT, 97 STOKE NEWINGTON ROAD, LONDON
-            "10008294598",  # 2 MIAH HOUSE 149D BROOKE ROAD, HACKNEY, LONDON
-            "10008306167",  # 4 MIAH HOUSE 149D BROOKE ROAD, HACKNEY, LONDON
-            "100021076655",  # 10 CORNTHWAITE ROAD, LONDON
-            "10008318539",  # FLAT B 45 STOKE NEWINGTON CHURCH STREET, HACKNEY, LONDON
             "10008340028",  # FLAT E, 112 KINGSLAND ROAD, LONDON
-            "100021030699",  # 108 GEORGE DOWNING ESTATE, CAZENOVE ROAD, LONDON
             "10008300957",  # 85 CASTLEWOOD ROAD, HACKNEY, LONDON
-            "10008245689",  # SECOND FLOOR FLAT 126 KINGSLAND HIGH STREET, HACKNEY, LONDON
-            "10008245688",  # FIRST FLOOR FLAT 126 KINGSLAND HIGH STREET, HACKNEY, LONDON
-            "10008245690",  # THIRD FLOOR FLAT 126 KINGSLAND HIGH STREET, HACKNEY, LONDON
-            "100021051676",  # SECOND FLOOR FLAT 417 KINGSLAND ROAD, HACKNEY, LONDON
             "200001073528",  # POTTERY HOUSE, ELRINGTON ROAD, LONDON
+            "10008233474",
+            "10008342758",
+            "10008354539",
+            "100021076655",
+            "10008340573",
+            "100021032861",
         ]:
             return None
 
         if record.addressline6 in [
-            "E2 8FZ",
-            "E5 8BE",
-            "N16 0SD",
-            "N16 0RT",
-            "N4 2WN",
+            "N1 6RH",
+            "E20 3AZ",
+            "E8 2NS",
+            "N4 2ZB",
             "N4 2LD",
-            "E8 3RL",
+            "N4 2WQ",
+            "E2 8FZ",
+            "N16 0SD",
+            "E5 9AP",
+            "N16 0RT",
+            "EC2A 2FJ",
         ]:
             return None
 
