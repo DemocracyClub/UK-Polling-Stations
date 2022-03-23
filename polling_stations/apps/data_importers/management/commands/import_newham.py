db7567265216a8714db298c10763f1e2dbe1d703
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-23 16:03:14 +0000

    Import script for Newham (2022-05-05) (closes #4417)

--- polling_stations/apps/data_importers/management/commands/import_newham.py
+++ polling_stations/apps/data_importers/management/commands/import_newham.py
@@ -3,31 +3,27 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "NWM"
-    addresses_name = "2021-03-22T13:30:16.944061/Democracy_Club__06May2021.CSV"
-    stations_name = "2021-03-22T13:30:16.944061/Democracy_Club__06May2021.CSV"
-    elections = ["2021-05-06"]
+    addresses_name = (
+        "2022-05-05/2022-03-23T16:02:55.134381/LBNewham_Democracy_Club__05May2022.tsv"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-23T16:02:55.134381/LBNewham_Democracy_Club__05May2022.tsv"
+    )
+    elections = ["2022-05-05"]
+    csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
-        # Fix from: 890908:polling_stations/apps/data_importers/management/commands/misc_fixes.py:224
-        # Carpenters and Docklands Centre 98 Gibbins Road Stratford London
-        if record.polling_place_id == "5614":
-            record = record._replace(polling_place_easting="538526.11")
-            record = record._replace(polling_place_northing="184252.81")
+        # Winsor Primary School, East Ham Manor Way, Beckton, London
+        if record.polling_place_id == "7284":
+            record = record._replace(polling_place_postcode="E6 5NA")  # was E6 4NA
+
+        # Checked The Hall, 2-4 Victory Parade, as it's out of district.
+        # Its postcode (E20 1FS) is correct.
 
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "46003425",  # 499A BARKING ROAD, LONDON
-            "10090756873",  # FLAT 1 23 NOTTINGHAM AVENUE, WEST BECKTON, LONDON
-            "10009012301",  # 140B BARKING ROAD, LONDON
-            "10034508484",  # FLAT ABOVE 33 VICARAGE LANE, EAST HAM, LONDON
-            "10009012905",  # 62A TREE ROAD, WEST BECKTON, LONDON
-            "10008988958",
-            "10094371250",
-        ]:
-            return None  # in an otherwise multiple station postcode, so safe to remove
+        if record.addressline6 in ["E6 3JE"]:
+            return None  # split
 
         return super().address_record_to_dict(record)
