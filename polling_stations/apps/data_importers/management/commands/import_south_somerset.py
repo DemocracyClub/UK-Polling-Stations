58df27ce54391fbaca64a273634ed8c1488b634b
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-23 16:27:20 +0000

    Import script for South Somerset (2022-05-05) (closes #4425)

--- polling_stations/apps/data_importers/management/commands/import_south_somerset.py
+++ polling_stations/apps/data_importers/management/commands/import_south_somerset.py
@@ -3,32 +3,29 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "SSO"
-    addresses_name = "2021-03-17T11:59:50.337243/South Somerset polling_station_export-2021-03-17.csv"
-    stations_name = "2021-03-17T11:59:50.337243/South Somerset polling_station_export-2021-03-17.csv"
-    elections = ["2021-05-06"]
-    csv_delimiter = ","
+    addresses_name = (
+        "2022-05-05/2022-03-23T16:27:11.495824/polling_station_export-2022-03-23.csv"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-23T16:27:11.495824/polling_station_export-2022-03-23.csv"
+    )
+    elections = ["2022-05-05"]
 
     def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
-
-        if uprn in [
-            "30065145",  # HARE FARM, HARE LANE, BUCKLAND ST. MARY, CHARD
-            "30127709",  # MOBILE HOME AT OAK VIEW FARM HARE LANE, BUCKLAND ST MARY, CHARD
-            "30095367",  # ST. MARYS HOUSE, WEST COKER HILL, WEST COKER, YEOVIL
-            "30515573",  # GRANGE FARM, BOWDEN, HENSTRIDGE, TEMPLECOMBE
-            "30105103",  # THE ORCHARD FARMHOUSE, STOWELL, SHERBORNE
-        ]:
-            return None
 
         if record.housepostcode in [
-            "TA20 2BE",
-            "TA14 6TS",
-            "TA10 0QH",
             "BA10 0BU",
-            "TA10 0DL",
-            "TA3 6RP",
+            "BA22 8NT",
             "BA9 9NZ",
-            "BA21 5DX",
+            "TA10 0DL",
+            "TA10 0HF",
+            "TA10 0PJ",
+            "TA10 0QH",
+            "TA11 7AU",
+            "TA14 6TS",
+            "TA20 2BE",
+            "TA3 6RP",
+            "TA10 9ER",
         ]:
             return None
 
