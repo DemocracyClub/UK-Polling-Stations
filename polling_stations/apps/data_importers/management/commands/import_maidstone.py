fc9c1a45a7d946ac3e68b2a2679c13c3ea62a22b
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-23 15:48:05 +0000

    Import script for Maidstone (2022-05-05) (closes #4413)

--- polling_stations/apps/data_importers/management/commands/import_maidstone.py
+++ polling_stations/apps/data_importers/management/commands/import_maidstone.py
@@ -4,46 +4,24 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "MAI"
     addresses_name = (
-        "2021-03-23T12:00:12.575638/Maidstone Democracy_Club__06May2021.tsv"
+        "2022-05-05/2022-03-23T15:47:49.310266/Democracy_Club__05May2022.tsv"
     )
-    stations_name = "2021-03-23T12:00:12.575638/Maidstone Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
+    stations_name = (
+        "2022-05-05/2022-03-23T15:47:49.310266/Democracy_Club__05May2022.tsv"
+    )
+    elections = ["2022-05-05"]
     csv_delimiter = "\t"
-    csv_encoding = "windows-1252"
+
+    def station_record_to_dict(self, record):
+        # Ulcombe Village Hall, Headcorn Road, Ulcombe, Maidstone
+        if record.polling_place_id == "3183":
+            record = record._replace(polling_place_postcode="ME17 1EB")
+        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "200003731812",  # LITTLE MAGPIE FARM WHITE HILL ROAD, DETLING
-            "200003726116",  # CARAVAN SQUIRREL LODGE RUMSTEAD LANE, STOCKBURY
-            "200003731685",  # MOBILE HOME 2 THE GLEN PITT ROAD, KINGSWOOD
-            "10022893205",  # CONIFER FARM, EMMET HILL LANE, LADDINGFORD, MAIDSTONE
-            "10014308569",  # KENT AND MEDWAY NHS AND SOCIAL CARE PARTNERSHIP, TREVOR GIBBENS UNIT HERMITAGE LANE, MAIDSTONE
-        ]:
-            return None
-
         if record.addressline6 in [
-            "ME17 3SW",
-            "ME7 3JW",
-            "ME15 0PN",
-            "ME17 1DG",
-            "ME17 1LG",
-            "ME17 2DN",
-            "ME17 2AH",
-            "ME18 6AT",
-            "TN12 9NZ",
             "ME15 9RA",
-            "ME17 3XW",
-            "ME17 3XX",
-            "ME17 3XT",
-            "ME17 3XY",
-            "ME17 3XU",
-            "ME17 3XS",
-            "ME16 0WA",
-            "ME16 0FU",
-            "ME17 4EF",
+            "ME18 6AT",
         ]:
-            return None
-
+            return None  #  split
         return super().address_record_to_dict(record)
