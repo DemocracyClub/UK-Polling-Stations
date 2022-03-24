3b432a4bbb1407da3c26e2fa9d779cf5bba0be3e
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-24 08:42:53 +0000

    Import script for Wakefield (2022-05-05) (closes #4437)

--- polling_stations/apps/data_importers/management/commands/import_wakefield.py
+++ polling_stations/apps/data_importers/management/commands/import_wakefield.py
@@ -3,50 +3,34 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "WKF"
-    addresses_name = (
-        "2021-03-24T10:33:03.462547/Democracy Club Polling Districts 24_03_21.csv"
-    )
-    stations_name = (
-        "2021-03-24T10:33:03.462547/Democracy Club Polling Stations 24_03_21.csv"
-    )
-    elections = ["2021-05-06"]
+    addresses_name = "2022-05-05/2022-03-24T08:44:15.467165/Democracy Club - Polling Districts - May 2022 Elections Wakefield.csv"
+    stations_name = "2022-05-05/2022-03-24T08:44:15.467165/Democracy Club - Polling Stations - May 2022 Elections Wakefield.csv"
+    elections = ["2022-05-05"]
 
     def address_record_to_dict(self, record):
-        if record.postcode.strip() in [
-            "WF2 6QY",
-            "WF4 1PU",
-            "WF4 6DL",
-            "WF5 0RT",
-            "WF8 4EB",
-            "WF2 6JA",
+        if record.uprn in [
+            "63111468",
+            "63194364",
+            "63192244",
+            "63185795",
+            "63192242",
+            "63065258",
+            "63194974",
         ]:
-            return None  # split
-
+            return None
         if record.postcode.strip() in [
-            # spurious distance for one property, would be unsplit if removed alone
-            "WF9 3AB",
-            "WF10 2SR",
-            # two properties at same point, different polling stations
-            "WF10 2RH",
+            "WF5 0RT",
+            "WF2 6JA",
+            "WF2 0RG",
+            "WF1 4GA",
         ]:
             return None
 
         return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
-        if record.stationcode == "12NF":
-            record = record._replace(placename="Kings Croft")
 
-        if record.stationcode == "15MG":
-            record = record._replace(placename="Temporary Polling Station")
-            record = record._replace(
-                add1="Land Opposite Lofthouse Gate Working Men's Club"
-            )
-            record = record._replace(add2="Canal Lane")
-            record = record._replace(add3="Lofthouse Gate")
-            record = record._replace(add4="Wakefield")
-            record = record._replace(postcode="WF3 3HN")
-            record = record._replace(xordinate="433449")
-            record = record._replace(yordinate="424608")
-
+        # THREE LANE ENDS ACADEMY
+        if record.stationcode == "22-03NG":
+            record = record._replace(xordinate="", yordinate="")
         return super().station_record_to_dict(record)
