da6c1ce1834cc5e280f89a7283c6d80c21a84cd1
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-24 16:05:40 +0000

    Import script for Harrogate (2022-05-05) (closes #4458)

--- polling_stations/apps/data_importers/management/commands/import_harrogate.py
+++ polling_stations/apps/data_importers/management/commands/import_harrogate.py
@@ -3,27 +3,29 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "HAG"
-    addresses_name = "2021-11-16T10:10:34.996455/Democracy_Club__25November2021 (1).CSV"
-    stations_name = "2021-11-16T10:10:34.996455/Democracy_Club__25November2021 (1).CSV"
-    elections = ["2021-11-25"]
+    addresses_name = (
+        "2022-05-05/2022-03-24T16:05:13.475089/Democracy_Club__05May2022.CSV"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-24T16:05:13.475089/Democracy_Club__05May2022.CSV"
+    )
+    elections = ["2022-05-05"]
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "100050407904",  # GROVE HOUSE BUNGALOW, SKIPTON ROAD, HARROGATE
-            "100050429604",  # HAWTHORNS, STUDLEY ROAD, RIPON
             "10093034565",  # OAK HOUSE PENNY POT LANE TO CENTRAL HOUSE FARM, HAMPSTHWAITE
         ]:
             return None
 
         if record.addressline6 in [
             "HG3 5QF",
-            "HG3 3AT",
             "HG2 9LJ",
             "HG1 4JW",
             "HG2 9NW",
             "YO51 9LN",
+            "YO61 2RT",
         ]:
             return None
 
