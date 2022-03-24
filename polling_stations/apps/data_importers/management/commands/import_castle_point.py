fe8f01a9b1cde9a38a0c418aa485fd86e223ec6f
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-24 16:14:04 +0000

    Import script for Castle Point (2022-05-05) (closes #4460)

--- polling_stations/apps/data_importers/management/commands/import_castle_point.py
+++ polling_stations/apps/data_importers/management/commands/import_castle_point.py
@@ -3,9 +3,13 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "CAS"
-    addresses_name = "2021-04-12T09:02:05.837447/Democracy_Club__06May2021.tsv"
-    stations_name = "2021-04-12T09:02:05.837447/Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
+    addresses_name = (
+        "2022-05-05/2022-03-24T16:13:54.843482/Democracy_Club__05May2022.tsv"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-24T16:13:54.843482/Democracy_Club__05May2022.tsv"
+    )
+    elections = ["2022-05-05"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -14,21 +18,13 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         if uprn in [
             "100090361765",  # 35 LONDON ROAD, BENFLEET
             "100091600419",  # THE GRANGE, GRANGE ROAD, BENFLEET
-            "10004940533",  # CARAVAN 9E2 THORNEY BAY CARAVAN PARK THORNEY BAY ROAD, CANVEY ISLAND
-            "10004940535",  # CARAVAN 9I3 THORNEY BAY CARAVAN PARK THORNEY BAY ROAD, CANVEY ISLAND
-            "10004940532",  # CARAVAN 8A1 THORNEY BAY CARAVAN PARK THORNEY BAY ROAD, CANVEY ISLAND
-            "10004940527",  # CARAVAN 8F5 THORNEY BAY CARAVAN PARK THORNEY BAY ROAD, CANVEY ISLAND
             "10004934986",  # 5 STATION ROAD, CANVEY ISLAND
-            "100090382770",  # 62 WAARDEN ROAD, CANVEY ISLAND
         ]:
             return None
 
         if record.addressline6 in [
-            "SS8 9JH",
             "SS8 8HN",
-            "SS8 7RN",
             "SS8 9SL",
-            "SS7 3PJ",
         ]:
             return None
 
