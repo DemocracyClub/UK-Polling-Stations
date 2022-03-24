1afe2425ef2f30100ce63957ebe7b16a26f72842
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-24 15:19:20 +0000

    Import script for Highland (2022-05-05) (closes #4456)

--- polling_stations/apps/data_importers/management/commands/import_highland.py
+++ polling_stations/apps/data_importers/management/commands/import_highland.py
@@ -3,7 +3,22 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "HLD"
-    addresses_name = "2021-03-22T10:19:17.028289/Democracy Club - Polling Districts.csv"
-    stations_name = "2021-03-22T10:19:17.028289/Democracy Club - Polling Stations.csv"
-    elections = ["2021-05-06"]
-    csv_encoding = "latin-1"
+    addresses_name = (
+        "2022-05-05/2022-03-24T15:19:09.669660/H Democracy Club - Polling Districts.csv"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-24T15:19:09.669660/H Democracy Club - Polling Stations.csv"
+    )
+    elections = ["2022-05-05"]
+
+    def address_record_to_dict(self, record):
+        if record.uprn in ["130131593"]:
+            return None
+        if record.postcode == "IV17 0QY":
+            return None
+        return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        if record.stationcode in ["W01 001", "W05 053"]:
+            return None
+        return super().station_record_to_dict(record)
