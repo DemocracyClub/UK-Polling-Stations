d220d4fba95649a8b10e4a168a5e6a287f6fd463
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-24 13:26:51 +0000

    Import script for Wolverhampton (2022-05-05) (closes #4452)

--- polling_stations/apps/data_importers/management/commands/import_wolverhampton.py
+++ polling_stations/apps/data_importers/management/commands/import_wolverhampton.py
@@ -1,40 +1,42 @@
-from data_importers.github_importer import BaseGitHubImporter
+from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
-class Command(BaseGitHubImporter):
-
-    srid = 4326
-    districts_srid = 4326
+class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WLV"
-    elections = ["2021-05-06"]
-    scraper_name = "wdiv-scrapers/DC-PollingStations-Wolverhampton"
-    geom_type = "geojson"
-
-    def district_record_to_dict(self, record):
-        poly = self.extract_geometry(record, self.geom_type, self.get_srid("districts"))
-
-        return {
-            "internal_council_id": record["DIS_CODE_NEW"],
-            "name": record["WARD"] + " - " + record["DIS_CODE_NEW"],
-            "area": poly,
-            "polling_station_id": record["DIS_CODE_NEW"],
-        }
+    addresses_name = (
+        "2022-05-05/2022-03-24T13:26:30.475155/Democracy_Club__05May2022.tsv"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-24T13:26:30.475155/Democracy_Club__05May2022.tsv"
+    )
+    elections = ["2022-05-05"]
+    csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
-        location = self.extract_geometry(
-            record, self.geom_type, self.get_srid("stations")
-        )
-        codes = record["DIS_CODE_NEW"].split(",")
-        codes = [code.strip() for code in codes]
 
-        stations = []
-        for code in codes:
-            stations.append(
-                {
-                    "internal_council_id": code,
-                    "address": record["ADDRESS"],
-                    "postcode": record["POSTCODE"],
-                    "location": location,
-                }
-            )
-        return stations
+        # St Joseph`s Church Hall, Coalway Road, Wolverhampton
+        if record.polling_place_id == "29400":
+            record = record._replace(polling_place_postcode="")
+
+        return super().station_record_to_dict(record)
+
+    def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "100071373072",
+            "10007123304",
+            "10093323695",
+            "10093323696",
+            "10093325431",
+            "10090013969",
+            "10090642339",
+            "10090643975",
+            "100071556719",
+        ]:
+            return None
+
+        if record.addressline6 in []:
+            return None
+
+        return super().address_record_to_dict(record)
