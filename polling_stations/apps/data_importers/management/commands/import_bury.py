1f013637460af5dec53299aaf84d2ef999767d73
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-24 12:32:12 +0000

    Import script for Bury (2022-05-05) (closes #4450)

--- polling_stations/apps/data_importers/management/commands/import_bury.py
+++ polling_stations/apps/data_importers/management/commands/import_bury.py
@@ -2,36 +2,26 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E08000002"
-    addresses_name = "local.2018-05-03/Version 1/Democracy Club PS DETAILS.csv"
-    stations_name = "local.2018-05-03/Version 1/Democracy Club PS DETAILS.csv"
-    elections = ["local.2018-05-03"]
-
-    def station_record_to_dict(self, record):
-
-        if record.polling_place_id == "2574":
-            record = record._replace(polling_place_postcode="BL0 0BJ")
-
-        return super().station_record_to_dict(record)
+    council_id = "BUR"
+    addresses_name = (
+        "2022-05-05/2022-03-24T12:31:09.109523/Democracy_Club__05May2022.tsv"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-24T12:31:09.109523/Democracy_Club__05May2022.tsv"
+    )
+    elections = ["2022-05-05"]
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in ["100010957935", "100010957934"]:
-            rec = super().address_record_to_dict(record)
-            rec["postcode"] = "BL9 6JW"
-            return rec
-
-        if record.addressline6 == "BL9 9JN":
-            return None
-
-        if record.addressline6 == "BL8 4LA":
-            return None
-
-        if record.addressline6 == "M26 1FP":
-            return None
-
-        if record.addressline6 == "BL8 2HH":
+        if record.addressline6 in [
+            "BL9 8HB",
+            "BL9 9JW",
+            "BL9 8JJ",
+            "BL9 8JW",
+            "BL9 9PQ",
+            "BL8 2HH",
+            "BL8 1TF",
+        ]:
             return None
 
         return super().address_record_to_dict(record)
