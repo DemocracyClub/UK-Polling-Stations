f638cb1852b7f0b0ffe91a4ff0216bff486327c1
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-24 11:12:46 +0000

    Import script for Camden (2022-05-05) (closes #4448)

--- polling_stations/apps/data_importers/management/commands/import_camden.py
+++ polling_stations/apps/data_importers/management/commands/import_camden.py
@@ -1,19 +1,12 @@
-"""
-Imports Camden
-"""
-
-
 from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "CMD"
-    addresses_name = "2021-03-24T10:52:03.478834/Democracy Club Polling Districts.csv"
-    stations_name = "2021-03-24T10:52:03.478834/Democracy Club polling stations.csv"
-    elections = ["2021-05-06"]
-
-    def station_record_to_dict(self, record):
-        if record.stationcode == "MB_1":
-            record = record._replace(postcode="NW1 7EA")
-
-        return super().station_record_to_dict(record)
+    addresses_name = (
+        "2022-05-05/2022-04-05T16:06:41.598889/Democracy Club - Polling Districts.csv"
+    )
+    stations_name = (
+        "2022-05-05/2022-04-05T16:06:41.598889/Democracy Club - Polling Stations.csv"
+    )
+    elections = ["2022-05-05"]
