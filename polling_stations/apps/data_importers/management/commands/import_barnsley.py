e169f7b7f466da189a452783a40984a0195c1b04
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-25 09:17:14 +0000

    Import script for Barnsley (2022-05-05) (closes #4468)

--- polling_stations/apps/data_importers/management/commands/import_barnsley.py
+++ polling_stations/apps/data_importers/management/commands/import_barnsley.py
@@ -3,30 +3,27 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "BNS"
-    addresses_name = "2021-03-12T14:20:28.775183/Democracy Club Polling Districts.csv"
-    stations_name = "2021-03-12T14:20:28.775183/Democracy Club Polling Stations.csv"
-    elections = ["2021-05-06"]
+    addresses_name = (
+        "2022-05-05/2022-03-25T09:18:02.587996/Democracy Club Polling Districts.csv"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-25T09:18:02.587996/Democracy Club Polling Stations.csv"
+    )
+    elections = ["2022-05-05"]
 
     def station_record_to_dict(self, record):
         # TEMPORARY BUILDING AT GRASMERE CRESCENT
-        if record.stationcode == "24A":
+        if record.stationcode == "24":
             record = record._replace(xordinate="", yordinate="")
 
-        # DARFIELD COMMUNITY CENTRE, DARHAVEN, DARFIELD, BARNSLEY
-        if record.stationcode == "15A":
-            record = record._replace(xordinate="441313", yordinate="404680")
-        #
         return super().station_record_to_dict(record)
 
-    #
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
 
         if uprn in [
-            "2007005952",  # BLUE SLATES COTTAGE BROCKHOLES LANE, CUBLEY, BARNSLEY
-            "2007021481",  # 24 REDHAW ROAD, ROYSTON, BARNSLEY
-            "2007003887",  # WOOD END, OUGHTIBRIDGE LANE, OUGHTIBRIDGE, SHEFFIELD
-            "2007003913",  # HOLMES FARM, MAIN ROAD, WHARNCLIFFE SIDE, SHEFFIELD
+            "2007020712",
+            "2007021402",
         ]:
             return None
 
