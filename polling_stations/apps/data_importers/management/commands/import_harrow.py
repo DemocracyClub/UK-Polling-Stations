c42e83a4062e530cdf1f3129cd00d7d6bfa45dd1
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-28 16:54:05 +0100

    Import script for Harrow (2022-05-05) (closes #4488)

--- polling_stations/apps/data_importers/management/commands/import_harrow.py
+++ polling_stations/apps/data_importers/management/commands/import_harrow.py
@@ -3,13 +3,17 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "HRW"
-    addresses_name = "2021-03-25T11:47:14.508721/Democracy_Club__06May2021 - 2.tsv"
-    stations_name = "2021-03-25T11:47:14.508721/Democracy_Club__06May2021 - 2.tsv"
-    elections = ["2021-05-06"]
+    addresses_name = (
+        "2022-05-05/2022-03-28T16:53:48.820120/Democracy_Club__05May2022.tsv"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-28T16:53:48.820120/Democracy_Club__05May2022.tsv"
+    )
+    elections = ["2022-05-05"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
-        if record.addressline6.strip() in ["HA1 2LH", "HA2 7SJ", "HA5 3HF", "HA5 2AB"]:
-            return None
+        if record.addressline6.strip() in ["HA5 3HF"]:
+            return None  # split
 
         return super().address_record_to_dict(record)
