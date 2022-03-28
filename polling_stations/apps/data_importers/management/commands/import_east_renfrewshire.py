a1d03b78d6461c5b596ec1be2222b42bced9a19e
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-28 09:28:21 +0100

    Import script for East Renfrewshire (2022-05-05) (closes #4478)

--- polling_stations/apps/data_importers/management/commands/import_east_renfrewshire.py
+++ polling_stations/apps/data_importers/management/commands/import_east_renfrewshire.py
@@ -1,110 +1,46 @@
-from data_importers.ems_importers import BaseDemocracyCountsCsvImporter
-
-ERW_STATIONCODES = (
-    "53",
-    "58",
-    "2",
-    "42",
-    "48",
-    "29",
-    "34",
-    "44",
-    "51",
-    "CROSSAPS_1",
-    "25",
-    "54",
-    "35",
-    "45",
-    "46",
-    "10",
-    "NEILSTONPS_3",
-    "7",
-    "38",
-    "11",
-    "23",
-    "5",
-    "NEILSTONPS_4",
-    "MUREHALL_1",
-    "37",
-    "13",
-    "AUCHENBACK_3",
-    "52",
-    "3",
-    "8",
-    "21",
-    "AUCHENBACK_2",
-    "49",
-    "30",
-    "4",
-    "STANREWCH_3",
-    "CROSSAPS_3",
-    "CROSSAPS_4",
-    "28",
-    "61",
-    "DALMENYCC_2",
-    "9",
-    "CROSSAPS_2",
-    "STANREWCH_1",
-    "41",
-    "59",
-    "16",
-    "CARLIBARPS_1",
-    "39",
-    "36",
-    "DALMENYCC_1",
-    "24",
-    "1",
-    "55",
-    "20",
-    "47",
-    "15",
-    "AUCHENBACK_4",
-    "50",
-    "32",
-    "NEILSTONPS_5",
-    "CARLIBARPS_3",
-    "CARLIBARPS_2",
-    "NEILSTONPS_1",
-    "31",
-    "56",
-    "14",
-    "AUCHENBACK_1",
-    "19",
-    "22",
-    "60",
-    "26",
-    "18",
-    "NEILSTONPS_2",
-    "6",
-    "57",
-    "STANREWCH_2",
-    "27",
-    "17",
-    "40",
-    "12",
-    "43",
-    "33",
-)
+from addressbase.models import UprnToCouncil
+from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "ERW"
     addresses_name = (
-        "2021-03-25T10:46:11.501580/Refrew DemocracyClub_PollingDistricts.csv"
+        "2022-05-05/2022-03-28T09:28:06.019937/DemocracyClub_PollingDistricts2022.csv"
     )
     stations_name = (
-        "2021-03-25T10:46:11.501580/Refrew DEmocracyClub_PollingStations.csv"
+        "2022-05-05/2022-03-28T09:28:06.019937/DemocracyClub_PollingStations2022.csv"
     )
-    elections = ["2021-05-06"]
+    elections = ["2022-05-05"]
+
+    def pre_import(self):
+        # We need to consider rows that don't have a uprn when importing data.
+        # However there are lots of rows for other councils in this file.
+        # So build a list of stations from rows that do have UPRNS
+        # and then use that list of stations to make sure we check relevant rows, even if they don't have a UPRN
+
+        council_uprns = set(
+            UprnToCouncil.objects.filter(lad=self.council.geography.gss).values_list(
+                "uprn", flat=True
+            )
+        )
+        self.COUNCIL_STATIONS = set()
+        data = self.get_addresses()
+
+        for record in data:
+            if record.uprn in council_uprns:
+                self.COUNCIL_STATIONS.add(record.stationcode)
 
     def address_record_to_dict(self, record):
-        if record.stationcode not in ERW_STATIONCODES:
+        if record.stationcode not in self.COUNCIL_STATIONS:
             return None
-        if record.postcode == "G76 8RW":
+
+        if record.postcode in ["G76 8RW"]:
             return None
+
         return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
-        if record.stationcode not in ERW_STATIONCODES:
+        if record.stationcode not in self.COUNCIL_STATIONS:
             return None
+
         return super().station_record_to_dict(record)
