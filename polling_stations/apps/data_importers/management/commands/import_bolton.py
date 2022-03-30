4cbc0dacc828a3fd36ecf67dcfae636ddaddd57a
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-30 11:53:18 +0100

    Import script for Bolton (2022-05-05) (closes #4513)

--- polling_stations/apps/data_importers/management/commands/import_bolton.py
+++ polling_stations/apps/data_importers/management/commands/import_bolton.py
@@ -3,20 +3,13 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "BOL"
-    addresses_name = "2021-05-01T18:52:07.783562/Democracy_Club__06May2021.CSV"
-    stations_name = "2021-05-01T18:52:07.783562/Democracy_Club__06May2021.CSV"
-    elections = ["2021-05-06"]
-    csv_delimiter = ","
-
-    def station_record_to_dict(self, record):
-        if record.polling_place_id == "4291":
-            # Trinity Methodist Hall (postcode geocode puts this quite away from actual location, making error spotting
-            # more difficult)
-            record = record._replace(
-                polling_place_easting=374156, polling_place_northing=405696
-            )
-
-        return super().station_record_to_dict(record)
+    addresses_name = (
+        "2022-05-05/2022-03-30T11:53:04.741749/Democracy_Club__05May2022.CSV"
+    )
+    stations_name = (
+        "2022-05-05/2022-03-30T11:53:04.741749/Democracy_Club__05May2022.CSV"
+    )
+    elections = ["2022-05-05"]
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
@@ -27,39 +20,22 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "100012431797",  # 321 DERBY STREET, BOLTON
             "10001244960",  # FLAT 3, 115-117 DERBY STREET, BOLTON
             "100012556511",  # 152 LONGSIGHT, BOLTON
+            "10001244221",  # FLAT 1 290 ST HELENS ROAD, BOLTON
+            "100010919316",
         ]:
             return None
 
-        # FLAT 1 290 ST HELENS ROAD, BOLTON
-        if uprn == "10001244221":
-            record = record._replace(property_urn="", post_code="BL1 4JU")
-
         if record.addressline6 in [
             "BL2 4JU",
-            "BL2 3EL",
-            "BL2 3BQ",
-            "BL2 6DZ",
-            "BL1 3QW",
-            "BL2 2JU",
             "BL4 8JA",
             "BL1 5DB",
-            "BL1 3AU",
             "BL1 5HP",
             "BL1 3SJ",
             "BL1 2HZ",
             "BL3 2DP",
             "BL4 0LW",
-            "BL4 7PQ",
             "BL5 2DL",
-            "BL4 7BB",
-            "BL3 1BA",
             "BL6 4ED",
-            "BL6 6PX",
-            "BL6 6HN",
-            "BL3 6ST",
-            "BL4 0HU",
-            "BL5 3LT",
-            "BL5 2JX",
             "BL5 2DJ",
         ]:
             return None
