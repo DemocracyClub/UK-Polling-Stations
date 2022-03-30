3c69bf477d6668b3015f90bf43a5585334e8db49
polling-bot-4000 <developers@democracyclub.org.uk>
2022-03-30 11:37:49 +0100

    Import script for South Ribble (2022-05-05) (closes #4511)

--- polling_stations/apps/data_importers/management/commands/import_south_ribble.py
+++ polling_stations/apps/data_importers/management/commands/import_south_ribble.py
@@ -4,14 +4,28 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 class Command(BaseHalaroseCsvImporter):
     council_id = "SRI"
     addresses_name = (
-        "2021-03-09T23:18:28.806724/South Ribble polling_station_export-2021-03-09.csv"
+        "2022-05-05/2022-03-30T11:37:35.084389/polling_station_export-2022-03-24-2.csv"
     )
     stations_name = (
-        "2021-03-09T23:18:28.806724/South Ribble polling_station_export-2021-03-09.csv"
+        "2022-05-05/2022-03-30T11:37:35.084389/polling_station_export-2022-03-24-2.csv"
     )
-    elections = ["2021-05-06"]
+    elections = ["2022-05-05"]
+
+    def station_record_to_dict(self, record):
+        # Warning for BUCKSHAW VILLAGE COMMUNITY CENTRE (2-buckshaw-village-community-centre)
+        # being outside the target council area, but it's fine.
+
+        # COUPE GREEN PRIMARY SCHOOL, COUPE GREEN, HOGHTON, PRESTON
+        if record.pollingstationnumber == "62":
+            record = record._replace(pollingstationpostcode="PR5 0JR")  # was PR5 OJR
+
+        # HIGHER  WALTON COMMUNITY CENTRE, OFF HIGHER  WALTON ROAD, HIGHER WALTON, PRESTON
+        if record.pollingstationnumber == "64":
+            record = record._replace(pollingstationpostcode="PR5 4HU")  # was PR5 4HV
+
+        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         if record.housepostcode in ["PR5 5AU", "PR5 4TB"]:
-            return None
+            return None  # split
         return super().address_record_to_dict(record)
