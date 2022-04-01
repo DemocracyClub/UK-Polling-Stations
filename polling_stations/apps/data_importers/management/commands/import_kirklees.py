83e2faa8bfd0232f32206e7abbc167d536cb4cec
polling-bot-4000 <developers@democracyclub.org.uk>
2022-04-01 09:39:47 +0100

    Import script for Kirklees (2022-05-05) (closes #4532)

--- polling_stations/apps/data_importers/management/commands/import_kirklees.py
+++ polling_stations/apps/data_importers/management/commands/import_kirklees.py
@@ -4,10 +4,10 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "KIR"
     addresses_name = (
-        "2022-05-05/2022-03-10T15:06:01.054793/Democracy_Club__05May2022.tsv"
+        "2022-05-05/2022-04-01T09:37:46.018185/NEWDemocracy_Club__05May2022.tsv"
     )
     stations_name = (
-        "2022-05-05/2022-03-10T15:06:01.054793/Democracy_Club__05May2022.tsv"
+        "2022-05-05/2022-04-01T09:37:46.018185/NEWDemocracy_Club__05May2022.tsv"
     )
     elections = ["2022-05-05"]
     csv_encoding = "windows-1252"
@@ -18,17 +18,6 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         if record.polling_place_id == "14951":
             record = record._replace(polling_place_easting="")
             record = record._replace(polling_place_northing="")
-
-        # Pre-School Room - Lydgate J & I School, Lydgate Road, Soothill
-        if record.polling_place_id == "14866":
-            record = record._replace(polling_place_easting="")
-            record = record._replace(polling_place_northing="")
-
-        # Marsden United Reform Church, Peel Street, Marsden
-        if record.polling_place_id == "15516":
-            record = record._replace(polling_place_easting="")
-            record = record._replace(polling_place_northing="")
-            record = record._replace(polling_place_uprn="")
 
         # Roberttown Community Centre
         # Entrance is on Church Rd
