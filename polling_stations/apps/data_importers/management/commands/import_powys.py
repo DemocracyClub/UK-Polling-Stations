ffba0d1fa3f131bcf9a0c036be096ef3d2c00e5e
polling-bot-4000 <developers@democracyclub.org.uk>
2022-04-04 12:17:45 +0100

    Import script for Powys (2022-05-05) (closes #4543)

--- polling_stations/apps/data_importers/management/commands/import_powys.py
+++ polling_stations/apps/data_importers/management/commands/import_powys.py
@@ -1,70 +1,60 @@
-from data_importers.ems_importers import BaseHalaroseCsvImporter
+from data_importers.management.commands import BaseHalaroseCsvImporter
 
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "POW"
-    addresses_name = "2021-04-07T12:37:18.812269/polling_station_export-2021-04-07.csv"
-    stations_name = "2021-04-07T12:37:18.812269/polling_station_export-2021-04-07.csv"
-    elections = ["2021-05-06"]
-
-    def station_record_to_dict(self, record):
-        if record.pollingstationnumber == "106":
-            # Same station as station 146
-            record = record._replace(
-                pollingstationaddress_3="MACHYNLLETH",  # typo in original
-                pollingstationpostcode="SY20 8ER",  # was SY20 8QR; copied from 146
-            )
-
-        return super().station_record_to_dict(record)
+    addresses_name = (
+        "2022-05-05/2022-04-04T12:17:28.212236/polling_station_export-2022-03-30.csv"
+    )
+    stations_name = (
+        "2022-05-05/2022-04-04T12:17:28.212236/polling_station_export-2022-03-30.csv"
+    )
+    elections = ["2022-05-05"]
 
     def address_record_to_dict(self, record):
         if record.housepostcode in [
-            "SY18 6QT",
-            "LD1 6TY",
-            "LD1 6UT",
-            "LD2 3UD",
-            "HR3 5JY",
-            "LD3 7HN",
-            "LD3 9EF",
-            "LD3 0HG",
-            "SY16 3LS",
-            "SY21 0NG",
-            "SY21 0DT",
-            "SY21 8QL",
-            "SY22 6DE",
             "SY5 9BT",
-            "SY21 9AN",
-            "SY18 6JD",
-            "SY16 3DR",
-            "SY18 6LS",
-            "SY18 6NR",
-            "SY20 9NL",
-            "SY10 0LH",
-            "SY17 5SA",
-            "SY17 5PA",
-            "SY17 5NG",
-            "SY20 8EX",
+            "LD3 0HG",
             "SY20 8DJ",
-            "SY21 9AY",
-            "SY21 9AP",
+            "LD3 9EF",
+            "SY21 9AN",
+            "HR3 5JY",
+            "SY16 3DR",
+            "SY17 5PA",
             "SY15 6LD",
+            "LD1 6TY",
+            "SY16 1HG",
+            "SY10 0LH",
+            "LD3 7HN",
+            "SY22 6DE",
+            "SY17 5SA",
             "SY22 5LX",
-            "SY22 6JG",
-            "SY22 6DG",
+            "SY17 5NG",
+            "SY18 6QT",
+            "SY18 6LS",
+            "LD1 6UT",
+            "SY16 3LS",
             "SY21 0HE",
+            "SY18 6LT",
+            "SY18 6JD",
             "SY16 3DW",
-            "SY21 8TD",
-            "SY21 7QU",
-            "SY21 7NB",
+            "SY18 6NR",
+            "SY21 9AY",
             "SY21 9HZ",
+            "SY21 0NG",
+            "SY20 8EX",
+            "SY21 7QU",
+            "SY21 0DT",
+            "SY21 8TD",
+            "SY22 6JG",
+            "LD2 3UD",
+            "SY21 9AP",
         ]:
             return None  # split
 
         uprn = record.uprn.lstrip(" 0")
 
-        if uprn in [
-            "10011738931",  # in another area
-        ]:
+        if uprn in ["10011795190"]:
             return None
 
         return super().address_record_to_dict(record)
