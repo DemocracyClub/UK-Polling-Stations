962285e7c82b6e10bf8afc2ddc11384693a8731c
polling-bot-4000 <developers@democracyclub.org.uk>
2022-04-04 12:50:45 +0100

    Import script for Hastings (2022-05-05) (closes #4545)

--- polling_stations/apps/data_importers/management/commands/import_hastings.py
+++ polling_stations/apps/data_importers/management/commands/import_hastings.py
@@ -3,33 +3,33 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "HAS"
-    addresses_name = "2021-03-17T16:31:21.505378/Democracy_Club__06May2021.tsv"
-    stations_name = "2021-03-17T16:31:21.505378/Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
+    addresses_name = (
+        "2022-05-05/2022-04-04T12:50:30.594021/Democracy_Club__05May2022-2.tsv"
+    )
+    stations_name = (
+        "2022-05-05/2022-04-04T12:50:30.594021/Democracy_Club__05May2022-2.tsv"
+    )
+    elections = ["2022-05-05"]
     csv_delimiter = "\t"
-    csv_encoding = "windows-1252"
-
-    def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "100062577569",  # THE ROYAL ALBERT, 293 BATTLE ROAD, ST. LEONARDS-ON-SEA
-            "10002500023",  # BEECH VIEW, CAMPKIN GARDENS, ST. LEONARDS-ON-SEA
-            "10070607789",  # 120 BATTLE ROAD, ST. LEONARDS-ON-SEA
-            "100062577571",  # NUMBER 46 GUEST HOUSE, 46 CAMBRIDGE GARDENS, HASTINGS
-            "10002501567",  # 285 ELPHINSTONE ROAD, HASTINGS
-        ]:
-            return None
-
-        if record.addressline6 in ["TN38 9EL", "TN37 7JL"]:
-            return None
-
-        return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
-
-        # Downs Farm Community Centre Crowborough Road Hastings TN35 5EA
-        if record.polling_place_id == "887":
-            record = record._replace(polling_place_postcode="TN35 5EE")
+        # Bannatyne Spa Hotel (Montgomerie Suite), Battle Road, St Leonards on Sea, East Sussex
+        if record.polling_place_id == "1004":
+            # postcode was out-of-area
+            record = record._replace(
+                polling_place_postcode="TN38 8EZ", polling_place_uprn="10070602485"
+            )
 
         return super().station_record_to_dict(record)
+
+    def address_record_to_dict(self, record):
+        if record.addressline6 in [
+            "TN34 3JE",
+            "TN37 7QX",
+            "TN38 9BP",
+            "TN38 0YJ",
+            "TN34 1TB",
+            "TN38 0PB",
+        ]:
+            return None  # split
+        return super().address_record_to_dict(record)
