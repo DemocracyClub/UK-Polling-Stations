18ede77f4e49f573cff2cedacdcc72871f831005
polling-bot-4000 <developers@democracyclub.org.uk>
2022-04-07 14:47:59 +0100

    Import script for Barrow-in-Furness (2022-05-05) (closes #4577)

--- polling_stations/apps/data_importers/management/commands/import_barrow_in_furness.py
+++ polling_stations/apps/data_importers/management/commands/import_barrow_in_furness.py
@@ -3,25 +3,22 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "BAR"
-    addresses_name = "2021-03-08T19:54:15.110811/Barrow in Furness Democracy Club - Polling Districts (2).csv"
-    stations_name = "2021-03-08T19:54:15.110811/Barrow in Furness Democracy Club - Polling Stations (2).csv"
-    elections = ["2021-05-06"]
-    allow_station_point_from_postcode = False
-
-    def address_record_to_dict(self, record):
-        if record.postcode in ["LA13 9SF", "LA13 0NF"]:
-            return None
-
-        return super().address_record_to_dict(record)
+    addresses_name = (
+        "2022-05-05/2022-04-07T14:47:44.793330/Democracy Club - Polling Districts.csv"
+    )
+    stations_name = (
+        "2022-05-05/2022-04-07T14:47:44.793330/Democracy Club - polling stations.csv"
+    )
+    elections = ["2022-05-05"]
 
     def station_record_to_dict(self, record):
         # ASKAM COMMUNITY CENTRE
-        if record.stationcode in ["LC/1", "LC/2"]:
+        if record.stationcode in ["LC1", "LC2"]:
             record = record._replace(xordinate="321435")
             record = record._replace(yordinate="477563")
 
         # VICKERSTOWN METHODIST CHURCH
-        if record.stationcode == "AA/1":
+        if record.stationcode == "AA1":
             record = record._replace(xordinate="318614")
             record = record._replace(yordinate="468960")
 
