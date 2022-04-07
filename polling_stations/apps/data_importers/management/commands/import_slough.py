3de442018c7dd1f863de90b138badfda3f9c0275
polling-bot-4000 <developers@democracyclub.org.uk>
2022-04-07 09:35:27 +0100

    Import script for Slough (2022-05-05) (closes #4570)

--- polling_stations/apps/data_importers/management/commands/import_slough.py
+++ polling_stations/apps/data_importers/management/commands/import_slough.py
@@ -2,31 +2,25 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E06000039"
+    council_id = "SLG"
     addresses_name = (
-        "parl.2019-12-12/Version 1/Democracy_Club__12December2019slough.CSV"
+        "2022-05-05/2022-04-07T09:34:53.056236/Democracy_Club__05May2022.tsv"
     )
-    stations_name = "parl.2019-12-12/Version 1/Democracy_Club__12December2019slough.CSV"
-    elections = ["parl.2019-12-12"]
-    allow_station_point_from_postcode = False
-
-    def station_record_to_dict(self, record):
-        if record.polling_place_id == "1000":  # Claycots School [Town Hall]
-            record = record._replace(polling_place_easting="0")
-            record = record._replace(polling_place_northing="0")
-        return super().station_record_to_dict(record)
+    stations_name = (
+        "2022-05-05/2022-04-07T09:34:53.056236/Democracy_Club__05May2022.tsv"
+    )
+    elections = ["2022-05-05"]
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
-        uprn = record.property_urn.strip().lstrip("0")
 
-        if uprn in [
-            "100081042223",
-            "10022917421",
+        if record.addressline6 in [
+            "SL2 2LZ",
+            "SL3 7FU",
+            "SL3 8QT",
+            "SL1 2LT",
+            "SL2 2DW",
         ]:
-            rec["accept_suggestion"] = False
+            return None
 
-        if uprn == "100080321307":
-            rec["postcode"] = "SL6 0LG"
-
-        return rec
+        return super().address_record_to_dict(record)
