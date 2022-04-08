b05414f8761038793c792b4ad8bdbb286619ad0b
polling-bot-4000 <developers@democracyclub.org.uk>
2022-04-08 14:35:26 +0100

    Import script for Craven (2022-05-05) (closes #4605)

--- polling_stations/apps/data_importers/management/commands/import_craven.py
+++ polling_stations/apps/data_importers/management/commands/import_craven.py
@@ -1,12 +1,14 @@
-from data_importers.management.commands import (
-    BaseXpressDCCsvInconsistentPostcodesImporter,
-)
+from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
-class Command(BaseXpressDCCsvInconsistentPostcodesImporter):
+class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "CRA"
-    addresses_name = "2021-11-17T10:33:36.800611/Democracy_Club__25November2021.tsv"
-    stations_name = "2021-11-17T10:33:36.800611/Democracy_Club__25November2021.tsv"
-    elections = ["2021-11-25"]
-    csv_delimiter = "\t"
+    addresses_name = (
+        "2022-05-05/2022-04-08T14:35:11.201254/Democracy_Club__05May2022-7.tsv"
+    )
+    stations_name = (
+        "2022-05-05/2022-04-08T14:35:11.201254/Democracy_Club__05May2022-7.tsv"
+    )
+    elections = ["2022-05-05"]
     csv_encoding = "windows-1252"
+    csv_delimiter = "\t"
