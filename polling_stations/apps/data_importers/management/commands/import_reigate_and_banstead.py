0f4dae13e95c44f8e76231120f221950863341ab
polling-bot-4000 <developers@democracyclub.org.uk>
2022-04-08 09:13:08 +0100

    Import script for Reigate and Banstead (2022-05-05) (closes #4588)

--- polling_stations/apps/data_importers/management/commands/import_reigate_and_banstead.py
+++ polling_stations/apps/data_importers/management/commands/import_reigate_and_banstead.py
@@ -1,27 +1,31 @@
-from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
+from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 
-class Command(BaseXpressDemocracyClubCsvImporter):
+class Command(BaseDemocracyCountsCsvImporter):
     council_id = "REI"
-    addresses_name = "2021-04-30T16:00:10.118165/Democracy_Club__06May2021.tsv"
-    stations_name = "2021-04-30T16:00:10.118165/Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
-    csv_delimiter = "\t"
+    addresses_name = "2022-05-05/2022-04-08T09:12:49.913080/RBBC Polling Districts.csv"
+    stations_name = "2022-05-05/2022-04-08T09:12:49.913080/RBBC Polling Stations.csv"
+    elections = ["2022-05-05"]
 
     def station_record_to_dict(self, record):
-        if record.polling_place_id == "4151":
-            record = record._replace(polling_place_postcode="")
+        # Woodmansterne Village Hall, Carshalton Road, Woodmansterne, Banstead, Surrey
+        if record.stationcode == "CKW1":
+            record = record._replace(xordinate="527572", yordinate="160117")
+
+        # St John`s Church Hall, The Avenue, Tadworth, Surrey
+        if record.stationcode in ["KTW1.1", "KTW1.2"]:
+            record = record._replace(postcode="KT20 5DB")  # was KT20 5AB
 
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
+        uprn = record.uprn.lstrip("0")
 
-        if record.addressline6 in [
-            "RH1 1PH",
-            "RH1 2JG",
-            "RH2 9JB",
-            "RH6 9BS",
-            "RH6 8QG",
+        if uprn in [
+            "68176955",  # 1 PUTNEY CROFY, HORLEY
+            "68176948",  # 2 PUTNEY CROFY, HORLEY
+            "68181592",  # 2A PARKHURST ROAD, HORLEY
         ]:
             return None
+
         return super().address_record_to_dict(record)
