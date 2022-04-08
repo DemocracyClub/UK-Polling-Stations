c262f1d7b442da0282263dc81af5e379a5c7300a
polling-bot-4000 <developers@democracyclub.org.uk>
2022-04-08 13:04:09 +0100

    Import script for Basildon (2022-05-05) (closes #4596)

--- polling_stations/apps/data_importers/management/commands/import_basildon.py
+++ polling_stations/apps/data_importers/management/commands/import_basildon.py
@@ -3,43 +3,32 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "BAI"
-    addresses_name = "2021-03-16T14:11:49.270209/Democracy_Club__06May2021.csv"
-    stations_name = "2021-03-16T14:11:49.270209/Democracy_Club__06May2021.csv"
-    elections = ["2021-05-06"]
-    csv_delimiter = ","
+    addresses_name = "2022-05-05/2022-04-08T13:03:58.514518/Basildon Council - Democracy_Club__05May2022.CSV"
+    stations_name = "2022-05-05/2022-04-08T13:03:58.514518/Basildon Council - Democracy_Club__05May2022.CSV"
+    elections = ["2022-05-05"]
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "100091587330",  # CEDAR COTTAGE, LOWER DUNTON ROAD, BULPHAN, UPMINSTER
             "10090682049",  # FERNDALE, TYE COMMON ROAD, BILLERICAY
             "100090239089",  # FLAT 1, ST. DAVIDS COURT, LONDON ROAD, BASILDON
             "100091212751",  # PROBATION OFFICE, 1 FELMORES, BASILDON
             "10013352273",  # 17 CHURCH ROAD, LAINDON, BASILDON
-            "100090273531",  # 23 GREENS FARM LANE, BILLERICAY
         ]:
             return None
 
         if record.addressline6 in [
-            "SS12 0AU",
+            "SS15 5DS",
             "SS15 5GX",
-            "CM11 2ER",
-            "SS13 2EA",
-            "CM11 2JX",
-            "CM11 2AD",
-            "CM12 9JJ",
-            "SS15 6GJ",
-            "SS15 6PF",
-            "SS13 3EA",
-            "CM11 1HH",
-            "SS15 5NZ",
             "CM11 2RU",
-            "SS16 5PW",
-            "SS13 2LG",
+            "SS15 6PF",
+            "CM11 2JX",
             "SS16 6PH",
-            "SS12 9LE",
-            "SS14 3RZ",
+            "SS12 0AU",
+            "CM11 2AD",
+            "SS15 5NZ",
+            "SS15 5NS",
         ]:
             return None
 
