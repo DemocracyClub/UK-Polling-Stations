0fe2bb2b8a19a36a6be841229e614a37952c295d
polling-bot-4000 <developers@democracyclub.org.uk>
2022-04-11 14:31:50 +0100

    Import script for Hart (2022-05-05) (closes #4617)

--- polling_stations/apps/data_importers/management/commands/import_hart.py
+++ polling_stations/apps/data_importers/management/commands/import_hart.py
@@ -4,10 +4,10 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "HAT"
     addresses_name = (
-        "2022-05-05/2022-03-02T14:33:14.821410/Democracy_Club__05May2022.tsv"
+        "2022-05-05/2022-04-11T14:31:37.365816/Democracy_Club__05May2022.tsv"
     )
     stations_name = (
-        "2022-05-05/2022-03-02T14:33:14.821410/Democracy_Club__05May2022.tsv"
+        "2022-05-05/2022-04-11T14:31:37.365816/Democracy_Club__05May2022.tsv"
     )
     elections = ["2022-05-05"]
     csv_delimiter = "\t"
@@ -15,6 +15,6 @@ class Command(BaseXpressDemocracyClubCsvImporter):
     def address_record_to_dict(self, record):
 
         if record.addressline6 in ["GU52 0AF"]:
-            return None
+            return None  # split
 
         return super().address_record_to_dict(record)
