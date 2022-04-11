96c286d675a5d7eb824a808a197e4d95601b13a6
polling-bot-4000 <developers@democracyclub.org.uk>
2022-04-11 14:05:03 +0100

    Import script for Woking (2022-05-05) (closes #4615)

--- polling_stations/apps/data_importers/management/commands/import_woking.py
+++ polling_stations/apps/data_importers/management/commands/import_woking.py
@@ -3,29 +3,33 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WOI"
-    addresses_name = "2021-03-19T11:17:13.350243/Democracy_Club__06May2021.tsv"
-    stations_name = "2021-03-19T11:17:13.350243/Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
+    addresses_name = (
+        "2022-05-05/2022-04-11T14:04:40.495448/Democracy_Club__05May2022.tsv"
+    )
+    stations_name = (
+        "2022-05-05/2022-04-11T14:04:40.495448/Democracy_Club__05May2022.tsv"
+    )
+    elections = ["2022-05-05"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "10002421652",  # FLAT ABOVE CLUBHOUSE SUTTON GREEN GOLF CLUB NEW LANE, SUTTON GREEN, WOKING
             "200000218826",  # THE BUNGALOW DAWNEY HILL, PIRBRIGHT
-            "200000201900",  # ASAD, MAYBURY HILL, WOKING
+            "10002428855",  # FLAT 7, THE OLD BREW HOUSE 130-132, HIGH STREET, OLD WOKING, WOKING
+            "10002428856",  # FLAT 8, THE OLD BREW HOUSE 130-132, HIGH STREET, OLD WOKING, WOKING"
         ]:
             return None
 
         if record.addressline6 in ["KT14 6LT", "GU22 8AF"]:
-            return None
+            return None  # split
 
         return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
         # Horsell Evangelical Church High Street Horsell Woking GU21 3SZ
-        if record.polling_place_id == "4159":
+        if record.polling_place_id == "4392":
             record = record._replace(polling_place_postcode="GU21 4SZ")
 
         return super().station_record_to_dict(record)
