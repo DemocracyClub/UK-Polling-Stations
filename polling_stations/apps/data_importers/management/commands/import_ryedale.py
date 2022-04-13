a7e50b1c2a71e458e669f219894f4c96c1296046
polling-bot-4000 <developers@democracyclub.org.uk>
2022-04-13 15:04:08 +0100

    Import script for Ryedale (2022-05-05) (closes #4640)

--- polling_stations/apps/data_importers/management/commands/import_ryedale.py
+++ polling_stations/apps/data_importers/management/commands/import_ryedale.py
@@ -4,69 +4,42 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 class Command(BaseHalaroseCsvImporter):
     council_id = "RYE"
     addresses_name = (
-        "2021-11-08T12:14:12.775016/RYEDALE_polling_station_export-2021-11-03.csv"
+        "2022-05-05/2022-04-13T15:03:49.381329/polling_station_export-2022-03-03.csv"
     )
     stations_name = (
-        "2021-11-08T12:14:12.775016/RYEDALE_polling_station_export-2021-11-03.csv"
+        "2022-05-05/2022-04-13T15:03:49.381329/polling_station_export-2022-03-03.csv"
     )
-    elections = ["2021-11-25"]
+    elections = ["2022-05-05"]
+
+    def station_record_to_dict(self, record):
+
+        # Swinton Reading Rooms, Swinton, Malton North YO17 6SG
+        if record.pollingstationnumber == "4":
+            record = record._replace(pollingstationpostcode="")
+
+        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
+        uprn = record.uprn.strip().lstrip("0")
         if record.housepostcode in [
-            "YO62 6PE",
-            "YO17 9RL",
+            "YO13 9PT",
             "YO17 9LB",
-            "YO60 7HQ",
+            "YO17 9QY",
+            "YO17 9RL",
             "YO41 1JF",
+            "YO60 7HQ",
             "YO60 7NB",
             "YO62 6JA",
-            "YO17 8AD",
-            "YO17 9QY",
-            "YO13 9PT",
-            "YO18 7UE",
-            "YO17 6BU",
-            "YO17 6BW",
-            "YO17 6BX",
+            "YO62 6PA",
+            "YO62 6PE",
         ]:
             return None
 
-        if record.houseid == "2004865":
-            rec = super().address_record_to_dict(record)
-            rec["postcode"] = "YO25 3XZ"
-            return rec
-
-        if record.houseid in ["2019306", "2019299"]:
-            rec = super().address_record_to_dict(record)
-            rec["postcode"] = "YO60 6SF"
-            return rec
-
-        if record.housepostcode == "YO6O 7JU":
-            rec = super().address_record_to_dict(record)
-            rec["postcode"] = "YO60 7JU"
-            return rec
-
-        if record.housepostcode == "YO17 9L7":
-            rec = super().address_record_to_dict(record)
-
-            rec["postcode"] = "YO17 9LU"
-            return rec
-
-        if record.houseid == "2020682":
-            rec = super().address_record_to_dict(record)
-            rec["postcode"] = "YO60 6PA"
-            return rec
-
-        if record.houseid == "2020417":
-            rec = super().address_record_to_dict(record)
-            rec["postcode"] = "YO60 6PE"
-            return rec
-
-        if record.housepostcode == "YO17 8DG":
+        if uprn in [
+            "10007633975",
+            "10007630076",
+            "10002318338",
+        ]:
             return None
-
-        if record.houseid == "6000115":
-            rec = super().address_record_to_dict(record)
-            rec["postcode"] = "YO62 5JA"
-            return rec
 
         return super().address_record_to_dict(record)
