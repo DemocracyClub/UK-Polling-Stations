bd42c03b32bddacae9c391bf86dcf183a49f92f2
polling-bot-4000 <developers@democracyclub.org.uk>
2022-04-13 11:33:34 +0100

    Import script for Dumfries and Galloway (2022-05-05) (closes #4607)

--- polling_stations/apps/data_importers/management/commands/import_dumfries_and_galloway.py
+++ polling_stations/apps/data_importers/management/commands/import_dumfries_and_galloway.py
@@ -1,12 +1,31 @@
-from data_importers.ems_importers import BaseDemocracyCountsCsvImporter
+from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "DGY"
     addresses_name = (
-        "2021-04-01T10:19:31.362798/D&GPollingDistrics_DemocractClub250321.csv"
+        "2022-05-05/2022-04-13T11:33:08.115803/April 22 Polling Districts.csv"
     )
     stations_name = (
-        "2021-04-01T10:19:31.362798/D&GPollingStations_DemocractClub250321.csv"
+        "2022-05-05/2022-04-13T11:33:08.115803/April 2022 Polling Stations.csv"
     )
-    elections = ["2021-05-06"]
+    elections = ["2022-05-05"]
+
+    def station_record_to_dict(self, record):
+        # MIDDLEBIE COMMUNITY CENTRE, MIDDLEBIE, LOCKERBIE
+        if record.stationcode == "AEE8":
+            record = record._replace(xordinate="")
+            record = record._replace(yordinate="")
+
+        # THORNHILL COMMUNITY CENTRE, EAST BACK STREET, THORNHILL
+        if record.stationcode == "MUN11":
+            record = record._replace(xordinate="")
+            record = record._replace(yordinate="")
+
+        return super().station_record_to_dict(record)
+
+    def address_record_to_dict(self, record):
+        if record.postcode in ["DG8 0BZ"]:
+            return None
+
+        return super().address_record_to_dict(record)
