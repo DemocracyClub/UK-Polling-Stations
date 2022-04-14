b09f0b7dcbbbea8617193ccf142e723a590123f3
polling-bot-4000 <developers@democracyclub.org.uk>
2022-04-14 13:37:20 +0100

    Import script for Ceredigion (2022-05-05) (closes #4651)

--- polling_stations/apps/data_importers/management/commands/import_ceredigion.py
+++ polling_stations/apps/data_importers/management/commands/import_ceredigion.py
@@ -1,31 +1,41 @@
-from data_importers.ems_importers import BaseDemocracyCountsCsvImporter
+from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 
 class Command(BaseDemocracyCountsCsvImporter):
-    """
-    Imports the Polling Station data from Ceredigion
-    """
-
     council_id = "CGN"
-    addresses_name = (
-        "2021-04-07T14:45:08.852408/DEMOCRACY CLUB - POLLING DISTRICTS 30235.csv"
-    )
-    stations_name = (
-        "2021-04-07T14:45:08.852408/DEMOCRACY CLUB - POLLING STATIONS (2).csv"
-    )
-    csv_encoding = "latin-1"
-    elections = ["2021-05-06"]
+    addresses_name = "2022-05-05/2022-04-14T13:37:04.763415/Democracy Club - Polling Districts CEREDIGION.csv"
+    stations_name = "2022-05-05/2022-04-14T13:37:04.763415/Democracy Club - Polling Stations CEREDIGION.csv"
+    elections = ["2022-05-05"]
+    csv_encoding = "windows-1252"
 
     def address_record_to_dict(self, record):
-        uprn = record.uprn.lstrip(" 0")
+        uprn = record.uprn.lstrip("0")
 
         if uprn in [
-            "49061701",
+            "49062840",
             "49049138",
             "49069702",
             "49047840",
+            "49040771",
+            "49065819",
+            "49069384",
+            "49075931",
+            "49037718",
+            "49062368",
+            "49061701",
             "49041491",
+            "49040891",
+            "49073822",
         ]:
-            return None  # significantly overlaps other polling areas
+            return None
 
+        if record.postcode in ["SA43 1PT"]:
+            return None
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # CANOLFAN CYMUNEDOL MYNACH COMMUNITY CENTRE
+        if record.stationcode == "10-1026/1027":
+            record = record._replace(postcode="")
+
+        return super().station_record_to_dict(record)
