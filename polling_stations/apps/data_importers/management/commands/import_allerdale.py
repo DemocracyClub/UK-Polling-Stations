6cb92f4daa04af790d0120792efc445a6f36e75a
polling-bot-4000 <developers@democracyclub.org.uk>
2022-04-15 17:53:25 +0100

    Import script for Allerdale (2022-05-05) (closes #4658)
    Author: Alex Dutton <alex@cottagelabs.com>

--- polling_stations/apps/data_importers/management/commands/import_allerdale.py
+++ polling_stations/apps/data_importers/management/commands/import_allerdale.py
@@ -2,7 +2,12 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 
 class Command(BaseDemocracyCountsCsvImporter):
-    council_id = "E07000026"
-    addresses_name = "europarl.2019-05-23/Version 1/Allerdale Polling Districts for Democracy Club.csv"
-    stations_name = "europarl.2019-05-23/Version 1/Allerdale Polling Stations for Democracy Club.csv"
-    elections = ["europarl.2019-05-23"]
+    council_id = "ALL"
+    addresses_name = "2022-05-05/2022-04-15T17:53:15.235889/Polling SDistricts.csv"
+    stations_name = "2022-05-05/2022-04-15T17:53:15.235889/Polling Stations.csv"
+    elections = ["2022-05-05"]
+
+    def address_record_to_dict(self, record):
+        if record.postcode in ["CA15 7RL"]:
+            return None  # split
+        return super().address_record_to_dict(record)
