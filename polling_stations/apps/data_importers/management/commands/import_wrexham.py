19699e7d23e0f4c55df9c6fad07d3b7b620837a1
polling-bot-4000 <developers@democracyclub.org.uk>
2022-04-20 12:48:39 +0100

    Import script for Wrexham (2022-05-05) (closes #4673)

--- polling_stations/apps/data_importers/management/commands/import_wrexham.py
+++ polling_stations/apps/data_importers/management/commands/import_wrexham.py
@@ -4,47 +4,23 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WRX"
     addresses_name = (
-        "2021-03-31T21:21:48.762291/Wrexham New Democracy_Club__06May2021.tsv"
+        "2022-05-05/2022-04-20T12:48:21.082477/Democracy_Club__05May2022.tsv"
     )
     stations_name = (
-        "2021-03-31T21:21:48.762291/Wrexham New Democracy_Club__06May2021.tsv"
+        "2022-05-05/2022-04-20T12:48:21.082477/Democracy_Club__05May2022.tsv"
     )
-    elections = ["2021-05-06"]
+    elections = ["2022-05-05"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         if record.addressline6 in [
-            "LL13 0AW",
-            "SY14 7LL",
-            "LL13 0SF",
-            "SY14 7LB",
-            "SY13 3BU",
-            "SY13 3DS",
-            "LL13 0DA",
-            "LL13 0DW",
-            "LL13 0LS",
-            "LL13 0TT",
-            "SY13 3DR",
-            "LL14 4LU",
-            "LL14 3BH",
-            "LL14 3BJ",
-            "LL14 3RD",
-            "LL20 7DD",
-            "LL20 7HJ",
-            "LL12 0RY",
-            "LL12 8RH",
-            "LL12 8SG",
-            "LL12 8NT",
-            "LL11 4UY",
-            "LL11 4AP",
-            "LL11 4HB",
-            "LL13 8US",
-            "LL13 0JW",
+            "LL13 0YU",
             "LL13 9EN",
-            "LL13 9ND",
-            "LL13 9NB",
-            "LL11 4AH",
-            "LL13 7TB",
+            "LL11 4UY",
+            "LL13 8US",
+            "LL12 0RY",
+            "LL13 0HP",
+            "LL12 9YG",
         ]:
             return None  # split
 
