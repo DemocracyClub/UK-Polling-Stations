2cbef7f10d2a705172453d957175a6108c788e1e
polling-bot-4000 <developers@democracyclub.org.uk>
2022-04-28 18:59:12 +0100

    Import script for Plymouth (2022-05-05) (closes #4712)

--- polling_stations/apps/data_importers/management/commands/import_plymouth.py
+++ polling_stations/apps/data_importers/management/commands/import_plymouth.py
@@ -4,22 +4,13 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "PLY"
     addresses_name = (
-        "2022-05-05/2022-03-31T10:39:45.450211/Democracy_Club__05May2022.tsv"
+        "2022-05-05/2022-04-28T18:58:10.618180/Democracy_Club__05May2022v2.tsv"
     )
     stations_name = (
-        "2022-05-05/2022-03-31T10:39:45.450211/Democracy_Club__05May2022.tsv"
+        "2022-05-05/2022-04-28T18:58:10.618180/Democracy_Club__05May2022v2.tsv"
     )
     elections = ["2022-05-05"]
     csv_delimiter = "\t"
-
-    def station_record_to_dict(self, record):
-
-        # Chaddlewood Farm Community Centre 80 Chaddlewood District Centre Glen Road Plympton Plymouth PL7 2XS
-        if record.polling_place_id == "5916":
-            record = record._replace(polling_place_easting="256137")
-            record = record._replace(polling_place_northing="56152")
-
-        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
