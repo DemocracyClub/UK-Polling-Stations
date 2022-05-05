38dcdb696e4b40600353c6d50775e1be1f3dfd7b
polling-bot-4000 <developers@democracyclub.org.uk>
2022-05-05 16:26:09 +0100

    Import script for Ryedale (2022-05-05) (closes #4738)

--- polling_stations/apps/data_importers/management/commands/import_ryedale.py
+++ polling_stations/apps/data_importers/management/commands/import_ryedale.py
@@ -3,43 +3,6 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "RYE"
-    addresses_name = (
-        "2022-05-05/2022-04-13T15:03:49.381329/polling_station_export-2022-03-03.csv"
-    )
-    stations_name = (
-        "2022-05-05/2022-04-13T15:03:49.381329/polling_station_export-2022-03-03.csv"
-    )
-    elections = ["2022-05-05"]
-
-    def station_record_to_dict(self, record):
-
-        # Swinton Reading Rooms, Swinton, Malton North YO17 6SG
-        if record.pollingstationnumber == "4":
-            record = record._replace(pollingstationpostcode="YO17 6SL")
-
-        return super().station_record_to_dict(record)
-
-    def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
-        if record.housepostcode in [
-            "YO13 9PT",
-            "YO17 9LB",
-            "YO17 9QY",
-            "YO17 9RL",
-            "YO41 1JF",
-            "YO60 7HQ",
-            "YO60 7NB",
-            "YO62 6JA",
-            "YO62 6PA",
-            "YO62 6PE",
-        ]:
-            return None
-
-        if uprn in [
-            "10007633975",
-            "10007630076",
-            "10002318338",
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
+    addresses_name = "2022-05-05/2022-05-05T16:25:57.178337/polling_station_export-2022-05-05.csv"
+    stations_name = "2022-05-05/2022-05-05T16:25:57.178337/polling_station_export-2022-05-05.csv"
+    elections = ['2022-05-05']
