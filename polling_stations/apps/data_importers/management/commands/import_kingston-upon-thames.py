923ffcf42e1eac5c357ecf44b07e58c1d94977e1
GeoWill <roper.william@gmail.com>
2022-05-27 12:05:07 +0100

    Import script for Kingston upon Thames (2022-06-23)

--- polling_stations/apps/data_importers/management/commands/import_kingston-upon-thames.py
+++ polling_stations/apps/data_importers/management/commands/import_kingston-upon-thames.py
@@ -5,4 +5,4 @@ class Command(BaseDemocracyCountsCsvImporter):
     council_id = "KTT"
     addresses_name = "2022-05-05/2022-04-08T10:23:22.521537/RBK_Polling Districs.csv"
     stations_name = "2022-05-05/2022-04-08T10:23:22.521537/RBK_Polling Stations.csv"
-    elections = ["2022-05-05"]
+    elections = ["2022-06-23"]
