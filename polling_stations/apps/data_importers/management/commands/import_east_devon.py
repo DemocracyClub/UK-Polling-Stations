5f472f53b7752c423bcc474b60c27569c725dac1
polling-bot-4000 <developers@democracyclub.org.uk>
2022-05-31 10:17:15 +0100

    Import script for East Devon (2022-06-23) (closes #4780#issuecomment-1141883920)

--- polling_stations/apps/data_importers/management/commands/import_east_devon.py
+++ polling_stations/apps/data_importers/management/commands/import_east_devon.py
@@ -3,8 +3,8 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "EDE"
-    addresses_name = "2022-06-23/2022-05-30T15:41:34.577745/Democracy_Club__23June2022 (1).tsv"
-    stations_name = "2022-06-23/2022-05-30T15:41:34.577745/Democracy_Club__23June2022 (1).tsv"
+    addresses_name = "2022-06-23/2022-05-31T10:17:07.773032/Democracy_Club__23June2022 new.tsv"
+    stations_name = "2022-06-23/2022-05-31T10:17:07.773032/Democracy_Club__23June2022 new.tsv"
     elections = ['2022-06-23']
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
