ecd147fde224cbd3b5e1a7ece7e584fd66943476
polling-bot-4000 <developers@democracyclub.org.uk>
2022-09-26 11:41:40 +0100

    Import script for Rhondda Cynon Taf (2022-09-29) (closes #4828)

--- polling_stations/apps/data_importers/management/commands/import_rhondda_cynon_taf.py
+++ polling_stations/apps/data_importers/management/commands/import_rhondda_cynon_taf.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 class Command(BaseHalaroseCsvImporter):
     council_id = "RCT"
     addresses_name = (
-        "2022-05-05/2022-03-17T12:13:36.470138/polling_station_export-2022-03-17.csv"
+        "2022-09-29/2022-09-26T11:41:22.544794/polling_station_export-2022-09-26.csv"
     )
     stations_name = (
-        "2022-05-05/2022-03-17T12:13:36.470138/polling_station_export-2022-03-17.csv"
+        "2022-09-29/2022-09-26T11:41:22.544794/polling_station_export-2022-09-26.csv"
     )
-    elections = ["2022-05-05"]
+    elections = ["2022-09-29"]
 
     def station_record_to_dict(self, record):
         if record.pollingstationnumber == "124":
@@ -19,35 +19,36 @@ class Command(BaseHalaroseCsvImporter):
             record = record._replace(pollingstationpostcode="CF72 8NS")
 
         if record.pollingstationnumber == "97":
-            # CILFYNYDD & NORTON BRIDGE COMMUNITY CENTRE, CILFYNYDD ROAD, …
-            # Was "CF37 3NR", but that's not on Cilfyndd Road
+            #     # CILFYNYDD & NORTON BRIDGE COMMUNITY CENTRE, CILFYNYDD ROAD, …
+            #     # Was "CF37 3NR", but that's not on Cilfyndd Road
             record = record._replace(pollingstationpostcode="")
 
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         if record.housepostcode in [
-            "CF37 3BS",
-            "CF38 1DR",
             "CF37 1LD",
-            "CF38 2JF",
-            "CF37 3EW",
-            "CF38 2HH",
-            "CF44 0PD",
-            "CF37 1UA",
-            "CF38 2SA",
-            "CF37 1TG",
             "CF44 9TB",
-            "CF40 2SN",
-            "CF44 9DT",
+            "CF37 3BS",
+            "CF44 0PD",
+            "CF38 1DR",
+            "CF37 3EW",
+            "CF38 2SA",
+            "CF38 2HH",
             "CF39 8FA",
+            "CF40 2SN",
             "CF40 2ER",
-            "CF37 4DX",
+            "CF38 2JF",
+            "CF37 1TG",
+            "CF42 6BH",
+            "CF44 9BP",
+            "CF72 9JZ",
             "CF39 8AT",
             "CF38 2JZ",
-            "CF42 6BH",
-            "CF72 9JZ",
+            "CF37 1UA",
+            "CF37 4DX",
             "CF42 6LX",
+            "CF44 9DT",
         ]:
             return None
 
