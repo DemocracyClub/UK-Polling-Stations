40d97b1e43820aaff6d0763d4a2d6a70df46c4ee
Virginia Dooley <vcdooley@gmail.com>
2022-10-17 10:33:20 +0100

    Remove deprecated LANGUAGE_SESSION_KEY
    USE_L10N deprecated for Django 5.0

--- polling_stations/settings/base.py
+++ polling_stations/settings/base.py
@@ -52,10 +52,6 @@ SITE_ID = 1
 # to load the internationalization machinery.
 USE_I18N = True
 
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale.
-USE_L10N = True
-
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
@@ -239,7 +235,7 @@ LANGUAGES = [
     ("cy", "Welsh"),
 ]
 USE_I18N = (True,)
-USE_L10N = (True,)
+
 LOCALE_PATHS = (repo_root("locale"),)
 
 
