d4bbaf38943bbce2930bb714c93c67b96ba18041
Beka Lewis <bekabyx@gmail.com>
2022-10-31 23:37:07 +0000

    Remove harvest test content
    Remove aloe dependencies
    Remove /features dir from data_finder
    Remove old cassettes

--- polling_stations/settings/testing.py
+++ polling_stations/settings/testing.py
@@ -6,9 +6,6 @@ NEXT_CHARISMATIC_ELECTION_DATE = None
 DISABLE_GA = True  # don't log to Google Analytics when we are running tests
 
 INSTALLED_APPS = list(INSTALLED_APPS)  # noqa
-INSTALLED_APPS.append("aloe_django")
-
-NOSE_ARGS = ["--verbosity=2", "--nologcapture", "--nocapture"]
 
 # MIGRATION_MODULES = {app: None for app in INSTALLED_APPS if "django" not in app}
 
