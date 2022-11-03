e2de0800c6a6948e3b630f8924112bc169fe4dd6
polling-bot-4000 <developers@democracyclub.org.uk>
2022-11-03 10:02:27 +0000

    Import script for Rhondda Cynon Taf (2022-11-17) (closes #4852)

--- polling_stations/apps/data_importers/management/commands/import_rhondda_cynon_taf.py
+++ polling_stations/apps/data_importers/management/commands/import_rhondda_cynon_taf.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 class Command(BaseHalaroseCsvImporter):
     council_id = "RCT"
     addresses_name = (
-        "2022-09-29/2022-09-26T11:41:22.544794/polling_station_export-2022-09-26.csv"
+        "2022-11-17/2022-11-03T10:02:02.538154/polling_station_export-2022-11-03.csv"
     )
     stations_name = (
-        "2022-09-29/2022-09-26T11:41:22.544794/polling_station_export-2022-09-26.csv"
+        "2022-11-17/2022-11-03T10:02:02.538154/polling_station_export-2022-11-03.csv"
     )
-    elections = ["2022-09-29"]
+    elections = ["2022-11-17"]
 
     def station_record_to_dict(self, record):
         if record.pollingstationnumber == "124":
@@ -27,28 +27,33 @@ class Command(BaseHalaroseCsvImporter):
 
     def address_record_to_dict(self, record):
         if record.housepostcode in [
-            "CF37 1LD",
-            "CF44 9TB",
             "CF37 3BS",
-            "CF44 0PD",
-            "CF38 1DR",
-            "CF37 3EW",
-            "CF38 2SA",
-            "CF38 2HH",
-            "CF39 8FA",
-            "CF40 2SN",
-            "CF40 2ER",
-            "CF38 2JF",
             "CF37 1TG",
-            "CF42 6BH",
-            "CF44 9BP",
-            "CF72 9JZ",
-            "CF39 8AT",
-            "CF38 2JZ",
-            "CF37 1UA",
-            "CF37 4DX",
-            "CF42 6LX",
+            "CF39 8FA",
+            "CF38 1DR",
+            "CF37 1LD",
             "CF44 9DT",
+            "CF38 2HH",
+            "CF37 4DX",
+            "CF38 2JF",
+            "CF37 3EW",
+            "CF42 6LX",
+            "CF42 6BH",
+            "CF40 2SN",
+            "CF38 2JZ",
+            "CF40 2ER",
+            "CF38 2SA",
+            "CF44 0PD",
+            "CF39 8AT",
+            "CF44 9TB",
+            "CF72 9JZ",
+            "CF37 1UA",
+        ]:
+            return None
+
+        if record.uprn in [
+            "10001300469",
+            "10094101040",
         ]:
             return None
 
