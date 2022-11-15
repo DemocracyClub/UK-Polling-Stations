e7735135a28318f65ce4ae3e549af3127440a14c
GeoWill <roper.william@gmail.com>
2022-11-15 15:23:54 +0000

    Install Whitenoise middleware for static files

--- polling_stations/settings/base.py
+++ polling_stations/settings/base.py
@@ -64,25 +64,27 @@ MEDIA_ROOT = root("assets", "uploads")
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = "/media/"
 
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = "asdasdasdasdasdasdasd"
+
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
 STATIC_ROOT = root("static")
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
 STATIC_URL = "/static/"
 
 # Additional locations of static files
 STATICFILES_DIRS = (root("assets"), root("../node_modules"))
 
-from .static_files import PIPELINE, STATICFILES_FINDERS, STATICFILES_STORAGE  # noqa
+from dc_utils.settings.pipeline import *  # noqa
 
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = "asdasdasdasdasdasdasd"
+
+from .static_files import PIPELINE, STATICFILES_FINDERS  # noqa
+
 
 MIDDLEWARE = (
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -94,6 +96,7 @@ MIDDLEWARE = (
     "whitelabel.middleware.WhiteLabelMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "pollingstations.middleware.BasicAuthMiddleware",
+    "django.middleware.security.SecurityMiddleware",
 )
 
 TEMPLATES = [
