58d92fe43ff98e67e7411a5d91ebd6d7a13cfe91
polling-bot-4000 <developers@democracyclub.org.uk>
2022-11-25 14:34:39 +0000

    Import script for South Kesteven (2022-12-15) (closes #4878)

--- polling_stations/apps/data_importers/management/commands/import_south_kesteven.py
+++ polling_stations/apps/data_importers/management/commands/import_south_kesteven.py
@@ -4,10 +4,10 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SKE"
     addresses_name = (
-        "2022-11-10/2022-10-17T16:55:49.332386/Democracy_Club__10November2022.tsv"
+        "2022-12-15/2022-11-25T14:34:32.124360/Democracy_Club__15December2022.tsv"
     )
     stations_name = (
-        "2022-11-10/2022-10-17T16:55:49.332386/Democracy_Club__10November2022.tsv"
+        "2022-12-15/2022-11-25T14:34:32.124360/Democracy_Club__15December2022.tsv"
     )
-    elections = ["2022-11-10"]
+    elections = ["2022-12-15"]
     csv_delimiter = "\t"
