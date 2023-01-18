193f99cf76897398dc17b7dffcbcd9ea0cd549de
polling-bot-4000 <developers@democracyclub.org.uk>
2023-01-18 11:33:32 +0000

    Import script for North Warwickshire (2023-05-04) (closes #4937)

--- polling_stations/apps/data_importers/management/commands/import_north_warwickshire.py
+++ polling_stations/apps/data_importers/management/commands/import_north_warwickshire.py
@@ -4,38 +4,29 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "NWA"
     addresses_name = (
-        "2021-03-26T10:37:06.912765/North Warwickshire Democracy_Club__06May2021.tsv"
+        "2023-05-04/2023-01-18T11:33:19.887373/Democracy_Club__04May2023.tsv"
     )
     stations_name = (
-        "2021-03-26T10:37:06.912765/North Warwickshire Democracy_Club__06May2021.tsv"
+        "2023-05-04/2023-01-18T11:33:19.887373/Democracy_Club__04May2023.tsv"
     )
-    elections = ["2021-05-06"]
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
-
-    def station_record_to_dict(self, record):
-        # Dordon Village Hall Browns Lane Dordon B78 1LT
-        if record.polling_place_id == "3738":
-            record = record._replace(polling_place_postcode="B78 1TR")
-
-        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
             "10001177416",  # WARREN FARM HOUSE, KINWALSEY LANE, MERIDEN, COVENTRY
-            "100071230625",  # GRIFFIN INN, COLESHILL ROAD, SHUSTOKE, COLESHILL, BIRMINGHAM
+            "100071230930",  # I.S.I LTD, LEA COTTAGE NUNEATON ROAD, ANSLEY
+            "200001812992",  # LEA LODGE, ANSLEY, NUNEATON
+            "10001177408",  # CHURCH TREE BARN, KINWALSEY LANE, MERIDEN, COVENTRY
         ]:
             return None
 
         if record.addressline6 in [
-            "B46 2NX",
-            "CV9 1AX",
-            "CV9 2HS",
-            "B46 1AA",
-            "B46 1BB",
-            "B46 2HS",
-            "CV10 0SL",
+            "B46 1BB",  # split in council data
+            "ST4 7LG",
+            "CV10 9SW",
         ]:
             return None
 
