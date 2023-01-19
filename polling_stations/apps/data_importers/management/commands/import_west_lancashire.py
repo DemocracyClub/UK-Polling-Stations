091e4f1db603ec3088c287c49f21473276abc53b
polling-bot-4000 <developers@democracyclub.org.uk>
2023-01-19 09:29:22 +0000

    Import script for West Lancashire (2023-02-09) (closes #4940)

--- polling_stations/apps/data_importers/management/commands/import_west_lancashire.py
+++ polling_stations/apps/data_importers/management/commands/import_west_lancashire.py
@@ -3,34 +3,6 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WLA"
-    addresses_name = (
-        "2022-05-05/2022-03-24T16:56:09.201434/Democracy_Club__05May2022.tsv"
-    )
-    stations_name = (
-        "2022-05-05/2022-03-24T16:56:09.201434/Democracy_Club__05May2022.tsv"
-    )
-    elections = ["2022-05-05"]
-    csv_delimiter = "\t"
-
-    def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "10012357871",  # WILLOW BARN, GREEN LANE, TARLETON, PRESTON
-            "10012353358",  # 24 SOUTHPORT ROAD, SCARISBRICK, SOUTHPORT
-            "10012348052",  # GREEN LANE FARM, GREEN LANE, TARLETON, PRESTON
-        ]:
-            return None
-
-        if record.addressline6 in [
-            "L40 5BE",
-            "L40 8JB",
-            "L40 9QL",
-            "WN8 8LL",
-            "WN8 6SH",
-            "WN8 7XA",
-            "WN8 0BY",
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
+    addresses_name = "2023-02-09/2023-01-19T09:29:09.859821/Democracy_Club__09February2023.CSV"
+    stations_name = "2023-02-09/2023-01-19T09:29:09.859821/Democracy_Club__09February2023.CSV"
+    elections = ['2023-02-09']
