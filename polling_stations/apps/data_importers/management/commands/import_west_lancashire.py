e428bdfb14d3f05f44760348e7ae96a1baa5909f
Virginia Dooley <vcdooley@gmail.com>
2023-01-30 14:20:08 +0000

    Run black on import script

--- polling_stations/apps/data_importers/management/commands/import_west_lancashire.py
+++ polling_stations/apps/data_importers/management/commands/import_west_lancashire.py
@@ -3,6 +3,10 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WLA"
-    addresses_name = "2023-02-09/2023-01-19T09:29:09.859821/Democracy_Club__09February2023.CSV"
-    stations_name = "2023-02-09/2023-01-19T09:29:09.859821/Democracy_Club__09February2023.CSV"
-    elections = ['2023-02-09']
+    addresses_name = (
+        "2023-02-09/2023-01-19T09:29:09.859821/Democracy_Club__09February2023.CSV"
+    )
+    stations_name = (
+        "2023-02-09/2023-01-19T09:29:09.859821/Democracy_Club__09February2023.CSV"
+    )
+    elections = ["2023-02-09"]
