361ee60e259fe9942bf186eaa7567f3b85c053b1
polling-bot-4000 <developers@democracyclub.org.uk>
2023-02-03 16:08:12 +0000

    LOCALTEST/Import script for West Lancashire (2023-02-09) (closes #4959)

--- polling_stations/apps/data_importers/management/commands/import_west_lancashire.py
+++ polling_stations/apps/data_importers/management/commands/import_west_lancashire.py
@@ -4,9 +4,9 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WLA"
     addresses_name = (
-        "2023-02-09/2023-02-01T10:29:45.813431/Democracy_Club__09February2023.CSV"
+        "2023-02-09/2023-02-03T16:07:38.111136/Democracy_Club__09February2023.CSV"
     )
     stations_name = (
-        "2023-02-09/2023-02-01T10:29:45.813431/Democracy_Club__09February2023.CSV"
+        "2023-02-09/2023-02-03T16:07:38.111136/Democracy_Club__09February2023.CSV"
     )
     elections = ["2023-02-09"]
