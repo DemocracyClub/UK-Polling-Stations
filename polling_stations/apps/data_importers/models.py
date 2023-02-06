e375a82074bca52a94d94a97fc751770563f0897
Virginia Dooley <vcdooley@gmail.com>
2023-02-06 19:44:55 +0000

    Add station_data_file to DataQuality

--- polling_stations/apps/data_importers/models.py
+++ polling_stations/apps/data_importers/models.py
@@ -1,6 +1,7 @@
-from django.db import models
-
+from django.db.models.signals import post_save
 from councils.models import Council
+from django.db import models
+from file_uploads.models import File
 
 
 class DataQuality(models.Model):
@@ -19,9 +20,9 @@ class DataQuality(models.Model):
     num_stations = models.IntegerField(default=0)
     num_districts = models.IntegerField(default=0)
     num_addresses = models.IntegerField(default=0)
-
-
-from django.db.models.signals import post_save
+    station_data_file = models.ForeignKey(
+        File, default=None, null=True, on_delete=models.SET_NULL
+    )
 
 
 def council_saved(sender, **kwargs):
