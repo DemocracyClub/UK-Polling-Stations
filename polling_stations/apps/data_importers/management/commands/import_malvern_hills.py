5db6870d5d863f215d70fe0cfc23afca0a921826
polling-bot-4000 <developers@democracyclub.org.uk>
2023-02-07 10:13:41 +0000

    Import script for Malvern Hills (2023-05-04) (closes #4967)

--- polling_stations/apps/data_importers/management/commands/import_malvern_hills.py
+++ polling_stations/apps/data_importers/management/commands/import_malvern_hills.py
@@ -3,28 +3,20 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "MAV"
-    addresses_name = "2021-03-03T13:39:58.634524/Democracy_Club__06May2021.tsv"
-    stations_name = "2021-03-03T13:39:58.634524/Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
+    addresses_name = (
+        "2023-05-04/2023-02-07T10:13:27.529328/Democracy_Club__04May2023.tsv"
+    )
+    stations_name = (
+        "2023-05-04/2023-02-07T10:13:27.529328/Democracy_Club__04May2023.tsv"
+    )
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
-        if record.polling_place_id == "11488":
-            record = record._replace(polling_place_easting="379264")
-            record = record._replace(polling_place_northing="246303")
-
-        # Council not using Welland Village Hall instead of
-        # Castlemorton Village Hall Castlemorton Malvern
-        if record.polling_place_id == "11382":
+        # St Mary`s Church Hall Sherrards Green Road Malvern
+        if record.polling_place_id == "13513":
             record = record._replace(
-                polling_place_name="Welland Village Hall",
-                polling_place_address_1="Welland",
-                polling_place_address_2="Malvern",
-                polling_place_address_3="",
-                polling_place_address_4="",
-                polling_place_postcode="WR13 6AJ",
-                polling_place_easting="379639",
-                polling_place_northing="240002",
+                polling_place_easting="379264", polling_place_northing="246303"
             )
 
         return super().station_record_to_dict(record)
@@ -33,19 +25,29 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
+            "10093062320",  # LOG CABIN AT 2 STRAWBERRY COTTAGE A443 NEWNHAM BRIDGE, NEWNHAM BRIDGE
+            "10014087730",  # 1 STRAWBERRY COTTAGE, NEWNHAM BRIDGE, TENBURY WELLS
+            "10014087731",  # 2 STRAWBERRY COTTAGE, NEWNHAM BRIDGE, TENBURY WELLS
+            "10014087728",  # KEEPERS COTTAGE, NEWNHAM BRIDGE, TENBURY WELLS
+            "10014088277",  # 2 BRICKYARD COTTAGES, FEATHERBED LANE, NEWNHAM BRIDGE, TENBURY WELLS"
+            "10014088278",  # 1 BRICKYARD COTTAGES, FEATHERBED LANE, NEWNHAM BRIDGE, TENBURY WELLS
+            "10014090439",  # BAKERS COTTAGE, WICHENFORD, WORCESTER
+            "10024321016",  # THE CARTHOUSE, BIRCHLEY MILL, BOCKLETON ROAD, OLDWOOD, TENBURY WELLS
+            "100120595356",  # HUNTERS LODGE, MAIN ROAD, KEMPSEY, WORCESTER
             "100121268209",  # CHERRY TREE COTTAGE MOSELEY ROAD, HALLOW
+            "10014090474",  # PENTOWAN, WICHENFORD, WORCESTER
         ]:
             return None
 
         if record.addressline6 in [
-            "WR5 3PA",
-            "WR6 6YY",
-            "WR15 8JF",
-            "WR15 8DP",
-            "WR2 6RB",
+            "WR1 42WE",
+            "WR2 4TD",
             "WR14 4JY",
+            "WR2 6RB",
+            "WR6 6BH",
+            "WR15 8DP",
+            "WR8 0AN",
         ]:
             return None
-        rec = super().address_record_to_dict(record)
 
-        return rec
+        return super().address_record_to_dict(record)
