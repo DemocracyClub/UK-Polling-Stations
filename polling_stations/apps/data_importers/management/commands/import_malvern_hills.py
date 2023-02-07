e570d801194010c4b2d8b033a3c5b37ce0afce9a
polling-bot-4000 <developers@democracyclub.org.uk>
2023-02-07 10:13:41 +0000

    Import script for Malvern Hills (2023-05-04) (closes #4967)

--- polling_stations/apps/data_importers/management/commands/import_malvern_hills.py
+++ polling_stations/apps/data_importers/management/commands/import_malvern_hills.py
@@ -3,50 +3,11 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "MAV"
-    addresses_name = "2021-03-03T13:39:58.634524/Democracy_Club__06May2021.tsv"
-    stations_name = "2021-03-03T13:39:58.634524/Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
+    addresses_name = (
+        "2023-05-04/2023-02-07T10:13:27.529328/Democracy_Club__04May2023.tsv"
+    )
+    stations_name = (
+        "2023-05-04/2023-02-07T10:13:27.529328/Democracy_Club__04May2023.tsv"
+    )
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
-
-    def station_record_to_dict(self, record):
-        if record.polling_place_id == "11488":
-            record = record._replace(polling_place_easting="379264")
-            record = record._replace(polling_place_northing="246303")
-
-        # Council not using Welland Village Hall instead of
-        # Castlemorton Village Hall Castlemorton Malvern
-        if record.polling_place_id == "11382":
-
-            record = record._replace(
-                polling_place_name="Welland Village Hall",
-                polling_place_address_1="Welland",
-                polling_place_address_2="Malvern",
-                polling_place_address_3="",
-                polling_place_address_4="",
-                polling_place_postcode="WR13 6AJ",
-                polling_place_easting="379639",
-                polling_place_northing="240002",
-            )
-
-        return super().station_record_to_dict(record)
-
-    def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "100121268209",  # CHERRY TREE COTTAGE MOSELEY ROAD, HALLOW
-        ]:
-            return None
-
-        if record.addressline6 in [
-            "WR5 3PA",
-            "WR6 6YY",
-            "WR15 8JF",
-            "WR15 8DP",
-            "WR2 6RB",
-            "WR14 4JY",
-        ]:
-            return None
-        rec = super().address_record_to_dict(record)
-
-        return rec
