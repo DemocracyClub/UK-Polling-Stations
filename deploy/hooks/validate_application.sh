f7f6f3db8f0888197b04bdd9b47297981ef3ab88
GeoWill <roper.william@gmail.com>
2023-02-13 12:38:56 +0000

    Only validate on gunicorn service

--- deploy/hooks/validate_application.sh
+++ deploy/hooks/validate_application.sh
@@ -3,5 +3,5 @@ set -xeuo pipefail
 
 # if either check returns a non-zero exit code, return exit code 1 to
 # ensure CodeDeploy recognises the validation failed
-systemctl is-active --quiet polling_stations_db_replication || exit 1
+#systemctl is-active --quiet polling_stations_db_replication || exit 1
 systemctl is-active --quiet polling_stations_gunicorn.socket || exit 1
