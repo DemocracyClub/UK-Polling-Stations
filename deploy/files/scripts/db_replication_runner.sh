00e1bd4e8bb98ea66b8cca7f9b1a8061a0a53285
GeoWill <roper.william@gmail.com>
2023-02-20 14:27:05 +0000

    Postgres configs for replication and service

--- deploy/files/scripts/db_replication_runner.sh
+++ deploy/files/scripts/db_replication_runner.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 set -xeEo pipefail
 
+
+# Optimise DB for replication
+su -c "/var/www/polling_stations/postgres_writes_config.sh" - polling_stations
+sudo service postgresql restart
+
+# Do the replication
 su -c "/var/www/polling_stations/setup_db_replication.sh" - polling_stations
+
+# Optimise DB for Reads
+su -c "/var/www/polling_stations/postgres_reads_config.sh" - polling_stations
+sudo service postgresql restart
+
 sudo service polling_stations_gunicorn reload
