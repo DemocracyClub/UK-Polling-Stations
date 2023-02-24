a20227b3714299b257df8f2f7fe9deb21477219f
polling-bot-4000 <developers@democracyclub.org.uk>
2023-02-24 07:53:21 +0000

    TEST/Import script for Aberdeenshire (2023-02-23) (closes #5022)

--- polling_stations/apps/data_importers/management/commands/import_aberdeenshire.py
+++ polling_stations/apps/data_importers/management/commands/import_aberdeenshire.py
@@ -4,9 +4,9 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 class Command(BaseHalaroseCsvImporter):
     council_id = "ABD"
     addresses_name = (
-        "2023-02-23/2023-02-24T07:46:46.532930/polling_station_export-2023-02-23.csv"
+        "2023-02-23/2023-02-24T07:52:57.895726/polling_station_export-2023-02-23.csv"
     )
     stations_name = (
-        "2023-02-23/2023-02-24T07:46:46.532930/polling_station_export-2023-02-23.csv"
+        "2023-02-23/2023-02-24T07:52:57.895726/polling_station_export-2023-02-23.csv"
     )
     elections = ["2023-02-23"]
