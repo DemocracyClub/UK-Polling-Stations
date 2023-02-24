0d5fcdbea4c4d8a837f80aec360bb7e21c9c3621
polling-bot-4000 <developers@democracyclub.org.uk>
2023-02-24 13:04:51 +0000

    Import script for City of Edinburgh (2023-03-09) (closes #5026)

--- polling_stations/apps/data_importers/management/commands/import_edinburgh.py
+++ polling_stations/apps/data_importers/management/commands/import_edinburgh.py
@@ -1,39 +1,8 @@
-from data_importers.geo_utils import fix_bad_polygons
-from data_importers.github_importer import BaseGitHubImporter
+from data_importers.management.commands import BaseHalaroseCsvImporter
 
 
-class Command(BaseGitHubImporter):
-    srid = 4326
+class Command(BaseHalaroseCsvImporter):
     council_id = "EDH"
-    elections = ["2022-05-05"]
-    scraper_name = "wdiv-scrapers/DC-PollingStations-Edinburgh"
-    geom_type = "geojson"
-
-    def district_record_to_dict(self, record):
-        poly = self.extract_geometry(record, self.geom_type, self.get_srid("districts"))
-
-        return {
-            "internal_council_id": record["Code2016"],
-            "name": record["NEWWARD"] + " - " + record["Code2016"],
-            "area": poly,
-            "polling_station_id": record["Code2016"],
-        }
-
-    def station_record_to_dict(self, record):
-        location = self.extract_geometry(
-            record, self.geom_type, self.get_srid("stations")
-        )
-
-        station = {
-            "internal_council_id": record["System_Ide"],
-            "address": "\n".join(
-                [record[field] for field in ["Address_Li", "Address__1", "Address__2"]]
-            ),
-            "postcode": record["Post_Code"],
-            "location": location,
-        }
-
-        return station
-
-    def post_import(self):
-        fix_bad_polygons()
+    addresses_name = "2023-03-09/2023-02-24T13:04:29.352950/Eros_SQL_Output003.csv"
+    stations_name = "2023-03-09/2023-02-24T13:04:29.352950/Eros_SQL_Output003.csv"
+    elections = ["2023-03-09"]
