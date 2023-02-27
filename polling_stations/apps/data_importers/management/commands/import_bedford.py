94c0ebc28ab3099a15470d14802e4ca023903eac
polling-bot-4000 <developers@democracyclub.org.uk>
2023-02-27 10:52:15 +0000

    TEST/Import script for Bedford (2023-05-04) (closes #5036)

--- polling_stations/apps/data_importers/management/commands/import_bedford.py
+++ polling_stations/apps/data_importers/management/commands/import_bedford.py
@@ -3,6 +3,6 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "BDF"
-    addresses_name = "2023-05-04/2023-02-27T10:45:48.799065/Bedford Polling Districts - Democracy Club.csv"
-    stations_name = "2023-05-04/2023-02-27T10:45:48.799065/Bedford Polling Stations for Democracy Club.csv"
+    addresses_name = "2023-05-04/2023-02-27T10:49:10.104528/Bedford Polling Districts - Democracy Club.csv"
+    stations_name = "2023-05-04/2023-02-27T10:49:10.104528/Bedford Polling Stations for Democracy Club.csv"
     elections = ["2023-05-04"]
