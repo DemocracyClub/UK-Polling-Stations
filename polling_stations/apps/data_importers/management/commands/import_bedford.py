559724970b9b95d5abed799124298c034324c6b0
polling-bot-4000 <developers@democracyclub.org.uk>
2023-02-27 10:40:29 +0000

    TEST/Import script for Bedford (2023-05-04) (closes #5036)

--- polling_stations/apps/data_importers/management/commands/import_bedford.py
+++ polling_stations/apps/data_importers/management/commands/import_bedford.py
@@ -3,29 +3,6 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "BDF"
-    addresses_name = (
-        "2021-03-17T12:11:37.989847/Bedford Polling Districts - Democracy Club.csv"
-    )
-    stations_name = (
-        "2021-03-17T12:11:37.989847/Bedford Polling Stations for Democracy Club.csv"
-    )
-    elections = ["2021-05-06"]
-
-    def station_record_to_dict(self, record):
-        # KEMPSTON WEST METHODIST CHURCH Carried forward from local.2019-05-02
-        if record.stationcode == "44":
-            record = record._replace(xordinate="502614")
-            record = record._replace(yordinate="247440")
-
-        return super().station_record_to_dict(record)
-
-    def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
-
-        if record.postcode in ["MK43 9AH"]:
-            return None
-
-        if uprn in ["100080991603"]:
-            return None
-
-        return super().address_record_to_dict(record)
+    addresses_name = "2023-05-04/2023-02-27T10:40:20.843815/Bedford Polling Districts - Democracy Club.csv"
+    stations_name = "2023-05-04/2023-02-27T10:40:20.843815/Bedford Polling Stations for Democracy Club.csv"
+    elections = ["2023-05-04"]
