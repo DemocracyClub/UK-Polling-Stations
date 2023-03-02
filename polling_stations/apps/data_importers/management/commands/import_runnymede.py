edd138cbb055885c53d22ace769b4837428471c0
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-02 14:04:11 +0000

    Import script for Runnymede (2023-05-04) (closes #5070)

--- polling_stations/apps/data_importers/management/commands/import_runnymede.py
+++ polling_stations/apps/data_importers/management/commands/import_runnymede.py
@@ -4,16 +4,35 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "RUN"
     addresses_name = (
-        "2022-05-05/2022-03-03T14:49:14.647146/Democracy_Club__05May2022.tsv"
+        "2023-05-04/2023-03-02T14:04:03.113510/Democracy_Club__04May2023.tsv"
     )
     stations_name = (
-        "2022-05-05/2022-03-03T14:49:14.647146/Democracy_Club__05May2022.tsv"
+        "2023-05-04/2023-03-02T14:04:03.113510/Democracy_Club__04May2023.tsv"
     )
-    elections = ["2022-05-05"]
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
-        if record.addressline6 in ["KT16 8QD", "KT16 8AG"]:
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "10091662635",  # 3 CLIFTON GRANGE, BLACKPOOL ROAD, CLIFTON, PRESTON
+            "10092963299",  # FLAT AT 79 STATION ROAD, ADDLESTONE
+            "10095815752",  # FLAT 50, AVIATOR HOUSE, 227 STATION ROAD, ADDLESTONE
+            "10095815848",  # FLAT 136, AVIATOR HOUSE, 227 STATION ROAD, ADDLESTONE
+            "10095815743",  # FLAT 32, AVIATOR HOUSE, 227 STATION ROAD, ADDLESTONE
+            "200004029979",  # LODGE, ST. GEORGES COLLEGE, WEYBRIDGE ROAD, ADDLESTONE
+            "100062604551",  # ST. GEORGES COLLEGE, WEYBRIDGE ROAD, ADDLESTONE
+        ]:
+            return None
+        if record.addressline6 in [
+            # splits
+            "KT16 8QD",
+            "KT16 8AG",
+            "KT15 2US",
+            "KT15 2DB",
+            "KT16 0AB",  # 10 WATERY LANE, CHERTSEY
+        ]:
             return None
 
         return super().address_record_to_dict(record)
