9ba97be5df882b98f38826d08cefea1022888322
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-02 16:39:45 +0000

    Import script for South Norfolk (2023-05-04) (closes #5079)

--- polling_stations/apps/data_importers/management/commands/import_south_norfolk.py
+++ polling_stations/apps/data_importers/management/commands/import_south_norfolk.py
@@ -3,12 +3,16 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "SNO"
-    addresses_name = "2021-03-04T15:54:30.296900/South Norfolk Democracy Club - Polling Districts - Election ID 7 County Council.csv"
-    stations_name = "2021-03-04T15:54:30.296900/South Norfolk Democracy Club - Polling Stations - Election ID 7 County Council.csv"
-    elections = ["2021-05-06"]
+    addresses_name = "2023-05-04/2023-03-02T16:39:18.229053/DemocracyClub-PollingDistricts-May2023-1.csv"
+    stations_name = "2023-05-04/2023-03-02T16:39:18.229053/DemocracyClub-PollingStations-May2023-1.csv"
+    elections = ["2023-05-04"]
+
+    def station_record_to_dict(self, record):
+        if record.stationcode.startswith("B"):
+            return None
+        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-        if record.postcode in ["IP21 4QR", "IP22 4JW", "NR18 0TR"]:
+        if record.stationcode.startswith("B"):
             return None
-
         return super().address_record_to_dict(record)
