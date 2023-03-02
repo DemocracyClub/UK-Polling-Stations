5eaec16622255e3a9469bb7553de7a21407a3a19
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-02 08:28:44 +0000

    Import script for West Lindsey (2023-05-04) (closes #5060)

--- polling_stations/apps/data_importers/management/commands/import_west_lindsey.py
+++ polling_stations/apps/data_importers/management/commands/import_west_lindsey.py
@@ -4,35 +4,43 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WLI"
     addresses_name = (
-        "2021-04-16T11:36:59.020893/West Lindsay Democracy_Club__06May2021 (1).CSV"
+        "2023-05-04/2023-03-02T08:28:35.838932/Democracy_Club__04May2023.tsv"
     )
     stations_name = (
-        "2021-04-16T11:36:59.020893/West Lindsay Democracy_Club__06May2021 (1).CSV"
+        "2023-05-04/2023-03-02T08:28:35.838932/Democracy_Club__04May2023.tsv"
     )
-    elections = ["2021-05-06"]
-    csv_delimiter = ","
+    elections = ["2023-05-04"]
     csv_encoding = "windows-1252"
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "10090698579",  # HORSESHOE BARN, LISSINGLEY LANE, LISSINGTON, LINCOLN
+            "100061614139",  # KINGSLEY, CATTESHALL LANE, GODALMING
+            "10034686366",  # THE COTTAGE, LAUGHTON WOOD CORNER, LAUGHTON, GAINSBOROUGH
+            "10034686362",  # PINE VIEW COTTAGE, LAUGHTON WOOD CORNER, LAUGHTON, GAINSBOROUGH
+            "100030955999",  # ELLERS FARM, BLYTON CARR, GAINSBOROUGH
+            "100032029197",  # EAST LODGE, THE BELT ROAD, GAINSBOROUGH
+            "10034697585",  # THORNOCK GROVE FARM COTTAGE, THE BELT ROAD, GAINSBOROUGH
+            "100032029197",  # EAST LODGE, THE BELT ROAD, GAINSBOROUGH
+            "10013815969",  # WEST VIEW FARM, STOW PARK, LINCOLN
             "10090696854",  # HORIZON BARN, BULLY HILL TOP, TEALBY, MARKET RASEN
-            "10013812180",  # 1 NETTLETON PARK MOORTOWN ROAD, NETTLETON, MARKET RASEN
+            "200001151020",  # HILL CREST, BULLY HILL TOP, TEALBY, MARKET RASEN
+            "10013810973",  # WOLD VIEW HOUSE, BULLY HILL TOP, TEALBY, MARKET RASEN
+            "200001151019",  # HAZE HIGH, BULLY HILL TOP, TEALBY, MARKET RASEN
+            "10013811352",  # SPRINGFIELD, BLEASBY MOOR, MARKET RASEN
+            "10013811351",  # THE SANCTUARY, BLEASBY MOOR, MARKET RASEN
+            "10013810009",  # INGS FARM, TOFT NEXT NEWTON, MARKET RASEN
+            "10034701979",  # NETTLETON GAP, MOORTOWN ROAD, NETTLETON, MARKET RASEN
+            "10013809476",  # SWINTHORPE HOUSE, SNARFORD, MARKET RASEN
+            "100030954683",  # WEST WHARTON FARM, MILL LANE, MORTON, GAINSBOROUGH
+            "10013809617",  # GREENFIELD HOUSE, LISSINGLEY LANE, LISSINGTON, LINCOLN
+            "10013809613",  # LISSINGLEA HOUSE FARM, LISSINGLEY LANE, LISSINGTON, LINCOLN
         ]:
             return None
 
-        if record.addressline6 in [
-            "LN7 6JD",
-            "LN1 2XG",
-            "LN1 2PL",
-            "LN2 2YX",
-            "LN8 3SF",
-            "LN2 3PD",
-            "LN2 3RS",
-            "DN21 2PL",
-        ]:
+        if record.addressline6 in ["DN21 1HL", "DN21 1TU", "LN8 3SU", "LN2 3PD"]:
             return None
 
         return super().address_record_to_dict(record)
