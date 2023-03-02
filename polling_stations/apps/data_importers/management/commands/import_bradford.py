1110f4a7b52b3e97672999ff761fa3ce9c796934
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-02 16:22:15 +0000

    TEST/Import script for Bradford (2023-05-04) (closes #5076)

--- polling_stations/apps/data_importers/management/commands/import_bradford.py
+++ polling_stations/apps/data_importers/management/commands/import_bradford.py
@@ -4,53 +4,10 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "BRD"
     addresses_name = (
-        "2022-05-05/2022-03-10T15:15:55.571130/Democracy_Club__05May2022-2.tsv"
+        "2023-05-04/2023-03-02T16:21:41.882014/Democracy_Club__04May2023.tsv"
     )
     stations_name = (
-        "2022-05-05/2022-03-10T15:15:55.571130/Democracy_Club__05May2022-2.tsv"
+        "2023-05-04/2023-03-02T16:21:41.882014/Democracy_Club__04May2023.tsv"
     )
-    elections = ["2022-05-05"]
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
-
-    def station_record_to_dict(self, record):
-        # St John`s Church South Street Keighley BD22 7BU
-        if record.polling_place_id == "28550":
-            record = record._replace(polling_place_easting="405613")
-            record = record._replace(polling_place_northing="439897")
-
-        return super().station_record_to_dict(record)
-
-    def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "100051942648",
-            "10010571811",
-            "10010571812",
-            "10090402080",
-            "10090402777",
-            "10090978516",
-            "100051268913",
-            "10090404116",
-            "100051268917",
-            "10091675520",  # 12 BRIGGS WAY, BRADFORD
-            "10091675521",  # 10 BRIGGS WAY, BRADFORD
-        ]:
-            return None
-
-        if record.addressline6 in [
-            "BD13 3SD",
-            "BD1 1NE",
-            "BD1 1SX",
-            "BD16 1NT",
-            "BD15 7WB",
-            "BD7 4RA",
-            "LS29 6QJ",
-            "BD9 6AS",
-            "BD10 8LL",
-            "BD21 5QF",
-            "BD1 4AB",
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
