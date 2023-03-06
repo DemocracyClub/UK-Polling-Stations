16946b93b8ee70f52d7822c64060a557f369b99e
Virginia Dooley <vcdooley@gmail.com>
2023-03-06 12:50:14 +0000

    Refactor to simplify mutiple upload attempts

--- polling_stations/apps/file_uploads/api.py
+++ polling_stations/apps/file_uploads/api.py
@@ -20,48 +20,42 @@ class UploadSerializer(serializers.ModelSerializer):
 
     @transaction.atomic
     def create(self, validated_data):
-        def save(validated_data):
-            upload = Upload.objects.create(
+        def update_file_set(validated_data):
+            upload = Upload.objects.get(
                 gss=validated_data["gss"],
                 timestamp=validated_data["timestamp"],
                 election_date=validated_data["election_date"],
-                github_issue=validated_data["github_issue"],
             )
             for f in validated_data["file_set"]:
                 File.objects.create(upload=upload, **f)
             return upload
 
-        try:
-            existing_upload = Upload.objects.get(
-                gss=validated_data["gss"],
-                election_date=validated_data["election_date"],
-                timestamp=validated_data["timestamp"],
-            )
-            existing_files = File.objects.filter(upload=existing_upload)
-            if len(validated_data["file_set"]) > len(existing_files):
-                # Only overwrite the old data with the new report
-                # if the new one has more stuff in it
-                existing_files.delete()
-                existing_upload.delete()
-                upload = save(validated_data)
-            else:
-                # In the situation where we're processing multiple files
-                # if there's already an upload and it has more files in it
-                # than the new one we're trying to write, assume an
-                # out-of-order delivery has happened (entirely possible)
-                # and leave the existing data in place.
-                upload = existing_upload
-        except Upload.DoesNotExist:
-            upload = save(validated_data)
+        existing_upload = Upload.objects.get(
+            gss=validated_data["gss"],
+            election_date=validated_data["election_date"],
+            timestamp=validated_data["timestamp"],
+        )
+        existing_files = File.objects.filter(upload=existing_upload)
+        if len(validated_data["file_set"]) > len(existing_files):
+            # Only overwrite the old data with the new report
+            # if the new one has more stuff in it
+            existing_files.delete()
+            upload = update_file_set(validated_data)
+        else:
+            # In the situation where we're processing multiple files
+            # if there's already an upload and it has more files in it
+            # than the new one we're trying to write, assume an
+            # out-of-order delivery has happened (entirely possible)
+            # and leave the existing data in place.
+            upload = existing_upload
 
         file_set = upload.file_set
         if file_set.exists():
             if (
                 file_set.first().ems == "Democracy Counts" and file_set.count() == 2
             ) or file_set.first().ems != "Democracy Counts":
-                user = self.context["request"].user
                 upload.make_pull_request()
-                upload.send_confirmation_email(user=user)
+                upload.send_confirmation_email()
         else:
             upload.send_error_email()
         return upload
