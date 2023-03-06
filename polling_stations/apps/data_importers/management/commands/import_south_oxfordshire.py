8581e2953670ee544cf063e3560f6df7904acc4b
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-06 12:19:04 +0000

    Import script for South Oxfordshire (2023-05-04) (closes #5102)

--- polling_stations/apps/data_importers/management/commands/import_south_oxfordshire.py
+++ polling_stations/apps/data_importers/management/commands/import_south_oxfordshire.py
@@ -3,36 +3,16 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SOX"
-    addresses_name = "2021-03-18T11:05:52.448256/UPDATED Democracy_Club__06May2021.TSV"
-    stations_name = "2021-03-18T11:05:52.448256/UPDATED Democracy_Club__06May2021.TSV"
-    elections = ["2021-05-06"]
-    csv_delimiter = "\t"
+    addresses_name = "2023-05-04/2023-03-06T12:18:16.065364/South Oxfordshire District Council - Democracy_Club__04May2023.TSV"
+    stations_name = "2023-05-04/2023-03-06T12:18:16.065364/South Oxfordshire District Council - Democracy_Club__04May2023.TSV"
+    elections = ["2023-05-04"]
     csv_encoding = "windows-1252"
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "100121305054",  # THE OXFORDSHIRE, RYCOTE LANE, MILTON COMMON, THAME
-            "100120895687",  # THE OLD PUMP HOUSE, KINGSEY ROAD, THAME
-        ]:
-            return None
-
         if record.addressline6 in [
-            "OX11 8JW",
-            "OX11 9EW",
-            "RG9 2HZ",
-            "RG9 4PX",
-            "OX11 7PT",
-            "OX11 6DE",
-            "OX10 0FJ",
-            "RG8 0BS",
-            "OX11 8RE",
-            "OX9 2AD",
-            "OX11 8AU",
-            "OX11 7DT",
-            "OX11 6JL",
-            "OX11 6ES",
+            "OX11 7TP",
+            "OX11 7SE",
         ]:
             return None
 
@@ -40,7 +20,7 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
     def station_record_to_dict(self, record):
         # Thame Snooker Club 1a Saint Andrew Court Wellington Street Thame OX9 3WT
-        if record.polling_place_id == "11459":
+        if record.polling_place_id == "15012":
             record = record._replace(polling_place_easting="")
             record = record._replace(polling_place_northing="")
 
