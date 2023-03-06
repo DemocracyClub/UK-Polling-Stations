0b935d348bacc7a626ce5753ba6db6e285892710
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-06 13:52:27 +0000

    Import script for Waverley (2023-05-04) (closes #5104)

--- polling_stations/apps/data_importers/management/commands/import_waverley.py
+++ polling_stations/apps/data_importers/management/commands/import_waverley.py
@@ -3,36 +3,23 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WAE"
-    addresses_name = "2021-03-01T10:16:29.232903/Democracy_Club__06May2021.tsv"
-    stations_name = "2021-03-01T10:16:29.232903/Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
-    csv_delimiter = "\t"
+    addresses_name = (
+        "2023-05-04/2023-03-06T13:52:11.644072/Democracy_Club__04May2023.CSV"
+    )
+    stations_name = (
+        "2023-05-04/2023-03-06T13:52:11.644072/Democracy_Club__04May2023.CSV"
+    )
+    elections = ["2023-05-04"]
 
     def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "100061614139",  # KINGSLEY, CATTESHALL LANE, GODALMING
-            "200001293683",  # 1 GREEN LANE VILLAS, GREEN LANE, CHURT, FARNHAM
-            "100061610563",  # 75 UPPER HALE ROAD, FARNHAM
-        ]:
-            return None
-
         if record.addressline6 in [
+            "GU8 5TE",
+            "GU9 0NZ",
+            "GU10 4BT",
+            "GU9 8EU",
             "GU9 9JT",
-            "GU9 0HR",
-            "GU10 2JT",
-            "GU8 4BH",
-            "GU7 3LG",
-            "GU7 1LN",
+            "RH12 3ZD",
         ]:
             return None
 
         return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # Clock Barn Hall, Clock Barn Farm, Hambledon Road, Busbrudge, Godalming
-        if record.polling_place_id == "4959":
-            record = record._replace(polling_place_address_3="Busbridge")
-
-        return super().station_record_to_dict(record)
