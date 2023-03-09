3d80240e8e9adc1fcf5e215dc47707375f1ab472
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-09 15:35:50 +0000

    Import script for Brighton and Hove (2023-05-04) (closes #5174)

--- polling_stations/apps/data_importers/management/commands/import_brighton_hove.py
+++ polling_stations/apps/data_importers/management/commands/import_brighton_hove.py
@@ -4,10 +4,23 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "BNH"
     addresses_name = (
-        "2022-05-05/2022-03-18T16:35:41.044948/Democracy_Club__05May2022.tsv"
+        "2023-05-04/2023-03-09T15:35:23.204420/Democracy_Club__04May2023.tsv"
     )
     stations_name = (
-        "2022-05-05/2022-03-18T16:35:41.044948/Democracy_Club__05May2022.tsv"
+        "2023-05-04/2023-03-09T15:35:23.204420/Democracy_Club__04May2023.tsv"
     )
-    elections = ["2022-05-05"]
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
+
+    def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "22057280",  # 2 COMET WAY, WROUGHTON, SWINDON
+        ]:
+            return None
+
+        if record.addressline6 in ["BN1 8NF", "BN2 9PA", "BN1 3AE"]:  # splits
+            return None
+
+        return super().address_record_to_dict(record)
