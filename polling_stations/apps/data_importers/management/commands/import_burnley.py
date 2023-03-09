65f05d27e4df09017b15f2b8ccd04f0494ab363b
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-09 13:54:00 +0000

    Import script for Burnley (2023-05-04) (closes #5158)

--- polling_stations/apps/data_importers/management/commands/import_burnley.py
+++ polling_stations/apps/data_importers/management/commands/import_burnley.py
@@ -4,54 +4,45 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "BUN"
     addresses_name = (
-        "2022-05-05/2022-03-30T12:24:27.611217/Democracy_Club__05May2022.CSV"
+        "2023-05-04/2023-03-15T16:01:16.960415/Democracy_Club__04May2023.CSV"
     )
     stations_name = (
-        "2022-05-05/2022-03-30T12:24:27.611217/Democracy_Club__05May2022.CSV"
+        "2023-05-04/2023-03-15T16:01:16.960415/Democracy_Club__04May2023.CSV"
     )
-    elections = ["2022-05-05"]
+    elections = ["2023-05-04"]
 
     def station_record_to_dict(self, record):
         # Rosewood Primary School Moorland Road Entrance Burnley
-        if record.polling_place_id == "5161":
+        if record.polling_place_id == "5399":
             record = record._replace(polling_place_postcode="BB11 2PH")
             record = record._replace(polling_place_easting=383468)
             record = record._replace(polling_place_northing=431243)
 
         # St Matthews Church Hall Albion Street Burnley
-        if record.polling_place_id == "5174":
+        if record.polling_place_id == "5413":
             record = record._replace(polling_place_postcode="BB11 4JJ")
             record = record._replace(polling_place_easting=383312)
             record = record._replace(polling_place_northing=431998)
 
         # Dorset Street Entrance Rosegrove Infants School Dorset Street Burnley
-        if record.polling_place_id == "5142":
+        if record.polling_place_id == "5379":
             record = record._replace(polling_place_postcode="BB12 6HW")
             record = record._replace(polling_place_easting=381471)
             record = record._replace(polling_place_northing=432575)
 
-        # Burnley Football Club 1882 Lounge Harry Potts Way Burnley
-        if record.polling_place_id == "5265":
-            record = record._replace(polling_place_postcode="BB10 4BX")
-
         # St Cuthbert`s Community Hall Sharp Street Burnley BB10 1UG
-        if record.polling_place_id == "5246":
+        if record.polling_place_id == "5484":
             record = record._replace(polling_place_postcode="BB10 1UJ")
-
-        # Middlesex Over 50s Social Centre, Middlesex Avenue, Burnley
-        if record.polling_place_id == "5287":
-            record = record._replace(polling_place_postcode="BB12 6AA")
 
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         if record.addressline6 in [
-            "BB10 3BD",
-            "BB10 3PF",
-            "BB11 2QR",
-            "BB12 8EH",
             "BB10 3JY",
-            "BB10 2DT",
+            "BB10 3PF",
+            "BB10 3BD",
+            "BB12 8EH",
+            "BB11 2QR",
         ]:
             return None
 
