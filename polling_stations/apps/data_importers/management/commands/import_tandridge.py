dd1727c4a9190f789fb98ac26c68394a4988369a
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-09 16:53:58 +0000

    Import script for Tandridge (2023-05-04) (closes #5184)

--- polling_stations/apps/data_importers/management/commands/import_tandridge.py
+++ polling_stations/apps/data_importers/management/commands/import_tandridge.py
@@ -3,27 +3,6 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "TAN"
-    addresses_name = (
-        "2022-05-05/2022-03-17T15:54:54.749645/polling_station_export-2022-03-17.csv"
-    )
-    stations_name = (
-        "2022-05-05/2022-03-17T15:54:54.749645/polling_station_export-2022-03-17.csv"
-    )
-    elections = ["2022-05-05"]
-
-    # For some reason the export only has these fields for station address
-    station_address_fields = [
-        "pollingstationname",
-        "pollingstationaddress_1",
-        "pollingstationaddress_2",
-        "pollingstationaddress_3",
-    ]
-
-    def address_record_to_dict(self, record):
-        if record.housepostcode in [
-            "CR3 6HG",
-            "RH7 6JH",
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
+    addresses_name = "2023-05-04/2023-03-09T16:53:45.065935/Eros_SQL_Output001.csv"
+    stations_name = "2023-05-04/2023-03-09T16:53:45.065935/Eros_SQL_Output001.csv"
+    elections = ["2023-05-04"]
