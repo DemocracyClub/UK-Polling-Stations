b3b69a46495c3a2154eac2dd53d15a5b491521a8
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-09 12:06:17 +0000

    Import script for Basildon (2023-05-04) (closes #5144)

--- polling_stations/apps/data_importers/management/commands/import_basildon.py
+++ polling_stations/apps/data_importers/management/commands/import_basildon.py
@@ -3,32 +3,28 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "BAI"
-    addresses_name = "2022-05-05/2022-04-08T13:03:58.514518/Basildon Council - Democracy_Club__05May2022.CSV"
-    stations_name = "2022-05-05/2022-04-08T13:03:58.514518/Basildon Council - Democracy_Club__05May2022.CSV"
-    elections = ["2022-05-05"]
+    addresses_name = (
+        "2023-05-04/2023-03-09T12:06:08.847842/Democracy_Club__04May2023.CSV"
+    )
+    stations_name = (
+        "2023-05-04/2023-03-09T12:06:08.847842/Democracy_Club__04May2023.CSV"
+    )
+    elections = ["2023-05-04"]
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "10090682049",  # FERNDALE, TYE COMMON ROAD, BILLERICAY
-            "100090239089",  # FLAT 1, ST. DAVIDS COURT, LONDON ROAD, BASILDON
             "100091212751",  # PROBATION OFFICE, 1 FELMORES, BASILDON
             "10013352273",  # 17 CHURCH ROAD, LAINDON, BASILDON
         ]:
             return None
 
         if record.addressline6 in [
-            "SS15 5DS",
-            "SS15 5GX",
-            "CM11 2RU",
             "SS15 6PF",
-            "CM11 2JX",
-            "SS16 6PH",
-            "SS12 0AU",
+            "CM11 2RU",
             "CM11 2AD",
             "SS15 5NZ",
-            "SS15 5NS",
         ]:
             return None
 
