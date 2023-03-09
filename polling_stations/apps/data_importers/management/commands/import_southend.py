5af423fd14419f7ef72f306f8741cb13d10781fb
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-09 13:07:40 +0000

    Import script for Southend-on-Sea (2023-05-04) (closes #5150)

--- polling_stations/apps/data_importers/management/commands/import_southend.py
+++ polling_stations/apps/data_importers/management/commands/import_southend.py
@@ -1,30 +1,34 @@
-from data_importers.management.commands import (
-    BaseXpressDCCsvInconsistentPostcodesImporter,
-)
+from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
-class Command(BaseXpressDCCsvInconsistentPostcodesImporter):
+class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SOS"
-
     addresses_name = (
-        "2022-05-05/2022-03-23T11:58:06.809992/Democracy_Club__05May2022.CSV"
+        "2023-05-04/2023-03-09T13:07:25.305816/Democracy_Club__04May2023.tsv"
     )
     stations_name = (
-        "2022-05-05/2022-03-23T11:58:06.809992/Democracy_Club__05May2022.CSV"
+        "2023-05-04/2023-03-09T13:07:25.305816/Democracy_Club__04May2023.tsv"
     )
-    elections = ["2022-05-05"]
+    elections = ["2023-05-04"]
+    csv_delimiter = "\t"
+
+    def station_record_to_dict(self, record):
+        if record.polling_place_id == "7260":
+            record = record._replace(
+                polling_place_easting="586595", polling_place_northing="187653"
+            )
+
+        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         if record.addressline6 in [
-            "SS3 9QH",
-            "SS9 5EW",
-            "SS9 4RP",
-            "SS2 6UY",
-            "SS9 1LN",
-            "SS9 1NH",
             "SS9 1RP",
+            "SS9 1LN",
+            "SS3 9QH",
+            "SS9 4RP",
+            "SS9 1NH",
+            "SS9 5EW",
             "SS9 1QY",
-            "SS9 3BG",
         ]:
             return None
 
