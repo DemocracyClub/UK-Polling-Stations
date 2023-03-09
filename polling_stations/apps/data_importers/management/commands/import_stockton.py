d6360146cac7bf3437f2a8dee0ae09a8c22bd545
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-09 14:43:49 +0000

    Import script for Stockton-on-Tees (2023-05-04) (closes #5164)

--- polling_stations/apps/data_importers/management/commands/import_stockton.py
+++ polling_stations/apps/data_importers/management/commands/import_stockton.py
@@ -3,46 +3,23 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "STT"
-    addresses_name = "2021-03-08T13:26:49.693904/Democracy_Club__06May2021.tsv"
-    stations_name = "2021-03-08T13:26:49.693904/Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
+    addresses_name = (
+        "2023-05-04/2023-03-09T14:43:24.514702/Democracy_Club__04May2023.tsv"
+    )
+    stations_name = (
+        "2023-05-04/2023-03-09T14:43:24.514702/Democracy_Club__04May2023.tsv"
+    )
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
-        if record.addressline6 in ["TS18 3EF", "TS20 2TJ", "TS17 9PA"]:
+        if record.addressline6 in [
+            "TS17 9PA",
+            "TS20 2TJ",
+        ]:
             return None
 
         return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
-        if (
-            record.polling_place_id == "12442"
-        ):  # Kirklevington & Castlelevington Memorial Hall Pump Lane Kirklevington Yarm
-            record = record._replace(polling_place_postcode="TS15 9LQ")
-
-        if (
-            record.polling_place_id == "12555"
-        ):  # Elim Pentecostal Church Ragpath Lane Roseworth Stockton on Tees
-            record = record._replace(polling_place_postcode="TS19 9AT")
-
-        if (
-            record.polling_place_id == "12408"
-        ):  # North Billingham Methodist Church Activity Room - rear of the Church Marsh House Avenue Billingham TS23 3TG
-            record = record._replace(polling_place_postcode="TS23 3ET")
-
-        if (
-            record.polling_place_id == "12409"
-        ):  # North Billingham Methodist Church Front Door Marsh House Avenue Billingham TS23 3TG
-            record = record._replace(polling_place_postcode="TS23 3ET")
-
-        if (
-            record.polling_place_id == "12599"
-        ):  # Oxbridge Christian Fellowship The Apostolic Church Main Entrance 65 Oxbridge Lane Stockton-on-Tees TS18 4DN (copied postcode from address row to postcode row)
-            record = record._replace(polling_place_postcode="TS18 4DN")
-
-        if (
-            record.polling_place_id == "12648"
-        ):  # Eltham Crescent Community Centre Eltham Crescent Thornaby Stockton-on-Tees
-            record = record._replace(polling_place_postcode="TS17 9RG")
-
         return super().station_record_to_dict(record)
