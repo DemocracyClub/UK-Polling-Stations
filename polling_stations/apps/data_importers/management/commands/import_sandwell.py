b8495da0480a5604b8b451d7795a319dbdb517cc
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-09 10:20:38 +0000

    Import script for Sandwell (2023-05-04) (closes #5136)

--- polling_stations/apps/data_importers/management/commands/import_sandwell.py
+++ polling_stations/apps/data_importers/management/commands/import_sandwell.py
@@ -4,41 +4,31 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SAW"
     addresses_name = (
-        "2022-05-05/2022-02-21T14:46:13.389652/Democracy_Club__05May2022.tsv"
+        "2023-05-04/2023-03-09T10:20:20.081832/Democracy_Club__04May2023.tsv"
     )
     stations_name = (
-        "2022-05-05/2022-02-21T14:46:13.389652/Democracy_Club__05May2022.tsv"
+        "2023-05-04/2023-03-09T10:20:20.081832/Democracy_Club__04May2023.tsv"
     )
-    elections = ["2022-05-05"]
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
-        rec = super().address_record_to_dict(record)
-
         if uprn in [
             "10093004484",  # FLAT THE DOVECOTE HILL TOP, WEST BROMWICH B70 0SD
-            "100071578415",  # 622A BEARWOOD ROAD, SMETHWICK B664DP
-            "32072080",  # 251 HOLYHEAD ROAD, WEDNESBURY WS107DQ
-            "32072081",  # 253 HOLYHEAD ROAD, WEDNESBURY WS107DQ
+            "10094430909",  # 586B BEARWOOD ROAD, SMETHWICK
         ]:
             return None
 
         if record.addressline6 in [
-            "B64 7JA",
-            "B65 0BS",
+            "B69 4BZ",
+            "B71 2LS",
+            # split
             "B67 7EP",
-            "B70 8DB",
-            "DY4 9NB",
-            "B71 4HS",
+            "B65 0BS",
             "DY4 7TY",
-            "B66 4DP",
+            "DY4 9NB",
         ]:
             return None
 
-        if record.post_code == "B70 OSH":
-            rec["postcode"] = "B700SH"
-        if record.post_code == "B68 OLH":
-            rec["postcode"] = "B680LH"
-
-        return rec
+        return super().address_record_to_dict(record)
