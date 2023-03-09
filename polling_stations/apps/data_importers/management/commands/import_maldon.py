2e9345b9cf7bed3dd55ac0f84c6a68ea0ece6e8b
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-09 19:29:23 +0000

    Import script for Maldon (2023-05-04) (closes #5191)

--- polling_stations/apps/data_importers/management/commands/import_maldon.py
+++ polling_stations/apps/data_importers/management/commands/import_maldon.py
@@ -3,9 +3,13 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "MAL"
-    addresses_name = "2021-04-19T13:28:23.590902/Maldon Democracy_Club__06May2021.tsv"
-    stations_name = "2021-04-19T13:28:23.590902/Maldon Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
+    addresses_name = (
+        "2023-05-04/2023-03-09T19:29:09.367982/Democracy_Club__04May2023.tsv"
+    )
+    stations_name = (
+        "2023-05-04/2023-03-09T19:29:09.367982/Democracy_Club__04May2023.tsv"
+    )
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -13,21 +17,19 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
         if uprn in [
             "10094633716",  # PARK HOUSE MALDON ROAD, LATCHINGDON
-            "200000918556",  # PLOT E5 BARROW MARSH CARAVAN SITE GOLDHANGER ROAD, HEYBRIDGE
-            "10014001292",  # PLOT D2 BARROW MARSH CARAVAN SITE GOLDHANGER ROAD, HEYBRIDGE
-            "10013998717",  # CARAVAN C6 BARROW MARSH CARAVAN SITE GOLDHANGER ROAD, HEYBRIDGE
-            "200000916950",  # PLOT E48 BARROW MARSH CARAVAN SITE GOLDHANGER ROAD, HEYBRIDGE
-            "10014000997",  # PLOT C19 BARROW MARSH CARAVAN SITE GOLDHANGER ROAD, HEYBRIDGE
-            "10013998650",  # PLOT D45 BARROW MARSH CARAVAN SITE GOLDHANGER ROAD, HEYBRIDGE
-            "200000916798",  # PLOT E36 BARROW MARSH CARAVAN SITE GOLDHANGER ROAD, HEYBRIDGE
-            "10014001505",  # PLOT C23 BARROW MARSH CARAVAN SITE GOLDHANGER ROAD, HEYBRIDGE
-            "10014001698",  # PLOT B11 BARROW MARSH CARAVAN SITE GOLDHANGER ROAD, HEYBRIDGE
-            "100091258201",  # FLAT MILL BEACH PUBLIC HOUSE GOLDHANGER ROAD, HEYBRIDGE
             "200000910329",  # ROSEWOOD LODGE, HOWE GREEN ROAD, PURLEIGH, CHELMSFORD
         ]:
             return None
 
-        if record.addressline6 in ["CM8 3LS", "CM9 4NY", "CM9 6HR"]:
+        if record.addressline6.strip() in [
+            "CM9 4RA",  # confusing
+            # split
+            "CM9 8RU",
+            "CM9 4NY",
+            "CM9 6YN",
+            "CM8 3LS",
+            "CM9 6QP",
+        ]:
             return None
 
         return super().address_record_to_dict(record)
