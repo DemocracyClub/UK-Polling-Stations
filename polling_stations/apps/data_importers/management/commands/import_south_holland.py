8ac4d5a91cc190ebc8f76367de558dd29bc04682
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-10 10:03:12 +0000

    Import script for South Holland (2023-05-04) (closes #5196)

--- polling_stations/apps/data_importers/management/commands/import_south_holland.py
+++ polling_stations/apps/data_importers/management/commands/import_south_holland.py
@@ -4,10 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SHO"
     addresses_name = (
-        "2021-04-16T14:09:22.768202/S Holland Democracy_Club__06May2021.tsv"
+        "2023-05-04/2023-03-10T10:02:51.770731/Democracy_Club__04May2023.tsv"
     )
-    stations_name = "2021-04-16T14:09:22.768202/S Holland Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
+    stations_name = (
+        "2023-05-04/2023-03-10T10:02:51.770731/Democracy_Club__04May2023.tsv"
+    )
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -15,6 +17,7 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
         if uprn in [
             "100030894500",  # BUSLEY, SOUTH DROVE, SPALDING
+            "10094356949",  # LITTLE ACRE, SOUTH DROVE, SPALDING
             "100030888845",  # AMOW, MIDDLE MARSH ROAD, MOULTON MARSH, SPALDING
             "100030887883",  # 1 FARM COTTAGE, GEDNEY DYKE, SPALDING
             "100032311625",  # 2 FARM COTTAGE, GEDNEY DYKE, SPALDING
@@ -22,30 +25,11 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             return None
 
         if record.addressline6 in [
-            "PE12 8LT",
-            "PE12 0BL",
-            "PE12 8SE",
-            "PE12 8BP",
-            "PE6 0LR",
-            "PE12 8EP",
-            "PE12 0HY",
-            "PE11 3TB",
-            "PE11 3NB",
+            # split
             "PE12 9QJ",
             "PE11 4JH",
-            "PE12 0XA",
-            "PE12 6SD",
-            "PE12 6DN",
-            "PE12 7FG",
-            "PE12 0HZ",
+            "PE6 0LR",
         ]:
             return None
 
         return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # Scout & Guide Headquarters Park Lane Long Sutton Spalding PE12 9DH
-        if record.polling_place_id == "3793":
-            record = record._replace(polling_place_postcode="")
-
-        return super().station_record_to_dict(record)
