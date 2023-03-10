a47ccd358d8b0086a982421c7c6b3c84d4ce389e
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-10 14:02:51 +0000

    Import script for West Devon (2023-05-04) (closes #5211)

--- polling_stations/apps/data_importers/management/commands/import_west_devon.py
+++ polling_stations/apps/data_importers/management/commands/import_west_devon.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WDE"
     addresses_name = (
-        "2021-03-12T10:39:08.998016/West Devon Democracy_Club__06May2021.tsv"
+        "2023-05-04/2023-03-10T14:02:41.313172/Democracy_Club__04May2023.tsv"
     )
     stations_name = (
-        "2021-03-12T10:39:08.998016/West Devon Democracy_Club__06May2021.tsv"
+        "2023-05-04/2023-03-10T14:02:41.313172/Democracy_Club__04May2023.tsv"
     )
-    elections = ["2021-05-06"]
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -17,29 +17,18 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
         if uprn in [
             "10013758398",  # MOWHAY BARN, HIGHER CARLEY BARNS, LIFTON
-            "10001332226",  # BERRYDOWN STABLES, EXBOURNE, OKEHAMPTON
-            "10013751665",  # HOMEFIELD, DREWSTEIGNTON, EXETER
-            "10001330732",  # ROWDEN, BRIDESTOWE, OKEHAMPTON
+            "10013754124",  # OLD MILL, HOLDITCH FARM, MARY TAVY, TAVISTOCK
         ]:
             return None
 
         if record.addressline6 in [
-            "EX20 3NX",
-            "EX20 3NW",
+            # split
             "EX20 1QB",
+            "EX20 3NW",
+            "EX20 1SY",
             "EX20 2TP",
             "EX20 2SD",
-            "EX20 1SY",
-            "EX20 2NF",
-            "PL19 9HA",
         ]:
             return None
 
         return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # Lifton Community Centre Park Wood Rise Lifton PL16 0AL
-        if record.polling_place_id == "8001":
-            record = record._replace(polling_place_postcode="PL16 0LA")
-
-        return super().station_record_to_dict(record)
