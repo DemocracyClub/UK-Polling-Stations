3f2dd81d914fc4a24d4226e9c02b6a0bfa7c7fc7
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-10 17:54:25 +0000

    Import script for West Berkshire (2023-05-04) (closes #5215)

--- polling_stations/apps/data_importers/management/commands/import_westberks.py
+++ polling_stations/apps/data_importers/management/commands/import_westberks.py
@@ -1,39 +1,33 @@
 from django.contrib.gis.geos import Point
+
 from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WBK"
-    addresses_name = "2021-04-06T15:21:01.728722/Democracy_Club__06May2021 (1).tsv"
-    stations_name = "2021-04-06T15:21:01.728722/Democracy_Club__06May2021 (1).tsv"
-    elections = ["2021-05-06"]
+    addresses_name = (
+        "2023-05-04/2023-03-10T17:54:14.260272/Democracy_Club__04May2023.tsv"
+    )
+    stations_name = (
+        "2023-05-04/2023-03-10T17:54:14.260272/Democracy_Club__04May2023.tsv"
+    )
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
         # Peasemore Village Hall Peasemore RG20 7JE
-        if record.polling_place_id == "6161":
+        if record.polling_place_id == "7579":
             record = record._replace(
                 polling_place_postcode="RG20 7JE ", polling_place_address_2=""
             )
 
-        rec = super().station_record_to_dict(record)
-
-        # Holybrook Centre Carters Rise Calcot
-        if record.polling_place_id == "6238":
-            rec["location"] = Point(-1.025829, 51.441617, srid=4326)
-
-        return rec
+        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "200004735144",  # THE BUNGALOW WILCO POULTRY FARM TIDMARSH LANE, TIDMARSH, READING
-            "200002495545",  # HUNTERS MOON, BURYS BANK ROAD, GREENHAM, THATCHAM
+        if record.addressline6 in [
+            # split
+            "RG14 1EP",
         ]:
-            return None
-
-        if record.addressline6 in ["RG7 6TP", "RG14 1EP", "RG14 6BY", "RG14 3BP"]:
             return None
 
         return super().address_record_to_dict(record)
