ceb62f97e5fcdbb046eb68b33720892797c6394b
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-10 12:47:30 +0000

    Import script for Dacorum (2023-05-04) (closes #5207)

--- polling_stations/apps/data_importers/management/commands/import_dacorum.py
+++ polling_stations/apps/data_importers/management/commands/import_dacorum.py
@@ -3,13 +3,18 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "DAC"
-    addresses_name = "2021-01-22T12:23:53.522413/Democracy_Club__06May2021.CSV"
-    stations_name = "2021-01-22T12:23:53.522413/Democracy_Club__06May2021.CSV"
-    elections = ["2021-05-06"]
-    csv_delimiter = ","
+    addresses_name = (
+        "2023-05-04/2023-03-10T12:47:03.450416/Democracy_Club__04May2023.tsv"
+    )
+    stations_name = (
+        "2023-05-04/2023-03-10T12:47:03.450416/Democracy_Club__04May2023.tsv"
+    )
+    elections = ["2023-05-04"]
+    csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
-        if record.polling_place_id == "1504":
+        # Nash Mills Village Hall
+        if record.polling_place_id == "2277":
             record = record._replace(polling_place_easting="507211")
             record = record._replace(polling_place_northing="204366")
 
@@ -19,13 +24,15 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
-        if uprn in [
-            "10093316891",  # 82 LANCASTER HOUSE FROGMORE ROAD, HEMEL HEMPSTEAD
-            "10093316895",  # 78 LANCASTER HOUSE FROGMORE ROAD, HEMEL HEMPSTEAD
-            "200004058405",  # WHITEHOUSE GREEN LANE, FLAMSTEAD - NB Avoids confusion with "100081154056 - WHITE HOUSE, GREEN LANE, MARKYATE, ST. ALBANS"
-        ]:
+        if uprn in []:
             return None
 
-        if record.addressline6 in ["HP2 4AP", "HP2 6JN"]:
+        if record.addressline6 in [
+            # split
+            "HP2 4AP",
+            "HP2 6JN",
+            # wrong
+            "HP3 9GT",
+        ]:
             return None
         return rec
