7c44e3da841e74ad9f2081eb225e33158de5eb83
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-10 13:26:19 +0000

    Import script for Trafford (2023-05-04) (closes #5209)

--- polling_stations/apps/data_importers/management/commands/import_trafford.py
+++ polling_stations/apps/data_importers/management/commands/import_trafford.py
@@ -4,18 +4,28 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "TRF"
     addresses_name = (
-        "2022-12-15/2022-11-21T12:49:06.121803/Democracy_Club__15December2022.CSV"
+        "2023-05-04/2023-03-10T13:25:57.792849/Democracy_Club__04May2023.CSV"
     )
     stations_name = (
-        "2022-12-15/2022-11-21T12:49:06.121803/Democracy_Club__15December2022.CSV"
+        "2023-05-04/2023-03-10T13:25:57.792849/Democracy_Club__04May2023.CSV"
     )
-    elections = ["2022-12-15"]
+    elections = ["2023-05-04"]
 
     def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "10070405338",  # 183A CROSS STREET, SALE
+        ]:
+            return None
+
         if record.addressline6 in [
-            "M32 0SF",
-            "M41 9AS",
-            "M41 6JU",
+            # split
+            "M33 2BT",
+            "WA14 4AN",
+            "M33 3GG",
+            "M33 5GN",
+            "M33 2NL",
         ]:
             return None
 
