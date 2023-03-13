7cca925e7977b6eb682aa22edd7e4e2285be86ae
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-13 13:53:40 +0000

    Import script for Dartford (2023-05-04) (closes #5238)

--- polling_stations/apps/data_importers/management/commands/import_dartford.py
+++ polling_stations/apps/data_importers/management/commands/import_dartford.py
@@ -3,30 +3,15 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "DAR"
-    addresses_name = "2021-03-12T12:25:27.291565/polling_station_export-2021-03-12.csv"
-    stations_name = "2021-03-12T12:25:27.291565/polling_station_export-2021-03-12.csv"
-    elections = ["2021-05-06"]
-    csv_delimiter = ","
+    addresses_name = "2023-05-04/2023-03-13T13:53:29.216498/Eros_SQL_Output003.csv"
+    stations_name = "2023-05-04/2023-03-13T13:53:29.216498/Eros_SQL_Output003.csv"
+    elections = ["2023-05-04"]
 
     def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
-
-        if uprn in [
-            "10094156345",  # 447A PRINCES ROAD, DARTFORD
-            "200000533812",  # THE NOOK, POWDER MILL LANE, DARTFORD
-            "200000538710",  # THE COACH HOUSE, HAWLEY ROAD, DARTFORD
+        if record.housepostcode in [
+            # Split
+            "DA10 1FB",
         ]:
             return None
 
-        if record.housepostcode in ["DA9 9XT"]:
-            return None
-
         return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # 8th Dartford Scout Hall (PRI5) Oakfield Lane Dartford Kent DA1 2EU and
-        # 8th Dartford Scout Hall (WSH5) Oakfield Lane Dartford Kent DA1 2EU
-        if "8th Dartford Scout Hall" in record.pollingstationname:
-            record = record._replace(pollingstationpostcode="DA1 2SP")
-
-        return super().station_record_to_dict(record)
