ca762e3c47d99e24fac761b5e4025efea5abbf52
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-13 13:51:06 +0000

    Import script for Swale (2023-05-04) (closes #5236)

--- polling_stations/apps/data_importers/management/commands/import_swale.py
+++ polling_stations/apps/data_importers/management/commands/import_swale.py
@@ -3,25 +3,30 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "SWL"
-    addresses_name = "2021-04-19T15:31:29.401515/polling_station_export-2021-03-02.csv"
-    stations_name = "2021-04-19T15:31:29.401515/polling_station_export-2021-03-02.csv"
-    elections = ["2021-05-06"]
+    addresses_name = (
+        "2023-05-04/2023-03-13T13:50:50.268451/Democracy Club Swale PS export.csv"
+    )
+    stations_name = (
+        "2023-05-04/2023-03-13T13:50:50.268451/Democracy Club Swale PS export.csv"
+    )
+    elections = ["2023-05-04"]
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
         if record.housepostcode in [
-            "ME13 7NB",
+            # split
             "ME10 2EF",
-            "ME12 2HP",
-            "ME12 2SG",
             "ME10 3TU",
             "ME12 1TF",
+            "ME12 2SG",
+            # LOOK WRONG
+            "ME10 2JH",
+            "ME10 2JQ",
         ]:
             return None
 
         if uprn in [
             "200002533735",  # POPPINGTON BUNGALOW, WHITE HILL, SELLING, FAVERSHAM
-            "200002536488",  # SIMEL HOUSE, MINTCHING WOOD LANE, KINGSDOWN, SITTINGBOURNE
         ]:
             return None
 
