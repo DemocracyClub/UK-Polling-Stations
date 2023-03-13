d1ac40de0d24bd7b72375e993831aae1a6b43c84
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-13 11:02:26 +0000

    Import script for Ashfield (2023-05-04) (closes #)

--- polling_stations/apps/data_importers/management/commands/import_ashfield.py
+++ polling_stations/apps/data_importers/management/commands/import_ashfield.py
@@ -3,19 +3,14 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "ASH"
-    addresses_name = "2021-03-29T10:18:22.398168/Ashfield Democracy_Club__06May2021.CSV"
-    stations_name = "2021-03-29T10:18:22.398168/Ashfield Democracy_Club__06May2021.CSV"
-    elections = ["2021-05-06"]
-    csv_delimiter = ","
-
-    def station_record_to_dict(self, record):
-        # The Summit Centre Room 2 Pavilion Road Kirkby in Ashfield Nottingham NG17 7LL
-        # changing to the same as The Summit Centre Room 1 Pavilion Road Kirkby In Ashfield Nottingham NG17 7LL
-        if record.polling_place_id == "3638":
-            record = record._replace(polling_place_easting="450822")
-            record = record._replace(polling_place_northing="356660")
-
-        return super().station_record_to_dict(record)
+    addresses_name = (
+        "2023-05-04/2023-03-13T11:02:12.723251/Democracy_Club__04May2023.tsv"
+    )
+    stations_name = (
+        "2023-05-04/2023-03-13T11:02:12.723251/Democracy_Club__04May2023.tsv"
+    )
+    elections = ["2023-05-04"]
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
@@ -25,7 +20,14 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         ]:
             return None
 
-        if record.addressline6 in ["NG17 8BE", "NG17 8JR", "NG17 5HS"]:
+        if record.addressline6 in [
+            # split
+            "NG17 8BE",
+            "NG17 5HS",
+            # wrong
+            "NG17 8JR",
+            "NG17 5HS",
+        ]:
             return None
 
         return super().address_record_to_dict(record)
