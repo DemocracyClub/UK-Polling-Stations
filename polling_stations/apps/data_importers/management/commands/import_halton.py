f73aee31bba79eb5f9ac25950f77cb3eef0dc9eb
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-14 10:56:02 +0000

    Import script for Halton (2023-05-04) (closes #5246)

--- polling_stations/apps/data_importers/management/commands/import_halton.py
+++ polling_stations/apps/data_importers/management/commands/import_halton.py
@@ -4,41 +4,37 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "HAL"
     addresses_name = (
-        "2022-05-05/2022-03-21T14:24:18.499238/Democracy_Club__05May2022.tsv"
+        "2023-05-04/2023-03-14T10:55:51.877415/Democracy_Club__04May2023.tsv"
     )
     stations_name = (
-        "2022-05-05/2022-03-21T14:24:18.499238/Democracy_Club__05May2022.tsv"
+        "2023-05-04/2023-03-14T10:55:51.877415/Democracy_Club__04May2023.tsv"
     )
-    elections = ["2022-05-05"]
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
         # Holy Trinity Church Trinity Street Runcorn WA7 1BJ
-        if record.polling_place_id == "2817":
+        if record.polling_place_id == "3405":
             record = record._replace(polling_place_easting="351595")
             record = record._replace(polling_place_northing="383048")
 
         # Mobile Polling Station Galway Ave. Widnes
-        if record.polling_place_id == "2809":
-            record = record._replace(polling_place_easting="350205")
-            record = record._replace(polling_place_northing="387147")
+        if record.polling_place_id == "3396":
+            record = record._replace(polling_place_easting="")
+            record = record._replace(polling_place_northing="")
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "10090873639",  # THE BLACKSMITHS, NORTON LANE, NORTON, RUNCORN
-        ]:
-            return None
-
         if record.addressline6 in [
-            "WA8 7TF",
+            # split
             "WA8 7TB",
-            "WA8 8PZ",
             "WA7 1BH",
             "WA7 2QA",
+            "WA8 8PZ",
+            "WA8 8HZ",
             "WA4 4BL",
+            "WA8 7TF",
+            "WA7 4SX",
         ]:
             return None
 
