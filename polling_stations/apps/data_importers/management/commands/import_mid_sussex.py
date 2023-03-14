8e3e9927f9d9d9e2fbe92882e1f501e0de9bd741
GeoWill <roper.william@gmail.com>
2023-03-14 15:33:20 +0000

    Import script for Mid Sussex (2023-05-04) (closes #5096)

--- polling_stations/apps/data_importers/management/commands/import_mid_sussex.py
+++ polling_stations/apps/data_importers/management/commands/import_mid_sussex.py
@@ -1,53 +1,20 @@
-from django.contrib.gis.geos import Point
-
 from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "MSS"
     addresses_name = (
-        "2021-03-26T11:11:22.528819/Democracy_Club__06May2021 Mid Sussex.tsv"
+        "2023-05-04/2023-03-03T13:09:09.891542/Democracy_Club__04May2023.tsv"
     )
     stations_name = (
-        "2021-03-26T11:11:22.528819/Democracy_Club__06May2021 Mid Sussex.tsv"
+        "2023-05-04/2023-03-03T13:09:09.891542/Democracy_Club__04May2023.tsv"
     )
-    elections = ["2021-05-06"]
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
     csv_encoding = "windows-1252"
 
-    def station_record_to_dict(self, record):
-        rec = super().station_record_to_dict(record)
-
-        # Church Hall, St Edward the Confessor Church
-        if record.polling_place_id == "3445":
-            rec["location"] = Point(-0.14863, 50.96048, srid=4326)
-
-        return rec
-
     def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "10093413710",  # 13 THE HOLT, HAYWARDS HEATH
-            "10070621745",  # 149 ROYAL GEORGE ROAD, BURGESS HILL
-        ]:
-            return None
-
-        if record.post_code in [
-            "RH19 2DL",
-            "RH10 4SH",
-            "RH19 4LF",
-            "RH16 2QG",
-            "RH16 2QB",
-            "RH16 2QF",
-            "RH17 5AL",
-            "RH17 5UQ",
-            "RH15 8AZ",
-            "RH15 9QU",
-            "BN6 9NE",
-            "RH16 4ET",
-            "RH19 1ET",
-        ]:
+        if record.post_code in ["RH17 7DY", "BN6 9NA", "RH16 2QB"]:
             return None
 
         return super().address_record_to_dict(record)
