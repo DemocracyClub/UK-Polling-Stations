310fcc4962a200d8e47a4374e05c2d6340097802
GeoWill <roper.william@gmail.com>
2023-03-14 09:01:15 +0000

    Import script for Gravesham (2023-05-04) (closes #4974)

--- polling_stations/apps/data_importers/management/commands/import_gravesham.py
+++ polling_stations/apps/data_importers/management/commands/import_gravesham.py
@@ -3,15 +3,12 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "GRA"
-    addresses_name = "2021-03-04T15:08:48.032358/polling_station_export-2021-03-02.csv"
-    stations_name = "2021-03-04T15:08:48.032358/polling_station_export-2021-03-02.csv"
-    elections = ["2021-05-06"]
+    addresses_name = "2023-05-04/2023-02-21T21:14:28.593307/polling-stations-export.csv"
+    stations_name = "2023-05-04/2023-02-21T21:14:28.593307/polling-stations-export.csv"
+    elections = ["2023-05-04"]
 
     def address_record_to_dict(self, record):
-        if record.housepostcode in ["DA11 0JT", "DA13 9EH", "ME3 7NB"]:
-            return None
-
-        if record.uprn.lstrip("0") in ["10012030386", "10012030387"]:
+        if record.housepostcode in ["ME3 7NB"]:
             return None
 
         return super().address_record_to_dict(record)
