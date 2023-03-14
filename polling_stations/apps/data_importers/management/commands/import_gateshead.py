b1f7ef35e72c37409fc3bd35ecc18a8b3f479df2
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-14 11:31:07 +0000

    Import script for Gateshead (2023-05-04) (closes #5248)

--- polling_stations/apps/data_importers/management/commands/import_gateshead.py
+++ polling_stations/apps/data_importers/management/commands/import_gateshead.py
@@ -4,17 +4,30 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "GAT"
     addresses_name = (
-        "2022-05-05/2022-03-16T12:09:15.983600/Democracy_Club__05May2022.CSV"
+        "2023-05-04/2023-03-14T11:30:54.886338/Democracy_Club__04May2023.tsv"
     )
     stations_name = (
-        "2022-05-05/2022-03-16T12:09:15.983600/Democracy_Club__05May2022.CSV"
+        "2023-05-04/2023-03-14T11:30:54.886338/Democracy_Club__04May2023.tsv"
     )
-    elections = ["2022-05-05"]
+    elections = ["2023-05-04"]
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "10093489937",  # 1B SPLIT CROW ROAD, HIGH FELLING, GATESHEAD
+            "100000029999",  # 125 ELLISON ROAD, GATESHEAD
+        ]:
+            return None
+
         if record.addressline6 in [
+            # split
             "NE9 5XP",
             "NE9 6JR",
+            # wrong
+            "NE39 2EA",
+            "NE17 7BQ",
         ]:
             return None
 
