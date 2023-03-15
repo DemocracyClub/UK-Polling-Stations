3091805e4409d379537aa24f728f9de65c079390
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-15 10:13:55 +0000

    Import script for Ashford (2023-05-04) (closes #5195)

--- polling_stations/apps/data_importers/management/commands/import_ashford.py
+++ polling_stations/apps/data_importers/management/commands/import_ashford.py
@@ -3,9 +3,13 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "ASF"
-    addresses_name = "2021-03-18T09:55:17.756012/Ashford Democracy_Club__06May2021.tsv"
-    stations_name = "2021-03-18T09:55:17.756012/Ashford Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
+    addresses_name = (
+        "2023-05-04/2023-03-15T10:12:47.076239/Democracy_Club__04May2023.tsv"
+    )
+    stations_name = (
+        "2023-05-04/2023-03-15T10:12:47.076239/Democracy_Club__04May2023.tsv"
+    )
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -13,21 +17,10 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
         if uprn in [
             "200004389187",  # MOBILE HOME AT CLOVER FARM THE PINNOCK, PLUCKLEY
-            "200001880702",  # THE CHAPEL, FRITH ROAD, ALDINGTON, ASHFORD
-            "200004382948",  # OAKDENE, STATION ROAD, PLUCKLEY, ASHFORD
         ]:
             return None
 
-        if record.addressline6 in ["TN25 7AS", "TN26 3LL"]:
+        if record.addressline6 in ["TN24 9FA", "TN25 7AS"]:
             return None
 
         return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # Rainbow Room, Tenterden Leisure Centre, Recreation Ground Road, Tenterden, Ashford, Kent, TN30 6RA - postocde was in address line 4 so reorganised data
-        if record.polling_place_id == "8050":
-            record = record._replace(
-                polling_place_address_4="Kent", polling_place_postcode="TN30 6RA"
-            )
-
-        return super().station_record_to_dict(record)
