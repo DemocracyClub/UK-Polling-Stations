5a14a4400f3f8fc5ea0f679512369ad0f40b1186
GeoWill <roper.william@gmail.com>
2023-03-16 16:01:26 +0000

    Optimise DB for writes/reads before/after replication

--- deploy/files/scripts/db_replication_runner.sh
+++ deploy/files/scripts/db_replication_runner.sh
@@ -3,14 +3,15 @@ set -xeEo pipefail
 
 
 # Optimise DB for replication
-#su -c "/var/www/polling_stations/postgres_writes_config.sh" - polling_stations
-#sudo service postgresql restart
+su -c "/var/www/polling_stations/postgres_writes_config.sh" - polling_stations
+echo "fsync = off" | sudo tee -a /etc/postgresql/14/main/postgresql.conf
+sudo service postgresql restart
 
 # Do the replication
 su -c "/var/www/polling_stations/setup_db_replication.sh" - polling_stations
 
 # Optimise DB for Reads
-#su -c "/var/www/polling_stations/postgres_reads_config.sh" - polling_stations
-#sudo service postgresql restart
+su -c "/var/www/polling_stations/postgres_reads_config.sh" - polling_stations
+sudo service postgresql restart
 
 sudo service polling_stations_gunicorn reload
--- deploy/files/scripts/postgres_writes_config.sh
+++ deploy/files/scripts/postgres_writes_config.sh
@@ -4,9 +4,9 @@ set -xeEo pipefail
 DB_USER=${PROJECT_NAME}
 DB=${PROJECT_NAME}
 
-psql "$DB" -U "$DB_USER" -c "ALTER SYSTEM work_mem = '32MB'"
-psql "$DB" -U "$DB_USER" -c "ALTER SYSTEM shared_buffers = '4GB'"
-psql "$DB" -U "$DB_USER" -c "ALTER SYSTEM maintenance_work_mem = '2GB'"
-psql "$DB" -U "$DB_USER" -c "ALTER SYSTEM full_page_writes = 'off'"
-psql "$DB" -U "$DB_USER" -c "ALTER SYSTEM autovacuum = 'off'"
-psql "$DB" -U "$DB_USER" -c "ALTER SYSTEM wal_buffers = '-1'"
+psql "$DB" -U "$DB_USER" -c "ALTER SYSTEM SET work_mem = '32MB'"
+psql "$DB" -U "$DB_USER" -c "ALTER SYSTEM SET shared_buffers = '4GB'"
+psql "$DB" -U "$DB_USER" -c "ALTER SYSTEM SET maintenance_work_mem = '2GB'"
+psql "$DB" -U "$DB_USER" -c "ALTER SYSTEM SET full_page_writes = 'off'"
+psql "$DB" -U "$DB_USER" -c "ALTER SYSTEM SET autovacuum = 'off'"
+psql "$DB" -U "$DB_USER" -c "ALTER SYSTEM SET wal_buffers = '-1'"
