de7ca48affea9a3d66d18a5b92a6f4f6e0108eea
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-16 13:16:58 +0000

    Import script for Cambridge (2023-05-04) (closes #5300)

--- polling_stations/apps/data_importers/management/commands/import_cambridge.py
+++ polling_stations/apps/data_importers/management/commands/import_cambridge.py
@@ -1,21 +1,21 @@
 from django.contrib.gis.geos import Point
+
 from data_importers.management.commands import BaseHalaroseCsvImporter
 
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "CAB"
-    addresses_name = (
-        "2022-08-18/2022-07-14T16:27:44.445503/polling_station_export-2022-07-14.csv"
-    )
-    stations_name = (
-        "2022-08-18/2022-07-14T16:27:44.445503/polling_station_export-2022-07-14.csv"
-    )
-    elections = ["2022-08-18"]
+    addresses_name = "2023-05-04/2023-03-16T13:16:16.307677/Eros_SQL_Output005.csv"
+    stations_name = "2023-05-04/2023-03-16T13:16:16.307677/Eros_SQL_Output005.csv"
+    elections = ["2023-05-04"]
 
     def address_record_to_dict(self, record):
         if record.housepostcode in [
+            # split
             "CB4 2QA",
             "CB4 1LD",
+            # wrong
+            "CB1 7UF",
         ]:
             return None
         return super().address_record_to_dict(record)
@@ -23,22 +23,19 @@ class Command(BaseHalaroseCsvImporter):
     def station_record_to_dict(self, record):
         rec = super().station_record_to_dict(record)
 
-        if rec["internal_council_id"] == "47-the-c3-centre":
+        if rec["internal_council_id"] == "43-the-c3-centre":
             rec["location"] = Point(0.1572, 52.2003, srid=4326)
 
         # user issue report #105
-        if rec["internal_council_id"] == "35-trinity-old-field-pavilion":
+        if rec["internal_council_id"] == "32-trinity-old-field-pavilion":
             rec["location"] = Point(0.106029, 52.207284, srid=4326)
 
         # user issue report #129
-        if rec["internal_council_id"] == "2-east-barnwell-community-centre":
+        if rec["internal_council_id"] == "3-east-barnwell-community-centre":
             rec["location"] = Point(0.165576, 52.212132, srid=4326)
 
         # report from council
-        if (
-            rec["internal_council_id"] == "14-seminar-rooms-3-4-churchill-college"
-            or rec["internal_council_id"] == "15-seminar-rooms-3-4-churchill-college"
-        ):
+        if rec["internal_council_id"] == "13-seminar-rooms-3-4":
             rec["location"] = Point(0.099002518, 52.215464, srid=4326)
 
         return rec
