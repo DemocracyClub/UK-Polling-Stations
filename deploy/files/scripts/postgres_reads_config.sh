53befd6ffdf5d5603a4b0b25484d2ac5305abff6
GeoWill <roper.william@gmail.com>
2023-03-16 16:01:26 +0000

    Optimise DB for writes/reads before/after replication

--- deploy/files/scripts/postgres_reads_config.sh
+++ deploy/files/scripts/postgres_reads_config.sh
@@ -4,12 +4,12 @@ set -xeEo pipefail
 DB_USER=${PROJECT_NAME}
 DB=${PROJECT_NAME}
 
-psql $DB -U $DB_USER -c "ALTER SYSTEM work_mem = '100MB'"
-psql $DB -U $DB_USER -c "ALTER SYSTEM shared_buffers = '512MB'"
-psql $DB -U $DB_USER -c "ALTER SYSTEM maintenance_work_mem = '1GB'"
-psql $DB -U $DB_USER -c "ALTER SYSTEM full_page_writes 'on'"
-psql $DB -U $DB_USER -c "ALTER SYSTEM autovacuum 'on'"
-psql $DB -U $DB_USER -c "ALTER SYSTEM wal_buffers '-1'"
-psql $DB -U $DB_USER -c "ALTER SYSTEM effective_cache_size = '1024MB'"
-psql $DB -U $DB_USER -c "ALTER SYSTEM checkpoint_completion_target = '0.9'"
-psql $DB -U $DB_USER -c "ALTER SYSTEM default_statistics_target = '1000'"
+psql $DB -U $DB_USER -c "ALTER SYSTEM SET work_mem = '100MB'"
+psql $DB -U $DB_USER -c "ALTER SYSTEM SET shared_buffers = '512MB'"
+psql $DB -U $DB_USER -c "ALTER SYSTEM SET maintenance_work_mem = '1GB'"
+psql $DB -U $DB_USER -c "ALTER SYSTEM SET full_page_writes = on"
+psql $DB -U $DB_USER -c "ALTER SYSTEM SET autovacuum = on"
+psql $DB -U $DB_USER -c "ALTER SYSTEM SET wal_buffers = '-1'"
+psql $DB -U $DB_USER -c "ALTER SYSTEM SET effective_cache_size = '1024MB'"
+psql $DB -U $DB_USER -c "ALTER SYSTEM SET checkpoint_completion_target = '0.9'"
+psql $DB -U $DB_USER -c "ALTER SYSTEM SET default_statistics_target = '1000'"
