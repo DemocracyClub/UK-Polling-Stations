2197615c00a6ed1e2931700593297a7e3fe76062
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-16 12:04:05 +0000

    Import script for Cannock Chase (2023-05-04) (closes #5295)

--- polling_stations/apps/data_importers/management/commands/import_cannock_chase.py
+++ polling_stations/apps/data_importers/management/commands/import_cannock_chase.py
@@ -4,19 +4,29 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "CAN"
     addresses_name = (
-        "2022-05-05/2022-03-24T09:12:50.703130/Democracy_Club__05May2022.tsv"
+        "2023-05-04/2023-03-16T12:03:23.219491/Democracy_Club__04May2023.tsv"
     )
     stations_name = (
-        "2022-05-05/2022-03-24T09:12:50.703130/Democracy_Club__05May2022.tsv"
+        "2023-05-04/2023-03-16T12:03:23.219491/Democracy_Club__04May2023.tsv"
     )
-    elections = ["2022-05-05"]
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "100032224228",  # 179 HEDNESFORD ROAD, HEATH HAYES, CANNOCK
+        ]:
+            return None
+
         if record.addressline6 in [
+            # split
             "WS12 3YG",
             "WS11 9NW",
-            "WS15 2XA",
+            # look wrong
+            "WS11 1LF",
+            "WS12 1RB",
         ]:
             return None
 
