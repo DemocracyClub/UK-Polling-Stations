bf1605e3c08fd7dc44b19a17ee6b4a05c1a6bb49
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-16 11:44:43 +0000

    Import script for Winchester (2023-05-04) (closes #5291)

--- polling_stations/apps/data_importers/management/commands/import_winchester.py
+++ polling_stations/apps/data_importers/management/commands/import_winchester.py
@@ -3,28 +3,32 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WIN"
-    addresses_name = (
-        "2022-05-05/2022-03-23T16:46:37.210490/Democracy_Club__05May2022.tsv"
-    )
-    stations_name = (
-        "2022-05-05/2022-03-23T16:46:37.210490/Democracy_Club__05May2022.tsv"
-    )
-    elections = ["2022-05-05"]
+    addresses_name = "2023-05-04/2023-03-30T13:36:19.020481/ems_exports_combined.tsv"
+    stations_name = "2023-05-04/2023-03-30T13:36:19.020481/ems_exports_combined.tsv"
+    elections = ["2023-05-04"]
+    csv_encoding = "windows-1252"
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+        if uprn in [
+            "200000182939",  # CARAVAN 1 POINTERS PADDOCK SHEARDLEY LANE, DROXFORD
+            "200000179695",  # CARAVAN 2 POINTERS PADDOCK SHEARDLEY LANE, DROXFORD
+            "10094862207",  # 3 CAMPION ROAD, CURBRIDGE
+        ]:
+            return None
         if record.addressline6 in [
+            # split
             "SO24 9HZ",
             "SO32 1HP",
             "SO32 3PJ",
-            "SO32 3LA",
         ]:
             return None
         return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
         # Upham New Millennium Village Hall (Cttee Room)
-        if record.polling_place_id == "10194":
+        if record.polling_place_id == "10504":
             record = record._replace(polling_place_easting="452220")
             record = record._replace(polling_place_northing="119570")
 
