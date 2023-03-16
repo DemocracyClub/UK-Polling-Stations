3e696df040181eaa399627f2463154998edbb86c
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-16 11:44:43 +0000

    Import script for Winchester (2023-05-04) (closes #5291)

--- polling_stations/apps/data_importers/management/commands/import_winchester.py
+++ polling_stations/apps/data_importers/management/commands/import_winchester.py
@@ -4,28 +4,11 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WIN"
     addresses_name = (
-        "2022-05-05/2022-03-23T16:46:37.210490/Democracy_Club__05May2022.tsv"
+        "2023-05-04/2023-03-16T11:44:33.067222/Democracy_Club__04May2023 - 15 wards.tsv"
     )
     stations_name = (
-        "2022-05-05/2022-03-23T16:46:37.210490/Democracy_Club__05May2022.tsv"
+        "2023-05-04/2023-03-16T11:44:33.067222/Democracy_Club__04May2023 - 15 wards.tsv"
     )
-    elections = ["2022-05-05"]
+    elections = ["2023-05-04"]
+    csv_encoding = "windows-1252"
     csv_delimiter = "\t"
-
-    def address_record_to_dict(self, record):
-        if record.addressline6 in [
-            "SO24 9HZ",
-            "SO32 1HP",
-            "SO32 3PJ",
-            "SO32 3LA",
-        ]:
-            return None
-        return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # Upham New Millennium Village Hall (Cttee Room)
-        if record.polling_place_id == "10194":
-            record = record._replace(polling_place_easting="452220")
-            record = record._replace(polling_place_northing="119570")
-
-        return super().station_record_to_dict(record)
