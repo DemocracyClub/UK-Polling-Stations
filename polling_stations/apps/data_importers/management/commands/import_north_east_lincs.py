516a4a1fac06cd9c6fda36753e1916c7d235bcdd
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-17 16:59:04 +0000

    Import script for North East Lincolnshire (2023-05-04) (closes #5322)

--- polling_stations/apps/data_importers/management/commands/import_north_east_lincs.py
+++ polling_stations/apps/data_importers/management/commands/import_north_east_lincs.py
@@ -3,22 +3,16 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "NEL"
-    addresses_name = (
-        "2022-05-05/2022-03-24T10:29:54.923749/polling_station_export-2022-03-24.csv"
-    )
-    stations_name = (
-        "2022-05-05/2022-03-24T10:29:54.923749/polling_station_export-2022-03-24.csv"
-    )
-    elections = ["2022-05-05"]
+    addresses_name = "2023-05-04/2023-03-17T16:58:51.018219/Eros_SQL_Output001.csv"
+    stations_name = "2023-05-04/2023-03-17T16:58:51.018219/Eros_SQL_Output001.csv"
+    elections = ["2023-05-04"]
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
 
-        if uprn in []:
-            return None
-
-        if record.housepostcode in [
-            "DN33 2AD",
+        if uprn in [
+            "10090078903",  # FLAT, 247 GRIMSBY ROAD, CLEETHORPES
+            "10090086605",  # THE ROOST, DIANA PRINCESS OF WALES HOSPITAL, SCARTHO ROAD, GRIMSBY
         ]:
             return None
 
