090bc180885d8f6e78de951cff8f9615f19f7eaa
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-17 09:21:23 +0000

    Import script for Chorley (2023-05-04) (closes #5310)

--- polling_stations/apps/data_importers/management/commands/import_chorley.py
+++ polling_stations/apps/data_importers/management/commands/import_chorley.py
@@ -3,16 +3,26 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "CHO"
-    addresses_name = (
-        "2022-05-05/2022-03-28T09:45:04.518415/polling_station_export-2022-03-28.csv"
-    )
-    stations_name = (
-        "2022-05-05/2022-03-28T09:45:04.518415/polling_station_export-2022-03-28.csv"
-    )
-    elections = ["2022-05-05"]
+    addresses_name = "2023-05-04/2023-03-17T09:21:10.517030/Eros_SQL_Output008.csv"
+    stations_name = "2023-05-04/2023-03-17T09:21:10.517030/Eros_SQL_Output008.csv"
+    elections = ["2023-05-04"]
 
     def address_record_to_dict(self, record):
-        if record.housepostcode in ["PR7 2QL", "PR6 0HT", "PR6 0BS", "PR6 7YL"]:
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "100010387618",  # MILLSTONE HOUSE, THE GREEN, ECCLESTON, CHORLEY
+        ]:
+            return None
+
+        if record.housepostcode in [
+            # split
+            "PR7 2QL",
+            "PR6 0HT",
+            "PR6 0BS",
+            # look wrong
+            "PR26 9HE",
+        ]:
             return None  # split
 
         return super().address_record_to_dict(record)
