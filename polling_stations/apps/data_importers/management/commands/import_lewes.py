8b8d0f2eaaf1f1e47720dd54937b9192b1f50b94
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-17 14:56:16 +0000

    Import script for Lewes (2023-05-04) (closes #5315)

--- polling_stations/apps/data_importers/management/commands/import_lewes.py
+++ polling_stations/apps/data_importers/management/commands/import_lewes.py
@@ -3,14 +3,23 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "LEE"
-    addresses_name = "2021-03-29T16:19:50.895276/polling_station_export-2021-03-24.csv"
-    stations_name = "2021-03-29T16:19:50.895276/polling_station_export-2021-03-24.csv"
-    elections = ["2021-05-06"]
+    addresses_name = "2023-05-04/2023-03-17T14:55:49.863600/Eros_SQL_Output001.csv"
+    stations_name = "2023-05-04/2023-03-17T14:55:49.863600/Eros_SQL_Output001.csv"
+    elections = ["2023-05-04"]
 
-    #
+    def station_record_to_dict(self, record):
+        if (
+            record.pollingstationname
+            == "SAINT PETER THE APOSTLE CHURCH HALL-THE CHAPEL ROOMS"
+        ):
+            record = record._replace(pollingstationpostcode="")
+
+        return super().station_record_to_dict(record)
+
     def address_record_to_dict(self, record):
         if record.housepostcode in [
-            "BN8 5NX" "RH17 7QH",
+            # split
+            "BN10 8UG",
         ]:
             return None
 
