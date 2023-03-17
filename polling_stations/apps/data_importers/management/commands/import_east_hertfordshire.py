f4408bb0b00f671bee986f401b785a0bcc2c7894
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-17 09:02:02 +0000

    Import script for East Hertfordshire (2023-05-04) (closes #5308)

--- polling_stations/apps/data_importers/management/commands/import_east_hertfordshire.py
+++ polling_stations/apps/data_importers/management/commands/import_east_hertfordshire.py
@@ -1,39 +1,45 @@
-from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 from django.contrib.gis.geos import Point
+
+from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "EHE"
-    addresses_name = "2021-04-27T12:44:26.723598/Democracy_Club__06May2021.tsv"
-    stations_name = "2021-04-27T12:44:26.723598/Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
+    addresses_name = (
+        "2023-05-04/2023-03-17T09:01:49.570538/Democracy_Club__04May2023.tsv"
+    )
+    stations_name = (
+        "2023-05-04/2023-03-17T09:01:49.570538/Democracy_Club__04May2023.tsv"
+    )
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "10034624304",  # JACOB NEURO CENTRE, HIGH WYCH ROAD, SAWBRIDGEWORTH
+        ]:
+            return None
+
         if record.addressline6 in [
-            "CM23 3QY",
+            # split
             "CM21 0BD",
-            "CM21 9BD",
+            "CM23 3QY",
             "CM21 0HX",
-            "SG9 9DW",
             "SG12 8RB",
-            "SG14 1LR",
-            "SG14 3NE",
+            "SG9 9DW",
+            # look wrong
+            "SG12 0XY",
         ]:
             return None
         return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
         # Albury Village Hall
-        if record.polling_place_id == "3908":
+        if record.polling_place_id == "4434":
             rec = super().station_record_to_dict(record)
             rec["location"] = Point(0.094440, 51.903865, srid=4326)
-            return rec
-
-        # Rhodes Arts Complex
-        if record.polling_place_id == "3953":
-            rec = super().station_record_to_dict(record)
-            rec["location"] = Point(0.163535, 51.863442, srid=4326)
             return rec
 
         return super().station_record_to_dict(record)
