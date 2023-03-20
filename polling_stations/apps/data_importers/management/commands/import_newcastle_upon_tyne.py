213cf490c57b37afe530c807768bd0848b8c29c7
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-20 13:15:24 +0000

    Import script for Newcastle (2023-05-04) (closes #5331)

--- polling_stations/apps/data_importers/management/commands/import_newcastle_upon_tyne.py
+++ polling_stations/apps/data_importers/management/commands/import_newcastle_upon_tyne.py
@@ -1,31 +1,70 @@
+from django.contrib.gis.geos import Point
+
 from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "NET"
-    addresses_name = (
-        "2022-05-05/2022-03-21T11:45:58.853591/Democracy_Club__05May2022.tsv"
-    )
-    stations_name = (
-        "2022-05-05/2022-03-21T11:45:58.853591/Democracy_Club__05May2022.tsv"
-    )
-    elections = ["2022-05-05"]
+    addresses_name = "2023-05-04/2023-03-27T13:58:53.599234/Updated 27..03.23 Democracy_Club__04May2023.tsv"
+    stations_name = "2023-05-04/2023-03-27T13:58:53.599234/Updated 27..03.23 Democracy_Club__04May2023.tsv"
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
 
-    # West End Community Centre and Library, Condercum Road's postcode is correct, but
-    # the postcode centroid has been dragged east by an errant property.
-
-    # Sandringham Road properties in Gosforth and East Denton have apparent long
-    # distances due to locations being transposed across the two areas in AddressBase.
-    # Will be fine as the council data is correct and we're not displaying directions
-    # maps.
-
     def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "4510034357",  # LOUGH BRIDGE HOUSE, CALLERTON, NEWCASTLE UPON TYNE
+            "4510044349",  # 11 ROKEBY AVENUE, NEWCASTLE UPON TYNE
+            "4510044350",  # 12 ROKEBY AVENUE, NEWCASTLE UPON TYNE
+            "4510014924",  # 34 DENTON AVENUE, NEWCASTLE UPON TYNE
+            "4510095971",  # 2 WOODSTOCK ROAD, NEWCASTLE UPON TYNE
+            "4510048289",  # 56 WOODSTOCK ROAD, NEWCASTLE UPON TYNE
+            "4510755893",  # 63 WHITE HOUSE ROAD, NEWCASTLE UPON TYNE
+            "4510001747",  # 53 CAROLINE STREET, NEWCASTLE UPON TYNE
+            "4510106463",  # 2 CLIFFORD ROAD, NEWCASTLE UPON TYNE
+            "4510115273",  # 250 ST. ANTHONYS ROAD, NEWCASTLE UPON TYNE
+            "4510755706",  # 633 WELBECK ROAD, NEWCASTLE UPON TYNE
+            "4510083769",  # 278 FOSSWAY, NEWCASTLE UPON TYNE
+            "4510116542",  # 515 SHIELDS ROAD, NEWCASTLE UPON TYNE
+            "4510141284",  # P T E SOCIAL CLUB, MILLERS ROAD, NEWCASTLE UPON TYNE
+            "4510759485",  # FLAT PTE SOCIAL CLUB MILLERS ROAD, NEWCASTLE UPON TYNE
+            "4510759781",  # 135 FENHAM HALL DRIVE, NEWCASTLE UPON TYNE
+            "4510092926",  # 15 SANDRINGHAM ROAD, GOSFORTH, NEWCASTLE UPON TYNE
+            "4510041714",  # 11 SANDRINGHAM ROAD, GOSFORTH, NEWCASTLE UPON TYNE
+            "4510041715",  # 13 SANDRINGHAM ROAD, GOSFORTH, NEWCASTLE UPON TYNE
+            "4510116916",  # 5 STONEYHURST ROAD WEST, NEWCASTLE UPON TYNE
+            "4510731041",  # FLAT, 6 ST. MARYS PLACE, NEWCASTLE UPON TYNE
+            "4510728933",  # THE BUNGALOW EXHIBITION PARK CLAREMONT ROAD, NEWCASTLE UPON TYNE
+        ]:
+            return None
+
         if record.addressline6 in [
-            "NE3 1AR",
+            # splits
             "NE4 9NQ",
             "NE5 1QF",
+            "NE15 9FD",  # GOLDCREST ROAD, NEWCASTLE UPON TYNE
+            "NE6 4AZ",  # HADRIANS DRIVE, NEWCASTLE UPON TYNE
+            "NE5 2BR",  # SANDRINGHAM ROAD, EAST DENTON, NEWCASTLE UPON TYNE
+            "NE7 7BQ",  # JESMOND DENE, NEWCASTLE UPON TYNE
         ]:
             return None  # split
 
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # Following warning was checked and there is no need for changes:
+        # WARNING: P'Civic Centre Grand Entrance' and 'Civic Centre Arches Reception' are at approximately
+        # the same location, but have different postcodes: '12808','12805'
+
+        rec = super().station_record_to_dict(record)
+
+        # West End Library and Community Hub, Condercum Road, Newcastle upon Tyne, NE4 9JH
+        if rec["internal_council_id"] == "12697":
+            rec["location"] = Point(-1.660097, 54.971494, srid=4326)
+
+        # Gosforth Parish Church Hall, Wardle Street, Off Church Avenue, Newcastle upon Tyne
+        if rec["internal_council_id"] == "12650":
+            rec["location"] = Point(-1.608869, 55.006395, srid=4326)
+
+        return rec
