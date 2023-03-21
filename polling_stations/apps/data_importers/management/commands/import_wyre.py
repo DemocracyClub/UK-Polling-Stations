9ff94fac93a4d5a113720a74b7d9a6a218f9509c
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-21 14:04:31 +0000

    Import script for Wyre (2023-05-04) (closes #5349)

--- polling_stations/apps/data_importers/management/commands/import_wyre.py
+++ polling_stations/apps/data_importers/management/commands/import_wyre.py
@@ -3,41 +3,35 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WYR"
-    addresses_name = "2021-02-15T11:25:19.329853/Wyre Democracy_Club__06May2021.CSV"
-    stations_name = "2021-02-15T11:25:19.329853/Wyre Democracy_Club__06May2021.CSV"
-    elections = ["2021-05-06"]
-    csv_delimiter = ","
+    addresses_name = (
+        "2023-05-04/2023-03-21T14:04:15.276082/Democracy_Club__04May2023.tsv"
+    )
+    stations_name = (
+        "2023-05-04/2023-03-21T14:04:15.276082/Democracy_Club__04May2023.tsv"
+    )
+    elections = ["2023-05-04"]
     csv_encoding = "windows-1252"
-
-    def station_record_to_dict(self, record):
-        if record.polling_place_id == "4304":  # Charles Saer Community Primary School
-            record = record._replace(polling_place_postcode="FY7 8DD")
-
-        if (
-            record.polling_place_id == "4295"
-        ):  # Senior Citizens Assoc Hall, 190 Victoria Road West, Thornton Cleveleys - FY5 7JA ==> FY5 3NG
-            record = record._replace(polling_place_postcode="FY5 3NG")
-
-        return super().station_record_to_dict(record)
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
-        if record.post_code in [
-            "FY6 8FS",  # Astray postcode
-            "FY6 7GH",  # Astray postcode
-            "FY7 6FJ",  # Astray postcode
-            "PR3 1TS",  # Astray postcode
-            "FY5 3LS",  # Postcode split over large distance
-            "FY6 9EX",  # Postcode split over large distance
-        ]:
-            return None
-
         if uprn in [
-            "10034088556",  # FLAT AT GREAT SEASONS GARSTANG BYPASS ROAD, GARSTANG, PR3 1PH
-            "10093998387",  # 97A CROSTON ROAD, GARSTANG, PR3 0HQ - postcode likely should be PR3 1HQ
+            "10003527163",  # 3 GARNET CLOSE, THORNTON-CLEVELEYS
+            "10024171572",  # WELL WOOD VIEW, STATION LANE, SCORTON, PRESTON
+            "10093999437",  # PLEASANT VIEW, BLACKPOOL ROAD, POULTON-LE-FYLDE
+            "10034085598",  # MANOR VALE, ST. MICHAELS ROAD, BILSBORROW, PRESTON
+            "100012422216",  # MANOR HOUSE FARM, ST. MICHAELS ROAD, BILSBORROW, PRESTON
+            "10034083802",  # FIELDVIEW, PINFOLD LANE, SOWERBY, PRESTON
         ]:
             return None
 
-        return rec
+        if record.post_code in [
+            "FY6 7GH",  # CHURCH COURT, POULTON-LE-FYLDE
+            "FY7 6FJ",  # SPINNAKER CLOSE, FLEETWOOD
+            "PR3 1TS",  # FOREST VIEW PLACE, SCORTON
+            "PR3 1ZW",  # CEDAR WOOD CLOSE, BOWGREAVE, PRESTON
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
