6ff492ea05c0ba4766d0b0d8dd5ca51721e191d7
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-21 16:27:15 +0000

    Import script for Crawley (2023-05-04) (closes #5351)

--- polling_stations/apps/data_importers/management/commands/import_crawley.py
+++ polling_stations/apps/data_importers/management/commands/import_crawley.py
@@ -4,28 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "CRW"
     addresses_name = (
-        "2022-05-05/2022-03-23T15:28:26.755741/Democracy_Club__05May2022.tsv"
+        "2023-05-04/2023-03-21T16:26:51.918460/Democracy_Club__04May2023.tsv"
     )
     stations_name = (
-        "2022-05-05/2022-03-23T15:28:26.755741/Democracy_Club__05May2022.tsv"
+        "2023-05-04/2023-03-21T16:26:51.918460/Democracy_Club__04May2023.tsv"
     )
-    elections = ["2022-05-05"]
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
 
-    def station_record_to_dict(self, record):
-        # Furnace Green Community Centre
-        if record.polling_place_id == "1056":
-            record = record._replace(polling_place_easting="528414")
-            record = record._replace(polling_place_northing="135782")
-
-        # Wakehams Green Community Centre
-        if record.polling_place_id == "1103":
-            record = record._replace(polling_place_easting="529968")
-            record = record._replace(polling_place_northing="138172")
-
-        # Southgate West Community Centre
-        if record.polling_place_id == "1073":
-            record = record._replace(polling_place_easting="526307")
-            record = record._replace(polling_place_northing="135610")
-
-        return super().station_record_to_dict(record)
+    # Nothing to correct, hurray!
