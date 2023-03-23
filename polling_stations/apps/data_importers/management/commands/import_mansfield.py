3bb27ab3784913dc669dcebb0ab78d1fd2cb47ee
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-23 16:12:11 +0000

    Import script for Mansfield (2023-05-04) (closes #5367)

--- polling_stations/apps/data_importers/management/commands/import_mansfield.py
+++ polling_stations/apps/data_importers/management/commands/import_mansfield.py
@@ -3,33 +3,6 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "MAS"
-    addresses_name = "2021-03-17T14:10:56.004783/polling_station_export-2021-03-17.csv"
-    stations_name = "2021-03-17T14:10:56.004783/polling_station_export-2021-03-17.csv"
-    elections = ["2021-05-06"]
-    csv_delimiter = ","
-
-    def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
-
-        if uprn in [
-            "10091484435",  # 33B ALBERT STREET, MANSFIELD
-            "100031427156",  # 207 WESTFIELD LANE, MANSFIELD
-            "100031413924",  # FIRBANK HOUSE, NEWLANDS ROAD, FOREST TOWN, MANSFIELD
-            "100031415038",  # GREENACRES, OAKFIELD LANE, WARSOP, MANSFIELD
-            "10023932919",  # GLEADTHORPE GRANGE FARM NETHERFIELD LANE, MEDEN VALE
-        ]:
-            return None
-
-        if record.housepostcode in [
-            "NG19 6JF",
-            "NG19 6AT",
-            "NG18 5RT",
-            "NG18 1EU",
-            "NG18 1ER",
-            "NG18 1EJ",
-            "NG18 1BQ",
-            "NG18 4LN",
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
+    addresses_name = "2023-05-04/2023-03-23T16:11:56.327479/Eros_SQL_Output005.csv"
+    stations_name = "2023-05-04/2023-03-23T16:11:56.327479/Eros_SQL_Output005.csv"
+    elections = ["2023-05-04"]
