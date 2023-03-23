9c2652e2119789a863b3f14972f99247c1a0639a
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-23 15:30:51 +0000

    Import script for Wigan (2023-05-04) (closes #5364)

--- polling_stations/apps/data_importers/management/commands/import_wigan.py
+++ polling_stations/apps/data_importers/management/commands/import_wigan.py
@@ -3,28 +3,46 @@ from data_importers.management.commands import BaseXpressWebLookupCsvImporter
 
 class Command(BaseXpressWebLookupCsvImporter):
     council_id = "WGN"
-    addresses_name = "2022-05-05/2022-04-07T12:02:19.829718/PropertyPostCodePollingStationWebLookup-2022-04-07.TSV"
-    stations_name = "2022-05-05/2022-04-07T12:02:19.829718/PropertyPostCodePollingStationWebLookup-2022-04-07.TSV"
-    elections = ["2022-05-05"]
-    csv_delimiter = "\t"
+    addresses_name = "2023-05-04/2023-03-23T15:30:38.891498/PropertyPostCodePollingStationWebLookup-2023-03-23.CSV"
+    stations_name = "2023-05-04/2023-03-23T15:30:38.891498/PropertyPostCodePollingStationWebLookup-2023-03-23.CSV"
+    elections = ["2023-05-04"]
 
     def address_record_to_dict(self, record):
+        uprn = record.uprn.strip().lstrip("0")
+
+        if uprn in [
+            "10095422568",  # 2 ELDERFIELD, WORSLEY, MANCHESTER
+            "200002861926",  # THE COTTAGE, SANDY LANE, HINDLEY, WIGAN
+            "100012500668",  # DODHURST BROW FARM, SANDY LANE, HINDLEY, WIGAN
+            "10009207679",  # 2A NORLEY HALL AVENUE, WIGAN
+            "100011823007",  # 165 WIGAN ROAD, ASHTON-IN-MAKERFIELD, WIGAN
+        ]:
+            return None
+
         if record.postcode in [
+            # splits
+            "WN1 2QL",
+            "WN1 2PQ",
+            "WN7 1LU",
             "WN7 2BL",
             "WN6 7NZ",
-            "WN1 2PQ",
-            "WN1 2QL",
+            "WA3 3UJ",
         ]:
             return None
 
         return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
-        if record.pollingplaceid in [
-            "7863",  # Standish Community Centre Moody Street Off Church Street Standish WN6 0JY
-            "7848",  # Shevington Youth Club, Highfield Avenue
-        ]:
-            record = record._replace(pollingplaceeasting="")
-            record = record._replace(pollingplacenorthing="")
+        # St Aidans Parish Centre, Highfield Grange Ave, Winstanley, Wigan, WN3 6EE
+        if record.pollingplaceid == "9100":
+            record = record._replace(
+                pollingplaceeasting="355847", pollingplacenorthing="402852"
+            )
+
+        # Standish Community Centre, Moody Street, Off Church, Street Standish, WN6 0JY
+        if record.pollingplaceid == "8982":
+            record = record._replace(
+                pollingplaceeasting="356374", pollingplacenorthing="410043"
+            )
 
         return super().station_record_to_dict(record)
