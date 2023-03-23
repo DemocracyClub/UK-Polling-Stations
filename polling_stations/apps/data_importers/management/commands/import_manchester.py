c88506c6364ac022a4dd1e6b1d8a36d8d0be376d
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-23 16:33:10 +0000

    Import script for Manchester (2023-05-04) (closes #5369)

--- polling_stations/apps/data_importers/management/commands/import_manchester.py
+++ polling_stations/apps/data_importers/management/commands/import_manchester.py
@@ -1,38 +1,10 @@
-from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
+from data_importers.management.commands import Unknown
 
 
-class Command(BaseXpressDemocracyClubCsvImporter):
+class Command(Unknown):
     council_id = "MAN"
-    addresses_name = (
-        "2022-05-05/2022-03-24T10:46:00.928091/Democracy_Club__05May2022.tsv"
-    )
-    stations_name = (
-        "2022-05-05/2022-03-24T10:46:00.928091/Democracy_Club__05May2022.tsv"
-    )
-    elections = ["2022-05-05"]
+    addresses_name = "2023-05-04/2023-03-23T16:29:47.358436/Polling_Place_Postcode_Lookup__04May2023.tsv"
+    stations_name = "2023-05-04/2023-03-23T16:29:47.358436/Polling_Place_Postcode_Lookup__04May2023.tsv"
+    elections = ["2023-05-04"]
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
-
-    def station_record_to_dict(self, record):
-        # Moston Methodist Church corner of Ilkley Street and Moston Lane Moston Manchester
-        if record.polling_place_id == "9372":
-            record = record._replace(
-                polling_place_easting="387244", polling_place_northing="401944"
-            )
-
-        if record.addressline1 in ["NEW POLLING STATION", "CHANGE OF STATION"]:
-            record = record._replace(addressline1="")
-
-        return super().station_record_to_dict(record)
-
-    def address_record_to_dict(self, record):
-        if record.addressline6 in [
-            "M8 9AE",
-            "M1 2PE",
-            "M12 5PR",
-            "M8 4RB",
-            "M22 8BE",
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
