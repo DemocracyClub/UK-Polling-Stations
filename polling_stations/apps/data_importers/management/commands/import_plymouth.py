b78cfe4640659aeb05a79b23aa81c92b90314857
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-29 16:44:30 +0100

    Import script for Plymouth (2023-05-04) (closes #5417)

--- polling_stations/apps/data_importers/management/commands/import_plymouth.py
+++ polling_stations/apps/data_importers/management/commands/import_plymouth.py
@@ -4,10 +4,51 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "PLY"
     addresses_name = (
-        "2023-01-12/2022-12-14T10:55:16.177854/Democracy_Club__12January2023.tsv"
+        "2023-05-04/2023-03-29T16:44:17.695108/Democracy_Club__04May2023.tsv"
     )
     stations_name = (
-        "2023-01-12/2022-12-14T10:55:16.177854/Democracy_Club__12January2023.tsv"
+        "2023-05-04/2023-03-29T16:44:17.695108/Democracy_Club__04May2023.tsv"
     )
-    elections = ["2023-01-12"]
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
+
+    def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "100040493091",  # THE HOLLOW, TAMERTON FOLIOT ROAD, PLYMOUTH
+            "10070767325",  # CANN LODGE, TAMERTON FOLIOT, PLYMOUTH
+            "10070771403",  # FLAT 1, 24 DRINA LANE, PLYMOUTH
+            "10070771404",  # FLAT 2, 24 DRINA LANE, PLYMOUTH
+            "100040409446",  # LOWER GROUND FLOOR FLAT 11 ATHENAEUM STREET, PLYMOUTH
+            "100040409450",  #  14A ATHENAEUM STREET, PLYMOUTH
+            "10012062433",  # POINT COTTAGE, SALTRAM, PLYMOUTH
+            "10012058496",  # HARDWICKE LODGE, DRUNKEN BRIDGE HILL, PLYMPTON, PLYMOUTH
+            "10091562408",  # 43 PLYMBRIDGE ROAD, PLYMPTON, PLYMOUTH
+            "100040489286",  # 180 ST. MARGARETS ROAD, PLYMOUTH
+            "100040475894",  # 1 PLYMBRIDGE ROAD, PLYMPTON, PLYMOUTH
+            "10094641493",  # FLAT 1, 16A COLEBROOK ROAD, PLYMPTON, PLYMOUTH
+            "10094641494",  # FLAT 2, 16A COLEBROOK ROAD, PLYMPTON, PLYMOUTH
+            "100040415376",  # FLAT A ELFORDE HOUSE BLANDFORD ROAD, PLYMOUTH
+            "100040415377",  # FLAT B ELFORDE HOUSE BLANDFORD ROAD, PLYMOUTH
+            "10094641036",  # FLAT ARMY RESERVE CENTRE BREST ROAD, PLYMOUTH
+            "100040439817",  # FORDER COTTAGE, FORDER VALLEY ROAD, PLYMOUTH
+            "100040439788",  # 65 FORD PARK ROAD, PLYMOUTH
+            "100040482969",  # FIRST FLOOR FLAT 52 SALISBURY ROAD, PLYMOUTH
+            "100040482972",  # HUNNY B FLORIST, 55 SALISBURY ROAD, PLYMOUTH
+            "100040434952",  # CARETAKERS FLAT (FLAT 1) METHODIST CENTRAL HALL EASTLAKE STREET, PLYMOUTH
+            "10091564022",  # FLAT 2 METHODIST CENTRAL HALL EASTLAKE STREET, PLYMOUTH
+        ]:
+            return None
+
+        if record.addressline6 in [
+            # splits
+            "PL4 7QB",
+            "PL3 6EP",
+            "PL2 3BL",  # WOLSELEY ROAD, PLYMOUTH
+            "PL7 1UF",  # RIDGE ROAD, PLYMPTON, PLYMOUTH
+            "PL7 1UE",  # HEATHER GRANGE RIDGE ROAD, PLYMOUTH
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
