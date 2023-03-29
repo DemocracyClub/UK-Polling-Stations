f3eb5382c385f47757fe1a6d8142e1f179fcc123
GeoWill <roper.william@gmail.com>
2023-03-29 12:37:48 +0100

    Import script for Sefton (2023-05-04) (closes #5326)

--- polling_stations/apps/data_importers/management/commands/import_sefton.py
+++ polling_stations/apps/data_importers/management/commands/import_sefton.py
@@ -4,28 +4,25 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SFT"
     addresses_name = (
-        "2022-05-05/2022-03-23T16:21:32.597129/Democracy_Club__05May2022.tsv"
+        "2023-05-04/2023-03-18T09:10:00.674374/Democracy_Club__04May2023.tsv"
     )
     stations_name = (
-        "2022-05-05/2022-03-23T16:21:32.597129/Democracy_Club__05May2022.tsv"
+        "2023-05-04/2023-03-18T09:10:00.674374/Democracy_Club__04May2023.tsv"
     )
-    elections = ["2022-05-05"]
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
 
 
 def address_record_to_dict(self, record):
+    uprn = record.property_urn.strip().lstrip("0")
+
+    if uprn in [
+        "41063021",  # 8 MOOR LANE, THORNTON, LIVERPOOL
+    ]:
+        return None
     if record.addressline6 in [
         "L23 7TX",
-        "L20 6EA",
     ]:
         return None
 
     return super().address_record_to_dict(record)
-
-
-def station_record_to_dict(self, record):
-    # Bootle Main Library 220 Stanley Road Bootle L20 3GN
-    if record.polling_place_id == "8672":
-        record = record._replace(polling_place_postcode="")
-
-    return super().station_record_to_dict(record)
