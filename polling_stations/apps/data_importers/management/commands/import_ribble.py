89d02caab381c7496b1055182816a9bc35fd14fb
GeoWill <roper.william@gmail.com>
2023-03-29 15:27:45 +0100

    Import script for Ribble Valley (2023-05-04) (closes #5116)

--- polling_stations/apps/data_importers/management/commands/import_ribble.py
+++ polling_stations/apps/data_importers/management/commands/import_ribble.py
@@ -1,34 +1,50 @@
-from data_importers.base_importers import BaseShpStationsShpDistrictsImporter
+from django.contrib.gis.geos import Point
+
+from data_importers.base_importers import BaseStationsDistrictsImporter
 
 
-class Command(BaseShpStationsShpDistrictsImporter):
+class Command(BaseStationsDistrictsImporter):
     srid = 27700
     council_id = "RIB"
-    elections = ["2021-05-06"]
-    districts_name = "2021-03-25T12:14:33.026795747/RVBC Polling Districts.shp"
-    stations_name = "2021-03-25T12:14:33.026795747/RVBC Polling Stations 2021.shp"
-
-    def district_record_to_dict(self, record):
-        return {"internal_council_id": record[1], "name": record[2]}
+    elections = ["2023-05-04"]
+    districts_name = "2023-05-04/2023-03-29T13:51:45/PD.geojson"
+    stations_name = "2023-05-04/2023-03-29T13:51:45/PS.geojson"
+    stations_filetype = "geojson"
+    districts_filetype = "geojson"
 
     def station_record_to_dict(self, record):
         stations = []
-
-        name = record[1].strip()
-        address = record[2].strip()
-        postcode = record[3].strip()
-
-        district_codes = [record[4].strip(), record[5].strip(), record[6].strip()]
-        district_codes = [code for code in district_codes if code]
-
-        for code in district_codes:
+        codes = (
+            c.strip()
+            for c in (
+                record["properties"]["CODE"],
+                record["properties"]["CODE 2"],
+                record["properties"]["CODE 3"],
+            )
+            if c
+        )
+        name = record["properties"]["Name"].strip()
+        address = record["properties"]["Address"].strip()
+        postcode = record["properties"]["Postcode"].strip()
+        location = Point(
+            record["properties"]["longitude"],
+            record["properties"]["latitude"],
+            srid=4326,
+        )
+        for code in codes:
             stations.append(
                 {
                     "internal_council_id": code,
-                    "address": "%s\n%s" % (name, address),
+                    "address": f"{name}\n{address}",
                     "postcode": postcode,
                     "polling_district_id": code,
+                    "location": location,
                 }
             )
-
         return stations
+
+    def district_record_to_dict(self, record):
+        return {
+            "internal_council_id": record["properties"]["CODE"].strip(),
+            "name": record["properties"]["NAME"],
+        }
