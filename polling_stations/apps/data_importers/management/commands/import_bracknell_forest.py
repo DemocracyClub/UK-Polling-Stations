0cc109be64dbab9e517cf1d6ae34583d87c45dc4
polling-bot-4000 <developers@democracyclub.org.uk>
2023-03-30 16:50:42 +0100

    Import script for Bracknell Forest (2023-05-04) (closes #5428)

--- polling_stations/apps/data_importers/management/commands/import_bracknell_forest.py
+++ polling_stations/apps/data_importers/management/commands/import_bracknell_forest.py
@@ -1,20 +1,18 @@
-from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
+from data_importers.management.commands import BaseHalaroseCsvImporter
 
 
-class Command(BaseXpressDemocracyClubCsvImporter):
+class Command(BaseHalaroseCsvImporter):
     council_id = "BRC"
-    addresses_name = "2021-04-12T13:33:17.499979/Democracy_Club__06May2021.tsv"
-    stations_name = "2021-04-12T13:33:17.499979/Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
-    csv_delimiter = "\t"
+    addresses_name = "2023-05-04/2023-03-30T16:50:30.358526/Eros_SQL_Output002.csv"
+    stations_name = "2023-05-04/2023-03-30T16:50:30.358526/Eros_SQL_Output002.csv"
+    elections = ["2023-05-04"]
 
     def address_record_to_dict(self, record):
         if record.addressline6 in [
-            "RG42 6HB",
+            # split
+            "SL5 8RY",
+            "RG42 2FB",
             "RG42 6BX",
-            "SL5 8DH",
-            "RG12 9TH",
-            "RG40 3YZ",
         ]:
             return None
 
