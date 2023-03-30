a20c70a4586dd442e2792f89295149c4a47481b1
Will Roper <GeoWill@users.noreply.github.com>
2023-03-30 18:43:45 +0100

    Import script for East Lindsey (2023-05-04) (closes #5337)

--- polling_stations/apps/data_importers/management/commands/import_east_lindsey.py
+++ polling_stations/apps/data_importers/management/commands/import_east_lindsey.py
@@ -3,50 +3,56 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "ELI"
-    addresses_name = "2021-03-29T14:54:03.784744/Democracy_Club__06May2021.csv"
-    stations_name = "2021-03-29T14:54:03.784744/Democracy_Club__06May2021.csv"
-    elections = ["2021-05-06"]
-    csv_delimiter = ","
+    addresses_name = (
+        "2023-05-04/2023-03-20T15:39:49.119114/Democracy_Club__04May2023.tsv"
+    )
+    stations_name = (
+        "2023-05-04/2023-03-20T15:39:49.119114/Democracy_Club__04May2023.tsv"
+    )
+    elections = ["2023-05-04"]
+    csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
-        # St Marys Church Church Lane Fotherby Louth
-        if record.polling_place_id == "9064":
+        #  Multi Purpose Room, Meridian Leisure Centre Wood Lane, Louth, Lincolnshire, LN11 8RA
+        if record.polling_place_id == "9885":
+            record = record._replace(polling_place_postcode="LN11 8SA")
+
+        # St Marys Church, Church Lane, Fotherby, Louth, LN11 0UJ
+        if record.polling_place_id == "9974":
             record = record._replace(polling_place_easting="531703")
             record = record._replace(polling_place_northing="391688")
-
-        # Church Institute Church Lane South Elkington LN11 OSA
-        if record.polling_place_id == "9060":
-            record = record._replace(polling_place_postcode="LN11 0SA")
 
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
-        if uprn == "10008528388":  # AMERICA FARM, HANNAH, ALFORD
-            record = record._replace(addressline6="LN13 9QP")
-
         if uprn in [
-            "100030775814",  # SEA SHADOW, CHURCHILL LANE, THEDDLETHORPE, MABLETHORPE
             "200001828818",  # GRANGE FARM, WELTON-LE-MARSH, SPILSBY
             "10024297692",  # LAKE VIEW STATION ROAD, LITTLE STEEPING
             "100032167500",  # WINDSOR COTTAGE, HAKERLEY BRIDGE, FRITHVILLE, BOSTON
             "200002780835",  # BARBRIDGE HOUSE, MAIN ROAD, SIBSEY, BOSTON
-            "100030754138",  # NORTHERN LODGE, MAIN ROAD, SIBSEY, BOSTON
+            "100032077624",  # EVENWOOD, FIRE BEACON LANE, COVENHAM ST. BARTHOLOMEW, LOUTH
+            "100032260114",  # FIREBEACON BRIDGE FARM, FIREBEACON LANE, WRAGHOLME, LOUTH
+            "10090638895",  # THE PADDOCKS, CROFT LANE, CROFT, SKEGNESS
+            "100032166988",  # STONEY BROKE, TRADER BANK, SIBSEY, BOSTON
+            "10008523556",  # JASMINE LODGE, MAIN ROAD, SIBSEY, BOSTON
         ]:
             return None
 
         if record.addressline6 in [
-            "PE24 5RE",
-            "PE24 5UT",
-            "LN9 5JP",
+            # splits
             "LN11 0EG",
+            "LN11 8QG",
+            "LN9 5JP",
             "LN11 8DW",
             "LN12 2HX",
             "PE25 2PX",
-            "PE25 3BS",
-            "PE22 8DQ",
-            "PE25 1SH",
+            "PE24 5UT",
+            "PE25 1SH",  # TILE HOUSE, ROSE GROVE, SKEGNESS
+            "PE25 2BG",  # LEEMING DRIVE, SKEGNESS
+            "PE25 2FD",  # RAY CLEMENCE WAY, SKEGNESS
+            "PE22 7SL",  # STATION ROAD, TUMBY WOODSIDE, BOSTON
         ]:
             return None
 
