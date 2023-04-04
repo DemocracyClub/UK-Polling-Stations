c757c9e8d7ef214e615373b36ee039307f352561
polling-bot-4000 <developers@democracyclub.org.uk>
2023-04-04 11:27:38 +0100

    Import script for South Norfolk (2023-05-04) (closes #5461)

--- polling_stations/apps/data_importers/management/commands/import_south_norfolk.py
+++ polling_stations/apps/data_importers/management/commands/import_south_norfolk.py
@@ -3,8 +3,8 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "SNO"
-    addresses_name = "2023-05-04/2023-03-02T16:39:18.229053/DemocracyClub-PollingDistricts-May2023-1.csv"
-    stations_name = "2023-05-04/2023-03-02T16:39:18.229053/DemocracyClub-PollingStations-May2023-1.csv"
+    addresses_name = "2023-05-04/2023-04-04T11:27:01.256937/3DemocracyClub_PollingDistricts_040523.csv"
+    stations_name = "2023-05-04/2023-04-04T11:27:01.256937/3DemocracyClub_PollingStations_04052023.csv"
     elections = ["2023-05-04"]
 
     def station_record_to_dict(self, record):
