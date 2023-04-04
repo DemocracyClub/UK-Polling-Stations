fcc8adec0be44f3ee7a8641a4f4b0641c0c3b182
polling-bot-4000 <developers@democracyclub.org.uk>
2023-04-04 11:04:41 +0100

    Import script for Wakefield (2023-05-04) (closes #5455)

--- polling_stations/apps/data_importers/management/commands/import_wakefield.py
+++ polling_stations/apps/data_importers/management/commands/import_wakefield.py
@@ -3,16 +3,6 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "WKF"
-    addresses_name = "2022-06-23/2022-05-24T13:35:14.816630/Democracy Club - Polling Districts - June 2022 Parliamentary By Election Wakefield.csv"
-    stations_name = "2022-06-23/2022-05-24T13:35:14.816630/Democracy Club - Polling Stations - June 2022 Parliamentary By Election Wakefield.csv"
-    elections = ["2022-06-23"]
-
-    def address_record_to_dict(self, record):
-        if record.postcode.strip() in [
-            "WF5 0RT",
-            "WF5 0NR",
-            "WF1 4GA",
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
+    addresses_name = "2023-05-04/2023-04-04T11:04:06.375522/20230320_DemocracyClubPollingDistricts_DistrictElections4May2023.csv"
+    stations_name = "2023-05-04/2023-04-04T11:04:06.375522/20230320_DemocracyClubPollingStations_DistrictElections4May2023.csv"
+    elections = ["2023-05-04"]
