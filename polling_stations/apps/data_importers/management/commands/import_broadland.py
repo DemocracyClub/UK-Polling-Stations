b83b84346e5fd3b7d21175ef243cc19ff73564db
polling-bot-4000 <developers@democracyclub.org.uk>
2023-04-04 11:20:20 +0100

    Import script for Broadland (2023-05-04) (closes #5459)

--- polling_stations/apps/data_importers/management/commands/import_broadland.py
+++ polling_stations/apps/data_importers/management/commands/import_broadland.py
@@ -3,8 +3,8 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "BRO"
-    addresses_name = "2023-05-04/2023-03-02T16:21:01.206095/DemocracyClub-PollingDistricts-May2023-1.csv"
-    stations_name = "2023-05-04/2023-03-02T16:21:01.206095/DemocracyClub-PollingStations-May2023-1.csv"
+    addresses_name = "2023-05-04/2023-04-04T11:19:44.580379/3DemocracyClub_PollingDistricts_040523.csv"
+    stations_name = "2023-05-04/2023-04-04T11:19:44.580379/3DemocracyClub_PollingStations_04052023.csv"
     elections = ["2023-05-04"]
 
     def station_record_to_dict(self, record):
