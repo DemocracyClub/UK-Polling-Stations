7406699dc9c12d8fdfc5ad51da20539bb96a3b76
polling-bot-4000 <developers@democracyclub.org.uk>
2023-04-04 11:04:41 +0100

    Import script for Wakefield (2023-05-04) (closes #5455)

--- polling_stations/apps/data_importers/management/commands/import_wakefield.py
+++ polling_stations/apps/data_importers/management/commands/import_wakefield.py
@@ -3,16 +3,38 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "WKF"
-    addresses_name = "2022-06-23/2022-05-24T13:35:14.816630/Democracy Club - Polling Districts - June 2022 Parliamentary By Election Wakefield.csv"
-    stations_name = "2022-06-23/2022-05-24T13:35:14.816630/Democracy Club - Polling Stations - June 2022 Parliamentary By Election Wakefield.csv"
-    elections = ["2022-06-23"]
+    addresses_name = "2023-05-04/2023-04-04T11:04:06.375522/20230320_DemocracyClubPollingDistricts_DistrictElections4May2023.csv"
+    stations_name = "2023-05-04/2023-04-04T11:04:06.375522/20230320_DemocracyClubPollingStations_DistrictElections4May2023.csv"
+    elections = ["2023-05-04"]
 
     def address_record_to_dict(self, record):
+        uprn = record.uprn.lstrip("0").strip()
+
+        if uprn in [
+            "63205108",  # THE COTTAGE, WENTBRIDGE, PONTEFRACT
+            "63111468",  # 14 THE GREEN, SOUTH KIRKBY, PONTEFRACT
+            "63200615",  # FLAT 1 PLUMPTON STREET, WAKEFIELD
+            "63194364",  # 2 STARLING WAY, CASTLEFORD
+        ]:
+            return None
+
         if record.postcode.strip() in [
+            # split
+            "WF2 6JA",
             "WF5 0RT",
-            "WF5 0NR",
-            "WF1 4GA",
+            # wrong
+            "WF9 2FB",
+            "WF9 2FA",
         ]:
             return None
 
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        if record.stationcode in [
+            "152-20WF",  # WESTGATE COMMON W.M.C.
+            "23-03NG",  # THREE LANE ENDS ACADEMY
+        ]:
+            record = record._replace(xordinate="", yordinate="")
+
+        return super().station_record_to_dict(record)
