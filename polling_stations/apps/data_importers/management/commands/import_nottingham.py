911c0175a40e93fe9caa5880c2d28a094f66868c
polling-bot-4000 <developers@democracyclub.org.uk>
2023-04-05 16:34:23 +0100

    Import script for Nottingham (2023-05-04) (closes #5485)

--- polling_stations/apps/data_importers/management/commands/import_nottingham.py
+++ polling_stations/apps/data_importers/management/commands/import_nottingham.py
@@ -1,30 +1,22 @@
-from data_importers.github_importer import BaseGitHubImporter
+from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
-class Command(BaseGitHubImporter):
-    srid = 4326
-    districts_srid = 4326
+class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "NGM"
-    elections = ["2021-05-06"]
-    scraper_name = "wdiv-scrapers/DC-PollingStations-Nottingham"
-    geom_type = "geojson"
+    addresses_name = (
+        "2023-05-04/2023-04-05T16:34:11.754241/Democracy_Club__04May2023.tsv"
+    )
+    stations_name = (
+        "2023-05-04/2023-04-05T16:34:11.754241/Democracy_Club__04May2023.tsv"
+    )
+    elections = ["2023-05-04"]
+    csv_delimiter = "\t"
 
-    def district_record_to_dict(self, record):
-        poly = self.extract_geometry(record, self.geom_type, self.get_srid("districts"))
-        return {
-            "internal_council_id": record["POLLINGDIS"],
-            "name": record["POLLINGDIS"],
-            "area": poly,
-            "polling_station_id": record["POLLINGDIS"],
-        }
+    def address_record_to_dict(self, record):
+        if record.addressline6 in [
+            # split
+            "NG7 1BZ",
+        ]:
+            return None
 
-    def station_record_to_dict(self, record):
-        location = self.extract_geometry(
-            record, self.geom_type, self.get_srid("stations")
-        )
-        return {
-            "internal_council_id": record["CONST"],
-            "postcode": "",
-            "address": record["NAME"] + "\n" + record["ADDRESS"],
-            "location": location,
-        }
+        return super().address_record_to_dict(record)
