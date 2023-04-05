f45cee43092e3a9b942accf6d4658b0e1c04f6dc
polling-bot-4000 <developers@democracyclub.org.uk>
2023-04-05 11:22:20 +0100

    Import script for Redditch (2023-05-04) (closes #5473)

--- polling_stations/apps/data_importers/management/commands/import_redditch.py
+++ polling_stations/apps/data_importers/management/commands/import_redditch.py
@@ -4,9 +4,17 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "RED"
     addresses_name = (
-        "2022-05-05/2022-03-10T16:07:01.999058/Democracy_Club__05May2022.CSV"
+        "2023-05-04/2023-04-05T11:22:08.304289/Redditch Democracy_Club__04May2023.CSV"
     )
     stations_name = (
-        "2022-05-05/2022-03-10T16:07:01.999058/Democracy_Club__05May2022.CSV"
+        "2023-05-04/2023-04-05T11:22:08.304289/Redditch Democracy_Club__04May2023.CSV"
     )
-    elections = ["2022-05-05"]
+    elections = ["2023-05-04"]
+
+    def station_record_to_dict(self, record):
+        # Matchborough Meeting Rooms Clifton Close Off Breaches Lane Redditch
+        if record.polling_place_id == "7688":
+            record = record._replace(polling_place_easting="")
+            record = record._replace(polling_place_northing="")
+
+        return super().station_record_to_dict(record)
