109848c7f819349e080eac4eea95b75f8a4ce22c
polling-bot-4000 <developers@democracyclub.org.uk>
2023-04-05 12:04:35 +0100

    Import script for Reigate and Banstead (2023-05-04) (closes #5475)

--- polling_stations/apps/data_importers/management/commands/import_reigate_and_banstead.py
+++ polling_stations/apps/data_importers/management/commands/import_reigate_and_banstead.py
@@ -1,29 +1,30 @@
-from data_importers.management.commands import BaseDemocracyCountsCsvImporter
+from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
-class Command(BaseDemocracyCountsCsvImporter):
+class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "REI"
-    addresses_name = "2022-05-05/2022-04-08T09:12:49.913080/RBBC Polling Districts.csv"
-    stations_name = "2022-05-05/2022-04-08T09:12:49.913080/RBBC Polling Stations.csv"
-    elections = ["2022-05-05"]
+    addresses_name = (
+        "2023-05-04/2023-04-05T12:04:27.040829/Democracy_Club__04May2023.tsv"
+    )
+    stations_name = (
+        "2023-05-04/2023-04-05T12:04:27.040829/Democracy_Club__04May2023.tsv"
+    )
+    elections = ["2023-05-04"]
+    csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
         # Woodmansterne Village Hall, Carshalton Road, Woodmansterne, Banstead, Surrey
-        if record.stationcode == "CKW1":
-            record = record._replace(xordinate="527572", yordinate="160117")
-
-        # St John`s Church Hall, The Avenue, Tadworth, Surrey
-        if record.stationcode in ["KTW1.1", "KTW1.2"]:
-            record = record._replace(postcode="KT20 5DB")  # was KT20 5AB
+        if record.polling_place_id == "4916":
+            record = record._replace(
+                polling_place_easting="527572", polling_place_northing="160117"
+            )
 
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-        uprn = record.uprn.lstrip("0")
+        uprn = record.property_urn.lstrip("0")
 
         if uprn in [
-            "68176955",  # 1 PUTNEY CROFY, HORLEY
-            "68176948",  # 2 PUTNEY CROFY, HORLEY
             "68181592",  # 2A PARKHURST ROAD, HORLEY
         ]:
             return None
