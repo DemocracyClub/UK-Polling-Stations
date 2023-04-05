f3b71a84dbe4a9000298e56f42d487b9743fa7b4
polling-bot-4000 <developers@democracyclub.org.uk>
2023-04-05 14:43:22 +0100

    Import script for Redcar and Cleveland (2023-05-04) (closes #5480)

--- polling_stations/apps/data_importers/management/commands/import_redcar_cleveland.py
+++ polling_stations/apps/data_importers/management/commands/import_redcar_cleveland.py
@@ -3,27 +3,21 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "RCC"
-    addresses_name = "2021-03-10T11:51:24.840947/Redcar Democracy_Club__06May2021.tsv"
-    stations_name = "2021-03-10T11:51:24.840947/Redcar Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
+    addresses_name = (
+        "2023-05-04/2023-04-05T14:43:08.187824/Democracy_Club__04May2023.tsv"
+    )
+    stations_name = (
+        "2023-05-04/2023-04-05T14:43:08.187824/Democracy_Club__04May2023.tsv"
+    )
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "10034524783",  # SCHOOL HOUSE, STATION LANE, SKELTON-IN-CLEVELAND, SALTBURN-BY-THE-SEA
+        if record.addressline6 in [
+            # split
+            "TS10 4AJ",
+            "TS6 0PA",
         ]:
             return None
 
-        if record.addressline6 in ["TS10 4AJ"]:
-            return None
-
         return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # Whale Hill Community Centre Goathland Road Whale Hill Estate Eston TS6 8EW
-        if record.polling_place_id == "15850":
-            record = record._replace(polling_place_postcode="TS6 8AW")
-
-        return super().station_record_to_dict(record)
