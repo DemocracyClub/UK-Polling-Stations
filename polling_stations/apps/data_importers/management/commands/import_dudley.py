3cf719ff64a130353345c9716e1c6b913f41b3ad
blacktyger <blacktyg3r@gmail.com>
2023-04-05 13:27:51 +0100

    Import script for Dudley (2023-05-04) (closes #5384)

--- polling_stations/apps/data_importers/management/commands/import_dudley.py
+++ polling_stations/apps/data_importers/management/commands/import_dudley.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "DUD"
     addresses_name = (
-        "2022-05-05/2022-04-13T09:59:32.342801/Democracy_Club__05May2022.tsv"
+        "2023-05-04/2023-03-26T19:37:35.489411/Democracy_Club__04May2023.tsv"
     )
     stations_name = (
-        "2022-05-05/2022-04-13T09:59:32.342801/Democracy_Club__05May2022.tsv"
+        "2023-05-04/2023-03-26T19:37:35.489411/Democracy_Club__04May2023.tsv"
     )
-    elections = ["2022-05-05"]
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
     csv_encoding = "windows-1252"
 
@@ -19,16 +19,25 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         if uprn in [
             "90159047",  # THE JAYS, HYPERION ROAD, STOURTON, STOURBRIDGE
             "90153608",  # NEW BROMLEY FARM, BROMLEY LANE, KINGSWINFORD
-            "90127392",  # FLAT A 106 STOURBRIDGE ROAD, DUDLEY
             "90135708",  # 106B STOURBRIDGE ROAD, DUDLEY
             "90142681",  # 106C STOURBRIDGE ROAD, DUDLEY
             "90163097",  # 50 WOLVERHAMPTON STREET, DUDLEY
             "90163098",  # 51 WOLVERHAMPTON STREET, DUDLEY
             "90163100",  # 52 WOLVERHAMPTON STREET, DUDLEY
+            "90147692",  # SCHOOL HOUSE, COTWALL END ROAD, DUDLEY
+            "90056006",  # 1 BELLS LANE, STOURBRIDGE
+            "90151695",  # HURST LEA, PEDMORE ROAD, BRIERLEY HILL
+            "90202527",  # LIVING ACCOMMODATION HINDU CULTURAL CENTRE 57-59 KING STREET, DUDLEY
         ]:
             return None
 
-        if record.addressline6 in ["DY1 3EP"]:
-            return None  # split
+        if record.addressline6 in [
+            # split
+            "DY1 3EP",
+            "DY6 7LT",
+            "DY8 4LU",  # ADJACENT THE BONDED WAREHOUSE CANAL STREET, AMBLECOTE
+            "DY2 8QB",  # THE OLD VICARAGE, KING STREET, DUDLEY
+        ]:
+            return None
 
         return super().address_record_to_dict(record)
