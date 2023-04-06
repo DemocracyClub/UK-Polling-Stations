1aa0fac33cc7de75cb31ed2bf48f9658ff4ce5e3
GeoWill <roper.william@gmail.com>
2023-04-06 15:10:26 +0100

    Bunch of imports

--- polling_stations/apps/data_importers/management/commands/import_south_staffs.py
+++ polling_stations/apps/data_importers/management/commands/import_south_staffs.py
@@ -4,10 +4,10 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SST"
     addresses_name = (
-        "2023-05-04/2023-03-21T09:13:08.644819/Democracy_Club__04May2023.tsv"
+        "2023-05-04/2023-04-03T14:58:59.354138/Democracy_Club__04May2023v.2.tsv"
     )
     stations_name = (
-        "2023-05-04/2023-03-21T09:13:08.644819/Democracy_Club__04May2023.tsv"
+        "2023-05-04/2023-04-03T14:58:59.354138/Democracy_Club__04May2023v.2.tsv"
     )
     elections = ["2023-05-04"]
     csv_delimiter = "\t"
