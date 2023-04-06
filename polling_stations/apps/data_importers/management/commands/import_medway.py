7367d1575331eb86f6b5d5d7d37fad69f2df9f4f
GeoWill <roper.william@gmail.com>
2023-04-06 15:47:30 +0100

    Import script for Medway (2023-05-04) (closes #5409)

--- polling_stations/apps/data_importers/management/commands/import_medway.py
+++ polling_stations/apps/data_importers/management/commands/import_medway.py
@@ -2,12 +2,19 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 
 class Command(BaseHalaroseCsvImporter):
-    council_id = "E06000035"
-    addresses_name = (
-        "parl.2017-06-08/Version 1/Medway polling_station_export-2017-05-25.csv"
-    )
-    stations_name = (
-        "parl.2017-06-08/Version 1/Medway polling_station_export-2017-05-25.csv"
-    )
-    elections = ["parl.2017-06-08"]
-    csv_encoding = "windows-1252"
+    council_id = "MDW"
+    addresses_name = "2023-05-04/2023-03-29T08:36:18.336726/Eros_SQL_Output007.csv"
+    stations_name = "2023-05-04/2023-03-29T08:36:18.336726/Eros_SQL_Output007.csv"
+    elections = ["2023-05-04"]
+
+    def address_record_to_dict(self, record):
+        if record.housepostcode in [
+            # split
+            "ME8 8DB",
+            "ME8 8QQ",
+            # Look wrong
+            "ME2 3TG",
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
