0aa3a60b044cacde504a11820926a2a39ee01602
polling-bot-4000 <developers@democracyclub.org.uk>
2023-04-06 12:58:32 +0100

    Import script for Manchester (2023-05-04) (closes #5369)

--- polling_stations/apps/data_importers/management/commands/import_manchester.py
+++ polling_stations/apps/data_importers/management/commands/import_manchester.py
@@ -1,10 +1,14 @@
-from data_importers.management.commands import Unknown
+from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
-class Command(Unknown):
+class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "MAN"
-    addresses_name = "2023-05-04/2023-03-23T16:29:47.358436/Polling_Place_Postcode_Lookup__04May2023.tsv"
-    stations_name = "2023-05-04/2023-03-23T16:29:47.358436/Polling_Place_Postcode_Lookup__04May2023.tsv"
+    addresses_name = (
+        "2023-05-04/2023-04-06T12:57:54.299279/Democracy_Club__04May2023.tsv"
+    )
+    stations_name = (
+        "2023-05-04/2023-04-06T12:57:54.299279/Democracy_Club__04May2023.tsv"
+    )
     elections = ["2023-05-04"]
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
