7bad841082dc07e21ac033b7ff49d2086c6129e7
polling-bot-4000 <developers@democracyclub.org.uk>
2023-04-07 12:35:15 +0100

    Import script for Welwyn Hatfield (2023-05-04) (closes #5501)

--- polling_stations/apps/data_importers/management/commands/import_welwyn_hatfield.py
+++ polling_stations/apps/data_importers/management/commands/import_welwyn_hatfield.py
@@ -3,13 +3,6 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "WEW"
-    addresses_name = (
-        "2022-05-05/2022-04-08T11:34:35.289156/polling_station_export-2022-04-07.csv"
-    )
-    stations_name = (
-        "2022-05-05/2022-04-08T11:34:35.289156/polling_station_export-2022-04-07.csv"
-    )
-    elections = ["2022-05-05"]
-
-    # Checked the properties on Digswell Hill/Welwyn Bypass Road/Great North Road
-    # and White Hill, and concluded they're fine, despite looking embedded in another area.
+    addresses_name = "2023-05-04/2023-04-07T12:35:07.022003/Eros_SQL_Output002.csv"
+    stations_name = "2023-05-04/2023-04-07T12:35:07.022003/Eros_SQL_Output002.csv"
+    elections = ["2023-05-04"]
