ac1d11a98787065e2e135d1599e8c9c875cbff86
polling-bot-4000 <developers@democracyclub.org.uk>
2023-04-08 10:06:22 +0100

    Import script for Leicester (2023-05-04) (closes #5504)

--- polling_stations/apps/data_importers/management/commands/import_leicester.py
+++ polling_stations/apps/data_importers/management/commands/import_leicester.py
@@ -6,14 +6,15 @@ from data_importers.management.commands import (
 class Command(BaseXpressDCCsvInconsistentPostcodesImporter):
     council_id = "LCE"
     addresses_name = (
-        "2023-05-04/2023-03-09T11:16:48.756990/Democracy_Club__04May2023.tsv"
+        "2023-05-04/2023-04-08T10:03:21.739775/Democracy_Club__04May2023 08-04-2023.tsv"
     )
     stations_name = (
-        "2023-05-04/2023-03-09T11:16:48.756990/Democracy_Club__04May2023.tsv"
+        "2023-05-04/2023-04-08T10:03:21.739775/Democracy_Club__04May2023 08-04-2023.tsv"
     )
     elections = ["2023-05-04"]
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
+
     station_postcode_search_fields = [
         "polling_place_postcode",
         "polling_place_address_4",
@@ -22,7 +23,13 @@ class Command(BaseXpressDCCsvInconsistentPostcodesImporter):
     ]
 
     def address_record_to_dict(self, record):
-        if record.addressline6 in ["LE3 0FP", "LE1 7GT", "LE4", "LE4 2RD"]:
+        if len(record.addressline6) == 3:
+            return None
+        if record.addressline6 in [
+            # split
+            "LE1 7GT",
+            "LE3 0FP",
+        ]:
             return None
 
         return super().address_record_to_dict(record)
