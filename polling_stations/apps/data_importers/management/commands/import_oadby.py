37ad74ee60b9ec8c2587b5c106d57911d28c813a
polling-bot-4000 <developers@democracyclub.org.uk>
2023-04-09 10:32:56 +0100

    Import script for Oadby & Wigston (2023-05-04) (closes #5464)

--- polling_stations/apps/data_importers/management/commands/import_oadby.py
+++ polling_stations/apps/data_importers/management/commands/import_oadby.py
@@ -3,24 +3,7 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "OAD"
-    addresses_name = "2023-05-04/2023-04-04T17:54:00.271699/Democracy_Club__04May2023 (Rev. 04-04-2023).tsv"
-    stations_name = "2023-05-04/2023-04-04T17:54:00.271699/Democracy_Club__04May2023 (Rev. 04-04-2023).tsv"
+    addresses_name = "2023-05-04/2023-04-09T10:32:46.735243/Democracy_Club__04May2023 (Rev. 09-04-2023).tsv"
+    stations_name = "2023-05-04/2023-04-09T10:32:46.735243/Democracy_Club__04May2023 (Rev. 09-04-2023).tsv"
     elections = ["2023-05-04"]
     csv_delimiter = "\t"
-
-    def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "10010146840",  # 46A LONG STREET, WIGSTON
-        ]:
-            return None
-
-        if record.addressline6 in [
-            # look wrong
-            "LE18 2GQ",
-            "LE18 3AN",
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
