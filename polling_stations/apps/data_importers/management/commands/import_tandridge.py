61cd954cd5f7f9fb558029b3e097c1021820889e
polling-bot-4000 <developers@democracyclub.org.uk>
2023-04-11 12:32:07 +0100

    Import script for Tandridge (2023-05-04) (closes #5508)

--- polling_stations/apps/data_importers/management/commands/import_tandridge.py
+++ polling_stations/apps/data_importers/management/commands/import_tandridge.py
@@ -3,6 +3,6 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "TAN"
-    addresses_name = "2023-05-04/2023-04-11T11:32:27.436962/Eros_SQL_Output001.csv"
-    stations_name = "2023-05-04/2023-04-11T11:32:27.436962/Eros_SQL_Output001.csv"
+    addresses_name = "2023-05-04/2023-04-11T12:32:00.173227/Eros_SQL_Output001.csv"
+    stations_name = "2023-05-04/2023-04-11T12:32:00.173227/Eros_SQL_Output001.csv"
     elections = ["2023-05-04"]
