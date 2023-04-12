1328d2fd1f174877e8483618c82da35b45470142
polling-bot-4000 <developers@democracyclub.org.uk>
2023-04-12 10:03:13 +0100

    Import script for Tendring (2023-05-04) (closes #5518)

--- polling_stations/apps/data_importers/management/commands/import_tendring.py
+++ polling_stations/apps/data_importers/management/commands/import_tendring.py
@@ -1,32 +1,32 @@
-from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
+from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 
-class Command(BaseXpressDemocracyClubCsvImporter):
+class Command(BaseDemocracyCountsCsvImporter):
     council_id = "TEN"
-    addresses_name = "2021-03-16T13:49:32.237046/Democracy_Club__06May2021.tsv"
-    stations_name = "2021-03-16T13:49:32.237046/Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
+    addresses_name = (
+        "2023-05-04/2023-04-12T10:02:51.779215/Democracy Club - Polling Districts.tsv"
+    )
+    stations_name = (
+        "2023-05-04/2023-04-12T10:02:51.779215/Democracy Club - Polling Stations.tsv"
+    )
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
+        uprn = record.uprn.strip().lstrip("0")
 
         if uprn in [
-            "100091463517",  # 9 HEDGE END, WALTON ON THE NAZE
-            "100091463343",  # 582 MAIN ROAD, HARWICH
-            "100091269055",  # HEALTH & HAPPINESS, 558-560 MAIN ROAD, HARWICH
-            "100090634963",  # GREYFRIARS, 2 BEACH ROAD, CLACTON-ON-SEA
-            "100090602739",  # 18 ORWELL ROAD, CLACTON-ON-SEA
-            "100091461472",  # 302 HIGH STREET, HARWICH
-            "100090602737",  # 66 DULWICH ROAD, HOLLAND-ON-SEA, CLACTON-ON-SEA
-            "10090658325",  # 64 DULWICH ROAD, HOLLAND-ON-SEA, CLACTON-ON-SEA
-            "100090635156",  # FOOTS FARM RIDING CENTRE, THORPE ROAD, CLACTON-ON-SEA
-            "100091273215",  # RIDGEMONT HOUSE LOW ROAD, DOVERCOURT
-            "10007941736",  # SANS ETAGE, PLOUGH ROAD, GREAT BENTLEY, COLCHESTER
+            "10094248268",  # 43 MIDDLETON MEWS, BRIGHTLINGSEA, COLCHESTER
         ]:
             return None
 
-        if record.addressline6 in ["CO16 8GU", "CO16 9R"]:
+        if record.postcode in [
+            # split
+            "CO13 0FZ",
+            "CO16 9UQ",
+            "CO15 7AP",
+            "CO14 8GH",
+        ]:
             return None
 
         return super().address_record_to_dict(record)
