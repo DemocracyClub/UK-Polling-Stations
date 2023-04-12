5bca7073cd25a4b4db4f4de8f32bdc75f7417e08
polling-bot-4000 <developers@democracyclub.org.uk>
2023-04-12 16:11:50 +0100

    Import script for Chesterfield (2023-05-04) (closes #5529)

--- polling_stations/apps/data_importers/management/commands/import_chesterfield.py
+++ polling_stations/apps/data_importers/management/commands/import_chesterfield.py
@@ -3,24 +3,38 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "CHS"
-    addresses_name = "2021-04-01T14:48:16.336851/Democracy_Club__06May2021.tsv"
-    stations_name = "2021-04-01T14:48:16.336851/Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
+    addresses_name = (
+        "2023-05-04/2023-04-12T16:11:40.810471/Democracy_Club__04May2023.tsv"
+    )
+    stations_name = (
+        "2023-05-04/2023-04-12T16:11:40.810471/Democracy_Club__04May2023.tsv"
+    )
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
-        if record.addressline6 in ["S43 1ER", "S40 3LA", "S41 9RL"]:
+        if uprn in [
+            "74088505",  # 11B NEWBRIDGE LANE, BRIMINGTON
+            "74068238",  # THE PADDOCKS, CROW LANE, CHESTERFIELD
+            "74006059",  # DOBBIN CLOUGH FARM, CROW LANE, CHESTERFIELD
+            "74078771",  # 84 SALTERGATE, CHESTERFIELD
+        ]:
             return None
 
-        if uprn in ["74085483"]:
+        if record.addressline6 in [
+            # splits
+            "S40 3LA",
+        ]:
             return None
 
         return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
-        # Manor Centre Manor Road Brimington Chesterfield
-        if record.polling_place_id == "6217":
+        # Christ Church Primary School, Tapton View Road, Chesterfield, S41 1JU
+        # Proposed correction: S41 7JU
+        if record.polling_place_id == "6915":
             record = record._replace(polling_place_postcode="")
+
         return super().station_record_to_dict(record)
