f0fff2f586bf2371eed77fb5526fb06c77df3905
GeoWill <roper.william@gmail.com>
2023-04-13 12:01:43 +0100

    Fix markdown filter
    Not sure how this was working before hand, but it was broken. This
    change is cribbed from wcivf, and seems to work locally now.

---
+++ polling_stations/templatetags/markdown_filter.py
@@ -0,0 +1,14 @@
+from django import template
+from django.utils.safestring import mark_safe
+
+import markdown
+
+register = template.Library()
+
+
+@register.filter(name="markdown")
+def markdown_filter(text):
+    return mark_safe(markdown.markdown(text))
+
+
+markdown_filter.is_safe = True
