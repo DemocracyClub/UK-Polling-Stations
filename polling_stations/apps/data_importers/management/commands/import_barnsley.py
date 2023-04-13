81f2d36ae775e772ac223d365a4982a360310751
polling-bot-4000 <developers@democracyclub.org.uk>
2023-04-13 15:17:27 +0100

    Import script for Barnsley (2023-05-04) (closes #5546)

--- polling_stations/apps/data_importers/management/commands/import_barnsley.py
+++ polling_stations/apps/data_importers/management/commands/import_barnsley.py
@@ -3,20 +3,9 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "BNS"
-    addresses_name = (
-        "2022-05-05/2022-03-25T09:18:02.587996/Democracy Club Polling Districts.csv"
-    )
-    stations_name = (
-        "2022-05-05/2022-03-25T09:18:02.587996/Democracy Club Polling Stations.csv"
-    )
-    elections = ["2022-05-05"]
-
-    def station_record_to_dict(self, record):
-        # TEMPORARY BUILDING AT GRASMERE CRESCENT
-        if record.stationcode == "24":
-            record = record._replace(xordinate="", yordinate="")
-
-        return super().station_record_to_dict(record)
+    addresses_name = "2023-05-04/2023-04-13T15:16:58.037951/Polling Districts.csv"
+    stations_name = "2023-05-04/2023-04-13T15:16:58.037951/Polling Stations.csv"
+    elections = ["2023-05-04"]
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
