e231b90355d810294b50b04f0b2d11cc464eac5b
polling-bot-4000 <developers@democracyclub.org.uk>
2023-04-13 15:18:06 +0100

    Import script for Calderdale (2023-05-04) (closes #5550)

--- polling_stations/apps/data_importers/management/commands/import_calderdale.py
+++ polling_stations/apps/data_importers/management/commands/import_calderdale.py
@@ -3,18 +3,20 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "CLD"
-    addresses_name = "2022-05-05/2022-03-21T11:38:20.891364/Calderdale Polling Districts 2022 05 05.csv"
-    stations_name = "2022-05-05/2022-03-21T11:38:20.891364/Calderdale Polling Stations 2022 05 05.csv"
-    elections = ["2022-05-05"]
+    addresses_name = (
+        "2023-05-04/2023-04-13T15:17:42.827927/Calderdale Polling Districts 2023.csv"
+    )
+    stations_name = (
+        "2023-05-04/2023-04-13T15:17:42.827927/Calderdale Polling Stations 2023.csv"
+    )
+    elections = ["2023-05-04"]
 
     def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
-
-        if record.postcode == "OL14 6RS":
-            return None
-        if uprn in [
-            "200001691811",
-            "200001826572",
+        if record.postcode in [
+            # split
+            "OL14 6RS",
+            "HX3 5JF",
         ]:
             return None
+
         return super().address_record_to_dict(record)
