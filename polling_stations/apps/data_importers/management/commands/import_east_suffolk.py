4fe6023cc2b088eeb898a6dd85c46804a4777db5
polling-bot-4000 <developers@democracyclub.org.uk>
2023-04-13 15:35:47 +0100

    Import script for East Suffolk (2023-05-04) (closes #5561)

--- polling_stations/apps/data_importers/management/commands/import_east_suffolk.py
+++ polling_stations/apps/data_importers/management/commands/import_east_suffolk.py
@@ -3,21 +3,6 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "ESK"
-    addresses_name = (
-        "2021-03-31T14:17:43.933405/East Suffolk Council Area - Polling Districts.csv"
-    )
-    stations_name = "2021-03-31T14:17:43.933405/East Suffolk Council Area - Polling Stations - v2.csv"
-    elections = ["2021-05-06"]
-
-    def station_record_to_dict(self, record):
-        if record.stationcode in ("12", "12X", "12XX", "12XXX"):
-            # Shadingfield Village Hall
-            record = record._replace(postcode="NR34 8DH")
-
-        return super().station_record_to_dict(record)
-
-    def address_record_to_dict(self, record):
-        if record.postcode in ["NR35 1BZ", "IP12 2SY", "NR32 4ER"]:
-            return None  # split
-
-        return super().address_record_to_dict(record)
+    addresses_name = "2023-05-04/2023-04-13T15:35:25.165461/East Suffolk Council - Democracy Club - Polling Districts May 2023.csv"
+    stations_name = "2023-05-04/2023-04-13T15:35:25.165461/East Suffolk Council - Democracy Club - Polling Stations May 2023.csv"
+    elections = ["2023-05-04"]
