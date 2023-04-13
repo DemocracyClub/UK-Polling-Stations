ea6237a913baf92d2769656ba96a81a24b0ea39c
polling-bot-4000 <developers@democracyclub.org.uk>
2023-04-13 17:59:20 +0100

    Import script for Southampton (2023-05-04) (closes #5564)

--- polling_stations/apps/data_importers/management/commands/import_southampton.py
+++ polling_stations/apps/data_importers/management/commands/import_southampton.py
@@ -4,31 +4,50 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "STH"
     addresses_name = (
-        "2022-05-05/2022-04-12T12:29:28.468901/Democracy_Club__05May2022.tsv"
+        "2023-05-04/2023-04-13T17:56:48.873726/Democracy_Club__04May2023.tsv"
     )
     stations_name = (
-        "2022-05-05/2022-04-12T12:29:28.468901/Democracy_Club__05May2022.tsv"
+        "2023-05-04/2023-04-13T17:56:48.873726/Democracy_Club__04May2023.tsv"
     )
-    elections = ["2022-05-05"]
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
 
-    def station_record_to_dict(self, record):
-        # The Royal British Legion Club (Function Room), Windover Close, Southampton
-        if record.polling_place_id == "13603":
-            record = record._replace(polling_place_postcode="SO19 5JS")  # was S019 5JS
-
-        # Highfield Church Centre (Lounge), Highfield Lane, Southampton
-        if record.polling_place_id == "13739":
-            record = record._replace(polling_place_postcode="SO17 1RL")  # was SO17 7RL
-
-        # Testlands Hub (Spin/Conference Room), (Formerly Millbrook Secondary School), Green Lane
-        if record.polling_place_id == "13764":
-            record = record._replace(polling_place_postcode="SO16 9FQ")  # was SO16 9RG
-
-        return super().station_record_to_dict(record)
-
     def address_record_to_dict(self, record):
-        if record.addressline6 in ["SO15 2NS", "SO16 7AS", "SO16 7GQ"]:
-            return None  # split
+        if record.addressline6 in [
+            # splits
+            "SO15 2NS",
+            "SO16 7AS",
+            "SO16 7GQ",
+        ]:
+            return None
 
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # The Royal British Legion Club (Function Room), Windover Close, Southampton, S019 5JS
+        # Postcode mistype, should be a letter "O" instead of a number "0"
+        if record.polling_place_id == "14024":
+            record = record._replace(polling_place_postcode="SO19 5JS")
+
+        # Highfield Church Centre (Lounge), Highfield Lane, Southampton, SO17 7RL
+        if record.polling_place_id == "14162":
+            record = record._replace(polling_place_postcode="SO17 1RL")
+
+        # Testlands Hub (Spin/Conference Room), (Formerly Millbrook Secondary School), Green Lane, SO16 9RG
+        if record.polling_place_id == "14187":
+            record = record._replace(polling_place_postcode="SO16 9FQ")
+
+        # The Ascension Centre (Grace Hall), 1 Thorold Road, Southampton, SO18 1HZ
+        # The Ascension Centre (Vite Room, 1 Thorold Road, Southampton, SO18 1HZ
+        if record.polling_place_id in ["14059", "14056"]:
+            record = record._replace(
+                polling_place_easting="444086", polling_place_northing="113955"
+            )
+
+        # Moorlands Community Centre (Main Hall), Townhill Way, Southampton, SO18 2ER
+        if record.polling_place_id == "14106":
+            record = record._replace(
+                polling_place_easting="445540", polling_place_northing="114282"
+            )
+
+        return super().station_record_to_dict(record)
