c37019f54c808702a53fc9be97773fb453487135
polling-bot-4000 <developers@democracyclub.org.uk>
2023-04-13 15:35:47 +0100

    Import script for East Suffolk (2023-05-04) (closes #5561)

--- polling_stations/apps/data_importers/management/commands/import_east_suffolk.py
+++ polling_stations/apps/data_importers/management/commands/import_east_suffolk.py
@@ -3,21 +3,26 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "ESK"
-    addresses_name = (
-        "2021-03-31T14:17:43.933405/East Suffolk Council Area - Polling Districts.csv"
-    )
-    stations_name = "2021-03-31T14:17:43.933405/East Suffolk Council Area - Polling Stations - v2.csv"
-    elections = ["2021-05-06"]
+    addresses_name = "2023-05-04/2023-04-13T15:35:25.165461/East Suffolk Council - Democracy Club - Polling Districts May 2023.csv"
+    stations_name = "2023-05-04/2023-04-13T15:35:25.165461/East Suffolk Council - Democracy Club - Polling Stations May 2023.csv"
+    elections = ["2023-05-04"]
 
     def station_record_to_dict(self, record):
-        if record.stationcode in ("12", "12X", "12XX", "12XXX"):
-            # Shadingfield Village Hall
-            record = record._replace(postcode="NR34 8DH")
+        if record.stationcode == "S110":
+            # Dallinghoo Jubilee Hall
+            record = record._replace(xordinate="", yordinate="")
+
+        if record.stationcode == "S138":
+            # Bruisyard Village Hall
+
+            record = record._replace(xordinate="", yordinate="")
 
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-        if record.postcode in ["NR35 1BZ", "IP12 2SY", "NR32 4ER"]:
-            return None  # split
+        if record.postcode in [
+            "NR35 1BZ",  # split
+        ]:
+            return None
 
         return super().address_record_to_dict(record)
