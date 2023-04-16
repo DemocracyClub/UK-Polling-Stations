55d52add19d86c77ff420b0b43bd1dd0c3deefed
blacktyger <blacktyg3r@gmail.com>
2023-04-16 09:50:38 +0100

    Import script for Salford (2023-05-04) (closes #5381)

--- polling_stations/apps/data_importers/management/commands/import_salford.py
+++ polling_stations/apps/data_importers/management/commands/import_salford.py
@@ -3,13 +3,45 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SLF"
-    addresses_name = "2022-05-05/2022-03-29T11:40:42.009781/Democracy_Club__05May2022 - Salford City Council.tsv"
-    stations_name = "2022-05-05/2022-03-29T11:40:42.009781/Democracy_Club__05May2022 - Salford City Council.tsv"
-    elections = ["2022-05-05"]
+    addresses_name = (
+        "2023-05-04/2023-03-24T11:25:53.715865/Democracy_Club__04May2023.tsv"
+    )
+    stations_name = (
+        "2023-05-04/2023-03-24T11:25:53.715865/Democracy_Club__04May2023.tsv"
+    )
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
-        if record.addressline6 in ["M27 0JE"]:
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "200001037110",  # PADIHAM FARM ENGINE LANE, TYLDESLEY
+            "10004682203",  # ALDERWOOD COTTAGE, LEIGH ROAD, WORSLEY, MANCHESTER
+            "10095311109",  # APARTMENT 1, THORNCLIFFE, 48 VINE STREET, SALFORD
+            "10090621036",  #  FLAT ABOVE 2 ST JAMES ROAD, SALFORD
+            "10090621073",  # FLAT AT SWINTON PARK GOLF CLUB EAST LANCASHIRE ROAD, SWINTON
+            "100012471364",  # WESTWOOD LODGE, PARRIN LANE, ECCLES, MANCHESTER
+            "100011357863",  # 2A DUDLEY ROAD, CADISHEAD, MANCHESTER
+            "100011424265",  # 47 VINE STREET, SALFORD
+        ]:
+            return None
+
+        if record.addressline6 in [
+            # splits
+            "M27 0JE",
+            "M27 4BL",  # SWINTON HALL ROAD, SWINTON, MANCHESTER
+        ]:
             return None
 
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # The Boundary Stone, Bridgewater Road, Worsley, M28 1AD
+        if record.polling_place_id == "6643":
+            record = record._replace(
+                polling_place_easting="372564",
+                polling_place_northing="401679",
+            )
+
+        return super().station_record_to_dict(record)
