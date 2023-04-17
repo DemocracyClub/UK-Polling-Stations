43cee3032d6cc1e76be4b7fb39a17843cbf93db9
polling-bot-4000 <developers@democracyclub.org.uk>
2023-04-17 14:44:03 +0100

    Import script for Mid Suffolk (2023-05-04) (closes #5588)

--- polling_stations/apps/data_importers/management/commands/import_mid_suffolk.py
+++ polling_stations/apps/data_importers/management/commands/import_mid_suffolk.py
@@ -1,53 +1,39 @@
-from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
+from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 
-class Command(BaseXpressDemocracyClubCsvImporter):
-    council_id = "E07000203"
-    addresses_name = "parl.2019-12-12/Version 1/MSDCDemocracy_Club__07May2020.CSV"
-    stations_name = "parl.2019-12-12/Version 1/MSDCDemocracy_Club__07May2020.CSV"
-    elections = ["parl.2019-12-12"]
-    allow_station_point_from_postcode = False
-
-    def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn == "10090893715":
-            rec["postcode"] = "IP6 8HJ"
-            rec["accept_suggestion"] = False
-
-        if uprn == "100091375182":
-            rec["postcode"] = "IP30 9PE"
-            rec["accept_suggestion"] = False
-
-        if uprn == "10090890000":
-            rec["postcode"] = "IP6 0EP"
-            rec["accept_suggestion"] = False
-
-        if uprn == "100091382079":
-            return None
-
-        return rec
+class Command(BaseDemocracyCountsCsvImporter):
+    council_id = "MSU"
+    addresses_name = (
+        "2023-05-04/2023-04-17T14:43:48.331284/DC Polling Districts - Mid Suffolk.csv"
+    )
+    stations_name = (
+        "2023-05-04/2023-04-17T14:43:48.331284/DC Polling Stations - Mid Suffolk.csv"
+    )
+    elections = ["2023-05-04"]
 
     def station_record_to_dict(self, record):
-        if record.polling_place_id == "17450":
-            record = record._replace(polling_place_postcode="IP20 0JE")
-            record = record._replace(polling_place_easting="627286.25")
-            record = record._replace(polling_place_northing="281055.48")
+        # correction from council
+        if record.stationcode in [
+            "M49",  # Wetheringsett Village Hall
+            "M84",  # All Saints Church Beyton
+        ]:
+            record = record._replace(xordinate="", yordinate="")
 
-        # recieved from council
-        if record.polling_place_id == "17440":  # Kenton - Caravan near former Station
-            record = record._replace(
-                polling_place_name="Bedingfield Village Hall",
-                polling_place_address_1="Church Corner",
-                polling_place_address_2="Bedingfield",
-                polling_place_postcode="IP23 7QG",
-            )
-        if record.polling_place_id == "17355":  # Elmswell (ELS) - The Wesley
-            record = record._replace(
-                polling_place_name="Elmswell (ELN) - Blackbourne Centre",
-                polling_place_address_1="Blackbourne Road",
-                polling_place_address_2="Elmswell",
-                polling_place_postcode="IP30 9UH",
-            )
         return super().station_record_to_dict(record)
+
+    def address_record_to_dict(self, record):
+        if record.postcode in [
+            # split
+            "IP14 5PE",
+            "IP14 1LU",
+            "IP14 5LN",
+            "IP14 6ET",
+            # look wrong
+            "IP14 4FW",
+        ]:
+            return None
+        if record.uprn in [
+            "10095542589",  # THE PIGGERY, BROAD VIEW FARM, LOWER FARM ROAD, RINGSHALL, STOWMARKET
+        ]:
+            return None
+        return super().address_record_to_dict(record)
