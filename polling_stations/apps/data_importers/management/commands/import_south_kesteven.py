299977dd002f02cb60434a03f632d83d91443983
polling-bot-4000 <developers@democracyclub.org.uk>
2023-04-17 15:03:59 +0100

    Import script for South Kesteven (2023-05-04) (closes #5592)

--- polling_stations/apps/data_importers/management/commands/import_south_kesteven.py
+++ polling_stations/apps/data_importers/management/commands/import_south_kesteven.py
@@ -4,10 +4,35 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SKE"
     addresses_name = (
-        "2022-12-15/2022-11-25T14:34:32.124360/Democracy_Club__15December2022.tsv"
+        "2023-05-04/2023-04-17T15:03:42.448696/Democracy_Club__04May2023.tsv"
     )
     stations_name = (
-        "2022-12-15/2022-11-25T14:34:32.124360/Democracy_Club__15December2022.tsv"
+        "2023-05-04/2023-04-17T15:03:42.448696/Democracy_Club__04May2023.tsv"
     )
-    elections = ["2022-12-15"]
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
+
+    def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "10007278973",  # ANNEXE 7 HIGH STREET, CAYTHORPE
+            "10007248725",  # KLONDYKE FARM BUNGALOW HARVEY CLOSE, BOURNE
+            "10007276237",  # 86 THE DEEPINGS CARAVAN PARK TOWNGATE EAST, MARKET DEEPING
+            "100030901687",  # 5 DYKE DROVE, BOURNE
+        ]:
+            return None
+
+        if record.addressline6 in [
+            # splits
+            "NG32 3AU",
+            "NG31 9JZ",
+            "NG32 1AT",
+            "NG32 2LW",
+            "PE10 9RP",
+            "NG31 8NH",  # MANTHORPE, GRANTHAM
+            "PE10 9NG",  # BURGHLEY STREET, BOURNE
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
