68c78b580534308ae2f9b0d0f0fe9a35f36d7d62
GeoWill <roper.william@gmail.com>
2023-04-19 09:10:14 +0100

    Import script for East Staffs (2023-05-04) (closes #5552)

--- polling_stations/apps/data_importers/management/commands/import_east_staffs.py
+++ polling_stations/apps/data_importers/management/commands/import_east_staffs.py
@@ -3,30 +3,10 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "EST"
-    addresses_name = "2021-02-23T13:27:13.045364/Democracy Club - Polling Districts.csv"
-    stations_name = "2021-02-23T13:27:13.045364/Democracy Club - Polling Stations.csv"
-    elections = ["2021-05-06"]
-
-    def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
-
-        if rec and rec["postcode"] in ["ST14 8SG"]:
-            # Spurious polling place for one property
-            return None
-
-        return rec
-
-    def station_record_to_dict(self, record):
-        # Shobnall Primary School
-        if record.stationcode == "BP_21":
-            record = record._replace(
-                xordinate="422735", yordinate="323640", postcode="DE14 2BB"
-            )
-
-        # St Giles Church, Croxden Lane
-        if record.stationcode == "AA_63":
-            record = record._replace(
-                xordinate="406486", yordinate="339871", postcode="ST14 5JG"
-            )
-
-        return super().station_record_to_dict(record)
+    addresses_name = (
+        "2023-05-04/2023-04-13T15:18:20.766665/Democracy Club Polling Districts.csv"
+    )
+    stations_name = (
+        "2023-05-04/2023-04-13T15:18:20.766665/Democracy Club Polling Stations.csv"
+    )
+    elections = ["2023-05-04"]
