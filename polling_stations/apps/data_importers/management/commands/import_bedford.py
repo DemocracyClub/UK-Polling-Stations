86f40b271461a1b2ecf9049906800f2b597732db
polling-bot-4000 <developers@democracyclub.org.uk>
2023-04-19 10:28:55 +0100

    Import script for Bedford (2023-05-04) (closes #5610)

--- polling_stations/apps/data_importers/management/commands/import_bedford.py
+++ polling_stations/apps/data_importers/management/commands/import_bedford.py
@@ -4,28 +4,28 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "BDF"
     addresses_name = (
-        "2021-03-17T12:11:37.989847/Bedford Polling Districts - Democracy Club.csv"
+        "2023-05-04/2023-04-19T10:28:35.386737/Democracy Club - Polling Districts.csv"
     )
     stations_name = (
-        "2021-03-17T12:11:37.989847/Bedford Polling Stations for Democracy Club.csv"
+        "2023-05-04/2023-04-19T10:28:35.386737/Democracy Club - Polling Stations.csv"
     )
-    elections = ["2021-05-06"]
+    elections = ["2023-05-04"]
 
     def station_record_to_dict(self, record):
-        # KEMPSTON WEST METHODIST CHURCH Carried forward from local.2019-05-02
-        if record.stationcode == "44":
-            record = record._replace(xordinate="502614")
-            record = record._replace(yordinate="247440")
+        # Lots of dodgy looking points
+        if record.stationcode.strip() in [
+            "76",  # WOOTTON MEMORIAL HALL
+            "81",  # WOOTTON MEMORIAL HALL
+            "78",  # Wootton Community Centre
+            "79",  # Wootton Community Centre
+            "80",  # Wootton Community Centre
+            "39",  # CRYSELCO PAVILION
+            "31",  # PARK ROAD METHODIST CHURCH
+            "32",  # PARK ROAD METHODIST CHURCH
+            "42",  # SPRINGFIELD SCHOOL
+            "43",  # SPRINGFIELD SCHOOL
+            "10",  # BEDFORD HOSPITAL
+        ]:
+            record = record._replace(xordinate="", yordinate="")
 
         return super().station_record_to_dict(record)
-
-    def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
-
-        if record.postcode in ["MK43 9AH"]:
-            return None
-
-        if uprn in ["100080991603"]:
-            return None
-
-        return super().address_record_to_dict(record)
