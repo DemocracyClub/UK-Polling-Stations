f85c83ac2c90fa220631299d8130701cae14cb1c
polling-bot-4000 <developers@democracyclub.org.uk>
2023-04-19 13:01:08 +0100

    Import script for Thurrock (2023-05-04) (closes #5614)

--- polling_stations/apps/data_importers/management/commands/import_thurrock.py
+++ polling_stations/apps/data_importers/management/commands/import_thurrock.py
@@ -4,18 +4,28 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "THR"
     addresses_name = (
-        "2022-05-05/2022-03-23T16:02:05.092550/Democracy_Club__05May2022.tsv"
+        "2023-05-04/2023-04-19T13:00:54.850754/Democracy_Club__04May2023.tsv"
     )
     stations_name = (
-        "2022-05-05/2022-03-23T16:02:05.092550/Democracy_Club__05May2022.tsv"
+        "2023-05-04/2023-04-19T13:00:54.850754/Democracy_Club__04May2023.tsv"
     )
-    elections = ["2022-05-05"]
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "10095907154",  # 1A HIGH STREET, GRAYS
+            "200001553308",  # HILLVIEW, SOUTHEND ROAD, CORRINGHAM, STANFORD-LE-HOPE
+            "100091297871",  # HOB HILL FARM, BIGGIN LANE, GRAYS
+        ]:
+            return None
+
         if record.addressline6.strip() in [
-            "RM16 4RB",
+            # splits
             "RM19 1QJ",
+            "RM16 4RB",
         ]:
             return None  # split
 
