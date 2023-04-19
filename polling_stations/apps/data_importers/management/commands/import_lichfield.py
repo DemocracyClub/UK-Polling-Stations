c4d22a7d03d83d310f66f73ed4a5276113642d70
polling-bot-4000 <developers@democracyclub.org.uk>
2023-04-19 15:20:23 +0100

    Import script for Lichfield (2023-05-04) (closes #5618)

--- polling_stations/apps/data_importers/management/commands/import_lichfield.py
+++ polling_stations/apps/data_importers/management/commands/import_lichfield.py
@@ -4,37 +4,13 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "LIF"
     addresses_name = (
-        "2021-03-24T10:59:01.635563/Lichfield Democracy_Club__06May2021.tsv"
+        "2023-05-04/2023-04-19T15:20:06.691978/Democracy_Club__04May2023.tsv"
     )
-    stations_name = "2021-03-24T10:59:01.635563/Lichfield Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
+    stations_name = (
+        "2023-05-04/2023-04-19T15:20:06.691978/Democracy_Club__04May2023.tsv"
+    )
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
-    csv_encoding = "windows-1252"
-
-    def station_record_to_dict(self, record):
-        # Guildroom, Guildhall, Bore Street, Lichfield
-        if record.polling_place_id == "5596":
-            record = record._replace(polling_place_postcode="WS13 6LU")
-
-        # Mobile Polling Station, Staffs University West Car Park, Monks Close, Lichfield
-        if record.polling_place_id == "5583":
-            record = record._replace(
-                polling_place_easting=411530, polling_place_northing=309223
-            )
-
-        # Holy Cross, Community Meeting Room, Holy Cross Church
-        if record.polling_place_id == "5595":
-            record = record._replace(
-                polling_place_easting=411921, polling_place_northing=308789
-            )
-
-        # Holy Cross Parish Hall
-        if record.polling_place_id == "5591":
-            record = record._replace(
-                polling_place_easting=411908, polling_place_northing=308797
-            )
-
-        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
@@ -42,14 +18,29 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         if uprn in [
             "10002775637",  # HORSEY LANE LODGE, HORSEY LANE, RUGELEY
             "100031701504",  # MILL DAM HOUSE, MILL LANE, ALDRIDGE, WALSALL
-            "100031713231",  # THE GRANARY MILL GREEN FARM MILL LANE, ALDRIDGE, WALSALL
             "10002767279",  # 4 WISSAGE ROAD, LICHFIELD
             "10002767278",  # 2 WISSAGE ROAD, LICHFIELD
-            "100031692345",  # MILL ACRES HOUSE, DAISY LANE, ALREWAS, BURTON-ON-TRENT
+            "100032225708",  # THE COURTYARD, LICHFIELD ROAD, PIPEHILL, LICHFIELD
         ]:
             return None
 
-        if record.addressline6 in ["B78 3AU", "WS7 0HZ"]:
-            return None
-
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # Guildroom, Guildhall, Bore Street, Lichfield, WS13 6LX
+        if record.polling_place_id == "6502":
+            record = record._replace(polling_place_postcode="WS13 6LU")
+
+        # Holy Cross, Community Meeting Room, Holy Cross Church, WS14 9BA
+        if record.polling_place_id == "6758":
+            record = record._replace(
+                polling_place_easting=411921, polling_place_northing=308789
+            )
+
+        # Holy Cross Parish Hall, Holy Cross Parish Hall, Chapel Lane, Lichfield, WS14 9BA
+        if record.polling_place_id == "6754":
+            record = record._replace(
+                polling_place_easting=411908, polling_place_northing=308797
+            )
+
+        return super().station_record_to_dict(record)
