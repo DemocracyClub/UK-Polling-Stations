b5f1db3f602e86c2f68a5f610e01ca0673ce281a
polling-bot-4000 <developers@democracyclub.org.uk>
2023-04-19 10:28:20 +0100

    Import script for Amber Valley (2023-05-04) (closes #5608)

--- polling_stations/apps/data_importers/management/commands/import_amber_valley.py
+++ polling_stations/apps/data_importers/management/commands/import_amber_valley.py
@@ -3,11 +3,27 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "AMB"
-    addresses_name = "2022-05-05/2022-03-22T09:50:28.814111/Polling Districts 2022.csv"
-    stations_name = "2022-05-05/2022-03-22T09:50:28.814111/Polling Stations 2022.csv"
-    elections = ["2022-05-05"]
+    addresses_name = "2023-05-04/2023-04-19T10:28:01.382651/18 04 23 District.csv"
+    stations_name = (
+        "2023-05-04/2023-04-19T10:28:01.382651/D C Polling Stations 2023.csv"
+    )
+    elections = ["2023-05-04"]
 
-    # Not all wards are up for election this year. Hence 40,880 of ~64k
-    # addresses are mapped. Have checked that mapped areas align with ward
-    # boundaries. For details of which wards have elections, see
-    # https://www.ambervalley.gov.uk/councillors-and-elections/elections/5-may-2022/.
+    def station_record_to_dict(self, record):
+        # ST.ANDREWS CHURCH HALL Station Road'
+        if record.stationcode in ["CMS", "CME"]:
+            record = record._replace(postcode="")
+
+        if record.stationcode == "HAL":
+            record = record._replace(xordinate="", yordinate="")
+
+        return super().station_record_to_dict(record)
+
+    def address_record_to_dict(self, record):
+        uprn = record.uprn.strip().lstrip("0")
+        if uprn in [
+            "10013426910",  # 21 DALBY GREEN CLOSE, WAINGROVES, RIPLEY
+            "10013426909",  # 22 DALBY GREEN CLOSE, WAINGROVES, RIPLEY
+        ]:
+            return None
+        return super().address_record_to_dict(record)
