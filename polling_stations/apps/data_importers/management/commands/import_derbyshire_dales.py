39abce6f6ff3d3fdef01b6397b1453afa615f869
polling-bot-4000 <developers@democracyclub.org.uk>
2023-04-19 10:30:31 +0100

    Import script for Derbyshire Dales (2023-05-04) (closes #5612)

--- polling_stations/apps/data_importers/management/commands/import_derbyshire_dales.py
+++ polling_stations/apps/data_importers/management/commands/import_derbyshire_dales.py
@@ -3,10 +3,15 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "DEB"
-    addresses_name = (
-        "2021-03-29T10:09:54.269326/Derbyshire Dales - Polling Districts Data.csv"
-    )
-    stations_name = (
-        "2021-03-29T10:09:54.269326/Derbyshire Dales - Polling Station Data.csv"
-    )
-    elections = ["2021-05-06"]
+    addresses_name = "2023-05-04/2023-04-19T10:30:20.449617/Democracy Club - Polling Districts 170423.csv"
+    stations_name = "2023-05-04/2023-04-19T10:30:20.449617/Democracy Club - Polling Stations 180423.csv"
+    elections = ["2023-05-04"]
+
+    def address_record_to_dict(self, record):
+        if record.postcode in [
+            # split
+            "DE6 2AR",
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
