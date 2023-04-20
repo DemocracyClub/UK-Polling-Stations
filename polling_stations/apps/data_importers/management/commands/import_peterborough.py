dc15d782db70457dc93a141bb41604ddc72b7210
blacktyger <blacktyg3r@gmail.com>
2023-04-20 12:31:41 +0100

    Import script for Peterborough (2023-05-04) (closes #5584)

--- polling_stations/apps/data_importers/management/commands/import_peterborough.py
+++ polling_stations/apps/data_importers/management/commands/import_peterborough.py
@@ -4,47 +4,39 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "PTE"
     addresses_name = (
-        "2022-05-05/2022-03-24T09:40:07.399960/Democracy_Club__05May2022.tsv"
+        "2023-05-04/2023-04-17T09:51:32.818127/Democracy_Club__04May2023.tsv"
     )
     stations_name = (
-        "2022-05-05/2022-03-24T09:40:07.399960/Democracy_Club__05May2022.tsv"
+        "2023-05-04/2023-04-17T09:51:32.818127/Democracy_Club__04May2023.tsv"
     )
-    elections = ["2022-05-05"]
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
-
-    def station_record_to_dict(self, record):
-        # Council fix Madeley House -> All Saints Church Hall
-        if record.polling_place_id == "9228":
-            record = record._replace(
-                polling_place_name="All Saints Church Hall",
-                polling_place_address_1="Park Road",
-                polling_place_address_2="",
-                polling_place_address_3="",
-                polling_place_address_4="Peterborough",
-                polling_place_postcode="PE1 2UL",
-            )
-
-        # Hampton Leisure Centre Clayburn Road Hampton Vale Peterborough PE7 8HQ
-        if record.polling_place_id == "9249":
-            record = record._replace(polling_place_postcode="PE7 8HG")
-
-        # St Andrews Church Russell Hill Thornhaugh Peterborough PE6 6NW
-        if record.polling_place_id == "9397":
-            record = record._replace(polling_place_postcode="PE8 6HL")
-
-        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
             "10094542529",  # 64 GREENFIELD WAY, HAMPTON WATER, PETERBOROUGH
+            "10090763503",  # MEDICAL & STAFF ACCOMMODATION BLOCK, PETERBOROUGH CITY HOSPITAL, BRETTON GATE, BRETTON, PETERBOROUGH
+            "100091206701",  # WOODLANDS, HAM LANE, ORTON WATERVILLE, PETERBOROUGH
+            "10008073540",  # 1 NEWARK AVENUE, PETERBOROUGH
         ]:
             return None
 
         if record.addressline6 in [
+            # splits
             "PE7 0LE",
+            "PE3 8AA",  # KATHARINE WAY, BRETTON, PETERBOROUGH
+            "PE7 8RU",  # WILTON AVENUE, HAMPTON GARDENS, PETERBOROUGH
         ]:
             return None
 
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # Copeland Community Centre, 37 Copeland, Bretton, Peterborough, PE3 9YJ
+        # Proposed correction: PE3 6YJ
+        if record.polling_place_id == "9666":
+            record = record._replace(polling_place_postcode="")
+
+        return super().station_record_to_dict(record)
