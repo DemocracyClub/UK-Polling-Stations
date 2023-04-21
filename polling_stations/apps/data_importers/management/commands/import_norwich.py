bc86ebe94974fdc58a70722235d88a8d79bd184a
GeoWill <roper.william@gmail.com>
2023-04-21 13:45:07 +0100

    Import script for Norwich (2023-05-04) (closes #5633)

--- polling_stations/apps/data_importers/management/commands/import_norwich.py
+++ polling_stations/apps/data_importers/management/commands/import_norwich.py
@@ -3,6 +3,10 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "NOW"
-    addresses_name = "2023-05-04/2023-04-13T15:24:40.310670/Democracy Club polling districts export.csv"
-    stations_name = "2023-05-04/2023-04-13T15:24:40.310670/Democracy Club polling stations export.csv"
+    addresses_name = (
+        "2023-05-04/2023-04-21T13:20:24.756727/DC - Polling districts export.csv"
+    )
+    stations_name = (
+        "2023-05-04/2023-04-21T13:20:24.756727/DC - Polling stations export.csv"
+    )
     elections = ["2023-05-04"]
