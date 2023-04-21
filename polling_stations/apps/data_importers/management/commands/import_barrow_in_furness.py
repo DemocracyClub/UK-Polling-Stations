9e7fbf628718bd138bc7b15675503bc425f73d28
GeoWill <roper.william@gmail.com>
2023-04-21 16:51:15 +0100

    Import script for Westmoreland and Furness (Barrow) (2023-05-04) (closes #5538)

--- polling_stations/apps/data_importers/management/commands/import_barrow_in_furness.py
+++ polling_stations/apps/data_importers/management/commands/import_barrow_in_furness.py
@@ -4,22 +4,9 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "BAR"
     addresses_name = (
-        "2022-05-05/2022-04-07T14:47:44.793330/Democracy Club - Polling Districts.csv"
+        "2023-05-04/2023-04-21T14:38:15/Democracy_Club_-_Polling_DistrictsOBandH.csv"
     )
     stations_name = (
-        "2022-05-05/2022-04-07T14:47:44.793330/Democracy Club - polling stations.csv"
+        "2023-05-04/2023-04-21T14:38:15/Democracy_Club_Polling_Stations_OBandH.csv"
     )
-    elections = ["2022-05-05"]
-
-    def station_record_to_dict(self, record):
-        # ASKAM COMMUNITY CENTRE
-        if record.stationcode in ["LC1", "LC2"]:
-            record = record._replace(xordinate="321435")
-            record = record._replace(yordinate="477563")
-
-        # VICKERSTOWN METHODIST CHURCH
-        if record.stationcode == "AA1":
-            record = record._replace(xordinate="318614")
-            record = record._replace(yordinate="468960")
-
-        return super().station_record_to_dict(record)
+    elections = ["2023-05-04"]
