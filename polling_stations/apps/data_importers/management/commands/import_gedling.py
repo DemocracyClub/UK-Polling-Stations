92d0eb957952b3c86c9ef9cf765f8c986c23b2c6
blacktyger <blacktyg3r@gmail.com>
2023-04-26 00:51:08 +0100

    Import script for Gedling (2023-05-04) (closes #5637)

--- polling_stations/apps/data_importers/management/commands/import_gedling.py
+++ polling_stations/apps/data_importers/management/commands/import_gedling.py
@@ -3,9 +3,13 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "GED"
-    addresses_name = "2021-03-15T11:19:52.370890/Democracy_Club__06May2021.tsv"
-    stations_name = "2021-03-15T11:19:52.370890/Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
+    addresses_name = (
+        "2023-05-04/2023-04-21T14:14:24.161747/Democracy_Club__04May2023.tsv"
+    )
+    stations_name = (
+        "2023-05-04/2023-04-21T14:14:24.161747/Democracy_Club__04May2023.tsv"
+    )
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -24,25 +28,20 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "10035286332",  # 4 MILL LANE, LAMBLEY, NOTTINGHAM
             "100032276589",  # 163 STANDHILL ROAD, CARLTON, NOTTINGHAM
             "100032302726",  # THE FLAT FOX AND HOUNDS HOTEL STATION ROAD, CARLTON
-            "100032123022",  # DAISIES DAY NURSERY, WOOD LANE, GEDLING, NOTTINGHAM
+            "10035288244",  # 93 SPRING LANE, LAMBLEY
+            "100032123942",  # 127 SPRING LANE, LAMBLEY, NOTTINGHAM
+            "100031349971",  # E W WOOD BUILDERS, MANSFIELD HOUSE, 148 BURTON ROAD, GEDLING, NOTTINGHAM
+            "100031345829",  # SCOTGRAVE FARM, ARNOLD LANE, GEDLING, NOTTINGHAM
         ]:
             return None
 
         if record.addressline6 in [
-            "NG5 8PJ",
-            "NG4 1EN",
-            "NG4 3DF",
-            "NG14 6FN",
-            "NG4 2DX",
-            "NG4 3FT",
-            "NG4 3GQ",
-            "NG5 8AU",
-            "NG4 3DH",
-            "NG15 0BD",
-            "NG15 8GD",
-            "NG3 6HE",
+            # splits
+            "NG5 8AH",
             "NG3 6BN",
-            "NG4 3DY",
+            "NG4 2DX",
+            "NG5 8AU",
+            "NG4 4FN",
         ]:
             return None
 
