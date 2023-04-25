1d6429483088ec9a3a254580e9633089ce01ebc2
blacktyger <blacktyg3r@gmail.com>
2023-04-25 23:00:27 +0100

    Import script for Telford and Wrekin (2023-05-04) (closes #5629)

--- polling_stations/apps/data_importers/management/commands/import_telford.py
+++ polling_stations/apps/data_importers/management/commands/import_telford.py
@@ -3,18 +3,38 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "TFW"
-    addresses_name = "2021-04-28T15:13:31.221139/telford-deduped-mk2.tsv"
-    stations_name = "2021-04-28T15:13:31.221139/telford-deduped-mk2.tsv"
-    elections = ["2021-05-06"]
+    addresses_name = (
+        "2023-05-04/2023-04-20T13:49:05.049834/Democracy_Club__04May2023.tsv"
+    )
+    stations_name = (
+        "2023-05-04/2023-04-20T13:49:05.049834/Democracy_Club__04May2023.tsv"
+    )
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "452098161",  # AQUALATE MANOR, STAFFORD ROAD, NEWPORT
+            "200001754669",  # CHARTLEY, CHURCH ASTON, NEWPORT
+            "452098134",  # CARAVAN 3 GREENFIELDS COUNTRY STORE STATION ROAD, DONNINGTON, TELFORD
+            "452063031",  # HOMECROFT, ADMASTON, TELFORD
+            "452084193",  # MEESE COTTAGE, HOWLE, NEWPORT
+            "452063475",  # 10 RUSHMOOR LANE, BRATTON, TELFORD
+            "452094687",  # THE RIDGEWAYS, THE HEM, SHIFNAL
+        ]:
+            return None
+
         if record.addressline6 in [
-            "TF4 3JZ",
+            # splits
+            "TF10 7ZN",
             "TF8 7NN",
             "TF2 8SF",
             "TF10 7RG",
-            "TF2 7PE",
+            "TF4 3JZ",
+            "TF2 9HF",
+            "TF3 4LY",  # BEEFEATER EUSTON WAY, TELFORD TOWN CENTRE, TELFORD
         ]:
             return None
 
