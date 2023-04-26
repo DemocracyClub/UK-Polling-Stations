480e891baa7dd62f4389c94a7348a13ce6262ed5
polling-bot-4000 <developers@democracyclub.org.uk>
2023-04-26 20:47:17 +0100

    Import script for East Suffolk (2023-05-04) (closes #5679)

--- polling_stations/apps/data_importers/management/commands/import_east_suffolk.py
+++ polling_stations/apps/data_importers/management/commands/import_east_suffolk.py
@@ -3,20 +3,29 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "ESK"
-    addresses_name = "2023-05-04/2023-04-13T15:35:25.165461/East Suffolk Council - Democracy Club - Polling Districts May 2023.csv"
-    stations_name = "2023-05-04/2023-04-13T15:35:25.165461/East Suffolk Council - Democracy Club - Polling Stations May 2023.csv"
+    addresses_name = "2023-05-04/2023-04-26T20:46:56.038428/East Suffolk Council - Democracy Club - Polling Districts May 2023.csv"
+    stations_name = "2023-05-04/2023-04-26T20:46:56.038428/East Suffolk Council - Democracy Club - Polling Stations May 2023.csv"
     elections = ["2023-05-04"]
 
     def station_record_to_dict(self, record):
-        if record.stationcode == "S110":
-            # Dallinghoo Jubilee Hall
+        # Loads of dodgy points
+        if record.stationcode in [
+            "S110",  # Dallinghoo Jubilee Hall
+            "S138",  # Bruisyard Village Hall
+            "S196",  # Fred Reynolds Centre
+            "S195",  # Avenue Evangelical Church Hall
+            "S159",  # Burness Parish Room
+            "S184",  # Colneis Division Guiding HQ
+            "S182",  # Christ Church
+            "S187",  # Campsea Ashe Victory Hall
+            "S160",  # Burness Parish Room
+            "S141",  # Cransford Village Hall
+            "S147",  # Great Glemham Village Hall
+            "S109",  # Clopton Village Hall
+            "S165",  # Kirton Church Hall
+            "N69",  # Linstead Village Hall
+        ]:
             record = record._replace(xordinate="", yordinate="")
-
-        if record.stationcode == "S138":
-            # Bruisyard Village Hall
-
-            record = record._replace(xordinate="", yordinate="")
-
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
