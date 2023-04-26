bac0033d2d26ac33ce4a28077c0f58f5deba9b14
blacktyger <blacktyg3r@gmail.com>
2023-04-26 01:29:08 +0100

    Import script for Tamworth (2023-05-04) (closes #5660)

--- polling_stations/apps/data_importers/management/commands/import_tamworth.py
+++ polling_stations/apps/data_importers/management/commands/import_tamworth.py
@@ -3,19 +3,24 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "TAW"
-    addresses_name = "2022-05-05/2022-03-02T14:26:27.337999/Tamworth_BC_Democracy_Club__05May2022.tsv"
-    stations_name = "2022-05-05/2022-03-02T14:26:27.337999/Tamworth_BC_Democracy_Club__05May2022.tsv"
-    elections = ["2022-05-05"]
+    addresses_name = "2023-05-04/2023-04-25T14:30:27.355270/Tamworth_polling-stations_May _2023_for_Democracy-Club.tsv"
+    stations_name = "2023-05-04/2023-04-25T14:30:27.355270/Tamworth_polling-stations_May _2023_for_Democracy-Club.tsv"
+
+    elections = ["2023-05-04"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "394040617",  # 77C AMINGTON ROAD, TAMWORTH, B77 3LN
+            "394037200",  # 105A COMBERFORD ROAD, TAMWORTH
+            "394030964",  # STATFOLD BARN FARM ASHBY ROAD, TAMWORTH
         ]:
             return None
-        if record.addressline6 in ["B79 0BY"]:
+        if record.addressline6 in [
+            # splits
+            "B77 3EU",
+        ]:
             return None
 
         return super().address_record_to_dict(record)
