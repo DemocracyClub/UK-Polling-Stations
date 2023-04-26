156a83a3a5b291d034ec99fdb0fb5677c4cad538
blacktyger <blacktyg3r@gmail.com>
2023-04-26 14:39:18 +0100

    Add station locations to Southend

--- polling_stations/apps/data_importers/management/commands/import_southend.py
+++ polling_stations/apps/data_importers/management/commands/import_southend.py
@@ -18,6 +18,34 @@ class Command(BaseXpressDemocracyClubCsvImporter):
                 polling_place_easting="586595", polling_place_northing="187653"
             )
 
+        # Sandpiper Pre-school, Sandpiper Close/Kingfisher Close, Shoeburyness, SS3 9YB
+        # correction for the council
+        if record.polling_place_id == "7561":
+            record = record._replace(
+                polling_place_easting="593511", polling_place_northing="185908"
+            )
+
+        # Hinguar School, New Garrison Road, Shoeburyness, Southend-on-Sea, Essex, SS3 9FE
+        # correction for the council
+        if record.polling_place_id == "7414":
+            record = record._replace(
+                polling_place_easting="593387", polling_place_northing="184765"
+            )
+
+        # Centre Place Community Centre, 15 Prospect Close, Essex, SS1 6XY
+        # correction for the council
+        if record.polling_place_id == "7363":
+            record = record._replace(
+                polling_place_easting="589315", polling_place_northing="185206"
+            )
+
+        # Booth 1, John Wesley Hall	Belfairs Methodist Church, Eastwood Road North, Leigh-on-Sea, SS9 4NG
+        # correction for the council
+        if record.polling_place_id == "7331":
+            record = record._replace(
+                polling_place_easting="583854", polling_place_northing="187894"
+            )
+
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
