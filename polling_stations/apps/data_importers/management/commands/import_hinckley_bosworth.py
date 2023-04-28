c8db7233d14f0aa77549a04ec729fcb12802386a
polling-bot-4000 <developers@democracyclub.org.uk>
2023-04-28 11:22:23 +0100

    Import script for Hinckley & Bosworth (2023-05-04) (closes #5704)

--- polling_stations/apps/data_importers/management/commands/import_hinckley_bosworth.py
+++ polling_stations/apps/data_importers/management/commands/import_hinckley_bosworth.py
@@ -1,28 +1,32 @@
-from data_importers.management.commands import BaseXpressWebLookupCsvImporter
+from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
-class Command(BaseXpressWebLookupCsvImporter):
+class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "HIN"
-    addresses_name = "2021-04-16T11:29:13.327047/PropertyPostCodePollingStationWebLookup-2021-04-09.CSV"
-    stations_name = "2021-04-16T11:29:13.327047/PropertyPostCodePollingStationWebLookup-2021-04-09.CSV"
-    elections = ["2021-05-06"]
-    csv_delimiter = ","
+    addresses_name = (
+        "2023-05-04/2023-04-28T11:19:56.050247/Democracy_Club__04May2023.CSV"
+    )
+    stations_name = (
+        "2023-05-04/2023-04-28T11:19:56.050247/Democracy_Club__04May2023.CSV"
+    )
+    elections = ["2023-05-04"]
 
     def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
+        uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
             "10033831876",  # THE STABLES, DESFORD LANE, RATBY, LEICESTER
-            "100030520613",  # WOODCOCK), BROCKEY FARM, KIRKBY ROAD, BARWELL, LEICESTER
-            "100030520609",  # BROCKEY FARM KIRKBY ROAD, BARWELL
             "10033728621",  # BARWELL SPORTS BAR, KIRKBY ROAD, BARWELL, LEICESTER
-            "200002769579",  # START FARM, WATLING STREET, HINCKLEY
-            "10033728228",  # KATES EQUESTRIAN SUPPLIES, 18 NEWTON ROAD, HINCKLEY
             "100030503769",  # 10 HILL STREET, BARWELL
         ]:
             return None
 
-        if record.postcode in ["LE10 0QL", "LE9 8JA", "LE10 2GJ"]:
+        if record.addressline6 in [
+            # split
+            "LE10 0QL",
+            "LE10 2GJ",
+            "LE9 8JA",
+            "CV13 6LR",
+        ]:
             return None
-
         return super().address_record_to_dict(record)
