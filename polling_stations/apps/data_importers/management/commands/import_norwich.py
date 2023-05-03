20772f1dd53830200126c213665b2110c5bb46a4
GeoWill <roper.william@gmail.com>
2023-05-03 15:36:22 +0100

    Remove station point in Norwich

--- polling_stations/apps/data_importers/management/commands/import_norwich.py
+++ polling_stations/apps/data_importers/management/commands/import_norwich.py
@@ -10,3 +10,11 @@ class Command(BaseDemocracyCountsCsvImporter):
         "2023-05-04/2023-04-21T13:20:24.756727/DC - Polling stations export.csv"
     )
     elections = ["2023-05-04"]
+
+    def station_record_to_dict(self, record):
+        # ST THOMAS PARISH HALL
+        # https://wheredoivote.co.uk/admin/bug_reports/bugreport/576/change/
+        if record.stationcode == "33NE2":
+            record = record._replace(xordinate="", yordinate="")
+
+        return super().station_record_to_dict(record)
