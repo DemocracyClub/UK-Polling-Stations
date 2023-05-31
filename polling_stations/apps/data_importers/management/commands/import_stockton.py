c2b7fc6b38478a3b8b391b7547017363228a01cf
polling-bot-4000 <developers@democracyclub.org.uk>
2023-05-31 14:45:01 +0100

    Import script for Stockton-on-Tees (2023-06-22) (closes #5775)

--- polling_stations/apps/data_importers/management/commands/import_stockton.py
+++ polling_stations/apps/data_importers/management/commands/import_stockton.py
@@ -4,22 +4,10 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "STT"
     addresses_name = (
-        "2023-05-04/2023-03-09T14:43:24.514702/Democracy_Club__04May2023.tsv"
+        "2023-06-22/2023-05-31T14:44:53.803777/Democracy_Club__22June2023.tsv"
     )
     stations_name = (
-        "2023-05-04/2023-03-09T14:43:24.514702/Democracy_Club__04May2023.tsv"
+        "2023-06-22/2023-05-31T14:44:53.803777/Democracy_Club__22June2023.tsv"
     )
-    elections = ["2023-05-04"]
+    elections = ["2023-06-22"]
     csv_delimiter = "\t"
-
-    def address_record_to_dict(self, record):
-        if record.addressline6 in [
-            "TS17 9PA",
-            "TS20 2TJ",
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        return super().station_record_to_dict(record)
