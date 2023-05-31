c92c6736a2bd792458b8d1524b963130cfcdb80d
polling-bot-4000 <developers@democracyclub.org.uk>
2023-05-31 14:59:39 +0100

    Import script for Cambridge (2023-07-04) (closes #5777)

--- polling_stations/apps/data_importers/management/commands/import_cambridge.py
+++ polling_stations/apps/data_importers/management/commands/import_cambridge.py
@@ -3,6 +3,6 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "CAB"
-    addresses_name = "2023-07-04/2023-05-31T10:12:12.782444/Eros_SQL_Output013.csv"
-    stations_name = "2023-07-04/2023-05-31T10:12:12.782444/Eros_SQL_Output013.csv"
+    addresses_name = "2023-07-04/2023-05-31T14:59:32.696409/Eros_SQL_Output013.csv"
+    stations_name = "2023-07-04/2023-05-31T14:59:32.696409/Eros_SQL_Output013.csv"
     elections = ["2023-07-04"]
