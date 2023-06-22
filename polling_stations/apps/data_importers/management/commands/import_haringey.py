8593907383415c63804b74d1a6019ed4be716e01
polling-bot-4000 <developers@democracyclub.org.uk>
2023-06-22 14:23:04 +0100

    Import script for Haringey (2023-06-29) (closes #5808)

--- polling_stations/apps/data_importers/management/commands/import_haringey.py
+++ polling_stations/apps/data_importers/management/commands/import_haringey.py
@@ -4,10 +4,10 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "HRY"
     addresses_name = (
-        "2023-03-09/2023-02-09T10:24:24.779394/Democracy_Club__09March2023.tsv"
+        "2023-06-29/2023-06-22T14:22:57.377959/Democracy_Club__29June2023.tsv"
     )
     stations_name = (
-        "2023-03-09/2023-02-09T10:24:24.779394/Democracy_Club__09March2023.tsv"
+        "2023-06-29/2023-06-22T14:22:57.377959/Democracy_Club__29June2023.tsv"
     )
-    elections = ["2023-03-09"]
+    elections = ["2023-06-29"]
     csv_delimiter = "\t"
