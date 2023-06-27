e26dc0e360dbfc08f0b225302e60f5dd45dc9803
GeoWill <roper.william@gmail.com>
2023-06-27 17:04:09 +0100

    Use x,ys for Cambridge stations

--- polling_stations/apps/data_importers/management/commands/import_cambridge.py
+++ polling_stations/apps/data_importers/management/commands/import_cambridge.py
@@ -1,3 +1,5 @@
+from django.contrib.gis.geos import Point
+
 from data_importers.management.commands import BaseHalaroseCsvImporter
 
 
@@ -6,3 +8,25 @@ class Command(BaseHalaroseCsvImporter):
     addresses_name = "2023-07-04/2023-05-31T14:59:32.696409/Eros_SQL_Output013.csv"
     stations_name = "2023-07-04/2023-05-31T14:59:32.696409/Eros_SQL_Output013.csv"
     elections = ["2023-07-04"]
+    easting_field = "pollingstationeasting"
+    northing_field = "pollingstationnorthing"
+
+    def get_station_point(self, record):
+        if (
+            hasattr(record, self.easting_field)
+            and hasattr(record, self.northing_field)
+            and getattr(record, self.easting_field) != "0"
+            and getattr(record, self.easting_field) != ""
+            and getattr(record, self.northing_field) != "0"
+            and getattr(record, self.northing_field) != ""
+        ):
+            # if we've got points, use them
+
+            return Point(
+                float(getattr(record, self.easting_field)),
+                float(getattr(record, self.northing_field)),
+                srid=27700,
+            )
+
+        else:
+            return super().get_station_point(record)
