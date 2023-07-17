e42410fa7f56cb2f715e6d309e6f3c329ceaf6a1
polling-bot-4000 <developers@democracyclub.org.uk>
2023-07-17 09:13:27 +0100

    Import script for Havering (2023-08-10) (closes #5820)

--- polling_stations/apps/data_importers/management/commands/import_havering.py
+++ polling_stations/apps/data_importers/management/commands/import_havering.py
@@ -4,24 +4,10 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "HAV"
     addresses_name = (
-        "2022-05-05/2022-02-11T11:24:25.766169/Democracy_Club__05May2022.tsv"
+        "2023-08-10/2023-07-17T09:13:19.161631/Democracy_Club__10August2023.tsv"
     )
     stations_name = (
-        "2022-05-05/2022-02-11T11:24:25.766169/Democracy_Club__05May2022.tsv"
+        "2023-08-10/2023-07-17T09:13:19.161631/Democracy_Club__10August2023.tsv"
     )
-    elections = ["2022-05-05"]
+    elections = ["2023-08-10"]
     csv_delimiter = "\t"
-
-    def address_record_to_dict(self, record):
-        if record.addressline6 in ["RM7 7BX", "RM11 2BY", "RM7 8DX", "RM12 4LG"]:
-            return None
-
-        return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # Name change from council - don't carry forward
-        # https://trello.com/c/Ig6nZ8NZ
-        if record.polling_place_id == "9417":
-            record = record._replace(polling_place_name="James Oglethorpe Pre-School")
-
-        return super().station_record_to_dict(record)
