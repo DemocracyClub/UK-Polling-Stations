1104cebbc4ef521837f3d4a556f5ac9b9c9778bb
polling-bot-4000 <developers@democracyclub.org.uk>
2023-08-22 15:36:24 +0100

    TEST/Import script for Highland (2023-09-28) (closes #5865)

--- polling_stations/apps/data_importers/management/commands/import_highland.py
+++ polling_stations/apps/data_importers/management/commands/import_highland.py
@@ -4,22 +4,9 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "HLD"
     addresses_name = (
-        "2022-05-05/2022-03-24T15:19:09.669660/H Democracy Club - Polling Districts.csv"
+        "2023-09-28/2023-08-22T15:33:12.407009/Democracry Club - Polling Districts.csv"
     )
     stations_name = (
-        "2022-05-05/2022-03-24T15:19:09.669660/H Democracy Club - Polling Stations.csv"
+        "2023-09-28/2023-08-22T15:33:12.407009/Democracry Club - Polling Stations.csv"
     )
-    elections = ["2022-05-05"]
-
-    def address_record_to_dict(self, record):
-        if record.uprn in ["130131593"]:
-            return None
-        if record.postcode == "IV17 0QY":
-            return None
-        return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # Coigach Community Hall, Achiltibuie
-        if record.stationcode in ["W01 001", "W05 053"]:
-            record = record._replace(xordinate="202914", yordinate="907677")
-        return super().station_record_to_dict(record)
+    elections = ["2023-09-28"]
