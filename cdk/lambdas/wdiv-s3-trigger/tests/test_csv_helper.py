f57270a52612c01844fa09147c7af66f58628328
GeoWill <roper.william@gmail.com>
2023-08-23 16:27:50 +0100

    Let CSVs with NULs fail, but do better error reporting
    This reverts the changes from the previous commit, but keeps the better
    errors.

--- cdk/lambdas/wdiv-s3-trigger/tests/test_csv_helper.py
+++ cdk/lambdas/wdiv-s3-trigger/tests/test_csv_helper.py
@@ -138,17 +138,6 @@ class CsvHelperTests(TestCase):
             get_fixture("not-a-csv.csv", "text/csv"), "not-a-csv.csv"
         )
         self.assertFalse(report["csv_valid"])
-        print(report["errors"])
         self.assertEqual(
-            "File has only 2 columns. We might have failed to detect the delimiter",
-            report["errors"][0],
+            "Failed to parse body -> line 1: line contains NUL", report["errors"][0]
         )
-
-    def test_valid_with_nul(self):
-        report = get_csv_report(
-            get_fixture("ems-xpress-dc-nul.csv", "text/csv"), "ems-xpress-dc.csv"
-        )
-        self.assertTrue(report["csv_valid"])
-        self.assertEqual(10, report["csv_rows"])
-        self.assertEqual("utf-8", report["csv_encoding"])
-        self.assertEqual("Xpress DC", report["ems"])
