c50ea754253a21766a0d5f438036fa328ae97ce2
polling-bot-4000 <developers@democracyclub.org.uk>
2023-09-18 15:14:33 +0100

    Import script for Central Bedfordshire (2023-10-19) (closes #5913)

--- polling_stations/apps/data_importers/management/commands/import_central_bedfordshire.py
+++ polling_stations/apps/data_importers/management/commands/import_central_bedfordshire.py
@@ -4,25 +4,9 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "CBF"
     addresses_name = (
-        "2023-05-04/2023-04-19T15:46:39.334042/Democracy Club - polling districts2.csv"
+        "2023-10-19/2023-09-18T15:14:23.313921/Democracy Club - PD-utf-8.csv"
     )
     stations_name = (
-        "2023-05-04/2023-04-19T15:46:39.334042/polling stations for Democracy Club2.csv"
+        "2023-10-19/2023-09-18T15:14:23.313921/Democracy Club - PS-utf-8.csv"
     )
-    elections = ["2023-05-04"]
-
-    def station_record_to_dict(self, record):
-        # Flitton Church Hall Brook Lane Flitton Beds MK45 5EJ - removing misleading point
-        if record.stationcode == "Bx178WF-G1":
-            record = record._replace(xordinate="", yordinate="")
-
-        return super().station_record_to_dict(record)
-
-    def address_record_to_dict(self, record):
-        if record.postcode in [
-            # split
-            "MK17 9QG",
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
+    elections = ["2023-10-19"]
