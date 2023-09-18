4318a823d460a0bad8741e6f7ec64c9e3df46fad
polling-bot-4000 <developers@democracyclub.org.uk>
2023-09-18 16:40:23 +0100

    Import script for Central Bedfordshire (2023-10-19) (closes #5919)

--- polling_stations/apps/data_importers/management/commands/import_central_bedfordshire.py
+++ polling_stations/apps/data_importers/management/commands/import_central_bedfordshire.py
@@ -3,10 +3,6 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "CBF"
-    addresses_name = (
-        "2023-10-19/2023-09-18T15:14:23.313921/Democracy Club - PD-utf-8.csv"
-    )
-    stations_name = (
-        "2023-10-19/2023-09-18T15:14:23.313921/Democracy Club - PS-utf-8.csv"
-    )
+    addresses_name = "2023-10-19/2023-09-18T16:40:13.338932/districts.csv"
+    stations_name = "2023-10-19/2023-09-18T16:40:13.338932/stations.csv"
     elections = ["2023-10-19"]
