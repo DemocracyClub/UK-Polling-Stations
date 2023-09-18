62d58ef49ee6c2b0ed73614e6c3f97ade92f32e9
polling-bot-4000 <developers@democracyclub.org.uk>
2023-09-18 14:25:53 +0100

    Import script for Bedford (2023-10-19) (closes #5910)

--- polling_stations/apps/data_importers/management/commands/import_bedford.py
+++ polling_stations/apps/data_importers/management/commands/import_bedford.py
@@ -4,28 +4,9 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "BDF"
     addresses_name = (
-        "2023-05-04/2023-04-19T10:28:35.386737/Democracy Club - Polling Districts.csv"
+        "2023-10-19/2023-09-18T14:25:44.359517/Democracy Club - PD-utf-8.csv"
     )
     stations_name = (
-        "2023-05-04/2023-04-19T10:28:35.386737/Democracy Club - Polling Stations.csv"
+        "2023-10-19/2023-09-18T14:25:44.359517/Democracy Club - PS-utf-8.csv"
     )
-    elections = ["2023-05-04"]
-
-    def station_record_to_dict(self, record):
-        # Lots of dodgy looking points
-        if record.stationcode.strip() in [
-            "76",  # WOOTTON MEMORIAL HALL
-            "81",  # WOOTTON MEMORIAL HALL
-            "78",  # Wootton Community Centre
-            "79",  # Wootton Community Centre
-            "80",  # Wootton Community Centre
-            "39",  # CRYSELCO PAVILION
-            "31",  # PARK ROAD METHODIST CHURCH
-            "32",  # PARK ROAD METHODIST CHURCH
-            "42",  # SPRINGFIELD SCHOOL
-            "43",  # SPRINGFIELD SCHOOL
-            "10",  # BEDFORD HOSPITAL
-        ]:
-            record = record._replace(xordinate="", yordinate="")
-
-        return super().station_record_to_dict(record)
+    elections = ["2023-10-19"]
