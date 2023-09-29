0de4fa9c6b7565f7ad7b12f958d7653dddbfcc43
polling-bot-4000 <developers@democracyclub.org.uk>
2023-09-29 10:50:44 +0100

    Import script for Lambeth (2023-10-05) (closes #5933)

--- polling_stations/apps/data_importers/management/commands/import_lambeth.py
+++ polling_stations/apps/data_importers/management/commands/import_lambeth.py
@@ -4,20 +4,11 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "LBH"
     addresses_name = (
-        "2022-05-05/2022-03-29T12:35:09.475993/LBDemocracy_Club__05May2022_2.tsv"
+        "2023-10-05/2023-09-29T10:50:35.830333/Democracy_Club__05October2023.tsv"
     )
     stations_name = (
-        "2022-05-05/2022-03-29T12:35:09.475993/LBDemocracy_Club__05May2022_2.tsv"
+        "2023-10-05/2023-09-29T10:50:35.830333/Democracy_Club__05October2023.tsv"
     )
-    elections = ["2022-05-05"]
+    elections = ["2023-10-05"]
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
-
-    def address_record_to_dict(self, record):
-        if record.addressline6 in [
-            "SE11 5UG",
-            "SW2 5RS",
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
