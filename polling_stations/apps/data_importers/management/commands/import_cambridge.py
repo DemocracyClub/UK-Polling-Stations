04ca692bb08b2b0b7275eb1141697e8dec4ee261
polling-bot-4000 <developers@democracyclub.org.uk>
2023-10-25 13:50:10 +0100

    Import script for Cambridge (2023-11-23) (closes #5960)

--- polling_stations/apps/data_importers/management/commands/import_cambridge.py
+++ polling_stations/apps/data_importers/management/commands/import_cambridge.py
@@ -1,30 +1,8 @@
 from data_importers.management.commands import BaseHalaroseCsvImporter
-from django.contrib.gis.geos import Point
 
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "CAB"
-    addresses_name = "2023-07-04/2023-05-31T14:59:32.696409/Eros_SQL_Output013.csv"
-    stations_name = "2023-07-04/2023-05-31T14:59:32.696409/Eros_SQL_Output013.csv"
-    elections = ["2023-07-04"]
-    easting_field = "pollingstationeasting"
-    northing_field = "pollingstationnorthing"
-
-    def get_station_point(self, record):
-        if (
-            hasattr(record, self.easting_field)
-            and hasattr(record, self.northing_field)
-            and getattr(record, self.easting_field) != "0"
-            and getattr(record, self.easting_field) != ""
-            and getattr(record, self.northing_field) != "0"
-            and getattr(record, self.northing_field) != ""
-        ):
-            # if we've got points, use them
-
-            return Point(
-                float(getattr(record, self.easting_field)),
-                float(getattr(record, self.northing_field)),
-                srid=27700,
-            )
-
-        return super().get_station_point(record)
+    addresses_name = "2023-11-23/2023-10-25T13:50:03.120466/Eros_SQL_Output003.csv"
+    stations_name = "2023-11-23/2023-10-25T13:50:03.120466/Eros_SQL_Output003.csv"
+    elections = ["2023-11-23"]
