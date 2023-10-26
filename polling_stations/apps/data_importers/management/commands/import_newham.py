3473184a7309bf7a2116d17814595935058fe527
polling-bot-4000 <developers@democracyclub.org.uk>
2023-10-26 16:15:03 +0100

    Import script for Newham (2023-11-23) (closes #5963)

--- polling_stations/apps/data_importers/management/commands/import_newham.py
+++ polling_stations/apps/data_importers/management/commands/import_newham.py
@@ -4,10 +4,10 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "NWM"
     addresses_name = (
-        "2023-07-13/2023-06-16T10:52:14.592700/Democracy_Club__13July2023 (2).tsv"
+        "2023-11-23/2023-10-26T16:14:56.962812/Democracy_Club__23November2023.tsv"
     )
     stations_name = (
-        "2023-07-13/2023-06-16T10:52:14.592700/Democracy_Club__13July2023 (2).tsv"
+        "2023-11-23/2023-10-26T16:14:56.962812/Democracy_Club__23November2023.tsv"
     )
-    elections = ["2023-07-13"]
+    elections = ["2023-11-23"]
     csv_delimiter = "\t"
