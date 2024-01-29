eb15fe2bcc02efc9be301634c3ee0f570b3d6707
polling-bot-4000 <developers@democracyclub.org.uk>
2024-01-29 15:53:21 +0000

    TEST/Import script for North Northamptonshire (2024-02-15) (closes #6078)

--- polling_stations/apps/data_importers/management/commands/import_north_northamptonshire.py
+++ polling_stations/apps/data_importers/management/commands/import_north_northamptonshire.py
@@ -4,10 +4,10 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "NNT"
     addresses_name = (
-        "2024-02-15/2024-01-25T10:56:24.479815/Democracy_Club__15February2024.tsv"
+        "2024-02-15/2024-01-29T15:50:58.118774/Democracy_Club__15February2024.tsv"
     )
     stations_name = (
-        "2024-02-15/2024-01-25T10:56:24.479815/Democracy_Club__15February2024.tsv"
+        "2024-02-15/2024-01-29T15:50:58.118774/Democracy_Club__15February2024.tsv"
     )
     elections = ["2024-02-15"]
     csv_delimiter = "\t"
