8fe40985ba249cf4661ab2ee21b4e8cae3d03173
polling-bot-4000 <developers@democracyclub.org.uk>
2024-01-29 17:04:17 +0000

    Import script for Nuneaton and Bedworth (2024-05-02) (closes #6037)

--- polling_stations/apps/data_importers/management/commands/import_nuneaton_and_bedworth.py
+++ polling_stations/apps/data_importers/management/commands/import_nuneaton_and_bedworth.py
@@ -3,26 +3,7 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "NUN"
-    addresses_name = (
-        "2022-05-05/2022-02-09T16:30:28.635962/Democracy_Club__05May2022.tsv"
-    )
-    stations_name = (
-        "2022-05-05/2022-02-09T16:30:28.635962/Democracy_Club__05May2022.tsv"
-    )
-    elections = ["2022-05-05"]
+    addresses_name = "2024-05-02/2024-01-29T17:04:12.841324/NUN_test.tsv"
+    stations_name = "2024-05-02/2024-01-29T17:04:12.841324/NUN_test.tsv"
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
-
-    def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
-
-        if record.addressline6 in [
-            "CV10 9QF",
-            "CV7 9NQ",
-            "CV11 6NL",
-            "CV11 6JF",
-            "CV11 6JE",
-            "CV11 4NW",
-        ]:
-            return None
-
-        return rec
