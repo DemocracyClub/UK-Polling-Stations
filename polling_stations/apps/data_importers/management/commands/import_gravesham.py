619fbdb88bea7ad1979053a4daf33c0fa583afb4
polling-bot-4000 <developers@democracyclub.org.uk>
2024-02-01 15:10:08 +0000

    Import script for Gravesham (2024-05-02) (closes #6237)

--- polling_stations/apps/data_importers/management/commands/import_gravesham.py
+++ polling_stations/apps/data_importers/management/commands/import_gravesham.py
@@ -3,50 +3,48 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "GRA"
-    addresses_name = "2023-05-04/2023-02-21T21:14:28.593307/polling-stations-export.csv"
-    stations_name = "2023-05-04/2023-02-21T21:14:28.593307/polling-stations-export.csv"
-    elections = ["2023-05-04"]
+    addresses_name = "2024-05-02/2024-02-22T11:43:22.514208/Eros_SQL_Output004 (1).csv"
+    stations_name = "2024-05-02/2024-02-22T11:43:22.514208/Eros_SQL_Output004 (1).csv"
+    elections = ["2024-05-02"]
 
     def address_record_to_dict(self, record):
-        if record.housepostcode.strip() in ["ME3 7NB"]:
+        uprn = record.uprn.strip().lstrip("0")
+
+        if uprn in [
+            "100060927470",  # 88 DOVER ROAD, NORTHFLEET, GRAVESEND
+            "100060927472",  # 90 DOVER ROAD, NORTHFLEET, GRAVESEND
+            "100060927474",  # 92 DOVER ROAD, NORTHFLEET, GRAVESEND
+            "10012012612",  # FLAT ABOVE THE SHIP AND LOBSTER MARK LANE, GRAVESEND
+            "100060938783",  # 162 OLD ROAD EAST, GRAVESEND
+            "100060933575",  # 1 JUBILEE CRESCENT, GRAVESEND
+            "100060933577",  # 3 JUBILEE CRESCENT, GRAVESEND
+            "100060944345",  # RECTORY COTTAGE, SPRINGHEAD ENTERPRISE PARK, SPRINGHEAD ROAD, NORTHFLEET, GRAVESEND
+            "100060939369",  # 36-38 OVERCLIFFE, GRAVESEND
+            "100062310253",  # 99B PELHAM ROAD, GRAVESEND
+            "100062310252",  # 99A PELHAM ROAD, GRAVESEND
+            "100062309977",  # 99C PELHAM ROAD, GRAVESEND
+            "10012012658",  # THE SPRINGS WATLING STREET, GRAVESEND
+            "10012013951",  # LEYWOOD COTTAGE, LEYWOOD ROAD, MEOPHAM, GRAVESEND
+            "100060934936",  # OAKLANDS, LEYWOOD ROAD, MEOPHAM, GRAVESEND
+            "100060934937",  # PRIMROSE COTTAGE, LEYWOOD ROAD, MEOPHAM, GRAVESEND
+            "100062063828",  # 1 GATEHOUSE COTTAGE, CHURCH ROAD, IFIELD, COBHAM, GRAVESEND
+            "100060938600",  # YEW TREES, OAKENDEN ROAD, LUDDESDOWN, GRAVESEND
+            "10012013958",  # WOODHILL FARM, BRIMSTONE HILL, MEOPHAM, GRAVESEND
+            "10012014473",  # BRIMSTONE WOOD, BRIMSTONE HILL, MEOPHAM, GRAVESEND
+            "10012014411",  # BRIMSTONE LODGE, BRIMSTONE HILL, MEOPHAM, GRAVESEND
+        ]:
+            return None
+
+        if record.housepostcode.strip() in [
+            # splits
+            "ME3 7NB",
+            "DA12 4JS",  # GREGORYS CRESCENT, GRAVESEND
+            "DA13 0XA",  # GOLD STREET, LUDDESDOWN, GRAVESEND
+            "DA13 0UF",  # WHITE HORSE ROAD, MEOPHAM, GRAVESEND
+            "DA13 0UA",  # SWANSWOOD FARM, HARVEL ROAD, MEOPHAM, GRAVESEND
+            "DA13 0RN",  # HARVEL ROAD, MEOPHAM, GRAVESEND
+            "DA3 7AL",  # SHIPLEY HILL COTTAGES, LONGFIELD ROAD, LONGFIELD
+        ]:
             return None
 
         return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        if record.pollingstationnumber in (
-            "4",
-            "52",
-        ):  # Shears Green Community Centre, Shears Green Community Centre, 9 Westcott Avenue, Northfleet, Kent
-            record = record._replace(
-                pollingstationname="Shears Green Community Centre",
-                pollingstationaddress_1="9 Westcott Avenue",
-                pollingstationaddress_2="",
-            )
-        if (
-            record.pollingstationnumber == "23"
-        ):  # Hive House Library, Hive House, 10-11 The Hive, Northfleet, Kent
-            record = record._replace(
-                pollingstationname="The Hive Library",
-            )
-        if (
-            record.pollingstationnumber == "7"
-        ):  # Riverside Family Learning, Riverside Community Resource Trust, Dickens Road, Gravesend, Kent
-            record = record._replace(
-                pollingstationname="Riverside Family Learning Centre",
-                pollingstationaddress_1="",
-            )
-        if (
-            record.pollingstationnumber == "21"
-        ):  # Culverstone Community Hall, Culverstone Community Centre, Whitepost Lane, Culverstone, Meopham, Kent
-            record = record._replace(
-                pollingstationname="Culverstone Community Centre",
-                pollingstationaddress_1="",
-            )
-
-        if record.pollingstationname == "WHISSENDINE MEMORIAL HALL":
-            record = record._replace(pollingstationpostcode="LE15 7ET")
-        if record.pollingstationname == "LANGHAM VILLAGE HALL":
-            record = record._replace(pollingstationpostcode="LE15 7JE")
-
-        return super().station_record_to_dict(record)
