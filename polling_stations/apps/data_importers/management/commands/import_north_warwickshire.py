e7c6fc38dcb1976c2438aced4f4006f6c2ab22e0
polling-bot-4000 <developers@democracyclub.org.uk>
2024-02-07 08:53:32 +0000

    Import script for North Warwickshire (2024-05-02) (closes #6118)

--- polling_stations/apps/data_importers/management/commands/import_north_warwickshire.py
+++ polling_stations/apps/data_importers/management/commands/import_north_warwickshire.py
@@ -4,44 +4,33 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "NWA"
     addresses_name = (
-        "2023-05-04/2023-01-18T11:33:19.887373/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-02-07T08:53:01.197969/Democracy_Club__02May2024.tsv"
     )
     stations_name = (
-        "2023-05-04/2023-01-18T11:33:19.887373/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-02-07T08:53:01.197969/Democracy_Club__02May2024.tsv"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "10001177416",  # WARREN FARM HOUSE, KINWALSEY LANE, MERIDEN, COVENTRY
             "100071230930",  # I.S.I LTD, LEA COTTAGE NUNEATON ROAD, ANSLEY
             "200001812992",  # LEA LODGE, ANSLEY, NUNEATON
-            "10001177408",  # CHURCH TREE BARN, KINWALSEY LANE, MERIDEN, COVENTRY
         ]:
             return None
 
         if record.addressline6 in [
-            "B46 1BB",  # split in council data
-            "ST4 7LG",
+            # split in council data
+            "B46 1BB",
+            # suspect
             "CV10 9SW",
+            "CV9 3DW",
+            "CV9 2PB",
+            "CV10 0TB",
+            "B76 0DT",
         ]:
             return None
 
         return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # Council Station Change via slack
-        # Piccadilly Community Centre -> Kingsbury Sports & Community Centre
-        if record.polling_place_id == "3997":
-            record = record._replace(
-                polling_place_name="Kingsbury Sports & Community Centre,,,B78 2LG",
-                polling_place_address_1="Pear Tree Avenue",
-                polling_place_address_2="",
-                polling_place_address_3="",
-                polling_place_address_4="Kingsbury",
-                polling_place_postcode="B78 2LG",
-            )
-        return super().station_record_to_dict(record)
