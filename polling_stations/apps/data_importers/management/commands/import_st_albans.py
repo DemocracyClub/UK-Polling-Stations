66aabdd015d72c346745db4a2d5ada89822a09e9
polling-bot-4000 <developers@democracyclub.org.uk>
2024-02-09 15:56:36 +0000

    Import script for St Albans (2024-05-02) (closes #6133)

--- polling_stations/apps/data_importers/management/commands/import_st_albans.py
+++ polling_stations/apps/data_importers/management/commands/import_st_albans.py
@@ -3,23 +3,23 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SAL"
-    addresses_name = (
-        "2023-05-04/2023-03-02T17:12:56.253915/Democracy_Club__04May2023.tsv"
-    )
-    stations_name = (
-        "2023-05-04/2023-03-02T17:12:56.253915/Democracy_Club__04May2023.tsv"
-    )
-    elections = ["2023-05-04"]
+    addresses_name = "2024-05-02/2024-02-09T15:55:50.075896/Democracy_Club__02May2024_St Albans City and District Council.tsv"
+    stations_name = "2024-05-02/2024-02-09T15:55:50.075896/Democracy_Club__02May2024_St Albans City and District Council.tsv"
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
+
+    def station_record_to_dict(self, record):
+        # Wood End School, Yeomans Avenue, Harpenden
+        if record.polling_place_id == "5061":
+            record = record._replace(polling_place_northing="215414")
+
+        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
             "100081131978",  # PADDOCK VIEW, AYRES END LANE, CHILDWICKBURY, ST. ALBANS
-            "100080863526",  # 2 ROSE WALK, ST. ALBANS
-            "100080863527",  # 3 ROSE WALK, ST. ALBANS
-            "100080863525",  # 1 ROSE WALK, ST. ALBANS
             "10024115888",  # FLAT AT ST ALBANS SCHOOL PAVILLION 162 HARPENDEN ROAD, ST ALBANS
             "200003633693",  # WOOD END, HATCHING GREEN, HARPENDEN
             "100081134635",  # PLAISTOWES FARM, NOKE LANE, ST. ALBANS
@@ -28,10 +28,17 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
         if record.addressline6 in [
             # splits
-            "AL3 5PR",
-            "AL4 0TP",
-            "AL4 0TG",
+            "AL4 0LD",
+            "AL1 3NS",
             "AL3 5PD",
+            "AL3 5PR",
+            "AL1 4RJ",
+            "AL4 0TP",
+            # suspect
+            "AL5 1AB",
+            "AL5 1AA",
+            "AL1 5NT",
+            "AL5 2AB",
         ]:
             return None
 
