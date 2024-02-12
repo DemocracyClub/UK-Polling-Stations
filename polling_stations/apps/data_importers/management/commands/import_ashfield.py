cf9ed8e721df42fbddb6e83144f8602cf6f7003c
polling-bot-4000 <developers@democracyclub.org.uk>
2024-02-12 11:35:23 +0000

    Import script for Ashfield (2024-05-02) (closes #6139)

--- polling_stations/apps/data_importers/management/commands/import_ashfield.py
+++ polling_stations/apps/data_importers/management/commands/import_ashfield.py
@@ -4,29 +4,17 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "ASH"
     addresses_name = (
-        "2023-05-04/2023-03-13T11:02:12.723251/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-02-12T11:35:02.573130/Democracy_Club__02May2024.tsv"
     )
     stations_name = (
-        "2023-05-04/2023-03-13T11:02:12.723251/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-02-12T11:35:02.573130/Democracy_Club__02May2024.tsv"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "100031248754",  # 31 COXMOOR ROAD, SUTTON-IN-ASHFIELD
-        ]:
-            return None
-
         if record.addressline6 in [
-            # split
-            "NG17 8BE",
-            "NG17 5HS",
-            # wrong
-            "NG17 8JR",
-            "NG17 5HS",
+            "NG17 8BE",  # split
         ]:
             return None
 
