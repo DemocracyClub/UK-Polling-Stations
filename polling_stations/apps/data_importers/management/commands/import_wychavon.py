8c6a9b154f6d50030afde1202ead33c56b69bf1d
polling-bot-4000 <developers@democracyclub.org.uk>
2024-02-13 14:02:04 +0000

    Import script for Wychavon (2024-05-02) (closes #6150)

--- polling_stations/apps/data_importers/management/commands/import_wychavon.py
+++ polling_stations/apps/data_importers/management/commands/import_wychavon.py
@@ -5,16 +5,18 @@ from django.contrib.gis.geos import Point
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WYC"
     addresses_name = (
-        "2023-05-04/2023-03-09T16:32:09.389409/Democracy_Club__04May2023.CSV"
+        "2024-05-02/2024-02-13T14:01:52.087758/Democracy_Club__02May2024.tsv"
     )
     stations_name = (
-        "2023-05-04/2023-03-09T16:32:09.389409/Democracy_Club__04May2023.CSV"
+        "2024-05-02/2024-02-13T14:01:52.087758/Democracy_Club__02May2024.tsv"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
+    csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
         # user issue report #197
-        if record.polling_place_id == "7278":  # Northwick Hotel
+        # Northwick Arms Hotel (Monroe Suite), Waterside, Evesham
+        if record.polling_place_id == "7982":
             rec = super().station_record_to_dict(record)
             rec["location"] = Point(-1.942675, 52.090313, srid=4326)
             return rec
@@ -22,6 +24,17 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "100120715029",  # ORCHARD COTTAGE EARLS COMMON ROAD, STOCK GREEN
+            "100121276049",  # GLENFIELD HOUSE, WORCESTER ROAD, UPTON WARREN, BROMSGROVE
+            "10013939808",  # COMPOST CORNER, KNOWLE HILL, EVESHAM
+            "100121281842",  # PRIORY REST HOME, CRUTCH LANE, ELMBRIDGE, DROITWICH
+            "100120708134",  # HALCYON, HANBURY ROAD, DROITWICH
+        ]:
+            return None
+
         if record.addressline6 in [
             # split
             "WR11 7UQ",
