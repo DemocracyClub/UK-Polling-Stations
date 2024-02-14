ebe46c1f8b8976439ea0fb3528ace9c35d6a0b4a
polling-bot-4000 <developers@democracyclub.org.uk>
2024-02-14 15:34:58 +0000

    Import script for Gosport (2024-05-02) (closes #6159)

--- polling_stations/apps/data_importers/management/commands/import_gosport.py
+++ polling_stations/apps/data_importers/management/commands/import_gosport.py
@@ -3,12 +3,25 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "GOS"
-    addresses_name = "2022-05-05/2022-03-07T15:47:28.644792/2022 Borough of Gosport - Democracy Club - Polling Districts v1 (07 03 2022).csv"
-    stations_name = "2022-05-05/2022-03-07T15:47:28.644792/2022 Borough of Gosport - Democracy Club - Polling Stations v1 (07 03 2022).csv"
-    elections = ["2022-05-05"]
+    addresses_name = "2024-05-02/2024-02-14T15:34:50.440698/2024 LGE & PCC - Democracy Club - Polling Districts.csv"
+    stations_name = "2024-05-02/2024-02-14T15:34:50.440698/2024 LGE & PCC - Democracy Club - Polling Stations.csv"
+    elections = ["2024-05-02"]
+
+    def station_record_to_dict(self, record):
+        # St. Marys Church Parish Centre, Green Road, Gosport, Hampshire
+        if record.stationcode == "3AL3" or record.stationcode == "4AN1":
+            record = record._replace(xordinate="460159", yordinate="98802")
+
+        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-        if record.postcode in ["PO12 2EH"]:
+        if record.postcode in [
+            # suspect
+            "PO12 1SE",
+            "PO12 4AW",
+            "PO12 4QE",
+            "PO12 4JP",
+        ]:
             return None
 
         return super().address_record_to_dict(record)
