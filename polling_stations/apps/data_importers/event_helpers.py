013d803125ffac4755c95cab1f24c994691975f7
GeoWill <roper.william@gmail.com>
2024-02-14 12:30:34 +0000

    Add event helper functions to replay station field events.

--- polling_stations/apps/data_importers/event_helpers.py
+++ polling_stations/apps/data_importers/event_helpers.py
@@ -1,6 +1,11 @@
 from councils.models import Council
 from data_importers.event_types import DataEventType
 from data_importers.models import DataEvent
+from pollingstations.models import PollingStation, VisibilityChoices
+
+
+class EventValidationError(Exception):
+    pass
 
 
 def record_teardown_event(council_id):
@@ -8,3 +13,38 @@ def record_teardown_event(council_id):
         council=Council.objects.get(council_id=council_id),
         event_type=DataEventType.TEARDOWN,
     )
+
+
+def set_station_visibility(event):
+    payload = event.payload
+    if not (visibility := payload.get("visibility", None)):
+        raise EventValidationError(f"{event} payload missing visibility.")
+    if visibility not in VisibilityChoices:
+        raise EventValidationError(
+            f"DataEvent visibility not a valid choice. Visibility was '{visibility}', choices are {VisibilityChoices.choices}."
+        )
+    try:
+        station = PollingStation.objects.get(
+            internal_council_id=payload["internal_council_id"],
+            council_id=event.council_id,
+        )
+    except PollingStation.DoesNotExist:
+        # If the station doesn't exist this is a no op.
+        return
+    station.visibility = visibility
+    station.save()
+
+
+def set_station_field_from_event(event: DataEvent):
+    if event.event_type not in DataEventType.station_update_event_types():
+        raise EventValidationError(
+            f"{event} event_type ('{event.event_type}') does not appear in DataEventType.station_update_event_types"
+        )
+    if not event.payload.get("internal_council_id", None):
+        raise EventValidationError(f"{event} payload missing station id.")
+
+    match event.event_type:
+        case DataEventType.SET_STATION_VISIBILITY:
+            set_station_visibility(event)
+        case _:
+            pass
--- polling_stations/apps/data_importers/tests/test_event_helpers.py
+++ polling_stations/apps/data_importers/tests/test_event_helpers.py
@@ -1,8 +1,20 @@
 from councils.tests.factories import CouncilFactory
-from data_importers.event_helpers import record_teardown_event
-from data_importers.event_types import DataEventType
+from data_importers.event_helpers import (
+    EventValidationError,
+    record_teardown_event,
+    set_station_field_from_event,
+    set_station_visibility,
+)
+from data_importers.event_types import (
+    DataEventType,
+    EventUserType,
+    StationCorrectionSource,
+)
 from data_importers.models import DataEvent
+from data_importers.tests.factories import DataEventFactory
 from django.test import TestCase
+from pollingstations.models import VisibilityChoices
+from pollingstations.tests.factories import PollingStationFactory
 
 
 class CouncilTest(TestCase):
@@ -33,3 +45,138 @@ class CouncilTest(TestCase):
             1,
         )
         self.assertEqual(len(DataEvent.objects.all()), 1)
+
+
+class PollingStationEventTest(TestCase):
+    def setUp(self):
+        CouncilFactory(council_id="ABC")
+        self.station = PollingStationFactory(
+            internal_council_id="32A",
+            address="Village Hall\nWinklesford Merriot",
+            postcode="FOOBAR",
+            council_id="ABC",
+        )
+        self.unpublish_event = DataEventFactory(
+            council_id="ABC",
+            event_type=DataEventType.SET_STATION_VISIBILITY,
+            payload={
+                "internal_council_id": "32A",
+                "visibility": VisibilityChoices.UNPUBLISHED,
+                "payload_version": 1,
+            },
+            metadata={
+                "reason": StationCorrectionSource.DEMOCRACY_CLUB,
+                "reason_detail": "Station address is wrong",
+                "source": EventUserType.ADMIN,
+            },
+        )
+        self.publish_event = DataEventFactory(
+            council_id="ABC",
+            event_type=DataEventType.SET_STATION_VISIBILITY,
+            payload={
+                "internal_council_id": "32A",
+                "visibility": VisibilityChoices.PUBLISHED,
+                "payload_version": 1,
+            },
+            metadata={
+                "reason": StationCorrectionSource.DEMOCRACY_CLUB,
+                "reason_detail": "All ok!",
+                "source": EventUserType.ADMIN,
+            },
+        )
+
+    def test_set_station_visibility(self):
+        # Make check station is published
+        self.assertEqual(self.station.visibility, VisibilityChoices.PUBLISHED)
+
+        # replay unpublish event
+        set_station_visibility(self.unpublish_event)
+        # refresh station from db
+        self.station.refresh_from_db()
+        # Check station is unpublished
+        self.assertEqual(self.station.visibility, VisibilityChoices.UNPUBLISHED)
+
+        # Do the same, but the other way:
+        # Replay publish event
+        set_station_visibility(self.publish_event)
+        # refresh station from db
+        self.station.refresh_from_db()
+        # Check station is unpublished
+        self.assertEqual(self.station.visibility, VisibilityChoices.PUBLISHED)
+
+    def test_set_station_field_from_event(self):
+        # Make check station is published
+        self.assertEqual(self.station.visibility, VisibilityChoices.PUBLISHED)
+
+        # replay unpublish event
+        set_station_field_from_event(self.unpublish_event)
+        # refresh station from db
+        self.station.refresh_from_db()
+        # Check station is unpublished
+        self.assertEqual(self.station.visibility, VisibilityChoices.UNPUBLISHED)
+        # Do the same, but the other way:
+        # Replay publish event
+        set_station_field_from_event(self.publish_event)
+        # refresh station from db
+        self.station.refresh_from_db()
+        # Check station is unpublished
+        self.assertEqual(self.station.visibility, VisibilityChoices.PUBLISHED)
+
+    def test_set_station_visibility_payload_missing_visibility(self):
+        event_missing_visibility = DataEventFactory(
+            council_id="ABC",
+            event_type=DataEventType.SET_STATION_VISIBILITY,
+            payload={
+                "internal_council_id": "32A",
+                "payload_version": 1,
+            },
+        )
+        with self.assertRaises(EventValidationError) as e:
+            set_station_visibility(event_missing_visibility)
+        self.assertEqual(
+            str(e.exception),
+            f"DataEvent object ({event_missing_visibility.pk}) payload missing visibility.",
+        )
+
+    def test_set_station_visibility_invalid_visibility(self):
+        event_invalid_visibility = DataEventFactory(
+            council_id="ABC",
+            event_type=DataEventType.SET_STATION_VISIBILITY,
+            payload={
+                "internal_council_id": "32A",
+                "visibility": ("HIDDEN", "Hidden"),
+                "payload_version": 1,
+            },
+        )
+        with self.assertRaises(EventValidationError) as e:
+            set_station_visibility(event_invalid_visibility)
+        self.assertEqual(
+            str(e.exception),
+            f"DataEvent visibility not a valid choice. Visibility was '('HIDDEN', 'Hidden')', choices are {VisibilityChoices.choices}.",
+        )
+
+    def test_set_station_field_wrong_event_type(self):
+        import_event = DataEventFactory(
+            council_id="ABC", event_type=DataEventType.IMPORT
+        )
+        with self.assertRaises(EventValidationError) as e:
+            set_station_field_from_event(import_event)
+        self.assertEqual(
+            str(e.exception),
+            f"{import_event} event_type ('IMPORT') does not appear in DataEventType.station_update_event_types",
+        )
+
+    def test_set_station_field_missing_station_id(self):
+        event_missing_station_id = DataEventFactory(
+            council_id="ABC",
+            event_type=DataEventType.SET_STATION_VISIBILITY,
+            payload={
+                "visibility": VisibilityChoices.UNPUBLISHED,
+                "payload_version": 1,
+            },
+        )
+        with self.assertRaises(EventValidationError) as e:
+            set_station_field_from_event(event_missing_station_id)
+        self.assertEqual(
+            str(e.exception), f"{event_missing_station_id} payload missing station id."
+        )
