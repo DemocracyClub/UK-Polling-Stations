6557574a13835685ac501c27fadfce716111b521
GeoWill <roper.william@gmail.com>
2024-02-14 12:30:34 +0000

    Add event helper functions to replay station field events.

--- polling_stations/apps/data_importers/event_types.py
+++ polling_stations/apps/data_importers/event_types.py
@@ -8,3 +8,18 @@ class DataEventType(models.TextChoices):
     TEARDOWN = "TEARDOWN", "Council data torn down"
     # Set polling station visibility
     SET_STATION_VISIBILITY = "SET_STATION_VISIBILITY", "Station visibility changed"
+
+    @classmethod
+    def station_update_event_types(cls):
+        return (cls.SET_STATION_VISIBILITY,)
+
+
+class EventUserType(models.TextChoices):
+    ADMIN = "ADMIN", "Admin Panel User"
+    COUNCIL = "COUNCIL", "Council User"
+
+
+class StationCorrectionSource(models.TextChoices):
+    COUNCIL = "COUNCIL", "Report from council"
+    DEMOCRACY_CLUB = "DEMOCRACY_CLUB", "Report from Democracy Club"
+    PUBLIC = "PUBLIC", "Report from public"
