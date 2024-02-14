f2ab37e16403e0279c2bfa7388ab7dcdd979a6f3
Sym Roe <sym.roe@talusdesign.co.uk>
2024-02-14 10:12:15 +0000

    Allow writing more env vars from SSM globally

---
+++ deploy/hooks/afterinstall/write_global_env_vars.py
@@ -0,0 +1,23 @@
+#!/var/www/polling_stations/code/venv/bin/python
+
+import sys
+
+import boto3.exceptions
+import botocore
+
+client = boto3.client("ssm", region_name="eu-west-2")
+
+vars = {"SENTRY_DSN": "", "DC_ENVIRONMENT": ""}
+
+
+for var in vars:
+    try:
+        value = client.get_parameter(Name=var)["Parameter"]["Value"]
+    except botocore.exceptions.ClientError:
+        sys.exit(f"{var} not found")
+    vars[var] = value
+
+
+with open("/etc/environment", "a") as env_file:
+    for key, value in vars.items():
+        env_file.write(f"""\n{key}="{value}"\n""")
