ad8d50f3335b62e646cad8b4073610b389c5f7c4
polling-bot-4000 <developers@democracyclub.org.uk>
2024-02-14 16:35:43 +0000

    Import script for Bury (2024-05-02) (closes #6161)

--- polling_stations/apps/data_importers/management/commands/import_bury.py
+++ polling_stations/apps/data_importers/management/commands/import_bury.py
@@ -4,24 +4,51 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "BUR"
     addresses_name = (
-        "2023-05-04/2023-03-09T16:44:01.921720/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-02-14T16:35:28.213398/Democracy_Club__02May2024.tsv"
     )
     stations_name = (
-        "2023-05-04/2023-03-09T16:44:01.921720/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-02-14T16:35:28.213398/Democracy_Club__02May2024.tsv"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
 
+    def station_record_to_dict(self, record):
+        # Greenmount Cricket Club, Brandlesholme Road, Bury
+        if record.polling_place_id == "4977":
+            record = record._replace(polling_place_postcode="BL8 4DX")
+
+        # Tottington Library, Market Street, Tottington
+        if record.polling_place_id == "4988":
+            record = record._replace(polling_place_postcode="BL8 3LL")
+
+        # Sunnybank Community Centre, 248 Sunnybank Road, Unsworth, Bury
+        if record.polling_place_id == "5082":
+            record = record._replace(polling_place_postcode="BL9 8EH")
+
+        return super().station_record_to_dict(record)
+
     def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "4210011016",  # BIRCHEM BOWER FARM, HARWOOD ROAD, TOTTINGTON, BURY
+            "100010966697",  # MANSONS, 10 MANCHESTER OLD ROAD, BURY
+        ]:
+            return None
+
         if record.addressline6 in [
-            "BL8 1TF",  # wrong
             # split
-            "BL9 8HB",
-            "BL9 9PQ",
+            "M25 1JW",
+            "BL9 8JW",
+            "BL8 2HH",
             "BL9 9JW",
             "BL9 8JJ",
-            "BL8 2HH",
-            "BL9 8JW",
+            "BL9 9PQ",
+            "M25 1ED",
+            # suspect
+            "BL8 1TF",
+            "M26 1RZ",
+            "BL8 4LB",
         ]:
             return None
 
