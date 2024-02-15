7131e7f1abc532ddd6adeab188ce29f2a84d5fd2
polling-bot-4000 <developers@democracyclub.org.uk>
2024-02-15 13:48:19 +0000

    Import script for Harlow (2024-05-02) (closes #6172)

--- polling_stations/apps/data_importers/management/commands/import_harlow.py
+++ polling_stations/apps/data_importers/management/commands/import_harlow.py
@@ -5,12 +5,12 @@ from django.contrib.gis.geos import Point
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "HAR"
     addresses_name = (
-        "2023-05-04/2023-03-03T09:41:36.035764/Democracy_Club__04May2023.CSV"
+        "2024-05-02/2024-02-15T13:48:10.279104/Democracy_Club__02May2024.CSV"
     )
     stations_name = (
-        "2023-05-04/2023-03-03T09:41:36.035764/Democracy_Club__04May2023.CSV"
+        "2024-05-02/2024-02-15T13:48:10.279104/Democracy_Club__02May2024.CSV"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
@@ -20,14 +20,28 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "10023422799",  # BENSONS BARN, HARLOW COMMON, HARLOW
             "100091255381",  # KITCHEN HALL FARM, RED LION LANE, HARLOW
             "100091438137",  # 1 KITCHEN HALL COTTAGES, RED LION LANE, HARLOW
-            "200002567081",  # CARETAKERS HOUSE KINGSMOOR COUNTY PRIMARY SCHOOL PLOYTERS ROAD, HARLOW
             "100091438138",  # 2 KITCHEN HALL COTTAGES, RED LION LANE, HARLOW
+            "100091255475",  # NEXUS HOUSE, SCHOOL LANE, HARLOW
+            "100091443174",  # THE PRESBYTERY OUR LADY OF FATIMA ROMAN CATHOLIC CHURCH HOWARD WAY, HARLOW
+            "200002567135",  # STEWARDS RESIDENCE CANONS BROOK GOLF CLUB ELIZABETH WAY, HARLOW
+            "10003710616",  # HEART & CLUB, PYPERS HATCH, HARLOW
+            "100091255570",  # GOLDINGS FARM, TYE GREEN VILLAGE, HARLOW
+            "100091255180",  # THE HAWTHORNS, MONKSWICK ROAD, HARLOW
         ]:
             return None
 
         if record.addressline6 in [
-            "CM20 1BA",  # split
-            "CM20 2EP",  # HARLOW MILL LOCKHOUSE, CAMBRIDGE ROAD, HARLOW
+            # split
+            "CM20 1BA",
+            # looks wrong
+            "CM20 2EP",
+            "CM20 3QF",
+            "CM19 4FE",
+            "CM20 3RA",
+            "CM20 3PA",
+            "CM20 3RL",
+            "CM17 0AR",
+            "CM19 4EU",
         ]:
             return None
 
@@ -37,7 +51,7 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         rec = super().station_record_to_dict(record)
 
         # St Thomas More Church Hall, Hodings Road, Harlow
-        if rec["internal_council_id"] == "4390":
+        if rec["internal_council_id"] == "4649":
             rec["location"] = Point(0.080382, 51.770798, srid=4326)
 
         return rec
