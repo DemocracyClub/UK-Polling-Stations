a83b8f828bd061d103569c3155bf91b3c8b7b79e
polling-bot-4000 <developers@democracyclub.org.uk>
2024-02-16 15:26:42 +0000

    Import script for Calderdale (2024-05-02) (closes #6183)

--- polling_stations/apps/data_importers/management/commands/import_calderdale.py
+++ polling_stations/apps/data_importers/management/commands/import_calderdale.py
@@ -3,19 +3,28 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "CLD"
-    addresses_name = (
-        "2023-05-04/2023-04-13T15:17:42.827927/Calderdale Polling Districts 2023.csv"
-    )
-    stations_name = (
-        "2023-05-04/2023-04-13T15:17:42.827927/Calderdale Polling Stations 2023.csv"
-    )
-    elections = ["2023-05-04"]
+    addresses_name = "2024-05-02/2024-02-16T15:25:54.920405/2024 02 16 Democracy Club Polling Districts export.csv"
+    stations_name = "2024-05-02/2024-02-16T15:25:54.920405/2024 02 16 Democracy Club Polling Stations export.csv"
+    elections = ["2024-05-02"]
+    csv_encoding = "utf-16le"
+
+    def station_record_to_dict(self, record):
+        # LEE MOUNT BAPTIST CHURCH, MELBOURNE STREET, LEE MOUNT, HALIFAX, WEST YORKSHIRE HX3 5BQ
+        if record.stationcode == "60JB":
+            record = record._replace(xordinate="408412")
+            record = record._replace(yordinate="426427")
+
+        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
+        if record.uprn in [
+            "10093206213",  # 1 PARKIN LANE, TODMORDEN
+            "10093208972",  # GREAT LEAR INGS BARN, HEPTONSTALL, HEBDEN BRIDGE
+        ]:
+            return None
+
         if record.postcode in [
-            # split
-            "OL14 6RS",
-            "HX3 5JF",
+            "HX2 0UW",  # suspect
         ]:
             return None
 
