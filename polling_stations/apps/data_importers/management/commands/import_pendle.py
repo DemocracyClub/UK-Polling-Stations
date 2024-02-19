f3d3ca51ac59c641c331f13b6502a20ed15740c7
polling-bot-4000 <developers@democracyclub.org.uk>
2024-02-19 13:04:59 +0000

    Import script for Pendle (2024-05-02) (closes #6196)

--- polling_stations/apps/data_importers/management/commands/import_pendle.py
+++ polling_stations/apps/data_importers/management/commands/import_pendle.py
@@ -4,27 +4,34 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "PEN"
     addresses_name = (
-        "2023-05-04/2023-03-09T12:04:45.963515/Democracy_Club__04May2023.CSV"
+        "2024-05-02/2024-02-19T13:04:13.385326/Democracy_Club__02May2024 (2).tsv"
     )
     stations_name = (
-        "2023-05-04/2023-03-09T12:04:45.963515/Democracy_Club__04May2023.CSV"
+        "2024-05-02/2024-02-19T13:04:13.385326/Democracy_Club__02May2024 (2).tsv"
     )
-    elections = ["2023-05-04"]
-
-    def station_record_to_dict(self, record):
-        # Thomas Street Bowling Pavilion, Percy Street, Nelson BB9 9AY
-        if record.polling_place_id == "4554":
-            record = record._replace(
-                polling_place_northing=437261,
-                polling_place_easting=386085,
-            )
-
-        return super().station_record_to_dict(record)
+    elections = ["2024-05-02"]
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "10022950288",  # PEARL COTTAGE, ALMA ROAD, COLNE
+            "10090961710",  # FLAT, TOWER BUILDINGS 2A, KEIGHLEY ROAD, COLNE
+            "61014294",  # 117 EDWARD STREET, NELSON
+            "100012398597",  # PLANTATION COTTAGE, GREENHEAD LANE, FENCE, BURNLEY
+            "10024181464",  # FOSTERS LEAP BARN, TRAWDEN, COLNE
+            "10024181048",  # WHITEMOOR BOTTOM FARM, WHITEMOOR ROAD, FOULRIDGE, COLNE
+            "100012400306",  # BROWN HOUSE FARM, GISBURN OLD ROAD, BLACKO, NELSON
+            "100012400306",  # BROWN HOUSE FARM, GISBURN OLD ROAD, BLACKO, NELSON
+        ]:
+            return None
+
         if record.addressline6 in [
+            # splits
             "BB9 0RQ",
-            "BB9 7YS",
+            "BB9 7YS",  # REEDYFORD COTTAGE, SCOTLAND ROAD, NELSON
+            "BB8 7DN",  # GRASMERE CLOSE, COLNE
         ]:
             return None
 
