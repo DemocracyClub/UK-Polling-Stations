bd82124c449150a94e541cb7f3119e7cdee4143d
polling-bot-4000 <developers@democracyclub.org.uk>
2024-02-20 16:02:21 +0000

    Import script for Hart (2024-05-02) (closes #6209)

--- polling_stations/apps/data_importers/management/commands/import_hart.py
+++ polling_stations/apps/data_importers/management/commands/import_hart.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "HAT"
     addresses_name = (
-        "2023-05-04/2023-03-06T14:01:01.740038/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-02-20T16:01:42.397817/Democracy_Club__02May2024.tsv"
     )
     stations_name = (
-        "2023-05-04/2023-03-06T14:01:01.740038/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-02-20T16:01:42.397817/Democracy_Club__02May2024.tsv"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -17,19 +17,18 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
         if uprn in [
             "200000999587",  # HOOK CROSS COTTAGE READING ROAD, ROTHERWICK, HOOK
-            "100060420959",  # SHAPLEY LODGE, LONDON ROAD, HARTLEY WINTNEY, HOOK
-            "10008960932",  # ANNEXE, SHAPLEY LODGE, LONDON ROAD, HARTLEY WINTNEY, HOOK
             "100060417933",  # STILLERS FARM, EWSHOT LANE, EWSHOT, FARNHAM
             "200001011509",  # WILLOW HOUSE, ALBANY ROAD, FLEET
             "10008963593",  # 1 OAKTREE PADDOCK, POTBRIDGE, ODIHAM, HOOK
             "10008963594",  # 2 OAKTREE PADDOCK POTBRIDGE ROAD, ODIHAM, HOOK
+            "10008962564",  # BAILEYS FARMHOUSE, ODIHAM ROAD, ODIHAM, HOOK
         ]:
             return None
 
         if record.addressline6 in [
-            "GU52 0AF",  # splits
-            "RG27 9RJ",  # splits
-            "GU14 0LL",  # 128-130 OLD IVELY ROAD, FARNBOROUGH
+            # split
+            "RG27 9RJ",
+            "GU52 0AF",
         ]:
             return None
 
