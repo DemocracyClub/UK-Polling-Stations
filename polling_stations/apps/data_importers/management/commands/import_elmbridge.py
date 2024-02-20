4d773f470def057d94eac5784922d8f8daa6fa3f
polling-bot-4000 <developers@democracyclub.org.uk>
2024-02-20 17:29:50 +0000

    Import script for Elmbridge (2024-05-02) (closes #6217)

--- polling_stations/apps/data_importers/management/commands/import_elmbridge.py
+++ polling_stations/apps/data_importers/management/commands/import_elmbridge.py
@@ -3,6 +3,19 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "ELM"
-    addresses_name = "2023-05-04/2023-03-09T12:52:17.627189/Elmbridge_Democracy Club data_Eros_SQL_Output004.csv"
-    stations_name = "2023-05-04/2023-03-09T12:52:17.627189/Elmbridge_Democracy Club data_Eros_SQL_Output004.csv"
-    elections = ["2023-05-04"]
+    addresses_name = "2024-05-02/2024-02-20T17:29:24.243165/Eros_SQL_Output007.csv"
+    stations_name = "2024-05-02/2024-02-20T17:29:24.243165/Eros_SQL_Output007.csv"
+    elections = ["2024-05-02"]
+
+    def address_record_to_dict(self, record):
+        uprn = record.uprn.strip().lstrip("0")
+
+        if uprn in [
+            "100062356889",  # 1 ICKLINGHAM GATE, COBHAM
+            "100061307354",  # BRACKENBURY, BYFLEET ROAD, COBHAM
+            "100061326835",  # 133 BURWOOD ROAD, HERSHAM, WALTON-ON-THAMES
+            "100061326833",  # 131 BURWOOD ROAD, HERSHAM, WALTON-ON-THAMES
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
