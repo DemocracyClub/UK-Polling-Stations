c71916e8099e74c15b35103731a6039c27d802e7
polling-bot-4000 <developers@democracyclub.org.uk>
2024-02-22 10:50:50 +0000

    Import script for Runnymede (2024-05-02) (closes #6233)

--- polling_stations/apps/data_importers/management/commands/import_runnymede.py
+++ polling_stations/apps/data_importers/management/commands/import_runnymede.py
@@ -4,32 +4,33 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "RUN"
     addresses_name = (
-        "2023-05-04/2023-03-02T14:04:03.113510/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-02-22T10:50:15.362737/Democracy_Club__02May2024.tsv"
     )
     stations_name = (
-        "2023-05-04/2023-03-02T14:04:03.113510/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-02-22T10:50:15.362737/Democracy_Club__02May2024.tsv"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "10091662635",  # 3 CLIFTON GRANGE, BLACKPOOL ROAD, CLIFTON, PRESTON
             "10092963299",  # FLAT AT 79 STATION ROAD, ADDLESTONE
             "10095815752",  # FLAT 50, AVIATOR HOUSE, 227 STATION ROAD, ADDLESTONE
             "10095815848",  # FLAT 136, AVIATOR HOUSE, 227 STATION ROAD, ADDLESTONE
             "10095815743",  # FLAT 32, AVIATOR HOUSE, 227 STATION ROAD, ADDLESTONE
             "200004029979",  # LODGE, ST. GEORGES COLLEGE, WEYBRIDGE ROAD, ADDLESTONE
             "100062604551",  # ST. GEORGES COLLEGE, WEYBRIDGE ROAD, ADDLESTONE
+            "100062600234",  # RUSHAM COTTAGE, PRUNE HILL, ENGLEFIELD GREEN, EGHAM
+            "10092962917",  # JAKES FARM, BROX LANE, OTTERSHAW, CHERTSEY
+            "10092961689",  # GEORGE ELIOT HOUSE, HARVEST ROAD, ENGLEFIELD GREEN, EGHAM
         ]:
             return None
         if record.addressline6 in [
             # splits
             "KT16 8QD",
             "KT16 8AG",
-            "KT15 2US",
             "KT15 2DB",
             "KT16 0AB",  # 10 WATERY LANE, CHERTSEY
         ]:
