56ce3d1ef44124e5506413b1685a52941aa47e66
polling-bot-4000 <developers@democracyclub.org.uk>
2024-02-22 11:36:28 +0000

    Import script for Spelthorne (2024-05-02) (closes #6235)

--- polling_stations/apps/data_importers/management/commands/import_spelthorne.py
+++ polling_stations/apps/data_importers/management/commands/import_spelthorne.py
@@ -3,18 +3,39 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "SPE"
-    addresses_name = "2023-05-04/2023-04-13T10:33:05.733273/Eros_SQL_Output009.csv"
-    stations_name = "2023-05-04/2023-04-13T10:33:05.733273/Eros_SQL_Output009.csv"
-    elections = ["2023-05-04"]
+    addresses_name = "2024-05-02/2024-02-22T11:36:05.197338/Eros_SQL_Output011 (2).csv"
+    stations_name = "2024-05-02/2024-02-22T11:36:05.197338/Eros_SQL_Output011 (2).csv"
+    elections = ["2024-05-02"]
 
     def address_record_to_dict(self, record):
+        uprn = record.uprn.strip().lstrip("0")
+
+        if uprn in [
+            "33012923",  # 259 FELTHAM HILL ROAD, ASHFORD
+            "33012909",  # 257 FELTHAM HILL ROAD, ASHFORD
+            "33012894",  # 255 FELTHAM HILL ROAD, ASHFORD
+            "33035052",  # DERRYMORE, WRENS AVENUE, ASHFORD
+            "33049394",  # VALDOR, WRENS AVENUE, ASHFORD
+            "33049393",  # 54A WRENS AVENUE, ASHFORD
+        ]:
+            return None
+
         if record.housepostcode in [
             # split
             "TW15 1AG",
             "TW18 1HE",
             "TW15 3SH",
-            # look wrong
+            # looks wrong
             "TW17 8SY",
+            "TW18 1HD",
+            "TW18 1QP",
+            "TW18 1QW",
+            "TW18 1QL",
+            "TW18 1QN",
+            "TW18 1HH",
+            "TW18 1HG",
+            "TW18 1HQ",
+            "TW18 1HJ",
         ]:
             return None
 
