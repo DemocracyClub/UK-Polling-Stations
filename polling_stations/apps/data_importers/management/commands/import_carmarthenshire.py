e8bf2e4494794cf0c119136c2283c8d5149b51f1
polling-bot-4000 <developers@democracyclub.org.uk>
2024-02-22 09:44:52 +0000

    Import script for Carmarthenshire (2024-05-02) (closes #6228)

--- polling_stations/apps/data_importers/management/commands/import_carmarthenshire.py
+++ polling_stations/apps/data_importers/management/commands/import_carmarthenshire.py
@@ -3,50 +3,55 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "CMN"
-    addresses_name = (
-        "2022-05-05/2022-03-03T09:57:57.949812/polling_station_export-2022-03-03.csv"
-    )
-    stations_name = (
-        "2022-05-05/2022-03-03T09:57:57.949812/polling_station_export-2022-03-03.csv"
-    )
-    elections = ["2022-05-05"]
+    addresses_name = "2024-05-02/2024-02-22T09:38:19.926469/Eros_SQL_Output001.csv"
+    stations_name = "2024-05-02/2024-02-22T09:38:19.926469/Eros_SQL_Output001.csv"
+    elections = ["2024-05-02"]
     csv_encoding = "windows-1252"
 
     def address_record_to_dict(self, record):
+        if record.uprn in [
+            "10004882480",  # PENYRHEOLWEN, TALLEY, LLANDEILO
+            "10009546779",  # PENGOILAN, BETHLEHEM, LLANDEILO
+            "10004853865",  # FLAT 1, 3 STATION ROAD, LLANELLI
+            "10004848463",  # MERRY VALE, HALFPENNY FURZE, LAUGHARNE, CARMARTHEN
+            "10009164875",  # COOMB LODGE, LLANYBRI, CARMARTHEN
+        ]:
+            return None
+
         if record.housepostcode in [
-            "SA33 5QQ",
-            "SA14 9AW",
-            "SA32 7AS",
+            # split
+            "SA18 3DZ",
+            "SA44 5YB",
+            "SA33 5DH",
+            "SA39 9EU",
+            "SA16 0PP",
+            "SA14 8TP",
             "SA34 0HX",
             "SA15 1HP",
-            "SA16 0PP",
-            "SA44 5YB",
-            "SA32 8BX",
-            "SA14 8TP",
-            "SA16 0LE",
-            "SA15 5LP",
-            "SA31 3JJ",
-            "SA14 8BZ",
-            "SA14 8AY",
-            "SA18 3TB",
-            "SA18 2SU",
-            "SA19 8TA",
-            "SA34 0XA",
-            "SA20 0EY",
-            "SA19 8BR",
-            "SA33 6HB",
-            "SA19 7YE",
-            "SA39 9HN",
-            "SA14 8JA",
-            "SA32 7QJ",
-            "SA19 7SG",
             "SA39 9EJ",
+            "SA15 5LP",
+            "SA16 0LE",
+            "SA32 7AS",
+            "SA19 7YE",
+            "SA19 8TA",
+            "SA32 8BX",
+            "SA34 0XA",
+            "SA14 8JA",
+            "SA14 8AY",
+            "SA31 3JJ",
+            "SA32 7QJ",
+            "SA19 8BR",
+            "SA14 8BZ",
+            "SA20 0EY",
+            "SA18 3TB",
             "SA17 5US",
-            "SA17 4NF",
-            "SA33 5DH",
+            "SA19 7SG",
             "SA18 3NB",
+            "SA17 4NF",
             "SA19 9AS",
-            "SA19 7DL",
+            # suspect
+            "SA18 2ET",
+            "SA15 1JE",
         ]:
             return None
 
