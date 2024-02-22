daf4b92b62ccf1bf7192ef742ecf08b7d709ff69
polling-bot-4000 <developers@democracyclub.org.uk>
2024-02-22 15:03:55 +0000

    Import script for Watford (2024-05-02) (closes #6256)

--- polling_stations/apps/data_importers/management/commands/import_watford.py
+++ polling_stations/apps/data_importers/management/commands/import_watford.py
@@ -3,15 +3,20 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "WAT"
-    addresses_name = "2023-05-04/2023-03-09T16:41:25.854790/Eros_SQL_Output071.csv"
-    stations_name = "2023-05-04/2023-03-09T16:41:25.854790/Eros_SQL_Output071.csv"
-    elections = ["2023-05-04"]
+    addresses_name = (
+        "2024-05-02/2024-02-22T15:03:32.581902/Watford BC Democracy Club Data.csv"
+    )
+    stations_name = (
+        "2024-05-02/2024-02-22T15:03:32.581902/Watford BC Democracy Club Data.csv"
+    )
+    elections = ["2024-05-02"]
 
     def address_record_to_dict(self, record):
         if record.housepostcode in [
             "WD25 9AS",
-            "WD25 7DA",
             "WD18 7BS",
+            "WD25 7DA",
         ]:
             return None
+
         return super().address_record_to_dict(record)
