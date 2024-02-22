6189ce0501cde6bfb4dbd702bcc9c8eb28cc7607
polling-bot-4000 <developers@democracyclub.org.uk>
2024-02-22 10:25:49 +0000

    Import script for East Staffordshire (2024-05-02) (closes #6230)

--- polling_stations/apps/data_importers/management/commands/import_east_staffs.py
+++ polling_stations/apps/data_importers/management/commands/import_east_staffs.py
@@ -4,9 +4,76 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "EST"
     addresses_name = (
-        "2023-05-04/2023-04-13T15:18:20.766665/Democracy Club Polling Districts.csv"
+        "2024-05-02/2024-02-22T10:25:21.333127/Democracy Club Polling Districts.csv"
     )
     stations_name = (
-        "2023-05-04/2023-04-13T15:18:20.766665/Democracy Club Polling Stations.csv"
+        "2024-05-02/2024-02-22T10:25:21.333127/Democracy Club Polling Stations.csv"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
+    csv_encoding = "utf-16le"
+
+    def address_record_to_dict(self, record):
+        uprn = record.uprn.strip().lstrip("0")
+
+        if uprn in [
+            "100031648376",  # OUSLEY CROSS FARM, STANTON LANE, ELLASTONE, ASHBOURNE
+            "10008036775",  # THORSWOOD GRANGE, STANTON, ASHBOURNE
+            "10008036394",  # PARKGATE COTTAGE, WOOTTON ROAD, ELLASTONE, ASHBOURNE
+            "10094378229",  # DOVE CROFT BARN, BARROW HILL, ROCESTER, UTTOXETER
+            "100031678921",  # 34 HOLLY ROAD, UTTOXETER
+            "100031678918",  # 32 HOLLY ROAD, UTTOXETER
+            "10008040309",  # NOAHS ARK FARM, BY PASS ROAD, UTTOXETER
+            "100032199973",  # WOODFORD HALL FARM, MOISTY LANE, MARCHINGTON, UTTOXETER
+            "10010540344",  # THE CROFT, HIGHWOOD, UTTOXETER
+            "10090991258",  # LEAFIELDS BARN, LOWER LOXLEY, UTTOXETER
+            "10008040504",  # REDBANK FARM, WOODMILL, YOXALL, BURTON-ON-TRENT
+            "10008039588",  # STONEYFORD FARM, STONEYFORD, BARTON UNDER NEEDWOOD, BURTON-ON-TRENT
+            "10008042769",  # 307 LICHFIELD ROAD, BARTON UNDER NEEDWOOD, BURTON-ON-TRENT
+            "10008042692",  # GREENAWAYS, LICHFIELD ROAD, BRANSTON, BURTON-ON-TRENT
+            "100031666894",  # HENESFIELD FARM, OUTWOODS LANE, BURTON-ON-TRENT
+            "100031664179",  # OUTWOODS HILL FARM, LOWER OUTWOODS ROAD, BURTON-ON-TRENT
+            "10008039970",  # MYRTLE COTTAGE, JARDINES LANE, STUBWOOD, UTTOXETER
+            "100031649648",  # 100 ASHBY ROAD, BURTON-ON-TRENT
+            "100031650804",  # 14 BEARWOOD HILL ROAD, BURTON-ON-TRENT
+            "100031650805",  # 15 BEARWOOD HILL ROAD, BURTON-ON-TRENT
+            "10010540757",  # 4A LODGE HILL, TUTBURY, BURTON-ON-TRENT
+            "10009257620",  # OAKAMORE, ANSLOW ROAD, HANBURY, BURTON-ON-TRENT
+            "10010542140",  # ANFIELD HOUSE FARM, NEW ROAD, UTTOXETER
+            "100031680029",  # PARK VIEW, NEW ROAD, UTTOXETER
+            "100031680026",  # ASHDENE, NEW ROAD, UTTOXETER
+            "100031680035",  # THE HAVEN, NEW ROAD, UTTOXETER
+            "100031678433",  # 1 HAWTHORNDEN AVENUE, UTTOXETER
+            "200001719328",  # SCHOOL HOUSE, STONE ROAD, UTTOXETER
+            "10008037294",  # OAKLANDS, SCOUNSLOW GREEN ROAD, SCOUNSLOW GREEN, UTTOXETER
+            "100032002408",  # THREE LANE END FARM, THORNEY LANES, NEWBOROUGH, BURTON-ON-TRENT
+            "100031679871",  # NEWLANDS COTTAGE FARM, MOISTY LANE, MARCHINGTON, UTTOXETER
+            "10009259655",  # DOVEDALE HOUSE, CALWICH, ASHBOURNE
+            "10009259632",  # WHITEHALL BANK FARM, DRAYCOTT-IN-THE-CLAY, ASHBOURNE
+            "100031679871",  # NEWLANDS COTTAGE FARM, MOISTY LANE, MARCHINGTON, UTTOXETER
+            "100031672646",  # OAKLANDS COTTAGE, THORNEY LANES, NEWBOROUGH, BURTON-ON-TRENT
+            "100031672649",  # TOMLINSONS CORNER FARM, THORNEY LANES, NEWBOROUGH, BURTON-ON-TRENT
+        ]:
+            return None
+
+        if record.postcode in [
+            # splits
+            "DE13 9AZ",
+            "ST14 5DS",  # HAWTHORNE, NEW ROAD, UTTOXETER
+            "ST14 7RB",  # HOLLYSTONE, STONE ROAD, UTTOXETER
+            "DE15 0FD",  # WEST STREET, BURTON-ON-TRENT
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # Shobnall Primary School, Shobnall Road, Burton upon Trent, Staffordshire
+        if record.pollingstationid == "4331":
+            record = record._replace(postcode="DE14 2BB")
+
+        # St Giles Church, Croxden Lane, Croxden ,Uttoxeter, Staffordshire
+        if record.pollingstationid == "4297":
+            record = record._replace(xordinate="406486")
+            record = record._replace(yordinate="339871")
+
+        return super().station_record_to_dict(record)
