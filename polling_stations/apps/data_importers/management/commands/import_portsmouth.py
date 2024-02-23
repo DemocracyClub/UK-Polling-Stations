286713313fc512548a5d893c1667a69c161995a2
polling-bot-4000 <developers@democracyclub.org.uk>
2024-02-23 14:58:41 +0000

    Import script for Portsmouth (2024-05-02) (closes #6274)

--- polling_stations/apps/data_importers/management/commands/import_portsmouth.py
+++ polling_stations/apps/data_importers/management/commands/import_portsmouth.py
@@ -4,12 +4,10 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "POR"
     addresses_name = (
-        "2023-05-04/2023-03-13T12:41:54.727276/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-02-23T14:57:35.343217/DemocracyClub__02May2024.tsv"
     )
-    stations_name = (
-        "2023-05-04/2023-03-13T12:41:54.727276/Democracy_Club__04May2023.tsv"
-    )
-    elections = ["2023-05-04"]
+    stations_name = "2024-05-02/2024-02-23T14:57:35.343217/DemocracyClub__02May2024.tsv"
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -23,24 +21,45 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "1775064482",  # 40 PERONNE ROAD, PORTSMOUTH
             "1775081845",  # 115 VICTORIA ROAD SOUTH, SOUTHSEA
             "1775129057",  # ADMIRAL JELLICOE HOUSE, LOCKSWAY ROAD, SOUTHSEA
+            "1775044838",  # 153 KIRBY ROAD, PORTSMOUTH
+            "1775030023",  # ST, MARYS VICARAGE, FRATTON ROAD, PORTSMOUTH
+            "1775016273",  # FLAT, 146 COMMERCIAL ROAD, PORTSMOUTH
+            "1775020413",  # 37 DELAMERE ROAD, SOUTHSEA
+            "1775117109",  # MANOR HOUSE, MANNERS ROAD, SOUTHSEA
+            "1775122433",  # 1A MANNERS ROAD, SOUTHSEA
+            "1775122434",  # 1B MANNERS ROAD, SOUTHSEA
+            "1775091957",  # 2A ST, EDWARDS ROAD, SOUTHSEA
+            "1775068980",  # MILL COTTAGE, RICHMOND ROAD, SOUTHSEA
+            "1775082102",  # 21 VILLIERS ROAD, SOUTHSEA
+            "1775094668",  # 8 HAMILTON ROAD, SOUTHSEA
+            "1775034486",  # 20 HAMILTON ROAD, SOUTHSEA
+            "1775034488",  # 22 HAMILTON ROAD, SOUTHSEA
+            "1775034484",  # 18 HAMILTON ROAD, SOUTHSEA
         ]:
             return None
 
         if record.addressline6 in [
-            "PO6 4SB",  # SOUTHAMPTON ROAD, PORTSMOUTH
-            "PO6 3LX",  # POLICE RESIDENCES, SOUTHWICK ROAD, COSHAM, PORTSMOUTH
-            "PO3 5NB",  # FLAT 1-3, BUILDBASE, BURRFIELDS ROAD, PORTSMOUTH
+            # looks wrong
+            "PO6 4SB",
+            "PO6 3LX",
+            "PO3 5NB",
+            "PO4 9JF",
+            "PO5 2HH",
         ]:
             return None
 
         return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
-        # Import warnings, but no correction needed:
-
-        # Church of the Ascension, Stubbington Avenue, Portsmouth, PO2 0JG
-        # Howard Road Community Centre, Howard Road, Portsmouth, PO2 9PS
-        # Portacabin on north side of green, Fairfield Square/Hythe Road, Portsmouth, PO6 3JS
+        # Import warnings, but no correction needed (checked with the council):
+        # Portsmouth Methodist Church (Eastney), Highland Road, Southsea, PO4 9NJ
+        # John Pounds Centre, Queen Street, Portsmouth, PO1 3HN
+        # Highslopes Community Centre (6235) is in Fareham Borough Council (FAR) but target council is Portsmouth City Council (POR)
         # St Peter & St Paul Hall, Old Wymering Lane, Wymering, PO6 3NH
+        # Portacabin on north side of green, Fairfield Square/Hythe Road, Portsmouth, PO6 3JS
+
+        # postcode correction for: Church of the Ascension, Stubbington Avenue, Portsmouth, PO2 0JG
+        if record.polling_place_id == "6210":
+            record = record._replace(polling_place_postcode="PO2 0JW")
 
         return super().station_record_to_dict(record)
