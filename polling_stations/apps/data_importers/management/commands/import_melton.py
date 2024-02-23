d0fcd7537ec8a4bf72dbc68e575a7e758be8a11f
polling-bot-4000 <developers@democracyclub.org.uk>
2024-02-23 09:34:00 +0000

    Import script for Melton (2024-05-02) (closes #6268)

--- polling_stations/apps/data_importers/management/commands/import_melton.py
+++ polling_stations/apps/data_importers/management/commands/import_melton.py
@@ -4,18 +4,32 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "MEL"
     addresses_name = (
-        "2023-05-04/2023-05-03T13:04:51.058305/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-02-23T09:32:15.458646/Democracy_Club__02May2024.tsv"
     )
     stations_name = (
-        "2023-05-04/2023-05-03T13:04:51.058305/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-02-23T09:32:15.458646/Democracy_Club__02May2024.tsv"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
+
+    def station_record_to_dict(self, record):
+        # corrections from council:
+        # The Old School, 4 Main Street, Muston, Nottingham
+        if record.polling_place_id == "2694":
+            record = record._replace(
+                polling_place_uprn="100030550160",
+                polling_place_easting="482883",
+                polling_place_northing="337945",
+                polling_place_postcode="NG13 0FB",
+            )
+
+        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
         if uprn in [
-            "10002082195",  # 1 POTTER HILL, NOTTINGHAM ROAD, MELTON MOWBRAY
+            "100032044579",  # GLEBE FARM, MAIN STREET, SAXELBY, MELTON MOWBRAY
+            "200001041532",  # HOMESTEAD COTTAGE, WALTHAM ROAD, THORPE ARNOLD, MELTON MOWBRAY
         ]:
             return None
         if record.addressline6 in [
