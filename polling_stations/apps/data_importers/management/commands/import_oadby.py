ef12f97d0c96273f1b9b6c83c13cfbee9a287a28
polling-bot-4000 <developers@democracyclub.org.uk>
2024-02-23 12:05:19 +0000

    Import script for Oadby & Wigston (2024-05-02) (closes #6272)

--- polling_stations/apps/data_importers/management/commands/import_oadby.py
+++ polling_stations/apps/data_importers/management/commands/import_oadby.py
@@ -3,22 +3,29 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "OAD"
-    addresses_name = "2023-05-04/2023-04-09T10:32:46.735243/Democracy_Club__04May2023 (Rev. 09-04-2023).tsv"
-    stations_name = "2023-05-04/2023-04-09T10:32:46.735243/Democracy_Club__04May2023 (Rev. 09-04-2023).tsv"
-    elections = ["2023-05-04"]
+    addresses_name = "2024-05-02/2024-02-23T12:04:52.815246/Democracy_Club__02May2024 (Rev. 22-02-2024).tsv"
+    stations_name = "2024-05-02/2024-02-23T12:04:52.815246/Democracy_Club__02May2024 (Rev. 22-02-2024).tsv"
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "10010146840",  # 46A LONG STREET, WIGSTON
-        ]:
-            return None
-
-        if record.addressline6 in [
-            # look wrong
-            "LE18 2GQ",
+            "10010146840",  # OADBY LODGE FARMHOUSE, GARTREE ROAD, LEICESTER
+            "100032050960",  # 109 WELFORD ROAD, WIGSTON
+            "100030590752",  # 1 BODMIN AVENUE, WIGSTON
+            "10010149302",  # FLAT 1, MOAT HOUSE, 107 MOAT STREET, WIGSTON
+            "10010149303",  # FLAT 2, MOAT HOUSE, 107 MOAT STREET, WIGSTON
+            "10010149304",  # FLAT 3, MOAT HOUSE, 107 MOAT STREET, WIGSTON
+            "10010149305",  # FLAT 4, MOAT HOUSE, 107 MOAT STREET, WIGSTON
+            "10010149306",  # FLAT 5, MOAT HOUSE, 107 MOAT STREET, WIGSTON
+            "10010149307",  # FLAT 6, MOAT HOUSE, 107 MOAT STREET, WIGSTON
+            "10010149308",  # FLAT 7, MOAT HOUSE, 107 MOAT STREET, WIGSTON
+            "10010149309",  # FLAT 8, MOAT HOUSE, 107 MOAT STREET, WIGSTON
+            "10010149310",  # FLAT 9, MOAT HOUSE, 107 MOAT STREET, WIGSTON
+            "100030601417",  # 1 TRURO DRIVE, WIGSTON
+            "100030589686",  # MELROSE HOUSE, NEWTON LANE, WIGSTON
         ]:
             return None
 
