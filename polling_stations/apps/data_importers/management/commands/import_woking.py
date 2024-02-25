456640b8052fa88a0c09a765a643ad99955bf94a
polling-bot-4000 <developers@democracyclub.org.uk>
2024-02-25 13:25:37 +0000

    Import script for Woking (2024-05-02) (closes #6278)

--- polling_stations/apps/data_importers/management/commands/import_woking.py
+++ polling_stations/apps/data_importers/management/commands/import_woking.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WOI"
     addresses_name = (
-        "2023-05-04/2023-03-05T11:18:57.001716/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-02-25T13:24:35.080186/Democracy_Club__02May2024.tsv - WBC.tsv"
     )
     stations_name = (
-        "2023-05-04/2023-03-05T11:18:57.001716/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-02-25T13:24:35.080186/Democracy_Club__02May2024.tsv - WBC.tsv"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -17,17 +17,28 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
         if uprn in [
             "200000218826",  # THE BUNGALOW DAWNEY HILL, PIRBRIGHT
+            "10002428855",  # FLAT 7, THE OLD BREW HOUSE 130-132, HIGH STREET, OLD WOKING, WOKING
+            "200000216418",  # THORLEY COTTAGE, PYRFORD ROAD, WOKING
+            "100061656800",  # HEDGE COTTAGE, SAUNDERS LANE, WOKING
         ]:
             return None
 
-        if record.addressline6 in ["KT14 6LT", "GU22 8AF"]:
-            return None  # split
+        if record.addressline6 in [
+            # splits
+            "KT14 6LT",
+            "GU22 8AF",
+            "GU21 2NJ",
+            # looks wrong
+            "KT15 3QA",
+            "KT15 3QD",
+        ]:
+            return None
 
         return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
-        # Horsell Evangelical Church High Street Horsell Woking GU21 3SZ
-        if record.polling_place_id == "4943":
+        # postcode correction for: Horsell Evangelical Church High Street Horsell Woking GU21 3SZ
+        if record.polling_place_id == "5299":
             record = record._replace(polling_place_postcode="GU21 4SZ")
 
         return super().station_record_to_dict(record)
