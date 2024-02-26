8dc552977a664e8066bd06438ee87f360bc2bc56
polling-bot-4000 <developers@democracyclub.org.uk>
2024-02-26 16:12:32 +0000

    Import script for St Helens (2024-05-02) (closes #6301)

--- polling_stations/apps/data_importers/management/commands/import_st_helens.py
+++ polling_stations/apps/data_importers/management/commands/import_st_helens.py
@@ -4,10 +4,30 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SHN"
     addresses_name = (
-        "2022-10-20/2022-10-03T17:06:44.959671/Democracy_Club__20October2022.tsv"
+        "2024-05-02/2024-02-26T16:10:35.670143/Democracy_Club__02May2024.tsv"
     )
     stations_name = (
-        "2022-10-20/2022-10-03T17:06:44.959671/Democracy_Club__20October2022.tsv"
+        "2024-05-02/2024-02-26T16:10:35.670143/Democracy_Club__02May2024.tsv"
     )
-    elections = ["2022-10-20"]
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
+
+    def station_record_to_dict(self, record):
+        # Crank Recreation Ground, Crank Hill, Crank, Rainford WA11 7SD
+        if record.polling_place_id == "5261":
+            record = record._replace(
+                polling_place_easting="350545",
+                polling_place_northing="399665",
+            )
+
+        return super().station_record_to_dict(record)
+
+    def address_record_to_dict(self, record):
+        if record.addressline6 in [
+            # split
+            "WA10 1HT",
+            "WA9 3RR",
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
