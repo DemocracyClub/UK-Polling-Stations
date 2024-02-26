08b229dc463f68de8ed67159f734a0748db48da2
polling-bot-4000 <developers@democracyclub.org.uk>
2024-02-26 14:54:48 +0000

    Import script for Teignbridge (2024-05-02) (closes #6292)

--- polling_stations/apps/data_importers/management/commands/import_teignbridge.py
+++ polling_stations/apps/data_importers/management/commands/import_teignbridge.py
@@ -4,47 +4,45 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "TEI"
     addresses_name = (
-        "2023-05-04/2023-03-10T11:50:22.113903/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-02-26T14:53:52.912490/Democracy_Club__02May2024.tsv"
     )
     stations_name = (
-        "2023-05-04/2023-03-10T11:50:22.113903/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-02-26T14:53:52.912490/Democracy_Club__02May2024.tsv"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
 
-    def station_record_to_dict(self, record):
-        # Alice Cross Day Centre
-        if record.polling_place_id == "8273":
-            record = record._replace(polling_place_easting="293930")
-            record = record._replace(polling_place_northing="73047")
-
-        if record.polling_place_id == "8292":
-            record = record._replace(polling_place_postcode="")
-
-        return super().station_record_to_dict(record)
-
-    #
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
             "10032954133",  # BARN COTTAGE, HIGHER BRIMLEY, BOVEY TRACEY, NEWTON ABBOT
             "100041145017",  # GLENDARAGH, EXETER ROAD, DAWLISH
+            "10032961123",  # WEST LODGE HEDGE BARTON, WIDECOMBE-IN-THE-MOOR, NEWTON ABBOT
+            "10032961778",  # TREETOPS, HIGHER ASHTON, EXETER
+            "10032961361",  # HIGHER BRAKE, HAYTOR, NEWTON ABBOT
+            "100040322956",  # BROCKS WAY, GREEN LANE, ILSINGTON, NEWTON ABBOT
         ]:
             return None
 
         if record.addressline6 in [
-            "TQ13 9TR",
+            # split
             "TQ14 8NL",
+            "TQ14 9AZ",
             "TQ13 9YW",
             "TQ14 9LZ",
-            "EX7 9PL",
-            "TQ14 8FW",
             "TQ13 7BU",
+            "EX7 9PL",
             "TQ12 1HR",
-            "TQ14 9AZ",
             "TQ14 9AA",
+            "TQ14 8FW",
             "TQ13 9NW",
+            # suspect
+            "TQ14 9EP",
+            "TQ14 9GZ",
+            "TQ14 8NT",
+            "TQ13 9JA",
+            "TQ13 9EJ",
         ]:
             return None
 
