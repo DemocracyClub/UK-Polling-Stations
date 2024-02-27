32b553d503ca4b4de976018be5446f9ad457ba2e
polling-bot-4000 <developers@democracyclub.org.uk>
2024-02-27 15:05:20 +0000

    Import script for Swansea (2024-05-02) (closes #6324)

--- polling_stations/apps/data_importers/management/commands/import_swansea.py
+++ polling_stations/apps/data_importers/management/commands/import_swansea.py
@@ -3,13 +3,9 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "SWA"
-    addresses_name = (
-        "2022-05-05/2022-03-03T11:41:12.072581/polling_station_export-2022-03-03.csv"
-    )
-    stations_name = (
-        "2022-05-05/2022-03-03T11:41:12.072581/polling_station_export-2022-03-03.csv"
-    )
-    elections = ["2022-05-05"]
+    addresses_name = "2024-05-02/2024-02-27T15:04:31.542588/Eros_SQL_Output008.csv"
+    stations_name = "2024-05-02/2024-02-27T15:04:31.542588/Eros_SQL_Output008.csv"
+    elections = ["2024-05-02"]
     csv_encoding = "windows-1252"
 
     def address_record_to_dict(self, record):
@@ -17,33 +13,23 @@ class Command(BaseHalaroseCsvImporter):
 
         if uprn in [
             "10091617114",  # 5 DUNVANT PARK HOUSES, DUNVANT, SWANSEA, SA2 7SH
-            "10010061246",  # SWANSEA & BRECON DIOCESAN BOARD, 1-3 BEACONS VIEW ROAD, CLASE, SWANSEA, SA6 7HJ
             "10010037747",  # FIRST FLOOR FLAT ABOVE YATES 1-4 CAER STREET, CITY CENTRE, SWANSEA, SA1 3PP
             "10094784277",  # ROOM A 116 OLDWAY CENTRE 39 HIGH STREET, CITY CENTRE, SWANSEA, SA1 1LD
             "100100401496",  # 37A HIGH STREET, GORSEINON, SWANSEA, SA4 4BT
             "10011729995",  # ROSE COTTAGE UNCLASSIFIED SECTION-Y2117, OXWICH, SWANSEA, SA3 1LN
             "10010058515",  # THE LAUNDRY MIDDLETON HALL UNCLASSIFIED SECTION-Y2400, RHOSSILI, SWANSEA,SA3 1PJ
+            "10010046963",  # LLE'R FEDWEN FARM, FELINDRE, SWANSEA
         ]:
             return None
 
         if record.housepostcode in [
-            "SA1 8PN",
-            "SA5 7HY",
-            "SA1 3LQ",
-            "SA1 6NQ",
-            "SA3 1AS",
-            "SA5 7DR",
-            "SA3 1BX",
-            "SA3 4QE",
-            "SA6 6BW",
-            "SA6 5JS",
+            # split
             "SA6 6DS",
-            "SA5 4NN",
-            "SA3 3JS",
-            "SA5 7PH",
+            "SA1 8PN",
+            "SA1 6NQ",
+            "SA5 7HY",
             "SA4 3QX",
-            "SA1 7GE",
-            "SA2 0EU",
+            "SA3 3JS",
         ]:
             return None
 
