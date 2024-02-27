22d12bb57e8676beb0ef0e0484cb93a914b10515
polling-bot-4000 <developers@democracyclub.org.uk>
2024-02-27 18:20:27 +0000

    Import script for Windsor and Maidenhead (2024-05-02) (closes #6332)

--- polling_stations/apps/data_importers/management/commands/import_windsor_maidenhead.py
+++ polling_stations/apps/data_importers/management/commands/import_windsor_maidenhead.py
@@ -4,16 +4,27 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "WNM"
     addresses_name = (
-        "2023-05-04/2023-04-13T15:19:53.305902/democracy club polling districts.csv"
+        "2024-05-02/2024-02-27T18:19:44.691508/Democracy Club - Polling Districts.csv"
     )
     stations_name = (
-        "2023-05-04/2023-04-13T15:19:53.305902/democracy club polling stations.csv"
+        "2024-05-02/2024-02-27T18:19:44.691508/Democracy Club - Polling Stations.csv"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
+    csv_encoding = "utf-16le"
 
     def station_record_to_dict(self, record):
         # Polling station A Mobile Unit at Waitrose Carpark
-        if record.stationcode == "75WSC3":
+        if record.stationcode == "62WSC3":
             record = record._replace(xordinate="", yordinate="")
 
         return super().station_record_to_dict(record)
+
+    def address_record_to_dict(self, record):
+        uprn = record.uprn.strip().lstrip("0")
+
+        if uprn in [
+            "10012304920",  # LONG WALK GATE LODGE, FROGMORE, WINDSOR
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
