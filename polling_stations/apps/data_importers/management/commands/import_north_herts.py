a3f1f45659024a56d60c05c43d1630c975176e78
polling-bot-4000 <developers@democracyclub.org.uk>
2024-02-27 10:52:08 +0000

    Import script for North Hertfordshire (2024-05-02) (closes #6319)

--- polling_stations/apps/data_importers/management/commands/import_north_herts.py
+++ polling_stations/apps/data_importers/management/commands/import_north_herts.py
@@ -4,24 +4,41 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "NHE"
     addresses_name = (
-        "2023-05-04/2023-03-16T12:13:10.397086/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-02-27T10:50:57.357829/Democracy_Club__02May2024.tsv"
     )
     stations_name = (
-        "2023-05-04/2023-03-16T12:13:10.397086/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-02-27T10:50:57.357829/Democracy_Club__02May2024.tsv"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
-        if uprn in []:
+        if uprn in [
+            "10070037178",  # LARK RISE, REDHILL, RUSHDEN, BUNTINGFORD
+            "10023318781",  # OLD WESTMILL FARMHOUSE, WESTMILL LANE, ICKLEFORD, HITCHIN
+            "10070041819",  # 2 RYE END COTTAGES, CODICOTE, HITCHIN
+            "10070041818",  # 1 RYE END COTTAGES, CODICOTE, HITCHIN
+            "10023322537",  # DUCKS NEST COTTAGE, NEWSELLS PARK STUD, BARKWAY, ROYSTON
+            "10023322541",  # GRANGE COTTAGE, NEWSELLS PARK STUD, BARKWAY, ROYSTON
+        ]:
             return None
 
         if record.addressline6 in [
-            # split
+            # splits
             "SG8 8AD",
+            # looks wrong
+            "LU2 8NH",
         ]:
             return None
 
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # postcode correction for: Market Hill Rooms, Fish Hill, Royston, SG8 9JL
+        # Council requested to use below postcode
+        if record.polling_place_id == "9796":
+            record = record._replace(polling_place_postcode="SG8 9LB")
+
+        return super().station_record_to_dict(record)
