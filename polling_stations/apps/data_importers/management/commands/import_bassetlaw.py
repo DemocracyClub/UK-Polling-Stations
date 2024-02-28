a00b7ab1e0092b6400afd3f5e357ddf597846881
polling-bot-4000 <developers@democracyclub.org.uk>
2024-02-28 19:57:56 +0000

    Import script for Bassetlaw (2024-05-02) (closes #6354)

--- polling_stations/apps/data_importers/management/commands/import_bassetlaw.py
+++ polling_stations/apps/data_importers/management/commands/import_bassetlaw.py
@@ -1,46 +1,82 @@
-from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
+from data_importers.management.commands import (
+    BaseXpressDCCsvInconsistentPostcodesImporter,
+)
 from django.contrib.gis.geos import Point
 
 
-class Command(BaseXpressDemocracyClubCsvImporter):
+class Command(BaseXpressDCCsvInconsistentPostcodesImporter):
     council_id = "BAE"
     addresses_name = (
-        "2023-05-04/2023-03-17T15:33:20.577893/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-02-28T19:57:25.344833/Democracy_Club__02May2024.tsv"
     )
     stations_name = (
-        "2023-05-04/2023-03-17T15:33:20.577893/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-02-28T19:57:25.344833/Democracy_Club__02May2024.tsv"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "10090589160",  # ROSE BANK, MAIN STREET, NORTH LEVERTON, RETFORD
+            "100032031084",  # WILLOW LODGE, LEVERTON ROAD, RETFORD
+            "10023264273",  # LOCK KEEPERS COTTAGE, WHARF ROAD, RETFORD
+            "100031268741",  # THE OLD LODGE, BABWORTH, RETFORD
+            "100031268740",  # SWISS COTTAGE, BABWORTH, RETFORD
+            "10013978440",  # STATION HOUSE, FLEDBOROUGH, NEWARK
+            "10013976977",  # DOVER LODGE, DOVER BOTTOM, ELKESLEY, RETFORD
+            "10013976869",  # WEST LODGE, OSBERTON, WORKSOP
+            "10013975796",  # LILAC COTTAGE, OSBERTON, WORKSOP
+            "10013973556",  # WEDDING DRIVE LODGE, WELBECK, WORKSOP
+            "10013978834",  # COLLINGTHWAITE FARM, CUCKNEY, MANSFIELD
+            "100031283284",  # 298 CARLTON ROAD, WORKSOP
+            "100031285616",  # HODSOCK GRANGE, DONCASTER ROAD, LANGOLD, WORKSOP
+            "10023268927",  # LOW FARM, GROVE, RETFORD
+            "10023264575",  # D SKELTON SONS, BISHOPFIELD FARM, SERLBY, DONCASTER
+            "10013975962",  # THE CARAVAN DANESHILL PIGGERIES DANESHILL ROAD, LOUND
+        ]:
+            return None
+
         if record.addressline6 in [
-            "S80 2DS",
-            "S81 7HT",
+            # splits
             "DN22 8AH",
             "S80 1QU",
+            "S80 2DS",
+            # looks wrong
+            "DN11 8GF",
+            "DN22 7SH",
+            "DN22 8BJ",
         ]:
             return None
 
         return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
-        if record.polling_place_id == "12602":  # Carlton Youth Centre
-            record = record._replace(polling_place_postcode="")
-        if record.polling_place_id == "12331":  # Lound Village Hall
+        # postcode correction for: Carlton Youth Centre, Lawn Road, Costhorpe, Worksop, S81 9RJ
+        if record.polling_place_id == "12787":
+            record = record._replace(polling_place_postcode="S81 9LB")
+
+        # added postcode for: Lound Village Hall, Town Street, Lound, Retford, Notts
+        if record.polling_place_id == "12941":
             record = record._replace(polling_place_postcode="DN22 8RX")
-        if record.polling_place_id == "12328":  # Barnby Moor Village Hall
+
+        # added postcode for: Barnby Moor Village Hall, Kennel Drive, Barnby Moor, Retford, Notts
+        if record.polling_place_id == "12939":
             record = record._replace(polling_place_postcode="DN22 8QU")
-        if record.polling_place_id == "12560":  # The Butter Market
-            record = record._replace(polling_place_postcode="DN22 6DB")
-        if record.polling_place_id == "12527":  # Langold Village Hall - Committee Room
-            record = record._replace(polling_place_postcode="S81 9SW")
-        if record.polling_place_id == "12392":  # Harworth & Bircotes Town Hall
+
+        # added postcode for: Harworth & Bircotes Town Hall, Scrooby Road, Harworth, Doncaster
+        if record.polling_place_id == "12892":
             record = record._replace(polling_place_postcode="DN11 8JP")
 
+        # postcode correction for: Walks of Life Museum, 33 Lincoln Road, Tuxford, NG22 OHR
+        if record.polling_place_id == "13126":
+            record = record._replace(polling_place_postcode="NG22 0HR")
+
         rec = super().station_record_to_dict(record)
-        # Manton Parish Hall - correction brought forward from locals.
-        if record.polling_place_id == "12284":
+
+        # correction brought forward from locals: Manton Parish Hall, 2a Cavendish Road, S80 2PG
+        if record.polling_place_id == "13051":
             rec["location"] = Point(-1.1105063, 53.2957291, srid=4326)
 
         return rec
