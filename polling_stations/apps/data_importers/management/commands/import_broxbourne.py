c9c4df5cc59231e553ae734780f817df71e0fe15
polling-bot-4000 <developers@democracyclub.org.uk>
2024-02-28 15:22:36 +0000

    Import script for Broxbourne (2024-05-02) (closes #6348)

--- polling_stations/apps/data_importers/management/commands/import_broxbourne.py
+++ polling_stations/apps/data_importers/management/commands/import_broxbourne.py
@@ -3,43 +3,17 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "BRX"
-    addresses_name = "2023-05-04/2023-04-04T11:13:30.818764/Democracy club - 2nd attempt - polling districts 4-5-2023.csv"
-    stations_name = "2023-05-04/2023-04-04T11:13:30.818764/Democracy Club - 2nd attempt - polling stations 4-5-2023.csv"
-    elections = ["2023-05-04"]
-
-    def station_record_to_dict(self, record):
-        # FAIRFIELDS PRIMARY  SCHOOL, (LITTLE FIELDS PRE SCHOOL)
-        if record.stationcode in ("23", "24"):
-            record = record._replace(xordinate="534213")
-            record = record._replace(yordinate="203580")
-
-        # FLAMSTEAD END HALL, MAYO CLOSE
-        if record.stationcode == "22":
-            record = record._replace(xordinate="535062")
-            record = record._replace(yordinate="203335")
-
-        # FAIRLEY CROSS HALL, ROSEDALE COMMUNITY CHURCH
-        if record.stationcode == "27":
-            record = record._replace(xordinate="534455")
-            record = record._replace(yordinate="203051")
-
-        # HALSEY HALL, TURNERS HILL
-        if record.stationcode in ["16", "17", "17A"]:
-            record = record._replace(xordinate="535906")
-            record = record._replace(yordinate="201922")
-
-        # CHRIST CHURCH, CHURCH HALL, TRINITY LANE, WALTHAM CROSS, EN8 7ED
-        if record.stationcode == "18":
-            record = record._replace(xordinate="535999")
-            record = record._replace(yordinate="201098")
-
-        return super().station_record_to_dict(record)
+    addresses_name = "2024-05-02/2024-02-28T15:22:27.029172/Democracy Club - Polling Districts for 2-5-2024.csv"
+    stations_name = "2024-05-02/2024-02-28T15:22:27.029172/Democracy Club - Polling Stations for 2-5-2024.csv"
+    elections = ["2024-05-02"]
+    csv_encoding = "utf-16le"
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.lstrip("0").strip()
 
         if uprn in [
             "148043016",  # 11 ABINGDON COURT, HIGH STREET, WALTHAM CROSS
+            "148046333",  # KIMBLE, HIGHFIELD STABLES, WHITE STUBBS LANE, BROXBOURNE
         ]:
             return None
 
