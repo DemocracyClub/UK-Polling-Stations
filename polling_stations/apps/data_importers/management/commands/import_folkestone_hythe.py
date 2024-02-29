944235922db75b58d0b895244d97cbe3109fdd84
polling-bot-4000 <developers@democracyclub.org.uk>
2024-02-29 10:38:55 +0000

    Import script for Folkestone & Hythe (2024-05-02) (closes #6361)

--- polling_stations/apps/data_importers/management/commands/import_folkestone_hythe.py
+++ polling_stations/apps/data_importers/management/commands/import_folkestone_hythe.py
@@ -3,9 +3,10 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "SHE"
-    addresses_name = "2023-05-04/2023-04-18T10:57:30.072283/Polling District - Folkestone and Hythe (1).csv"
-    stations_name = "2023-05-04/2023-04-18T10:57:30.072283/Polling Stations - Folkestone and Hythe.csv"
-    elections = ["2023-05-04"]
+    addresses_name = "2024-05-02/2024-02-29T10:38:45.624345/Polling Districts.csv"
+    stations_name = "2024-05-02/2024-02-29T10:38:45.624345/Polling Stations.csv"
+    elections = ["2024-05-02"]
+    csv_encoding = "utf-16le"
 
     def address_record_to_dict(self, record):
         if record.postcode in [
@@ -18,8 +19,15 @@ class Command(BaseDemocracyCountsCsvImporter):
         return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
+        # Amendment from council:
+        # Old: Hawkinge Pavillion and Sports Ground, Pavillion Road, Hawkinge, Kent CT18 7AY
+        # New: Hawkinge Pavilion and Sports Ground, Pavilion Road, Hawkinge, Kent, CT18 7UA
         if record.stationcode == "28":
-            # Hawkinge Pavillion and Sports Ground
-            record = record._replace(xordinate="", yordinate="")
+            record = record._replace(
+                xordinate="621844", yordinate="140755", postcode="CT18 7UA"
+            )
+        # 1st Lyminge Scout Hut, Woodland Road, Lyminge, Kent CT18 8EW
+        if record.stationcode == "42":
+            record = record._replace(xordinate="615894", yordinate="140864")
 
         return super().station_record_to_dict(record)
