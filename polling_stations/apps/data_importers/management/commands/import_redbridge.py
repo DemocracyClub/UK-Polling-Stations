d04fa189533051e9d0938080dc83ce00fd81d5eb
awdem <wdemontmollin@gmail.com>
2024-02-29 16:35:31 +0000

    minor station change from redbridge council

--- polling_stations/apps/data_importers/management/commands/import_redbridge.py
+++ polling_stations/apps/data_importers/management/commands/import_redbridge.py
@@ -11,6 +11,18 @@ class Command(BaseXpressDemocracyClubCsvImporter):
     )
     elections = ["2024-05-02"]
 
+    # change from council:
+    # OLD: All Saints Church Hall, 51 Goodmayes Lane, Ilford, IG3 9SJ
+    # NEW: All Saints Church Hall, 51A Goodmayes Lane, Ilford, IG3 9PB
+    def station_record_to_dict(self, record):
+        if record.polling_place_id == "2801":
+            record = record._replace(
+                polling_place_address_1="51A Goodmayes Lane",
+                polling_place_postcode="IG3 9PB",
+            )
+
+        return super().station_record_to_dict(record)
+
     def address_record_to_dict(self, record):
         if record.addressline6 in [
             # split
