654e2a666f0c0aa51da84cd90440966fd8b9e17a
polling-bot-4000 <developers@democracyclub.org.uk>
2024-02-29 13:56:24 +0000

    Import script for Kingston Upon Thames (2024-05-02) (closes #6364, closes #6365)

--- polling_stations/apps/data_importers/management/commands/import_kingston-upon-thames.py
+++ polling_stations/apps/data_importers/management/commands/import_kingston-upon-thames.py
@@ -3,6 +3,31 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "KTT"
-    addresses_name = "2022-11-10/2022-10-24T10:18:05.344501/RBK_Polling_Districts.csv"
-    stations_name = "2022-11-10/2022-10-24T10:18:05.344501/RBK_Polling_Stations.csv"
-    elections = ["2022-11-10"]
+    addresses_name = (
+        "2024-05-02/2024-03-05T11:00:16.877806/RBK Polling Districts List.csv"
+    )
+    stations_name = (
+        "2024-05-02/2024-03-05T11:00:16.877806/RBK Polling Stations List.csv"
+    )
+    elections = ["2024-05-02"]
+    csv_encoding = "latin-1"
+
+    # Following warnings checked and no correction needed
+    # WARNING: Polling station Oak Hall, Maple Lodge (MPC) is in London Borough of Sutton (STN)
+
+    def address_record_to_dict(self, record):
+        uprn = record.uprn.strip().lstrip("0")
+
+        if uprn in [
+            "128039658",  # FLAT 59 CAMBRIDGE ROAD, KINGSTON UPON THAMES
+            "128039914",  # ANNEXE 58 GLOUCESTER ROAD, KINGSTON UPON THAMES
+            "128038772",  # 58A CLIFTON ROAD, KINGSTON UPON THAMES
+            "128038771",  # 58 CLIFTON ROAD, KINGSTON UPON THAMES
+            "128040253",  # 1 CAMBRIDGE GARDENS, KINGSTON UPON THAMES
+            "128014595",  # 28 PENRHYN ROAD, KINGSTON UPON THAMES
+            "128043396",  # SILVERWOOD HOUSE, GEORGE ROAD, KINGSTON UPON THAMES
+            "128007539",  # MAXGATE, GEORGE ROAD, KINGSTON UPON THAMES
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
