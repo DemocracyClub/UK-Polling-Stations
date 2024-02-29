82b862e2276358edb20d9f90af0bf3fbd96ad132
polling-bot-4000 <developers@democracyclub.org.uk>
2024-02-29 08:23:01 +0000

    Import script for Malvern Hills (2024-05-02) (closes #6356)

--- polling_stations/apps/data_importers/management/commands/import_malvern_hills.py
+++ polling_stations/apps/data_importers/management/commands/import_malvern_hills.py
@@ -4,17 +4,17 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "MAV"
     addresses_name = (
-        "2023-05-04/2023-02-07T10:13:27.529328/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-02-29T08:22:45.481950/Democracy_Club__02May2024 (5).tsv"
     )
     stations_name = (
-        "2023-05-04/2023-02-07T10:13:27.529328/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-02-29T08:22:45.481950/Democracy_Club__02May2024 (5).tsv"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
         # St Mary`s Church Hall Sherrards Green Road Malvern
-        if record.polling_place_id == "13513":
+        if record.polling_place_id == "14431":
             record = record._replace(
                 polling_place_easting="379264", polling_place_northing="246303"
             )
@@ -26,27 +26,25 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
         if uprn in [
             "10093062320",  # LOG CABIN AT 2 STRAWBERRY COTTAGE A443 NEWNHAM BRIDGE, NEWNHAM BRIDGE
-            "10014087730",  # 1 STRAWBERRY COTTAGE, NEWNHAM BRIDGE, TENBURY WELLS
-            "10014087731",  # 2 STRAWBERRY COTTAGE, NEWNHAM BRIDGE, TENBURY WELLS
-            "10014087728",  # KEEPERS COTTAGE, NEWNHAM BRIDGE, TENBURY WELLS
-            "10014088277",  # 2 BRICKYARD COTTAGES, FEATHERBED LANE, NEWNHAM BRIDGE, TENBURY WELLS"
-            "10014088278",  # 1 BRICKYARD COTTAGES, FEATHERBED LANE, NEWNHAM BRIDGE, TENBURY WELLS
             "10014090439",  # BAKERS COTTAGE, WICHENFORD, WORCESTER
             "10024321016",  # THE CARTHOUSE, BIRCHLEY MILL, BOCKLETON ROAD, OLDWOOD, TENBURY WELLS
             "100120595356",  # HUNTERS LODGE, MAIN ROAD, KEMPSEY, WORCESTER
-            "100121268209",  # CHERRY TREE COTTAGE MOSELEY ROAD, HALLOW
-            "10014090474",  # PENTOWAN, WICHENFORD, WORCESTER
+            "200003222513",  # CATTERHALL FARM HOUSE, STOCKS ROAD, ALFRICK, WORCESTER
         ]:
             return None
 
         if record.addressline6 in [
-            "WR1 42WE",
+            # split
             "WR2 4TD",
-            "WR14 4JY",
             "WR2 6RB",
-            "WR6 6BH",
             "WR15 8DP",
-            "WR8 0AN",
+            "WR14 4JY",
+            "WR6 6BH",
+            # suspect
+            "WR2 5RG",
+            "WR2 5GY",
+            "WR2 4FB",
+            "WR14 2WE",
         ]:
             return None
 
