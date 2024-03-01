ea142f4eb0413b48b70c274490ff37da2fca98f0
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-01 11:52:00 +0000

    Import script for Telford and Wrekin (2024-05-02) (closes #6375)

--- polling_stations/apps/data_importers/management/commands/import_telford.py
+++ polling_stations/apps/data_importers/management/commands/import_telford.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "TFW"
     addresses_name = (
-        "2023-05-04/2023-04-20T13:49:05.049834/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-03-01T11:51:31.642592/Democracy_Club__02May2024.tsv"
     )
     stations_name = (
-        "2023-05-04/2023-04-20T13:49:05.049834/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-03-01T11:51:31.642592/Democracy_Club__02May2024.tsv"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -23,19 +23,39 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "452084193",  # MEESE COTTAGE, HOWLE, NEWPORT
             "452063475",  # 10 RUSHMOOR LANE, BRATTON, TELFORD
             "452094687",  # THE RIDGEWAYS, THE HEM, SHIFNAL
+            "452044353",  # 12 WATERLOO ROAD, EDGMOND, NEWPORT
+            "10090446309",  # LAKE VIEW RESIDENTIAL CARE HOME, BROOKSIDE AVENUE, TELFORD
+            "452005400",  # 2 POOL HILL, DAWLEY, TELFORD
+            "452005403",  # 5 POOL HILL, DAWLEY, TELFORD
+            "452043117",  # INGLESIDE, EDGMOND ROAD, NEWPORT
         ]:
             return None
 
         if record.addressline6 in [
             # splits
             "TF10 7ZN",
-            "TF8 7NN",
-            "TF2 8SF",
             "TF10 7RG",
             "TF4 3JZ",
+            "TF2 8SF",
+            "TF4 3AZ",
             "TF2 9HF",
-            "TF3 4LY",  # BEEFEATER EUSTON WAY, TELFORD TOWN CENTRE, TELFORD
+            "TF8 7NN",
+            # looks wrong
+            "TF3 4LY",
         ]:
             return None
 
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # more accurate point for: Donnington Community Hub, St Matthews Road, Donnington Wood, Telford, Shropshire, TF2 7RB
+        if record.polling_place_id == "8782":
+            record = record._replace(polling_place_easting="370966")
+            record = record._replace(polling_place_northing="312997")
+
+        # more accurate point for: Horsehay Village Golf Club, Wellington Road, Horsehay, Telford, TF4 3BT
+        if record.polling_place_id == "8933":
+            record = record._replace(polling_place_easting="366836")
+            record = record._replace(polling_place_northing="307950")
+
+        return super().station_record_to_dict(record)
