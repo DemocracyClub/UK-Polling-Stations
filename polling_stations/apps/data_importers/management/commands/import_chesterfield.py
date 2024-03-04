7e1f4b018cb1f20c71d67bd2f6760c60002927d0
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-04 13:24:37 +0000

    Import script for Chesterfield (2024-05-02) (closes #6398)

--- polling_stations/apps/data_importers/management/commands/import_chesterfield.py
+++ polling_stations/apps/data_importers/management/commands/import_chesterfield.py
@@ -4,37 +4,31 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "CHS"
     addresses_name = (
-        "2023-05-04/2023-04-12T16:11:40.810471/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-03-04T13:24:25.268238/Democracy_Club__02May2024.tsv"
     )
     stations_name = (
-        "2023-05-04/2023-04-12T16:11:40.810471/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-03-04T13:24:25.268238/Democracy_Club__02May2024.tsv"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "74088505",  # 11B NEWBRIDGE LANE, BRIMINGTON
             "74068238",  # THE PADDOCKS, CROW LANE, CHESTERFIELD
             "74006059",  # DOBBIN CLOUGH FARM, CROW LANE, CHESTERFIELD
             "74078771",  # 84 SALTERGATE, CHESTERFIELD
+            "74026116",  # HILL TOP, HALL LANE, STAVELEY, CHESTERFIELD
         ]:
             return None
 
         if record.addressline6 in [
-            # splits
+            # split
             "S40 3LA",
+            # suspect
+            "S40 2AL",
         ]:
             return None
 
         return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # Christ Church Primary School, Tapton View Road, Chesterfield, S41 1JU
-        # Proposed correction: S41 7JU
-        if record.polling_place_id == "6915":
-            record = record._replace(polling_place_postcode="")
-
-        return super().station_record_to_dict(record)
