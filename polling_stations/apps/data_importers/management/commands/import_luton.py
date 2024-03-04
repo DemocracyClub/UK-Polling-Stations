63cf861fe4584bea4bcdcb7676fae802045d06b3
blacktyger <blacktyg3r@gmail.com>
2024-03-04 13:28:27 +0000

    Import script for Luton (2024-05-02) (closes #6334)

--- polling_stations/apps/data_importers/management/commands/import_luton.py
+++ polling_stations/apps/data_importers/management/commands/import_luton.py
@@ -4,9 +4,28 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "LUT"
     addresses_name = (
-        "2023-05-04/2023-04-21T13:28:21.602825/Democracy Club - Polling Districts.csv"
+        "2024-05-02/2024-02-27T18:23:10.452598/Democracy Club - Polling Districts.csv"
     )
     stations_name = (
-        "2023-05-04/2023-04-21T13:28:21.602825/Democracy Club - Polling Stations.csv"
+        "2024-05-02/2024-02-27T18:23:10.452598/Democracy Club - Polling Stations.csv"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
+    csv_encoding = "utf-16le"
+
+    def address_record_to_dict(self, record):
+        uprn = record.uprn.strip().lstrip("0")
+
+        if uprn in [
+            "100080141870",  # 169 DEWSBURY ROAD, LUTON
+            "100080163617",  # 117 NEVILLE ROAD, LUTON
+            "100080124758",  # 166 ALEXANDRA AVENUE, LUTON
+        ]:
+            return None
+
+        if record.postcode in [
+            # looks wrong
+            "LU2 8PE"
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
