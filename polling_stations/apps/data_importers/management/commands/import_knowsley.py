43af66290ecf0a0cb2017fd28271f1d54d0c433a
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-04 15:31:37 +0000

    Import script for Knowsley (2024-05-02) (closes #6402)

--- polling_stations/apps/data_importers/management/commands/import_knowsley.py
+++ polling_stations/apps/data_importers/management/commands/import_knowsley.py
@@ -4,13 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "KWL"
     addresses_name = (
-        "2023-05-04/2023-04-12T12:21:05.053431/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-03-04T15:30:34.030306/Democracy_Club__02May2024.CSV"
     )
     stations_name = (
-        "2023-05-04/2023-04-12T12:21:05.053431/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-03-04T15:30:34.030306/Democracy_Club__02May2024.CSV"
     )
-    elections = ["2023-05-04"]
-    csv_delimiter = "\t"
+    elections = ["2024-05-02"]
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
@@ -21,24 +20,23 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "40003719",  # 121 BEWLEY DRIVE, SOUTHDENE, KIRKBY
             "40036487",  # 134 ROUGHWOOD DRIVE, LIVERPOOL
             "40025501",  # FOREST HOUSE, LIVERPOOL ROAD, PRESCOT
+            "40048270",  # 10 CHURCH STREET, PRESCOT
         ]:
             return None
 
         if record.addressline6 in [
             # splits
-            "L34 1LP",
             "L36 5YR",
+            "L34 1LP",
             "L35 1QN",
-            "L35 5AW",
         ]:
             return None
 
         return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
-        # Stockbridge Village Neighbourhood Centre, The Withens, Stockbridge Village, Knowsley, Merseyside, L28 1SU
-        # Proposed postcode correction: L28 1AB
-        if record.polling_place_id == "5142":
-            record = record._replace(polling_place_postcode="")
+        # postcode correction for: Stockbridge Village Neighbourhood Centre, The Withens, Stockbridge Village, Knowsley, Merseyside, L28 1SU
+        if record.polling_place_id == "5789":
+            record = record._replace(polling_place_postcode="L28 1AB")
 
         return super().station_record_to_dict(record)
