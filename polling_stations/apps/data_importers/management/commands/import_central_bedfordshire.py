ad5f604acbaf6859f8697dd2823e42f0f16b48b7
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-05 14:33:29 +0000

    Import script for Central Bedfordshire (2024-05-02) (closes #6430)

--- polling_stations/apps/data_importers/management/commands/import_central_bedfordshire.py
+++ polling_stations/apps/data_importers/management/commands/import_central_bedfordshire.py
@@ -3,6 +3,19 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "CBF"
-    addresses_name = "2023-10-19/2023-09-18T16:40:13.338932/districts.csv"
-    stations_name = "2023-10-19/2023-09-18T16:40:13.338932/stations.csv"
-    elections = ["2023-10-19"]
+    addresses_name = (
+        "2024-05-02/2024-03-05T14:33:07.893624/Democracy Club - Polling Districts.csv"
+    )
+    stations_name = (
+        "2024-05-02/2024-03-05T14:33:07.893624/Democracy Club - Polling Stations.csv"
+    )
+    elections = ["2024-05-02"]
+    csv_encoding = "utf-16le"
+
+    def address_record_to_dict(self, record):
+        if record.postcode in [
+            "MK17 9QG",  # split
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
