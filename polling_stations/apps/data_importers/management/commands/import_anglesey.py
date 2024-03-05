dd8545d7075e03d3169331eb692a5821719e0388
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-05 14:12:10 +0000

    Import script for Isle of Anglesey (2024-05-02) (closes #6426)

--- polling_stations/apps/data_importers/management/commands/import_anglesey.py
+++ polling_stations/apps/data_importers/management/commands/import_anglesey.py
@@ -3,44 +3,43 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "AGY"
-    addresses_name = (
-        "2022-05-05/2022-03-25T12:55:17.013189/polling_station_export-2022-03-24.csv"
-    )
-    stations_name = (
-        "2022-05-05/2022-03-25T12:55:17.013189/polling_station_export-2022-03-24.csv"
-    )
-    elections = ["2022-05-05"]
+    addresses_name = "2024-05-02/2024-03-05T14:11:42.668428/Eros_SQL_Output001.csv"
+    stations_name = "2024-05-02/2024-03-05T14:11:42.668428/Eros_SQL_Output001.csv"
+    elections = ["2024-05-02"]
 
     def station_record_to_dict(self, record):
+        # NEUADD BENTREF LLANFACHRAETH, LLANFACHRAETH, YNYS MON LL65 2UH
         if record.pollingstationnumber == "21":
-            # NEUADD BENTREF LLANFACHRAETH, LLANFACHRAETH, YNYS MON
-            # Is LL65 2UH; should probably be LL65 4UL, but let's not guess
-            record = record._replace(pollingstationpostcode="")
+            # confirming postcode
+            record = record._replace(pollingstationpostcode="LL65 4UH")
 
+        # YSGOLDY CAPEL M C CARMEL, CARMEL, YNYS MON LL71 8DA
         if record.pollingstationnumber == "20":
-            # YSGOLDY CAPEL M C CARMEL CARMEL
-            # Postcode isn't actually in Carmel, and not within polling area
-            record = record._replace(pollingstationpostcode="")
+            # confirming postcode
+            record = record._replace(pollingstationpostcode="LL71 7DH")
 
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         if record.housepostcode in [
-            "LL65 1NU",
-            "LL65 2ED",
-            "LL72 8LJ",
-            "LL65 1BG",
-            "LL65 2EL",
+            # split
             "LL74 8ST",
+            "LL65 2EL",
+            "LL77 7NW",
         ]:
-            return None  # split
+            return None
 
         uprn = record.uprn.strip().lstrip("0")
 
         if uprn in [
-            "200002650495",  # suspicious distance, overlap
-            "10013457915",  # suspicious distance, overlap
-            "200002650498",  # suspicious distance, overlap; coincident
+            "200002649098",  # 4 TREMWYLFA, CARMEL, LLANNERCH-Y-MEDD
+            "10024357287",  # CHAPEL HOUSE, SOUTH STACK ROAD, HOLYHEAD
+            "10024356434",  # HENLLYS, LLANGAFFO, GAERWEN
+            "10024359360",  # CAE'R WENNOL, LLANGAFFO, GAERWEN
+            "10013463367",  # HARBOUR HOUSE, TRAETH BYCHAN, MARIANGLAS
+            "10013466388",  # OLD GRANARY, PENTRAETH
+            "10013853106",  # PANT Y BWLCH, LLANDDONA, BEAUMARIS
+            "10013457915",  # CASTELL GRUG, SOUTH STACK, HOLYHEAD
         ]:
             return None
         return super().address_record_to_dict(record)
