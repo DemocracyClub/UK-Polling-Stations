e0ae96bdda208058051be0861ca2565f80a34738
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-06 09:36:08 +0000

    Import script for Brent (2024-05-02) (closes #6441)

--- polling_stations/apps/data_importers/management/commands/import_brent.py
+++ polling_stations/apps/data_importers/management/commands/import_brent.py
@@ -4,16 +4,17 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "BEN"
     addresses_name = (
-        "2022-05-05/2022-03-22T16:39:49.511741/Democracy Club - Polling Districts.csv"
+        "2024-05-02/2024-03-06T09:38:20.428353/Polling District  - Democracy Club.csv"
     )
     stations_name = (
-        "2022-05-05/2022-03-22T16:39:49.511741/Democracy Club - Polling Stations.csv"
+        "2024-05-02/2024-03-06T09:38:20.428353/Polling Stations - Democracy Club.csv"
     )
-    elections = ["2022-05-05"]
+    elections = ["2024-05-02"]
+    csv_encoding = "utf-16le"
 
     def station_record_to_dict(self, record):
-        # St Mary's Willesden Parish Church, Neasden Lane, London
-        if record.stationcode == "RW4_1":
+        # point correction for: St Mary's Willesden Parish Church, Neasden Lane, London, NW10 2TS
+        if record.stationcode == "ERW480":
             record = record._replace(yordinate="184797")
             record = record._replace(xordinate="521445")
 
@@ -22,9 +23,18 @@ class Command(BaseDemocracyCountsCsvImporter):
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
         if uprn in [
-            "202228638",  # long way from rest of postcode
-            "202053053",
-            "202053054",
+            "202033643",  # 164 WALM LANE, LONDON
+            "202066948",  # 6 MOUNT PLEASANT, WEMBLEY
+            "202196807",  # FLAT EAST LANE PAVILION EAST LANE, WEMBLEY
+            "202030534",  # 101 WINCHESTER AVENUE, LONDON
+            "202142340",  # 106 NORTON ROAD, WEMBLEY
+            "202208354",  # 108 NORTON ROAD, WEMBLEY
+        ]:
+            return None
+
+        if record.postcode in [
+            # looks wrong
+            "HA0 1WY",
         ]:
             return None
 
