d6374ede2f8e40f5b551b15a3b22e7fbd0c65f23
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-06 20:22:28 +0000

    Import script for Bournemouth, Christchurch & Poole (2024-05-02) (closes #6462)

--- polling_stations/apps/data_importers/management/commands/import_bcp.py
+++ polling_stations/apps/data_importers/management/commands/import_bcp.py
@@ -4,9 +4,37 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "BPC"
     addresses_name = (
-        "2023-06-29/2023-05-25T11:35:14.762046/Democracy_Club__29June2023.CSV"
+        "2024-05-02/2024-03-06T20:20:34.559585/Democracy_Club__02May2024.tsv"
     )
     stations_name = (
-        "2023-06-29/2023-05-25T11:35:14.762046/Democracy_Club__29June2023.CSV"
+        "2024-05-02/2024-03-06T20:20:34.559585/Democracy_Club__02May2024.tsv"
     )
-    elections = ["2023-06-29"]
+    elections = ["2024-05-02"]
+    csv_delimiter = "\t"
+
+    def station_record_to_dict(self, record):
+        # Fix to match the coordinates of Station A
+        # Station B - St. Andrew`s Parish Centre, 123 Shelbourne Road, Bournemouth BH8 8RD
+        if record.polling_place_id == "16806":
+            record = record._replace(
+                polling_place_northing="92720",
+            )
+
+        return super().station_record_to_dict(record)
+
+    def address_record_to_dict(self, record):
+        if record.addressline6 in [
+            # split
+            "BH1 1NF",
+            "BH14 0RD",
+            "BH11 8BA",
+            "BH23 3JJ",
+            "BH1 3EB",
+            "BH7 6LL",
+            "BH6 3LF",
+            "BH5 1DL",
+            "BH10 5JF",
+            "BH6 3NH",
+        ]:
+            return None
+        return super().address_record_to_dict(record)
