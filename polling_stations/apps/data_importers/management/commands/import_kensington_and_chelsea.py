23edce35cd6eab5738463912f80d2a0ce295b076
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-07 14:44:47 +0000

    Import script for Kensington and Chelsea (2024-05-02) (closes #6480)

--- polling_stations/apps/data_importers/management/commands/import_kensington_and_chelsea.py
+++ polling_stations/apps/data_importers/management/commands/import_kensington_and_chelsea.py
@@ -4,19 +4,21 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "KEC"
     addresses_name = (
-        "2022-05-05/2022-03-10T11:16:41.646308/Democracy_Club__05May2022.CSV"
+        "2024-05-02/2024-03-07T14:43:23.540506/Democracy_Club__02May2024.tsv"
     )
     stations_name = (
-        "2022-05-05/2022-03-10T11:16:41.646308/Democracy_Club__05May2022.CSV"
+        "2024-05-02/2024-03-07T14:43:23.540506/Democracy_Club__02May2024.tsv"
     )
-    elections = ["2022-05-05"]
+    elections = ["2024-05-02"]
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         if record.addressline6 in [
-            "SW1X 9SG",
+            # split
             "SW7 4DW",
-            "W11 4JJ",
             "W11 4HD",
+            "W11 4JJ",
+            "SW3 3LA",
         ]:
             return None
 
@@ -24,6 +26,7 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         if uprn in [
             "217113295",  # DOCTORS SURGERY, 12 RADDINGTON ROAD, LONDON
             "217130227",  # BASEMENT AND GROUND FLOOR FLAT 20 TREGUNTER ROAD, LONDON
+            "217016105",  # 4 CLAREVILLE GROVE, LONDON
         ]:
             return None
 
