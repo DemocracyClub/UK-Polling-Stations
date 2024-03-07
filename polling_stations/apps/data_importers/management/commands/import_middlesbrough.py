0e9eb649bbd5a544d0d7d544cb76098498d32dc5
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-07 15:21:16 +0000

    Import script for Middlesbrough (2024-05-02) (closes #6468)

--- polling_stations/apps/data_importers/management/commands/import_middlesbrough.py
+++ polling_stations/apps/data_importers/management/commands/import_middlesbrough.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "MDB"
     addresses_name = (
-        "2023-05-04/2023-03-13T11:24:05.406792/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-03-07T15:17:39.494958/Democracy_Club__02May2024.tsv"
     )
     stations_name = (
-        "2023-05-04/2023-03-13T11:24:05.406792/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-03-07T15:17:39.494958/Democracy_Club__02May2024.tsv"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -18,32 +18,47 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         if uprn in [
             "200001683756",  # 220A LINTHORPE ROAD, MIDDLESBROUGH
             "10023181276",  # THE GRANARY, STAINSBY HALL FARM, THORNABY, STOCKTON-ON-TEES
-            "10023174667",  # FLAT CAPTAIN COOK HOTEL 7-11 DURHAM STREET, MIDDLESBROUGH
             "10023181276",  # FLAT 1 169 BOROUGH ROAD, MIDDLESBROUGH
             "200000259465",  # 220A LINTHORPE ROAD, MIDDLESBROUGH
-            "100110140269",  # 30 THORNFIELD ROAD, MIDDLESBROUGH
-            "100110140271",  # 32 THORNFIELD ROAD, MIDDLESBROUGH
             "100110109926",  # 44 FALKLAND STREET, MIDDLESBROUGH
             "10093978798",  # FLAT 5 73 BOROUGH ROAD, MIDDLESBROUGH
             "10093978797",  # FLAT 4 73 BOROUGH ROAD, MIDDLESBROUGH
             "10023175713",  # FLAT NAVIGATION INN MARSH ROAD, MIDDLESBROUGH
+            "10023176944",  # FLAT 1, 27 BOROUGH ROAD, MIDDLESBROUGH
+            "100110104313",  # 64 CROFT AVENUE, MIDDLESBROUGH
+            "100110120765",  # 29 LEVICK CRESCENT, MIDDLESBROUGH
+            "100110113890",  # 1 GROVE ROAD, MIDDLESBROUGH
+            "100110095666",  # 163 BOROUGH ROAD, MIDDLESBROUGH
+            "100110105569",  # 69 DEEPDALE AVENUE, MIDDLESBROUGH
         ]:
             return None
 
         if record.addressline6 in [
-            "TS5 5EG",  # splits
-            "TS1 3QD",  # VICTORIA ROAD, MIDDLESBROUGH
-            "TS1 4AD",  # 27 BOROUGH ROAD, MIDDLESBROUGH
-            "TS1 4AG",  # 149 LINTHORPE ROAD, MIDDLESBROUGH
+            # splits
+            "TS5 5EG",
+            # looks wrong
+            "TS1 3QD",
+            "TS1 4AG",
+            "TS1 2PX",
         ]:
             return None
 
         return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
-        # Mobile Station - Hemlington Grange Way, Junction of Ravensgill Road, Middlesbrough
-        if record.polling_place_id == "9525":
-            record = record._replace(polling_place_easting="")
-            record = record._replace(polling_place_northing="")
+        # adding point for: Mobile Station - Hemlington Grange Way, Junction of Ravengill Road, Middlesbrough, TS8 9FX
+        if record.polling_place_id == "9713":
+            record = record._replace(polling_place_easting="450225")
+            record = record._replace(polling_place_northing="514104")
+
+        # adding point for: Mobile Station - Medina Gardens, Junction of Medina Gardens and Acklam Road, TS5 8BN
+        if record.polling_place_id == "9694":
+            record = record._replace(polling_place_easting="449116")
+            record = record._replace(polling_place_northing="515625")
+
+        # adding point for: St. Chad`s Church Hall, Keith Road, Middlesbrough, TS5 7QW
+        if record.polling_place_id == "9804":
+            record = record._replace(polling_place_easting="449587")
+            record = record._replace(polling_place_northing="517743")
 
         return super().station_record_to_dict(record)
