4b6e7414e48d1c0f848d8c8c4ad0b41ce600fffb
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-07 09:41:15 +0000

    Import script for Hartlepool (2024-05-02) (closes #6464)

--- polling_stations/apps/data_importers/management/commands/import_hartlepool.py
+++ polling_stations/apps/data_importers/management/commands/import_hartlepool.py
@@ -4,13 +4,10 @@ from django.contrib.gis.geos import Point
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "HPL"
-    addresses_name = (
-        "2023-05-04/2023-03-20T10:49:01.444458/TEST Democracy_Club__04May2023.CSV"
-    )
-    stations_name = (
-        "2023-05-04/2023-03-20T10:49:01.444458/TEST Democracy_Club__04May2023.CSV"
-    )
-    elections = ["2023-05-04"]
+    addresses_name = "2024-05-02/2024-03-07T09:40:56.937784/Democracy_Club__02May2024 - Polling Stations.tsv"
+    stations_name = "2024-05-02/2024-03-07T09:40:56.937784/Democracy_Club__02May2024 - Polling Stations.tsv"
+    elections = ["2024-05-02"]
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
@@ -18,43 +15,45 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         if uprn in [
             "100110020667",  # 76 MIDDLETON ROAD, HARTLEPOOL
             "10090069600",  # THE CARAVAN X-PDI UNIT MIDDLETON ROAD, HARTLEPOOL
-            "10090070990",  # 2 PHILLIPS ROAD, HARTLEPOOL
             "100110786581",  # TUNSTALL HALL FARM, ELWICK ROAD, HARTLEPOOL
             "100110673984",  # ANELVILLE, THROSTON GRANGE LANE, HARTLEPOOL
             "10090073016",  # 319 RABY ROAD, HARTLEPOOL
             "10009715735",  # HOLE HOUSE, WYNYARD, BILLINGHAM
-            "10095352391",  # 17 ASPEN GARDENS, HARTLEPOOL
-            "10095352946",  # 42 REEDMACE GARDENS, HARTLEPOOL
-            "10095353490",  # 107C PARK ROAD, HARTLEPOOL
-            "10090073137",  # 109 GRAYTHORP INDUSTRIAL ESTATE ROAD, HARTLEPOOL
-            "100110673014",  # FULTHORPE COTTAGE, DUCHY ROAD, HARTLEPOOL
-            "100110035228",  # 80 WILTSHIRE WAY, HARTLEPOOL
+            "10095353814",  # 317 BRENDA ROAD, HARTLEPOOL
+            "10090073376",  # 31 RODNEY STREET, HARTLEPOOL
         ]:
             return None
 
         if record.addressline6 in [
             # splits
-            "TS25 4PE",
-            "TS25 5BF",
-            "TS24 9BD",
-            "TS26 0LA",
-            "TS24 0PZ",
-            "TS24 8DD",
-            "TS25 5QB",
-            "TS25 5DY",
             "TS24 8PR",
+            "TS24 9BD",
+            "TS25 5QB",
+            "TS25 5BF",
+            "TS24 8DD",
+            "TS25 5DY",
             "TS24 9SF",
+            "TS24 0PZ",
+            "TS25 4PE",
+            "TS26 0LA",
+            # suspect
             "TS25 1EN",  # CASTLETON ROAD, HARTLEPOOL
+            "TS26 0JD",  # JESMOND ROAD, HARTLEPOOL
         ]:
             return None
 
         return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
-        rec = super().station_record_to_dict(record)
+        # Space To Learn, King Oswy Drive, Hartlepool TS26 0UP
+        if record.polling_place_id == "12460":
+            record._replace(polling_place_postcode="TS24 9PB")
+            return None
 
         # Gillen Arms Public House, Clavering Road, Hartlepool, TS27 3QY
-        if rec["internal_council_id"] == "11874":
+        if record.polling_place_id == "12354":
+            rec = super().station_record_to_dict(record)
             rec["location"] = Point(-1.249549, 54.712337, srid=4326)
+            return rec
 
-        return rec
+        return super().station_record_to_dict(record)
