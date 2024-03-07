cb691039f5811330ab133c60b0f800e75196387b
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-07 17:53:03 +0000

    Import script for Sutton (2024-05-02) (closes #6497)

--- polling_stations/apps/data_importers/management/commands/import_sutton.py
+++ polling_stations/apps/data_importers/management/commands/import_sutton.py
@@ -4,14 +4,16 @@ from django.contrib.gis.geos import Point
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "STN"
-    addresses_name = "2022-05-05/2022-03-08T12:51:22.118970/Democracy Club Data Polling Districts.csv"
-    stations_name = (
-        "2022-05-05/2022-03-08T12:51:22.118970/Democracy Club Data Polling Stations.csv"
+    addresses_name = (
+        "2024-05-02/2024-03-07T17:52:52.063099/Democracy Club Polling Districts.csv"
     )
-    elections = ["2022-05-05"]
+    stations_name = (
+        "2024-05-02/2024-03-07T17:52:52.063099/Democracy Club Polling Stations.csv"
+    )
+    elections = ["2024-05-02"]
+    csv_encoding = "utf-16le"
 
     def station_record_to_dict(self, record):
-        # bug report #6
         # Holy Trinity Church Centre
         if record.stationcode in ["RB/1", "RB/2"]:
             rec = super().station_record_to_dict(record)
@@ -19,9 +21,3 @@ class Command(BaseDemocracyCountsCsvImporter):
             return rec
 
         return super().station_record_to_dict(record)
-
-    def address_record_to_dict(self, record):
-        if record.postcode == "SM6 9BY":
-            return None
-
-        return super().address_record_to_dict(record)
