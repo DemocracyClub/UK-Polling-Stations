b88f63972a89ee25cc822c353c39f005c3c978bd
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-08 13:42:17 +0000

    Import script for South Cambridgeshire (2024-05-02) (closes #6511)

--- polling_stations/apps/data_importers/management/commands/import_south_cambridge.py
+++ polling_stations/apps/data_importers/management/commands/import_south_cambridge.py
@@ -4,10 +4,50 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SCA"
     addresses_name = (
-        "2022-11-03/2022-10-06T09:58:47.244148/Democracy_Club__03November2022.tsv"
+        "2024-05-02/2024-03-08T13:42:07.827447/Democracy_Club__02May2024.tsv"
     )
     stations_name = (
-        "2022-11-03/2022-10-06T09:58:47.244148/Democracy_Club__03November2022.tsv"
+        "2024-05-02/2024-03-08T13:42:07.827447/Democracy_Club__02May2024.tsv"
     )
-    elections = ["2022-11-03"]
+    elections = ["2024-05-02"]
+    csv_encoding = "windows-1252"
     csv_delimiter = "\t"
+
+    # Below warnings checked, no correction needed:
+    # WARNING: Polling station Meadows Community Centre (8932) is in Cambridge City Council (CAB)
+
+    def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "10096204414",  # BEAUFORT, STATION ROAD, LONGSTANTON, CAMBRIDGE
+            "10095469809",  # THE COPPICE CARAVAN, STATION ROAD, LONGSTANTON, CAMBRIDGE
+            "10003191905",  # LITTLE BARHAM HALL, BARTLOW, CAMBRIDGE
+            "10091625172",  # LITTLE BARHAM HALL, BARTLOW, CAMBRIDGE
+            "10033028149",  # NEW BUNGALOW, CHRISHALL GRANGE, HEYDON, ROYSTON
+            "10095469885",  # FLEDGLING BARN, BRIDGEFOOT, BARLEY ROAD, HEYDON, ROYSTON
+            "10033031432",  # GREENGAGE COTTAGE, BURY LANE, MELBOURN, ROYSTON
+            "10096204379",  # HIGH BARN, SHINGAY ROAD, STEEPLE MORDEN, ROYSTON
+            "10008080007",  # OAKDENE, MONKFIELD, BOURN, CAMBRIDGE
+            "10008079271",  # CROWDEAN, CROWDEAN LANE, ELSWORTH, CAMBRIDGE
+            "10003194120",  # THE OAKS, SCHOOL LANE, LOWER CAMBOURNE, CAMBRIDGE
+            "200002748859",  # TWO POTS COTTAGES ST NEOTS ROAD, BOURN
+            "10003185702",  # CHAPELGATE, ST. NEOTS ROAD, DRY DRAYTON, CAMBRIDGE
+            "10095471389",  # FOXHILL SIDE, HINTON WAY, GREAT SHELFORD, CAMBRIDGE
+            "10003195362",  # RIVER COTTAGE, SAWSTON ROAD, STAPLEFORD, CAMBRIDGE
+            "100091416223",  # LORDS BRIDGE STATIONHOUSE, WIMPOLE ROAD, BARTON, CAMBRIDGE
+            "10033031425",  # MOUNT FARMHOUSE MOUNT FARM CHALKY LANE, BABRAHAM
+        ]:
+            return None
+
+        if record.addressline6 in [
+            # splits
+            "CB21 5LF",
+            "SG8 0BD",
+            # looks wrong
+            "SG7 6SD",
+            "SG7 5RW",
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
