9f38fb0cd32297bfc7bdd060a836f82094f80a14
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-08 15:57:16 +0000

    Import script for Isle of Wight (2024-05-02) (closes #6515)

--- polling_stations/apps/data_importers/management/commands/import_isle_of_wight.py
+++ polling_stations/apps/data_importers/management/commands/import_isle_of_wight.py
@@ -3,38 +3,44 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "IOW"
-    addresses_name = "2021-03-10T21:36:45.258417/IOW Democracy_Club__06May2021.tsv"
-    stations_name = "2021-03-10T21:36:45.258417/IOW Democracy_Club__06May2021.tsv"
-    elections = ["2021-05-06"]
-    csv_delimiter = "\t"
+    addresses_name = (
+        "2024-05-02/2024-03-08T15:57:00.210856/Democracy_Club__02May2024.tsv"
+    )
+    stations_name = (
+        "2024-05-02/2024-03-08T15:57:00.210856/Democracy_Club__02May2024.tsv"
+    )
+    elections = ["2024-05-02"]
     csv_encoding = "windows-1252"
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
             "100060763282",  # 28A HIGH STREET, RYDE
+            "10023714741",  # FYNELEA, PRINCES ROAD, FRESHWATER
+            "200001868290",  # THE ORCHARDS, PRINCES ROAD, FRESHWATER
+            "100062441673",  # WHITE COTTAGE, MILITARY ROAD, ATHERFIELD, VENTNOR
+            "200001711156",  # KILN SIDE, NEWPORT ROAD, WHITWELL, VENTNOR
+            "200001659225",  # FERNDALE, NEWPORT ROAD, WHITWELL, VENTNOR
+            "200001659224",  # BEXHILL COTTAGE, NEWPORT ROAD, WHITWELL, VENTNOR
+            "10024249131",  # 34A CASTLE CLOSE, VENTNOR
+            "10024249197",  # BATTS LODGE, BATTS ROAD, SHANKLIN
+            "10003321336",  # BEACH RETREAT, POND LANE, SEAVIEW
+            "10003321335",  # 1 POND LANE, SEAVIEW
+            "100060761402",  # CORNERWAYS, BINSTEAD ROAD, RYDE
+            "10003318523",  # THE FIELD, MAIN ROAD, HAVENSTREET, RYDE
         ]:
             return None
 
         if record.addressline6 in [
-            "PO33 2BP",
+            # splits
             "PO30 5GT",
             "PO36 9NQ",
             "PO30 2DH",
-            "PO34 5AF",
+            # looks wrong
+            "PO31 7HA",
         ]:
             return None
 
         return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # The Annex St Johns Church St. Johns Crescent Sandown PO36 9EQ
-        if record.polling_place_id == "7140":
-            record = record._replace(polling_place_postcode="PO36 8EQ")
-
-        # Chillerton Village Hall Chillerton Newport
-        if record.polling_place_id == "7024":
-            record = record._replace(polling_place_postcode="PO30 3ER")
-
-        return super().station_record_to_dict(record)
