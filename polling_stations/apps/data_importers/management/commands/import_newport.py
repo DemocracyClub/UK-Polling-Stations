9c080584ec7def6c27f02c8a94deae33f56ce55a
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-08 12:48:29 +0000

    Import script for Newport (2024-05-02) (closes #6508)

--- polling_stations/apps/data_importers/management/commands/import_newport.py
+++ polling_stations/apps/data_importers/management/commands/import_newport.py
@@ -4,34 +4,73 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "NWP"
     addresses_name = (
-        "2022-05-05/2022-04-05T12:34:16.135232/Democracy_Club__05May2022.tsv"
+        "2024-05-02/2024-03-08T12:48:14.169180/Democracy_Club__02May2024.tsv"
     )
     stations_name = (
-        "2022-05-05/2022-04-05T12:34:16.135232/Democracy_Club__05May2022.tsv"
+        "2024-05-02/2024-03-08T12:48:14.169180/Democracy_Club__02May2024.tsv"
     )
-    elections = ["2022-05-05"]
+    elections = ["2024-05-02"]
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
-        if uprn in ["10094537748"]:
+        if uprn in [
+            "10093295441",  # TY TERNION, LODGE ROAD, CAERLEON, NEWPORT
+            "10002155274",  # ORCHARD FARM, ST. BRIDES WENTLOOGE, NEWPORT
+            "10002155651",  # BLUEBELL COTTAGE, ST. BRIDES WENTLOOGE, NEWPORT
+            "100100646081",  # GREENFIELD HOUSE, TYLA LANE, OLD ST. MELLONS, CARDIFF
+            "100100646082",  # GEM COTTAGE, TYLA LANE, OLD ST. MELLONS, CARDIFF
+            "10002147773",  # WERN FARM, RHIWDERIN, NEWPORT
+            "10014126660",  # MAISONETTE SECOND AND THIRD FLOOR 46 COMMERCIAL STREET, NEWPORT
+            "10090277060",  # 6C TURNER STREET, NEWPORT
+            "10090277061",  # 6B TURNER STREET, NEWPORT
+            "10090277062",  # 6A TURNER STREET, NEWPORT
+            "10010553788",  # 39 REMBRANDT WAY, NEWPORT
+            "10002153590",  # RED ROBIN HOUSE, LLANDEVAUD, NEWPORT
+            "10002153795",  # WOODLAND VIEW, PENHOW, CALDICOT
+            "100100668792",  # YEW TREE COTTAGE, HENDREW LANE, LLANDEVAUD, NEWPORT
+            "10002153732",  # SEYMOUR LODGE, PENHOW, CALDICOT
+            "10002153780",  # HOLLY TREE COTTAGE, PENHOW, CALDICOT
+            "10002153746",  # SEYMOUR COTTAGE, PENHOW, CALDICOT
+            "10002153749",  # GROES WEN COTTAGE, PENHOW, CALDICOT
+            "10014125572",  # MANAGERS ACCOMMODATION GROES WEN INN CHEPSTOW ROAD, NEWPORT
+        ]:
             return None
 
         if record.post_code in [
-            "NP19 9BX",
-            "NP10 8NT",
+            "NP10 8NT",  # split
+            # suspect
             "NP10 8AT",
-            "NP26 3AD",
+            "NP20 1LQ",
+            "NP20 1HY",
+            "NP20 7DY",
+            "NP19 8AY",
         ]:
             return None
 
         return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
-        # East Hub 282 Ringland Circle Newport
-        if record.polling_place_id == "12591":
+        # Maesglas Community Centre, Bideford Road, Newport
+        if record.polling_place_id == "13060":
+            record = record._replace(polling_place_postcode="NP20 3XT")
+
+        # East Hub, 282 Ringland Circle, Newport
+        if record.polling_place_id == "13173":
             record = record._replace(polling_place_postcode="NP19 9PS")
+
+        # Shaftesbury Community Centre, Side entrance off Hoskins Street, Newport
+        if record.polling_place_id == "13343":
+            record = record._replace(polling_place_postcode="NP20 5LD")
+
+        # Just Dance, Locke Street Community Centre, 8 Queens Hill, Newport, NP20 5HJ
+        if record.polling_place_id == "13269":
+            record = record._replace(polling_place_postcode="NP20 5HL")
+
+        # Maindee Library, 79 Chepstow Road, Newport, NP18 8BY
+        if record.polling_place_id == "13243":
+            record = record._replace(polling_place_postcode="NP19 8BY")
 
         return super().station_record_to_dict(record)
