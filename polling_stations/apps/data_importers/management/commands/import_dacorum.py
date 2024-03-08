a618b122acb026923d3395f6473c5f5ec454376b
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-08 09:14:45 +0000

    Import script for Dacorum (2024-05-02) (closes #6503)

--- polling_stations/apps/data_importers/management/commands/import_dacorum.py
+++ polling_stations/apps/data_importers/management/commands/import_dacorum.py
@@ -4,35 +4,57 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "DAC"
     addresses_name = (
-        "2023-05-04/2023-03-10T12:47:03.450416/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-03-08T09:14:29.199753/Democracy_Club__02May2024 (13).tsv"
     )
     stations_name = (
-        "2023-05-04/2023-03-10T12:47:03.450416/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-03-08T09:14:29.199753/Democracy_Club__02May2024 (13).tsv"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
-        # Nash Mills Village Hall
-        if record.polling_place_id == "2277":
+        # more accurate point for: Nash Mills Village Hall, 4 Lower Road, Nash Mills, HP3 8RU
+        if record.polling_place_id == "2764":
             record = record._replace(polling_place_easting="507211")
             record = record._replace(polling_place_northing="204366")
+
+        # address correction for: St Pauls Church Hall, 39 Meadow Road, Hemel Hempstead, HP3 8AJ
+        if record.polling_place_id == "2675":
+            record = record._replace(polling_place_name="St Paul's Church")
+            record = record._replace(polling_place_address_1="Solway")
+            record = record._replace(polling_place_address_4="Hemel Hempstead")
+            record = record._replace(polling_place_postcode="HP2 5QN")
 
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
         uprn = record.property_urn.strip().lstrip("0")
 
-        if uprn in []:
+        if uprn in [
+            "200001852107",  # SEBRIGHT FARM, CLEMENTS END ROAD, GADDESDEN ROW, HEMEL HEMPSTEAD
+            "100081181210",  # HOLLOWAY COTTAGE, HASTOE, TRING
+            "10095348524",  # LAKE VIEW, PIX FARM LANE, HEMEL HEMPSTEAD
+            "10093312995",  # 1 LOCKERS PARK LANE, HEMEL HEMPSTEAD
+            "100080710707",  # 8 WOODFIELD GARDENS, HEMEL HEMPSTEAD
+            "100080710712",  # 14 WOODFIELD GARDENS, HEMEL HEMPSTEAD
+            "200004054571",  # FLAT THE YOUNG PRETENDER 37 HEMPSTEAD ROAD, KINGS LANGLEY
+            "100081111251",  # WOODILL FARM, NETTLEDEN ROAD, LITTLE GADDESDEN, BERKHAMSTED
+            "200001052307",  # THE COTTAGE, BULLBEGGARS LANE, BERKHAMSTED
+            "100081113670",  # OLD FISHERY COTTAGE, OLD FISHERY LANE, HEMEL HEMPSTEAD
+            "100081113665",  # BARGEMOOR, OLD FISHERY LANE, HEMEL HEMPSTEAD
+            "100081113665",  # BARGEMOOR, OLD FISHERY LANE, HEMEL HEMPSTEAD
+        ]:
             return None
 
         if record.addressline6 in [
-            # split
+            # splits
             "HP2 4AP",
             "HP2 6JN",
-            # wrong
-            "HP3 9GT",
+            # looks wrong
+            "HP3 9DJ",
+            "HP1 2RE",
+            "HP23 5BE",
         ]:
             return None
-        return rec
+
+        return super().address_record_to_dict(record)
