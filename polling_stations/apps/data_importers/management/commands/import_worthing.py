b1ff88d8e86f2b08287f63420934e05e1744de50
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-11 13:15:53 +0000

    Import script for Worthing (2024-05-02) (closes #6533)

--- polling_stations/apps/data_importers/management/commands/import_worthing.py
+++ polling_stations/apps/data_importers/management/commands/import_worthing.py
@@ -4,10 +4,10 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WOT"
     addresses_name = (
-        "2023-05-04/2023-03-09T20:45:06.092674/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-03-11T13:15:37.268460/Democracy_Club__02May2024.tsv"
     )
     stations_name = (
-        "2023-05-04/2023-03-09T20:45:06.092674/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-03-11T13:15:37.268460/Democracy_Club__02May2024.tsv"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
