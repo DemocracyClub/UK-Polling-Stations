626d3c984f8dd67a92ef145f19acc9152c3ba2da
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-11 11:14:25 +0000

    Import script for South Derbyshire (2024-05-02) (closes #6529)

--- polling_stations/apps/data_importers/management/commands/import_south_derbyshire.py
+++ polling_stations/apps/data_importers/management/commands/import_south_derbyshire.py
@@ -4,35 +4,26 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SDE"
     addresses_name = (
-        "2023-05-04/2023-04-20T10:35:14.553119/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-03-11T11:14:15.537926/Democracy_Club__02May2024.tsv"
     )
     stations_name = (
-        "2023-05-04/2023-04-20T10:35:14.553119/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-03-11T11:14:15.537926/Democracy_Club__02May2024.tsv"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
+
+    # Checked and no action required, it is a mobile station close to the council border
+    # WARNING: Polling station Mobile Unit (8706) is in Derby City Council (DER)
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
             "10091583505",  # LONG MEADOW, GRANGEWOOD, NETHERSEAL, SWADLINCOTE
-            "10023236472",  # COPSWOOD, GRANGEWOOD, NETHERSEAL, SWADLINCOTE
-            "200003151264",  # WILLOW COTTAGE GRANGEWOOD, GRANGEWOOD, SWADLINCOTE
-            "10094711751",  # 40 APPLEBY CLOSE, LITTLEOVER, DERBY
-            "10094711760",  # 25 APPLEBY CLOSE, LITTLEOVER, DERBY
             "10091583496",  # SALT COTTAGE HEATH LANE, BOUNDARY, SWADLINCOTE
             "10091581856",  # 49 ASHBY ROAD, WOODVILLE, SWADLINCOTE
             "10091581857",  # 51 ASHBY ROAD, WOODVILLE, SWADLINCOTE
             "200001489411",  # STABLE LODGE, TWYFORD ROAD, BARROW-ON-TRENT, DERBY
-            "10091582711",  # 1 SANDGATE ROAD, CHELLASTON, DERBY
-            "10091582715",  # 5 SANDGATE ROAD, CHELLASTON, DERBY
-            "10091582720",  # 11 SANDGATE ROAD, CHELLASTON, DERBY
-            "10091582712",  # 2 SANDGATE ROAD, CHELLASTON, DERBY
-            "10091582714",  # 4 SANDGATE ROAD, CHELLASTON, DERBY
-            "10091582716",  # 6 SANDGATE ROAD, CHELLASTON, DERBY
-            "10091582718",  # 8 SANDGATE ROAD, CHELLASTON, DERBY
-            "10012010331",  # BONDWOOD BARN UNNAMED ROAD FROM BROOK STREET AND GREYSICH LANE TO FAIRVIEW FARM
             "100032026593",  # WATERWORKS HOUSE, DERBY ROAD, STANTON-BY-BRIDGE, DERBY
             "100030238970",  # 1 MARSTON LANE, HATTON, DERBY
             "100030231955",  # 2 NEWTON MOUNT COTTAGES, BRETBY LANE, BRETBY, BURTON-ON-TRENT
@@ -40,21 +31,40 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "10000820723",  # SALTERSFORD HOUSE, EGGINTON, DERBY
             "200003154106",  # BIRCHTREES FARM, EGGINTON, DERBY
             "10094713650",  # 46 SWARTLING DRIVE, WOODVILLE, SWADLINCOTE
+            "10024227070",  # 8 BURTON ROAD, OVERSEAL, SWADLINCOTE
+            "200003152322",  # MUSE LANE FARM, MUSE LANE, BOYLESTONE, ASHBOURNE
+            "10013902860",  # FLAT 222 RYKNELD ROAD, FINDERN, DERBY
+            "200003148307",  # ROYDON HALL COTTAGE CANAL BANK, SHARDLOW, DERBY
+            "100030233812",  # BROADSTONE, BROADSTONE LANE, TICKNALL, DERBY
+            "100030233814",  # STAUNTON VIEW, BROADSTONE LANE, TICKNALL, DERBY
+            "100030233813",  # BROAD STONE END, BROADSTONE LANE, TICKNALL, DERBY
+            "10023236230",  # 211 ASHBY ROAD, WOODVILLE, SWADLINCOTE
+            "100030244640",  # 207 ASHBY ROAD, WOODVILLE, SWADLINCOTE
+            "100032000594",  # 483 BURTON ROAD, MIDWAY, SWADLINCOTE
+            "10094715060",  # 194 HIGH STREET, NEWHALL, SWADLINCOTE
+            "10094715065",  # 204 HIGH STREET, NEWHALL, SWADLINCOTE
+            "100030232079",  # 213 BRETBY LANE, BRETBY, BURTON-ON-TRENT
+            "100030232080",  # 215 BRETBY LANE, BRETBY, BURTON-ON-TRENT
         ]:
             return None
 
         if record.addressline6 in [
             # splits
             "DE65 6LE",
+            "DE11 9HA",
             "DE11 0DQ",
-            "DE3 0AX",  # KINGLAS DRIVE, MICKLEOVER, DERBY
-            "DE6 5JF",  # SUTTON-ON-THE-HILL, ASHBOURNE
+            # looks wrong
+            "DE11 7FL",
+            "DE11 9NT",
+            "DE6 5JF",
+            # reported to council, they will review after the elections in May '24
+            "DE73 8JA",
+            "DE24 5BL",
+            "DE24 5BB",
+            "DE24 5AR",
+            "DE24 5BL",
+            "DE24 5AW",
         ]:
             return None
 
         return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # WARNING: Polling station Mobile Unit (6211) is in Derby City Council (DER)
-        # Checked and no action required, it is a mobile station close to the council border
-        return super().station_record_to_dict(record)
