dd0fe1a40d8ae1701024922b4d0647fa5a4fb42f
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-11 10:31:46 +0000

    Import script for North Tyneside (2024-05-02) (closes #6522)

--- polling_stations/apps/data_importers/management/commands/import_north_tyneside.py
+++ polling_stations/apps/data_importers/management/commands/import_north_tyneside.py
@@ -4,23 +4,51 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "NTY"
     addresses_name = (
-        "2023-05-04/2023-03-10T12:13:25.758626/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-03-11T10:30:17.403600/Democracy_Club__02May2024.tsv"
     )
     stations_name = (
-        "2023-05-04/2023-03-10T12:13:25.758626/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-03-11T10:30:17.403600/Democracy_Club__02May2024.tsv"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "47243589",  # 14 BEECH WAY, KILLINGWORTH
+            "47070238",  # EAST WIDEOPEN FARM COTTAGE, WIDEOPEN, NEWCASTLE UPON TYNE
+            "47241411",  # OAK HOUSE, WIDEOPEN, NEWCASTLE UPON TYNE
+            "47072178",  # FLAT ABOVE THE FOXHUNTERS PUBLIC HOUSE PRESTON NORTH ROAD, NORTH SHIELDS
+            "47072124",  # CRICKET CLUB COTTAGE, PRESTON AVENUE, NORTH SHIELDS
+            "47014289",  # FLAT ABOVE LOW LIGHTS TAVERN BREWHOUSE BANK, NORTH SHIELDS
+            "47049172",  # 83 HOWDON ROAD, NORTH SHIELDS
+            "47049170",  # 81 HOWDON ROAD, NORTH SHIELDS
+            "47049168",  # 79 HOWDON ROAD, NORTH SHIELDS
+            "47084844",  # FLAT ABOVE STATION HOTEL STATION ROAD, KILLINGWORTH
+            "47229242",  # REDESDALE COURT, RAKE LANE, NORTH SHIELDS
+            "47072123",  # PERCY PARK BUNGALOW, PRESTON AVENUE, NORTH SHIELDS
+        ]:
+            return None
+
         if record.addressline6 in [
-            # SPLIT
+            # splits
             "NE12 8EE",
-            # not sure
-            "NE27 0XP",
-            "NE13 6DX",
+            # looks wrong
+            "NE26 1LX",
+            "NE28 9UR",
+            "NE28 9UT",
+            "NE13 6EN",
+            "NE30 2ET",
         ]:
             return None
 
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # postcode correction for: Fernedene, Threap Gardens, Howdon, NE28 6SL
+        if record.polling_place_id == "2825":
+            record = record._replace(polling_place_postcode="NE28 7HT")
+
+        return super().station_record_to_dict(record)
