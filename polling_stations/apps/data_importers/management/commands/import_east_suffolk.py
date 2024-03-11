0a499aa60947a894113b1b711d77ee8f96544ae0
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-11 11:06:35 +0000

    Import script for East Suffolk (2024-05-02) (closes #6526)

--- polling_stations/apps/data_importers/management/commands/import_east_suffolk.py
+++ polling_stations/apps/data_importers/management/commands/import_east_suffolk.py
@@ -3,38 +3,23 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "ESK"
-    addresses_name = "2023-05-04/2023-04-26T20:46:56.038428/East Suffolk Council - Democracy Club - Polling Districts May 2023.csv"
-    stations_name = "2023-05-04/2023-04-26T20:46:56.038428/East Suffolk Council - Democracy Club - Polling Stations May 2023.csv"
-    elections = ["2023-05-04"]
-
-    def station_record_to_dict(self, record):
-        # Loads of dodgy points
-        if record.stationcode in [
-            "S110",  # Dallinghoo Jubilee Hall
-            "S138",  # Bruisyard Village Hall
-            "S196",  # Fred Reynolds Centre
-            "S195",  # Avenue Evangelical Church Hall
-            "S159",  # Burness Parish Room
-            "S184",  # Colneis Division Guiding HQ
-            "S182",  # Christ Church
-            "S187",  # Campsea Ashe Victory Hall
-            "S160",  # Burness Parish Room
-            "S141",  # Cransford Village Hall
-            "S147",  # Great Glemham Village Hall
-            "S109",  # Clopton Village Hall
-            "S165",  # Kirton Church Hall
-            "N69",  # Linstead Village Hall
-        ]:
-            record = record._replace(xordinate="", yordinate="")
-
-        # Council said get rid of all of them...
-        # List above left in for future reference
-        record = record._replace(xordinate="", yordinate="")
-        return super().station_record_to_dict(record)
+    addresses_name = "2024-05-02/2024-03-11T11:06:27.248527/Democracy Club - Polling Districts - East Suffolk - 2 May 2024.csv"
+    stations_name = "2024-05-02/2024-03-11T11:06:27.248527/Democracy Club - Polling Stations - East Suffolk - 2 May 2024.csv"
+    elections = ["2024-05-02"]
+    csv_encoding = "utf-16le"
 
     def address_record_to_dict(self, record):
+        uprn = record.uprn.strip().lstrip("0")
+
+        if uprn in [
+            "100091154936",  # 6 THE PIGHTLE, NORTH COVE, BECCLES
+            "100091487163",  # ISLAND COTTAGE, THE STREET, HACHESTON, WOODBRIDGE
+            "100091638545",  # HEATH COTTAGE, FOXHALL, IPSWICH
+        ]:
+            return None
         if record.postcode in [
             "NR35 1BZ",  # split
+            "NR34 8ET",  # weird
         ]:
             return None
 
