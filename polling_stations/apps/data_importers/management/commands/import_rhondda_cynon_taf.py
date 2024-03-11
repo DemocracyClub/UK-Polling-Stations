8d62b23eabb667b5df8f25b6f18ac7ca207e0381
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-11 10:56:33 +0000

    Import script for Rhondda Cynon Taf (2024-05-02) (closes #6524)

--- polling_stations/apps/data_importers/management/commands/import_rhondda_cynon_taf.py
+++ polling_stations/apps/data_importers/management/commands/import_rhondda_cynon_taf.py
@@ -3,57 +3,66 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "RCT"
-    addresses_name = (
-        "2022-11-17/2022-11-03T10:02:02.538154/polling_station_export-2022-11-03.csv"
-    )
-    stations_name = (
-        "2022-11-17/2022-11-03T10:02:02.538154/polling_station_export-2022-11-03.csv"
-    )
-    elections = ["2022-11-17"]
+    addresses_name = "2024-05-02/2024-03-11T10:56:12.180124/Eros_SQL_Output004.csv"
+    stations_name = "2024-05-02/2024-03-11T10:56:12.180124/Eros_SQL_Output004.csv"
+    elections = ["2024-05-02"]
+    csv_encoding = "windows-1252"
 
     def station_record_to_dict(self, record):
-        if record.pollingstationnumber == "124":
-            # ST DAVIDS CHURCH, LLANTRISANT ROAD, GROESFAEN, PONTYCLUN
-            # "CF72 8NU" → "CF72 8NS"
-            # Source: https://www.churchinwales.org.uk/en/structure/church/4123/
+        # postcode correction for: ST DAVIDS CHURCH, LLANTRISANT ROAD, GROESFAEN, PONTYCLUN, CF72 8NU
+        if record.pollingstationnumber == "123":
             record = record._replace(pollingstationpostcode="CF72 8NS")
 
+        # postcode correction for: CILFYNYDD AND NORTON BRIDGE COMMUNITY CENTRE, CILFYNYDD ROAD, CILFYNYDD, PONTYPRIDD, CF37 3NR
         if record.pollingstationnumber == "97":
-            #     # CILFYNYDD & NORTON BRIDGE COMMUNITY CENTRE, CILFYNYDD ROAD, …
-            #     # Was "CF37 3NR", but that's not on Cilfyndd Road
-            record = record._replace(pollingstationpostcode="")
+            record = record._replace(pollingstationpostcode="CF37 4NR")
+
+        # postcode correction for: ST LUKES CHURCH, CARDIFF ROAD, HAWTHORN, PONTYPRIDD, CF37 5LG
+        if record.pollingstationnumber == "113":
+            record = record._replace(pollingstationpostcode="CF37 5LN")
+
+        # adding postcode for: RHYDYFELIN LIBRARY, POPLAR ROAD, RHYDYFELIN, PONTYPRIDD
+        if record.pollingstationnumber == "111":
+            record = record._replace(pollingstationpostcode="CF37 5LR")
 
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-        if record.housepostcode in [
-            "CF37 3BS",
-            "CF37 1TG",
-            "CF39 8FA",
-            "CF38 1DR",
-            "CF37 1LD",
-            "CF44 9DT",
-            "CF38 2HH",
-            "CF37 4DX",
-            "CF38 2JF",
-            "CF37 3EW",
-            "CF42 6LX",
-            "CF42 6BH",
-            "CF40 2SN",
-            "CF38 2JZ",
-            "CF40 2ER",
-            "CF38 2SA",
-            "CF44 0PD",
-            "CF39 8AT",
-            "CF44 9TB",
-            "CF72 9JZ",
-            "CF37 1UA",
+        uprn = record.uprn.strip().lstrip("0")
+
+        if uprn in [
+            "10090841724",  # THE STABLES MILL STREET, PONTYPRIDD
+            "10024274643",  # 15 CLIFF TERRACE, TREFOREST, PONTYPRIDD
+            "10094101639",  # 30 ILAN AVENUE, PONTYPRIDD
+            "10094101649",  # 40 ILAN AVENUE, PONTYPRIDD
+            "10001300469",  # 34 CAPEL FARM, TONYREFAIL, PORTH
+            "200001640536",  # SAWMILLS HOUSE, LLANTRISANT, PONTYCLUN
+            "10012770573",  # THE CROFT, LLANTRISANT, PONTYCLUN
+            "10090596281",  # THE OLD LAMB & FLAG, LLANTRISANT, PONTYCLUN
+            "10090596664",  # FARMHOUSE CASTELLAU FARM ROAD TO CASTELLAU FAWR FARM, CASTELLAU, BEDDAU
+            "200003777546",  # SIGNALMANS COTTAGE, ELY VALLEY ROAD, YNYSMAERDY, PONTYCLUN
+            "10090596707",  # FARMHOUSE FFORCH ORKY FARM CEMETERY ROAD, TREORCHY
         ]:
             return None
 
-        if record.uprn in [
-            "10001300469",
-            "10094101040",
+        if record.housepostcode in [
+            # splits
+            "CF44 0PD",
+            "CF38 1DR",
+            "CF37 3BS",
+            "CF42 6BH",
+            "CF44 9TB",
+            "CF39 8FA",
+            "CF40 2ER",
+            "CF37 1UA",
+            "CF37 2HL",
+            "CF37 4DX",
+            "CF39 8AT",
+            "CF38 2JZ",
+            "CF44 9DT",
+            # looks wrong
+            "CF37 4BP",
+            "CF37 1PS",
         ]:
             return None
 
