f7056daa1706d089b332e8877d97444ad4b695db
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-11 16:42:02 +0000

    Import script for Stroud (2024-05-02) (closes #6540)

--- polling_stations/apps/data_importers/management/commands/import_stroud.py
+++ polling_stations/apps/data_importers/management/commands/import_stroud.py
@@ -3,32 +3,36 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "STO"
-    addresses_name = "2021-04-19T13:57:28.472090/Democracy_Club__06May2021.CSV"
-    stations_name = "2021-04-19T13:57:28.472090/Democracy_Club__06May2021.CSV"
-    elections = ["2021-05-06"]
-    csv_delimiter = ","
+    addresses_name = (
+        "2024-05-02/2024-03-11T16:41:31.315330/Democracy_Club__02May2024.tsv"
+    )
+    stations_name = (
+        "2024-05-02/2024-03-11T16:41:31.315330/Democracy_Club__02May2024.tsv"
+    )
+    elections = ["2024-05-02"]
     csv_encoding = "windows-1252"
+    csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
-        # Arthur S Winterbotham Memorial Hall High Street Cam
-        if record.polling_place_id == "17930":
+        # more accurate point for: Arthur S Winterbotham Memorial Hall High Street Cam
+        if record.polling_place_id == "19839":
             record = record._replace(polling_place_easting="374915")
             record = record._replace(polling_place_northing="200440")
 
-        # Bussage Village Hall Bussage Stroud
-        if record.polling_place_id == "18022":
-            record = record._replace(polling_place_postcode="GL6 8BB")
-            record = record._replace(polling_place_easting="388393")
-            record = record._replace(polling_place_northing="203373")
+        # add point for: Pavillion in the Park, Victory Park, Cashes Green, GL5 4JE
+        if record.polling_place_id == "19632":
+            record = record._replace(polling_place_easting="382911")
+            record = record._replace(polling_place_northing="204929")
 
-        # Rodborough Tabernacle United Reformed Church Tabernacle Walk Rodborough Stroud GL5 3JJ
-        if record.polling_place_id == "18181":
-            record = record._replace(polling_place_postcode="")
+        # add point for: France Lynch Church Rooms, Lynch Road, Chalford Hill, Stroud, GL6 8LL
+        if record.polling_place_id == "19652":
+            record = record._replace(polling_place_easting="390069")
+            record = record._replace(polling_place_northing="203122")
 
-        # Trinity Rooms, Field Road
-        if record.polling_place_id == "18121":
-            record = record._replace(polling_place_easting="385644")
-            record = record._replace(polling_place_northing="204909")
+        # add point for: Stone Village Hall, Lower Stone Lane, Stone, GL13 9LE
+        if record.polling_place_id == "19602":
+            record = record._replace(polling_place_easting="368339")
+            record = record._replace(polling_place_northing="195210")
 
         return super().station_record_to_dict(record)
 
@@ -38,18 +42,26 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         if uprn in [
             "200003108372",  # 5 WORKMANS CLOSE, DURSLEY
             "100120525233",  # THE FORMER TELEPHONE EXCHANGE, BATH ROAD, HARDWICKE, GLOUCESTER
+            "10014280441",  # FLAT FAGINS STEAKHOUSE STROUD ROAD, BROOKTHORPE, GLOUCESTER
+            "10094463969",  # 1 WYNSTONES DRIVE, BROOKTHORPE, GLOUCESTER
+            "100120529784",  # BROADRIDGE GREEN, HARESFIELD LANE, EDGE, STROUD
+            "200003110822",  # HILLESLEIGH HOUSE, FRITHWOOD, BROWNSHILL, STROUD
+            "100121257583",  # FOURWAYS BUNGALOW, MIDDLE HILL, CHALFORD HILL, STROUD
+            "10090802788",  # STUDIO FLAT SOUTHANGER FARM MARLEY LANE, CHALFORD, STROUD
+            "100120528842",  # TAPANUI, COWCOMBE LANE, CHALFORD, STROUD
+            "200003118020",  # THE TOWER WOODCHESTER PARK, NYMPSFIELD, STONEHOUSE
         ]:
             return None
 
         if record.post_code in [
-            "GL13 9JL",
+            # splits
             "GL5 4NY",
             "GL5 3JL",
             "GL5 1RG",
-            "GL2 7NJ",
-            "GL5 3JN",
-            "GL10 3BN",
-            "GL6 9AH",
+            # looks wrong:
+            "GL5 4SP",
+            "GL6 8QP",
+            "GL11 5ND",
         ]:
             return None
 
