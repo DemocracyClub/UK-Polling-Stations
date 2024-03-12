ceb9a408ea8f634486257b04a8e8c61c67f33cbb
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-12 11:47:46 +0000

    Import script for North East Lincolnshire (2024-05-02) (closes #6561)

--- polling_stations/apps/data_importers/management/commands/import_north_east_lincs.py
+++ polling_stations/apps/data_importers/management/commands/import_north_east_lincs.py
@@ -3,9 +3,9 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "NEL"
-    addresses_name = "2023-05-04/2023-03-17T16:58:51.018219/Eros_SQL_Output001.csv"
-    stations_name = "2023-05-04/2023-03-17T16:58:51.018219/Eros_SQL_Output001.csv"
-    elections = ["2023-05-04"]
+    addresses_name = "2024-05-02/2024-03-12T11:47:38.056186/Eros_SQL_Output004.csv"
+    stations_name = "2024-05-02/2024-03-12T11:47:38.056186/Eros_SQL_Output004.csv"
+    elections = ["2024-05-02"]
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
@@ -13,6 +13,13 @@ class Command(BaseHalaroseCsvImporter):
         if uprn in [
             "10090078903",  # FLAT, 247 GRIMSBY ROAD, CLEETHORPES
             "10090086605",  # THE ROOST, DIANA PRINCESS OF WALES HOSPITAL, SCARTHO ROAD, GRIMSBY
+            "10090078890",  # FLAT 3, 50 RUTLAND STREET, GRIMSBY
+            "10090078888",  # FLAT 1, 50 RUTLAND STREET, GRIMSBY
+            "10090078889",  # FLAT 2, 50 RUTLAND STREET, GRIMSBY
+            "10090078891",  # FLAT 4, 50 RUTLAND STREET, GRIMSBY
+            "11023550",  # 38 BRAMHALL STREET, CLEETHORPES
+            "11088787",  # 4 WALTHAM HOUSE FARM COTTAGE, LOUTH ROAD, NEW WALTHAM, GRIMSBY
+            "11088786",  # WALTHAM HOUSE FARM COTTAGE 3 LOUTH ROAD, WALTHAM
         ]:
             return None
 
