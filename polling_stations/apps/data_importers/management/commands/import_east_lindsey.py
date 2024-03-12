a8237519a5aaa63aa578b73123243e46910a5cde
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-12 10:45:18 +0000

    Import script for East Lindsey (2024-05-02) (closes #6555)

--- polling_stations/apps/data_importers/management/commands/import_east_lindsey.py
+++ polling_stations/apps/data_importers/management/commands/import_east_lindsey.py
@@ -4,21 +4,17 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "ELI"
     addresses_name = (
-        "2023-05-04/2023-03-20T15:39:49.119114/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-03-21T11:00:56.741087/Democracy_Club__02May2024 (20) (1).tsv"
     )
     stations_name = (
-        "2023-05-04/2023-03-20T15:39:49.119114/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-03-21T11:00:56.741087/Democracy_Club__02May2024 (20) (1).tsv"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
-        #  Multi Purpose Room, Meridian Leisure Centre Wood Lane, Louth, Lincolnshire, LN11 8RA
-        if record.polling_place_id == "9885":
-            record = record._replace(polling_place_postcode="LN11 8SA")
-
-        # St Marys Church, Church Lane, Fotherby, Louth, LN11 0UJ
-        if record.polling_place_id == "9974":
+        # more accurate point for: St Marys Church, Church Lane, Fotherby, Louth, LN11 0UJ
+        if record.polling_place_id == "11523":
             record = record._replace(polling_place_easting="531703")
             record = record._replace(polling_place_northing="391688")
 
@@ -31,28 +27,31 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "200001828818",  # GRANGE FARM, WELTON-LE-MARSH, SPILSBY
             "10024297692",  # LAKE VIEW STATION ROAD, LITTLE STEEPING
             "100032167500",  # WINDSOR COTTAGE, HAKERLEY BRIDGE, FRITHVILLE, BOSTON
-            "200002780835",  # BARBRIDGE HOUSE, MAIN ROAD, SIBSEY, BOSTON
             "100032077624",  # EVENWOOD, FIRE BEACON LANE, COVENHAM ST. BARTHOLOMEW, LOUTH
             "100032260114",  # FIREBEACON BRIDGE FARM, FIREBEACON LANE, WRAGHOLME, LOUTH
             "10090638895",  # THE PADDOCKS, CROFT LANE, CROFT, SKEGNESS
             "100032166988",  # STONEY BROKE, TRADER BANK, SIBSEY, BOSTON
             "10008523556",  # JASMINE LODGE, MAIN ROAD, SIBSEY, BOSTON
+            "10096344217",  # A97 MERMAID CARAVAN PARK SEAHOLME ROAD, MABLETHORPE
+            "10093437401",  # A37 MERMAID CARAVAN PARK SEAHOLME ROAD, MABLETHORPE
+            "10008514781",  # DUDDLES FARM, SANDY BANK, NEW YORK, LINCOLN
+            "10008521252",  # CORNFIELDS, LITTLE CARLTON, LOUTH
+            "10094011546",  # FOXHALL, PARK ROW, LOUTH
         ]:
             return None
 
         if record.addressline6 in [
             # splits
-            "LN11 0EG",
             "LN11 8QG",
-            "LN9 5JP",
             "LN11 8DW",
-            "LN12 2HX",
+            "LN9 5JP",
             "PE25 2PX",
+            "LN8 5LR",
+            "LN11 0EG",
             "PE24 5UT",
-            "PE25 1SH",  # TILE HOUSE, ROSE GROVE, SKEGNESS
-            "PE25 2BG",  # LEEMING DRIVE, SKEGNESS
-            "PE25 2FD",  # RAY CLEMENCE WAY, SKEGNESS
-            "PE22 7SL",  # STATION ROAD, TUMBY WOODSIDE, BOSTON
+            "LN12 2HX",
+            # looks wrong
+            "PE25 2FE",
         ]:
             return None
 
