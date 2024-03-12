265d283de067b8ee9995bc95d812b75a4d9b8f0d
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-12 10:53:28 +0000

    Import script for Warrington (2024-05-02) (closes #6557)

--- polling_stations/apps/data_importers/management/commands/import_warrington.py
+++ polling_stations/apps/data_importers/management/commands/import_warrington.py
@@ -1,43 +1,32 @@
-from data_importers.management.commands import BaseDemocracyCountsCsvImporter
+from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
-class Command(BaseDemocracyCountsCsvImporter):
+class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WRT"
     addresses_name = (
-        "2021-03-12T13:29:25.271939/Democracy Club - Polling Districts 2021.csv"
+        "2024-05-02/2024-03-12T10:53:14.351979/Democracy_Club__02May2024.csv"
     )
     stations_name = (
-        "2021-03-12T13:29:25.271939/Democracy Club - Polling Stations 2021.csv"
+        "2024-05-02/2024-03-12T10:53:14.351979/Democracy_Club__02May2024.csv"
     )
-    elections = ["2021-05-06"]
+    elections = ["2024-05-02"]
 
     def station_record_to_dict(self, record):
-        # correction from council
-        if record.stationcode in ["138", "139"]:
-            record = record._replace(add1="Ellesmere Road")
-            record = record._replace(postcode="WA4 6DS")
-
-        # Correction from Council
-        # Rixton with Glazebrook Community Hall -> St Helen Church
-        if record.stationcode == "55":
+        # St Mary Magdalenes Church Hall, 87 Dingleway, Appleton, Warrington WA4 3AG
+        if record.polling_place_id == "2142":
             record = record._replace(
-                add1="Church House",
-                add2="Manchester Road",
-                add3="Hollinfare",
-                add4="Rixton with Glazebrook",
-                add5="",
-                add6="",
-                postcode="WA3 6LB",
-                xordinate="",
-                yordinate="",
-                placename="St Helen Church",
+                polling_place_easting="362050",
+                polling_place_northing="385487",
             )
 
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-        if record.postcode in ["WA4 5PQ", "WA4 1UN"]:
-            return None
-        if record.uprn in ["200000979589"]:
+        if record.addressline6 in [
+            # split
+            "WA4 5PQ",
+            # suspect
+            "WA5 3BF",
+        ]:
             return None
         return super().address_record_to_dict(record)
