8f66abb05e5c300360984fc6a2e2031b1587b98d
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-12 17:14:40 +0000

    Import script for South Tyneside (2024-05-02) (closes #6569)

--- polling_stations/apps/data_importers/management/commands/import_south_tyneside.py
+++ polling_stations/apps/data_importers/management/commands/import_south_tyneside.py
@@ -3,27 +3,33 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "STY"
-    addresses_name = "2023-05-04/2023-03-09T15:03:06.116940/Democracy_Club__04May2023_South Tyneside.tsv"
-    stations_name = "2023-05-04/2023-03-09T15:03:06.116940/Democracy_Club__04May2023_South Tyneside.tsv"
-    elections = ["2023-05-04"]
+    addresses_name = (
+        "2024-05-02/2024-03-12T17:14:20.914959/Democracy_Club__02May2024.tsv"
+    )
+    stations_name = (
+        "2024-05-02/2024-03-12T17:14:20.914959/Democracy_Club__02May2024.tsv"
+    )
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "100000322715",  # 72 BEACH ROAD, SOUTH SHIELDS
             "200000000962",  # 2 CLIFF COTTAGES, QUAY CORNER AVENUE, JARROW
             "200000000963",  # CLIFF HOUSE, QUAY CORNER AVENUE, JARROW
             "200000000961",  # 1 CLIFF COTTAGES, QUAY CORNER AVENUE, JARROW
-            "200000000543",  # MANAGERS ACCOMMODATION PUBLIC HOUSE 1 BLACKETT STREET, HEBBURN
-            "200000000814",  # THE SCHOOL HOUSE, CAMPBELL PARK ROAD, HEBBURN
             "103001354",  # MANAGERS ACCOMMODATION THE STORYBOOK ABINGDON WAY, SOUTH TYNESIDE, BOLDON COLLIERY
             "200000006403",  # THE COTTAGE, WHITBURN MOORS FARM, SUNDERLAND ROAD, SUNDERLAND
         ]:
             return None
 
-        if record.addressline6 in ["NE31 2XF", "NE34 8AE"]:  # splits
+        if record.addressline6 in [
+            # splits
+            "NE31 2EA",
+            "NE31 2XF",
+            "NE34 8AE",
+        ]:
             return None
 
         return super().address_record_to_dict(record)
