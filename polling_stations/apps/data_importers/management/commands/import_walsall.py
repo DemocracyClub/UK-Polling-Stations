1874c7071134edd74d516febe80b02031d316e85
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-13 13:08:37 +0000

    Import script for Walsall (2024-05-02) (closes #6579)

--- polling_stations/apps/data_importers/management/commands/import_walsall.py
+++ polling_stations/apps/data_importers/management/commands/import_walsall.py
@@ -1,34 +1,44 @@
-from data_importers.management.commands import BaseHalaroseCsvImporter
+from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
-class Command(BaseHalaroseCsvImporter):
+class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WLL"
-    addresses_name = "2023-05-04/2023-03-29T14:11:04.952178/Eros_SQL_Output001.csv"
-    stations_name = "2023-05-04/2023-03-29T14:11:04.952178/Eros_SQL_Output001.csv"
-    elections = ["2023-05-04"]
+    addresses_name = (
+        "2024-05-02/2024-03-13T13:07:56.064490/Democracy_Club__02May2024.tsv"
+    )
+    stations_name = (
+        "2024-05-02/2024-03-13T13:07:56.064490/Democracy_Club__02May2024.tsv"
+    )
+    elections = ["2024-05-02"]
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
+        uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "100071072854",  # 59 NEW STREET, SHELFIELD, WALSALL
-            "100071072810",  # 9A NEW STREET, SHELFIELD, WALSALL
-            "10090903310",  # FLAT 1 73 STAFFORD STREET, WILLENHALL
-            "200002877500",  # 43A HIGH STREET, BROWNHILLS, WALSALL
+            "100071349802",  # 659A WALSALL ROAD, WEDNESBURY
+            "100071109642",  # 124 GOUGH STREET, WILLENHALL
+            "10090065467",  # 123A GOUGH STREET, WILLENHALL
+            "10090065466",  # 123 GOUGH STREET, WILLENHALL
+            "100071062219",  # 240 INGRAM ROAD, WALSALL
+            "100071062221",  # 242 INGRAM ROAD, WALSALL
+            "200003317397",  # CHRIST CHURCH RECTORY, BLAKENALL HEATH, WALSALL
+            "10013664575",  # 52 MATTESLEY COURT, CRESSWELL CRESCENT, WALSALL
+            "200003317043",  # 103 SNEYD LANE, ESSINGTON, WOLVERHAMPTON
+            "200003317044",  # 105 SNEYD LANE, ESSINGTON, WOLVERHAMPTON
+            "200003317045",  # 107 SNEYD LANE, ESSINGTON, WOLVERHAMPTON
+            "200003317046",  # 109 SNEYD LANE, ESSINGTON, WOLVERHAMPTON
         ]:
             return None
 
-        if record.housepostcode in [
-            # split
-            "WS3 1FJ",
-            "WS1 3LD",
-            "WV12 4BZ",
+        if record.addressline6 in [
+            # splits
             "WS3 2DX",
             # look wrong
             "WS10 7TG",
             "WS2 0HS",
-            "WS3 4LX",
             "WS3 4NX",
+            "WS3 4NT",
         ]:
             return None
 
