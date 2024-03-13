a31bccd5fc5d218480d38c4fe9f4bb6ea40a94c4
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-13 17:38:52 +0000

    Import script for East Hampshire (2024-05-02) (closes #6583)

--- polling_stations/apps/data_importers/management/commands/import_east_hampshire.py
+++ polling_stations/apps/data_importers/management/commands/import_east_hampshire.py
@@ -4,10 +4,10 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "EHA"
     addresses_name = (
-        "2024-05-02/2024-03-07T15:45:57.741886/Democracy_Club__02May2024.tsv"
+        "2024-05-02/2024-03-13T17:38:37.672177/Democracy_Club__02May2024 (1).tsv"
     )
     stations_name = (
-        "2024-05-02/2024-03-07T15:45:57.741886/Democracy_Club__02May2024.tsv"
+        "2024-05-02/2024-03-13T17:38:37.672177/Democracy_Club__02May2024 (1).tsv"
     )
     elections = ["2024-05-02"]
     csv_delimiter = "\t"
@@ -22,13 +22,16 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "10094123612",  # WATERCRESS COTTAGE, STATION ROAD, BENTLEY, FARNHAM
             "10094121538",  # THE STABLES, COLDREY FARM, LOWER FROYLE, ALTON
             "10009812215",  # THE LITTLE BARN, WOODSIDE FARM, GOSPORT ROAD, PRIVETT, ALTON
-            "10094122454",  # LUMBRY BARN, SELBORNE ROAD, ALTON
             "10094121982",  # TREESIDE VIEW, THE SHRAVE, FOUR MARKS, ALTON
             "10094122454",  # LUMBRY BARN, SELBORNE ROAD, ALTON
             "10094123735",  # OAK LODGE, SELBORNE ROAD, ALTON
             "1710105096",  # 21 WINCHESTER ROAD, ALTON
             "10032905135",  # HALF ACRE, HAWKLEY ROAD, LISS
             "10009812211",  # THE COTTAGE, LOWER BORDEAN, BORDEAN, PETERSFIELD
+            "10096347180",  # RHODESIDE THE SHRAVE, FOUR MARKS, ALTON
+            "100060259943",  # KITWOOD PLACE, SWELLING HILL, ROPLEY, ALRESFORD
+            "10032907288",  # KITCOMBE BARN, KITCOMBE LANE, FARRINGDON, ALTON
+            "1710109658",  # THE HERMITAGE, HEADLEY HILL ROAD, HEADLEY, BORDON
         ]:
             return None
 
@@ -37,8 +40,6 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "GU33 7BB",
             "GU30 7QL",
             "PO8 0QR",
-            # looks wrong
-            "PO8 0QA",
         ]:
             return None
 
