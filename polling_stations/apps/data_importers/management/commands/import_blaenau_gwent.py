bf35a85e36cff26e6dc0b27be62e58f120459264
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-14 15:06:57 +0000

    Import script for Blaenau Gwent (2024-05-02) (closes #6603)

--- polling_stations/apps/data_importers/management/commands/import_blaenau_gwent.py
+++ polling_stations/apps/data_importers/management/commands/import_blaenau_gwent.py
@@ -1,54 +1,27 @@
-from addressbase.models import Address, UprnToCouncil
-from core.opening_times import OpeningTimes
 from data_importers.management.commands import BaseHalaroseCsvImporter
-from data_importers.mixins import AdvanceVotingMixin
-from pollingstations.models import AdvanceVotingStation
 
 
-class Command(BaseHalaroseCsvImporter, AdvanceVotingMixin):
+class Command(BaseHalaroseCsvImporter):
     council_id = "BGW"
-    addresses_name = (
-        "2022-05-05/2022-03-21T12:39:42.933221/polling_station_export-2022-03-21.csv"
-    )
-    stations_name = (
-        "2022-05-05/2022-03-21T12:39:42.933221/polling_station_export-2022-03-21.csv"
-    )
-    elections = ["2022-05-05"]
+    addresses_name = "2024-05-02/2024-03-20T09:59:04.174956/Eros_SQL_Output008.csv"
+    stations_name = "2024-05-02/2024-03-20T09:59:04.174956/Eros_SQL_Output008.csv"
+    elections = ["2024-05-02"]
     csv_encoding = "windows-1252"
 
-    def station_record_to_dict(self, record):
-        if record.pollingstationnumber in [
-            "61",  # EBENEZER CHAPEL VESTRY
-        ]:
-            record = record._replace(pollingstationpostcode="NP22 4RQ")
-        return super().station_record_to_dict(record)
-
     def address_record_to_dict(self, record):
+        uprn = record.uprn.strip().lstrip("0")
+
+        if uprn in [
+            "100100460975",  # 27A QUEEN STREET, NANTYGLO
+            "100100460974",  # 27B QUEEN STREET, NANTYGLO
+            "10014116610",  # 1 MOUNTAIN AIR, EBBW VALE
+            "10014116959",  # 8 RESERVOIR ROAD, BEAUFORT, EBBW VALE
+        ]:
+            return None
+
         if record.housepostcode in [
-            "NP23 5DH",
-            "NP13 3JU",
-            "NP13 3AQ",
+            "NP23 5DH",  # split
         ]:
             return None
 
         return super().address_record_to_dict(record)
-
-    def add_advance_voting_stations(self):
-        opening_times = OpeningTimes()
-        opening_times.add_open_time("2022-05-03", "08:00", "16:00")
-        opening_times.add_open_time("2022-05-04", "08:00", "16:00")
-
-        advance_station = AdvanceVotingStation(
-            name="Ebbw Vale Learning Zone",
-            address="""Lime Avenue
-            Ebbw Vale
-                """,
-            postcode="NP23 6GL",
-            location=Address.objects.get(uprn="10014120799").location,
-            opening_times=opening_times.as_string_table(),
-            council=self.council,
-        )
-        advance_station.save()
-        UprnToCouncil.objects.filter(lad=self.council.geography.gss).update(
-            advance_voting_station=advance_station
-        )
