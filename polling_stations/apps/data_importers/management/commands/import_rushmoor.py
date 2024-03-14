302a8cf9592d53d7c59b6353bb820b5793fee084
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-14 10:26:03 +0000

    Import script for Rushmoor (2024-05-02) (closes #6594)

--- polling_stations/apps/data_importers/management/commands/import_rushmoor.py
+++ polling_stations/apps/data_importers/management/commands/import_rushmoor.py
@@ -4,22 +4,19 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "RUH"
     addresses_name = (
-        "2023-05-04/2023-03-15T16:53:47.237360/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-03-14T10:25:53.338856/Democracy_Club__02May2024.tsv"
     )
     stations_name = (
-        "2023-05-04/2023-03-15T16:53:47.237360/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-03-14T10:25:53.338856/Democracy_Club__02May2024.tsv"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "100062006907",  # 1B WATERLOO ROAD, ALDERSHOT
-            "100062323687",  # 1 WATERLOO ROAD, ALDERSHOT
-            "100062323733",  # 1A WATERLOO ROAD, ALDERSHOT
-            "100062323188",  # YELLOW RIVER, 50 GROSVENOR ROAD, ALDERSHOT
+            "100062323099",  # GOLD FARM, GOVERNMENT ROAD, ALDERSHOT
         ]:
             return None
 
