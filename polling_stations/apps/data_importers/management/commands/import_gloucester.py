876a518021bda372f0bb31a47b8722c7f871e743
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-18 15:13:35 +0000

    Import script for Gloucester (2024-05-02) (closes #6654)

--- polling_stations/apps/data_importers/management/commands/import_gloucester.py
+++ polling_stations/apps/data_importers/management/commands/import_gloucester.py
@@ -4,20 +4,25 @@ from django.contrib.gis.geos import Point
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "GLO"
-    addresses_name = "2021-05-04T14:32:57.046293/Democracy_Club__06May2021.CSV"
-    stations_name = "2021-05-04T14:32:57.046293/Democracy_Club__06May2021.CSV"
-    elections = ["2021-05-06"]
-    csv_delimiter = ","
+    addresses_name = (
+        "2024-05-02/2024-03-18T15:13:17.171189/Democracy_Club__02May2024.tsv"
+    )
+    stations_name = (
+        "2024-05-02/2024-03-18T15:13:17.171189/Democracy_Club__02May2024.tsv"
+    )
+    elections = ["2024-05-02"]
+    csv_encoding = "windows-1252"
+    csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
-        # St Paul and St Stephen Church Stroud Road Gloucester GL1 5AL
-        if record.polling_place_id == "3508":
+        # more accurate point for: St Paul and St Stephen Church Stroud Road Gloucester GL1 5AL
+        if record.polling_place_id == "4078":
             rec = super().station_record_to_dict(record)
             rec["location"] = Point(-2.245029, 51.854501, srid=4326)
             return rec
 
-        # St Lawrence Church Centre 32 Church Lane Gloucester GL4 3JB
-        if record.polling_place_id == "3428":
+        # more accurate point for: St Lawrence Church Centre 32 Church Lane Gloucester GL4 3JB
+        if record.polling_place_id == "4021":
             rec = super().station_record_to_dict(record)
             rec["location"] = Point(-2.206526, 51.858234, srid=4326)
             return rec
@@ -30,10 +35,21 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         if uprn in [
             "10007316043",  # 70 GOOSE BAY DRIVE KINGSWAY, QUEDGELEY, GLOUCESTER
             "10007317774",  # 41 AWEBRIDGE WAY, GLOUCESTER
+            "10007323649",  # FLAT 6 15 HARKER CLOSE, GLOUCESTER
+            "10007317846",  # FLAT, 1 LLANTHONY ROAD, GLOUCESTER
+            "10007311538",  # FLAT 8 LLANTHONY ROAD, GLOUCESTER
+            "10007316151",  # 82 GOOSE BAY DRIVE KINGSWAY, QUEDGELEY, GLOUCESTER
+            "200004486714",  # 24A SEYMOUR ROAD, GLOUCESTER
         ]:
             return None
 
-        if record.addressline6 in ["GL4 6JR", "GL2 4DE"]:
+        if record.addressline6 in [
+            # splits
+            "GL3 3HW",
+            # looks wrong
+            "GL2 5QU",
+            "GL2 4SA",
+        ]:
             return None
 
         return super().address_record_to_dict(record)
