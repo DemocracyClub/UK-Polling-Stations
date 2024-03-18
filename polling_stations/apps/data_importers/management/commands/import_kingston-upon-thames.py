c760f9816d19789ac422aa2fa6a02c5c14ec0aa9
awdem <wdemontmollin@gmail.com>
2024-03-18 15:33:08 +0000

    fixes from kingston-upon-thames council

--- polling_stations/apps/data_importers/management/commands/import_kingston-upon-thames.py
+++ polling_stations/apps/data_importers/management/commands/import_kingston-upon-thames.py
@@ -15,6 +15,24 @@ class Command(BaseDemocracyCountsCsvImporter):
     # Following warnings checked and no correction needed
     # WARNING: Polling station Oak Hall, Maple Lodge (MPC) is in London Borough of Sutton (STN)
 
+    def station_record_to_dict(self, record):
+        # fix from council:
+        # old address: St. John's Parish Hall, Grove Lane, Kingston upon Thames KT1 2SU
+        # new address: St. John's Parish Hall, Grove Lane, Kingston upon Thames KT1 2ST
+        if record.stationcode == "KTC":
+            record = record._replace(postcode="KT1 2ST")
+
+        # fix from council:
+        # old address: Catholic Church of St. Pius X, The Triangle, Kingston Upon Thames, Surrey KT1 3SB
+        # new address: Temporary station side of St Pius X Catholic Church, The Triangle, Kingston Upon Thames, Surrey KT1 3RU
+        if record.stationcode == "CVD":
+            record = record._replace(
+                placename="Temporary station side of St Pius X Catholic Church",
+                postcode="KT1 3RU",
+            )
+
+        return super().station_record_to_dict(record)
+
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
 
