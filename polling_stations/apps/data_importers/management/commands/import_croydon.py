1052455af95ca9e0c1a25040833234b60c1a9159
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-18 13:09:12 +0000

    Import script for Croydon (2024-05-02) (closes #6636)

--- polling_stations/apps/data_importers/management/commands/import_croydon.py
+++ polling_stations/apps/data_importers/management/commands/import_croydon.py
@@ -4,30 +4,57 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "CRY"
     addresses_name = (
-        "2022-05-05/2022-03-15T22:27:09.311565/Democracy_Club__05May2022.tsv"
+        "2024-05-02/2024-03-18T13:08:49.912425/Democracy_Club__02May2024.tsv"
     )
     stations_name = (
-        "2022-05-05/2022-03-15T22:27:09.311565/Democracy_Club__05May2022.tsv"
+        "2024-05-02/2024-03-18T13:08:49.912425/Democracy_Club__02May2024.tsv"
     )
-    elections = ["2022-05-05"]
+    elections = ["2024-05-02"]
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
+
+    # Warning below checked and no correction needed:
+    # WARNING: Polling station Chipstead Valley Primary School (16272) is in Reigate and Banstead Borough Council (REI)
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
             "100020676357",  # 18A CROHAM ROAD, SOUTH CROYDON
+            "10094492830",  # 1A COVINGTON WAY, LONDON
+            "10001006634",  # 3 LANSDOWNE PLACE, LONDON
+            "100020642075",  # BASEMENT FLAT, 193 CHURCH ROAD, LONDON
+            "100020642076",  # 193 CHURCH ROAD, LONDON
+            "100020622449",  # 5A SHIRLEY OAKS ROAD, CROYDON
+            "100020622448",  # 2 SHIRLEY OAKS ROAD, CROYDON
+            "100020584917",  # WEST LODGE, BISHOPS WALK, CROYDON3
+            "10093049200",  # 16 LIMPSFIELD ROAD, SOUTH CROYDON
+            "10093049201",  # 16A LIMPSFIELD ROAD, SOUTH CROYDON
+            "10093049201",  # 16A LIMPSFIELD ROAD, SOUTH CROYDON
+            "100020635515",  # 260 HAYES LANE, KENLEY
+            "200001221192",  # 141 BRIGHTON ROAD, PURLEY
+            "100020593700",  # 49 CROWLEY CRESCENT, CROYDON
+            "10014054732",  # 97 NOVA ROAD, CROYDON
+            "10014054731",  # 95 NOVA ROAD, CROYDON
+            "10094493457",  # 44 COOMBE ROAD, CROYDON
+            "10093046375",  # 237A SYDENHAM ROAD, CROYDON
+            "10093756168",  # FLAT 1, 80 WADDON NEW ROAD, CROYDON
+            "10093756169",  # FLAT 2, 80 WADDON NEW ROAD, CROYDON
+            "100020688674",  # 56 WEST HILL, SOUTH CROYDON
         ]:
             return None
 
         if record.addressline6 in [
-            "CR0 0NX",
+            # splits
             "CR0 2JB",
-            "CR0 4BF",
             "CR2 0JB",
-            "CR5 2YS",
-            "CR5 2FN",
+            "CR0 4BF",
+            # looks wrong
+            "SE19 3FB",
+            "CR2 8JT",
+            "CR2 0RW",
+            "CR8 5AR",
+            "CR8 3ES",
         ]:
             return None
 
