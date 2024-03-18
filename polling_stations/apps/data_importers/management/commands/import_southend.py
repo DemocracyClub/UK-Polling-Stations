4ffc541e2cd52e92abe9d3b66e74e6edd7f56b31
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-18 12:14:12 +0000

    Import script for Southend-on-Sea (2024-05-02) (closes #6634)

--- polling_stations/apps/data_importers/management/commands/import_southend.py
+++ polling_stations/apps/data_importers/management/commands/import_southend.py
@@ -4,59 +4,32 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SOS"
     addresses_name = (
-        "2023-05-04/2023-03-09T13:07:25.305816/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-03-18T12:13:52.746718/Democracy_Club__02May2024.tsv"
     )
     stations_name = (
-        "2023-05-04/2023-03-09T13:07:25.305816/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-03-18T12:13:52.746718/Democracy_Club__02May2024.tsv"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
-        if record.polling_place_id == "7260":
+        # Booth 2, Earls Hall Primary School, (Colemans Avenue Entrance), Westcliff-on-Sea
+        if record.polling_place_id == "9382":
             record = record._replace(
                 polling_place_easting="586595", polling_place_northing="187653"
             )
-
-        # Sandpiper Pre-school, Sandpiper Close/Kingfisher Close, Shoeburyness, SS3 9YB
-        # correction for the council
-        if record.polling_place_id == "7561":
-            record = record._replace(
-                polling_place_easting="593511", polling_place_northing="185908"
-            )
-
-        # Hinguar School, New Garrison Road, Shoeburyness, Southend-on-Sea, Essex, SS3 9FE
-        # correction for the council
-        if record.polling_place_id == "7414":
-            record = record._replace(
-                polling_place_easting="593387", polling_place_northing="184765"
-            )
-
-        # Centre Place Community Centre, 15 Prospect Close, Essex, SS1 6XY
-        # correction for the council
-        if record.polling_place_id == "7363":
-            record = record._replace(
-                polling_place_easting="589315", polling_place_northing="185206"
-            )
-
-        # Booth 1, John Wesley Hall	Belfairs Methodist Church, Eastwood Road North, Leigh-on-Sea, SS9 4NG
-        # correction for the council
-        if record.polling_place_id == "7331":
-            record = record._replace(
-                polling_place_easting="583854", polling_place_northing="187894"
-            )
-
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         if record.addressline6 in [
+            # split
             "SS9 1RP",
-            "SS9 1LN",
-            "SS3 9QH",
-            "SS9 4RP",
-            "SS9 1NH",
-            "SS9 5EW",
             "SS9 1QY",
+            "SS9 1NH",
+            "SS9 1LN",
+            "SS9 4RP",
+            "SS3 9QH",
+            "SS9 5EW",
         ]:
             return None
 
