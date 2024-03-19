0ba3ea32faf84368957cc00a62741e554b115c32
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-19 09:41:30 +0000

    Import script for Southwark (2024-05-02) (closes #6684)

--- polling_stations/apps/data_importers/management/commands/import_southwark.py
+++ polling_stations/apps/data_importers/management/commands/import_southwark.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SWK"
     addresses_name = (
-        "2022-05-05/2022-04-25T15:20:20.339451/Democracy_Club__05May2022.tsv"
+        "2024-05-02/2024-03-19T09:41:01.760435/Democracy_Club__02May2024.tsv"
     )
     stations_name = (
-        "2022-05-05/2022-04-25T15:20:20.339451/Democracy_Club__05May2022.tsv"
+        "2024-05-02/2024-03-19T09:41:01.760435/Democracy_Club__02May2024.tsv"
     )
-    elections = ["2022-05-05"]
+    elections = ["2024-05-02"]
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
 
@@ -18,32 +18,24 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
         if uprn in [
             "200003451291",  # ST. FAITHS VICARAGE, 62 RED POST HILL, LONDON
-            "200003394858",  # 17 LYNDHURST WAY, LONDON
-            "10090283768",  # KILIMANJARO LIVE LTD, SECOND FLOOR NORTH 15 BERMONDSEY SQUARE, LONDON
-            "200003468937",  # GROUNDSMANS COTTAGE COLLEGE ROAD, LONDON
-            "10091665680",  # 23 CAMBERWELL GROVE, LONDON
-            "10094086807",
+            "10096036520",  # FLAT 1, SEACOLE COURT 62A, RED POST HILL, LONDON
+            "10096036521",  # FLAT 2, SEACOLE COURT 62A, RED POST HILL, LONDON
+            "10096036522",  # FLAT 3, SEACOLE COURT 62A, RED POST HILL, LONDON
+            "10096036523",  # FLAT 4, SEACOLE COURT 62A, RED POST HILL, LONDON
+            "10096036524",  # FLAT 5, SEACOLE COURT 62A, RED POST HILL, LONDON
+            "10096036525",  # FLAT 6, SEACOLE COURT 62A, RED POST HILL, LONDON
+            "10096036526",  # FLAT 7, SEACOLE COURT 62A, RED POST HILL, LONDON
+            "10096036527",  # FLAT 8, SEACOLE COURT 62A, RED POST HILL, LONDON
+            "10096036528",  # FLAT 9, SEACOLE COURT 62A, RED POST HILL, LONDON
         ]:
             return None
 
         if record.addressline6 in [
+            # split
+            "SE16 6AZ",
             "SE5 0SY",
             "SE15 6BJ",
-            "SE16 6AZ",
-            "SE5 8DB",
-            "SE1 2PS",
-            "SE16 2QU",
-            "SE1 3UL",
-            "SE15 3DN",
             "SE5 7HY",
-            "SE1 2AD",
-            "SE15 2ND",
-            "SE1 0AA",
-            "SE5 8DB",
         ]:
             return None
-
-        if record.addressline1 == "Excluding Third Floor and Fourth Floor":
-            return None
-
         return super().address_record_to_dict(record)
