38fe69124b2b495466b1e4ad53498cc4f6db2813
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-19 09:28:07 +0000

    Import script for Redcar and Cleveland (2024-05-02) (closes #6682)

--- polling_stations/apps/data_importers/management/commands/import_redcar_cleveland.py
+++ polling_stations/apps/data_importers/management/commands/import_redcar_cleveland.py
@@ -4,19 +4,20 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "RCC"
     addresses_name = (
-        "2023-05-04/2023-04-05T14:43:08.187824/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-03-27T08:05:02.256536/Democracy_Club__02May2024.CSV"
     )
     stations_name = (
-        "2023-05-04/2023-04-05T14:43:08.187824/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-03-27T08:05:02.256536/Democracy_Club__02May2024.CSV"
     )
-    elections = ["2023-05-04"]
-    csv_delimiter = "\t"
+    elections = ["2024-05-02"]
 
     def address_record_to_dict(self, record):
         if record.addressline6 in [
             # split
             "TS10 4AJ",
             "TS6 0PA",
+            # suspect
+            "TS7 9HR",
         ]:
             return None
 
