06fceb6927a14e8a91d797bfb42c9c74d2b0bf52
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-19 13:50:18 +0000

    Import script for Bracknell Forest (2024-05-02) (closes #6699)

--- polling_stations/apps/data_importers/management/commands/import_bracknell_forest.py
+++ polling_stations/apps/data_importers/management/commands/import_bracknell_forest.py
@@ -3,17 +3,21 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "BRC"
-    addresses_name = "2023-05-04/2023-03-30T16:50:30.358526/Eros_SQL_Output002.csv"
-    stations_name = "2023-05-04/2023-03-30T16:50:30.358526/Eros_SQL_Output002.csv"
-    elections = ["2023-05-04"]
+    addresses_name = "2024-05-02/2024-03-19T13:50:10.302143/Eros_SQL_Output003.csv"
+    stations_name = "2024-05-02/2024-03-19T13:50:10.302143/Eros_SQL_Output003.csv"
+    elections = ["2024-05-02"]
 
     def address_record_to_dict(self, record):
+        uprn = record.uprn.strip().lstrip("0")
+
+        if uprn in [
+            "10022826533",  # THE BUNGALOW OAKWOOD PARK KENNELS PEACOCK LANE, WOKINGHAM
+        ]:
+            return None
         if record.housepostcode in [
             # split
             "SL5 8RY",
-            "RG42 2FB",
             "RG42 6BX",
         ]:
             return None
-
         return super().address_record_to_dict(record)
