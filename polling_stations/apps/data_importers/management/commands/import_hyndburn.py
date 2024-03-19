49b8c5e3b87dec12c49eca6dad33bb96de7226a7
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-19 16:59:58 +0000

    Import script for Hyndburn (2024-05-02) (closes #6713)

--- polling_stations/apps/data_importers/management/commands/import_hyndburn.py
+++ polling_stations/apps/data_importers/management/commands/import_hyndburn.py
@@ -4,19 +4,25 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "HYN"
     addresses_name = (
-        "2023-05-04/2023-03-09T16:17:00.789871/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-03-19T16:59:46.456514/Democracy_Club__02May2024.tsv"
     )
     stations_name = (
-        "2023-05-04/2023-03-09T16:17:00.789871/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-03-19T16:59:46.456514/Democracy_Club__02May2024.tsv"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
 
+    def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
 
-def address_record_to_dict(self, record):
-    if record.addressline6 in [
-        "BB5 5QA",  # split
-    ]:
-        return None
+        if uprn in [
+            "10009967792",  # 3 HIGH STREET, ACCRINGTON
+            "100012878207",  # LEG LOCK FARM, SOUGH LANE, GUIDE, BLACKBURN
+        ]:
+            return None
+        if record.addressline6 in [
+            "BB5 5QA",  # split
+        ]:
+            return None
 
-    return super().address_record_to_dict(record)
+        return super().address_record_to_dict(record)
