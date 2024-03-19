d67f603cbf9d93e1dc217caa86266aebd5dc7767
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-19 10:22:41 +0000

    Import script for Ealing (2024-05-02) (closes #6686)

--- polling_stations/apps/data_importers/management/commands/import_ealing.py
+++ polling_stations/apps/data_importers/management/commands/import_ealing.py
@@ -5,19 +5,36 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "EAL"
-    addresses_name = "2022-05-05/2022-04-13T16:32:47.995941/LB of Ealing Democracy_Club__05May2022.tsv"
-    stations_name = "2022-05-05/2022-04-13T16:32:47.995941/LB of Ealing Democracy_Club__05May2022.tsv"
-    elections = ["2022-05-05"]
+    addresses_name = (
+        "2024-05-02/2024-03-19T10:22:00.536492/Democracy_Club__02May2024.tsv"
+    )
+    stations_name = (
+        "2024-05-02/2024-03-19T10:22:00.536492/Democracy_Club__02May2024.tsv"
+    )
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
-        if record.addressline6 in ["W4 5HL"]:
-            return None  # split
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "12180637",  # ANNEXE 20 LOCARNO ROAD, GREENFORD
+            "12147146",  # 35A PRIORY GARDENS, LONDON
+            "12181729",  # FLAT 3 55 PARK AVENUE, PARK ROYAL
+            "12181728",  # FLAT 2 55 PARK AVENUE, PARK ROYAL
+            "12181730",  # FLAT 4 55 PARK AVENUE, PARK ROYAL
+            "12181731",  # FLAT 5 55 PARK AVENUE, PARK ROYAL
+        ]:
+            return None
+
+        if record.addressline6 in [
+            "W4 5HL",  # split
+        ]:
+            return None
         return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
         # Incomplete postcodes for temporary stations
         if re.match(r"^[A-Z]{1,2}[0-9]{1,2}$", record.polling_place_postcode):
             record = record._replace(polling_place_postcode="")
-
         return super().station_record_to_dict(record)
