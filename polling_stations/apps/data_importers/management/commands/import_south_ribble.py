11b56271b549e5d1edbc8e9014194a21cc220ae4
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-19 15:12:11 +0000

    Import script for South Ribble (2024-05-02) (closes #6707)

--- polling_stations/apps/data_importers/management/commands/import_south_ribble.py
+++ polling_stations/apps/data_importers/management/commands/import_south_ribble.py
@@ -3,32 +3,18 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "SRI"
-    addresses_name = (
-        "2023-05-04/2023-02-10T14:28:21.813272/polling_station_export-2022-03-24.csv"
-    )
-    stations_name = (
-        "2023-05-04/2023-02-10T14:28:21.813272/polling_station_export-2022-03-24.csv"
-    )
-    elections = ["2023-05-04"]
-
-    def station_record_to_dict(self, record):
-        # Warning for BUCKSHAW VILLAGE COMMUNITY CENTRE (2-buckshaw-village-community-centre)
-        # being outside the target council area, but it's fine.
-
-        # COUPE GREEN PRIMARY SCHOOL, COUPE GREEN, HOGHTON, PRESTON
-        if record.pollingstationnumber == "62":
-            record = record._replace(pollingstationpostcode="PR5 0JR")  # was PR5 OJR
-
-        # HIGHER  WALTON COMMUNITY CENTRE, OFF HIGHER  WALTON ROAD, HIGHER WALTON, PRESTON
-        if record.pollingstationnumber == "64":
-            record = record._replace(pollingstationpostcode="PR5 4HU")  # was PR5 4HV
-
-        return super().station_record_to_dict(record)
+    addresses_name = "2024-05-02/2024-03-19T15:12:00.400914/Eros_SQL_Output004.csv"
+    stations_name = "2024-05-02/2024-03-19T15:12:00.400914/Eros_SQL_Output004.csv"
+    elections = ["2024-05-02"]
 
     def address_record_to_dict(self, record):
-        if record.housepostcode in ["PR5 5AU", "PR5 4TB"]:
-            return None  # split
+        if record.housepostcode in [
+            "PR5 5AU",  # split
+        ]:
+            return None
 
-        if record.uprn in ["100010637839"]:
+        if record.uprn in [
+            "100010637839",  # 73A LIVERPOOL OLD ROAD, WALMER BRIDGE, PRESTON
+        ]:
             return None
         return super().address_record_to_dict(record)
