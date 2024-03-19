077da6a41c4dd5613a2e7c6b8f83ce3f326f47c6
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-19 14:34:55 +0000

    Import script for Leeds (2024-05-02) (closes #6703)

--- polling_stations/apps/data_importers/management/commands/import_leeds.py
+++ polling_stations/apps/data_importers/management/commands/import_leeds.py
@@ -3,13 +3,9 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "LDS"
-    addresses_name = (
-        "2023-05-04/2023-03-09T13:54:42.945922/Democracy_Club__04May2023.tsv"
-    )
-    stations_name = (
-        "2023-05-04/2023-03-09T13:54:42.945922/Democracy_Club__04May2023.tsv"
-    )
-    elections = ["2023-05-04"]
+    addresses_name = "2024-05-02/2024-03-19T14:32:27.465948/leeds_deduplicated.tsv"
+    stations_name = "2024-05-02/2024-03-19T14:32:27.465948/leeds_deduplicated.tsv"
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -18,40 +14,80 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         if uprn in [
             "72132919",  # EBOR COTTAGE, MIDDLETON ROAD, LEEDS
             "72721195",  # FLAT PENDAS ARMS NABURN APPROACH, WHINMOOR, LEEDS
+            "72665355",  # THE BEECHES, WIGHILL, TADCASTER
+            "72390208",  # THE GABLES, BARROWBY LANE, GARFORTH, LEEDS
+            "72652486",  # FIRS HILL FARM, POOL BANK NEW ROAD, POOL IN WHARFEDALE, OTLEY
+            "72528574",  # BANKEND FARM, BEDLAM LANE, ARTHINGTON, OTLEY
+            "72305010",  # THE BUNGALOW, DRURY LANE, HORSFORTH, LEEDS
+            "72304132",  # 34 ALEXANDRA ROAD, HORSFORTH, LEEDS
+            "72304134",  # 36 ALEXANDRA ROAD, HORSFORTH, LEEDS
+            "72304135",  # 37 ALEXANDRA ROAD, HORSFORTH, LEEDS
+            "72304136",  # 38 ALEXANDRA ROAD, HORSFORTH, LEEDS
+            "72304137",  # 39 ALEXANDRA ROAD, HORSFORTH, LEEDS
+            "72304138",  # 40 ALEXANDRA ROAD, HORSFORTH, LEEDS
+            "72304139",  # 41 ALEXANDRA ROAD, HORSFORTH, LEEDS
+            "72304140",  # 42 ALEXANDRA ROAD, HORSFORTH, LEEDS
+            "72304141",  # 43 ALEXANDRA ROAD, HORSFORTH, LEEDS
+            "72304142",  # 44 ALEXANDRA ROAD, HORSFORTH, LEEDS
+            "72304143",  # 45 ALEXANDRA ROAD, HORSFORTH, LEEDS
+            "72304144",  # 46 ALEXANDRA ROAD, HORSFORTH, LEEDS
+            "72304145",  # 47 ALEXANDRA ROAD, HORSFORTH, LEEDS
+            "72304147",  # 49 ALEXANDRA ROAD, HORSFORTH, LEEDS
+            "72536279",  # THE LODGE, LOW LANE, HORSFORTH, LEEDS
+            "72108807",  # 145 KENTMERE AVENUE, LEEDS
+            "72719195",  # FLAT THE SPORTSMAN STONEY ROCK LANE, BURMANTOFTS, LEEDS
+            "72700401",  # FLAT THE REGENT 109 KIRKGATE, LEEDS
+            "72721594",  # FLAT THE DUCK AND DRAKE INN 43 KIRKGATE, LEEDS
+            "72160712",  # 29 PRESTON PARADE, LEEDS
+            "72160713",  # 30 PRESTON PARADE, LEEDS
+            "72160714",  # 31 PRESTON PARADE, LEEDS
+            "72160715",  # 32 PRESTON PARADE, LEEDS
+            "72201722",  # 22 STRATFORD TERRACE, LEEDS
+            "72663962",  # 26A STRATFORD TERRACE, LEEDS
+            "72659064",  # ROOM 2 22 STRATFORD TERRACE, LEEDS
+            "72686179",  # FLAT STAR INN 205 TONG ROAD, FARNLEY, LEEDS
+            "72288076",  # 58 INTAKE ROAD, PUDSEY
+            "72288078",  # 60 INTAKE ROAD, PUDSEY
+            "72288079",  # 62 INTAKE ROAD, PUDSEY
+            "72288080",  # 64 INTAKE ROAD, PUDSEY
+            "72288081",  # 66 INTAKE ROAD, PUDSEY
         ]:
             return None
 
         if record.addressline6 in [
             # SPLIT
             "LS10 4BD",
-            "LS25 1AX",
-            "WF3 2GL",
+            "LS7 2HN",
             "WF3 2GN",
+            "LS10 4AZ",
+            "LS12 2BN",
+            "LS25 1AX",
             "WF3 1TB",
             "LS9 6LP",
-            "WF3 1GQ",
-            "LS12 2BN",
+            "LS15 8JZ",
+            "LS13 3DX",
+            "WF3 2GL",
             "LS15 0LG",
-            "LS10 4AZ",
+            "WF3 1GQ",
             "LS17 9ED",
-            "LS8 2QG",
-            "WF3 1FX",
-            "LS10 4ET",
             "LS16 7SU",
+            "WF3 1FX",
             "LS18 5HN",
+            "LS8 2QG",
             # WRONG
             "LS2 7DJ",
-            "LS19 7RZ",
             "LS9 8DU",
+            "LS22 6RF",
+            "LS15 8FD",
+            "LS15 8QP",
+            "LS18 5EU",
+            "LS16 6FE",
+            "LS15 8TZ",
+            "LS15 8RQ",
+            "LS15 8RG",
+            "WF3 3LS",
+            "LS11 7PQ",
         ]:
             return None
 
         return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # Haigh Road Community Centre
-        if record.polling_place_id == "14987":
-            record = record._replace(
-                polling_place_easting="434066", polling_place_northing="428893"
-            )
-        return super().station_record_to_dict(record)
