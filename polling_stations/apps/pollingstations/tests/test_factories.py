746d03ada804487fabb5f893936193810d2d4811
GeoWill <roper.william@gmail.com>
2024-03-20 17:03:07 +0000

    Polling Station AcessibilityInformation model
    Includes editable inline form on PollingStation admin page.

--- polling_stations/apps/pollingstations/tests/test_factories.py
+++ polling_stations/apps/pollingstations/tests/test_factories.py
@@ -1,6 +1,9 @@
 from councils.models import Council
+from django.db import IntegrityError, transaction
 from django.test import TestCase
+from pollingstations.models import PollingStation
 from pollingstations.tests.factories import (
+    AccessibilityInformationFactory,
     AdvanceVotingStationFactory,
     PollingDistrictFactory,
     PollingStationFactory,
@@ -27,3 +30,19 @@ class TestAdvanceVotingStationFactory(TestCase):
             postcode="CF99 1SN", name="Welsh Parliament"
         )
         self.assertEqual(str(advance_station), "Welsh Parliament (CF99 1SN)")
+
+
+class TestAccessibilityInformationFactory(TestCase):
+    def test_accessibility_information_factory(self):
+        AccessibilityInformationFactory()
+        self.assertEqual(len(PollingStation.objects.all()), 1)
+
+    def test_accessibility_information_factory_constraint(self):
+        with self.assertRaises(IntegrityError), transaction.atomic():
+            AccessibilityInformationFactory(level_access=True, temporary_ramp=True)
+
+        AccessibilityInformationFactory(level_access=True, temporary_ramp=False)
+        AccessibilityInformationFactory(level_access=False, temporary_ramp=True)
+        AccessibilityInformationFactory(level_access=False, temporary_ramp=False)
+        AccessibilityInformationFactory(level_access=None, temporary_ramp=True)
+        AccessibilityInformationFactory(level_access=None, temporary_ramp=False)
