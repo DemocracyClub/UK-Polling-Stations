eca9106926d718b4855e04997452e2d2221d6eaf
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-21 08:34:42 +0000

    Import script for Stockton-on-Tees (2024-05-02) (closes #6733)

--- polling_stations/apps/data_importers/management/commands/import_stockton.py
+++ polling_stations/apps/data_importers/management/commands/import_stockton.py
@@ -4,10 +4,23 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "STT"
     addresses_name = (
-        "2023-06-22/2023-05-31T14:44:53.803777/Democracy_Club__22June2023.tsv"
+        "2024-05-02/2024-03-21T08:33:57.392759/Democracy_Club__02May2024 2.CSV"
     )
     stations_name = (
-        "2023-06-22/2023-05-31T14:44:53.803777/Democracy_Club__22June2023.tsv"
+        "2024-05-02/2024-03-21T08:33:57.392759/Democracy_Club__02May2024 2.CSV"
     )
-    elections = ["2023-06-22"]
-    csv_delimiter = "\t"
+    elections = ["2024-05-02"]
+
+    def station_record_to_dict(self, record):
+        # Mobile Polling Station, Grassed area to the side of The Merlin Pub, Marsh House Avenue, Billingham TS23 3SY
+        if record.polling_place_id == "15366":
+            record = record._replace(polling_place_postcode="TS23 3QJ")
+
+        return super().station_record_to_dict(record)
+
+    def address_record_to_dict(self, record):
+        if record.addressline6 in [
+            "TS17 9PA",  # split
+        ]:
+            return None
+        return super().address_record_to_dict(record)
