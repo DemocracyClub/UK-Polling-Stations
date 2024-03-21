41ae3349a09f429c16e7a37f924748024afffe0b
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-21 14:41:12 +0000

    Import script for Medway (2024-05-02) (closes #6752)

--- polling_stations/apps/data_importers/management/commands/import_medway.py
+++ polling_stations/apps/data_importers/management/commands/import_medway.py
@@ -3,18 +3,23 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "MDW"
-    addresses_name = "2023-05-04/2023-03-29T08:36:18.336726/Eros_SQL_Output007.csv"
-    stations_name = "2023-05-04/2023-03-29T08:36:18.336726/Eros_SQL_Output007.csv"
-    elections = ["2023-05-04"]
+    addresses_name = "2024-05-02/2024-03-21T14:40:57.823024/Eros_SQL_Output010.csv"
+    stations_name = "2024-05-02/2024-03-21T14:40:57.823024/Eros_SQL_Output010.csv"
+    elections = ["2024-05-02"]
 
     def address_record_to_dict(self, record):
-        if record.housepostcode in [
-            # split
-            "ME8 8DB",
-            "ME8 8QQ",
-            # Look wrong
-            "ME2 3TG",
+        uprn = record.uprn.strip().lstrip("0")
+
+        if uprn in [
+            "100062394561",  # WESTBANK FARM, CAPSTONE ROAD, GILLINGHAM
         ]:
             return None
 
+        if record.housepostcode in [
+            "ME8 8DB",  # split
+            # suspect
+            "ME1 2EZ",
+            "ME1 2FD",
+        ]:
+            return None
         return super().address_record_to_dict(record)
