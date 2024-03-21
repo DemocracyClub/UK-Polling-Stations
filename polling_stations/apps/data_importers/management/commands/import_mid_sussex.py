783e47073658a5a7f83e55fd0bfa8690dd67a5e7
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-21 11:19:13 +0000

    Import script for Mid Sussex (2024-05-02) (closes #6745)

--- polling_stations/apps/data_importers/management/commands/import_mid_sussex.py
+++ polling_stations/apps/data_importers/management/commands/import_mid_sussex.py
@@ -4,37 +4,23 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "MSS"
     addresses_name = (
-        "2023-05-04/2023-04-24T16:40:02.441254/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-03-21T11:19:04.829570/Democracy_Club__02May2024.tsv"
     )
     stations_name = (
-        "2023-05-04/2023-04-24T16:40:02.441254/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-03-21T11:19:04.829570/Democracy_Club__02May2024.tsv"
     )
-    elections = ["2023-05-04"]
-    csv_delimiter = "\t"
+    elections = ["2024-05-02"]
     csv_encoding = "windows-1252"
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "10070621576",  # 1 SAWMILLS COTTAGES, PADDOCKHURST ROAD, TURNERS HILL, CRAWLEY
-            "10070641437",  # SHOOTERS LODGE, BRANTRIDGE LANE, BALCOMBE, HAYWARDS HEATH
-            "100061873895",  # NORTHLANDS, STAIRBRIDGE LANE, BOLNEY, HAYWARDS HEATH
-            "10090944299",  # 10 OLDFIELD DRIVE, HAYWARDS HEATH
-            "10090944298",  # 9 OLDFIELD DRIVE, HAYWARDS HEATH
-            "10090944297",  # 8 OLDFIELD DRIVE, HAYWARDS HEATH
-            "10093413540",  # 7A OLDFIELD DRIVE, HAYWARDS HEATH
-            "100062485213",  # 3 ROSE COTTAGES, SNOWDROP LANE, LINDFIELD, HAYWARDS HEATH
-        ]:
-            return None
-
-        if record.post_code in [
-            # splits
+        if record.addressline6.replace("\xa0", " ") in [
+            # split
             "RH17 7DY",
             "BN6 9NA",
             "RH16 2QB",
-            "RH17 7GD",  # PRIMROSE WAY, HAYWARDS HEATH
+            # suspect
+            "RH10 4SH",
         ]:
             return None
-
         return super().address_record_to_dict(record)
