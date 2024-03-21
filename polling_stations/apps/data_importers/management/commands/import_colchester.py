dcd5e5340e457a7e8f7bc9d22d6276e568e9d8b7
awdem <wdemontmollin@gmail.com>
2024-03-21 09:25:51 +0000

    fix from colchester council

--- polling_stations/apps/data_importers/management/commands/import_colchester.py
+++ polling_stations/apps/data_importers/management/commands/import_colchester.py
@@ -15,7 +15,7 @@ class Command(BaseXpressDemocracyClubCsvImporter):
     def station_record_to_dict(self, record):
         # Old Heath Community Centre, D'Arcy Road, Old Heath, Colchester, CO2 8BB
         if record.polling_place_id == "12587":
-            record = record._replace(polling_place_postcode="")
+            record = record._replace(polling_place_postcode="CO2 8BA")
 
         return super().station_record_to_dict(record)
 
@@ -32,33 +32,6 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "CO2 8BU",
             "CO4 5LG",
             "CO6 1HA",
-            # suspect (waiting on council response)
-            "CO4 5HG",
-            "CO4 6AU",
-            "CO4 6AW",
-            "CO4 6AX",
-            "CO4 6BE",
-            "CO4 6BG",
-            "CO4 6BH",
-            "CO4 6BP",
-            "CO4 6BS",
-            "CO4 6BD",
-            "CO4 6BB",
-            "CO4 6BF",
-            "CO4 6BJ",
-            "CO4 6BL",
-            "CO4 6BN",
-            "CO4 6BQ",
-            "CO4 6BU",
-            "CO4 6BX",
-            "CO4 6AY",
-            "CO4 6BR",
-            "CO4 6BT",
-            "CO4 6BW",
-            "CO4 6BY",
-            "CO4 6BZ",
-            "CO4 6DB",
-            "CO4 6DH",
         ]:
             return None
 
