526c69cf929846543fcab1827f18a2ee4cfb4319
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-22 11:00:19 +0000

    Import script for East Riding of Yorkshire (2024-05-02) (closes #6762)

--- polling_stations/apps/data_importers/management/commands/import_east_riding.py
+++ polling_stations/apps/data_importers/management/commands/import_east_riding.py
@@ -3,15 +3,18 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "ERY"
-    addresses_name = "2023-05-04/2023-04-25T18:30:43.685241/Democracy Club - LOCAL polling districts 2023.csv"
-    stations_name = "2023-05-04/2023-04-25T18:30:43.685241/Democracy Club - LOCAL polling stations 2023.csv"
-    elections = ["2023-05-04"]
+    addresses_name = "2024-05-02/2024-03-22T11:00:10.036289/Democracy Club - Polling Districts - PCC.csv"
+    stations_name = "2024-05-02/2024-03-22T11:00:10.036289/Democracy Club - Polling Stations - PCC.csv"
+    elections = ["2024-05-02"]
+    csv_encoding = "utf-16le"
 
     def address_record_to_dict(self, record):
-        if record.postcode in [
-            # split
-            "HU18 1EH",
+        uprn = record.uprn.strip.lstrip("0")
+
+        if uprn in [
+            "10093602661",  # APARTMENT 26, ROGERSON COURT, SCAIFE GARTH, POCKLINGTON, YORK
+            "10095943069",  # FLAT 3, 74 HALLGATE, COTTINGHAM
+            "10095943071",  # FLAT 5, 74 HALLGATE, COTTINGHAM
         ]:
             return None
-
         return super().address_record_to_dict(record)
