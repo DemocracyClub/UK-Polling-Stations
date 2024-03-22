6d6a57d40c18da4057ad90fdd2f2cfc47cbc2c3c
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-22 10:01:24 +0000

    Import script for Sunderland (2024-05-02) (closes #6760)

--- polling_stations/apps/data_importers/management/commands/import_sunderland.py
+++ polling_stations/apps/data_importers/management/commands/import_sunderland.py
@@ -4,39 +4,44 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SND"
     addresses_name = (
-        "2023-05-04/2023-04-05T14:43:52.611477/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-03-22T10:01:04.870722/Democracy_Club__02May2024.CSV"
     )
     stations_name = (
-        "2023-05-04/2023-04-05T14:43:52.611477/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-03-22T10:01:04.870722/Democracy_Club__02May2024.CSV"
     )
-    elections = ["2023-05-04"]
-    csv_encoding = "windows-1252"
-    csv_delimiter = "\t"
+    elections = ["2024-05-02"]
+
+    def station_record_to_dict(self, record):
+        # Plains Farm Youth and Community Centre, Grounds of Plains Farm Academy, Tudor Grove, Sunderland
+        if record.polling_place_id == "18488":
+            record = record._replace(
+                polling_place_easting="438000",
+            )
+        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         if record.addressline6 in [
             # split
-            "DH4 7RD",
-            "SR2 0AQ",
-            "SR2 9JG",
-            "SR4 8JF",
             "DH4 5HY",
-            "SR2 8RA",
-            "SR5 3EP",
-            "SR4 6NP",
-            "DH4 4JH",
-            "SR6 9DY",
+            "SR2 0AQ",
             "SR2 7HZ",
             "SR2 0LE",
+            "DH4 7RD",
+            "SR4 8JF",
+            "SR4 6NP",
+            "SR6 9DY",
             "SR4 7SD",
-            "SR3 1XF",
-            "DH4 6SN",
             "SR4 0BT",
-            "SR4 8HA",
-            "DH4 4FL",
-            "SR4 8JU",
+            "SR3 1XF",
+            "SR2 9JG",
+            "SR5 3EP",
+            "DH4 4JH",
             "SR6 0NB",
+            "SR2 8RA",
+            "SR4 8HA",
+            "SR4 8JU",
             "SR3 3QH",
         ]:
             return None
+
         return super().address_record_to_dict(record)
