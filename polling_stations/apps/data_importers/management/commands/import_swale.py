fd016b47d33fcc87f861761d083ef8fad7feb650
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-22 14:00:28 +0000

    Import script for Swale (2024-05-02) (closes #6765)

--- polling_stations/apps/data_importers/management/commands/import_swale.py
+++ polling_stations/apps/data_importers/management/commands/import_swale.py
@@ -4,29 +4,34 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 class Command(BaseHalaroseCsvImporter):
     council_id = "SWL"
     addresses_name = (
-        "2023-05-04/2023-03-13T13:50:50.268451/Democracy Club Swale PS export.csv"
+        "2024-05-02/2024-03-22T14:00:13.485984/SWALE_Eros_SQL_Output001.csv"
     )
-    stations_name = (
-        "2023-05-04/2023-03-13T13:50:50.268451/Democracy Club Swale PS export.csv"
-    )
-    elections = ["2023-05-04"]
+    stations_name = "2024-05-02/2024-03-22T14:00:13.485984/SWALE_Eros_SQL_Output001.csv"
+    elections = ["2024-05-02"]
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
         if record.housepostcode in [
             # split
             "ME10 2EF",
-            "ME10 3TU",
-            "ME12 1TF",
             "ME12 2SG",
-            # LOOK WRONG
-            "ME10 2JH",
-            "ME10 2JQ",
+            "ME12 1TF",
+            "ME10 3TU",
+            # suspect
+            "ME12 1RW",
+            "ME12 1RN",
         ]:
             return None
 
         if uprn in [
             "200002533735",  # POPPINGTON BUNGALOW, WHITE HILL, SELLING, FAVERSHAM
+            "100061091546",  # 31 HAMBROOK WALK, SITTINGBOURNE
+            "100061091542",  # 27 HAMBROOK WALK, SITTINGBOURNE
+            "100061091544",  # 29 HAMBROOK WALK, SITTINGBOURNE
+            "100061091540",  # 25 HAMBROOK WALK, SITTINGBOURNE
+            "10093084893",  # 3 HOUSSON AVENUE, SITTINGBOURNE
+            "10093084895",  # 5 HOUSSON AVENUE, SITTINGBOURNE
+            "10093084891",  # 1 HOUSSON AVENUE, SITTINGBOURNE
         ]:
             return None
 
