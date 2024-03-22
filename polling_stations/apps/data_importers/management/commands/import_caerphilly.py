1dd8315c290ae459a9e06c22136f5c07104d6111
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-22 14:36:58 +0000

    Import script for Caerphilly (2024-05-02) (closes #6767)

--- polling_stations/apps/data_importers/management/commands/import_caerphilly.py
+++ polling_stations/apps/data_importers/management/commands/import_caerphilly.py
@@ -1,49 +1,22 @@
-from addressbase.models import Address, UprnToCouncil
-from core.opening_times import OpeningTimes
 from data_importers.management.commands import BaseHalaroseCsvImporter
-from data_importers.mixins import AdvanceVotingMixin
-from pollingstations.models import AdvanceVotingStation
 
 
-class Command(BaseHalaroseCsvImporter, AdvanceVotingMixin):
+class Command(BaseHalaroseCsvImporter):
     council_id = "CAY"
-    addresses_name = (
-        "2022-05-05/2022-04-06T17:29:17.555011/polling_station_export-2022-04-06.csv"
-    )
-    stations_name = (
-        "2022-05-05/2022-04-06T17:29:17.555011/polling_station_export-2022-04-06.csv"
-    )
-    elections = ["2022-05-05"]
+    addresses_name = "2024-05-02/2024-03-22T14:36:48.765501/Eros_SQL_Output001.csv"
+    stations_name = "2024-05-02/2024-03-22T14:36:48.765501/Eros_SQL_Output001.csv"
+    elections = ["2024-05-02"]
 
     def address_record_to_dict(self, record):
+        uprn = record.uprn.strip().lstrip("0")
+
+        if uprn in [
+            "43171669",  # THE LODGE, STABLES COMPOUND, WEST ROAD, PENALLTA INDUSTRIAL ESTATE, PENALLTA, HENGOED
+        ]:
+            return None
         if record.housepostcode in [
-            "NP22 5HU",
-            "NP10 9GG",
             "NP11 6JE",
             "CF83 8RL",
         ]:
-            return None  # split
-
+            return None
         return super().address_record_to_dict(record)
-
-    def add_advance_voting_stations(self):
-        opening_times = OpeningTimes()
-        opening_times.add_open_time("2022-04-30", "10:00", "16:00")
-        opening_times.add_open_time("2022-05-01", "10:00", "16:00")
-
-        advance_station = AdvanceVotingStation(
-            name="Council Headquarters",
-            address="""Penallta House,
-            Tredomen Business Park,
-            Ystrad Mynach,
-            Hengoed
-                """,
-            postcode="CF82 7PG",
-            location=Address.objects.get(uprn=43164445).location,
-            opening_times=opening_times.as_string_table(),
-            council=self.council,
-        )
-        advance_station.save()
-        UprnToCouncil.objects.filter(lad=self.council.geography.gss).update(
-            advance_voting_station=advance_station
-        )
