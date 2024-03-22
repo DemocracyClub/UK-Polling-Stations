eaed40c99531aef4199e471a0049a90075a60397
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-22 16:09:00 +0000

    Import script for Vale of Glamorgan (2024-05-02) (closes #6770)

--- polling_stations/apps/data_importers/management/commands/import_vale_of_glamorgan.py
+++ polling_stations/apps/data_importers/management/commands/import_vale_of_glamorgan.py
@@ -3,14 +3,33 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "VGL"
-    addresses_name = "2022-05-05/2022-03-11T14:01:39.044454/democracy club polling districts vale of glamorgan.csv"
-    stations_name = "2022-05-05/2022-03-11T14:01:39.044454/democracy club polling stations vale of glamorgan.csv"
-    elections = ["2022-05-05"]
-    csv_encoding = "windows-1252"
+    addresses_name = (
+        "2024-05-02/2024-03-22T16:08:51.555473/Democracy Club Polling Districts.csv"
+    )
+    stations_name = (
+        "2024-05-02/2024-03-22T16:08:51.555473/Democracy Club Polling Stations.csv"
+    )
+    elections = ["2024-05-02"]
+    csv_encoding = "utf-16le"
+
+    def station_record_to_dict(self, record):
+        # ROMILLY BOWLING CLUB, ROMILLY PARK ROAD, BARRY, VALE OF GLAMORGAN CF62 6RB
+        if (record.stationcode, record.placename) == ("2", "ROMILLY BOWLING CLUB"):
+            record = record._replace(postcode="")
+
+        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
+        uprn = record.uprn.strip().lstrip("0")
+        if uprn in [
+            "64105362",  # FLAT GLAMORGANSHIRE GOLF CLUB LAVERNOCK ROAD, PENARTH
+            "64116062",  # FIVE ACRES, ST. HILARY, COWBRIDGE
+        ]:
+            return None
+
         if record.postcode in [
-            "CF62 6BA",
+            "CF62 6BA",  # split
+            "CF71 7PY",  # suspect
         ]:
             return None
         return super().address_record_to_dict(record)
