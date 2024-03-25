a480db38e9cc2db6d4615f42b2929a68d11493c7
awdem <wdemontmollin@gmail.com>
2024-03-25 09:31:50 +0000

    coordinate fixes from folkestone council

--- polling_stations/apps/data_importers/management/commands/import_folkestone_hythe.py
+++ polling_stations/apps/data_importers/management/commands/import_folkestone_hythe.py
@@ -24,10 +24,41 @@ class Command(BaseDemocracyCountsCsvImporter):
         # New: Hawkinge Pavilion and Sports Ground, Pavilion Road, Hawkinge, Kent, CT18 7UA
         if record.stationcode == "28":
             record = record._replace(
-                xordinate="621844", yordinate="140755", postcode="CT18 7UA"
+                xordinate="621844",
+                yordinate="140755",
+                postcode="CT18 7UA",
+                placename="Hawkinge Pavilion and Sports Ground",
+                add1="Pavilion Road",
             )
         # 1st Lyminge Scout Hut, Woodland Road, Lyminge, Kent CT18 8EW
         if record.stationcode == "42":
             record = record._replace(xordinate="615894", yordinate="140864")
+        # Pembroke Court, Dover Road, Folkestone, CT20 1TA
+        if record.stationcode == "15":
+            record = record._replace(xordinate="623254", yordinate="136698")
+        # New Romney Methodist Church Hall, High Street, New Romney, Kent, TN28 8AH
+        if record.stationcode == "26":
+            record = record._replace(xordinate="606553", yordinate="124860")
+        # New Romney Methodist Church Hall, High Street, New Romney, Kent, TN28 8AH
+        if record.stationcode == "26":
+            record = record._replace(xordinate="606553", yordinate="124860")
+        # Hawkinge Cricket Club, Cricketers Close, Hawkinge, Folkestone, CT18 7NH
+        if record.stationcode == "27":
+            record = record._replace(xordinate="621904", yordinate="140713")
+        # Hawkinge Community Centre, Heron Forstal Avenue, Hawkinge, Folkestone, Kent, CT18 7FP
+        if record.stationcode in [
+            "29",
+            "30",
+        ]:
+            record = record._replace(xordinate="621563", yordinate="139795")
+        # Stelling Minnis Village Hall, Bossingham Road, Stelling Minnis, Kent, CT4 6AG
+        if record.stationcode == "41":
+            record = record._replace(xordinate="614748", yordinate="146625")
+        # Rose and Crown, Swamp Road, Old Romney, Romney Marsh, Kent, TN29 9SQ
+        if record.stationcode == "55":
+            record = record._replace(xordinate="603097", yordinate="125105")
+        # Brookland Village Hall, Boarmans Lane, Brookland, Kent, TN29 9QZ
+        if record.stationcode == "57":
+            record = record._replace(xordinate="598840", yordinate="125772")
 
         return super().station_record_to_dict(record)
