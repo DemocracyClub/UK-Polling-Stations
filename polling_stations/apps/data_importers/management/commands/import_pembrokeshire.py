5f3469163704488026a20b46b3803b766ebf2175
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-25 14:51:16 +0000

    Import script for Pembrokeshire (2024-05-02) (closes #6785)

--- polling_stations/apps/data_importers/management/commands/import_pembrokeshire.py
+++ polling_stations/apps/data_importers/management/commands/import_pembrokeshire.py
@@ -1,47 +1,41 @@
-import re
-
-from data_importers.github_importer import BaseGitHubImporter
+from data_importers.management.commands import BaseHalaroseCsvImporter
 
 
-class Command(BaseGitHubImporter):
-    srid = 4326
-    districts_srid = 4326
+class Command(BaseHalaroseCsvImporter):
     council_id = "PEM"
-    elections = ["2022-05-05"]
-    scraper_name = "wdiv-scrapers/DC-PollingStations-Pembrokeshire"
-    geom_type = "geojson"
-    seen_stations = set()
+    addresses_name = "2024-05-02/2024-03-25T14:51:07.747895/Eros_SQL_Output001.csv"
+    stations_name = "2024-05-02/2024-03-25T14:51:07.747895/Eros_SQL_Output001.csv"
+    elections = ["2024-05-02"]
 
-    def district_record_to_dict(self, record):
-        poly = self.extract_geometry(record, self.geom_type, self.get_srid("districts"))
-        return {
-            "internal_council_id": record["DistrictRef"],
-            "name": record["DistrictName"],
-            "area": poly,
-        }
-
-    def station_record_to_dict(self, record):
-        if record["County"] == "Carmarthenshire":
+    def address_record_to_dict(self, record):
+        record.uprn.strip().lstrip("0")
+        if record.uprn in [
+            "200003255203",  # LOWER FLAT, FERN HOUSE, WARREN STREET, TENBY
+        ]:
             return None
-
-        location = self.extract_geometry(
-            record, self.geom_type, self.get_srid("stations")
-        )
-        codes = re.findall("[A-Z]{2}[0-9][A-Z]?", record["DistricttRef"])
-        stations = []
-        for code in codes:
-            if code not in self.seen_stations:
-                self.seen_stations.add(code)
-                stations.append(
-                    {
-                        "internal_council_id": code,
-                        "postcode": "",
-                        "address": record["StationName"],
-                        "location": location,
-                        "polling_district_id": code,
-                    }
-                )
-            else:
-                print(f"Discarding duplicate station for code {code}")
-
-        return stations
+        if record.housepostcode in [
+            # split
+            "SA73 2EB",
+            "SA62 4NJ",
+            "SA73 3RA",
+            "SA68 0XN",
+            "SA73 1NR",
+            "SA62 3PW",
+            "SA71 5BP",
+            "SA73 1BB",
+            "SA68 0QR",
+            "SA73 1RG",
+            "SA73 3HF",
+            "SA62 5RJ",
+            "SA72 6BH",
+            "SA72 6BE",
+            "SA62 5UD",
+            "SA67 8RY",
+            "SA62 5DB",
+            "SA62 6TD",
+            "SA62 5NL",
+            "SA73 1RJ",
+            "SA66 7QW",
+        ]:
+            return None
+        return super().address_record_to_dict(record)
