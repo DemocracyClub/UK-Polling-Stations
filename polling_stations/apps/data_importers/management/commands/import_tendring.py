54ddf2be78c2f94e14525f005530a7907a8e3237
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-25 14:34:58 +0000

    Import script for Tendring (2024-05-02) (closes #6783)

--- polling_stations/apps/data_importers/management/commands/import_tendring.py
+++ polling_stations/apps/data_importers/management/commands/import_tendring.py
@@ -3,30 +3,26 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "TEN"
-    addresses_name = (
-        "2023-05-04/2023-04-12T10:02:51.779215/Democracy Club - Polling Districts.tsv"
-    )
-    stations_name = (
-        "2023-05-04/2023-04-12T10:02:51.779215/Democracy Club - Polling Stations.tsv"
-    )
-    elections = ["2023-05-04"]
-    csv_delimiter = "\t"
+    addresses_name = "2024-05-02/2024-04-11T11:20:01.019608/DC POlling Districts.csv"
+    stations_name = "2024-05-02/2024-04-11T11:20:01.019608/DC Polling Stations.csv"
+    elections = ["2024-05-02"]
+    csv_encoding = "utf-16le"
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
 
         if uprn in [
-            "10094248268",  # 43 MIDDLETON MEWS, BRIGHTLINGSEA, COLCHESTER
+            "10094250773",  # 5A CHAPEL LANE, THORRINGTON, COLCHESTER
         ]:
             return None
-
         if record.postcode in [
-            # split
-            "CO13 0FZ",
-            "CO16 9UQ",
-            "CO15 7AP",
-            "CO14 8GH",
+            "CO12 4QT",  # split
+            # suspect
+            "CO15 1HW",
+            "CO15 3BN",
+            "CO15 1JG",
+            "CO15 1HN",
+            "CO15 1JL",
         ]:
             return None
-
         return super().address_record_to_dict(record)
