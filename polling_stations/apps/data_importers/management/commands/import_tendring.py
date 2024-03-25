f69fc5c216bb088f5c4b143b2894c4aae022c70f
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-25 14:34:58 +0000

    Import script for Tendring (2024-05-02) (closes #6783)

--- polling_stations/apps/data_importers/management/commands/import_tendring.py
+++ polling_stations/apps/data_importers/management/commands/import_tendring.py
@@ -3,30 +3,7 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "TEN"
-    addresses_name = (
-        "2023-05-04/2023-04-12T10:02:51.779215/Democracy Club - Polling Districts.tsv"
-    )
-    stations_name = (
-        "2023-05-04/2023-04-12T10:02:51.779215/Democracy Club - Polling Stations.tsv"
-    )
-    elections = ["2023-05-04"]
-    csv_delimiter = "\t"
-
-    def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
-
-        if uprn in [
-            "10094248268",  # 43 MIDDLETON MEWS, BRIGHTLINGSEA, COLCHESTER
-        ]:
-            return None
-
-        if record.postcode in [
-            # split
-            "CO13 0FZ",
-            "CO16 9UQ",
-            "CO15 7AP",
-            "CO14 8GH",
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
+    addresses_name = "2024-05-02/2024-03-25T14:34:45.437403/DC Polling Districts.csv"
+    stations_name = "2024-05-02/2024-03-25T14:34:45.437403/DC Polling stations.csv"
+    elections = ["2024-05-02"]
+    csv_encoding = "utf-16le"
