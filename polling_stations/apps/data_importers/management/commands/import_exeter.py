d40baa4ec8b7f531c7aa66422f4b7fb3637d870d
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-26 16:59:14 +0000

    Import script for Exeter (2024-05-02) (closes #6795)

--- polling_stations/apps/data_importers/management/commands/import_exeter.py
+++ polling_stations/apps/data_importers/management/commands/import_exeter.py
@@ -3,33 +3,28 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "EXE"
-    addresses_name = (
-        "2023-05-04/2023-04-06T16:06:54.372260/Democracy_Club__04May2023.tsv"
-    )
-    stations_name = (
-        "2023-05-04/2023-04-06T16:06:54.372260/Democracy_Club__04May2023.tsv"
-    )
-    elections = ["2023-05-04"]
-    csv_delimiter = "\t"
+    addresses_name = "2024-05-02/2024-04-03T11:11:17.239870/Democracy_Club__02May2024-Exeter City.CSV"
+    stations_name = "2024-05-02/2024-04-03T11:11:17.239870/Democracy_Club__02May2024-Exeter City.CSV"
+    elections = ["2024-05-02"]
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "10091473295",  # EXE VIEW LODGE STOKE HILL, EXETER
-            "100040222032",  # ARGYLL GARDENS, LOWER ARGYLL ROAD, EXETER
-            "10023116924",  # PARK HOUSE, PARK LANE, EXETER
+            "10094735127",  # 1 COLLINGWOOD CLOSE, TOPSHAM, EXETER
+            "10094735128",  # 2 COLLINGWOOD CLOSE, TOPSHAM, EXETER
             "10023121146",  # 210A TOPSHAM ROAD, EXETER
-            "100040241841",  # BELLENDEN, WREFORDS LANE, EXETER
+            "10091473295",  # EXE VIEW LODGE, STOKE HILL, EXETER
+            "10094202708",  # THE OLD MESS ROOM, ST. MARKS AVENUE, EXETER
         ]:
             return None
 
         if record.addressline6 in [
+            # split
             "EX2 7AY",
             "EX4 9HE",
-            "EX2 7TF",
             "EX4 5AJ",
+            "EX2 7TF",
         ]:
             return None
-
         return super().address_record_to_dict(record)
