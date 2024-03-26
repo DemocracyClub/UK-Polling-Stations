e2ac9e640384718ea8a51484afd53b6fb1372248
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-26 15:55:19 +0000

    Import script for Ceredigion (2024-05-02) (closes #6793)

--- polling_stations/apps/data_importers/management/commands/import_ceredigion.py
+++ polling_stations/apps/data_importers/management/commands/import_ceredigion.py
@@ -3,39 +3,47 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "CGN"
-    addresses_name = "2022-05-05/2022-04-14T13:37:04.763415/Democracy Club - Polling Districts CEREDIGION.csv"
-    stations_name = "2022-05-05/2022-04-14T13:37:04.763415/Democracy Club - Polling Stations CEREDIGION.csv"
-    elections = ["2022-05-05"]
-    csv_encoding = "windows-1252"
+    addresses_name = (
+        "2024-05-02/2024-03-26T15:55:08.555130/Democracy Club - Polling Districts.csv"
+    )
+    stations_name = (
+        "2024-05-02/2024-03-26T15:55:08.555130/Democracy Club - Polling Stations.csv"
+    )
+    elections = ["2024-05-02"]
+    csv_encoding = "utf-16le"
 
     def address_record_to_dict(self, record):
-        uprn = record.uprn.lstrip("0")
+        uprn = record.uprn.strip().lstrip("0")
 
         if uprn in [
-            "49062840",
-            "49049138",
-            "49069702",
-            "49047840",
-            "49040771",
-            "49065819",
-            "49069384",
-            "49075931",
-            "49037718",
-            "49062368",
-            "49061701",
-            "49041491",
-            "49040891",
-            "49073822",
+            "49062840",  # TY GWYN, LLANARTH
+            "49049138",  # AWEL Y BRYN, RHYDLEWIS, LLANDYSUL
+            "49069702",  # TYDDYN MEHERYN, RHYDLEWIS, LLANDYSUL
+            "49047840",  # ALMA, TANYGROES, CARDIGAN
+            "49040771",  # TROEDYRHIW, BETHANIA, LLANON
+            "49065819",  # THE WILLOW, CROSS INN, LLANON
+            "49069384",  # TROEDYRHIW CAPELI, CRIBYN, LAMPETER
+            "49075931",  # BRYN BARCUD, GORSGOCH, LLANYBYDDER
+            "49037718",  # CARTREFLE, MYDROILYN, LAMPETER
+            "49062368",  # TY GWALIA, LLWYNYGROES, TREGARON
+            "49061701",  # GER Y NANT, CAPEL BANGOR, ABERYSTWYTH
+            "49040891",  # TYNGWNDWN, LLANFARIAN, ABERYSTWYTH
+            "49065752",  # TY'R IET, CARDIGAN
+            "49063434",  # CARDIGAN SAND & GRAVEL CO LTD, CNWC Y SAESON, PENPARC, CARDIGAN
+            "49047095",  # CRYNGA MAWR, BLAENANNERCH, CARDIGAN
+            "200001099702",  # PLASBRYNIAU, LADY ROAD, BLAENPORTH, CARDIGAN
+            "49048192",  # MAESGWYN, GLYNARTHEN, LLANDYSUL
+            "49038741",  # TEGLAN, CILIAU AERON, LAMPETER
         ]:
             return None
 
-        if record.postcode in ["SA43 1PT"]:
+        if record.postcode in [
+            # split
+            "SA44 5UB",
+            "SA43 1PT",
+            # suspect
+            "SA44 5NQ",
+            "SY23 5JP",
+        ]:
             return None
         return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # CANOLFAN CYMUNEDOL MYNACH COMMUNITY CENTRE
-        if record.stationcode == "10-1026/1027":
-            record = record._replace(postcode="")
-
-        return super().station_record_to_dict(record)
