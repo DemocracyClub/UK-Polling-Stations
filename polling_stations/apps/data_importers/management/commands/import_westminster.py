36f8565413f4ccdeddbc3b6c3c9e72682cb5ddbc
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-27 14:42:29 +0000

    Import script for City of Westminster (2024-05-02) (closes #6813)

--- polling_stations/apps/data_importers/management/commands/import_westminster.py
+++ polling_stations/apps/data_importers/management/commands/import_westminster.py
@@ -3,60 +3,33 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "WSM"
-    addresses_name = (
-        "2022-05-05/2022-03-04T15:30:07.536856/polling_station_export-2022-03-04.csv"
-    )
-    stations_name = (
-        "2022-05-05/2022-03-04T15:30:07.536856/polling_station_export-2022-03-04.csv"
-    )
-    elections = ["2022-05-05"]
-
-    def station_record_to_dict(self, record):
-        # Danubius Hotel
-        if record.pollingstationnumber == "74":
-            rec = super().station_record_to_dict(record)
-            rec[
-                "address"
-            ] = "Danubius Hotel\n18 Lodge Road\n(Entrance on Park Road)\nSt John's Wood\nLondon"
-            return rec
-
-        # Seymour Leisure Centre
-        if record.pollingstationnumber == "48":
-            rec = super().station_record_to_dict(record)
-            rec[
-                "address"
-            ] = "Seymour Leisure Centre\nSeymour Place\n(Entrance on Shouldham Street)\nLondon"
-            return rec
-
-        return super().station_record_to_dict(record)
+    addresses_name = "2024-05-02/2024-03-27T14:42:03.748146/Eros_SQL_Output001.csv"
+    stations_name = "2024-05-02/2024-03-27T14:42:03.748146/Eros_SQL_Output001.csv"
+    elections = ["2024-05-02"]
 
     def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
         uprn = record.uprn.strip().lstrip("0")
 
         if uprn in [
             "10033552804",  # FLAT 3, 2 MORETON CLOSE, LONDON
             "10033618772",  # THIRD FLOOR 48 CHANDOS PLACE, LONDON
-            "100022803552",  # 9 ST.BARNABAS STREET, LONDON
             "100022749688",  # 50 ELNATHAN MEWS, LONDON
+            "10033529430",  # HYDE PARK BUCK HILL LODGE BAYSWATER ROAD, LONDON
+            "100023382377",  # QUEENS GATE LODGE, HYDE PARK GATE, LONDON
         ]:
             return None
 
         if record.housepostcode in [
-            "W1K 7JB",
+            # split
+            "W2 5HA",
             "W2 6PF",
             "SW1P 4JZ",
-            "W1U 8BD",
             "NW8 8LH",
-            "W10 4PR",
-            "W2 5HA",
+            "W1K 7JB",
             "W9 3DW",
             "W9 2AL",
             "SW1V 4AF",
-            "NW8 6DT",
-            "NW8 6DU",
-            "W1K 2HX",
         ]:
             return None
 
-        return rec
+        return super().address_record_to_dict(record)
