bd06c612f78a67b1409523f2dd1eec5d7b967704
polling-bot-4000 <developers@democracyclub.org.uk>
2024-03-27 11:40:05 +0000

    Import script for Merthyr Tydfil (2024-05-02) (closes #6805)

--- polling_stations/apps/data_importers/management/commands/import_merthyr.py
+++ polling_stations/apps/data_importers/management/commands/import_merthyr.py
@@ -3,32 +3,22 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "MTY"
-    addresses_name = "2022-05-05/2022-03-25T08:16:11.219335/Eros_SQL_Output004.csv"
-    stations_name = "2022-05-05/2022-03-25T08:16:11.219335/Eros_SQL_Output004.csv"
-    elections = ["2022-05-05"]
+    addresses_name = "2024-05-02/2024-03-27T11:39:56.682597/Eros_SQL_Output004.csv"
+    stations_name = "2024-05-02/2024-03-27T11:39:56.682597/Eros_SQL_Output004.csv"
+    elections = ["2024-05-02"]
 
     def station_record_to_dict(self, record):
-        if record.pollingstationnumber == "55":
-            # PONTSTICILL MEMORIAL HALL, CF48 2UR
-            # ignore 10034657547 Council says it's CF48 2UD
+        # PONTSTICILL MEMORIAL HALL, CF48 2UR
+        if record.pollingstationnumber == "54":
             record = record._replace(pollingstationpostcode="CF48 2UD")
-
-        # Station Change from council
-        # REAR OF THE WINDSOR HOTEL & BAR
-        if record.pollingstationnumber == "37":
-            record = record._replace(
-                pollingstationname="MOBILE POLLING STATION",
-                pollingstationaddress_1="BELLE VUE TERRACE",
-                pollingstationaddress_2="MERTHYR VALE",
-                pollingstationaddress_3="MERTHYR TYDFIL",
-                pollingstationpostcode="CF48 4SP",
-            )
-
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
-        if uprn in ["10034658436"]:
+        if uprn in [
+            "10034658436",  # TRIGG BROS, TY-NEWYDD FARM, TRELEWIS, TREHARRIS
+            "200001851107",  # TYN-Y-COEDCAE FARM, SWANSEA ROAD, MERTHYR TYDFIL
+        ]:
             return None
         if record.housepostcode in [
             "CF48 1TL",  # split
