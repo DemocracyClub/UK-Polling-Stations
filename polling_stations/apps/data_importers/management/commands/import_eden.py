1c5df8b0a147f5fdbc35f6a1218da855a78aa935
polling-bot-4000 <developers@democracyclub.org.uk>
2024-04-04 12:10:48 +0100

    Import script for Westmorland and Furness (formerly Eden) (2024-05-02) (closes #6858)

--- polling_stations/apps/data_importers/management/commands/import_eden.py
+++ polling_stations/apps/data_importers/management/commands/import_eden.py
@@ -1,34 +1,58 @@
+from addressbase.models import Address, UprnToCouncil
 from data_importers.management.commands import BaseHalaroseCsvImporter
+from django.core.exceptions import ObjectDoesNotExist
 
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "EDN"
-    addresses_name = (
-        "2022-05-05/2022-03-30T12:29:45.352395/polling_station_export-2022-03-24e.csv"
-    )
-    stations_name = (
-        "2022-05-05/2022-03-30T12:29:45.352395/polling_station_export-2022-03-24e.csv"
-    )
-    elections = ["2022-05-05"]
+    addresses_name = "2024-05-02/2024-04-04T12:10:32.315869/Eros_SQL_Output003.csv"
+    stations_name = "2024-05-02/2024-04-04T12:10:32.315869/Eros_SQL_Output003.csv"
+    elections = ["2024-05-02"]
+
+    def pre_import(self):
+        # We need to consider rows that don't have a uprn when importing data.
+        # However there are lots of rows for other councils in this file.
+        # So build a list of stations from rows that do have UPRNS
+        # and then use that list of stations to make sure we check relevant rows, even if they don't have a UPRN
+
+        council_uprns = set(
+            UprnToCouncil.objects.filter(lad=self.council.geography.gss).values_list(
+                "uprn", flat=True
+            )
+        )
+        self.COUNCIL_STATIONS = set()
+        data = self.get_addresses()
+
+        for record in data:
+            if record.uprn in council_uprns:
+                self.COUNCIL_STATIONS.add(record.pollingstationnumber)
 
     def address_record_to_dict(self, record):
-        if record.housepostcode in [
-            "CA11 7UR",
-            "CA11 8BX",
-            "CA10 2DQ",
-            "CA11 9HZ",
-            "CA10 2BZ",
-            "CA4 0HR",
-            "CA9 3BU",
-            "CA11 9RJ",
-        ]:
-            return None  # split
-
-        uprn = record.uprn.lstrip("0")
-
-        if uprn in [
-            "10070544465",  # BRAMBLE HOUSE ACCESS ROAD TO MEADOW FLATT FROM THE B6277, ALSTON
-        ]:
+        if record.pollingstationnumber not in self.COUNCIL_STATIONS:
             return None
 
+        if record.housepostcode in [
+            # split
+            "CA4 0HR",
+            "CA11 9HZ",
+            "CA11 8BX",
+            "CA9 3ND",
+            "CA10 2DQ",
+            "CA11 7UR",
+        ]:
+            return None
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        if record.pollingstationnumber not in self.COUNCIL_STATIONS:
+            return None
+        return super().station_record_to_dict(record)
+
+    # quick fix to show maps for Halarose records that have a valid UPRN in the PollingVenueUPRN field
+    def get_station_point(self, record):
+        uprn = record.pollingvenueuprn.strip().lstrip("0")
+        try:
+            ab_rec = Address.objects.get(uprn=uprn)
+            return ab_rec.location
+        except ObjectDoesNotExist:
+            return super().get_station_point(record)
