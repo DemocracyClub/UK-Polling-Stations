f0d38d34cff9e5118e94645b4c3aad19a7013eff
polling-bot-4000 <developers@democracyclub.org.uk>
2024-04-08 17:18:03 +0100

    Import script for Tameside (2024-05-02) (closes #6879)

--- polling_stations/apps/data_importers/management/commands/import_tameside.py
+++ polling_stations/apps/data_importers/management/commands/import_tameside.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "TAM"
     addresses_name = (
-        "2023-05-04/2023-04-12T13:11:26.820087/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-04-08T17:17:52.316523/Democracy_Club__02May2024.tsv"
     )
     stations_name = (
-        "2023-05-04/2023-04-12T13:11:26.820087/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-04-08T17:17:52.316523/Democracy_Club__02May2024.tsv"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -20,24 +20,28 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "10090073763",  #  21A WELLINGTON STREET, ASHTON-UNDER-LYNE
             "10090073764",  # 21B WELLINGTON STREET, ASHTON-UNDER-LYNE
             "200004419358",  # FLAT 2 34 HUDSON ROAD, HYDE
-            "200004406539",  # FLAT 2 BANK MILL MANCHESTER ROAD, MOSSLEY
-            "10093148520",  # APARTMENT 3, 1 MARY STREET, HYDE
-            "10093148339",  # 15 BECKER CLOSE, DENTON, MANCHESTER
+            "10096633108",  # 1 TAYLOR STREET, DENTON, MANCHESTER
+            "10096633109",  # 3 TAYLOR STREET, DENTON, MANCHESTER
             "200004410061",  # FLAT 2 2 OLD HALL LANE, MOTTRAM
             "100012777909",  # HOPKINS FARM COTTAGE, ARLIES LANE, STALYBRIDGE
             "100012777908",  # HOPKINS COTTAGE, ARLIES LANE, STALYBRIDGE
+            "100011600288",  # 39 MARKET STREET, DENTON, MANCHESTER
+            "100011545502",  # 2 JOHN STREET, DENTON, MANCHESTER
+            "10014255906",  # THE HOLLIES BUNGALOW, BOYDS WALK, DUKINFIELD
+            "200002857633",  # PORTLAND BASIN MARINA, LOWER ALMA STREET, DUKINFIELD
+            "10014258331",  # ARMSTRONGS OFFICE FURNITURE, 29 PENNY MEADOW, ASHTON-UNDER-LYNE
         ]:
             return None
 
         if record.addressline6 in [
-            # splits
-            "SK15 3QZ",
-            "M34 7RZ",
+            # split
             "OL6 9LS",
-            "M43 6DG",
-            "SK14 2PF",
+            "M34 7RZ",
+            "M43 6NY",
+            "SK15 3QZ",
             "M43 7ZD",
+            # suspect
+            "SK14 4RD",  # MARY STREET, HYDE
         ]:
             return None
-
         return super().address_record_to_dict(record)
