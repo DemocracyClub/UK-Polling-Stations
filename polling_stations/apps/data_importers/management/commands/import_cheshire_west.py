eb43b6a174917515b126df30637eb59c2751a2cf
polling-bot-4000 <developers@democracyclub.org.uk>
2024-04-08 13:57:38 +0100

    Import script for Cheshire West and Chester (2024-05-02) (closes #6872)

--- polling_stations/apps/data_importers/management/commands/import_cheshire_west.py
+++ polling_stations/apps/data_importers/management/commands/import_cheshire_west.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "CHW"
     addresses_name = (
-        "2023-05-04/2023-04-14T11:06:40.402596/Democracy_Club__04May2023.CSV"
+        "2024-05-02/2024-04-08T13:57:14.878521/Democracy_Club__02May2024.CSV"
     )
     stations_name = (
-        "2023-05-04/2023-04-14T11:06:40.402596/Democracy_Club__04May2023.CSV"
+        "2024-05-02/2024-04-08T13:57:14.878521/Democracy_Club__02May2024.CSV"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
@@ -20,48 +20,41 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "10091137622",  # 72 MUSKETT DRIVE, NORTHWICH
             "10091137631",  # 68 MUSKETT DRIVE, NORTHWICH
             "200002842851",  # GREVILLE HOUSE, GREVILLE DRIVE, WINSFORD
-            "100012372274",  # STANTHORNE HALL FARM, MIDDLEWICH ROAD, STANTHORNE, MIDDLEWICH
-            "100012353201",  # STREET FARM, KELSALL ROAD, TARVIN, CHESTER
             "200000831359",  # OAK BANK, PARKGATE ROAD, CHESTER
-            "200000832902",  # LOWCROSS MILL, LOWCROSS, TILSTON, MALPAS
-            "200000990850",  # KIDNAL COTTAGE, KIDNAL, MALPAS
+            "200000990849",  # KIDNAL HOUSE, KIDNAL, MALPAS
+            "100012360649",  # 6 LEIGHTON COTTAGES, BOATHOUSE LANE, PARKGATE, NESTON
+            "10095485945",  # WESTVIEW, WELSH ROAD, LEDSHAM, ELLESMERE PORT
+            "10094685291",  # 6 BALISTER DRIVE, HARTFORD, NORTHWICH
         ]:
             return None
 
         if record.addressline6 in [
             # splits
-            "CW7 3EQ",
-            "CW9 8PU",
-            "CH66 1NZ",
             "CW9 6EL",
             "CW9 8XB",
-            "CH65 9JU",
-            "CW8 4AB",
+            "CW9 7SQ",
+            "CH66 1NZ",
+            "CW9 8PU",
             "CW7 2GG",
-            "CW9 7RX",
             "CH64 3SG",
-            "CW8 2NQ",
+            "CW7 3EQ",
+            "CH4 7DG",
+            "CW8 4AB",
             "CW6 9EP",
+            "CW9 7RX",
+            "CW8 4QS",
+            "CH65 9JU",
             "WA6 0JA",
+            "WA6 6LN",
         ]:
             return None
 
         return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
-        # The Upton Royal British Legion, 20 Heath Road, Upton, Chester, CH1 1HX
-        # Proposed postcode correction: CH2 1HX
-        if record.polling_place_id == "9689":
-            record = record._replace(polling_place_postcode="")
-
-        # Mobile Polling Station - Cheviot Square, Cheviot Square, Winsford
-        # Proposed postcode correction: CW7 1QS
-        if record.polling_place_id == "9643":
-            record = record._replace(polling_place_postcode="")
-
         # Lower Peover CE Primary School, The Cobbles, Lower Peover, Knutsford, Cheshire, WA16 9PZ
         # Fixing the warning, correction is cosmetic and safe
-        if record.polling_place_id == "9447":
+        if record.polling_place_id == "11682":
             record = record._replace(
                 polling_place_easting="374254", polling_place_northing="374113"
             )
