ed0a85aa25045c87840dbd958325aaf89af4e549
polling-bot-4000 <developers@democracyclub.org.uk>
2024-04-08 16:52:10 +0100

    Import script for Castle Point (2024-05-02) (closes #6877)

--- polling_stations/apps/data_importers/management/commands/import_castle_point.py
+++ polling_stations/apps/data_importers/management/commands/import_castle_point.py
@@ -4,28 +4,38 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "CAS"
     addresses_name = (
-        "2023-05-04/2023-04-05T10:06:21.347042/Democracy_Club__04May2023.CSV"
+        "2024-05-02/2024-04-08T16:51:53.256779/Democracy_Club__02May2024 (2).CSV"
     )
     stations_name = (
-        "2023-05-04/2023-04-05T10:06:21.347042/Democracy_Club__04May2023.CSV"
+        "2024-05-02/2024-04-08T16:51:53.256779/Democracy_Club__02May2024 (2).CSV"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
+    csv_encoding = "windows-1252"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "100091600419",  # THE GRANGE, GRANGE ROAD, BENFLEET
-            "200000992512",  # THE BRAMBLES SHIPWRIGHTS CLOSE, SOUTH BENFLEET, BENFLEET
-            "100091228650",  # MANOR HOUSE, SHIPWRIGHTS CLOSE, BENFLEET
+            "100090360367",  # 210 KENTS HILL ROAD, BENFLEET
+            "100091599588",  # 115 DOWNER ROAD, BENFLEET
+            "10004941351",  # 420 DAWS HEATH ROAD, BENFLEET
+            "10004941352",  # 422 DAWS HEATH ROAD, BENFLEET
         ]:
             return None
-
         if record.addressline6 in [
             # split
             "SS8 8HN",
             "SS8 9SL",
+            "SS8 7PJ",
+            "SS7 1HH",
         ]:
             return None
-
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # Fixing missing postcode:
+        # Runnymede Hall Foyer, Rear of Council Offices, Kiln Road, Thundersley, Benfleet
+        if record.polling_place_id == "2789":
+            record = record._replace(polling_place_postcode="SS7 1TF")
+
+        return super().station_record_to_dict(record)
