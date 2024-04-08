ebcd9937b48c1cbdc2d46987d8ed92e163d8815b
polling-bot-4000 <developers@democracyclub.org.uk>
2024-04-08 16:12:10 +0100

    Import script for Hinckley & Bosworth (2024-05-02) (closes #6875)

--- polling_stations/apps/data_importers/management/commands/import_hinckley_bosworth.py
+++ polling_stations/apps/data_importers/management/commands/import_hinckley_bosworth.py
@@ -4,27 +4,26 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "HIN"
     addresses_name = (
-        "2023-05-04/2023-04-28T11:19:56.050247/Democracy_Club__04May2023.CSV"
+        "2024-05-02/2024-04-08T16:10:06.620042/Democracy_Club__02May2024.CSV"
     )
     stations_name = (
-        "2023-05-04/2023-04-28T11:19:56.050247/Democracy_Club__04May2023.CSV"
+        "2024-05-02/2024-04-08T16:10:06.620042/Democracy_Club__02May2024.CSV"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "10033831876",  # THE STABLES, DESFORD LANE, RATBY, LEICESTER
+            "10033831876",  # THE STABLES, DESFORD LANE, RATBY, LEICESTER"
             "10033728621",  # BARWELL SPORTS BAR, KIRKBY ROAD, BARWELL, LEICESTER
             "100030503769",  # 10 HILL STREET, BARWELL
         ]:
             return None
-
         if record.addressline6 in [
             # split
-            "LE10 0QL",
             "LE10 2GJ",
+            "LE10 0QL",
             "LE9 8JA",
             "CV13 6LR",
         ]:
