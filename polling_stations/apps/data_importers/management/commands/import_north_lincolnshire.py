4283c93dc234ac67a2ad323c4996c1e3625a7a7d
polling-bot-4000 <developers@democracyclub.org.uk>
2024-04-09 16:45:22 +0100

    Import script for North Lincolnshire (2024-05-02) (closes #6892)

--- polling_stations/apps/data_importers/management/commands/import_north_lincolnshire.py
+++ polling_stations/apps/data_importers/management/commands/import_north_lincolnshire.py
@@ -1,64 +1,39 @@
 from data_importers.management.commands import BaseHalaroseCsvImporter
 
 
-def station_update(record):
-    # Council station change from:
-    # Main Street, Redbourne, Gainsborough, Lincolnshire, DN21 4QN
-    # to:
-    # St Andrews Church, 2 School Lane, Redbourne, Lincolnshire, DN21 4QN
-
-    if record.pollingstationnumber == "109":
-        record = record._replace(
-            pollingstationname="St Andrews Church",
-            pollingstationnumber="109",
-            pollingstationaddress_1="2 School Lane",
-            pollingstationaddress_2="Redbourne",
-            pollingstationaddress_3="Gainsborough",
-            pollingstationaddress_4="Lincolnshire",
-            pollingstationaddress_5="",
-            pollingstationpostcode="DN21 4QN",
-        )
-    return record
-
-
 class Command(BaseHalaroseCsvImporter):
     council_id = "NLN"
-    addresses_name = "2023-05-04/2023-04-12T16:20:54.855801/Eros_SQL_Output002.csv"
-    stations_name = "2023-05-04/2023-04-12T16:20:54.855801/Eros_SQL_Output002.csv"
-    elections = ["2023-05-04"]
+    addresses_name = (
+        "2024-05-02/2024-04-09T16:45:06.805129/NLC-Polling Station Data (1).csv"
+    )
+    stations_name = (
+        "2024-05-02/2024-04-09T16:45:06.805129/NLC-Polling Station Data (1).csv"
+    )
+    elections = ["2024-05-02"]
 
     def address_record_to_dict(self, record):
-        record = station_update(record)
-
         uprn = record.uprn.strip().lstrip("0")
-
         if uprn in [
+            "200000888905",  # LITTLE GRANGE, FERRIBY ROAD, BARTON-UPON-HUMBER
+            "10095555254",  # THE BARN, LITTLE GRANGE, FERRIBY ROAD, BARTON-UPON-HUMBER
+            "10095555254",  # THE BARN, LITTLE GRANGE, FERRIBY ROAD, BARTON-UPON-HUMBER
+            "100050213509",  # REAR OF 121, MARY STREET, SCUNTHORPE
             "100051967026",  # 68A MARY STREET, SCUNTHORPE
-            "10095555490",  # FLAT 3 175 HIGH STREET, SCUNTHORPE
-            "200000893947",  # FLAT, 265 FRODINGHAM ROAD, SCUNTHORPE
-            "100051966763",  # 147A FRODINGHAM ROAD, SCUNTHORPE
-            "100051966807",  # FLAT, 145 FRODINGHAM ROAD, SCUNTHORPE
-            "100050226727",  # 77 WHARF ROAD, CROWLE, SCUNTHORPE
+            "200000890811",  # WAVERNEY, SUSWORTH, SCUNTHORPE
         ]:
             return None
 
         if record.housepostcode in [
             # split
+            "DN20 0SD",
             "DN17 1SB",
             "DN15 8XL",
-            "DN17 4EJ",
-            "DN20 0SD",
             "DN15 7JH",
-            "DN21 4JF",
-            # look wrong
+            "DN17 4EJ",
+            # suspect,
+            "DN18 5BF",  # COTTINGHAM COURT, BARTON-UPON-HUMBER
             "DN16 2RX",
             "DN16 2SE",
         ]:
             return None
-
         return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        record = station_update(record)
-
-        return super().station_record_to_dict(record)
