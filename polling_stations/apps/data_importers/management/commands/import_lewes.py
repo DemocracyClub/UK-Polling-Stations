87d49357192940d3e7532e9e0a2e5c99a6f2c588
polling-bot-4000 <developers@democracyclub.org.uk>
2024-04-09 16:18:26 +0100

    Import script for Lewes (2024-05-02) (closes #6890)

--- polling_stations/apps/data_importers/management/commands/import_lewes.py
+++ polling_stations/apps/data_importers/management/commands/import_lewes.py
@@ -3,23 +3,27 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "LEE"
-    addresses_name = "2023-05-04/2023-03-17T14:55:49.863600/Eros_SQL_Output001.csv"
-    stations_name = "2023-05-04/2023-03-17T14:55:49.863600/Eros_SQL_Output001.csv"
-    elections = ["2023-05-04"]
+    addresses_name = "2024-05-02/2024-04-09T16:18:14.963158/Eros_SQL_Output002.csv"
+    stations_name = "2024-05-02/2024-04-09T16:18:14.963158/Eros_SQL_Output002.csv"
+    elections = ["2024-05-02"]
 
     def station_record_to_dict(self, record):
+        # SAINT PETER THE APOSTLE CHURCH HALL-THE CHAPEL ROOMS, BELGRAVE ROAD, SEAFORD BN25 4PE
         if (
-            record.pollingstationname
-            == "SAINT PETER THE APOSTLE CHURCH HALL-THE CHAPEL ROOMS"
+            self.get_station_hash(record)
+            == "34-saint-peter-the-apostle-church-hall-the-chapel-rooms"
         ):
             record = record._replace(pollingstationpostcode="")
 
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-        if record.housepostcode in [
-            # split
-            "BN10 8UG",
+        uprn = record.uprn.strip().lstrip("0")
+
+        if uprn in [
+            "10033272374",  # 24A STEYNING AVENUE, PEACEHAVEN
+            "200001466148",  # OLD WHEEL COTTAGE, EASTERN ROAD, WIVELSFIELD GREEN, HAYWARDS HEATH
+            "100062487484",  # WOODS COTTAGE, EASTERN ROAD, WIVELSFIELD GREEN, HAYWARDS HEATH
         ]:
             return None
 
