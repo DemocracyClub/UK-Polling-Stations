90480f9ff516c42611afce27452ec7be8676e4ab
polling-bot-4000 <developers@democracyclub.org.uk>
2024-04-10 16:39:56 +0100

    Import script for Rochford (2024-05-02) (closes #6909)

--- polling_stations/apps/data_importers/management/commands/import_rochford.py
+++ polling_stations/apps/data_importers/management/commands/import_rochford.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "ROC"
     addresses_name = (
-        "2023-05-04/2023-04-17T15:07:22.505468/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-04-10T16:39:43.413423/Democracy_Club__02May2024.tsv"
     )
     stations_name = (
-        "2023-05-04/2023-04-17T15:07:22.505468/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-04-10T16:39:43.413423/Democracy_Club__02May2024.tsv"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -17,7 +17,6 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
         if uprn in [
             "10010561894",  # THE PADDOCKS, VANDERBILT AVENUE, RAYLEIGH
-            "100090575372",  # 4 TYRELLS, HOCKLEY
             "100090592879",  # 157 STAMBRIDGE ROAD, ROCHFORD
             "100091264335",  # BARROW HALL FARMHOUSE BARROW HALL ROAD, LITTLE WAKERING
             "100090595184",  # 56 LITTLE WAKERING ROAD, GREAT WAKERING, SOUTHEND-ON-SEA
@@ -25,12 +24,10 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             return None
 
         if record.addressline6 in [
-            "SS3 0GF",
+            # split
             "SS3 0LQ",
-            "SS6 8DF",
             "SS3 0HH",
-            "SS3 0GE",
-            "SS5 6FF",  # HILTON CRESCENT, HULLBRIDGE
+            "SS6 8DF",
         ]:
             return None
 
@@ -39,12 +36,14 @@ class Command(BaseXpressDemocracyClubCsvImporter):
     def station_record_to_dict(self, record):
         # Wesley Room, Methodist Church Hall, Eastwood Road, Rayleigh, Essex
         # Fixing the warning, correction is safe
-        if record.polling_place_id == "5723":
+        if record.polling_place_id == "6131":
             record = record._replace(polling_place_postcode="SS6 7JP")
 
-        # Grange Free Church, London Road, Rayleigh, Essex, SS6 9AY
-        if record.polling_place_id == "5869":
-            record = record._replace(polling_place_easting="")
-            record = record._replace(polling_place_northing="")
+        # Rayleigh Vineyard Church, London Road, Rayleigh, Essex SS6 9AY
+        if record.polling_place_id == "6100":
+            record = record._replace(
+                polling_place_easting="",
+                polling_place_northing="",
+            )
 
         return super().station_record_to_dict(record)
