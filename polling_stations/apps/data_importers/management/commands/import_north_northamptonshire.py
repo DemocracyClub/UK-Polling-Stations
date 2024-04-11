670f08fcc2417aa46f793c2441461e578fc631d9
polling-bot-4000 <developers@democracyclub.org.uk>
2024-04-11 10:09:49 +0100

    Import script for North Northamptonshire (2024-05-02) (closes #6916)

--- polling_stations/apps/data_importers/management/commands/import_north_northamptonshire.py
+++ polling_stations/apps/data_importers/management/commands/import_north_northamptonshire.py
@@ -3,21 +3,78 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "NNT"
-    addresses_name = "2024-02-15/2024-01-30T16:43:45.816756/Democracy_Club__15February2024_version2.tsv"
-    stations_name = "2024-02-15/2024-01-30T16:43:45.816756/Democracy_Club__15February2024_version2.tsv"
-    elections = ["2024-02-15"]
+    addresses_name = (
+        "2024-05-02/2024-04-11T10:08:50.273571/Democracy_Club__02May2024.tsv"
+    )
+    stations_name = (
+        "2024-05-02/2024-04-11T10:08:50.273571/Democracy_Club__02May2024.tsv"
+    )
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
-        if record.addressline6 in [
-            # split
-            "NN10 9NJ",
-            "NN8 4WG",
-            "NN10 9JD",
-            "NN8 1SH",
-            "NN8 3JX",
-            "NN29 7NL",
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "100031061536",  # STABLE COTTAGE, HEADLANDS, KETTERING
+            "100031076255",  # THE SHEILINGS STOKE ALBANY ROAD, DESBOROUGH
+            "10094095301",  # ASH TREE FARM, STOKE ALBANY ROAD, DESBOROUGH, KETTERING
+            "10007877421",  # CARAVAN 1 DESBOROUGH ROAD, BRAYBROOKE
+            "100031081643",  # THE LODGE SUTTON LANE, DINGLEY
+            "10008566697",  # HARPERS BROOK, 17 CHARTER COURT, CORBY
         ]:
             return None
-
+        if record.addressline6 in [
+            # split
+            "NN6 0FT",
+            "NN18 9BD",
+            "NN15 6YF",
+            "NN18 8DZ",
+            "NN15 6GQ",
+            "NN14 1PF",
+            "NN10 9NJ",
+            "NN10 6EU",
+            "NN8 1SH",
+            "NN14 4EE",
+            "NN14 3BY",
+            "NN18 8RS",
+            "NN29 7NL",
+            "NN8 4WG",
+            "NN10 9JD",
+            "NN18 8QG",
+            "NN8 3JX",
+            "NN17 2HP",
+            "NN14 4AJ",
+            # suspect
+            "NN9 6US",
+            "NN9 6UQ",
+            "NN14 2SP",
+            "NN17 1FP",
+        ]:
+            return None
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # Hope Methodist Church, Linnet Way Entrance, 19 High Street, Higham Ferrers NN10 0RW
+        # postcode looks wrong
+        if record.polling_place_id == "30649":
+            record = record._replace(polling_place_postcode="")
+
+        # The following stations have UPRNs that don't match their postcode:
+
+        # 'The Autumn Centre, Counts Farm Road, Corby, NN18 8BH' (id: 30448)
+        if record.polling_place_id == "30448":
+            record = record._replace(polling_place_postcode="")
+
+        # 'Cottingham/Middleton Village Hall, Berryfield Road, Cottingham, Market Harborough, LE16 8XD' (id: 30517)
+        if record.polling_place_id == "30517":
+            record = record._replace(polling_place_postcode="")
+
+        # 'Stanion Village Hall, Brigstock Road, Stanion, Kettering, NN14 1BX' (id: 30502)
+        if record.polling_place_id == "30502":
+            record = record._replace(polling_place_postcode="")
+
+        # 'East Carlton Cricket Club, East Carlton Park, East Carlton, Market Harborough, LE16 8YD' (id: 30524)
+        if record.polling_place_id == "30524":
+            record = record._replace(polling_place_postcode="")
+        return super().station_record_to_dict(record)
