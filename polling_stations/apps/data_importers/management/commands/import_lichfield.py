19cbe5a47f55dd5fc0e283fdbe52c8bf3a27858e
polling-bot-4000 <developers@democracyclub.org.uk>
2024-04-12 15:39:46 +0100

    Import script for Lichfield (2024-05-02) (closes #6938)

--- polling_stations/apps/data_importers/management/commands/import_lichfield.py
+++ polling_stations/apps/data_importers/management/commands/import_lichfield.py
@@ -4,41 +4,41 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "LIF"
     addresses_name = (
-        "2023-05-04/2023-04-19T15:20:06.691978/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-04-12T15:39:26.541706/Democracy_Club__02May2024.tsv"
     )
     stations_name = (
-        "2023-05-04/2023-04-19T15:20:06.691978/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-04-12T15:39:26.541706/Democracy_Club__02May2024.tsv"
     )
-    elections = ["2023-05-04"]
+    elections = ["2024-05-02"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "10002775637",  # HORSEY LANE LODGE, HORSEY LANE, RUGELEY
             "100031701504",  # MILL DAM HOUSE, MILL LANE, ALDRIDGE, WALSALL
             "10002767279",  # 4 WISSAGE ROAD, LICHFIELD
             "10002767278",  # 2 WISSAGE ROAD, LICHFIELD
             "100032225708",  # THE COURTYARD, LICHFIELD ROAD, PIPEHILL, LICHFIELD
+            "200001160102",  # 86 HIGH STREET, CHASETOWN, BURNTWOOD
+            "10013216680",  # MANAGERS ACCOMMODATION TOBY CARVERY BIRMINGHAM ROAD, SHENSTONE WOODEND, LICHFIELD
         ]:
             return None
 
         return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
-        # Guildroom, Guildhall, Bore Street, Lichfield, WS13 6LX
-        if record.polling_place_id == "6502":
-            record = record._replace(polling_place_postcode="WS13 6LU")
+        # Below warning was checked nad no correction is needed
+        # Polling station Cannock Wood & Gentleshaw Village Hall (8162) is in Cannock Chase District Council (CAN)
 
         # Holy Cross, Community Meeting Room, Holy Cross Church, WS14 9BA
-        if record.polling_place_id == "6758":
+        if record.polling_place_id == "7791":
             record = record._replace(
                 polling_place_easting=411921, polling_place_northing=308789
             )
 
         # Holy Cross Parish Hall, Holy Cross Parish Hall, Chapel Lane, Lichfield, WS14 9BA
-        if record.polling_place_id == "6754":
+        if record.polling_place_id == "7787":
             record = record._replace(
                 polling_place_easting=411908, polling_place_northing=308797
             )
