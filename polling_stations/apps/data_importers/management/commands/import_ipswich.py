be149ee6f22be3e25074b64def8e19d450229d07
polling-bot-4000 <developers@democracyclub.org.uk>
2024-04-12 10:31:59 +0100

    Import script for Ipswich (2024-05-02) (closes #6927)

--- polling_stations/apps/data_importers/management/commands/import_ipswich.py
+++ polling_stations/apps/data_importers/management/commands/import_ipswich.py
@@ -4,15 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "IPS"
     addresses_name = (
-        "2023-05-04/2023-04-13T10:22:11.453698/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-04-12T10:31:42.332438/Democracy_Club__02May2024.CSV"
     )
-
     stations_name = (
-        "2023-05-04/2023-04-13T10:22:11.453698/Democracy_Club__04May2023.tsv"
+        "2024-05-02/2024-04-12T10:31:42.332438/Democracy_Club__02May2024.CSV"
     )
-
-    elections = ["2023-05-04"]
-    csv_delimiter = "\t"
+    elections = ["2024-05-02"]
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
@@ -20,49 +17,58 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         if uprn in [
             "100091059987",  # 22 LINGFIELD ROAD, IPSWICH
             "100091074230",  # 74 STRATFORD ROAD, IPSWICH
+            "100091040532",  # 34 BROCKLEY CRESCENT, IPSWICH
+            "10093557759",  # FLAT, THE INKERMAN PUBLIC HOUSE, 197 NORWICH ROAD, IPSWICH
         ]:
             return None
 
+        if record.addressline6 in [
+            "IP1 6GF",  # split
+        ]:
+            return None
         return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
         # Sikh Temple, Guru Nanak Gurdwara, 719 Bramford Road, Ipswich, IP1 5BD
-        if record.polling_place_id == "8287":
+        if record.polling_place_id == "8691":
             record = record._replace(
                 polling_place_easting="613488",
                 polling_place_northing="245784",
             )
 
         # Castle Hill United Reformed Church, Dryden Road, Ipswich, IP1 6QF
-        if record.polling_place_id == "8272":
+        if record.polling_place_id == "8673":
             record = record._replace(
                 polling_place_easting="615235",
                 polling_place_northing="247231",
             )
 
         # Stoke Green Baptist Church Hall, Halifax Road, Ipswich, IP2 8RE
-        if record.polling_place_id == "8316":
+        if record.polling_place_id == "8718":
             record = record._replace(
                 polling_place_easting="615857",
                 polling_place_northing="242747",
             )
 
         # Ascension Hall, Larchcroft Road, Ipswich, IP1 6AN
-        if record.polling_place_id == "8207":
+        if record.polling_place_id == "8604":
             record = record._replace(
                 polling_place_easting="615625",
                 polling_place_northing="246829",
             )
 
         # Broomhill Library, Sherrington Road, Ipswich, IP1 4HT
-        if record.polling_place_id == "8211":
+        if record.polling_place_id == "8608":
             record = record._replace(
                 polling_place_easting="615423",
                 polling_place_northing="245891",
             )
 
         # Belstead Arms Public House, Radcliffe Drive, Ipswich, IP2 9QA
-        if record.polling_place_id == "8072":
+        if record.polling_place_id in [
+            "8470",
+            "9039",
+        ]:
             record = record._replace(
                 polling_place_postcode="IP2 9QU",
                 polling_place_easting="613819",
