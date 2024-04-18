8c0e9da20835d030e02abcc90a123d94e8bec9c8
awdem <wdemontmollin@gmail.com>
2024-04-18 09:09:10 -0400

    coord fix for station from NOW council

--- polling_stations/apps/data_importers/management/commands/import_norwich.py
+++ polling_stations/apps/data_importers/management/commands/import_norwich.py
@@ -7,6 +7,16 @@ class Command(BaseDemocracyCountsCsvImporter):
     stations_name = "2024-05-02/2024-03-18T08:50:17.047358/Democracy Club - 2024 Polling Stations export 4.csv"
     elections = ["2024-05-02"]
 
+    def station_record_to_dict(self, record):
+        # Coordinate change from council for: ST ALBAN'S CHURCH HALL, GROVE WALK, NORWICH
+        if record.stationcode in ("48TO5", "46TO3"):
+            record = record._replace(
+                xordinate="622820",
+                yordinate="307543",
+            )
+
+        return super().station_record_to_dict(record)
+
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
 
