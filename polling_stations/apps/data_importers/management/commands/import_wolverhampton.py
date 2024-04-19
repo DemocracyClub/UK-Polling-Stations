0b4aa0cfa9412d31e05ade4c8308a4a4641ae8e6
GeoWill <roper.william@gmail.com>
2024-04-19 15:22:46 +0100

    imports/wolverhampton-2024-05-02

--- polling_stations/apps/data_importers/management/commands/import_wolverhampton.py
+++ polling_stations/apps/data_importers/management/commands/import_wolverhampton.py
@@ -1,49 +1,41 @@
-from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
-from django.contrib.gis.geos import Point
+import re
+
+from data_importers.github_importer import BaseGitHubImporter
 
 
-class Command(BaseXpressDemocracyClubCsvImporter):
+class Command(BaseGitHubImporter):
+    srid = 4326
+    districts_srid = 4326
     council_id = "WLV"
-    addresses_name = (
-        "2023-05-04/2023-02-28T09:51:32.288722/Democracy_Club__04May2023.tsv"
-    )
-    stations_name = (
-        "2023-05-04/2023-02-28T09:51:32.288722/Democracy_Club__04May2023.tsv"
-    )
-    elections = ["2023-05-04"]
-    csv_delimiter = "\t"
+    elections = ["2024-05-02"]
+    scraper_name = "wdiv-scrapers/DC-PollingStations-Wolverhampton"
+    geom_type = "geojson"
 
-    def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
+    def district_record_to_dict(self, record):
+        poly = self.extract_geometry(record, self.geom_type, self.get_srid("districts"))
 
-        if uprn in [
-            "10093325431",  # 2A RAYNOR ROAD, WOLVERHAMPTON
-            "10090643975",  # 11 KIRKWALL CRESCENT, WOLVERHAMPTON
-            "10093326250",  # FLAT 1, SMART BUILDING, VULCAN ROAD, BILSTON
-            "10007124221",  # MOSELEY COURT LODGE, BRIDAL WAY, NORTHYCOTE LANE, WOLVERHAMPTON
-            "10090641653",  # 1 HENRY FOWLER DRIVE, WOLVERHAMPTON
-            "10090641654",  # 3 HENRY FOWLER DRIVE, WOLVERHAMPTON
-            "10090641655",  # 4 HENRY FOWLER DRIVE, WOLVERHAMPTON
-            "10090641656",  # 5 HENRY FOWLER DRIVE, WOLVERHAMPTON
-            "10090641657",  # 6 HENRY FOWLER DRIVE, WOLVERHAMPTON
-            "100071556719",  # THE CHIP INN, 32 THORNEYCROFT LANE, WOLVERHAMPTON
-        ]:
-            return None
-
-        if record.addressline6 in ["WV13 3RG"]:
-            return None
-
-        return super().address_record_to_dict(record)
+        return {
+            "internal_council_id": record["DIS_CODE_NEW"],
+            "name": record["WARD"] + " - " + record["DIS_CODE_NEW"],
+            "area": poly,
+        }
 
     def station_record_to_dict(self, record):
-        rec = super().station_record_to_dict(record)
+        location = self.extract_geometry(
+            record, self.geom_type, self.get_srid("stations")
+        )
+        codes = re.split("[, ]", record["STA_CODE"])
+        codes = [code.strip() for code in codes if code.strip()]
 
-        # [WRONG LOC] St Joseph`s Church Hall, Coalway Road,  Wolverhampton
-        if rec["internal_council_id"] == "30057":
-            rec["location"] = Point(-2.170664443278646, 52.57076293329278, srid=4326)
-
-        # [WRONG LOC] Moathouse Community Centre, 52 Moathouse Lane East, Wednesfield
-        if rec["internal_council_id"] == "30219":
-            rec["location"] = Point(-2.0731047162921405, 52.608227577664984, srid=4326)
-
-        return rec
+        stations = []
+        for code in codes:
+            stations.append(
+                {
+                    "internal_council_id": code,
+                    "address": record["ADDRESS"],
+                    "postcode": record["POSTCODE"],
+                    "location": location,
+                    "polling_district_id": code,
+                }
+            )
+        return stations
