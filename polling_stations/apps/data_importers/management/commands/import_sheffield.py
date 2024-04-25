32df7beb2c606e3cb58a37ec4a3c3a98f76fdc53
awdem <wdemontmollin@gmail.com>
2024-04-25 13:31:36 +0100

    map fixes for idox-halarose EMS batch 3

--- polling_stations/apps/data_importers/management/commands/import_sheffield.py
+++ polling_stations/apps/data_importers/management/commands/import_sheffield.py
@@ -1,5 +1,7 @@
+from addressbase.models import Address
 from data_importers.management.commands import BaseHalaroseCsvImporter
 from django.contrib.gis.geos import Point
+from django.core.exceptions import ObjectDoesNotExist
 
 THE_CLUBHOUSE = {
     "pollingstationname": "The Clubhouse",
@@ -94,3 +96,12 @@ class Command(BaseHalaroseCsvImporter):
             record = record._replace(**THE_CLUBHOUSE)
 
         return super().address_record_to_dict(record)
+
+    # quick fix to show maps for Halarose records that have a valid UPRN in the PollingVenueUPRN field
+    def get_station_point(self, record):
+        uprn = record.pollingvenueuprn.strip().lstrip("0")
+        try:
+            ab_rec = Address.objects.get(uprn=uprn)
+            return ab_rec.location
+        except ObjectDoesNotExist:
+            return super().get_station_point(record)
