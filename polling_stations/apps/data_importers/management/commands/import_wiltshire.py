c343f4ba8516c6f08b6e6bcb44eef130f2ea2883
polling-bot-4000 <developers@democracyclub.org.uk>
2024-04-30 08:22:12 +0100

    Import script for Wiltshire (2024-05-02) (closes #6994)

--- polling_stations/apps/data_importers/management/commands/import_wiltshire.py
+++ polling_stations/apps/data_importers/management/commands/import_wiltshire.py
@@ -4,10 +4,10 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WIL"
     addresses_name = (
-        "2024-05-02/2024-04-11T09:06:51.472490/Democracy_Club__02May2024.tsv"
+        "2024-05-02/2024-04-30T08:22:02.367729/Democracy_Club__02May2024.tsv"
     )
     stations_name = (
-        "2024-05-02/2024-04-11T09:06:51.472490/Democracy_Club__02May2024.tsv"
+        "2024-05-02/2024-04-30T08:22:02.367729/Democracy_Club__02May2024.tsv"
     )
     elections = ["2024-05-02"]
     csv_delimiter = "\t"
@@ -17,21 +17,22 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
         if uprn in [
             "10094306546",  # MAREEBA, CARISBROOKE STUD, WEST SOLEY, CHILTON FOLIAT, HUNGERFORD
+            "100121330656",  # 77 BOURNE AVENUE, SALISBURY
         ]:
             return None
 
         if record.addressline6 in [
             # split
-            "SP5 1RN",
             "SN10 4AD",
-            "SP5 2NL",
-            "SN8 1QB",
+            "SP5 1RN",
+            "SN14 6HT",
             "SN10 2PA",
-            "BA12 7JH",
+            "SN8 1QB",
+            "SP5 2NL",
             "SP3 6DY",
+            "BA12 7JH",
             "SN8 1HG",
             "SN15 5JL",
-            "SN14 6HT",
             # suspect
             "SN16 0HT",
             "BA14 8RA",
