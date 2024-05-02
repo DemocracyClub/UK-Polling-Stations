7beebf09e364128508bfbbce605f369beb23fe33
awdem <wdemontmollin@gmail.com>
2024-05-02 15:07:08 +0100

    map fix for bug report #649

--- polling_stations/apps/data_importers/management/commands/import_camden.py
+++ polling_stations/apps/data_importers/management/commands/import_camden.py
@@ -1,4 +1,5 @@
 from data_importers.management.commands import BaseDemocracyCountsCsvImporter
+from django.contrib.gis.geos import Point
 
 
 class Command(BaseDemocracyCountsCsvImporter):
@@ -11,3 +12,15 @@ class Command(BaseDemocracyCountsCsvImporter):
     )
     elections = ["2024-05-02"]
     csv_encoding = "utf-16le"
+
+    def station_record_to_dict(self, record):
+        # bug report # 649:
+        # moving point closer to road for: GOSPEL OAK METHODIST CHURCH, Agincourt Road, London
+        rec = super().station_record_to_dict(record)
+        if rec["internal_council_id"] in (
+            "IA-b",
+            "IA-a",
+        ):
+            rec["location"] = Point(527726, 185479, srid=27700)
+
+        return rec
