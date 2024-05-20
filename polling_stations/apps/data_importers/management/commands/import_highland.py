f87b225e44b37eabef21023bc9a493e0a34af472
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-20 10:40:29 +0100

    Import script for Highland (2024-06-13) (closes #7042)

--- polling_stations/apps/data_importers/management/commands/import_highland.py
+++ polling_stations/apps/data_importers/management/commands/import_highland.py
@@ -3,11 +3,9 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "HLD"
-    addresses_name = (
-        "2024-04-11/2024-03-03T10:43:56.333233/Democracy Club - Polling Districts.csv"
-    )
+    addresses_name = "2024-06-13/2024-05-20T10:40:21.769643/Democracy Club - Polling Districts W7.csv"
     stations_name = (
-        "2024-04-11/2024-03-03T10:43:56.333233/Democracy Club - Polling Stations.csv"
+        "2024-06-13/2024-05-20T10:40:21.769643/Democracy Club - Polling Stations W7.csv"
     )
-    elections = ["2024-04-11"]
+    elections = ["2024-06-13"]
     csv_encoding = "utf-16le"
