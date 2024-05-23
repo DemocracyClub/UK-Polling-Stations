04c1d081f62966ca0ec1164c4f130c094b1fe6d4
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-23 17:50:36 +0100

    Import script for Bracknell Forest (2024-07-04) (closes #7055)

--- polling_stations/apps/data_importers/management/commands/import_bracknell_forest.py
+++ polling_stations/apps/data_importers/management/commands/import_bracknell_forest.py
@@ -3,6 +3,29 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "BRC"
-    addresses_name = "2024-07-04/2024-05-23T17:44:58.932762/Eros_SQL_Output005.csv"
-    stations_name = "2024-07-04/2024-05-23T17:44:58.932762/Eros_SQL_Output005.csv"
+    addresses_name = "2024-07-04/2024-05-23T17:49:18.668326/Eros_SQL_Output006.csv"
+    stations_name = "2024-07-04/2024-05-23T17:49:18.668326/Eros_SQL_Output006.csv"
     elections = ["2024-07-04"]
+
+    def address_record_to_dict(self, record):
+        uprn = record.uprn.strip().lstrip("0")
+
+        if uprn in [
+            "10022826533",  # THE BUNGALOW OAKWOOD PARK KENNELS PEACOCK LANE, WOKINGHAM
+            "200000332122",  # THRUMS, FOLIEJON PARK, WINKFIELD, WINDSOR
+        ]:
+            return None
+        if record.housepostcode in [
+            # split
+            "SL5 8RY",
+            "RG42 6BX",
+        ]:
+            return None
+        return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # 'Binfield Memorial Hall, Terrace Road South, Binfield, BRACKNELL, RG42 4DJ'
+        if record.pollingvenueid == "3":
+            record = record._replace(pollingstationpostcode="")
+
+        return super().station_record_to_dict(record)
