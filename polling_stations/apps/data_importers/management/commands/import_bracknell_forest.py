64b5c6d7b33b11c3898ca8c0930825d40c7b63be
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-24 14:12:17 +0100

    Import script for Bracknell Forest (2024-07-04) (closes #7071)

--- polling_stations/apps/data_importers/management/commands/import_bracknell_forest.py
+++ polling_stations/apps/data_importers/management/commands/import_bracknell_forest.py
@@ -3,8 +3,12 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "BRC"
-    addresses_name = "2024-07-04/2024-05-23T17:49:18.668326/Eros_SQL_Output006.csv"
-    stations_name = "2024-07-04/2024-05-23T17:49:18.668326/Eros_SQL_Output006.csv"
+    addresses_name = (
+        "2024-07-04/2024-05-27T09:27:09.452526/Bracknell_Forest_combined.csv"
+    )
+    stations_name = (
+        "2024-07-04/2024-05-27T09:27:09.452526/Bracknell_Forest_combined.csv"
+    )
     elections = ["2024-07-04"]
 
     def address_record_to_dict(self, record):
@@ -24,8 +28,33 @@ class Command(BaseHalaroseCsvImporter):
         return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
-        # 'Binfield Memorial Hall, Terrace Road South, Binfield, BRACKNELL, RG42 4DJ'
+        # Binfield Memorial Hall, Terrace Road South, Binfield, BRACKNELL
+        # postcode correction from council: RG42 4DJ -> RG42 4DS
         if record.pollingvenueid == "3":
+            record = record._replace(pollingstationpostcode="RG42 4DS")
+
+        # 'Bracknell Central Library, Town Square, Bracknell, RG12 1AT' (id: 4495)
+        if record.pollingvenueid == "4495":
+            record = record._replace(pollingstationpostcode="")
+
+        # 'Bracknell Methodist Church Hall, Shepherds Lane, BRACKNELL, RG42 2DD' (id: 5)
+        if record.pollingvenueid == "5":
+            record = record._replace(pollingstationpostcode="")
+
+        # 'Bracknell Methodist Church Hall, Shepherds Lane, BRACKNELL, RG42 2DD' (id: 5)
+        if record.pollingvenueid == "5":
+            record = record._replace(pollingstationpostcode="")
+
+        # 'Bracknell Central Library, Town Square, Bracknell, RG12 1AT' (id: 4495)
+        if record.pollingvenueid == "4495":
+            record = record._replace(pollingstationpostcode="")
+
+        # 'Sandhurst School Sports Centre, Owlsmoor Road, Owlsmoor, Sandhurst, GU47 0SP' (id: 4018)
+        if record.pollingvenueid == "4018":
+            record = record._replace(pollingstationpostcode="")
+
+        # 'Sandhurst School Sports Centre, Owlsmoor Road, Owlsmoor, Sandhurst, GU47 0SP' (id: 4018)
+        if record.pollingvenueid == "4018":
             record = record._replace(pollingstationpostcode="")
 
         return super().station_record_to_dict(record)
