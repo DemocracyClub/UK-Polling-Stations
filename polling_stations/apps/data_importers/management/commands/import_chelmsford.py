53bc42e8496ef99abc95e3d3faea7c810831c57a
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-24 17:36:25 +0100

    Import script for Chelmsford (2024-07-04) (closes #7080)

--- polling_stations/apps/data_importers/management/commands/import_chelmsford.py
+++ polling_stations/apps/data_importers/management/commands/import_chelmsford.py
@@ -4,12 +4,10 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "CHL"
     addresses_name = (
-        "2024-05-02/2024-03-06T09:57:53.761776/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-05-24T17:36:06.888403/Chelmsford Cons Stations.tsv"
     )
-    stations_name = (
-        "2024-05-02/2024-03-06T09:57:53.761776/Democracy_Club__02May2024.tsv"
-    )
-    elections = ["2024-05-02"]
+    stations_name = "2024-07-04/2024-05-24T17:36:06.888403/Chelmsford Cons Stations.tsv"
+    elections = ["2024-07-04"]
     csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
@@ -18,37 +16,24 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         ## 'East Hanningfield Village Hall, The Tye, East Hanningfield, Chelmsford, CM3 8AE' (id: 13209)
         ## 'Civic Centre - Chelmsford City Council, Duke Street, Chelmsford, CM1 1JE' (id: 13420)
 
-        # '11th Chelmsford Scout Group, Scout Hut, Gloucester Avenue, Chelmsford, CM2 9LG' (id: 13004)
-        if record.polling_place_id == "13004":
-            record = record._replace(polling_place_postcode="CM2 9DS")
+        # The following corrections were not in the data supplied by the council,
+        # so I've commented them out for now:
 
-        # 'Moulsham High Youth Centre, Access via path between 41 & 49 Gloucester Avenue, Chelmsford, CM2 9ES' (id: 13012)
-        if record.polling_place_id == "13012":
-            record = record._replace(polling_place_postcode="CM2 9DZ")
+        # # 'St. John the Evangelist`s Church, Church Lane/Sandon Hill, Ford End, Chelmsford, Essex, CM3 1LQ' (id: 13103)
+        # if record.polling_place_id == "13103":
+        #     record = record._replace(polling_place_postcode="CM3 1LH")
 
-        # 'St Andrews Church, Melbourne Avenue, Chelmsford, CM1 2DT' (id: 13039)
-        if record.polling_place_id == "13039":
-            record = record._replace(polling_place_postcode="CM1 2JB")
+        # # 'Pleshey Village Hall, The Street, Pleshey, Chelmsford, CM3 1HE' (id: 13131)
+        # if record.polling_place_id == "13131":
+        #     record = record._replace(polling_place_postcode="CM3 1HB")
 
-        # 'St. John the Evangelist`s Church, Church Lane/Sandon Hill, Ford End, Chelmsford, Essex, CM3 1LQ' (id: 13103)
-        if record.polling_place_id == "13103":
-            record = record._replace(polling_place_postcode="CM3 1LH")
+        # # 'Rettendon Memorial Hall, Main Road, Rettendon, Chelmsford, CM3 8DP' (id: 13220)
+        # if record.polling_place_id == "13220":
+        #     record = record._replace(polling_place_postcode="CM3 8DR")
 
-        # 'Pleshey Village Hall, The Street, Pleshey, Chelmsford, CM3 1HE' (id: 13131)
-        if record.polling_place_id == "13131":
-            record = record._replace(polling_place_postcode="CM3 1HB")
-
-        # 'Our Lady Immaculate Church Hall, New London Road, Chelmsford, CM2 0AR' (id: 13147)
-        if record.polling_place_id == "13147":
-            record = record._replace(polling_place_postcode="CM2 0RG")
-
-        # 'Rettendon Memorial Hall, Main Road, Rettendon, Chelmsford, CM3 8DP' (id: 13220)
-        if record.polling_place_id == "13220":
-            record = record._replace(polling_place_postcode="CM3 8DR")
-
-        # 'Downham Village Hall, 50 School Road, Downham, Billericay, CM11 1QR' (id: 13231)
-        if record.polling_place_id == "13231":
-            record = record._replace(polling_place_postcode="CM11 1QP")
+        # # 'Downham Village Hall, 50 School Road, Downham, Billericay, CM11 1QR' (id: 13231)
+        # if record.polling_place_id == "13231":
+        #     record = record._replace(polling_place_postcode="CM11 1QP")
 
         return super().station_record_to_dict(record)
 
@@ -68,9 +53,6 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         if record.addressline6 in [
             # split
             "CM1 1FU",
-            "CM1 7AR",
-            "CM3 1ER",
-            "CM4 9JL",
         ]:
             return None
 
