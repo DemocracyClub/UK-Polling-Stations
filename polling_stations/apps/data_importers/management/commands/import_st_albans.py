6419605605d6d9e0b1a4daefbc23d1e76fc8e9a9
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-27 15:28:31 +0100

    Import script for St Albans (2024-07-04) (closes #7099)

--- polling_stations/apps/data_importers/management/commands/import_st_albans.py
+++ polling_stations/apps/data_importers/management/commands/import_st_albans.py
@@ -3,23 +3,14 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SAL"
-    addresses_name = "2024-05-02/2024-02-09T15:55:50.075896/Democracy_Club__02May2024_St Albans City and District Council.tsv"
-    stations_name = "2024-05-02/2024-02-09T15:55:50.075896/Democracy_Club__02May2024_St Albans City and District Council.tsv"
-    elections = ["2024-05-02"]
+    addresses_name = (
+        "2024-07-04/2024-05-27T15:28:11.936187/Democracy_Club__04July2024.tsv"
+    )
+    stations_name = (
+        "2024-07-04/2024-05-27T15:28:11.936187/Democracy_Club__04July2024.tsv"
+    )
+    elections = ["2024-07-04"]
     csv_delimiter = "\t"
-
-    def station_record_to_dict(self, record):
-        # Wood End School, Yeomans Avenue, Harpenden
-        if record.polling_place_id == "5061":
-            record = record._replace(polling_place_northing="215414")
-
-        # coordinate correction from council:
-        # Park Hall, Leyton Road, Harpenden, AL5 2LX
-        if record.polling_place_id == "5084":
-            record = record._replace(polling_place_northing="214003")
-            record = record._replace(polling_place_easting="513471")
-
-        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
@@ -34,12 +25,11 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
         if record.addressline6 in [
             # splits
+            "AL3 5PR",
             "AL4 0LD",
             "AL1 3NS",
-            "AL3 5PD",
-            "AL3 5PR",
-            "AL1 4RJ",
             "AL4 0TP",
+            "AL3 5PD",
             # suspect
             "AL5 1AB",
             "AL5 1AA",
