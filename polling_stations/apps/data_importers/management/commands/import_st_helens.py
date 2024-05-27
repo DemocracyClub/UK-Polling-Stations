3b3fb98a41feb8dab17aa5b8297392dd6f357758
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-27 21:12:02 +0100

    Import script for St Helens (2024-07-04) (closes #7106)

--- polling_stations/apps/data_importers/management/commands/import_st_helens.py
+++ polling_stations/apps/data_importers/management/commands/import_st_helens.py
@@ -4,53 +4,22 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SHN"
     addresses_name = (
-        "2024-05-02/2024-03-08T16:27:14.461070/Democracy_Club__02May2024 - updated.tsv"
+        "2024-07-04/2024-05-27T21:11:46.157637/Democracy_Club__04July2024.tsv"
     )
     stations_name = (
-        "2024-05-02/2024-03-08T16:27:14.461070/Democracy_Club__02May2024 - updated.tsv"
+        "2024-07-04/2024-05-27T21:11:46.157637/Democracy_Club__04July2024.tsv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
     csv_delimiter = "\t"
 
-    def station_record_to_dict(self, record):
-        # Coordinate fixes from council:
-        # Billinge St.Aidans Primary School, Off London Fields, Billinge, St Helens, WN5 7LS
-        if record.polling_place_id == "5104":
-            record = record._replace(
-                polling_place_easting="353402",
-                polling_place_northing="400375",
-            )
-
-        # Mobile at Fir Tree Farm, Pimbo Road, Rainford, WA11 8RG
-        if record.polling_place_id == "5265":
-            record = record._replace(
-                polling_place_easting="350518",
-                polling_place_northing="401287",
-            )
-
-        # Clock Face Miners Recreation Club, Crawford Street, Clock Face, St Helens
-        if record.polling_place_id == "5095":
-            record = record._replace(
-                polling_place_easting="352927",
-                polling_place_northing="391453",
-            )
-        # Chester Lane Library, Chester Lane, Sutton, St Helens, WA9 4DE
-        if record.polling_place_id == "5099":
-            record = record._replace(
-                polling_place_easting="351948",
-                polling_place_northing="391612",
-            )
-
-        # Mobile at Bird i'th Hand, Dunriding Lane, West Park, St Helens, WA10 3HE
-        if record.polling_place_id == "5361":
-            record = record._replace(
-                polling_place_easting="349689",
-                polling_place_northing="394757",
-            )
-
-        return super().station_record_to_dict(record)
-
     def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "39097215",  # 8 PARK AVENUE NORTH, NEWTON-LE-WILLOWS
+            "39053135",  # ROOM 5, COHAB HOUSE, 19 SHAW STREET, ST. HELENS
+        ]:
+            return None
         if record.addressline6 in [
             # split
             "WA10 1HT",
