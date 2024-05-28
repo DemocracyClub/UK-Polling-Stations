712dff97d41f160b7264c706c3fbfc7cf639cb7f
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-28 15:07:22 +0100

    Import script for Crawley (2024-07-04) (closes #7136)

--- polling_stations/apps/data_importers/management/commands/import_crawley.py
+++ polling_stations/apps/data_importers/management/commands/import_crawley.py
@@ -4,10 +4,22 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "CRW"
     addresses_name = (
-        "2024-05-02/2024-03-21T08:19:50.035033/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-05-28T15:07:15.444005/Democracy_Club__04July2024.tsv"
     )
     stations_name = (
-        "2024-05-02/2024-03-21T08:19:50.035033/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-05-28T15:07:15.444005/Democracy_Club__04July2024.tsv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
     csv_delimiter = "\t"
+
+    def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "100061788135",  # 79 PUNCH COPSE ROAD, CRAWLEY
+            "100061767151",  # OAK COTTAGE, BALCOMBE ROAD, CRAWLEY
+            "200001244362",  # PREMIER INN, CRAWLEY AVENUE, CRAWLEY
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
