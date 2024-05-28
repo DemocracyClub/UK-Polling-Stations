4bf8fd5633b7ebcd2fc6b969ac841e5c06a1daa3
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-28 20:43:52 +0100

    Import script for Test Valley (2024-07-04) (closes #7171)

--- polling_stations/apps/data_importers/management/commands/import_test_valley.py
+++ polling_stations/apps/data_importers/management/commands/import_test_valley.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "TES"
     addresses_name = (
-        "2024-05-02/2024-03-05T16:26:54.791042/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-05-28T20:43:41.926480/Democracy_Club__04July2024.tsv"
     )
     stations_name = (
-        "2024-05-02/2024-03-05T16:26:54.791042/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-05-28T20:43:41.926480/Democracy_Club__04July2024.tsv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
 
