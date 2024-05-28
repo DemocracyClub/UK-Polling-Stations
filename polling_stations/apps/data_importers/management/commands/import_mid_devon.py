b2455818df153f79af1de0a581e873b438593050
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-28 14:15:15 +0100

    Import script for Mid Devon (2024-07-04) (closes #7124)

--- polling_stations/apps/data_importers/management/commands/import_mid_devon.py
+++ polling_stations/apps/data_importers/management/commands/import_mid_devon.py
@@ -1,22 +1,29 @@
-from data_importers.management.commands import BaseXpressWebLookupCsvImporter
+from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
-class Command(BaseXpressWebLookupCsvImporter):
+class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "MDE"
-    addresses_name = "2024-05-02/2024-02-26T12:46:32.302333/PropertyPostCodePollingStationWebLookup-2024-02-26 MID DEVON.TSV"
-    stations_name = "2024-05-02/2024-02-26T12:46:32.302333/PropertyPostCodePollingStationWebLookup-2024-02-26 MID DEVON.TSV"
-    elections = ["2024-05-02"]
+    addresses_name = (
+        "2024-07-04/2024-05-28T14:15:09.339060/Democracy_Club__04July2024 MID DEVON.tsv"
+    )
+    stations_name = (
+        "2024-07-04/2024-05-28T14:15:09.339060/Democracy_Club__04July2024 MID DEVON.tsv"
+    )
+    elections = ["2024-07-04"]
     csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
         # Chawleigh Jubilee Hall, Chawleigh
-        if record.pollingplaceid == "11109":
-            record = record._replace(pollingplaceeasting="", pollingplacenorthing="")
+        if record.polling_place_id == "11714":
+            record = record._replace(
+                polling_place_easting="",
+                polling_place_northing="",
+            )
 
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
+        uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
             "10024639781",  # FOXHOLES FARM, CLAYHIDON, CULLOMPTON
@@ -32,10 +39,12 @@ class Command(BaseXpressWebLookupCsvImporter):
             "10009451330",  # TOADYPARK, ZEAL MONACHORUM, CREDITON
             "10093538015",  # HIGHER POND BARN, KEYMELFORD, YEOFORD, CREDITON
             "10002162357",  # RADDS COTTAGE, CALVERLEIGH, TIVERTON
+            "10095583600",  # HOLIDAY COTTAGE 1 ROAD FROM PITTON CROSS TO HOOK FARM, CHERITON BISHOP
+            "10095583601",  # HOLIDAY COTTAGE 2 ROAD FROM PITTON CROSS TO HOOK FARM, CHERITON BISHOP
         ]:
             return None
 
-        if record.postcode in [
+        if record.addressline6 in [
             # look wrong
             "EX16 8RA",
         ]:
