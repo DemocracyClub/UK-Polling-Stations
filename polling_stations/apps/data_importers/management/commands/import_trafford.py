b14c4fe6f334282e20cda5f93daa13895f372e6a
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-28 18:53:19 +0100

    Import script for Trafford (2024-07-04) (closes #7165)

--- polling_stations/apps/data_importers/management/commands/import_trafford.py
+++ polling_stations/apps/data_importers/management/commands/import_trafford.py
@@ -4,20 +4,13 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "TRF"
     addresses_name = (
-        "2024-05-02/2024-02-19T09:14:43.265455/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-05-28T18:53:03.135580/Democracy_Club__04July2024.tsv"
     )
     stations_name = (
-        "2024-05-02/2024-02-19T09:14:43.265455/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-05-28T18:53:03.135580/Democracy_Club__04July2024.tsv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
     csv_delimiter = "\t"
-
-    def station_record_to_dict(self, record):
-        # Hale Methodist Church, 119 Hale Road, Hale WA15 9QH
-        if record.polling_place_id == "6090":
-            record = record._replace(polling_place_postcode="WA15 9HL")
-
-        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
@@ -29,13 +22,14 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
         if record.addressline6 in [
             # split
-            "M33 3GG",
-            "M33 2BT",
-            "WA14 4AN",
             "M33 5GN",
+            "M33 2BT",
+            "M33 3GG",
+            "WA14 4AN",
             # confusing
             "M31 4SU",
             "M31 4ST",
+            "WA15 8TR",
         ]:
             return None
 
