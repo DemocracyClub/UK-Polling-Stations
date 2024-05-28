3edc426eb381815b0cc9e9f8fdfab18dd9edbf81
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-28 17:32:34 +0100

    Import script for Epping Forest (2024-07-04) (closes #7161)

--- polling_stations/apps/data_importers/management/commands/import_epping_forest.py
+++ polling_stations/apps/data_importers/management/commands/import_epping_forest.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "EPP"
     addresses_name = (
-        "2024-05-02/2024-02-20T15:33:21.896325/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-05-28T17:32:24.547455/Democracy_Club__04July2024.tsv"
     )
     stations_name = (
-        "2024-05-02/2024-02-20T15:33:21.896325/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-05-28T17:32:24.547455/Democracy_Club__04July2024.tsv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -43,7 +43,7 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         rec = super().station_record_to_dict(record)
         # WCIVF bug report:
         # Removes map for: High Beach Village Hall, Avey Lane, High Beach, Loughton
-        if rec["internal_council_id"] == "3468":
+        if rec["internal_council_id"] == "3596":
             rec["location"] = None
 
         return rec
