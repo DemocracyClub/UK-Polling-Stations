3ac84b4b57dbb0b683065e278cdf357894a6c888
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-29 22:18:05 +0100

    Import script for Lambeth (2024-07-04) (closes #7231)

--- polling_stations/apps/data_importers/management/commands/import_lambeth.py
+++ polling_stations/apps/data_importers/management/commands/import_lambeth.py
@@ -4,13 +4,14 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "LBH"
     addresses_name = (
-        "2024-05-02/2024-03-06T17:57:26.808182/Democracy_Club__02May2024.CSV"
+        "2024-07-04/2024-05-29T22:16:57.756279/Democracy_Club__04July2024.tsv"
     )
     stations_name = (
-        "2024-05-02/2024-03-06T17:57:26.808182/Democracy_Club__02May2024.CSV"
+        "2024-07-04/2024-05-29T22:16:57.756279/Democracy_Club__04July2024.tsv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
     csv_encoding = "windows-1252"
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
@@ -26,17 +27,17 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         ]:
             return None
 
-        if record.addressline6 in [
+        if record.addressline6.replace("\xa0", " ") in [
             # splits
-            "SW2 5RS",
-            "SW8 2EN",
-            "SE11 5UG",
-            "SE21 8HX",
-            "SW16 2UU",
-            "SW9 6ED",
-            "SW9 9NT",
-            "SW9 7AR",
-            "SW9 6HN",
+            "SW2 5RS",
+            "SW8 2EN",
+            "SE11 5UG",
+            "SE21 8HX",
+            "SW16 2UU",
+            "SW9 6ED",
+            "SW9 9NT",
+            "SW9 7AR",
+            "SW9 6HN",
         ]:
             return None
 
@@ -44,12 +45,12 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
     def station_record_to_dict(self, record):
         # add point for: Weir Link Community Centre, 33 Weir Road, London, SW12 0NU
-        if record.polling_place_id == "7494":
+        if record.polling_place_id == "7815":
             record = record._replace(polling_place_easting="529369")
             record = record._replace(polling_place_northing="173631")
 
         # add point for: Elmgreen Secondary School, Elmcourt Road, London, SE27 9BZ
-        if record.polling_place_id == "7402":
+        if record.polling_place_id == "7938":
             record = record._replace(polling_place_easting="532013")
             record = record._replace(polling_place_northing="172802")
 
