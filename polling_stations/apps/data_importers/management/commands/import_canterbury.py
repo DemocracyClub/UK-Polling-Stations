ec471015c58d093f9853396751de54c3ae3538f1
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-29 11:43:16 +0100

    Import script for Canterbury (2024-07-04) (closes #7195)

--- polling_stations/apps/data_importers/management/commands/import_canterbury.py
+++ polling_stations/apps/data_importers/management/commands/import_canterbury.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "CAT"
     addresses_name = (
-        "2024-05-02/2024-03-06T14:08:58.739559/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-05-29T11:43:00.234327/Democracy_Club__04July2024.tsv"
     )
     stations_name = (
-        "2024-05-02/2024-03-06T14:08:58.739559/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-05-29T11:43:00.234327/Democracy_Club__04July2024.tsv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
 
