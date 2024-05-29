07f457f6c770ba3e544fd4141589d09b3ff062f6
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-29 09:01:04 +0100

    Import script for Runnymede (2024-07-04) (closes #7185)

--- polling_stations/apps/data_importers/management/commands/import_runnymede.py
+++ polling_stations/apps/data_importers/management/commands/import_runnymede.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "RUN"
     addresses_name = (
-        "2024-05-02/2024-02-22T10:50:15.362737/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-05-29T09:00:57.033219/Democracy_Club__04July2024.tsv"
     )
     stations_name = (
-        "2024-05-02/2024-02-22T10:50:15.362737/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-05-29T09:00:57.033219/Democracy_Club__04July2024.tsv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
