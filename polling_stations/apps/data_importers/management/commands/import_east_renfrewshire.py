153690eb86b72a9cb909ecfc5808418339ad11d0
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-29 09:31:41 +0100

    Import script for East Renfrewshire (2024-07-04) (closes #7189)

--- polling_stations/apps/data_importers/management/commands/import_east_renfrewshire.py
+++ polling_stations/apps/data_importers/management/commands/import_east_renfrewshire.py
@@ -1,46 +1,19 @@
-from addressbase.models import UprnToCouncil
 from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "ERW"
-    addresses_name = (
-        "2022-05-05/2022-03-28T09:28:06.019937/DemocracyClub_PollingDistricts2022.csv"
-    )
-    stations_name = (
-        "2022-05-05/2022-03-28T09:28:06.019937/DemocracyClub_PollingStations2022.csv"
-    )
-    elections = ["2022-05-05"]
-
-    def pre_import(self):
-        # We need to consider rows that don't have a uprn when importing data.
-        # However there are lots of rows for other councils in this file.
-        # So build a list of stations from rows that do have UPRNS
-        # and then use that list of stations to make sure we check relevant rows, even if they don't have a UPRN
-
-        council_uprns = set(
-            UprnToCouncil.objects.filter(lad=self.council.geography.gss).values_list(
-                "uprn", flat=True
-            )
-        )
-        self.COUNCIL_STATIONS = set()
-        data = self.get_addresses()
-
-        for record in data:
-            if record.uprn in council_uprns:
-                self.COUNCIL_STATIONS.add(record.stationcode)
+    addresses_name = "2024-07-04/2024-05-29T09:31:34.253537/Polling Districts.csv"
+    stations_name = "2024-07-04/2024-05-29T09:31:34.253537/Polling Stations.csv"
+    elections = ["2024-07-04"]
+    csv_encoding = "utf-16le"
 
     def address_record_to_dict(self, record):
-        if record.stationcode not in self.COUNCIL_STATIONS:
-            return None
-
-        if record.postcode in ["G76 8RW"]:
+        if record.postcode in [
+            # split
+            "G46 7JL",
+            "G76 8RW",
+        ]:
             return None
 
         return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        if record.stationcode not in self.COUNCIL_STATIONS:
-            return None
-
-        return super().station_record_to_dict(record)
