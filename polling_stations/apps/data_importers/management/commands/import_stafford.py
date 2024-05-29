6318c8a00eee87b8d08cbd50ef16af00df964064
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-29 15:57:40 +0100

    Import script for Stafford (2024-07-04) (closes #7217)

--- polling_stations/apps/data_importers/management/commands/import_stafford.py
+++ polling_stations/apps/data_importers/management/commands/import_stafford.py
@@ -3,9 +3,9 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "STA"
-    addresses_name = "2024-05-02/2024-02-27T09:37:25.220071/Eros_SQL_Output020.csv"
-    stations_name = "2024-05-02/2024-02-27T09:37:25.220071/Eros_SQL_Output020.csv"
-    elections = ["2024-05-02"]
+    addresses_name = "2024-07-04/2024-06-03T09:07:55.257705/STA_combined.csv"
+    stations_name = "2024-07-04/2024-06-03T09:07:55.257705/STA_combined.csv"
+    elections = ["2024-07-04"]
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
@@ -20,6 +20,7 @@ class Command(BaseHalaroseCsvImporter):
             "10002085035",  # BARNSIDE, UTTOXETER ROAD, STONE
             "200001327641",  # LIVING ACCOMMODATION HOP POLE 22 SANDON ROAD, STAFFORD
             "10002090418",  # OAKFIELD HOUSE, THE OLD FRUIT FARM, FRADSWELL, STAFFORD
+            "100031780034",  # 28 SANDON ROAD, STAFFORD
         ]:
             return None
 
@@ -33,10 +34,3 @@ class Command(BaseHalaroseCsvImporter):
             return None
 
         return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # Wedgwood Cricket Club Barlaston Park Barlaston Stoke on Trent ST12 9ES
-        if (record.pollingstationnumber) == "1029":
-            record = record._replace(pollingstationpostcode="ST12 9ER")
-
-        return super().station_record_to_dict(record)
