274ab9074f901f3e1c45a880becacbce734ec323
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-29 11:45:31 +0100

    Import script for Rushcliffe (2024-07-04) (closes #7197)

--- polling_stations/apps/data_importers/management/commands/import_rushcliffe.py
+++ polling_stations/apps/data_importers/management/commands/import_rushcliffe.py
@@ -4,29 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "RUS"
     addresses_name = (
-        "2024-05-02/2024-02-14T11:48:41.864356/Democracy_Club__02May2024.CSV"
+        "2024-07-04/2024-05-29T11:45:24.392981/Democracy_Club__04July2024.CSV"
     )
     stations_name = (
-        "2024-05-02/2024-02-14T11:48:41.864356/Democracy_Club__02May2024.CSV"
+        "2024-07-04/2024-05-29T11:45:24.392981/Democracy_Club__04July2024.CSV"
     )
-    elections = ["2024-05-02"]
-
-    def station_record_to_dict(self, record):
-        # Bingham Methodist Centre, Union Street, Bingham, Nottingham
-        if record.polling_place_id == "6285":
-            record = record._replace(polling_place_postcode="NG13 8AD")
-            record = record._replace(polling_place_easting="470368")
-
-        # Edwalton Church Hall, Vicarage Green, Edwalton
-        if record.polling_place_id == "6145":
-            record = record._replace(polling_place_postcode="NG12 4AP")
-            record = record._replace(polling_place_easting="459770")
-
-        # West Bridgford Baptist Church, Melton Road, West Bridgford
-        if record.polling_place_id == "6290":
-            record = record._replace(polling_place_easting="458385")
-
-        return super().station_record_to_dict(record)
+    elections = ["2024-07-04"]
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
