79cf6c33f9c976b13b4c985d43b7a82e671632f7
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-29 14:37:24 +0100

    Import script for South Tyneside (2024-07-04) (closes #7211)

--- polling_stations/apps/data_importers/management/commands/import_south_tyneside.py
+++ polling_stations/apps/data_importers/management/commands/import_south_tyneside.py
@@ -4,24 +4,13 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "STY"
     addresses_name = (
-        "2024-05-02/2024-03-12T17:14:20.914959/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-05-29T14:37:13.886671/Democracy_Club__04July2024.tsv"
     )
     stations_name = (
-        "2024-05-02/2024-03-12T17:14:20.914959/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-05-29T14:37:13.886671/Democracy_Club__04July2024.tsv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
     csv_delimiter = "\t"
-
-    def station_record_to_dict(self, record):
-        # coordinate fix from council:
-        # Bilton Hall Community Trust, Low Simonside Community Centre, Taunton Avenue, Jarrow, NE32 3RT
-        if record.polling_place_id == "5403":
-            record = record._replace(
-                polling_place_easting="434136",
-                polling_place_northing="564254",
-            )
-
-        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
