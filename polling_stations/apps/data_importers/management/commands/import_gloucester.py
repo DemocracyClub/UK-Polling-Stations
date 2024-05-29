2e458b29acc6ab46fafa40e7d2f6e031ac04eac9
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-29 10:21:07 +0100

    Import script for Gloucester (2024-07-04) (closes #7191)

--- polling_stations/apps/data_importers/management/commands/import_gloucester.py
+++ polling_stations/apps/data_importers/management/commands/import_gloucester.py
@@ -5,12 +5,12 @@ from django.contrib.gis.geos import Point
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "GLO"
     addresses_name = (
-        "2024-05-02/2024-03-18T15:13:17.171189/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-05-29T10:20:56.039406/GLOS_Democracy_Club__04July2024.tsv"
     )
     stations_name = (
-        "2024-05-02/2024-03-18T15:13:17.171189/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-05-29T10:20:56.039406/GLOS_Democracy_Club__04July2024.tsv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
 
