aff1386cb8d6103a05a37647c6a1925526f33dfd
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-30 15:24:43 +0100

    Import script for Maidstone (2024-07-04) (closes #7286)

--- polling_stations/apps/data_importers/management/commands/import_maidstone.py
+++ polling_stations/apps/data_importers/management/commands/import_maidstone.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "MAI"
     addresses_name = (
-        "2024-05-02/2024-03-07T16:44:02.001238/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-05-30T15:24:34.390824/Democracy_Club__04July2024.tsv"
     )
     stations_name = (
-        "2024-05-02/2024-03-07T16:44:02.001238/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-05-30T15:24:34.390824/Democracy_Club__04July2024.tsv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
