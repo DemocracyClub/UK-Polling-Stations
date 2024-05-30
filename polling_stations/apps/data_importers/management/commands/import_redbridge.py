1593152cda2167ef1ed8e753d1bb199134758d15
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-30 14:28:18 +0100

    Import script for Redbridge (2024-07-04) (closes #7276)

--- polling_stations/apps/data_importers/management/commands/import_redbridge.py
+++ polling_stations/apps/data_importers/management/commands/import_redbridge.py
@@ -1,43 +1,16 @@
 from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
-THE_STOCKADE = {
-    "polling_place_id": "2781",
-    "polling_place_name": "The Stockade",
-    "polling_place_address_1": "Davids Way",
-    "polling_place_address_2": "Hainault",
-    "polling_place_address_3": "Ilford",
-    "polling_place_address_4": "",
-    "polling_place_postcode": "IG6 3BQ",
-    "default_polling_place_id": "269",
-}
-
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "RDB"
     addresses_name = (
-        "2024-05-02/2024-02-20T16:50:55.903244/Democracy_Club__02May2024.CSV"
+        "2024-07-04/2024-05-30T14:27:35.423405/Democracy_Club__04July2024.tsv"
     )
     stations_name = (
-        "2024-05-02/2024-02-20T16:50:55.903244/Democracy_Club__02May2024.CSV"
+        "2024-07-04/2024-05-30T14:27:35.423405/Democracy_Club__04July2024.tsv"
     )
-    elections = ["2024-05-02"]
-
-    def station_record_to_dict(self, record):
-        # changes from council:
-
-        # OLD: All Saints Church Hall, 51 Goodmayes Lane, Ilford, IG3 9SJ
-        # NEW: All Saints Church Hall, 51A Goodmayes Lane, Ilford, IG3 9PB
-        if record.polling_place_id == "2801":
-            record = record._replace(
-                polling_place_address_1="51A Goodmayes Lane",
-                polling_place_postcode="IG3 9PB",
-            )
-
-        # OLD: Hainault Baptist Church Hall, Franklyn Gardens, Ilford IG6 2UT
-        # EXISTING: The Stockade, Davids Way, Hainault, Ilford IG6 3BQ
-        if record.polling_place_id == "2769":
-            record = record._replace(**THE_STOCKADE)
-        return super().station_record_to_dict(record)
+    elections = ["2024-07-04"]
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         if record.addressline6 in [
@@ -45,9 +18,5 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "IG5 0FF",
         ]:
             return None
-
-        # station change: Hainault Baptist to The Stockade
-        if record.polling_place_district_reference == "FA1":
-            record = record._replace(**THE_STOCKADE)
 
         return super().address_record_to_dict(record)
