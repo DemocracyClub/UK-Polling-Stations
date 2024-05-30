8860041b061ab789b6faff8d07e55f6483e55214
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-30 10:38:23 +0100

    Import script for Dover (2024-07-04) (closes #7243)

--- polling_stations/apps/data_importers/management/commands/import_dover.py
+++ polling_stations/apps/data_importers/management/commands/import_dover.py
@@ -1,32 +1,39 @@
-from data_importers.github_importer import BaseGitHubImporter
+from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
-class Command(BaseGitHubImporter):
-    srid = 4326
-    districts_srid = 4326
+class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "DOV"
-    elections = ["2024-05-02"]
-    scraper_name = "wdiv-scrapers/DC-PollingStations-Dover"
-    geom_type = "geojson"
+    addresses_name = "2024-07-04/2024-05-30T10:38:09.548818/DoverDC Democracy_Club__04July2024 (1).CSV"
+    stations_name = "2024-07-04/2024-05-30T10:38:09.548818/DoverDC Democracy_Club__04July2024 (1).CSV"
+    elections = ["2024-07-04"]
 
-    def district_record_to_dict(self, record):
-        poly = self.extract_geometry(record, self.geom_type, self.get_srid("districts"))
+    def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
 
-        return {
-            "internal_council_id": record["Polling_District"],
-            "name": record["Polling_District_ID"],
-            "area": poly,
-            "polling_station_id": f'{record["UPRN"]}-{record["Polling_District"]}',
-        }
+        if uprn in [
+            "10034885006",  # MANAGERS ACCOMMODATION 9 CASTLE HILL ROAD, DOVER
+            "10034877732",  # MOUNT PLEASANT, DENTON LANE, WOOTTON, CANTERBURY
+            "10094398745",  # 3 OAK TREE FARM, ALKHAM, DOVER
+            "10094398744",  # 2 OAK TREE FARM, ALKHAM, DOVER
+            "10094398743",  # 1 OAK TREE FARM, ALKHAM, DOVER
+            "10034877135",  # COPT HILL FARM, WEST HOUGHAM, DOVER
+            "10034875463",  # FERN FARM, ABBEY ROAD, HOUGHAM, DOVER
+            "10034889700",  # MANAGERS ACCOMMODATION THE PLOUGH INN BEEFEATER FOLKESTONE ROAD, CHURCH HOUGHAM
+            "100060911692",  # 53 PRIORY HILL, DOVER
+            "100060911690",  # 51 PRIORY HILL, DOVER
+            "10034873939",  # UPDOWN LODGE, UPDOWN, BETTESHANGER, DEAL
+            "100060883820",  # CRIXHALL HOUSE, BUCKLAND LANE, STAPLE, CANTERBURY
+            "10034882943",  # THE WILLOWS, SANDWICH ROAD, HACKLINGE, DEAL
+            "10034875431",  # ADELAIDE FARM HOUSE, SANDWICH ROAD, HACKLINGE, DEAL
+        ]:
+            return None
 
-    def station_record_to_dict(self, record):
-        location = self.extract_geometry(
-            record, self.geom_type, self.get_srid("stations")
-        )
+        if record.addressline6 in [
+            # suspect
+            "CT3 2BW",
+            "CT14 7PA",
+            "CT14 6AE",
+        ]:
+            return None
 
-        return {
-            "internal_council_id": f'{record["UPRN"]}-{record["Polling_District"]}',
-            "postcode": "",
-            "address": f"{record['Polling_Station']},{record['Station_Address']}",
-            "location": location,
-        }
+        return super().address_record_to_dict(record)
