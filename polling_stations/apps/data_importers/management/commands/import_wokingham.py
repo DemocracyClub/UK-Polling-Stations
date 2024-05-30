76657d4d75743fa870c0a0178914454b5e95eee7
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-30 09:05:16 +0100

    Import script for Wokingham (2024-07-04) (closes #7239)

--- polling_stations/apps/data_importers/management/commands/import_wokingham.py
+++ polling_stations/apps/data_importers/management/commands/import_wokingham.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WOK"
     addresses_name = (
-        "2024-05-02/2024-03-07T15:21:25.732078/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-05-30T09:05:00.651996/Democracy_Club__04July2024.tsv"
     )
     stations_name = (
-        "2024-05-02/2024-03-07T15:21:25.732078/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-05-30T09:05:00.651996/Democracy_Club__04July2024.tsv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
 
@@ -19,6 +19,8 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         if uprn in [
             "14023395",  # SCHOOL BUNGALOW, WESTERN AVENUE, WOODLEY, READING
             "14006686",  # THE LODGE, BEARWOOD COLLEGE, WINNERSH, WOKINGHAM
+            "14000374",  # OAKLEY COTTAGE, BARGE LANE, SWALLOWFIELD, READING
+            "14036682",  # HERONS REACH, LODDON DRIVE, WARGRAVE, READING
         ]:
             return None
 
@@ -28,6 +30,8 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "RG7 1NL",
             # looks wrong
             "RG7 1PS",
+            "RG2 9WE",
+            "RG40 4DA",
         ]:
             return None
 
@@ -35,12 +39,12 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
     def station_record_to_dict(self, record):
         # adding point for: St John’s Church Centre, Church Road, Woodley, Reading, RG5 4QN
-        if record.polling_place_id == "3700":
+        if record.polling_place_id == "4424":
             record = record._replace(polling_place_easting="476847")
             record = record._replace(polling_place_northing="173732")
 
         # adding point for: St Sebastian`s Memorial Hall, Nine Mile Ride, Wokingham, RG40 3BA
-        if record.polling_place_id == "3695":
+        if record.polling_place_id == "4501":
             record = record._replace(polling_place_easting="483182")
             record = record._replace(polling_place_northing="165582")
 
