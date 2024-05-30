0ef9b1f4d02d57704b0c1b3bb3745bf56e0b13bd
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-30 15:38:36 +0100

    Import script for Bournemouth, Christchurch & Poole (2024-07-04) (closes #7288)

--- polling_stations/apps/data_importers/management/commands/import_bcp.py
+++ polling_stations/apps/data_importers/management/commands/import_bcp.py
@@ -3,38 +3,36 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "BPC"
-    addresses_name = (
-        "2024-05-02/2024-03-06T20:20:34.559585/Democracy_Club__02May2024.tsv"
-    )
-    stations_name = (
-        "2024-05-02/2024-03-06T20:20:34.559585/Democracy_Club__02May2024.tsv"
-    )
-    elections = ["2024-05-02"]
+    addresses_name = "2024-07-04/2024-05-30T15:37:51.177065/BCPCouncil_Democracy_Club__04July2024.tsv"
+    stations_name = "2024-07-04/2024-05-30T15:37:51.177065/BCPCouncil_Democracy_Club__04July2024.tsv"
+    elections = ["2024-07-04"]
     csv_delimiter = "\t"
-
-    def station_record_to_dict(self, record):
-        # Fix to match the coordinates of Station A
-        # Station B - St. Andrew`s Parish Centre, 123 Shelbourne Road, Bournemouth BH8 8RD
-        if record.polling_place_id == "16806":
-            record = record._replace(
-                polling_place_northing="92720",
-            )
-
-        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         if record.addressline6 in [
             # split
             "BH1 1NF",
-            "BH14 0RD",
-            "BH11 8BA",
-            "BH23 3JJ",
             "BH1 3EB",
+            "BH11 8BA",
             "BH7 6LL",
-            "BH6 3LF",
+            "BH1 1NF",
             "BH5 1DL",
-            "BH10 5JF",
+            "BH14 9HF",
             "BH6 3NH",
+            "BH23 3JJ",
+            "BH6 3LF",
+            "BH10 5JF",
+            "BH14 0RD",
         ]:
             return None
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # removing postcodes until council responds for Station A and B at:
+        # St. George`s Church Hall (Oakdale), Darby`s Lane, (entrance at junction with Johnston Road), Oakdale, Poole
+        if record.polling_place_id in [
+            "17399",
+            "17425",
+        ]:
+            record = record._replace(polling_place_postcode="")
+        return super().station_record_to_dict(record)
