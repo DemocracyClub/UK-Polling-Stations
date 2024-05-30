a79866bbee20ce03f0096fc7ea7b9a2b4514e13d
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-30 11:06:08 +0100

    Import script for North West Leicestershire (2024-07-04) (closes #7250)

--- polling_stations/apps/data_importers/management/commands/import_north_west_leicestershire.py
+++ polling_stations/apps/data_importers/management/commands/import_north_west_leicestershire.py
@@ -4,12 +4,13 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "NWL"
     addresses_name = (
-        "2024-05-02/2024-03-18T16:40:04.016144/Democracy_Club__02May2024.CSV"
+        "2024-07-04/2024-05-30T11:05:57.723777/Democracy_Club__04July2024.tsv"
     )
     stations_name = (
-        "2024-05-02/2024-03-18T16:40:04.016144/Democracy_Club__02May2024.CSV"
+        "2024-07-04/2024-05-30T11:05:57.723777/Democracy_Club__04July2024.tsv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
@@ -18,6 +19,10 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "200003503741",  # OLD FARMHOUSE, NOTTINGHAM ROAD, STAUNTON HAROLD, ASHBY-DE-LA-ZOUCH
             "10002361080",  # KEEPERS COTTAGE, KEGWORTH LANE, LONG WHATTON, LOUGHBOROUGH
             "200003505244",  # THE AVIARY, WARREN LANE, WHITWICK, COALVILLE
+            "10002353672",  # THE MALTINGS, STATION ROAD, HUGGLESCOTE, COALVILLE
+            "10002361801",  # FLAT 1, SHELLBROOK HOUSE IC, MARKET STREET, ASHBY-DE-LA-ZOUCH
+            "100030551333",  # 68 BURTON ROAD, ASHBY-DE-LA-ZOUCH
+            "10002345003",  # AMBROW HILL, ISLEY WALTON, CASTLE DONINGTON, DERBY
         ]:
             return None
 
