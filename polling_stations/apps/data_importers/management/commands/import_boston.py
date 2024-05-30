f8bda10cd04d33456c360ebe8e93f344a183e1dd
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-30 16:37:01 +0100

    Import script for Boston (2024-07-04) (closes #7293)

--- polling_stations/apps/data_importers/management/commands/import_boston.py
+++ polling_stations/apps/data_importers/management/commands/import_boston.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "BOT"
     addresses_name = (
-        "2024-05-02/2024-04-12T08:19:30.732163/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-05-30T16:36:53.343765/Democracy_Club__04July2024.tsv"
     )
     stations_name = (
-        "2024-05-02/2024-04-12T08:19:30.732163/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-05-30T16:36:53.343765/Democracy_Club__04July2024.tsv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -18,6 +18,8 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         if uprn in [
             "200004473651",  # HARDEN CASTLE, KIRTON DROVE, KIRTON FEN, LINCOLN
             "200004471198",  # 3 WALNUT LODGE, WAINFLEET ROAD, BOSTON
+            "200004471896",  # FARM COTTAGE, IRELAND FARM, FREISTON INGS, BOSTON
+            "200001828505",  # HIDEAWAY COTTAGE, HAMPTON LANE, OLD LEAKE, BOSTON
         ]:
             return None
 
@@ -36,7 +38,7 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
     def station_record_to_dict(self, record):
         # Holland Fen Parish Hall, Holland Fen, Lincoln
-        if record.polling_place_id == "4589":
+        if record.polling_place_id == "4805":
             record = record._replace(
                 polling_place_postcode="LN4 4QH",
                 polling_place_easting="523174",
@@ -44,28 +46,28 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             )
 
         # Church Room Kirton Holme, Kirton Holme
-        if record.polling_place_id == "4576":
+        if record.polling_place_id == "4790":
             record = record._replace(
                 polling_place_easting="526252",
                 polling_place_northing="341966",
             )
 
         # Skirbeck St Nicholas Community Centre, Fishtoft Road, Boston, Lincs
-        if record.polling_place_id == "4490":
+        if record.polling_place_id == "4701":
             record = record._replace(
                 polling_place_easting="533755",
                 polling_place_northing="343209",
             )
 
         # Fenside Community Centre, Taverner Road, Boston
-        if record.polling_place_id == "4471":
+        if record.polling_place_id == "4682":
             record = record._replace(
                 polling_place_easting="531820",
                 polling_place_northing="344645",
             )
 
         # St Thomas Church Hall, London Road Boston,
-        if record.polling_place_id == "4493":
+        if record.polling_place_id == "4705":
             record = record._replace(
                 polling_place_easting="532487",
                 polling_place_northing="342724",
