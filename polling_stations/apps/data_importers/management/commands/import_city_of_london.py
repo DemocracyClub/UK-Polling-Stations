bd1209adf998bfddbdda85e5d19e6057f7327977
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-31 09:20:20 +0100

    Import script for City of London Corporation (2024-07-04) (closes #7309)

--- polling_stations/apps/data_importers/management/commands/import_city_of_london.py
+++ polling_stations/apps/data_importers/management/commands/import_city_of_london.py
@@ -3,23 +3,11 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "LND"
-    addresses_name = "2024-05-02/2024-03-02T12:50:38.332841/Democracy Club - Polling Districts GLA.csv"
-    stations_name = "2024-05-02/2024-03-02T12:50:38.332841/Democracy Club - Polling Stations GLA.csv"
-    elections = ["2024-05-02"]
+    addresses_name = (
+        "2024-07-04/2024-05-31T09:20:13.426583/CoL DemClub Polling Districts.csv"
+    )
+    stations_name = (
+        "2024-07-04/2024-05-31T09:20:13.426583/CoL DemClub Polling Stations.csv"
+    )
+    elections = ["2024-07-04"]
     csv_encoding = "utf-16le"
-
-    def station_record_to_dict(self, record):
-        # "St Bride Foundation Institute"
-        if record.stationcode == "SBF":
-            record = record._replace(xordinate="531586")
-            record = record._replace(yordinate="181081")
-        # "St Giles' Cripplegate"
-        if record.stationcode == "SGC1":
-            record = record._replace(xordinate="532350")
-            record = record._replace(yordinate="181702")
-        # "St Giles' Cripplegate"
-        if record.stationcode == "SGC2":
-            record = record._replace(xordinate="532350")
-            record = record._replace(yordinate="181702")
-
-        return super().station_record_to_dict(record)
