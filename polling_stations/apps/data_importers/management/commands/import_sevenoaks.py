be6e4e1f63c84653dc4c1be108ad606754c94b74
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-31 17:07:52 +0100

    Import script for Sevenoaks (2024-07-04) (closes #7349)

--- polling_stations/apps/data_importers/management/commands/import_sevenoaks.py
+++ polling_stations/apps/data_importers/management/commands/import_sevenoaks.py
@@ -1,29 +1,33 @@
-from data_importers.github_importer import BaseGitHubImporter
+from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
-class Command(BaseGitHubImporter):
-    srid = 4326
+class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SEV"
-    elections = ["2024-05-02"]
-    scraper_name = "wdiv-scrapers/DC-PollingStations-Sevenoaks"
-    geom_type = "geojson"
-
-    def district_record_to_dict(self, record):
-        poly = self.extract_geometry(record, self.geom_type, self.get_srid("districts"))
-        return {
-            "internal_council_id": record["AreaCode"],
-            "name": "%s - %s" % (record["Name"], record["AreaCode"]),
-            "area": poly,
-            "polling_station_id": record["AreaCode"],
-        }
+    addresses_name = "2024-07-04/2024-06-06T12:07:44.241204/SEV_combined.tsv"
+    stations_name = "2024-07-04/2024-06-06T12:07:44.241204/SEV_combined.tsv"
+    elections = ["2024-07-04"]
+    csv_encoding = "windows-1252"
+    csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
-        location = self.extract_geometry(
-            record, self.geom_type, self.get_srid("stations")
-        )
-        return {
-            "internal_council_id": record["AreaCode"],
-            "postcode": "",
-            "address": record["Address"],
-            "location": location,
-        }
+        # St. John`s Church Hall, Quakers Hall Lane, Sevenoaks TN13 3NU
+        if record.polling_place_id == "9509":
+            record = record._replace(
+                polling_place_easting="",
+                polling_place_northing="",
+            )
+
+        return super().station_record_to_dict(record)
+
+    def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "100061006745",  # STANWELL HOUSE, BOTSOM LANE, WEST KINGSDOWN, SEVENOAKS
+            "10094320762",  # 1 THE VIEW, BOTSOM LANE, WEST KINGSDOWN, SEVENOAKS
+            "50002013131",  # HIGH WOOD, BOTSOM LANE, WEST KINGSDOWN, SEVENOAKS
+            "100062544307",  # 1 PUMPING STATION, SAINTS HILL, PENSHURST, TONBRIDGE
+            "100062544308",  # 2 PUMPING STATION, SAINTS HILL, PENSHURST, TONBRIDGE
+        ]:
+            return None
+        return super().address_record_to_dict(record)
