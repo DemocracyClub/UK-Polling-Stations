45e09de76386add57e77734330cf3300db91c11e
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-31 12:38:26 +0100

    Import script for Gosport (2024-07-04) (closes #7324)

--- polling_stations/apps/data_importers/management/commands/import_gosport.py
+++ polling_stations/apps/data_importers/management/commands/import_gosport.py
@@ -3,14 +3,24 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "GOS"
-    addresses_name = "2024-05-02/2024-02-14T15:34:50.440698/2024 LGE & PCC - Democracy Club - Polling Districts.csv"
-    stations_name = "2024-05-02/2024-02-14T15:34:50.440698/2024 LGE & PCC - Democracy Club - Polling Stations.csv"
-    elections = ["2024-05-02"]
+    addresses_name = "2024-07-04/2024-05-31T12:38:18.136820/2024 PGE - Democracy Club Polling Districts v1 (31 05 2024).csv"
+    stations_name = "2024-07-04/2024-05-31T12:38:18.136820/2024 PGE - Democracy Club Polling Stations v1 (31 05 2024).csv"
+    elections = ["2024-07-04"]
+    csv_encoding = "utf-16le"
 
     def station_record_to_dict(self, record):
-        # St. Marys Church Parish Centre, Green Road, Gosport, Hampshire
-        if record.stationcode == "3AL3" or record.stationcode == "4AN1":
-            record = record._replace(xordinate="460159", yordinate="98802")
+        # Fareham stations
+        if record.stationcode in (
+            "48ST1",  # Crofton Community Centre
+            "47HH4",  # Crofton Community Centre
+            "46HH3",  # Crofton Community Centre
+            "44HH1",  # Crofton Community Centre
+            "45HH2",  # Crofton Community Centre
+            "49ST2",  # Stubbington Methodist Church Hall
+            "50ST3",  # Stubbington Methodist Church Hall
+            "51ST4",  # Stubbington Baptist Church
+        ):
+            return None
 
         return super().station_record_to_dict(record)
 
