b6f80e311200838789ff8903e9dc2fd3a1abddf2
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-31 10:12:39 +0100

    Import script for Reigate and Banstead (2024-07-04) (closes #7312)

--- polling_stations/apps/data_importers/management/commands/import_reigate_and_banstead.py
+++ polling_stations/apps/data_importers/management/commands/import_reigate_and_banstead.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "REI"
     addresses_name = (
-        "2024-05-02/2024-03-05T11:26:20.067521/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-06-05T14:13:19.010990/REI_Democracy_Club__04July2024.tsv"
     )
     stations_name = (
-        "2024-05-02/2024-03-05T11:26:20.067521/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-06-05T14:13:19.010990/REI_Democracy_Club__04July2024.tsv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -31,27 +31,3 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             return None
 
         return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # Station change requested by council:
-        # OLD: Banstead Methodist Church, The Drive, Banstead, SM7 1DA
-        # NEW: Nork Community Centre, Nork Way, Banstead SM7 1JB
-        if record.polling_place_id == "4979":
-            record = record._replace(
-                polling_place_name="Nork Community Centre",
-                polling_place_address_1="Nork Way",
-                polling_place_address_2="Banstead",
-                polling_place_address_3="",
-                polling_place_address_4="",
-                polling_place_postcode="SM7 1JB",
-                polling_place_easting="523774",
-                polling_place_northing="159336",
-            )
-
-        # more accurate point for: Woodmansterne Village Hall, Carshalton Road, Woodmansterne, Banstead, Surrey
-        if record.polling_place_id == "5001":
-            record = record._replace(
-                polling_place_easting="527572", polling_place_northing="160117"
-            )
-
-        return super().station_record_to_dict(record)
