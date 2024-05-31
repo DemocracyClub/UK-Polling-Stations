bfceb3c4e307301b4f8a2e79255e45afec783a0c
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-31 14:22:28 +0100

    Import script for Mid Sussex (2024-07-04) (closes #7335)

--- polling_stations/apps/data_importers/management/commands/import_mid_sussex.py
+++ polling_stations/apps/data_importers/management/commands/import_mid_sussex.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "MSS"
     addresses_name = (
-        "2024-05-02/2024-03-21T11:19:04.829570/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-05-31T14:22:13.337174/Democracy_Club__04July2024 (8).tsv"
     )
     stations_name = (
-        "2024-05-02/2024-03-21T11:19:04.829570/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-05-31T14:22:13.337174/Democracy_Club__04July2024 (8).tsv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
 
@@ -19,6 +19,7 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "RH17 7DY",
             "BN6 9NA",
             "RH16 2QB",
+            "RH19 4NQ",
             # suspect
             "RH10 4SH",
         ]:
