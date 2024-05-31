7d3ed99bba642285d30c7cc9727d4abdd1d84a68
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-31 12:53:52 +0100

    Import script for Birmingham (2024-07-04) (closes #7327)

--- polling_stations/apps/data_importers/management/commands/import_birmingham.py
+++ polling_stations/apps/data_importers/management/commands/import_birmingham.py
@@ -1,68 +1,38 @@
-from addressbase.models import Address
 from data_importers.management.commands import BaseHalaroseCsvImporter
-from django.core.exceptions import ObjectDoesNotExist
 
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "BIR"
-    addresses_name = "2024-05-02/2024-03-18T17:12:50.103093/Eros_SQL_Output070.csv"
-    stations_name = "2024-05-02/2024-03-18T17:12:50.103093/Eros_SQL_Output070.csv"
-    elections = ["2024-05-02"]
+    addresses_name = "2024-07-04/2024-06-10T09:15:31.652803/Eros_SQL_Output075.csv"
+    stations_name = "2024-07-04/2024-06-10T09:15:31.652803/Eros_SQL_Output075.csv"
+    elections = ["2024-07-04"]
 
     def station_record_to_dict(self, record):
         # postcode correction for: St Mary and Ambrose Church Hall, Pershore Road, B5 7RL
-        if record.pollingstationnumber in ["123", "124"]:
+        if record.pollingstationnumber in ["18", "19"]:
             record = record._replace(pollingstationpostcode="B5 7RA")
 
         # postcode correction for: The Good Shepherd Hall, R/O St Andrews Church, Slack Lane, B20 9RE
-        if record.pollingstationnumber in ["180", "181"]:
+        if record.pollingstationnumber in ["314", "315"]:
             record = record._replace(pollingstationpostcode="B21 9RE")
 
         # Removing wrong UPRNs:
-
-        # Birchfield Primary School, Trinity Road B6 6AJ
-        # suggested UPRN: 100071470999
-        if self.get_station_hash(record) == "30-birchfield-primary-school":
-            record = record._replace(pollingvenueuprn="")
-
-        # George Dixon Primary School, City Road B17 8LE
-        # suggested UPRN: 100071411446
-        if self.get_station_hash(record) in (
-            "276-george-dixon-primary-school",
-            "277-george-dixon-primary-school",
-        ):
-            record = record._replace(pollingvenueuprn="")
-
-        # St Mary's Catholic Primary School, Vivian Road B17 0DN
-        # suggested UPRN: 100071417689
-        if self.get_station_hash(record) in (
-            "173-st-marys-catholic-primary-school",
-            "172-st-marys-catholic-primary-school",
-        ):
-            record = record._replace(pollingvenueuprn="")
-
-        # Grove Hall, Grove Lane B17 0QB
-        # suggested UPRN: can't find in addressbase, postcode might be wrong?
-        if self.get_station_hash(record) in (
-            "174-grove-hall",
-            "175-grove-hall",
-        ):
-            record = record._replace(pollingvenueuprn="")
-
-        # Wattville Primary School, Wattville Road B21 0DP
-        # suggested UPRN: 100071440040
-        if self.get_station_hash(record) in (
-            "211-wattville-primary-school",
-            "210-wattville-primary-school",
-        ):
-            record = record._replace(pollingvenueuprn="")
-
-        # Shenley Lane Community Association & Sports Centre, 472 Shenley Lane B29 4HZ
-        # suggested UPRN: 100070510240
-        if (
-            self.get_station_hash(record)
-            == "15-shenley-lane-community-association-sports-centre"
-        ):
+        if self.get_station_hash(record) in [
+            "297-birchfield-primary-school",  # Birchfield Primary School, Trinity Road B6 6AJ - suggested UPRN: 100071470999
+            "36-barford-primary-school-nursery-unit",  # George Dixon Primary School, City Road B17 8LE - suggested UPRN: 100071411446
+            "38-george-dixon-primary-school",  # George Dixon Primary School, City Road B17 8LE - suggested UPRN: 100071411446
+            "37-george-dixon-primary-school",  # George Dixon Primary School, City Road B17 8LE - suggested UPRN: 100071411446
+            "9-st-marys-catholic-church-hall",  # St Mary's Catholic Primary School, Vivian Road B17 0DN - suggested UPRN: 100071417689
+            "10-st-marys-catholic-church-hall",  # St Mary's Catholic Primary School, Vivian Road B17 0DN - suggested UPRN: 100071417689
+            "31-grove-hall",  # Grove Hall, Grove Lane B17 0QB - suggested UPRN: can't find in addressbase, postcode might be wrong?
+            "32-grove-hall",  # Grove Hall, Grove Lane B17 0QB - suggested UPRN: can't find in addressbase, postcode might be wrong?
+            "321-wattville-primary-school",  # Wattville Primary School, Wattville Road B21 0DP - suggested UPRN: 100071440040
+            "320-wattville-primary-school",  # Wattville Primary School, Wattville Road B21 0DP - suggested UPRN: 100071440040
+            "245-shenley-lane-community-association-sports-centre",  # Shenley Lane Community Association & Sports Centre, 472 Shenley Lane B29 4HZ - suggested UPRN: 100070510240
+            "322-wattville-primary-school",  # Wattville Primary School Wattville Road
+            "29-st-marys-catholic-primary-school",  # St Mary's Catholic Primary School Vivian Road
+            "30-st-marys-catholic-primary-school",  # St Mary's Catholic Primary School Vivian Road
+        ]:
             record = record._replace(pollingvenueuprn="")
 
         return super().station_record_to_dict(record)
@@ -94,26 +64,27 @@ class Command(BaseHalaroseCsvImporter):
 
         if record.housepostcode in [
             # splits
-            "B31 2AE",
-            "B75 5QB",
-            "B34 6NE",
-            "B23 5AL",
-            "B34 6HN",
-            "B75 5NE",
+            "B32 2NT",
             "B31 5NH",
+            "B23 5AL",
             "B26 3HH",
-            "B31 3JE",
+            "B31 2FL",
             "B10 9JS",
             "B20 3QT",
-            "B32 2NT",
-            "B31 1AE",
             "B13 9UA",
-            "B31 2FL",
-            "B28 9QL",
-            "B24 9HX",
+            "B75 5NE",
+            "B31 1AE",
+            "B75 5QB",
+            "B34 6HN",
+            "B34 6NE",
             "B23 7XE",
+            "B11 1LJ",
+            "B31 2AE",
+            "B28 9QL",
             "B18 5BU",
+            "B31 3JE",
             "B33 9QD",
+            "B24 9HX",
             # looks wrong
             "B32 3QY",
             "B33 0AW",
@@ -122,12 +93,3 @@ class Command(BaseHalaroseCsvImporter):
             return None
 
         return super().address_record_to_dict(record)
-
-    # quick fix to show maps for Halarose records that have a valid UPRN in the PollingVenueUPRN field
-    def get_station_point(self, record):
-        uprn = record.pollingvenueuprn.strip().lstrip("0")
-        try:
-            ab_rec = Address.objects.get(uprn=uprn)
-            return ab_rec.location
-        except ObjectDoesNotExist:
-            return super().get_station_point(record)
