d51706fe5fe6bdc0f044b1b93dafd748b812a1a0
polling-bot-4000 <developers@democracyclub.org.uk>
2024-05-31 13:32:50 +0100

    Import script for Islington (2024-07-04) (closes #7331)

--- polling_stations/apps/data_importers/management/commands/import_islington.py
+++ polling_stations/apps/data_importers/management/commands/import_islington.py
@@ -4,21 +4,20 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "ISL"
     addresses_name = (
-        "2024-05-02/2024-03-15T12:44:49.222514/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-05-31T13:32:36.325253/Democracy_Club__04July2024.tsv"
     )
     stations_name = (
-        "2024-05-02/2024-03-15T12:44:49.222514/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-05-31T13:32:36.325253/Democracy_Club__04July2024.tsv"
     )
-    elections = ["2024-05-02"]
-    csv_encoding = "windows-1252"
+    elections = ["2024-07-04"]
     csv_delimiter = "\t"
 
     # The following stations generate mismatched postcode warnings but have been verified
     # with the council as correct:
-    # 'St Joan of Arc Community Centre, Kelross Road, London, N5 2QN' (id: 2689)
-    # 'N4 Library, 26 Blackstock Road, London, N4 2JF' (id: 2692)
-    # 'Harry Rice Hall, 72-74 Hargrave Park, London, N19 5JN' (id: 2571)
-    # 'Cornwallis Adventure Playground, Cornwallis Road, London, N19 4LP' (id: 2583)
-    # 'Vernon Hall, Vernon Square, King's Cross Road, WC1X 9EP' (id: 2636)
-    # 'Williamson Street Community Centre, Trefil Walk, Parkhurst Road, London, N7 0SX' (id: 2662)
-    # 'Aubert Court Community Centre, Avenell Road, N5 1BT' (id: 2700)
+    # 'St Joan of Arc Community Centre, Kelross Road, London, N5 2QN' (id: 2904)
+    # 'N4 Library, 26 Blackstock Road, London, N4 2JF' (id: 2926)
+    # 'Harry Rice Hall, 72-74 Hargrave Park, London, N19 5JN' (id: 2870)
+    # 'Cornwallis Adventure Playground, Cornwallis Road, London, N19 4LP' (id: 2882)
+    # 'Vernon Hall, Vernon Square, King's Cross Road, WC1X 9EP' (id: 2966)
+    # 'Williamson Street Community Centre, Trefil Walk, Parkhurst Road, London, N7 0SX' (id: 2987)
+    # 'Aubert Court Community Centre, Avenell Road, N5 1BT' (id: 2895)
