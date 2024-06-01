76fa5cb5d9f35ca0dc601e4b0a8668ae4414c67d
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-01 16:22:58 +0100

    Import script for North Yorkshire (2024-07-04) (closes #7357)

--- polling_stations/apps/data_importers/management/commands/import_north_yorkshire.py
+++ polling_stations/apps/data_importers/management/commands/import_north_yorkshire.py
@@ -4,24 +4,14 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "NYE"
     addresses_name = (
-        "2024-05-02/2024-04-04T11:45:50.805143/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-06-01T16:22:27.231258/Democracy_Club__04July2024.tsv"
     )
     stations_name = (
-        "2024-05-02/2024-04-04T11:45:50.805143/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-06-01T16:22:27.231258/Democracy_Club__04July2024.tsv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
-
-    def station_record_to_dict(self, record):
-        # St Michaels & All Angels Church Hall - Scarborough, Filey Road, Scarborough, YO11 3JG
-        if record.polling_place_id == "80279":
-            record = record._replace(polling_place_postcode="YO11 3AA")
-        # Low Row and Feetham Literary Institute, Low Row DL10 6NA
-        if record.polling_place_id == "79859":
-            record = record._replace(polling_place_postcode="DL11 6NA")
-
-        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
@@ -67,8 +57,12 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "DL11 6NT",
             "YO41 1JF",
             "HG5 0SB",
+            "DL11 6PE",
+            "HG3 1LT",
+            "HG5 0FT",
             # suspect
-            "DL10 5EY" "DL8 3DF",
+            "DL10 5EY",
+            "DL8 3DF",
         ]:
             return None
         return super().address_record_to_dict(record)
