e54d501dcfa1edeed312ccc686686009d58db8c7
awdem <wdemontmollin@gmail.com>
2024-06-01 09:50:38 +0100

    restores postcodes after DND council confirmation

--- polling_stations/apps/data_importers/management/commands/import_dundee.py
+++ polling_stations/apps/data_importers/management/commands/import_dundee.py
@@ -8,17 +8,10 @@ class Command(BaseHalaroseCsvImporter):
     elections = ["2024-07-04"]
 
     def station_record_to_dict(self, record):
+        # The following stations' postcodes have been verified by the council:
         # 'Wellgate Sheltered Housing, 8 King Street, Dundee, DD1 2JB' (id: 78)
-        if record.pollingvenueid == "78":
-            record = record._replace(pollingstationpostcode="")
-
         # 'Marryat Hall, City Square, Dundee, DD1 3BG' (id: 79)
-        if record.pollingvenueid == "79":
-            record = record._replace(pollingstationpostcode="")
-
         # 'Foula Terrace Sheltered, Housing Communal Lounge, Foula Terrace, Dundee, DD4 9SS' (id: 5)
-        if record.pollingvenueid == "5":
-            record = record._replace(pollingstationpostcode="")
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
