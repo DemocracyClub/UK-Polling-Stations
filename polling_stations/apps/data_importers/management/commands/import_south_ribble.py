3f30144bd7d0234287131c2b592349b22a36bddc
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-03 17:55:24 +0100

    Import script for South Ribble (2024-07-04) (closes #7422)

--- polling_stations/apps/data_importers/management/commands/import_south_ribble.py
+++ polling_stations/apps/data_importers/management/commands/import_south_ribble.py
@@ -1,13 +1,11 @@
-from addressbase.models import Address
 from data_importers.management.commands import BaseHalaroseCsvImporter
-from django.core.exceptions import ObjectDoesNotExist
 
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "SRI"
-    addresses_name = "2024-05-02/2024-03-19T15:12:00.400914/Eros_SQL_Output004.csv"
-    stations_name = "2024-05-02/2024-03-19T15:12:00.400914/Eros_SQL_Output004.csv"
-    elections = ["2024-05-02"]
+    addresses_name = "2024-07-04/2024-06-10T14:32:56.226915/combined.csv"
+    stations_name = "2024-07-04/2024-06-10T14:32:56.226915/combined.csv"
+    elections = ["2024-07-04"]
 
     def address_record_to_dict(self, record):
         if record.housepostcode in [
@@ -20,12 +18,3 @@ class Command(BaseHalaroseCsvImporter):
         ]:
             return None
         return super().address_record_to_dict(record)
-
-    # quick fix to show maps for Halarose records that have a valid UPRN in the PollingVenueUPRN field
-    def get_station_point(self, record):
-        uprn = record.pollingvenueuprn.strip().lstrip("0")
-        try:
-            ab_rec = Address.objects.get(uprn=uprn)
-            return ab_rec.location
-        except ObjectDoesNotExist:
-            return super().get_station_point(record)
