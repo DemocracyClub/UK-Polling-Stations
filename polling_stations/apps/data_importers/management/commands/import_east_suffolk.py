ffaf32ce32229c4260d9a35bb2605729e30bf714
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-03 18:14:41 +0100

    Import script for East Suffolk (2024-07-04) (closes #7425)

--- polling_stations/apps/data_importers/management/commands/import_east_suffolk.py
+++ polling_stations/apps/data_importers/management/commands/import_east_suffolk.py
@@ -3,20 +3,10 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "ESK"
-    addresses_name = "2024-05-02/2024-03-11T11:06:27.248527/Democracy Club - Polling Districts - East Suffolk - 2 May 2024.csv"
-    stations_name = "2024-05-02/2024-03-11T11:06:27.248527/Democracy Club - Polling Stations - East Suffolk - 2 May 2024.csv"
-    elections = ["2024-05-02"]
+    addresses_name = "2024-07-04/2024-06-03T18:14:32.218707/East Suffolk Council - Democracy Club - Polling Districts 4 July 2024.csv"
+    stations_name = "2024-07-04/2024-06-03T18:14:32.218707/East Suffolk Council - Democracy Club - Polling Stations 4 July 2024.csv"
+    elections = ["2024-07-04"]
     csv_encoding = "utf-16le"
-
-    def station_record_to_dict(self, record):
-        # Coordinate change from council for:
-        # The Fromus Community Centre, Saxmundham Hub, Street Farm Road, Saxmundham
-        if record.pollingstationid == "4794":
-            record = record._replace(
-                xordinate="638713",
-                yordinate="263352",
-            )
-        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
@@ -28,8 +18,11 @@ class Command(BaseDemocracyCountsCsvImporter):
         ]:
             return None
         if record.postcode in [
-            "NR35 1BZ",  # split
-            "NR34 8ET",  # weird
+            # splits
+            "NR34 7PU",
+            "NR35 1BZ",
+            # suspect
+            "NR34 8ET",
         ]:
             return None
 
