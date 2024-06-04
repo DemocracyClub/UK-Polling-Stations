bd2191b8817dd274bd0fdaf2a450725cbe346747
GeoWill <roper.william@gmail.com>
2024-06-04 13:13:23 +0100

    New Addressbase means updating uprn to ballot lookups

--- deploy/files/scripts/user_scripts/sync_baked_elections_parquet.sh
+++ deploy/files/scripts/user_scripts/sync_baked_elections_parquet.sh
@@ -4,4 +4,4 @@ set -xeEo pipefail
 
 mkdir -p /var/www/polling_stations/baked_elections/
 # Always get the prod data, as we don't have stage or dev copies of this data yet.
-aws s3 sync s3://pollingstations.private.data/addressbase/2023-12-15/uprn-to-ballots-outcodes/ /var/www/polling_stations/baked_elections/
+aws s3 sync s3://pollingstations.private.data/addressbase/2024-05-01/uprn-to-ballots-outcodes/ /var/www/polling_stations/baked_elections/
