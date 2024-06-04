f1b71cea3cd789e98260519954d7aac965a7d528
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-04 09:33:53 +0100

    Import script for Tendring (2024-07-04) (closes #7442)

--- polling_stations/apps/data_importers/management/commands/import_tendring.py
+++ polling_stations/apps/data_importers/management/commands/import_tendring.py
@@ -3,26 +3,31 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "TEN"
-    addresses_name = "2024-05-02/2024-04-11T11:20:01.019608/DC POlling Districts.csv"
-    stations_name = "2024-05-02/2024-04-11T11:20:01.019608/DC Polling Stations.csv"
-    elections = ["2024-05-02"]
+    addresses_name = (
+        "2024-07-04/2024-06-04T09:33:44.379451/Democracy Club - Polling Districts.csv"
+    )
+    stations_name = (
+        "2024-07-04/2024-06-04T09:33:44.379451/Democracy Club - Polling Stations.csv"
+    )
+    elections = ["2024-07-04"]
     csv_encoding = "utf-16le"
 
     def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
-
-        if uprn in [
-            "10094250773",  # 5A CHAPEL LANE, THORRINGTON, COLCHESTER
-        ]:
-            return None
         if record.postcode in [
-            "CO12 4QT",  # split
             # suspect
             "CO15 1HW",
             "CO15 3BN",
             "CO15 1JG",
-            "CO15 1HN",
             "CO15 1JL",
         ]:
             return None
+
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # dirty workaround to remove 32 Colchester stations with no addresses assigned
+        # these records are ordered, we can just exclude them by station_code
+        if int(record.stationcode) > 77:
+            return None
+
+        return super().station_record_to_dict(record)
