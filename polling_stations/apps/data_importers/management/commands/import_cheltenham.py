22d9ed3c6591a79dd79abd95303fef2b576ffa2f
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-04 14:53:53 +0100

    Import script for Cheltenham (2024-07-04) (closes #7465)

--- polling_stations/apps/data_importers/management/commands/import_cheltenham.py
+++ polling_stations/apps/data_importers/management/commands/import_cheltenham.py
@@ -1,13 +1,11 @@
-from addressbase.models import Address
 from data_importers.management.commands import BaseHalaroseCsvImporter
-from django.core.exceptions import ObjectDoesNotExist
 
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "CHT"
-    addresses_name = "2024-05-02/2024-03-15T09:36:32.391996/Eros_SQL_Output009.csv"
-    stations_name = "2024-05-02/2024-03-15T09:36:32.391996/Eros_SQL_Output009.csv"
-    elections = ["2024-05-02"]
+    addresses_name = "2024-07-04/2024-06-04T14:53:31.031794/Eros_SQL_Output011.csv"
+    stations_name = "2024-07-04/2024-06-04T14:53:31.031794/Eros_SQL_Output011.csv"
+    elections = ["2024-07-04"]
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
@@ -17,6 +15,7 @@ class Command(BaseHalaroseCsvImporter):
             "200002684616",  # 49A BACK MONTPELLIER TERRACE, CHELTENHAM
             "100120413433",  # FLAT, 19 SUFFOLK ROAD, CHELTENHAM
             "100120399951",  # FARROW & BALL, 15-17 SUFFOLK ROAD, CHELTENHAM
+            "10091672800",  # HOLLY BLUE HOUSE, LONDON ROAD, CHELTENHAM
         ]:
             return None
 
@@ -32,11 +31,9 @@ class Command(BaseHalaroseCsvImporter):
 
         return super().address_record_to_dict(record)
 
-    # quick fix to show maps for Halarose records that have a valid UPRN in the PollingVenueUPRN field
-    def get_station_point(self, record):
-        uprn = record.pollingvenueuprn.strip().lstrip("0")
-        try:
-            ab_rec = Address.objects.get(uprn=uprn)
-            return ab_rec.location
-        except ObjectDoesNotExist:
-            return super().get_station_point(record)
+    def station_record_to_dict(self, record):
+        # Below stations are throwing a warning about different postcode in the addressbase, but council requested to keep them.
+        # HILLVIEW COMMUNITY CENTRE, HULBERT CRESCENT, UP HATHERLEY, CHELTENHAM, GL51 3EB
+        # OAKLEY COMMUNITY, RESOURCE CENTRE, CLYDE CRESCENT, CHELTENHAM, GL52 5QL
+
+        return super().station_record_to_dict(record)
