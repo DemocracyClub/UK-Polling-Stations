087a1d85b796f8f07c35d21d0313ddb77cd9415e
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-04 17:04:47 +0100

    Import script for Tandridge (2024-07-04) (closes #7487)

--- polling_stations/apps/data_importers/management/commands/import_tandridge.py
+++ polling_stations/apps/data_importers/management/commands/import_tandridge.py
@@ -3,9 +3,9 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "TAN"
-    addresses_name = "2024-05-02/2024-04-05T13:03:57.410329/Eros_SQL_Output005.csv"
-    stations_name = "2024-05-02/2024-04-05T13:03:57.410329/Eros_SQL_Output005.csv"
-    elections = ["2024-05-02"]
+    addresses_name = "2024-07-04/2024-06-04T17:04:40.456886/Eros_SQL_Output006.csv"
+    stations_name = "2024-07-04/2024-06-04T17:04:40.456886/Eros_SQL_Output006.csv"
+    elections = ["2024-07-04"]
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
@@ -14,10 +14,13 @@ class Command(BaseHalaroseCsvImporter):
             "200000141065",  # OLD MOAT BARN, ARDENRUN, LINGFIELD
             "200000140050",  # SURREY BEECHES, WESTERHAM ROAD, WESTERHAM
             "200000140051",  # SURREY BEECHES WEST, WESTERHAM ROAD, WESTERHAM
+            "100061586276",  # CLOVERLAY, ROCKFIELD ROAD, OXTED
         ]:
             return None
+
         if record.housepostcode in [
             "CR6 9LB",  # split
         ]:
             return None
+
         return super().address_record_to_dict(record)
