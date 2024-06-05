aa2445f5c58e36c7fa720a50d3733dc357fce0e2
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-05 23:48:45 +0100

    Import script for Brent (2024-07-04) (closes #7540)

--- polling_stations/apps/data_importers/management/commands/import_brent.py
+++ polling_stations/apps/data_importers/management/commands/import_brent.py
@@ -4,21 +4,13 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "BEN"
     addresses_name = (
-        "2024-05-02/2024-03-06T09:38:20.428353/Polling District  - Democracy Club.csv"
+        "2024-07-04/2024-06-05T23:48:16.041079/DC -polling districts.csv 4 July.csv"
     )
     stations_name = (
-        "2024-05-02/2024-03-06T09:38:20.428353/Polling Stations - Democracy Club.csv"
+        "2024-07-04/2024-06-05T23:48:16.041079/DC -polling stations.csv 4 July.csv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
     csv_encoding = "utf-16le"
-
-    def station_record_to_dict(self, record):
-        # point correction for: St Mary's Willesden Parish Church, Neasden Lane, London, NW10 2TS
-        if record.stationcode == "ERW480":
-            record = record._replace(yordinate="184797")
-            record = record._replace(xordinate="521445")
-
-        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
@@ -27,8 +19,6 @@ class Command(BaseDemocracyCountsCsvImporter):
             "202066948",  # 6 MOUNT PLEASANT, WEMBLEY
             "202196807",  # FLAT EAST LANE PAVILION EAST LANE, WEMBLEY
             "202030534",  # 101 WINCHESTER AVENUE, LONDON
-            "202142340",  # 106 NORTON ROAD, WEMBLEY
-            "202208354",  # 108 NORTON ROAD, WEMBLEY
         ]:
             return None
 
