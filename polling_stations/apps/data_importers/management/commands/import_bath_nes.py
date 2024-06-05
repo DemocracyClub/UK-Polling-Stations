7a5d17b33d2463735448c52426d40fa1ca678f73
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-05 10:22:52 +0100

    Import script for Bath & North East Somerset (2024-07-04) (closes #7493)

--- polling_stations/apps/data_importers/management/commands/import_bath_nes.py
+++ polling_stations/apps/data_importers/management/commands/import_bath_nes.py
@@ -1,13 +1,11 @@
-from addressbase.models import Address
 from data_importers.management.commands import BaseHalaroseCsvImporter
-from django.core.exceptions import ObjectDoesNotExist
 
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "BAS"
-    addresses_name = "2024-05-02/2024-03-15T16:30:23.414487/Bath&NESomerset.csv"
-    stations_name = "2024-05-02/2024-03-15T16:30:23.414487/Bath&NESomerset.csv"
-    elections = ["2024-05-02"]
+    addresses_name = "2024-07-04/2024-06-21T14:41:21.417224/BAS_combined_v3.csv"
+    stations_name = "2024-07-04/2024-06-21T14:41:21.417224/BAS_combined_v3.csv"
+    elections = ["2024-07-04"]
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
@@ -29,21 +27,3 @@ class Command(BaseHalaroseCsvImporter):
         ]:
             return None
         return super().address_record_to_dict(record)
-
-    def get_station_point(self, record):
-        uprn = record.pollingvenueuprn.strip().lstrip("0")
-        try:
-            ab_rec = Address.objects.get(uprn=uprn)
-            return ab_rec.location
-        except ObjectDoesNotExist:
-            return super().get_station_point(record)
-
-    def station_record_to_dict(self, record):
-        # Weston Free Church, High Street, Upper Weston, Bath BA1 4DB
-        if self.get_station_hash(record) in (
-            "144-weston-free-church",
-            "145-weston-free-church",
-        ):
-            record = record._replace(pollingvenueuprn="10001147059")
-
-        return super().station_record_to_dict(record)
