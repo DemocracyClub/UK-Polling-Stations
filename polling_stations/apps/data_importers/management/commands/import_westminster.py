93b903510b721b7369fab9398a2a1ccc873a0763
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-05 15:41:42 +0100

    Import script for City of Westminster (2024-07-04) (closes #7520)

--- polling_stations/apps/data_importers/management/commands/import_westminster.py
+++ polling_stations/apps/data_importers/management/commands/import_westminster.py
@@ -3,9 +3,9 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "WSM"
-    addresses_name = "2024-05-02/2024-03-27T14:42:03.748146/Eros_SQL_Output001.csv"
-    stations_name = "2024-05-02/2024-03-27T14:42:03.748146/Eros_SQL_Output001.csv"
-    elections = ["2024-05-02"]
+    addresses_name = "2024-07-04/2024-06-13T16:14:48.759304/WSM_combined.csv"
+    stations_name = "2024-07-04/2024-06-13T16:14:48.759304/WSM_combined.csv"
+    elections = ["2024-07-04"]
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
@@ -16,19 +16,22 @@ class Command(BaseHalaroseCsvImporter):
             "100022749688",  # 50 ELNATHAN MEWS, LONDON
             "10033529430",  # HYDE PARK BUCK HILL LODGE BAYSWATER ROAD, LONDON
             "100023382377",  # QUEENS GATE LODGE, HYDE PARK GATE, LONDON
+            "10033641257",  # HOUSEBOAT WILLOW OPPOSITE 49 BLOMFIELD ROAD, LONDON
+            "10033575787",  # UPPER PENTHOUSE FLAT 12 PARK STREET, LONDON
+            "10033575789",  # LOWER PENTHOUSE FLAT 12 PARK STREET, LONDON
         ]:
             return None
 
         if record.housepostcode in [
             # split
             "W2 5HA",
+            "W1K 7JB",
             "W2 6PF",
             "SW1P 4JZ",
-            "NW8 8LH",
-            "W1K 7JB",
+            "SW1V 4AF",
             "W9 3DW",
             "W9 2AL",
-            "SW1V 4AF",
+            "NW8 8LH",
         ]:
             return None
 
