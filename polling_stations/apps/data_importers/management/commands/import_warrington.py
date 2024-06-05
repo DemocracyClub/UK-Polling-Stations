8f438419b1d77ba7d68ce64154335ce5f19e0463
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-05 23:13:41 +0100

    Import script for Warrington (2024-07-04) (closes #7538)

--- polling_stations/apps/data_importers/management/commands/import_warrington.py
+++ polling_stations/apps/data_importers/management/commands/import_warrington.py
@@ -3,28 +3,38 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WRT"
-    addresses_name = (
-        "2024-05-02/2024-03-12T10:53:14.351979/Democracy_Club__02May2024.csv"
-    )
-    stations_name = (
-        "2024-05-02/2024-03-12T10:53:14.351979/Democracy_Club__02May2024.csv"
-    )
-    elections = ["2024-05-02"]
+    addresses_name = "2024-07-04/2024-06-14T13:23:35.081082/WRT_combined.tsv"
+    stations_name = "2024-07-04/2024-06-14T13:23:35.081082/WRT_combined.tsv"
+    elections = ["2024-07-04"]
+    csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
-        # St Mary Magdalenes Church Hall, 87 Dingleway, Appleton, Warrington WA4 3AG
-        if record.polling_place_id == "2142":
+        # Woolston Social Club, 50 Manchester Road, Woolston, Warrington
+        if record.polling_place_id == "3224":
             record = record._replace(
-                polling_place_easting="362050",
-                polling_place_northing="385487",
+                polling_place_easting="",
+                polling_place_northing="",
             )
 
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "200000979589",  # CULCHETH VET SURGERY, 487 WARRINGTON ROAD, CULCHETH, WARRINGTON
+            "10094964210",  # 1 SWANICK WAY, WARRINGTON
+            "200000985719",  # 37A WINWICK STREET, WARRINGTON
+            "10094964692",  # 14 WESTON PARK DRIVE, WARRINGTON
+        ]:
+            return None
+
         if record.addressline6 in [
             # split
             "WA4 5PQ",
+            "WA4 1UN",
+            "WA5 9YX",
+            "WA2 0RT",
             # suspect
             "WA5 3BF",
         ]:
