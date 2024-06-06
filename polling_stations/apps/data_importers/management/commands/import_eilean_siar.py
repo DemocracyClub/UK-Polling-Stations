7773a74647f9397e38e858201234583077ed8667
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-06 13:12:18 +0100

    Import script for Comhairle nan Eilean Siar (2024-07-04) (closes #7583)

--- polling_stations/apps/data_importers/management/commands/import_eilean_siar.py
+++ polling_stations/apps/data_importers/management/commands/import_eilean_siar.py
@@ -3,8 +3,7 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "ELS"
-    addresses_name = "2022-05-05/2022-03-18T12:34:38.484076/WI Democracy Club - Polling Districts.csv"
-    stations_name = (
-        "2022-05-05/2022-03-18T12:34:38.484076/WI Democracy Club - Polling Stations.csv"
-    )
-    elections = ["2022-05-05"]
+    addresses_name = "2024-07-04/2024-06-06T13:12:09.503968/Democracy Club - Polling Districts NHEAI.csv"
+    stations_name = "2024-07-04/2024-06-06T13:12:09.503968/Democracy Club - Polling Stations NHEAI.csv"
+    elections = ["2024-07-04"]
+    csv_encoding = "utf-16le"
