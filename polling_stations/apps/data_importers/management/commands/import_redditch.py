b2fca2273d4a92908c45d98ca54006aa86284b1a
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-06 10:35:07 +0100

    Import script for Redditch (2024-07-04) (closes #7567)

--- polling_stations/apps/data_importers/management/commands/import_redditch.py
+++ polling_stations/apps/data_importers/management/commands/import_redditch.py
@@ -4,19 +4,9 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "RED"
     addresses_name = (
-        "2024-05-02/2024-03-08T10:45:07.238107/Redditch Democracy_Club__02May2024.CSV"
+        "2024-07-04/2024-06-06T10:34:58.710969/Redditch Democracy_Club__04July2024.CSV"
     )
     stations_name = (
-        "2024-05-02/2024-03-08T10:45:07.238107/Redditch Democracy_Club__02May2024.CSV"
+        "2024-07-04/2024-06-06T10:34:58.710969/Redditch Democracy_Club__04July2024.CSV"
     )
-    elections = ["2024-05-02"]
-
-    def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "100120639304",  # PARKLANDS CARE HOME, CALLOW HILL LANE, CALLOW HILL, REDDITCH
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
+    elections = ["2024-07-04"]
