c5c513f52d3891f528787fe25c6c9ec52d0097a6
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-06 11:29:14 +0100

    Import script for Council of the Isles of Scilly (2024-07-04) (closes #7575)

--- polling_stations/apps/data_importers/management/commands/import_isles_of_scilly.py
+++ polling_stations/apps/data_importers/management/commands/import_isles_of_scilly.py
@@ -4,26 +4,10 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "IOS"
     addresses_name = (
-        "2024-05-02/2024-04-16T18:51:35.252695/2024_05_Scilly_polling_districts.csv"
+        "2024-07-04/2024-06-06T11:29:05.281066/2024_07_Scilly_polling_districts.csv"
     )
     stations_name = (
-        "2024-05-02/2024-04-16T18:51:35.252695/2024_05_Scilly_polling_stations.csv"
+        "2024-07-04/2024-06-06T11:29:05.281066/2024_07_Scilly_polling_stations.csv"
     )
-    elections = ["2024-05-02"]
-
-    def address_record_to_dict(self, record):
-        if record.postcode in [
-            # splits
-            "TR21 0AB"
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # more accurate point for: Old Wesleyan Chapel, Old Wesleyan Chapel (rear entrance), Garrison Lane, St Mary's, Isles of Scilly, TR21 0JD
-        if record.pollingstationid == "36":
-            record = record._replace(xordinate="090208")
-            record = record._replace(yordinate="010539")
-
-        return super().station_record_to_dict(record)
+    elections = ["2024-07-04"]
+    csv_encoding = "utf-16le"
