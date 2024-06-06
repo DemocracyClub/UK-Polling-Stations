59cfae82683c540ed1d6d2f2e814dbfc78acc794
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-06 09:59:30 +0100

    Import script for Fareham (2024-07-04) (closes #7550)

--- polling_stations/apps/data_importers/management/commands/import_fareham.py
+++ polling_stations/apps/data_importers/management/commands/import_fareham.py
@@ -3,13 +3,9 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "FAR"
-    addresses_name = (
-        "2024-05-02/2024-03-15T10:22:32.464446/Democracy_Club__02May2024.tsv"
-    )
-    stations_name = (
-        "2024-05-02/2024-03-15T10:22:32.464446/Democracy_Club__02May2024.tsv"
-    )
-    elections = ["2024-05-02"]
+    addresses_name = "2024-07-04/2024-06-14T13:14:35.522442/FAR_combined.tsv"
+    stations_name = "2024-07-04/2024-06-14T13:14:35.522442/FAR_combined.tsv"
+    elections = ["2024-07-04"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
