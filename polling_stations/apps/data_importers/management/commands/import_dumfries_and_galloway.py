1e8c7bd879ccc82357f005be136589c9d6bbdf14
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-06 10:38:46 +0100

    Import script for Dumfries and Galloway (2024-06-27) (closes #7569)

--- polling_stations/apps/data_importers/management/commands/import_dumfries_and_galloway.py
+++ polling_stations/apps/data_importers/management/commands/import_dumfries_and_galloway.py
@@ -3,29 +3,7 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "DGY"
-    addresses_name = (
-        "2022-05-05/2022-04-13T11:33:08.115803/April 22 Polling Districts.csv"
-    )
-    stations_name = (
-        "2022-05-05/2022-04-13T11:33:08.115803/April 2022 Polling Stations.csv"
-    )
-    elections = ["2022-05-05"]
-
-    def station_record_to_dict(self, record):
-        # MIDDLEBIE COMMUNITY CENTRE, MIDDLEBIE, LOCKERBIE
-        if record.stationcode == "AEE8":
-            record = record._replace(xordinate="")
-            record = record._replace(yordinate="")
-
-        # THORNHILL COMMUNITY CENTRE, EAST BACK STREET, THORNHILL
-        if record.stationcode == "MUN11":
-            record = record._replace(xordinate="")
-            record = record._replace(yordinate="")
-
-        return super().station_record_to_dict(record)
-
-    def address_record_to_dict(self, record):
-        if record.postcode in ["DG8 0BZ"]:
-            return None
-
-        return super().address_record_to_dict(record)
+    addresses_name = "2024-06-27/2024-06-06T10:38:14.688830/Democracy Club - Dumfries and Galloway Polling Districts.csv"
+    stations_name = "2024-06-27/2024-06-06T10:38:14.688830/Democracy Club - Dumfries and Galloway Polling Stations.csv"
+    elections = ["2024-06-27"]
+    csv_encoding = "utf-16le"
