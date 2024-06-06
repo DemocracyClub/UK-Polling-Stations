369571f2d99a66b0fb2e08f6726db26410e37cda
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-06 10:34:44 +0100

    Import script for Bromsgrove (2024-07-04) (closes #7565)

--- polling_stations/apps/data_importers/management/commands/import_bromsgrove.py
+++ polling_stations/apps/data_importers/management/commands/import_bromsgrove.py
@@ -3,14 +3,9 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "BRM"
-    addresses_name = (
-        "2024-05-02/2024-03-18T11:26:35.230552/Bromsgrove Democracy_Club__02May2024.tsv"
-    )
-    stations_name = (
-        "2024-05-02/2024-03-18T11:26:35.230552/Bromsgrove Democracy_Club__02May2024.tsv"
-    )
-    elections = ["2024-05-02"]
-    csv_delimiter = "\t"
+    addresses_name = "2024-07-04/2024-06-06T10:34:34.598327/Bromsgrove Democracy_Club__04July2024.CSV"
+    stations_name = "2024-07-04/2024-06-06T10:34:34.598327/Bromsgrove Democracy_Club__04July2024.CSV"
+    elections = ["2024-07-04"]
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
@@ -22,11 +17,11 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
         if record.addressline6 in [
             # splits
+            "B60 3AZ",
             "B61 0NX",
+            "B48 7BS",
             "B45 8HY",
             "DY9 9XT",
-            "B60 3AZ",
-            "B48 7BS",
             "B61 7AY",
         ]:
             return None
@@ -35,28 +30,16 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
     def station_record_to_dict(self, record):
         # Warnings below checked and no correction needed:
-        # WARNING: Polling station Broadmeadow Junior School (7941) is in Birmingham City Council (BIR)
-
-        # adding point for: Changing Rooms Barnsley Hall, off Barnsley Hall Road, Bromsgrove, B61 0EX
-        if record.polling_place_id == "7948":
-            record = record._replace(
-                polling_place_easting="396005", polling_place_northing="272868"
-            )
+        # WARNING: Polling station Broadmeadow Junior School (8390) is in Birmingham City Council (BIR)
 
         # adding point for: Catshill Village Hall, Golden Cross Lane, Catshill, B61 0JZ
-        if record.polling_place_id == "8218":
+        if record.polling_place_id == "8401":
             record = record._replace(
                 polling_place_easting="396119", polling_place_northing="274035"
             )
 
-        # adding point for: Cofton Village Hall, Community Centre, 1 Village Hall Way, Cofton Hackett, B45 8PD
-        if record.polling_place_id == "8068":
-            record = record._replace(
-                polling_place_easting="400942", polling_place_northing="276167"
-            )
-
         # adding point for: Beacon Suite at The Old Rose and Crown Hotel, Rose Hill, Lickey, B45 8RT
-        if record.polling_place_id == "8217":
+        if record.polling_place_id == "8405":
             record = record._replace(
                 polling_place_easting="399517", polling_place_northing="275838"
             )
