9736fa0ac9086ada83d54f3db7b499b1b78bad90
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-06 15:02:08 +0100

    Import script for Coventry (2024-06-20) (closes #7599)

--- polling_stations/apps/data_importers/management/commands/import_coventry.py
+++ polling_stations/apps/data_importers/management/commands/import_coventry.py
@@ -4,67 +4,19 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "COV"
     addresses_name = (
-        "2024-05-02/2024-03-20T10:51:34.592908/Democracy_Club__02May2024.tsv"
+        "2024-06-20/2024-06-06T15:02:00.371617/Democracy_Club__20June2024.tsv"
     )
     stations_name = (
-        "2024-05-02/2024-03-20T10:51:34.592908/Democracy_Club__02May2024.tsv"
+        "2024-06-20/2024-06-06T15:02:00.371617/Democracy_Club__20June2024.tsv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-06-20"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "10094852156",  # 1 MATHS HOUSE HALLS OF RESIDENCE GIBBET HILL ROAD, COVENTRY
-            "10094852157",  # 2 MATHS HOUSE HALLS OF RESIDENCE GIBBET HILL ROAD, COVENTRY
-            "10094852158",  # 3 MATHS HOUSE HALLS OF RESIDENCE GIBBET HILL ROAD, COVENTRY
-            "10094852159",  # 4 MATHS HOUSE HALLS OF RESIDENCE GIBBET HILL ROAD, COVENTRY
-            "10094852160",  # 5 MATHS HOUSE HALLS OF RESIDENCE GIBBET HILL ROAD, COVENTRY
-            "10094852161",  # 6A MATHS HOUSE HALLS OF RESIDENCE GIBBET HILL ROAD, COVENTRY
-            "10094852162",  # 6B MATHS HOUSE HALLS OF RESIDENCE GIBBET HILL ROAD, COVENTRY
-            "100070642497",  # 112 DUNCROFT AVENUE, COVENTRY
-            "100071316506",  # CHURCH FARM, CHURCH LANE, EASTERN GREEN, COVENTRY
-            "100071316511",  # THE COTTAGE, CHURCH LANE, EASTERN GREEN, COVENTRY
-            "100070624346",  # 1 BRISCOE ROAD, COVENTRY
-            "100070624347",  # 2 BRISCOE ROAD, COVENTRY
-            "100070659202",  # 1 HEN LANE, COVENTRY
-            "10096164089",  # FLAT 4 WHEELWRIGHT LANE, COVENTRY
-            "100070617832",  # 503 BEAKE AVENUE, COVENTRY
-            "100070617834",  # 505 BEAKE AVENUE, COVENTRY
-            "100070642497",  # 112 DUNCROFT AVENUE, COVENTRY
-            "10024028309",  # FLAT ABOVE THE COCKED HAT HOTEL BRANDON ROAD, COVENTRY
-            "100070666519",  # 47 KENILWORTH ROAD, COVENTRY
-            "10024028287",  # STAFF BLOCK HILTON COVENTRY PARADISE WAY, COVENTRY
-            "10014008379",  # CROSS POINT BREWERS FAYRE, GIELGUD WAY, CROSS POINT BUSINESS PARK, COVENTRY
-            "100070663871",  # 138 HUMBER AVENUE, COVENTRY
-            "100070666517",  # 45 KENILWORTH ROAD, COVENTRY
-            "100071321235",  # COUNDON HALL FARM, TAMWORTH ROAD, KERESLEY END, COVENTRY
-            "100070707551",  # 180 SWAN LANE, COVENTRY
-            "100070707550",  # 178 SWAN LANE, COVENTRY
-            "100070707549",  # 176 SWAN LANE, COVENTRY
-            "100071367506",  # 239 WALSGRAVE ROAD, COVENTRY
-            "100070679552",  # 2 MILTON STREET, COVENTRY
-            "100070705753",  # 454 STONEY STANTON ROAD, COVENTRY
-            "100071517299",  # 298 FOLESHILL ROAD, COVENTRY
-            "100071367655",  # A P S ACCOUNTANT (UK) LTD, 204 WINSFORD AVENUE, COVENTRY
-            "100071319758",  # SPRINGFIELD FARM, PENNY PARK LANE, COVENTRY
-            "100071319757",  # SPRINGFIELD COTTAGE, PENNY PARK LANE, COVENTRY
-            "100071504282",  # THE NEW TIGER MOTH, QUORN WAY, BINLEY, COVENTRY
-            "10024029583",  # FLAT ABOVE THE TIGER MOTH PUBLIC HOUSE QUORN WAY, COVENTRY
-        ]:
-            return None
-
         if record.addressline6 in [
-            # split
-            "CV6 5NU",
-            "CV6 2PY",
-            "CV4 8GB",
-            "CV4 9LP",
-            "CV2 1FX",
-            "CV4 9JB",
-            "CV3 5DU",
-            "CV2 5FN",
+            # suspect
+            "CV6 3FU",
+            "CV6 3FT",
         ]:
             return None
 
