cb41a46dc4eb0ae0964d7161c4586aec5f23f637
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-06 10:15:31 +0100

    Import script for Sunderland (2024-07-04) (closes #7556)

--- polling_stations/apps/data_importers/management/commands/import_sunderland.py
+++ polling_stations/apps/data_importers/management/commands/import_sunderland.py
@@ -4,43 +4,42 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SND"
     addresses_name = (
-        "2024-05-02/2024-03-22T10:01:04.870722/Democracy_Club__02May2024.CSV"
+        "2024-07-04/2024-06-06T10:15:05.242461/Democracy_Club__04July2024.CSV"
     )
     stations_name = (
-        "2024-05-02/2024-03-22T10:01:04.870722/Democracy_Club__02May2024.CSV"
+        "2024-07-04/2024-06-06T10:15:05.242461/Democracy_Club__04July2024.CSV"
     )
-    elections = ["2024-05-02"]
-
-    def station_record_to_dict(self, record):
-        # Plains Farm Youth and Community Centre, Grounds of Plains Farm Academy, Tudor Grove, Sunderland
-        if record.polling_place_id == "18488":
-            record = record._replace(
-                polling_place_easting="438000",
-            )
-        return super().station_record_to_dict(record)
+    elections = ["2024-07-04"]
 
     def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "45002135",  # SUNHILL, NORTH ROAD, HETTON-LE-HOLE, HOUGHTON LE SPRING
+        ]:
+            return None
+
         if record.addressline6 in [
             # split
-            "DH4 5HY",
-            "SR2 0AQ",
-            "SR2 7HZ",
-            "SR2 0LE",
             "DH4 7RD",
+            "DH4 5HY",
+            "SR2 0LE",
             "SR4 8JF",
-            "SR4 6NP",
-            "SR6 9DY",
             "SR4 7SD",
-            "SR4 0BT",
-            "SR3 1XF",
-            "SR2 9JG",
             "SR5 3EP",
+            "SR2 9JG",
+            "SR2 0AQ",
             "DH4 4JH",
-            "SR6 0NB",
             "SR2 8RA",
-            "SR4 8HA",
+            "SR6 9DY",
+            "SR4 0BT",
+            "SR2 7HZ",
             "SR4 8JU",
+            "SR4 6NP",
+            "SR3 1XF",
             "SR3 3QH",
+            "SR6 0NB",
+            "SR4 8HA",
         ]:
             return None
 
