1757313f4b472ac8fd59b1a9e18b28f94e756d96
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-06 13:28:25 +0100

    Import script for Moray (2024-07-04) (closes #7589)

--- polling_stations/apps/data_importers/management/commands/import_moray.py
+++ polling_stations/apps/data_importers/management/commands/import_moray.py
@@ -1,31 +1,44 @@
-from data_importers.github_importer import BaseGitHubImporter
+from addressbase.models import UprnToCouncil
+from data_importers.management.commands import BaseHalaroseCsvImporter
 
 
-class Command(BaseGitHubImporter):
-    srid = 4326
+class Command(BaseHalaroseCsvImporter):
     council_id = "MRY"
-    elections = ["2022-05-05"]
-    scraper_name = "wdiv-scrapers/DC-PollingStations-Moray"
-    geom_type = "geojson"
+    addresses_name = "2024-07-04/2024-06-17T14:56:43.378925/MRY_combined.csv"
+    stations_name = "2024-07-04/2024-06-17T14:56:43.378925/MRY_combined.csv"
+    elections = ["2024-07-04"]
 
-    def district_record_to_dict(self, record):
-        poly = self.extract_geometry(record, self.geom_type, self.get_srid("districts"))
+    def pre_import(self):
+        # We need to consider rows that don't have a uprn when importing data.
+        # However there are lots of rows for other councils in this file.
+        # So build a list of stations from rows that do have UPRNS
+        # and then use that list of stations to make sure we check relevant rows, even if they don't have a UPRN
 
-        return {
-            "internal_council_id": record["PD_CODE"],
-            "name": record["PD_NAME"],
-            "area": poly,
-            "polling_station_id": record["PD_CODE"],
-        }
+        council_uprns = set(
+            UprnToCouncil.objects.filter(lad=self.council.geography.gss).values_list(
+                "uprn", flat=True
+            )
+        )
+        self.COUNCIL_STATIONS = set()
+        data = self.get_addresses()
+
+        for record in data:
+            if record.uprn in council_uprns:
+                self.COUNCIL_STATIONS.add(self.get_station_hash(record))
 
     def station_record_to_dict(self, record):
-        location = self.extract_geometry(
-            record, self.geom_type, self.get_srid("stations")
-        )
+        if self.get_station_hash(record) not in self.COUNCIL_STATIONS:
+            return None
 
-        return {
-            "internal_council_id": record["PD_CODE"],
-            "address": record["Addr"],
-            "postcode": "",
-            "location": location,
-        }
+        return super().station_record_to_dict(record)
+
+    def address_record_to_dict(self, record):
+        if self.get_station_hash(record) not in self.COUNCIL_STATIONS:
+            return None
+
+        if record.housepostcode in [
+            "IV30 8AB",  # split
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
