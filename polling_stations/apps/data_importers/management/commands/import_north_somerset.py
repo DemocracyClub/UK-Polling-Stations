4044c658b5493e1222214b6bbd7b02558b2eab09
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-06 09:26:06 +0100

    Import script for North Somerset (2024-07-04) (closes #7545)

--- polling_stations/apps/data_importers/management/commands/import_north_somerset.py
+++ polling_stations/apps/data_importers/management/commands/import_north_somerset.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "NSM"
     addresses_name = (
-        "2024-05-02/2024-03-21T10:16:21.138046/Democracy_Club__02May2024.CSV"
+        "2024-07-04/2024-06-06T09:25:50.552110/Democracy_Club__04July2024.CSV"
     )
     stations_name = (
-        "2024-05-02/2024-03-21T10:16:21.138046/Democracy_Club__02May2024.CSV"
+        "2024-07-04/2024-06-06T09:25:50.552110/Democracy_Club__04July2024.CSV"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
@@ -21,8 +21,6 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "24014702",  # 30A BYRON ROAD, LOCKING, WESTON-SUPER-MARE
             "24141074",  # 11A BYRON ROAD, WESTON-SUPER-MARE
             "24013232",  # 6 PURN LANE, WESTON-SUPER-MARE
-            "24148079",  # TWIN ELM FARM HOUSE, STOCK LANE, CONGRESBURY, BRISTOL
-            "24058834",  # BAY TREE COTTAGE, LANGFORD ROAD, LANGFORD, BRISTOL
             "24076278",  # ASHTON HILL HOUSE, WESTON ROAD, FAILAND, BRISTOL
         ]:
             return None
@@ -32,31 +30,10 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "BS24 8GE",
             "BS20 0LJ",
             "BS22 6EA",
-            "BS40 7AP",
             # suspect
-            "BS49 4FJ",  # CLARENCE GROVE, CLAVERHAM, BRISTOL
             "BS23 1WN",
-            "BS23 1FHJ",
+            "BS23 1FH",
         ]:
             return None
 
         return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # St James Church Hall, 52 Woodborough Road, Winscombe, BS25 1BA
-        # easting/northing is terribly wrong
-        if record.polling_place_id == "18143":
-            record = record._replace(
-                polling_place_easting="", polling_place_northing=""
-            )
-
-        # Wick St. Lawrence Village Hall, Wick St. Lawrence, BS22 7YP
-        # Wick St Lawrence Village Hall, Wick St Lawrence, BS22 7YP
-        # Those two stations addresses are almost identical (St vs St.),
-        # but for some reason easting/northing is different
-        if record.polling_place_id in ["18138", "18136"]:
-            record = record._replace(
-                polling_place_easting="", polling_place_northing=""
-            )
-
-        return super().station_record_to_dict(record)
