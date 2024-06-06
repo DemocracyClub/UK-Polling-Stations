a488095fbf82711ad7b360543b2565eceec1f2ad
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-06 13:19:16 +0100

    Import script for Central Bedfordshire (2024-07-04) (closes #7587)

--- polling_stations/apps/data_importers/management/commands/import_central_bedfordshire.py
+++ polling_stations/apps/data_importers/management/commands/import_central_bedfordshire.py
@@ -3,14 +3,9 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "CBF"
-    addresses_name = (
-        "2024-05-02/2024-03-05T14:33:07.893624/Democracy Club - Polling Districts.csv"
-    )
-    stations_name = (
-        "2024-05-02/2024-03-05T14:33:07.893624/Democracy Club - Polling Stations.csv"
-    )
-    elections = ["2024-05-02"]
-    csv_encoding = "utf-16le"
+    addresses_name = "2024-07-04/2024-06-10T12:59:59.601292/districts-combined.csv"
+    stations_name = "2024-07-04/2024-06-10T12:59:59.601292/stations-combined.csv"
+    elections = ["2024-07-04"]
 
     def address_record_to_dict(self, record):
         if record.postcode in [
@@ -19,3 +14,22 @@ class Command(BaseDemocracyCountsCsvImporter):
             return None
 
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        if record.stationcode in (
+            # Stations with no addresses assigned
+            "BX122M-BKMC",  # Elstow Playing Field Association Hall  (BDF)
+            "BX123MD",  # Elstow Playing Field Association Hall  (BDF)
+            "BX124MF-1",  # Lakeview Village Hall  (BDF)
+            "BX125MF-2",  # Lakeview Village Hall  (BDF)
+            "BX126ME/1",  # Stewartby Village Hall  (BDF)
+            "BX127ME/2",  # Stewartby Village Hall  (BDF)
+            "BX128MG",  # Wilstead Village Hall  (BDF)
+            "BX129MH",  # Wilstead Village Hall  (BDF)
+            "BX130MK-A",  # Wootton Community Centre  (BDF)
+            "BX131MK-B",  # Wootton Community Centre  (BDF)
+            "BX132ML",  # Wootton Memorial Hall  (BDF)
+            "BX133MJ",  # Wootton Village Hall  (BDF)
+        ):
+            return None
+        return super().station_record_to_dict(record)
