e0e7a67ed0e11fc9c862a6a1dd8e88fd3c74b00e
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-07 20:22:25 +0100

    Import script for Sutton (2024-07-04) (closes #7622)

--- polling_stations/apps/data_importers/management/commands/import_sutton.py
+++ polling_stations/apps/data_importers/management/commands/import_sutton.py
@@ -5,16 +5,16 @@ from django.contrib.gis.geos import Point
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "STN"
     addresses_name = (
-        "2024-05-02/2024-03-07T17:52:52.063099/Democracy Club Polling Districts.csv"
+        "2024-07-04/2024-06-07T20:21:55.085664/Democracy Club Polling Districts.csv"
     )
     stations_name = (
-        "2024-05-02/2024-03-07T17:52:52.063099/Democracy Club Polling Stations.csv"
+        "2024-07-04/2024-06-07T20:21:55.085664/Democracy Club Polling Stations.csv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
     csv_encoding = "utf-16le"
 
     def station_record_to_dict(self, record):
-        # Holy Trinity Church Centre
+        # more accurate point for: Holy Trinity Church Centre, Maldon Road, Wallington, SM6 8BL
         if record.stationcode in ["RB/1", "RB/2"]:
             rec = super().station_record_to_dict(record)
             rec["location"] = Point(-0.151435, 51.364474, srid=4326)
