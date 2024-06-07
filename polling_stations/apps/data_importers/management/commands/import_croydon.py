dfe3e86ee5d7557d3208dc09963e45db886226a7
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-07 09:53:35 +0100

    Import script for Croydon (2024-07-04) (closes #7604)

--- polling_stations/apps/data_importers/management/commands/import_croydon.py
+++ polling_stations/apps/data_importers/management/commands/import_croydon.py
@@ -4,17 +4,17 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "CRY"
     addresses_name = (
-        "2024-05-02/2024-03-18T13:08:49.912425/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-06-07T09:53:19.848506/Democracy_Club__04July2024.tsv"
     )
     stations_name = (
-        "2024-05-02/2024-03-18T13:08:49.912425/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-06-07T09:53:19.848506/Democracy_Club__04July2024.tsv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
 
     # Warning below checked and no correction needed:
-    # WARNING: Polling station Chipstead Valley Primary School (16272) is in Reigate and Banstead Borough Council (REI)
+    # WARNING: Polling station Chipstead Valley Primary School (17097) is in Reigate and Banstead Borough Council (REI)
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
@@ -46,9 +46,10 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
         if record.addressline6 in [
             # splits
-            "CR0 2JB",
             "CR2 0JB",
             "CR0 4BF",
+            "CR0 2JB",
+            "SE25 4BA",
             # looks wrong
             "SE19 3FB",
             "CR2 8JT",
