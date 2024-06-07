5f7d74608e8b7432175f2a36b162692b401ed999
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-07 16:12:26 +0100

    Import script for Monmouthshire (2024-07-04) (closes #7616)

--- polling_stations/apps/data_importers/management/commands/import_monmouthshire.py
+++ polling_stations/apps/data_importers/management/commands/import_monmouthshire.py
@@ -1,37 +1,28 @@
-from data_importers.management.commands import (
-    BaseXpressDCCsvInconsistentPostcodesImporter,
-)
+from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
-class Command(BaseXpressDCCsvInconsistentPostcodesImporter):
+class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "MON"
     addresses_name = (
-        "2024-05-02/2024-03-18T11:47:38.967548/Democracy_Club__02May2024 - Updated.tsv"
+        "2024-07-04/2024-06-07T16:12:18.330990/Democracy_Club__04July2024.tsv"
     )
     stations_name = (
-        "2024-05-02/2024-03-18T11:47:38.967548/Democracy_Club__02May2024 - Updated.tsv"
+        "2024-07-04/2024-06-07T16:12:18.330990/Democracy_Club__04July2024.tsv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
     csv_delimiter = "\t"
-
-    station_postcode_search_fields = [
-        "polling_place_postcode",
-        "polling_place_address_4",
-        "polling_place_address_3",
-        "polling_place_address_2",
-    ]
 
     def station_record_to_dict(self, record):
         # postcode correction for: Leisure Centre, Crossway Green, Chepstow, Monmouthshire, NP6 5LX
-        if record.polling_place_id == "11172":
+        if record.polling_place_id == "11598":
             record = record._replace(polling_place_postcode="NP16 5LX")
 
         # postcode correction for: Village Hall, Whitebrook, Monmouth, NP27 4TT
-        if record.polling_place_id == "11458":
+        if record.polling_place_id == "11883":
             record = record._replace(polling_place_postcode="NP25 4TT")
 
         # postcode correction for: Rectory Hall, Llanfair Kilgeddin, NP4 9BB
-        if record.polling_place_id == "11333":
+        if record.polling_place_id == "11758":
             record = record._replace(polling_place_postcode="NP7 9BD")
 
         return super().station_record_to_dict(record)
@@ -40,37 +31,38 @@ class Command(BaseXpressDCCsvInconsistentPostcodesImporter):
         uprn = record.polling_place_uprn.strip().lstrip("0")
 
         if uprn in [
-            "10033357396",  # COED POETH COTTAGE, TREGARE, MONMOUTH
+            "10033356635",  # FELLBANK, VEDDW, DEVAUDEN, CHEPSTOW
+            "10033347903",  # KAIDEX, PERCUS BARN, DEVAUDEN, CHEPSTOW
         ]:
             return None
 
         if record.addressline6.strip() in [
             # splits
-            "NP7 5LL",
-            "NP25 5BG",
-            "NP7 7DH",
-            "NP26 4PQ",
-            "NP25 5DJ",
-            "NP7 8RP",
             "NP16 5BE",
             "NP7 5PW",
-            "NP7 9EU",
-            "NP26 5SD",
-            "NP25 3LP",
+            "NP25 5BG",
+            "NP26 4PQ",
+            "NP7 7DH",
             "NP7 5LF",
-            "NP26 4NB",
+            "NP7 5LL",
+            "NP7 9EU",
+            "NP25 5DJ",
+            "NP7 7DW",
+            "NP25 3BQ",
+            "NP7 8RP",
+            "NP25 4AE",
+            "NP26 3AR",
+            "NP25 4TS",
+            "NP16 6NF",
+            "NP26 4HL",
+            "NP26 5SD",
+            "NP16 6HX",
+            "NP25 3LP",
+            "NP7 7EY",
             "NP7 9HS",
             "NP7 7EE",
-            "NP26 3AR",
-            "NP25 3BQ",
-            "NP26 4HL",
-            "NP16 6NF",
-            "NP7 7EY",
-            "NP16 6HX",
+            "NP26 4NB",
             "NP7 9EG",
-            "NP7 7DW",
-            "NP25 4AE",
-            "NP25 4TS",
         ]:
             return None
 
