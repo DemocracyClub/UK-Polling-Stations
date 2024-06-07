968aca2bac27850742f8e5ac6ed0bf6232cf5cca
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-07 11:53:29 +0100

    Import script for North Norfolk (2024-07-04) (closes #7606)

--- polling_stations/apps/data_importers/management/commands/import_north_norfolk.py
+++ polling_stations/apps/data_importers/management/commands/import_north_norfolk.py
@@ -5,13 +5,12 @@ from django.contrib.gis.geos import Point
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "NNO"
     addresses_name = (
-        "2024-05-02/2024-04-09T17:03:40.944266/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-06-07T11:53:17.956702/Democracy_Club__04July2024.CSV"
     )
     stations_name = (
-        "2024-05-02/2024-04-09T17:03:40.944266/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-06-07T11:53:17.956702/Democracy_Club__04July2024.CSV"
     )
-    elections = ["2024-05-02"]
-    csv_delimiter = "\t"
+    elections = ["2024-07-04"]
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
@@ -44,21 +43,17 @@ class Command(BaseXpressDemocracyClubCsvImporter):
     def station_record_to_dict(self, record):
         # Walcott Village Hall, Coast Road, Walcott, NR12 ONG
         # Misspell postcode, capital letter "O" should be a number "0"
-        if record.polling_place_id == "18951":
+        if record.polling_place_id == "21665":
             record = record._replace(polling_place_postcode="NR12 0NG")
 
         # St Benet Hall, St Nicholas Church Halls Complex, Vicarage Street, North Walsham, NR28 9BQ
-        if record.polling_place_id == "19306":
+        if record.polling_place_id == "21925":
             record = record._replace(polling_place_postcode="NR28 9BT")
-
-        # Ashmanhaugh Preston Rooms, Neatishead Road, Ashmanhaugh, Wroxham NR12 8LB
-        if record.polling_place_id == "18819":
-            record = record._replace(polling_place_postcode="NR12 8YN")
 
         rec = super().station_record_to_dict(record)
 
         # Walsingham Parish Hall, 14 High Street, Walsingham, NR22 6AA
-        if rec["internal_council_id"] == "20352":
+        if rec["internal_council_id"] == "22086":
             rec["location"] = Point(0.873339, 52.892674, srid=4326)
 
         return rec
