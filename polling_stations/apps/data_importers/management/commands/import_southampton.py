2cb51de0252726ce7d15c28b8a032cfb8b50e83f
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-09 17:32:06 +0100

    Import script for Southampton (2024-07-04) (closes #7630)

--- polling_stations/apps/data_importers/management/commands/import_southampton.py
+++ polling_stations/apps/data_importers/management/commands/import_southampton.py
@@ -4,20 +4,18 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "STH"
     addresses_name = (
-        "2024-05-02/2024-04-09T19:58:39.347148/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-06-09T17:28:00.538366/Democracy_Club__04July2024.tsv"
     )
     stations_name = (
-        "2024-05-02/2024-04-09T19:58:39.347148/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-06-09T17:28:00.538366/Democracy_Club__04July2024.tsv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         if record.addressline6 in [
             # split
-            "SO16 7GQ",
             "SO15 2NS",
-            "SO16 7AS",
         ]:
             return None
 
@@ -26,13 +24,13 @@ class Command(BaseXpressDemocracyClubCsvImporter):
     def station_record_to_dict(self, record):
         # The Ascension Centre (Grace Hall), 1 Thorold Road, Southampton, SO18 1HZ
         # The Ascension Centre (Vite Room, 1 Thorold Road, Southampton, SO18 1HZ
-        if record.polling_place_id in ["14759", "14756"]:
+        if record.polling_place_id in ["15482", "15486"]:
             record = record._replace(
                 polling_place_easting="444086", polling_place_northing="113955"
             )
 
         # Moorlands Community Centre (Main Hall), Townhill Way, Southampton, SO18 2ER
-        if record.polling_place_id == "14808":
+        if record.polling_place_id == "15495":
             record = record._replace(
                 polling_place_easting="445540", polling_place_northing="114282"
             )
