dd5cbaa8365c0871bd66dab62a1281b40125ccb2
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-10 14:51:58 +0100

    Import script for Barnet (2024-07-04) (closes #7662)

--- polling_stations/apps/data_importers/management/commands/import_barnet.py
+++ polling_stations/apps/data_importers/management/commands/import_barnet.py
@@ -3,11 +3,9 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "BNE"
-    addresses_name = "2024-05-02/2024-03-26T15:58:39.559553/Democracy Club data.csv"
-    stations_name = (
-        "2024-05-02/2024-03-26T15:58:39.559553/Democracy Club Polling Station data.csv"
-    )
-    elections = ["2024-05-02"]
+    addresses_name = "2024-07-04/2024-06-17T11:08:27.771011/BNE_districts_combined.csv"
+    stations_name = "2024-07-04/2024-06-17T11:08:27.771011/BNE_stations_combined.csv"
+    elections = ["2024-07-04"]
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
@@ -15,10 +13,13 @@ class Command(BaseDemocracyCountsCsvImporter):
         if uprn in [
             "10093301425",  # FLAT 1, 15 COLLISON AVENUE, BARNET
             "200001537",  # 1 ABERCORN CLOSE, LONDON
+            "10095744883",  # 5 EGRET SQUARE, LONDON
         ]:
             return None
         if record.postcode in [
-            "EN5 3DP",  # split
+            # split
+            "EN5 3DP",
+            "N12 7LT",
         ]:
             return None
         return super().address_record_to_dict(record)
