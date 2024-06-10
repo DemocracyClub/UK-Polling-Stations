e1541122416e9c978132773a357b403f479b21cc
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-10 15:52:00 +0100

    Import script for Tamworth (2024-07-04) (closes #7674)

--- polling_stations/apps/data_importers/management/commands/import_tamworth.py
+++ polling_stations/apps/data_importers/management/commands/import_tamworth.py
@@ -3,7 +3,11 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "TAW"
-    addresses_name = "2024-05-02/2024-03-20T13:40:40.110534/Tamworth_Borough_Council_Democracy_Club__02May2024 v2.tsv"
-    stations_name = "2024-05-02/2024-03-20T13:40:40.110534/Tamworth_Borough_Council_Democracy_Club__02May2024 v2.tsv"
-    elections = ["2024-05-02"]
+    addresses_name = (
+        "2024-07-04/2024-06-10T15:51:50.159605/Tamworth_Democracy_Club__04July2024.tsv"
+    )
+    stations_name = (
+        "2024-07-04/2024-06-10T15:51:50.159605/Tamworth_Democracy_Club__04July2024.tsv"
+    )
+    elections = ["2024-07-04"]
     csv_delimiter = "\t"
