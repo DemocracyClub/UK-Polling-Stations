5abcbfebb2b6bb507b222a06706f20f14b02525c
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-10 15:15:09 +0100

    Import script for Telford and Wrekin (2024-07-04) (closes #7668)

--- polling_stations/apps/data_importers/management/commands/import_telford.py
+++ polling_stations/apps/data_importers/management/commands/import_telford.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "TFW"
     addresses_name = (
-        "2024-05-02/2024-03-01T11:51:31.642592/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-06-10T15:14:52.275515/Democracy_Club__04July2024 (13).tsv"
     )
     stations_name = (
-        "2024-05-02/2024-03-01T11:51:31.642592/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-06-10T15:14:52.275515/Democracy_Club__04July2024 (13).tsv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -27,19 +27,18 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "10090446309",  # LAKE VIEW RESIDENTIAL CARE HOME, BROOKSIDE AVENUE, TELFORD
             "452005400",  # 2 POOL HILL, DAWLEY, TELFORD
             "452005403",  # 5 POOL HILL, DAWLEY, TELFORD
-            "452043117",  # INGLESIDE, EDGMOND ROAD, NEWPORT
         ]:
             return None
 
         if record.addressline6 in [
             # splits
-            "TF10 7ZN",
-            "TF10 7RG",
-            "TF4 3JZ",
-            "TF2 8SF",
-            "TF4 3AZ",
-            "TF2 9HF",
             "TF8 7NN",
+            "TF2 8SF",
+            "TF10 7ZN",
+            "TF4 3AZ",
+            "TF4 3JZ",
+            "TF2 9HF",
+            "TF10 7RG",
             # looks wrong
             "TF3 4LY",
         ]:
@@ -49,12 +48,12 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
     def station_record_to_dict(self, record):
         # more accurate point for: Donnington Community Hub, St Matthews Road, Donnington Wood, Telford, Shropshire, TF2 7RB
-        if record.polling_place_id == "8782":
+        if record.polling_place_id == "9169":
             record = record._replace(polling_place_easting="370966")
             record = record._replace(polling_place_northing="312997")
 
         # more accurate point for: Horsehay Village Golf Club, Wellington Road, Horsehay, Telford, TF4 3BT
-        if record.polling_place_id == "8933":
+        if record.polling_place_id == "9173":
             record = record._replace(polling_place_easting="366836")
             record = record._replace(polling_place_northing="307950")
 
