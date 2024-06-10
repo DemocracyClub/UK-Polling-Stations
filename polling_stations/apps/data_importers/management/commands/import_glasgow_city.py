02a0f75ac6a0be7d6304e145a8b7dd8bed8f67bd
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-10 12:22:35 +0100

    Import script for Glasgow (2024-07-04) (closes #7651)

--- polling_stations/apps/data_importers/management/commands/import_glasgow_city.py
+++ polling_stations/apps/data_importers/management/commands/import_glasgow_city.py
@@ -3,20 +3,31 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "GLG"
-    addresses_name = "2022-05-05/2022-03-30T10:30:41.528184/Glasgow_polling_station_export-2022-03-28.csv"
-    stations_name = "2022-05-05/2022-03-30T10:30:41.528184/Glasgow_polling_station_export-2022-03-28.csv"
-    elections = ["2022-05-05"]
+    addresses_name = "2024-07-04/2024-06-10T12:22:22.251389/Eros_SQL_Output003.csv"
+    stations_name = "2024-07-04/2024-06-10T12:22:22.251389/Eros_SQL_Output003.csv"
+    elections = ["2024-07-04"]
 
     def address_record_to_dict(self, record):
+        uprn = record.uprn.strip().lstrip("0")
+
+        if uprn in [
+            "906700541051",  # 25 HAWICK COURT, GLASGOW
+            "906700541052",  # 27 HAWICK COURT, GLASGOW
+            "906700531273",  # UNIT 508B 47 KYLE STREET, GLASGOW
+            "906700531270",  # UNIT 507H 47 KYLE STREET, GLASGOW
+            "906700496057",  # FLAT 0/1 18 GREAT GEORGE STREET, GLASGOW
+        ]:
+            return None
+
         if record.housepostcode in [
-            "G14 9HQ",
-            "G21 2LN",
-            "G21 4UH",
-            "G22 6RL",
-            "G33 3SU",
-            "G52 1RZ",
+            # splits
             "G53 6UW",
+            "G22 6RL",
+            "G21 2LN",
             "G61 1QE",
+            "G33 3SU",
+            "G14 9HQ",
+            "G52 1RZ",
         ]:
             return None
 
