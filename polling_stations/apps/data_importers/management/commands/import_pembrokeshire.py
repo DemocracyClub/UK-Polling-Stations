e780d00a6eb2daf62fbdd47a6d31b6d320f601f7
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-10 14:14:25 +0100

    Import script for Pembrokeshire (2024-07-04) (closes #7657)

--- polling_stations/apps/data_importers/management/commands/import_pembrokeshire.py
+++ polling_stations/apps/data_importers/management/commands/import_pembrokeshire.py
@@ -3,9 +3,9 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "PEM"
-    addresses_name = "2024-05-02/2024-03-25T14:51:07.747895/Eros_SQL_Output001.csv"
-    stations_name = "2024-05-02/2024-03-25T14:51:07.747895/Eros_SQL_Output001.csv"
-    elections = ["2024-05-02"]
+    addresses_name = "2024-07-04/2024-06-17T09:55:22.022484/PEM_combined.csv"
+    stations_name = "2024-07-04/2024-06-17T09:55:22.022484/PEM_combined.csv"
+    elections = ["2024-07-04"]
 
     def address_record_to_dict(self, record):
         record.uprn.strip().lstrip("0")
@@ -16,26 +16,29 @@ class Command(BaseHalaroseCsvImporter):
         if record.housepostcode in [
             # split
             "SA73 2EB",
+            "SA73 2EB",
             "SA62 4NJ",
-            "SA73 3RA",
-            "SA68 0XN",
-            "SA73 1NR",
             "SA62 3PW",
-            "SA71 5BP",
-            "SA73 1BB",
-            "SA68 0QR",
             "SA73 1RG",
-            "SA73 3HF",
-            "SA62 5RJ",
-            "SA72 6BH",
             "SA72 6BE",
-            "SA62 5UD",
-            "SA67 8RY",
+            "SA72 6BH",
+            "SA73 1NR",
+            "SA73 1BB",
             "SA62 5DB",
+            "SA66 7QW",
+            "SA62 3DA",
+            "SA73 3HF",
+            "SA73 3RA",
+            "SA68 0QR",
+            "SA67 8RY",
+            "SA71 5BP",
+            "SA62 5RJ",
+            "SA71 4JT",
+            "SA68 0XN",
+            "SA62 5UD",
+            "SA73 1RJ",
             "SA62 6TD",
             "SA62 5NL",
-            "SA73 1RJ",
-            "SA66 7QW",
         ]:
             return None
         return super().address_record_to_dict(record)
