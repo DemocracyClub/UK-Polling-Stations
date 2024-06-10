10e90b1b680e1be388dfff4cba87bb94038b3389
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-10 14:49:13 +0100

    Import script for Cannock Chase (2024-07-04) (closes #7660)

--- polling_stations/apps/data_importers/management/commands/import_cannock_chase.py
+++ polling_stations/apps/data_importers/management/commands/import_cannock_chase.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "CAN"
     addresses_name = (
-        "2024-05-02/2024-04-02T10:28:11.535414/Democracy_Club__02May2024-edited.csv"
+        "2024-07-04/2024-06-10T14:49:01.631956/Democracy_Club__04July2024.tsv"
     )
     stations_name = (
-        "2024-05-02/2024-04-02T10:28:11.535414/Democracy_Club__02May2024-edited.csv"
+        "2024-07-04/2024-06-10T14:49:01.631956/Democracy_Club__04July2024.tsv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
