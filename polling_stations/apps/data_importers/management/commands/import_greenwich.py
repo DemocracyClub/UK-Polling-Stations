22ae72b376373e2740d26b336851cfebd99a0166
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-10 12:05:28 +0100

    Import script for Greenwich (2024-07-04) (closes #7649)

--- polling_stations/apps/data_importers/management/commands/import_greenwich.py
+++ polling_stations/apps/data_importers/management/commands/import_greenwich.py
@@ -4,11 +4,28 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "GRE"
     addresses_name = (
-        "2024-06-13/2024-05-21T09:54:03.570856/Democracy_Club__13June2024.tsv"
+        "2024-07-04/2024-06-10T12:05:14.810020/Democracy_Club__04July2024.tsv"
     )
     stations_name = (
-        "2024-06-13/2024-05-21T09:54:03.570856/Democracy_Club__13June2024.tsv"
+        "2024-07-04/2024-06-10T12:05:14.810020/Democracy_Club__04July2024.tsv"
     )
-    elections = ["2024-06-13"]
+    elections = ["2024-07-04"]
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
+
+    def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "100020975456",  # 56A HILLREACH, LONDON
+        ]:
+            return None
+
+        if record.addressline6 in [
+            # splits
+            "SE9 2BU",
+            "SE3 9BT",
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
