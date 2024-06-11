261349169d5e7303311eae6bed2548c96ce02e5f
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-11 14:51:24 +0100

    Import script for South Oxfordshire (2024-07-04) (closes #7707)

--- polling_stations/apps/data_importers/management/commands/import_south_oxfordshire.py
+++ polling_stations/apps/data_importers/management/commands/import_south_oxfordshire.py
@@ -1,57 +1,10 @@
-from addressbase.models import UprnToCouncil
 from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SOX"
-    addresses_name = "2024-07-04/2024-06-10T17:51:15.466072/SOXandVAL_combined.tsv"
-    stations_name = "2024-07-04/2024-06-10T17:51:15.466072/SOXandVAL_combined.tsv"
+    addresses_name = "2024-07-04/2024-06-11T14:51:16.708318/Democracy_Club__04July2024 - Witney (1).tsv"
+    stations_name = "2024-07-04/2024-06-11T14:51:16.708318/Democracy_Club__04July2024 - Witney (1).tsv"
     elections = ["2024-07-04"]
+    csv_encoding = "windows-1252"
     csv_delimiter = "\t"
-
-    def pre_import(self):
-        # We need to consider rows that don't have a uprn when importing data.
-        # However there are lots of rows for other councils in this file.
-        # So build a list of stations from rows that do have UPRNS
-        # and then use that list of stations to make sure we check relevant rows, even if they don't have a UPRN
-
-        council_uprns = set(
-            UprnToCouncil.objects.filter(lad=self.council.geography.gss).values_list(
-                "uprn", flat=True
-            )
-        )
-        self.COUNCIL_STATIONS = set()
-        data = self.get_addresses()
-
-        for record in data:
-            if record.property_urn in council_uprns:
-                self.COUNCIL_STATIONS.add(record.polling_place_id)
-
-    def station_record_to_dict(self, record):
-        if record.polling_place_id not in self.COUNCIL_STATIONS:
-            return None
-
-        return super().station_record_to_dict(record)
-
-    def address_record_to_dict(self, record):
-        if record.polling_place_id not in self.COUNCIL_STATIONS:
-            return None
-
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "10033006434",  # GLADE HOUSE, PISHILL, HENLEY-ON-THAMES
-        ]:
-            return None
-
-        if record.addressline6 in [
-            # split
-            "RG8 0PY",
-            "OX11 7SE",
-            "OX11 7TP",
-            # suspect
-            "OX44 7NW",
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
