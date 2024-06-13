25e6f545c5be0878bb559cd0eda774650e8d14fd
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-13 12:53:58 +0100

    Import script for North East Lincolnshire (2024-07-04) (closes #7767)

--- polling_stations/apps/data_importers/management/commands/import_north_east_lincs.py
+++ polling_stations/apps/data_importers/management/commands/import_north_east_lincs.py
@@ -1,13 +1,11 @@
-from addressbase.models import Address
 from data_importers.management.commands import BaseHalaroseCsvImporter
-from django.core.exceptions import ObjectDoesNotExist
 
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "NEL"
-    addresses_name = "2024-05-02/2024-03-12T11:47:38.056186/Eros_SQL_Output004.csv"
-    stations_name = "2024-05-02/2024-03-12T11:47:38.056186/Eros_SQL_Output004.csv"
-    elections = ["2024-05-02"]
+    addresses_name = "2024-07-04/2024-06-13T12:53:46.360961/Eros_SQL_Output002.csv"
+    stations_name = "2024-07-04/2024-06-13T12:53:46.360961/Eros_SQL_Output002.csv"
+    elections = ["2024-07-04"]
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
@@ -25,13 +23,77 @@ class Command(BaseHalaroseCsvImporter):
         ]:
             return None
 
+        if record.housepostcode in [
+            "DN35 0RA",  # split
+        ]:
+            return None
         return super().address_record_to_dict(record)
 
-    # quick fix to show maps for Halarose records that have a valid UPRN in the PollingVenueUPRN field
-    def get_station_point(self, record):
-        uprn = record.pollingvenueuprn.strip().lstrip("0")
-        try:
-            ab_rec = Address.objects.get(uprn=uprn)
-            return ab_rec.location
-        except ObjectDoesNotExist:
-            return super().get_station_point(record)
+    def station_record_to_dict(self, record):
+        # The following stations postcodes didn't match their postcode in addressbase
+        # Because they're off by only one letter, I'm commmenting them out pending council response.
+
+        # # CLEETHORPES LIBRARY, ALEXANDRA ROAD, CLEETHORPES, DN35 8LG (id: 17)
+        # if record.pollingvenueid == "17":
+        #     record = record._replace(pollingstationpostcode="DN35 8LQ")
+
+        # # WENDOVER HALL, CHURCH LANE, HUMBERSTON, DN36 4HZ (id: 148)
+        # if record.pollingvenueid == "148":
+        #     record = record._replace(pollingstationpostcode="DN36 4HX")
+
+        # # ALL SAINTS CHURCH HALL, HIGH STREET, WALTHAM, DN37 0PN (id: 80)
+        # if record.pollingvenueid == "80":
+        #     record = record._replace(pollingstationpostcode="DN37 0PL")
+
+        # # HABROUGH VILLAGE HALL, STATION ROAD, HABROUGH, DN40 3BD (id: 48)
+        # if record.pollingvenueid == "48":
+        #     record = record._replace(pollingstationpostcode="DN40 3BQ")
+
+        # # STALLINGBOROUGH VILLAGE HALL, STATION ROAD, STALLINGBOROUGH, DN41 8AJ (id: 49)
+        # if record.pollingvenueid == "49":
+        #     record = record._replace(pollingstationpostcode="DN41 8AX")
+
+        # # T.A. CENTRE, WESTWARD HO, GRIMSBY, DN34 5AE (id: 57)
+        # if record.pollingvenueid == "57":
+        #     record = record._replace(pollingstationpostcode="DN34 5AT")
+
+        # # PORTACABIN, SCARTHO HALL, MATTHEW TELFORD PARK, GRIMSBY, DN33 2DU (id: 131)
+        # if record.pollingvenueid == "131":
+        #     record = record._replace(pollingstationpostcode="DN33 2DZ")
+
+        # # ST GILES PARISH ROOM, CHURCH LANE, GRIMSBY, DN33 2EY (id: 64)
+        # if record.pollingvenueid == "64":
+        #     record = record._replace(pollingstationpostcode="DN33 2EU")
+
+        # # ST GILES PARISH ROOM, CHURCH LANE, GRIMSBY, DN33 2EY (id: 64)
+        # if record.pollingvenueid == "64":
+        #     record = record._replace(pollingstationpostcode="DN33 2EU")
+
+        # # ST MARTIN'S CHURCH, SUTCLIFFE AVENUE, GRIMSBY, DN33 1AE (id: 68)
+        # if record.pollingvenueid == "68":
+        #     record = record._replace(pollingstationpostcode="DN33 2AD")
+
+        # # ALL SAINTS CHURCH HALL, HIGH STREET, WALTHAM, DN37 0PN (id: 80)
+        # if record.pollingvenueid == "80":
+        #     record = record._replace(pollingstationpostcode="DN37 0PL")
+
+        # # WEST MARSH FAMILY HUB, MACAULAY STREET, GRIMSBY, DN31 2ES (id: 85)
+        # if record.pollingvenueid == "85":
+        #     record = record._replace(pollingstationpostcode="DN31 2EP")
+
+        # # STANFORD CENTRE, (LACEBY LIBRARY), COOPER LANE, LACEBY, DN37 7AX (id: 94)
+        # if record.pollingvenueid == "94":
+        #     record = record._replace(pollingstationpostcode="DN37 7AY")
+
+        # # ST HELEN'S CHURCH, CHURCH LANE, BARNOLDBY LE BECK, DN37 0AZ (id: 87)
+        # if record.pollingvenueid == "87":
+        #     record = record._replace(pollingstationpostcode="DN37 0BA")
+
+        # # FARM OFFICE (FENWICK BROS), BEELSBY HOUSE FARM, BEELSBY, DN37 0TL (id: 88)
+        # if record.pollingvenueid == "88":
+        #     record = record._replace(pollingstationpostcode="DN37 0TN")
+
+        # # STANFORD CENTRE, (LACEBY LIBRARY), COOPER LANE, LACEBY, DN37 7AX (id: 94)
+        # if record.pollingvenueid == "94":
+        #     record = record._replace(pollingstationpostcode="DN37 7AY")
+        return super().station_record_to_dict(record)
