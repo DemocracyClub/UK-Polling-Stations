c95568b6674127200876e5783c99708d54320f4e
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-13 10:25:52 +0100

    Import script for Fife (2024-07-04) (closes #7756)

--- polling_stations/apps/data_importers/management/commands/import_fife.py
+++ polling_stations/apps/data_importers/management/commands/import_fife.py
@@ -3,30 +3,39 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "FIF"
-    addresses_name = (
-        "2022-05-05/2022-04-19T11:25:34.787070/Democracy Club - Polling Districts.csv"
-    )
-    stations_name = (
-        "2022-05-05/2022-04-19T11:25:34.787070/Democracy Club - Polling Stations.csv"
-    )
-    elections = ["2022-05-05"]
+    addresses_name = "2024-07-04/2024-06-20T14:09:30.467880/Polling Districts UTF-8.csv"
+    stations_name = "2024-07-04/2024-06-20T14:09:30.467880/Polling Stations.csv"
+    elections = ["2024-07-04"]
 
     def station_record_to_dict(self, record):
+        # ST ANDREWS-ERSKINE PARISH CHURCH HALL, ROBERTSON ROAD, DUNFERMLINE
+        if record.stationcode == "26":
+            record = record._replace(xordinate="", yordinate="")
+
+        # OAKLEY CENTRE, STATION ROAD, OAKLEY (two polling stations)
         if record.stationcode in [
-            "176",  # MARKINCH TOWN HALL
-            "177",  # MARKINCH TOWN HALL
-            "178",  # MARKINCH TOWN HALL
-            "229",  # BURNSIDE VILLAGE HALL
-            "230",  # BURNSIDE VILLAGE HALL
+            "12",
+            "11",
         ]:
+            record = record._replace(xordinate="", yordinate="")
+
+        # BRITISH LEGION, THE CROSS, KENNOWAY
+        if record.stationcode == "302":
             record = record._replace(xordinate="", yordinate="")
 
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
+        uprn = record.uprn.strip().lstrip("0")
+
+        if uprn in [
+            "320329489",  # 34 RITCHIE AVENUE, DUNFERMLINE
+            "320329487",  # 32 RITCHIE AVENUE, DUNFERMLINE
+        ]:
+            return None
+
         if record.postcode in [
-            "KY1 2EZ",
-            "KY5 9EY",
+            "KY12 8DT",  # split
         ]:
             return None
 
