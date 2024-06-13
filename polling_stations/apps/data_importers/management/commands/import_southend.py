2847b6a7b55df865b24302bb0a65c9466a6a5fa9
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-13 11:17:00 +0100

    Import script for Southend-on-Sea (2024-07-04) (closes #7762)

--- polling_stations/apps/data_importers/management/commands/import_southend.py
+++ polling_stations/apps/data_importers/management/commands/import_southend.py
@@ -4,30 +4,22 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SOS"
     addresses_name = (
-        "2024-05-02/2024-03-18T12:13:52.746718/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-06-13T11:16:47.619894/Democracy_Club__04July2024.tsv"
     )
     stations_name = (
-        "2024-05-02/2024-03-18T12:13:52.746718/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-06-13T11:16:47.619894/Democracy_Club__04July2024.tsv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
     csv_delimiter = "\t"
-
-    def station_record_to_dict(self, record):
-        # Booth 2, Earls Hall Primary School, (Colemans Avenue Entrance), Westcliff-on-Sea
-        if record.polling_place_id == "9382":
-            record = record._replace(
-                polling_place_easting="586595", polling_place_northing="187653"
-            )
-        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         if record.addressline6 in [
             # split
-            "SS9 1RP",
-            "SS9 1QY",
-            "SS9 1NH",
-            "SS9 1LN",
             "SS9 4RP",
+            "SS9 1QY",
+            "SS9 1LN",
+            "SS9 1RP",
+            "SS9 1NH",
             "SS3 9QH",
             "SS9 5EW",
         ]:
