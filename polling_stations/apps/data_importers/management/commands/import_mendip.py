cb2a39ea840417219807b2e9a27fffdfc8dfa47e
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-14 15:29:30 +0100

    Import script for Somerset (formerly Mendip) (2024-07-04) (closes #7799)

--- polling_stations/apps/data_importers/management/commands/import_mendip.py
+++ polling_stations/apps/data_importers/management/commands/import_mendip.py
@@ -1,15 +1,110 @@
-from addressbase.models import Address
 from data_importers.management.commands import BaseHalaroseCsvImporter
-from django.core.exceptions import ObjectDoesNotExist
 
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "MEN"
-    addresses_name = "2024-05-02/2024-02-27T10:25:12.441978/Eros_SQL_Output001 (2).csv"
-    stations_name = "2024-05-02/2024-02-27T10:25:12.441978/Eros_SQL_Output001 (2).csv"
-    elections = ["2024-05-02"]
+    addresses_name = "2024-07-04/2024-06-21T15:57:24.816333/MEN_combined.csv"
+    stations_name = "2024-07-04/2024-06-21T15:57:24.816333/MEN_combined.csv"
+    elections = ["2024-07-04"]
+    not_in_mendip_stations = [
+        "80-ilchester-town-hall-no2",
+        "79-ilchester-town-hall-no1",
+        "78-marston-magna-village-hall",
+        "77-rimpton-village-hall",
+        "76-corton-denham-village-hall",
+        "75-wincanton-memorial-hall",
+        "74-wincanton-memorial-hall",
+        "73-the-davis-hall",
+        "72-united-reformed-church-hall",
+        "71-tintinhull-village-hall",
+        "70-bayford-village-hall",
+        "69-sparkford-village-hall",
+        "68-sutton-montis-village-hall",
+        "67-stoke-sub-hamdon-memorial-hall",
+        "66-wessex-rooms",
+        "65-wessex-rooms",
+        "64-queen-camel-memorial-hall",
+        "63-pitney-village-hall",
+        "62-hadspen-village-hall",
+        "61-pen-selwood-village-hall",
+        "60-north-cheriton-village-hall",
+        "59-norton-sub-hamdon-village-hall",
+        "58-north-cadbury-village-hall",
+        "57-mudford-village-hall",
+        "56-muchelney-abbey",
+        "554-wedmore-village-hall",
+        "553-theale-village-hall",
+        "552-blackford-village-hall",
+        "551-weare-memorial-hall",
+        "550-shipham-village-hall",
+        "55-montacute-village-hall",
+        "549-mark-village-hall",
+        "548-lympsham-sports-club",
+        "547-east-brent-village-hall",
+        "546-cross-memorial-hall",
+        "545-cheddar-village-hall-formerly-the-church-house",
+        "544-cheddar-village-hall-formerly-the-church-house",
+        "543-cheddar-village-hall-formerly-the-church-house",
+        "542-the-old-schoolroom",
+        "541-brent-knoll-parish-hall",
+        "540-biddisham-parish-hall",
+        "54-the-camelot-room-milborne-port-village-hall",
+        "539-axbridge-town-hall",
+        "538-stawell-village-hall",
+        "537-shapwick-village-hall",
+        "536-moorlinch-church-hall",
+        "535-edington-village-hall",
+        "534-east-huntspill-village-hall",
+        "533-chilton-polden-village-hall",
+        "532-edington-village-hall",
+        "531-burtle-village-hall",
+        "530-ashcott-village-hall",
+        "53-the-camelot-room-milborne-port-village-hall",
+        "52-martock-youth-centre-nos12",
+        "51-martock-youth-centre-nos12",
+        "50-north-barrow-village-hall",
+        "49-long-load-village-hall",
+        "48-long-sutton-village-hall",
+        "47-ridgeway-hall",
+        "46-kingsdon-village-hall",
+        "45-the-church-rooms",
+        "44-keinton-mandeville-village-hall",
+        "43-barton-st-david-village-hall",
+        "42-all-saints-church",
+        "41-isle-abbotts-village-hall",
+        "40-merryfield-hall",
+        "39-huish-leisure-lifestyle-fitness-langport",
+        "38-st-margarets-hall",
+        "37-high-ham-village-hall",
+        "36-henstridge-village-hall",
+        "35-hambridge-village-hall",
+        "34-fivehead-village-hall",
+        "33-drayton-village-hall",
+        "32-curry-rivel-village-hall",
+        "31-curry-rivel-village-hall",
+        "30-curry-mallet-and-beercrocombe-village-hall",
+        "29-cucklington-village-hall",
+        "28-chilthorne-domer-village-hall",
+        "27-blackford-reading-room",
+        "26-compton-dundon-village-hall",
+        "25-charlton-musgrove-memorial-hall",
+        "24-charlton-mackrell-reading-room",
+        "23-charlton-horethorne-village-hall",
+        "22-the-market-house",
+        "21-caryford-hall",
+        "20-bruton-community-hall",
+        "19-brewham-village-hall",
+        "18-martock-united-reformed-church-hall",
+        "17-barrington-village-hall",
+        "16-babcary-playing-field-hut",
+        "15-ash-village-hall",
+        "14-aller-village-hall",
+    ]
 
     def address_record_to_dict(self, record):
+        station_hash = self.get_station_hash(record)
+        if station_hash in self.not_in_mendip_stations:
+            return None
         uprn = record.uprn.strip().lstrip("0")
 
         if uprn in [
@@ -54,14 +149,11 @@ class Command(BaseHalaroseCsvImporter):
             "BA11 1GN",  # PRINTWORKS ROAD
         ]:
             return None
-
         return super().address_record_to_dict(record)
 
-    # quick fix to show maps for Halarose records that have a valid UPRN in the PollingVenueUPRN field
-    def get_station_point(self, record):
-        uprn = record.pollingvenueuprn.strip().lstrip("0")
-        try:
-            ab_rec = Address.objects.get(uprn=uprn)
-            return ab_rec.location
-        except ObjectDoesNotExist:
-            return super().get_station_point(record)
+    def station_record_to_dict(self, record):
+        station_hash = self.get_station_hash(record)
+        if station_hash in self.not_in_mendip_stations:
+            return None
+
+        return super().station_record_to_dict(record)
