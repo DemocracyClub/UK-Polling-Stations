e4d0df6ba1f509c88fd9dacf3cb96bc8c337f3ae
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-14 15:47:16 +0100

    Import script for Redcar and Cleveland (2024-07-04) (closes #7807)

--- polling_stations/apps/data_importers/management/commands/import_redcar_cleveland.py
+++ polling_stations/apps/data_importers/management/commands/import_redcar_cleveland.py
@@ -4,14 +4,21 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "RCC"
     addresses_name = (
-        "2024-05-02/2024-03-27T08:05:02.256536/Democracy_Club__02May2024.CSV"
+        "2024-07-04/2024-06-14T15:47:03.892737/Democracy_Club__04July2024.CSV"
     )
     stations_name = (
-        "2024-05-02/2024-03-27T08:05:02.256536/Democracy_Club__02May2024.CSV"
+        "2024-07-04/2024-06-14T15:47:03.892737/Democracy_Club__04July2024.CSV"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
 
     def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "10034529362",  # LIVERTON MILL, MOORSHOLM, SALTBURN-BY-THE-SEA
+        ]:
+            return None
+
         if record.addressline6 in [
             # split
             "TS10 4AJ",
