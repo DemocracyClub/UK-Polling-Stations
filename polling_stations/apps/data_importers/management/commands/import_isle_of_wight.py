110c38af5118d10b239344734caa4d30ee83f46c
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-14 14:16:06 +0100

    Import script for Isle of Wight (2024-07-04) (closes #7784)

--- polling_stations/apps/data_importers/management/commands/import_isle_of_wight.py
+++ polling_stations/apps/data_importers/management/commands/import_isle_of_wight.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "IOW"
     addresses_name = (
-        "2024-05-02/2024-03-08T15:57:00.210856/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-06-14T14:15:49.603033/Democracy_Club__04July2024.tsv"
     )
     stations_name = (
-        "2024-05-02/2024-03-08T15:57:00.210856/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-06-14T14:15:49.603033/Democracy_Club__04July2024.tsv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
 
@@ -35,7 +35,6 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
         if record.addressline6 in [
             # splits
-            "PO30 5GT",
             "PO36 9NQ",
             "PO30 2DH",
             # looks wrong
