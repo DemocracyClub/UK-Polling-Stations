8088d6c46c1ffa624af9d13de309d59f180b75b5
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-14 14:22:38 +0100

    Import script for Ceredigion (2024-07-04) (closes #7786)

--- polling_stations/apps/data_importers/management/commands/import_ceredigion.py
+++ polling_stations/apps/data_importers/management/commands/import_ceredigion.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "CGN"
     addresses_name = (
-        "2024-05-02/2024-03-26T15:55:08.555130/Democracy Club - Polling Districts.csv"
+        "2024-07-04/2024-06-14T14:22:31.403452/Democracy Club - Polling Districts.csv"
     )
     stations_name = (
-        "2024-05-02/2024-03-26T15:55:08.555130/Democracy Club - Polling Stations.csv"
+        "2024-07-04/2024-06-14T14:22:31.403452/Democracy Club - Polling Stations.csv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
     csv_encoding = "utf-16le"
 
     def address_record_to_dict(self, record):
@@ -34,20 +34,56 @@ class Command(BaseDemocracyCountsCsvImporter):
             "200001099702",  # PLASBRYNIAU, LADY ROAD, BLAENPORTH, CARDIGAN
             "49048192",  # MAESGWYN, GLYNARTHEN, LLANDYSUL
             "49038741",  # TEGLAN, CILIAU AERON, LAMPETER
+            "49061980",  # Y DDOL LLAWEN, CWMRHEIDOL, ABERYSTWYTH
         ]:
             return None
 
         if record.postcode in [
             # split
             "SA44 5UB",
-            "SA43 1PT",
             # suspect
             "SA44 5NQ",
             "SY23 5JP",
         ]:
             return None
+
         return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
+        # Remove duplicated stations names
         record = record._replace(placename="")
+
+        # Remove stations from the PEM council
+        if record.stationcode in [
+            "100-2025",
+            "00-2025",
+            "101-2026",
+            "102-2027",
+            "77-2001",
+            "78-2002",
+            "79-2003",
+            "80-2004",
+            "81-2005",
+            "82-2006",
+            "83-2007",
+            "83-2008",
+            "84-2009",
+            "85-2010",
+            "86-2011",
+            "87-2012",
+            "88-2013",
+            "89-2014",
+            "90-2015",
+            "91-2016",
+            "92-2017",
+            "93-2018",
+            "94-2019",
+            "95-2020",
+            "96-2021",
+            "97-2022",
+            "98-2023",
+            "99-2024",
+        ]:
+            return None
+
         return super().station_record_to_dict(record)
