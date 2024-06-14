bd7e6a7ddce0929987e483d14ad670d9effea6d0
awdem <wdemontmollin@gmail.com>
2024-06-14 11:10:54 +0100

    misc fixes for KTT council

--- polling_stations/apps/data_importers/management/commands/import_kingston-upon-thames.py
+++ polling_stations/apps/data_importers/management/commands/import_kingston-upon-thames.py
@@ -26,3 +26,27 @@ class Command(BaseDemocracyCountsCsvImporter):
         ]:
             return None
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # The following are stations amendments for the council:
+
+        # Christ Church C Of E Primary School, Pine Garden, Surbiton
+        if record.stationcode == "BRA-K_3":
+            record = record._replace(xordinate="519309", yordinate="167339")
+
+        # Old Malden Children's Centre, Lawrence Avenue, New Malden
+        if record.stationcode == "OMA-W_46":
+            record = record._replace(xordinate="521105", yordinate="166696")
+
+        # Temporary Station - Kings College Sports Ground, Windsor Avenue, New Malden
+        if record.stationcode == "GLD-W_22":
+            record = record._replace(xordinate="520201.89", yordinate="167894.86")
+
+        # Temporary Station Norbiton Train Station, Coombe Road, Kingston Upon Thames
+        if record.stationcode == "RP-CHB":
+            record = record._replace(xordinate="519450.08", yordinate="169539.84")
+
+        # St Pius X Catholic Church, The Triangle, The Triangle, Kingston Upon Thames, Surrey
+        if record.stationcode == "RP-CVD":
+            record = record._replace(placename="St Pius X Catholic Church")
+        return super().station_record_to_dict(record)
