c3dbe9dd88646d8404b2972aa9d4a371c956490f
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-14 15:12:26 +0100

    Import script for Hyndburn (2024-07-04) (closes #7793)

--- polling_stations/apps/data_importers/management/commands/import_hyndburn.py
+++ polling_stations/apps/data_importers/management/commands/import_hyndburn.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "HYN"
     addresses_name = (
-        "2024-05-02/2024-03-19T16:59:46.456514/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-06-14T15:12:16.881791/Democracy_Club__04July2024 (30).tsv"
     )
     stations_name = (
-        "2024-05-02/2024-03-19T16:59:46.456514/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-06-14T15:12:16.881791/Democracy_Club__04July2024 (30).tsv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -18,10 +18,24 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         if uprn in [
             "10009967792",  # 3 HIGH STREET, ACCRINGTON
             "100012878207",  # LEG LOCK FARM, SOUGH LANE, GUIDE, BLACKBURN
+            "10070894822",  # 2A HIGH STREET, RISHTON, BLACKBURN
+            "10070896603",  # THE OLD STABLES BROWNSILLS, MILL LANE, GREAT HARWOOD, BLACKBURN
+            "100012393501",  # WELLSPRINGS FARM, SHAWCLIFFE LANE, GREAT HARWOOD, BLACKBURN
+            "100012393502",  # WYNGATE, SHAWCLIFFE LANE, GREAT HARWOOD, BLACKBURN
+            "100012545092",  # CARPET PLANET & BEDS LTD, 55-57 WHALLEY ROAD, ACCRINGTON
+            "10070894060",  # 2A WATER STREET, ACCRINGTON
+            "10009969364",  # 2A LEMONIUS STREET, ACCRINGTON
+            "100012392450",  # BUMPER HALL FARM, HASLINGDEN OLD ROAD, OSWALDTWISTLE, ACCRINGTON
+            "10070895687",  # SQUIRREL BARN, GREEN HAWORTH, ACCRINGTON
+            "10010696899",  # DUNSCAR FARM COTTAGE, WILPSHIRE ROAD, RISHTON, BLACKBURN
         ]:
             return None
+
         if record.addressline6 in [
-            "BB5 5QA",  # split
+            # split
+            "BB5 5QA",
+            # suspect
+            "BB5 6PW",
         ]:
             return None
 
