412b717300b7c05fe44974306a02acb170f2e041
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-14 15:02:43 +0100

    Import script for Denbighshire (2024-07-04) (closes #7789)

--- polling_stations/apps/data_importers/management/commands/import_denbighshire.py
+++ polling_stations/apps/data_importers/management/commands/import_denbighshire.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "DEN"
     addresses_name = (
-        "2024-05-02/2024-04-10T10:59:19.045149/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-06-14T15:02:30.247466/Democracy_Club__04July2024 (28).tsv"
     )
     stations_name = (
-        "2024-05-02/2024-04-10T10:59:19.045149/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-06-14T15:02:30.247466/Democracy_Club__04July2024 (28).tsv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
 
@@ -18,6 +18,14 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
         if uprn in [
             "200004298031",  # AELWYD UCHA, RHUALLT, ST. ASAPH
+            "10003928367",  # MYNYDD LLANFAIR, BRYNEGLWYS, CORWEN
         ]:
             return None
+
+        if record.addressline6 in [
+            # suspect
+            "LL15 1FF",
+        ]:
+            return None
+
         return super().address_record_to_dict(record)
