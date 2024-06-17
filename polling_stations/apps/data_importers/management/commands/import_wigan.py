515b7dec4221904785c18146849f01aa22329cdb
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-17 11:12:03 +0100

    Import script for Wigan (2024-07-04) (closes #7818)

--- polling_stations/apps/data_importers/management/commands/import_wigan.py
+++ polling_stations/apps/data_importers/management/commands/import_wigan.py
@@ -3,9 +3,9 @@ from data_importers.management.commands import BaseXpressWebLookupCsvImporter
 
 class Command(BaseXpressWebLookupCsvImporter):
     council_id = "WGN"
-    addresses_name = "2024-05-02/2024-03-26T12:26:18.296702/wigan-stations.csv"
-    stations_name = "2024-05-02/2024-03-26T12:26:18.296702/wigan-stations.csv"
-    elections = ["2024-05-02"]
+    addresses_name = "2024-07-04/2024-06-22T08:32:01.577572/wigan-cleaned.csv"
+    stations_name = "2024-07-04/2024-06-22T08:32:01.577572/wigan-cleaned.csv"
+    elections = ["2024-07-04"]
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
@@ -32,31 +32,3 @@ class Command(BaseXpressWebLookupCsvImporter):
             return None
 
         return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # St Aidans Parish Centre, Highfield Grange Ave, Winstanley, Wigan, WN3 6EE
-        if record.pollingplaceid == "9761":
-            record = record._replace(
-                pollingplaceeasting="355847", pollingplacenorthing="402852"
-            )
-
-        # Standish Community Centre, Moody Street, Off Church, Street Standish, WN6 0JY
-        if record.pollingplaceid == "9906":
-            record = record._replace(
-                pollingplaceeasting="356374", pollingplacenorthing="410043"
-            )
-        # User bug report #634
-        # Moving point closer to access road for:
-        # The Community Centre Hope Carr Road, Siddow Common, Leigh WN7 3ET
-        if record.pollingplaceid == "9664":
-            record = record._replace(
-                pollingplaceeasting="366257", pollingplacenorthing="399402"
-            )
-
-        # User feedback, removes map of:
-        # Ince Independent Methodist Church, Keble Street - Stopford Street Entrance, Ince, Wigan
-        rec = super().station_record_to_dict(record)
-        if rec["internal_council_id"] == "10025":
-            rec["location"] = None
-            return rec
-        return super().station_record_to_dict(record)
