0f145210170d66117bbc6f3ac34bdf0428677735
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-18 10:00:39 +0100

    Import script for Cheshire West and Chester (2024-07-04) (closes #7840)

--- polling_stations/apps/data_importers/management/commands/import_cheshire_west.py
+++ polling_stations/apps/data_importers/management/commands/import_cheshire_west.py
@@ -4,18 +4,18 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "CHW"
     addresses_name = (
-        "2024-05-02/2024-04-08T13:57:14.878521/Democracy_Club__02May2024.CSV"
+        "2024-07-04/2024-06-18T10:00:13.527644/Democracy_Club__04July2024 (31).tsv"
     )
     stations_name = (
-        "2024-05-02/2024-04-08T13:57:14.878521/Democracy_Club__02May2024.CSV"
+        "2024-07-04/2024-06-18T10:00:13.527644/Democracy_Club__04July2024 (31).tsv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "200003238923",  # THE OLD BARN, BRADSHAW BROOK FARM, MIDDLEWICH ROAD, ALLOSTOCK, KNUTSFORD
             "10091137623",  # 70 MUSKETT DRIVE, NORTHWICH
             "10091137622",  # 72 MUSKETT DRIVE, NORTHWICH
             "10091137631",  # 68 MUSKETT DRIVE, NORTHWICH
@@ -25,38 +25,27 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "100012360649",  # 6 LEIGHTON COTTAGES, BOATHOUSE LANE, PARKGATE, NESTON
             "10095485945",  # WESTVIEW, WELSH ROAD, LEDSHAM, ELLESMERE PORT
             "10094685291",  # 6 BALISTER DRIVE, HARTFORD, NORTHWICH
+            "10093981388",  # 16 LANCASTER GARDENS, ELLESMERE PORT
+            "10093486377",  # 89 THORNTON ROAD, ELLESMERE PORT
+            "10093486380",  # 1 CINDER CLOSE, ELLESMERE PORT
+            "10093486372",  # 2 VICTORY COURT, ELLESMERE PORT
+            "200000832902",  # LOWCROSS MILL, LOWCROSS, TILSTON, MALPAS
         ]:
             return None
 
         if record.addressline6 in [
             # splits
-            "CW9 6EL",
-            "CW9 8XB",
-            "CW9 7SQ",
-            "CH66 1NZ",
             "CW9 8PU",
-            "CW7 2GG",
-            "CH64 3SG",
-            "CW7 3EQ",
+            "CW8 4BT",
             "CH4 7DG",
-            "CW8 4AB",
-            "CW6 9EP",
-            "CW9 7RX",
-            "CW8 4QS",
             "CH65 9JU",
-            "WA6 0JA",
-            "WA6 6LN",
+            "CH64 3SG",
+            "CW7 2GG",
+            "CW7 3EQ",
+            "CW8 4AB",
+            "CH66 1NZ",
+            "CW6 9EP",
         ]:
             return None
 
         return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # Lower Peover CE Primary School, The Cobbles, Lower Peover, Knutsford, Cheshire, WA16 9PZ
-        # Fixing the warning, correction is cosmetic and safe
-        if record.polling_place_id == "11682":
-            record = record._replace(
-                polling_place_easting="374254", polling_place_northing="374113"
-            )
-
-        return super().station_record_to_dict(record)
