e849f2693b2faba6e4bfceaf2797a1114552fbc5
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-18 10:36:34 +0100

    Import script for Castle Point (2024-07-04) (closes #7850)

--- polling_stations/apps/data_importers/management/commands/import_castle_point.py
+++ polling_stations/apps/data_importers/management/commands/import_castle_point.py
@@ -4,13 +4,14 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "CAS"
     addresses_name = (
-        "2024-05-02/2024-04-08T16:51:53.256779/Democracy_Club__02May2024 (2).CSV"
+        "2024-07-04/2024-06-18T10:36:29.124832/Democracy_Club__04July2024.tsv"
     )
     stations_name = (
-        "2024-05-02/2024-04-08T16:51:53.256779/Democracy_Club__02May2024 (2).CSV"
+        "2024-07-04/2024-06-18T10:36:29.124832/Democracy_Club__04July2024.tsv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
     csv_encoding = "windows-1252"
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
@@ -20,6 +21,7 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "100091599588",  # 115 DOWNER ROAD, BENFLEET
             "10004941351",  # 420 DAWS HEATH ROAD, BENFLEET
             "10004941352",  # 422 DAWS HEATH ROAD, BENFLEET
+            "10094905546",  # BIRD OF PARADISE SMALL GAINS MARINA POINT ROAD, CANVEY ISLAND
         ]:
             return None
         if record.addressline6 in [
@@ -35,7 +37,7 @@ class Command(BaseXpressDemocracyClubCsvImporter):
     def station_record_to_dict(self, record):
         # Fixing missing postcode:
         # Runnymede Hall Foyer, Rear of Council Offices, Kiln Road, Thundersley, Benfleet
-        if record.polling_place_id == "2789":
+        if record.polling_place_id in ["2853", "2855"]:
             record = record._replace(polling_place_postcode="SS7 1TF")
 
         return super().station_record_to_dict(record)
