878b3fcd40de5abdc6b62c5f7c1acc13e0ae28d3
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-19 11:49:07 +0100

    Import script for Harrow (2024-07-04) (closes #7868)

--- polling_stations/apps/data_importers/management/commands/import_harrow.py
+++ polling_stations/apps/data_importers/management/commands/import_harrow.py
@@ -3,12 +3,8 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "HRW"
-    addresses_name = (
-        "2024-07-04/2024-06-11T08:45:16.958438/Democracy_Club__04July2024.tsv"
-    )
-    stations_name = (
-        "2024-07-04/2024-06-11T08:45:16.958438/Democracy_Club__04July2024.tsv"
-    )
+    addresses_name = "2024-07-04/2024-06-19T11:48:48.902893/HRW_combined.tsv"
+    stations_name = "2024-07-04/2024-06-19T11:48:48.902893/HRW_combined.tsv"
     elections = ["2024-07-04"]
     csv_delimiter = "\t"
 
