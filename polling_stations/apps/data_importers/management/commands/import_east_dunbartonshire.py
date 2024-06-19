5434d1d6fb317119ab989d6f0bb900c8b021622c
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-19 09:26:44 +0100

    Import script for East Dunbartonshire (2024-07-04) (closes #7862)

--- polling_stations/apps/data_importers/management/commands/import_east_dunbartonshire.py
+++ polling_stations/apps/data_importers/management/commands/import_east_dunbartonshire.py
@@ -3,8 +3,8 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "EDU"
-    addresses_name = "2024-07-04/2024-06-06T10:22:02.550284/Eros_SQL_Output016.csv"
-    stations_name = "2024-07-04/2024-06-06T10:22:02.550284/Eros_SQL_Output016.csv"
+    addresses_name = "2024-07-04/2024-06-19T09:26:31.007903/EDU_combined.csv"
+    stations_name = "2024-07-04/2024-06-19T09:26:31.007903/EDU_combined.csv"
     elections = ["2024-07-04"]
 
     def address_record_to_dict(self, record):
