195cb7cc9ea4fac05779c51e2e7ffb0be1425265
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-20 14:58:35 +0100

    Import script for Orkney Islands (2024-07-04) (closes #7896)

--- polling_stations/apps/data_importers/management/commands/import_orkney_islands.py
+++ polling_stations/apps/data_importers/management/commands/import_orkney_islands.py
@@ -3,13 +3,10 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "ORK"
-    addresses_name = "2022-05-05/2022-03-10T11:14:48.303621/polling_station_export-2022-03-07.csv 1.csv"
-    stations_name = "2022-05-05/2022-03-10T11:14:48.303621/polling_station_export-2022-03-07.csv 1.csv"
-    elections = ["2022-05-05"]
-
-    def address_record_to_dict(self, record):
-        if record.housepostcode in [
-            "KW15 1WU",
-        ]:
-            return None
-        return super().address_record_to_dict(record)
+    addresses_name = (
+        "2024-07-04/2024-06-20T14:58:29.571792/Orkney Eros_SQL_Output012.csv"
+    )
+    stations_name = (
+        "2024-07-04/2024-06-20T14:58:29.571792/Orkney Eros_SQL_Output012.csv"
+    )
+    elections = ["2024-07-04"]
