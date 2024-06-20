117a9de81e73f65c4dfb887e27e49318958131d3
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-20 12:34:57 +0100

    Import script for Cornwall (2024-07-04) (closes #7890)

--- polling_stations/apps/data_importers/management/commands/import_cornwall.py
+++ polling_stations/apps/data_importers/management/commands/import_cornwall.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "CON"
     addresses_name = (
-        "2024-05-02/2024-04-04T14:05:55.904632/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-06-20T12:34:38.090059/Democracy_Club__04July2024.tsv"
     )
     stations_name = (
-        "2024-05-02/2024-04-04T14:05:55.904632/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-06-20T12:34:38.090059/Democracy_Club__04July2024.tsv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
 
@@ -17,34 +17,21 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         # Amendments from previous elections:
 
         # Methodist Sunday School Room Canworthy Water Launceston PL15 8UW
-        if record.polling_place_id == "15105":
+        if record.polling_place_id == "16273":
             record = record._replace(polling_place_uprn="10003300470")
 
         # The Cove Hall Wilcove Torpoint PL11 2PT
-        if record.polling_place_id == "14619":
+        if record.polling_place_id == "15768":
             record = record._replace(polling_place_uprn="10003065058")
 
         # St Austell Rugby Club Tregorrick Lane St Austell PL26 7FH
-        if record.polling_place_id == "14259":
+        if record.polling_place_id == "16494":
             record = record._replace(polling_place_uprn="10002694687")
 
         # Millbrook Village Hall The Parade Millbrook Torpoint
-        if record.polling_place_id == "14757":
+        if record.polling_place_id == "15911":
             record = record._replace(polling_place_uprn="10023432417")
 
-        # Amendments from council:
-
-        # The Victory Hall St Francis Road Indian Queens St Columb TR9 6JR TR9 6JR
-        if record.polling_place_id == "14315":
-            record = record._replace(
-                polling_place_postcode="TR9 6QN",
-            )
-        # The Conference Room, Tregawn Farm, Michaelstow, Bodmin PL30 3PB
-        if record.polling_place_id == "15166":
-            record = record._replace(
-                polling_place_easting="207303",
-                polling_place_northing="78823",
-            )
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
@@ -57,26 +44,26 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             return None
         if record.addressline6 in [
             # split
+            "TR11 4PT",
+            "PL11 2HW",
             "TR26 1EZ",
             "PL15 7BJ",
-            "PL11 2HW",
-            "TR27 4RZ",
             "TR27 4AG",
-            "TR11 4PT",
-            "TR26 2AQ",
-            "PL18 9BJ",
-            "TR19 6BH",
-            "PL30 3DH",
+            "TR18 5NA",
+            "TR27 4RZ",
             "TR8 4NR",
+            "TR26 2AQ",
             "PL32 9UN",
             "TR16 5QD",
-            "TR2 5JS",
-            "TR20 8TG",
-            "TR18 5NA",
-            "PL15 8RF",
-            "PL25 4EL",
-            "TR18 3NH",
             "PL15 7SE",
+            "PL18 9BJ",
+            "PL30 3DH",
+            "TR19 6BH",
+            "PL25 4EL",
+            "TR20 8TG",
+            "PL15 8RF",
+            "TR2 5JS",
+            "TR18 3NH",
             "PL31 2PB",
         ]:
             return None
