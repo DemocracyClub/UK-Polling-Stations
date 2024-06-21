9931c3a40031525f8361bccd6b701a9311fc4b29
GeoWill <roper.william@gmail.com>
2024-06-21 15:44:41 +0100

    Import script for Thanet

--- polling_stations/apps/data_importers/management/commands/import_thanet.py
+++ polling_stations/apps/data_importers/management/commands/import_thanet.py
@@ -3,8 +3,8 @@ from data_importers.management.commands import BaseFcsDemocracyClubApiImporter
 
 class Command(BaseFcsDemocracyClubApiImporter):
     council_id = "THA"
-    elections = ["2024-05-02"]
-    fcs_election_id = 92
+    elections = ["2024-07-04"]
+    fcs_election_id = 96
 
     def address_record_to_dict(self, record):
         if record["addressPostCode"] in [
