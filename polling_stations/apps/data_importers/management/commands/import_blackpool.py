c7b8e354bccb079096e78a0bc02bfe1693b95f0b
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-21 14:36:01 +0100

    Import script for Blackpool (2024-07-04) (closes #7917)

--- polling_stations/apps/data_importers/management/commands/import_blackpool.py
+++ polling_stations/apps/data_importers/management/commands/import_blackpool.py
@@ -4,28 +4,22 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "BPL"
     addresses_name = (
-        "2024-05-02/2024-03-25T14:22:10.220217/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-06-21T14:35:52.301348/Democracy_Club__04July2024.CSV"
     )
     stations_name = (
-        "2024-05-02/2024-03-25T14:22:10.220217/Democracy_Club__02May2024.tsv"
+        "2024-07-04/2024-06-21T14:35:52.301348/Democracy_Club__04July2024.CSV"
     )
-    elections = ["2024-05-02"]
-    csv_delimiter = "\t"
+    elections = ["2024-07-04"]
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
-
         if uprn in [
             "10000109140",  # 1 WINDMILL MOBILE HOME PARK PRESTON NEW ROAD, BLACKPOOL
             "10000109142",  # 2 WINDMILL MOBILE HOME PARK PRESTON NEW ROAD, BLACKPOOL
-            "10070847268",  # 89A HOLMFIELD ROAD, BLACKPOOL
-            "100010797546",  # 519 DEVONSHIRE ROAD, BISPHAM, BLACKPOOL
-            "100012612017",  # BLACKPOOL PARK GOLF CLUB, NORTH PARK DRIVE, BLACKPOOL
             "100010810222",  # 2 JEPSON WAY, BLACKPOOL
             "10090937406",  # FLAT 3, 128 CENTRAL DRIVE, BLACKPOOL
         ]:
             return None
-
         if record.addressline6 in [
             # splits
             "FY4 3DJ",
