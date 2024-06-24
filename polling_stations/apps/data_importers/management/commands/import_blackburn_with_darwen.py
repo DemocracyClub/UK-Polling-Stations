728977fb0526bb4f0760d4df14773a31d6ebeb08
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-24 14:23:54 +0100

    Import script for Blackburn with Darwen (2024-07-04) (closes #7848)

--- polling_stations/apps/data_importers/management/commands/import_blackburn_with_darwen.py
+++ polling_stations/apps/data_importers/management/commands/import_blackburn_with_darwen.py
@@ -3,11 +3,48 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "BBD"
-    addresses_name = (
-        "2024-07-04/2024-06-18T10:36:16.375411/Democracy_Club__04July2024 (32).tsv"
-    )
-    stations_name = (
-        "2024-07-04/2024-06-18T10:36:16.375411/Democracy_Club__04July2024 (32).tsv"
-    )
+    addresses_name = "2024-07-04/2024-06-24T14:23:42.710102/BBD_combined.tsv"
+    stations_name = "2024-07-04/2024-06-24T14:23:42.710102/BBD_combined.tsv"
     elections = ["2024-07-04"]
     csv_delimiter = "\t"
+
+    # Following warning checked and no correction needed:
+    # WARNING: Polling station Belthorn Primary School (5798) is in Hyndburn Borough Council (HYN)
+
+    def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "100012541481",  # DARR CLOTH HOUSE, 5-7 NEW BANK ROAD, BLACKBURN
+            "100010753970",  # 2 PLECKGATE ROAD, BLACKBURN
+            "10010321691",  # FLAT, 149 WHALLEY OLD ROAD, BLACKBURN
+            "200004508099",  # CARETAKERS FLAT SOMME BARRACKS MOSS STREET, BLACKBURN
+            "10010324584",  # STUDENT ACCOMODATION 30 MOSS STREET, BLACKBURN
+            "10091620653",  # 197A AUDLEY RANGE, BLACKBURN
+            "100010730844",  # 1 ARRAN AVENUE, BLACKBURN
+            "10024626659",  # FLAT AT 51 MARKET STREET, DARWEN
+            "10010322678",  # 150A BLACKBURN ROAD, DARWEN
+            "100010759333",  # 83 SHORROCK LANE, BLACKBURN
+            "100010732406",  # 53 BEECHWOOD DRIVE, BLACKBURN
+        ]:
+            return None
+
+        if record.addressline6 in [
+            # splits
+            "BB1 7LT",
+            "BB3 2NQ",
+            "BB1 7LS",
+            "BB1 1EB",
+            "BB1 2NL",
+            "BB1 1JT",
+            # looks wrong
+            "BB2 6TE",
+            "BB2 2SS",
+            "BB1 1JS",
+            "BB3 3QP",
+            # "BB2 5FT",  # waiting for council response
+            # "BB2 5FX",  # waiting for council response
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
