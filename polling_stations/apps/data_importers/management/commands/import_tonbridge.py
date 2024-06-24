6ad02feeb6fe05d90c988076297f6b1995fcf3ba
GeoWill <roper.william@gmail.com>
2024-06-24 13:48:33 +0100

    Import script for Tonbridge and Malling (2024-07-04) (closes #7795)

--- polling_stations/apps/data_importers/management/commands/import_tonbridge.py
+++ polling_stations/apps/data_importers/management/commands/import_tonbridge.py
@@ -1,31 +1,30 @@
-from addressbase.models import Address
 from data_importers.management.commands import BaseHalaroseCsvImporter
-from django.core.exceptions import ObjectDoesNotExist
 
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "TON"
-    addresses_name = "2024-05-02/2024-04-02T15:35:52.601856/Eros_SQL_Output002.csv"
-    stations_name = "2024-05-02/2024-04-02T15:35:52.601856/Eros_SQL_Output002.csv"
-    elections = ["2024-05-02"]
+    addresses_name = "2024-07-04/2024-06-24T13:00:52.040925/ton-combined.csv"
+    stations_name = "2024-07-04/2024-06-24T13:00:52.040925/ton-combined.csv"
+    elections = ["2024-07-04"]
+
+    duplicate_stations = ["JUDD-31", "JUDD-32", "JUDD-33", "JUDD-34"]
+
+    def station_record_to_dict(self, record):
+        if record.pollingstationnumber in self.duplicate_stations:
+            return None
+        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
+        if record.pollingstationnumber in self.duplicate_stations:
+            return None
+
         if record.housepostcode in [
             # split
-            "TN11 0AJ",
-            "TN10 4JJ",
-            "TN11 0ES",
             "ME20 6HZ",
+            "TN11 0ES",
+            "TN10 4JJ",
+            "TN11 0AJ",
             "ME19 5PA",
         ]:
             return None
         return super().address_record_to_dict(record)
-
-    # quick fix to show maps for Halarose records that have a valid UPRN in the PollingVenueUPRN field
-    def get_station_point(self, record):
-        uprn = record.pollingvenueuprn.strip().lstrip("0")
-        try:
-            ab_rec = Address.objects.get(uprn=uprn)
-            return ab_rec.location
-        except ObjectDoesNotExist:
-            return super().get_station_point(record)
