de5daed8da550c21c32b7cd7296e621a8b732dbf
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-25 12:25:14 +0100

    Import script for Ribble Valley (2024-07-04) (closes #7960)

--- polling_stations/apps/data_importers/management/commands/import_ribble.py
+++ polling_stations/apps/data_importers/management/commands/import_ribble.py
@@ -5,12 +5,8 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "RIB"
-    addresses_name = (
-        "2024-07-04/2024-05-30T15:23:33.573373/Democracy_Club__04July2024.tsv"
-    )
-    stations_name = (
-        "2024-07-04/2024-05-30T15:23:33.573373/Democracy_Club__04July2024.tsv"
-    )
+    addresses_name = "2024-07-04/2024-06-25T12:25:05.523108/RIB_combined.tsv"
+    stations_name = "2024-07-04/2024-06-25T12:25:05.523108/RIB_combined.tsv"
     elections = ["2024-07-04"]
     csv_delimiter = "\t"
 
