e961e008deba4c5f7174c033ca1ca5467a4e9fa0
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-25 14:39:21 +0100

    Import script for East Riding of Yorkshire (2024-07-04) (closes #7965)

--- polling_stations/apps/data_importers/management/commands/import_east_riding.py
+++ polling_stations/apps/data_importers/management/commands/import_east_riding.py
@@ -3,10 +3,9 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "ERY"
-    addresses_name = "2024-07-04/2024-06-08T17:09:09.105422/ERYC - Democracy Club - Polling Districts.csv"
-    stations_name = "2024-07-04/2024-06-08T17:09:09.105422/ERYC - Democracy Club - Polling Stations.csv"
+    addresses_name = "2024-07-04/2024-06-25T14:39:04.816838/ERY_PD_combined.csv"
+    stations_name = "2024-07-04/2024-06-25T14:39:04.816838/ERY_PS_combined.csv"
     elections = ["2024-07-04"]
-    csv_encoding = "utf-16le"
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
@@ -21,6 +20,7 @@ class Command(BaseDemocracyCountsCsvImporter):
         if record.postcode in [
             # splits
             "HU18 1EH",
+            "HU10 7AD",
         ]:
             return None
 
