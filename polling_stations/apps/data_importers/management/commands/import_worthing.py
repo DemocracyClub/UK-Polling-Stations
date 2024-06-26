5a14e50e8f13fb3dd33315975c5118201e39d62e
awdem <wdemontmollin@gmail.com>
2024-06-26 16:16:50 +0100

    bug report 666: removes map pending council response

--- polling_stations/apps/data_importers/management/commands/import_worthing.py
+++ polling_stations/apps/data_importers/management/commands/import_worthing.py
@@ -7,3 +7,15 @@ class Command(BaseXpressDemocracyClubCsvImporter):
     stations_name = "2024-07-04/2024-06-07T16:41:56.977816/WOT_combined.tsv"
     elections = ["2024-07-04"]
     csv_delimiter = "\t"
+
+    def station_record_to_dict(self, record):
+        # WDIV bugfix 666: removes map pending council response for:
+        # Richmond Room (adj. to Assembly Hall), Stoke Abbott Road, Worthing, BN11 1HQ
+        # suggested coords (x,y): 514733.31, 102962.60
+        if record.polling_place_id == "3160":
+            record = record._replace(
+                polling_place_easting="",
+                polling_place_northing="",
+                polling_place_uprn="",
+            )
+        return super().station_record_to_dict(record)
