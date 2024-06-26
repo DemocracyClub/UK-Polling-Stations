02336761e2f43c67d4ee3aee989d975a0293f09a
awdem <wdemontmollin@gmail.com>
2024-06-26 16:34:23 +0100

    station change from TEN council

--- polling_stations/apps/data_importers/management/commands/import_tendring.py
+++ polling_stations/apps/data_importers/management/commands/import_tendring.py
@@ -29,5 +29,17 @@ class Command(BaseDemocracyCountsCsvImporter):
         # these records are ordered, we can just exclude them by station_code
         if int(record.stationcode) > 77:
             return None
-
+        # station change from council:
+        # old: The 1912 Centre, Kings Quay Street, Harwich, Essex CO12 3ES
+        # new: Temporary Unit, Wellington road car park, Wellington road, Harwich, Essex, CO12 3DL
+        if record.stationcode == "66":
+            record = record._replace(
+                add1="Wellington Road Car Park",
+                add2="Harwich",
+                placename="Temporary Unit",
+                pollingplaceid="53",
+                postcode="CO12 3DL",
+                xordinate="",
+                yordinate="",
+            )
         return super().station_record_to_dict(record)
