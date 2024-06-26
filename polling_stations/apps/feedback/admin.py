c6e18b96af7fe1c27a5d695336e2f923a017780b
GeoWill <roper.william@gmail.com>
2024-06-26 11:25:36 +0100

    Add preview url to feedback admin

--- polling_stations/apps/feedback/admin.py
+++ polling_stations/apps/feedback/admin.py
@@ -1,20 +1,47 @@
 import csv
 import datetime
+import re
 
 from core.admin_mixins import AsanaUrlExistsFilter, send_to_asana
 from django.contrib import admin
 from django.http import HttpResponse, HttpResponseForbidden
 from django.urls import re_path
+from django.utils.http import url_has_allowed_host_and_scheme
+from django.utils.safestring import mark_safe
 
 from .models import Feedback
+
+
+def check_source_url_for_preview(source_url):
+    pattern = re.compile(
+        r"""
+            ^            # Start of string
+            /address/    # "/address/"
+            [^/]+        # One or more characters that are not a forward slash
+            /            # "/"
+        |                # OR
+            /postcode/   # "/postcode/"
+            [^/]+        # One or more characters that are not a forward slash
+            /            # "/"
+            $            # End of string
+        """,
+        re.VERBOSE,
+    )
+
+    return bool(pattern.fullmatch(source_url))
 
 
 class FeedbackAdmin(admin.ModelAdmin):
     list_filter = ("found_useful", AsanaUrlExistsFilter)
     list_display = ("id", "found_useful", "vote", "comments", "created")
-    readonly_fields = [f.name for f in Feedback._meta.get_fields()]
+    readonly_fields = [f.name for f in Feedback._meta.get_fields()] + ["preview_url"]
     ordering = ("-created", "id")
     actions = [send_to_asana]
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        self.request = request
+        return qs
 
     def has_delete_permission(self, request, obj=None):
         return False
@@ -29,6 +56,14 @@ class FeedbackAdmin(admin.ModelAdmin):
             re_path("export_comments/", self.export_feedback_with_comments),
         ]
         return my_urls + urls
+
+    def preview_url(self, obj):
+        if check_source_url_for_preview(
+            obj.source_url
+        ) and url_has_allowed_host_and_scheme(obj.source_url, allowed_hosts=None):
+            link = self.request.build_absolute_uri(obj.source_url)
+            return mark_safe('<a href="%s">%s</a>' % (link, link))
+        return "-"
 
     def export_all_feedback(self, request):
         if not request.user.is_superuser:
