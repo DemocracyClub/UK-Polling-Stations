0e36b1b69887711821cc02ca77d5c2d018ac86dd
GeoWill <roper.william@gmail.com>
2024-06-27 16:37:48 +0100

    Station change for Crawley (CRW)

--- polling_stations/apps/data_importers/management/commands/import_crawley.py
+++ polling_stations/apps/data_importers/management/commands/import_crawley.py
@@ -12,6 +12,23 @@ class Command(BaseXpressDemocracyClubCsvImporter):
     elections = ["2024-07-04"]
     csv_delimiter = "\t"
 
+    def station_record_to_dict(self, record):
+        # Change of station
+        # Old: Northgate Community Centre, Barnfield Road, RH10 8DS
+        # New: St. Elizabeth’s Church, RH10 8DS
+        if record.polling_place_id == "1579":
+            record = record._replace(
+                polling_place_name="St. Elizabeth’s Church",
+                polling_place_address_1="(formally Louise Ryrie Dance School)",
+                polling_place_address_2="corner of Barnfield Road and Boundary Road,",
+                polling_place_address_3="Northgate",
+                polling_place_address_4="Crawley",
+                polling_place_postcode="RH10 8DS",
+                polling_place_easting="527314",
+                polling_place_northing="137286",
+            )
+        return super().station_record_to_dict(record)
+
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
