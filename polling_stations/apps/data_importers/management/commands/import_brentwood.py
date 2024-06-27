4db77edf744c61b32d051fa68d2ab7d71ccf4fa3
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-27 13:18:35 +0100

    Import script for Brentwood (2024-07-04) (closes #8002)

--- polling_stations/apps/data_importers/management/commands/import_brentwood.py
+++ polling_stations/apps/data_importers/management/commands/import_brentwood.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "BRW"
     addresses_name = (
-        "2024-05-02/2024-03-21T15:27:59.716517/Democracy_Club__02May2024 (22).tsv"
+        "2024-07-04/2024-06-27T13:18:28.950349/Democracy_Club__04July2024.tsv"
     )
     stations_name = (
-        "2024-05-02/2024-03-21T15:27:59.716517/Democracy_Club__02May2024 (22).tsv"
+        "2024-07-04/2024-06-27T13:18:28.950349/Democracy_Club__04July2024.tsv"
     )
-    elections = ["2024-05-02"]
+    elections = ["2024-07-04"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
