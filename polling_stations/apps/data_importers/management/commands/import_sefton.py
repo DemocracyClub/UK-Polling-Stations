0853a6e118effbdece01f759da1f7aeeefd66b41
polling-bot-4000 <developers@democracyclub.org.uk>
2024-06-28 11:36:28 +0100

    Import script for Sefton (2024-07-04) (closes #7690)

--- polling_stations/apps/data_importers/management/commands/import_sefton.py
+++ polling_stations/apps/data_importers/management/commands/import_sefton.py
@@ -4,10 +4,10 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SFT"
     addresses_name = (
-        "2024-07-04/2024-06-10T20:57:58.525616/Democracy_Club__04July2024.tsv"
+        "2024-07-04/2024-06-28T11:36:11.728532/Democracy_Club__04July2024.tsv"
     )
     stations_name = (
-        "2024-07-04/2024-06-10T20:57:58.525616/Democracy_Club__20June2024.tsv"
+        "2024-07-04/2024-06-28T11:36:11.728532/Democracy_Club__04July2024.tsv"
     )
     elections = ["2024-07-04"]
     csv_delimiter = "\t"
