c72bdaf97bfbfc4811301ae8238eb5652488a93f
awdem <wdemontmollin@gmail.com>
2024-07-01 15:43:54 +0100

    bugfix for WDIV bugreport #683

--- polling_stations/apps/data_importers/management/commands/import_vale_of_glamorgan.py
+++ polling_stations/apps/data_importers/management/commands/import_vale_of_glamorgan.py
@@ -10,3 +10,11 @@ class Command(BaseDemocracyCountsCsvImporter):
         "2024-07-04/2024-06-06T10:32:09.163599/polling-stations-combined.csv"
     )
     elections = ["2024-07-04"]
+
+    def station_record_to_dict(self, record):
+        # bug report #683: postcode correction confirmed by council for:
+        # HEBRON CHURCH ANNEXE - STATION A, PILL STREET, COGAN, PENARTH, VALE OF GLAMORGAN
+        if record.stationcode == "14-a":
+            record = record._replace(postcode="CF64 2JS")
+
+        return super().station_record_to_dict(record)
