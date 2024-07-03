9e5c98f5877ebd67635751cce3da057fa56b27ba
awdem <wdemontmollin@gmail.com>
2024-07-03 12:25:38 +0100

    station change from ERY council

--- polling_stations/apps/data_importers/management/commands/import_east_riding.py
+++ polling_stations/apps/data_importers/management/commands/import_east_riding.py
@@ -25,3 +25,19 @@ class Command(BaseDemocracyCountsCsvImporter):
             return None
 
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # station change from council:
+        # old: MOBILE UNIT - MARTON ROAD Car Park 26 Marton Road Bridlington East Riding Of Yorkshire
+        # new: : MOBILE UNIT - AMENITY LAND, PINFOLD LANE, Amenity Land, Pinfold Lane, Bridlington, YO16 7AQ
+        if record.stationcode == "30":
+            record = record._replace(
+                add1="PINFOLD LANE",
+                add2="Amenity Land",
+                placename="MOBILE UNIT - AMENITY LAND",
+                postcode="YO16 7AQ",
+                stationcode="30",
+                xordinate="517607.13",
+                yordinate="468293.28",
+            )
+        return super().station_record_to_dict(record)
