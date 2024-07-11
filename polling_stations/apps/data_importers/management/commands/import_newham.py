0d0713b2aedc557df913c5ddf39ea717c9581de4
polling-bot-4000 <developers@democracyclub.org.uk>
2024-07-11 10:46:56 +0100

    Import script for Newham (2024-07-18) (closes #8066)

--- polling_stations/apps/data_importers/management/commands/import_newham.py
+++ polling_stations/apps/data_importers/management/commands/import_newham.py
@@ -4,10 +4,37 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "NWM"
     addresses_name = (
-        "2024-07-18/2024-07-09T09:08:42.898950/Democracy_Club__18July2024.tsv"
+        "2024-07-18/2024-07-11T10:46:37.336867/Democracy_Club__04July2024_revised.tsv"
     )
     stations_name = (
-        "2024-07-18/2024-07-09T09:08:42.898950/Democracy_Club__18July2024.tsv"
+        "2024-07-18/2024-07-11T10:46:37.336867/Democracy_Club__04July2024_revised.tsv"
     )
     elections = ["2024-07-18"]
     csv_delimiter = "\t"
+
+    def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "10023995039",  # FLAT AT THE DOCKLANDS EQUESTRIAN CENTRE 2 CLAPS GATE LANE, BECKTON, LONDON
+            "10094880629",  # FLAT 1 200 THE GROVE, STRATFORD, LONDON
+            "10009003474",  # FLAT 2 200 THE GROVE, STRATFORD, LONDON
+            "10009003475",  # FLAT 3 200 THE GROVE, STRATFORD, LONDON
+            "46001049",  # 96 ALDERSBROOK ROAD, LONDON
+            "10012838007",  # FLAT ABOVE 24 STEPHENSON STREET, CANNING TOWN, LONDON
+            "10012838012",  # FLAT, 162 BIDDER STREET, LONDON
+            "10093472922",  # ALAIN CODY DOCK 11C SOUTH CRESCENT, CANNING TOWN, LONDON
+            "10093472923",  # MADORCHA CODY DOCK 11C SOUTH CRESCENT, CANNING TOWN, LONDON
+        ]:
+            return None
+
+        if record.addressline6 in [
+            # split
+            "E13 0DZ",
+            # looks wrong
+            "E15 1BQ",
+            "E15 1BG",
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
