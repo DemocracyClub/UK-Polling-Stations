2e99090f900b0f1ff312940430ac196952a53586
chris48s <git@chris-shaw.dev>
2024-09-19 13:59:27 +0100

    rewrite file upload and s3 wrapper to use boto3/roles

--- polling_stations/settings/constants/importers.py
+++ polling_stations/settings/constants/importers.py
@@ -1,20 +1,4 @@
-"""
-Amazon S3 config:
-By default, we will look for a section
-
-[wheredoivote]
-aws_access_key_id = xxxx
-aws_secret_access_key = xxxx
-
-in a /etc/boto.cfg or ~/.boto file, etc
-See: http://boto.cloudhackers.com/en/latest/boto_config_tut.html
-
-We can change the section name using the BOTO_SECTION setting
-"""
-
 import os
-
-BOTO_SECTION = "wheredoivote"
 
 if SERVER_ENVIRONMENT := os.environ.get("DC_ENVIRONMENT"):
     S3_DATA_BUCKET = f"pollingstations.elections.{SERVER_ENVIRONMENT}"
