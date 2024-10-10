e1eee39a5e9bea33fdd859e2f3ece7df2b70176c
chris48s <git@chris-shaw.dev>
2024-10-10 17:23:30 +0100

    import script for greenwich 2024-11-14

--- polling_stations/apps/data_importers/management/commands/import_greenwich.py
+++ polling_stations/apps/data_importers/management/commands/import_greenwich.py
@@ -4,19 +4,11 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "GRE"
     addresses_name = (
-        "2024-10-17/2024-09-18T09:48:05.132507/Democracy_Club__17October2024.CSV"
+        "2024-11-14/2024-10-10T15:30:40.316715/Democracy_Club__14November2024.tsv"
     )
     stations_name = (
-        "2024-10-17/2024-09-18T09:48:05.132507/Democracy_Club__17October2024.CSV"
+        "2024-11-14/2024-10-10T15:30:40.316715/Democracy_Club__14November2024.tsv"
     )
-    elections = ["2024-10-17"]
+    elections = ["2024-11-14"]
     csv_encoding = "windows-1252"
-
-    def address_record_to_dict(self, record):
-        if record.addressline6 in [
-            # splits
-            "SE9 2BU",
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
+    csv_delimiter = "\t"
