919509e4e86820d087e78736b5f65c022a48dd26
polling-bot-4000 <developers@democracyclub.org.uk>
2024-10-24 13:54:07 +0100

    Import script for Gosport (2024-11-21) (closes #8133)

--- polling_stations/apps/data_importers/management/commands/import_gosport.py
+++ polling_stations/apps/data_importers/management/commands/import_gosport.py
@@ -3,35 +3,21 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "GOS"
-    addresses_name = "2024-07-04/2024-05-31T12:38:18.136820/2024 PGE - Democracy Club Polling Districts v1 (31 05 2024).csv"
-    stations_name = "2024-07-04/2024-05-31T12:38:18.136820/2024 PGE - Democracy Club Polling Stations v1 (31 05 2024).csv"
-    elections = ["2024-07-04"]
+    addresses_name = "2024-11-21/2024-10-24T13:53:59.874871/2024 LGBE - Democracy Club Polling Districts v1.csv"
+    stations_name = "2024-11-21/2024-10-24T13:53:59.874871/2024 LGBE - Democracy Club Polling Stations v1.csv"
+    elections = ["2024-11-21"]
     csv_encoding = "utf-16le"
 
-    def station_record_to_dict(self, record):
-        # Fareham stations
-        if record.stationcode in (
-            "48ST1",  # Crofton Community Centre
-            "47HH4",  # Crofton Community Centre
-            "46HH3",  # Crofton Community Centre
-            "44HH1",  # Crofton Community Centre
-            "45HH2",  # Crofton Community Centre
-            "49ST2",  # Stubbington Methodist Church Hall
-            "50ST3",  # Stubbington Methodist Church Hall
-            "51ST4",  # Stubbington Baptist Church
-        ):
-            return None
+    # Preserving the postcode exclusions for reference during the next local elections
 
-        return super().station_record_to_dict(record)
+    # def address_record_to_dict(self, record):
+    #     if record.postcode in [
+    #         # suspect
+    #         "PO12 1SE",
+    #         "PO12 4AW",
+    #         "PO12 4QE",
+    #         "PO12 4JP",
+    #     ]:
+    #         return None
 
-    def address_record_to_dict(self, record):
-        if record.postcode in [
-            # suspect
-            "PO12 1SE",
-            "PO12 4AW",
-            "PO12 4QE",
-            "PO12 4JP",
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
+    #     return super().address_record_to_dict(record)
