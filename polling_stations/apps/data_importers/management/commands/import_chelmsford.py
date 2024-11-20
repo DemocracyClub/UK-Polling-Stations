f1f2dc04c4101bfe3087fcf3d5032636410c3c47
polling-bot-4000 <developers@democracyclub.org.uk>
2024-11-20 15:03:53 +0000

    Import script for Chelmsford (2024-12-12) (closes #8165)

--- polling_stations/apps/data_importers/management/commands/import_chelmsford.py
+++ polling_stations/apps/data_importers/management/commands/import_chelmsford.py
@@ -3,15 +3,19 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "CHL"
-    addresses_name = "2024-07-04/2024-06-04T09:01:23.444439/CHL_combined.tsv"
-    stations_name = "2024-07-04/2024-06-04T09:01:23.444439/CHL_combined.tsv"
-    elections = ["2024-07-04"]
+    addresses_name = (
+        "2024-12-12/2024-11-20T15:03:43.866922/Democracy_Club__12December2024.tsv"
+    )
+    stations_name = (
+        "2024-12-12/2024-11-20T15:03:43.866922/Democracy_Club__12December2024.tsv"
+    )
+    elections = ["2024-12-12"]
     csv_delimiter = "\t"
 
-    ## Ignoring the warnings RE the following stations
-    ## (Council has confirmed the current postcodes are correct):
-    ## 'East Hanningfield Village Hall, The Tye, East Hanningfield, Chelmsford, CM3 8AE' (id: 13209)
-    ## 'Civic Centre - Chelmsford City Council, Duke Street, Chelmsford, CM1 1JE' (id: 13420)
+    # Ignoring the warnings RE the following stations
+    # (Council has confirmed the current postcodes are correct):
+    # 'East Hanningfield Village Hall, The Tye, East Hanningfield, Chelmsford, CM3 8AE' (id: 13209)
+    # 'Civic Centre - Chelmsford City Council, Duke Street, Chelmsford, CM1 1JE' (id: 13420)
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
@@ -19,23 +23,28 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         if (
             uprn
             in [
-                "100091430409",  # BARNES MILL HOUSE, MILL VUE ROAD, CHELMSFORD
-                "10093928503",  # 67 BROOMFIELD ROAD, CHELMSFORD
-                "200004630041",  # 1 LIBERTY WAY, RUNWELL, WICKFORD
+                "100091430409",  # BASSMENT NIGHTCLUB, 16-18 WELLS STREET, CHELMSFORD
+                "200004630041",  # NEW BUNGALOW, TURKEY FARM, WINDSOR ROAD, DOWNHAM, BILLERICAY
                 "10093928503",  # HONEYSTONE, SOUTHEND ROAD, HOWE GREEN, CHELMSFORD
-                "200004627211",  # BARNES MILL HOUSE, MILL VUE ROAD, CHELMSFORD
-                "10093928515",  # CARAVAN 2 AT OAKVALE DOMSEY LANE, LITTLE WALTHAM, CHELMSFORD
             ]
         ):
             return None
 
-        if record.addressline6 in [
-            # split
-            "CM1 7AR",
-            "CM1 1FU",
-            "CM3 1ER",
-            "CM4 9JL",
+        # removing addresses for NPR
+        if record.polling_place_id in [
+            "14153",  # Danbury Chapel
+            "14039",  # Danbury Leisure Centre
         ]:
             return None
 
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # removing stations for NPR
+        if record.polling_place_id in [
+            "14153",  # Danbury Chapel
+            "14039",  # Danbury Leisure Centre
+        ]:
+            return None
+
+        return super().station_record_to_dict(record)
