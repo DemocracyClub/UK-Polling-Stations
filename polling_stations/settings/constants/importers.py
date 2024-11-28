291fefa73d87412deb950b9cf5695a3098b28b2a
GeoWill <roper.william@gmail.com>
2024-11-28 13:03:15 +0000

    Add constant for EONI import Scheme

--- polling_stations/settings/constants/importers.py
+++ polling_stations/settings/constants/importers.py
@@ -1,4 +1,6 @@
 import os
+from django.db import models
+
 
 if SERVER_ENVIRONMENT := os.environ.get("DC_ENVIRONMENT"):
     S3_DATA_BUCKET = f"pollingstations.elections.{SERVER_ENVIRONMENT}"
@@ -6,3 +8,11 @@ else:
     S3_DATA_BUCKET = os.environ.get(
         "S3_DATA_BUCKET", "pollingstations.elections.development"
     )
+
+
+class EONIImportScheme(models.TextChoices):
+    NATIONAL = ("NATIONAL", "National (NI Assembly and Westminster Elections)")
+    LOCAL = ("LOCAL", "Local (Council Elections)")
+
+
+EONI_IMPORT_SCHEME = EONIImportScheme.NATIONAL
