228c30fb7af8d1766a67f98ffd79c84cf26180f2
GeoWill <roper.william@gmail.com>
2024-12-18 11:40:19 +0000

    attempt to use bunding options for trigger
    This is pretty hacky, and basically involved fighting round permissions
    issues inside the lambda handler. I couldn't find much prior art apart
    from https://github.com/aws-samples/amazon-bedrock-serverless-prompt-chaining/blob/e583e865e8f140f93ff582e1ab17670feebdbf28/stacks/util.py#L18
    
    Not sure if this is the best way to go, or if we'd be better off calling
    uv export in CI, and documenting the fact you need to call 'uv export' if
    you want to run 'cdk synth' locally.
    
    we could even have scripts/cdk-synth.sh which created and removed the
    necessary requirements.txt.

--- deploy/files/scripts/install_uv.sh
+++ deploy/files/scripts/install_uv.sh
@@ -5,6 +5,10 @@ UV_CONSTRAINT=">=0.4.27,<0.5.0"
 
 if [ "$CI" = "true" ]; then
     pip install uv"$UV_CONSTRAINT"
+elif [ -v LAMBDA_TASK_ROOT ]; then
+    python -m venv /tmp/uv_venv
+    source /tmp/uv_venv/bin/activate
+    pip install uv"$UV_CONSTRAINT"
 else
     sudo PIP_BREAK_SYSTEM_PACKAGES=1 pip install uv"$UV_CONSTRAINT"
 fi
