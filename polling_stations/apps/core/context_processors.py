36503c3de379664fd3d90bef93f4a7d32c3022c1
chris48s <git@chris-shaw.dev>
2025-01-21 15:36:48 +0000

    update/fix footers for dc-django-utils v8

--- polling_stations/apps/core/context_processors.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-
-
-def canonical_url(request):
-    return {"CANONICAL_URL": f"{request.scheme}://{request.get_host()}"}
-
-
-def site_title(request):
-    return {"SITE_TITLE": settings.SITE_TITLE}
