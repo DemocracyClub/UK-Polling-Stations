1abd0d559cdc08141fc9101c094462590571857c
chris48s <git@chris-shaw.dev>
2025-01-28 15:13:42 +0000

    make DB router code easier to understand

--- polling_stations/db_routers.py
+++ polling_stations/db_routers.py
@@ -3,20 +3,27 @@ import os
 from django.conf import settings
 from django.db import DEFAULT_DB_ALIAS
 
+PRIMARY = settings.PRINCIPAL_DB_NAME
+REPLICA = DEFAULT_DB_ALIAS
+
 
 class ReplicationRouter(object):
     def db_for_read(self, model, **hints):
+        if os.environ.get("CIRCLECI"):
+            return DEFAULT_DB_ALIAS
+
         if model._meta.label in (
             "file_uploads.Upload",
             "file_uploads.File",
-        ) and not os.environ.get("CIRCLECI"):
-            return settings.PRINCIPAL_DB_NAME
-        return DEFAULT_DB_ALIAS
+        ):
+            return PRIMARY
+        return REPLICA
 
     def db_for_write(self, model, **hints):
         if os.environ.get("CIRCLECI"):
             return DEFAULT_DB_ALIAS
-        return settings.PRINCIPAL_DB_NAME
+
+        return PRIMARY
 
     def allow_relation(self, obj1, obj2, **hints):
         return True
@@ -26,6 +33,6 @@ class ReplicationRouter(object):
 
 
 def get_principal_db_name():
-    if settings.PRINCIPAL_DB_NAME in settings.DATABASES:
-        return settings.PRINCIPAL_DB_NAME
-    return DEFAULT_DB_ALIAS
+    if PRIMARY in settings.DATABASES:
+        return PRIMARY
+    return REPLICA
