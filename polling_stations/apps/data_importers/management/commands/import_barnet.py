e30e0051034a83e30d59971ff870536451c37d46
chris48s <git@chris-shaw.dev>
2025-02-11 16:15:38 +0000

    revert Barnet import script

--- polling_stations/apps/data_importers/management/commands/import_barnet.py
+++ polling_stations/apps/data_importers/management/commands/import_barnet.py
@@ -4,10 +4,10 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "BNE"
     addresses_name = (
-        "2025-03-06/2025-02-10T12:43:03.278468/Democracy Club - Polling Districts.csv"
+        "2025-02-13/2025-01-16T10:13:41.822386/Polling District data Burnt Oak.csv"
     )
     stations_name = (
-        "2025-03-06/2025-02-10T12:43:03.278468/Democracy Club - Polling Stations.csv"
+        "2025-02-13/2025-01-16T10:13:41.822386/Polling Station data Burnt Oak.csv"
     )
-    elections = ["2025-03-06"]
+    elections = ["2025-02-13"]
     csv_encoding = "utf-16le"
