2c7a4e39d140466d71b8b14849cb13b9fa599c7f
polling-bot-4000 <developers@democracyclub.org.uk>
2025-02-25 15:07:27 +0000

    Import script for North Warwickshire (2025-05-01) (closes #8262)

--- polling_stations/apps/data_importers/management/commands/import_north_warwickshire.py
+++ polling_stations/apps/data_importers/management/commands/import_north_warwickshire.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "NWA"
     addresses_name = (
-        "2024-07-04/2024-05-23T21:11:22.991963/Democracy_Club__04July2024.tsv"
+        "2025-05-01/2025-02-25T15:07:20.110575/Democracy_Club__01May2025.tsv"
     )
     stations_name = (
-        "2024-07-04/2024-05-23T21:11:22.991963/Democracy_Club__04July2024.tsv"
+        "2025-05-01/2025-02-25T15:07:20.110575/Democracy_Club__01May2025.tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2025-05-01"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -18,6 +18,8 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         if uprn in [
             "100071230930",  # I.S.I LTD, LEA COTTAGE NUNEATON ROAD, ANSLEY
             "200001812992",  # LEA LODGE, ANSLEY, NUNEATON
+            "100071519252",  # SLOWLEY HALL, TAMWORTH ROAD, FILLONGLEY, COVENTRY
+            "100071232116",  # OAKLEAVES, BIRCHLEY HEATH ROAD, BIRCHLEY HEATH, NUNEATON
         ]:
             return None
 
@@ -29,8 +31,14 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "CV9 3DW",
             "CV9 2PB",
             "CV10 0TB",
-            "B76 0DT",
         ]:
             return None
 
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # The following Warning was investigated and ignored:
+        # WARNING: Polling station Dosthill Boys Club (4864) is in Tamworth Borough Council (TAW)
+        # but target council is North Warwickshire Borough Council (NWA) - manual check recommended
+
+        return super().station_record_to_dict(record)
