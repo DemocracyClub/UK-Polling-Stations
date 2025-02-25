2dbd0cbef259afb2f845c72e35dee708af135491
polling-bot-4000 <developers@democracyclub.org.uk>
2025-02-25 14:26:33 +0000

    Import script for Pendle (2025-05-01) (closes #8258)

--- polling_stations/apps/data_importers/management/commands/import_pendle.py
+++ polling_stations/apps/data_importers/management/commands/import_pendle.py
@@ -4,33 +4,30 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "PEN"
     addresses_name = (
-        "2024-07-04/2024-05-24T16:52:43.349129/Democracy_Club__04July2024.CSV"
+        "2025-05-01/2025-02-25T14:26:26.747567/Democracy_Club__01May2025.tsv"
     )
     stations_name = (
-        "2024-07-04/2024-05-24T16:52:43.349129/Democracy_Club__04July2024.CSV"
+        "2025-05-01/2025-02-25T14:26:26.747567/Democracy_Club__01May2025.tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2025-05-01"]
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
             "10022950288",  # PEARL COTTAGE, ALMA ROAD, COLNE
-            "10090961710",  # FLAT, TOWER BUILDINGS 2A, KEIGHLEY ROAD, COLNE
             "61014294",  # 117 EDWARD STREET, NELSON
             "100012398597",  # PLANTATION COTTAGE, GREENHEAD LANE, FENCE, BURNLEY
             "10024181464",  # FOSTERS LEAP BARN, TRAWDEN, COLNE
             "10024181048",  # WHITEMOOR BOTTOM FARM, WHITEMOOR ROAD, FOULRIDGE, COLNE
             "100012400306",  # BROWN HOUSE FARM, GISBURN OLD ROAD, BLACKO, NELSON
-            "100012400306",  # BROWN HOUSE FARM, GISBURN OLD ROAD, BLACKO, NELSON
         ]:
             return None
 
         if record.addressline6 in [
-            # splits
-            "BB9 0RQ",
-            "BB9 7YS",  # REEDYFORD COTTAGE, SCOTLAND ROAD, NELSON
-            "BB8 7DN",  # GRASMERE CLOSE, COLNE
+            # suspect
+            "BB9 7YS",
         ]:
             return None
 
