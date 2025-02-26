94226ef3b60ee89ace887cc3d013ed88a740ac52
polling-bot-4000 <developers@democracyclub.org.uk>
2025-02-26 08:45:30 +0000

    Import script for St Albans (2025-05-01) (closes #8268)

--- polling_stations/apps/data_importers/management/commands/import_st_albans.py
+++ polling_stations/apps/data_importers/management/commands/import_st_albans.py
@@ -4,40 +4,31 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SAL"
     addresses_name = (
-        "2024-07-04/2024-05-27T15:28:11.936187/Democracy_Club__04July2024.tsv"
+        "2025-05-01/2025-02-26T08:45:19.492509/Democracy_Club__01May2025.tsv"
     )
     stations_name = (
-        "2024-07-04/2024-05-27T15:28:11.936187/Democracy_Club__04July2024.tsv"
+        "2025-05-01/2025-02-26T08:45:19.492509/Democracy_Club__01May2025.tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2025-05-01"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
-        if (
-            uprn
-            in [
-                "100081131978",  # PADDOCK VIEW, AYRES END LANE, CHILDWICKBURY, ST. ALBANS
-                "10024115888",  # FLAT AT ST ALBANS SCHOOL PAVILLION 162 HARPENDEN ROAD, ST ALBANS
-                "200003633693",  # WOOD END, HATCHING GREEN, HARPENDEN
-                "100081134635",  # PLAISTOWES FARM, NOKE LANE, ST. ALBANS
-            ]
-        ):
+        if uprn in [
+            "200003633693",  # WOOD END, HATCHING GREEN, HARPENDEN
+            "100081134635",  # PLAISTOWES FARM, NOKE LANE, ST. ALBANS
+        ]:
             return None
 
         if record.addressline6 in [
             # splits
-            "AL3 5PR",
             "AL4 0LD",
             "AL1 3NS",
-            "AL4 0TP",
+            "AL3 5PR",
             "AL3 5PD",
             # suspect
-            "AL5 1AB",
-            "AL5 1AA",
             "AL1 5NT",
-            "AL5 2AB",
         ]:
             return None
 
