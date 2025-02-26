d868fd726a22793247cc851ea98c73cc54f52f2e
polling-bot-4000 <developers@democracyclub.org.uk>
2025-02-26 16:59:21 +0000

    Import script for City of Lincoln (2025-05-01) (closes #8278)

--- polling_stations/apps/data_importers/management/commands/import_lincoln_city.py
+++ polling_stations/apps/data_importers/management/commands/import_lincoln_city.py
@@ -3,9 +3,14 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "LIC"
-    addresses_name = "2024-07-04/2024-05-31T11:04:59.933101/Eros_SQL_Output013.csv"
-    stations_name = "2024-07-04/2024-05-31T11:04:59.933101/Eros_SQL_Output013.csv"
-    elections = ["2024-07-04"]
+    addresses_name = (
+        "2025-05-01/2025-02-26T16:59:12.325060/SupportSql_20250226_132609.tsv"
+    )
+    stations_name = (
+        "2025-05-01/2025-02-26T16:59:12.325060/SupportSql_20250226_132609.tsv"
+    )
+    elections = ["2025-05-01"]
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
@@ -17,18 +22,17 @@ class Command(BaseHalaroseCsvImporter):
 
         if record.housepostcode in [
             # split
-            "LN1 1BU",
-            "LN1 1DR",
-            "LN1 1QD",
-            "LN2 4DY",
-            "LN2 4NA",
-            "LN2 5EJ",
-            "LN5 7LA",
             "LN5 8AG",
-            "LN6 0LH",
-            "LN6 7UY",
-            "LN6 8AZ",
             "LN6 8DB",
+            "LN1 1BU",
+            "LN2 4DY",
+            "LN1 1QD",
+            "LN5 7LA",
+            "LN2 4NA",
+            "LN6 0LH",
+            "LN2 5EJ",
+            "LN1 1DR",
+            "LN6 8AZ",
         ]:
             return None
 
@@ -36,7 +40,7 @@ class Command(BaseHalaroseCsvImporter):
 
     def station_record_to_dict(self, record):
         # removes wrong UPRN for: St Peter in Eastgate Church, Eastgate, Lincoln, LN2 4AA
-        if record.pollingvenueid == "52":
+        if self.get_station_hash(record) == "13-st-peter-in-eastgate-church":
             record = record._replace(pollingvenueuprn="")
 
         return super().station_record_to_dict(record)
