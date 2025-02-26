e14b2c3e354146b56a3f82de3ef1d42f3c34fe30
polling-bot-4000 <developers@democracyclub.org.uk>
2025-02-26 09:47:21 +0000

    Import script for Bolsover (2025-05-01) (closes #8270)

--- polling_stations/apps/data_importers/management/commands/import_bolsover.py
+++ polling_stations/apps/data_importers/management/commands/import_bolsover.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "BOS"
     addresses_name = (
-        "2024-07-04/2024-05-31T15:19:45.804642/Democracy_Club__04July2024 (10).tsv"
+        "2025-05-01/2025-02-26T09:47:14.141094/Democracy_Club__01May2025.tsv"
     )
     stations_name = (
-        "2024-07-04/2024-05-31T15:19:45.804642/Democracy_Club__04July2024 (10).tsv"
+        "2025-05-01/2025-02-26T09:47:14.141094/Democracy_Club__01May2025.tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2025-05-01"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -25,29 +25,22 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         ]:
             return None
 
-        if record.addressline6 in [
-            # suspect
-            "NG20 9BF",  # Primrose Way
-            "NG20 9AQ",  # Primrose Way
-            "NG20 9AL",  # Primrose Way
-        ]:
-            return None
         return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
         # Mobile Unit, Whaley Common Adjacent Henton Memorial Hall, Whaley Common, Langwith, Mansfield
-        if record.polling_place_id == "5816":
+        if record.polling_place_id == "6400":
             record = record._replace(polling_place_postcode="NG20 9HU")
 
         # Whaley Thorns and Langwith Village Hall, Portland Road, Langwith, Mansfield, NG20 9EZ
-        if record.polling_place_id == "5846":
+        if record.polling_place_id == "6295":
             record = record._replace(
                 polling_place_easting="453287",
                 polling_place_northing="371077",
             )
 
         # Bolsover Parish Rooms, Hornscroft Road, Bolsover, Chesterfield, S44 6HG
-        if record.polling_place_id == "5812":
+        if record.polling_place_id == "6392":
             record = record._replace(
                 polling_place_easting="447491",
                 polling_place_northing="370263",
