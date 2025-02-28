1f60175c8132369cec45eee51c5b14577c9b3cd9
polling-bot-4000 <developers@democracyclub.org.uk>
2025-02-28 12:20:58 +0000

    Import script for Stevenage (2025-05-01) (closes #8302)

--- polling_stations/apps/data_importers/management/commands/import_stevenage.py
+++ polling_stations/apps/data_importers/management/commands/import_stevenage.py
@@ -3,24 +3,7 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "STV"
-    addresses_name = "2024-07-04/2024-06-11T12:21:03.880800/Democracy Club - Polling Districts UTF-8.csv"
-    stations_name = (
-        "2024-07-04/2024-06-11T12:21:03.880800/Democracy Club - Polling Stations.csv"
-    )
-    elections = ["2024-07-04"]
-
-    def station_record_to_dict(self, record):
-        # The following stations are not in the council are and have no addresses associated:
-        if (
-            record.stationcode
-            in [
-                "ADW1-46",  # ASTON VILLAGE HALL, 9 NEW PARK LANE, ASTON, HERTS
-                "ADW2-47",  # DATCHWORTH VILLAGE HALL, THE GREEN, DATCHWORTH
-                "S-KN-1-43",  # KNEBWORTH VILLAGE HALL, PARK LANE, KNEBWORTH
-                "S-KN-2-44",  # KNEBWORTH VILLAGE HALL, PARK LANE, KNEBWORTH
-                "S-KN-CDE-45",  # JOHN CLEMENTS SPORTS & COMMUNITY CENTRE, BURY LANE, CODICOTE, HITCHIN
-                "S-CK-CVL-42",  # PEACE MEMORIAL HALL, HIGH STREET, CODICOTE, HITCHIN
-            ]
-        ):
-            return None
-        return super().station_record_to_dict(record)
+    addresses_name = "2025-05-01/2025-02-28T12:20:51.689422/Democracy Club - Polling Districts 1 May 2025.csv"
+    stations_name = "2025-05-01/2025-02-28T12:20:51.689422/Democracy Club - Polling Stations 1 May 2025.csv"
+    elections = ["2025-05-01"]
+    csv_encoding = "utf-16le"
