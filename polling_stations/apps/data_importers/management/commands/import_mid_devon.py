2b23ba78d62ccd7c1ed551c202d3a5253f3ae5cb
polling-bot-4000 <developers@democracyclub.org.uk>
2025-02-28 12:05:49 +0000

    Import script for Mid Devon (2025-05-01) (closes #8300)

--- polling_stations/apps/data_importers/management/commands/import_mid_devon.py
+++ polling_stations/apps/data_importers/management/commands/import_mid_devon.py
@@ -4,17 +4,17 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "MDE"
     addresses_name = (
-        "2024-07-04/2024-05-28T14:15:09.339060/Democracy_Club__04July2024 MID DEVON.tsv"
+        "2025-05-01/2025-02-28T12:05:41.703963/Democracy_Club__01May2025 MID DEVON.tsv"
     )
     stations_name = (
-        "2024-07-04/2024-05-28T14:15:09.339060/Democracy_Club__04July2024 MID DEVON.tsv"
+        "2025-05-01/2025-02-28T12:05:41.703963/Democracy_Club__01May2025 MID DEVON.tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2025-05-01"]
     csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
         # Chawleigh Jubilee Hall, Chawleigh
-        if record.polling_place_id == "11714":
+        if record.polling_place_id == "12927":
             record = record._replace(
                 polling_place_easting="",
                 polling_place_northing="",
@@ -28,13 +28,11 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         if (
             uprn
             in [
-                "10024639781",  # FOXHOLES FARM, CLAYHIDON, CULLOMPTON
                 "10002162361",  # POTTERS, SHILLINGFORD, TIVERTON
                 "200004004367",  # CARAVANS 3 AND 4 THREE ACRES UFFCULME ROAD, WILLAND
                 "200004004366",  # CARAVANS 1 AND 2 THREE ACRES UFFCULME ROAD, WILLAND
                 "10009448630",  # MAYFIELD HOUSE, TEMPLETON, TIVERTON
                 "10002165647",  # POUND CASTLE, BICKLEIGH, TIVERTON
-                "10095581422",  # OAKLEIGH BARN, BICKLEIGH, TIVERTON
                 "200004005422",  # GUNSTONE LODGE GUNSTONE PARK ROAD FROM GUNSTONE CROSS TO GUNSTONE MILL CROSS, GUNSTONE
                 "200004006381",  # GUNSTONE PARK, GUNSTONE, CREDITON
                 "10093538401",  # COPSTONE HOUSE, BLACK DOG, CREDITON
