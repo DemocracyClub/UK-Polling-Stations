2b7e63f9fcb5758e851d92bb8f0107a5a1cd84ba
polling-bot-4000 <developers@democracyclub.org.uk>
2025-02-28 13:08:56 +0000

    Import script for Nuneaton and Bedworth (2025-05-01) (closes #8307)

--- polling_stations/apps/data_importers/management/commands/import_nuneaton_and_bedworth.py
+++ polling_stations/apps/data_importers/management/commands/import_nuneaton_and_bedworth.py
@@ -4,26 +4,34 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "NUN"
     addresses_name = (
-        "2024-07-04/2024-06-01T22:49:27.542755/Democracy_Club__04July2024.tsv"
+        "2025-05-01/2025-02-28T13:08:50.423474/Democracy_Club__01May2025.tsv"
     )
     stations_name = (
-        "2024-07-04/2024-06-01T22:49:27.542755/Democracy_Club__04July2024.tsv"
+        "2025-05-01/2025-02-28T13:08:50.423474/Democracy_Club__01May2025.tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2025-05-01"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         rec = super().address_record_to_dict(record)
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "100071496605",  # BRAMCOTE FIELDS FARM, LUTTERWORTH ROAD, BRAMCOTE, NUNEATON
+            "100071235227",  # MEADOW FARM, LUTTERWORTH ROAD, BRAMCOTE, NUNEATON
+        ]:
+            return None
 
         if record.addressline6 in [
             # split
-            "CV11 6JF",
             "CV11 4NW",
+            "CV11 6JF",
             "CV10 9QF",
-            "CV11 6JE",
             "CV11 6NL",
+            "CV11 6JE",
             # suspect
             "CV12 9HJ",
+            "CV10 0PL",
         ]:
             return None
 
