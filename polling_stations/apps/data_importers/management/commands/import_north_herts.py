aa32e6d57de3db9fc0c566d4fa7de32d990f7dff
polling-bot-4000 <developers@democracyclub.org.uk>
2025-02-28 15:19:58 +0000

    Import script for North Hertfordshire (2025-05-01) (closes #8309)

--- polling_stations/apps/data_importers/management/commands/import_north_herts.py
+++ polling_stations/apps/data_importers/management/commands/import_north_herts.py
@@ -4,32 +4,19 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "NHE"
     addresses_name = (
-        "2024-07-04/2024-05-29T12:03:29.474027/Democracy_Club__04July2024.tsv"
+        "2025-05-01/2025-02-28T15:19:33.716963/Democracy_Club__01May2025.CSV"
     )
     stations_name = (
-        "2024-07-04/2024-05-29T12:03:29.474027/Democracy_Club__04July2024.tsv"
+        "2025-05-01/2025-02-28T15:19:33.716963/Democracy_Club__01May2025.CSV"
     )
-    elections = ["2024-07-04"]
-    csv_delimiter = "\t"
+    elections = ["2025-05-01"]
+    csv_encoding = "windows-1252"
 
     def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "10070037178",  # LARK RISE, REDHILL, RUSHDEN, BUNTINGFORD
-            "10023318781",  # OLD WESTMILL FARMHOUSE, WESTMILL LANE, ICKLEFORD, HITCHIN
-            "10070041819",  # 2 RYE END COTTAGES, CODICOTE, HITCHIN
-            "10070041818",  # 1 RYE END COTTAGES, CODICOTE, HITCHIN
-            "10023322537",  # DUCKS NEST COTTAGE, NEWSELLS PARK STUD, BARKWAY, ROYSTON
-            "10023322541",  # GRANGE COTTAGE, NEWSELLS PARK STUD, BARKWAY, ROYSTON
-        ]:
-            return None
-
         if record.addressline6 in [
             # splits
             "SG8 8AD",
             # looks wrong
-            "LU2 8NH",
             "SG5 1ET",
         ]:
             return None
