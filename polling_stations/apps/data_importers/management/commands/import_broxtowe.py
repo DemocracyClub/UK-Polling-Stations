fac1c76e30963d3b154d82ce5ac96701e40febbc
polling-bot-4000 <developers@democracyclub.org.uk>
2025-03-04 15:02:38 +0000

    Import script for Broxtowe (2025-05-01) (closes #8341)

--- polling_stations/apps/data_importers/management/commands/import_broxtowe.py
+++ polling_stations/apps/data_importers/management/commands/import_broxtowe.py
@@ -3,9 +3,9 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "BRT"
-    addresses_name = "2024-07-04/2024-05-30T14:47:06.249789/Eros_SQL_Output002.csv"
-    stations_name = "2024-07-04/2024-05-30T14:47:06.249789/Eros_SQL_Output002.csv"
-    elections = ["2024-07-04"]
+    addresses_name = "2025-05-01/2025-03-04T15:02:27.653222/Eros_SQL_Output003.csv"
+    stations_name = "2025-05-01/2025-03-04T15:02:27.653222/Eros_SQL_Output003.csv"
+    elections = ["2025-05-01"]
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
@@ -15,6 +15,7 @@ class Command(BaseHalaroseCsvImporter):
             "10013966491",  # 34 TRENT ROAD, BEESTON, NOTTINGHAM
             "10003431083",  # 5A TRENT ROAD, BEESTON, NOTTINGHAM
             "10013964473",  # 44A MEADOW ROAD, BEESTON, NOTTINGHAM
+            "100031336319",  # 37 SCARGILL AVENUE, NEWTHORPE, NOTTINGHAM
         ]:
             return None
 
