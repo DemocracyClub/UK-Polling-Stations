b4e5e022ca10e24dacb6e159f6ce462d6c012fa0
polling-bot-4000 <developers@democracyclub.org.uk>
2025-03-05 16:29:08 +0000

    Import script for Wychavon (2025-05-01) (closes #8368)

--- polling_stations/apps/data_importers/management/commands/import_wychavon.py
+++ polling_stations/apps/data_importers/management/commands/import_wychavon.py
@@ -5,18 +5,18 @@ from django.contrib.gis.geos import Point
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WYC"
     addresses_name = (
-        "2024-07-04/2024-06-03T17:16:49.585397/Democracy_Club__04July2024 (15).tsv"
+        "2025-05-01/2025-03-05T16:28:55.760571/Democracy_Club__01May2025.tsv"
     )
     stations_name = (
-        "2024-07-04/2024-06-03T17:16:49.585397/Democracy_Club__04July2024 (15).tsv"
+        "2025-05-01/2025-03-05T16:28:55.760571/Democracy_Club__01May2025.tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2025-05-01"]
     csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
         # bugreport #645
         # coords correction for: Droitwich Spa Community (Main) Hall, Heritage Way, DROITWICH SPA, WR9 8RF
-        if record.polling_place_id == "8392":
+        if record.polling_place_id == "9361":
             record = record._replace(
                 polling_place_easting="389734",
                 polling_place_northing="263143",
@@ -24,13 +24,13 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
         # user issue report #197
         # Northwick Arms Hotel (Monroe Suite), Waterside, Evesham
-        if record.polling_place_id == "8364":
+        if record.polling_place_id == "9347":
             rec = super().station_record_to_dict(record)
             rec["location"] = Point(-1.942675, 52.090313, srid=4326)
             return rec
         # Coord correction to fix map for council:
         # Woodland View Care Home, Woodland Way, DROITWICH SPA
-        if record.polling_place_id == "8409":
+        if record.polling_place_id == "9365":
             record = record._replace(
                 polling_place_easting="390412",
                 polling_place_northing="261303",
@@ -44,19 +44,17 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         if uprn in [
             "100120715029",  # ORCHARD COTTAGE EARLS COMMON ROAD, STOCK GREEN
             "100121276049",  # GLENFIELD HOUSE, WORCESTER ROAD, UPTON WARREN, BROMSGROVE
-            "10013939808",  # COMPOST CORNER, KNOWLE HILL, EVESHAM
-            "100121281842",  # PRIORY REST HOME, CRUTCH LANE, ELMBRIDGE, DROITWICH
-            "100120708134",  # HALCYON, HANBURY ROAD, DROITWICH
         ]:
             return None
 
         if record.addressline6 in [
             # split
-            "WR11 7UQ",
             "WR11 8PZ",
-            "WR11 7UQ",
-            "WR10 3HG",
+            "WR9 8PR",
             "WR7 4PB",
+            "WR10 3HG",
+            # suspect
+            "WR9 7JJ",
         ]:
             return None
 
