b1a55eee59ed8c65732dcfc1b909ca1d9310bcb1
polling-bot-4000 <developers@democracyclub.org.uk>
2025-03-05 12:03:27 +0000

    Import script for Bromsgrove (2025-05-01) (closes #8355)

--- polling_stations/apps/data_importers/management/commands/import_bromsgrove.py
+++ polling_stations/apps/data_importers/management/commands/import_bromsgrove.py
@@ -3,25 +3,21 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "BRM"
-    addresses_name = "2024-07-04/2024-06-06T10:34:34.598327/Bromsgrove Democracy_Club__04July2024.CSV"
-    stations_name = "2024-07-04/2024-06-06T10:34:34.598327/Bromsgrove Democracy_Club__04July2024.CSV"
-    elections = ["2024-07-04"]
+    addresses_name = (
+        "2025-05-01/2025-03-05T12:03:17.688205/Democracy_Club__01May2025 Bromsgrove.CSV"
+    )
+    stations_name = (
+        "2025-05-01/2025-03-05T12:03:17.688205/Democracy_Club__01May2025 Bromsgrove.CSV"
+    )
+    elections = ["2025-05-01"]
 
     def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "100120560854",  # 34 GUNNER LANE, RUBERY, REDNAL, BIRMINGHAM
-        ]:
-            return None
-
         if record.addressline6 in [
             # splits
-            "B60 3AZ",
-            "B61 0NX",
             "B48 7BS",
+            "B61 0NX",
+            "B60 3AZ",
             "B45 8HY",
-            "DY9 9XT",
             "B61 7AY",
         ]:
             return None
@@ -33,13 +29,13 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         # WARNING: Polling station Broadmeadow Junior School (8390) is in Birmingham City Council (BIR)
 
         # adding point for: Catshill Village Hall, Golden Cross Lane, Catshill, B61 0JZ
-        if record.polling_place_id == "8401":
+        if record.polling_place_id == "8985":
             record = record._replace(
                 polling_place_easting="396119", polling_place_northing="274035"
             )
 
         # adding point for: Beacon Suite at The Old Rose and Crown Hotel, Rose Hill, Lickey, B45 8RT
-        if record.polling_place_id == "8405":
+        if record.polling_place_id == "8989":
             record = record._replace(
                 polling_place_easting="399517", polling_place_northing="275838"
             )
