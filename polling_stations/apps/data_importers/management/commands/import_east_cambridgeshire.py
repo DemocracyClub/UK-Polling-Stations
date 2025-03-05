1ce52cc41d10429de5f1a6ccfc2e43904cd32b96
polling-bot-4000 <developers@democracyclub.org.uk>
2025-03-05 14:07:52 +0000

    Import script for East Cambridgeshire (2025-05-01) (closes #8364)

--- polling_stations/apps/data_importers/management/commands/import_east_cambridgeshire.py
+++ polling_stations/apps/data_importers/management/commands/import_east_cambridgeshire.py
@@ -3,13 +3,9 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "ECA"
-    addresses_name = (
-        "2024-07-04/2024-06-14T16:11:50.619064/Democracy Club - Polling Districts.csv"
-    )
-    stations_name = (
-        "2024-07-04/2024-06-14T16:11:50.619064/Democracy Club - Polling Stations.csv"
-    )
-    elections = ["2024-07-04"]
+    addresses_name = "2025-05-01/2025-03-31T08:58:46.747046/Democracy Club - Polling Districts 2025.csv"
+    stations_name = "2025-05-01/2025-03-31T08:58:46.747046/Democracy Club - Polling Stations 2025.csv"
+    elections = ["2025-05-01"]
     csv_encoding = "utf-16le"
 
     def address_record_to_dict(self, record):
@@ -22,22 +18,3 @@ class Command(BaseDemocracyCountsCsvImporter):
             return None
 
         return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # Below warnings checked and no correction needed:
-        # Polling station NEWMARKET TOWN FOOTBALL CLUB - CHEVELEY (MD1) is in West Suffolk Council (WSK)
-        # Polling station NEWMARKET TOWN FOOTBALL CLUB - WOODDITTON (MJ1) is in West Suffolk Council (WSK)
-
-        # Remove stations from SCA council
-        if record.stationcode in [
-            "OB1",
-            "OC1",
-            "OC1,OC2",
-            "OD1/1",
-            "OD2",
-            "SA1/1",
-            "SC1",
-        ]:
-            return None
-
-        return super().station_record_to_dict(record)
