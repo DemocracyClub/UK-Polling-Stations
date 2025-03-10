e96d8998973fdf6d0eeba87d3992e58bc65e86f4
polling-bot-4000 <developers@democracyclub.org.uk>
2025-03-10 16:09:28 +0000

    Import script for Blaby (2025-05-01) (closes #8397)

--- polling_stations/apps/data_importers/management/commands/import_blaby.py
+++ polling_stations/apps/data_importers/management/commands/import_blaby.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "BLA"
     addresses_name = (
-        "2024-07-04/2024-05-27T15:58:42.972617/Democracy_Club__04July2024.tsv"
+        "2025-05-01/2025-03-10T16:05:45.756880/Democracy_Club__01May2025.tsv"
     )
     stations_name = (
-        "2024-07-04/2024-05-27T15:58:42.972617/Democracy_Club__04July2024.tsv"
+        "2025-05-01/2025-03-10T16:05:45.756880/Democracy_Club__01May2025.tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2025-05-01"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -22,7 +22,6 @@ class Command(BaseXpressDemocracyClubCsvImporter):
                 "100030407216",  # 2 WARWICK ROAD, WHETSTONE, LEICESTER
                 "100030407215",  # 4 WARWICK ROAD, WHETSTONE, LEICESTER
                 "10001236400",  # PITCH 11 LAND TO SOUTH EAST OF WHITE GATE STABLES HINCKLEY ROAD, SAPCOTE
-                "10001225839",  # GREEN ACRES, HOSPITAL LANE, BLABY, LEICESTER
             ]
         ):
             return None
@@ -41,9 +40,22 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         # WARNING: Polling station Riverside Football Pavilion (4314) is in Leicester City Council (LCE)
 
         # Millfield Community Nursery, Millfield L.E.A.D. Academy, Hat Road, Braunstone Town, Leicester, LE3 2WF
-        if record.polling_place_id == "4379":
+        if record.polling_place_id == "4784":
             record = record._replace(
                 polling_place_easting="455360", polling_place_northing="301508"
             )
 
+        # The following stations don't have coordinate data in the council data, but they are located at a polling place
+        # with the multiple stations, so we can use the coordinates of the other station at the polling place.
+
+        # Whetstone Memorial Hall - Station 2 High Street Whetstone Leicester
+        if record.polling_place_id == "4791":
+            record = record._replace(
+                polling_place_easting="455703", polling_place_northing="297428"
+            )
+        # Kirby Muxloe Village Hall - Station 2 Station Road Kirby Muxloe
+        if record.polling_place_id == "4794":
+            record = record._replace(
+                polling_place_easting="452030", polling_place_northing="303926"
+            )
         return super().station_record_to_dict(record)
