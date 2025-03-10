37de2368ebbb411d045cbea1d5eb51c62e1c2d78
polling-bot-4000 <developers@democracyclub.org.uk>
2025-03-10 10:23:46 +0000

    Import script for Mansfield (2025-05-01) (closes #8389)

--- polling_stations/apps/data_importers/management/commands/import_mansfield.py
+++ polling_stations/apps/data_importers/management/commands/import_mansfield.py
@@ -1,29 +1,33 @@
-from data_importers.management.commands import BaseHalaroseCsvImporter
+from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
-class Command(BaseHalaroseCsvImporter):
+class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "MAS"
-    addresses_name = "2024-07-04/2024-06-04T15:33:34.031516/Eros_SQL_Output006 (2).csv"
-    stations_name = "2024-07-04/2024-06-04T15:33:34.031516/Eros_SQL_Output006 (2).csv"
-    elections = ["2024-07-04"]
+    addresses_name = (
+        "2025-05-01/2025-03-10T10:23:31.659610/Democracy_Club__01May2025.CSV"
+    )
+    stations_name = (
+        "2025-05-01/2025-03-10T10:23:31.659610/Democracy_Club__01May2025.CSV"
+    )
+    elections = ["2025-05-01"]
 
     def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
+        uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
             "10091487792",  # 65 CROMWELL STREET, MANSFIELD
             "10023935274",  # 31 BIRCHLANDS, FOREST TOWN, MANSFIELD
             "10096388538",  # 72 BIFROST BOULEVARD, WARSOP
             "10096388523",  # 17 KEMPTON ROAD, MANSFIELD
-            "100031396502",  # 66 CHATSWORTH DRIVE, MANSFIELD
         ]:
             return None
 
-        if record.housepostcode in [
+        if record.addressline6 in [
             # splits
             "NG19 6AT",
             # looks wrong
             "NG18 4TG",
+            # Council confirmed correct: NG18 6BH
         ]:
             return None
 
