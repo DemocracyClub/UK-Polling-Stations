fdee2f13fc835c2dd4853d251b40f8fbcf911a22
polling-bot-4000 <developers@democracyclub.org.uk>
2025-03-10 10:49:41 +0000

    Import script for Gedling (2025-05-01) (closes #8391)

--- polling_stations/apps/data_importers/management/commands/import_gedling.py
+++ polling_stations/apps/data_importers/management/commands/import_gedling.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "GED"
     addresses_name = (
-        "2024-07-04/2024-05-28T16:17:42.794080/Democracy_Club__04July2024 (2).tsv"
+        "2025-05-01/2025-03-10T10:49:30.103808/Democracy_Club__01May2025 (3).tsv"
     )
     stations_name = (
-        "2024-07-04/2024-05-28T16:17:42.794080/Democracy_Club__04July2024 (2).tsv"
+        "2025-05-01/2025-03-10T10:49:30.103808/Democracy_Club__01May2025 (3).tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2025-05-01"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -28,35 +28,22 @@ class Command(BaseXpressDemocracyClubCsvImporter):
                 "10035282319",  # PENRHYN, MILL LANE, LAMBLEY, NOTTINGHAM
                 "10035286332",  # 4 MILL LANE, LAMBLEY, NOTTINGHAM
                 "100032276589",  # 163 STANDHILL ROAD, CARLTON, NOTTINGHAM
-                "100032123942",  # 127 SPRING LANE, LAMBLEY, NOTTINGHAM
                 "100031349971",  # E W WOOD BUILDERS, MANSFIELD HOUSE, 148 BURTON ROAD, GEDLING, NOTTINGHAM
-                "100031349428",  # BROOKWOOD INSTALATIONS, 8A BROOKWOOD CRESCENT, CARLTON, NOTTINGHAM
-                "100031350956",  # 216 CARLTON HILL, CARLTON, NOTTINGHAM
-                "100031363324",  # 3 HASTINGS STREET, CARLTON, NOTTINGHAM
-                "100031363326",  # 5 HASTINGS STREET, CARLTON, NOTTINGHAM
                 "10035282383",  # 34 SUNRISE AVENUE, KILLARNEY PARK, NOTTINGHAM
                 "100032276572",  # RESPECT FOR ANIMALS, 30 STATION ROAD, CARLTON, NOTTINGHAM
                 "100031382797",  # FLAT 26-28 STATION ROAD, CARLTON
-                "10035286081",  # 351B WESTDALE LANE, MAPPERLEY, NOTTINGHAM
-                "200001152414",  # 361 WESTDALE LANE, MAPPERLEY, NOTTINGHAM
-                "200001152416",  # 363 WESTDALE LANE, MAPPERLEY, NOTTINGHAM
-                "10002888131",  # WESTDALE PHARMACY, 354 WESTDALE LANE, MAPPERLEY, NOTTINGHAM
-                "100031382749",  # 38A STATION ROAD, CARLTON, NOTTINGHAM
-                "100032126239",  # LEVERTONS, LODGE FARM, LIME LANE, ARNOLD, NOTTINGHAM
-                "100032126019",  # FOREST FARM, MANSFIELD ROAD, ARNOLD, NOTTINGHAM
             ]
         ):
             return None
 
         if record.addressline6 in [
             # splits
-            "NG4 2DX",
-            "NG5 8AH",
-            "NG3 6BN",
             "NG5 8AU",
             "NG4 4FN",
+            "NG5 8AH",
+            "NG3 6BN",
+            "NG4 2DX",
             # looks wrong
-            "NG14 6TN",
             "NG14 6RU",
         ]:
             return None
