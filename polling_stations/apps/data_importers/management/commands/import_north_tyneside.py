0068029887e6311d1a02890dff8ef02b1c960e48
polling-bot-4000 <developers@democracyclub.org.uk>
2025-03-10 17:32:31 +0000

    Import script for North Tyneside (2025-05-01) (closes #8402)

--- polling_stations/apps/data_importers/management/commands/import_north_tyneside.py
+++ polling_stations/apps/data_importers/management/commands/import_north_tyneside.py
@@ -3,44 +3,35 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "NTY"
-    addresses_name = "2024-07-04/2024-06-14T14:09:24.864672/North Tyneside Council.tsv"
-    stations_name = "2024-07-04/2024-06-14T14:09:24.864672/North Tyneside Council.tsv"
-    elections = ["2024-07-04"]
+    addresses_name = (
+        "2025-05-01/2025-03-10T17:28:20.212349/Democracy_Club__01May2025.tsv"
+    )
+    stations_name = (
+        "2025-05-01/2025-03-10T17:28:20.212349/Democracy_Club__01May2025.tsv"
+    )
+    elections = ["2025-05-01"]
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
-        if (
-            uprn
-            in [
-                "47243589",  # 14 BEECH WAY, KILLINGWORTH
-                "47070238",  # EAST WIDEOPEN FARM COTTAGE, WIDEOPEN, NEWCASTLE UPON TYNE
-                "47241411",  # OAK HOUSE, WIDEOPEN, NEWCASTLE UPON TYNE
-                "47072178",  # FLAT ABOVE THE FOXHUNTERS PUBLIC HOUSE PRESTON NORTH ROAD, NORTH SHIELDS
-                "47072124",  # CRICKET CLUB COTTAGE, PRESTON AVENUE, NORTH SHIELDS
-                "47014289",  # FLAT ABOVE LOW LIGHTS TAVERN BREWHOUSE BANK, NORTH SHIELDS
-                "47049172",  # 83 HOWDON ROAD, NORTH SHIELDS
-                "47049170",  # 81 HOWDON ROAD, NORTH SHIELDS
-                "47049168",  # 79 HOWDON ROAD, NORTH SHIELDS
-                "47084844",  # FLAT ABOVE STATION HOTEL STATION ROAD, KILLINGWORTH
-                "47229242",  # REDESDALE COURT, RAKE LANE, NORTH SHIELDS
-                "47072123",  # PERCY PARK BUNGALOW, PRESTON AVENUE, NORTH SHIELDS
-            ]
-        ):
+        if uprn in [
+            "47243589",  # 14 BEECH WAY, KILLINGWORTH
+            "47070238",  # EAST WIDEOPEN FARM COTTAGE, WIDEOPEN, NEWCASTLE UPON TYNE
+            "47014289",  # FLAT ABOVE LOW LIGHTS TAVERN BREWHOUSE BANK, NORTH SHIELDS
+            "47049172",  # 83 HOWDON ROAD, NORTH SHIELDS
+            "47049170",  # 81 HOWDON ROAD, NORTH SHIELDS
+            "47049168",  # 79 HOWDON ROAD, NORTH SHIELDS
+            "47229242",  # REDESDALE COURT, RAKE LANE, NORTH SHIELDS
+        ]:
             return None
 
         if record.addressline6 in [
             # splits
-            "NE28 9GF",
             "NE12 8EE",
-            # looks wrong
-            "NE26 1LX",
-            "NE28 9UR",
-            "NE28 9UT",
-            "NE13 6EN",
-            "NE30 2ET",
+            # suspect
+            "NE28 8AQ",
         ]:
             return None
 
