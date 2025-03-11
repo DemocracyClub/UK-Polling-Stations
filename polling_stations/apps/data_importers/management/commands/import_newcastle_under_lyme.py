b7b5b6d205aea6a702c2df00a65537acc7001ed2
polling-bot-4000 <developers@democracyclub.org.uk>
2025-03-11 11:45:26 +0000

    Import script for Newcastle Under Lyme (2025-05-01) (closes #8406)

--- polling_stations/apps/data_importers/management/commands/import_newcastle_under_lyme.py
+++ polling_stations/apps/data_importers/management/commands/import_newcastle_under_lyme.py
@@ -4,17 +4,17 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "NEC"
     addresses_name = (
-        "2024-07-04/2024-05-29T09:09:41.048776/Democracy_Club__04July2024.tsv"
+        "2025-05-01/2025-03-11T11:45:15.531010/Democracy_Club__01May2025.tsv"
     )
     stations_name = (
-        "2024-07-04/2024-05-29T09:09:41.048776/Democracy_Club__04July2024.tsv"
+        "2025-05-01/2025-03-11T11:45:15.531010/Democracy_Club__01May2025.tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2025-05-01"]
     csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
         # Higherland Methodist Church Hall Higherland Newcastle Staffs ST5 2TF
-        if record.polling_place_id == "3666":
+        if record.polling_place_id == "4070":
             record = record._replace(polling_place_easting="384574")
             record = record._replace(polling_place_northing="345703")
 
@@ -25,23 +25,17 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
         if uprn in [
             "200002872601",  # 125A LIVERPOOL ROAD, NEWCASTLE
-            "100032216255",  # CROSS HEATH NEWS, 121 LIVERPOOL ROAD, NEWCASTLE
-            "10024252556",  # BRIERYHURST FARM HOUSE, MOW LANE, MOW COP, STOKE-ON-TRENT
-            "10002238961",  # 2 BRIERYHURST FARM BUNGALOW THE HOLLOW, MOW COP
-            "10094839598",  # BRIERY VIEW, MOW LANE, MOW COP, STOKE-ON-TRENT
-            "10002237282",  # WILLOUGHBRIDGE WELLS LODGE, WILLOUGHBRIDGE, MARKET DRAYTON
             "100031723127",  # 7 DEANS LANE, NEWCASTLE
             "100031723126",  # 6 DEANS LANE, NEWCASTLE
             "100031723129",  # STARBOROUGH HOUSE, DEANS LANE, NEWCASTLE
             "100031723128",  # 8 DEANS LANE, NEWCASTLE
+            "200004609337",  # 290 AUDLEY ROAD, NEWCASTLE
         ]:
             return None
 
         if record.addressline6 in [
             # split
             "ST5 8QG",
-            "ST5 4DU",
-            "ST7 1DY",
             # suspect
             "ST5 6BS",
         ]:
