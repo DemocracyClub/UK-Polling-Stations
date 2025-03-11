6a84ff74bc350358b915b72b4250927e2df73049
polling-bot-4000 <developers@democracyclub.org.uk>
2025-03-11 14:58:22 +0000

    Import script for Fenland (2025-05-01) (closes #8416)

--- polling_stations/apps/data_importers/management/commands/import_fenland.py
+++ polling_stations/apps/data_importers/management/commands/import_fenland.py
@@ -3,15 +3,7 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "FEN"
-    addresses_name = "2024-07-04/2024-06-03T09:57:53.342984/Democracy Club FDC Polling Districts June 2024.csv"
-    stations_name = "2024-07-04/2024-06-03T09:57:53.342984/Democracy Club FDC Polling Stations June 2024.csv"
-    elections = ["2024-07-04"]
+    addresses_name = "2025-05-01/2025-03-11T14:58:14.457984/Democracy Club FDC Polling Districts for May 2025.csv"
+    stations_name = "2025-05-01/2025-03-11T14:58:14.457984/Democracy Club FDC Polling Stations for May 2025.csv"
+    elections = ["2025-05-01"]
     csv_encoding = "utf-16le"
-
-    def address_record_to_dict(self, record):
-        if record.postcode in [
-            "PE14 0LF",  # split
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
