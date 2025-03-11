811a69d304d43fc5c159defb12876cfe20cef794
polling-bot-4000 <developers@democracyclub.org.uk>
2025-03-11 17:08:48 +0000

    Import script for North Lincolnshire (2025-05-01) (closes #8419)

--- polling_stations/apps/data_importers/management/commands/import_north_lincolnshire.py
+++ polling_stations/apps/data_importers/management/commands/import_north_lincolnshire.py
@@ -3,20 +3,27 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "NLN"
-    addresses_name = "2024-07-04/2024-06-23T07:58:29.348175/nln-combined.csv"
-    stations_name = "2024-07-04/2024-06-23T07:58:29.348175/nln-combined.csv"
-    elections = ["2024-07-04"]
+    addresses_name = "2025-05-01/2025-03-11T17:08:37.404782/Polling Stations-North Lincolnshire Council.csv"
+    stations_name = "2025-05-01/2025-03-11T17:08:37.404782/Polling Stations-North Lincolnshire Council.csv"
+    elections = ["2025-05-01"]
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
-        if uprn in [
-            "200000888905",  # LITTLE GRANGE, FERRIBY ROAD, BARTON-UPON-HUMBER
-            "10095555254",  # THE BARN, LITTLE GRANGE, FERRIBY ROAD, BARTON-UPON-HUMBER
-            "10095555254",  # THE BARN, LITTLE GRANGE, FERRIBY ROAD, BARTON-UPON-HUMBER
-            "100050213509",  # REAR OF 121, MARY STREET, SCUNTHORPE
-            "100051967026",  # 68A MARY STREET, SCUNTHORPE
-            "200000890811",  # WAVERNEY, SUSWORTH, SCUNTHORPE
-        ]:
+        if (
+            uprn
+            in [
+                "200000888905",  # LITTLE GRANGE, FERRIBY ROAD, BARTON-UPON-HUMBER
+                "10095555254",  # THE BARN, LITTLE GRANGE, FERRIBY ROAD, BARTON-UPON-HUMBER
+                "10095555254",  # THE BARN, LITTLE GRANGE, FERRIBY ROAD, BARTON-UPON-HUMBER
+                "100050213509",  # REAR OF 121, MARY STREET, SCUNTHORPE
+                "100051967026",  # 68A MARY STREET, SCUNTHORPE
+                "200000890811",  # WAVERNEY, SUSWORTH, SCUNTHORPE
+                "200000889029",  # MASLAM HALL, BRIGG ROAD, BARTON-UPON-HUMBER
+                "200000890331",  # WILFREDS TOP BUNGALOW B1206 BETWEEN NORTHWOLD FARM AND BRIGG ROAD, BONBY
+                "10002635448",  # FLAT ASHBY LODGE MORTAL ASH HILL, SCUNTHORPE
+                "200000876730",  # SNOW SEWER BUNGALOW, PARK DRAIN, WESTWOODSIDE, DONCASTER
+            ]
+        ):
             return None
 
         if record.housepostcode in [
@@ -25,11 +32,20 @@ class Command(BaseHalaroseCsvImporter):
             "DN17 1SB",
             "DN15 8XL",
             "DN15 7JH",
-            "DN17 4EJ",
             # suspect,
-            "DN18 5BF",  # COTTINGHAM COURT, BARTON-UPON-HUMBER
+            "DN18 5BF",
             "DN16 2RX",
             "DN16 2SE",
+            "DN8 5SW",
+            "DN8 5SX",
+            "DN15 6LN",
         ]:
             return None
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # Add missing postcode for Function Room, Haven Inn, Ferry Road, Barrow Haven
+        if record.pollingstationnumber == "83":
+            record = record._replace(pollingstationpostcode="DN19 7EX")
+
+        return super().station_record_to_dict(record)
