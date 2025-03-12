7713a9098b658b00b75ff4c7bcd8d39d896354df
polling-bot-4000 <developers@democracyclub.org.uk>
2025-03-12 15:56:21 +0000

    Import script for Dover (2025-05-01) (closes #8427)

--- polling_stations/apps/data_importers/management/commands/import_dover.py
+++ polling_stations/apps/data_importers/management/commands/import_dover.py
@@ -3,9 +3,14 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "DOV"
-    addresses_name = "2024-07-04/2024-05-30T10:38:09.548818/DoverDC Democracy_Club__04July2024 (1).CSV"
-    stations_name = "2024-07-04/2024-05-30T10:38:09.548818/DoverDC Democracy_Club__04July2024 (1).CSV"
-    elections = ["2024-07-04"]
+    addresses_name = (
+        "2025-05-01/2025-03-12T15:56:09.094358/Democracy_Club__01May2025.tsv"
+    )
+    stations_name = (
+        "2025-05-01/2025-03-12T15:56:09.094358/Democracy_Club__01May2025.tsv"
+    )
+    elections = ["2025-05-01"]
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
@@ -13,7 +18,6 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         if (
             uprn
             in [
-                "10034885006",  # MANAGERS ACCOMMODATION 9 CASTLE HILL ROAD, DOVER
                 "10034877732",  # MOUNT PLEASANT, DENTON LANE, WOOTTON, CANTERBURY
                 "10094398745",  # 3 OAK TREE FARM, ALKHAM, DOVER
                 "10094398744",  # 2 OAK TREE FARM, ALKHAM, DOVER
@@ -24,9 +28,10 @@ class Command(BaseXpressDemocracyClubCsvImporter):
                 "100060911692",  # 53 PRIORY HILL, DOVER
                 "100060911690",  # 51 PRIORY HILL, DOVER
                 "10034873939",  # UPDOWN LODGE, UPDOWN, BETTESHANGER, DEAL
-                "100060883820",  # CRIXHALL HOUSE, BUCKLAND LANE, STAPLE, CANTERBURY
-                "10034882943",  # THE WILLOWS, SANDWICH ROAD, HACKLINGE, DEAL
                 "10034875431",  # ADELAIDE FARM HOUSE, SANDWICH ROAD, HACKLINGE, DEAL
+                "10096171213",  # THE BARGE J W, RICHBOROUGH ROAD, RICHBOROUGH, SANDWICH
+                "10096168842",  # COSS, RICHBOROUGH ROAD, RICHBOROUGH, SANDWICH
+                "100062059067",  # FIRST FLOOR AND SECOND FLOOR FLAT 13 KING STREET, DEAL
             ]
         ):
             return None
