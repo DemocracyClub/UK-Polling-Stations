73476cdb0f798e60d04190724825d99e714a49c1
polling-bot-4000 <developers@democracyclub.org.uk>
2025-03-12 17:16:28 +0000

    Import script for North East Derbyshire (2025-05-01) (closes #8431)

--- polling_stations/apps/data_importers/management/commands/import_north_east_derbyshire.py
+++ polling_stations/apps/data_importers/management/commands/import_north_east_derbyshire.py
@@ -4,26 +4,10 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "NED"
     addresses_name = (
-        "2024-07-04/2024-06-12T11:55:09.054594/Democracy_Club__04July2024.tsv"
+        "2025-05-01/2025-03-12T17:16:16.143353/Democracy_Club__01May2025.tsv"
     )
     stations_name = (
-        "2024-07-04/2024-06-12T11:55:09.054594/Democracy_Club__04July2024.tsv"
+        "2025-05-01/2025-03-12T17:16:16.143353/Democracy_Club__01May2025.tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2025-05-01"]
     csv_delimiter = "\t"
-
-    def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "10034037761",  # GLENDALE HOUSE, STONEDGE, ASHOVER, CHESTERFIELD
-        ]:
-            return None
-
-        if record.addressline6 in [
-            # suspect
-            "S45 0LN",
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
