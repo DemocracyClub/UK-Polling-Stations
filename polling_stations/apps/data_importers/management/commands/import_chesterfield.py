44a463209055b43c1e910db08d90a2a2b3037bc8
polling-bot-4000 <developers@democracyclub.org.uk>
2025-03-12 13:25:08 +0000

    Import script for Chesterfield (2025-05-01) (closes #8423)

--- polling_stations/apps/data_importers/management/commands/import_chesterfield.py
+++ polling_stations/apps/data_importers/management/commands/import_chesterfield.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "CHS"
     addresses_name = (
-        "2024-07-04/2024-06-05T14:34:42.061867/Democracy_Club__04July2024.tsv"
+        "2025-05-01/2025-03-12T13:24:59.688646/Democracy_Club__01May2025.tsv"
     )
     stations_name = (
-        "2024-07-04/2024-06-05T14:34:42.061867/Democracy_Club__04July2024.tsv"
+        "2025-05-01/2025-03-12T13:24:59.688646/Democracy_Club__01May2025.tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2025-05-01"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -18,8 +18,6 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         if uprn in [
             "74068238",  # THE PADDOCKS, CROW LANE, CHESTERFIELD
             "74006059",  # DOBBIN CLOUGH FARM, CROW LANE, CHESTERFIELD
-            "74078771",  # 84 SALTERGATE, CHESTERFIELD
-            "74026116",  # HILL TOP, HALL LANE, STAVELEY, CHESTERFIELD
         ]:
             return None
 
@@ -28,7 +26,6 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "S40 3LA",
             # suspect
             "S40 2AL",
-            "S41 9RL",
         ]:
             return None
 
@@ -37,13 +34,9 @@ class Command(BaseXpressDemocracyClubCsvImporter):
     def station_record_to_dict(self, record):
         # postcode correction for: Christ Church Primary School, Tapton View Road, Chesterfield, S41 1JU
         # postcode is also in a wrong column
-        if record.polling_place_id == "7624":
+        if record.polling_place_id == "8292":
             record = record._replace(
                 polling_place_postcode="S41 7JU", polling_place_address_3=""
             )
-
-        # postcode correction for: Hasland Methodist Church, Hampton Street, Hasland, Chesterfield, S41 1LH
-        if record.polling_place_id == "7577":
-            record = record._replace(polling_place_postcode="S41 0LH")
 
         return super().station_record_to_dict(record)
