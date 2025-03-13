e0b116363207b0b40698e446e12d8625d72e7719
polling-bot-4000 <developers@democracyclub.org.uk>
2025-03-13 12:57:40 +0000

    Import script for Tonbridge and Malling (2025-05-01) (closes #8434)

--- polling_stations/apps/data_importers/management/commands/import_tonbridge.py
+++ polling_stations/apps/data_importers/management/commands/import_tonbridge.py
@@ -3,49 +3,24 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "TON"
-    addresses_name = "2024-07-04/2024-06-24T13:00:52.040925/ton-combined.csv"
-    stations_name = "2024-07-04/2024-06-24T13:00:52.040925/ton-combined.csv"
-    elections = ["2024-07-04"]
-
-    duplicate_stations = ["JUDD-31", "JUDD-32", "JUDD-33", "JUDD-34"]
+    addresses_name = (
+        "2025-05-01/2025-03-13T12:57:10.156992/Tonbridge & Malling data.csv"
+    )
+    stations_name = "2025-05-01/2025-03-13T12:57:10.156992/Tonbridge & Malling data.csv"
+    elections = ["2025-05-01"]
 
     def station_record_to_dict(self, record):
-        if record.pollingstationnumber in self.duplicate_stations:
-            return None
-        # station change from council for stations at:
-        # Larkfield Village Hall New Hythe Lane Larkfield Kent ME20 6PU
-        if self.get_station_hash(record) in [
-            "CA-207-larkfield-village-hall",
-            "CA-208-larkfield-village-hall",
-        ]:
-            record = record._replace(
-                pollingstationname="Larkfield Leisure Centre",
-                pollingstationpostcode="ME20 6RH",
-            )
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-        if record.pollingstationnumber in self.duplicate_stations:
-            return None
-
         if record.housepostcode in [
             # split
             "ME20 6HZ",
-            "TN11 0ES",
-            "TN10 4JJ",
             "TN11 0AJ",
             "ME19 5PA",
+            "TN10 4JJ",
+            "TN11 0ES",
         ]:
             return None
-
-        # reassign addresses for Larkfield Village Hall station change:
-        if self.get_station_hash(record) in [
-            "CA-207-larkfield-village-hall",
-            "CA-208-larkfield-village-hall",
-        ]:
-            record = record._replace(
-                pollingstationname="Larkfield Leisure Centre",
-                pollingstationpostcode="ME20 6RH",
-            )
 
         return super().address_record_to_dict(record)
