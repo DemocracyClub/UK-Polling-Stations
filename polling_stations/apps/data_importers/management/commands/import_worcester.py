f207164073b91154ee6fd951f7ea0a040e1d28ef
polling-bot-4000 <developers@democracyclub.org.uk>
2025-03-14 11:42:22 +0000

    Import script for Worcester (2025-05-01) (closes #8442)

--- polling_stations/apps/data_importers/management/commands/import_worcester.py
+++ polling_stations/apps/data_importers/management/commands/import_worcester.py
@@ -4,46 +4,31 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WOC"
     addresses_name = (
-        "2024-07-04/2024-06-03T12:30:53.495756/Democracy_Club__04July2024.CSV"
+        "2025-05-01/2025-03-14T11:42:13.125033/Democracy_Club__01May2025.tsv"
     )
     stations_name = (
-        "2024-07-04/2024-06-03T12:30:53.495756/Democracy_Club__04July2024.CSV"
+        "2025-05-01/2025-03-14T11:42:13.125033/Democracy_Club__01May2025.tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2025-05-01"]
     csv_encoding = "windows-1252"
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
             "10090440987",  # FLAT AT THE BEDWARDINE 128 BROMYARD ROAD, WORCESTER
-            "100120648786",  # 145A COLUMBIA DRIVE, WORCESTER
-            "10090440987",  # FLAT AT THE BEDWARDINE 128 BROMYARD ROAD, WORCESTER
-        ]:
-            return None
-
-        if record.addressline6 in [
-            "WR5 3EX",  # split
         ]:
             return None
 
         return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
-        # Station change from council:
-        # old station: Communal Room, Lincoln Green, Off Chelmsford Drive, Worcester, WR5 1QU
-        # new station: Youth Room, Ronkswood Hub, Canterbury Road, Worcester, WR5 1PJ
-        if record.polling_place_id == "6772":
+        # Location correction for: Henry Sandon Hall Royal Porcelain Works, WR1 2NE
+        if record.polling_place_id == "7018":
             record = record._replace(
-                polling_place_name="Youth Room",
-                polling_place_address_1="Ronkswood Hub",
-                polling_place_address_2="Canterbury Road",
-                polling_place_address_3="",
-                polling_place_address_4="Worcester",
-                polling_place_postcode="WR5 1PJ",
-                polling_place_easting="387019",
-                polling_place_northing="254640",
-                polling_place_uprn="100121275449",
+                polling_place_easting="385149",
+                polling_place_northing="254311",
             )
 
         return super().station_record_to_dict(record)
