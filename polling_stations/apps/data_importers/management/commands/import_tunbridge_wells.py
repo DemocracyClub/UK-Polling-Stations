9185dbc8d1cfa6c70d72658d3ec117174ec69f07
polling-bot-4000 <developers@democracyclub.org.uk>
2025-03-17 16:00:19 +0000

    Import script for Tunbridge Wells (2025-05-01) (closes #8461)

--- polling_stations/apps/data_importers/management/commands/import_tunbridge_wells.py
+++ polling_stations/apps/data_importers/management/commands/import_tunbridge_wells.py
@@ -4,12 +4,21 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "TUN"
     addresses_name = (
-        "2024-07-04/2024-05-30T09:04:15.273966/Democracy_Club__04July2024 (1).CSV"
+        "2025-05-01/2025-03-17T16:00:04.636176/Democracy_Club__01May2025.tsv"
     )
     stations_name = (
-        "2024-07-04/2024-05-30T09:04:15.273966/Democracy_Club__04July2024 (1).CSV"
+        "2025-05-01/2025-03-17T16:00:04.636176/Democracy_Club__01May2025.tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2025-05-01"]
+    csv_encoding = "windows-1252"
+    csv_delimiter = "\t"
+
+    # Ignore warnings about addressbase postcode for below stations - council decision:
+    # - St Barnabas Hall, Quarry Road, Tunbridge Wells
+    # - The Hub, Grosvenor Recreation Ground, Auckland Road, Royal Tunbridge Wells
+    # - St James Church Hall, St James Road, Royal Tunbridge Wells
+    # - St Mark`s Hall, Bayham Road, Royal Tunbridge Wells
+    # - The Library, Number One Community Centre, Rowan Tree Road, Tunbridge Wells
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
@@ -18,14 +27,15 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "100062544084",  # COURTYARD FLAT, COLEBROOKE, PEMBURY ROAD, TONBRIDGE
             "100062543933",  # COLEBROOKE HOUSE, PEMBURY ROAD, TONBRIDGE
             "100061193913",  # HUNTERS LODGE, PEMBURY ROAD, TONBRIDGE
+            "100062551952",  # BLACK BUSH COTTAGE, BEDGEBURY ROAD, GOUDHURST, Cranbrook
+            "10090055018",  # CRABTREE HOUSE, COURSE HORN LANE, CRANBROOK
         ]:
             return None
 
         if record.addressline6 in [
             # split
-            "TN12 6GY",
-            "TN3 0HX",
             "TN4 0AB",
+            "TN3 0HX",
             # suspect
             "TN11 0PG",
             "TN18 5AH",
@@ -33,3 +43,17 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             return None
 
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # UPRN correction from council for: Kilndown Village Hall, Church Road, Kilndown, Cranbrook, Kent, TN17 2RY
+        if record.polling_place_id == "4303":
+            record = record._replace(polling_place_uprn="10008671449")
+
+        # UPRN correction from council for: The Function Room, The Hawkhurst Club Unity Hall, TN18 4AG
+        if record.polling_place_id == "4289":
+            record = record._replace(
+                polling_place_uprn="100062553029",
+                polling_place_easting="",
+                polling_place_northing="",
+            )
+        return super().station_record_to_dict(record)
