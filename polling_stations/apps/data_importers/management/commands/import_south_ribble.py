ac238177f16a5f5a7e72929c5f9e629ec97a20a9
polling-bot-4000 <developers@democracyclub.org.uk>
2025-03-17 10:24:02 +0000

    Import script for South Ribble (2025-05-01) (closes #8452)

--- polling_stations/apps/data_importers/management/commands/import_south_ribble.py
+++ polling_stations/apps/data_importers/management/commands/import_south_ribble.py
@@ -3,9 +3,14 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "SRI"
-    addresses_name = "2024-07-04/2024-06-10T14:32:56.226915/combined.csv"
-    stations_name = "2024-07-04/2024-06-10T14:32:56.226915/combined.csv"
-    elections = ["2024-07-04"]
+    addresses_name = (
+        "2025-05-01/2025-03-17T10:23:51.105469/Democracy Club - SR data.csv"
+    )
+    stations_name = "2025-05-01/2025-03-17T10:23:51.105469/Democracy Club - SR data.csv"
+    elections = ["2025-05-01"]
+
+    # Ignore warning: Polling station Buckshaw Village Community Centre (2-buckshaw-village-community-centre) is in Chorley Borough
+    # Station have correct location, just outside the council border.
 
     def address_record_to_dict(self, record):
         if record.housepostcode in [
