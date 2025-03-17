61d83a19554f544e6073874ea556d5fd48f4fc28
polling-bot-4000 <developers@democracyclub.org.uk>
2025-03-17 15:39:48 +0000

    Import script for Newark & Sherwood (2025-05-01) (closes #8459)

--- polling_stations/apps/data_importers/management/commands/import_newark_and_sherwood.py
+++ polling_stations/apps/data_importers/management/commands/import_newark_and_sherwood.py
@@ -3,33 +3,6 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "NEA"
-    addresses_name = "2024-07-04/2024-06-04T11:45:51.882314/Eros_SQL_Output012.csv"
-    stations_name = "2024-07-04/2024-06-04T11:45:51.882314/Eros_SQL_Output012.csv"
-    elections = ["2024-07-04"]
-
-    def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
-
-        if uprn in [
-            "100031448570",  # 131 LIME GROVE, NEWARK
-            "100032115885",  # HOLY TRINITY PRESBYTERY, BOUNDARY ROAD, NEWARK
-            "100031446573",  # 2 HAWTON ROAD, NEWARK
-            "100031458023",  # 74 STAUNTON ROAD, NEWARK
-        ]:
-            return None
-
-        if record.housepostcode in [
-            # split
-            "NG24 4JW",
-            "NG24 4BT",
-            "NG24 3PA",
-            "NG25 0LQ",
-            "NG24 5BS",
-            "NG21 9EE",
-            # suspect
-            "NG24 4RZ",
-            "NG22 9DY",
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
+    addresses_name = "2025-05-01/2025-03-17T15:39:24.857102/Eros_SQL_Output001.csv"
+    stations_name = "2025-05-01/2025-03-17T15:39:24.857102/Eros_SQL_Output001.csv"
+    elections = ["2025-05-01"]
