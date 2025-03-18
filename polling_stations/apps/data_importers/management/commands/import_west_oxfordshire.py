35fac092779ddb6f62a273fb217a1c6061fd7de2
polling-bot-4000 <developers@democracyclub.org.uk>
2025-03-18 14:40:42 +0000

    Import script for West Oxfordshire (2025-05-01) (closes #8466)

--- polling_stations/apps/data_importers/management/commands/import_west_oxfordshire.py
+++ polling_stations/apps/data_importers/management/commands/import_west_oxfordshire.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WOX"
     addresses_name = (
-        "2024-07-04/2024-06-06T14:04:29.414902/Democracy_Club__04July2024 (23).tsv"
+        "2025-05-01/2025-03-18T14:40:33.046028/Democracy_Club__01May2025.tsv"
     )
     stations_name = (
-        "2024-07-04/2024-06-06T14:04:29.414902/Democracy_Club__04July2024 (23).tsv"
+        "2025-05-01/2025-03-18T14:40:33.046028/Democracy_Club__01May2025.tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2025-05-01"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -22,30 +22,29 @@ class Command(BaseXpressDemocracyClubCsvImporter):
                 "10033224093",  # BREW HOUSE COTTAGE CHASTLETON HOUSE ROAD THROUGH CHASTLETON, CHASTLETON
                 "10093354541",  # BARN CONVERSION BLISS FARM CHURCHILL ROAD, CHIPPING NORTON
                 "10093355282",  # BOULTERS GRANGE, CHURCHILL ROAD, CHIPPING NORTON
-                "100120970316",  # SUNNYCROFT, BURFORD ROAD, MINSTER LOVELL, WITNEY
                 "10024177121",  # 1 TIMBER YARD, BRADWELL GROVE, BURFORD
                 "10024177122",  # 2 TIMBER YARD, BRADWELL GROVE, BURFORD
                 "10024177123",  # 3 TIMBER YARD, BRADWELL GROVE, BURFORD
                 "10093356610",  # ANNEXE AT COLDSTONE HOUSE 50 SHIPTON ROAD, ASCOTT UNDER WYCHWOOD
-                "100120966699",  # JOHNSONS FARM, SHILTON ROAD, BURFORD
-                "10093354734",  # ANNEXE JOHNSONS FARM SHILTON ROAD, BURFORD
+                "100120969301",  # MANOR FARM, WESTCOTE BARTON, CHIPPING NORTON
+                "100120968287",  # RICKYARD COTTAGE, FAIRGREEN, CHURCHILL, CHIPPING NORTON
+                "10033227811",  # MANAGERS ACCOMMODATION CROSS KEYS 1 MARKET SQUARE, WITNEY
             ]
         ):
             return None
 
         if record.addressline6 in [
             # split
-            "OX18 1PU",
-            "OX18 3NU",
-            "OX20 1RZ",
             "OX28 6DH",
-            "OX7 4BJ",
+            "OX18 1PU",
+            "OX20 1RZ",
             "OX7 5YE",
+            "OX7 4BJ",
             # suspect
             "OX7 6UQ",
             "OX7 6WJ",
-            "OX18 1PS",
             "OX7 6HX",
+            "OX18 2BE",
         ]:
             return None
 
