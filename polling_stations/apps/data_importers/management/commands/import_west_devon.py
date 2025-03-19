901a2e2c3c1bc58d25384b9109a7c4083797215a
polling-bot-4000 <developers@democracyclub.org.uk>
2025-03-19 17:07:13 +0000

    Import script for West Devon (2025-05-01) (closes #8475)

--- polling_stations/apps/data_importers/management/commands/import_west_devon.py
+++ polling_stations/apps/data_importers/management/commands/import_west_devon.py
@@ -3,9 +3,13 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WDE"
-    addresses_name = "2024-07-04/2024-06-24T08:08:18.645993/wde-combined.tsv"
-    stations_name = "2024-07-04/2024-06-24T08:08:18.645993/wde-combined.tsv"
-    elections = ["2024-07-04"]
+    addresses_name = (
+        "2025-05-01/2025-03-19T17:07:01.840411/Democracy_Club__01May2025 (3).tsv"
+    )
+    stations_name = (
+        "2025-05-01/2025-03-19T17:07:01.840411/Democracy_Club__01May2025 (3).tsv"
+    )
+    elections = ["2025-05-01"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -18,16 +22,19 @@ class Command(BaseXpressDemocracyClubCsvImporter):
                 "10001330732",  # HOMEFIELD, DREWSTEIGNTON, EXETER
                 "10001322729",  # SOUTH MOOR, JACOBSTOWE, OKEHAMPTON
                 "10001322728",  # SOUTHMOOR FARM ROAD FROM LANGABEAR MOOR TO SOUTHMOOR FARM, JACOBSTOWE
+                "10013751665",  # BERRYDOWN STABLES, EXBOURNE, OKEHAMPTON
+                "100040396098",  # LITTLE GREENSLADE, SAMPFORD COURTENAY, OKEHAMPTON
             ]
         ):
             return None
         if record.addressline6 in [
             # split
-            "EX20 2SD",
             "EX20 1SY",
-            "EX20 2TP",
+            "PL20 7DN",
             "EX20 3NW",
+            "EX20 2TP",
             "EX20 1QB",
+            "EX20 2SD",
         ]:
             return None
         return super().address_record_to_dict(record)
