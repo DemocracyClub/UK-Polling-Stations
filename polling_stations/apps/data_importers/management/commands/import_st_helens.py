7bc7ee47cd6af5c2e8ebffaeb409045091cb2e61
polling-bot-4000 <developers@democracyclub.org.uk>
2025-03-20 09:55:28 +0000

    Import script for St Helens (2025-04-03) (closes #8480)

--- polling_stations/apps/data_importers/management/commands/import_st_helens.py
+++ polling_stations/apps/data_importers/management/commands/import_st_helens.py
@@ -3,30 +3,7 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SHN"
-    addresses_name = (
-        "2024-12-12/2024-11-19T11:02:39.999329/Democracy_Club__12December2024.tsv"
-    )
-    stations_name = (
-        "2024-12-12/2024-11-19T11:02:39.999329/Democracy_Club__12December2024.tsv"
-    )
-    elections = ["2024-12-12"]
+    addresses_name = "2025-04-03/2025-03-20T09:55:21.248536/Democracy_Club__03April2025 - Sutton South East By Election.tsv"
+    stations_name = "2025-04-03/2025-03-20T09:55:21.248536/Democracy_Club__03April2025 - Sutton South East By Election.tsv"
+    elections = ["2025-04-03"]
     csv_delimiter = "\t"
-
-    # Maintaining GE exclusions for future reference
-
-    # def address_record_to_dict(self, record):
-    #     uprn = record.property_urn.strip().lstrip("0")
-
-    # if uprn in [
-    #     "39097215",  # 8 PARK AVENUE NORTH, NEWTON-LE-WILLOWS
-    #     "39053135",  # ROOM 5, COHAB HOUSE, 19 SHAW STREET, ST. HELENS
-    # ]:
-    #     return None
-    # if record.addressline6 in [
-    #     # split
-    #     "WA10 1HT",
-    #     "WA9 3RR",
-    # ]:
-    #     return None
-
-    # return super().address_record_to_dict(record)
