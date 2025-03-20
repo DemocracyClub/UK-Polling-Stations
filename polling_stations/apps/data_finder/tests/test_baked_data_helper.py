aa3c317cdf29165b24338a3eabf7a87016df46bf
chris48s <git@chris-shaw.dev>
2025-03-20 16:22:35 +0000

    add unit test for get_full_ballots function

--- polling_stations/apps/data_finder/tests/test_baked_data_helper.py
+++ polling_stations/apps/data_finder/tests/test_baked_data_helper.py
@@ -6,6 +6,7 @@ from uk_geo_utils.helpers import Postcode
 
 import pytest
 import polars
+import responses
 
 
 @pytest.fixture()
@@ -276,3 +277,95 @@ def test_uprn_with_elections(temp_data_root, sample_data_writer):
         "request_success": True,
     }
     assert helper.get_ballot_list(postcode, uprn="000001") == expected
+
+
+@responses.activate
+def test_ee_requests_all_success(temp_data_root, sample_data_writer, caplog):
+    ballot_ids = [
+        "local.north-kesteven.bracebridge-heath.by.2025-03-20",
+        "mayor.greater-lincolnshire-cca.2025-05-01",
+        "local.lincolnshire.washingborough.2025-05-01",
+    ]
+
+    sample_data_writer(
+        [
+            {
+                "postcode": "AA1 1AA",
+                "uprn": "000001",
+                "current_elections": ballot_ids,
+            }
+        ]
+    )
+
+    responses.add(
+        responses.GET,
+        "https://elections.democracyclub.org.uk/api/elections/local.north-kesteven.bracebridge-heath.by.2025-03-20/",
+        json={"election_id": "local.north-kesteven.bracebridge-heath.by.2025-03-20"},
+        status=200,
+    )
+    responses.add(
+        responses.GET,
+        "https://elections.democracyclub.org.uk/api/elections/mayor.greater-lincolnshire-cca.2025-05-01/",
+        json={"election_id": "mayor.greater-lincolnshire-cca.2025-05-01"},
+        status=200,
+    )
+    responses.add(
+        responses.GET,
+        "https://elections.democracyclub.org.uk/api/elections/local.lincolnshire.washingborough.2025-05-01/",
+        json={"election_id": "local.lincolnshire.washingborough.2025-05-01"},
+        status=200,
+    )
+
+    helper = LocalParquetElectionsHelper(elections_parquet_path=Path(temp_data_root))
+
+    result = helper.get_response(Postcode("AA1 1AA"))
+    assert result["request_success"]
+    assert len(result["ballots"]) == 3
+    assert len(caplog.records) == 0
+
+
+@responses.activate
+def test_ee_requests_with_failure(temp_data_root, sample_data_writer, caplog):
+    ballot_ids = [
+        "local.north-kesteven.bracebridge-heath.by.2025-03-20",
+        "mayor.greater-lincolnshire-cca.2025-05-01",
+        "local.lincolnshire.washingborough.2025-05-01",
+    ]
+
+    sample_data_writer(
+        [
+            {
+                "postcode": "AA1 1AA",
+                "uprn": "000001",
+                "current_elections": ballot_ids,
+            }
+        ]
+    )
+
+    responses.add(
+        responses.GET,
+        "https://elections.democracyclub.org.uk/api/elections/local.north-kesteven.bracebridge-heath.by.2025-03-20/",
+        json={"election_id": "local.north-kesteven.bracebridge-heath.by.2025-03-20"},
+        status=200,
+    )
+    responses.add(
+        responses.GET,
+        "https://elections.democracyclub.org.uk/api/elections/mayor.greater-lincolnshire-cca.2025-05-01/",
+        json={"election_id": "mayor.greater-lincolnshire-cca.2025-05-01"},
+        status=200,
+    )
+    responses.add(
+        responses.GET,
+        "https://elections.democracyclub.org.uk/api/elections/local.lincolnshire.washingborough.2025-05-01/",
+        json={"error": "oh no"},
+        status=500,
+    )
+
+    helper = LocalParquetElectionsHelper(elections_parquet_path=Path(temp_data_root))
+
+    result = helper.get_response(Postcode("AA1 1AA"))
+    assert not result["request_success"]
+    assert (
+        len([rec for rec in caplog.records if rec.message.startswith("Error fetching")])
+        == 1
+    )
