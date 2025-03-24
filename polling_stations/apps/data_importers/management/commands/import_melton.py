1da33ee0cd7c13cc105748367a5e24bc9b22305e
polling-bot-4000 <developers@democracyclub.org.uk>
2025-03-24 11:08:22 +0000

    Import script for Melton (2025-05-01) (closes #8492)

--- polling_stations/apps/data_importers/management/commands/import_melton.py
+++ polling_stations/apps/data_importers/management/commands/import_melton.py
@@ -4,24 +4,24 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "MEL"
     addresses_name = (
-        "2024-07-04/2024-06-17T11:41:17.871965/Democracy_Club__04July2024.tsv"
+        "2025-05-01/2025-03-24T11:04:11.230653/Democracy_Club__01May2025.tsv"
     )
     stations_name = (
-        "2024-07-04/2024-06-17T11:41:17.871965/Democracy_Club__04July2024.tsv"
+        "2025-05-01/2025-03-24T11:04:11.230653/Democracy_Club__01May2025.tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2025-05-01"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
         if uprn in [
-            "100032044579",  # GLEBE FARM, MAIN STREET, SAXELBY, MELTON MOWBRAY
             "100032044777",  # HILL FARM, EASTWELL ROAD, SCALFORD, MELTON MOWBRAY
             "10095606152",  # BRAMBLE BARN, EASTWELL ROAD, SCALFORD, MELTON MOWBRAY
         ]:
             return None
         if record.addressline6 in [
             # split
+            "LE13 1FR",
             "NG32 1QG",
             "LE14 2XB",
             "NG32 1QQ",
@@ -31,3 +31,11 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             return None
 
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # Add missing poostcode for: Freeby Village Hall, Main Street, Freeby, Melton Mowbray
+        # Source is from other station in the same bulding (same name/address, ID: 3445)
+        if record.polling_place_id == "3413":
+            record = record._replace(polling_place_postcode="LE14 2RY")
+
+        return super().station_record_to_dict(record)
