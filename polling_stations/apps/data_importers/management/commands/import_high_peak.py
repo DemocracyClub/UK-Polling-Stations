0d2a3b58ce468309847f6fa4b5c6fa14098a70d9
polling-bot-4000 <developers@democracyclub.org.uk>
2025-03-25 10:11:10 +0000

    Import script for High Peak (2025-05-01) (closes #8519)

--- polling_stations/apps/data_importers/management/commands/import_high_peak.py
+++ polling_stations/apps/data_importers/management/commands/import_high_peak.py
@@ -4,37 +4,10 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "HIG"
     addresses_name = (
-        "2024-07-04/2024-06-11T15:57:01.284842/Democracy_Club__04July2024 (20).tsv"
+        "2025-05-01/2025-03-25T10:11:00.608557/Democracy_Club__01May2025 (5).tsv"
     )
     stations_name = (
-        "2024-07-04/2024-06-11T15:57:01.284842/Democracy_Club__04July2024 (20).tsv"
+        "2025-05-01/2025-03-25T10:11:00.608557/Democracy_Club__01May2025 (5).tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2025-05-01"]
     csv_delimiter = "\t"
-
-    def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if (
-            uprn
-            in [
-                "10010727932",  # HOCKERLEY HALL FARM, HOCKERLEY LANE, WHALEY BRIDGE, HIGH PEAK
-                "10010747214",  # THE BUNGALOW, HARPUR HILL BUSINESS PARK, BUXTON
-                "10010715355",  # RED GAP FARM, GREEN FAIRFIELD, BUXTON
-                "10010720655",  # BLACK HILLGATE FARM, KETTLESHULME, HIGH PEAK
-                "10010751272",  # MILTON FARMHOUSE, CHAPEL MILTON, CHAPEL-EN-LE-FRITH, HIGH PEAK
-            ]
-        ):
-            return None
-
-        if record.addressline6 in [
-            # splits
-            "S33 0AB",
-            "SK23 6BR",
-            # suspect
-            "SK22 3DU",  # LARKHILL TERRACE, NEW MILLS, HIGH PEAK
-            "SK22 1BW",  # CRESCENT ROW, BIRCH VALE
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
