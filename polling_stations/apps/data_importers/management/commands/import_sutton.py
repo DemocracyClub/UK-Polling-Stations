952265da49f0c2f02f96b9182f1aac0d0faa2737
polling-bot-4000 <developers@democracyclub.org.uk>
2025-03-25 16:57:37 +0000

    Import script for Sutton (2025-04-10) (closes #8531)

--- polling_stations/apps/data_importers/management/commands/import_sutton.py
+++ polling_stations/apps/data_importers/management/commands/import_sutton.py
@@ -1,23 +1,23 @@
 from data_importers.management.commands import BaseDemocracyCountsCsvImporter
-from django.contrib.gis.geos import Point
 
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "STN"
     addresses_name = (
-        "2024-07-04/2024-06-07T20:21:55.085664/Democracy Club Polling Districts.csv"
+        "2025-04-10/2025-03-25T16:57:30.766897/Democracy Club Polling Districts.csv"
     )
     stations_name = (
-        "2024-07-04/2024-06-07T20:21:55.085664/Democracy Club Polling Stations.csv"
+        "2025-04-10/2025-03-25T16:57:30.766897/Democracy Club Polling Stations.csv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2025-04-10"]
     csv_encoding = "utf-16le"
 
-    def station_record_to_dict(self, record):
-        # more accurate point for: Holy Trinity Church Centre, Maldon Road, Wallington, SM6 8BL
-        if record.stationcode in ["RB/1", "RB/2"]:
-            rec = super().station_record_to_dict(record)
-            rec["location"] = Point(-0.151435, 51.364474, srid=4326)
-            return rec
+    # By-election, Maintaining unused script changes as comment for future reference:
+    # def station_record_to_dict(self, record):
+    #     # more accurate point for: Holy Trinity Church Centre, Maldon Road, Wallington, SM6 8BL
+    #     if record.stationcode in ["RB/1", "RB/2"]:
+    #         rec = super().station_record_to_dict(record)
+    #         rec["location"] = Point(-0.151435, 51.364474, srid=4326)
+    #         return rec
 
-        return super().station_record_to_dict(record)
+    #     return super().station_record_to_dict(record)
