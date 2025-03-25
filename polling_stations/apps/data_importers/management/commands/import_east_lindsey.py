a8aafda7855542b286769fe70377440572f32f6c
polling-bot-4000 <developers@democracyclub.org.uk>
2025-03-25 10:24:02 +0000

    Import script for East Lindsey (2025-05-01) (closes #8521)

--- polling_stations/apps/data_importers/management/commands/import_east_lindsey.py
+++ polling_stations/apps/data_importers/management/commands/import_east_lindsey.py
@@ -4,17 +4,17 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "ELI"
     addresses_name = (
-        "2024-07-04/2024-06-06T11:05:09.011311/Democracy_Club__04July2024 (22).tsv"
+        "2025-05-01/2025-03-25T10:23:40.621530/ELDC - Democracy_Club__01May2025.tsv"
     )
     stations_name = (
-        "2024-07-04/2024-06-06T11:05:09.011311/Democracy_Club__04July2024 (22).tsv"
+        "2025-05-01/2025-03-25T10:23:40.621530/ELDC - Democracy_Club__01May2025.tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2025-05-01"]
     csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
         # more accurate point for: St Marys Church, Church Lane, Fotherby, Louth, LN11 0UJ
-        if record.polling_place_id == "12140":
+        if record.polling_place_id == "12564":
             record = record._replace(polling_place_easting="531703")
             record = record._replace(polling_place_northing="391688")
 
@@ -23,36 +23,26 @@ class Command(BaseXpressDemocracyClubCsvImporter):
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
-        if (
-            uprn
-            in [
-                "200001828818",  # GRANGE FARM, WELTON-LE-MARSH, SPILSBY
-                "10024297692",  # LAKE VIEW STATION ROAD, LITTLE STEEPING
-                "100032167500",  # WINDSOR COTTAGE, HAKERLEY BRIDGE, FRITHVILLE, BOSTON
-                "100032077624",  # EVENWOOD, FIRE BEACON LANE, COVENHAM ST. BARTHOLOMEW, LOUTH
-                "100032260114",  # FIREBEACON BRIDGE FARM, FIREBEACON LANE, WRAGHOLME, LOUTH
-                "10090638895",  # THE PADDOCKS, CROFT LANE, CROFT, SKEGNESS
-                "100032166988",  # STONEY BROKE, TRADER BANK, SIBSEY, BOSTON
-                "10008523556",  # JASMINE LODGE, MAIN ROAD, SIBSEY, BOSTON
-                "10096344217",  # A97 MERMAID CARAVAN PARK SEAHOLME ROAD, MABLETHORPE
-                "10093437401",  # A37 MERMAID CARAVAN PARK SEAHOLME ROAD, MABLETHORPE
-                "10008514781",  # DUDDLES FARM, SANDY BANK, NEW YORK, LINCOLN
-                "10008521252",  # CORNFIELDS, LITTLE CARLTON, LOUTH
-                "10094011546",  # FOXHALL, PARK ROW, LOUTH
-            ]
-        ):
+        if uprn in [
+            "200001828818",  # GRANGE FARM, WELTON-LE-MARSH, SPILSBY
+            "10024297692",  # LAKE VIEW STATION ROAD, LITTLE STEEPING
+            "100032167500",  # WINDSOR COTTAGE, HAKERLEY BRIDGE, FRITHVILLE, BOSTON
+            "10090638895",  # THE PADDOCKS, CROFT LANE, CROFT, SKEGNESS
+            "100032166988",  # STONEY BROKE, TRADER BANK, SIBSEY, BOSTON
+            "10008523556",  # JASMINE LODGE, MAIN ROAD, SIBSEY, BOSTON
+            "10008523556",  # A97 MERMAID CARAVAN PARK SEAHOLME ROAD, MABLETHORPE
+            "10093437401",  # A37 MERMAID CARAVAN PARK SEAHOLME ROAD, MABLETHORPE
+        ]:
             return None
 
         if record.addressline6 in [
             # splits
-            "LN11 8QG",
-            "LN11 8DW",
-            "LN9 5JP",
             "PE25 2PX",
-            "LN8 5LR",
-            "LN11 0EG",
             "PE24 5UT",
+            "LN8 5LR",
             "LN12 2HX",
+            "LN9 5JP",
+            "LN11 0EG",
             # looks wrong
             "PE25 2FE",
         ]:
