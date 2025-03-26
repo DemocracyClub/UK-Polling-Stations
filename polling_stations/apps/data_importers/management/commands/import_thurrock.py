e31b788ff873fe6a0f2466a9c9f6a094ea91d5be
polling-bot-4000 <developers@democracyclub.org.uk>
2025-03-26 14:00:29 +0000

    Import script for Thurrock (2025-05-01) (closes #8538)

--- polling_stations/apps/data_importers/management/commands/import_thurrock.py
+++ polling_stations/apps/data_importers/management/commands/import_thurrock.py
@@ -4,34 +4,35 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "THR"
     addresses_name = (
-        "2024-07-04/2024-05-31T09:03:49.659033/Democracy_Club__04July2024 (4).tsv"
+        "2025-05-01/2025-03-26T14:00:22.033216/Democracy_Club__01May2025 (6).tsv"
     )
     stations_name = (
-        "2024-07-04/2024-05-31T09:03:49.659033/Democracy_Club__04July2024 (4).tsv"
+        "2025-05-01/2025-03-26T14:00:22.033216/Democracy_Club__01May2025 (6).tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2025-05-01"]
     csv_delimiter = "\t"
 
-    def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
+    # By-election so maintaining exclusions from previous elections for future reference
+    # def address_record_to_dict(self, record):
+    #     uprn = record.property_urn.strip().lstrip("0")
 
-        if (
-            uprn
-            in [
-                "10095907154",  # 1A HIGH STREET, GRAYS
-                "200001546102",  # LANGDON HALL FARM, OLD CHURCH HILL, LANGDON HILLS, BASILDON
-                "100091297522",  # BENTLEY FARM, OLD CHURCH HILL, LANGDON HILLS, BASILDON
-                "100091297871",  # HOB HILL FARM, BIGGIN LANE, GRAYS
-                "10095908244",  # 251 BRANKSOME AVENUE, STANFORD-LE-HOPE
-            ]
-        ):
-            return None
+    #     if (
+    #         uprn
+    #         in [
+    #             "10095907154",  # 1A HIGH STREET, GRAYS
+    #             "200001546102",  # LANGDON HALL FARM, OLD CHURCH HILL, LANGDON HILLS, BASILDON
+    #             "100091297522",  # BENTLEY FARM, OLD CHURCH HILL, LANGDON HILLS, BASILDON
+    #             "100091297871",  # HOB HILL FARM, BIGGIN LANE, GRAYS
+    #             "10095908244",  # 251 BRANKSOME AVENUE, STANFORD-LE-HOPE
+    #         ]
+    #     ):
+    #         return None
 
-        if record.addressline6.strip() in [
-            # splits
-            "RM19 1QJ",
-            "RM16 4RB",
-        ]:
-            return None  # split
+    #     if record.addressline6.strip() in [
+    #         # splits
+    #         "RM19 1QJ",
+    #         "RM16 4RB",
+    #     ]:
+    #         return None  # split
 
-        return super().address_record_to_dict(record)
+    #     return super().address_record_to_dict(record)
