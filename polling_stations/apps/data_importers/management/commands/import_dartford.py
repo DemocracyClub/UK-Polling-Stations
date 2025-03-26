dc871ab3b37add7f88ae012742c4021b6086810d
polling-bot-4000 <developers@democracyclub.org.uk>
2025-03-26 12:58:46 +0000

    Import script for Dartford (2025-05-01) (closes #8536)

--- polling_stations/apps/data_importers/management/commands/import_dartford.py
+++ polling_stations/apps/data_importers/management/commands/import_dartford.py
@@ -3,19 +3,21 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "DAR"
-    addresses_name = "2024-11-21/2024-11-18T16:18:26.090524/Eros_SQL_Output004.csv"
-    stations_name = "2024-11-21/2024-11-18T16:18:26.090524/Eros_SQL_Output004.csv"
-    elections = ["2024-11-21"]
+    addresses_name = "2025-05-01/2025-03-26T12:58:37.459986/Eros_SQL_Output001.csv"
+    stations_name = "2025-05-01/2025-03-26T12:58:37.459986/Eros_SQL_Output001.csv"
+    elections = ["2025-05-01"]
 
+    def station_record_to_dict(self, record):
+        # Removing suspect station:
+        if self.get_station_hash(record) == "99-mobile-unit-or-polling-station-gre4":
+            return None
 
-# Maintaining GE notes as comments for future reference:
+        # Adding missing coords to 2nd station at the same venue as a station with coords
+        if self.get_station_hash(record) == "24-long-valley-hall-lns2":
+            record = record._replace(
+                pollingvenueeasting="559819",
+                pollingvenuenorthing="169259",
+                pollingvenueuprn="200000538035",
+            )
 
-# def station_record_to_dict(self, record):
-#     # Below stations are throwing a warning about different postcode in the addressbase, but council requested to keep them.
-#     # Wentworth Primary School, Entrance via James Road, Wentworth Drive, Dartford, Kent, DA1 3NF
-#     # Temple Hill Community Centre, Temple Hill Square, Dartford, Kent, DA1 5HX
-#     # The Gateway Primary Academy (SHO1), Milestone Road, Dartford, Kent, DA2 6PL
-#     # St Albans Church, St Albans Road, Dartford, Kent, DA1 1TE
-#     # Oakfield Primary Academy (PRI5), Oakfield Lane, Dartford, Kent, DA1 2SW
-
-#     return super().station_record_to_dict(record)
+        return super().station_record_to_dict(record)
