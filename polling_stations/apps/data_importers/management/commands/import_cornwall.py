6ebece37760b28233d32ef5ab480779f80af1b97
polling-bot-4000 <developers@democracyclub.org.uk>
2025-03-26 14:41:33 +0000

    Import script for Cornwall (2025-05-01) (closes #8544)

--- polling_stations/apps/data_importers/management/commands/import_cornwall.py
+++ polling_stations/apps/data_importers/management/commands/import_cornwall.py
@@ -3,64 +3,72 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "CON"
-    addresses_name = "2024-07-04/2024-06-27T11:29:04.482644/UPDATE27062024-Democracy_Club__04July2024.tsv"
-    stations_name = "2024-07-04/2024-06-27T11:29:04.482644/UPDATE27062024-Democracy_Club__04July2024.tsv"
-    elections = ["2024-07-04"]
+    addresses_name = (
+        "2025-05-01/2025-03-26T14:41:05.807414/Democracy_Club__01May2025 1.tsv"
+    )
+    stations_name = (
+        "2025-05-01/2025-03-26T14:41:05.807414/Democracy_Club__01May2025 1.tsv"
+    )
+    elections = ["2025-05-01"]
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
-        # Amendments from previous elections:
-
-        # Methodist Sunday School Room Canworthy Water Launceston PL15 8UW
-        if record.polling_place_id == "16273":
-            record = record._replace(polling_place_uprn="10003300470")
-
-        # The Cove Hall Wilcove Torpoint PL11 2PT
-        if record.polling_place_id == "15768":
-            record = record._replace(polling_place_uprn="10003065058")
-
-        # St Austell Rugby Club Tregorrick Lane St Austell PL26 7FH
-        if record.polling_place_id == "16494":
-            record = record._replace(polling_place_uprn="10002694687")
-
+        # Amendment from previous election:
         # Millbrook Village Hall The Parade Millbrook Torpoint
-        if record.polling_place_id == "15911":
+        if record.polling_place_id == "18055":
             record = record._replace(polling_place_uprn="10023432417")
+
+        # Removing wrong points for:
+        # Trevenson Church Community Hall, Church Road, Pool, Redruth, TR15 3PT
+        if record.polling_place_id == "18651":
+            record = record._replace(
+                polling_place_easting="",
+                polling_place_northing="",
+            )
+
+        # East Taphouse Community Hall, Salts Meadow Road, East Taphouse, Liskeard, PL14 4TG
+        if record.polling_place_id == "18556":
+            record = record._replace(
+                polling_place_easting="",
+                polling_place_northing="",
+                polling_place_uprn="",
+            )
 
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
+
         if uprn in [
-            "10094078885",  # CARAVAN 5A GWINEAR DOWNS, LEEDSTOWN
-            "10003919329",  # LOWER ORCHARD, ADIT LANE, SOUTH PILL, SALTASH
-            "10002045559",  # THE ORCHARD, ADIT LANE, SOUTH PILL, SALTASH
+            "100040012372",  # REDCLIFFE HOUSE, ST. MARTINS ROAD, LOOE
         ]:
             return None
+
         if record.addressline6 in [
             # split
-            "TR11 4PT",
-            "PL11 2HW",
-            "TR26 1EZ",
+            "EX23 9FQ",
             "PL15 7BJ",
-            "TR27 4AG",
-            "TR18 5NA",
+            "PL11 2HW",
             "TR27 4RZ",
             "TR8 4NR",
-            "TR26 2AQ",
-            "PL32 9UN",
+            "TR18 3NH",
             "TR16 5QD",
-            "PL15 7SE",
+            "TR27 4AG",
+            "TR20 8TG",
+            "TR11 4PT",
             "PL18 9BJ",
             "PL30 3DH",
-            "TR19 6BH",
             "PL25 4EL",
-            "TR20 8TG",
-            "PL15 8RF",
-            "TR2 5JS",
-            "TR18 3NH",
+            "TR10 9LJ",
             "PL31 2PB",
+            "PL11 3DA",
+            "TR2 5JS",
+            "TR18 3NA",
+            "PL32 9UN",
+            "TR18 5NA",
+            "PL15 8RF",
+            "PL15 7SE",
         ]:
             return None
         return super().address_record_to_dict(record)
