e3e6655bd547da0ca7db3ae3cee17530a56bddd5
polling-bot-4000 <developers@democracyclub.org.uk>
2025-03-26 14:28:56 +0000

    Import script for Chichester (2025-05-01) (closes #8542)

--- polling_stations/apps/data_importers/management/commands/import_chichester.py
+++ polling_stations/apps/data_importers/management/commands/import_chichester.py
@@ -3,43 +3,40 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "CHI"
-    addresses_name = "2024-07-04/2024-05-29T08:24:22.907129/chichester_combined.csv"
-    stations_name = "2024-07-04/2024-05-29T08:24:22.907129/chichester_combined.csv"
-    elections = ["2024-07-04"]
+    addresses_name = (
+        "2025-05-01/2025-03-26T14:28:46.054551/Democracy_Club__01May2025.CSV"
+    )
+    stations_name = (
+        "2025-05-01/2025-03-26T14:28:46.054551/Democracy_Club__01May2025.CSV"
+    )
+    elections = ["2025-05-01"]
 
+    # By-election so maintaining exclusions from previous elections as comment for reference
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if (
             uprn
             in [
-                "100062411430",  # CONNOLLY HOUSE, CONNOLLY WAY, CHICHESTER
-                "10090340673",  # FLAT, CO-OPERATIVE RETAIL SERVICES LTD, OLIVER WHITBY ROAD, CHICHESTER
-                "10008884574",  # PADDOCK HOUSE, PLAISTOW ROAD, LOXWOOD, BILLINGSHURST
-                "10002469404",  # NEWLANDS COTTAGE, LINCHMERE, HASLEMERE
-                "10008887597",  # THE GRANARY UPWALTHAM HOUSE FARM CHURCH FARM LANE, UPWALTHAM
-                "200001740959",  # BADGERS, BORDER CLOSE, HILL BROW, LISS
-                "10002467828",  # PALFREY FARM, LONDON ROAD, PETWORTH
-                "10014107960",  # HORSESHOE HOUSE, HENLEY HILL, HENLEY, HASLEMERE
-                "100061753348",  # GIG HOUSE, STUBCROFT LANE, EAST WITTERING, CHICHESTER
+                # "100062411430",  # CONNOLLY HOUSE, CONNOLLY WAY, CHICHESTER
+                # "10090340673",  # FLAT, CO-OPERATIVE RETAIL SERVICES LTD, OLIVER WHITBY ROAD, CHICHESTER
+                # "10008884574",  # PADDOCK HOUSE, PLAISTOW ROAD, LOXWOOD, BILLINGSHURST
+                # "10002469404",  # NEWLANDS COTTAGE, LINCHMERE, HASLEMERE
+                # "10008887597",  # THE GRANARY UPWALTHAM HOUSE FARM CHURCH FARM LANE, UPWALTHAM
+                # "200001740959",  # BADGERS, BORDER CLOSE, HILL BROW, LISS
+                # "10002467828",  # PALFREY FARM, LONDON ROAD, PETWORTH
+                # "10014107960",  # HORSESHOE HOUSE, HENLEY HILL, HENLEY, HASLEMERE
+                # "100061753348",  # GIG HOUSE, STUBCROFT LANE, EAST WITTERING, CHICHESTER
             ]
         ):
             return None
 
         if record.addressline6 in [
             # split
-            "PO18 0PR",
-            "PO19 3PX",
             "GU29 9QT",
-            "PO18 0PR",
-            "PO18 8QG",
-            "GU28 0LD",
-            "RH20 1PW",
-            "PO20 9AD",
-            "PO19 7QL",
             #  suspect
-            "PO20 8NX",
-            "PO20 8SP",
+            # "PO20 8NX",
+            # "PO20 8SP",
         ]:
             return None
 
