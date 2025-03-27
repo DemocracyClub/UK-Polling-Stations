b7857c5b8ecb70c72fe920e7581634576569ab06
awdem <wdemontmollin@gmail.com>
2025-03-27 14:27:00 +0000

    station change from ERY council

--- polling_stations/apps/data_importers/management/commands/import_east_riding.py
+++ polling_stations/apps/data_importers/management/commands/import_east_riding.py
@@ -29,3 +29,21 @@ class Command(BaseDemocracyCountsCsvImporter):
             return None
 
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # Station Change from council:
+        # Old station: Cowden Holiday Park, Main Road, Cowden HU11 4UD
+        # New Station: The Old Post Office, Cliff Lane, Mappleton, Hornsea, East Riding of Yorkshire, HU18 1XX
+        if record.stationcode == "173":
+            record = record._replace(
+                add1="The Old Post Office",
+                add2="Cliff Lane",
+                add3="Mappleton",
+                add4="Hornsea",
+                placename="OLD POST OFFICE - MAPPLETON",
+                postcode="HU18 1XX",
+                xordinate="522629",
+                yordinate="443830",
+            )
+
+        return super().station_record_to_dict(record)
