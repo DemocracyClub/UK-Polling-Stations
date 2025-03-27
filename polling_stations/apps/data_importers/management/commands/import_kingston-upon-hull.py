19e4f4c7de6720606f7939ca5069c4e7d80063f4
polling-bot-4000 <developers@democracyclub.org.uk>
2025-03-27 11:43:42 +0000

    Import script for Hull (2025-05-01) (closes #8552)

--- polling_stations/apps/data_importers/management/commands/import_kingston-upon-hull.py
+++ polling_stations/apps/data_importers/management/commands/import_kingston-upon-hull.py
@@ -3,13 +3,9 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "KHL"
-    addresses_name = (
-        "2024-07-04/2024-06-24T11:06:48.123709/Democracy_Club__04July2024.tsv"
-    )
-    stations_name = (
-        "2024-07-04/2024-06-24T11:06:48.123709/Democracy_Club__04July2024.tsv"
-    )
-    elections = ["2024-07-04"]
+    addresses_name = "2025-05-01/2025-03-27T11:43:28.234897/Democracy Club.tsv"
+    stations_name = "2025-05-01/2025-03-27T11:43:28.234897/Democracy Club.tsv"
+    elections = ["2025-05-01"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -18,17 +14,14 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         if uprn in [
             "21133711",  # FLAT OVER 19 18-19 WITHAM, KINGSTON UPON HULL
             "21133712",  # FLAT OVER 121 121-127 WITHAM, KINGSTON UPON HULL
-            "10093952515",  # 321A BEVERLEY ROAD, HULL
-            "10093952516",  # 323A BEVERLEY ROAD, HULL
-            "21051039",  # 86 HOWDALE ROAD, HULL
         ]:
             return None
 
         if record.addressline6 in [
             # split
-            "HU5 2RH",
-            "HU5 3LT",
             "HU5 5NT",
+            "HU5 3LT",
+            "HU5 2RH",
         ]:
             return None
 
