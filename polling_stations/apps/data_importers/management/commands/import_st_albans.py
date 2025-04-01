2bd5787bf0e939f54a08e34100bfba466a80254e
polling-bot-4000 <developers@democracyclub.org.uk>
2025-04-01 10:09:07 +0100

    Import script for St Albans (2025-05-01) (closes #8564)

--- polling_stations/apps/data_importers/management/commands/import_st_albans.py
+++ polling_stations/apps/data_importers/management/commands/import_st_albans.py
@@ -3,33 +3,7 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SAL"
-    addresses_name = (
-        "2025-05-01/2025-02-26T08:45:19.492509/Democracy_Club__01May2025.tsv"
-    )
-    stations_name = (
-        "2025-05-01/2025-02-26T08:45:19.492509/Democracy_Club__01May2025.tsv"
-    )
+    addresses_name = "2025-05-01/2025-04-01T10:09:01.084097/Democracy_Club__01May2025_Redbourn by election.tsv"
+    stations_name = "2025-05-01/2025-04-01T10:09:01.084097/Democracy_Club__01May2025_Redbourn by election.tsv"
     elections = ["2025-05-01"]
     csv_delimiter = "\t"
-
-    def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "200003633693",  # WOOD END, HATCHING GREEN, HARPENDEN
-            "100081134635",  # PLAISTOWES FARM, NOKE LANE, ST. ALBANS
-        ]:
-            return None
-
-        if record.addressline6 in [
-            # splits
-            "AL4 0LD",
-            "AL1 3NS",
-            "AL3 5PR",
-            "AL3 5PD",
-            # suspect
-            "AL1 5NT",
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
