8bd1309d06f3a86bae13baa7f283d4eb78b19216
polling-bot-4000 <developers@democracyclub.org.uk>
2025-04-02 16:19:51 +0100

    Import script for Brighton and Hove (2025-05-01) (closes #8570)

--- polling_stations/apps/data_importers/management/commands/import_brighton_hove.py
+++ polling_stations/apps/data_importers/management/commands/import_brighton_hove.py
@@ -3,34 +3,23 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "BNH"
-    addresses_name = "2024-07-04/2024-05-28T14:03:04.608768/Brighton & Hove Democracy_Club__04July2024.tsv"
-    stations_name = "2024-07-04/2024-05-28T14:03:04.608768/Brighton & Hove Democracy_Club__04July2024.tsv"
-    elections = ["2024-07-04"]
+    addresses_name = (
+        "2025-05-01/2025-04-02T16:19:44.317384/Democracy_Club__01May2025 (9).tsv"
+    )
+    stations_name = (
+        "2025-05-01/2025-04-02T16:19:44.317384/Democracy_Club__01May2025 (9).tsv"
+    )
+    elections = ["2025-05-01"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "22031382",  # COURT FARM HOUSE COURT FARM DEVIL'S DYKE ROAD, HOVE
-            "22175494",  # THE LODGE, BRIGHTON RACE COURSE, FRESHFIELD ROAD, BRIGHTON
             "22272586",  # 1 GREEN MEWS, 53 HOGARTH ROAD, HOVE
             "22272587",  # 2 GREEN MEWS, 53 HOGARTH ROAD, HOVE
             "22272588",  # 3 GREEN MEWS, 53 HOGARTH ROAD, HOVE
             "22279648",  # 184C PORTLAND ROAD, HOVE
-            "22057837",  # 260 LONDON ROAD, PRESTON, BRIGHTON
-            "22057280",  # 90 PEACOCK LANE, BRIGHTON
-        ]:
-            return None
-
-        if record.addressline6 in [
-            # split
-            "BN2 9PA",
-            "BN2 7AB",
-            "BN1 8NF",
-            "BN1 3AE",
-            # suspect
-            "BN4 12PL",  # MILE OAK ROAD, PORTSLADE, BRIGHTON
         ]:
             return None
 
