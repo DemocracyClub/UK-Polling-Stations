69a8374b56df3586f1eae90e629dcaec34d6d6bb
polling-bot-4000 <developers@democracyclub.org.uk>
2025-04-07 11:04:51 +0100

    Import script for Wyre Forest (2025-05-01) (closes #8577)

--- polling_stations/apps/data_importers/management/commands/import_wyre_forest.py
+++ polling_stations/apps/data_importers/management/commands/import_wyre_forest.py
@@ -3,24 +3,22 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WYE"
-    addresses_name = (
-        "2024-07-04/2024-06-24T10:18:50.159000/Democracy_Club__04July2024.CSV"
-    )
-    stations_name = (
-        "2024-07-04/2024-06-24T10:18:50.159000/Democracy_Club__04July2024.CSV"
-    )
-    elections = ["2024-07-04"]
+    addresses_name = "2025-05-01/2025-04-07T11:04:37.863398/Wyre Forest Democracy_Club__01May2025.CSV"
+    stations_name = "2025-05-01/2025-04-07T11:04:37.863398/Wyre Forest Democracy_Club__01May2025.CSV"
+    elections = ["2025-05-01"]
 
     def address_record_to_dict(self, record):
         if record.addressline6 in [
             # split
-            "DY10 1LS",
-            "DY10 3TF",
-            "DY10 3HJ",
             "DY11 5QT",
+            "DY10 3TF",
+            "DY13 8XF",
+            "DY10 1LS",
             "DY12 2TN",
+            "DY10 3HJ",
             # suspect
-            "DY10 2FG",  # MILL PARK MEWS, KIDDERMINSTER
+            "DY10 2FG",
+            "DY11 6FG",
         ]:
             return None
         return super().address_record_to_dict(record)
