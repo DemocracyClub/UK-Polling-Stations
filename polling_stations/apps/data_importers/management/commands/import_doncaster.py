412725ba874af63e4f8a201a74c0ade3aa8f4f17
polling-bot-4000 <developers@democracyclub.org.uk>
2025-04-07 13:03:29 +0100

    Import script for City of Doncaster (2025-05-01) (closes #8579)

--- polling_stations/apps/data_importers/management/commands/import_doncaster.py
+++ polling_stations/apps/data_importers/management/commands/import_doncaster.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "DNC"
     addresses_name = (
-        "2024-07-04/2024-06-18T10:42:04.528561/Democracy_Club__04July2024.tsv"
+        "2025-05-01/2025-04-07T13:03:16.237741/Democracy_Club__01May2025.tsv"
     )
     stations_name = (
-        "2024-07-04/2024-06-18T10:42:04.528561/Democracy_Club__04July2024.tsv"
+        "2025-05-01/2025-04-07T13:03:16.237741/Democracy_Club__01May2025.tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2025-05-01"]
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
 
@@ -22,23 +22,28 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "10006574333",  # FIRST FLOOR 187A URBAN ROAD, HEXTHORPE, DONCASTER
             "10095783014",  # BEAWOOD HOUSE, GREAT NORTH ROAD, BAWTRY, DONCASTER
             "100051982929",  # 5 REX CORNER, BROXHOLME LANE, DONCASTER
+            "100050726352",  # THE HUGGINS, GATE WOOD LANE, HATFIELD, DONCASTER
+            "100050804695",  # CLIFTON CHASE, COMMON LANE, CLIFTON, MALTBY, ROTHERHAM
+            "10094838488",  # 80 DONCASTER ROAD, CONISBROUGH, DONCASTER
+            "100050704471",  # THE BUNGALOW, BURCROFT HILL, CONISBROUGH, DONCASTER
+            "10006712461",  # 41 ORCHARD STREET, THORNE, DONCASTER
+            "10006712462",  # 43 ORCHARD STREET, THORNE, DONCASTER
         ]:
             return None
 
         if record.addressline6 in [
             # splits
-            "DN7 6AQ",
-            "DN7 5DG",
-            "DN4 7BQ",
-            "DN8 5AD",
-            "S64 0DE",
-            "DN7 6PW",
-            "DN3 2AX",
-            "DN6 9JU",
-            "DN4 8ES",
-            "DN5 7DP",
-            "S64 0LL",
             "DN5 0QW",
+            "DN6 9JU",
+            "DN4 7BQ",
+            "S64 0DE",
+            "S64 0LL",
+            "DN8 5AD",
+            "DN7 5DG",
+            "DN5 7DP",
+            "DN4 8ES",
+            "DN3 2AX",
+            "DN7 6PW",
             # suspect
             "DN9 3HF",
             "DN4 7AL",
