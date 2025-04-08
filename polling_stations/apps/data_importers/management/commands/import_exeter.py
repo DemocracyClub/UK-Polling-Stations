e7c919718e911e77ffb78fb9573e3aade9aeb657
polling-bot-4000 <developers@democracyclub.org.uk>
2025-04-08 11:14:12 +0100

    Import script for Exeter (2025-05-01) (closes #8583)

--- polling_stations/apps/data_importers/management/commands/import_exeter.py
+++ polling_stations/apps/data_importers/management/commands/import_exeter.py
@@ -4,32 +4,28 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "EXE"
     addresses_name = (
-        "2024-07-04/2024-06-10T15:56:47.388239/Democracy_Club__04July2024 (15).tsv"
+        "2025-05-01/2025-04-10T12:52:21.787573/Democracy_Club__01May2025.CSV"
     )
     stations_name = (
-        "2024-07-04/2024-06-10T15:56:47.388239/Democracy_Club__04July2024 (15).tsv"
+        "2025-05-01/2025-04-10T12:52:21.787573/Democracy_Club__01May2025.CSV"
     )
-    elections = ["2024-07-04"]
-    csv_delimiter = "\t"
+    elections = ["2025-05-01"]
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "10094735127",  # 1 COLLINGWOOD CLOSE, TOPSHAM, EXETER
-            "10094735128",  # 2 COLLINGWOOD CLOSE, TOPSHAM, EXETER
             "10023121146",  # 210A TOPSHAM ROAD, EXETER
             "10091473295",  # EXE VIEW LODGE, STOKE HILL, EXETER
-            "10094202708",  # THE OLD MESS ROOM, ST. MARKS AVENUE, EXETER
         ]:
             return None
 
         if record.addressline6 in [
             # split
-            "EX2 7AY",
-            "EX4 9HE",
-            "EX4 5AJ",
             "EX2 7TF",
+            "EX2 7AY",
+            "EX4 5AJ",
+            "EX4 9HE",
         ]:
             return None
         return super().address_record_to_dict(record)
