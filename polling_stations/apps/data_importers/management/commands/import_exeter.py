780eb5cddfa82f5fc5cc5e0a77803c2db8adf0bf
polling-bot-4000 <developers@democracyclub.org.uk>
2025-04-10 12:52:34 +0100

    Import script for Exeter (2025-05-01) (closes #8583)

--- polling_stations/apps/data_importers/management/commands/import_exeter.py
+++ polling_stations/apps/data_importers/management/commands/import_exeter.py
@@ -4,9 +4,9 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "EXE"
     addresses_name = (
-        "2025-05-01/2025-04-08T11:14:05.411381/Democracy_Club__01May2025.CSV"
+        "2025-05-01/2025-04-10T12:52:21.787573/Democracy_Club__01May2025.CSV"
     )
     stations_name = (
-        "2025-05-01/2025-04-08T11:14:05.411381/Democracy_Club__01May2025.CSV"
+        "2025-05-01/2025-04-10T12:52:21.787573/Democracy_Club__01May2025.CSV"
     )
     elections = ["2025-05-01"]
