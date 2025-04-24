197b13abf9098e12f0ee12a1f83d2b5ce39656af
awdem <wdemontmollin@gmail.com>
2025-04-24 14:57:12 +0100

    station change from NKE council

--- polling_stations/apps/data_importers/management/commands/import_north_kesteven.py
+++ polling_stations/apps/data_importers/management/commands/import_north_kesteven.py
@@ -20,3 +20,20 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             return None
 
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # Station change from council:
+        # old: Little Hale, Parva House (Small Hall), Main Road, Little Hale, Sleaford NG34 9BA
+        # new: Little Hale Methodist Church, Chapel Lane, Little Hale, Sleaford NG34 9BE
+        if record.polling_place_id == "9836":
+            record = record._replace(
+                polling_place_name="Little Hale Methodist Church",
+                polling_place_address_1="Chapel Lane",
+                polling_place_address_2="Little Hale",
+                polling_place_address_3="Sleaford",
+                polling_place_address_4="",
+                polling_place_postcode="NG34 9BE",
+                polling_place_easting="0",
+                polling_place_northing="0",
+            )
+        return super().station_record_to_dict(record)
