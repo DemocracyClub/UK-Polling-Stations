a3a1268e08abe248c66826e53bee1ca8d63f73e9
chris48s <git@chris-shaw.dev>
2025-05-13 08:47:44 +0100

    keep old fixes as a comment

--- polling_stations/apps/data_importers/management/commands/import_mansfield.py
+++ polling_stations/apps/data_importers/management/commands/import_mansfield.py
@@ -11,3 +11,47 @@ class Command(BaseXpressDemocracyClubCsvImporter):
     )
     elections = ["2025-06-12"]
     csv_delimiter = "\t"
+
+
+"""
+By-election, Maintaining unused script changes as comment for future reference:
+
+    def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "10091487792",  # 65 CROMWELL STREET, MANSFIELD
+            "10023935274",  # 31 BIRCHLANDS, FOREST TOWN, MANSFIELD
+            "10096388538",  # 72 BIFROST BOULEVARD, WARSOP
+            "10096388523",  # 17 KEMPTON ROAD, MANSFIELD
+        ]:
+            return None
+
+        if record.addressline6 in [
+            # splits
+            "NG19 6AT",
+            # looks wrong
+            "NG18 4TG",
+            # Council confirmed correct: NG18 6BH
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # The counci; has supplied missing postcodes for the following stations:
+        # Meden Vale Methodist Church, Meden Vale
+        if record.polling_place_id == "932":
+            record = record._replace(polling_place_postcode="NG20 9PT")
+        # Friends Meeting House, Mansfield
+        if record.polling_place_id == "931":
+            record = record._replace(polling_place_postcode="NG19 6AB")
+        # Carr Bank Wedding Venue, Carr Bank Park, Windmill Lane, Mansfield
+        if record.polling_place_id == "743":
+            record = record._replace(polling_place_postcode="NG18 2AL")
+        # Langford Road Community Centre, Mansfield
+        if record.polling_place_id == "989":
+            record = record._replace(polling_place_postcode="NG19 6QD")
+
+        return super().station_record_to_dict(record)
+"""
