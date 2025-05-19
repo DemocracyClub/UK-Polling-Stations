5146f24ac37cf9cdc04bc5e2d2e68f446cca5dd4
chris48s <git@chris-shaw.dev>
2025-05-19 09:05:01 +0100

    apply individual address corrections
    there are several of these postcodes/UPRNs in the
    input data for these by-elections
    just apply them all

--- polling_stations/apps/data_importers/management/commands/import_highland.py
+++ polling_stations/apps/data_importers/management/commands/import_highland.py
@@ -11,3 +11,42 @@ class Command(BaseDemocracyCountsCsvImporter):
     )
     elections = ["2025-06-19"]
     csv_encoding = "utf-16le"
+
+    def station_record_to_dict(self, record):
+        # # correcting obviously wrong point for:
+        # # CROWN CHURCH, INVERNESS, KINGSMILLS ROAD, INVERNESS
+        # if record.stationcode in [
+        #     "I085",
+        #     "I086",
+        # ]:
+        #     record = record._replace(xordinate="267087")
+
+        # # SCOURIE COMMUNITY HALL, SCOURIE, LAIRG, SUTHERLAND, IV27 4TE
+        # if record.stationcode == "C006":
+        #     record = record._replace(xordinate="", yordinate="")
+
+        return super().station_record_to_dict(record)
+
+    def address_record_to_dict(self, record):
+        uprn = record.uprn.strip().lstrip("0")
+
+        if (
+            uprn
+            in [
+                "130150387",  # MILL COTTAGE A863 B884 JUNCTION - A850 JUNCTION, KILMUIR, DUNVEGAN
+                "130186635",  # 1 MACFARLANE BUILDINGS, CRUACHAN PLACE, PORTREE
+                "130131942",  # 3 WOODLAND PARK, CONTIN
+                "130178830",  # 12 COUNTY PLACE, CULDUTHEL, INVERNESS
+                "130197101",  # 4 BLACK ISLE VIEW, STRATTON, INVERNESS
+                "130147608",  # NEWLANDS OF URCHANY, NAIRN
+            ]
+        ):
+            return None
+
+        if record.postcode in [
+            # split
+            "IV17 0QY",
+            "IV23 2RW",
+        ]:
+            return None
+        return super().address_record_to_dict(record)
