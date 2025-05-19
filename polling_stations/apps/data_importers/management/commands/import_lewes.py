1da0e0d82ef663bd801ddbe974e24d1e15750940
chris48s <git@chris-shaw.dev>
2025-05-19 13:41:05 +0100

    keep uprn fixes

--- polling_stations/apps/data_importers/management/commands/import_lewes.py
+++ polling_stations/apps/data_importers/management/commands/import_lewes.py
@@ -6,3 +6,18 @@ class Command(BaseHalaroseCsvImporter):
     addresses_name = "2025-05-29/2025-05-19T11:04:34.585409/Eros_SQL_Output001.csv"
     stations_name = "2025-05-29/2025-05-19T11:04:34.585409/Eros_SQL_Output001.csv"
     elections = ["2025-05-29"]
+
+    def address_record_to_dict(self, record):
+        uprn = record.uprn.strip().lstrip("0")
+
+        if (
+            uprn
+            in [
+                "10033272374",  # 24A STEYNING AVENUE, PEACEHAVEN
+                "200001466148",  # OLD WHEEL COTTAGE, EASTERN ROAD, WIVELSFIELD GREEN, HAYWARDS HEATH
+                "100062487484",  # WOODS COTTAGE, EASTERN ROAD, WIVELSFIELD GREEN, HAYWARDS HEATH
+            ]
+        ):
+            return None
+
+        return super().address_record_to_dict(record)
