fb93dc79151527529e030b0060f57ffdbf95b5ea
polling-bot-4000 <developers@democracyclub.org.uk>
2025-05-19 11:04:40 +0100

    Import script for Lewes (2025-05-29) (closes #8636)

--- polling_stations/apps/data_importers/management/commands/import_lewes.py
+++ polling_stations/apps/data_importers/management/commands/import_lewes.py
@@ -3,21 +3,6 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "LEE"
-    addresses_name = "2024-07-04/2024-06-19T16:26:19.708577/LEE_combined.csv"
-    stations_name = "2024-07-04/2024-06-19T16:26:19.708577/LEE_combined.csv"
-    elections = ["2024-07-04"]
-
-    def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
-
-        if (
-            uprn
-            in [
-                "10033272374",  # 24A STEYNING AVENUE, PEACEHAVEN
-                "200001466148",  # OLD WHEEL COTTAGE, EASTERN ROAD, WIVELSFIELD GREEN, HAYWARDS HEATH
-                "100062487484",  # WOODS COTTAGE, EASTERN ROAD, WIVELSFIELD GREEN, HAYWARDS HEATH
-            ]
-        ):
-            return None
-
-        return super().address_record_to_dict(record)
+    addresses_name = "2025-05-29/2025-05-19T11:04:34.585409/Eros_SQL_Output001.csv"
+    stations_name = "2025-05-29/2025-05-19T11:04:34.585409/Eros_SQL_Output001.csv"
+    elections = ["2025-05-29"]
