923f558c232a5958160045d76e1c76a35e20eb4c
chris48s <git@chris-shaw.dev>
2025-05-29 11:19:04 +0100

    don't overwrite tracking params with empty dict

--- polling_stations/apps/data_finder/middleware.py
+++ polling_stations/apps/data_finder/middleware.py
@@ -12,4 +12,5 @@ class UTMTrackerMiddleware(object):
 
         keys = ("utm_source", "utm_medium", "utm_campaign")
         utm_data = {k: v for k, v in map(_get_value_from_req, keys) if v}
-        request.session["utm_data"] = utm_data
+        if not request.session.get("utm_data"):
+            request.session["utm_data"] = utm_data
