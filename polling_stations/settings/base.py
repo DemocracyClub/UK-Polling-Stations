8426040fcef17dee5720898e90706afe8357c643
GeoWill <roper.william@gmail.com>
2025-06-12 10:45:26 +0100

    Drop django-database-url

--- polling_stations/settings/base.py
+++ polling_stations/settings/base.py
@@ -51,9 +51,6 @@ DATABASES = {
     }
 }
 
-# DATABASES["default"] = dj_database_url.config()
-# DATABASES["default"]["ENGINE"] = "django.contrib.gis.db.backends.postgis"
-
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = [
