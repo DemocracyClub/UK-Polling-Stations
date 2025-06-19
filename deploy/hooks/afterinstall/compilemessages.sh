f1cc25ffa8831a525eac9ce7adc9ec15a0f32c0c
GeoWill <roper.william@gmail.com>
2025-06-19 10:41:44 +0100

    Use uv in code deploy

--- deploy/hooks/afterinstall/compilemessages.sh
+++ deploy/hooks/afterinstall/compilemessages.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -xeE
 
-source /var/www/polling_stations/code/venv/bin/activate
 cd /var/www/polling_stations/code/
-python manage.py compilemessages
+uv run python manage.py compilemessages
--- deploy/hooks/afterinstall/install_python_deps.sh
+++ deploy/hooks/afterinstall/install_python_deps.sh
@@ -4,13 +4,5 @@ set -xeE
 
 # Create a virtualenv
 cd /var/www/polling_stations/code/
-python3 -m venv venv
 
-# Activate the virtualenv
-source /var/www/polling_stations/code/venv/bin/activate
-
-# Upgrade pip
-pip install --upgrade pip
-
-# Install requirements
-pip install --requirement /var/www/polling_stations/code/requirements/base.txt --requirement /var/www/polling_stations/code/requirements/production.txt
+uv sync --group production --no-group dev --no-group cdk
