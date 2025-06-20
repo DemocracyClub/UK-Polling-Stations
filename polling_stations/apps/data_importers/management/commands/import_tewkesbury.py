f7d1c7088f2e143d9b9711915bbfb9b440009cc9
polling-bot-4000 <developers@democracyclub.org.uk>
2025-06-20 10:37:55 +0100

    Import script for Tewkesbury (2025-07-10) (closes #8660)

--- polling_stations/apps/data_importers/management/commands/import_tewkesbury.py
+++ polling_stations/apps/data_importers/management/commands/import_tewkesbury.py
@@ -4,13 +4,10 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "TEW"
     addresses_name = (
-        "2025-05-01/2025-04-08T17:38:02.752651/Democracy_Club__01May2025.tsv"
+        "2025-07-10/2025-06-20T10:37:50.424790/Democracy_Club__10July2025.tsv"
     )
     stations_name = (
-        "2025-05-01/2025-04-08T17:38:02.752651/Democracy_Club__01May2025.tsv"
+        "2025-07-10/2025-06-20T10:37:50.424790/Democracy_Club__10July2025.tsv"
     )
-    elections = ["2025-05-01"]
+    elections = ["2025-07-10"]
     csv_delimiter = "\t"
-
-    # Checked and no correction needed:
-    # WARNING: Polling station The Edge Community Centre (10774) is in Stroud District Council (STO)
