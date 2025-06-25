6f742155e9bfc3f3ec6ba755d8d865327633277d
GeoWill <roper.william@gmail.com>
2025-06-25 14:15:02 +0100

    fixup! Use uv in code deploy

--- deploy/hooks/afterinstall/collectstatic.sh
+++ deploy/hooks/afterinstall/collectstatic.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -xeE
 
-source /var/www/polling_stations/code/.venv/bin/activate
-python /var/www/polling_stations/code/manage.py collectstatic --noinput --clear
+uv run python /var/www/polling_stations/code/manage.py collectstatic --noinput --clear
