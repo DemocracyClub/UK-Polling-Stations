b902ce4a99427897843797bb0c924033e9a51df3
polling-bot-4000 <developers@democracyclub.org.uk>
2025-07-28 09:31:00 +0100

    Import script for City of Cardiff (2025-08-14) (closes #8680)

--- polling_stations/apps/data_importers/management/commands/import_cardiff.py
+++ polling_stations/apps/data_importers/management/commands/import_cardiff.py
@@ -4,87 +4,9 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "CRF"
     addresses_name = (
-        "2025-08-14/2025-07-28T08:32:29.634802/Democracy_Club__14August2025.CSV"
+        "2025-08-14/2025-07-28T09:30:53.569803/Democracy_Club__14August2025.CSV"
     )
     stations_name = (
-        "2025-08-14/2025-07-28T08:32:29.634802/Democracy_Club__14August2025.CSV"
+        "2025-08-14/2025-07-28T09:30:53.569803/Democracy_Club__14August2025.CSV"
     )
     elections = ["2025-08-14"]
-
-    # Maintaining GE 2024 exclusions as comments for future reference:
-
-    def station_record_to_dict(self, record):
-        # # Community Suite, Llanishen Leisure Centre, Ty Glas Avenue, Llanishen, Cardiff
-        # if record.polling_place_id == "21256":
-        #     # geocode was a way off, postcode was right, but found the building so here it is anyway
-        #     record = record._replace(polling_place_uprn="10002526454")
-
-        # # Canolfan Beulah, (Church Community Centre), Beulah Crossroads, Rhiwbina, Cardiff, CF14 6AX
-        # if record.polling_place_id == "21192":
-        #     record = record._replace(polling_place_postcode="CF14 6LT")
-
-        # # The Church Hall, Kelston Road, Whitchurch, Cardiff
-        # if record.polling_place_id == "21205":
-        #     record = record._replace(polling_place_uprn="200001850852")
-
-        # # St Mary`s Church Hall, Church Road, Cardiff, CF14 2ED
-        # # Council request to use below postcode, ignore the warning
-        # if record.polling_place_id == "21208":
-        #     record = record._replace(
-        #         polling_place_postcode="CF14 2DX", polling_place_uprn="10008903814"
-        #     )
-
-        return super().station_record_to_dict(record)
-
-    def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if (
-            uprn
-            in [
-                # "100100127979",  # 17 LON-Y-RHYD, CARDIFF
-                # "100100110392",  # 10 SOMERSET COURT, BURNHAM AVENUE, LLANRUMNEY, CARDIFF
-                # "100100107993",  # 890 NEWPORT ROAD, RUMNEY, CARDIFF
-                # "100100107313",  # 45 LLANSTEPHAN ROAD, RUMNEY, CARDIFF
-                # "10002524512",  # CLUB HOUSE RUMNEY R F C HARTLAND ROAD, LLANRUMNEY, CARDIFF
-                # "10008904957",  # OAK HOUSE, 340 NEWPORT ROAD, CARDIFF
-                # "100100066910",  # 1 WHITMUIR ROAD, CARDIFF
-                # "10090486882",  # SECOND FLOOR DOCK CHAMBERS 5 BUTE STREET, BUTETOWN, CARDIFF
-                # "200002932357",  # 46 AMHERST STREET, CARDIFF
-                # "200002932358",  # 48 AMHERST STREET, CARDIFF
-                # "200002932356",  # 44 AMHERST STREET, CARDIFF
-                # "10090488749",  # 456A COWBRIDGE ROAD EAST, CARDIFF
-                # "10092985344",  # FIRST FLOOR FLAT 452 COWBRIDGE ROAD EAST, CANTON, CARDIFF
-                # "100101043751",  # VVV RETAIL LTD, THE CABIN, WAUNGRON ROAD, CARDIFF
-                # "100100094616",  # CROSSLANDS CHILDRENS UNIT, 318 COWBRIDGE ROAD WEST, CARDIFF
-                # "100100098970",  # 144 SNOWDEN ROAD, CARDIFF
-                # "100100094085",  # 2 CAERWENT ROAD, CARDIFF
-                # "10002507423",  # MAES Y LLECH FARM, RADYR, CARDIFF
-                # "100100127342",  # 145 HEOL Y DERI, CARDIFF
-                # "100100896720",  # PARK HOUSE, MUIRTON ROAD, CARDIFF
-                # "100100890356",  # UNIVERSITY CATHOLIC CHAPLAINCY, 62 PARK PLACE, CARDIFF
-                # "100100094728",  # 7 CROSSWAYS ROAD, CARDIFF
-                # "100100139505",  # WHITE LODGE, CHURCH ROAD, PENTYRCH, CARDIFF
-                # "10002509001",  # LLANFAIR COURT, ST. Y NYLL LANE, CAPEL LLANILLTERN, CARDIFF
-            ]
-        ):
-            return None
-
-        if (
-            record.addressline6
-            in [
-                # splits
-                # "CF15 8EL",
-                # "CF24 2DG",
-                # looks wrong
-                # "CF3 1XY",
-                # "CF24 2EE",
-                # "CF24 0DF",
-                # "CF11 6BN",
-                # "CF5 5SB",
-                # "CF14 9UA",
-            ]
-        ):
-            return None
-
-        return super().address_record_to_dict(record)
