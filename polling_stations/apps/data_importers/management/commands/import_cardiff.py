4187e5fedfc5043e10f208f3f0561a4accdd2ef2
polling-bot-4000 <developers@democracyclub.org.uk>
2025-08-14 10:54:01 +0100

    Import script for City of Cardiff (2025-09-18) (closes #8693)

--- polling_stations/apps/data_importers/management/commands/import_cardiff.py
+++ polling_stations/apps/data_importers/management/commands/import_cardiff.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "CRF"
     addresses_name = (
-        "2025-08-14/2025-07-28T08:32:29.634802/Democracy_Club__14August2025.CSV"
+        "2025-09-18/2025-08-14T10:53:54.804675/Democracy_Club__18September2025.CSV"
     )
     stations_name = (
-        "2025-08-14/2025-07-28T08:32:29.634802/Democracy_Club__14August2025.CSV"
+        "2025-09-18/2025-08-14T10:53:54.804675/Democracy_Club__18September2025.CSV"
     )
-    elections = ["2025-08-14"]
+    elections = ["2025-09-18"]
 
     # Maintaining GE 2024 exclusions as comments for future reference:
 
