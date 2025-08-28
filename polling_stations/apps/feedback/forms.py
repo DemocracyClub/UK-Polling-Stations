07b23c13cb140f70bfb51d5b647aa88831be87b4
awdem <wdemontmollin@gmail.com>
2025-08-28 13:27:43 +0100

    add feedback form to no election page

--- polling_stations/apps/feedback/forms.py
+++ polling_stations/apps/feedback/forms.py
@@ -3,7 +3,7 @@ import uuid
 from django import forms
 from django.utils.translation import gettext_lazy as _
 
-from .models import FOUND_USEFUL_CHOICES, VOTE_CHOICES, Feedback
+from .models import FOUND_USEFUL_CHOICES, VOTE_CHOICES, Feedback, NoElectionFeedback
 
 
 class FeedbackForm(forms.ModelForm):
@@ -31,3 +31,25 @@ class FeedbackForm(forms.ModelForm):
         widget=forms.Textarea(attrs={"rows": 3, "class": "form-control"}),
         required=False,
     )
+
+
+class NoElectionFeedbackForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(NoElectionFeedbackForm, self).__init__(*args, **kwargs)
+        self.fields["token"].initial = uuid.uuid4().hex
+
+    class Meta:
+        model = NoElectionFeedback
+        fields = ["no_election_feedback_text", "source_url"]
+
+    no_election_feedback_text = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                "rows": 4,
+                "cols": 40,
+            }
+        ),
+        required=True,
+    )
+    source_url = forms.CharField(widget=forms.HiddenInput())
+    token = forms.CharField(widget=forms.HiddenInput())
