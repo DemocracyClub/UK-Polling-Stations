1c78fde20c23d414b4a1e410ffb30ae25b75e358
polling-bot-4000 <developers@democracyclub.org.uk>
2025-09-15 11:51:50 +0100

    Import script for Colchester (2025-10-23) (closes #8711)

--- polling_stations/apps/data_importers/management/commands/import_colchester.py
+++ polling_stations/apps/data_importers/management/commands/import_colchester.py
@@ -4,19 +4,10 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "COL"
     addresses_name = (
-        "2025-02-20/2025-02-11T10:40:31.821365/Democracy_Club__20February2025.tsv"
+        "2025-10-23/2025-09-15T11:51:43.956849/Democracy_Club__23October2025.tsv"
     )
     stations_name = (
-        "2025-02-20/2025-02-11T10:40:31.821365/Democracy_Club__20February2025.tsv"
+        "2025-10-23/2025-09-15T11:51:43.956849/Democracy_Club__23October2025.tsv"
     )
-    elections = ["2025-02-20"]
+    elections = ["2025-10-23"]
     csv_delimiter = "\t"
-
-    def station_record_to_dict(self, record):
-        # Tiptree United Reformed Church Hall, Chapel Road, Tiptree
-        if record.polling_place_id == "14019":
-            record = record._replace(
-                polling_place_easting="590290", polling_place_northing="216096"
-            )
-
-        return super().station_record_to_dict(record)
