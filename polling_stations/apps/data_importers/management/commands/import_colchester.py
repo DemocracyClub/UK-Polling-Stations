feb946ff4b75f0a4fef3ce22a228422b56983faa
awdem <wdemontmollin@gmail.com>
2025-09-23 12:35:20 +0100

    add points for COL stations

--- polling_stations/apps/data_importers/management/commands/import_colchester.py
+++ polling_stations/apps/data_importers/management/commands/import_colchester.py
@@ -11,3 +11,14 @@ class Command(BaseXpressDemocracyClubCsvImporter):
     )
     elections = ["2025-10-23"]
     csv_delimiter = "\t"
+
+    def station_record_to_dict(self, record):
+        # Roman Circus Visitor Centre Roman Circus Walk Colchester CO2 7GZ
+        if record.polling_place_id == "14126":
+            record = record._replace(polling_place_uprn="10024406304")
+
+        # Salvation Army Citadel Butt Road Colchester CO3 3DA
+        if record.polling_place_id == "14131":
+            record = record._replace(polling_place_uprn="10004953252")
+
+        return super().station_record_to_dict(record)
