8c37df6b1b6cafe6e1ca515c2883d32a02f7975c
GeoWill <roper.william@gmail.com>
2025-10-01 16:26:28 +0100

    Log whether user had election
    This commit only adds a small change to log whether the user had an
    election.
    
    It also adds tests to make sure we're logging where we want to:
    
    - non split postcodes should log (n.b. this only happens on redirect to
      AddressView)
    - split postcodes shouldn't log
    - address view should log
    - WeDontKnowView and MultipleCouncilView should log. These are only
      reached if user pick's 'my address isn't in the list' option from the
      addresspicker.

--- polling_stations/apps/data_finder/views.py
+++ polling_stations/apps/data_finder/views.py
@@ -92,6 +92,7 @@ class LogLookUpMixin(object):
             entry = settings.POSTCODE_LOGGER.entry_class(
                 postcode=postcode,
                 dc_product=settings.POSTCODE_LOGGER.dc_product.wdiv,
+                had_election=context.get("has_election", False),
                 **utm_data,
             )
 
