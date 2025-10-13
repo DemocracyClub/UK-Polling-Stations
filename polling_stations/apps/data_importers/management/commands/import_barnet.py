a1c0e04c001c50a9bc492827115a7f1e796d30d2
polling-bot-4000 <developers@democracyclub.org.uk>
2025-10-13 10:16:05 +0100

    Import script for Barnet (2025-10-30) (closes #8728)

--- polling_stations/apps/data_importers/management/commands/import_barnet.py
+++ polling_stations/apps/data_importers/management/commands/import_barnet.py
@@ -3,7 +3,11 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "BNE"
-    addresses_name = "2025-05-15/2025-04-24T10:25:36.116562/Democracy Club - Polling Districts WBE UTF-16LE.csv"
-    stations_name = "2025-05-15/2025-04-24T10:25:36.116562/Democracy Club - Polling Stations WBE UTF-16LE.csv"
-    elections = ["2025-05-15"]
+    addresses_name = (
+        "2025-10-30/2025-10-13T10:15:59.127701/Democracy Club - Polling Districts.csv"
+    )
+    stations_name = (
+        "2025-10-30/2025-10-13T10:15:59.127701/Democracy Club - Polling Stations.csv"
+    )
+    elections = ["2025-10-30"]
     csv_encoding = "utf-16le"
