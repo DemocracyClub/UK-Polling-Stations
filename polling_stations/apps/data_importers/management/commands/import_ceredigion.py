02fc8145f55e6fec87d44ddb49813fe1b18b7cf2
chris48s <git@chris-shaw.dev>
2025-11-21 13:13:31 +0000

    pivot to ceredigion as example

--- polling_stations/apps/data_importers/management/commands/import_ceredigion.py
+++ polling_stations/apps/data_importers/management/commands/import_ceredigion.py
@@ -4,89 +4,50 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "CGN"
     addresses_name = (
-        "2024-07-04/2024-06-14T14:22:31.403452/Democracy Club - Polling Districts.csv"
+        "2024-05-02/2024-03-26T15:55:08.555130/Democracy Club - Polling Districts.csv"
     )
     stations_name = (
-        "2024-07-04/2024-06-14T14:22:31.403452/Democracy Club - Polling Stations.csv"
+        "2024-05-02/2024-03-26T15:55:08.555130/Democracy Club - Polling Stations.csv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2026-05-07"]
     csv_encoding = "utf-16le"
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
 
-        if (
-            uprn
-            in [
-                "49062840",  # TY GWYN, LLANARTH
-                "49049138",  # AWEL Y BRYN, RHYDLEWIS, LLANDYSUL
-                "49069702",  # TYDDYN MEHERYN, RHYDLEWIS, LLANDYSUL
-                "49047840",  # ALMA, TANYGROES, CARDIGAN
-                "49040771",  # TROEDYRHIW, BETHANIA, LLANON
-                "49065819",  # THE WILLOW, CROSS INN, LLANON
-                "49069384",  # TROEDYRHIW CAPELI, CRIBYN, LAMPETER
-                "49075931",  # BRYN BARCUD, GORSGOCH, LLANYBYDDER
-                "49037718",  # CARTREFLE, MYDROILYN, LAMPETER
-                "49062368",  # TY GWALIA, LLWYNYGROES, TREGARON
-                "49061701",  # GER Y NANT, CAPEL BANGOR, ABERYSTWYTH
-                "49040891",  # TYNGWNDWN, LLANFARIAN, ABERYSTWYTH
-                "49065752",  # TY'R IET, CARDIGAN
-                "49063434",  # CARDIGAN SAND & GRAVEL CO LTD, CNWC Y SAESON, PENPARC, CARDIGAN
-                "49047095",  # CRYNGA MAWR, BLAENANNERCH, CARDIGAN
-                "200001099702",  # PLASBRYNIAU, LADY ROAD, BLAENPORTH, CARDIGAN
-                "49048192",  # MAESGWYN, GLYNARTHEN, LLANDYSUL
-                "49038741",  # TEGLAN, CILIAU AERON, LAMPETER
-                "49061980",  # Y DDOL LLAWEN, CWMRHEIDOL, ABERYSTWYTH
-            ]
-        ):
+        if uprn in [
+            "49062840",  # TY GWYN, LLANARTH
+            "49049138",  # AWEL Y BRYN, RHYDLEWIS, LLANDYSUL
+            "49069702",  # TYDDYN MEHERYN, RHYDLEWIS, LLANDYSUL
+            "49047840",  # ALMA, TANYGROES, CARDIGAN
+            "49040771",  # TROEDYRHIW, BETHANIA, LLANON
+            "49065819",  # THE WILLOW, CROSS INN, LLANON
+            "49069384",  # TROEDYRHIW CAPELI, CRIBYN, LAMPETER
+            "49075931",  # BRYN BARCUD, GORSGOCH, LLANYBYDDER
+            "49037718",  # CARTREFLE, MYDROILYN, LAMPETER
+            "49062368",  # TY GWALIA, LLWYNYGROES, TREGARON
+            "49061701",  # GER Y NANT, CAPEL BANGOR, ABERYSTWYTH
+            "49040891",  # TYNGWNDWN, LLANFARIAN, ABERYSTWYTH
+            "49065752",  # TY'R IET, CARDIGAN
+            "49063434",  # CARDIGAN SAND & GRAVEL CO LTD, CNWC Y SAESON, PENPARC, CARDIGAN
+            "49047095",  # CRYNGA MAWR, BLAENANNERCH, CARDIGAN
+            "200001099702",  # PLASBRYNIAU, LADY ROAD, BLAENPORTH, CARDIGAN
+            "49048192",  # MAESGWYN, GLYNARTHEN, LLANDYSUL
+            "49038741",  # TEGLAN, CILIAU AERON, LAMPETER
+        ]:
             return None
 
         if record.postcode in [
             # split
             "SA44 5UB",
+            "SA43 1PT",
             # suspect
             "SA44 5NQ",
             "SY23 5JP",
         ]:
             return None
-
         return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
-        # Remove duplicated stations names
         record = record._replace(placename="")
-
-        # Remove stations from the PEM council
-        if record.stationcode in [
-            "100-2025",
-            "00-2025",
-            "101-2026",
-            "102-2027",
-            "77-2001",
-            "78-2002",
-            "79-2003",
-            "80-2004",
-            "81-2005",
-            "82-2006",
-            "83-2007",
-            "83-2008",
-            "84-2009",
-            "85-2010",
-            "86-2011",
-            "87-2012",
-            "88-2013",
-            "89-2014",
-            "90-2015",
-            "91-2016",
-            "92-2017",
-            "93-2018",
-            "94-2019",
-            "95-2020",
-            "96-2021",
-            "97-2022",
-            "98-2023",
-            "99-2024",
-        ]:
-            return None
-
         return super().station_record_to_dict(record)
