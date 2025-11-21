9f2ba8e09a4843589afdbb9d190253b12504cb58
chris48s <git@chris-shaw.dev>
2025-11-21 13:19:23 +0000

    ruff

--- polling_stations/apps/data_importers/management/commands/import_ceredigion.py
+++ polling_stations/apps/data_importers/management/commands/import_ceredigion.py
@@ -15,26 +15,29 @@ class Command(BaseDemocracyCountsCsvImporter):
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
 
-        if uprn in [
-            "49062840",  # TY GWYN, LLANARTH
-            "49049138",  # AWEL Y BRYN, RHYDLEWIS, LLANDYSUL
-            "49069702",  # TYDDYN MEHERYN, RHYDLEWIS, LLANDYSUL
-            "49047840",  # ALMA, TANYGROES, CARDIGAN
-            "49040771",  # TROEDYRHIW, BETHANIA, LLANON
-            "49065819",  # THE WILLOW, CROSS INN, LLANON
-            "49069384",  # TROEDYRHIW CAPELI, CRIBYN, LAMPETER
-            "49075931",  # BRYN BARCUD, GORSGOCH, LLANYBYDDER
-            "49037718",  # CARTREFLE, MYDROILYN, LAMPETER
-            "49062368",  # TY GWALIA, LLWYNYGROES, TREGARON
-            "49061701",  # GER Y NANT, CAPEL BANGOR, ABERYSTWYTH
-            "49040891",  # TYNGWNDWN, LLANFARIAN, ABERYSTWYTH
-            "49065752",  # TY'R IET, CARDIGAN
-            "49063434",  # CARDIGAN SAND & GRAVEL CO LTD, CNWC Y SAESON, PENPARC, CARDIGAN
-            "49047095",  # CRYNGA MAWR, BLAENANNERCH, CARDIGAN
-            "200001099702",  # PLASBRYNIAU, LADY ROAD, BLAENPORTH, CARDIGAN
-            "49048192",  # MAESGWYN, GLYNARTHEN, LLANDYSUL
-            "49038741",  # TEGLAN, CILIAU AERON, LAMPETER
-        ]:
+        if (
+            uprn
+            in [
+                "49062840",  # TY GWYN, LLANARTH
+                "49049138",  # AWEL Y BRYN, RHYDLEWIS, LLANDYSUL
+                "49069702",  # TYDDYN MEHERYN, RHYDLEWIS, LLANDYSUL
+                "49047840",  # ALMA, TANYGROES, CARDIGAN
+                "49040771",  # TROEDYRHIW, BETHANIA, LLANON
+                "49065819",  # THE WILLOW, CROSS INN, LLANON
+                "49069384",  # TROEDYRHIW CAPELI, CRIBYN, LAMPETER
+                "49075931",  # BRYN BARCUD, GORSGOCH, LLANYBYDDER
+                "49037718",  # CARTREFLE, MYDROILYN, LAMPETER
+                "49062368",  # TY GWALIA, LLWYNYGROES, TREGARON
+                "49061701",  # GER Y NANT, CAPEL BANGOR, ABERYSTWYTH
+                "49040891",  # TYNGWNDWN, LLANFARIAN, ABERYSTWYTH
+                "49065752",  # TY'R IET, CARDIGAN
+                "49063434",  # CARDIGAN SAND & GRAVEL CO LTD, CNWC Y SAESON, PENPARC, CARDIGAN
+                "49047095",  # CRYNGA MAWR, BLAENANNERCH, CARDIGAN
+                "200001099702",  # PLASBRYNIAU, LADY ROAD, BLAENPORTH, CARDIGAN
+                "49048192",  # MAESGWYN, GLYNARTHEN, LLANDYSUL
+                "49038741",  # TEGLAN, CILIAU AERON, LAMPETER
+            ]
+        ):
             return None
 
         if record.postcode in [
