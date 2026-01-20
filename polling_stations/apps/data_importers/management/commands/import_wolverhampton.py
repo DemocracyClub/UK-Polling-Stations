1d0ee9adc7e0aa3ce738293d53c7d05f586c6510
polling-bot-4000 <developers@democracyclub.org.uk>
2026-01-20 16:51:30 +0000

    Import script for Wolverhampton (2026-05-07) (closes #8771)

--- polling_stations/apps/data_importers/management/commands/import_wolverhampton.py
+++ polling_stations/apps/data_importers/management/commands/import_wolverhampton.py
@@ -3,36 +3,32 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WLV"
-    addresses_name = "2024-07-04/2024-05-31T11:56:01.424909/Wolverhampton polling stations - Democracy Club.tsv"
-    stations_name = "2024-07-04/2024-05-31T11:56:01.424909/Wolverhampton polling stations - Democracy Club.tsv"
-    elections = ["2024-07-04"]
+    addresses_name = "2026-05-07/2026-01-22T16:58:37.246479/Update - Democracy_Club Wolverhampton 07May2026.tsv"
+    stations_name = "2026-05-07/2026-01-22T16:58:37.246479/Update - Democracy_Club Wolverhampton 07May2026.tsv"
+    elections = ["2026-05-07"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
-        if record.addressline6 in [
-            # split
-            "WV2 2BF",
-            "WV3 9AY",
+        uprn = record.uprn.strip().lstrip("0")
+
+        if uprn in [
+            "10093324969",  #  24 WOOD STREET, HEATH TOWN, WOLVERHAMPTON
         ]:
             return None
+
+        if record.addressline6 in [
+            # split
+            "WV3 9AY",
+            "WV2 2BF",
+            "WV10 8BA",
+        ]:
+            return None
+
         return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
         # St Joseph`s Church Hall, Coalway Road, Wolverhampton WV3 7LF
-        if record.polling_place_id == "31519":
+        if record.polling_place_id == "32993":
             record = record._replace(polling_place_postcode="")
-
-        # fix from council
-        # Old:  St Martin`s Church, Dixon Street, Wolverhampton
-        # New: Parkfield Primary School, Dimmock Street, Parkfield
-        if record.polling_place_id == "31432":
-            record = record._replace(
-                polling_place_name="Parkfield Primary School",
-                polling_place_address_1="Dimmock Street",
-                polling_place_address_2="Parkfield",
-                polling_place_address_3="",
-                polling_place_address_4="Wolverhampton",
-                polling_place_postcode="",
-            )
 
         return super().station_record_to_dict(record)
