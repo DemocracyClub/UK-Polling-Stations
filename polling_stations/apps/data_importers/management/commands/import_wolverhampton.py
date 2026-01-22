73718564bdcf2ef6f0f116f0647e941b7cfed975
polling-bot-4000 <developers@democracyclub.org.uk>
2026-01-22 16:59:12 +0000

    Import script for Wolverhampton (2026-05-07) (closes #8771)

--- polling_stations/apps/data_importers/management/commands/import_wolverhampton.py
+++ polling_stations/apps/data_importers/management/commands/import_wolverhampton.py
@@ -3,7 +3,7 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WLV"
-    addresses_name = "2026-05-07/2026-01-20T16:51:22.129100/Democracy_Club Wolverhampton Polling Stations__07May2026.tsv"
-    stations_name = "2026-05-07/2026-01-20T16:51:22.129100/Democracy_Club Wolverhampton Polling Stations__07May2026.tsv"
+    addresses_name = "2026-05-07/2026-01-22T16:58:37.246479/Update - Democracy_Club Wolverhampton 07May2026.tsv"
+    stations_name = "2026-05-07/2026-01-22T16:58:37.246479/Update - Democracy_Club Wolverhampton 07May2026.tsv"
     elections = ["2026-05-07"]
     csv_delimiter = "\t"
