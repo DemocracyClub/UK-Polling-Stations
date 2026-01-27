165867822c51454ad4d4ad406ec46c5f9556139b
polling-bot-4000 <developers@democracyclub.org.uk>
2026-01-27 16:52:47 +0000

    TEST/Import script for Aberdeen (2026-05-07) (closes #8774)

--- polling_stations/apps/data_importers/management/commands/import_aberdeen.py
+++ polling_stations/apps/data_importers/management/commands/import_aberdeen.py
@@ -1,22 +1,13 @@
-from data_importers.management.commands import BaseHalaroseCsvImporter
+from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 
-class Command(BaseHalaroseCsvImporter):
+class Command(BaseDemocracyCountsCsvImporter):
     council_id = "ABE"
     addresses_name = (
-        "2024-07-04/2024-06-07T15:39:48.734869/Eros_SQL_Output001-Aberdeen.csv"
+        "2026-05-07/2026-01-27T16:52:41.436757/Democracy Club - Polling Districts.csv"
     )
     stations_name = (
-        "2024-07-04/2024-06-07T15:39:48.734869/Eros_SQL_Output001-Aberdeen.csv"
+        "2026-05-07/2026-01-27T16:52:41.436757/Democracy Club - Polling Stations.csv"
     )
-    elections = ["2024-07-04"]
-
-    def address_record_to_dict(self, record):
-        if record.housepostcode in [
-            # split
-            "AB21 9BE",
-            "AB12 5UQ",
-            "AB21 9RN",
-        ]:
-            return None
-        return super().address_record_to_dict(record)
+    elections = ["2026-05-07"]
+    csv_encoding = "utf-16le"
