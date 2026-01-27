00c25de63cc2c5fdf52f0ba149499c3ec85680ff
polling-bot-4000 <developers@democracyclub.org.uk>
2026-01-27 16:52:47 +0000

    Import script for Aberdeen (2026-05-07) (closes #8774)

--- polling_stations/apps/data_importers/management/commands/import_aberdeen.py
+++ polling_stations/apps/data_importers/management/commands/import_aberdeen.py
@@ -1,22 +1,47 @@
-from data_importers.management.commands import BaseHalaroseCsvImporter
+from addressbase.models import UprnToCouncil
+from data_importers.management.commands import BaseHalarose2026UpdateCsvImporter
 
 
-class Command(BaseHalaroseCsvImporter):
+class Command(BaseHalarose2026UpdateCsvImporter):
     council_id = "ABE"
-    addresses_name = (
-        "2024-07-04/2024-06-07T15:39:48.734869/Eros_SQL_Output001-Aberdeen.csv"
-    )
-    stations_name = (
-        "2024-07-04/2024-06-07T15:39:48.734869/Eros_SQL_Output001-Aberdeen.csv"
-    )
-    elections = ["2024-07-04"]
+    addresses_name = "2026-05-07/2026-03-09T16:25:32.686844/Democracy Club - Idox_2026-03-05 10-02.csv"
+    stations_name = "2026-05-07/2026-03-09T16:25:32.686844/Democracy Club - Idox_2026-03-05 10-02.csv"
+    elections = ["2026-05-07"]
+
+    def pre_import(self):
+        # We need to consider rows that don't have a uprn when importing data.
+        # However there are lots of rows for other councils in this file.
+        # So build a list of stations from rows that do have UPRNS
+        # and then use that list of stations to make sure we check relevant rows, even if they don't have a UPRN
+
+        council_uprns = set(
+            UprnToCouncil.objects.filter(lad=self.council.geography.gss).values_list(
+                "uprn", flat=True
+            )
+        )
+        self.COUNCIL_STATIONS = set()
+        data = self.get_addresses()
+
+        for record in data:
+            if record.uprn in council_uprns:
+                self.COUNCIL_STATIONS.add(record.pollingvenueid)
 
     def address_record_to_dict(self, record):
-        if record.housepostcode in [
-            # split
-            "AB21 9BE",
-            "AB12 5UQ",
-            "AB21 9RN",
-        ]:
+        if record.pollingvenueid not in self.COUNCIL_STATIONS:
             return None
+
+        if record.postcode in (
+            # splits
+            "AB21 9RN",
+            "AB21 9BE",
+            "AB13 0HQ",
+        ):
+            return None
+
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        if record.pollingvenueid not in self.COUNCIL_STATIONS:
+            return None
+
+        return super().station_record_to_dict(record)
