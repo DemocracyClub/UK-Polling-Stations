d5def4aa9bcf7a3cb38141f68ccd899d62c772e2
polling-bot-4000 <developers@democracyclub.org.uk>
2026-01-28 11:47:13 +0000

    Import script for Ceredigion (2026-05-07) (closes #8776)

--- polling_stations/apps/data_importers/management/commands/import_ceredigion.py
+++ polling_stations/apps/data_importers/management/commands/import_ceredigion.py
@@ -3,11 +3,7 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "CGN"
-    addresses_name = (
-        "2026-05-07/2025-11-21T13:13:01.523539/Democracy Club - Polling Districts.csv"
-    )
-    stations_name = (
-        "2026-05-07/2025-11-21T13:13:01.523539/Democracy Club - Polling Stations.csv"
-    )
+    addresses_name = "2026-05-07/2026-01-28T11:46:49.953017/CEREDIGION - Polling Districts - DATA FILE.csv"
+    stations_name = "2026-05-07/2026-01-28T11:46:49.953017/CEREDIGION - Data - Democracy Club - Polling Districts.csv"
     elections = ["2026-05-07"]
-    csv_encoding = "utf-16le"
+    csv_encoding = "windows-1252"
