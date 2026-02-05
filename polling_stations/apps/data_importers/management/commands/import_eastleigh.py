95a5d6cd90794675acc26d9ba108e3f31471d36c
polling-bot-4000 <developers@democracyclub.org.uk>
2026-02-05 14:50:52 +0000

    Import script for Eastleigh (2026-05-07) (closes #8804)

--- polling_stations/apps/data_importers/management/commands/import_eastleigh.py
+++ polling_stations/apps/data_importers/management/commands/import_eastleigh.py
@@ -4,33 +4,30 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "EAT"
     addresses_name = (
-        "2024-07-04/2024-05-30T17:35:45.611401/Democracy_Club__04July2024.tsv"
+        "2026-05-07/2026-02-05T14:50:21.960578/Democracy_Club__07May2026.tsv"
     )
     stations_name = (
-        "2024-07-04/2024-05-30T17:35:45.611401/Democracy_Club__04July2024.tsv"
+        "2026-05-07/2026-02-05T14:50:21.960578/Democracy_Club__07May2026.tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2026-05-07"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "200002902856",  # FLAT, 2 WINCHESTER ROAD, CHANDLER'S FORD, EASTLEIGH
             "10091134664",  # CHERRYWOOD, LOWFORD HILL, BURSLEDON, SOUTHAMPTON
             "10091134663",  # LARCHWOOD, LOWFORD HILL, BURSLEDON, SOUTHAMPTON
             "10091134662",  # ASHWOOD, LOWFORD HILL, BURSLEDON, SOUTHAMPTON
             "10091134661",  # SPINDLEWOOD, LOWFORD HILL, BURSLEDON, SOUTHAMPTON
             "10009640001",  # OAK COTTAGE, ALLINGTON LANE, FAIR OAK, EASTLEIGH
-            "10094386469",  # THE HOUND WB02, SATCHELL LANE, HAMBLE, SOUTHAMPTON
-            "100060324530",  # POND FARM, MORTIMERS LANE, UPHAM, SOUTHAMPTON
         ]:
             return None
 
         if record.addressline6 in [
             # split
-            "SO30 0DF",
             "SO31 8AF",
+            "SO30 0DF",
         ]:
             return None
 
