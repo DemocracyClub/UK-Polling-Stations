815227a1d4c15fd7394aa5584cb4ef20409c217c
polling-bot-4000 <developers@democracyclub.org.uk>
2026-02-05 11:27:38 +0000

    Import script for Runnymede (2026-05-07) (closes #8786)

--- polling_stations/apps/data_importers/management/commands/import_runnymede.py
+++ polling_stations/apps/data_importers/management/commands/import_runnymede.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "RUN"
     addresses_name = (
-        "2024-07-04/2024-05-29T09:00:57.033219/Democracy_Club__04July2024.tsv"
+        "2026-05-07/2026-02-05T11:27:32.285822/Democracy_Club__07May2026.tsv"
     )
     stations_name = (
-        "2024-07-04/2024-05-29T09:00:57.033219/Democracy_Club__04July2024.tsv"
+        "2026-05-07/2026-02-05T11:27:32.285822/Democracy_Club__07May2026.tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2026-05-07"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -17,21 +17,11 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
         if uprn in [
             "10092963299",  # FLAT AT 79 STATION ROAD, ADDLESTONE
-            "10095815752",  # FLAT 50, AVIATOR HOUSE, 227 STATION ROAD, ADDLESTONE
-            "10095815848",  # FLAT 136, AVIATOR HOUSE, 227 STATION ROAD, ADDLESTONE
-            "10095815743",  # FLAT 32, AVIATOR HOUSE, 227 STATION ROAD, ADDLESTONE
-            "200004029979",  # LODGE, ST. GEORGES COLLEGE, WEYBRIDGE ROAD, ADDLESTONE
             "100062604551",  # ST. GEORGES COLLEGE, WEYBRIDGE ROAD, ADDLESTONE
-            "100062600234",  # RUSHAM COTTAGE, PRUNE HILL, ENGLEFIELD GREEN, EGHAM
-            "10092962917",  # JAKES FARM, BROX LANE, OTTERSHAW, CHERTSEY
-            "10092961689",  # GEORGE ELIOT HOUSE, HARVEST ROAD, ENGLEFIELD GREEN, EGHAM
         ]:
             return None
         if record.addressline6 in [
-            # splits
-            "KT16 8QD",
-            "KT16 8AG",
-            "KT15 2DB",
+            "KT16 8AG",  # split
             "KT16 0AB",  # 10 WATERY LANE, CHERTSEY
         ]:
             return None
