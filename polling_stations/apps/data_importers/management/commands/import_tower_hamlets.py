30f70cd090fa18b6a2d9fb4b00541ca6ae7805dc
polling-bot-4000 <developers@democracyclub.org.uk>
2026-02-05 12:26:54 +0000

    Import script for Tower Hamlets (2026-05-07) (closes #8794)

--- polling_stations/apps/data_importers/management/commands/import_tower_hamlets.py
+++ polling_stations/apps/data_importers/management/commands/import_tower_hamlets.py
@@ -4,24 +4,23 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "TWH"
     addresses_name = (
-        "2024-07-04/2024-06-10T15:31:22.976107/Democracy_Club__04July2024 (14).tsv"
+        "2026-05-07/2026-02-05T12:26:43.974020/Democracy_Club__07May2026.tsv"
     )
     stations_name = (
-        "2024-07-04/2024-06-10T15:31:22.976107/Democracy_Club__04July2024 (14).tsv"
+        "2026-05-07/2026-02-05T12:26:43.974020/Democracy_Club__07May2026.tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2026-05-07"]
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
+
+    # Ignore addresses at Luke House, Canton Street, which look like they're on Burdett Road
+    # it's an OS issue: https://app.asana.com/1/1204880536137786/task/1206776365939224
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "6141280",  # FLAT 3 28 CANNON STREET ROAD, LONDON
             "6146893",  # 80B BRUCE ROAD, LONDON
-            "6141280",  # FLAT 3 28 CANNON STREET ROAD, LONDON
-            "6141279",  # FLAT 2 28 CANNON STREET ROAD, LONDON
-            "6141278",  # FLAT 1 28 CANNON STREET ROAD, LONDON
             "6653815",  # ROOM 733 7A WEST INDIA DOCK ROAD, LONDON
             "6653812",  # ROOM 722 7A WEST INDIA DOCK ROAD, LONDON
             "6653816",  # ROOM 721 7A WEST INDIA DOCK ROAD, LONDON
@@ -29,6 +28,7 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "6653814",  # ROOM 732 7A WEST INDIA DOCK ROAD, LONDON
             "6739342",  # 98 ROSEBANK GARDENS NORTH, LONDON
             "6036970",  # 7B PORTMAN PLACE, LONDON
+            "6748892",  # FLAT 5, 133 BRICK LANE, LONDON
         ]:
             return None
         if record.addressline6 in [
