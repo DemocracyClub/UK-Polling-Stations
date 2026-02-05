af7852cbf3b5862482b0c2ebf0fa7b1b9165840f
polling-bot-4000 <developers@democracyclub.org.uk>
2026-02-05 13:31:30 +0000

    Import script for Croydon (2026-05-07) (closes #8796)

--- polling_stations/apps/data_importers/management/commands/import_croydon.py
+++ polling_stations/apps/data_importers/management/commands/import_croydon.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "CRY"
     addresses_name = (
-        "2024-07-04/2024-06-25T21:19:12.858136/Democracy_Club__04July2024.tsv"
+        "2026-05-07/2026-02-05T13:28:59.885856/Democracy_Club__07May2026.tsv"
     )
     stations_name = (
-        "2024-07-04/2024-06-25T21:19:12.858136/Democracy_Club__04July2024.tsv"
+        "2026-05-07/2026-02-05T13:28:59.885856/Democracy_Club__07May2026.tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2026-05-07"]
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
 
@@ -18,41 +18,25 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
         if uprn in [
             "100020676357",  # 18A CROHAM ROAD, SOUTH CROYDON
-            "10094492830",  # 1A COVINGTON WAY, LONDON
-            "10001006634",  # 3 LANSDOWNE PLACE, LONDON
-            "100020642075",  # BASEMENT FLAT, 193 CHURCH ROAD, LONDON
-            "100020642076",  # 193 CHURCH ROAD, LONDON
-            "100020622449",  # 5A SHIRLEY OAKS ROAD, CROYDON
-            "100020622448",  # 2 SHIRLEY OAKS ROAD, CROYDON
-            "100020584917",  # WEST LODGE, BISHOPS WALK, CROYDON3
             "10093049200",  # 16 LIMPSFIELD ROAD, SOUTH CROYDON
             "10093049201",  # 16A LIMPSFIELD ROAD, SOUTH CROYDON
             "10093049201",  # 16A LIMPSFIELD ROAD, SOUTH CROYDON
-            "100020635515",  # 260 HAYES LANE, KENLEY
             "200001221192",  # 141 BRIGHTON ROAD, PURLEY
-            "100020593700",  # 49 CROWLEY CRESCENT, CROYDON
             "10014054732",  # 97 NOVA ROAD, CROYDON
             "10014054731",  # 95 NOVA ROAD, CROYDON
             "10094493457",  # 44 COOMBE ROAD, CROYDON
-            "10093046375",  # 237A SYDENHAM ROAD, CROYDON
-            "10093756168",  # FLAT 1, 80 WADDON NEW ROAD, CROYDON
-            "10093756169",  # FLAT 2, 80 WADDON NEW ROAD, CROYDON
-            "100020688674",  # 56 WEST HILL, SOUTH CROYDON
+            "100020690731",  # 39 BRICKFIELD ROAD, THORNTON HEATH
+            "100020584917",  # WEST LODGE, BISHOPS WALK, CROYDON
         ]:
             return None
 
         if record.addressline6 in [
-            # splits
-            "CR0 2JB",
-            "CR0 4BF",
-            "CR2 0JB",
-            "SE25 4BA",
             # looks wrong
             "SE19 3FB",
-            "CR2 8JT",
-            "CR2 0RW",
-            "CR8 5AR",
             "CR8 3ES",
+            "CR5 3NY",
+            "CR3 5QQ",
+            "CR2 0FP",
         ]:
             return None
 
