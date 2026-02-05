edec920f40c2644d51b74b011cd3b910c1ca604f
polling-bot-4000 <developers@democracyclub.org.uk>
2026-02-05 16:37:41 +0000

    Import script for Woking (2026-05-07) (closes #8806)

--- polling_stations/apps/data_importers/management/commands/import_woking.py
+++ polling_stations/apps/data_importers/management/commands/import_woking.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WOI"
     addresses_name = (
-        "2024-07-04/2024-05-28T18:55:42.997031/Democracy_Club__04July2024.CSV"
+        "2026-05-07/2026-02-05T16:37:29.551006/Democracy_Club__07May2026.CSV"
     )
     stations_name = (
-        "2024-07-04/2024-05-28T18:55:42.997031/Democracy_Club__04July2024.CSV"
+        "2026-05-07/2026-02-05T16:37:29.551006/Democracy_Club__07May2026.CSV"
     )
-    elections = ["2024-07-04"]
+    elections = ["2026-05-07"]
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
@@ -20,16 +20,13 @@ class Command(BaseXpressDemocracyClubCsvImporter):
                 "200000218826",  # THE BUNGALOW DAWNEY HILL, PIRBRIGHT
                 "10002428855",  # FLAT 7, THE OLD BREW HOUSE 130-132, HIGH STREET, OLD WOKING, WOKING
                 "200000216418",  # THORLEY COTTAGE, PYRFORD ROAD, WOKING
-                "100061656800",  # HEDGE COTTAGE, SAUNDERS LANE, WOKING
+                "100062608792",  # THE LETTING CENTRE, 22 HIGH STREET, WOKING
             ]
         ):
             return None
 
         if record.addressline6 in [
-            # splits
-            "KT14 6LT",
-            "GU22 8AF",
-            "GU21 2NJ",
+            "GU21 5NQ",  # split
             # looks wrong
             "KT15 3QA",
             "KT15 3QD",
@@ -40,7 +37,7 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
     def station_record_to_dict(self, record):
         # postcode correction for: Horsell Evangelical Church High Street Horsell Woking GU21 3SZ
-        if record.polling_place_id == "5531":
+        if record.polling_place_id == "6002":
             record = record._replace(polling_place_postcode="GU21 4SZ")
 
         return super().station_record_to_dict(record)
