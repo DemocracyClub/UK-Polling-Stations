e7fc14b5101b6937f84616ef1e9289bcb8467c85
polling-bot-4000 <developers@democracyclub.org.uk>
2026-02-06 10:31:19 +0000

    Import script for Islington (2026-05-07) (closes #8810)

--- polling_stations/apps/data_importers/management/commands/import_islington.py
+++ polling_stations/apps/data_importers/management/commands/import_islington.py
@@ -4,9 +4,28 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "ISL"
     addresses_name = (
-        "2024-11-28/2024-10-30T14:47:36.249110/Democracy_Club__28November2024.CSV"
+        "2026-05-07/2026-02-06T10:31:08.966210/Democracy_Club__07May2026.tsv"
     )
     stations_name = (
-        "2024-11-28/2024-10-30T14:47:36.249110/Democracy_Club__28November2024.CSV"
+        "2026-05-07/2026-02-06T10:31:08.966210/Democracy_Club__07May2026.tsv"
     )
-    elections = ["2024-11-28"]
+    elections = ["2026-05-07"]
+    csv_encoding = "windows-1252"
+    csv_delimiter = "\t"
+    # Ignore postcode mismatches for:
+    # 'N4 Library, 26 Blackstock Road, London, N4 2JF'
+    # 'St Joan of Arc Community Centre, Kelross Road, London, N5 2QN'
+    # 'Harry Rice Hall, 72-74 Hargrave Park, London, N19 5JN'
+    # 'Cornwallis Adventure Playground, Cornwallis Road, London, N19 4LP'
+    # 'Vernon Hall, Vernon Square, King's Cross Road, WC1X 9EP' (id: 3152)
+    # 'New Life Centre, 8-10 Lennox Road, London, N4 3NW' (id: 3064)
+    # 'Whittington Community Centre, Yerbury Road, London, N19 4RS' (id: 3085)
+
+    def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+        if uprn in [
+            "10093113062",  #  103C SEVEN SISTERS ROAD, LONDON
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
