79914f109145fa86b506081f06e13395deae7d16
polling-bot-4000 <developers@democracyclub.org.uk>
2026-02-06 11:32:05 +0000

    Import script for Waverley (2026-05-07) (closes #8812)

--- polling_stations/apps/data_importers/management/commands/import_waverley.py
+++ polling_stations/apps/data_importers/management/commands/import_waverley.py
@@ -1,34 +1,30 @@
-from data_importers.management.commands import BaseXpressWebLookupCsvImporter
+from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
-class Command(BaseXpressWebLookupCsvImporter):
+class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WAE"
-    addresses_name = "2024-07-04/2024-06-11T16:14:05.131641/WAE_combined.tsv"
-    stations_name = "2024-07-04/2024-06-11T16:14:05.131641/WAE_combined.tsv"
-    elections = ["2024-07-04"]
-    csv_delimiter = "\t"
+    addresses_name = (
+        "2026-05-07/2026-02-06T11:31:58.095008/Democracy_Club__07May2026.CSV"
+    )
+    stations_name = (
+        "2026-05-07/2026-02-06T11:31:58.095008/Democracy_Club__07May2026.CSV"
+    )
+    elections = ["2026-05-07"]
 
     def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
+        uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
             "100061602142",  # 3 FARNBOROUGH ROAD, FARNHAM
-            "100062350984",  # 25 HIGH STREET, CRANLEIGH
-            "200002335818",  # WHITE PLACE, 19 HIGH STREET, CRANLEIGH
-            "100062339000",  # EASEDALE, WHITMORE VALE ROAD, HINDHEAD
-            "200001293824",  # TRUXFORD STREAM THURSLEY ROAD, ELSTEAD, GODALMING
-            "100061618397",  # HANDON COTTAGE, MARKWICK LANE, LOXHILL, GODALMING
-            "100061618401",  # SHENTONS COTTAGE, MARKWICK LANE, LOXHILL, GODALMING
+            "10096746074",  # FLAT 1, GAINSBOROUGH HOUSE, 204 HIGH STREET, CRANLEIGH
+            "10096746075",  # FLAT 2, GAINSBOROUGH HOUSE, 204 HIGH STREET, CRANLEIGH
+            "100061616831",  # KENNELS COTTAGE, HASLEMERE ROAD, WITLEY, GODALMING
         ]:
             return None
 
-        if record.postcode in [
-            # splits
-            "GU9 0NZ",
-            "GU9 9JT",
-            # looks wrong
-            "RH12 3BQ",
-            "GU6 8EJ",
+        if record.addressline6 in [
+            "GU9 0NZ",  # split
+            "RH12 3BQ",  # looks wrong
         ]:
             return None
 
