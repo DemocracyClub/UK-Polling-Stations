a50c2dbd2d0bb805f8f348f201dc25546e6f9cfc
polling-bot-4000 <developers@democracyclub.org.uk>
2026-02-06 14:50:12 +0000

    Import script for East Renfrewshire (2026-05-07) (closes #8814)

--- polling_stations/apps/data_importers/management/commands/import_east_renfrewshire.py
+++ polling_stations/apps/data_importers/management/commands/import_east_renfrewshire.py
@@ -4,19 +4,18 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "ERW"
     addresses_name = (
-        "2024-07-04/2024-06-06T09:13:28.285718/Democracy Counts - Polling Districts.csv"
+        "2026-05-07/2026-02-06T14:50:06.403472/Democracy Club - Polling Districts.csv"
     )
     stations_name = (
-        "2024-07-04/2024-06-06T09:13:28.285718/Democracy Counts - Polling Stations.csv"
+        "2026-05-07/2026-02-06T14:50:06.403472/Democracy Club - Polling Stations.csv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2026-05-07"]
     csv_encoding = "utf-16le"
 
     def address_record_to_dict(self, record):
         if record.postcode in [
             # split
             "G46 7JL",
-            "G76 8RW",
         ]:
             return None
 
