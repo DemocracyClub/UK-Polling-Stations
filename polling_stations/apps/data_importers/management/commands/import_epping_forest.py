2f5830ccc832927481bb7816bd2d10e9dfca3e91
polling-bot-4000 <developers@democracyclub.org.uk>
2026-02-09 13:52:15 +0000

    Import script for Epping Forest (2026-05-07) (closes #8823)

--- polling_stations/apps/data_importers/management/commands/import_epping_forest.py
+++ polling_stations/apps/data_importers/management/commands/import_epping_forest.py
@@ -1,12 +1,11 @@
 from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
-from django.contrib.gis.geos import Point
 
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "EPP"
-    addresses_name = "2024-07-04/2024-06-26T13:59:04.521969/EPP_combined.tsv"
-    stations_name = "2024-07-04/2024-06-26T13:59:04.521969/EPP_combined.tsv"
-    elections = ["2024-07-04"]
+    addresses_name = "2026-05-07/2026-02-09T13:51:53.694969/Democracy_Club__07May2026.tsv pre noms.tsv"
+    stations_name = "2026-05-07/2026-02-09T13:51:53.694969/Democracy_Club__07May2026.tsv pre noms.tsv"
+    elections = ["2026-05-07"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -18,32 +17,23 @@ class Command(BaseXpressDemocracyClubCsvImporter):
                 "10012157511",  # WALLED GARDEN HOUSE, EPPING
                 "10012157510",  # TIMBER LODGE, EPPING
                 "100091477396",  # SCHOOL HOUSE, KING HAROLD SCHOOL, BROOMSTICK HALL ROAD, WALTHAM ABBEY
-                "100091249452",  # SHONKS FARM, MILL STREET, HARLOW
                 "100091251326",  # THE PANTILES, DUNMOW ROAD, FYFIELD, ONGAR
                 "100091251152",  # THATCHED COTTAGE, BIRDS GREEN, WILLINGALE, ONGAR
-                "10022857710",  # BLUNTS FARMHOUSE COOPERSALE LANE, THEYDON BOIS, EPPING
                 "100091247388",  # BROOKSIDE, GRAVEL LANE, CHIGWELL
                 "10022857825",  # CARAVAN 2 MOSS NURSERY SEDGE GREEN, ROYDON, HARLOW
                 "200002755166",  # GARDEN COTTAGE, NURSERY ROAD, LOUGHTON
+                "100090505679",  # 94A HIGH ROAD, LOUGHTON
+                "10012166253",  # 1 BUSH HALL COTTAGES, THRESHERS BUSH, HARLOW
+                "10012166254",  # 2 BUSH HALL COTTAGES, THRESHERS BUSH, HARLOW
             ]
         ):
             return None
 
         if record.addressline6 in [
-            # splits
-            "CM16 6JA",
-            "CM16 7QR",
             # suspect
             "CM5 0HP",
+            "CM5 9LY",
         ]:
             return None
 
         return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        rec = super().station_record_to_dict(record)
-        # Coord fix from council for: High Beach Village Hall, Avey Lane, High Beach, Loughton
-        if rec["internal_council_id"] == "3596":
-            rec["location"] = Point(0.02918, 51.66369, srid=4326)
-
-        return rec
