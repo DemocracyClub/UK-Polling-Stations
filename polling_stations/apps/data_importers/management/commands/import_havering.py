3e022de5e9d6756fd440743d8fb63e7920c17ee0
polling-bot-4000 <developers@democracyclub.org.uk>
2026-02-09 12:13:58 +0000

    Import script for Havering (2026-05-07) (closes #8821)

--- polling_stations/apps/data_importers/management/commands/import_havering.py
+++ polling_stations/apps/data_importers/management/commands/import_havering.py
@@ -4,44 +4,29 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "HAV"
     addresses_name = (
-        "2024-07-04/2024-06-01T21:00:40.351345/Democracy_Club__04July2024.tsv"
+        "2026-05-07/2026-02-09T12:13:49.257559/Democracy_Club__07May2026.tsv"
     )
     stations_name = (
-        "2024-07-04/2024-06-01T21:00:40.351345/Democracy_Club__04July2024.tsv"
+        "2026-05-07/2026-02-09T12:13:49.257559/Democracy_Club__07May2026.tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2026-05-07"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
-            "10033421759",  # GROVE FARM, BROOK STREET, BRENTWOOD
-            "10033422184",  # 57 NAGS HEAD LANE, BRENTWOOD
-            "10033425629",  # THE FARM HOUSE BROADFIELDS FARM PIKE LANE, UPMINSTER
             "100021384326",  # 97 LOWER BEDFORDS ROAD, ROMFORD
             "10033422378",  # 17 KILN WOOD LANE, HAVERING-ATTE-BOWER, ROMFORD
-            "100021370259",  # 98 DEE WAY, ROMFORD
-            "10091580271",  # FLAT 1 171 NORTH STREET, ROMFORD
-            "100023219217",  # 175A OLDCHURCH ROAD, ROMFORD
-            "100021390436",  # 3-5 PARK END ROAD, ROMFORD
-            "100021326271",  # 43 CLYDESDALE ROAD, HORNCHURCH
-            "100021326269",  # 41 CLYDESDALE ROAD, HORNCHURCH
-            "200002675962",  # THE WINDMILL, 167 UPMINSTER ROAD, UPMINSTER
-            "200002668849",  # UPPER BEDFORDS FARM, LOWER BEDFORDS ROAD, ROMFORD
-            "100021384325",  # 83 LOWER BEDFORDS ROAD, ROMFORD
-            "100021322397",  # 2A ARDLEIGH GREEN ROAD, HORNCHURCH
         ]:
             return None
 
         if record.addressline6 in [
             # splits
-            "RM11 2BY",
-            "RM12 4LG",
             "RM7 8DX",
+            "RM12 4LG",
             "RM7 7BX",
-            # looks wrong
-            "RM12 6SH",
+            "RM11 2BY",
         ]:
             return None
 
