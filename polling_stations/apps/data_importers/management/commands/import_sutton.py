2a5f3d2ab221bac55e6f7900033057a30228a9d6
polling-bot-4000 <developers@democracyclub.org.uk>
2026-02-10 16:14:05 +0000

    Import script for Sutton (2026-05-07) (closes #8832)

--- polling_stations/apps/data_importers/management/commands/import_sutton.py
+++ polling_stations/apps/data_importers/management/commands/import_sutton.py
@@ -1,23 +1,31 @@
 from data_importers.management.commands import BaseDemocracyCountsCsvImporter
+from django.contrib.gis.geos import Point
 
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "STN"
     addresses_name = (
-        "2025-05-22/2025-04-28T09:58:32.350706/Democracy Club- Polling Districts.csv"
+        "2026-05-07/2026-02-10T16:13:59.794024/Modern Democracy Export PDs.csv"
     )
-    stations_name = (
-        "2025-05-22/2025-04-28T09:58:32.350706/Democracy clubs- Polling stations.csv"
-    )
-    elections = ["2025-05-22"]
+    stations_name = "2026-05-07/2026-02-10T16:13:59.794024/Modern Democracy Export.csv"
+    elections = ["2026-05-07"]
     csv_encoding = "utf-16le"
 
-    # By-election, Maintaining unused script changes as comment for future reference:
-    # def station_record_to_dict(self, record):
-    #     # more accurate point for: Holy Trinity Church Centre, Maldon Road, Wallington, SM6 8BL
-    #     if record.stationcode in ["RB/1", "RB/2"]:
-    #         rec = super().station_record_to_dict(record)
-    #         rec["location"] = Point(-0.151435, 51.364474, srid=4326)
-    #         return rec
+    def station_record_to_dict(self, record):
+        # more accurate point for: Holy Trinity Church Centre, Maldon Road, Wallington, SM6 8BL
+        if record.stationcode in ["RB/1", "RB/2"]:
+            rec = super().station_record_to_dict(record)
+            rec["location"] = Point(-0.151435, 51.364474, srid=4326)
+            return rec
 
-    #     return super().station_record_to_dict(record)
+        return super().station_record_to_dict(record)
+
+    def address_record_to_dict(self, record):
+        uprn = record.uprn.strip().lstrip("0")
+
+        if uprn in [
+            "5870080008",  # 4 BELMONT ROAD, WALLINGTON
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
