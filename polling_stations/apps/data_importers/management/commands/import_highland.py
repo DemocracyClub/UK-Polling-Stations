07077d4ffc93622a05aad8f6011249bd7cf10eac
polling-bot-4000 <developers@democracyclub.org.uk>
2026-02-16 17:20:38 +0000

    Import script for Highland (2026-05-07) (closes #8855)

--- polling_stations/apps/data_importers/management/commands/import_highland.py
+++ polling_stations/apps/data_importers/management/commands/import_highland.py
@@ -3,52 +3,28 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "HLD"
-    addresses_name = (
-        "2025-12-11/2025-11-04T10:27:04.561058/Democracy Club - Polling Districts.csv"
-    )
-    stations_name = (
-        "2025-12-11/2025-11-04T10:27:04.561058/Democracy Club - Polling Stations.csv"
-    )
-    elections = ["2025-12-11"]
+    addresses_name = "2026-05-07/2026-02-16T17:20:11.291474/Democracy Club - Polling Districts Highland SPE.csv"
+    stations_name = "2026-05-07/2026-02-16T17:20:11.291474/Democracy Club - Polling Stations Highland SPE.csv"
+    elections = ["2026-05-07"]
     csv_encoding = "utf-16le"
 
-    # Maintaining exclusions through by-election
-
-    def station_record_to_dict(self, record):
-        # # correcting obviously wrong point for:
-        # # CROWN CHURCH, INVERNESS, KINGSMILLS ROAD, INVERNESS
-        # if record.stationcode in [
-        #     "I085",
-        #     "I086",
-        # ]:
-        #     record = record._replace(xordinate="267087")
-
-        # # SCOURIE COMMUNITY HALL, SCOURIE, LAIRG, SUTHERLAND, IV27 4TE
-        # if record.stationcode == "C006":
-        #     record = record._replace(xordinate="", yordinate="")
-
-        return super().station_record_to_dict(record)
-
     def address_record_to_dict(self, record):
-        # uprn = record.uprn.strip().lstrip("0")
+        uprn = record.uprn.strip().lstrip("0")
 
-        # if (
-        #     uprn
-        #     in [
-        #         "130150387",  # MILL COTTAGE A863 B884 JUNCTION - A850 JUNCTION, KILMUIR, DUNVEGAN
-        #         "130186635",  # 1 MACFARLANE BUILDINGS, CRUACHAN PLACE, PORTREE
-        #         "130131942",  # 3 WOODLAND PARK, CONTIN
-        #         "130178830",  # 12 COUNTY PLACE, CULDUTHEL, INVERNESS
-        #         "130197101",  # 4 BLACK ISLE VIEW, STRATTON, INVERNESS
-        #         "130147608",  # NEWLANDS OF URCHANY, NAIRN
-        #     ]
-        # ):
-        #     return None
+        if (
+            uprn
+            in [
+                "130128845",  # MELLNESS HOUSE, GLEN URQUHART, DRUMNADROCHIT, INVERNESS, IV63 6TW
+                "130000426",  # 22 NESS ROAD, FORTROSE, IV10 8SD
+                "130147608",  # NEWLANDS OF URCHANY, NAIRN
+            ]
+        ):
+            return None
 
-        # if record.postcode in [
-        #     # split
-        #     "IV17 0QY",
-        #     "IV23 2RW",
-        # ]:
-        #     return None
+        if record.postcode in [
+            # looks wrong
+            "PH33 6FP",
+        ]:
+            return None
+
         return super().address_record_to_dict(record)
