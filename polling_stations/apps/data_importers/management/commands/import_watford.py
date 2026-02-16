14777a47c3b04c1cd2c37a81c64781bac4e3aaee
polling-bot-4000 <developers@democracyclub.org.uk>
2026-02-16 14:24:46 +0000

    Import script for Watford (2026-05-07) (closes #8846)

--- polling_stations/apps/data_importers/management/commands/import_watford.py
+++ polling_stations/apps/data_importers/management/commands/import_watford.py
@@ -3,37 +3,6 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "WAT"
-    addresses_name = "2025-05-01/2025-02-25T15:00:12.120364/Democracy Club Data.csv"
-    stations_name = "2025-05-01/2025-02-25T15:00:12.120364/Democracy Club Data.csv"
-    elections = ["2025-05-01"]
-
-    def address_record_to_dict(self, record):
-        if record.housepostcode in [
-            # splits
-            "WD25 9AS",
-            "WD18 7BS",
-            "WD25 7DA",
-        ]:
-            return None
-
-        # station reassignment from council for stations at:
-        # Imam Hussein Foundation Centre, 205 North Approach, Kingswood, Watford
-        if self.get_station_hash(record) == "7-imam-hussein-foundation-centre":
-            record = record._replace(
-                pollingstationname="Kingsway Infant School",
-                pollingstationaddress_1="North Approach",
-                pollingstationaddress_2="",
-            )
-
-        return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # station change from council for stations at:
-        # Imam Hussein Foundation Centre, 205 North Approach, Kingswood, Watford
-        if self.get_station_hash(record) == "7-imam-hussein-foundation-centre":
-            record = record._replace(
-                pollingstationname="Kingsway Infant School",
-                pollingstationaddress_1="North Approach",
-                pollingstationaddress_2="",
-            )
-        return super().station_record_to_dict(record)
+    addresses_name = "2026-05-07/2026-02-16T14:24:39.850620/Democracy Club - Idox_2026-02-16 14-17.csv"
+    stations_name = "2026-05-07/2026-02-16T14:24:39.850620/Democracy Club - Idox_2026-02-16 14-17.csv"
+    elections = ["2026-05-07"]
