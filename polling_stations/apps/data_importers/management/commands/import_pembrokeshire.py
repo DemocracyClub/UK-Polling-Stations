a1e68eb7968956578f69edba788d0dfdc6ff82a5
polling-bot-4000 <developers@democracyclub.org.uk>
2026-02-16 14:34:50 +0000

    Import script for Pembrokeshire (2026-05-07) (closes #8850)

--- polling_stations/apps/data_importers/management/commands/import_pembrokeshire.py
+++ polling_stations/apps/data_importers/management/commands/import_pembrokeshire.py
@@ -1,59 +1,46 @@
-from data_importers.management.commands import BaseHalaroseCsvImporter
+from data_importers.management.commands import BaseHalarose2026UpdateCsvImporter
 
 
-class Command(BaseHalaroseCsvImporter):
+class Command(BaseHalarose2026UpdateCsvImporter):
     council_id = "PEM"
-    addresses_name = "2024-07-04/2024-06-17T09:55:22.022484/PEM_combined.csv"
-    stations_name = "2024-07-04/2024-06-17T09:55:22.022484/PEM_combined.csv"
-    elections = ["2024-07-04"]
+    addresses_name = "2026-05-07/2026-02-17T13:17:53.759530/Democracy Club - Idox_2026-02-16 14-31.csv"
+    stations_name = "2026-05-07/2026-02-17T13:17:53.759530/Democracy Club - Idox_2026-02-16 14-31.csv"
+    elections = ["2026-05-07"]
 
     def address_record_to_dict(self, record):
         record.uprn.strip().lstrip("0")
         if record.uprn in [
-            "200003255203",  # LOWER FLAT, FERN HOUSE, WARREN STREET, TENBY
+            "10013172306",  # HILLFORT PRIORYRATH, HOWARTH CLOSE, MILFORD HAVEN
         ]:
             return None
         if record.housepostcode in [
             # split
-            "SA73 2EB",
-            "SA73 2EB",
             "SA62 4NJ",
-            "SA62 3PW",
-            "SA73 1RG",
-            "SA72 6BE",
-            "SA72 6BH",
-            "SA73 1NR",
-            "SA73 1BB",
-            "SA62 5DB",
-            "SA66 7QW",
-            "SA62 3DA",
-            "SA73 3HF",
             "SA73 3RA",
-            "SA68 0QR",
-            "SA67 8RY",
-            "SA71 5BP",
+            "SA62 3DA",
             "SA62 5RJ",
-            "SA71 4JT",
-            "SA68 0XN",
-            "SA62 5UD",
+            "SA62 3PW",
+            "SA71 5BP",
+            "SA73 1RG",
+            "SA73 1BB",
+            "SA71 4BP",
             "SA73 1RJ",
+            "SA68 0QR",
+            "SA72 6BH",
+            "SA72 6BE",
+            "SA73 3HF",
+            "SA73 2EB",
             "SA62 6TD",
+            "SA42 0QG",
+            "SA73 1BL",
+            "SA68 0XN",
+            "SA73 1NR",
+            "SA67 8RY",
+            "SA62 5UD",
+            "SA66 7QW",
+            "SA62 5DB",
             "SA62 5NL",
+            "SA71 4JT",
         ]:
             return None
         return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # station change from council:
-        # old: The Old Watersports Centre, Goodwick SA64 0DE
-        # new: Goodwick Community School, Hill Street, Goodwick, SA64 0ET
-        if record.pollingvenueid == "241":
-            record = record._replace(
-                pollingstationname="Goodwick Community School",
-                pollingstationnumber="2014",
-                pollingstationaddress_1="Hill Street",
-                pollingstationaddress_2="Goodwick",
-                pollingstationaddress_3="",
-                pollingstationpostcode="SA64 0ET",
-            )
-        return super().station_record_to_dict(record)
