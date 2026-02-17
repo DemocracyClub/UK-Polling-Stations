f9759663f10dd3b70904b2cf1a11d331966d8dab
polling-bot-4000 <developers@democracyclub.org.uk>
2026-02-17 17:40:19 +0000

    Import script for Broxbourne (2026-05-07) (closes #8874)

--- polling_stations/apps/data_importers/management/commands/import_broxbourne.py
+++ polling_stations/apps/data_importers/management/commands/import_broxbourne.py
@@ -3,9 +3,9 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "BRX"
-    addresses_name = "2025-05-01/2025-03-24T16:55:00.760095/Democracy Club - polling districts for 1-5-2025.csv"
-    stations_name = "2025-05-01/2025-03-24T16:55:00.760095/Democracy Club - polling stations for 1-5-2025.csv"
-    elections = ["2025-05-01"]
+    addresses_name = "2026-05-07/2026-02-17T17:40:08.019036/Democracy club- polling districts for Borough of Broxbourne 7-5-2026.csv"
+    stations_name = "2026-05-07/2026-02-17T17:40:08.019036/Democracy club- polling stations for Borough of Broxbourne 7-5-2026.csv"
+    elections = ["2026-05-07"]
     csv_encoding = "utf-16le"
 
     def address_record_to_dict(self, record):
@@ -14,6 +14,7 @@ class Command(BaseDemocracyCountsCsvImporter):
         if uprn in [
             "148043016",  # 11 ABINGDON COURT, HIGH STREET, WALTHAM CROSS
             "148046333",  # KIMBLE, HIGHFIELD STABLES, WHITE STUBBS LANE, BROXBOURNE
+            "148000974",  # 1 GOFFS LANE, GOFFS OAK, WALTHAM CROSS, EN7 5EG
         ]:
             return None
 
