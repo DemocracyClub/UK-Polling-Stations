529bf5b31ade09d70bcc02be3c378c3e8a44d298
polling-bot-4000 <developers@democracyclub.org.uk>
2026-02-18 21:56:37 +0000

    Import script for Brent (2026-05-07) (closes #8884)

--- polling_stations/apps/data_importers/management/commands/import_brent.py
+++ polling_stations/apps/data_importers/management/commands/import_brent.py
@@ -3,39 +3,35 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "BEN"
-    addresses_name = "2025-02-18/2025-01-27T15:00:43.238818/Democracy club Alperton by election Polling Districts.csv"
-    stations_name = (
-        "2025-02-18/2025-01-27T15:00:43.238818/Democracy club Alperton by election.csv"
-    )
-    elections = ["2025-02-18"]
+    addresses_name = "2026-05-07/2026-02-18T21:56:26.335831/Democracy Club Polling Districts 2026.csv"
+    stations_name = "2026-05-07/2026-02-18T21:56:26.335831/Democracy Club Polling Polling Stations 2026.csv"
+    elections = ["2026-05-07"]
     csv_encoding = "utf-16le"
 
-    # Maintaining GE exclusions as the comment below for future reference
+    def address_record_to_dict(self, record):
+        uprn = record.uprn.strip().lstrip("0")
+        if uprn in [
+            "202033643",  # 164 WALM LANE, LONDON
+            "202066948",  # 6 MOUNT PLEASANT, WEMBLEY
+            "202196807",  # FLAT EAST LANE PAVILION EAST LANE, WEMBLEY
+            "202030534",  # 101 WINCHESTER AVENUE, LONDON
+        ]:
+            return None
 
-    # def address_record_to_dict(self, record):
-    #     uprn = record.uprn.strip().lstrip("0")
-    #     if uprn in [
-    #         "202033643",  # 164 WALM LANE, LONDON
-    #         "202066948",  # 6 MOUNT PLEASANT, WEMBLEY
-    #         "202196807",  # FLAT EAST LANE PAVILION EAST LANE, WEMBLEY
-    #         "202030534",  # 101 WINCHESTER AVENUE, LONDON
-    #     ]:
-    #         return None
+        if record.postcode in [
+            # looks wrong
+            "HA0 1WY",
+        ]:
+            return None
 
-    #     if record.postcode in [
-    #         # looks wrong
-    #         "HA0 1WY",
-    #     ]:
-    #         return None
+        return super().address_record_to_dict(record)
 
-    #     return super().address_record_to_dict(record)
+    def station_record_to_dict(self, record):
+        # removes wrong point for: Brent Hubs Kilburn Hornbill House 2 Rudolph Road London
+        if record.stationcode in [
+            "50",
+            "51",
+        ]:
+            record = record._replace(xordinate="", yordinate="")
 
-    # def station_record_to_dict(self, record):
-    #     # removes wrong point for: Brent Hubs Kilburn Hornbill House 2 Rudolph Road London
-    #     if record.stationcode in [
-    #         "69",
-    #         "70",
-    #     ]:
-    #         record = record._replace(xordinate="", yordinate="")
-
-    #     return super().station_record_to_dict(record)
+        return super().station_record_to_dict(record)
