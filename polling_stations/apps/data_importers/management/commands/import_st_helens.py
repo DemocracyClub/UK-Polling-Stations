a61d0a39ccb12d8294c900f8582c30990fcf949f
polling-bot-4000 <developers@democracyclub.org.uk>
2026-02-18 09:56:19 +0000

    Import script for St Helens (2026-05-07) (closes #8876)

--- polling_stations/apps/data_importers/management/commands/import_st_helens.py
+++ polling_stations/apps/data_importers/management/commands/import_st_helens.py
@@ -3,7 +3,11 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SHN"
-    addresses_name = "2025-04-03/2025-03-20T09:55:21.248536/Democracy_Club__03April2025 - Sutton South East By Election.tsv"
-    stations_name = "2025-04-03/2025-03-20T09:55:21.248536/Democracy_Club__03April2025 - Sutton South East By Election.tsv"
-    elections = ["2025-04-03"]
+    addresses_name = (
+        "2026-05-07/2026-02-18T09:56:09.315370/Democracy_Club__07May2026.tsv"
+    )
+    stations_name = (
+        "2026-05-07/2026-02-18T09:56:09.315370/Democracy_Club__07May2026.tsv"
+    )
+    elections = ["2026-05-07"]
     csv_delimiter = "\t"
