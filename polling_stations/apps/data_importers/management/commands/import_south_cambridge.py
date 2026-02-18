4f5e6d83b1e89e27a1da8b8dc3438e0a725ca370
polling-bot-4000 <developers@democracyclub.org.uk>
2026-02-18 11:01:07 +0000

    Import script for South Cambridgeshire (2026-05-07) (closes #8878)

--- polling_stations/apps/data_importers/management/commands/import_south_cambridge.py
+++ polling_stations/apps/data_importers/management/commands/import_south_cambridge.py
@@ -4,37 +4,11 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SCA"
     addresses_name = (
-        "2025-05-01/2025-03-14T09:31:36.010664/Democracy_Club__01May2025.tsv"
+        "2026-05-07/2026-02-18T11:00:52.688916/Democracy_Club__07May2026.tsv"
     )
     stations_name = (
-        "2025-05-01/2025-03-14T09:31:36.010664/Democracy_Club__01May2025.tsv"
+        "2026-05-07/2026-02-18T11:00:52.688916/Democracy_Club__07May2026.tsv"
     )
-    elections = ["2025-05-01"]
+    elections = ["2026-05-07"]
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
-
-    # Below warnings checked, no correction needed:
-    # WARNING: Polling station Meadows Community Centre (8932) is in Cambridge City Council (CAB)
-
-    def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "10096204414",  # BEAUFORT, STATION ROAD, LONGSTANTON, CAMBRIDGE
-            "10095469809",  # THE COPPICE CARAVAN, STATION ROAD, LONGSTANTON, CAMBRIDGE
-            "10095469885",  # FLEDGLING BARN, BRIDGEFOOT, BARLEY ROAD, HEYDON, ROYSTON
-            "10033031432",  # GREENGAGE COTTAGE, BURY LANE, MELBOURN, ROYSTON
-            "10096204379",  # HIGH BARN, SHINGAY ROAD, STEEPLE MORDEN, ROYSTON
-            "10003185702",  # CHAPELGATE, ST. NEOTS ROAD, DRY DRAYTON, CAMBRIDGE
-            "10095471389",  # FOXHILL SIDE, HINTON WAY, GREAT SHELFORD, CAMBRIDGE
-        ]:
-            return None
-
-        if record.addressline6 in [
-            # splits
-            "SG8 0BD",
-            "CB21 5LF",
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
