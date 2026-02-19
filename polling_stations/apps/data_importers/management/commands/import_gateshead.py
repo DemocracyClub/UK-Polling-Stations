b97e52034d242804395b226860b424e726ae26f8
awdem <wdemontmollin@gmail.com>
2026-02-19 11:33:10 +0000

    station change for GAT

--- polling_stations/apps/data_importers/management/commands/import_gateshead.py
+++ polling_stations/apps/data_importers/management/commands/import_gateshead.py
@@ -13,6 +13,22 @@ class Command(BaseXpressDemocracyClubCsvImporter):
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
 
+    def station_record_to_dict(self, record):
+        # station change from council:
+        # old station: Low Fell Library 710a Durham Road Low Fell Gateshead NE9 6HT
+        # new station: St Ninans Church, Ivy Lane, Harlow Green, NE9 6QD
+        if record.polling_place_id == "12210":
+            record = record._replace(
+                polling_place_name="St Ninans Church",
+                polling_place_address_1="Ivy Lane",
+                polling_place_address_2="Harlow Green",
+                polling_place_address_3="",
+                polling_place_address_4="",
+                polling_place_postcode="NE9 6QD",
+            )
+
+        return super().station_record_to_dict(record)
+
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
