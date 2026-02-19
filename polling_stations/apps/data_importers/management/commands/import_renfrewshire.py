7e7971febb697f62ef81e418f65aca7532510d53
polling-bot-4000 <developers@democracyclub.org.uk>
2026-02-19 12:45:21 +0000

    Import script for Renfrewshire (2026-05-07) (closes #8887)

--- polling_stations/apps/data_importers/management/commands/import_renfrewshire.py
+++ polling_stations/apps/data_importers/management/commands/import_renfrewshire.py
@@ -4,13 +4,9 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "RFW"
-    addresses_name = (
-        "2024-07-04/2024-06-11T16:20:32.780888/DemocracyClub_PollingDistricts2024.csv"
-    )
-    stations_name = (
-        "2024-07-04/2024-06-11T16:20:32.780888/DemocracyClub_PollingStations2024.csv"
-    )
-    elections = ["2024-07-04"]
+    addresses_name = "2026-05-07/2026-02-19T12:45:14.327541/Democracy Club - Polling District (Ren).csv"
+    stations_name = "2026-05-07/2026-02-19T12:45:14.327541/Democracy Club - Polling Stations (Ren).csv"
+    elections = ["2026-05-07"]
     csv_encoding = "utf-16le"
 
     def pre_import(self):
@@ -35,6 +31,21 @@ class Command(BaseDemocracyCountsCsvImporter):
         if record.stationcode not in self.COUNCIL_STATIONS:
             return None
 
+        if record.postcode in (
+            # splits
+            "PA4 8YX",
+            "PA2 8BF",
+            "PA12 4DL",
+            "PA4 8DB",
+            "PA6 7HT",
+            "PA3 2QT",
+            "PA2 6QJ",
+            "PA6 7LH",
+            "PA5 8YP",
+            "PA1 2JL",
+        ):
+            return None
+
         return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
@@ -44,9 +55,27 @@ class Command(BaseDemocracyCountsCsvImporter):
         # removes name duplication
         record = record._replace(add1="")
 
-        # HOUSTON & KILLELLAN CHURCH HALLS location
-        # https://app.asana.com/0/1207538772343223/1207727040327547/f
-        if record.stationcode in ("3_IN03/1", "3_IN03/2", "3_IN03/3"):
-            record = record._replace(xordinate="240494", yordinate="666785")
+        # Postcodes below provided by council
+        # https://app.asana.com/1/1204880536137786/project/1205951895246773/task/1213451307586879
+
+        # Johnstone Town Hall, 25 Church Street, Johnstone, PA5 8EG
+        if record.stationcode in ("SW11 - JOHNSTH_1", "SW11 - JOHNSTH_2"):
+            record = record._replace(postcode="PA5 8FA")
+
+        # Tweedie Hall Ardlamont Square, Linwood, Paisley, Renfrewshire, PA3 3DE
+        if record.stationcode in ("SW17 - TWEED_1", "SW17 - TWEED_2", "SW17 - TWEED_3"):
+            record = record._replace(postcode="PA3 3DB")
+
+        # Houston & Killellan Kirk Hall,Main Street, Houston
+        if record.stationcode in (
+            "IW01A - HOUSTKK_1",
+            "IW01A - HOUSTKK_2",
+            "IW01A - HOUSTKK_3",
+            "IW01B - HOUSTKK_4",
+            "IW01B - HOUSTKK_5",
+        ):
+            record = record._replace(
+                postcode="PA6 7EL", xordinate="240494", yordinate="666785"
+            )
 
         return super().station_record_to_dict(record)
