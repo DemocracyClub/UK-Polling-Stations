f8234a66aa91795bd61160f871f86d457556cebf
polling-bot-4000 <developers@democracyclub.org.uk>
2026-02-20 10:35:20 +0000

    Import script for Conwy (2026-05-07) (closes #8895)

--- polling_stations/apps/data_importers/management/commands/import_conwy.py
+++ polling_stations/apps/data_importers/management/commands/import_conwy.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "CWY"
     addresses_name = (
-        "2024-07-04/2024-06-11T10:50:12.580277/Democracy_Club__04July2024 (19).tsv"
+        "2026-05-07/2026-02-23T09:57:03.243783/Democracy_Club__07May2026 (2).tsv"
     )
     stations_name = (
-        "2024-07-04/2024-06-11T10:50:12.580277/Democracy_Club__04July2024 (19).tsv"
+        "2026-05-07/2026-02-23T09:57:03.243783/Democracy_Club__07May2026 (2).tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2026-05-07"]
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
 
@@ -19,51 +19,33 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "200002948867",  # THE LILLY, WEST PARADE, LLANDUDNO
             "10094422154",  # WEST VIEW, WEST PARADE, LLANDUDNO
             "100100430017",  # 91 LLANDUDNO ROAD, PENRHYN BAY, LLANDUDNO
-            "50000014355",  # DELWOOD, OLD HIGHWAY, COLWYN BAY
-            "200002514853",  # SUNNYSIDE, OLD HIGHWAY, COLWYN BAY
             "100100441132",  # 20 CAMBRIA ROAD, OLD COLWYN, COLWYN BAY
-            "50000005841",  # BONA HOUSE, ABERGELE ROAD, LLANDDULAS, ABERGELE
             "50000016206",  # FLAT 1 REGENT HOUSE BRIDGE STREET, ABERGELE
             "10091011054",  # FLAT 2 REGENT HOUSE BRIDGE STREET, ABERGELE
-            "50000015204",  # FFORDD LAS BACH, TAN Y FRON ROAD, ABERGELE
-            "10024204208",  # TY GWYN FARM TOWYN ROAD, TOWYN
-            "50000015204",  # FFORDD LAS BACH, TAN Y FRON ROAD, ABERGELE
             "10035304364",  # PLAS ISAF, GROESFFORDD MARLI, ABERGELE
-            "10035304439",  # RHAN HIR, BYLCHAU, DENBIGH
-            "10024205763",  # TY ISA, HAFOD ELWY, BYLCHAU, DENBIGH
-            "10024206079",  # HAFOTTY GELYNEN, MAERDY, CORWEN
             "10024205839",  # ALLTWEN, NANT BWLCH YR HAIARN, TREFRIW
             "10024205527",  # BUARTH FARM, ROWEN, CONWY
-            "50000000886",  # PINEWOOD RIDING STABLES, SYCHNANT PASS ROAD, CONWY
-            "10035042289",  # THE PADDOCKS, SYCHNANT PASS ROAD, CONWY
-            "10035303297",  # PENOROS, TROFARTH, ABERGELE
-            "50000016886",  # COEDTEG FARM, NANT Y GLYN ROAD, COLWYN BAY
             "10024340201",  # HEN STABL, PLAS UCHA, LLANRWST
-            "100100953626",  # 7A QUEENS ROAD, OLD COLWYN, COLWYN BAY
             "50000019771",  # SIAMBRWEN, BETWS ROAD, LLANRWST
-            "10035303189",  # PANT Y RHEDYN, TROFARTH, ABERGELE
-            "10035303093",  # CROESENGAN UCHA, TROFARTH, ABERGELE
-            "10035303086",  # FFYNNON MEIRCH, TROFARTH, ABERGELE
+            "10091007935",  # ABATY HEN COLWYN, ABERGELE ROAD, OLD COLWYN, COLWYN BAY
         ]:
             return None
 
         if record.addressline6 in [
             # splits
-            "LL22 7DT",
-            "LL30 1NT",
-            "LL26 0YU",
-            "LL31 9EQ",
-            "LL30 1YQ",
-            "LL28 4AN",
-            "LL24 0LP",
-            "LL21 9PH",
             "LL32 8HW",
+            "LL22 7DT",
+            "LL30 1YQ",
+            "LL24 0LP",
+            "LL30 1NT",
+            "LL31 9EQ",
+            "LL26 0YU",
+            "LL21 9PH",
+            "LL28 4AN",
             # suspect
             "LL22 8FB",
             "LL30 2DB",
-            "LL30 2NR",
-            "LL29 9AB",
-            "LL21 0PR",
+            "LL34 6AQ",
         ]:
             return None
 
