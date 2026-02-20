2b8cbfa8245fcb41e55a4a37a9a5a12da904debd
polling-bot-4000 <developers@democracyclub.org.uk>
2026-02-20 15:35:50 +0000

    Import script for Bridgend (2026-05-07) (closes #8902)

--- polling_stations/apps/data_importers/management/commands/import_bridgend.py
+++ polling_stations/apps/data_importers/management/commands/import_bridgend.py
@@ -1,28 +1,36 @@
-from data_importers.management.commands import BaseHalaroseCsvImporter
+from data_importers.management.commands import BaseHalarose2026UpdateCsvImporter
 
 
-class Command(BaseHalaroseCsvImporter):
+class Command(BaseHalarose2026UpdateCsvImporter):
     council_id = "BGE"
-    addresses_name = "2024-07-04/2024-06-24T18:01:04.431232/bge-combined.csv"
-    stations_name = "2024-07-04/2024-06-24T18:01:04.431232/bge-combined.csv"
-    elections = ["2024-07-04"]
+    addresses_name = "2026-05-07/2026-02-23T11:10:42.383762/BGE_combined.csv"
+    stations_name = "2026-05-07/2026-02-23T11:10:42.383762/BGE_combined.csv"
+    elections = ["2026-05-07"]
+
+    def station_record_to_dict(self, record):
+        # Removing bad coordinates for:
+        # St John's Centre, SJAC, Minerva Street, Bridgend CF31 1TD
+        if self.get_station_hash(record) == "86-st-johns-centre":
+            record = record._replace(
+                pollingvenueeasting="0",
+                pollingvenuenorthing="0",
+            )
+        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-        if record.housepostcode in [
+        if record.postcode in [
             # split
-            "CF34 0UF",
-            "CF32 8TY",
             "CF31 2DH",
-            "CF31 5FD",
-            "CF31 3HL",
-            "CF31 1NP",
             "CF34 9SD",
-            "CF35 6GD",
-            "CF33 6PL",
-            "CF35 6HZ",
+            "CF32 8TY",
+            "CF31 3HL",
+            "CF36 3TB",
+            "CF31 1NP",
+            "CF34 0UF",
             "CF32 0NR",
-            # suspect
-            "CF31 2DL",  #
+            "CF35 6GD",
+            "CF31 5FD",
+            "CF35 6HZ",
         ]:
             return None
 
