e0fd9a2c876378e8b86a3343bd39f3ba8f950b62
polling-bot-4000 <developers@democracyclub.org.uk>
2026-02-23 10:07:35 +0000

    Import script for Chelmsford (2026-03-05) (closes #8913)

--- polling_stations/apps/data_importers/management/commands/import_chelmsford.py
+++ polling_stations/apps/data_importers/management/commands/import_chelmsford.py
@@ -4,10 +4,10 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "CHL"
     addresses_name = (
-        "2025-05-01/2025-03-24T15:58:38.061989/Democracy_Club__01May2025 (4).tsv"
+        "2026-03-05/2026-02-23T10:07:26.370972/Democracy_Club__07May2026 (3).tsv"
     )
     stations_name = (
-        "2025-05-01/2025-03-24T15:58:38.061989/Democracy_Club__01May2025 (4).tsv"
+        "2026-03-05/2026-02-23T10:07:26.370972/Democracy_Club__07May2026 (3).tsv"
     )
-    elections = ["2025-05-01"]
+    elections = ["2026-03-05"]
     csv_delimiter = "\t"
