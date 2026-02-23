4ed21c64eebe4f71e8f713825550f7e815d3f197
polling-bot-4000 <developers@democracyclub.org.uk>
2026-02-23 10:35:23 +0000

    Import script for Dundee (2026-05-07) (closes #8915)

--- polling_stations/apps/data_importers/management/commands/import_dundee.py
+++ polling_stations/apps/data_importers/management/commands/import_dundee.py
@@ -1,34 +1,8 @@
-from data_importers.management.commands import BaseHalaroseCsvImporter
+from data_importers.management.commands import BaseHalarose2026UpdateCsvImporter
 
 
-class Command(BaseHalaroseCsvImporter):
+class Command(BaseHalarose2026UpdateCsvImporter):
     council_id = "DND"
-    addresses_name = "2024-07-04/2024-05-28T10:15:27.215621/dundee_combined.csv"
-    stations_name = "2024-07-04/2024-05-28T10:15:27.215621/dundee_combined.csv"
-    elections = ["2024-07-04"]
-
-    def station_record_to_dict(self, record):
-        # The following stations' postcodes have been verified by the council:
-        # 'Wellgate Sheltered Housing, 8 King Street, Dundee, DD1 2JB' (id: 78)
-        # 'Marryat Hall, City Square, Dundee, DD1 3BG' (id: 79)
-        # 'Foula Terrace Sheltered, Housing Communal Lounge, Foula Terrace, Dundee, DD4 9SS' (id: 5)
-        return super().station_record_to_dict(record)
-
-    def address_record_to_dict(self, record):
-        if record.housepostcode in [
-            # split
-            "DD4 0FD",
-            "DD4 9ET",
-            "DD4 6JU",
-            "DD1 1JS",
-            "DD4 0TQ",
-            "DD4 8RX",
-            "DD2 1DJ",
-            "DD3 6AU",
-            "DD2 2LR",
-            "DD3 0JG",
-            # suspect
-            "DD2 1RT",
-        ]:
-            return None
-        return super().address_record_to_dict(record)
+    addresses_name = "2026-05-07/2026-02-23T10:35:14.409147/Democracy Club - Idox_2026-02-20 13-27.csv"
+    stations_name = "2026-05-07/2026-02-23T10:35:14.409147/Democracy Club - Idox_2026-02-20 13-27.csv"
+    elections = ["2026-05-07"]
