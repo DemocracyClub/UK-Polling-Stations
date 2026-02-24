6b88fc5f26a6a5af0f20a9a0dcd88219922323a9
polling-bot-4000 <developers@democracyclub.org.uk>
2026-02-24 12:43:57 +0000

    Import script for Guildford (2026-05-07) (closes #8927)

--- polling_stations/apps/data_importers/management/commands/import_guildford.py
+++ polling_stations/apps/data_importers/management/commands/import_guildford.py
@@ -1,24 +1,25 @@
-from data_importers.management.commands import BaseHalaroseCsvImporter
+from data_importers.management.commands import BaseHalarose2026UpdateCsvImporter
 
 
-class Command(BaseHalaroseCsvImporter):
+class Command(BaseHalarose2026UpdateCsvImporter):
     council_id = "GRT"
-    addresses_name = "2024-07-04/2024-06-26T09:13:56.038251/GRT_combined.csv"
-    stations_name = "2024-07-04/2024-06-26T09:13:56.038251/GRT_combined.csv"
-    elections = ["2024-07-04"]
+    addresses_name = "2026-05-07/2026-02-24T12:43:44.220496/Democracy Club - Idox_2026-02-24 12-26.csv"
+    stations_name = "2026-05-07/2026-02-24T12:43:44.220496/Democracy Club - Idox_2026-02-24 12-26.csv"
+    elections = ["2026-05-07"]
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
 
         if uprn in [
             "100062134282",  # FLAT 1 30 YORK ROAD, GUILDFORD
+            "10007066650",  # 2 MERROW LODGE, CLANDON PARK, WEST CLANDON, GUILDFORD
+            "10007066649",  # 1 MERROW LODGE, CLANDON PARK, WEST CLANDON, GUILDFORD
         ]:
             return None
-        if record.housepostcode in [
+        if record.postcode in [
             # split
-            "GU1 4TJ",
-            "GU23 7JL",
             "GU10 1BP",
+            "GU2 4LS",
             # suspect
             "GU5 9QN",
         ]:
