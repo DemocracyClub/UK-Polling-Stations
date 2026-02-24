37ffbb15e26587258cb402e7640b6e5e9d17cf5d
polling-bot-4000 <developers@democracyclub.org.uk>
2026-02-24 11:22:59 +0000

    Import script for Basildon (2026-05-07) (closes #8923)

--- polling_stations/apps/data_importers/management/commands/import_basildon.py
+++ polling_stations/apps/data_importers/management/commands/import_basildon.py
@@ -4,37 +4,45 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "BAI"
     addresses_name = (
-        "2024-07-04/2024-05-29T11:06:33.058529/Democracy_Club__04July2024.CSV"
+        "2026-05-07/2026-02-24T11:22:45.059332/Democracy_Club__07May2026.tsv"
     )
     stations_name = (
-        "2024-07-04/2024-05-29T11:06:33.058529/Democracy_Club__04July2024.CSV"
+        "2026-05-07/2026-02-24T11:22:45.059332/Democracy_Club__07May2026.tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2026-05-07"]
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
-        if uprn in [
-            "100091212751",  # PROBATION OFFICE, 1 FELMORES, BASILDON
-            "100090239089",  # 17 CHURCH ROAD, LAINDON, BASILDON
-            "100090273531",  # 23 GREENS FARM LANE, BILLERICAY
-            "10090682049",  # CEDAR COTTAGE, LOWER DUNTON ROAD, BULPHAN, UPMINSTER
-        ]:
+        if (
+            uprn
+            in [
+                "100091213142",  # MAYBURY, BORWICK LANE, WICKFORD
+                "100091589698",  # ST. NICHOLAS SCHOOL HOUSE, LEINSTER ROAD, BASILDON, SS15 5NS
+                "100091432152",  # THE GRANARY, GREENS FARM LANE, BILLERICAY, CM11 2NY
+                "100091212751",  # PROBATION OFFICE, 1 FELMORES, BASILDON
+                "100090239089",  # 17 CHURCH ROAD, LAINDON, BASILDON
+                "100090273531",  # 23 GREENS FARM LANE, BILLERICAY
+                "10090682049",  # CEDAR COTTAGE, LOWER DUNTON ROAD, BULPHAN, UPMINSTER
+            ]
+        ):
             return None
 
         if record.addressline6 in [
             # split
-            "SS15 5GX",
-            "CM11 2JX",
-            "SS15 6PF",
-            "CM11 2RU",
-            "SS12 0AU",
+            "SS14 3QQ",
             "CM11 2AD",
-            "CM11 2LH",
-            "SS16 6PH",
+            "SS15 6PF",
+            "SS12 0AU",
+            "CM11 2RU",
+            "CM11 2JX",
             "SS15 5NZ",
+            "SS15 5GX",
+            "CM11 2LH",
             # suspect
             "SS13 3RL",
+            "CM11 2XF",
         ]:
             return None
 
