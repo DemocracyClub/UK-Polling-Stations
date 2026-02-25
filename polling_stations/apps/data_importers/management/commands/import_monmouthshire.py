a1d80e9b1832e9fb26b6d7b6d97756ee67eccf3d
polling-bot-4000 <developers@democracyclub.org.uk>
2026-02-25 13:17:21 +0000

    Import script for Monmouthshire (2026-05-07) (closes #8929)

--- polling_stations/apps/data_importers/management/commands/import_monmouthshire.py
+++ polling_stations/apps/data_importers/management/commands/import_monmouthshire.py
@@ -4,25 +4,25 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "MON"
     addresses_name = (
-        "2024-07-04/2024-06-07T16:12:18.330990/Democracy_Club__04July2024.tsv"
+        "2026-05-07/2026-02-25T13:17:13.002236/Democracy_Club__07May2026.tsv"
     )
     stations_name = (
-        "2024-07-04/2024-06-07T16:12:18.330990/Democracy_Club__04July2024.tsv"
+        "2026-05-07/2026-02-25T13:17:13.002236/Democracy_Club__07May2026.tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2026-05-07"]
     csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
         # postcode correction for: Leisure Centre, Crossway Green, Chepstow, Monmouthshire, NP6 5LX
-        if record.polling_place_id == "11598":
+        if record.polling_place_id == "13008":
             record = record._replace(polling_place_postcode="NP16 5LX")
 
         # postcode correction for: Village Hall, Whitebrook, Monmouth, NP27 4TT
-        if record.polling_place_id == "11883":
+        if record.polling_place_id == "13237":
             record = record._replace(polling_place_postcode="NP25 4TT")
 
         # postcode correction for: Rectory Hall, Llanfair Kilgeddin, NP4 9BB
-        if record.polling_place_id == "11758":
+        if record.polling_place_id == "13055":
             record = record._replace(polling_place_postcode="NP7 9BD")
 
         return super().station_record_to_dict(record)
@@ -38,31 +38,28 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
         if record.addressline6.strip() in [
             # splits
-            "NP16 5BE",
-            "NP7 5PW",
-            "NP25 5BG",
-            "NP26 4PQ",
-            "NP7 7DH",
-            "NP7 5LF",
-            "NP7 5LL",
-            "NP7 9EU",
-            "NP25 5DJ",
-            "NP7 7DW",
-            "NP25 3BQ",
-            "NP7 8RP",
             "NP25 4AE",
-            "NP26 3AR",
-            "NP25 4TS",
-            "NP16 6NF",
-            "NP26 4HL",
+            "NP7 5LF",
             "NP26 5SD",
             "NP16 6HX",
-            "NP25 3LP",
+            "NP25 5DJ",
+            "NP16 5BE",
+            "NP7 8RP",
             "NP7 7EY",
-            "NP7 9HS",
-            "NP7 7EE",
+            "NP7 5LL",
+            "NP7 5PW",
+            "NP26 4PQ",
+            "NP7 7DH",
+            "NP7 9EU",
             "NP26 4NB",
+            "NP7 9HS",
+            "NP7 7DW",
+            "NP7 7EE",
             "NP7 9EG",
+            "NP25 4TS",
+            "NP25 3LP",
+            "NP26 3AR",
+            "NP16 6NF",
         ]:
             return None
 
