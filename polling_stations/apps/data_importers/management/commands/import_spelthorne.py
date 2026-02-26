aaac4bda683a67c709be72842f1a9e9617ae73b9
polling-bot-4000 <developers@democracyclub.org.uk>
2026-02-26 11:34:48 +0000

    Import script for Spelthorne (2026-05-07) (closes #8942)

--- polling_stations/apps/data_importers/management/commands/import_spelthorne.py
+++ polling_stations/apps/data_importers/management/commands/import_spelthorne.py
@@ -1,42 +1,8 @@
-from data_importers.management.commands import BaseHalaroseCsvImporter
+from data_importers.management.commands import BaseHalarose2026UpdateCsvImporter
 
 
-class Command(BaseHalaroseCsvImporter):
+class Command(BaseHalarose2026UpdateCsvImporter):
     council_id = "SPE"
-    addresses_name = "2024-07-04/2024-06-29T10:37:45.305544/Eros_SQL_Output016.csv"
-    stations_name = "2024-07-04/2024-06-29T10:37:45.305544/Eros_SQL_Output016.csv"
-    elections = ["2024-07-04"]
-
-    def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
-
-        if uprn in [
-            "33012923",  # 259 FELTHAM HILL ROAD, ASHFORD
-            "33012909",  # 257 FELTHAM HILL ROAD, ASHFORD
-            "33012894",  # 255 FELTHAM HILL ROAD, ASHFORD
-            "33035052",  # DERRYMORE, WRENS AVENUE, ASHFORD
-            "33049394",  # VALDOR, WRENS AVENUE, ASHFORD
-            "33049393",  # 54A WRENS AVENUE, ASHFORD
-        ]:
-            return None
-
-        if record.housepostcode in [
-            # split
-            "TW15 1AG",
-            "TW18 1HE",
-            "TW15 3SH",
-            # looks wrong
-            "TW17 8SY",
-            "TW18 1HD",
-            "TW18 1QP",
-            "TW18 1QW",
-            "TW18 1QL",
-            "TW18 1QN",
-            "TW18 1HH",
-            "TW18 1HG",
-            "TW18 1HQ",
-            "TW18 1HJ",
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
+    addresses_name = "2026-05-07/2026-02-26T11:34:38.261833/Democracy Club - Idox_2026-02-25 16-08.csv"
+    stations_name = "2026-05-07/2026-02-26T11:34:38.261833/Democracy Club - Idox_2026-02-25 16-08.csv"
+    elections = ["2026-05-07"]
