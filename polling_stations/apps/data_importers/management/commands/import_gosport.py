3ffc6b69b178323e56832325a04939a2d717d097
polling-bot-4000 <developers@democracyclub.org.uk>
2026-02-27 10:00:33 +0000

    Import script for Gosport (2026-05-07) (closes #8842)

--- polling_stations/apps/data_importers/management/commands/import_gosport.py
+++ polling_stations/apps/data_importers/management/commands/import_gosport.py
@@ -3,7 +3,6 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "GOS"
-    addresses_name = "2026-05-07/2026-02-13T14:03:56.860338/Democracy Club - Polling Districts - GOSPORT.csv"
-    stations_name = "2026-05-07/2026-02-13T14:03:56.860338/Democracy Club - Polling Stations - GOSPORT.csv"
+    addresses_name = "2026-05-07/2026-02-27T10:00:28.959884/GOS_districts_UTF8.csv"
+    stations_name = "2026-05-07/2026-02-27T10:00:28.959884/GOS_stations_UTF8.csv"
     elections = ["2026-05-07"]
-    csv_encoding = "utf-16le"
