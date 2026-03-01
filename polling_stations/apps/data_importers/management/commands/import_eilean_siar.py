815900c56f54c22a284e3c7cd00c0881e88e781d
polling-bot-4000 <developers@democracyclub.org.uk>
2026-03-01 07:46:55 +0000

    Import script for Comhairle nan Eilean Siar (2026-05-07) (closes #8961)

--- polling_stations/apps/data_importers/management/commands/import_eilean_siar.py
+++ polling_stations/apps/data_importers/management/commands/import_eilean_siar.py
@@ -3,7 +3,23 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "ELS"
-    addresses_name = "2024-07-04/2024-06-06T13:12:09.503968/Democracy Club - Polling Districts NHEAI.csv"
-    stations_name = "2024-07-04/2024-06-06T13:12:09.503968/Democracy Club - Polling Stations NHEAI.csv"
-    elections = ["2024-07-04"]
+    addresses_name = "2026-05-07/2026-03-01T07:46:42.121381/Democracy Club - Polling Districts WI.csv"
+    stations_name = (
+        "2026-05-07/2026-03-01T07:46:42.121381/Democracy Club - Polling Stations WI.csv"
+    )
+    elections = ["2026-05-07"]
     csv_encoding = "utf-16le"
+
+    def address_record_to_dict(self, record):
+        if record.uprn in [
+            "139014479",  # 5 SEAFORTH HEAD, ISLE OF LEWIS, HS2 9LG
+        ]:
+            return None
+
+        if record.postcode in [
+            # looks wrong
+            "HS3 3AE",
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
