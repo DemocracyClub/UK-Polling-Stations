2c01c0dd2ef89fd1c3ff32b675884e7a6b0c13be
polling-bot-4000 <developers@democracyclub.org.uk>
2026-03-04 16:52:37 +0000

    Import script for Stockport (2026-05-07) (closes #8980)

--- polling_stations/apps/data_importers/management/commands/import_stockport.py
+++ polling_stations/apps/data_importers/management/commands/import_stockport.py
@@ -4,13 +4,13 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SKP"
     addresses_name = (
-        "2024-07-04/2024-06-13T16:35:20.697123/Democracy_Club__04July2024.CSV"
+        "2026-05-07/2026-03-04T16:52:27.869548/Democracy_Club__07May2026.tsv"
     )
     stations_name = (
-        "2024-07-04/2024-06-13T16:35:20.697123/Democracy_Club__04July2024.CSV"
+        "2026-05-07/2026-03-04T16:52:27.869548/Democracy_Club__07May2026.tsv"
     )
-    elections = ["2024-07-04"]
-    csv_encoding = "windows-1252"
+    elections = ["2026-05-07"]
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
@@ -18,15 +18,10 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         if (
             uprn
             in [
-                "10094501452",  # 35A GREAVE, ROMILEY, STOCKPORT
-                "10090542735",  # FLAT ABOVE 136 ACRE LANE, CHEADLE HULME, CHEADLE
-                "10007621264",  # FIRST FLOOR FLAT 258B FINNEY LANE, HEALD GREEN, CHEADLE
-                "10090542735",  # FLAT ABOVE 136 ACRE LANE, CHEADLE HULME, CHEADLE
                 "100012786974",  # 30 BUXTON ROAD, HIGH LANE, STOCKPORT
                 "100011532317",  # 2 WESTMORLAND DRIVE, STOCKPORT
                 "100011449293",  # 1A ACK LANE EAST, BRAMHALL, STOCKPORT
                 "100011448628",  # 29 WOODFIELD ROAD, CHEADLE HULME, CHEADLE
-                "100011431085",  # 9 CHURCH ROAD, CHEADLE HULME, CHEADLE
                 "100012478041",  # HIGH GROVE FARM, ST. ANNS ROAD NORTH, HEALD GREEN, CHEADLE
                 "10090542752",  # FLAT GATLEY GOLF CLUB MOTCOMBE ROAD, HEALD GREEN, CHEADLE
                 "100011460719",  # 42 BRANKSOME ROAD, STOCKPORT
@@ -42,27 +37,24 @@ class Command(BaseXpressDemocracyClubCsvImporter):
                 "100012482785",  # YEW TREE FARM, OFFERTON ROAD, STOCKPORT
                 "10090543093",  # YEW TREE FARM BARN OFFERTON ROAD, OFFERTON, STOCKPORT
                 "100012482780",  # HIGH TREES, OFFERTON ROAD, STOCKPORT
-                "100012483705",  # LUM HOUSE, STRINES ROAD, MARPLE, STOCKPORT
-                "100012483704",  # LUM BARN, STRINES ROAD, MARPLE, STOCKPORT
             ]
         ):
             return None
 
         if record.addressline6 in [
             # splits
+            "SK8 4BU",
             "SK7 3DQ",
+            "SK6 6LP",
             "SK3 9HB",
-            "SK6 2BD",
             "SK4 5BS",
-            "SK5 7BJ",
+            "SK6 2BD",
             "SK7 4NX",
             "SK7 1JX",
-            "SK6 6LP",
-            "SK7 2AA",
-            "SK8 4BU",
             # looks wrong
             "SK6 7AY",
             "SK6 5NP",
+            "SK4 1TG",
         ]:
             return None
 
@@ -70,7 +62,7 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
     def station_record_to_dict(self, record):
         # more accurate point for: St Chads Church, Church Lane, Romiley, Stockport, SK6 4AA
-        if record.polling_place_id == "15100":
+        if record.polling_place_id == "17191":
             record = record._replace(polling_place_easting="394124")
             record = record._replace(polling_place_northing="390681")
 
