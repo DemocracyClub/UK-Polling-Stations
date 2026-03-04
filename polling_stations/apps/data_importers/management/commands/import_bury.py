927b842b331886adb1cbe7e873e41e5b69e9fa29
polling-bot-4000 <developers@democracyclub.org.uk>
2026-03-04 17:11:39 +0000

    Import script for Bury (2026-04-02) (closes #8984)

--- polling_stations/apps/data_importers/management/commands/import_bury.py
+++ polling_stations/apps/data_importers/management/commands/import_bury.py
@@ -4,35 +4,18 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "BUR"
     addresses_name = (
-        "2024-07-04/2024-05-23T17:32:53.988362/Democracy_Club__04July2024.CSV"
+        "2026-04-02/2026-03-04T17:11:34.630288/Democracy_Club__02April2026.tsv"
     )
     stations_name = (
-        "2024-07-04/2024-05-23T17:32:53.988362/Democracy_Club__04July2024.CSV"
+        "2026-04-02/2026-03-04T17:11:34.630288/Democracy_Club__02April2026.tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2026-04-02"]
+    csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "4210011016",  # BIRCHEM BOWER FARM, HARWOOD ROAD, TOTTINGTON, BURY
-            "100010966697",  # MANSONS, 10 MANCHESTER OLD ROAD, BURY
-        ]:
-            return None
-
         if record.addressline6 in [
-            # split
-            "M25 1ED",
-            "BL9 8JJ",
-            "M25 1JW",
-            "BL9 9PQ",
-            "BL9 8JW",
-            "BL9 9JW",
-            "BL8 2HH",
             # suspect
             "BL8 1TF",
-            "M26 1RZ",
-            "BL8 4LB",
         ]:
             return None
 
