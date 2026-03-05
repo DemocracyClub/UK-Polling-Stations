c3961debe6e3d95f1df9312f58ba78806bf6c640
polling-bot-4000 <developers@democracyclub.org.uk>
2026-03-05 10:21:25 +0000

    Import script for Hyndburn (2026-05-07) (closes #8994)

--- polling_stations/apps/data_importers/management/commands/import_hyndburn.py
+++ polling_stations/apps/data_importers/management/commands/import_hyndburn.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "HYN"
     addresses_name = (
-        "2025-05-01/2025-03-05T12:41:30.941681/Democracy_Club__01May2025.tsv"
+        "2026-05-07/2026-03-05T10:21:19.955514/Democracy_Club__07May2026.tsv"
     )
     stations_name = (
-        "2025-05-01/2025-03-05T12:41:30.941681/Democracy_Club__01May2025.tsv"
+        "2026-05-07/2026-03-05T10:21:19.955514/Democracy_Club__07May2026.tsv"
     )
-    elections = ["2025-05-01"]
+    elections = ["2026-05-07"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -18,11 +18,10 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         if (
             uprn
             in [
-                "10009967792",  # 3 HIGH STREET, ACCRINGTON
+                "100012393501",  # WELLSPRINGS FARM, SHAWCLIFFE LANE, GREAT HARWOOD, BLACKBURN, BB6 7UT
+                "10009973780",  # RODGER HEY FARM, WHALLEY ROAD, GREAT HARWOOD, BLACKBURN, BB6 7UH
                 "100012878207",  # LEG LOCK FARM, SOUGH LANE, GUIDE, BLACKBURN
                 "10070894822",  # 2A HIGH STREET, RISHTON, BLACKBURN
-                "10070896603",  # THE OLD STABLES BROWNSILLS, MILL LANE, GREAT HARWOOD, BLACKBURN
-                "10070894060",  # 2A WATER STREET, ACCRINGTON
                 "10070894668",  # HARWOOD EDGE BARN, WILPSHIRE ROAD, RISHTON, BLACKBURN
                 "100012393578",  # HARWOOD EDGE FARM, WILPSHIRE ROAD, RISHTON, BLACKBURN
             ]
@@ -36,17 +35,3 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             return None
 
         return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # The council has supplied missing postcodes for the following stations:
-        # Mobile Unit G M Fitted Furniture The Ideal Home Centre Whalley Road
-        if record.polling_place_id == "2623":
-            record = record._replace(polling_place_postcode="BB5 5DH")
-        # The Pavilion King George Playing Fields Off Royds Avenue Accrington
-        if record.polling_place_id == "2642":
-            record = record._replace(polling_place_postcode="BB5 2JX")
-        # St Paul`s Church, Catlow Hall Street, Oswaldtwistle
-        if record.polling_place_id == "2722":
-            record = record._replace(polling_place_postcode="BB5 3EY")
-
-        return super().station_record_to_dict(record)
