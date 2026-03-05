bc6d277b042195669a1ebbd3afee795c1cc448d8
polling-bot-4000 <developers@democracyclub.org.uk>
2026-03-05 11:54:03 +0000

    Import script for Nuneaton and Bedworth (2026-05-07) (closes #9007)

--- polling_stations/apps/data_importers/management/commands/import_nuneaton_and_bedworth.py
+++ polling_stations/apps/data_importers/management/commands/import_nuneaton_and_bedworth.py
@@ -1,33 +1,9 @@
-from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
+from data_importers.management.commands import BaseXpressWebLookupCsvImporter
 
 
-class Command(BaseXpressDemocracyClubCsvImporter):
+class Command(BaseXpressWebLookupCsvImporter):
     council_id = "NUN"
-    addresses_name = "2025-05-01/2025-04-22T14:13:38.111437/NUN_new.csv"
-    stations_name = "2025-05-01/2025-04-22T14:13:38.111437/NUN_new.csv"
-    elections = ["2025-05-01"]
-
-    def address_record_to_dict(self, record):
-        rec = super().address_record_to_dict(record)
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "100071496605",  # BRAMCOTE FIELDS FARM, LUTTERWORTH ROAD, BRAMCOTE, NUNEATON
-            "100071235227",  # MEADOW FARM, LUTTERWORTH ROAD, BRAMCOTE, NUNEATON
-        ]:
-            return None
-
-        if record.addressline6 in [
-            # split
-            "CV11 4NW",
-            "CV11 6JF",
-            "CV10 9QF",
-            "CV11 6NL",
-            "CV11 6JE",
-            # suspect
-            "CV12 9HJ",
-            "CV10 0PL",
-        ]:
-            return None
-
-        return rec
+    addresses_name = "2026-05-07/2026-03-05T11:53:55.856561/PropertyPostCodePollingStationWebLookup-2026-03-05.TSV"
+    stations_name = "2026-05-07/2026-03-05T11:53:55.856561/PropertyPostCodePollingStationWebLookup-2026-03-05.TSV"
+    elections = ["2026-05-07"]
+    csv_delimiter = "\t"
