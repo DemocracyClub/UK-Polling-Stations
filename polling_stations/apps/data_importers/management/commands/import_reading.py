189cda70c0b3a86c33ebe340e55612c96d2bdb6a
polling-bot-4000 <developers@democracyclub.org.uk>
2026-03-05 15:41:06 +0000

    Import script for Reading (2026-05-07) (closes #9021)

--- polling_stations/apps/data_importers/management/commands/import_reading.py
+++ polling_stations/apps/data_importers/management/commands/import_reading.py
@@ -4,33 +4,9 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "RDG"
     addresses_name = (
-        "2024-07-04/2024-06-12T11:03:00.786314/Democracy_Club__04July2024 2 (1).tsv"
+        "2026-05-07/2026-03-05T15:40:52.159471/Democracy_Club__07May2026.CSV"
     )
     stations_name = (
-        "2024-07-04/2024-06-12T11:03:00.786314/Democracy_Club__04July2024 2 (1).tsv"
+        "2026-05-07/2026-03-05T15:40:52.159471/Democracy_Club__07May2026.CSV"
     )
-    elections = ["2024-07-04"]
-    csv_delimiter = "\t"
-
-    def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "310084858",  # FLAT 3, 69 NORTHUMBERLAND AVENUE, READING
-            "310084857",  # FLAT 2, 69 NORTHUMBERLAND AVENUE, READING
-            "310051073",  # 221 BASINGSTOKE ROAD, READING
-            "310008722",  # 64 ALEXANDRA ROAD, READING
-            "310057403",  # 66 ALEXANDRA ROAD, READING
-            "310064645",  # 1 STAR ROAD, CAVERSHAM, READING
-            "310084858",  # FLAT 3, 69 NORTHUMBERLAND AVENUE, READING
-            "310084857",  # FLAT 2, 69 NORTHUMBERLAND AVENUE, READING
-        ]:
-            return None
-
-        if record.addressline6 in [
-            # split
-            "RG30 3NB",
-            "RG30 4RX",
-        ]:
-            return None
-        return super().address_record_to_dict(record)
+    elections = ["2026-05-07"]
