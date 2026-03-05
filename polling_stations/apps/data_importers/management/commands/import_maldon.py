50b51328941152604411f6f93c926d259f0709e7
polling-bot-4000 <developers@democracyclub.org.uk>
2026-03-05 14:13:45 +0000

    Import script for Maldon (2026-05-07) (closes #9018)

--- polling_stations/apps/data_importers/management/commands/import_maldon.py
+++ polling_stations/apps/data_importers/management/commands/import_maldon.py
@@ -3,9 +3,9 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "MAL"
-    addresses_name = "2024-07-04/2024-06-13T16:01:20.257096/MAL_combined.tsv"
-    stations_name = "2024-07-04/2024-06-13T16:01:20.257096/MAL_combined.tsv"
-    elections = ["2024-07-04"]
+    addresses_name = "2026-05-07/2026-03-05T14:13:38.499642/Democracy_Club__07May2026 -2026.03.05.tsv"
+    stations_name = "2026-05-07/2026-03-05T14:13:38.499642/Democracy_Club__07May2026 -2026.03.05.tsv"
+    elections = ["2026-05-07"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -18,8 +18,8 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
         if record.addressline6.strip() in [
             # split
-            "CM9 6YN",
             "CM9 4NY",
+            "CM9 6YN",
             # suspect
             "CM9 4RA",
         ]:
