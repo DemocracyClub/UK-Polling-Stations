4e78e93ff129443d9b0ef7ececd494e229d952e6
polling-bot-4000 <developers@democracyclub.org.uk>
2026-03-05 18:03:58 +0000

    Import script for Elmbridge (2026-05-07) (closes #9034)

--- polling_stations/apps/data_importers/management/commands/import_elmbridge.py
+++ polling_stations/apps/data_importers/management/commands/import_elmbridge.py
@@ -1,21 +1,8 @@
-from data_importers.management.commands import BaseHalaroseCsvImporter
+from data_importers.management.commands import BaseHalarose2026UpdateCsvImporter
 
 
-class Command(BaseHalaroseCsvImporter):
+class Command(BaseHalarose2026UpdateCsvImporter):
     council_id = "ELM"
-    addresses_name = "2024-07-04/2024-05-27T14:17:22.652530/combined.csv"
-    stations_name = "2024-07-04/2024-05-27T14:17:22.652530/combined.csv"
-    elections = ["2024-07-04"]
-
-    def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
-
-        if uprn in [
-            "100062356889",  # 1 ICKLINGHAM GATE, COBHAM
-            "100061307354",  # BRACKENBURY, BYFLEET ROAD, COBHAM
-            "100061326835",  # 133 BURWOOD ROAD, HERSHAM, WALTON-ON-THAMES
-            "100061326833",  # 131 BURWOOD ROAD, HERSHAM, WALTON-ON-THAMES
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
+    addresses_name = "2026-05-07/2026-03-05T18:03:47.545023/Democracy Club - Idox_2026-03-03 21-35.csv"
+    stations_name = "2026-05-07/2026-03-05T18:03:47.545023/Democracy Club - Idox_2026-03-03 21-35.csv"
+    elections = ["2026-05-07"]
