bc90904af22e149a386d9ddfa9c98857c7d88a2c
polling-bot-4000 <developers@democracyclub.org.uk>
2026-03-05 18:03:58 +0000

    Import script for Elmbridge (2026-05-07) (closes #9034)

--- polling_stations/apps/data_importers/management/commands/import_elmbridge.py
+++ polling_stations/apps/data_importers/management/commands/import_elmbridge.py
@@ -1,11 +1,11 @@
-from data_importers.management.commands import BaseHalaroseCsvImporter
+from data_importers.management.commands import BaseHalarose2026UpdateCsvImporter
 
 
-class Command(BaseHalaroseCsvImporter):
+class Command(BaseHalarose2026UpdateCsvImporter):
     council_id = "ELM"
-    addresses_name = "2024-07-04/2024-05-27T14:17:22.652530/combined.csv"
-    stations_name = "2024-07-04/2024-05-27T14:17:22.652530/combined.csv"
-    elections = ["2024-07-04"]
+    addresses_name = "2026-05-07/2026-03-05T18:03:47.545023/Democracy Club - Idox_2026-03-03 21-35.csv"
+    stations_name = "2026-05-07/2026-03-05T18:03:47.545023/Democracy Club - Idox_2026-03-03 21-35.csv"
+    elections = ["2026-05-07"]
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
@@ -15,6 +15,8 @@ class Command(BaseHalaroseCsvImporter):
             "100061307354",  # BRACKENBURY, BYFLEET ROAD, COBHAM
             "100061326835",  # 133 BURWOOD ROAD, HERSHAM, WALTON-ON-THAMES
             "100061326833",  # 131 BURWOOD ROAD, HERSHAM, WALTON-ON-THAMES
+            "100061307354",  # BRACKENBURY, BYFLEET ROAD, COBHAM
+            "100061344692",  # 1 HIGH PINE CLOSE, WEYBRIDGE
         ]:
             return None
 
