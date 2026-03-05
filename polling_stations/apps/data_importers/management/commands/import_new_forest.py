c101b2adf7f5eae6462846cd52859be567c0014a
polling-bot-4000 <developers@democracyclub.org.uk>
2026-03-05 12:01:08 +0000

    Import script for New Forest (2026-05-07) (closes #9009)

--- polling_stations/apps/data_importers/management/commands/import_new_forest.py
+++ polling_stations/apps/data_importers/management/commands/import_new_forest.py
@@ -4,28 +4,28 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "NEW"
     addresses_name = (
-        "2024-07-04/2024-05-28T14:29:06.229594/Democracy_Club__04July2024.tsv"
+        "2026-05-07/2026-03-05T12:00:58.887905/Democracy_Club__07May2026 (1).tsv"
     )
     stations_name = (
-        "2024-07-04/2024-05-28T14:29:06.229594/Democracy_Club__04July2024.tsv"
+        "2026-05-07/2026-03-05T12:00:58.887905/Democracy_Club__07May2026 (1).tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2026-05-07"]
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
 
     def station_record_to_dict(self, record):
         # Fordingbridge Town Hall, 63 High Street, Fordingbridge, SP6 1AS
-        if record.polling_place_id == "9382":
+        if record.polling_place_id == "10163":
             record = record._replace(polling_place_easting="414713")
             record = record._replace(polling_place_northing="114134")
 
         # Totton & Eling Cricket Club, Southern Gardens, Ringwood Road, Totton, Southampton, SO40 8RW
-        if record.polling_place_id == "9608":
+        if record.polling_place_id == "10316":
             record = record._replace(polling_place_easting="435298")
             record = record._replace(polling_place_northing="113155")
 
         # Fawley Royal British Legion, 3 Exbury Road, Blackfield, Southampton, SO45 1XD
-        if record.polling_place_id == "9543":
+        if record.polling_place_id == "10294":
             record = record._replace(polling_place_easting="444266")
             record = record._replace(polling_place_northing="102155")
 
@@ -34,14 +34,18 @@ class Command(BaseXpressDemocracyClubCsvImporter):
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
 
-        if uprn in [
-            "10013640427",  # ACORNS 47 BELMORE LANE, LYMINGTON, SO41 3NR
-            "100060502037",  # THE GLEN, BUTTS ASH LANE, HYTHE, SOUTHAMPTON
-            "10007451144",  # MOPLEY FARM, MOPLEY, LANGLEY, SOUTHAMPTON
-            "10013640427",  # ACORNS 47 BELMORE LANE, LYMINGTON
-            "10007450313",  # HILLCROFT, IPERS BRIDGE, HOLBURY, SOUTHAMPTON
-            "10007450771",  # GATEWOOD FARM, GATEWOOD HILL, BLACKFIELD, SOUTHAMPTON
-        ]:
+        if (
+            uprn
+            in [
+                "10013640427",  # ACORNS 47 BELMORE LANE, LYMINGTON, SO41 3NR
+                "100060502037",  # THE GLEN, BUTTS ASH LANE, HYTHE, SOUTHAMPTON
+                "10007451144",  # MOPLEY FARM, MOPLEY, LANGLEY, SOUTHAMPTON
+                "10013640427",  # ACORNS 47 BELMORE LANE, LYMINGTON
+                "10007450313",  # HILLCROFT, IPERS BRIDGE, HOLBURY, SOUTHAMPTON
+                "10007450771",  # GATEWOOD FARM, GATEWOOD HILL, BLACKFIELD, SOUTHAMPTON
+                "10090350922",  # GREEN ROLLESTONE COPSE, ROLLESTONE ROAD, HOLBURY, SOUTHAMPTON
+            ]
+        ):
             return None
 
         return super().address_record_to_dict(record)
