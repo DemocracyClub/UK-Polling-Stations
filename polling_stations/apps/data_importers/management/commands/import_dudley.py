82508135c66d43e95f4d14d86a29289e2ea7d82f
polling-bot-4000 <developers@democracyclub.org.uk>
2026-03-05 11:16:06 +0000

    Import script for Dudley (2026-05-07) (closes #9002)

--- polling_stations/apps/data_importers/management/commands/import_dudley.py
+++ polling_stations/apps/data_importers/management/commands/import_dudley.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "DUD"
     addresses_name = (
-        "2024-07-04/2024-06-06T10:13:23.366029/Democracy_Club__04July2024 (21).tsv"
+        "2026-05-07/2026-03-05T11:15:58.564952/Democracy_Club__07May2026.tsv"
     )
     stations_name = (
-        "2024-07-04/2024-06-06T10:13:23.366029/Democracy_Club__04July2024 (21).tsv"
+        "2026-05-07/2026-03-05T11:15:58.564952/Democracy_Club__07May2026.tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2026-05-07"]
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
 
@@ -19,6 +19,9 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         if (
             uprn
             in [
+                "90150847",  # 8 CANAL COTTAGES, NETHERTON, DUDLEY, DY2 0HA
+                "90123190",  # THE OLD WHARF INN, HIGH STREET, AMBLECOTE, STOURBRIDGE, DY8 4LY
+                "90115156",  # 162 TURLS HILL ROAD, BILSTON, WV14 9HH
                 "90159047",  # THE JAYS, HYPERION ROAD, STOURTON, STOURBRIDGE
                 "90153608",  # NEW BROMLEY FARM, BROMLEY LANE, KINGSWINFORD
                 "90135708",  # 106B STOURBRIDGE ROAD, DUDLEY
@@ -27,11 +30,6 @@ class Command(BaseXpressDemocracyClubCsvImporter):
                 "90163098",  # 51 WOLVERHAMPTON STREET, DUDLEY
                 "90163100",  # 52 WOLVERHAMPTON STREET, DUDLEY
                 "90147692",  # SCHOOL HOUSE, COTWALL END ROAD, DUDLEY
-                "90202527",  # LIVING ACCOMMODATION HINDU CULTURAL CENTRE 57-59 KING STREET, DUDLEY
-                "100071072854",  # 59 NEW STREET, SHELFIELD, WALSALL
-                "100071072810",  # 9A NEW STREET, SHELFIELD, WALSALL
-                "10090903310",  # FLAT 1 73 STAFFORD STREET, WILLENHALL
-                "200002877500",  # 43A HIGH STREET, BROWNHILLS, WALSALL
                 "90150770",  # HICKMERELANDS FARM, HICKMERELANDS, DUDLEY
                 "90062185",  # 58 BROOM ROAD, DUDLEY
                 "90017201",  # 188 WRENS HILL ROAD, DUDLEY
@@ -44,7 +42,6 @@ class Command(BaseXpressDemocracyClubCsvImporter):
                 "90053199",  # 11 BANK STREET, STOURBRIDGE
                 "90038823",  # 2 SANDFORD ROAD, DUDLEY
                 "90091991",  # 2 HINBROOK ROAD, DUDLEY
-                "90091991",  # 2 HINBROOK ROAD, DUDLEY
                 "90109211",  # 31 NETHERBY ROAD, DUDLEY
                 "90109229",  # 66 NETHERBY ROAD, DUDLEY
                 "90157092",  # 74 HAYES LANE, STOURBRIDGE
@@ -54,15 +51,14 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
         if record.addressline6 in [
             # splits
-            "DY1 4AN",
             "B63 4BN",
-            "DY1 3EP",
-            "DY1 3QY",
+            "B63 2DY",
             "DY6 7LT",
+            "DY6 9BL",
             # looks wrong
-            "DY8 4LU",
             "DY2 8QB",
             "DY8 3TH",
+            "DY5 1TA",
         ]:
             return None
 
