a0296f71507cb38a5124e10957e512c41cdcee21
polling-bot-4000 <developers@democracyclub.org.uk>
2026-03-06 12:53:24 +0000

    Import script for Rushmoor (2026-05-07) (closes #9041)

--- polling_stations/apps/data_importers/management/commands/import_rushmoor.py
+++ polling_stations/apps/data_importers/management/commands/import_rushmoor.py
@@ -4,20 +4,10 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "RUH"
     addresses_name = (
-        "2024-07-04/2024-06-11T09:44:37.221736/Democracy_Club__04July2024.tsv"
+        "2026-05-07/2026-03-06T12:53:17.844890/Democracy_Club__07May2026.tsv"
     )
     stations_name = (
-        "2024-07-04/2024-06-11T09:44:37.221736/Democracy_Club__04July2024.tsv"
+        "2026-05-07/2026-03-06T12:53:17.844890/Democracy_Club__07May2026.tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2026-05-07"]
     csv_delimiter = "\t"
-
-    def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "100062323099",  # GOLD FARM, GOVERNMENT ROAD, ALDERSHOT
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
