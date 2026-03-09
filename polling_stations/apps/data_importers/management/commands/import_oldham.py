198bccbb87456de6fc9dc11233dffd1bee089175
polling-bot-4000 <developers@democracyclub.org.uk>
2026-03-09 14:32:33 +0000

    Import script for Oldham (2026-05-07) (closes #9056)

--- polling_stations/apps/data_importers/management/commands/import_oldham.py
+++ polling_stations/apps/data_importers/management/commands/import_oldham.py
@@ -5,12 +5,12 @@ from django.contrib.gis.geos import Point
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "OLD"
     addresses_name = (
-        "2024-07-04/2024-06-04T08:55:04.244061/Democracy_Club__04July2024 (17).tsv"
+        "2026-05-07/2026-03-09T14:32:25.829578/Oldham Democracy_Club__07May2026.tsv"
     )
     stations_name = (
-        "2024-07-04/2024-06-04T08:55:04.244061/Democracy_Club__04July2024 (17).tsv"
+        "2026-05-07/2026-03-09T14:32:25.829578/Oldham Democracy_Club__07May2026.tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2026-05-07"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -21,9 +21,6 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             in [
                 "100011199050",  # 109 BROADWAY, ROYTON, OLDHAM
                 "100011199092",  # 144 BROADWAY, ROYTON, OLDHAM
-                "100011210768",  # 83 DERBY STREET, OLDHAM
-                "422000014564",  # 81 DERBY STREET, CHADDERTON, OLDHAM
-                "422000014571",  # 91 DERBY STREET, CHADDERTON, OLDHAM
                 "422000034427",  # 66 ROMAN ROAD, FAILSWORTH, MANCHESTER
                 "422000061537",  # 81 OLDHAM ROAD, GRASSCROFT, OLDHAM
                 "422000064639",  # 2 STONEBOTTOM BROW, DOBCROSS, OLDHAM
@@ -31,11 +28,6 @@ class Command(BaseXpressDemocracyClubCsvImporter):
                 "422000069004",  # 3 STATION LANE, GREENFIELD, OLDHAM
                 "422000102588",  # FLAT 2 76-78 MILNROW ROAD, SHAW
                 "422000102589",  # FLAT 1 76-78 MILNROW ROAD, SHAW
-                "422000107838",  # 16 CORBETT WAY, DENSHAW, OLDHAM
-                "422000120352",  # FLAT 1 THE BUNGALOW 43 GRAINS ROAD, DELPH
-                "422000120353",  # FLAT 2 THE BUNGALOW 43 GRAINS ROAD, DELPH
-                "422000120354",  # FLAT 3 THE BUNGALOW 43 GRAINS ROAD, DELPH
-                "422000120355",  # FLAT 4 THE BUNGALOW 43 GRAINS ROAD, DELPH
                 "422000120408",  # VALE COTTAGE, STANDEDGE, DELPH, OLDHAM
                 "422000110780",  # FLOATING LIGHT, STANDEDGE, DELPH, OLDHAM
                 "422000060230",  # UPPERWOOD HOUSE, HOLMFIRTH ROAD, GREENFIELD, OLDHAM
@@ -58,18 +50,21 @@ class Command(BaseXpressDemocracyClubCsvImporter):
 
         if record.addressline6 in [
             # splits
-            "OL8 2NE",
             "OL9 6BB",
             "OL8 3SF",
             "OL2 8DT",
+            "OL8 2NE",
             "OL2 8TN",
             # looks wrong
             "OL8 3HP",
             "OL1 1TD",
-            "OL1 1SJ",
             "OL1 1AP",
             "OL1 3EU",
-            "OL4 2QH",
+            "OL2 8HZ",
+            "OL8 3HW",
+            "M35 9RG",
+            "OL1 3ET",
+            "OL3 7AJ",
         ]:
             return None
 
@@ -79,7 +74,7 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         rec = super().station_record_to_dict(record)
 
         # postcode and point correction for: Waterhead Academy Sports Campus, Counthill Road, Moorside, Oldham, OL4 2PY
-        if rec["internal_council_id"] == "11495":
+        if rec["internal_council_id"] == "12068":
             rec["postcode"] = "OL4 2PZ"
             rec["location"] = Point(-2.076175, 53.556648, srid=4326)
 
