d88b27b7d000b2abedbf0dc37f3b7626e5b968a4
polling-bot-4000 <developers@democracyclub.org.uk>
2026-03-09 12:26:22 +0000

    Import script for Camden (2026-05-07) (closes #9053)

--- polling_stations/apps/data_importers/management/commands/import_camden.py
+++ polling_stations/apps/data_importers/management/commands/import_camden.py
@@ -4,46 +4,45 @@ from django.contrib.gis.geos import Point
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "CMD"
-    addresses_name = "2024-07-04/2024-06-19T16:20:32.968135/DemocracyClub_PollingDistricts_CAMDEN.csv"
-    stations_name = (
-        "2024-07-04/2024-06-19T16:20:32.968135/DemocracyClub_PollingStations_CAMDEN.csv"
-    )
-    elections = ["2024-07-04"]
+    addresses_name = "2026-05-07/2026-03-09T12:26:15.372546/Democracy Club - Polling Districts_CAMDEN.csv"
+    stations_name = "2026-05-07/2026-03-09T12:26:15.372546/Democracy Club - Polling Stations_CAMDEN.csv"
+    elections = ["2026-05-07"]
     csv_encoding = "utf-16le"
 
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
 
         if uprn in [
-            "5196036",  # 21A WEST HAMPSTEAD MEWS, LONDON
+            "5193668",  # 21 MILL LANE, LONDON
+        ]:
+            return None
+
+        if record.postcode in [
+            # looks wrong
+            "NW6 5UA",
         ]:
             return None
 
         return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
-        # remove stations from HRY council
-        if record.stationcode in (
-            "HGH-A",
-            "HGH-B",
-            "HGH-C",
-            "HGH-Da",
-            "HGH-Db",
-        ):
-            return None
-
-        # bug report # 649:
         # moving point closer to road for: GOSPEL OAK METHODIST CHURCH, Agincourt Road, London
         rec = super().station_record_to_dict(record)
 
         if rec["internal_council_id"] in (
-            "IA - b",
-            "IA - a",
+            "IA/1",
+            "IA/2",
         ):
             rec["location"] = Point(527726, 185479, srid=27700)
 
-        # removes wrong point for: ABBEY COMMUNITY CENTRE 172 Belsize Road London
+        # remove point for: ABBEY COMMUNITY CENTRE 172 Belsize Road London pending council response
         if rec["internal_council_id"] == "FB":
+            rec["location"] = None
+
+        # remove points  for 2 stations under same address pending councile response:
+        # N1C CENTRE, Plimsoll Building, 1 Handyside Street, London, N1C 4BQ
+        if rec["internal_council_id"] in ("PD", "PC"):
+            rec["postcode"] = "NIC 4BQ"
             rec["location"] = None
 
         return rec
