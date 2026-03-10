efb8f755ed7843f1a4677302ea9b26b25eb7b760
polling-bot-4000 <developers@democracyclub.org.uk>
2026-03-10 13:38:12 +0000

    Import script for Halton (2026-05-07) (closes #9073)

--- polling_stations/apps/data_importers/management/commands/import_halton.py
+++ polling_stations/apps/data_importers/management/commands/import_halton.py
@@ -3,59 +3,57 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "HAL"
-    addresses_name = (
-        "2025-05-01/2025-04-08T14:50:19.463564/Democracy_Club__01May2025.tsv"
-    )
-    stations_name = (
-        "2025-05-01/2025-04-08T14:50:19.463564/Democracy_Club__01May2025.tsv"
-    )
-    elections = ["2025-05-01"]
+    addresses_name = "2026-05-07/2026-03-10T13:38:01.597846/Halton Borough Council Democracy_Club__07May2026.tsv"
+    stations_name = "2026-05-07/2026-03-10T13:38:01.597846/Halton Borough Council Democracy_Club__07May2026.tsv"
+    elections = ["2026-05-07"]
     csv_delimiter = "\t"
 
-    # By-election so maintaining some exclusions as comments
     def station_record_to_dict(self, record):
         name = record.polling_place_name.replace("`", "'")
         record = record._replace(polling_place_name=name)
 
         # point correction for: Holy Trinity Church Trinity Street Runcorn WA7 1BJ
-        # if record.polling_place_id == "5453":
-        #     record = record._replace(polling_place_easting="351595")
-        #     record = record._replace(polling_place_northing="383048")
+        if record.polling_place_id == "6541":
+            record = record._replace(polling_place_easting="351595")
+            record = record._replace(polling_place_northing="383048")
 
         # User issue #572
         # point correction for: St Michael's Catholic Church, St Michael's Road, Widnes, WA8 8TF
-        # if record.polling_place_id == "5427":
-        #     record = record._replace(polling_place_easting="349445")
-        #     record = record._replace(polling_place_northing="385338")
+        if record.polling_place_id == "6583":
+            record = record._replace(polling_place_easting="349445")
+            record = record._replace(polling_place_northing="385338")
 
         # point correction for: St Johns Church, 134 Greenway Road, Widnes, WA8 6HA
-        # if record.polling_place_id == "5315":
-        #     record = record._replace(polling_place_easting="351638")
-        #     record = record._replace(polling_place_northing="386586")
+        if record.polling_place_id == "6489":
+            record = record._replace(polling_place_easting="351638")
+            record = record._replace(polling_place_northing="386586")
 
         # point correction for: Mobile Polling Station Galway Ave. Widnes
-        # if record.polling_place_id == "5358":
-        #     record = record._replace(polling_place_easting="350203")
-        #     record = record._replace(polling_place_northing="387149")
+        if record.polling_place_id == "6532":
+            record = record._replace(polling_place_easting="350203")
+            record = record._replace(polling_place_northing="387149")
 
         # point correction for: Beechwood Community Centre, Beechwood Avenue, Runcorn, WA7 3HB
-        if record.polling_place_id == "5826":
+        if record.polling_place_id == "6522":
             record = record._replace(polling_place_easting="353075")
             record = record._replace(polling_place_northing="380317")
 
         # point correction for: Prescot Road Changing Rooms, Hough Green Road, Widnes, WA8 7PD
-        # if record.polling_place_id == "5424":
-        #     record = record._replace(polling_place_easting="349493")
-        #     record = record._replace(polling_place_northing="387054")
+        if record.polling_place_id == "6678":
+            record = record._replace(polling_place_easting="349493")
+            record = record._replace(polling_place_northing="387054")
 
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         if record.addressline6 in [
             # splits
-            "WA7 2QA",
-            "WA7 1BH",
+            "WA8 7TB",
             "WA7 4SX",
+            "WA7 1BH",
+            "WA8 7TF",
+            "WA8 8PZ",
+            "WA7 4BS",
         ]:
             return None
 
