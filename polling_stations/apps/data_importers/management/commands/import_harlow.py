10c8eeb3d2d2f3e894562a3bc015ffce1a5fcbc0
polling-bot-4000 <developers@democracyclub.org.uk>
2026-03-10 11:20:27 +0000

    Import script for Harlow (2026-05-07) (closes #9069)

--- polling_stations/apps/data_importers/management/commands/import_harlow.py
+++ polling_stations/apps/data_importers/management/commands/import_harlow.py
@@ -4,9 +4,25 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "HAR"
     addresses_name = (
-        "2025-05-01/2025-04-02T12:50:00.400248/Democracy_Club__01May2025.CSV"
+        "2026-05-07/2026-03-10T11:20:18.634645/Democracy_Club__07May2026.CSV"
     )
     stations_name = (
-        "2025-05-01/2025-04-02T12:50:00.400248/Democracy_Club__01May2025.CSV"
+        "2026-05-07/2026-03-10T11:20:18.634645/Democracy_Club__07May2026.CSV"
     )
-    elections = ["2025-05-01"]
+    elections = ["2026-05-07"]
+
+    def address_record_to_dict(self, record):
+        uprn = record.property_urn.strip().lstrip("0")
+
+        if uprn in [
+            "100091255475",  # NEXUS HOUSE, SCHOOL LANE, HARLOW
+        ]:
+            return None
+
+        if record.addressline6 in [
+            # splits
+            "CM20 1BA",
+        ]:
+            return None
+
+        return super().address_record_to_dict(record)
