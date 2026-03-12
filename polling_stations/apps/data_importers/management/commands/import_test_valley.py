b00df9a14800b8a11ca485d56ac2a7969fa80739
GeoWill <roper.william@gmail.com>
2026-03-12 14:22:26 +0000

    Update Test Valley

--- polling_stations/apps/data_importers/management/commands/import_test_valley.py
+++ polling_stations/apps/data_importers/management/commands/import_test_valley.py
@@ -6,34 +6,10 @@ class Command(BaseFcsDemocracyClubApiImporter):
     elections = ["2026-05-07"]
     fcs_election_id = 99
 
-    def station_record_to_dict(self, record):
-        station_id = record.get(self.station_id_field)
-
-        # add point for: Weir Link Community Centre, 33 Weir Road, London, SW12 0NU
-        if (
-            station_id
-            in [
-                14725,  # Personal Best Education Lounge The Mountbatten School Whitenap Lane Romsey
-                14747,  # Sports Pavilion, Abbottswood Sports Ground Cutforth Way Romsey Hampshire
-                14737,  # Portakabin at Sports Field Kiel Drive Saxon Fields Andover Hampshire
-            ]
-        ):
-            record["longitude"] = 0
-            record["latitude"] = 0
-
-        return super().station_record_to_dict(record)
-
-    # def address_record_to_dict(self, record):
-    #     uprn = record.property_urn.strip().lstrip("0")
-    #
-    #     if uprn in [
-    #         "200000704153",  # WOODLANDS, LOCKES DROVE, PILL HEATH, ANDOVER
-    #     ]:
-    #         return None
-    #     if record.addressline6 in [
-    #         # split
-    #         "SP11 0HB",
-    #         "SO51 6EB",
-    #     ]:
-    #         return None
-    #     return super().address_record_to_dict(record)
+    def address_record_to_dict(self, record):
+        if record.get(self.postcode_field).strip() in [
+            # split
+            "SP11 0HB"
+        ]:
+            return None
+        return super().address_record_to_dict(record)
