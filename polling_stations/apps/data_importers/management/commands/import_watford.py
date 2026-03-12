5651aeb4b7ca79a0f3a3d5ab52122a38cf583641
awdem <wdemontmollin@gmail.com>
2026-03-12 15:27:24 +0000

    station name change for WAT council

--- polling_stations/apps/data_importers/management/commands/import_watford.py
+++ polling_stations/apps/data_importers/management/commands/import_watford.py
@@ -7,6 +7,17 @@ class Command(BaseHalarose2026UpdateCsvImporter):
     stations_name = "2026-05-07/2026-02-16T14:24:39.850620/Democracy Club - Idox_2026-02-16 14-17.csv"
     elections = ["2026-05-07"]
 
+    def station_record_to_dict(self, record):
+        # venue name change from council:
+        # OLD: Holywell Primary School, Tolpits Lane, Watford
+        # NEW: Willow Tree Primary School, Tolpits Lane, Watford
+        if record.pollingvenueid == "30":
+            record = record._replace(
+                pollingstationname="Willow Tree Primary School",
+            )
+
+        return super().station_record_to_dict(record)
+
     def address_record_to_dict(self, record):
         if record.postcode in [
             # splits
@@ -16,4 +27,11 @@ class Command(BaseHalarose2026UpdateCsvImporter):
         ]:
             return None
 
+        # venue name change from council:
+        # OLD: Holywell Primary School, Tolpits Lane, Watford
+        # NEW: Willow Tree Primary School, Tolpits Lane, Watford
+        if record.pollingvenueid == "30":
+            record = record._replace(
+                pollingstationname="Willow Tree Primary School",
+            )
         return super().address_record_to_dict(record)
