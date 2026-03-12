b77b530d20fc35eb080525c25583764339145366
polling-bot-4000 <developers@democracyclub.org.uk>
2026-03-12 12:24:24 +0000

    Import script for Huntingdonshire (2026-05-07) (closes #9090)

--- polling_stations/apps/data_importers/management/commands/import_huntingdonshire.py
+++ polling_stations/apps/data_importers/management/commands/import_huntingdonshire.py
@@ -4,20 +4,10 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "HUN"
     addresses_name = (
-        "2025-05-01/2025-03-10T17:31:30.888147/Democracy_Club__01May2025.tsv"
+        "2026-05-07/2026-03-12T12:24:10.715270/Democracy_Club__07May2026.tsv"
     )
     stations_name = (
-        "2025-05-01/2025-03-10T17:31:30.888147/Democracy_Club__01May2025.tsv"
+        "2026-05-07/2026-03-12T12:24:10.715270/Democracy_Club__07May2026.tsv"
     )
-    elections = ["2025-05-01"]
+    elections = ["2026-05-07"]
     csv_delimiter = "\t"
-
-    def address_record_to_dict(self, record):
-        if record.addressline6 in [
-            # split
-            "PE19 1HW",
-            "PE28 2QG",
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
