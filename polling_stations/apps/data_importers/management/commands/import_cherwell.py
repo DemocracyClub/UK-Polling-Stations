4417ddd1d79138ed15824ff95cf82bad288a314c
polling-bot-4000 <developers@democracyclub.org.uk>
2026-03-13 13:20:22 +0000

    Import script for Cherwell (2026-05-07) (closes #9097)

--- polling_stations/apps/data_importers/management/commands/import_cherwell.py
+++ polling_stations/apps/data_importers/management/commands/import_cherwell.py
@@ -4,10 +4,10 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "CHR"
     addresses_name = (
-        "2026-05-07/2026-02-26T15:45:11.260943/Democracy_Club__07May2026.tsv"
+        "2026-05-07/2026-03-13T13:20:13.892713/Democracy_Club__07May2026.tsv"
     )
     stations_name = (
-        "2026-05-07/2026-02-26T15:45:11.260943/Democracy_Club__07May2026.tsv"
+        "2026-05-07/2026-03-13T13:20:13.892713/Democracy_Club__07May2026.tsv"
     )
     elections = ["2026-05-07"]
     csv_encoding = "windows-1252"
