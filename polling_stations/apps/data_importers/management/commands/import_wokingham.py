6cae88a5df77fa9260b31802951725713e53c3ec
polling-bot-4000 <developers@democracyclub.org.uk>
2026-03-16 15:26:28 +0000

    Import script for Wokingham (2026-05-07) (closes #9123)

--- polling_stations/apps/data_importers/management/commands/import_wokingham.py
+++ polling_stations/apps/data_importers/management/commands/import_wokingham.py
@@ -4,10 +4,10 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WOK"
     addresses_name = (
-        "2026-05-07/2026-02-05T14:40:25.253534/Democracy_Club__07May2026.tsv"
+        "2026-05-07/2026-03-16T15:26:21.487332/Democracy_Club__07May2026.tsv"
     )
     stations_name = (
-        "2026-05-07/2026-02-05T14:40:25.253534/Democracy_Club__07May2026.tsv"
+        "2026-05-07/2026-03-16T15:26:21.487332/Democracy_Club__07May2026.tsv"
     )
     elections = ["2026-05-07"]
     csv_encoding = "windows-1252"
