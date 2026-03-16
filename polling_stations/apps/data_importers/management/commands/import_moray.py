e5acfd9baab93fdaedc2598a926ca7e4186e95b4
polling-bot-4000 <developers@democracyclub.org.uk>
2026-03-16 11:58:30 +0000

    Import script for Moray (2026-05-07) (closes #9061)

--- polling_stations/apps/data_importers/management/commands/import_moray.py
+++ polling_stations/apps/data_importers/management/commands/import_moray.py
@@ -3,6 +3,6 @@ from data_importers.management.commands import BaseHalarose2026UpdateCsvImporter
 
 class Command(BaseHalarose2026UpdateCsvImporter):
     council_id = "MRY"
-    addresses_name = "2026-05-07/2026-03-09T16:21:59.003966/Democracy Club - Idox_2026-03-05 09-30.csv"
-    stations_name = "2026-05-07/2026-03-09T16:21:59.003966/Democracy Club - Idox_2026-03-05 09-30.csv"
+    addresses_name = "2026-05-07/2026-03-16T11:58:21.052577/MRY_combined.csv"
+    stations_name = "2026-05-07/2026-03-16T11:58:21.052577/MRY_combined.csv"
     elections = ["2026-05-07"]
