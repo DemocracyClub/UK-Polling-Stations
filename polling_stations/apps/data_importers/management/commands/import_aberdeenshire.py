5a882a7bd9e6c8c3e5203f04aca14801ce3b1855
polling-bot-4000 <developers@democracyclub.org.uk>
2026-03-16 11:46:58 +0000

    Import script for Aberdeenshire (2026-05-07) (closes #9065)

--- polling_stations/apps/data_importers/management/commands/import_aberdeenshire.py
+++ polling_stations/apps/data_importers/management/commands/import_aberdeenshire.py
@@ -3,6 +3,6 @@ from data_importers.management.commands import BaseHalarose2026UpdateCsvImporter
 
 class Command(BaseHalarose2026UpdateCsvImporter):
     council_id = "ABD"
-    addresses_name = "2026-05-07/2026-03-09T16:26:40.637165/Democracy Club - Idox_2026-03-05 17-04.csv"
-    stations_name = "2026-05-07/2026-03-09T16:26:40.637165/Democracy Club - Idox_2026-03-05 17-04.csv"
+    addresses_name = "2026-05-07/2026-03-16T11:46:46.121836/ABD_combined_2.csv"
+    stations_name = "2026-05-07/2026-03-16T11:46:46.121836/ABD_combined_2.csv"
     elections = ["2026-05-07"]
