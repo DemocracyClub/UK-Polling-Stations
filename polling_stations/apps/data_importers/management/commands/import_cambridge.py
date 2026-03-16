36849595d1fa2631361b9ef4634d2415c556ed25
awdem <wdemontmollin@gmail.com>
2026-03-16 13:06:18 +0000

    add station coords for CAB

--- polling_stations/apps/data_importers/management/commands/import_cambridge.py
+++ polling_stations/apps/data_importers/management/commands/import_cambridge.py
@@ -7,6 +7,21 @@ class Command(BaseHalarose2026UpdateCsvImporter):
     stations_name = "2026-05-07/2026-02-16T14:26:03.938575/Democracy Club - Idox_2026-02-16 14-12.csv"
     elections = ["2026-05-07"]
 
+    def station_record_to_dict(self, record):
+        # Adding missing UPRN from council for:
+        # The Church of Jesus Christ of Latter-day Saints 670 Cherry Hinton Road Cambridge CB1 8ED
+        if self.get_station_hash(record) in [
+            "18-the-church-of-jesus-christ-of-latter-day-saints",
+            "17-the-church-of-jesus-christ-of-latter-day-saints",
+        ]:
+            record = record._replace(
+                pollingvenueuprn="200004216747",
+                pollingvenueeasting="548424",
+                pollingvenuenorthing="256186",
+            )
+
+        return super().station_record_to_dict(record)
+
     def address_record_to_dict(self, record):
         if record.uprn in [
             "10090966442",  # RIVERBOAT TUMBLING WATER G20635 RIVERSIDE, CAMBRIDGE
