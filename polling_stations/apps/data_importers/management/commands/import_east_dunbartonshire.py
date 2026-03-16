e32c5bcf28a31d895baa8bc9b8322a31a2f93374
polling-bot-4000 <developers@democracyclub.org.uk>
2026-03-16 14:17:10 +0000

    Import script for East Dunbartonshire (2026-05-07) (closes #9119)

--- polling_stations/apps/data_importers/management/commands/import_east_dunbartonshire.py
+++ polling_stations/apps/data_importers/management/commands/import_east_dunbartonshire.py
@@ -1,21 +1,8 @@
-from data_importers.management.commands import BaseHalaroseCsvImporter
+from data_importers.management.commands import BaseHalarose2026UpdateCsvImporter
 
 
-class Command(BaseHalaroseCsvImporter):
+class Command(BaseHalarose2026UpdateCsvImporter):
     council_id = "EDU"
-    addresses_name = "2024-07-04/2024-06-19T09:26:31.007903/EDU_combined.csv"
-    stations_name = "2024-07-04/2024-06-19T09:26:31.007903/EDU_combined.csv"
-    elections = ["2024-07-04"]
-
-    def address_record_to_dict(self, record):
-        if record.housepostcode in [
-            # split
-            "G66 7HL",
-            "G66 7HE",
-            "G64 3ND",
-            "G64 4DQ",
-            "G64 2DF",
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
+    addresses_name = "2026-05-07/2026-03-16T14:16:57.693002/combined.csv"
+    stations_name = "2026-05-07/2026-03-16T14:16:57.693002/combined.csv"
+    elections = ["2026-05-07"]
