915509cbb779763ce3c4d21218d6b2304addd2ee
polling-bot-4000 <developers@democracyclub.org.uk>
2026-03-17 16:40:45 +0000

    Import script for Calderdale (2026-05-07) (closes #9176)

--- polling_stations/apps/data_importers/management/commands/import_calderdale.py
+++ polling_stations/apps/data_importers/management/commands/import_calderdale.py
@@ -1,42 +1,31 @@
-from data_importers.management.commands import BaseDemocracyCountsCsvImporter
+from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
-class Command(BaseDemocracyCountsCsvImporter):
+class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "CLD"
-    addresses_name = "2024-07-04/2024-06-06T14:49:45.071526/CMBC_Democracy_Club_PS_Extract_PollingDistricts_UKPGE.csv"
-    stations_name = "2024-07-04/2024-06-06T14:49:45.071526/CMBC_Democracy_Club_PS_Extract_PollingStations_UKPGE.csv"
-    elections = ["2024-07-04"]
+    addresses_name = "2026-05-07/2026-03-17T16:40:35.655763/20260317_Democracy_Club__07May2026_CMBC.CSV"
+    stations_name = "2026-05-07/2026-03-17T16:40:35.655763/20260317_Democracy_Club__07May2026_CMBC.CSV"
+    elections = ["2026-05-07"]
+    csv_encoding = "windows-1252"
 
     def station_record_to_dict(self, record):
-        # LEE MOUNT BAPTIST CHURCH, MELBOURNE STREET, LEE MOUNT, HALIFAX, WEST YORKSHIRE HX3 5BQ
-        if record.stationcode == "60JB":
-            record = record._replace(xordinate="408412")
-            record = record._replace(yordinate="426427")
+        # # The following are coord amendments from the council:
 
-        # The following are coord amendments from the council:
-
-        # ST AUGUSTINE'S CENTRE, HANSON LANE, HALIFAX, HX1 5PG
-        if record.stationcode == "68KE":
-            record = record._replace(xordinate="408189")
-            record = record._replace(yordinate="425465")
         # PELLON BAPTIST SUNDAY SCHOOL, SPRING HALL LANE, HALIFAX, HX1 4UA
-        if record.stationcode in [
-            "110SA",
-            "114SE",
-        ]:
-            record = record._replace(xordinate="407498")
-            record = record._replace(yordinate="425770")
+        if record.polling_place_id == "1310":
+            record = record._replace(
+                polling_place_easting="407498",
+                polling_place_northing="425770",
+            )
+
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-        if record.uprn in [
-            "10093206213",  # 1 PARKIN LANE, TODMORDEN
-            "10093208972",  # GREAT LEAR INGS BARN, HEPTONSTALL, HEBDEN BRIDGE
-        ]:
-            return None
-
-        if record.postcode in [
-            "HX2 0UW",  # suspect
+        if record.addressline6.replace("\xa0", " ") in [
+            # split
+            "HX3 8FY",
+            # suspect
+            "HX2 0UW",
         ]:
             return None
 
