a62eb0d417916120537ec03fb34df560e893ea41
polling-bot-4000 <developers@democracyclub.org.uk>
2026-03-17 15:52:50 +0000

    Import script for Lewes (2026-05-07) (closes #9174)

--- polling_stations/apps/data_importers/management/commands/import_lewes.py
+++ polling_stations/apps/data_importers/management/commands/import_lewes.py
@@ -1,23 +1,8 @@
-from data_importers.management.commands import BaseHalaroseCsvImporter
+from data_importers.management.commands import BaseHalarose2026UpdateCsvImporter
 
 
-class Command(BaseHalaroseCsvImporter):
+class Command(BaseHalarose2026UpdateCsvImporter):
     council_id = "LEE"
-    addresses_name = "2025-05-29/2025-05-19T11:04:34.585409/Eros_SQL_Output001.csv"
-    stations_name = "2025-05-29/2025-05-19T11:04:34.585409/Eros_SQL_Output001.csv"
-    elections = ["2025-05-29"]
-
-    def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
-
-        if (
-            uprn
-            in [
-                "10033272374",  # 24A STEYNING AVENUE, PEACEHAVEN
-                "200001466148",  # OLD WHEEL COTTAGE, EASTERN ROAD, WIVELSFIELD GREEN, HAYWARDS HEATH
-                "100062487484",  # WOODS COTTAGE, EASTERN ROAD, WIVELSFIELD GREEN, HAYWARDS HEATH
-            ]
-        ):
-            return None
-
-        return super().address_record_to_dict(record)
+    addresses_name = "2026-05-07/2026-03-17T15:52:41.578389/Democracy Club - Idox_2026-03-17 15-46.csv"
+    stations_name = "2026-05-07/2026-03-17T15:52:41.578389/Democracy Club - Idox_2026-03-17 15-46.csv"
+    elections = ["2026-05-07"]
