7f3b35630aba95fa74bcd40c3bd0669e9dd58db3
polling-bot-4000 <developers@democracyclub.org.uk>
2026-03-17 11:41:11 +0000

    Import script for Leeds (2026-05-07) (closes #9150)

--- polling_stations/apps/data_importers/management/commands/import_leeds.py
+++ polling_stations/apps/data_importers/management/commands/import_leeds.py
@@ -3,15 +3,26 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "LDS"
-    addresses_name = (
-        "2024-07-04/2024-05-27T09:04:29.196802/Democracy_Club__04July2024.tsv"
-    )
-    stations_name = (
-        "2024-07-04/2024-05-27T09:04:29.196802/Democracy_Club__04July2024.tsv"
-    )
-    elections = ["2024-07-04"]
+    addresses_name = "2026-05-07/2026-03-23T13:53:24.869634/Democracy_Club__07May2026 amended version.tsv"
+    stations_name = "2026-05-07/2026-03-23T13:53:24.869634/Democracy_Club__07May2026 amended version.tsv"
+    elections = ["2026-05-07"]
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
+
+    def station_record_to_dict(self, record):
+        # removing bad coordinates for:
+        # Central Methodist Church, (Church Hall) (Desk B), 10a Wesley Street, Morley, Leeds
+        # Central Methodist Church, (Church Hall) (Desk A), 10a Wesley Street, Morley, Leeds
+        if record.polling_place_id in [
+            "26864",
+            "26662",
+        ]:
+            record = record._replace(
+                polling_place_easting="0",
+                polling_place_northing="0",
+            )
+
+        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
@@ -23,23 +34,7 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "72390208",  # THE GABLES, BARROWBY LANE, GARFORTH, LEEDS
             "72652486",  # FIRS HILL FARM, POOL BANK NEW ROAD, POOL IN WHARFEDALE, OTLEY
             "72528574",  # BANKEND FARM, BEDLAM LANE, ARTHINGTON, OTLEY
-            "72305010",  # THE BUNGALOW, DRURY LANE, HORSFORTH, LEEDS
-            "72304132",  # 34 ALEXANDRA ROAD, HORSFORTH, LEEDS
-            "72304134",  # 36 ALEXANDRA ROAD, HORSFORTH, LEEDS
-            "72304135",  # 37 ALEXANDRA ROAD, HORSFORTH, LEEDS
-            "72304136",  # 38 ALEXANDRA ROAD, HORSFORTH, LEEDS
-            "72304137",  # 39 ALEXANDRA ROAD, HORSFORTH, LEEDS
-            "72304138",  # 40 ALEXANDRA ROAD, HORSFORTH, LEEDS
-            "72304139",  # 41 ALEXANDRA ROAD, HORSFORTH, LEEDS
-            "72304140",  # 42 ALEXANDRA ROAD, HORSFORTH, LEEDS
-            "72304141",  # 43 ALEXANDRA ROAD, HORSFORTH, LEEDS
-            "72304142",  # 44 ALEXANDRA ROAD, HORSFORTH, LEEDS
-            "72304143",  # 45 ALEXANDRA ROAD, HORSFORTH, LEEDS
-            "72304144",  # 46 ALEXANDRA ROAD, HORSFORTH, LEEDS
-            "72304145",  # 47 ALEXANDRA ROAD, HORSFORTH, LEEDS
-            "72304147",  # 49 ALEXANDRA ROAD, HORSFORTH, LEEDS
             "72536279",  # THE LODGE, LOW LANE, HORSFORTH, LEEDS
-            "72108807",  # 145 KENTMERE AVENUE, LEEDS
             "72719195",  # FLAT THE SPORTSMAN STONEY ROCK LANE, BURMANTOFTS, LEEDS
             "72700401",  # FLAT THE REGENT 109 KIRKGATE, LEEDS
             "72721594",  # FLAT THE DUCK AND DRAKE INN 43 KIRKGATE, LEEDS
@@ -56,36 +51,31 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "72288079",  # 62 INTAKE ROAD, PUDSEY
             "72288080",  # 64 INTAKE ROAD, PUDSEY
             "72288081",  # 66 INTAKE ROAD, PUDSEY
-            "72541820",  # FORMER SITE OF BARN COTTAGE STATION ROAD, METHLEY, LEEDS
+            "72658394",  # ELLIS HOUSE, BRIDGE HOUSE, GLEDHOW LANE, LEEDS
         ]:
             return None
 
         if record.addressline6 in [
             # SPLIT
-            "WF3 2GL",
-            "LS9 6LP",
-            "LS17 9ED",
-            "LS13 3DX",
-            "LS25 1AX",
-            "WF3 2GN",
-            "LS15 0LG",
-            "WF3 1TB",
             "LS10 4AZ",
-            "LS12 2BN",
-            "LS7 2HN",
-            "WF3 1GQ",
-            "LS8 2QG",
-            "WF3 1FX",
             "LS10 4BD",
+            "LS12 2BN",
+            "LS13 3DX",
+            "LS14 1QA",
+            "LS14 1QX",
             "LS15 8JZ",
             "LS16 7SU",
-            "LS18 5HN",
+            "LS17 9ED",
+            "LS25 1AX",
+            "LS8 1NG",
+            "LS8 2QG",
+            "LS9 6LP",
+            "WF3 1GQ",
+            "WF3 1TB",
             # WRONG
             "LS2 7DJ",
             "LS9 8DU",
-            "LS22 6RF",
             "LS15 8FD",
-            "LS15 8QP",
             "LS18 5EU",
             "LS16 6FE",
             "LS15 8TZ",
@@ -93,36 +83,6 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "LS15 8RG",
             "WF3 3LS",
             "LS11 7PQ",
-            "LS15 7RE",
         ]:
             return None
         return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # Station change from council:
-        # old: Alder Tree Primary Academy, Potternewton Mount, LS7 2DR
-        # new: Scott Hall Leisure Centre, (Sports Hall), Scott Hall Road, Leeds, LS7 3DT
-        if record.polling_place_id == "24732":
-            record = record._replace(
-                polling_place_name="Scott Hall Leisure Centre",
-                polling_place_address_1="(Sports Hall)",
-                polling_place_address_2="Scott Hall Road",
-                polling_place_postcode="LS7 3DT",
-                polling_place_easting="429980",
-                polling_place_northing="436740",
-            )
-        # Station change from council:
-        # old: Bramley Christian Church, Main Hall, Snowden Crescent, LS13 2TP
-        # new: St Peter’s Church, (St Margaret’s Room) (Desk B) Hough Lane, LS13 3JF
-        if record.polling_place_id == "23945":
-            record = record._replace(
-                polling_place_name=" St Peter’s Church",
-                polling_place_address_1="(St Margaret’s Room) (Desk B)",
-                polling_place_address_2="Hough Lane",
-                polling_place_address_3="",
-                polling_place_postcode="LS13 3JF",
-                polling_place_easting="",
-                polling_place_northing="",
-                polling_place_uprn="",
-            )
-        return super().station_record_to_dict(record)
