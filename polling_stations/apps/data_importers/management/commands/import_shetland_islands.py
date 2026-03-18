703d01705b1cb66d7adebb4b1014b3b2c5d7baa9
awdem <wdemontmollin@gmail.com>
2026-03-18 16:49:50 +0000

    station postcode changes for ZET

--- polling_stations/apps/data_importers/management/commands/import_shetland_islands.py
+++ polling_stations/apps/data_importers/management/commands/import_shetland_islands.py
@@ -7,6 +7,16 @@ class Command(BaseHalarose2026UpdateCsvImporter):
     stations_name = "2026-05-07/2026-03-05T15:41:53.622621/Democracy Club - Idox_2026-03-05 11-54 (1).csv"
     elections = ["2026-05-07"]
 
+    def station_record_to_dict(self, record):
+        # The follow postcode changes have been agreed with the council:
+        # MID YELL PUBLIC HALL, MID YELL, SHETLAND, ZE2 9BJ
+        if record.pollingvenueid == "4":
+            record = record._replace(pollingstationpostcode="ZE2 9BN")
+        # QUARFF PUBLIC HALL, QUARFF, SHETLAND, ZE2 9EY
+        if record.pollingvenueid == "29":
+            record = record._replace(pollingstationpostcode="ZE2 9EZ")
+        return super().station_record_to_dict(record)
+
     def address_record_to_dict(self, record):
         uprn = record.uprn.strip().lstrip("0")
 
