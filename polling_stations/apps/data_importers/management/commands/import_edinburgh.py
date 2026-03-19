3a13519b65dbcfe6de526b3f457a92d36d9db09d
polling-bot-4000 <developers@democracyclub.org.uk>
2026-03-19 16:12:48 +0000

    Import script for City of Edinburgh (2026-05-07) (closes #9198)

--- polling_stations/apps/data_importers/management/commands/import_edinburgh.py
+++ polling_stations/apps/data_importers/management/commands/import_edinburgh.py
@@ -1,62 +1,57 @@
-from data_importers.management.commands import BaseHalaroseCsvImporter
+from addressbase.models import UprnToCouncil
+from data_importers.management.commands import BaseHalarose2026UpdateCsvImporter
 
 
-class Command(BaseHalaroseCsvImporter):
+class Command(BaseHalarose2026UpdateCsvImporter):
     council_id = "EDH"
-    addresses_name = "2024-07-04/2024-06-05T12:18:26.898141/Eros_SQL_Output002.csv"
-    stations_name = "2024-07-04/2024-06-05T12:18:26.898141/Eros_SQL_Output002.csv"
-    elections = ["2024-07-04"]
-    csv_encoding = "windows-1252"
+    addresses_name = "2026-05-07/2026-03-23T10:18:05.702679/lothian_vjb_combined.csv"
+    stations_name = "2026-05-07/2026-03-23T10:18:05.702679/lothian_vjb_combined.csv"
+    elections = ["2026-05-07"]
 
-    def station_record_to_dict(self, record):
-        # Removing the following stations that are outside of the council area and have no addresses assigned
-        if (
-            self.get_station_hash(record)
-            in [
-                "7-north-esk-parish-church-hall",  # North Esk Parish Church Hall, 16 Bridge Street, Musselburgh
-                "5-north-esk-parish-church-hall",  # North Esk Parish Church Hall, 16 Bridge Street, Musselburgh
-                "6-north-esk-parish-church-hall",  # North Esk Parish Church Hall, 16 Bridge Street, Musselburgh
-                "1-musselburgh-rugby-football-club",  # Musselburgh Rugby Football Club, 3A Stoneyhill Farm Road, Musselburgh
-                "2-musselburgh-rugby-football-club",  # Musselburgh Rugby Football Club, 3A Stoneyhill Farm Road, Musselburgh
-                "3-musselburgh-rugby-football-club",  # Musselburgh Rugby Football Club, 3A Stoneyhill Farm Road, Musselburgh
-                "4-musselburgh-rugby-football-club",  # Musselburgh Rugby Football Club, 3A Stoneyhill Farm Road, Musselburgh
-                "8-our-lady-of-loretto-church-hall",  # Our Lady Of Loretto Church Hall, 17 Newbigging, Musselburgh
-                "9-our-lady-of-loretto-church-hall",  # Our Lady Of Loretto Church Hall, 17 Newbigging, Musselburgh
-            ]
-        ):
-            return None
-        return super().station_record_to_dict(record)
+    def pre_import(self):
+        # We need to consider rows that don't have a uprn when importing data.
+        # However there are lots of rows for other councils in this file.
+        # So build a list of stations from rows that do have UPRNS
+        # and then use that list of stations to make sure we check relevant rows, even if they don't have a UPRN
+
+        council_uprns = set(
+            UprnToCouncil.objects.filter(lad=self.council.geography.gss).values_list(
+                "uprn", flat=True
+            )
+        )
+        self.COUNCIL_STATIONS = set()
+        data = self.get_addresses()
+
+        for record in data:
+            if record.uprn in council_uprns:
+                self.COUNCIL_STATIONS.add(record.pollingvenueid)
 
     def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
-
-        if uprn in [
-            "906499809",  # 365A BROOMHOUSE ROAD, EDINBURGH
-        ]:
+        if record.pollingvenueid not in self.COUNCIL_STATIONS:
             return None
 
-        if record.housepostcode in [
+        if record.postcode in (
             # splits
-            "EH16 6XE",
-            "EH7 4QJ",
-            "EH16 4TW",
-            "EH16 5TQ",
+            "EH4 4TN",
+            "EH16 5DZ",
             "EH11 1JU",
-            "EH14 7AB",
-            "EH11 4NU",
-            "EH10 5RF",
-            "EH11 1DU",
-            "EH6 7FL",
-            "EH6 7FN",
-            "EH29 9FQ",
-            "EH6 8DQ",
-            "EH7 6LG",
+            "EH16 5AU",
+            "EH16 5FB",
             "EH13 0DA",
-            "EH5 2DJ",
-            "EH29 9FS",
-            "EH7 6AW",
-            "EH5 2DN",
-            "EH4 7AW",
-        ]:
+            "EH6 7FN",
+            "EH6 8DQ",
+            "EH29 9FQ",
+            "EH16 4TW",
+            "EH16 6XE",
+            "EH17 8HU",
+            "EH15 1RB",
+        ):
             return None
+
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        if record.pollingvenueid not in self.COUNCIL_STATIONS:
+            return None
+
+        return super().station_record_to_dict(record)
