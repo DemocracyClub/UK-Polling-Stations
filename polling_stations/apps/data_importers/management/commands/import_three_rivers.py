c4ecf929467ddaab6f042434aebea96aaeb06c6b
polling-bot-4000 <developers@democracyclub.org.uk>
2026-03-24 11:55:53 +0000

    Import script for Three Rivers (2026-05-07) (closes #9238)

--- polling_stations/apps/data_importers/management/commands/import_three_rivers.py
+++ polling_stations/apps/data_importers/management/commands/import_three_rivers.py
@@ -4,31 +4,11 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "THE"
     addresses_name = (
-        "2025-05-01/2025-03-06T09:55:19.565747/Democracy_Club__01May2025.tsv"
+        "2026-05-07/2026-03-24T11:55:44.867070/Democracy_Club__07May2026.tsv"
     )
     stations_name = (
-        "2025-05-01/2025-03-06T09:55:19.565747/Democracy_Club__01May2025.tsv"
+        "2026-05-07/2026-03-24T11:55:44.867070/Democracy_Club__07May2026.tsv"
     )
-    elections = ["2025-05-01"]
+    elections = ["2026-05-07"]
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
-
-    def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-        if uprn in [
-            "200000937007",  # OAK COTTAGE, HIGH ROAD, LEAVESDEN, WATFORD
-            "200000937008",  # WAYSIDE, HIGH ROAD, LEAVESDEN, WATFORD
-        ]:
-            return None
-        if record.addressline6 in [
-            # split
-            "WD3 6AB",
-            "WD19 4LS",
-        ]:
-            return None
-        return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # Council confirmed postcode for:
-        # Coates Way School, Coates Way, Garston, Watford, WD25 9NW (id: 6026)
-        return super().station_record_to_dict(record)
