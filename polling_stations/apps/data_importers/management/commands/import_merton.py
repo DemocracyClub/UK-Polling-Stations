adddd83a5a3d3820aa62208cd54267fbe8379619
polling-bot-4000 <developers@democracyclub.org.uk>
2026-03-24 14:50:05 +0000

    Import script for Merton (2026-05-07) (closes #9241)

--- polling_stations/apps/data_importers/management/commands/import_merton.py
+++ polling_stations/apps/data_importers/management/commands/import_merton.py
@@ -1,20 +1,15 @@
-from data_importers.management.commands import BaseHalaroseCsvImporter
+from data_importers.management.commands import BaseHalarose2026UpdateCsvImporter
 
 
-class Command(BaseHalaroseCsvImporter):
+class Command(BaseHalarose2026UpdateCsvImporter):
     council_id = "MRT"
-    addresses_name = (
-        "2024-07-04/2024-06-01T11:42:53.841573/Democracy Club Data GE 2024.csv"
-    )
-    stations_name = (
-        "2024-07-04/2024-06-01T11:42:53.841573/Democracy Club Data GE 2024.csv"
-    )
-    elections = ["2024-07-04"]
+    addresses_name = "2026-05-07/2026-03-24T14:49:49.615691/Democracy Club - Idox_2026-03-24 13-07.csv"
+    stations_name = "2026-05-07/2026-03-24T14:49:49.615691/Democracy Club - Idox_2026-03-24 13-07.csv"
+    elections = ["2026-05-07"]
 
     def address_record_to_dict(self, record):
-        if record.housepostcode in [
+        if record.postcode in [
             # split
-            "KT3 6LZ",
             "CR4 2JR",
         ]:
             return None
