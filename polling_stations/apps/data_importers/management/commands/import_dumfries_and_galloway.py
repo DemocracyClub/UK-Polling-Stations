c417f21769d22beaafa3efac579c6dd68aa7b89d
polling-bot-4000 <developers@democracyclub.org.uk>
2026-03-24 17:03:52 +0000

    Import script for Dumfries and Galloway (2026-05-07) (closes #9252)

--- polling_stations/apps/data_importers/management/commands/import_dumfries_and_galloway.py
+++ polling_stations/apps/data_importers/management/commands/import_dumfries_and_galloway.py
@@ -1,30 +1,37 @@
-from data_importers.base_importers import BaseCsvStationsShpDistrictsImporter
-from django.contrib.gis.geos import Point
+from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 
-class Command(BaseCsvStationsShpDistrictsImporter):
+class Command(BaseDemocracyCountsCsvImporter):
     council_id = "DGY"
-    stations_name = "2024-07-04/2024-07-01T15:29:07/DGC_Polling_Stations_June24.csv"
-    districts_name = (
-        "2024-07-04/2024-07-01T15:29:07/Polling_Districts_Changes_Approved.shp"
-    )
-    elections = ["2024-07-04"]
-
-    def pre_import(self):
-        pass
+    addresses_name = "2026-05-07/2026-03-24T17:03:44.423040/D&G Democracy Counts - Polling Districts 240326.csv"
+    stations_name = "2026-05-07/2026-03-24T17:03:44.423040/D&G Democracy Counts - Polling Stations 240326.csv"
+    elections = ["2026-05-07"]
+    csv_encoding = "utf-16le"
 
     def station_record_to_dict(self, record):
-        return {
-            "internal_council_id": f"{record.district_c}-{record.uprn}",
-            "polling_district_id": record.district_c.strip(),
-            "postcode": None,
-            "address": record.polling_pl,
-            "location": Point(int(record.easting), int(record.northing), srid=27700),
-        }
+        # Removing bad coordinates for:
+        # THORNHILL COMMUNITY CENTRE, EAST BACK STREET, DG3 5LH
+        if record.stationcode in ["DFS11", "DFS10"]:
+            record = record._replace(
+                xordinate="0",
+                yordinate="0",
+            )
         return super().station_record_to_dict(record)
 
-    def district_record_to_dict(self, record):
-        return {
-            "internal_council_id": record["district_c"].strip(),
-            "name": record["district_n"].strip(),
-        }
+    def address_record_to_dict(self, record):
+        uprn = record.uprn.strip().lstrip("0")
+
+        if uprn in [
+            "137072115",  # THE STEADING, WALLACETON, AULDGIRTH, DUMFRIES
+        ]:
+            return None
+        if record.postcode in [
+            # splits
+            "DG5 4HS",
+            "DG8 0BZ",
+            # suspect
+            "DG7 3PF",
+            "DG7 3PQ",
+        ]:
+            return None
+        return super().address_record_to_dict(record)
