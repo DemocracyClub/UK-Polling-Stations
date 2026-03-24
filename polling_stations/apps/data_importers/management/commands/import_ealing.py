4c793ef51bd5642877f130a51726679e90f6d2f7
polling-bot-4000 <developers@democracyclub.org.uk>
2026-03-24 14:55:45 +0000

    Import script for Ealing (2026-05-07) (closes #9243)

--- polling_stations/apps/data_importers/management/commands/import_ealing.py
+++ polling_stations/apps/data_importers/management/commands/import_ealing.py
@@ -3,36 +3,7 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "EAL"
-    addresses_name = (
-        "2024-07-04/2024-06-14T15:39:39.081592/Democracy_Club__04July2024.tsv"
-    )
-    stations_name = (
-        "2024-07-04/2024-06-14T15:39:39.081592/Democracy_Club__04July2024.tsv"
-    )
-    elections = ["2024-07-04"]
+    addresses_name = "2026-05-07/2026-03-24T14:55:25.345777/EC Democracy_Club__07May2026 Polling Station Lookup.tsv"
+    stations_name = "2026-05-07/2026-03-24T14:55:25.345777/EC Democracy_Club__07May2026 Polling Station Lookup.tsv"
+    elections = ["2026-05-07"]
     csv_delimiter = "\t"
-
-    def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "12180637",  # ANNEXE 20 LOCARNO ROAD, GREENFORD
-            "12147146",  # 35A PRIORY GARDENS, LONDON
-            "12181729",  # FLAT 3 55 PARK AVENUE, PARK ROYAL
-            "12181728",  # FLAT 2 55 PARK AVENUE, PARK ROYAL
-            "12181730",  # FLAT 4 55 PARK AVENUE, PARK ROYAL
-            "12181731",  # FLAT 5 55 PARK AVENUE, PARK ROYAL
-        ]:
-            return None
-
-        if record.addressline6 in [
-            "W4 5HL",  # split
-        ]:
-            return None
-        return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # The postcode for the following station has been confirmed by the council:
-        # West London University Sports Pavilion, (Home of Pitshanger Football Club), Argyle Road, W13 8EL
-
-        return super().station_record_to_dict(record)
