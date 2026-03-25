699d81fcc28055e0ddcbd2564352ff7004a6ca87
polling-bot-4000 <developers@democracyclub.org.uk>
2026-03-25 13:16:20 +0000

    Import script for Dumfries and Galloway (2026-05-07) (closes #9251)

--- polling_stations/apps/data_importers/management/commands/import_dumfries_and_galloway.py
+++ polling_stations/apps/data_importers/management/commands/import_dumfries_and_galloway.py
@@ -3,7 +3,6 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "DGY"
-    addresses_name = "2026-05-07/2026-03-25T13:10:50.926263/D&G Democracy Counts - Polling Districts 250326.csv"
-    stations_name = "2026-05-07/2026-03-25T13:10:50.926263/D&G Democracy Counts - Polling Stations 250326.csv"
+    addresses_name = "2026-05-07/2026-03-25T13:16:15.827019/D&G Democracy Counts - Polling Districts 250326.csv"
+    stations_name = "2026-05-07/2026-03-25T13:16:15.827019/D&G Democracy Counts - Polling Stations 250326.csv"
     elections = ["2026-05-07"]
-    csv_encoding = "utf-16le"
