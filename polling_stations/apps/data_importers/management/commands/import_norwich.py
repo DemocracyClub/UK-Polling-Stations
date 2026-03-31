27267976bc78aaf7017a81bb426b824dc072b0bd
polling-bot-4000 <developers@democracyclub.org.uk>
2026-03-31 15:27:22 +0100

    Import script for Norwich (2026-05-07) (closes #9287)

--- polling_stations/apps/data_importers/management/commands/import_norwich.py
+++ polling_stations/apps/data_importers/management/commands/import_norwich.py
@@ -3,30 +3,36 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "NOW"
-    addresses_name = "2024-07-04/2024-06-12T10:48:23.075732/NOW_districts_combined.csv"
-    stations_name = "2024-07-04/2024-06-12T10:48:23.075732/Democracy Club Polling Stations Norwich 20240604.csv"
-    elections = ["2024-07-04"]
+    addresses_name = "2026-05-07/2026-04-08T13:23:22.994652/NOW_districts_UTF8.csv"
+    stations_name = (
+        "2026-05-07/2026-04-08T13:23:22.994652/Democracy Club - Polling Stations.csv"
+    )
+    elections = ["2026-05-07"]
 
     def station_record_to_dict(self, record):
-        # Removing the following stations because they are outside of the council boundaries and have no addresses assigned:
-        if record.stationcode in [
-            "NS42",
-            "NS41",
-        ]:
-            return None
+        # removing bad coordinates pending council response:
+        if (
+            record.stationcode
+            in [
+                "10CR1",  # PILLING PARK COMMUNITY CENTRE, 23 PILLING PARK ROAD, NORWICH, NR1 4PA
+                "45TO1",  # JENNY LIND COMMUNITY ROOM, VAUXHALL STREET ENTRANCE, 24 SUFFOLK SQUARE, NORWICH, NR2 2AA
+                "46TO2",  # HOLY TRINITY CHURCH HALL (FORMERLY CAMBRIDGE STREET HALL), 54 CAMBRIDGE STREET , NORWICH, NR2 2BB
+                "36NE5",  # DOURO PLACE CHAPEL, DOURO PLACE, DEREHAM ROAD, NORWICH, NR2 4BQ
+                "5UN5A",  # WEST EARLHAM COMMUNITY CENTRE, 10 WILBERFORCE ROAD, NORWICH, NR5 8ND
+                "50UN1",  # ST MARY'S CHURCH HALL, HUTCHINSON ROAD, NORWICH, NR5 8LB
+                "55WE2",  # ALIVE HOUSE, NELSON STREET, NORWICH NR2 4DR
+                "44TH4",  # ST MATTHEW'S CHURCH, TELEGRAPH LANE WEST, NORWICH, NR1 4JA
+            ]
+        ):
+            record = record._replace(xordinate="0", yordinate="0")
 
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
-
-        if (
-            uprn
-            in [
-                "10024023874",  # LIVING ACCOMMODATION THE MARSH HARRIER IPSWICH ROAD, NORWICH
-                "200004349456",  # 14A IPSWICH ROAD, NORWICH
-            ]
-        ):
+        if record.postcode in [
+            # looks wrong
+            "NR5 8NA",
+        ]:
             return None
 
         return super().address_record_to_dict(record)
