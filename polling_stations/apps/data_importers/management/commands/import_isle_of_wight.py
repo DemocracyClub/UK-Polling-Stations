8da91f596492283ea2fd4dd9dc2f83552a07ac81
awdem <wdemontmollin@gmail.com>
2026-04-07 16:09:21 +0100

    fix file path for IOW

--- polling_stations/apps/data_importers/management/commands/import_isle_of_wight.py
+++ polling_stations/apps/data_importers/management/commands/import_isle_of_wight.py
@@ -4,13 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "IOW"
     addresses_name = (
-        "2026-05-07/2026-03-27T11:29:48.036646/Democracy_Club__07May2026.CSV"
+        "2026-05-07/2026-04-07T16:00:02.432211/Democracy_Club__07May2026_fixed.CSV"
     )
     stations_name = (
-        "2026-05-07/2026-03-27T11:29:48.036646/Democracy_Club__07May2026.CSV"
+        "2026-05-07/2026-04-07T16:00:02.432211/Democracy_Club__07May2026_fixed.CSV"
     )
     elections = ["2026-05-07"]
-    csv_encoding = "windows-1252"
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
