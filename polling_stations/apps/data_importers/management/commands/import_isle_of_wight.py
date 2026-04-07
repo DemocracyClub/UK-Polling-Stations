7b0e97230d047a1b1bc5d3871968df43e384d72a
polling-bot-4000 <developers@democracyclub.org.uk>
2026-04-07 16:00:12 +0100

    Import script for Isle of Wight (2026-05-07) (closes #9299)

--- polling_stations/apps/data_importers/management/commands/import_isle_of_wight.py
+++ polling_stations/apps/data_importers/management/commands/import_isle_of_wight.py
@@ -4,32 +4,9 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "IOW"
     addresses_name = (
-        "2026-05-07/2026-03-27T11:29:48.036646/Democracy_Club__07May2026.CSV"
+        "2026-05-07/2026-04-07T16:00:02.432211/Democracy_Club__07May2026_fixed.CSV"
     )
     stations_name = (
-        "2026-05-07/2026-03-27T11:29:48.036646/Democracy_Club__07May2026.CSV"
+        "2026-05-07/2026-04-07T16:00:02.432211/Democracy_Club__07May2026_fixed.CSV"
     )
     elections = ["2026-05-07"]
-    csv_encoding = "windows-1252"
-
-    def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "100060763282",  # 28A HIGH STREET, RYDE
-            "10023714741",  # FYNELEA, PRINCES ROAD, FRESHWATER
-            "200001868290",  # THE ORCHARDS, PRINCES ROAD, FRESHWATER
-            "100062441673",  # WHITE COTTAGE, MILITARY ROAD, ATHERFIELD, VENTNOR
-            "10024249197",  # BATTS LODGE, BATTS ROAD, SHANKLIN
-            "10003318523",  # THE FIELD, MAIN ROAD, HAVENSTREET, RYDE
-        ]:
-            return None
-
-        if record.addressline6 in [
-            # split
-            "PO30 2DH",
-            "PO36 9NQ",
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
