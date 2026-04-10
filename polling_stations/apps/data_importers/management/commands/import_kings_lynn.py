f456b2d80519a86ede8b6f8c4e23ee47e3882bec
polling-bot-4000 <developers@democracyclub.org.uk>
2026-04-10 09:26:44 +0100

    Import script for King's Lynn & West Norfolk (2026-05-07) (closes #9307)

--- polling_stations/apps/data_importers/management/commands/import_kings_lynn.py
+++ polling_stations/apps/data_importers/management/commands/import_kings_lynn.py
@@ -4,29 +4,10 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "KIN"
     addresses_name = (
-        "2024-07-04/2024-05-29T08:55:20.820012/Democracy_Club__04July2024.tsv"
+        "2026-05-07/2026-04-10T09:26:35.937043/Democracy_Club__07May2026.tsv"
     )
     stations_name = (
-        "2024-07-04/2024-05-29T08:55:20.820012/Democracy_Club__04July2024.tsv"
+        "2026-05-07/2026-04-10T09:26:35.937043/Democracy_Club__07May2026.tsv"
     )
-    elections = ["2024-07-04"]
+    elections = ["2026-05-07"]
     csv_delimiter = "\t"
-
-    def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
-
-        if uprn in [
-            "10000033946",  # 81B HIGH STREET, KING'S LYNN
-            "10090917704",  # 199 STATION ROAD, WATLINGTON, KING'S LYNN
-            "10013001170",  # WHITE DYKE BUNGALOW, BLACK DYKE ROAD, HOCKWOLD, THETFORD
-        ]:
-            return None
-        if record.addressline6 in [
-            # split
-            "PE30 5BD",
-            # look wrong
-            "PE30 1JG",
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
