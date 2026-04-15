219a0142952e9944ab9ad2fc293c8dfcc7e48e67
polling-bot-4000 <developers@democracyclub.org.uk>
2026-04-15 11:55:54 +0100

    Import script for Wigan (2026-05-07) (closes #9319)

--- polling_stations/apps/data_importers/management/commands/import_wigan.py
+++ polling_stations/apps/data_importers/management/commands/import_wigan.py
@@ -1,14 +1,18 @@
-from data_importers.management.commands import BaseXpressWebLookupCsvImporter
+from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
 
 
-class Command(BaseXpressWebLookupCsvImporter):
+class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WGN"
-    addresses_name = "2024-07-04/2024-06-22T08:32:01.577572/wigan-cleaned.csv"
-    stations_name = "2024-07-04/2024-06-22T08:32:01.577572/wigan-cleaned.csv"
-    elections = ["2024-07-04"]
+    addresses_name = (
+        "2026-05-07/2026-04-15T11:55:41.446254/Democracy_Club__07May2026.CSV"
+    )
+    stations_name = (
+        "2026-05-07/2026-04-15T11:55:41.446254/Democracy_Club__07May2026.CSV"
+    )
+    elections = ["2026-05-07"]
 
     def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
+        uprn = record.property_urn.strip().lstrip("0")
 
         if uprn in [
             "200002861926",  # THE COTTAGE, SANDY LANE, HINDLEY, WIGAN
@@ -16,19 +20,24 @@ class Command(BaseXpressWebLookupCsvImporter):
             "100011823007",  # 165 WIGAN ROAD, ASHTON-IN-MAKERFIELD, WIGAN
             "10095424360",  # 109 ANCHOR FIELD, LEIGH
             "10095424324",  # 73 ANCHOR FIELD, LEIGH
-            "10095424371",  # 120 ANCHOR FIELD, LEIGH
+            "10096704515",  # 5 LILY BANK MEADOW, LEIGH
         ]:
             return None
 
-        if record.postcode in [
+        if record.post_code in [
             # splits
             "WN7 4TF",
-            "WN7 1QA",
             "WN7 2LS",
-            # suspect
-            "WN7 4GL",
-            "WN7 4GQ",
         ]:
             return None
 
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # remove wrong coords for: St Aidans Parish Centre, Highfield Grange Ave, Winstanley, Wigan, WN3 6EE
+        if record.polling_place_id == "12916":
+            record = record._replace(
+                polling_place_easting="0", polling_place_northing="0"
+            )
+
+        return super().station_record_to_dict(record)
