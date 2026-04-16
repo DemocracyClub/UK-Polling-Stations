b3669c2f2630a000ade5d7a410fbc4717ae1b9ab
polling-bot-4000 <developers@democracyclub.org.uk>
2026-04-16 16:06:55 +0100

    Import script for Hertsmere (2026-05-07) (closes #9328)

--- polling_stations/apps/data_importers/management/commands/import_hertsmere.py
+++ polling_stations/apps/data_importers/management/commands/import_hertsmere.py
@@ -1,8 +1,8 @@
-from data_importers.management.commands import BaseHalaroseCsvImporter
+from data_importers.management.commands import BaseHalarose2026UpdateCsvImporter
 
 
-class Command(BaseHalaroseCsvImporter):
+class Command(BaseHalarose2026UpdateCsvImporter):
     council_id = "HER"
-    addresses_name = "2026-05-07/2025-12-11T14:43:48.145373/Democracy Club data.csv"
-    stations_name = "2026-05-07/2025-12-11T14:43:48.145373/Democracy Club data.csv"
+    addresses_name = "2026-05-07/2026-04-16T16:06:49.796948/Democracy Club - Idox_2026-04-16 16-06.csv"
+    stations_name = "2026-05-07/2026-04-16T16:06:49.796948/Democracy Club - Idox_2026-04-16 16-06.csv"
     elections = ["2026-05-07"]
