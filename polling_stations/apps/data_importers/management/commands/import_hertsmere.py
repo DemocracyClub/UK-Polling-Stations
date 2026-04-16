e587dc722efc7b2d9d5ca164b4da1c10e3ff8787
polling-bot-4000 <developers@democracyclub.org.uk>
2026-04-16 16:06:55 +0100

    Import script for Hertsmere (2026-05-07) (closes #9328)

--- polling_stations/apps/data_importers/management/commands/import_hertsmere.py
+++ polling_stations/apps/data_importers/management/commands/import_hertsmere.py
@@ -1,31 +1,33 @@
-from data_importers.management.commands import BaseHalaroseCsvImporter
+from data_importers.management.commands import BaseHalarose2026UpdateCsvImporter
 
 
-class Command(BaseHalaroseCsvImporter):
+class Command(BaseHalarose2026UpdateCsvImporter):
     council_id = "HER"
-    addresses_name = "2025-05-01/2025-03-12T17:07:55.818621/Eros_SQL_Output001.csv"
-    stations_name = "2025-05-01/2025-03-12T17:07:55.818621/Eros_SQL_Output001.csv"
-    elections = ["2025-05-01"]
+    addresses_name = "2026-05-07/2026-04-16T16:06:49.796948/Democracy Club - Idox_2026-04-16 16-06.csv"
+    stations_name = "2026-05-07/2026-04-16T16:06:49.796948/Democracy Club - Idox_2026-04-16 16-06.csv"
+    elections = ["2026-05-07"]
 
-    # Following warnings checked and no need for correction:
-    # WARNING: Polling station MERIDEN COMMUNITY CENTRE (18-meriden-community-centre) is in Watford Borough Council (WAT)
+    # Commented out temporarily for a by-election
 
-    def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
+    # # Following warnings checked and no need for correction:
+    # # WARNING: Polling station MERIDEN COMMUNITY CENTRE (18-meriden-community-centre) is in Watford Borough Council (WAT)
 
-        if (
-            uprn
-            in [
-                "10013031843",  # SOUTH LODGE, BLACKHORSE LANE, SOUTH MIMMS, POTTERS BAR
-                "10034171504",  # SHENLEY LODGE COTTAGE MANOR LODGE SCHOOL RIDGE HILL, SHENLEY
-                "10034165002",  # 77 ALLUM LANE, ELSTREE, BOREHAMWOOD
-            ]
-        ):
-            return None
+    # def address_record_to_dict(self, record):
+    #     uprn = record.uprn.strip().lstrip("0")
 
-        if record.housepostcode in [
-            # split
-            "WD25 8BP",
-        ]:
-            return None
-        return super().address_record_to_dict(record)
+    #     if (
+    #         uprn
+    #         in [
+    #             "10013031843",  # SOUTH LODGE, BLACKHORSE LANE, SOUTH MIMMS, POTTERS BAR
+    #             "10034171504",  # SHENLEY LODGE COTTAGE MANOR LODGE SCHOOL RIDGE HILL, SHENLEY
+    #             "10034165002",  # 77 ALLUM LANE, ELSTREE, BOREHAMWOOD
+    #         ]
+    #     ):
+    #         return None
+
+    #     if record.housepostcode in [
+    #         # split
+    #         "WD25 8BP",
+    #     ]:
+    #         return None
+    #     return super().address_record_to_dict(record)
