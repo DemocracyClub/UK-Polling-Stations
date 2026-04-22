24465bdf353d38f339004154613d03a76b1b0252
polling-bot-4000 <developers@democracyclub.org.uk>
2026-04-22 14:43:09 +0100

    Import script for Vale of Glamorgan (2026-05-07) (closes #9365)

--- polling_stations/apps/data_importers/management/commands/import_vale_of_glamorgan.py
+++ polling_stations/apps/data_importers/management/commands/import_vale_of_glamorgan.py
@@ -3,10 +3,9 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "VGL"
-    addresses_name = "2026-05-07/2026-02-10T14:52:58.106281/Democracy Club Polling Districts VoG 2.csv"
-    stations_name = "2026-05-07/2026-02-10T14:52:58.106281/Democracy Club Polling Stations VoG 2.csv"
+    addresses_name = "2026-05-07/2026-04-22T15:31:44.783961/VGL_Districts_combined.csv"
+    stations_name = "2026-05-07/2026-04-22T15:31:44.783961/VGL_Stations_combined.csv"
     elections = ["2026-05-07"]
-    csv_encoding = "utf-16le"
 
     def station_record_to_dict(self, record):
         # Fixing bad coordinates for:
@@ -20,6 +19,13 @@ class Command(BaseDemocracyCountsCsvImporter):
         return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
+        uprn = record.uprn.strip().lstrip("0")
+
+        if uprn in [
+            "64118392",  # LONGBOAT, KNOLL CLOSE, COG ROAD, SULLY, PENARTH
+            "200001640409",  # TY BWCH, DINGLE LANE, PENARTH
+        ]:
+            return None
         if record.postcode in [
             # split
             "CF62 6BA",
