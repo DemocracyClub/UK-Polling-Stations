9e8e85c2e45e433e7e7aa565cc50aaf0a92a3296
polling-bot-4000 <developers@democracyclub.org.uk>
2026-04-28 10:00:30 +0100

    Import script for Enfield (2026-05-07) (closes #9380)

--- polling_stations/apps/data_importers/management/commands/import_enfield.py
+++ polling_stations/apps/data_importers/management/commands/import_enfield.py
@@ -3,8 +3,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "ENF"
-    addresses_name = "2026-05-07/2026-04-18T10:42:27.054426/Democracy_Club__07May2026 - Updated 17.4.26.tsv"
-    stations_name = "2026-05-07/2026-04-18T10:42:27.054426/Democracy_Club__07May2026 - Updated 17.4.26.tsv"
+    addresses_name = (
+        "2026-05-07/2026-04-28T10:00:19.619113/Democracy_Club__07May2026 - 27-4-26.tsv"
+    )
+    stations_name = (
+        "2026-05-07/2026-04-28T10:00:19.619113/Democracy_Club__07May2026 - 27-4-26.tsv"
+    )
     elections = ["2026-05-07"]
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
