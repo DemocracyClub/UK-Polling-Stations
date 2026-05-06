7197ce0cce8620c48a0768aa86f66b3377185968
awdem <wdemontmollin@gmail.com>
2026-05-06 11:31:48 +0100

    fix bad station point for WOT

--- polling_stations/apps/data_importers/management/commands/import_worthing.py
+++ polling_stations/apps/data_importers/management/commands/import_worthing.py
@@ -11,3 +11,13 @@ class Command(BaseXpressDemocracyClubCsvImporter):
     )
     elections = ["2026-05-07"]
     csv_delimiter = "\t"
+
+    def station_record_to_dict(self, record):
+        # fix bad point for Richmond Room:
+        # https://app.asana.com/1/1204880536137786/project/1207538772343223/task/1214570745863239?focus=true
+        if record.polling_place_id == "3617":
+            record = record._replace(
+                polling_place_easting="514751",
+                polling_place_northing="102980",
+            )
+        return super().station_record_to_dict(record)
