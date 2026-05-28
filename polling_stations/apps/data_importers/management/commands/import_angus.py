f1f5f33988af9875641450258bbb5a85f2446df9
polling-bot-4000 <developers@democracyclub.org.uk>
2026-05-28 15:29:46 +0100

    Import script for Angus (2026-06-18) (closes #9441)

--- polling_stations/apps/data_importers/management/commands/import_angus.py
+++ polling_stations/apps/data_importers/management/commands/import_angus.py
@@ -1,40 +1,29 @@
-from data_importers.management.commands import BaseHalarose2026UpdateCsvImporter
+from data_importers.management.commands import BaseHalaroseCsvImporter
 
 
-class Command(BaseHalarose2026UpdateCsvImporter):
+class Command(BaseHalaroseCsvImporter):
     council_id = "ANS"
-    addresses_name = "2026-05-07/2026-03-16T10:43:33.402171/Democracy Club - Idox_2026-02-26 10-26.csv"
-    stations_name = "2026-05-07/2026-03-16T10:43:33.402171/Democracy Club - Idox_2026-02-26 10-26.csv"
-    elections = ["2026-05-07"]
+    addresses_name = "2026-06-18/2026-05-28T15:29:36.811918/Democracy Club - Idox_2026-05-28 14-58.csv"
+    stations_name = "2026-06-18/2026-05-28T15:29:36.811918/Democracy Club - Idox_2026-05-28 14-58.csv"
+    elections = ["2026-06-18"]
 
     def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
-
-        if uprn in [
-            "117080095",  # LOWNIE HILL COTTAGE, FORFAR
-            "117087696",  # WOODSIDE, NETHER TULLOES FARM, FORFAR
-        ]:
-            return None
-
         if record.postcode in [
             # splits
-            "DD9 7EZ",
-            "DD8 2SF",
-            "DD8 4QB",
-            "DD8 5PP",
             "DD8 2TJ",
         ]:
             return None
 
         return super().address_record_to_dict(record)
 
-    def station_record_to_dict(self, record):
-        # add missing postcode for: Glamis Heritage Education Centre, Glamis
-        if record.pollingstationnumber == "80":
-            record = record._replace(pollingstationpostcode="DD8 1RS")
+    # maintaining some corrections through by-election
+    # def station_record_to_dict(self, record):
+    #     # add missing postcode for: Glamis Heritage Education Centre, Glamis
+    #     if record.pollingstationnumber == "80":
+    #         record = record._replace(pollingstationpostcode="DD8 1RS")
 
-        # add missing postcode for: Kingoldrum Village Hall, Kingoldrum
-        if record.pollingstationnumber == "70":
-            record = record._replace(pollingstationpostcode="DD8 5HW")
+    #     # add missing postcode for: Kingoldrum Village Hall, Kingoldrum
+    #     if record.pollingstationnumber == "70":
+    #         record = record._replace(pollingstationpostcode="DD8 5HW")
 
-        return super().station_record_to_dict(record)
+    #     return super().station_record_to_dict(record)
