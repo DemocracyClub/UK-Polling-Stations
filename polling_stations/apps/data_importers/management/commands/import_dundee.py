7170e4c11cf239fae0bafc5f955fd90e480a03eb
polling-bot-4000 <developers@democracyclub.org.uk>
2026-05-28 11:17:43 +0100

    Import script for Dundee (2026-06-18) (closes #9437)

--- polling_stations/apps/data_importers/management/commands/import_dundee.py
+++ polling_stations/apps/data_importers/management/commands/import_dundee.py
@@ -1,33 +1,45 @@
-from data_importers.management.commands import BaseHalarose2026UpdateCsvImporter
+from data_importers.management.commands import BaseHalaroseCsvImporter
 
 
-class Command(BaseHalarose2026UpdateCsvImporter):
+class Command(BaseHalaroseCsvImporter):
     council_id = "DND"
-    addresses_name = "2026-05-07/2026-02-23T10:35:14.409147/Democracy Club - Idox_2026-02-20 13-27.csv"
-    stations_name = "2026-05-07/2026-02-23T10:35:14.409147/Democracy Club - Idox_2026-02-20 13-27.csv"
-    elections = ["2026-05-07"]
+    addresses_name = "2026-06-18/2026-05-28T11:17:37.670858/Democracy Club - Idox_2026-05-28 11-14.csv"
+    stations_name = "2026-06-18/2026-05-28T11:17:37.670858/Democracy Club - Idox_2026-05-28 11-14.csv"
+    elections = ["2026-06-18"]
 
-    def station_record_to_dict(self, record):
-        # The following stations' postcodes have been verified by the council:
-        # 'Wellgate Sheltered Housing, 8 King Street, Dundee, DD1 2JB' (id: 78)
-        # 'Marryat Hall, City Square, Dundee, DD1 3BG' (id: 79)
-        # 'Foula Terrace Sheltered, Housing Communal Lounge, Foula Terrace, Dundee, DD4 9SS' (id: 5)
-        return super().station_record_to_dict(record)
+    # ignore the warnings for the following stations, council has confirmed postcodes:
+    # Foula Terrace Sheltered, Housing Communal Lounge, Foula Terrace, Dundee, DD4 9SS
+    # St Margaret's Church Hall, 1 Guthrie Terrace, Broughty Ferry, Dundee, DD5 2QY
 
     def address_record_to_dict(self, record):
+        uprn = record.uprn.strip().lstrip("0")
+
+        if (
+            uprn
+            in [
+                # "117080095",  # LOWNIE HILL COTTAGE, FORFAR
+                # "117087696",  # WOODSIDE, NETHER TULLOES FARM, FORFAR
+            ]
+        ):
+            return None
+
         if record.postcode in [
-            # split
-            "DD2 1DJ",
-            "DD3 6HW",
-            "DD4 0TQ",
+            # splits
+            "DD4 0FD",
             "DD4 9ET",
-            "DD1 1JS",
-            "DD3 0JG",
-            "DD4 6JU",
-            "DD4 8RX",
-            "DD4 0FD"
-            # suspect
-            "DD2 1RT",
         ]:
             return None
+
         return super().address_record_to_dict(record)
+
+    def station_record_to_dict(self, record):
+        # maintaing corrections through a by-election
+        # # add missing postcode for: Glamis Heritage Education Centre, Glamis
+        # if record.pollingstationnumber == "80":
+        #     record = record._replace(pollingstationpostcode="DD8 1RS")
+
+        # # add missing postcode for: Kingoldrum Village Hall, Kingoldrum
+        # if record.pollingstationnumber == "70":
+        #     record = record._replace(pollingstationpostcode="DD8 5HW")
+
+        return super().station_record_to_dict(record)
