23a5174435c0cc9175499c8f031191ee945faced
chris48s <git@chris-shaw.dev>
2026-06-01 15:24:28 +0100

    import script for Lambeth 2026-07-09

--- polling_stations/apps/data_importers/management/commands/import_lambeth.py
+++ polling_stations/apps/data_importers/management/commands/import_lambeth.py
@@ -3,11 +3,11 @@ from data_importers.management.commands import BaseFcsDemocracyClubApiImporter
 
 class Command(BaseFcsDemocracyClubApiImporter):
     council_id = "LBH"
-    elections = ["2026-05-07"]
-    fcs_election_id = 52
+    elections = ["2026-07-09"]
+    fcs_election_id = 54
 
-    # comment
     def station_record_to_dict(self, record):
+        """
         station_id = record.get(self.station_id_field)
 
         # add point for: Weir Link Community Centre, 33 Weir Road, London, SW12 0NU
@@ -19,5 +19,6 @@ class Command(BaseFcsDemocracyClubApiImporter):
         if station_id == 11311:
             record["longitude"] = -0.102054
             record["latitude"] = 51.438839
+        """
 
         return super().station_record_to_dict(record)
