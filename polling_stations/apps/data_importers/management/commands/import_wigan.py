1aa8dee74f06e1566d8fa031f3106e7882d86a7a
polling-bot-4000 <developers@democracyclub.org.uk>
2026-06-01 13:14:41 +0100

    Import script for Wigan (2026-06-18) (closes #9445)

--- polling_stations/apps/data_importers/management/commands/import_wigan.py
+++ polling_stations/apps/data_importers/management/commands/import_wigan.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WGN"
     addresses_name = (
-        "2026-05-07/2026-04-15T11:55:41.446254/Democracy_Club__07May2026.CSV"
+        "2026-06-18/2026-06-01T13:14:34.281958/Democracy_Club__18June2026.CSV"
     )
     stations_name = (
-        "2026-05-07/2026-04-15T11:55:41.446254/Democracy_Club__07May2026.CSV"
+        "2026-06-18/2026-06-01T13:14:34.281958/Democracy_Club__18June2026.CSV"
     )
-    elections = ["2026-05-07"]
+    elections = ["2026-06-18"]
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
@@ -34,18 +34,11 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         return super().address_record_to_dict(record)
 
     def station_record_to_dict(self, record):
-        # remove wrong coords for: St Aidans Parish Centre, Highfield Grange Ave, Winstanley, Wigan, WN3 6EE
-        if record.polling_place_id == "12916":
+        # remove wrong coords for:
+        # St Aidans Parish Centre, Highfield Grange Ave, Winstanley, Wigan, WN3 6EE
+        if record.polling_place_id == "13354":
             record = record._replace(
                 polling_place_easting="0", polling_place_northing="0"
-            )
-
-        # bug report # 835
-        # fix co-ordinates for Ince Independent Methodist Church
-        if record.polling_place_id == "12976":
-            record = record._replace(
-                polling_place_easting="359914",
-                polling_place_northing="405344",
             )
 
         return super().station_record_to_dict(record)
