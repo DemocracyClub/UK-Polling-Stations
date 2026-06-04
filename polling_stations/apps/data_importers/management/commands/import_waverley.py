cb6a25ef9273f0abc2b9ef3fa85489fd073ab54b
polling-bot-4000 <developers@democracyclub.org.uk>
2026-06-04 13:46:40 +0100

    Import script for Waverley (2026-07-07) (closes #9460)

--- polling_stations/apps/data_importers/management/commands/import_waverley.py
+++ polling_stations/apps/data_importers/management/commands/import_waverley.py
@@ -4,36 +4,38 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WAE"
     addresses_name = (
-        "2026-05-07/2026-02-06T11:31:58.095008/Democracy_Club__07May2026.CSV"
+        "2026-07-07/2026-06-08T15:28:11.723541/Democracy_Club__07July2026.tsv"
     )
     stations_name = (
-        "2026-05-07/2026-02-06T11:31:58.095008/Democracy_Club__07May2026.CSV"
+        "2026-07-07/2026-06-08T15:28:11.723541/Democracy_Club__07July2026.tsv"
     )
-    elections = ["2026-05-07"]
+    elections = ["2026-07-07"]
+    csv_delimiter = "\t"
 
-    def station_record_to_dict(self, record):
-        # UPRN from council for:
-        # Busbridge Village Hall Brighton Road Godalming, GU7 1XA
-        if record.polling_place_id == "7358":
-            record = record._replace(polling_place_uprn="100062609703")
+    # Maintaining exclusions as comments through a by-election
+    # def station_record_to_dict(self, record):
+    #     # UPRN from council for:
+    #     # Busbridge Village Hall Brighton Road Godalming, GU7 1XA
+    #     if record.polling_place_id == "7358":
+    #         record = record._replace(polling_place_uprn="100062609703")
 
-        return super().station_record_to_dict(record)
+    #     return super().station_record_to_dict(record)
 
-    def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
+    # def address_record_to_dict(self, record):
+    #     uprn = record.property_urn.strip().lstrip("0")
 
-        if uprn in [
-            "100061602142",  # 3 FARNBOROUGH ROAD, FARNHAM
-            "10096746074",  # FLAT 1, GAINSBOROUGH HOUSE, 204 HIGH STREET, CRANLEIGH
-            "10096746075",  # FLAT 2, GAINSBOROUGH HOUSE, 204 HIGH STREET, CRANLEIGH
-            "100061616831",  # KENNELS COTTAGE, HASLEMERE ROAD, WITLEY, GODALMING
-        ]:
-            return None
+    #     if uprn in [
+    #         "100061602142",  # 3 FARNBOROUGH ROAD, FARNHAM
+    #         "10096746074",  # FLAT 1, GAINSBOROUGH HOUSE, 204 HIGH STREET, CRANLEIGH
+    #         "10096746075",  # FLAT 2, GAINSBOROUGH HOUSE, 204 HIGH STREET, CRANLEIGH
+    #         "100061616831",  # KENNELS COTTAGE, HASLEMERE ROAD, WITLEY, GODALMING
+    #     ]:
+    #         return None
 
-        if record.addressline6 in [
-            "GU9 0NZ",  # split
-            "RH12 3BQ",  # looks wrong
-        ]:
-            return None
+    #     if record.addressline6 in [
+    #         "GU9 0NZ",  # split
+    #         "RH12 3BQ",  # looks wrong
+    #     ]:
+    #         return None
 
-        return super().address_record_to_dict(record)
+    #     return super().address_record_to_dict(record)
