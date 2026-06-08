01c4cd8d75f7089e940982c3f225cfe7ea4edf78
polling-bot-4000 <developers@democracyclub.org.uk>
2026-06-08 12:46:24 +0100

    Import script for Denbighshire (2026-06-25) (closes #9466)

--- polling_stations/apps/data_importers/management/commands/import_denbighshire.py
+++ polling_stations/apps/data_importers/management/commands/import_denbighshire.py
@@ -3,29 +3,10 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "DEN"
-    addresses_name = (
-        "2026-05-07/2026-02-26T14:09:17.885169/Democracy_Club__07May2026.tsv"
-    )
-    stations_name = (
-        "2026-05-07/2026-02-26T14:09:17.885169/Democracy_Club__07May2026.tsv"
-    )
-    elections = ["2026-05-07"]
-    csv_encoding = "windows-1252"
+    addresses_name = "2026-06-25/2026-06-08T12:46:18.760688/merged.tsv"
+    stations_name = "2026-06-25/2026-06-08T12:46:18.760688/merged.tsv"
+    elections = ["2026-06-25"]
     csv_delimiter = "\t"
-
-    def station_record_to_dict(self, record):
-        # add missing postcode from council for:
-        # Hafan Deg Day Centre Grange Road Rhyl
-        if record.polling_place_id == "13709":
-            record = record._replace(polling_place_postcode="LL18 4BS")
-
-        # Station change from council:
-        # Addresses at: Ystafell Y Foryd / Foryd Room Canolfan Wellington/Wellington Centre Wellington Road Rhyl LL18 1LE
-        # Now go to: Neuadd Tref Y Rhyl / Rhyl Town Hall Ffordd Wellington / Wellington Road Rhyl LL18 1AB
-        if record.polling_place_id == "13732":
-            return None
-
-        return super().station_record_to_dict(record)
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
@@ -42,11 +23,5 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "LL18 3AG",
         ]:
             return None
-
-        # Station change from council:
-        # Addresses at: Ystafell Y Foryd / Foryd Room Canolfan Wellington/Wellington Centre Wellington Road Rhyl LL18 1LE
-        # Now go to: Neuadd Tref Y Rhyl / Rhyl Town Hall Ffordd Wellington / Wellington Road Rhyl LL18 1AB
-        if record.polling_place_id == "13732":
-            record = record._replace(polling_place_id="13735")
 
         return super().address_record_to_dict(record)
