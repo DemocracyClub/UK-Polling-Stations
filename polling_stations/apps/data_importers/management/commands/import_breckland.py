dbef00f97e1047b14de2ebebbd0d48af3aa9fa68
polling-bot-4000 <developers@democracyclub.org.uk>
2026-06-16 10:00:06 +0100

    Import script for Breckland (2026-07-16) (closes #9479)

--- polling_stations/apps/data_importers/management/commands/import_breckland.py
+++ polling_stations/apps/data_importers/management/commands/import_breckland.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "BRE"
     addresses_name = (
-        "2026-05-07/2026-03-27T15:27:47.061067/Democracy_Club__07May2026.tsv"
+        "2026-07-16/2026-06-16T09:59:52.464931/Democracy_Club__16July2026.tsv"
     )
     stations_name = (
-        "2026-05-07/2026-03-27T15:27:47.061067/Democracy_Club__07May2026.tsv"
+        "2026-07-16/2026-06-16T09:59:52.464931/Democracy_Club__16July2026.tsv"
     )
-    elections = ["2026-05-07"]
+    elections = ["2026-07-16"]
     csv_delimiter = "\t"
 
     def address_record_to_dict(self, record):
@@ -20,20 +20,20 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             in [
                 "100091495478",  # M LAMBERT PLANT LTD, MILL POND FARM, THETFORD ROAD, GARBOLDISHAM, DISS
                 "100091497149",  # 60 CROXTON ROAD, THETFORD
+                "10094470114",  # 60C CROXTON ROAD, THETFORD
                 "100091497209",  # CROXTON END, CROXTON ROAD, THETFORD
                 "100091497248",  # ROSEISLE, CROXTON ROAD, THETFORD
                 "10011976226",  # FAITHFIELDS, MAIN ROAD, LITTLE FRANSHAM, DEREHAM
                 "10011976227",  # INDIGO HOUSE, MAIN ROAD, LITTLE FRANSHAM, DEREHAM
                 "10011976228",  # LUNA HOUSE, MAIN ROAD, LITTLE FRANSHAM, DEREHAM
-                "10011976645",  # HAWTHORNS, BOTTLE CORNER, BESTHORPE, ATTLEBOROUGH
                 "10011992496",  # MAY CROFT, MAIN ROAD, LITTLE FRANSHAM, DEREHAM
                 "10011992499",  # FIELDVIEW HOUSE, MAIN ROAD, LITTLE FRANSHAM, DEREHAM
                 "10011992500",  # BRAMBLEWOOD, MAIN ROAD, LITTLE FRANSHAM, DEREHAM
                 "10094466362",  # HEDGEROW, MAIN ROAD, LITTLE FRANSHAM, DEREHAM
                 "10094466363",  # PARTRIDGE, MAIN ROAD, LITTLE FRANSHAM, DEREHAM
                 "10094466365",  # OAKLANDS, MAIN ROAD, LITTLE FRANSHAM, DEREHAM
-                "10094470114",  # 60C CROXTON ROAD, THETFORD
                 "10096178957",  # WRENS NEST, MAIN ROAD, LITTLE FRANSHAM, DEREHAM
+                "10011976645",  # HAWTHORNS, BOTTLE CORNER, BESTHORPE, ATTLEBOROUGH
             ]
         ):
             return None
@@ -53,7 +53,6 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             # suspect
             "PE37 7JP",  # SEA LORD CLOSE, SWAFFHAM
             "NR19 2FJ",  # LITTLE FRANSHAM, DEREHAM
-            "IP24 2FR",  # DRYFIELD CLOSE, THETFORD
         ]:
             return None
 
@@ -62,15 +61,15 @@ class Command(BaseXpressDemocracyClubCsvImporter):
     def station_record_to_dict(self, record):
         # bugreport #627
         # coords change for: Dereham Town Football Club, Aldiss Park, Norwich Road, Dereham, NR20 3PX
-        if record.polling_place_id == "15539":
+        if record.polling_place_id == "17083":
             record = record._replace(
                 polling_place_easting="601036", polling_place_northing="313508"
             )
 
         if record.polling_place_id in [
-            "15784",  # Rocklands Village Hall The Street Rocklands
-            "15900",  # Ovington Village Hall Church Road Ovington
-            "15772",  # Oxborough Village Hall Swaffham Road Oxborough
+            "16814",  # Rocklands Village Hall The Street Rocklands
+            "16951",  # Ovington Village Hall Church Road Ovington
+            "17079",  # Oxborough Village Hall Swaffham Road Oxborough
         ]:
             # The E/N points are the wrong way round for these 3, perform the ol' switcheroo
             easting = record.polling_place_northing
@@ -81,17 +80,9 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         # Remove suspect coords for two stations at:
         # Thetford Church on the Way, Churchill Road, Thetford, IP24 2JZ
         if record.polling_place_id in [
-            "15751",
-            "15817",
+            "16982",
+            "17091",
         ]:
-            record = record._replace(
-                polling_place_easting="0",
-                polling_place_northing="0",
-            )
-        # bugreport # 778
-        # remove suspect coords for:
-        # Arts Centre Hall at Aurora Eccles School, Quidenham Road, Quidenham, NR16 2NZ
-        if record.polling_place_id == "15618":
             record = record._replace(
                 polling_place_easting="0",
                 polling_place_northing="0",
@@ -100,7 +91,7 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         # bugreport # 784
         # remove suspect coords for:
         # Attleborough Baptist Church Leys Lane Attleborough
-        if record.polling_place_id == "15644":
+        if record.polling_place_id == "17053":
             record = record._replace(
                 polling_place_easting="0",
                 polling_place_northing="0",
@@ -109,7 +100,7 @@ class Command(BaseXpressDemocracyClubCsvImporter):
         # https://app.asana.com/1/1204880536137786/project/1207538772343223/task/1214597478265162?focus=true
         # remove suspect coords for:
         # St Andrews Church, Kilverstone Road, Brettenham IP24 2RP
-        if record.polling_place_id == "15792":
+        if record.polling_place_id == "16848":
             record = record._replace(
                 polling_place_easting="0",
                 polling_place_northing="0",
