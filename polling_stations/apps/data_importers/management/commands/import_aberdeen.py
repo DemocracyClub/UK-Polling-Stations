78d6d3e6ba1807f2ce726f36c9957775da8edc4c
polling-bot-4000 <developers@democracyclub.org.uk>
2026-06-19 13:19:17 +0100

    Import script for Aberdeen (2026-06-25) (closes #9482)

--- polling_stations/apps/data_importers/management/commands/import_aberdeen.py
+++ polling_stations/apps/data_importers/management/commands/import_aberdeen.py
@@ -3,14 +3,6 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "ABE"
-    addresses_name = "2026-06-18/2026-05-28T12:59:29.293540/Aberdeen South_Democracy Club - Idox_2026-05-26 10-12 (1).csv"
-    stations_name = "2026-06-18/2026-05-28T12:59:29.293540/Aberdeen South_Democracy Club - Idox_2026-05-26 10-12 (1).csv"
-    elections = ["2026-06-18"]
-
-    def address_record_to_dict(self, record):
-        if record.postcode in [
-            "AB13 0HQ",  # split
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
+    addresses_name = "2026-06-25/2026-06-19T13:19:06.633385/Democracy Club - Idox_2026-05-14 17-14 (1).csv"
+    stations_name = "2026-06-25/2026-06-19T13:19:06.633385/Democracy Club - Idox_2026-05-14 17-14 (1).csv"
+    elections = ["2026-06-25"]
