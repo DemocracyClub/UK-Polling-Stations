c37165947e6816bf38d2e4fb2a65eef813c6e488
polling-bot-4000 <developers@democracyclub.org.uk>
2026-06-25 10:28:53 +0100

    Import script for Manchester (2026-07-30) (closes #9504)

--- polling_stations/apps/data_importers/management/commands/import_manchester.py
+++ polling_stations/apps/data_importers/management/commands/import_manchester.py
@@ -4,12 +4,12 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "MAN"
     addresses_name = (
-        "2026-05-07/2026-03-30T09:35:47.578722/Democracy_Club__07May2026.tsv"
+        "2026-07-30/2026-06-25T10:28:40.917111/Democracy_Club__30July2026.tsv"
     )
     stations_name = (
-        "2026-05-07/2026-03-30T09:35:47.578722/Democracy_Club__07May2026.tsv"
+        "2026-07-30/2026-06-25T10:28:40.917111/Democracy_Club__30July2026.tsv"
     )
-    elections = ["2026-05-07"]
+    elections = ["2026-07-30"]
     csv_encoding = "windows-1252"
     csv_delimiter = "\t"
 
@@ -23,6 +23,7 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "10024146870",  # SECOND FLOOR FLAT 76 RICHMOND GROVE, MANCHESTER
             "10024146868",  # GROUND FLOOR FLAT 76 RICHMOND GROVE, MANCHESTER
             "10024146869",  # FIRST FLOOR FLAT 76 RICHMOND GROVE, MANCHESTER
+            "10095846092",  # 276B YEW TREE ROAD, MANCHESTER
         ]:
             return None
 
@@ -37,26 +38,8 @@ class Command(BaseXpressDemocracyClubCsvImporter):
             "M19 3NW",
             "M22 5BL",
             "M8 4RB",
+            "M22 5BL",
         ]:
             return None
 
         return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # change requested by council
-        # Old station: Sandilands Primary School, Infant Department, Wendover Road, Brooklands, Manchester, M23 9JX
-        # New station (already in db): Church of the Nazarene, Wendover Road, Brooklands, Manchester, M23 9FN
-        if record.polling_place_id == "15420":
-            record = record._replace(
-                polling_place_name="Church of the Nazarene",
-                polling_place_address_1="Wendover Road",
-                polling_place_address_2="",
-                polling_place_address_3="Brooklands",
-                polling_place_address_4="Manchester",
-                polling_place_postcode="M23 9FN",
-                polling_place_uprn="",
-                polling_place_easting="379820",
-                polling_place_northing="389788",
-            )
-
-        return super().station_record_to_dict(record)
