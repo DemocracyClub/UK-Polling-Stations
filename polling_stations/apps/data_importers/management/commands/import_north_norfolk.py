80616430ad86f66414a0272ee3776b4723013757
polling-bot-4000 <developers@democracyclub.org.uk>
2026-07-01 10:00:52 +0100

    Import script for North Norfolk (2026-07-16) (closes #9510)

--- polling_stations/apps/data_importers/management/commands/import_north_norfolk.py
+++ polling_stations/apps/data_importers/management/commands/import_north_norfolk.py
@@ -5,13 +5,12 @@ from django.contrib.gis.geos import Point
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "NNO"
     addresses_name = (
-        "2026-05-07/2026-03-17T09:14:33.369588/NNDC Democracy_Club__07May2026.tsv"
+        "2026-07-16/2026-07-01T10:00:40.016239/Democracy_Club__16July2026.CSV"
     )
     stations_name = (
-        "2026-05-07/2026-03-17T09:14:33.369588/NNDC Democracy_Club__07May2026.tsv"
+        "2026-07-16/2026-07-01T10:00:40.016239/Democracy_Club__16July2026.CSV"
     )
-    elections = ["2026-05-07"]
-    csv_delimiter = "\t"
+    elections = ["2026-07-16"]
 
     def address_record_to_dict(self, record):
         uprn = record.property_urn.strip().lstrip("0")
@@ -47,17 +46,17 @@ class Command(BaseXpressDemocracyClubCsvImporter):
     def station_record_to_dict(self, record):
         # Walcott Village Hall, Coast Road, Walcott, NR12 ONG
         # Misspell postcode, capital letter "O" should be a number "0"
-        if record.polling_place_id == "26033":
+        if record.polling_place_id == "27240":
             record = record._replace(polling_place_postcode="NR12 0NG")
 
         # St Benet Hall, St Nicholas Church Halls Complex, Vicarage Street, North Walsham, NR28 9BQ
-        if record.polling_place_id == "26363":
+        if record.polling_place_id == "27586":
             record = record._replace(polling_place_postcode="NR28 9BT")
 
         rec = super().station_record_to_dict(record)
 
         # Walsingham Parish Hall, 14 High Street, Walsingham, NR22 6AA
-        if rec["internal_council_id"] == "26534":
+        if rec["internal_council_id"] == "27602":
             rec["location"] = Point(0.873339, 52.892674, srid=4326)
 
         return rec
