896916816d332c77a6ea2110eb5fae45473f333c
polling-bot-4000 <developers@democracyclub.org.uk>
2026-07-21 10:49:54 +0100

    Import script for Wandsworth (2026-08-27) (closes #9532)

--- polling_stations/apps/data_importers/management/commands/import_wandsworth.py
+++ polling_stations/apps/data_importers/management/commands/import_wandsworth.py
@@ -4,27 +4,28 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "WND"
     addresses_name = (
-        "2026-05-07/2026-03-05T16:23:21.565444/Democracy_Club__07May2026 (2).tsv"
+        "2026-08-27/2026-07-21T10:49:48.380732/Democracy_Club__27August2026.tsv"
     )
     stations_name = (
-        "2026-05-07/2026-03-05T16:23:21.565444/Democracy_Club__07May2026 (2).tsv"
+        "2026-08-27/2026-07-21T10:49:48.380732/Democracy_Club__27August2026.tsv"
     )
-    elections = ["2026-05-07"]
+    elections = ["2026-08-27"]
     csv_delimiter = "\t"
 
-    def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
+    # maintaining tweaks through by-election
+    # def address_record_to_dict(self, record):
+    #     uprn = record.property_urn.strip().lstrip("0")
 
-        if uprn in [
-            "100022701958",  # 5 THURLEIGH MANSIONS 33 THURLEIGH ROAD, LONDON
-            "100023328601",  # THE COTTAGE, MAGDALEN ROAD, LONDON
-        ]:
-            return None
+    #     if uprn in [
+    #         "100022701958",  # 5 THURLEIGH MANSIONS 33 THURLEIGH ROAD, LONDON
+    #         "100023328601",  # THE COTTAGE, MAGDALEN ROAD, LONDON
+    #     ]:
+    #         return None
 
-        if record.addressline6 in [
-            # split
-            "SW12 9RF",
-        ]:
-            return None
+    #     if record.addressline6 in [
+    #         # split
+    #         "SW12 9RF",
+    #     ]:
+    #         return None
 
-        return super().address_record_to_dict(record)
+    #     return super().address_record_to_dict(record)
