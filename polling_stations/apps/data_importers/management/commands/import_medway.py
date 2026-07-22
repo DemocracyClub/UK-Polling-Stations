bb12a8dfcdcae4e44d501809207848cc7cd9285d
polling-bot-4000 <developers@democracyclub.org.uk>
2026-07-22 16:04:21 +0100

    Import script for Medway (2026-07-23) (closes #9537)

--- polling_stations/apps/data_importers/management/commands/import_medway.py
+++ polling_stations/apps/data_importers/management/commands/import_medway.py
@@ -3,6 +3,6 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "MDW"
-    addresses_name = "2026-07-23/2026-07-22T10:35:15.560930/Democracy Club - Idox_2026-07-22 09-13.csv"
-    stations_name = "2026-07-23/2026-07-22T10:35:15.560930/Democracy Club - Idox_2026-07-22 09-13.csv"
+    addresses_name = "2026-07-23/2026-07-22T16:04:15.115707/Democracy Club - Idox_2026-07-22 09-13.csv"
+    stations_name = "2026-07-23/2026-07-22T16:04:15.115707/Democracy Club - Idox_2026-07-22 09-13.csv"
     elections = ["2026-07-23"]
