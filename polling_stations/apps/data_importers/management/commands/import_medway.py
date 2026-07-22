b71eaf06b2faf7a79fc75d534dd6a04632c88aa4
polling-bot-4000 <developers@democracyclub.org.uk>
2026-07-22 10:35:21 +0100

    Import script for Medway (2026-07-23) (closes #9535)

--- polling_stations/apps/data_importers/management/commands/import_medway.py
+++ polling_stations/apps/data_importers/management/commands/import_medway.py
@@ -3,31 +3,6 @@ from data_importers.management.commands import BaseHalaroseCsvImporter
 
 class Command(BaseHalaroseCsvImporter):
     council_id = "MDW"
-    addresses_name = "2024-07-04/2024-06-05T18:33:15.401473/Eros_SQL_Output012.csv"
-    stations_name = "2024-07-04/2024-06-05T18:33:15.401473/Eros_SQL_Output012.csv"
-    elections = ["2024-07-04"]
-
-    def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
-
-        if uprn in [
-            "100062394561",  # WESTBANK FARM, CAPSTONE ROAD, GILLINGHAM
-        ]:
-            return None
-
-        if record.housepostcode in [
-            # split
-            "ME8 8DB",
-            # suspect
-            "ME1 2EZ",
-            "ME1 2FD",
-        ]:
-            return None
-        return super().address_record_to_dict(record)
-
-    def station_record_to_dict(self, record):
-        # postcode correction for: Main Hall, Gillingham Baptist Church, Green Street, MR7 5TJ
-        if self.get_station_hash(record) == "27-main-hall-gillingham-baptist-church":
-            record = record._replace(pollingstationpostcode="ME7 5TJ")
-
-        return super().station_record_to_dict(record)
+    addresses_name = "2026-07-23/2026-07-22T10:35:15.560930/Democracy Club - Idox_2026-07-22 09-13.csv"
+    stations_name = "2026-07-23/2026-07-22T10:35:15.560930/Democracy Club - Idox_2026-07-22 09-13.csv"
+    elections = ["2026-07-23"]
