c2a3d444a3a6b6f90dfa47cf8c9d25820305eff0
polling-bot-4000 <developers@democracyclub.org.uk>
2026-07-24 09:33:40 +0100

    Import script for Tendring (2026-08-13) (closes #9545)

--- polling_stations/apps/data_importers/management/commands/import_tendring.py
+++ polling_stations/apps/data_importers/management/commands/import_tendring.py
@@ -3,17 +3,19 @@ from data_importers.management.commands import BaseDemocracyCountsCsvImporter
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "TEN"
-    addresses_name = "2026-05-07/2026-03-19T10:47:08.499497/Democracy Club - Polling Districts 2026.csv"
-    stations_name = "2026-05-07/2026-03-19T10:47:08.499497/Democracy Club - Polling Stations 2026.csv"
-    elections = ["2026-05-07"]
+    addresses_name = (
+        "2026-08-13/2026-07-28T14:10:29.318851/TEN - DC - Polling District CBE.csv"
+    )
+    stations_name = (
+        "2026-08-13/2026-07-28T14:10:29.318851/TEN - DC Polling Staions UKPBE.csv"
+    )
+    elections = ["2026-08-13"]
     csv_encoding = "utf-16le"
 
     def address_record_to_dict(self, record):
         if (
             record.uprn
             in [
-                "10007943316",  # 48 NEW HALL LODGE PARK LOW ROAD, DOVERCOURT
-                "100090634997",  # 366A MAIN ROAD, HARWICH
                 "10096724996",  # SWEET PEA, MILL LANE, THORPE-LE-SOKEN, CLACTON-ON-SEA
                 "100091268553",  # 163 MEADOW VIEW PARK, ST. OSYTH ROAD, LITTLE CLACTON, CLACTON-ON-SEA
             ]
@@ -21,8 +23,6 @@ class Command(BaseDemocracyCountsCsvImporter):
             return None
 
         if record.postcode in [
-            # split
-            "CO13 0FZ",
             # suspect
             "CO15 1HX",
         ]:
