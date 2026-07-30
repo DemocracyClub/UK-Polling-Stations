e09de64fe9dfc884ca2847ff5d4aeb902de8a6ae
polling-bot-4000 <developers@democracyclub.org.uk>
2026-07-30 10:33:57 +0100

    TEST/Import script for Wandsworth (2026-05-06) (closes #9557)

--- polling_stations/apps/data_importers/management/commands/import_wandsworth.py
+++ polling_stations/apps/data_importers/management/commands/import_wandsworth.py
@@ -1,31 +1,8 @@
-from data_importers.management.commands import BaseXpressDemocracyClubCsvImporter
+from data_importers.management.commands import BaseFcsDemocracyClubImporter
 
 
-class Command(BaseXpressDemocracyClubCsvImporter):
+class Command(BaseFcsDemocracyClubImporter):
     council_id = "WND"
-    addresses_name = (
-        "2026-08-27/2026-07-21T10:49:48.380732/Democracy_Club__27August2026.tsv"
-    )
-    stations_name = (
-        "2026-08-27/2026-07-21T10:49:48.380732/Democracy_Club__27August2026.tsv"
-    )
-    elections = ["2026-08-27"]
-    csv_delimiter = "\t"
-
-    # maintaining tweaks through by-election
-    # def address_record_to_dict(self, record):
-    #     uprn = record.property_urn.strip().lstrip("0")
-
-    #     if uprn in [
-    #         "100022701958",  # 5 THURLEIGH MANSIONS 33 THURLEIGH ROAD, LONDON
-    #         "100023328601",  # THE COTTAGE, MAGDALEN ROAD, LONDON
-    #     ]:
-    #         return None
-
-    #     if record.addressline6 in [
-    #         # split
-    #         "SW12 9RF",
-    #     ]:
-    #         return None
-
-    #     return super().address_record_to_dict(record)
+    addresses_name = "2026-05-06/2026-07-30T10:33:42.995231/snapshot.json"
+    stations_name = "2026-05-06/2026-07-30T10:33:42.995231/snapshot.json"
+    elections = ["2026-05-06"]
