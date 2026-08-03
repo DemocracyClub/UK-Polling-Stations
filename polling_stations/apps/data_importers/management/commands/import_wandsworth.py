9dd662977e8c2033bcb7ae8f0b270fbff99e0b51
polling-bot-4000 <developers@democracyclub.org.uk>
2026-08-03 14:19:31 +0100

    TEST/Import script for Wandsworth (2026-05-06) (closes #9564)

--- polling_stations/apps/data_importers/management/commands/import_wandsworth.py
+++ polling_stations/apps/data_importers/management/commands/import_wandsworth.py
@@ -3,6 +3,6 @@ from data_importers.management.commands import BaseFcsDemocracyClubImporter
 
 class Command(BaseFcsDemocracyClubImporter):
     council_id = "WND"
-    addresses_name = "2026-05-06/2026-07-30T10:33:42.995231/snapshot.json"
-    stations_name = "2026-05-06/2026-07-30T10:33:42.995231/snapshot.json"
+    addresses_name = "2026-05-06/2026-08-03T14:19:15.826594/snapshot.json"
+    stations_name = "2026-05-06/2026-08-03T14:19:15.826594/snapshot.json"
     elections = ["2026-05-06"]
