37ff7b39de70ac9efa04a3dd0a40e25625e59ee6
polling-bot-4000 <developers@democracyclub.org.uk>
2026-09-02 17:24:26 +0100

    Import script for Lambeth (2026-10-08) (closes #9583)

--- polling_stations/apps/data_importers/management/commands/import_lambeth.py
+++ polling_stations/apps/data_importers/management/commands/import_lambeth.py
@@ -1,24 +1,8 @@
-from data_importers.management.commands import BaseFcsDemocracyClubApiImporter
+from data_importers.management.commands import BaseFcsDemocracyClubImporter
 
 
-class Command(BaseFcsDemocracyClubApiImporter):
+class Command(BaseFcsDemocracyClubImporter):
     council_id = "LBH"
-    elections = ["2026-07-09"]
-    fcs_election_id = 54
-
-    def station_record_to_dict(self, record):
-        """
-        station_id = record.get(self.station_id_field)
-
-        # add point for: Weir Link Community Centre, 33 Weir Road, London, SW12 0NU
-        if station_id == 11449:
-            record["longitude"] = -0.139770
-            record["latitude"] = 51.446899
-
-        # add point for: Elmgreen Secondary School, Elmcourt Road, London, SE27 9BZ
-        if station_id == 11311:
-            record["longitude"] = -0.102054
-            record["latitude"] = 51.438839
-        """
-
-        return super().station_record_to_dict(record)
+    addresses_name = "2026-10-08/2026-09-02T17:24:18.811795/snapshot.json"
+    stations_name = "2026-10-08/2026-09-02T17:24:18.811795/snapshot.json"
+    elections = ["2026-10-08"]
