c69fc3aa35497168eb1b1bcad8129a494e713539
polling-bot-4000 <developers@democracyclub.org.uk>
2026-09-16 10:29:02 +0100

    Import script for South Kesteven (2026-10-22) (closes #9587)

--- polling_stations/apps/data_importers/management/commands/import_south_kesteven.py
+++ polling_stations/apps/data_importers/management/commands/import_south_kesteven.py
@@ -4,31 +4,33 @@ from data_importers.management.commands import BaseXpressDemocracyClubCsvImporte
 class Command(BaseXpressDemocracyClubCsvImporter):
     council_id = "SKE"
     addresses_name = (
-        "2025-12-11/2025-11-19T16:20:29.085210/Democracy_Club__11December2025.tsv"
+        "2026-10-22/2026-09-16T10:28:56.301466/Democracy_Club__22October2026.tsv"
     )
     stations_name = (
-        "2025-12-11/2025-11-19T16:20:29.085210/Democracy_Club__11December2025.tsv"
+        "2026-10-22/2026-09-16T10:28:56.301466/Democracy_Club__22October2026.tsv"
     )
-    elections = ["2025-12-11"]
+    elections = ["2026-10-22"]
     csv_delimiter = "\t"
 
-    def address_record_to_dict(self, record):
-        uprn = record.property_urn.strip().lstrip("0")
+    # preserving tweaks through by-election
 
-        if uprn in [
-            "10007248725",  # KLONDYKE FARM BUNGALOW HARVEY CLOSE, BOURNE
-        ]:
-            return None
+    # def address_record_to_dict(self, record):
+    #     uprn = record.property_urn.strip().lstrip("0")
 
-        if record.addressline6 in [
-            # split
-            "NG32 1AT",
-            "PE10 9RP",
-            "NG33 4JQ",
-            # suspect
-            "NG31 8NH",  # MANTHORPE, GRANTHAM
-            "PE10 9NG",  # BURGHLEY STREET, BOURNE
-        ]:
-            return None
+    #     if uprn in [
+    #         "10007248725",  # KLONDYKE FARM BUNGALOW HARVEY CLOSE, BOURNE
+    #     ]:
+    #         return None
 
-        return super().address_record_to_dict(record)
+    #     if record.addressline6 in [
+    #         # split
+    #         "NG32 1AT",
+    #         "PE10 9RP",
+    #         "NG33 4JQ",
+    #         # suspect
+    #         "NG31 8NH",  # MANTHORPE, GRANTHAM
+    #         "PE10 9NG",  # BURGHLEY STREET, BOURNE
+    #     ]:
+    #         return None
+
+    #     return super().address_record_to_dict(record)
