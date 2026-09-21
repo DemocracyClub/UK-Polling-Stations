70c8259fce1d98bdf75f9b1b758ed5593e89e231
polling-bot-4000 <developers@democracyclub.org.uk>
2026-09-21 12:03:00 +0100

    Import script for Camden (2026-10-08) (closes #9589)

--- polling_stations/apps/data_importers/management/commands/import_camden.py
+++ polling_stations/apps/data_importers/management/commands/import_camden.py
@@ -4,40 +4,13 @@ from django.contrib.gis.geos import Point
 
 class Command(BaseDemocracyCountsCsvImporter):
     council_id = "CMD"
-    addresses_name = "2026-05-07/2026-03-09T12:26:15.372546/Democracy Club - Polling Districts_CAMDEN.csv"
-    stations_name = "2026-05-07/2026-03-09T12:26:15.372546/Democracy Club - Polling Stations_CAMDEN.csv"
-    elections = ["2026-05-07"]
+    addresses_name = "2026-10-08/2026-09-21T12:02:48.533067/Polling Districts.csv"
+    stations_name = "2026-10-08/2026-09-21T12:02:48.533067/Polling Stations.csv"
+    elections = ["2026-10-08"]
     csv_encoding = "utf-16le"
 
-    def address_record_to_dict(self, record):
-        uprn = record.uprn.strip().lstrip("0")
-
-        if uprn in [
-            "5193668",  # 21 MILL LANE, LONDON
-        ]:
-            return None
-
-        if record.postcode in [
-            # looks wrong
-            "NW6 5UA",
-        ]:
-            return None
-
-        return super().address_record_to_dict(record)
-
     def station_record_to_dict(self, record):
-        # moving point closer to road for: GOSPEL OAK METHODIST CHURCH, Agincourt Road, London
         rec = super().station_record_to_dict(record)
-
-        if rec["internal_council_id"] in (
-            "IA/1",
-            "IA/2",
-        ):
-            rec["location"] = Point(527726, 185479, srid=27700)
-
-        # correct point for: ABBEY COMMUNITY CENTRE 172 Belsize Road London
-        if rec["internal_council_id"] == "FB":
-            rec["location"] = Point(525888, 183945, srid=27700)
 
         # fix data for 2 stations under same address:
         # N1C CENTRE, Plimsoll Building, 1 Handyside Street, London, NIC 4BQ
